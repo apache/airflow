5fed3d28b01fdbc2f4101ea1300174efd6f5edf8
Bolke de Bruin <bolke@xs4all.nl>
2015-10-22 10:58:20 +0200

    testing

--- scripts/ci/ldap.sh
+++ scripts/ci/ldap.sh
@@ -9,6 +9,8 @@ rm -rf ${LDAP_DB} && mkdir ${LDAP_DB} && cp  /usr/share/doc/slapd/examples/DB_CO
 
 echo "Launching OpenLDAP ..."
 
+netstat -an | grep 3890
+
 # Start slapd with non root privileges
 slapd -h "ldap://0.0.0.0:3890/" -f ${DIR}/slapd.conf
 
