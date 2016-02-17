7906160a03fc862fee9c0412aba1761f8583f7d1
Siddharth Anand <sanand@agari.com>
2016-02-17 02:46:58 +0000

    Fixing the DAGRUN_TIMEOUT check

--- airflow/jobs.py
+++ airflow/jobs.py
@@ -370,8 +370,8 @@ class SchedulerJob(BaseJob):
                 return
             for dr in active_runs:
                 if (
-                        dr.start_date and dag.dagrun_timeout and
-                        dr.start_date < datetime.now() - dag.dagrun_timeout):
+                        dr.execution_date and dag.dagrun_timeout and
+                        dr.execution_date < datetime.now() - dag.dagrun_timeout):
                     dr.state = State.FAILED
                     dr.end_date = datetime.now()
             session.commit()
