062053043bfcb4738e82c15c95697931b09e2f0d
Maxime Beauchemin <maximebeauchemin@gmail.com>
2016-10-04 08:27:19 -0700

    Moving python-daemon to setup_requires

--- setup.py
+++ setup.py
@@ -181,6 +181,7 @@ def do_setup():
         scripts=['airflow/bin/airflow'],
         setup_requires=[
             'numpy==1.11.0',
+            'python-daemon==2.1.1',
         ],
         install_requires=[
             'alembic==0.8.8',
@@ -201,7 +202,6 @@ def do_setup():
             'pandas==0.18.1',
             'psutil==4.3.1',
             'pygments==2.1.3',
-            'python-daemon==2.1.1',
             'python-dateutil==2.5.3',
             'python-nvd3==0.14.2',
             'requests==2.11.1',
