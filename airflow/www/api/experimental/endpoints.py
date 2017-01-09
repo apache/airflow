owlabs/incubator-airflow

[AIRFLOW-489] Allow specifying execution date in trigger_dag API

airflow/www/api/experimental/endpoints.py
@@ -22,17 +22,22 @@ from airflow.www.app import csrf
 from flask import (
     g, Markup, Blueprint, redirect, jsonify, abort, request, current_app, send_file
 )
+from datetime import datetime
+
+_log = logging.getLogger(__name__)
 
 requires_authentication = airflow.api.api_auth.requires_authentication
 
 api_experimental = Blueprint('api_experimental', __name__)
+
 
 @csrf.exempt
 @api_experimental.route('/dags/<string:dag_id>/dag_runs', methods=['POST'])
 @requires_authentication
 def trigger_dag(dag_id):
     """
-    Trigger a new dag run for a Dag
+    Trigger a new dag run for a Dag with an execution date of now unless
+    specified in the data.
     """
     data = request.get_json(force=True)
 
@@ -44,16 +49,35 @@ def trigger_dag(dag_id):
     if 'conf' in data:
         conf = data['conf']
 
+    execution_date = None
+    if 'exec_date' in data:
+        execution_date = data['exec_date']
+
+        # Convert string datetime into actual datetime
+        try:
+            execution_date = datetime.strptime(execution_date,
+                                               '%Y-%m-%dT%H:%M:%S')
+        except ValueError:
+            error_message = (
+                'Given execution date, {}, could not be identified '
+                'as a date. Example date format: 2015-11-16T14:34:15'
+                .format(execution_date))
+            _log.info(error_message)
+            response = jsonify({'error': error_message})
+            response.status_code = 400
+
+            return response
+
     try:
-        dr = trigger.trigger_dag(dag_id, run_id, conf)
+        dr = trigger.trigger_dag(dag_id, run_id, conf, execution_date)
     except AirflowException as err:
-        logging.error(err)
+        _log.error(err)
         response = jsonify(error="{}".format(err))
         response.status_code = 404
         return response
 
     if getattr(g, 'user', None):
-        logging.info("User {} created {}".format(g.user, dr))
+        _log.info("User {} created {}".format(g.user, dr))
 
     response = jsonify(message="Created {}".format(dr))
     return response

tests/www/api/experimental/test_endpoints.py
@@ -14,6 +14,7 @@
 import unittest
 
 from datetime import datetime
+from airflow.models import DagBag
 
 import json
 
@@ -59,4 +60,41 @@ class ApiExperimentalTests(unittest.TestCase):
         )
         self.assertEqual(404, response.status_code)
 
+    def test_trigger_dag_for_date(self):
+        url_template = '/api/experimental/dags/{}/dag_runs'
+        dag_id = 'example_bash_operator'
+        now = datetime.now()
+        execution_date = datetime(now.year, now.month, now.day, now.hour + 1)
+        datetime_string = execution_date.isoformat()
+
+        # Test Correct execution
+        response = self.app.post(
+            url_template.format(dag_id),
+            data=json.dumps(dict(exec_date=execution_date.isoformat())),
+            content_type="application/json"
+        )
+        self.assertEqual(200, response.status_code)
+
+        dagbag = DagBag()
+        dag = dagbag.get_dag(dag_id)
+        dag_run = dag.get_dagrun(execution_date)
+        self.assertTrue(dag_run,
+                        'Dag Run not found for execution date {}'
+                        .format(execution_date))
+
+        # Test error for nonexistent dag
+        response = self.app.post(
+            url_template.format('does_not_exist_dag'),
+            data=json.dumps(dict(exec_date=execution_date.isoformat())),
+            content_type="application/json"
+        )
+        self.assertEqual(404, response.status_code)
+
+        # Test error for bad datetime format
+        response = self.app.post(
+            url_template.format(dag_id),
+            data=json.dumps(dict(exec_date='not_a_datetime')),
+            content_type="application/json"
+        )
+        self.assertEqual(400, response.status_code)
 

apache/airflow

airflow/www/api/experimental/endpoints.py
@@ -22,17 +22,22 @@ from airflow.www.app import csrf
 from flask import (
     g, Markup, Blueprint, redirect, jsonify, abort, request, current_app, send_file
 )
+from datetime import datetime
+
+_log = logging.getLogger(__name__)
 
 requires_authentication = airflow.api.api_auth.requires_authentication
 
 api_experimental = Blueprint('api_experimental', __name__)
+
 
 @csrf.exempt
 @api_experimental.route('/dags/<string:dag_id>/dag_runs', methods=['POST'])
 @requires_authentication
 def trigger_dag(dag_id):
     """
-    Trigger a new dag run for a Dag
+    Trigger a new dag run for a Dag with an execution date of now unless
+    specified in the data.
     """
     data = request.get_json(force=True)
 
@@ -44,16 +49,35 @@ def trigger_dag(dag_id):
     if 'conf' in data:
         conf = data['conf']
 
+    execution_date = None
+    if 'execution_date' in data:
+        execution_date = data['execution_date']
+
+        # Convert string datetime into actual datetime
+        try:
+            execution_date = datetime.strptime(execution_date,
+                                               '%Y-%m-%dT%H:%M:%S')
+        except ValueError:
+            error_message = (
+                'Given execution date, {}, could not be identified '
+                'as a date. Example date format: 2015-11-16T14:34:15'
+                .format(execution_date))
+            _log.info(error_message)
+            response = jsonify({'error': error_message})
+            response.status_code = 400
+
+            return response
+
     try:
-        dr = trigger.trigger_dag(dag_id, run_id, conf)
+        dr = trigger.trigger_dag(dag_id, run_id, conf, execution_date)
     except AirflowException as err:
-        logging.error(err)
+        _log.error(err)
         response = jsonify(error="{}".format(err))
         response.status_code = 404
         return response
 
     if getattr(g, 'user', None):
-        logging.info("User {} created {}".format(g.user, dr))
+        _log.info("User {} created {}".format(g.user, dr))
 
     response = jsonify(message="Created {}".format(dr))
     return response

tests/www/api/experimental/test_endpoints.py
@@ -14,6 +14,7 @@
 import unittest
 
 from datetime import datetime
+from airflow.models import DagBag
 
 import json
 
@@ -59,4 +60,41 @@ class ApiExperimentalTests(unittest.TestCase):
         )
         self.assertEqual(404, response.status_code)
 
+    def test_trigger_dag_for_date(self):
+        url_template = '/api/experimental/dags/{}/dag_runs'
+        dag_id = 'example_bash_operator'
+        now = datetime.now()
+        execution_date = datetime(now.year, now.month, now.day, now.hour + 1)
+        datetime_string = execution_date.isoformat()
+
+        # Test Correct execution
+        response = self.app.post(
+            url_template.format(dag_id),
+            data=json.dumps(dict(execution_date=execution_date.isoformat())),
+            content_type="application/json"
+        )
+        self.assertEqual(200, response.status_code)
+
+        dagbag = DagBag()
+        dag = dagbag.get_dag(dag_id)
+        dag_run = dag.get_dagrun(execution_date)
+        self.assertTrue(dag_run,
+                        'Dag Run not found for execution date {}'
+                        .format(execution_date))
+
+        # Test error for nonexistent dag
+        response = self.app.post(
+            url_template.format('does_not_exist_dag'),
+            data=json.dumps(dict(execution_date=execution_date.isoformat())),
+            content_type="application/json"
+        )
+        self.assertEqual(404, response.status_code)
+
+        # Test error for bad datetime format
+        response = self.app.post(
+            url_template.format(dag_id),
+            data=json.dumps(dict(execution_date='not_a_datetime')),
+            content_type="application/json"
+        )
+        self.assertEqual(400, response.status_code)
 