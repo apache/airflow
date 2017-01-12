6faf39d5226692b47fbe35b555a4d77e83e32fba
Michael A. Gonzalez <Gonzalez.Michael.A.1987@gmail.com>
2017-01-12 11:45:27 -0500

    Removed str to pass CI

--- airflow/hooks/jdbc_hook.py
+++ airflow/hooks/jdbc_hook.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from builtins import str
-
 import jaydebeapi
 
 from airflow.hooks.dbapi_hook import DbApiHook
