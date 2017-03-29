2106ff57056d08436d8aab87ac5601d9f554935a
Chris Riccomini <criccomini@apache.org>
2017-03-29 14:09:56 -0700

    [AIRFLOW-1017] get_task_instance shouldn't throw exception when no TI
    get_task_instance should return None instead of
    throwing exception in the case where dagrun does not have the task
    instance.
    
    Closes #2178 from aoen/ddavydov--
    one_instead_of_first_for_dagrun
    
    (cherry picked from commit b2b9587cca9195229ab107394ad94b7702c70e37)

--- airflow/models.py
+++ airflow/models.py
@@ -3971,7 +3971,7 @@ class DagRun(Base):
             TI.dag_id == self.dag_id,
             TI.execution_date == self.execution_date,
             TI.task_id == task_id
-        ).one()
+        ).first()
 
         return ti
 
