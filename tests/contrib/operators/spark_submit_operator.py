34f072a1716350ec39464d73282127d08b83582c
Stephan Werges <swerges@accertify.com>
2017-04-25 11:28:31 +0200

    [AIRFLOW-1089] Add Spark application arguments
    Allows arguments to be passed to the Spark
    application being
    submitted. For example:
    
    - spark-submit --class foo.Bar foobar.jar arg1
    arg2
    - spark-submit app.py arg1 arg2
    
    Closes #2229 from camshrun/sparkSubmitAppArgs

--- tests/contrib/operators/spark_submit_operator.py
+++ tests/contrib/operators/spark_submit_operator.py
@@ -39,7 +39,11 @@ class TestSparkSubmitOperator(unittest.TestCase):
         'verbose': True,
         'application': 'test_application.py',
         'driver_memory': '3g',
-        'java_class': 'com.foo.bar.AppMain'
+        'java_class': 'com.foo.bar.AppMain',
+        'application_args': [
+            '-f foo',
+            '--bar bar'
+        ]
     }
 
     def setUp(self):
@@ -73,6 +77,7 @@ class TestSparkSubmitOperator(unittest.TestCase):
         self.assertEqual(self._config['verbose'], operator._verbose)
         self.assertEqual(self._config['java_class'], operator._java_class)
         self.assertEqual(self._config['driver_memory'], operator._driver_memory)
+        self.assertEqual(self._config['application_args'], operator._application_args)
 
 
 
