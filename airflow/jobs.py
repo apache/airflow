4e60701285177206e84e2b6bd21c7796935e3c91
Lulu Cheng <l1990790120@gmail.com>
2017-07-26 10:49:49 -0400

    fix so that domain and host name would match

--- airflow/jobs.py
+++ airflow/jobs.py
@@ -2351,7 +2351,7 @@ class LocalTaskJob(BaseJob):
         ti = self.task_instance
         if ti.state == State.RUNNING:
             self.was_running = True
-            fqdn = socket.getfqdn()
+            fqdn = socket.gethostname()
             if fqdn != ti.hostname:
                 logging.warning("The recorded hostname {ti.hostname} "
                                 "does not match this instance's hostname "
