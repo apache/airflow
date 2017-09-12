5d6cc80e6a20e08af24c3a9601555be68ca7d2bc
Andrew Stahlman <astahlman@lyft.com>
2017-09-12 14:21:33 -0700

    Upgrade flask-wtf to 0.14
    Airflow 1.8 depends on `flask-wtf==0.14`, so in order to run airflow 1.8
    and 1.7 side by side we need to force etl to upgrade from
    `flask-wtf==0.12` to `flask-wtf==0.14`.

--- setup.py
+++ setup.py
@@ -136,7 +136,7 @@ setup(
         'setproctitle>=1.1.8, <2',
         'sqlalchemy>=0.9.8',
         'thrift>=0.9.2, <0.10',
-        'Flask-WTF==0.12'
+        'Flask-WTF==0.14'
     ],
     extras_require={
         'all': devel_all,
