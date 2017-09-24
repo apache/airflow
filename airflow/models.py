51f03b89eb5e27fe3bff1e28cd40403c0aecab6e
Michael Crawford <dalupus@gmail.com>
2017-09-24 09:09:25 -0700

    [AIRFLOW-1636] Add AWS and EMR connection type
    aws and emr connection types are not specified in models, so when you
    update them the type gets cleared.  Since default ones are included
    ootb, they should be added to the model as valid options.

--- airflow/models.py
+++ airflow/models.py
@@ -553,6 +553,8 @@ class Connection(Base, LoggingMixin):
         ('redis', 'Redis',),
         ('wasb', 'Azure Blob Storage'),
         ('databricks', 'Databricks',),
+        ('aws', 'Amazon Web Services',),
+        ('emr', 'Elastic MapReduce',),
     ]
 
     def __init__(
