35f93411b02e082f948233dd610948f7fc73503c
Junda Yang <youngyjd@gmail.com>
2017-10-20 14:43:12 -0700

    fix encoding

--- airflow/task_runner/base_task_runner.py
+++ airflow/task_runner/base_task_runner.py
@@ -89,10 +89,14 @@ class BaseTaskRunner(LoggingMixin):
 
     def _read_task_logs(self, stream):
         while True:
-            line = stream.readline().decode('utf-8')
+            line = stream.readline()
+            line = line.decode('utf-8') if self._is_ascii(line) else line
             if len(line) == 0:
                 break
             self.logger.info('Subtask: {}'.format(line.rstrip('\n')))
+
+    def _is_ascii(self, line):
+        return all(ord(c) < 128 for c in line)
 
     def run_command(self, run_with, join_args=False):
         """
