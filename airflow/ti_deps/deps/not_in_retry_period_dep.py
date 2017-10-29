4f4a52a1de05696f8e4b9cd79498743a601cd549
Bolke de Bruin <bolke@xs4all.nl>
2017-10-29 12:14:17 +0100

    use utc timezone aware

--- airflow/ti_deps/deps/not_in_retry_period_dep.py
+++ airflow/ti_deps/deps/not_in_retry_period_dep.py
@@ -11,9 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from datetime import datetime
-
 from airflow.ti_deps.deps.base_ti_dep import BaseTIDep
+from airflow.utils import datetime
 from airflow.utils.db import provide_session
 from airflow.utils.state import State
 
