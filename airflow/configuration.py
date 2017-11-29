2318d4b7e2da616194f2037b3615d9150c8bb031
Le Trung Chanh <trungchanh.le@lazada.com>
2017-11-29 17:40:50 +0800

    [AIRFLOW-1519] Add server side paging in DAGs list

--- airflow/configuration.py
+++ airflow/configuration.py
@@ -276,6 +276,8 @@ log_fetch_timeout_sec = 5
 # DAGs by default
 hide_paused_dags_by_default = False
 
+page_size = 100
+
 [email]
 email_backend = airflow.utils.email.send_email_smtp
 
@@ -475,6 +477,8 @@ web_server_port = 8080
 dag_orientation = LR
 log_fetch_timeout_sec = 5
 hide_paused_dags_by_default = False
+
+page_size = 100
 
 [email]
 email_backend = airflow.utils.email.send_email_smtp
