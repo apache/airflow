7629aa4faf661dc1dfc2b88a0309abe857f5ca3e
Luke Bodeen <lbodeen@docutap.com>
2018-01-09 13:03:37 -0600

    indent line continuation issues

--- airflow/contrib/operators/winrm_operator.py
+++ airflow/contrib/operators/winrm_operator.py
@@ -76,9 +76,9 @@ class WinRMOperator(BaseOperator):
                 raise AirflowException("no command specified so nothing to execute here.")
 
             self.log.info("Starting command: '" + self.command + "' on remote host: " +
-                self.winrm_hook.remote_host)
-            command_id = self.winrm_hook.winrm_protocol. \
-            run_command(winrm_client, self.command)
+                    self.winrm_hook.remote_host)
+            command_id = self.winrm_hook.winrm_protocol.\
+                    run_command(winrm_client, self.command)
             std_out, std_err, status_code = self.winrm_hook.winrm_protocol. \
                 get_command_output(winrm_client, command_id)
 
