BestGigaCat/incubator-airflow

Add role_arn and aws_account_id/aws_iam_role support back to aws hook
In PR2532 (AIRFLOW-1520), the AWS credential code was refactored into a general
 AWS hook.  When that change was made, the existing assume role code was
 removed, leaving only ID/Secret credentials as an option.  Our dags rely on
 role assumption to access external S3 buckets, so this code re-adds role
 assumption via STS.

Additionally, in order to make this a bit easier, I changed _get_credentials to
 return a functioning boto3 session which is used by the public methods to
 initialize clients/resources/whatever.  This seemed a better route than
 adding another returnval in an already long list.

tests/contrib/sensors/test_emr_step_sensor.py
@@ -91,12 +91,15 @@ class TestEmrStepSensor(unittest.TestCase):
             DESCRIBE_JOB_STEP_COMPLETED_RETURN
         ]
 
+        mock_emr_session = MagicMock()
+        mock_emr_session.client.return_value = self.mock_emr_client
+
         # Mock out the emr_client creator
-        self.boto3_client_mock = MagicMock(return_value=self.mock_emr_client)
+        self.boto3_session_mock = MagicMock(return_value=mock_emr_session)
 
 
     def test_execute_calls_with_the_job_flow_id_and_step_id_until_it_reaches_a_terminal_state(self):
-        with patch('boto3.client', self.boto3_client_mock):
+        with patch('boto3.session.Session', self.boto3_session_mock):
 
             operator = EmrStepSensor(
                 task_id='test_task',

apache/airflow

tests/contrib/sensors/test_emr_step_sensor.py
@@ -121,15 +121,19 @@ class TestEmrStepSensor(unittest.TestCase):
             aws_conn_id='aws_default',
         )
 
+        mock_emr_session = MagicMock()
+        mock_emr_session.client.return_value = self.emr_client_mock
+
+        # Mock out the emr_client creator
+        self.boto3_session_mock = MagicMock(return_value=mock_emr_session)
+
     def test_step_completed(self):
         self.emr_client_mock.describe_step.side_effect = [
             DESCRIBE_JOB_STEP_RUNNING_RETURN,
             DESCRIBE_JOB_STEP_COMPLETED_RETURN
         ]
 
-        self.boto3_client_mock = MagicMock(return_value=self.emr_client_mock)
-
-        with patch('boto3.client', self.boto3_client_mock):
+        with patch('boto3.session.Session', self.boto3_session_mock):
             self.sensor.execute(None)
 
             self.assertEqual(self.emr_client_mock.describe_step.call_count, 2)
@@ -146,7 +150,7 @@ class TestEmrStepSensor(unittest.TestCase):
 
         self.boto3_client_mock = MagicMock(return_value=self.emr_client_mock)
 
-        with patch('boto3.client', self.boto3_client_mock):
+        with patch('boto3.session.Session', self.boto3_session_mock):
             self.assertRaises(AirflowException, self.sensor.execute, None)
 
 