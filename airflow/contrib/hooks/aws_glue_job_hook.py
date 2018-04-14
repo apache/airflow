f639747466fd959ab024c8226fd157441b2f3a12
olalekanelesin <elesin.olalekan@gmail.com>
2018-04-14 10:15:33 +0200

    [AIRFLOW-2310] : Add AWS Glue Job Compatibility to Airflow with tests

--- airflow/contrib/hooks/aws_glue_job_hook.py
+++ airflow/contrib/hooks/aws_glue_job_hook.py
@@ -15,8 +15,8 @@
 
 from airflow.exceptions import AirflowException
 from airflow.contrib.hooks.aws_hook import AwsHook
-import json
 import os.path
+import time
 
 
 class AwsGlueJobHook(AwsHook):
@@ -41,6 +41,8 @@ class AwsGlueJobHook(AwsHook):
     :type region_name: str
     :param s3_bucket: S3 bucket where logs and local etl script will be uploaded
     :type str
+    :param iam_role_name: AWS IAM Role for Glue Job
+    :type str
     """
 
     def __init__(self,
@@ -52,36 +54,38 @@ class AwsGlueJobHook(AwsHook):
                  retry_limit=None,
                  num_of_dpus=None,
                  region_name=None,
+                 iam_role_name=None,
                  s3_bucket=None, *args, **kwargs):
         self.job_name = job_name
         self.desc = desc
-        self.concurrent_run_limit = concurrent_run_limit
+        self.concurrent_run_limit = concurrent_run_limit or 1
         self.script_location = script_location
         self.conns = conns or ["s3"]
         self.retry_limit = retry_limit or 0
         self.num_of_dpus = num_of_dpus or 10
         self.region_name = region_name
+
+        if s3_bucket is None:
+            raise AirflowException(
+                'Failed to create aws glue job, error: Specify Parameter `s3_bucket`'
+            )
+
         self.s3_bucket = s3_bucket
-        self.assumed_policy = {
-            "Version": "2012-10-17",
-            "Statement": {
-                "Effect": "Allow",
-                "Principal": {"Service": "glue.amazonaws.com"},
-                "Action": "sts:AssumeRole"
-            }
-        }
-        self.role_name = "{}_ExecutionRole".format(self.job_name.capitalize())
+        self.role_name = iam_role_name
         self.S3_PROTOCOL = "s3://"
         self.S3_ARTIFACTS_PREFIX = 'artifacts/glue-scripts/'
         self.S3_GLUE_LOGS = 'logs/glue-logs/'
-        self.GLUE_SERVICE_ROLE = 'arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole'
         super(AwsGlueJobHook, self).__init__(*args, **kwargs)
 
     def get_conn(self):
         conn = self.get_client_type('glue', self.region_name)
         return conn
 
-    def _create_job_execution_role(self):
+    def list_jobs(self):
+        conn = self.get_conn()
+        return conn.get_jobs()
+
+    def get_iam_execution_role(self):
         """
         :return: iam role for job execution
         """
@@ -89,58 +93,14 @@ class AwsGlueJobHook(AwsHook):
 
         try:
             glue_execution_role = iam_client.get_role(RoleName=self.role_name)
-        except Exception:
-            glue_execution_role = None
-
-        if glue_execution_role is None:
-            self.log.info("Creating IAM Execution role for Glue Job")
-            # attach inline policy to access data bucket
-            self.log.info("Inline Policy attached: ")
-            glue_execution_role = iam_client.create_role(
-                Path="/",
-                RoleName=self.role_name,
-                Description="AWS IAM Execution Role for {}".format(self.role_name),
-                AssumeRolePolicyDocument=json.dumps(self.assumed_policy)
+            self.log.info("Iam Role Name: {}".format(self.role_name))
+            return glue_execution_role
+        except Exception as general_error:
+            raise AirflowException(
+                'Failed to create aws glue job, error: {error}'.format(
+                    error=str(general_error)
+                )
             )
-        policy = {
-            'Version': '2012-10-17',
-            'Statement': [
-                {
-                    'Sid': 'DataObjectAccess'.format(job_name=self.job_name),
-                    'Effect': 'Allow',
-                    'Action': ['s3:GetObject', 's3:PutObject'],
-                    'Resource': [
-                        'arn:aws:s3:::{s3_bucket}'.format(s3_bucket=self.s3_bucket),
-                        'arn:aws:s3:::{s3_bucket}/*'.format(s3_bucket=self.s3_bucket)
-                    ]
-                },
-                {
-                    'Sid': 'DataBucketAccess'.format(job_name=self.job_name),
-                    'Effect': 'Allow',
-                    'Action': [
-                        's3:ListBucket',
-                        's3:ListAllMyBuckets',
-                        's3:GetBucketLocation'
-                    ],
-                    'Resource': [
-                        'arn:aws:s3:::{s3_bucket}'.format(s3_bucket=self.s3_bucket)
-                    ]
-                }
-            ]
-        }
-        self.log.info("Attaching AWS Glue Service Role")
-        iam_client.attach_role_policy(
-            RoleName=self.role_name,
-            PolicyArn=self.GLUE_SERVICE_ROLE
-        )
-        self.log.info("Attaching Inline Policy for data and script bucket access")
-        iam_client.put_role_policy(
-            RoleName=self.role_name,
-            PolicyName='DataBucketAccessPolicy',
-            PolicyDocument=json.dumps(policy)
-        )
-
-        return glue_execution_role
 
     def initialize_job(self, script_arguments=None):
         """
@@ -149,7 +109,8 @@ class AwsGlueJobHook(AwsHook):
         glue_client = self.get_conn()
 
         try:
-            job_name = self._get_or_create_glue_job()
+            job_response = self.get_or_create_glue_job()
+            job_name = job_response['Name']
             job_run = glue_client.start_job_run(
                 JobName=job_name,
                 Arguments=script_arguments
@@ -179,15 +140,17 @@ class AwsGlueJobHook(AwsHook):
         stopped = job_run_state == 'STOPPED'
         completed = job_run_state == 'SUCCEEDED'
 
-        if failed or stopped or completed:
-            self.log.info("Exiting Job {} Run State: {}".format(run_id, job_run_state))
-            return job_run_state
-        else:
-            self.log.info("Polling for AWS Glue job {} status".format(job_name))
-            self.log.info("Current AWS Glue Job State: {}".format(job_run_state))
-            return self._job_completion(job_name, run_id)
+        while True:
+            if failed or stopped or completed:
+                self.log.info("Exiting Job {} Run State: {}"
+                              .format(run_id, job_run_state))
+                return job_run_state
+            else:
+                print(job_status, failed)
+                time.sleep(1000)
+                continue
 
-    def _get_or_create_glue_job(self):
+    def get_or_create_glue_job(self):
         glue_client = self.get_conn()
         try:
             self.log.info("Now creating and running AWS Glue Job")
@@ -196,7 +159,7 @@ class AwsGlueJobHook(AwsHook):
                         logs_path=self.S3_GLUE_LOGS,
                         job_name=self.job_name)
 
-            execution_role = self._create_job_execution_role()
+            execution_role = self.get_iam_execution_role()
             script_location = self._check_script_location()
             create_job_response = glue_client.create_job(
                 Name=self.job_name,
@@ -208,7 +171,8 @@ class AwsGlueJobHook(AwsHook):
                 MaxRetries=self.retry_limit,
                 AllocatedCapacity=self.num_of_dpus
             )
-            return create_job_response['Name']
+            # print(create_job_response)
+            return create_job_response
         except Exception as general_error:
             raise AirflowException(
                 'Failed to create aws glue job, error: {error}'.format(
@@ -229,31 +193,11 @@ class AwsGlueJobHook(AwsHook):
                                        self.s3_bucket,
                                        self.S3_ARTIFACTS_PREFIX + script_name)
 
-            s3_script_path = "s3://{s3_bucket}/{prefix}{script_name}" \
+            s3_script_path = "s3://{s3_bucket}/{prefix}{job_name}/{script_name}" \
                 .format(s3_bucket=self.s3_bucket,
                         prefix=self.S3_ARTIFACTS_PREFIX,
+                        job_name=self.job_name,
                         script_name=script_name)
             return s3_script_path
         else:
             return None
-
-    def job_tear_down(self):
-        """
-        This method tears down a running glue job and
-        deletes all attached IAM policies
-        :return:
-        """
-        glue_client = self.get_conn()
-        # delete iam role
-        iam_client = self.get_client_type('iam', self.region_name)
-        iam_client.detach_role_policy(
-            RoleName=self.role_name,
-            PolicyArn=self.GLUE_SERVICE_ROLE
-        )
-        iam_client.delete_role(
-            RoleName=self.role_name
-        )
-        # delete aws glue job
-        glue_client.delete_job(
-            JobName=self.job_name
-        )
