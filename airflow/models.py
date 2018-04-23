997d447e71086cec7bb9ea83f9039936b850067d
Ruslan Dautkhanov <Tagar@users.noreply.github.com>
2018-04-22 19:53:30 -0600

    AIRFLOW-2351 fix

--- airflow/models.py
+++ airflow/models.py
@@ -7,9 +7,9 @@
 # to you under the Apache License, Version 2.0 (the
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
-# 
+#
 #   http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -3081,7 +3081,10 @@ class DAG(BaseDag, LoggingMixin):
                 self.default_args['start_date'] = (
                     timezone.parse(self.default_args['start_date'])
                 )
-            self.timezone = self.default_args['start_date'].tzinfo
+            if self.default_args['start_date']:
+                self.timezone = self.default_args['start_date'].tzinfo
+            else:
+                self.timezone = settings.TIMEZONE
         else:
             self.timezone = settings.TIMEZONE
 
