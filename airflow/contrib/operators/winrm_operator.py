cdf94714a54ba5b5496d8272640360529ad6d522
Luke Bodeen <lbodeen@docutap.com>
2018-05-04 12:57:06 -0500

    linting

--- airflow/contrib/operators/winrm_operator.py
+++ airflow/contrib/operators/winrm_operator.py
@@ -83,7 +83,7 @@ class WinRMOperator(BaseOperator):
             self.log.info(
                 "Starting command: '{command}' on remote host: {remotehost}".
                 format(command=self.command, remotehost=self.winrm_hook.remote_host)
-                )
+            )
             command_id = self.winrm_hook.winrm_protocol. \
                 run_command(winrm_client, self.command)
             std_out, std_err, status_code = self.winrm_hook.winrm_protocol. \
