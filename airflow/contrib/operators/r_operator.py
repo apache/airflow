284b4974525baed405380d7623535ace0fa1e4b8
Brian Connelly <bconnelly@ookla.com>
2018-05-08 11:15:14 -0700

    Update ROperator to use rpy2 instead of CLI

--- airflow/contrib/operators/r_operator.py
+++ airflow/contrib/operators/r_operator.py
@@ -14,35 +14,23 @@
 
 from builtins import bytes
 import os
-import signal
-from subprocess import Popen, STDOUT, PIPE
-from tempfile import gettempdir, NamedTemporaryFile
+from tempfile import NamedTemporaryFile
 
-from airflow.exceptions import AirflowException
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
 from airflow.utils.file import TemporaryDirectory
 
+import rpy2.robjects as robjects
+from rpy2.rinterface import RRuntimeError
+
 
 class ROperator(BaseOperator):
     """
-    Execute an R script, command, or set of commands
+    Execute an R script or command
 
-    :param r_command: The command, set of commands, or a reference to an R
-        script (must have '.r' extension) to be executed (templated)
+    :param r_command: The command or a reference to an R script (must have
+        '.r' extension) to be executed (templated)
     :type r_command: string
-    :param rscript_bin: The command to run to execute an R script (default:
-        'Rscript'). If Rscript is not in the PATH, the full path can be
-        specified. Alternate interpreters can also be used (e.g., littler) by
-        specifying their executable here.
-    :type rscript_bin: string
-    :param env: If env is not None, it must be a mapping that defines the
-        environment variables for the new process; these are used instead
-        of inheriting the current process environment, which is the default
-        behavior. Note that this will remove PATH, which makes it impossible
-        to find `rscript_bin`. Either give the full path for `rscript_bin`,
-        or add `'PATH': os.environ['PATH']` to the given env. (templated)
-    :type env: dict
     :param xcom_push: If xcom_push is True (default: False), the last line
         written to stdout will also be pushed to an XCom (key 'return_value')
         when the R command completes.
@@ -52,7 +40,7 @@ class ROperator(BaseOperator):
 
     """
 
-    template_fields = ('r_command', 'env')
+    template_fields = ('r_command',)
     template_ext = ('.r', '.R')
     ui_color = '#C8D5E6'
 
@@ -60,25 +48,19 @@ class ROperator(BaseOperator):
     def __init__(
             self,
             r_command,
-            rscript_bin='Rscript',
-            env=None,
             xcom_push=False,
             output_encoding='utf-8',
             *args, **kwargs):
 
         super(ROperator, self).__init__(*args, **kwargs)
         self.r_command = r_command
-        self.rscript_bin = rscript_bin
-        self.env = env
         self.xcom_push = xcom_push
         self.output_encoding = output_encoding
 
     def execute(self, context):
         """
-        Execute the R command in a temporary directory
+        Execute the R command or script in a temporary directory
         """
-
-        self.log.info("Tmp dir root location: \n %s", gettempdir())
 
         with TemporaryDirectory(prefix='airflowtmp') as tmp_dir:
             with NamedTemporaryFile(dir=tmp_dir, prefix=self.task_id) as f:
@@ -87,47 +69,17 @@ class ROperator(BaseOperator):
                 f.flush()
                 fname = f.name
                 script_location = os.path.abspath(fname)
+
                 self.log.info("Temporary script location: %s", script_location)
+                self.log.info("Running command(s):\n%s", self.r_command)
 
-                def pre_exec():
-                    # Restore default signal disposition and invoke setsid
-                    for sig in ('SIGPIPE', 'SIGXFZ', 'SIGXFSZ'):
-                        if hasattr(signal, sig):
-                            signal.signal(getattr(signal, sig), signal.SIG_DFL)
-                    os.setsid()
+                try:
+                    res = robjects.r.source(fname, echo=False)
+                except RRuntimeError as e:
+                    self.log.error("Received R error: %s", e)
+                    res = None
 
-                self.log.info("Running command(s): %s", self.r_command)
-
-                sp = Popen(
-                    args=[self.rscript_bin, fname],
-                    stdout=PIPE,
-                    stderr=STDOUT,
-                    cwd=tmp_dir,
-                    env=self.env,
-                    preexec_fn=pre_exec,
-                )
-
-                self.sp = sp
-
-                self.log.info("Output:")
-                line = ''
-                for line in iter(sp.stdout.readline, b''):
-                    line = line.decode(self.output_encoding).rstrip()
-                    self.log.info(line)
-
-                sp.wait()
-                self.log.info(
-                    "Command exited with return code %s",
-                    sp.returncode
-                )
-
-                if sp.returncode:
-                    raise AirflowException('R command failed')
-
-        if self.xcom_push:
-            self.log.info('Pushing last line of output to Xcom')
-            return line
-
-    def on_kill(self):
-        self.log.info('Sending SIGTERM signal to R process group')
-        os.killpg(os.getpgid(self.sp.pid), signal.SIGTERM)
+        if self.xcom_push and res:
+            # This will be a pickled rpy2.robjects.vectors.ListVector
+            self.log.info('Pushing last line of output to Xcom: \n %s', res)
+            return res
