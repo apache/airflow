870521c221c6dca6b02228d7dc601c66c611cef6
Sandro Luck <sandro.luck@gmx.ch>
2018-06-13 14:10:11 +0200

    Update python_operator.py
    Small fix

--- airflow/operators/python_operator.py
+++ airflow/operators/python_operator.py
@@ -128,7 +128,7 @@ class BranchPythonOperator(PythonOperator, SkipMixin):
         self.log.debug("Downstream task_ids %s", downstream_tasks)
         # Avoid skipping tasks which are in the downstream of the branch we are taking
         branch_downstream_tasks = context['dag'].get_task(branch).downstream_list
-        skip_tasks = [t for t in downstream_tasks if t.task_id != branch and t.task_id]
+        skip_tasks = [t for t in downstream_tasks if t.task_id != branch]
         # Filter tasks which are also downstream tasks of the branch we are taking
         skip_tasks = [t for t in skip_tasks if t.task_id not in branch_downstream_tasks]
         if downstream_tasks:
