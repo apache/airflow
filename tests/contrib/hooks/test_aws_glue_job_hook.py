e8a6093339efa6150bb4894b3927a91cd50854da
olalekanelesin <elesin.olalekan@gmail.com>
2018-06-14 08:51:27 +0200

    [AIRFLOW-2310]: Add AWS Glue Integration to Apache Airflow

--- tests/contrib/hooks/test_aws_glue_job_hook.py
+++ tests/contrib/hooks/test_aws_glue_job_hook.py
@@ -7,26 +7,17 @@
 # to you under the Apache License, Version 2.0 (the
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
-<<<<<<< HEAD
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-=======
+
 # 
 #   http://www.apache.org/licenses/LICENSE-2.0
 # 
->>>>>>> [AIRFLOW-2310]: AWS Glue Job Integration with tests
+
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-<<<<<<< HEAD
-=======
-#
->>>>>>> [AIRFLOW-2310]: AWS Glue Job Integration with tests
-
 import unittest
 import json
 
