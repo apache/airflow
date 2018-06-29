2d606bc766eeac03ab602ef00bfa619ea1680029
James Meickle <jmeickle@quantopian.com>
2018-06-29 10:43:44 -0400

    Remove unused import

--- airflow/models.py
+++ airflow/models.py
@@ -86,7 +86,7 @@ from airflow.ti_deps.deps.prev_dagrun_dep import PrevDagrunDep
 from airflow.ti_deps.deps.trigger_rule_dep import TriggerRuleDep
 
 from airflow.ti_deps.dep_context import DepContext, QUEUE_DEPS, RUN_DEPS
-from airflow.utils import asciiart, timezone
+from airflow.utils import timezone
 from airflow.utils.dag_processing import list_py_file_paths
 from airflow.utils.dates import cron_presets, date_range as utils_date_range
 from airflow.utils.db import provide_session
