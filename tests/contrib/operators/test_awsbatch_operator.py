5fdfdf00a9888771243f92917c1d63e9a98b5240
Craig Forster <craig.forster@olono.ai>
2018-07-06 22:30:02 +0200

    [AIRFLOW-2706] AWS Batch Operator should use top-level job state to determine status
    Rather than inspecting the state of job attempts,
    the operator should use the top-level job status
    to determine the overall success or failure of the
    task. This means the following cases are handled
    correctly:
    
    1. Any infrastructure failure that results in no
    attempts being performed is now detected.
    2. Any retry policy that AWS Batch will do is now
    honored -- the job isn't marked FAILED until all
       attempts to retry have failed. Previously, the
    first failed *attempt* would make the task as
       failed.
    
    Closes #3567 from craigforster/master

--- tests/contrib/operators/test_awsbatch_operator.py
+++ tests/contrib/operators/test_awsbatch_operator.py
@@ -144,6 +144,7 @@ class TestAWSBatchOperator(unittest.TestCase):
         client_mock.describe_jobs.return_value = {
             'jobs': [{
                 'status': 'FAILED',
+                'statusReason': 'This is an error reason',
                 'attempts': [{
                     'exitCode': 1
                 }]
@@ -154,7 +155,7 @@ class TestAWSBatchOperator(unittest.TestCase):
             self.batch._check_success_task()
 
         # Ordering of str(dict) is not guaranteed.
-        self.assertIn('This containers encounter an error during execution ', str(e.exception))
+        self.assertIn('Job failed with status ', str(e.exception))
 
     def test_check_success_tasks_raises_pending(self):
         client_mock = mock.Mock()
@@ -181,6 +182,7 @@ class TestAWSBatchOperator(unittest.TestCase):
         client_mock.describe_jobs.return_value = {
             'jobs': [{
                 'status': 'FAILED',
+                'statusReason': 'This is an error reason',
                 'attempts': [{
                     'exitCode': 1
                 }, {
@@ -193,7 +195,7 @@ class TestAWSBatchOperator(unittest.TestCase):
             self.batch._check_success_task()
 
         # Ordering of str(dict) is not guaranteed.
-        self.assertIn('This containers encounter an error during execution ', str(e.exception))
+        self.assertIn('Job failed with status ', str(e.exception))
 
     def test_check_success_task_not_raises(self):
         client_mock = mock.Mock()
