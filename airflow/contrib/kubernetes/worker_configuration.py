b0679a5b1a82b3bcf8e4c1bbf029b266d40e3ca9
bo <bo.blanton@gmail.com>
2018-07-22 16:39:22 -0700

    skip dag volume if dags in container

--- airflow/contrib/kubernetes/worker_configuration.py
+++ airflow/contrib/kubernetes/worker_configuration.py
@@ -173,7 +173,6 @@ class WorkerConfiguration(LoggingMixin):
         if self.kube_config.logs_volume_subpath:
             logs_volume_mount['subPath'] = self.kube_config.logs_volume_subpath
 
-
         volume_mounts = [
             dags_volume_mount,
             logs_volume_mount
