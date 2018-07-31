1001d39255b5a24fae6d625e04e9a42f29b86443
Max Payton <maxspayton@gmail.com>
2018-07-31 15:21:39 -0700

    Upgrade announcement ui (#96)
    * Use bootstrap to draw a prettier announcement view
    
    Include a modal box to bring up the details
    Link the the atom feed
    
    * Fix formatting issue with close button
    
    * Remove fixme statement

--- airflow/www/static/feed.js
+++ airflow/www/static/feed.js
@@ -26,55 +26,43 @@
 // and publish them to an S3 bucket so any service at Lyft can
 // reference them
 
-// FIXME: Change to prod before pushing
-var DP_ANNOUNCEMENTS_FEED_URL = "https://dataplatformadmin.lyft.net/announcements.atom"
+var DP_ANNOUNCEMENTS_FEED_URL = "https://dataplatformadmin.lyft.net/announcements.atom";
 
 var setCss = function(el, attrs) {
     el.setAttribute("style", attrs.join('; '));
-}
+};
 
 var getXmlElementValue = function(el, node) {
     return el.getElementsByTagName(node)[0].firstChild.nodeValue;
-}
+};
 
 var onLoadRssEntries = function() {
     var xmlDoc = this.responseXML;
     var entries = Array.from(xmlDoc.getElementsByTagName("entry"));
     var latestEntry = entries[0];
 
-    var rssContainer = document.getElementById("rss-entries");
+    var external_link = document.getElementById("announcementsLink");
+    var title_box = document.getElementById("announcementHeader");
+    var modal_title = document.getElementById("announcementModalTitle");
+    var modal_body = document.getElementById("announcementBody");
+
     var title = getXmlElementValue(latestEntry, "title");
+    var summary = getXmlElementValue(latestEntry, "summary");
+    var body = getXmlElementValue(latestEntry, "content");
 
-    var entryUrl = (latestEntry.getElementsByTagName("link")[0]).getAttribute("href");
-    var titleNode = document.createElement("a");
-    titleNode.setAttribute("href", entryUrl);
-    titleNode.appendChild(document.createTextNode(title));
 
-    var rssIcon = document.createElement("img");
-    rssIcon.setAttribute("src", "../static/rss-logo.png");
-    setCss(rssIcon, ["float:right"])
-
-    var rssLink = document.createElement("a");
-    rssLink.setAttribute("href", DP_ANNOUNCEMENTS_FEED_URL);
-    rssLink.appendChild(rssIcon);
-
-    rssContainer.appendChild(titleNode);
-    rssContainer.appendChild(rssLink);
-    setCss(rssContainer, ["border:1px",
-                          "border-style:solid",
-                          "min-width:350px",
-                          "max-width:600px",
-                          "float:right",
-                          "padding:2px",
-                          "background-color:linen"]);
-}
+    external_link.setAttribute('href', DP_ANNOUNCEMENTS_FEED_URL);
+    title_box.innerHTML = title;
+    modal_title.innerHTML = summary;
+    modal_body.innerHTML = body;
+};
 
 var populateRssFeed = function() {
     var oReq = new XMLHttpRequest();
     oReq.addEventListener("load", onLoadRssEntries);
     oReq.open("GET", DP_ANNOUNCEMENTS_FEED_URL);
     oReq.send();
-}
+};
 
 document.addEventListener("DOMContentLoaded", function(event) {
     populateRssFeed();
