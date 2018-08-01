cefcf4c61f64be3792cbfed509b82a9eb4cc47be
bolkedebruin <bolkedebruin@users.noreply.github.com>
2018-08-01 11:25:31 +0200

    [AIRFLOW-2817] Force explicit choice on GPL dependency (#3660)
    By default one of Apache Airflow's dependencies pulls in a GPL
    library. Airflow should not install (and upgrade) without an explicit choice.
    
    This is part of the Apache requirements as we cannot depend on Category X
    software.
    
    (cherry picked from commit c37fc0b6ba19e3fe5656ae37cef9b59cef3c29e8)
    Signed-off-by: Bolke de Bruin <bolke@xs4all.nl>
    (cherry picked from commit b39e4532d9d1086c60b31553d08972bcc68df641)
    Signed-off-by: Bolke de Bruin <bolke@xs4all.nl>

--- setup.py
+++ setup.py
@@ -34,6 +34,17 @@ version = imp.load_source(
 PY3 = sys.version_info[0] == 3
 
 
+# See LEGAL-362
+def verify_gpl_dependency():
+    if (not os.getenv("AIRFLOW_GPL_UNIDECODE")
+            and not os.getenv("SLUGIFY_USES_TEXT_UNIDECODE") == "yes"):
+        raise RuntimeError("By default one of Airflow's dependencies installs a GPL "
+                           "dependency (unidecode). To avoid this dependency set "
+                           "SLUGIFY_USES_TEXT_UNIDECODE=yes in your environment when you "
+                           "install or upgrade Airflow. To force installing the GPL "
+                           "version set AIRFLOW_GPL_UNIDECODE")
+
+
 class Tox(TestCommand):
     user_options = [('tox-args=', None, "Arguments to pass to tox")]
 
@@ -235,6 +246,7 @@ else:
 
 
 def do_setup():
+    verify_gpl_dependency()
     write_version()
     setup(
         name='apache-airflow',
@@ -353,6 +365,7 @@ def do_setup():
             'License :: OSI Approved :: Apache Software License',
             'Programming Language :: Python :: 2.7',
             'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
             'Topic :: System :: Monitoring',
         ],
         author='Apache Software Foundation',
@@ -364,6 +377,7 @@ def do_setup():
             'test': Tox,
             'extra_clean': CleanCommand,
         },
+        python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
     )
 
 
