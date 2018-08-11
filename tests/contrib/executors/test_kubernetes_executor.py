e49767d4b4c4efc1e8a7a58271c5a55d98ebb3ac
Riccardo Bini <odracci@gmail.com>
2018-08-11 12:12:20 +0200

    Refactor Kubernetes operator with git-sync
    Currently the implementation of git-sync is broken because:
    - git-sync clones the repository in /tmp and not in airflow-dags volume
    - git-sync add a link to point to the revision required but it is not
    taken into account in AIRFLOW__CORE__DAGS_FOLDER
    
    Dags/logs hostPath volume has been added (needed if airflow run in
    kubernetes in local environment)
    
    To avoid false positive in CI `load_examples` is set to `False`
    otherwise DAGs from `airflow/example_dags` are always loaded. In this
    way is possible to test `import` in DAGs
    
    Remove `worker_dags_folder` config:
    `worker_dags_folder` is redundant and can lead to confusion.
    In WorkerConfiguration `self.kube_config.dags_folder` defines the path of
    the dags and can be set in the worker using airflow_configmap
    Refactor worker_configuration.py
    Use a docker container to run setup.py
    Compile web assets
    Fix codecov application path

--- tests/contrib/executors/test_kubernetes_executor.py
+++ tests/contrib/executors/test_kubernetes_executor.py
@@ -110,11 +110,15 @@ class TestKubernetesWorkerConfiguration(unittest.TestCase):
         self.kube_config.dags_volume_subpath = None
         self.kube_config.logs_volume_subpath = None
         self.kube_config.dags_in_image = False
+        self.kube_config.dags_folder = None
+        self.kube_config.git_dags_folder_mount_point = None
 
     def test_worker_configuration_no_subpaths(self):
         worker_config = WorkerConfiguration(self.kube_config)
         volumes, volume_mounts = worker_config.init_volumes_and_mounts()
-        for volume_or_mount in volumes + volume_mounts:
+        volumes_list = [value for value in volumes.values()]
+        volume_mounts_list = [value for value in volume_mounts.values()]
+        for volume_or_mount in volumes_list + volume_mounts_list:
             if volume_or_mount['name'] != 'airflow-config':
                 self.assertNotIn(
                     'subPath', volume_or_mount,
@@ -127,29 +131,69 @@ class TestKubernetesWorkerConfiguration(unittest.TestCase):
         worker_config = WorkerConfiguration(self.kube_config)
         volumes, volume_mounts = worker_config.init_volumes_and_mounts()
 
-        for volume in volumes:
+        for volume in [value for value in volumes.values()]:
             self.assertNotIn(
                 'subPath', volume,
                 "subPath isn't valid configuration for a volume"
             )
 
-        for volume_mount in volume_mounts:
+        for volume_mount in [value for value in volume_mounts.values()]:
             if volume_mount['name'] != 'airflow-config':
                 self.assertIn(
                     'subPath', volume_mount,
                     "subPath should've been passed to volumeMount configuration"
                 )
 
+    def test_worker_generate_dag_volume_mount_path(self):
+        self.kube_config.git_dags_folder_mount_point = '/root/airflow/git/dags'
+        self.kube_config.dags_folder = '/root/airflow/dags'
+        worker_config = WorkerConfiguration(self.kube_config)
+
+        self.kube_config.dags_volume_claim = 'airflow-dags'
+        self.kube_config.dags_volume_host = ''
+        dag_volume_mount_path = worker_config.generate_dag_volume_mount_path()
+        self.assertEqual(dag_volume_mount_path, self.kube_config.dags_folder)
+
+        self.kube_config.dags_volume_claim = ''
+        self.kube_config.dags_volume_host = '/host/airflow/dags'
+        dag_volume_mount_path = worker_config.generate_dag_volume_mount_path()
+        self.assertEqual(dag_volume_mount_path, self.kube_config.dags_folder)
+
+        self.kube_config.dags_volume_claim = ''
+        self.kube_config.dags_volume_host = ''
+        dag_volume_mount_path = worker_config.generate_dag_volume_mount_path()
+        self.assertEqual(dag_volume_mount_path,
+                         self.kube_config.git_dags_folder_mount_point)
+
     def test_worker_environment_no_dags_folder(self):
-        self.kube_config.worker_dags_folder = ''
+        self.kube_config.airflow_configmap = ''
+        self.kube_config.git_dags_folder_mount_point = ''
+        self.kube_config.dags_folder = ''
         worker_config = WorkerConfiguration(self.kube_config)
         env = worker_config._get_environment()
 
         self.assertNotIn('AIRFLOW__CORE__DAGS_FOLDER', env)
 
     def test_worker_environment_when_dags_folder_specified(self):
+        self.kube_config.airflow_configmap = 'airflow-configmap'
+        self.kube_config.git_dags_folder_mount_point = ''
         dags_folder = '/workers/path/to/dags'
-        self.kube_config.worker_dags_folder = dags_folder
+        self.kube_config.dags_folder = dags_folder
+
+        worker_config = WorkerConfiguration(self.kube_config)
+        env = worker_config._get_environment()
+
+        self.assertEqual(dags_folder, env['AIRFLOW__CORE__DAGS_FOLDER'])
+
+    def test_worker_environment_dags_folder_using_git_sync(self):
+        self.kube_config.airflow_configmap = 'airflow-configmap'
+        self.kube_config.git_sync_dest = 'repo'
+        self.kube_config.git_subpath = 'dags'
+        self.kube_config.git_dags_folder_mount_point = '/workers/path/to/dags'
+
+        dags_folder = '{}/{}/{}'.format(self.kube_config.git_dags_folder_mount_point,
+                                        self.kube_config.git_sync_dest,
+                                        self.kube_config.git_subpath)
 
         worker_config = WorkerConfiguration(self.kube_config)
         env = worker_config._get_environment()
