df7092a7f6e679d20221e0fbb0c97ea657829ef5
Kevin Yang <kevin.yang@airbnb.com>
2018-09-02 19:48:21 -0700

    [Airflow-2760] Decouple DAG parsing loop from scheduler loop

--- tests/utils/test_dag_processing.py
+++ tests/utils/test_dag_processing.py
@@ -17,11 +17,18 @@
 # specific language governing permissions and limitations
 # under the License.
 
+import os
 import unittest
 
 from mock import MagicMock
 
+from airflow import configuration as conf
+from airflow.jobs import DagFileProcessor
+from airflow.utils.dag_processing import DagFileProcessorAgent
 from airflow.utils.dag_processing import DagFileProcessorManager
+
+TEST_DAG_FOLDER = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), os.pardir, 'dags')
 
 
 class TestDagFileProcessorManager(unittest.TestCase):
@@ -29,10 +36,12 @@ class TestDagFileProcessorManager(unittest.TestCase):
         manager = DagFileProcessorManager(
             dag_directory='directory',
             file_paths=['abc.txt'],
-            parallelism=1,
-            process_file_interval=1,
             max_runs=1,
-            processor_factory=MagicMock().return_value)
+            processor_factory=MagicMock().return_value,
+            signal_conn=MagicMock(),
+            stat_queue=MagicMock(),
+            result_queue=MagicMock,
+            async_mode=True)
 
         mock_processor = MagicMock()
         mock_processor.stop.side_effect = AttributeError(
@@ -48,10 +57,12 @@ class TestDagFileProcessorManager(unittest.TestCase):
         manager = DagFileProcessorManager(
             dag_directory='directory',
             file_paths=['abc.txt'],
-            parallelism=1,
-            process_file_interval=1,
             max_runs=1,
-            processor_factory=MagicMock().return_value)
+            processor_factory=MagicMock().return_value,
+            signal_conn=MagicMock(),
+            stat_queue=MagicMock(),
+            result_queue=MagicMock,
+            async_mode=True)
 
         mock_processor = MagicMock()
         mock_processor.stop.side_effect = AttributeError(
@@ -62,3 +73,29 @@ class TestDagFileProcessorManager(unittest.TestCase):
 
         manager.set_file_paths(['abc.txt'])
         self.assertDictEqual(manager._processors, {'abc.txt': mock_processor})
+
+    def test_parse_once(self):
+        def processor_factory(file_path, zombies):
+            return DagFileProcessor(file_path,
+                                    False,
+                                    [],
+                                    zombies)
+
+        test_dag_path = os.path.join(TEST_DAG_FOLDER, 'test_scheduler_dags.py')
+        async_mode = 'sqlite' not in conf.get('core', 'sql_alchemy_conn')
+        processor_agent = DagFileProcessorAgent(test_dag_path,
+                                                [test_dag_path],
+                                                1,
+                                                processor_factory,
+                                                async_mode)
+        processor_agent.start()
+        parsing_result = []
+        while not processor_agent.done:
+            print(processor_agent.done)
+            if not async_mode:
+                processor_agent.heartbeat()
+                processor_agent.wait_until_finished()
+            parsing_result.extend(processor_agent.harvest_simple_dags())
+
+        dag_ids = [result.dag_id for result in parsing_result]
+        self.assertEqual(dag_ids.count('test_start_date_scheduling'), 1)
