bc97e6ee0b6136368d6aebc5c30df848420bb6b6
Kaxil Naik <kaxilnaik@gmail.com>
2018-09-05 00:46:41 +0100

    [AIRFLOW-3008] Move Kubernetes example DAGs to contrib

--- tests/core.py
+++ tests/core.py
@@ -73,7 +73,7 @@ from pendulum import utcnow
 
 import six
 
-NUM_EXAMPLE_DAGS = 20
+NUM_EXAMPLE_DAGS = 18
 DEV_NULL = '/dev/null'
 TEST_DAG_FOLDER = os.path.join(
     os.path.dirname(os.path.realpath(__file__)), 'dags')
