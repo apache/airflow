61d33929eb02826aabae35f0c459b3b0464b940a
Greg Sterin <greg.sterin@affirm.com>
2018-10-03 16:51:09 -0700

    new appbuilder release breaks www_rbac

--- setup.py
+++ setup.py
@@ -274,7 +274,7 @@ def do_setup():
             'croniter>=0.3.17, <0.4',
             'dill>=0.2.2, <0.3',
             'flask>=0.12.4, <0.13',
-            'flask-appbuilder>=1.11.1, <2.0.0',
+            'flask-appbuilder==1.11.1',
             'flask-admin==1.4.1',
             'flask-caching>=1.3.3, <1.4.0',
             'flask-login==0.2.11',
