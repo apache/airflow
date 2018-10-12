5b9515b1d1bf7ac5feed09529bb9cacab7ed3ce6
wayne.morris <wayne.morris@modmed.com>
2018-10-12 16:40:29 -0400

    Resolved error reported in travis.

--- tests/contrib/operators/test_s3_to_sftp_operator.py
+++ tests/contrib/operators/test_s3_to_sftp_operator.py
@@ -153,7 +153,7 @@ class S3ToSFTPOperatorTest(unittest.TestCase):
             ssh_hook=self.hook,
             command="rm {0}".format(self.sftp_path),
             do_xcom_push=True,
-            dag=self.dags
+            dag=self.dag
         )
         self.assertIsNotNone(remove_file_task)
         ti3 = TaskInstance(task=remove_file_task, execution_date=timezone.utcnow())
