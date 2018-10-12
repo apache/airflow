2ad664d2456ea97f937f6d27bb331224dcf2dd4d
Ash Berlin-Taylor <ash_github@firemirror.com>
2018-10-12 07:34:30 +0100

    [AIRFLOW-3099] Don't ever warn about missing sections of config (#4028)
    Rather than looping through and setting each config variable
    individually, and having to know which sections are optional and which
    aren't, instead we can just call a single function on ConfigParser and
    it will read the config from the dict, and more importantly here, never
    error about missing sections - it will just create them as needed.

--- airflow/bin/cli.py
+++ airflow/bin/cli.py
@@ -19,7 +19,6 @@
 # under the License.
 
 from __future__ import print_function
-from backports.configparser import NoSectionError
 import logging
 
 import os
@@ -455,28 +454,7 @@ def run(args, dag=None):
         if os.path.exists(args.cfg_path):
             os.remove(args.cfg_path)
 
-        # Do not log these properties since some may contain passwords.
-        # This may also set default values for database properties like
-        # core.sql_alchemy_pool_size
-        # core.sql_alchemy_pool_recycle
-        for section, config in conf_dict.items():
-            for option, value in config.items():
-                try:
-                    conf.set(section, option, value)
-                except NoSectionError:
-                    optional_sections = [
-                        'atlas', 'mesos', 'elasticsearch', 'kubernetes',
-                        'lineage', 'hive'
-                    ]
-                    if section in optional_sections:
-                        log.debug('Section {section} Option {option} '
-                                  'does not exist in the config!'.format(section=section,
-                                                                         option=option))
-                    else:
-                        log.error('Section {section} Option {option} '
-                                  'does not exist in the config!'.format(section=section,
-                                                                         option=option))
-
+        conf.conf.read_dict(conf_dict, source=args.cfg_path)
         settings.configure_vars()
 
     # IMPORTANT, have to use the NullPool, otherwise, each "run" command may leave
