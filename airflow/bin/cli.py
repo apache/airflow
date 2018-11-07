7bea66e689bce9197ec87b14e80561bb0aae0c8d
Ultrabug <ultrabug@gentoo.org>
2018-11-07 17:11:06 +0100

    Revert "leverage next_execution_date property in the cli"
    This reverts commit da9b738bf3f2bebe5249efddf4402e30ca060e79.

--- airflow/bin/cli.py
+++ airflow/bin/cli.py
@@ -600,9 +600,18 @@ def next_execution(args):
 
     if dag.is_paused:
         print("[INFO] Please be reminded this DAG is PAUSED now.")
-    if dag.next_execution_date is None:
+
+    if dag.latest_execution_date:
+        next_execution_dttm = dag.following_schedule(dag.latest_execution_date)
+
+        if next_execution_dttm is None:
+            print("[WARN] No following schedule can be found. " +
+                  "This DAG may have schedule interval '@once' or `None`.")
+
+        print(next_execution_dttm)
+    else:
         print("[WARN] Only applicable when there is execution record found for the DAG.")
-    print(dag.next_execution_date)
+        print(None)
 
 
 @cli_utils.action_logging
