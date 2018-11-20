d4f1d658149a21328042383317ca0f4a9f463709
johnhofman <johncarlhofman@gmail.com>
2018-11-20 11:13:53 +0100

    [AIRFLOW-2966] Catch ApiException in the Kubernetes Executor (#4209)
    Creating a pod that exceeds a namespace's resource quota throws an
    ApiException. This change catches the exception and the task is
    re-queued inside the Executor instead of killing the scheduler.

--- airflow/contrib/executors/kubernetes_executor.py
+++ airflow/contrib/executors/kubernetes_executor.py
@@ -651,8 +651,14 @@ class KubernetesExecutor(BaseExecutor, LoggingMixin):
         KubeResourceVersion.checkpoint_resource_version(last_resource_version)
 
         if not self.task_queue.empty():
-            key, command, kube_executor_config = self.task_queue.get()
-            self.kube_scheduler.run_next((key, command, kube_executor_config))
+            task = self.task_queue.get()
+
+            try:
+                self.kube_scheduler.run_next(task)
+            except ApiException:
+                self.log.exception('ApiException when attempting ' +
+                                   'to run task, re-queueing.')
+                self.task_queue.put(task)
 
     def _change_state(self, key, state, pod_id):
         if state != State.RUNNING:
