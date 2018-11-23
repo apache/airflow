c4ce3fa3113e99723646552c49235079364ce8a4
Jose Luis Ricon <jose@ricon.xyz>
2018-11-23 16:48:08 +0000

    bump flask admin

--- setup.py
+++ setup.py
@@ -300,7 +300,7 @@ def do_setup():
             'dill>=0.2.2, <0.3',
             'flask>=0.12.4, <0.13',
             'flask-appbuilder==1.12.1',
-            'flask-admin==1.4.1',
+            'flask-admin==1.5.2',
             'flask-caching>=1.3.3, <1.4.0',
             'flask-login>=0.3, <0.5',
             'flask-swagger==0.2.13',
