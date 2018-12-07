c7ffa4e116d2f15df1033a6459c5f59e757cca52
Yuri Bendana <ybendana@sangamo.com>
2018-12-06 16:02:23 -0800

    Added failed run test and commented successful run test

--- tests/contrib/sensors/test_dagrun_sensor.py
+++ tests/contrib/sensors/test_dagrun_sensor.py
@@ -16,7 +16,7 @@
 import os
 from os.path import dirname, realpath
 import unittest
-from datetime import timedelta, datetime
+from datetime import datetime
 
 from airflow.models import DagBag, TaskInstance, DagRun
 from airflow.settings import Session
@@ -27,6 +27,8 @@ from airflow.utils.timezone import convert_to_utc
 
 DEFAULT_DATE = convert_to_utc(datetime(2017, 1, 1))
 TEST_DAG_ID = 'test_dagrun_sensor_dag'
+TEST_DAG_PARENT = TEST_DAG_ID + '_parent'
+TEST_DAG_CHILD = TEST_DAG_ID + '_child'
 TEST_DAG_FOLDER = os.path.join(
     dirname(dirname(dirname(realpath(__file__)))), 'dags')
 
@@ -43,47 +45,35 @@ class TestDagRunSensor(unittest.TestCase):
         self.dagbag = DagBag(dag_folder=TEST_DAG_FOLDER)
 
     def test_poke(self):
-        dag_parent = self.dagbag.get_dag(TEST_DAG_ID + '_parent_clean')
-        dag_parent.run(
-            start_date=DEFAULT_DATE + timedelta(seconds=0),
-            end_date=DEFAULT_DATE + timedelta(seconds=8),
-        )
+        dag_parent = self.dagbag.get_dag(TEST_DAG_PARENT)
+        # dag_parent.run()
 
-        dag_child = self.dagbag.get_dag(TEST_DAG_ID + '_child_clean')
-
-        # One of the following two runs should succeed (00:00:00), while the
-        # other (00:00:05) should have its sensor time out, since 00:00:09
-        # will never be run for the parent dag.
-
-        # first (safe) run
-        dag_child.run(
-            start_date=DEFAULT_DATE + timedelta(seconds=0),
-            end_date=DEFAULT_DATE + timedelta(seconds=0),
-        )
+        # One of the following two runs should succeed, while the
+        # other should have its sensor time out.
 
         sess = Session()
         TI = TaskInstance
-        sensor_tis = sess.query(TI).filter(
-            TI.dag_id == TEST_DAG_ID + '_child_clean',
-            TI.task_id == 'sense_parent',
-            TI.state == State.SUCCESS,
-        ).all()
-        self.assertEqual(len(sensor_tis), 1)
+        # sensor_tis = sess.query(TI).filter(
+        #     TI.dag_id == TEST_DAG_PARENT,
+        #     TI.task_id == 'sense_child',
+        #     TI.state == State.SUCCESS,
+        # ).all()
+        # self.assertEqual(len(sensor_tis), 1)
 
-        do_stuff_tis = sess.query(TI).filter(
-            TI.dag_id == TEST_DAG_ID + '_child_clean',
-            TI.task_id == 'do_stuff',
-            TI.state == State.SUCCESS,
-        ).all()
-        self.assertEqual(len(do_stuff_tis), 1)
+        # do_stuff_tis = sess.query(TI).filter(
+        #     TI.dag_id == TEST_DAG_PARENT,
+        #     TI.task_id == 'do_stuff',
+        #     TI.state == State.SUCCESS,
+        # ).all()
+        # self.assertEqual(len(do_stuff_tis), 1)
 
         DR = DagRun
-        drs = sess.query(DR).filter(
-            DR.dag_id == TEST_DAG_ID + '_child_clean',
-            DR.state == State.SUCCESS,
-            DR.execution_date == DEFAULT_DATE,
-        ).all()
-        self.assertEqual(len(drs), 1)
+        # drs = sess.query(DR).filter(
+        #     DR.dag_id == TEST_DAG_ID + '_parent',
+        #     DR.state == State.SUCCESS,
+        #     DR.execution_date == DEFAULT_DATE,
+        # ).all()
+        # self.assertEqual(len(drs), 1)
 
         # second run
         with self.assertRaises(AirflowException):
@@ -91,29 +81,21 @@ class TestDagRunSensor(unittest.TestCase):
             # the executor, and what we see is an AirflowException for
             # the dependent task which fails because of a failed upstream
             # task.
-            dag_child.run(
-                start_date=DEFAULT_DATE + timedelta(seconds=5),
-                end_date=DEFAULT_DATE + timedelta(seconds=5),
-            )
+            dag_parent.run()
 
         failed_tis = sess.query(TI).filter(
             TI.state == State.FAILED,
         ).all()
         self.assertEqual(len(failed_tis), 1)
         failed_ti = failed_tis[0]
-        self.assertEqual(failed_ti.task_id, 'sense_parent')
-        self.assertEqual(failed_ti.dag_id, TEST_DAG_ID + '_child_clean')
-        self.assertEqual(failed_ti.execution_date,
-                         DEFAULT_DATE + timedelta(seconds=5))
+        self.assertEqual(failed_ti.task_id, 'sense_child')
+        self.assertEqual(failed_ti.dag_id, TEST_DAG_PARENT)
 
         failed_drs = sess.query(DR).filter(
-            DR.dag_id == TEST_DAG_ID + '_child_clean',
+            DR.dag_id == TEST_DAG_PARENT,
             DR.state == State.FAILED,
         ).all()
         self.assertEqual(len(failed_drs), 1)
-
-        self.assertEqual(failed_drs[0].execution_date,
-                         DEFAULT_DATE + timedelta(seconds=5))
 
 
 if __name__ == '__main__':
