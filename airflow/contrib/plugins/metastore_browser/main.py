2e083989498bc3deb0804f0228bc9c31fd7519a6
eladkal <45845474+eladkal@users.noreply.github.com>
2018-12-13 15:11:47 +0200

    Update main.py

--- airflow/contrib/plugins/metastore_browser/main.py
+++ airflow/contrib/plugins/metastore_browser/main.py
@@ -24,7 +24,7 @@ from flask import Blueprint, request
 from flask_admin import BaseView, expose
 import pandas as pd
 
-from airflow.hooks.hive_hooks import HiveMetastoreHook, HiveCliHook
+from airflow.hooks.hive_hook import HiveMetastoreHook, HiveCliHook
 from airflow.hooks.mysql_hook import MySqlHook
 from airflow.hooks.presto_hook import PrestoHook
 from airflow.plugins_manager import AirflowPlugin
