38a1a349a27153a7ef91d10fc3e2b339fcb0b1a7
gyamxxx <gyamxxx@gmail.com>
2018-12-13 19:38:33 +0900

    Update gcp_dataflow_hook.py
    #1

--- airflow/contrib/hooks/gcp_dataflow_hook.py
+++ airflow/contrib/hooks/gcp_dataflow_hook.py
@@ -272,7 +272,8 @@ class DataFlowHook(GoogleCloudBaseHook):
         # https://cloud.google.com/dataflow/docs/reference/rest/v1b3/RuntimeEnvironment
         environment = {}
         for key in ['maxWorkers', 'zone', 'serviceAccountEmail', 'tempLocation',
-                    'bypassTempDirValidation', 'machineType']:
+                    'bypassTempDirValidation', 'machineType', 'additionalExperiments',
+                    'network', 'subnetwork']:
             if key in variables:
                 environment.update({key: variables[key]})
         body = {"jobName": name,
