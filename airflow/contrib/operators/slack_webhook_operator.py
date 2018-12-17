358f84fc05588867fa5b5f37037e4ae6aef32296
Michael Holtzscher <mholtz@protonmail.com>
2018-12-17 08:56:23 -0600

    [AIRFLOW-3522] Add support for sending Slack attachments
    Add attachments args to SlackWebhookHook and
    SlackWebhookOperator to support sending more
    detailed Slack messages.

--- airflow/contrib/operators/slack_webhook_operator.py
+++ airflow/contrib/operators/slack_webhook_operator.py
@@ -37,6 +37,8 @@ class SlackWebhookOperator(SimpleHttpOperator):
     :type webhook_token: str
     :param message: The message you want to send on Slack
     :type message: str
+    :param attachments: The attachments you want to send on Slack
+    :type attachments: list
     :param channel: The channel the message should be posted to
     :type channel: str
     :param username: The username to post to slack with
@@ -55,6 +57,7 @@ class SlackWebhookOperator(SimpleHttpOperator):
                  http_conn_id=None,
                  webhook_token=None,
                  message="",
+                 attachments="",
                  channel=None,
                  username=None,
                  icon_emoji=None,
@@ -68,6 +71,7 @@ class SlackWebhookOperator(SimpleHttpOperator):
         self.http_conn_id = http_conn_id
         self.webhook_token = webhook_token
         self.message = message
+        self.attachments = attachments
         self.channel = channel
         self.username = username
         self.icon_emoji = icon_emoji
@@ -83,6 +87,7 @@ class SlackWebhookOperator(SimpleHttpOperator):
             self.http_conn_id,
             self.webhook_token,
             self.message,
+            self.attachments,
             self.channel,
             self.username,
             self.icon_emoji,
