bcacbf5615a64d3c0d1c331790ae7c39080dda12
pjvanthof <pjrvanthof@gmail.com>
2018-12-26 16:38:32 +0100

    Added timezone to DagModel

--- airflow/models/__init__.py
+++ airflow/models/__init__.py
@@ -3004,6 +3004,10 @@ class DagModel(Base):
     # Default view of the inside the webserver
     default_view = Column(String(25))
 
+    def __init__(self):
+        if not hasattr(self, 'timezone') or not self.timezone:
+            self.timezone = settings.TIMEZONE
+
     def __repr__(self):
         return "<DAG: {self.dag_id}>".format(self=self)
 
