7c92ce0710b87de733c5c60f23784efd78b330d7
pjvanthof <pjrvanthof@gmail.com>
2018-12-27 13:12:11 +0100

    Fixed headers

--- airflow/jobs/scheduler_job.py
+++ airflow/jobs/scheduler_job.py
@@ -1,3 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
 import os
 import signal
 import sys
@@ -10,9 +34,9 @@ from past.types import basestring
 from sqlalchemy import func, or_, and_, not_
 from sqlalchemy.orm import make_transient
 
+from airflow import jobs
 from airflow import settings, configuration as conf, models, DAG, executors
 from airflow.jobs.base import BaseJob
-from airflow.jobs.dag_file_processor import DagFileProcessor
 from airflow.jobs.backfill_job import BackfillJob
 from airflow.models import DagRun
 from airflow.settings import Stats
@@ -1010,7 +1034,7 @@ class SchedulerJob(BaseJob):
         self.log.info("There are %s files in %s", len(known_file_paths), self.subdir)
 
         def processor_factory(file_path, zombies):
-            return DagFileProcessor(file_path,
+            return jobs.dag_file_processor.DagFileProcessor(file_path,
                                     pickle_dags,
                                     self.dag_ids,
                                     zombies)
