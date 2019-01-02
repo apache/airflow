025e5592454f42225af3b881bdb7290128314cfa
Kaxil Naik <kaxilnaik@gmail.com>
2019-01-02 12:36:23 +0000

    [AIRFLOW-3578] Fix Type Error for BigQueryOperator (#4384)
    * Fix Type Error for BigQueryOperator and support the unicode object.
    * Add tests

--- tests/contrib/operators/test_bigquery_operator.py
+++ tests/contrib/operators/test_bigquery_operator.py
@@ -17,12 +17,20 @@
 # specific language governing permissions and limitations
 # under the License.
 
+import os
 import unittest
+from datetime import datetime
+
+import six
+
+from airflow import configuration, models
+from airflow.models import TaskInstance, DAG
 
 from airflow.contrib.operators.bigquery_operator import \
     BigQueryCreateExternalTableOperator, BigQueryCreateEmptyTableOperator, \
     BigQueryDeleteDatasetOperator, BigQueryCreateEmptyDatasetOperator, \
     BigQueryOperator
+from airflow.settings import Session
 
 try:
     from unittest import mock
@@ -39,7 +47,13 @@ TEST_TABLE_ID = 'test-table-id'
 TEST_GCS_BUCKET = 'test-bucket'
 TEST_GCS_DATA = ['dir1/*.csv']
 TEST_SOURCE_FORMAT = 'CSV'
+<<<<<<< HEAD
 TEST_LOCATION = 'asia-northeast1'
+=======
+DEFAULT_DATE = datetime(2015, 1, 1)
+TEST_DAG_ID = 'test-bigquery-operators'
+
+>>>>>>> aa2dc603... [AIRFLOW-3578] Fix Type Error for BigQueryOperator (#4384)
 
 class BigQueryCreateEmptyTableOperatorTest(unittest.TestCase):
 
@@ -149,6 +163,22 @@ class BigQueryCreateEmptyDatasetOperatorTest(unittest.TestCase):
 
 
 class BigQueryOperatorTest(unittest.TestCase):
+    def setUp(self):
+        configuration.conf.load_test_config()
+        self.dagbag = models.DagBag(
+            dag_folder='/dev/null', include_examples=True)
+        self.args = {'owner': 'airflow', 'start_date': DEFAULT_DATE}
+        self.dag = DAG(TEST_DAG_ID, default_args=self.args)
+
+    def tearDown(self):
+        session = Session()
+        session.query(models.TaskInstance).filter_by(
+            dag_id=TEST_DAG_ID).delete()
+        session.query(models.TaskFail).filter_by(
+            dag_id=TEST_DAG_ID).delete()
+        session.commit()
+        session.close()
+
     @mock.patch('airflow.contrib.operators.bigquery_operator.BigQueryHook')
     def test_execute(self, mock_hook):
         operator = BigQueryOperator(
@@ -199,9 +229,11 @@ class BigQueryOperatorTest(unittest.TestCase):
 
     @mock.patch('airflow.contrib.operators.bigquery_operator.BigQueryHook')
     def test_bigquery_operator_defaults(self, mock_hook):
+
         operator = BigQueryOperator(
             task_id=TASK_ID,
             sql='Select * from test_table',
+            dag=self.dag, default_args=self.args
         )
 
         operator.execute(None)
@@ -227,6 +259,11 @@ class BigQueryOperatorTest(unittest.TestCase):
                 api_resource_configs=None,
                 cluster_fields=None,
             )
+
+        self.assertTrue(isinstance(operator.sql, six.string_types))
+        ti = TaskInstance(task=operator, execution_date=DEFAULT_DATE)
+        ti.render_templates()
+        self.assertTrue(isinstance(ti.task.sql, six.string_types))
 
     @mock.patch('airflow.contrib.operators.bigquery_operator.BigQueryHook')
     def test_bigquery_operator_location(self, mock_hook):
