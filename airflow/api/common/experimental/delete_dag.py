apache/airflow

commit d67715d0983f3037a7c3f1274d600c36748740fe

[AIRFLOW-3599] Removed Dagbag from delete dag (#4406)
* Removed Dagbag from delete dag

* delete when fileloc does not exist

airflow/api/common/experimental/delete_dag.py
@@ -17,6 +17,8 @@
 # specific language governing permissions and limitations
 # under the License.
 
+import os
+
 from sqlalchemy import or_
 
 from airflow import models, settings
@@ -39,10 +41,9 @@ def delete_dag(dag_id, keep_records_in_log=True):
     if dag is None:
         raise DagNotFound("Dag id {} not found".format(dag_id))
 
-    dagbag = models.DagBag()
-    if dag_id in dagbag.dags:
+    if dag.fileloc and not os.path.exists(dag.fileloc):
         raise DagFileExists("Dag id {} is still in DagBag. "
-                            "Remove the DAG file first.".format(dag_id))
+                            "Remove the DAG file first: {}".format(dag_id, dag.fileloc))
 
     count = 0
 

commit 560355ff9b9be8b8a30d4d73338dd6c5981e27f7

[AIRFLOW-3635] Fix incorrect logic in detele_dag (introduced in PR#4406)
Incorrect logic was introduced in PR #4406
(https://github.com/apache/airflow/pull/4406)

This was not found out because the Travis CI was not working as expected.

airflow/api/common/experimental/delete_dag.py
@@ -41,7 +41,7 @@ def delete_dag(dag_id, keep_records_in_log=True):
     if dag is None:
         raise DagNotFound("Dag id {} not found".format(dag_id))
 
-    if dag.fileloc and not os.path.exists(dag.fileloc):
+    if dag.fileloc and os.path.exists(dag.fileloc):
         raise DagFileExists("Dag id {} is still in DagBag. "
                             "Remove the DAG file first: {}".format(dag_id, dag.fileloc))
 