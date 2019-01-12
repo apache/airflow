3c8ed03182f28f73fffb366bf1cbf439b1199a53
Elizabeth Halper <elhalper@microsoft.com>
2019-01-11 16:41:45 -0800

    [AIRFLOW-3284] Azure Batch AI Setup Operand Fix

--- setup.py
+++ setup.py
@@ -277,7 +277,7 @@ devel_all = (sendgrid + devel + all_dbs + doc + samba + s3 + slack + crypto + or
              docker + ssh + kubernetes + celery + azure_blob_storage + redis + gcp_api +
              datadog + zendesk + jdbc + ldap + kerberos + password + webhdfs + jenkins +
              druid + pinot + segment + snowflake + elasticsearch + azure_data_lake + azure_cosmos +
-             atlas + +azure_batch_ai + azure_container_instances)
+             atlas + azure_batch_ai + azure_container_instances)
 
 # Snakebite & Google Cloud Dataflow are not Python 3 compatible :'(
 if PY3:
