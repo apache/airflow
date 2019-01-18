216e8f0b49744751e06de7415ade6cb50239e153
Kamil Breguła <kamil.bregula@polidea.com>
2019-01-19 00:10:32 +0100

    [AIRFLOW-3730] Standarization use of logs mechanisms

--- airflow/contrib/hooks/gcp_api_base_hook.py
+++ airflow/contrib/hooks/gcp_api_base_hook.py
@@ -27,13 +27,12 @@ import google.oauth2.service_account
 
 from airflow.exceptions import AirflowException
 from airflow.hooks.base_hook import BaseHook
-from airflow.utils.log.logging_mixin import LoggingMixin
 
 
 _DEFAULT_SCOPES = ('https://www.googleapis.com/auth/cloud-platform',)
 
 
-class GoogleCloudBaseHook(BaseHook, LoggingMixin):
+class GoogleCloudBaseHook(BaseHook):
     """
     A base hook for Google cloud-related hooks. Google cloud has a shared REST
     API client that is built in the same way no matter which service you use.
