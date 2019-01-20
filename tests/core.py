97ac37d0b936fd565b113b79f418ff25b245de14
余建涛 <yujiantao@yujiantaodeMacBook-Pro.local>
2019-01-20 13:40:36 +0800

    add stmp_mime_from

--- tests/core.py
+++ tests/core.py
@@ -2640,7 +2640,9 @@ class EmailSmtpTest(unittest.TestCase):
         self.assertEqual(['to'], call_args[1])
         msg = call_args[2]
         self.assertEqual('subject', msg['Subject'])
-        self.assertEqual(configuration.conf.get('smtp', 'SMTP_MAIL_FROM'), msg['From'])
+        self.assertEqual(
+            configuration.conf.get('smtp', 'SMTP_MIME_FROM') or configuration.conf.get('smtp', 'SMTP_MAIL_FROM'),
+            msg['From'])
         self.assertEqual(2, len(msg.get_payload()))
         filename = u'attachment; filename="' + os.path.basename(attachment.name) + '"'
         self.assertEqual(filename, msg.get_payload()[-1].get(u'Content-Disposition'))
@@ -2668,7 +2670,9 @@ class EmailSmtpTest(unittest.TestCase):
         self.assertEqual(['to', 'cc', 'bcc'], call_args[1])
         msg = call_args[2]
         self.assertEqual('subject', msg['Subject'])
-        self.assertEqual(configuration.conf.get('smtp', 'SMTP_MAIL_FROM'), msg['From'])
+        self.assertEqual(
+            configuration.conf.get('smtp', 'SMTP_MIME_FROM') or configuration.conf.get('smtp', 'SMTP_MAIL_FROM'),
+            msg['From'])
         self.assertEqual(2, len(msg.get_payload()))
         self.assertEqual(u'attachment; filename="' + os.path.basename(attachment.name) + '"',
                          msg.get_payload()[-1].get(u'Content-Disposition'))
