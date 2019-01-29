aa93af48dc1c335d959ba55d7bae5ed135da2b7b
OmerJog <44576851+OmerJog@users.noreply.github.com>
2019-01-29 12:23:40 +0200

    [AIRFLOW-865] Configure FTP connection mode (#4535)

--- airflow/contrib/hooks/ftp_hook.py
+++ airflow/contrib/hooks/ftp_hook.py
@@ -62,8 +62,9 @@ class FTPHook(BaseHook):
     """
     Interact with FTP.
 
-    Errors that may occur throughout but should be handled
-    downstream.
+    Errors that may occur throughout but should be handled downstream.
+    You can specify mode for data transfers in the extra field of your
+    connection as ``{"passive": "true"}``.
     """
 
     def __init__(self, ftp_conn_id='ftp_default'):
@@ -83,7 +84,9 @@ class FTPHook(BaseHook):
         """
         if self.conn is None:
             params = self.get_connection(self.ftp_conn_id)
+            pasv = params.extra_dejson.get("passive", True)
             self.conn = ftplib.FTP(params.host, params.login, params.password)
+            self.conn.set_pasv(pasv)
 
         return self.conn
 
@@ -303,6 +306,7 @@ class FTPSHook(FTPHook):
         """
         if self.conn is None:
             params = self.get_connection(self.ftp_conn_id)
+            pasv = params.extra_dejson.get("passive", True)
 
             if params.port:
                 ftplib.FTP_TLS.port = params.port
@@ -310,5 +314,6 @@ class FTPSHook(FTPHook):
             self.conn = ftplib.FTP_TLS(
                 params.host, params.login, params.password
             )
+            self.conn.set_pasv(pasv)
 
         return self.conn
