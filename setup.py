9249ba930c6ab2d0bde1a32b96c1b03e325d11d1
Fokko Driesprong <fokko@driesprong.frl>
2019-01-31 07:59:49 +0100

    [AIRFLOW-2876] Update Tenacity to 4.12 (#3723)
    Tenacity 4.8 is not python 3.7 compatible because it contains
    reserved keywords in the code

--- setup.py
+++ setup.py
@@ -315,7 +315,7 @@ def do_setup():
             'setproctitle>=1.1.8, <2',
             'sqlalchemy>=1.1.15, <1.3.0',
             'tabulate>=0.7.5, <=0.8.2',
-            'tenacity==4.8.0',
+            'tenacity==4.12.0',
             'text-unidecode==1.2',
             'thrift>=0.9.2',
             'tzlocal>=1.4',
