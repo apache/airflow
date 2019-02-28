8ae575dda4f65b033dd93afae00f085a83284657
Merouane Benthameur <merouane.benthameur@ville.montreal.qc.ca>
2019-02-28 15:13:56 -0500

    [AIRFLOW-3063] - Make DockerOperator support all docker arguments
    A flexible implementation of dockerOperator class that takes all the
    containers config arguments in json kwargs dynamically.

--- airflow/operators/docker_operator.py
+++ airflow/operators/docker_operator.py
@@ -42,39 +42,18 @@ class DockerOperator(BaseOperator):
     Docker connection needs to be configured in Airflow and the connection ID
     be provided with the parameter ``docker_conn_id``.
 
-    :param image: Docker image from which to create the container.
-        If image tag is omitted, "latest" will be used.
-    :type image: str
     :param api_version: Remote API version. Set to ``auto`` to automatically
         detect the server's version.
     :type api_version: str
-    :param auto_remove: Auto-removal of the container on daemon side when the
-        container's process exits.
-        The default is False.
-    :type auto_remove: bool
-    :param command: Command to be run in the container. (templated)
-    :type command: str or list
     :param cpus: Number of CPUs to assign to the container.
         This value gets multiplied with 1024. See
         https://docs.docker.com/engine/reference/run/#cpu-share-constraint
     :type cpus: float
-    :param dns: Docker custom DNS servers
-    :type dns: list[str]
-    :param dns_search: Docker custom DNS search domain
-    :type dns_search: list[str]
     :param docker_url: URL of the host running the docker daemon.
         Default is unix://var/run/docker.sock
     :type docker_url: str
-    :param environment: Environment variables to set in the container. (templated)
-    :type environment: dict
     :param force_pull: Pull the docker image on every run. Default is False.
     :type force_pull: bool
-    :param mem_limit: Maximum amount of memory the container can use.
-        Either a float value, which represents the limit in bytes,
-        or a string like ``128m`` or ``1g``.
-    :type mem_limit: float or str
-    :param network_mode: Network mode for the container.
-    :type network_mode: str
     :param tls_ca_cert: Path to a PEM-encoded certificate authority
         to secure the docker connection.
     :type tls_ca_cert: str
@@ -93,85 +72,127 @@ class DockerOperator(BaseOperator):
         The path is also made available via the environment variable
         ``AIRFLOW_TMP_DIR`` inside the container.
     :type tmp_dir: str
-    :param user: Default user inside the docker container.
-    :type user: int or str
-    :param volumes: List of volumes to mount into the container, e.g.
-        ``['/host/path:/container/path', '/host/path2:/container/path2:ro']``.
-    :param working_dir: Working directory to
-        set on the container (equivalent to the -w switch the docker client)
-    :type working_dir: str
+    :param xcom_push: Does the stdout will be pushed to the next step using XCom.
+        The default is False.
+    :type xcom_push: bool
     :param xcom_all: Push all the stdout or just the last line.
         The default is False (last line).
     :type xcom_all: bool
     :param docker_conn_id: ID of the Airflow connection to use
     :type docker_conn_id: str
-    :param shm_size: Size of ``/dev/shm`` in bytes. The size must be
-        greater than 0. If omitted uses system default.
-    :type shm_size: int
+    :param container_config: a json that define all the config of the container .
+    :type container_config: json object
+        ** Use of container_config**
+        Takes a json object only, you can provide all the supported arguments in
+        create_container method. Check create_container method documentation for
+        more details: https://goo.gl/u5PQqE
+        Note: the image ``image`` attribute is mandatory.
+
+        .. code-block:: python
+
+            container_config = {'image':'ubuntu:latest',
+                                'command': 'echo hello world!'
+                                'environment':{'SOME_ENV_VAR':'SOME_VALUE'},
+                                'working_dir':'/container/some_path'
+            }
+    :param host_config: a json that define all host config of the container
+    :type host_config: json object
+        **Use of host_config**
+        Takes a json object only, you can provide all the supported arguments in
+        create_host_config method. Check create_host_config method documentation for
+        more details: https://goo.gl/65NcDi
+
+        .. code-block:: python
+
+            host_config = { 'binds': ['/home/some_path:/container/some_path'],
+                            'network_mode': 'bridge',
+                            'shm_size': 1000,
+                            'cpu_shares': 2048,
+                            'mem_limit': None,
+                            'auto_remove': False,
+                            'dns': None,
+                            'dns_search': None
+            }
     """
-    template_fields = ('command', 'environment',)
+    template_fields = ('container_config', 'host_config',)
     template_ext = ('.sh', '.bash',)
 
     @apply_defaults
     def __init__(
             self,
-            image,
             api_version=None,
-            command=None,
             cpus=1.0,
             docker_url='unix://var/run/docker.sock',
-            environment=None,
             force_pull=False,
-            mem_limit=None,
-            network_mode=None,
             tls_ca_cert=None,
             tls_client_cert=None,
             tls_client_key=None,
             tls_hostname=None,
             tls_ssl_version=None,
             tmp_dir='/tmp/airflow',
-            user=None,
-            volumes=None,
-            working_dir=None,
+            xcom_push=False,
             xcom_all=False,
             docker_conn_id=None,
-            dns=None,
-            dns_search=None,
-            auto_remove=False,
-            shm_size=None,
+            container_config=None,
+            host_config=None,
+
             *args,
             **kwargs):
 
         super(DockerOperator, self).__init__(*args, **kwargs)
         self.api_version = api_version
-        self.auto_remove = auto_remove
-        self.command = command
         self.cpus = cpus
-        self.dns = dns
-        self.dns_search = dns_search
         self.docker_url = docker_url
-        self.environment = environment or {}
         self.force_pull = force_pull
-        self.image = image
-        self.mem_limit = mem_limit
-        self.network_mode = network_mode
         self.tls_ca_cert = tls_ca_cert
         self.tls_client_cert = tls_client_cert
         self.tls_client_key = tls_client_key
         self.tls_hostname = tls_hostname
         self.tls_ssl_version = tls_ssl_version
         self.tmp_dir = tmp_dir
-        self.user = user
-        self.volumes = volumes or []
-        self.working_dir = working_dir
+        self.xcom_push_flag = xcom_push
         self.xcom_all = xcom_all
         self.docker_conn_id = docker_conn_id
-        self.shm_size = shm_size
-        if kwargs.get('xcom_push') is not None:
-            raise AirflowException("'xcom_push' was deprecated, use 'BaseOperator.do_xcom_push' instead")
+        self.container_config = container_config or {}
+        self.host_config = host_config or {}
 
         self.cli = None
         self.container = None
+
+    def __sanitize_host_config(self):
+        """
+        This private method validate the instance attribute ``self.host_config``.
+        It also sets the required default values of ``host_config`` dictionary
+        argument to :py:meth:`create_container`.
+        :return: None
+        """
+        if 'cpu_shares' not in self.host_config:
+            self.host_config['cpu_shares'] = int(round(self.cpus * 1024))
+        if 'binds' not in self.host_config:
+            self.host_config["binds"] = []
+        if 'volumes' in self.host_config:
+            for element in self.host_config['volumes']:
+                self.host_config['binds'].append(element)
+            del self.host_config['volumes']
+
+    def __sanitize_container_config(self):
+        """
+        This private method validate the instance attribute ``self.container_config``.
+        It also sets the required default values of ``container_config`` dictionary
+        argument :py:meth:`create_container`.
+        :return: None
+        """
+        if 'image' not in self.container_config:
+            raise AirflowException('No docker image provided')
+
+        if 'environment' not in self.container_config:
+            self.container_config['environment'] = {}
+        if 'command' in self.container_config and \
+                self.container_config['command'] is not None \
+                and self.container_config['command'].strip().find('[') == 0:
+            self.container_config['command'] = ast.literal_eval(self.container_config['command'])
+        elif 'command' not in self.container_config:
+            self.container_config['command'] = None
 
     def get_hook(self):
         return DockerHook(
@@ -182,8 +203,11 @@ class DockerOperator(BaseOperator):
         )
 
     def execute(self, context):
-        self.log.info('Starting docker container from image %s', self.image)
 
+        self.__sanitize_host_config()
+        self.__sanitize_container_config()
+
+        self.log.info('Starting docker container from image %s', self.container_config['image'])
         tls_config = self.__get_tls_config()
 
         if self.docker_conn_id:
@@ -195,33 +219,18 @@ class DockerOperator(BaseOperator):
                 tls=tls_config
             )
 
-        if self.force_pull or len(self.cli.images(name=self.image)) == 0:
-            self.log.info('Pulling docker image %s', self.image)
-            for l in self.cli.pull(self.image, stream=True):
+        if self.force_pull or len(self.cli.images(name=self.container_config['image'])) == 0:
+            self.log.info('Pulling docker image %s', self.container_config['image'])
+            for l in self.cli.pull(self.container_config['image'], stream=True):
                 output = json.loads(l.decode('utf-8').strip())
                 if 'status' in output:
                     self.log.info("%s", output['status'])
 
         with TemporaryDirectory(prefix='airflowtmp') as host_tmp_dir:
-            self.environment['AIRFLOW_TMP_DIR'] = self.tmp_dir
-            self.volumes.append('{0}:{1}'.format(host_tmp_dir, self.tmp_dir))
-
+            self.container_config['environment']['AIRFLOW_TMP_DIR'] = self.tmp_dir
+            self.host_config['binds'].append('{0}:{1}'.format(host_tmp_dir, self.tmp_dir))
             self.container = self.cli.create_container(
-                command=self.get_command(),
-                environment=self.environment,
-                host_config=self.cli.create_host_config(
-                    auto_remove=self.auto_remove,
-                    binds=self.volumes,
-                    network_mode=self.network_mode,
-                    shm_size=self.shm_size,
-                    dns=self.dns,
-                    dns_search=self.dns_search,
-                    cpu_shares=int(round(self.cpus * 1024)),
-                    mem_limit=self.mem_limit),
-                image=self.image,
-                user=self.user,
-                working_dir=self.working_dir
-            )
+                host_config=self.cli.create_host_config(**self.host_config), **self.container_config)
             self.cli.start(self.container['Id'])
 
             line = ''
@@ -235,17 +244,9 @@ class DockerOperator(BaseOperator):
             if result['StatusCode'] != 0:
                 raise AirflowException('docker container failed: ' + repr(result))
 
-            # duplicated conditional logic because of expensive operation
-            if self.do_xcom_push:
+            if self.xcom_push_flag:
                 return self.cli.logs(container=self.container['Id']) \
-                    if self.xcom_all else line.encode('utf-8')
-
-    def get_command(self):
-        if self.command is not None and self.command.strip().find('[') == 0:
-            commands = ast.literal_eval(self.command)
-        else:
-            commands = self.command
-        return commands
+                    if self.xcom_all else str(line)
 
     def on_kill(self):
         if self.cli is not None:
--- tests/operators/test_docker_operator.py
+++ tests/operators/test_docker_operator.py
@@ -35,8 +35,21 @@ class DockerOperatorTestCase(unittest.TestCase):
     @mock.patch('airflow.utils.file.mkdtemp')
     @mock.patch('airflow.operators.docker_operator.APIClient')
     def test_execute(self, client_class_mock, mkdtemp_mock):
-        host_config = mock.Mock()
-        mkdtemp_mock.return_value = '/mkdtemp'
+        mkdtemp_mock.return_value = '/tmp/mkdtemp'
+
+        host_config = {'binds': ['/host/path:/container/path'],
+                       'network_mode': 'bridge',
+                       'shm_size': 1000,
+                       'mem_limit': None,
+                       'auto_remove': False,
+                       'dns': None,
+                       'dns_search': None
+                       }
+        container_config = {'image': 'ubuntu:latest',
+                            'working_dir': '/container/path',
+                            'command': 'echo Hello world!',
+                            'environment': {'UNIT': 'TEST'}
+                            }
 
         client_mock = mock.Mock(spec=APIClient)
         client_mock.create_container.return_value = {'Id': 'some_id'}
@@ -48,34 +61,69 @@ class DockerOperatorTestCase(unittest.TestCase):
 
         client_class_mock.return_value = client_mock
 
-        operator = DockerOperator(api_version='1.19', command='env', environment={'UNIT': 'TEST'},
-                                  image='ubuntu:latest', network_mode='bridge', owner='unittest',
-                                  task_id='unittest', volumes=['/host/path:/container/path'],
-                                  working_dir='/container/path', shm_size=1000)
+        operator = DockerOperator(api_version='1.19',
+                                  task_id='unittest',
+                                  container_config=container_config,
+                                  host_config=host_config)
         operator.execute(None)
 
         client_class_mock.assert_called_with(base_url='unix://var/run/docker.sock', tls=None,
                                              version='1.19')
 
-        client_mock.create_container.assert_called_with(command='env',
-                                                        environment={
-                                                            'AIRFLOW_TMP_DIR': '/tmp/airflow',
-                                                            'UNIT': 'TEST'
-                                                        },
-                                                        host_config=host_config,
-                                                        image='ubuntu:latest',
-                                                        user=None,
-                                                        working_dir='/container/path'
-                                                        )
-        client_mock.create_host_config.assert_called_with(binds=['/host/path:/container/path',
-                                                                 '/mkdtemp:/tmp/airflow'],
-                                                          network_mode='bridge',
-                                                          shm_size=1000,
-                                                          cpu_shares=1024,
-                                                          mem_limit=None,
-                                                          auto_remove=False,
-                                                          dns=None,
-                                                          dns_search=None)
+        client_mock.create_container.assert_called_once_with(
+            host_config={'cpu_shares': 1024, 'auto_remove': False,
+                         'binds': ['/host/path:/container/path',
+                                   '/tmp/mkdtemp:/tmp/airflow'],
+                         'network_mode': 'bridge', 'shm_size': 1000, 'mem_limit': None,
+                         'dns': None, 'dns_search': None}, image='ubuntu:latest',
+            environment={'AIRFLOW_TMP_DIR': '/tmp/airflow', 'UNIT': 'TEST'},
+            working_dir='/container/path', command='echo Hello world!'
+        )
+
+        client_mock.create_host_config.assert_called_once_with(
+            auto_remove=False, binds=['/host/path:/container/path',
+                                      '/tmp/mkdtemp:/tmp/airflow'],
+            cpu_shares=1024, dns=None, dns_search=None, mem_limit=None,
+            network_mode='bridge', shm_size=1000
+        )
+
+        client_mock.images.assert_called_with(name='ubuntu:latest')
+        client_mock.logs.assert_called_with(container='some_id', stream=True)
+        client_mock.pull.assert_called_with('ubuntu:latest', stream=True)
+        client_mock.wait.assert_called_with('some_id')
+
+    @mock.patch('airflow.utils.file.mkdtemp')
+    @mock.patch('airflow.operators.docker_operator.APIClient')
+    def test_execute_without_host_config(self, client_class_mock, mkdtemp_mock):
+        mkdtemp_mock.return_value = '/tmp/mkdtemp'
+        client_mock = mock.Mock(spec=APIClient)
+        container_config = {'image': 'ubuntu:latest'}
+        client_mock.create_host_config.return_value = {'binds': ['/tmp/mkdtemp:/tmp/airflow'],
+                                                       'cpu_shares': 1024}
+        client_mock.create_container.return_value = {'Id': 'some_id'}
+        client_mock.images.return_value = []
+        client_mock.logs.return_value = ['container log']
+        client_mock.pull.return_value = [b'{"status":"pull log"}']
+        client_mock.wait.return_value = {"StatusCode": 0}
+        client_class_mock.return_value = client_mock
+
+        client_class_mock.return_value = client_mock
+
+        operator = DockerOperator(api_version='1.35',
+                                  task_id='unittest',
+                                  container_config=container_config)
+        operator.execute(None)
+
+        client_mock.create_container.assert_called_once_with(
+            host_config={'cpu_shares': 1024, 'binds': ['/tmp/mkdtemp:/tmp/airflow']},
+            image='ubuntu:latest',
+            environment={'AIRFLOW_TMP_DIR': '/tmp/airflow'},
+            command=None
+        )
+        client_mock.create_host_config.assert_called_once_with(
+            cpu_shares=1024, binds=['/tmp/mkdtemp:/tmp/airflow']
+        )
+
         client_mock.images.assert_called_with(name='ubuntu:latest')
         client_mock.logs.assert_called_with(container='some_id', stream=True)
         client_mock.pull.assert_called_with('ubuntu:latest', stream=True)
@@ -84,6 +132,7 @@ class DockerOperatorTestCase(unittest.TestCase):
     @mock.patch('airflow.operators.docker_operator.tls.TLSConfig')
     @mock.patch('airflow.operators.docker_operator.APIClient')
     def test_execute_tls(self, client_class_mock, tls_class_mock):
+        container_config = {'image': 'ubuntu:latest'}
         client_mock = mock.Mock(spec=APIClient)
         client_mock.create_container.return_value = {'Id': 'some_id'}
         client_mock.create_host_config.return_value = mock.Mock()
@@ -96,9 +145,9 @@ class DockerOperatorTestCase(unittest.TestCase):
         tls_mock = mock.Mock()
         tls_class_mock.return_value = tls_mock
 
-        operator = DockerOperator(docker_url='tcp://127.0.0.1:2376', image='ubuntu',
-                                  owner='unittest', task_id='unittest', tls_client_cert='cert.pem',
-                                  tls_ca_cert='ca.pem', tls_client_key='key.pem')
+        operator = DockerOperator(docker_url='tcp://127.0.0.1:2376', task_id='unittest',
+                                  tls_client_cert='cert.pem', tls_ca_cert='ca.pem', tls_client_key='key.pem',
+                                  container_config=container_config)
         operator.execute(None)
 
         tls_class_mock.assert_called_with(assert_hostname=None, ca_cert='ca.pem',
@@ -110,6 +159,7 @@ class DockerOperatorTestCase(unittest.TestCase):
 
     @mock.patch('airflow.operators.docker_operator.APIClient')
     def test_execute_unicode_logs(self, client_class_mock):
+        container_config = {'image': 'ubuntu:latest'}
         client_mock = mock.Mock(spec=APIClient)
         client_mock.create_container.return_value = {'Id': 'some_id'}
         client_mock.create_host_config.return_value = mock.Mock()
@@ -123,7 +173,7 @@ class DockerOperatorTestCase(unittest.TestCase):
         originalRaiseExceptions = logging.raiseExceptions
         logging.raiseExceptions = True
 
-        operator = DockerOperator(image='ubuntu', owner='unittest', task_id='unittest')
+        operator = DockerOperator(container_config=container_config, owner='unittest', task_id='unittest')
 
         with mock.patch('traceback.print_exception') as print_exception_mock:
             operator.execute(None)
@@ -132,6 +182,7 @@ class DockerOperatorTestCase(unittest.TestCase):
 
     @mock.patch('airflow.operators.docker_operator.APIClient')
     def test_execute_container_fails(self, client_class_mock):
+        container_config = {'image': 'ubuntu:latest'}
         client_mock = mock.Mock(spec=APIClient)
         client_mock.create_container.return_value = {'Id': 'some_id'}
         client_mock.create_host_config.return_value = mock.Mock()
@@ -142,16 +193,58 @@ class DockerOperatorTestCase(unittest.TestCase):
 
         client_class_mock.return_value = client_mock
 
-        operator = DockerOperator(image='ubuntu', owner='unittest', task_id='unittest')
+        operator = DockerOperator(container_config=container_config, owner='unittest', task_id='unittest')
 
         with self.assertRaises(AirflowException):
+            operator.execute(None)
+
+    @mock.patch('airflow.operators.docker_operator.APIClient')
+    def test_execute_container_fails_no_image_provided(self, client_class_mock):
+        container_config = {'network_mode': 'bridge',
+                            'shm_size': 1000}
+        client_mock = mock.Mock(spec=APIClient)
+        client_mock.create_container.return_value = {'Id': 'some_id'}
+        client_mock.create_host_config.return_value = mock.Mock()
+        client_mock.images.return_value = []
+        client_mock.logs.return_value = []
+        client_mock.pull.return_value = []
+        client_mock.wait.return_value = {"StatusCode": 1}
+
+        client_class_mock.return_value = client_mock
+
+        operator = DockerOperator(container_config=container_config, owner='unittest', task_id='unittest')
+
+        with self.assertRaises(AirflowException):
+            operator.execute(None)
+
+    @mock.patch('airflow.utils.file.mkdtemp')
+    @mock.patch('airflow.operators.docker_operator.APIClient')
+    def test_execute_with_host_config_fail(self, client_class_mock, mkdtemp_mock):
+        mkdtemp_mock.return_value = '/tmp/test'
+        client_mock = mock.Mock(spec=APIClient)
+        container_config = {'image': 'ubuntu:latest'}
+        client_mock.create_host_config.return_value.side_effect = TypeError('version value passed')
+        client_mock.create_container.side_effect = TypeError('version value passed')
+        client_mock.images.return_value = []
+        client_mock.logs.return_value = ['container log']
+        client_mock.pull.return_value = [b'{"status":"pull log"}']
+        client_mock.wait.return_value = {"StatusCode": 0}
+        client_class_mock.return_value = client_mock
+        operator = DockerOperator(api_version='1.35',
+                                  task_id='unittest',
+                                  container_config=container_config,
+                                  host_config={'version': 8,
+                                               'auto_remove': False,
+                                               'volumes': ['some_dummy_volume:/container/dummy_path']})
+
+        with self.assertRaises(TypeError):
             operator.execute(None)
 
     @staticmethod
     def test_on_kill():
         client_mock = mock.Mock(spec=APIClient)
-
-        operator = DockerOperator(image='ubuntu', owner='unittest', task_id='unittest')
+        container_config = {'image': 'ubuntu:latest'}
+        operator = DockerOperator(container_config=container_config, owner='unittest', task_id='unittest')
         operator.cli = client_mock
         operator.container = {'Id': 'some_id'}
 
@@ -162,6 +255,7 @@ class DockerOperatorTestCase(unittest.TestCase):
     @mock.patch('airflow.operators.docker_operator.APIClient')
     def test_execute_no_docker_conn_id_no_hook(self, operator_client_mock):
         # Mock out a Docker client, so operations don't raise errors
+        container_config = {'image': 'ubuntu:latest'}
         client_mock = mock.Mock(name='DockerOperator.APIClient mock', spec=APIClient)
         client_mock.images.return_value = []
         client_mock.create_container.return_value = {'Id': 'some_id'}
@@ -172,7 +266,7 @@ class DockerOperatorTestCase(unittest.TestCase):
 
         # Create the DockerOperator
         operator = DockerOperator(
-            image='publicregistry/someimage',
+            container_config=container_config,
             owner='unittest',
             task_id='unittest'
         )
@@ -196,6 +290,7 @@ class DockerOperatorTestCase(unittest.TestCase):
     @mock.patch('airflow.operators.docker_operator.APIClient')
     def test_execute_with_docker_conn_id_use_hook(self, operator_client_mock,
                                                   operator_docker_hook):
+        container_config = {'image': 'ubuntu:latest'}
         # Mock out a Docker client, so operations don't raise errors
         client_mock = mock.Mock(name='DockerOperator.APIClient mock', spec=APIClient)
         client_mock.images.return_value = []
@@ -207,7 +302,7 @@ class DockerOperatorTestCase(unittest.TestCase):
 
         # Create the DockerOperator
         operator = DockerOperator(
-            image='publicregistry/someimage',
+            container_config=container_config,
             owner='unittest',
             task_id='unittest',
             docker_conn_id='some_conn_id'
