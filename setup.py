24cc0edd95a847d4694459f8d2db59da2d702dbc
Ash Berlin-Taylor <ash_github@firemirror.com>
2019-03-01 21:05:01 +0000

    [AIRFLOW-XXX] Pin version of tornado pulled in by Celery. (#4815)
    https://github.com/tornadoweb/tornado/issues/2604

--- setup.py
+++ setup.py
@@ -141,7 +141,8 @@ azure_cosmos = ['azure-cosmos>=3.0.1']
 cassandra = ['cassandra-driver>=3.13.0']
 celery = [
     'celery>=4.1.1, <4.2.0',
-    'flower>=0.7.3, <1.0'
+    'flower>=0.7.3, <1.0',
+    'tornado>=4.2.0, <6.0',  # Dep of flower. Pin to a version that works on Py3.5.2
 ]
 cgroups = [
     'cgroupspy>=0.1.4',
