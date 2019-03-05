1a980dbcdfddbcd2e7b4605e8b071f1154c587e8
Kamil Breguła <mik-laj@users.noreply.github.com>
2019-03-05 07:14:30 -0800

    Use logger formatting instead of format string
    Co-Authored-By: briandconnelly <briandconnelly@users.noreply.github.com>

--- airflow/contrib/operators/r_operator.py
+++ airflow/contrib/operators/r_operator.py
@@ -72,7 +72,7 @@ class ROperator(BaseOperator):
 
         # Export context as environment variables
         airflow_context_vars = context_to_airflow_vars(context, in_env_var_format=True)
-        self.log.info('Exporting the following env vars:\n' +
+        self.log.info('Exporting the following env vars:\n%s',
                       '\n'.join(["{}={}".format(k, v)
                                  for k, v in
                                  airflow_context_vars.items()]))
