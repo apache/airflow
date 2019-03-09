3ca32c261507b9f29d8eab035f758a8aaacfb7bc
mans2singh <mans2singh@yahoo.com>
2019-03-09 15:35:52 -0500

    [AIRFLOW-4055] Add AWS SQS Sensor

---
+++ airflow/contrib/sensors/aws_sqs_sensor.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from airflow.sensors.base_sensor_operator import BaseSensorOperator
+from airflow.utils.decorators import apply_defaults
+from airflow.contrib.hooks.aws_sqs_hook import SQSHook
+from airflow.exceptions import AirflowException
+
+
+class SQSSensor(BaseSensorOperator):
+    """
+    Get messages from an SQS queue and passes them through XCom.
+
+    """
+
+    template_fields = ['sqs_queue', 'max_messages']
+
+    @apply_defaults
+    def __init__(self, aws_conn_id, sqs_queue, max_messages=5, *args, **kwargs):
+        """
+        :param aws_conn_id: AWS connection id
+        :type aws_conn_id: str
+        :param sqs_queue: The SQS queue
+        :type sqs_queue: str
+        :param max_messages: The maximum number of messages to retrieve for each poke
+        :type max_messages: int
+        """
+        super(SQSSensor, self).__init__(*args, **kwargs)
+        self.sqs_queue = sqs_queue
+        self.aws_conn_id = aws_conn_id
+        self.max_messages = max_messages
+        self.sqs_hook = SQSHook(aws_conn_id=self.aws_conn_id)
+
+    def poke(self, context):
+        """
+        Check for message on subscribed queue and write to xcom the message with key ``messages``
+
+        :param context: the context object
+        :type context: dict
+        :return: ``True`` if message is available or ``False``
+        """
+
+        self.log.debug('SQSSensor checking for message on queue: %s', self.sqs_queue)
+
+        try:
+            messages = self.sqs_hook.get_conn().receive_message(QueueUrl=self.sqs_queue,
+                                                                MaxNumberOfMessages=self.max_messages)
+
+            self.log.debug("reveived message %s", str(messages))
+
+            if 'Messages' not in messages:
+                self.log.debug('No message received ' + str(messages))
+                return False
+
+            if (len(messages['Messages']) > 0):
+                context['ti'].xcom_push(key='messages', value=messages)
+
+                entries = [{'Id': message['MessageId'], 'ReceiptHandle': message['ReceiptHandle']}
+                           for message in messages['Messages']]
+                result = self.sqs_hook.get_conn().delete_message_batch(QueueUrl=self.sqs_queue,
+                                                                       Entries=entries)
+
+                if ('Successful' in result):
+                    return True
+                else:
+                    raise AirflowException(
+                        'Delete SQS Messages failed ' + str(result) + ' for messages ' + str(messages))
+
+            return True
+
+        except AirflowException as ae:
+            self.log.error('AirflowException %s', str(ae))
+            raise ae
+        except Exception as e:
+            self.log.error('exception %s', str(e))
+
+        return False
---
+++ tests/contrib/sensors/test_sqs_sensor.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+
+import unittest
+from airflow import DAG, configuration
+from airflow.contrib.sensors.aws_sqs_sensor import SQSSensor
+from airflow.utils import timezone
+from mock import patch, call, MagicMock
+from airflow.exceptions import AirflowException
+
+DEFAULT_DATE = timezone.datetime(2017, 1, 1)
+
+
+class TestSQSSensor(unittest.TestCase):
+
+    @patch('airflow.contrib.sensors.aws_sqs_sensor.SQSHook')
+    def setUp(self, mock_sqs_hook):
+        configuration.load_test_config()
+
+        args = {
+            'owner': 'airflow',
+            'start_date': DEFAULT_DATE
+        }
+
+        self.dag = DAG('test_dag_id', default_args=args)
+        self.mock_sqs_hook = mock_sqs_hook
+        self.sensor = SQSSensor(
+            task_id='test_task',
+            dag=self.dag,
+            sqs_queue='test',
+            aws_conn_id='aws_default'
+        )
+
+        self.mock_context = MagicMock()
+
+    def test_poke_success(self):
+        message = {'Messages': [{'MessageId': 'c585e508-2ea0-44c7-bf3e-d1ba0cb87834',
+                                 'ReceiptHandle': 'mockHandle',
+                                 'MD5OfBody': 'e5a9d8684a8edfed460b8d42fd28842f',
+                                 'Body': 'h21'}],
+                   'ResponseMetadata': {'RequestId': '56cbf4aa-f4ef-5518-9574-a04e0a5f1411',
+                                        'HTTPStatusCode': 200,
+                                        'HTTPHeaders': {
+                                            'x-amzn-requestid': '56cbf4aa-f4ef-5518-9574-a04e0a5f1411',
+                                            'date': 'Mon, 18 Feb 2019 18:41:52 GMT',
+                                            'content-type': 'text/xml', 'mock_sqs_hook-length': '830'},
+                                        'RetryAttempts': 0}}
+
+        self.mock_sqs_hook().get_conn().delete_message_batch.return_value = \
+            {'Successful': [{'Id': '22f67273-4dbc-4c19-83b5-aee71bfeb832'}]}
+        self.mock_sqs_hook().get_conn().receive_message.return_value = message
+
+        result = self.sensor.poke(self.mock_context)
+        self.assertTrue(result)
+
+        context_calls = [call.xcom_push(key='messages', value=message)]
+
+        self.assertTrue(self.mock_context['ti'].method_calls == context_calls, "context call  should be same")
+
+    def test_poke_failed(self):
+        message = {
+            'ResponseMetadata': {'RequestId': '56cbf4aa-f4ef-5518-9574-a04e0a5f1411',
+                                 'HTTPStatusCode': 200,
+                                 'HTTPHeaders': {
+                                     'x-amzn-requestid': '56cbf4aa-f4ef-5518-9574-a04e0a5f1411',
+                                     'date': 'Mon, 18 Feb 2019 18:41:52 GMT',
+                                     'content-type': 'text/xml', 'mock_sqs_hook-length': '830'},
+                                 'RetryAttempts': 0}}
+        self.mock_sqs_hook().get_conn().receive_message.return_value = message
+
+        result = self.sensor.poke(self.mock_context)
+        self.assertFalse(result)
+
+        context_calls = []
+
+        self.assertTrue(self.mock_context['ti'].method_calls == context_calls, "context call  should be same")
+
+    def test_poke_delete_raise_airflow_exception(self):
+        message = {'Messages': [{'MessageId': 'c585e508-2ea0-44c7-bf3e-d1ba0cb87834',
+                                 'ReceiptHandle': 'mockHandle',
+                                 'MD5OfBody': 'e5a9d8684a8edfed460b8d42fd28842f',
+                                 'Body': 'h21'}],
+                   'ResponseMetadata': {'RequestId': '56cbf4aa-f4ef-5518-9574-a04e0a5f1411',
+                                        'HTTPStatusCode': 200,
+                                        'HTTPHeaders': {
+                                            'x-amzn-requestid': '56cbf4aa-f4ef-5518-9574-a04e0a5f1411',
+                                            'date': 'Mon, 18 Feb 2019 18:41:52 GMT',
+                                            'content-type': 'text/xml', 'mock_sqs_hook-length': '830'},
+                                        'RetryAttempts': 0}}
+        self.mock_sqs_hook().get_conn().receive_message.return_value = message
+        self.mock_sqs_hook().get_conn().delete_message_batch.return_value = \
+            {'Failed': [{'Id': '22f67273-4dbc-4c19-83b5-aee71bfeb832'}]}
+
+        with self.assertRaises(AirflowException) as context:
+            self.sensor.poke(self.mock_context)
+
+        self.assertTrue('Delete SQS Messages failed' in context.exception.args[0])
+
+
+if __name__ == '__main__':
+    unittest.main()
