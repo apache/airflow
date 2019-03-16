f84fb26f747c1140da17baf1c0257f825ae0bf2f
morgendave <morgendave@gmail.com>
2019-03-15 21:13:24 -0700

    [AIRFLOW-4092] fix documentation errors

--- airflow/contrib/operators/grpc_operator.py
+++ airflow/contrib/operators/grpc_operator.py
@@ -25,7 +25,32 @@ from airflow.utils.decorators import apply_defaults
 class GrpcOperator(BaseOperator):
     """
     Calls a gRPC endpoint to execute an action
+
+    :param stub_class: The stub client to use for this gRPC call
+    :type stub_class: gRPC stub class generated from proto file
+    :param call_func: The client function name to call the gRPC endpoint
+    :type call_func: gRPC client function name for the endpoint generated from proto file, str
+    :param grpc_conn_id: The connection to run the operator against
+    :type grpc_conn_id: str
+    :param data: The data to pass to the rpc call
+    :type data: A dict with key value pairs as kwargs of the call_func
+    :param auth_type: The auth_type
+    :type grpc_conn_id: str
+    :param interceptors: A list of gRPC interceptor objects to be used on the channel
+    :type interceptors: A list of gRPC interceptor objects, has to be initialized
+    :param custom_connection_func: The customized connection function to return channel object
+    :type custom_connection_func: A python function that returns channel object, take in
+        a connection object, can be a partial function
+    :param response_callback: The callback function to process the response from gRPC call
+    :type response_callback: A python function that process the response from gRPC call,
+        takes in response object and context object, context object can be used to perform
+        push xcom or other after task actions
+    :param streaming: A flag to indicate if the call is a streaming call
+    :type streaming: boolean
+    :param log_response: A flag to indicate if we need to log the response
+    :type log_response: boolean
     """
+
     template_fields = ('stub_class', 'call_func', 'data')
     # hook dispatcher dict, dispatch based on auth_type other than the ones
     # support in the base hook
@@ -44,31 +69,6 @@ class GrpcOperator(BaseOperator):
                  response_callback=None,
                  log_response=False,
                  *args, **kwargs):
-        """
-        :param stub_class: The stub client to use for this gRPC call
-        :type stub_class: gRPC stub class generated from proto file
-        :param call_func: The client function name to call the gRPC endpoint
-        :type call_func: gRPC client function name for the endpoint generated from proto file, str
-        :param grpc_conn_id: The connection to run the operator against
-        :type grpc_conn_id: str
-        :param data: The data to pass to the rpc call
-        :type data: A dict with key value pairs as kwargs of the call_func
-        :param auth_type: The auth_type
-        :type grpc_conn_id: str
-        :param interceptors: A list of gRPC interceptor objects to be used on the channel
-        :type interceptors: A list of gRPC interceptor objects, has to be initialized
-        :param custom_connection_func: The customized connection function to return channel object
-        :type custom_connection_func: A python function that returns channel object, take in
-            a connection object, can be a partial function
-        :param response_callback: The callback function to process the response from gRPC call
-        :type response_callback: A python function that process the response from gRPC call,
-            takes in response object and context object, context object can be used to perform
-            push xcom or other after task actions
-        :param streaming: A flag to indicate if the call is a streaming call
-        :type streaming: boolean
-        :param log_response: A flag to indicate if we need to log the response
-        :type log_response: boolean
-        """
         super(GrpcOperator, self).__init__(*args, **kwargs)
         self.stub_class = stub_class
         self.call_func = call_func
