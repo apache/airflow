apache/spark

[AIRFLOW-3771] Minor refactor securityManager (#4594)

tests/www/test_security.py
@@ -30,7 +30,7 @@ from flask_appbuilder.views import ModelView, BaseView
 
 from sqlalchemy import Column, Integer, String, Date, Float
 
-from airflow.www.security import AirflowSecurityManager, dag_perms
+from airflow.www.security import AirflowSecurityManager, DAG_PERMS
 
 
 logging.basicConfig(format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
@@ -176,7 +176,7 @@ class TestSecurity(unittest.TestCase):
     def test_sync_perm_for_dag(self):
         test_dag_id = 'TEST_DAG'
         self.security_manager.sync_perm_for_dag(test_dag_id)
-        for dag_perm in dag_perms:
+        for dag_perm in DAG_PERMS:
             self.assertIsNotNone(self.security_manager.
                                  find_permission_view_menu(dag_perm, test_dag_id))
 

apache/airflow

tests/www_rbac/test_security.py
@@ -30,7 +30,7 @@ from flask_appbuilder.views import ModelView, BaseView
 
 from sqlalchemy import Column, Integer, String, Date, Float
 
-from airflow.www_rbac.security import AirflowSecurityManager, dag_perms
+from airflow.www_rbac.security import AirflowSecurityManager, DAG_PERMS
 
 
 logging.basicConfig(format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
@@ -176,7 +176,7 @@ class TestSecurity(unittest.TestCase):
     def test_sync_perm_for_dag(self):
         test_dag_id = 'TEST_DAG'
         self.security_manager.sync_perm_for_dag(test_dag_id)
-        for dag_perm in dag_perms:
+        for dag_perm in DAG_PERMS:
             self.assertIsNotNone(self.security_manager.
                                  find_permission_view_menu(dag_perm, test_dag_id))
 