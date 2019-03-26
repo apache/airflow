6075e8988b7c902ca6c4241eddcb2fe37bd10183
Jacob <guzikq@gmail.com>
2019-03-26 15:09:03 +0100

    [AIRFLOW-3894] unstructured connection to webhdfs
    Change nn format

--- airflow/hooks/webhdfs_hook.py
+++ airflow/hooks/webhdfs_hook.py
@@ -57,8 +57,8 @@ class WebHDFSHook(BaseHook):
                 self.log.debug('Trying namenode %s', nn.host)
                 connection_str = self.connection_url(
                     'http://',
-                    '{nn.host}'.format(nn=nn),
-                    '{nn.port}'.format(nn=nn)
+                    nn.host,
+                    nn.port
                 )
                 self.log.debug("Connection to WebHDFS: %s", connection_str)
                 if _kerberos_security_mode:
