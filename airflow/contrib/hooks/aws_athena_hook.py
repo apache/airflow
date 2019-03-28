487dca233848a4ce81d61da634b96d8629e669c7
Bryan <kenshin2004528@gmail.com>
2019-03-29 01:36:05 +0800

    add insert into table

--- airflow/contrib/hooks/aws_athena_hook.py
+++ airflow/contrib/hooks/aws_athena_hook.py
@@ -17,8 +17,15 @@
 # specific language governing permissions and limitations
 # under the License.
 
+import re
 from time import sleep
+
 from airflow.contrib.hooks.aws_hook import AwsHook
+from airflow.contrib.hooks.aws_glue_catalog_hook import AwsGlueCatalogHook
+from airflow.exceptions import AirflowException
+from airflow.hooks.S3_hook import S3Hook
+
+from urllib.parse import urlparse
 
 
 class AWSAthenaHook(AwsHook):
@@ -35,8 +42,9 @@ class AWSAthenaHook(AwsHook):
     FAILURE_STATES = ('FAILED', 'CANCELLED',)
     SUCCESS_STATES = ('SUCCEEDED',)
 
-    def __init__(self, aws_conn_id='aws_default', sleep_time=30, *args, **kwargs):
+    def __init__(self, aws_conn_id='aws_default', region_name=None, sleep_time=30, *args, **kwargs):
         super(AWSAthenaHook, self).__init__(aws_conn_id, **kwargs)
+        self.region_name = region_name
         self.sleep_time = sleep_time
         self.conn = None
 
@@ -47,7 +55,7 @@ class AWSAthenaHook(AwsHook):
         :return: boto3 session
         """
         if not self.conn:
-            self.conn = self.get_client_type('athena')
+            self.conn = self.get_client_type('athena', region_name=self.region_name)
         return self.conn
 
     def run_query(self, query, query_context, result_configuration, client_request_token=None):
@@ -148,3 +156,105 @@ class AWSAthenaHook(AwsHook):
         :return: dict
         """
         return self.conn.stop_query_execution(QueryExecutionId=query_execution_id)
+
+
+class AWSAthenaHelpers(AWSAthenaHook):
+    """
+    The Athena Helpers contains helper methods to execute queries against
+    Athena. The methods can be used directly by operators.
+    """
+
+    def __init__(self, aws_conn_id='aws_default', region_name=None, *args, **kwargs):
+        super(AWSAthenaHelpers, self).__init__(
+            aws_conn_id=aws_conn_id, region_name=region_name, **kwargs)
+        self.region_name = region_name
+        self.s3_hook = None
+        self.glue_hook = None
+
+    def get_s3_hook(self):
+        """
+        check if s3 hook exists already or create one and return it
+        :return: s3 hook
+        """
+        if not self.s3_hook:
+            self.s3_hook = S3Hook(
+                aws_conn_id=self.aws_conn_id, verify=self.verify)
+        return self.s3_hook
+
+    def get_glue_hook(self):
+        """
+        check if glue hook exists already or create one and return it
+        :return: glue hook
+        """
+        if not self.glue_hook:
+            self.glue_hook = AwsGlueCatalogHook(
+                aws_conn_id=self.aws_conn_id, region_name=self.region_name)
+        return self.glue_hook
+
+    def run_insert_into_table(self, src_db, src_table, dst_db, dst_table, mode='error'):
+        """
+        insert data in s3 from the source table to the destination table
+
+        :param src_db: database name of the source table
+        :type src_db: str
+        :param src_table: the source table
+        :type src_table: str
+        :param dst_db: database name of the destination table
+        :type dst_db: str
+        :param dst_table: the destination table
+        :type dst_table: str
+        :param partitioned_by: the partition key
+        :type partitioned_by: list
+        :param mode: specifies the behavior when data or table already exists(default: error)
+                Options include:
+            * `append`: Append contents of this :class:`DataFrame` to existing data.
+            * `overwrite`: Overwrite existing data.
+            * `error`: Throw an exception if data already exists.
+            * `ignore`: Silently ignore this operation if data already exists.
+        :type mode: str
+        :return: boolean
+        """
+
+        modes = ['append', 'overwrite', 'error', 'ignore']
+        if mode not in modes:
+            raise ValueError('mode should be on of "append", "overwrite", "error" or "ignore"')
+
+        if not self.glue_hook:
+            self.get_glue_hook()
+        if not self.s3_hook:
+            self.get_s3_hook()
+
+        src_location = self.glue_hook.get_table_location(src_db, src_table)
+        dst_location = self.glue_hook.get_table_location(dst_db, dst_table)
+
+        src_bucket, src_prefix = S3Hook.parse_s3_url(src_location)
+        dst_bucket, dst_prefix = S3Hook.parse_s3_url(dst_location)
+
+        src_keys = self.s3_hook.list_keys(bucket_name=src_bucket, prefix=src_prefix)
+        dst_keys = self.s3_hook.list_keys(bucket_name=dst_bucket, prefix=dst_prefix)
+
+        if mode == 'overwrite':
+            for key in dst_keys:
+                self.s3_hook.delete_objects(dst_bucket, key)
+
+        elif mode == 'error' and len(dst_keys) > 0:
+                raise AirflowException('The location of destination table is not empty.')
+
+        elif mode == 'ignore' and len(dst_keys) > 0:
+                self.log.info('The location of the destination table is not empty.')
+
+        for key in src_keys:
+            suffix = re.sub(src_prefix + '/', '', key)
+            src_s3_path = 's3://{}/{}'.format(src_bucket, key)
+            dst_s3_path = 's3://{}/{}/{}'.format(dst_bucket, dst_prefix, suffix)
+            self.s3_hook.copy_object(src_s3_path, dst_s3_path)
+
+    @staticmethod
+    def get_prefix(s3url):
+        parsed_url = urlparse(s3url)
+        if not parsed_url.netloc:
+            raise AirflowException('Please provide a bucket_name instead of "%s"' % s3url)
+        else:
+            bucket_name = parsed_url.netloc
+            key = parsed_url.path.strip('/')
+            return bucket_name, key
