e22fea650c46c68960d787dce99f0504effc24a9
Jacob <guzikq@gmail.com>
2019-04-03 11:31:46 +0200

    [AIRFLOW-3888] HA for metastore connection
    Mock return_value edit

--- tests/hooks/test_hive_hook.py
+++ tests/hooks/test_hive_hook.py
@@ -284,7 +284,7 @@ class TestHiveMetastoreHook(HiveEnvironmentTest):
         self.assertIsInstance(self.hook.get_metastore_client(), HMSClient)
 
     @mock.patch("airflow.hooks.hive_hooks.HiveMetastoreHook.get_connection",
-                **{'return_value': [Connection(host="localhost", port=9802)]})
+                return_value = [Connection(host="localhost", port=9802)])
     @mock.patch("airflow.hooks.hive_hooks.socket")
     def test_error_metastore_client(self, socket_mock, _find_vaild_server_mock):
         socket_mock.socket.return_value.connect_ex.return_value = 0
