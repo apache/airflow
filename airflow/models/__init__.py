0043b338a21dca6bc2be4a50a5ef4532e24a63ad
pjvanthof <pjrvanthof@gmail.com>
2019-04-05 15:51:38 +0200

    Remove pyc exclusion

--- airflow/models/__init__.py
+++ airflow/models/__init__.py
@@ -357,9 +357,6 @@ class DagBag(BaseDagBag, LoggingMixin):
         if filepath is None or not os.path.isfile(filepath):
             return found_dags
 
-        if filepath.endswith(".pyc"):
-            filepath = filepath.rstrip("c")
-
         try:
             # This failed before in what may have been a git sync
             # race condition
