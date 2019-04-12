657db824a4ed4fe8a9cef64d23fd71578123da7d
mans2singh <mans2singh@yahoo.com>
2019-04-12 12:34:15 -0400

    [AIRFLOW-4055] - Updated documentation, changed default arguments based on review feedback

--- airflow/contrib/hooks/aws_sqs_hook.py
+++ airflow/contrib/hooks/aws_sqs_hook.py
@@ -32,9 +32,9 @@ class SQSHook(AwsHook):
 
     """
     Create queue using connection object
-    :param name: name of the queue.
-    :type name: str
-    :param attributes: additional attributes for the queue
+    :param queue_name: name of the queue.
+    :type queue_name: str
+    :param attributes: additional attributes for the queue (default: None)
     :type attributes: dict
 
         For details of the attributes parameter see :py:meth:`botocore.client.SQS.create_queue`
@@ -44,19 +44,19 @@ class SQSHook(AwsHook):
 
         For details of the returned value see :py:meth:`botocore.client.SQS.create_queue`
     """
-    def create_queue(self, queue_name, attributes={}):
-        return self.get_conn().create_queue(QueueName=queue_name, Attributes=attributes)
+    def create_queue(self, queue_name, attributes=None):
+        return self.get_conn().create_queue(QueueName=queue_name, Attributes=attributes or {})
 
     """
     Send message to the queue
     :param queue_url: queue url
-    :type name: str
+    :type queue_url: str
     :param message_body: the contents of the message
-    :type name: str
+    :type message_body: str
     :param delay_seconds: seconds to delay the message
-    :type name: int
-    :param message_attributes: additional attributes for the message
-    :type attributes: dict
+    :type delay_seconds: int
+    :param message_attributes: additional attributes for the message (default: None)
+    :type message_attributes: dict
 
         For details of the attributes parameter see :py:meth:`botocore.client.SQS.send_message`
 
@@ -65,8 +65,8 @@ class SQSHook(AwsHook):
 
         For details of the returned value see :py:meth:`botocore.client.SQS.send_message`
     """
-    def send_message(self, queue_url, message_body, delay_seconds=0, message_attributes={}):
+    def send_message(self, queue_url, message_body, delay_seconds=0, message_attributes=None):
         return self.get_conn().send_message(QueueUrl=queue_url,
                                             MessageBody=message_body,
                                             DelaySeconds=delay_seconds,
-                                            MessageAttributes=message_attributes)
+                                            MessageAttributes=message_attributes or {})
