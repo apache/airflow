68013a9e8d67331c558602ac0f2a0799f47d9449
Antoni Smoliński <45817886+antonimaciej@users.noreply.github.com>
2019-04-27 20:48:37 +0200

    [AIRFLOW-4168] Create Google Cloud Video Intelligence Operators (#4985)
     (cherry picked from commit df18b02d2abc98df2ef850f25b82f8202106a147)

--- setup.py
+++ setup.py
@@ -181,6 +181,7 @@ gcp_api = [
     'google-cloud-spanner>=1.7.1',
     'google-cloud-storage~=1.14',
     'google-cloud-translate>=1.3.3',
+    'google-cloud-videointelligence>=1.7.0',
     'google-cloud-vision>=0.35.2',
     'grpcio-gcp>=0.2.2',
     'httplib2~=0.9.2',
