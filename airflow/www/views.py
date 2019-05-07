9c46274d7fc4b76be12af49e8253a8ac4e2dcad0
Xiaodong <xd_deng@hotmail.com>
2019-05-07 13:12:15 +0800

    [AIRFLOW-4459] Fix wrong DAG count in /home page when DAG count is zero (#5235)
    (cherry picked from commit 1755c358d40e515ac91d8a23dad18bade68e6cd1)

--- airflow/www/views.py
+++ airflow/www/views.py
@@ -2220,7 +2220,7 @@ class HomeView(AirflowViewMixin, AdminIndexView):
             search_query=arg_search_query if arg_search_query else '',
             page_size=dags_per_page,
             num_of_pages=num_of_pages,
-            num_dag_from=start + 1,
+            num_dag_from=min(start + 1, num_of_all_dags),
             num_dag_to=min(end, num_of_all_dags),
             num_of_all_dags=num_of_all_dags,
             paging=wwwutils.generate_pages(current_page, num_of_pages,
