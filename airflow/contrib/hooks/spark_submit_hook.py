0abba6e195e2c323f5bfccdd9ba2b9fc250bd42b
Lee Yunsu <yunsou.lee@gmail.com>
2019-06-03 11:32:34 +0900

    remove white space

--- airflow/contrib/hooks/spark_submit_hook.py
+++ airflow/contrib/hooks/spark_submit_hook.py
@@ -186,7 +186,7 @@ class SparkSubmitHook(BaseHook, LoggingMixin):
             extra = conn.extra_dejson
             conn_data['queue'] = extra.get('queue', None)
             conn_data['deploy_mode'] = extra.get('deploy-mode', None)
-            conn_data['spark_home'] = extra.get('spark-home', None)            
+            conn_data['spark_home'] = extra.get('spark-home', None)
             conn_data['namespace'] = extra.get('namespace', 'default')
         except AirflowException:
             self.log.debug(
