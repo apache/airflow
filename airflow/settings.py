518d4ab93be00f46c04c6b2608d2363417f81f88
Philippe Gagnon <12717218+pgagnon@users.noreply.github.com>
2019-06-03 07:11:01 -0400

    [AIRFLOW-4572] Rename prepare_classpath() to prepare_syspath() (#5328)
    (cherry picked from commit 67263d2e4b9ff703e7a12fc99e1f8d3d53ca22fe)

--- airflow/settings.py
+++ airflow/settings.py
@@ -271,7 +271,7 @@ def configure_action_logging():
     pass
 
 
-def prepare_classpath():
+def prepare_syspath():
     """
     Ensures that certain subfolders of AIRFLOW_HOME are on the classpath
     """
@@ -298,7 +298,7 @@ except Exception:
 
 def initialize():
     configure_vars()
-    prepare_classpath()
+    prepare_syspath()
     global LOGGING_CLASS_PATH
     LOGGING_CLASS_PATH = configure_logging()
     configure_adapters()
