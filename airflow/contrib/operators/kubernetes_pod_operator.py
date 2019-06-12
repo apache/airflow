2c6b1ea9201bc4172ebce25b88a0c392cb1acbe5
Ron van Weverwijk <github@weverwijk.net>
2019-06-12 13:09:08 +0200

    [AIRFLOW-4781] Added the ability to specify ports in kubernetesOperator

--- airflow/contrib/operators/kubernetes_pod_operator.py
+++ airflow/contrib/operators/kubernetes_pod_operator.py
@@ -104,6 +104,8 @@ class KubernetesPodOperator(BaseOperator):
                                                  config_file=self.config_file)
             gen = pod_generator.PodGenerator()
 
+            for port in self.ports:
+                gen.add_port(port)
             for mount in self.volume_mounts:
                 gen.add_mount(mount)
             for volume in self.volumes:
@@ -167,6 +169,7 @@ class KubernetesPodOperator(BaseOperator):
                  name,
                  cmds=None,
                  arguments=None,
+                 ports=None,
                  volume_mounts=None,
                  volumes=None,
                  env_vars=None,
@@ -202,6 +205,7 @@ class KubernetesPodOperator(BaseOperator):
         self.startup_timeout_seconds = startup_timeout_seconds
         self.name = name
         self.env_vars = env_vars or {}
+        self.ports = ports or []
         self.volume_mounts = volume_mounts or []
         self.volumes = volumes or []
         self.secrets = secrets or []
