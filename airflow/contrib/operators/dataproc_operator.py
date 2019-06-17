d0325747afd9f87b9f7fa444f0e3b56b9bcc47da
Tomek <turbaszek@gmail.com>
2019-06-17 17:29:16 +0200

    [AIRFLOW-4765] Fix DataProcPigOperator execute method (#5426)

--- airflow/contrib/operators/dataproc_operator.py
+++ airflow/contrib/operators/dataproc_operator.py
@@ -741,7 +741,7 @@ class DataProcPigOperator(DataProcJobBaseOperator):
             self.job_template.add_query(self.query)
         self.job_template.add_variables(self.variables)
 
-        self.execute(context)
+        super(DataProcPigOperator, self).execute(context)
 
 
 class DataProcHiveOperator(DataProcJobBaseOperator):
