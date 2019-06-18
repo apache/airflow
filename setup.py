f17f42b03362a9c9774416fccc30e9aeac539825
RosterIn <48057736+RosterIn@users.noreply.github.com>
2019-06-18 22:38:04 +0300

    [AIRFLOW-4810] Bump supported mysqlclient to <1.5

--- setup.py
+++ setup.py
@@ -244,7 +244,7 @@ kubernetes = ['kubernetes>=3.0.0',
               'cryptography>=2.0.0']
 ldap = ['ldap3>=2.5.1']
 mssql = ['pymssql>=2.1.1']
-mysql = ['mysqlclient>=1.3.6,<1.4']
+mysql = ['mysqlclient>=1.3.6,<1.5']
 oracle = ['cx_Oracle>=5.1.2']
 papermill = ['papermill[all]>=1.0.0',
              'nteract-scrapbook[all]>=0.2.1']
