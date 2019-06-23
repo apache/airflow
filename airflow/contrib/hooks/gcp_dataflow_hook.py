fbfac70b6a162358154fdace0bb20555664d7993
Chaim Turkel <cyturel@gmail.com>
2019-06-23 10:29:04 +0300

    Update airflow/contrib/hooks/gcp_dataflow_hook.py
    Co-Authored-By: Fokko Driesprong <fokko@driesprong.frl>

--- airflow/contrib/hooks/gcp_dataflow_hook.py
+++ airflow/contrib/hooks/gcp_dataflow_hook.py
@@ -31,7 +31,7 @@ from airflow.utils.log.logging_mixin import LoggingMixin
 # This is the default location
 # https://cloud.google.com/dataflow/pipelines/specifying-exec-params
 DEFAULT_DATAFLOW_LOCATION = 'us-central1'
-FAILED_END_STATES = ['JOB_STATE_FAILED', 'JOB_STATE_CANCELLED']
+FAILED_END_STATES = {'JOB_STATE_FAILED', 'JOB_STATE_CANCELLED'}
 SUCCEEDED_END_STATES = ['JOB_STATE_DONE']
 END_STATES = SUCCEEDED_END_STATES + FAILED_END_STATES
 
