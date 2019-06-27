4a46638d4856f99084b2a51a8256ab87e599937b
Mingjie LI <mingjie.li@amadeus.com>
2019-06-27 11:00:20 +0200

    [AIRFLOW-4856] change hard coded run_as_user
    add back remove option

--- airflow/executors/kubernetes_executor.py
+++ airflow/executors/kubernetes_executor.py
@@ -237,6 +237,9 @@ class KubeConfig:
         self.git_sync_container = '{}:{}'.format(
             self.git_sync_container_repository, self.git_sync_container_tag)
 
+        self.git_sync_init_container_name = conf.get(
+            self.kubernetes_section, 'git_sync_init_container_name')
+
         self.git_sync_run_as_user = self._get_security_context_val('git_sync_run_as_user')
         
         # The worker pod may optionally have a  valid Airflow config loaded via a
