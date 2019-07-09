364fd594152070afc9741b53ef5234095b5be2fc
Yuri Bendana <ybendana@sangamo.com>
2019-07-08 18:41:10 -0700

    Rename runcount->run_count, successcount -> success_count

--- airflow/contrib/sensors/triggered_dagrun_sensor.py
+++ airflow/contrib/sensors/triggered_dagrun_sensor.py
@@ -45,35 +45,35 @@ class TriggeredDagRunSensor(BaseSensorOperator):
 
     def poke(self, context):
         with db.create_session() as session:
-            runcount = 0
+            run_count = 0
             ti = context['ti']
             dagrun_ids = ti.xcom_pull(task_ids=self.trigger_task_id)
             if dagrun_ids:
                 ids = dagrun_ids[:2]
                 ids = ids + ['...'] if len(dagrun_ids) > 2 else ids
                 self.log.info('Poking for %s', ','.join(ids))
-                runcount = session.query(DagRun).filter(
+                run_count = session.query(DagRun).filter(
                     DagRun.run_id.in_(dagrun_ids),
                     DagRun.state == State.RUNNING,
                 ).count()
             else:
                 raise AirflowException("No dagrun ids returned by '{}'".format(
                     self.trigger_task_id))
-            self.log.info('runcount=%s', runcount)
-            if runcount == 0:
+            self.log.info('run_count=%s', run_count)
+            if run_count == 0:
                 success_count = session.query(DagRun).filter(
                     DagRun.run_id.in_(dagrun_ids),
                     DagRun.state == State.SUCCESS,
                 ).count()
-                if successcount == 0:
+                if success_count == 0:
                     raise AirflowException("No dagruns completed successfully.")
                 if self.sensor_rule == TriggerRule.ONE_SUCCESS:
                     pass
                 elif self.sensor_rule == TriggerRule.ALL_SUCCESS:
-                    if successcount != len(dagrun_ids):
+                    if success_count != len(dagrun_ids):
                         raise AirflowException(
                             "All dagruns did not complete successfully.")
                 else:
                     raise AirflowException(
                         "sensor rule '{}' is not supported".format(self.sensor_rule))
-            return runcount == 0
+            return run_count == 0
