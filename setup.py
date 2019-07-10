ee5caf868a888e80cf0fd95da2ebeb39d94b873a
Anders Bennehag <anders@bennehag.com>
2019-07-10 13:12:04 -0400

    [AIRFLOW-4901] Relaxes jinja2 version requirement
    Fixes https://issues.apache.org/jira/browse/AIRFLOW-4901

--- setup.py
+++ setup.py
@@ -351,7 +351,7 @@ def do_setup():
             'gunicorn>=19.5.0, <20.0',
             'iso8601>=0.1.12',
             'json-merge-patch==0.2',
-            'jinja2>=2.10.1, <2.11.0',
+            'jinja2>=2.10.0, <2.11.0',
             'lazy_object_proxy~=1.3',
             'markdown>=2.5.2, <3.0',
             'pandas>=0.17.1, <1.0.0',
