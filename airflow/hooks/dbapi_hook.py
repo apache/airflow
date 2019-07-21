304bae9ad3cff265f6e8021342e9b6aa210a7da7
eladkal <45845474+eladkal@users.noreply.github.com>
2019-07-21 22:28:24 +0300

    [AIRFLOW-3189] - fix DbAPI get_uri returns invalid uri if schema is None
    Use conn.schema only if it's set

--- airflow/hooks/dbapi_hook.py
+++ airflow/hooks/dbapi_hook.py
@@ -68,8 +68,11 @@ class DbApiHook(BaseHook):
         host = conn.host
         if conn.port is not None:
             host += ':{port}'.format(port=conn.port)
-        return '{conn.conn_type}://{login}{host}/{conn.schema}'.format(
-            conn=conn, login=login, host=host)
+        uri = '{conn.conn_type}://{login}{host}'.format(
+            conn=conn, login=login, host=host)    
+        if conn.schema:
+            uri +='/{schema}'.format(schema=conn.schema)
+        return uri
 
     def get_sqlalchemy_engine(self, engine_kwargs=None):
         if engine_kwargs is None:
