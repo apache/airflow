76e0dcd90ba5ccb8c4df4f27d26335730a8920d4
Junda Yang <youngyjd@gmail.com>
2019-07-24 16:45:07 -0700

    nit

--- airflow/models.py
+++ airflow/models.py
@@ -1854,19 +1854,19 @@ class SkipMixin(object):
 
         return all_downstream
 
-    def find_all_downstream_skippable(self, task, blacklisted_ti_reprs=None):
-        if blacklisted_ti_reprs is None:
-            blacklisted_ti_reprs = set()
+    def find_all_downstream_skippable(self, task, processed_tis_repr=None):
+        if processed_tis_repr is None:
+            processed_tis_repr = set()
 
         immediate_downstream = task.downstream_list
         all_downstream = []
         for downstream_task in immediate_downstream:
             if downstream_task.trigger_rule in ['all_success', 'all_failed'] \
-                    and repr(downstream_task) not in blacklisted_ti_reprs:
+                    and repr(downstream_task) not in processed_tis_repr:
                 all_downstream.append(downstream_task)
-                blacklisted_ti_reprs.add(repr(downstream_task))
+                processed_tis_repr.add(repr(downstream_task))
         for downstream_task in all_downstream:
-            all_downstream.extend(self.find_all_downstream_skippable(downstream_task, blacklisted_ti_reprs))
+            all_downstream.extend(self.find_all_downstream_skippable(downstream_task, processed_tis_repr))
             
         return all_downstream
 
