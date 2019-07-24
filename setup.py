1120b12ebed94825c12ecb9b59dfbd2b82227b15
Qingping Hou <dave2008713@gmail.com>
2019-07-24 05:19:20 -0700

    [AIRFLOW-5021] move gitpython into setup_requires (#5640)
    Since this dependency is only used in setup.py
    
    (cherry picked from commit 32847180b68def10422649725ce0c714119d9305)

--- setup.py
+++ setup.py
@@ -339,7 +339,6 @@ def do_setup():
             'flask-wtf>=0.14.2, <0.15',
             'funcsigs==1.0.0',
             'future>=0.16.0, <0.17',
-            'gitpython>=2.0.2',
             'gunicorn>=19.5.0, <20.0',
             'iso8601>=0.1.12',
             'json-merge-patch==0.2',
@@ -366,6 +365,7 @@ def do_setup():
         ],
         setup_requires=[
             'docutils>=0.14, <1.0',
+            'gitpython>=2.0.2',
         ],
         extras_require={
             'all': devel_all,
