9faad59b74e7d5f5a015264bfe5d6da9c9c7b465
wayne.morris <wayne.morris@modmed.com>
2019-07-29 17:40:52 -0400

    Updated salesforce hook make_query method to resolve variable.

--- airflow/contrib/hooks/salesforce_hook.py
+++ airflow/contrib/hooks/salesforce_hook.py
@@ -87,7 +87,7 @@ class SalesforceHook(BaseHook):
         conn = self.get_conn()
 
         self.log.info("Querying for all objects")
-        query_results = self.sf.query_all(query, include_deleted=include_deleted, **kwargs)
+        query_results = conn.query_all(query, include_deleted=include_deleted, **kwargs)
 
         self.log.info("Received results: Total size: %s; Done: %s",
                       query_results['totalSize'], query_results['done'])
