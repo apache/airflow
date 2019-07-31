b0f3c1a3b42413beaa55c976cbb3e971f0c3aa0e
Esfahan <calito0131@gmail.com>
2019-07-31 16:30:06 +0900

    Added npm to apt-get install

--- scripts/ci/kubernetes/docker/compile.sh
+++ scripts/ci/kubernetes/docker/compile.sh
@@ -27,6 +27,6 @@ apt-get install -y --no-install-recommends curl gnupg2
 curl -sL https://deb.nodesource.com/setup_8.x | bash -
 
 apt-get update
-apt-get install -y --no-install-recommends git nodejs
+apt-get install -y --no-install-recommends git nodejs npm
 pip install GitPython
 python setup.py compile_assets sdist -q
