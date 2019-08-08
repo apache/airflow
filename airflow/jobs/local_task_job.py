703b26be55c72f23e2b053461123f9ba7345dba8
Kevin Yang <kevin.yang@airbnb.com>
2019-08-07 23:59:53 -0700

    [AIRFLOW-4956] Fix LocalTaskJob heartbeat log spamming (#5589)
    (cherry picked from commit e07e30460cfbec59a6f9729004e0e9f78a9801fe)

--- airflow/jobs/local_task_job.py
+++ airflow/jobs/local_task_job.py
@@ -26,8 +26,6 @@ import os
 import signal
 import time
 
-from sqlalchemy.exc import OperationalError
-
 from airflow.configuration import conf
 from airflow.exceptions import AirflowException
 from airflow.settings import Stats
@@ -95,7 +93,6 @@ class LocalTaskJob(BaseJob):
         try:
             self.task_runner.start()
 
-            last_heartbeat_time = time.time()
             heartbeat_time_limit = conf.getint('scheduler',
                                                'scheduler_zombie_task_threshold')
             while True:
@@ -107,16 +104,8 @@ class LocalTaskJob(BaseJob):
 
                 # Periodically heartbeat so that the scheduler doesn't think this
                 # is a zombie
-                try:
-                    self.heartbeat()
-                    last_heartbeat_time = time.time()
-                except OperationalError:
-                    Stats.incr('local_task_job_heartbeat_failure', 1, 1)
-                    self.log.exception(
-                        "Exception while trying to heartbeat! Sleeping for %s seconds",
-                        self.heartrate
-                    )
-                    time.sleep(self.heartrate)
+                last_heartbeat_time = time.time()
+                self.heartbeat()
 
                 # If it's been too long since we've heartbeat, then it's possible that
                 # the scheduler rescheduled this task, so kill launched processes.
@@ -128,6 +117,13 @@ class LocalTaskJob(BaseJob):
                                            "exceeded limit ({}s)."
                                            .format(time_since_last_heartbeat,
                                                    heartbeat_time_limit))
+
+                if time_since_last_heartbeat < self.heartrate:
+                    sleep_for = self.heartrate - time_since_last_heartbeat
+                    self.log.warning("Time since last heartbeat(%.2f s) < heartrate(%s s)"
+                                     ", sleeping for %s s", time_since_last_heartbeat,
+                                     self.heartrate, sleep_for)
+                    time.sleep(sleep_for)
         finally:
             self.on_kill()
 
