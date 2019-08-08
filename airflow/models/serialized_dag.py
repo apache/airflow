bf11bba43a792b102e6a223dce5cae8027714454
coufon <zhoufang@google.com>
2019-08-08 12:15:57 -0700

    use create_session instead of provide_session

--- airflow/models/serialized_dag.py
+++ airflow/models/serialized_dag.py
@@ -25,8 +25,7 @@ from sqlalchemy import Column, Index, Integer, String, Text, and_
 from sqlalchemy.sql import exists
 
 from airflow.models.base import Base, ID_LEN
-from airflow.utils import timezone
-from airflow.utils.db import provide_session
+from airflow.utils import db, timezone
 from airflow.utils.sqlalchemy import UtcDateTime
 
 
@@ -36,8 +35,22 @@ if TYPE_CHECKING:
 
 
 class SerializedDagModel(Base):
-    """A database table for serialized DAGs."""
+    """A table for serialized DAGs.
 
+    serialized_dag table is a snapshot of DAG files synchronized by scheduler.
+    This feature is controlled by:
+        [core] dagcached = False: enable this feature
+        [core] dagcached_min_update_interval = 30 (s):
+            serialized DAGs are updated in DB when a file gets processed by scheduler,
+            to reduce DB write rate, there is a minimal interval of updating serialized DAGs.
+        [scheduler] dag_dir_list_interval = 300 (s):
+            interval of deleting serialized DAGs in DB when the files are deleted, suggest
+            to use a smaller interval such as 60
+
+    It is used by webserver to load dagbags when dagcached=True. Because reading from
+    database is lightweight compared to importing from files, it solves the webserver
+    scalability issue.
+    """
     __tablename__ = 'serialized_dag'
 
     dag_id = Column(String(ID_LEN), primary_key=True)
@@ -74,32 +87,32 @@ class SerializedDagModel(Base):
             hashlib.sha1(full_filepath.encode('utf-8')).hexdigest(), 16))
 
     @classmethod
-    @provide_session
-    def write_dag(cls, dag: 'DAG', min_update_interval: Optional[int] = None, session=None):
+    def write_dag(cls, dag: 'DAG', min_update_interval: Optional[int] = None):
         """Serializes a DAG and writes it into database.
 
         :param dag: a DAG to be written into database
         :param min_update_interval: minimal interval in seconds to update serialized DAG
         """
-        if min_update_interval is not None:
-            result = session.query(cls.last_updated).filter(
-                cls.dag_id == dag.dag_id).first()
-            if result is not None and (
-                    timezone.utcnow() - result.last_updated).total_seconds() < min_update_interval:
-                return
-        session.merge(cls(dag))
-        session.commit()
+        with db.create_session() as session:
+            if min_update_interval is not None:
+                result = session.query(cls.last_updated).filter(
+                    cls.dag_id == dag.dag_id).first()
+                if result is not None and (
+                        timezone.utcnow() - result.last_updated).total_seconds() < min_update_interval:
+                    return
+            session.merge(cls(dag))
 
     @classmethod
-    @provide_session
-    def read_all_dags(cls, session=None) -> Dict[str, 'SerializedDAG']:
+    def read_all_dags(cls) -> Dict[str, 'SerializedDAG']:
         """Reads all DAGs in serialized_dag table.
 
         :param returns: a dict of DAGs read from database
         """
         from airflow.dag.serialization import Serialization
 
-        serialized_dags = session.query(cls.dag_id, cls.data).all()
+        with db.create_session() as session:
+            serialized_dags = session.query(cls.dag_id, cls.data).all()
+
         dags = {}
         for dag_id, data in serialized_dags:
             dag = Serialization.from_json(data)  # type: Any
@@ -109,35 +122,34 @@ class SerializedDagModel(Base):
         return dags
 
     @classmethod
-    @provide_session
-    def remove_dag(cls, dag_id: str, session=None):
+    def remove_dag(cls, dag_id: str):
         """Deletes a DAG with given dag_id.
 
         :param dag_id: dag_id to be deleted
         """
-        session.execute(cls.__table__.delete().where(cls.dag_id == dag_id))
-        session.commit()
+        with db.create_session() as session:
+            session.execute(cls.__table__.delete().where(cls.dag_id == dag_id))
 
     @classmethod
-    @provide_session
-    def remove_deleted_dags(cls, alive_dag_filelocs: List[str], session=None):
+    def remove_deleted_dags(cls, alive_dag_filelocs: List[str]):
         """Deletes DAGs not included in alive_dag_filelocs.
 
         :param alive_dag_ids: file paths of alive DAGs
         """
         alive_fileloc_hashes = [
             cls.dag_fileloc_hash(fileloc) for fileloc in alive_dag_filelocs]
-        session.execute(
-            cls.__table__.delete().where(
-                and_(cls.fileloc_hash.notin_(alive_fileloc_hashes),
-                     cls.fileloc.notin_(alive_dag_filelocs))))
-        session.commit()
+
+        with db.create_session() as session:
+            session.execute(
+                cls.__table__.delete().where(
+                    and_(cls.fileloc_hash.notin_(alive_fileloc_hashes),
+                         cls.fileloc.notin_(alive_dag_filelocs))))
 
     @classmethod
-    @provide_session
-    def has_dag(cls, dag_id: str, session=None) -> bool:
+    def has_dag(cls, dag_id: str) -> bool:
         """Checks a DAG exist in serialized_dag table.
 
         :param dag_id: the DAG to check
         """
-        return session.query(exists().where(cls.dag_id == dag_id)).scalar()
+        with db.create_session() as session:
+            return session.query(exists().where(cls.dag_id == dag_id)).scalar()
