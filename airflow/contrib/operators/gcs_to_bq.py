bf92472a7ed6defbe36fe3f096cef6d2352c0c29
Bingqin Zhou <bingqinz@wepay.com>
2019-08-09 11:58:19 -0700

    Change default value to be True in comment.

--- airflow/contrib/operators/gcs_to_bq.py
+++ airflow/contrib/operators/gcs_to_bq.py
@@ -133,7 +133,7 @@ class GoogleCloudStorageToBigQueryOperator(BaseOperator):
         Not applicable for external tables.
     :type cluster_fields: list[str]
     :param autodetect: [Optional] Indicates if we should automatically infer the
-        options and schema for CSV and JSON sources. (Default: ``False``).
+        options and schema for CSV and JSON sources. (Default: ``True``).
         Parameter must be setted to True if 'schema_fields' and 'schema_object' are undefined.
         It is suggested to set to True if table are create outside of Airflow.
     :type autodetect: bool
