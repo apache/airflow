f7ab8a99631bc6fa542fab5eb7e5f0e74de8959d
Tomek Urbaszel <turbaszek@gmail.com>
2019-08-23 12:23:31 +0200

    [AIRFLOW-4771] Initialization of hook in the GCP operator constructors
    Hook should be initalized in execute method not in operator's constructor.

--- airflow/gcp/operators/vision.py
+++ airflow/gcp/operators/vision.py
@@ -102,11 +102,11 @@ class CloudVisionProductSetCreateOperator(BaseOperator):
         self.timeout = timeout
         self.metadata = metadata
         self.gcp_conn_id = gcp_conn_id
-        self._hook = CloudVisionHook(gcp_conn_id=self.gcp_conn_id)
 
     def execute(self, context):
+        hook = CloudVisionHook(gcp_conn_id=self.gcp_conn_id)
         try:
-            return self._hook.create_product_set(
+            return hook.create_product_set(
                 location=self.location,
                 project_id=self.project_id,
                 product_set=self.product_set,
@@ -176,10 +176,10 @@ class CloudVisionProductSetGetOperator(BaseOperator):
         self.timeout = timeout
         self.metadata = metadata
         self.gcp_conn_id = gcp_conn_id
-        self._hook = CloudVisionHook(gcp_conn_id=self.gcp_conn_id)
 
     def execute(self, context):
-        return self._hook.get_product_set(
+        hook = CloudVisionHook(gcp_conn_id=self.gcp_conn_id)
+        return hook.get_product_set(
             location=self.location,
             product_set_id=self.product_set_id,
             project_id=self.project_id,
@@ -265,10 +265,10 @@ class CloudVisionProductSetUpdateOperator(BaseOperator):
         self.timeout = timeout
         self.metadata = metadata
         self.gcp_conn_id = gcp_conn_id
-        self._hook = CloudVisionHook(gcp_conn_id=self.gcp_conn_id)
 
     def execute(self, context):
-        return self._hook.update_product_set(
+        hook = CloudVisionHook(gcp_conn_id=self.gcp_conn_id)
+        return hook.update_product_set(
             location=self.location,
             product_set_id=self.product_set_id,
             project_id=self.project_id,
@@ -335,10 +335,10 @@ class CloudVisionProductSetDeleteOperator(BaseOperator):
         self.timeout = timeout
         self.metadata = metadata
         self.gcp_conn_id = gcp_conn_id
-        self._hook = CloudVisionHook(gcp_conn_id=self.gcp_conn_id)
 
     def execute(self, context):
-        self._hook.delete_product_set(
+        hook = CloudVisionHook(gcp_conn_id=self.gcp_conn_id)
+        hook.delete_product_set(
             location=self.location,
             product_set_id=self.product_set_id,
             project_id=self.project_id,
@@ -415,11 +415,11 @@ class CloudVisionProductCreateOperator(BaseOperator):
         self.timeout = timeout
         self.metadata = metadata
         self.gcp_conn_id = gcp_conn_id
-        self._hook = CloudVisionHook(gcp_conn_id=self.gcp_conn_id)
 
     def execute(self, context):
+        hook = CloudVisionHook(gcp_conn_id=self.gcp_conn_id)
         try:
-            return self._hook.create_product(
+            return hook.create_product(
                 location=self.location,
                 product=self.product,
                 project_id=self.project_id,
@@ -492,10 +492,10 @@ class CloudVisionProductGetOperator(BaseOperator):
         self.timeout = timeout
         self.metadata = metadata
         self.gcp_conn_id = gcp_conn_id
-        self._hook = CloudVisionHook(gcp_conn_id=self.gcp_conn_id)
 
     def execute(self, context):
-        return self._hook.get_product(
+        hook = CloudVisionHook(gcp_conn_id=self.gcp_conn_id)
+        return hook.get_product(
             location=self.location,
             product_id=self.product_id,
             project_id=self.project_id,
@@ -592,10 +592,10 @@ class CloudVisionProductUpdateOperator(BaseOperator):
         self.timeout = timeout
         self.metadata = metadata
         self.gcp_conn_id = gcp_conn_id
-        self._hook = CloudVisionHook(gcp_conn_id=self.gcp_conn_id)
 
     def execute(self, context):
-        return self._hook.update_product(
+        hook = CloudVisionHook(gcp_conn_id=self.gcp_conn_id)
+        return hook.update_product(
             product=self.product,
             location=self.location,
             product_id=self.product_id,
@@ -667,10 +667,10 @@ class CloudVisionProductDeleteOperator(BaseOperator):
         self.timeout = timeout
         self.metadata = metadata
         self.gcp_conn_id = gcp_conn_id
-        self._hook = CloudVisionHook(gcp_conn_id=self.gcp_conn_id)
 
     def execute(self, context):
-        self._hook.delete_product(
+        hook = CloudVisionHook(gcp_conn_id=self.gcp_conn_id)
+        hook.delete_product(
             location=self.location,
             product_id=self.product_id,
             project_id=self.project_id,
