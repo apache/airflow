feb09dd1dccb51b029119d031d4f90aa93499a00
Kamil Breguła <kamil.bregula@polidea.com>
2019-08-24 12:51:40 +0200

    [AIRFLOW-5304] Fix extra links in BigQueryOperator for task with multiple queriess

--- airflow/contrib/operators/bigquery_operator.py
+++ airflow/contrib/operators/bigquery_operator.py
@@ -45,6 +45,30 @@ class BigQueryConsoleLink(BaseOperatorLink):
             job_id=job_id) if job_id else ''
 
 
+class BigQueryConsoleIndexableLink(BaseOperatorLink):
+    """
+    Helper class for constructing BigQuery link.
+    """
+
+    def __init__(self, index) -> None:
+        super().__init__()
+        self.index = index
+
+    @property
+    def name(self) -> str:
+        return 'BigQuery Console #{index}'.format(index=self.index + 1)
+
+    def get_link(self, operator, dttm):
+        ti = TaskInstance(task=operator, execution_date=dttm)
+        job_ids = ti.xcom_pull(task_ids=operator.task_id, key='job_id')
+        if not job_ids:
+            return None
+        if len(job_ids) < self.index:
+            return None
+        job_id = job_ids[self.index]
+        return 'https://console.cloud.google.com/bigquery?j={job_id}'.format(job_id=job_id)
+
+
 # pylint: disable=too-many-instance-attributes
 class BigQueryOperator(BaseOperator):
     """
@@ -142,9 +166,18 @@ class BigQueryOperator(BaseOperator):
     template_ext = ('.sql', )
     ui_color = '#e4f0e8'
 
-    operator_extra_links = (
-        BigQueryConsoleLink(),
-    )
+    @property
+    def operator_extra_links(self):
+        """
+        Return operator extra links
+        """
+        if isinstance(self.sql, str):
+            return (
+                BigQueryConsoleLink(),
+            )
+        return (
+            BigQueryConsoleIndexableLink(i) for i, _ in enumerate(self.sql)
+        )
 
     # pylint: disable=too-many-arguments, too-many-locals
     @apply_defaults
