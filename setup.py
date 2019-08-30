3c25166f9f48a854fa91cf5852a6571277ad7006
kaxil <kaxilnaik@apache.org>
2019-08-30 23:16:03 +0100

    [AIRFLOW-5240] Latest version of Kombu is breaking airflow for py2

--- setup.py
+++ setup.py
@@ -159,6 +159,7 @@ celery = [
     'celery~=4.3',
     'flower>=0.7.3, <1.0',
     'tornado>=4.2.0, <6.0',  # Dep of flower. Pin to a version that works on Py3.5.2
+    'kombu==4.6.3'
 ]
 cgroups = [
     'cgroupspy>=0.1.4',
