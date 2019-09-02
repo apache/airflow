a7902739887de9200853c6f2e88fc85fb1c1e747
Tomek Urbaszek <tomasz.urbaszek@polidea.com>
2019-09-02 11:35:11 +0200

    fixup! [AIRFLOW-5347] Add system tests for GoogleCloudStorage

--- airflow/contrib/example_dags/example_gcs.py
+++ airflow/contrib/example_dags/example_gcs.py
@@ -35,46 +35,99 @@ from airflow.contrib.operators.gcs_delete_operator import (
     GoogleCloudStorageDeleteOperator,
 )
 from airflow.operators.local_to_gcs import FileToGoogleCloudStorageOperator
+from airflow.operators.gcs_to_gcs import GoogleCloudStorageToGoogleCloudStorageOperator
+from airflow.contrib.operators.gcs_acl_operator import (
+    GoogleCloudStorageBucketCreateAclEntryOperator,
+    GoogleCloudStorageObjectCreateAclEntryOperator,
+)
 
 default_args = {"start_date": airflow.utils.dates.days_ago(1)}
 
-BUCKET = os.environ.get("GCP_GCS_BUCKET", "test-gcs-example-bucket")
-PROJECT_ID = os.environ.get("GCP_PROJECT_ID", "an-id")
+# [START howto_operator_gcs_acl_args_common]
+PROJECT_ID = os.environ.get("GCP_PROJECT_ID", "example-id")
+BUCKET_1 = os.environ.get("GCP_GCS_BUCKET_1", "test-gcs-example-bucket")
+GCS_ACL_ENTITY = os.environ.get("GCS_ACL_ENTITY", "example-entity")
+GCS_ACL_BUCKET_ROLE = "OWNER"
+GCS_ACL_OBJECT_ROLE = "OWNER"
+# [END howto_operator_gcs_acl_args_common]
 
-PATH_TO_FILE = os.environ.get("GCS_UPLOAD_FILE_PATH", "test-gcs-example.txt")
-PATH_TO_SAVED_FILE = os.environ.get("PATH_TO_SAVED_FILE", "test-gcs-example-download.txt")
+BUCKET_2 = os.environ.get("GCP_GCS_BUCKET_1", "test-gcs-example-bucket-2")
 
-BUCKET_FILE_LOCATION = PATH_TO_FILE.rpartition("/")[-1]
+PATH_TO_UPLOAD_FILE = os.environ.get(
+    "GCP_GCS_PATH_TO_UPLOAD_FILE", "test-gcs-example.txt"
+)
+PATH_TO_SAVED_FILE = os.environ.get(
+    "GCP_GCS_PATH_TO_SAVED_FILE", "test-gcs-example-download.txt"
+)
+
+BUCKET_FILE_LOCATION = PATH_TO_UPLOAD_FILE.rpartition("/")[-1]
 
 
 with models.DAG(
     "example_gcs", default_args=default_args, schedule_interval=None
 ) as dag:
-    create_bucket = GoogleCloudStorageCreateBucketOperator(
-        task_id="create_bucket", bucket_name=BUCKET, project_id=PROJECT_ID
+    create_bucket1 = GoogleCloudStorageCreateBucketOperator(
+        task_id="create_bucket1", bucket_name=BUCKET_1, project_id=PROJECT_ID
     )
 
-    list_buckets = GoogleCloudStorageListOperator(task_id="list_buckets", bucket=BUCKET)
+    create_bucket2 = GoogleCloudStorageCreateBucketOperator(
+        task_id="create_bucket2", bucket_name=BUCKET_2, project_id=PROJECT_ID
+    )
+
+    list_buckets = GoogleCloudStorageListOperator(
+        task_id="list_buckets", bucket=BUCKET_1
+    )
 
     print_output = BashOperator(
         task_id="print_output",
-        bash_command="echo \"{{ task_instance.xcom_pull('list_buckets') }}\""
+        bash_command="echo \"{{ task_instance.xcom_pull('list_buckets') }}\"",
     )
 
     upload_file = FileToGoogleCloudStorageOperator(
-        task_id="upload_file", src=PATH_TO_FILE, dst=BUCKET_FILE_LOCATION, bucket=BUCKET
+        task_id="upload_file",
+        src=PATH_TO_UPLOAD_FILE,
+        dst=BUCKET_FILE_LOCATION,
+        bucket=BUCKET_1,
     )
+
+    # [START howto_operator_gcs_bucket_create_acl_entry_task]
+    gcs_bucket_create_acl_entry_task = GoogleCloudStorageBucketCreateAclEntryOperator(
+        bucket=BUCKET_1,
+        entity=GCS_ACL_ENTITY,
+        role=GCS_ACL_BUCKET_ROLE,
+        task_id="gcs_bucket_create_acl_entry_task",
+    )
+    # [END howto_operator_gcs_bucket_create_acl_entry_task]
+
+    # [START howto_operator_gcs_object_create_acl_entry_task]
+    gcs_object_create_acl_entry_task = GoogleCloudStorageObjectCreateAclEntryOperator(
+        bucket=BUCKET_1,
+        object_name=BUCKET_FILE_LOCATION,
+        entity=GCS_ACL_ENTITY,
+        role=GCS_ACL_OBJECT_ROLE,
+        task_id="gcs_object_create_acl_entry_task",
+    )
+    # [END howto_operator_gcs_object_create_acl_entry_task]
 
     download_file = GoogleCloudStorageDownloadOperator(
         task_id="download_file",
         object_name=BUCKET_FILE_LOCATION,
-        bucket=BUCKET,
+        bucket=BUCKET_1,
         filename=PATH_TO_SAVED_FILE,
     )
 
+    copy_file = GoogleCloudStorageToGoogleCloudStorageOperator(
+        task_id="copy_file",
+        source_bucket=BUCKET_1,
+        source_object=BUCKET_FILE_LOCATION,
+        destination_bucket=BUCKET_2,
+        destination_object=BUCKET_FILE_LOCATION,
+    )
     delete_files = GoogleCloudStorageDeleteOperator(
-        task_id="delete_files", bucket_name=BUCKET, prefix=""
+        task_id="delete_files", bucket_name=BUCKET_1, prefix=""
     )
 
-    create_bucket >> list_buckets >> print_output
-    create_bucket >> upload_file >> download_file >> delete_files
+    [create_bucket1, create_bucket2] >> list_buckets >> print_output
+    [create_bucket1, create_bucket2] >> upload_file
+    upload_file >> [download_file, copy_file]
+    upload_file >> gcs_bucket_create_acl_entry_task >> gcs_object_create_acl_entry_task >> delete_files
--- tests/contrib/operators/test_gcs_system_helper.py
+++ tests/contrib/operators/test_gcs_system_helper.py
@@ -21,22 +21,25 @@ import os
 
 from tests.contrib.utils.logging_command_executor import LoggingCommandExecutor
 
-PATH_TO_FILE = os.environ.get("GCS_UPLOAD_FILE_PATH", "test-gcs-example.txt")
-PATH_TO_SAVED_FILE = os.environ.get("PATH_TO_SAVED_FILE", "test-gcs-example-download.txt")
-BUCKET = os.environ.get("GCP_GCS_BUCKET", "test-gcs-example-bucket")
+BUCKET_1 = os.environ.get("GCP_GCS_BUCKET_1", "test-gcs-example-bucket")
+BUCKET_2 = os.environ.get("GCP_GCS_BUCKET_1", "test-gcs-example-bucket-2")
+
+PATH_TO_UPLOAD_FILE = os.environ.get("GCP_GCS_PATH_TO_UPLOAD_FILE", "test-gcs-example.txt")
+PATH_TO_SAVED_FILE = os.environ.get("GCP_GCS_PATH_TO_SAVED_FILE", "test-gcs-example-download.txt")
 
 
 class GcsSystemTestHelper(LoggingCommandExecutor):
     @staticmethod
     def create_test_file():
         # Create test file for upload
-        with open(PATH_TO_FILE, "w+") as file:
+        with open(PATH_TO_UPLOAD_FILE, "w+") as file:
             file.writelines(["This is a test file"])
 
     @staticmethod
     def remove_test_files():
-        os.remove(PATH_TO_FILE)
+        os.remove(PATH_TO_UPLOAD_FILE)
         os.remove(PATH_TO_SAVED_FILE)
 
     def remove_bucket(self):
-        self.execute_cmd(["gsutil", "rm", "-r", "gs://{bucket}".format(bucket=BUCKET)])
+        self.execute_cmd(["gsutil", "rm", "-r", "gs://{bucket}".format(bucket=BUCKET_1)])
+        self.execute_cmd(["gsutil", "rm", "-r", "gs://{bucket}".format(bucket=BUCKET_2)])
