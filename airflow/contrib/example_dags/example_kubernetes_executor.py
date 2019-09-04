18215bdc522d7335fe18fce7dedc7da8a28a33f4
eladkal <45845474+eladkal@users.noreply.github.com>
2019-09-04 20:22:06 +0300

    [AIRFLOW - 5398] Update contrib example DAGs to context manager

--- airflow/contrib/example_dags/example_kubernetes_executor.py
+++ airflow/contrib/example_dags/example_kubernetes_executor.py
@@ -22,82 +22,89 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.models import DAG
 import os
 
-args = {
+default_args = {
     'owner': 'airflow',
     'start_date': airflow.utils.dates.days_ago(2)
 }
 
-dag = DAG(
-    dag_id='example_kubernetes_executor', default_args=args,
+
+with DAG(
+    dag_id='example_kubernetes_executor',
+    default_args=default_args,
     schedule_interval=None
-)
+) as dag:
 
-affinity = {
-    'podAntiAffinity': {
-        'requiredDuringSchedulingIgnoredDuringExecution': [
-            {
-                'topologyKey': 'kubernetes.io/hostname',
-                'labelSelector': {
-                    'matchExpressions': [
-                        {
-                            'key': 'app',
-                            'operator': 'In',
-                            'values': ['airflow']
-                        }
-                    ]
+    affinity = {
+        'podAntiAffinity': {
+            'requiredDuringSchedulingIgnoredDuringExecution': [
+                {
+                    'topologyKey': 'kubernetes.io/hostname',
+                    'labelSelector': {
+                        'matchExpressions': [
+                            {
+                                'key': 'app',
+                                'operator': 'In',
+                                'values': ['airflow']
+                            }
+                        ]
+                    }
                 }
-            }
-        ]
+            ]
+        }
     }
-}
 
-tolerations = [{
-    'key': 'dedicated',
-    'operator': 'Equal',
-    'value': 'airflow'
-}]
-
-
-def print_stuff():
-    print("stuff!")
+    tolerations = [{
+        'key': 'dedicated',
+        'operator': 'Equal',
+        'value': 'airflow'
+    }]
 
 
-def use_zip_binary():
-    rc = os.system("zip")
-    assert rc == 0
+    def print_stuff():
+        print("stuff!")
 
 
-# You don't have to use any special KubernetesExecutor configuration if you don't want to
-start_task = PythonOperator(
-    task_id="start_task", python_callable=print_stuff, dag=dag
-)
+    def use_zip_binary():
+        rc = os.system("zip")
+        assert rc == 0
 
-# But you can if you want to
-one_task = PythonOperator(
-    task_id="one_task", python_callable=print_stuff, dag=dag,
-    executor_config={"KubernetesExecutor": {"image": "airflow/ci:latest"}}
-)
 
-# Use the zip binary, which is only found in this special docker image
-two_task = PythonOperator(
-    task_id="two_task", python_callable=use_zip_binary, dag=dag,
-    executor_config={"KubernetesExecutor": {"image": "airflow/ci_zip:latest"}}
-)
+    # You don't have to use any special KubernetesExecutor configuration if you don't want to
+    start_task = PythonOperator(
+        task_id="start_task",
+        python_callable=print_stuff
+    )
 
-# Limit resources on this operator/task with node affinity & tolerations
-three_task = PythonOperator(
-    task_id="three_task", python_callable=print_stuff, dag=dag,
-    executor_config={
-        "KubernetesExecutor": {"request_memory": "128Mi",
-                               "limit_memory": "128Mi",
-                               "tolerations": tolerations,
-                               "affinity": affinity}}
-)
+    # But you can if you want to
+    one_task = PythonOperator(
+        task_id="one_task",
+        python_callable=print_stuff,
+        executor_config={"KubernetesExecutor": {"image": "airflow/ci:latest"}}
+    )
 
-# Add arbitrary labels to worker pods
-four_task = PythonOperator(
-    task_id="four_task", python_callable=print_stuff, dag=dag,
-    executor_config={"KubernetesExecutor": {"labels": {"foo": "bar"}}}
-)
+    # Use the zip binary, which is only found in this special docker image
+    two_task = PythonOperator(
+        task_id="two_task",
+        python_callable=use_zip_binary,
+        executor_config={"KubernetesExecutor": {"image": "airflow/ci_zip:latest"}}
+    )
 
-start_task.set_downstream([one_task, two_task, three_task, four_task])
+    # Limit resources on this operator/task with node affinity & tolerations
+    three_task = PythonOperator(
+        task_id="three_task",
+        python_callable=print_stuff,
+        executor_config={
+            "KubernetesExecutor": {"request_memory": "128Mi",
+                                   "limit_memory": "128Mi",
+                                   "tolerations": tolerations,
+                                   "affinity": affinity}}
+    )
+
+    # Add arbitrary labels to worker pods
+    four_task = PythonOperator(
+        task_id="four_task",
+        python_callable=print_stuff,
+        executor_config={"KubernetesExecutor": {"labels": {"foo": "bar"}}}
+    )
+
+    start_task >> [one_task, two_task, three_task, four_task]
