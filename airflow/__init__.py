d28cf63ca21abed18e5246bb45df7ef7cf52655f
Ash Berlin-Taylor <ash_github@firemirror.com>
2019-09-18 14:14:21 +0100

    [AIRFLOW-4858] Deprecate "Historical convenience functions" in airflow.configuration (#6144)
    These places were missed in the original PR (#5495)
    
    (cherry picked from commit b2e06d0edcdbcfbe65e5135554bd45b3e2d76cd0)

--- airflow/__init__.py
+++ airflow/__init__.py
@@ -35,7 +35,8 @@ __version__ = version.version
 import sys
 
 # flake8: noqa: F401
-from airflow import settings, configuration as conf
+from airflow import settings
+from airflow.configuration import conf
 from airflow.models import DAG
 from flask_admin import BaseView
 from importlib import import_module
