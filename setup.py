335e00f3f8673fdb25eeafbaaae14eab022acc05
suburbanmtman <dsmith@evidation.com>
2019-09-25 14:41:02 -0700

    [AIRFLOW-5554] Require statsd 3.3.0 minimum
    Our environment configuration uses statsd 3.2.2, which is a valid dependency version choice in airflow's requirements.
    However if statsd is enabled in airflow config, there is a critical bug preventing dag runs from completing properly
    when a timedelta is passed to the statsd timing to be formatted into a float:
    
    https://github.com/apache/airflow/blob/66a139d734bd434caa792007c3b980ca4cf8f931/airflow/models/dagrun.py#L354
    
    Causing an exception to be raised due to the unexpected delta data type:
    
    airflow/jobs/scheduler_job.py", line 1547, in process_file
    self._process_dags(dagbag, dags, ti_keys_to_schedule)
    
    airflow/jobs/scheduler_job.py", line 1242, in _process_dags
    schedule_delay)
    
    statsd/client.py", line 93, in timing
    self._send_stat(stat, '%0.6f|ms' % delta, rate)
    TypeError: a float is required
    
    The code to detect the timedelta type and convert to millis was introduced in 3.3.0:
    
    https://github.com/jsocol/pystatsd/blob/1c90b9fdf322680e2625da659abc2aa5d79b5bff/statsd/client/base.py#L28
    
    So the setup.py in airflow should be updated to make this the minimum dependency:
    
    https://github.com/apache/airflow/blob/66a139d734bd434caa792007c3b980ca4cf8f931/setup.py#L262
    for timedelta stat transformation

--- setup.py
+++ setup.py
@@ -221,7 +221,7 @@ mongo = ['pymongo>=3.6.0']
 snowflake = ['snowflake-connector-python>=1.5.2',
              'snowflake-sqlalchemy>=1.1.0']
 ssh = ['paramiko>=2.1.1', 'pysftp>=0.2.9', 'sshtunnel>=0.1.4,<0.2']
-statsd = ['statsd>=3.0.1, <4.0']
+statsd = ['statsd>=3.3.0, <4.0']
 vertica = ['vertica-python>=0.5.1']
 webhdfs = ['hdfs[dataframe,avro,kerberos]>=2.0.4']
 winrm = ['pywinrm==0.2.2']
