9d3bed53cc25605ee5916ab4ecb21a2de1e1a25c
Kamil Breguła <kamil.bregula@polidea.com>
2019-10-09 15:19:39 +0200

    [AIRFLOW-5626] Add labels to MLEngine resources

--- airflow/gcp/hooks/mlengine.py
+++ airflow/gcp/hooks/mlengine.py
@@ -28,6 +28,9 @@ from googleapiclient.errors import HttpError
 
 from airflow.gcp.hooks.base import GoogleCloudBaseHook
 from airflow.utils.log.logging_mixin import LoggingMixin
+from airflow.version import version as airflow_version
+
+_AIRFLOW_VERSION = 'v' + airflow_version.replace('.', '-').replace('+', '-')
 
 
 def _poll_with_exponential_delay(request, max_n, is_done_func, is_error_func):
@@ -111,6 +114,8 @@ class MLEngineHook(GoogleCloudBaseHook):
 
         hook = self.get_conn()
 
+        self._append_label(job)
+
         request = hook.projects().jobs().create(  # pylint: disable=no-member
             parent='projects/{}'.format(project_id),
             body=job)
@@ -193,6 +198,9 @@ class MLEngineHook(GoogleCloudBaseHook):
         """
         hook = self.get_conn()
         parent_name = 'projects/{}/models/{}'.format(project_id, model_name)
+
+        self._append_label(version_spec)
+
         create_request = hook.projects().models().versions().create(  # pylint: disable=no-member
             parent=parent_name, body=version_spec)
         response = create_request.execute()
@@ -295,6 +303,8 @@ class MLEngineHook(GoogleCloudBaseHook):
                              "could not be an empty string")
         project = 'projects/{}'.format(project_id)
 
+        self._append_label(model)
+
         request = hook.projects().models().create(  # pylint: disable=no-member
             parent=project, body=model)
         return request.execute()
@@ -359,3 +369,7 @@ class MLEngineHook(GoogleCloudBaseHook):
         for version in default_versions:
             _, _, version_name = version['name'].rpartition('/')
             self.delete_version(project_id=project_id, model_name=model_name, version_name=version_name)
+
+    def _append_label(self, model: Dict) -> None:
+        model['labels'] = model.get('labels', {})
+        model['labels']['airflow-version'] = _AIRFLOW_VERSION
