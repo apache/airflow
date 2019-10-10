d6d6517e401977ff41b7b91e35203c7f4f471fbd
Ash Berlin-Taylor <ash_github@firemirror.com>
2019-10-10 13:33:18 +0100

    [AIRFLOW-5102] Worker jobs should terminate themselves if they can't heartbeat (#6284)
    If a LocalTaskJob fails to heartbeat for
    scheduler_zombie_task_threshold, it should shut itself down.
    
    However, at some point, a change was made to catch exceptions inside the
    heartbeat, so the LocalTaskJob thought it had managed to heartbeat
    successfully.
    
    This effectively means that zombie tasks don't shut themselves down.
    When the scheduler reschedules the job, this means we could have two
    instances of the task running concurrently.
    
    (cherry picked from commit 68b8ec5f415795e4fa4ff7df35a3e75c712a7bad)

--- tests/jobs/test_local_task_job.py
+++ tests/jobs/test_local_task_job.py
@@ -112,8 +112,7 @@ class LocalTaskJobTest(unittest.TestCase):
         session.merge(ti)
         session.commit()
 
-        ret = job1.heartbeat_callback()
-        self.assertEqual(ret, None)
+        job1.heartbeat_callback()
 
         mock_pid.return_value = 2
         self.assertRaises(AirflowException, job1.heartbeat_callback)
@@ -127,7 +126,7 @@ class LocalTaskJobTest(unittest.TestCase):
 
         heartbeat_records = []
 
-        def heartbeat_recorder():
+        def heartbeat_recorder(**kwargs):
             heartbeat_records.append(timezone.utcnow())
 
         with create_session() as session:
@@ -154,7 +153,7 @@ class LocalTaskJobTest(unittest.TestCase):
 
             job = LocalTaskJob(task_instance=ti, executor=TestExecutor(do_update=False))
             job.heartrate = 2
-            job.heartbeat = heartbeat_recorder
+            job.heartbeat_callback = heartbeat_recorder
             job._execute()
             self.assertGreater(len(heartbeat_records), 1)
             for i in range(1, len(heartbeat_records)):
