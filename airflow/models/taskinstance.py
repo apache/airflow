30ff89bede9093f3a9fdca163907b4a505c9d41a
Rajat Srivastava <rajatsri28@gmail.com>
2019-10-14 18:47:16 +0530

    AIRFLOW-5653 change log level from warning to info

--- airflow/models/taskinstance.py
+++ airflow/models/taskinstance.py
@@ -953,7 +953,7 @@ class TaskInstance(Base, LoggingMixin):
         except AirflowSkipException as e:
             # log only if exception has any arguments to prevent log flooding
             if e.args:
-                self.log.warning(e)
+                self.log.info(e)
             self.refresh_from_db(lock_for_update=True)
             self.state = State.SKIPPED
         except AirflowRescheduleException as reschedule_exception:
