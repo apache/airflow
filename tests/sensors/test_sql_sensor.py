8a608fd9effd22e6f81c2376e351c262d4f7dcf3
ToxaZ <7903257+ToxaZ@users.noreply.github.com>
2019-10-15 16:52:51 +0300

    fix diff with master

--- tests/sensors/test_sql_sensor.py
+++ tests/sensors/test_sql_sensor.py
@@ -16,11 +16,11 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-import unittest
 from unittest import mock
+import unittest
 
 from airflow import DAG
-from airflow.configuration import conf
+from airflow import configuration
 from airflow.exceptions import AirflowException
 from airflow.sensors.sql_sensor import SqlSensor
 from airflow.utils.timezone import datetime
@@ -50,7 +50,7 @@ class TestSqlSensor(unittest.TestCase):
             t.run(start_date=DEFAULT_DATE, end_date=DEFAULT_DATE, ignore_ti_state=True)
 
     @unittest.skipUnless(
-        'mysql' in conf.get('core', 'sql_alchemy_conn'), "this is a mysql test")
+        'mysql' in configuration.get('core', 'sql_alchemy_conn'), "this is a mysql test")
     def test_sql_sensor_mysql(self):
         t1 = SqlSensor(
             task_id='sql_sensor_check',
@@ -70,7 +70,7 @@ class TestSqlSensor(unittest.TestCase):
         t2.run(start_date=DEFAULT_DATE, end_date=DEFAULT_DATE, ignore_ti_state=True)
 
     @unittest.skipUnless(
-        'postgresql' in conf.get('core', 'sql_alchemy_conn'), "this is a postgres test")
+        'postgresql' in configuration.get('core', 'sql_alchemy_conn'), "this is a postgres test")
     def test_sql_sensor_postgres(self):
         t1 = SqlSensor(
             task_id='sql_sensor_check',
@@ -242,3 +242,32 @@ class TestSqlSensor(unittest.TestCase):
 
         mock_get_records.return_value = [[1]]
         self.assertRaises(AirflowException, t.poke, None)
+
+
+    @mock.patch('airflow.sensors.sql_sensor.BaseHook')
+    def test_sql_sensor_postgres_check_allow_null(self, mock_hook):
+        t1 = SqlSensor(
+            task_id='sql_sensor_check',
+            conn_id='postgres_default',
+            sql="SELECT NULL",
+            allow_null=True
+        )
+
+        mock_hook.get_connection('postgres_default').conn_type = "postgres"
+        mock_get_records = mock_hook.get_connection.return_value.get_hook.return_value.get_records
+
+        mock_get_records.return_value = [[None]]
+        self.assertTrue(t1.poke(None))
+
+        t2 = SqlSensor(
+            task_id='sql_sensor_check',
+            conn_id='postgres_default',
+            sql="SELECT NULL",
+            allow_null=False
+        )
+
+        mock_hook.get_connection('postgres_default').conn_type = "postgres"
+        mock_get_records = mock_hook.get_connection.return_value.get_hook.return_value.get_records
+
+        mock_get_records.return_value = [[None]]
+        self.assertFalse(t2.poke(None))
