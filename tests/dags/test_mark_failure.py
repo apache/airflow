e9a9aec3bff63b94d02fd82a24a3dcf1730511f3
Bharath Palaksha <Bharath.Palaksha@walmartlabs.com>
2019-10-15 16:35:33 +0530

    [AIRFLOW-5621] - When task is marked as failed through UI, failure callback is not called

--- tests/dags/test_mark_failure.py
+++ tests/dags/test_mark_failure.py
@@ -18,10 +18,9 @@
 # under the License.
 from datetime import datetime
 
-from tests.jobs.test_local_task_job import data
-
 from airflow.models import DAG
 from airflow.operators.bash_operator import BashOperator
+from tests.jobs.test_local_task_job import data
 
 DEFAULT_DATE = datetime(2016, 1, 1)
 
