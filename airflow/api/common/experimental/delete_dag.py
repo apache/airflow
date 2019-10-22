ashb/incubator-airflow

[AIRFLOW-5088] persisting serialized DAG in DB for webserver scalability

airflow/api/common/experimental/delete_dag.py
@@ -21,10 +21,12 @@ import os
 
 from sqlalchemy import or_
 
+from airflow import configuration
 from airflow import models
-from airflow.models import TaskFail, DagModel
+from airflow.models import TaskFail, DagModel, SerializedDagModel
 from airflow.utils.db import provide_session
 from airflow.exceptions import DagFileExists, DagNotFound
+from airflow.settings import DAGCACHED_ENABLED
 
 
 @provide_session
@@ -44,6 +46,11 @@ def delete_dag(dag_id: str, keep_records_in_log: bool = True, session=None) -> i
     if dag.fileloc and os.path.exists(dag.fileloc):
         raise DagFileExists("Dag id {} is still in DagBag. "
                             "Remove the DAG file first: {}".format(dag_id, dag.fileloc))
+
+    # Scheduler removes DAGs without files from serialized_dag table every dag_dir_list_interval.
+    # There may be a lag, so explicitly removes serialized DAG here.
+    if DAGCACHED_ENABLED and SerializedDagModel.has_dag(dag_id):
+        SerializedDagModel.remove_dag(dag_id)
 
     count = 0
 

airflow/models/dag.py
@@ -37,13 +37,15 @@ from sqlalchemy import Column, String, Boolean, Integer, Text, func, or_
 
 from airflow import configuration, settings, utils
 from airflow.dag.base_dag import BaseDag
-from airflow.exceptions import AirflowException, AirflowDagCycleException
+from airflow.exceptions import AirflowException, AirflowDagCycleException, DagNotFound
 from airflow.executors import LocalExecutor, get_default_executor
 from airflow.models.base import Base, ID_LEN
 from airflow.models.dagbag import DagBag
 from airflow.models.dagpickle import DagPickle
 from airflow.models.dagrun import DagRun
 from airflow.models.taskinstance import TaskInstance, clear_task_instances
+from airflow.models.serialized_dag import SerializedDagModel
+from airflow.settings import DAGCACHED_ENABLED, DAGCACHED_MIN_UPDATE_INTERVAL
 from airflow.utils import timezone
 from airflow.utils.dates import cron_presets, date_range as utils_date_range
 from airflow.utils.db import provide_session
@@ -1319,6 +1321,12 @@ class DAG(BaseDag, LoggingMixin):
         for subdag in self.subdags:
             subdag.sync_to_db(owner=owner, sync_time=sync_time, session=session)
 
+        # Write DAGs to serialized_dag table in DB.
+        # subdags are not written into serialized_dag, because they are not displayed
+        # in the DAG list on UI. They are included in the serialized parent DAG.
+        if DAGCACHED_ENABLED and not self.is_subdag:
+            SerializedDagModel.write_dag(self, min_update_interval=DAGCACHED_MIN_UPDATE_INTERVAL)
+
     @staticmethod
     @provide_session
     def deactivate_unknown_dags(active_dag_ids, session=None):
@@ -1509,8 +1517,10 @@ class DagModel(Base):
     def safe_dag_id(self):
         return self.dag_id.replace('.', '__dot__')
 
-    def get_dag(self):
-        return DagBag(dag_folder=self.fileloc).get_dag(self.dag_id)
+    def get_dag(self, dagcached_enabled=False):
+        # Calling it from UI must set dagcached_enabled = DAGCACHED_ENABLED.
+        return DagBag(
+            dag_folder=self.fileloc, dagcached_enabled=dagcached_enabled).get_dag(self.dag_id)
 
     @provide_session
     def create_dagrun(self,
@@ -1551,6 +1561,7 @@ class DagModel(Base):
     def set_is_paused(self,
                       is_paused: bool,
                       including_subdags: bool = True,
+                      dagcached_enabled = False,
                       session=None) -> None:
         """
         Pause/Un-pause a DAG.
@@ -1561,7 +1572,10 @@ class DagModel(Base):
         """
         dag_ids = [self.dag_id]  # type: List[str]
         if including_subdags:
-            subdags = self.get_dag().subdags
+            dag = self.get_dag(dagcached_enabled)
+            if dag is None:
+                raise DagNotFound("Dag id {} not found".format(self.dag_id))
+            subdags = dag.subdags
             dag_ids.extend([subdag.dag_id for subdag in subdags])
         dag_models = session.query(DagModel).filter(DagModel.dag_id.in_(dag_ids)).all()
         try:

apache/airflow

airflow/api/common/experimental/delete_dag.py
@@ -22,7 +22,8 @@ from sqlalchemy import or_
 
 from airflow import models
 from airflow.exceptions import DagNotFound
-from airflow.models import DagModel, TaskFail
+from airflow.models import DagModel, SerializedDagModel, TaskFail
+from airflow.settings import DAGCACHED_ENABLED
 from airflow.utils.db import provide_session
 
 
@@ -39,6 +40,11 @@ def delete_dag(dag_id: str, keep_records_in_log: bool = True, session=None) -> i
     dag = session.query(DagModel).filter(DagModel.dag_id == dag_id).first()
     if dag is None:
         raise DagNotFound("Dag id {} not found".format(dag_id))
+
+    # Scheduler removes DAGs without files from serialized_dag table every dag_dir_list_interval.
+    # There may be a lag, so explicitly removes serialized DAG here.
+    if DAGCACHED_ENABLED and SerializedDagModel.has_dag(dag_id):
+        SerializedDagModel.remove_dag(dag_id)
 
     count = 0
 

airflow/models/dag.py
@@ -38,13 +38,15 @@ from sqlalchemy import Boolean, Column, Integer, String, Text, func, or_
 from airflow import settings, utils
 from airflow.configuration import conf
 from airflow.dag.base_dag import BaseDag
-from airflow.exceptions import AirflowDagCycleException, AirflowException
+from airflow.exceptions import AirflowDagCycleException, AirflowException, DagNotFound
 from airflow.executors import LocalExecutor, get_default_executor
 from airflow.models.base import ID_LEN, Base
 from airflow.models.dagbag import DagBag
 from airflow.models.dagpickle import DagPickle
 from airflow.models.dagrun import DagRun
+from airflow.models.serialized_dag import SerializedDagModel
 from airflow.models.taskinstance import TaskInstance, clear_task_instances
+from airflow.settings import DAGCACHED_ENABLED, DAGCACHED_MIN_UPDATE_INTERVAL
 from airflow.utils import timezone
 from airflow.utils.dates import cron_presets, date_range as utils_date_range
 from airflow.utils.db import provide_session
@@ -1379,6 +1381,12 @@ class DAG(BaseDag, LoggingMixin):
         for subdag in self.subdags:
             subdag.sync_to_db(owner=owner, sync_time=sync_time, session=session)
 
+        # Write DAGs to serialized_dag table in DB.
+        # subdags are not written into serialized_dag, because they are not displayed
+        # in the DAG list on UI. They are included in the serialized parent DAG.
+        if DAGCACHED_ENABLED and not self.is_subdag:
+            SerializedDagModel.write_dag(self, min_update_interval=DAGCACHED_MIN_UPDATE_INTERVAL)
+
     @staticmethod
     @provide_session
     def deactivate_unknown_dags(active_dag_ids, session=None):
@@ -1569,8 +1577,10 @@ class DagModel(Base):
     def safe_dag_id(self):
         return self.dag_id.replace('.', '__dot__')
 
-    def get_dag(self):
-        return DagBag(dag_folder=self.fileloc).get_dag(self.dag_id)
+    def get_dag(self, dagcached_enabled=False):
+        # Calling it from UI must set dagcached_enabled = DAGCACHED_ENABLED.
+        return DagBag(
+            dag_folder=self.fileloc, dagcached_enabled=dagcached_enabled).get_dag(self.dag_id)
 
     @provide_session
     def create_dagrun(self,
@@ -1611,6 +1621,7 @@ class DagModel(Base):
     def set_is_paused(self,
                       is_paused: bool,
                       including_subdags: bool = True,
+                      dagcached_enabled = False,
                       session=None) -> None:
         """
         Pause/Un-pause a DAG.
@@ -1621,7 +1632,10 @@ class DagModel(Base):
         """
         dag_ids = [self.dag_id]  # type: List[str]
         if including_subdags:
-            subdags = self.get_dag().subdags
+            dag = self.get_dag(dagcached_enabled)
+            if dag is None:
+                raise DagNotFound("Dag id {} not found".format(self.dag_id))
+            subdags = dag.subdags
             dag_ids.extend([subdag.dag_id for subdag in subdags])
         dag_models = session.query(DagModel).filter(DagModel.dag_id.in_(dag_ids)).all()
         try: