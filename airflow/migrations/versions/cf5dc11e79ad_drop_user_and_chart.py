8cceb7bdeb98682c35ec8ea648cd6dd4757cf67e
bo <bblanton@asapp.com>
2019-10-23 22:38:00 -0300

    hmm

--- airflow/migrations/versions/cf5dc11e79ad_drop_user_and_chart.py
+++ airflow/migrations/versions/cf5dc11e79ad_drop_user_and_chart.py
@@ -44,8 +44,8 @@ def upgrade():
     conn = op.get_bind()
     inspector = Inspector.from_engine(conn)
 
-    if 'known_event' in inspector.get_table_names() != 'sqlite':
-        op.drop_constraint('known_event_user_id_fkey', 'known_event')
+    #if 'known_event' in inspector.get_table_names() != 'sqlite':
+    #    op.drop_constraint('known_event_user_id_fkey', 'known_event')
 
     op.drop_table("chart")
     op.drop_table("users")
