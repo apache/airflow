37376ca115652ef7b51d84488b8b3ee6fdac63db
Kamil Breguła <kamil.bregula@polidea.com>
2019-10-25 14:47:25 +0200

    Revert "[AIRFLOW-5746] move FakeDateTime into the only place it is used (#6416)"
    This reverts commit adfcf67d6532350071336390bcb4dad0b99ae450.

--- tests/dags/test_impersonation_custom.py
+++ tests/dags/test_impersonation_custom.py
@@ -29,6 +29,7 @@ from airflow.operators.python_operator import PythonOperator
 # import the custom package.
 # This DAG is used to test that impersonation propagates the PYTHONPATH environment
 # variable correctly.
+from fake_datetime import FakeDatetime
 
 
 DEFAULT_DATE = datetime(2016, 1, 1)
@@ -40,15 +41,6 @@ args = {
 }
 
 dag = DAG(dag_id='impersonation_with_custom_pkg', default_args=args)
-
-
-class FakeDatetime(datetime):
-    """
-    A fake replacement for datetime that can be mocked for testing.
-    """
-
-    def __new__(cls, *args, **kwargs):
-        return datetime.__new__(datetime, *args, **kwargs)
 
 
 def print_today():
