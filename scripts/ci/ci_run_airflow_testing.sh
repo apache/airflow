4790440295616163f0f9fd4d678742dde9710a6e
Jarek Potiuk <jarek@potiuk.com>
2019-11-07 08:02:25 +0100

    Disable shellcheck problem

--- scripts/ci/ci_run_airflow_testing.sh
+++ scripts/ci/ci_run_airflow_testing.sh
@@ -77,23 +77,27 @@ if [[ ${START_KUBERNETES_CLUSTER} == "true" ]]; then
     build_and_save_kubernetes_image
 
     set +u
+    # shellcheck: disable=SC2016
     docker-compose --log-level INFO \
       -f "${MY_DIR}/docker-compose.yml" \
       -f "${MY_DIR}/docker-compose-${BACKEND}.yml" \
       -f "${MY_DIR}/docker-compose-kubernetes.yml" \
       "${DOCKER_COMPOSE_LOCAL[@]}" \
-         run airflow-testing '/opt/airflow/scripts/ci/in_container/entrypoint_ci.sh "${@}"' \
+         run airflow-testing \
+           '/opt/airflow/scripts/ci/in_container/entrypoint_ci.sh "${@}"' \
            /opt/airflow/scripts/ci/in_container/entrypoint_ci.sh "${@}"
          # Note the command is there twice (!) because it is passed via bash -c 
          # and bash -c starts passing parameters from $0 
     set -u
 else
     set +u
+    # shellcheck: disable=SC2016
     docker-compose --log-level INFO \
       -f "${MY_DIR}/docker-compose.yml" \
       -f "${MY_DIR}/docker-compose-${BACKEND}.yml" \
       "${DOCKER_COMPOSE_LOCAL[@]}" \
-         run airflow-testing '/opt/airflow/scripts/ci/in_container/entrypoint_ci.sh "${@}"' \
+         run airflow-testing \
+           '/opt/airflow/scripts/ci/in_container/entrypoint_ci.sh "${@}"' \
            /opt/airflow/scripts/ci/in_container/entrypoint_ci.sh "${@}"
          # Note the command is there twice (!) because it is passed via bash -c 
          # and bash -c starts passing parameters from $0 
