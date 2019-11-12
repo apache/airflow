f6682d9f171485473a58b97b20e09b1836baf3d1
Kamil Breguła <kamil.bregula@polidea.com>
2019-11-12 01:59:05 +0100

    fixup! [AIRFLOW-5896] Move email stuff from tests/ccore.py

--- tests/utils/test_email.py
+++ tests/utils/test_email.py
@@ -16,6 +16,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+import os
 import tempfile
 import unittest
 from email.mime.application import MIMEApplication
