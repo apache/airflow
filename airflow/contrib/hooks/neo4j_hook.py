1d4c8054065ca55f958a221934bcdc150d61fc33
Timothy Findlay <findlay.t@thoughtworks.com>
2019-11-13 15:50:24 +1100

    AIRFLOW-5920: First draft, added hook + operator

---
+++ airflow/contrib/hooks/neo4j_hook.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from airflow.exceptions import AirflowException
+from airflow.hooks.base_hook import BaseHook
+from neo4j import GraphDatabase
+
+
+class Neo4JHook(BaseHook):
+    """
+    Interact with Neo4J.
+    This class is a thin wrapper around the neo4j python library.
+    :param: n4j_conn_id: Name of neo4j connection
+    """
+    _config = None
+    _driver = None
+    _session = None
+    _n4j_conn_id = None
+
+    def __init__(self, n4j_conn_id='n4j_default', *args, **kwargs):
+        # super().__init__(*args, **kwargs) TypeError: __init__() missing 1 required positional argument: 'source'
+        self._n4j_conn_id = n4j_conn_id
+
+    def _get_config(self):
+        """
+        Obtain the Username + Password from the Airflow connection definition
+        Store them in _config dictionary as:
+         credentials = a tuple of username/password eg. ("neo4j", "P@ssw0rd")
+         host = String for Neo4J URI eg. "bolt://10.41.17.128:7687"
+        :return: None
+        """
+        if self._n4j_conn_id:
+            # Initialize with empty dictionary
+            if self._config is None:
+                self._config = {}
+
+            connection_object = self.get_connection(self._n4j_conn_id)
+            if connection_object.login and connection_object.host:
+                self._config['credentials'] = connection_object.login, connection_object.password
+                self._config['host'] = connection_object.host
+        else:
+            raise AirflowException("No Neo4J connection: {}".format(self._n4j_conn_id))
+
+    def _get_driver(self):
+        """
+        Establish a TCP connection to the server
+        """
+        # We need the configuration information before we can establish a connection with the driver
+        if self._config is None:
+            self._get_config()
+
+        # Check if we already have a driver we can re-use before creating a new one
+        if self._driver is None:
+            self._driver = GraphDatabase.driver(
+                uri=self._config['host'],
+                auth=self._config['credentials']
+            )
+
+    def _get_session(self):
+        """
+        Get a neo4j.session from the driver.
+        """
+        # We need a driver to be established, so check this is done, if not, set it up
+        if self._driver is None:
+            self._get_driver()
+
+        # Check if we already have a session we can re-use before creating a new one
+        if self._session is None:
+            self._session = self._driver.session()
+
+        return self._session
+
+    def run_query(self, cypher_query, parameters=None):
+        """
+        Uses a session to execute submit a query for execution
+        :param cypher_query: Cypher query eg. MATCH (a) RETURN (a)
+        :param parameters: Optionally supply parameters to inject into the cypher query
+        :return:
+        """
+        with self._get_session() as session:
+            self.log.info("Executing query: {}".format(cypher_query))
+            result = session.read_transaction(
+                lambda tx, inputs: tx.run(cypher_query, inputs)
+                ,
+                parameters
+            )
+        return result
---
+++ airflow/contrib/operators/neo4j_operator.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+from airflow.exceptions import AirflowException
+from airflow.contrib.hooks.neo4j_hook import Neo4JHook
+import csv
+
+class Neo4JOperator(BaseOperator):
+    """
+    Neo4JOperator to interact and perform action on Neo4J graph database.
+    This operator is designed to use Neo4J Python driver: https://neo4j.com/docs/api/python-driver/current/
+
+    :param n4j_conn_id: reference to a pre-defined Neo4J Connection
+    :type n4j_conn_id: str
+    :param cypher_query: required cypher query to be executed on the Neo4J database
+    :type cypher_query: str
+    :param output_filename: required filename to produce with output from the query
+    :type output_filename: str
+    """
+    _cypher_query = None
+    _output_filename = None
+    _n4j_conn_id = None
+
+    @apply_defaults
+    def __init__(self,
+                 cypher_query,
+                 output_filename,
+                 n4j_conn_id='n4j_default',
+                 *args,
+                 **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._output_filename = output_filename
+        self._cypher_query = cypher_query
+        self._n4j_conn_id = n4j_conn_id
+
+    def execute(self, context):
+        hook = Neo4JHook(n4j_conn_id=self._n4j_conn_id)
+        if self._cypher_query is not None:
+            result = hook.run_query(cypher_query=self._cypher_query)
+            # result = 'neo4j.BoltStatementResult' See https://neo4j.com/docs/api/python-driver/current/results.html
+        else:
+            raise AirflowException("cypher_query is missing.")
+
+        # Log what is happening
+        self.log.info("Processing output with keys: {}".format(result.keys()))
+
+        row_count = self._make_csv(result)
+
+        # Provide some feedback to what was done...
+        self.log.info("Saved {0} with {1} rows".format(self._output_filename, row_count))
+
+    def _make_csv(self, result):
+        total_row_count = 0
+
+        # Use CSV by default, could expand to support different output formats
+        # Consider available disk space on the Airflow server, maybe support S3 bucket and bring in the S3 Hook
+        with open(self._output_filename, 'w', newline='') as output_file:
+            output_writer = csv.DictWriter(output_file, fieldnames=result.keys())
+            output_writer.writeheader()
+
+            for total_row_count, row in enumerate(result, start=1):
+                # row = 'neo4j.Record'
+                output_writer.writerow(row.data())
+
+        return total_row_count
