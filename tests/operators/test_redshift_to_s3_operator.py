4cc3c4adfbbd081d7d428d3fdcc14b7a9b282729
JavierLTAtr <javier.lopez@atrapalo.com>
2019-11-20 12:38:18 +0100

    adding parameterized test

--- tests/operators/test_redshift_to_s3_operator.py
+++ tests/operators/test_redshift_to_s3_operator.py
@@ -20,6 +20,7 @@
 
 import unittest
 from unittest import mock
+from parameterized import parameterized
 
 from boto3.session import Session
 
@@ -31,7 +32,8 @@ class TestRedshiftToS3Transfer(unittest.TestCase):
 
     @mock.patch("boto3.session.Session")
     @mock.patch("airflow.hooks.postgres_hook.PostgresHook.run")
-    def test_execute_true(self, mock_run, mock_session):
+    @parameterized([(True),(False)])
+    def test_execute(self, mock_run, mock_session, boolean_value):
         access_key = "aws_access_key_id"
         secret_key = "aws_secret_access_key"
         mock_session.return_value = Session(access_key, secret_key)
@@ -40,7 +42,7 @@ class TestRedshiftToS3Transfer(unittest.TestCase):
         s3_bucket = "bucket"
         s3_key = "key"
         unload_options = ['HEADER', ]
-        table_as_file_name = True
+        table_as_file_name = boolean_value
 
         RedshiftToS3Transfer(
             schema=schema,
@@ -66,49 +68,6 @@ class TestRedshiftToS3Transfer(unittest.TestCase):
                     {unload_options};
                     """.format(select_query=select_query,
                                table=table,
-                               s3_bucket=s3_bucket,
-                               s3_key=s3_key,
-                               access_key=access_key,
-                               secret_key=secret_key,
-                               unload_options=unload_options)
-
-        assert mock_run.call_count == 1
-        assertEqualIgnoreMultipleSpaces(self, mock_run.call_args[0][0], unload_query)
-
-    def test_execute_false(self, mock_run, mock_session):
-        access_key = "aws_access_key_id"
-        secret_key = "aws_secret_access_key"
-        mock_session.return_value = Session(access_key, secret_key)
-        schema = "schema"
-        table = "table"
-        s3_bucket = "bucket"
-        s3_key = "key"
-        unload_options = ['HEADER', ]
-        table_as_file_name = False
-
-        RedshiftToS3Transfer(
-            schema=schema,
-            table=table,
-            s3_bucket=s3_bucket,
-            s3_key=s3_key,
-            unload_options=unload_options,
-            include_header=True,
-            redshift_conn_id="redshift_conn_id",
-            aws_conn_id="aws_conn_id",
-            task_id="task_id",
-            table_as_file_name=table_as_file_name,
-            dag=None
-        ).execute(None)
-
-        unload_options = '\n\t\t\t'.join(unload_options)
-        select_query = "SELECT * FROM {schema}.{table}".format(schema=schema, table=table)
-        unload_query = """
-                    UNLOAD ('{select_query}')
-                    TO 's3://{s3_bucket}/{s3_key}'
-                    with credentials
-                    'aws_access_key_id={access_key};aws_secret_access_key={secret_key}'
-                    {unload_options};
-                    """.format(select_query=select_query,
                                s3_bucket=s3_bucket,
                                s3_key=s3_key,
                                access_key=access_key,
