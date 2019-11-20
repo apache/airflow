7b4d9338a6564d2dc70c9933bc14353c2c423f7c
Austin C Roos <roos.austin@gmail.com>
2019-11-20 18:27:28 +0100

    [AIRFLOW-3632] Only replace microseconds if execution_date is None in trigger_dag REST API (#6380)
    No need to require a user to pass in replace_microseconds to the
    request body; instead we should use exactly the date that is given.
    We will still replace the microseconds on execution_date if none is
    passed in (and the param is True, which is the default)
    
    (cherry picked from commit a45a209f38d0025bd10a00adfcda25fbaff3eabc)

--- airflow/www_rbac/api/experimental/endpoints.py
+++ airflow/www_rbac/api/experimental/endpoints.py
@@ -27,6 +27,7 @@ from airflow.api.common.experimental.get_code import get_code
 from airflow.api.common.experimental.get_dag_run_state import get_dag_run_state
 from airflow.exceptions import AirflowException
 from airflow.utils.log.logging_mixin import LoggingMixin
+from airflow.utils.strings import to_boolean
 from airflow.utils import timezone
 from airflow.www_rbac.app import csrf
 from airflow import models
@@ -77,8 +78,12 @@ def trigger_dag(dag_id):
 
             return response
 
+    replace_microseconds = (execution_date is None)
+    if 'replace_microseconds' in data:
+        replace_microseconds = to_boolean(data['replace_microseconds'])
+
     try:
-        dr = trigger.trigger_dag(dag_id, run_id, conf, execution_date)
+        dr = trigger.trigger_dag(dag_id, run_id, conf, execution_date, replace_microseconds)
     except AirflowException as err:
         _log.error(err)
         response = jsonify(error="{}".format(err))
