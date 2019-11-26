27d4125b43d5bc50ebda5747275eddbb15da9ed2
Timothy Findlay <findlay.t@thoughtworks.com>
2019-11-27 10:56:26 +1100

    AIRFLOW-5920: Add neo4j to dependency tree to fix the build

--- setup.py
+++ setup.py
@@ -321,7 +321,7 @@ devel_minreq = devel + kubernetes + mysql + doc + password + cgroups
 devel_hadoop = devel_minreq + hive + hdfs + webhdfs + kerberos
 devel_all = (sendgrid + devel + all_dbs + doc + samba + slack + oracle +
              docker + ssh + kubernetes + celery + redis + gcp + grpc +
-             datadog + zendesk + jdbc + ldap + kerberos + password + webhdfs + jenkins +
+             datadog + zendesk + jdbc + ldap + neo4j + kerberos + password + webhdfs + jenkins +
              druid + pinot + segment + snowflake + elasticsearch + sentry +
              atlas + azure + aws + salesforce + cgroups + papermill + virtualenv +
              pagerduty)
