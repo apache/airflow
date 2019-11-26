3dd1fe94008d323bcbac5ec9f8cae6a4658476be
tyg03485 <tyg03485@bluehole.net>
2019-11-26 23:41:21 +0900

    Delete trailing whitespace

--- airflow/contrib/operators/kubernetes_pod_operator.py
+++ airflow/contrib/operators/kubernetes_pod_operator.py
@@ -280,7 +280,7 @@ class KubernetesPodOperator(BaseOperator):  # pylint: disable=too-many-instance-
         def _set_instance(_class, _args):
             try:
                 return _class(**_args)
-            except Exception: 
+            except Exception:
                 return None
 
         if _args_list and isinstance(_args_list, list):
