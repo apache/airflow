f17a63fe4931af12536c6965c626569db54068ca
Tomek <tomasz.urbaszek@polidea.com>
2019-11-29 20:08:19 +0100

    [AIRFLOW-6099] Add host name to task runner log (#6688)
    (cherry picked from commit f2f8d01ba29492eeec6c669d66bb405426f42f40)

--- airflow/task/task_runner/base_task_runner.py
+++ airflow/task/task_runner/base_task_runner.py
@@ -24,10 +24,9 @@ import os
 import subprocess
 import threading
 
-from airflow.utils.log.logging_mixin import LoggingMixin
-
 from airflow.configuration import conf
 from airflow.utils.configuration import tmp_configuration_copy
+from airflow.utils.log.logging_mixin import LoggingMixin
 from airflow.utils.net import get_hostname
 
 PYTHONPATH_VAR = 'PYTHONPATH'
