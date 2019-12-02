184016ee1172433a93ff9e5708ff50f7a6b6cc73
wanglong <wanglong@akulaku.com>
2019-12-02 10:35:58 +0800

    Fix task get log
    When getting logs, querying task_instance does not get the exact hostname. Need to cooperate with the log table and try number

--- airflow/utils/log/file_task_handler.py
+++ airflow/utils/log/file_task_handler.py
@@ -110,11 +110,12 @@ class FileTaskHandler(logging.Handler):
                 log += "*** {}\n".format(str(e))
         else:
             url = os.path.join(
-                "http://{ti.hostname}:{worker_log_server_port}/log", log_relative_path
+                "http://{hostname}:{worker_log_server_port}/log", log_relative_path
             ).format(
-                ti=ti,
+                hostname=self.query_task_hostname(ti,try_number),
                 worker_log_server_port=conf.get('celery', 'WORKER_LOG_SERVER_PORT')
             )
+
             log += "*** Log file does not exist: {}\n".format(location)
             log += "*** Fetching from: {}\n".format(url)
             try:
@@ -211,3 +212,33 @@ class FileTaskHandler(logging.Handler):
             os.chmod(full_path, 0o666)
 
         return full_path
+
+    from airflow.utils.db import provide_session
+    @provide_session
+    def query_task_hostname(self, ti, try_number, session=None):
+        """
+        Get task log hostname by log table and  try_number
+
+        :param ti: task instance record
+        :param try_number: current try_number to read log from
+        :param session: database session
+        :type session: sqlalchemy.orm.session.Session
+        :return: log message hostname
+        """
+        from airflow import models
+        res_log = session.query(models.Log).filter(
+            models.Log.dag_id == ti.dag_id,
+            models.Log.task_id == ti.task_id,
+            models.Log.execution_date == ti.execution_date,
+            models.Log.owner != 'anonymous',
+            models.Log.event == 'cli_run',
+            models.Log.extra.like('%--raw%')
+        ).order_by(models.Log.id).limit(1).offset(try_number - 1).first()
+
+        import json
+        hostname = json.loads(res_log.extra)["host_name"]
+
+        logging.info('Task_instance Hostname : {}'.format(hostname))
+
+        return hostname
+
