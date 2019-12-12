apache/spark

[AIRFLOW-6045] Error on failed execution of compile_assets (#6640)
https://issues.apache.org/jira/browse/AIRFLOW-6045

setup.py
@@ -95,7 +95,7 @@ class CompileAssets(Command):
     # noinspection PyMethodMayBeStatic
     def run(self):
         """Run a command to compile and build assets."""
-        subprocess.call('./airflow/www/compile_assets.sh')
+        subprocess.check_call('./airflow/www/compile_assets.sh')
 
 
 def git_version(version_: str) -> str:

apache/airflow

(cherry picked from commit fa626f0768bf8a16f00dfc131b4a31dc82a40c05)

setup.py
@@ -92,7 +92,8 @@ class CompileAssets(Command):
 
     # noinspection PyMethodMayBeStatic
     def run(self):
-        subprocess.call('./airflow/www_rbac/compile_assets.sh')
+        """Run a command to compile and build assets."""
+        subprocess.check_call('./airflow/www_rbac/compile_assets.sh')
 
 
 def git_version(version_):