e06e2ef49c12ffcfc04015b03d61ebf04b0635a9
zuku1985 <tomek@tomekzukowski.com>
2019-12-14 07:53:19 +0100

    [AIRFLOW-6091] Add flushing in execute method for BigQueryCursor (#6683)
    If you execute multiple queries results of old ones will be
    flushed allowing to read results of recent execute without
    any issues.
    
    (cherry picked from commit 0cf9598b0885cb94eb89c5eca8717f2245cb5de7)

--- airflow/contrib/hooks/bigquery_hook.py
+++ airflow/contrib/hooks/bigquery_hook.py
@@ -2027,6 +2027,13 @@ class BigQueryCursor(BigQueryBaseCursor):
         for parameters in seq_of_parameters:
             self.execute(operation, parameters)
 
+    def flush_results(self):
+        """ Flush results related cursor attributes. """
+        self.page_token = None
+        self.job_id = None
+        self.all_pages_loaded = False
+        self.buffer = []
+
     def fetchone(self):
         """ Fetch the next row of a query result set. """
         return self.next()
@@ -2067,9 +2074,7 @@ class BigQueryCursor(BigQueryBaseCursor):
 
             else:
                 # Reset all state since we've exhausted the results.
-                self.page_token = None
-                self.job_id = None
-                self.page_token = None
+                self.flush_results()
                 return None
 
         return self.buffer.pop(0)
