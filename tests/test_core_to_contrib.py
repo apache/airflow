db49299d34d77c34c15b66f569535995a2e2887e
michalslowikowski00 <michal.slowikowski@polidea.com>
2019-12-15 01:25:37 +0100

    [AIRFLOW-6146] [AIP-21] Rename GCS operators regarding GDrive, BigQuery and SFTP
    PR contains changes regarding AIP-21 (renaming GCP operators and hooks):
    * renamed GCP modules
    * adde deprecation warnings to the contrib modules
    * fixed tests
    * updated UPDATING.md

--- tests/test_core_to_contrib.py
+++ tests/test_core_to_contrib.py
@@ -627,7 +627,7 @@ OPERATOR = [
         "CloudVisionRemoveProductFromProductSetOperator",
     ),
     (
-        "airflow.operators.gcs_to_bq.GoogleCloudStorageToBigQueryOperator",
+        "airflow.operators.gcs_to_bq.GCSToBigQueryOperator",
         "airflow.contrib.operators.gcs_to_bq.GoogleCloudStorageToBigQueryOperator",
     ),
     (
