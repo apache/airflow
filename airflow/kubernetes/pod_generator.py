858fd2908ccdbdc261d3def3262cc6494755b7ab
antonymayi <antonymayi@users.noreply.github.com>
2019-12-17 20:57:08 +0100

    [AIRFLOW-5349] Add schedulername option for KubernetesPodOperator (#6088)
    (cherry picked from commit 0d94c8e841cccb63e15a8cc6b5549a744cfe1428)

--- airflow/kubernetes/pod_generator.py
+++ airflow/kubernetes/pod_generator.py
@@ -146,6 +146,8 @@ class PodGenerator:
     :type configmaps: str
     :param dnspolicy: Specify a dnspolicy for the pod
     :type dnspolicy: str
+    :param schedulername: Specify a schedulername for the pod
+    :type schedulername: str
     :param pod: The fully specified pod.
     :type pod: kubernetes.client.models.V1Pod
     :param extract_xcom: Whether to bring up a container for xcom
@@ -178,6 +180,7 @@ class PodGenerator:
         security_context=None,
         configmaps=None,
         dnspolicy=None,
+        schedulername=None,
         priority_class_name=None,
         pod=None,
         extract_xcom=False,
@@ -230,6 +233,7 @@ class PodGenerator:
         self.spec.security_context = security_context
         self.spec.tolerations = tolerations
         self.spec.dns_policy = dnspolicy
+        self.spec.scheduler_name = schedulername
         self.spec.host_network = hostnetwork
         self.spec.affinity = affinity
         self.spec.service_account_name = service_account_name
@@ -367,6 +371,7 @@ class PodGenerator:
             security_context=namespaced.get('security_context'),
             configmaps=namespaced.get('configmaps'),
             dnspolicy=namespaced.get('dnspolicy'),
+            schedulername=namespaced.get('schedulername'),
             pod=namespaced.get('pod'),
             extract_xcom=namespaced.get('extract_xcom'),
         )
