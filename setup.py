f606a3a762906c039980afd83ef8f679f52b866a
Bolke de Bruin <bolke@xs4all.nl>
2019-12-25 16:18:36 +0100

    [AIRFLOW-5912] Expose lineage API
    Lineage data is exposed via the experimental api endpoint
    per dag.

--- setup.py
+++ setup.py
@@ -287,8 +287,8 @@ pagerduty = [
     'pypd>=1.1.0',
 ]
 papermill = [
-    'papermill[all]>=1.0.0',
-    'nteract-scrapbook[all]>=0.2.1',
+    'papermill[all]>=1.2.1',
+    'nteract-scrapbook[all]>=0.3.1',
 ]
 password = [
     'bcrypt>=2.0.0',
@@ -434,7 +434,7 @@ def do_setup():
         version=version,
         packages=find_packages(exclude=['tests*']),
         package_data={
-            '': ['airflow/alembic.ini', "airflow/git_version"],
+            '': ['airflow/alembic.ini', "airflow/git_version", "*.ipynb"],
             'airflow.serialization': ["*.json"],
         },
         include_package_data=True,
