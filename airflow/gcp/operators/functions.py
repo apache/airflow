27fbd61dab157a501e2ca4cf5a98e0a49cf50f05
chico <fxchiang@gmail.com>
2019-12-26 15:59:32 +0100

    [AIRFLOW-5739] Add deprecation warnings and backward compatibility

---
+++ airflow/gcp/operators/functions.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""
+This module is deprecated. Please use `airflow.providers.google.cloud.operators.functions`.
+"""
+
+import warnings
+
+from airflow.providers.google.cloud.operators.functions import (
+    GcfFunctionDeployOperator,
+    ZipPathPreprocessor,
+    GcfFunctionDeleteOperator,
+    GcfFunctionInvokeOperator,
+)
+
+warnings.warn(
+    "This module is deprecated. Please use `airflow.providers.google.cloud.operators.functions`",
+    DeprecationWarning, stacklevel=2
+)
+
+
+class GcfFunctionDeployOperator(GcfFunctionDeployOperator):
+    """
+    This class is deprecated.
+    Please use `airflow.providers.google.cloud.operators.functions.GcfFunctionDeployOperator`.
+    """
+
+    def __init__(self, *args, **kwargs):
+        warnings.warn(
+            """This class is deprecated.
+            Please use `airflow.providers.google.cloud.operators.functions.GcfFunctionDeployOperator`.""",
+            DeprecationWarning, stacklevel=2
+        )
+        super().__init__(*args, **kwargs)
+
+
+class ZipPathPreprocessor(ZipPathPreprocessor):
+    """
+    This class is deprecated.
+    Please use `airflow.providers.google.cloud.operators.functions.ZipPathPreprocessor`.
+    """
+
+    def __init__(self, *args, **kwargs):
+        warnings.warn(
+            """This class is deprecated.
+            Please use `airflow.providers.google.cloud.operators.functions.ZipPathPreprocessor`.""",
+            DeprecationWarning, stacklevel=2
+        )
+        super().__init__(*args, **kwargs)
+
+
+class GcfFunctionDeleteOperator(GcfFunctionDeleteOperator):
+    """
+    This class is deprecated.
+    Please use `airflow.providers.google.cloud.operators.functions.GcfFunctionDeleteOperator`.
+    """
+
+    def __init__(self, *args, **kwargs):
+        warnings.warn(
+            """This class is deprecated.
+            Please use `airflow.providers.google.cloud.operators.functions.GcfFunctionDeleteOperator`.""",
+            DeprecationWarning, stacklevel=2
+        )
+        super().__init__(*args, **kwargs)
+
+
+
+class GcfFunctionInvokeOperator(GcfFunctionInvokeOperator):
+    """
+    This class is deprecated.
+    Please use `airflow.providers.google.cloud.operators.functions.GcfFunctionInvokeOperator`.
+    """
+
+    def __init__(self, *args, **kwargs):
+        warnings.warn(
+            """This class is deprecated.
+            Please use `airflow.providers.google.cloud.operators.functions.GcfFunctionInvokeOperator`.""",
+            DeprecationWarning, stacklevel=2
+        )
+        super().__init__(*args, **kwargs)
