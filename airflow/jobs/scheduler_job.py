a9865191a64dc5e4930051a52a4f0d07ca02daa0
tooptoop4 <33283496+tooptoop4@users.noreply.github.com>
2019-12-28 09:57:24 +0000

    Update airflow/jobs/scheduler_job.py
    Co-Authored-By: Kaxil Naik <kaxilnaik@gmail.com>

--- airflow/jobs/scheduler_job.py
+++ airflow/jobs/scheduler_job.py
@@ -715,7 +715,7 @@ class DagFileProcessor(LoggingMixin):
                             schedule_delay)
                 self.log.info("Created %s", dag_run)
             self._process_task_instances(dag, tis_out)
-            if conf.getboolean('core', 'CHECK_SLAS'):
+            if conf.getboolean('core', 'CHECK_SLAS', fallback=True):
                 self.manage_slas(dag)
 
     def _find_dags_to_process(self, dags: List[DAG], paused_dag_ids: Set[str]):
