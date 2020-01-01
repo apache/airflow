341f9760c7e69ffaabcc67abcc16f5c7f08c2d5c
tooptoop4 <33283496+tooptoop4@users.noreply.github.com>
2020-01-01 20:19:33 +0000

    add comment

--- airflow/models/taskinstance.py
+++ airflow/models/taskinstance.py
@@ -964,6 +964,7 @@ class TaskInstance(Base, LoggingMixin):
             self.refresh_from_db(lock_for_update=True)
             self.state = State.SUCCESS
         except AirflowSkipException as e:
+            # Recording SKIP
             # log only if exception has any arguments to prevent log flooding
             if e.args:
                 self.log.info(e)
