820bab41bab1e716016a77d210c8900c664d80c6
Ahmed Elzeiny <alfred.e@leantaas.com>
2020-01-02 09:29:17 -0800

    Add Fargate Task Collection test-case suite

--- tests/executors/test_fargate_executor.py
+++ tests/executors/test_fargate_executor.py
@@ -1,47 +1,81 @@
-import unittest
-from unittest.mock import Mock
-
+import datetime as dt
+from unittest import mock, TestCase
+from botocore import loaders as botoloader
+from airflow.configuration import conf
 from airflow.executors.fargate_executor import *
 
 
-class TestFargateTaskCollection(unittest.TestCase):
-    def test_fargate_collection(self):
-        collection = FargateTaskCollection()
-        # Add first task
-        first_task = FargateTask({'arn': '001'})
-        first_airflow_key = Mock(spec=TaskInstanceKeyType)
-        collection.add_task(first_task, first_airflow_key)
-        # Add second task
-        second_task = FargateTask({'arn': '002'})
-        second_airflow_key = Mock(spec=TaskInstanceKeyType)
-        collection.add_task(second_task, second_airflow_key)
+class TestFargateTaskCollection(TestCase):
+    def test_add(self):
+        self.assertEqual(len(self.collection), 2)
 
         # Check basic get for first task
-        self.assertEqual(len(collection), 2)
-        self.assertEqual(collection.task_by_arn('001'), first_task)
-        self.assertEqual(collection['001'], first_task)
-        self.assertEqual(collection.task_by_key(first_airflow_key), first_task)
+        self.assertEqual(self.collection.task_by_arn('001'), self.first_task)
+        self.assertEqual(self.collection['001'], self.first_task)
+        self.assertEqual(self.collection.task_by_key(self.first_airflow_key), self.first_task)
 
         # Check basic get for second task
-        self.assertEqual(collection.task_by_arn('002'), second_task)
-        self.assertEqual(collection['002'], second_task)
-        self.assertEqual(collection.task_by_key(second_airflow_key), second_task)
+        self.assertEqual(self.collection.task_by_arn('002'), self.second_task)
+        self.assertEqual(self.collection['002'], self.second_task)
+        self.assertEqual(self.collection.task_by_key(self.second_airflow_key), self.second_task)
 
+    def test_list(self):
         # Check basic list by ARNs & airflow-task-keys
-        self.assertListEqual(collection.get_all_arns(), ['001', '002'])
-        self.assertListEqual(collection.get_all_task_keys(), [first_airflow_key, second_airflow_key])
+        self.assertListEqual(self.collection.get_all_arns(), ['001', '002'])
+        self.assertListEqual(self.collection.get_all_task_keys(), [self.first_airflow_key, self.second_airflow_key])
 
+    def test_pop(self):
         # pop first task & ensure that it's removed
-        self.assertEqual(collection.pop_by_arn('001'), first_task)
-        self.assertNotIn('001', collection.get_all_arns())
-        collection.add_task(first_task, first_airflow_key)
-        self.assertEqual(collection.pop_by_key(first_airflow_key), first_task)
-        self.assertNotIn('001', collection.get_all_arns())
+        self.assertEqual(self.collection.pop_by_arn('001'), self.first_task)
+        self.assertNotIn('001', self.collection.get_all_arns())
+        self.collection.add_task(self.first_task, self.first_airflow_key)
+        self.assertEqual(self.collection.pop_by_key(self.first_airflow_key), self.first_task)
+        self.assertNotIn('001', self.collection.get_all_arns())
 
+    def test_update(self):
         # update arn with new task object
-        collection.add_task(first_task, first_airflow_key)
-        self.assertEqual(collection['001'], first_task)
-        updated_task = FargateTask({'arn': '001'})
-        collection.update_task(updated_task)
-        self.assertEqual(collection['001'], updated_task)
+        self.collection.add_task(self.first_task, self.first_airflow_key)
+        self.assertEqual(self.collection['001'], self.first_task)
+        updated_task = FargateTask({'taskArn': '001'})
+        self.collection.update_task(updated_task)
+        self.assertEqual(self.collection['001'], updated_task)
+
+    def test_task_state(self):
+        running_fargate_task = FargateTask({
+            'taskArn': 'ABC',
+            'containers': [{
+                'lastStatus': 'PENDING',
+                'exitCode': 0
+            }]
+        })
+
+        success_fargate_task = FargateTask({
+            'taskArn': 'ABC',
+            'containers': [{
+                'lastStatus': 'STOPPED',
+                'exitCode': 0
+            }]
+        })
+
+        failed_fargate_task = FargateTask({
+            'taskArn': 'ABC',
+            'containers': [{
+                'lastStatus': 'STOPPED',
+                'exitCode': 100
+            }]
+        })
+        self.assertEqual(running_fargate_task.get_task_state(), State.RUNNING)
+        self.assertEqual(success_fargate_task.get_task_state(), State.SUCCESS)
+        self.assertEqual(failed_fargate_task.get_task_state(), State.FAILED)
+
+    def setUp(self):
+        self.collection = FargateTaskCollection()
+        # Add first task
+        self.first_task = FargateTask({'taskArn': '001'})
+        self.first_airflow_key = mock.Mock(spec=TaskInstanceKeyType)
+        self.collection.add_task(self.first_task, self.first_airflow_key)
+        # Add second task
+        self.second_task = FargateTask({'taskArn': '002'})
+        self.second_airflow_key = mock.Mock(spec=TaskInstanceKeyType)
+        self.collection.add_task(self.second_task, self.second_airflow_key)
 
