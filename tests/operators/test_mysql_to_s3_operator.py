79aaecb48fbc1089fd3d26b997085bc49d42d3c1
JavierLopezT <javierlopeztomas@gmail.com>
2020-01-02 16:13:56 +0100

    suggestions

--- tests/operators/test_mysql_to_s3_operator.py
+++ tests/operators/test_mysql_to_s3_operator.py
@@ -26,46 +26,41 @@ import pandas as pd
 from boto3.session import Session
 
 from airflow.operators.mysql_to_s3_operator import MySQLToS3Operator
-from airflow.utils.tests import assertEqualIgnoreMultipleSpaces
 
 
 class TestMySqlToS3Operator(unittest.TestCase):
 
     @mock.patch("boto3.session.Session")
-    @mock.patch("airflow.hooks.mysql_hook.MySqlHook.run")
-    def test_execute(self, mock_hook, mock_session,):
+    @mock.patch("airflow.hooks.mysql_hook.MySqlHook")
+    def test_execute(self, mock_hook, mock_session):
         access_key = "aws_access_key_id"
         secret_key = "aws_secret_access_key"
-        mock_session.return_value = Session(access_key, secret_key)
+        
         query = "query"
         s3_bucket = "bucket"
         s3_key = "key"
-        header = False
-        index = False
 
-        MySQLToS3Operator(query=query,
+        op = MySQLToS3Operator(query=query,
                           s3_bucket=s3_bucket,
                           s3_key=s3_key,
                           mysql_conn_id="mysql_conn_id",
                           aws_conn_id="aws_conn_id",
                           task_id="task_id",
-                          dag=None
-                          ).execute(None)
-
-        df = mock_hook.get_pandas_df(query)
-        for col in df:
-            if "float" in df[col].dtype.name and df[col].hasnans:
-                # inspect values to determine if dtype of non-null values is int or float
-                notna_series = df[col].dropna().values
-                if np.isclose(notna_series, notna_series.astype(int)).all():
-                    # set to dtype that retains integers and supports NaNs
-                    df[col] = np.where(df[col].isnull(), None, df[col]).astype(pd.Int64Dtype)
-
-        file_obj = StringIO()
-        df.to_csv(file_obj, header=header, index=index)
-        mock_session.load_file_obj(file_obj=file_obj,
-                                   key=s3_key,
-                                   bucket_name=s3_bucket)
-
-        assert mock_hook.call_count == 1
-        assertEqualIgnoreMultipleSpaces(self, mock_hook.call_args[0][0], mock_session.load_file_obj)
+                          header=False,
+                          index=False,
+                          dag=None,
+                          )
+        op.execute(None)
+        
+        mock_hook.assert_called_once_with(mysql_conn_id="mysql_conn_id")
+        
+        mock_hook.return_value.get_pandas_df.assert_called_once_with(query=query)
+        
+        test_df = pd.DataFrame({'a':'1','b':'2'}, index=[0,1])
+        mock_hook.return_value.get_pandas_df.return_value = test_df
+        
+        mock_hook.return_value.fix_int_dtypes.assert_called_once_with(test_df)
+        
+        mock_session.assert_called_once()
+        mock_session.return_value = Session(access_key, secret_key)
+        
