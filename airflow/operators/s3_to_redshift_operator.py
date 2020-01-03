c43b6c359ac962a9d218679eba80e67dd6403375
Antoine Krajnc <antoine@jedha.co>
2020-01-03 14:41:32 +0100

    Remove {table} line 94
    When using the path FROM 's3://{s3_bucket}/{s3_key}/{table}' 
    You get an error saying that there are no S3 prefix of {s3_key}/{table}
    removing {table} fixed the issue

--- airflow/operators/s3_to_redshift_operator.py
+++ airflow/operators/s3_to_redshift_operator.py
@@ -91,7 +91,7 @@ class S3ToRedshiftTransfer(BaseOperator):
 
         copy_query = """
             COPY {schema}.{table}
-            FROM 's3://{s3_bucket}/{s3_key}/{table}'
+            FROM 's3://{s3_bucket}/{s3_key}'
             with credentials
             'aws_access_key_id={access_key};aws_secret_access_key={secret_key}'
             {copy_options};
