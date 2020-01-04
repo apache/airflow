4149fa5ffb535474d12351b2afde1c89da63ac05
kaxil <kaxilnaik@gmail.com>
2020-01-04 12:06:12 +0000

    Update cli.py

--- airflow/bin/cli.py
+++ airflow/bin/cli.py
@@ -17,7 +17,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-"""Command-line interface"""
+"""Command-line iterface"""
 
 import argparse
 import os
