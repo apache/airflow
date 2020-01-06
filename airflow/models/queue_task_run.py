bf851f96f5f2ab9853649004cb413ee711d4af73
Kamil Breguła <kamil.bregula@polidea.com>
2020-01-06 21:48:31 +0100

    Remove unused parameters in QueueTaskRun

--- airflow/models/queue_task_run.py
+++ airflow/models/queue_task_run.py
@@ -30,19 +30,6 @@ class QueueTaskRun:  # pylint: disable=too-many-instance-attributes
     :type execution_date: datetime.datetime
     :param mark_success: Whether to mark the task as successful
     :type mark_success: bool
-    :param ignore_all_dependencies: Ignore all ignorable dependencies.
-        Overrides the other ignore_* parameters.
-    :type ignore_all_dependencies: bool
-    :param ignore_depends_on_past: Ignore depends_on_past parameter of DAGs
-        (e.g. for Backfills)
-    :type ignore_depends_on_past: bool
-    :param ignore_dependencies: Ignore task-specific dependencies such as depends_on_past
-        and trigger rule
-    :type ignore_dependencies: bool
-    :param force: Ignore the task instance's previous failure/success
-    :type force: bool
-    :param local: Whether to run the task locally
-    :type local: bool
     :param pickle_id: If the DAG was serialized to the DB, the ID
         associated with the pickled DAG
     :type pickle_id: unicode
@@ -63,11 +50,7 @@ class QueueTaskRun:  # pylint: disable=too-many-instance-attributes
         mark_success=None,
         pickle_id=None,
         job_id=None,
-        ignore_all_dependencies=None,
-        ignore_dependencies=None,
-        ignore_depends_on_past=None,
         force=None,
-        local=None,
         pool=None,
         raw=None,
         subdir=None,
@@ -83,11 +66,7 @@ class QueueTaskRun:  # pylint: disable=too-many-instance-attributes
         self.mark_success = mark_success
         self.pickle_id = pickle_id
         self.job_id = job_id
-        self.ignore_all_dependencies = ignore_all_dependencies
-        self.ignore_dependencies = ignore_dependencies
-        self.ignore_depends_on_past = ignore_depends_on_past
         self.force = force
-        self.local = local
         self.pool = pool
         self.raw = raw
         self.subdir = subdir
@@ -106,16 +85,8 @@ class QueueTaskRun:  # pylint: disable=too-many-instance-attributes
             cmd.extend(["--pickle", str(self.pickle_id)])
         if self.job_id:
             cmd.extend(["--job_id", str(self.job_id)])
-        if self.ignore_all_dependencies:
-            cmd.extend(["--ignore_all_dependencies"])
-        if self.ignore_dependencies:
-            cmd.extend(["--ignore_dependencies"])
-        if self.ignore_depends_on_past:
-            cmd.extend(["--ignore_depends_on_past"])
         if self.force:
             cmd.extend(["--force"])
-        if self.local:
-            cmd.extend(["--local"])
         if self.pool:
             cmd.extend(["--pool", self.pool])
         if self.raw:
