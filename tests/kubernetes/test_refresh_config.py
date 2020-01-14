f47e98f121b4001f52372595a68f7ba2ff5ba379
Jan Brusch <jan.brusch@neuland-bfi.de>
2020-01-14 16:49:22 +0100

    [AIRFLOW-5117] Fixed Bug with ISO-8601 Timestamps

--- tests/kubernetes/test_refresh_config.py
+++ tests/kubernetes/test_refresh_config.py
@@ -5,7 +5,7 @@ from airflow.kubernetes.refresh_config import _parse_timestamp
 
 class TestRefreshKubeConfigLoader(TestCase):
 
-    def test_parse_timestamp_should_convert_Z_timezone_to_unix_timestamp(self):
+    def test_parse_timestamp_should_convert_z_timezone_to_unix_timestamp(self):
         ts = _parse_timestamp("2020-01-13T13:42:20Z")
         self.assertEqual(1578922940, ts)
 
