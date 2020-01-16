cb8d53fbc64af8d6c175d0dda6ae51db65ccc19b
amichai07 <amichai.horvitz@bluevine.com>
2020-01-17 00:52:52 +0200

    [AIRFLOW-3607] Only query DB once per DAG run for TriggerRuleDep (#4751)
    This decreases scheduler delay between tasks by about 20% for larger DAGs,
    sometimes more for larger or more complex DAGs.
    
    The delay between tasks can be a major issue, especially when we have dags with
    many subdags, figures out that the scheduling process spends plenty of time in
    dependency checking, we took the trigger rule dependency which calls the db for
    each task instance, we made it call the db just once for each dag_run
    
    (cherry picked from commit 50efda5c69c1ddfaa869b408540182fb19f1a286)

--- tests/jobs/test_backfill_job.py
+++ tests/jobs/test_backfill_job.py
@@ -1300,6 +1300,7 @@ class BackfillJobTest(unittest.TestCase):
 
         session = settings.Session()
         session.merge(removed_task_ti)
+        session.commit()
 
         with timeout(seconds=30):
             job.run()
