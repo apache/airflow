apache/spark

[AIRFLOW-6397] ensure sub_process attribute exists before trying to kill it (#6958)

airflow/operators/bash_operator.py
@@ -82,6 +82,7 @@ class BashOperator(BaseOperator):
         self.output_encoding = output_encoding
         if kwargs.get('xcom_push') is not None:
             raise AirflowException("'xcom_push' was deprecated, use 'BaseOperator.do_xcom_push' instead")
+        self.sub_process = None
 
     def execute(self, context):
         """
@@ -114,7 +115,8 @@ class BashOperator(BaseOperator):
                 os.setsid()
 
             self.log.info('Running command: %s', self.bash_command)
-            sub_process = Popen(
+
+            self.sub_process = Popen(
                 ['bash', "-c", self.bash_command],
                 stdout=PIPE,
                 stderr=STDOUT,
@@ -122,23 +124,22 @@ class BashOperator(BaseOperator):
                 env=env,
                 preexec_fn=pre_exec)
 
-            self.sub_process = sub_process
-
             self.log.info('Output:')
             line = ''
-            for raw_line in iter(sub_process.stdout.readline, b''):
+            for raw_line in iter(self.sub_process.stdout.readline, b''):
                 line = raw_line.decode(self.output_encoding).rstrip()
                 self.log.info("%s", line)
 
-            sub_process.wait()
+            self.sub_process.wait()
 
-            self.log.info('Command exited with return code %s', sub_process.returncode)
+            self.log.info('Command exited with return code %s', self.sub_process.returncode)
 
-            if sub_process.returncode != 0:
+            if self.sub_process.returncode != 0:
                 raise AirflowException('Bash command failed. The command returned a non-zero exit code.')
 
         return line
 
     def on_kill(self):
         self.log.info('Sending SIGTERM signal to bash process group')
-        os.killpg(os.getpgid(self.sub_process.pid), signal.SIGTERM)
+        if self.sub_process and hasattr(self.sub_process, 'pid'):
+            os.killpg(os.getpgid(self.sub_process.pid), signal.SIGTERM)

apache/airflow

(cherry picked from commit 0f7c4563d2f8885e12312aae72bc70a22f734894)

airflow/operators/bash_operator.py
@@ -72,6 +72,7 @@ class BashOperator(BaseOperator):
         self.env = env
         self.xcom_push_flag = xcom_push
         self.output_encoding = output_encoding
+        self.sub_process = None
 
     def execute(self, context):
         """
@@ -113,26 +114,24 @@ class BashOperator(BaseOperator):
                     os.setsid()
 
                 self.log.info("Running command: %s", self.bash_command)
-                sp = Popen(
+                self.sub_process = Popen(
                     ['bash', fname],
                     stdout=PIPE, stderr=STDOUT,
                     cwd=tmp_dir, env=env,
                     preexec_fn=pre_exec)
 
-                self.sp = sp
-
                 self.log.info("Output:")
                 line = ''
-                for line in iter(sp.stdout.readline, b''):
+                for line in iter(self.sub_process.stdout.readline, b''):
                     line = line.decode(self.output_encoding).rstrip()
                     self.log.info(line)
-                sp.wait()
+                self.sub_process.wait()
                 self.log.info(
                     "Command exited with return code %s",
-                    sp.returncode
+                    self.sub_process.returncode
                 )
 
-                if sp.returncode:
+                if self.sub_process.returncode:
                     raise AirflowException("Bash command failed")
 
         if self.xcom_push_flag:
@@ -140,4 +139,5 @@ class BashOperator(BaseOperator):
 
     def on_kill(self):
         self.log.info('Sending SIGTERM signal to bash process group')
-        os.killpg(os.getpgid(self.sp.pid), signal.SIGTERM)
+        if self.sub_process and hasattr(self.sub_process, 'pid'):
+            os.killpg(os.getpgid(self.sub_process.pid), signal.SIGTERM)