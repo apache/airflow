0287ac3c02cae1b0e4acc132b02c0b1227b49488
Michael Spector <spektom@gmail.com>
2020-02-10 18:58:34 +0200

    Fixed for latest pre-commit hook

--- airflow/providers/microsoft/azure/operators/adx.py
+++ airflow/providers/microsoft/azure/operators/adx.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 #
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
@@ -17,6 +16,7 @@
 # specific language governing permissions and limitations
 # under the License.
 #
+
 """This module contains Azure Data Explorer operators"""
 from typing import Dict, Optional
 
