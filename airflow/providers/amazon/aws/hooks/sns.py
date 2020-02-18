3621fa707aa8d4c829d74bc032f998d0ee16481d
snazzyfox <snazzyfox@users.noreply.github.com>
2020-02-17 20:05:47 -0800

    [AIRFLOW-6830] Add Subject/MessageAttributes to SNS hook and operator

--- airflow/providers/amazon/aws/hooks/sns.py
+++ airflow/providers/amazon/aws/hooks/sns.py
@@ -24,6 +24,20 @@ import json
 from airflow.providers.amazon.aws.hooks.base_aws import AwsBaseHook
 
 
+def _get_message_attribute(o):
+    if isinstance(o, bytes):
+        return {'DataType': 'Binary', 'BinaryValue': o}
+    elif isinstance(o, str):
+        return {'DataType': 'String', 'StringValue': o}
+    elif isinstance(o, (int, float)):
+        return {'DataType': 'Number', 'StringValue': str(o)}
+    elif hasattr(o, '__iter__'):
+        return {'DataType': 'String.Array', 'StringValue': json.dumps(o)}
+    else:
+        raise TypeError('Values in MessageAttributes must be one of bytes, str, int, float, or iterable; '
+                        f'got {type(o)}')
+
+
 class AwsSnsHook(AwsBaseHook):
     """
     Interact with Amazon Simple Notification Service.
@@ -40,7 +54,7 @@ class AwsSnsHook(AwsBaseHook):
         self.conn = self.get_client_type('sns')
         return self.conn
 
-    def publish_to_target(self, target_arn, message, subject=None):
+    def publish_to_target(self, target_arn, message, subject=None, message_attributes=None):
         """
         Publish a message to a topic or an endpoint.
 
@@ -50,24 +64,33 @@ class AwsSnsHook(AwsBaseHook):
         :param message: str
         :param subject: subject of message
         :type subject: str
+        :param message_attributes: additional attributes to publish for message filtering. This should be
+                                   a flat dict; the DataType to be sent depends on the type of the value
+
+                                   - bytes = Binary
+                                   - str = String
+                                   - int, float = Number
+                                   - iterable = String.Array
+
+        :type message_attributes: mapping
         """
 
         conn = self.get_conn()
 
-        messages = {
-            'default': message
+        publish_kwargs = {
+            'TargetArn': target_arn,
+            'MessageStructure': 'json',
+            'Message': json.dumps({
+                'default': message
+            }),
         }
 
-        if subject is None:
-            return conn.publish(
-                TargetArn=target_arn,
-                Message=json.dumps(messages),
-                MessageStructure='json'
-            )
+        # Construct args this way because boto3 distinguishes from missing args and those set to None
+        if subject:
+            publish_kwargs['Subject'] = subject
+        if message_attributes:
+            publish_kwargs['MessageAttributes'] = {
+                key: _get_message_attribute(val) for key, val in message_attributes.items()
+            }
 
-        return conn.publish(
-            TargetArn=target_arn,
-            Message=json.dumps(messages),
-            MessageStructure='json',
-            Subject=subject
-        )
+        return conn.publish(**publish_kwargs)
--- tests/providers/amazon/aws/hooks/test_sns.py
+++ tests/providers/amazon/aws/hooks/test_sns.py
@@ -36,7 +36,7 @@ class TestAwsSnsHook(unittest.TestCase):
         self.assertIsNotNone(hook.get_conn())
 
     @mock_sns
-    def test_publish_to_target(self):
+    def test_publish_to_target_with_subject(self):
         hook = AwsSnsHook(aws_conn_id='aws_default')
 
         message = "Hello world"
@@ -49,7 +49,24 @@ class TestAwsSnsHook(unittest.TestCase):
         self.assertTrue('MessageId' in response)
 
     @mock_sns
-    def test_publish_to_target_without_subject(self):
+    def test_publish_to_target_with_attributes(self):
+        hook = AwsSnsHook(aws_conn_id='aws_default')
+
+        message = "Hello world"
+        topic_name = "test-topic"
+        target = hook.get_conn().create_topic(Name=topic_name).get('TopicArn')
+
+        response = hook.publish_to_target(target, message, message_attributes={
+            'test-string': 'string-value',
+            'test-number': 123456,
+            'test-array': ['first', 'second', 'third'],
+            'test-binary': b'binary-value',
+        })
+
+        self.assertTrue('MessageId' in response)
+
+    @mock_sns
+    def test_publish_to_target_plain(self):
         hook = AwsSnsHook(aws_conn_id='aws_default')
 
         message = "Hello world"
