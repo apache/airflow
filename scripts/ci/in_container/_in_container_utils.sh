ac45146dd21e64f5677c757dd4cf70ca834bbcc4
Jarek Potiuk <jarek.potiuk@polidea.com>
2020-02-19 01:58:53 +0100

    [AIRFLOW-6820] split breeze into functions (#7433)
    (cherry picked from commit 24f9f11775bf7b38cced9955f6c70705ad56f61c)

--- scripts/ci/in_container/_in_container_utils.sh
+++ scripts/ci/in_container/_in_container_utils.sh
@@ -43,7 +43,7 @@ function in_container_script_end() {
         echo "###########################################################################################"
 
     fi
-    if [[ ${AIRFLOW_CI_VERBOSE:="false"} == "true" ]]; then
+    if [[ ${AIRFLOW_CI_VERBOSE} == "true" ]]; then
         set +x
     fi
 }
@@ -58,9 +58,11 @@ function print_in_container_info() {
 # Cleans up PYC files (in case they come in mounted folders)
 #
 function in_container_cleanup_pyc() {
-    print_in_container_info
-    print_in_container_info "Cleaning up .pyc files"
-    print_in_container_info
+    if [[ ${AIRFLOW_CI_VERBOSE} == "true" ]]; then
+        print_in_container_info
+        print_in_container_info "Cleaning up .pyc files"
+        print_in_container_info
+    fi
     set +o pipefail
     NUM_FILES=$(sudo find . \
         -path "./airflow/www/node_modules" -prune -o \
@@ -69,19 +71,23 @@ function in_container_cleanup_pyc() {
         -path "./docs/_build" -prune -o \
         -path "./build" -prune -o \
         -name "*.pyc" | grep ".pyc$" | sudo xargs rm -vf | wc -l)
-    print_in_container_info "Number of deleted .pyc files: ${NUM_FILES}"
     set -o pipefail
-    print_in_container_info
-    print_in_container_info
+    if [[ ${AIRFLOW_CI_VERBOSE} == "true" ]]; then
+        print_in_container_info "Number of deleted .pyc files: ${NUM_FILES}"
+        print_in_container_info
+        print_in_container_info
+    fi
 }
 
 #
 # Cleans up __pycache__ directories (in case they come in mounted folders)
 #
 function in_container_cleanup_pycache() {
-    print_in_container_info
-    print_in_container_info "Cleaning up __pycache__ directories"
-    print_in_container_info
+    if [[ ${AIRFLOW_CI_VERBOSE} == "true" ]]; then
+        print_in_container_info
+        print_in_container_info "Cleaning up __pycache__ directories"
+        print_in_container_info
+    fi
     set +o pipefail
     NUM_FILES=$(find . \
         -path "./airflow/www/node_modules" -prune -o \
@@ -90,10 +96,12 @@ function in_container_cleanup_pycache() {
         -path "./docs/_build" -prune -o \
         -path "./build" -prune -o \
         -name "__pycache__" | grep "__pycache__" | sudo xargs rm -rvf | wc -l)
-    print_in_container_info "Number of deleted __pycache__ dirs (and files): ${NUM_FILES}"
     set -o pipefail
-    print_in_container_info
-    print_in_container_info
+    if [[ ${AIRFLOW_CI_VERBOSE} == "true" ]]; then
+        print_in_container_info "Number of deleted __pycache__ dirs (and files): ${NUM_FILES}"
+        print_in_container_info
+        print_in_container_info
+    fi
 }
 
 #
@@ -101,22 +109,28 @@ function in_container_cleanup_pycache() {
 # The host user.
 #
 function in_container_fix_ownership() {
-    print_in_container_info
-    print_in_container_info "Changing ownership of root-owned files to ${HOST_USER_ID}.${HOST_GROUP_ID}"
-    print_in_container_info
+    if [[ ${AIRFLOW_CI_VERBOSE} == "true" ]]; then
+        print_in_container_info
+        print_in_container_info "Changing ownership of root-owned files to ${HOST_USER_ID}.${HOST_GROUP_ID}"
+        print_in_container_info
+    fi
     set +o pipefail
     sudo find . -user root | sudo xargs chown -v "${HOST_USER_ID}.${HOST_GROUP_ID}" --no-dereference | \
         wc -l | xargs -n 1 echo "Number of files with changed ownership:"
     set -o pipefail
-    print_in_container_info
-    print_in_container_info
+    if [[ ${AIRFLOW_CI_VERBOSE} == "true" ]]; then
+        print_in_container_info
+        print_in_container_info
+    fi
 }
 
 function in_container_go_to_airflow_sources() {
     pushd "${AIRFLOW_SOURCES}"  &>/dev/null || exit 1
-    print_in_container_info
-    print_in_container_info "Running in $(pwd)"
-    print_in_container_info
+    if [[ ${AIRFLOW_CI_VERBOSE} == "true" ]]; then
+        print_in_container_info
+        print_in_container_info "Running in $(pwd)"
+        print_in_container_info
+    fi
 }
 
 function in_container_basic_sanity_check() {
