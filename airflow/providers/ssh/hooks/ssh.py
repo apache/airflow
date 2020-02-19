21963d9b1e9937a1372dd10b983180be29a82bbf
Bert Desmet <bedesmet@deloitte.com>
2020-02-19 08:53:06 +0100

    whitespace?

--- airflow/providers/ssh/hooks/ssh.py
+++ airflow/providers/ssh/hooks/ssh.py
@@ -114,7 +114,7 @@ class SSHHook(BaseHook):
                 cert_file = extra_options.get('cert_file')
 
                 if private_key and key_type == 'rsa':
-                     self.pkey = paramiko.RSAKey.from_private_key(StringIO(private_key))
+                    self.pkey = paramiko.RSAKey.from_private_key(StringIO(private_key))
                 elif private_key and key_type == 'ed25519':
                     self.pkey = paramiko.Ed25519Key.from_private_key(StringIO(private_key))
                 elif private_key and key_type == 'ecdsa':
@@ -125,7 +125,7 @@ class SSHHook(BaseHook):
                     self.pkey = paramiko.Ed25519Key.from_private_key_file(key_file)
                 elif key_file and key_type == 'ecdsa':
                     self.pkey = paramiko.ECDSAKey.from_private_key_file(key_file)
-                
+
                 if cert_key:
                     self.pkey.load_certificate(StringIO(cert_key))
                 elif cert_file:
