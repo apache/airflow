df9aa3034fee107f53a2bc35a5363e9b251dcc2a
dan <dferguson@ippon.fr>
2020-03-02 15:47:10 -0500

    Moved files to providers package.  Externalized connection logic from init function

--- airflow/providers/apache/kafka/hooks/kafka_consumer_hook.py
+++ airflow/providers/apache/kafka/hooks/kafka_consumer_hook.py
@@ -1,31 +1,21 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
 #
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-
+from airflow.hooks import base_hook as BaseHook
 from kafka import KafkaConsumer
-
-from airflow.hooks.base_hook import BaseHook
 
 
 class KafkaConsumerHook(BaseHook):
-    """
-    KafkaConsumerHook Class.
-    """
+
     DEFAULT_HOST = 'kafka1'
     DEFAULT_PORT = 9092
 
@@ -40,13 +30,7 @@ class KafkaConsumerHook(BaseHook):
         self.host = host
         self.port = port
 
-    def get_conn(self) -> KafkaConsumer:
-        """
-            A Kafka Consumer object.
-
-        :return:
-            A Kafka Consumer object.
-        """
+    def get_conn(self):
         if not self._conn:
             conn = self.get_connection(self.conn_id)
             service_options = conn.extra_dejson
@@ -61,12 +45,10 @@ class KafkaConsumerHook(BaseHook):
             )
         return self.consumer
 
-    def get_messages(self, timeout_ms=5000) -> dict:
+    def get_messages(self, timeout_ms=5000):
         """
         Get all the messages haven't been consumed, it doesn't
         block by default, then commit the offset.
-
-        :param timeout_ms:
         :return:
             A list of messages
         """
@@ -80,10 +62,7 @@ class KafkaConsumerHook(BaseHook):
 
     def __repr__(self):
         """
-            A pretty version of the connection string.
-
-        :return:
-            A pretty version of the connection string.
+        Pretty the hook with the connection info
         """
         connected = self.consumer is not None
         return '<KafkaConsumerHook ' \
