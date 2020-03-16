72df90291b60c352ca173d6786c9f1a5a1f3b810
aaron.fowles <aaron.fowles@ovoenergy.com>
2020-03-16 12:07:30 +0000

    attempt to increase field size

--- tests/providers/ssh/hooks/test_ssh.py
+++ tests/providers/ssh/hooks/test_ssh.py
@@ -58,12 +58,8 @@ def generate_public_key_string(pkey: paramiko.PKey):
 
 
 TEST_PKEY = paramiko.RSAKey.generate(4096)
-TEST_PRIVATE_KEY = str(generate_key_string(pkey=TEST_PKEY))
-TEST_HOST_PUBLIC_KEY = 'AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCP' \
-    'y6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZ' \
-    'ETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoa' \
-    'SjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4' \
-    'yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=='
+TEST_PRIVATE_KEY = generate_key_string(pkey=TEST_PKEY)
+TEST_HOST_PUBLIC_KEY = generate_public_key_string(pkey=TEST_PKEY)
 
 
 class TestSSHHook(unittest.TestCase):
