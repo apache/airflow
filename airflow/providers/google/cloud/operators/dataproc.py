bd08b43c7b7d3ebd0620093da1fe0d19c3833b35
EphraimBuddy <splendidzigy24@gmail.com>
2020-03-16 21:13:06 +0100

    [AIRFLOW-5610] Fix isort import test issues

--- airflow/providers/google/cloud/operators/dataproc.py
+++ airflow/providers/google/cloud/operators/dataproc.py
@@ -22,6 +22,7 @@ This module contains Google Dataproc operators.
 # pylint: disable=C0302
 
 import inspect
+import ntpath
 import os
 import re
 import time
@@ -30,7 +31,6 @@ import warnings
 from datetime import datetime, timedelta
 from typing import Dict, List, Optional, Sequence, Set, Tuple, Union
 
-import ntpath
 from google.api_core.exceptions import AlreadyExists
 from google.api_core.retry import Retry
 from google.cloud.dataproc_v1beta2.types import (  # pylint: disable=no-name-in-module
