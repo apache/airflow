1affd1d6733503404bfda7ad9da32a4c5264ec58
javier162380 <javierllorente16@gmail.com>
2020-03-29 18:57:38 +0200

    adding kubernetes command module for generating kubernetes pod template

---
+++ airflow/cli/commands/kubernetes_command.py
@@ -0,0 +1,84 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+import inspect
+import yaml
+
+from kubernetes.client import ApiClient
+
+from airflow.kubernetes.pod_generator import PodGenerator
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
+from airflow.kubernetes import pod_generator
+from airflow.kubernetes.k8s_model import append_to_pod
+from airflow.settings import pod_mutation_hook
+from airflow.utils.cli import get_dag, get_dag_by_file_location, process_subdir
+
+
+def is_a_kubernetes_pod_operator_instance(task):
+
+    return isinstance(task, KubernetesPodOperator)
+
+
+def get_kubernetes_pod_attributes(kubernetes_task_kwargs):
+
+    all_instances = inspect.getmro(KubernetesPodOperator)
+    attrs = []
+    for instance in all_instances:
+        attrs.extend(list(inspect.signature(instance.__init__).parameters.keys()))
+
+    return {arg: value for arg, value in kubernetes_task_kwargs.items() if arg in attrs}
+
+
+def generate_pod_preview(kubernetes_tasks_args):
+    POD_GENERATOR_ATTRS = inspect.getfullargspec(PodGenerator).args
+    pod_args = {attr: value for attr, value in kubernetes_tasks_args.items() if attr in POD_GENERATOR_ATTRS}
+
+    # TODO: This step should not be done there is naming conversion in all kubernetes classes args to env_vars to envs.
+    if not 'args' in pod_args:
+        pod_args['args'] = kubernetes_tasks_args.get('arguments', None)
+    if not 'envs' in pod_args:
+        pod_args['envs'] = kubernetes_tasks_args.get('env_vars', None)
+    if not 'extract_xcom' in pod_args:
+        pod_args['extract_xcom'] = kubernetes_tasks_args.get('do_xcom_push', None)
+
+    pod_instance = pod_generator.PodGenerator(**(pod_args)).gen_pod()
+    append_to_pod(pod_instance,
+                  kubernetes_tasks_args.get('pod_runtime_info_envs', None) +
+                  kubernetes_tasks_args.get('ports', None) +
+                  kubernetes_tasks_args.get('resources', None) +
+                  kubernetes_tasks_args.get('secrets', None) +
+                  kubernetes_tasks_args.get('volumes', None) +
+                  kubernetes_tasks_args.get('volume_mounts', None)
+                  )
+
+    res = ApiClient().sanitize_for_serialization(pod_instance)
+
+    res['metadata']['name'] = pod_args['name']
+
+    return res
+
+
+def kubernetes_preview(args):
+
+    dag_tasks = get_dag(args.subdir, args.dag_id).tasks
+    kubernetes_tasks_args = [task.__dict__ for task in dag_tasks if is_a_kubernetes_pod_operator_instance(task)]
+    if kubernetes_tasks_args:
+        for task_args in kubernetes_tasks_args:
+            kubernetes_pod_args = get_kubernetes_pod_attributes(task_args)
+            pod_request = generate_pod_preview(kubernetes_pod_args)
+            yaml_object = yaml.dump(pod_request)
+            print(yaml_object)
