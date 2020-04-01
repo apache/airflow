063deb19d4859434f83b7c7638222449c38a7472
Davide Malagoli <malagoli@google.com>
2020-04-01 15:30:06 +0200

    fixed test on google data fusion hook

--- tests/providers/google/cloud/hooks/test_datafusion.py
+++ tests/providers/google/cloud/hooks/test_datafusion.py
@@ -205,19 +205,11 @@ class TestDataFusionHook:
     @mock.patch(HOOK_STR.format("DataFusionHook._cdap_request"))
     def test_start_pipeline(self, mock_request, hook):
         mock_request.return_value.status = 200
-        hook.start_pipeline(pipeline_name=PIPELINE_NAME, instance_url=INSTANCE_URL)
-        mock_request.assert_called_once_with(
-            url=f"{INSTANCE_URL}/v3/namespaces/default/apps/{PIPELINE_NAME}/"
-                f"workflows/DataPipelineWorkflow/start",
-            method="POST",
-            body=None
+        hook.start_pipeline(
+            pipeline_name=PIPELINE_NAME,
+            instance_url=INSTANCE_URL,
+            runtime_args=RUNTIME_ARGS
         )
-
-    @mock.patch(HOOK_STR.format("DataFusionHook._cdap_request"))
-    def test_start_pipeline(self, mock_request, hook):
-        mock_request.return_value.status = 200
-        hook.start_pipeline(pipeline_name=PIPELINE_NAME, instance_url=INSTANCE_URL,
-            runtime_args=RUNTIME_ARGS)
         mock_request.assert_called_once_with(
             url=f"{INSTANCE_URL}/v3/namespaces/default/apps/{PIPELINE_NAME}/"
                 f"workflows/DataPipelineWorkflow/start",
