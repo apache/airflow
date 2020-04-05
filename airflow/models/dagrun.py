a258dbb1c7e57229efd208669848a649c94d4ebf
Muye Yang <muye.yang@gmail.com>
2020-04-05 17:09:27 +0800

    Delete cast(DR.execution_date, DateTime)
    The cast was added by #1871, but it prevents sqlalchemy from automatically
    converting execution_date to UTC and fails test test_schedule_dag_fake_scheduled_previous
    when your default_timezone is not set to UTC.

--- airflow/models/dagrun.py
+++ airflow/models/dagrun.py
@@ -106,11 +106,9 @@ class DagRun(Base, LoggingMixin):
         """
         DR = DagRun
 
-        exec_date = func.cast(self.execution_date, DateTime)
-
         dr = session.query(DR).filter(
             DR.dag_id == self.dag_id,
-            func.cast(DR.execution_date, DateTime) == exec_date,
+            DR.execution_date == self.execution_date,
             DR.run_id == self.run_id
         ).one()
 
