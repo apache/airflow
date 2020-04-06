10bc71d060cbd6f2ceb88d67c95d8941414b5247
Ojong Yoland Taku <ojongyoland13@gmail.com>
2020-04-06 06:41:53 +0100

    [AIRFLOW-2644] - Allow wild-cards in the search box in the UI

--- airflow/www/views.py
+++ airflow/www/views.py
@@ -17,7 +17,6 @@
 # under the License.
 #
 
-import re
 import copy
 import itertools
 import json
@@ -25,6 +24,7 @@ import logging
 import math
 import os
 import pkgutil
+import re
 import socket
 import traceback
 from collections import defaultdict
@@ -279,7 +279,7 @@ class Airflow(AirflowBaseView):
                     DagModel.dag_id.ilike('%' + arg_search_query + '%') |
                     DagModel.owners.ilike('%' + arg_search_query + '%')
                 )
-                dags_query = re.findall('*' + arg_search_query +'*', DagModel.dag_id)
+                dags_query = re.findall('*' + arg_search_query + '*', DagModel.dag_id)
 
             if arg_tags_filter:
                 dags_query = dags_query.filter(DagModel.tags.any(DagTag.name.in_(arg_tags_filter)))
