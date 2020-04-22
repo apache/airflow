4568d2845524fb8a3f14c221f8976e1ac8e65adf
Lokesh Lal <lokeshlal@gmail.com>
2020-04-22 15:00:10 +0530

    corrected static code analysis issue

--- airflow/sensors/external_task_sensor.py
+++ airflow/sensors/external_task_sensor.py
@@ -192,7 +192,7 @@ class ExternalTaskSensor(BaseSensorOperator):
             count = session.query(func.count()).filter(
                 TI.dag_id == self.external_dag_id,
                 TI.task_id == self.external_task_id,
-                TI.state.in_(states),
+                TI.state.in_(states),  # pylint: disable=no-member
                 TI.execution_date.in_(dttm_filter),
             ).scalar()
         else:
