apache/spark

commit 1dc14d503c5b58deab195f82e0f7357a0fda7f20

WTForms 2.3.0 break our Flask apps (#8512)
* WTForms 2.3.0 break our Flask apps

requirements/setup-3.6.md5
@@ -1,1 +1,1 @@
-a83d618cf60f585307063fd0f39cf1b3  /opt/airflow/setup.py
+d353a3cc6876e76d66261929ef6e459f  /opt/airflow/setup.py

requirements/setup-3.7.md5
@@ -1,1 +1,1 @@
-a83d618cf60f585307063fd0f39cf1b3  /opt/airflow/setup.py
+d353a3cc6876e76d66261929ef6e459f  /opt/airflow/setup.py

setup.py
@@ -598,6 +598,7 @@ INSTALL_REQUIREMENTS = [
     'tzlocal>=1.4,<2.0.0',
     'unicodecsv>=0.14.1',
     'werkzeug<1.0.0',
+    'WTforms<2.3.0',  # TODO: Remove after https://github.com/dpgaspar/Flask-AppBuilder/issues/1356 is fixed and released.
 ]
 
 

apache/airflow

commit 8b666e04f41d55aa03524d1831062f5db234f883

Add proper sequence of setup.py dependencies
Because of our unstable Travis we merged a failing build
with #8512 and #8515 :(

requirements/setup-3.6.md5
@@ -1,1 +1,1 @@
-d353a3cc6876e76d66261929ef6e459f  /opt/airflow/setup.py
+4741e8f02fd2d28c38d3713d19c9e247  /opt/airflow/setup.py

requirements/setup-3.7.md5
@@ -1,1 +1,1 @@
-d353a3cc6876e76d66261929ef6e459f  /opt/airflow/setup.py
+4741e8f02fd2d28c38d3713d19c9e247  /opt/airflow/setup.py

setup.py
@@ -598,7 +598,7 @@ INSTALL_REQUIREMENTS = [
     'tzlocal>=1.4,<2.0.0',
     'unicodecsv>=0.14.1',
     'werkzeug<1.0.0',
-    'WTforms<2.3.0',
+    'wtforms<2.3.0',
     # TODO: Remove after https://github.com/dpgaspar/Flask-AppBuilder/issues/1356 is fixed and released.
 ]
 