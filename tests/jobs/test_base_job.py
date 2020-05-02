0246f569873a386b06c98cc97ef1668b5e84ae62
Kaxil Naik <kaxilnaik@gmail.com>
2020-05-02 18:30:45 +0100

    Fix displaying Executor Class Name in "Base Job" table (#8679)
    (cherry picked from commit 0a7b5004ac5df830389e16f9344ef549d27e0353)

--- tests/jobs/test_base_job.py
+++ tests/jobs/test_base_job.py
@@ -24,10 +24,12 @@ import unittest
 from sqlalchemy.exc import OperationalError
 
 from airflow.jobs import BaseJob
+from airflow.executors.sequential_executor import SequentialExecutor
 from airflow.utils import timezone
 from airflow.utils.db import create_session
 from airflow.utils.state import State
 from tests.compat import Mock, patch
+from tests.test_utils.config import conf_vars
 
 
 class BaseJobTest(unittest.TestCase):
@@ -119,4 +121,24 @@ class BaseJobTest(unittest.TestCase):
 
             job.heartbeat()
 
-            self.assertEqual(job.latest_heartbeat, when, "attriubte not updated when heartbeat fails")
+            self.assertEqual(job.latest_heartbeat, when, "attribute not updated when heartbeat fails")
+
+    @conf_vars({('scheduler', 'max_tis_per_query'): '100'})
+    @patch('airflow.jobs.base_job.executors.get_default_executor')
+    @patch('airflow.jobs.base_job.get_hostname')
+    @patch('airflow.jobs.base_job.getpass.getuser')
+    def test_essential_attr(self, mock_getuser, mock_hostname, mock_default_executor):
+        mock_sequential_executor = SequentialExecutor()
+        mock_hostname.return_value = "test_hostname"
+        mock_getuser.return_value = "testuser"
+        mock_default_executor.return_value = mock_sequential_executor
+
+        test_job = self.TestJob(None, heartrate=10, dag_id="example_dag", state=State.RUNNING)
+        self.assertEqual(test_job.executor_class, "SequentialExecutor")
+        self.assertEqual(test_job.heartrate, 10)
+        self.assertEqual(test_job.dag_id, "example_dag")
+        self.assertEqual(test_job.hostname, "test_hostname")
+        self.assertEqual(test_job.max_tis_per_query, 100)
+        self.assertEqual(test_job.unixname, "testuser")
+        self.assertEqual(test_job.state, "running")
+        self.assertEqual(test_job.executor, mock_sequential_executor)
