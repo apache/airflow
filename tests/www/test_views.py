390282b026fd38280e8e84e36cd24e22ffee6424
XD-DENG <xd_deng@hotmail.com>
2020-05-07 08:25:53 +0200

    Further enhance UI test

--- tests/www/test_views.py
+++ tests/www/test_views.py
@@ -503,16 +503,23 @@ class TestAirflowBaseViews(TestBase):
         self.assertIsNone(None, resp_json['scheduler']['latest_scheduler_heartbeat'])
 
     def test_home(self):
-        resp = self.client.get('home', follow_redirects=True)
-        self.check_content_in_response('DAGs', resp)
-        val_state_color_mapping = 'const STATE_COLOR = {"failed": "red", ' \
-                                  '"null": "lightblue", "queued": "gray", ' \
-                                  '"removed": "lightgrey", "running": "lime", ' \
-                                  '"scheduled": "tan", "shutdown": "blue", ' \
-                                  '"skipped": "pink", "success": "green", ' \
-                                  '"up_for_reschedule": "turquoise", ' \
-                                  '"up_for_retry": "gold", "upstream_failed": "orange"};'
-        self.check_content_in_response(val_state_color_mapping, resp)
+        with self.capture_templates() as templates:
+            resp = self.client.get('home', follow_redirects=True)
+            self.check_content_in_response('DAGs', resp)
+            val_state_color_mapping = 'const STATE_COLOR = {"failed": "red", ' \
+                                      '"null": "lightblue", "queued": "gray", ' \
+                                      '"removed": "lightgrey", "running": "lime", ' \
+                                      '"scheduled": "tan", "shutdown": "blue", ' \
+                                      '"skipped": "pink", "success": "green", ' \
+                                      '"up_for_reschedule": "turquoise", ' \
+                                      '"up_for_retry": "gold", "upstream_failed": "orange"};'
+            self.check_content_in_response(val_state_color_mapping, resp)
+
+        self.assertEqual(len(templates), 1)
+        self.assertEqual(templates[0].name, 'airflow/dags.html')
+        state_color_mapping = State.state_color.copy()
+        state_color_mapping["null"] = state_color_mapping.pop(None)
+        self.assertEqual(templates[0].local_context['state_color'], state_color_mapping)
 
     def test_users_list(self):
         resp = self.client.get('users/list', follow_redirects=True)
