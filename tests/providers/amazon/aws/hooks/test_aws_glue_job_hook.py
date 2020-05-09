efeff6285ee7852ec74be1564586c271fd6b8f47
Kaxil Naik <kaxilnaik@gmail.com>
2020-05-09 23:19:09 +0100

    Update tests/providers/amazon/aws/hooks/test_aws_glue_job_hook.py

--- tests/providers/amazon/aws/hooks/test_aws_glue_job_hook.py
+++ tests/providers/amazon/aws/hooks/test_aws_glue_job_hook.py
@@ -1,14 +1,14 @@
 #
 # Licensed to the Apache Software Foundation (ASF) under one
-# TODO: This license is not consistent with license used in the project.
-#       Delete the inconsistent license and above line and rerun pre-commit to insert a good license.
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
 # regarding copyright ownership.  The ASF licenses this file
 # to you under the Apache License, Version 2.0 (the
 # "License"); you may not use this file except in compliance
 # with the License.  You may obtain a copy of the License at
-# http://www.apache.org/licenses/LICENSE-2.0
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
 # Unless required by applicable law or agreed to in writing,
 # software distributed under the License is distributed on an
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
