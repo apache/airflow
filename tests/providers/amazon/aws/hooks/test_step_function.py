aed57916013ca02b1ad365535ad4bb89e5e2f2cc
Chauncy McCaughey <chamcca@amazon.com>
2020-05-11 09:00:02 -0600

    replace usage of deprecated assertEquals with assertEqual

--- tests/providers/amazon/aws/hooks/test_step_function.py
+++ tests/providers/amazon/aws/hooks/test_step_function.py
@@ -33,7 +33,7 @@ class TestStepFunctionHook(unittest.TestCase):
     @mock_stepfunctions
     def test_get_conn_returns_a_boto3_connection(self):
         hook = StepFunctionHook(aws_conn_id='aws_default')
-        self.assertEquals('stepfunctions', hook.get_conn().meta.service_model.service_name)
+        self.assertEqual('stepfunctions', hook.get_conn().meta.service_model.service_name)
 
     @mock_stepfunctions
     def test_start_execution(self):
