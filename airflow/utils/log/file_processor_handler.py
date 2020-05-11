4c79dce338b8ec1524494d809109447eae466a1e
James Timmins <james@astronomer.io>
2020-05-11 16:44:11 -0700

    Use absolute path when creating log files.

--- airflow/utils/log/file_processor_handler.py
+++ airflow/utils/log/file_processor_handler.py
@@ -132,18 +132,19 @@ class FileProcessorHandler(logging.Handler):
         :param filename: task instance object
         :return: relative log path of the given task instance
         """
-        relative_path = self._render_filename(filename)
-        full_path = os.path.join(self._get_log_directory(), relative_path)
-        directory = os.path.dirname(full_path)
+        relative_log_file_path = os.path.join(
+            self._get_log_directory(), self._render_filename(filename))
+        log_file_path = os.path.abspath(relative_log_file_path)
+        directory = os.path.dirname(log_file_path)
 
         if not os.path.exists(directory):
             try:
-                os.makedirs(os.path.abspath(directory))
+                os.makedirs(directory)
             except OSError:
                 if not os.path.isdir(directory):
                     raise
 
-        if not os.path.exists(full_path):
-            open(os.path.abspath(full_path), "a").close()
+        if not os.path.exists(log_file_path):
+            open(log_file_path, "a").close()
 
-        return full_path
+        return log_file_path
