2643ab55926a6dfd439f350c13fd8c3e576a9d45
joppe <joppe.vos@gmail.com>
2020-05-18 13:21:10 +0200

    adding relative path to example yaml input

--- airflow/providers/google/cloud/example_dags/example_cloud_build.py
+++ airflow/providers/google/cloud/example_dags/example_cloud_build.py
@@ -29,6 +29,7 @@ This DAG relies on the following OS environment variables:
 """
 
 import os
+from pathlib import Path
 
 from future.backports.urllib.parse import urlparse
 
@@ -44,6 +45,8 @@ GCP_SOURCE_REPOSITORY_NAME = os.environ.get("GCP_CLOUD_BUILD_REPOSITORY_NAME", "
 
 GCP_SOURCE_ARCHIVE_URL_PARTS = urlparse(GCP_SOURCE_ARCHIVE_URL)
 GCP_SOURCE_BUCKET_NAME = GCP_SOURCE_ARCHIVE_URL_PARTS.netloc
+
+CURRENT_FOLDER = Path(__file__).parent
 
 # [START howto_operator_gcp_create_build_from_storage_body]
 create_build_from_storage_body = {
@@ -101,7 +104,8 @@ with models.DAG(
 
     # [START howto_operator_gcp_create_build_from_yaml_body]
     create_build_from_file = CloudBuildCreateOperator(
-        task_id="create_build_from_file", project_id=GCP_PROJECT_ID, body='example_cloud_build.yaml'
+        task_id="create_build_from_file", project_id=GCP_PROJECT_ID,
+        body=str(CURRENT_FOLDER.joinpath('example_cloud_build.yaml'))
     )
     # [END howto_operator_gcp_create_build_from_yaml_body]
     create_build_from_storage >> create_build_from_storage_result  # pylint: disable=pointless-statement
