d71839cd05d5c81b0615245021d121f32a2ed738
Daniel Imberman <daniel.imberman@gmail.com>
2020-05-20 09:00:06 -0700

    Update airflow/contrib/operators/file_to_wasb.py
    Co-authored-by: Kaxil Naik <kaxilnaik@gmail.com>

--- airflow/contrib/operators/file_to_wasb.py
+++ airflow/contrib/operators/file_to_wasb.py
@@ -58,7 +58,6 @@ class FileToWasbOperator(BaseOperator):
         hook = WasbHook(wasb_conn_id=self.wasb_conn_id)
         self.log.info(
             'Uploading %s to wasb://%s '  # noqa: F523
-            'as %s'.format(self.file_path, self.container_name, self.blob_name)  # noqa: F523
-        )
+            'as %s', self.file_path, self.container_name, self.blob_name)
         hook.load_file(self.file_path, self.container_name,
                        self.blob_name, **self.load_options)
