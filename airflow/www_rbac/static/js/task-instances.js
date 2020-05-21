3b3eae94105625111fa29a2a093e2a2033b8683d
Joe Harris <joe4291@btinternet.com>
2020-05-21 10:53:54 +0100

    Add TaskInstance state to TI Tooltip to be colour-blind friendlier (#8910)
    Currently there is no way to determine the state of a TaskInstance in the graph view or tree view for people with colour blindness
    
    Approximately 4.5% of people experience some form of colour vision deficiency
    
    (cherry picked from commit f3f74c73201d947ce9334c50630b9cf5debbc66a)

--- airflow/www_rbac/static/js/task-instances.js
+++ airflow/www_rbac/static/js/task-instances.js
@@ -62,6 +62,9 @@ function generateTooltipDateTimes(startDate, endDate, dagTZ) {
 
 export default function tiTooltip(ti, {includeTryNumber = false} = {}) {
   let tt = '';
+  if (ti.state !== undefined) {
+    tt += `<strong>Status:</strong> ${escapeHtml(ti.state)}<br><br>`;
+  }
   if (ti.task_id !== undefined) {
     tt += `Task_id: ${escapeHtml(ti.task_id)}<br>`;
   }
