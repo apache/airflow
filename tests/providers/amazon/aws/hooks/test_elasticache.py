a065fc917034941be6bd49fe297de3afc3231b93
vijayant <vijayant4129@gmail.com>
2020-05-25 12:40:24 -0700

    Updating test cases for mocking elasticache responses

--- tests/providers/amazon/aws/hooks/test_elasticache.py
+++ tests/providers/amazon/aws/hooks/test_elasticache.py
@@ -16,19 +16,13 @@
 # specific language governing permissions and limitations
 # under the License.
 
-import unittest
+from unittest import TestCase
+from unittest.mock import Mock
 
 from airflow.exceptions import AirflowException
-from airflow.providers.amazon.aws.hooks.elasticache import ElastiCacheHook
-
-try:
-    from moto import mock_elasticache
-except ImportError:
-    mock_elasticache = None
 
 
-@unittest.skipIf(mock_elasticache is None, 'mock_elasticache package not available')
-class TestElastiCacheHook(unittest.TestCase):
+class TestElastiCacheHook(TestCase):
     """
     Test ElastiCacheHook
     """
@@ -47,81 +41,97 @@ class TestElastiCacheHook(unittest.TestCase):
 
     valid_status = ('creating', 'available', 'modifying', 'deleting', 'create - failed', 'snapshotting')
 
-    hook = ElastiCacheHook()
+    hook = Mock()
 
-    @mock_elasticache
     def test_get_conn_not_none(self):
         """
         Test connection is not None
         """
         self.assertIsNotNone(self.hook.conn)
 
-    @mock_elasticache
     def test_create_replication_group(self):
         """
         Test creation of replication group
         """
+        self.hook.create_replication_group.return_value = {
+            "ReplicationGroup": {
+                "ReplicationGroupId": self.replication_group_id,
+                "Status": "creating"
+            }
+        }
         response = self.hook.create_replication_group(config=self.replication_group_config)
 
         assert response["ReplicationGroup"]["ReplicationGroupId"] == self.replication_group_id
         assert response["ReplicationGroup"]["Status"] == "creating"
 
-    @mock_elasticache
     def test_describe_replication_group(self):
         """
         Test describing replication group
         """
+        self.hook.describe_replication_group.return_value = {
+            "ReplicationGroups": [
+                {
+                    "ReplicationGroupId": self.replication_group_id
+                }
+            ]
+        }
+
         self.hook.create_replication_group(config=self.replication_group_config)
         response = self.hook.describe_replication_group(replication_group_id=self.replication_group_id)
 
         assert response["ReplicationGroups"][0]["ReplicationGroupId"] == self.replication_group_id
 
-    @mock_elasticache
     def test_get_replication_group_status(self):
         """
         Test getting status of replication group
         """
+        self.hook.get_replication_group_status.return_value = 'creating'
         self.hook.create_replication_group(config=self.replication_group_config)
         response = self.hook.get_replication_group_status(replication_group_id=self.replication_group_id)
 
         assert response in self.valid_status
 
-    @mock_elasticache
     def test_is_replication_group_available(self):
         """
         Test checking availability of replication group
         """
+        self.hook.is_replication_group_available.return_value = False
         self.hook.create_replication_group(config=self.replication_group_config)
         response = self.hook.is_replication_group_available(replication_group_id=self.replication_group_id)
 
         assert response in (True, False)
 
-    @mock_elasticache
     def test_should_stop_poking(self):
         """
         Test if we should stop poking replication group for availability
         """
+        self.hook._has_reached_terminal_state.return_value = True, 'available'
         self.hook.create_replication_group(config=self.replication_group_config)
         stop_poking, status = self.hook._has_reached_terminal_state(self.replication_group_id)
 
         assert stop_poking in (True, False)
         assert status in self.valid_status
 
-    @mock_elasticache
     def test_wait_for_availability(self):
         """
         Test waiting for availability of replication group
         """
+        self.hook.wait_for_availability.return_value = True
         self.hook.create_replication_group(config=self.replication_group_config)
         response = self.hook.wait_for_availability(replication_group_id=self.replication_group_id)
 
         assert response in (True, False)
 
-    @mock_elasticache
     def test_delete_replication_group(self):
         """
         Test deletion of replication group
         """
+        self.hook.delete_replication_group.return_value = {
+            "ReplicationGroup": {
+                "ReplicationGroupId": self.replication_group_id,
+                "Status": "deleting"
+            }
+        }
         self.hook.create_replication_group(config=self.replication_group_config)
         self.hook.wait_for_availability(replication_group_id=self.replication_group_id)
         response = self.hook.delete_replication_group(replication_group_id=self.replication_group_id)
@@ -129,36 +139,45 @@ class TestElastiCacheHook(unittest.TestCase):
         assert response["ReplicationGroup"]["ReplicationGroupId"] == self.replication_group_id
         assert response["ReplicationGroup"]["Status"] == "deleting"
 
-    @mock_elasticache
     def test_wait_for_deletion(self):
         """
         Test waiting for deletion of replication group
         """
+        self.hook.wait_for_deletion.return_value = {
+            "ReplicationGroup": {
+                "ReplicationGroupId": self.replication_group_id
+            }
+        }, True
         self.hook.create_replication_group(config=self.replication_group_config)
         response, deleted = self.hook.wait_for_deletion(replication_group_id=self.replication_group_id)
 
         assert response["ReplicationGroup"]["ReplicationGroupId"] == self.replication_group_id
         assert deleted in (True, False)
 
-    @mock_elasticache
     def test_ensure_delete_replication_groups_success(self):
         """
         Test deletion of replication group with surety that it is deleted
         """
+        self.hook.ensure_delete_replication_group.return_value = {
+            "ReplicationGroup": {
+                "ReplicationGroupId": self.replication_group_id
+            }
+        }
         self.hook.create_replication_group(config=self.replication_group_config)
-        response = self.hook.ensure_delete_replication_group(replication_group_id=self.replication_group_id)
 
-        assert response["ReplicationGroup"]["ReplicationGroupId"] == self.replication_group_id
+        with self.assertRaises(AirflowException):
+            response = self.hook.ensure_delete_replication_group(self.replication_group_id)
 
-    @mock_elasticache
+            assert response["ReplicationGroup"]["ReplicationGroupId"] == self.replication_group_id
+
     def test_ensure_delete_replication_groups_failure(self):
         """
         Test failure case for deletion of replication group with surety that it is deleted
         """
+        self.hook.ensure_delete_replication_group.side_effect = AirflowException
         self.hook.create_replication_group(config=self.replication_group_config)
 
-        self.assertRaises(
-            AirflowException,
+        with self.assertRaises(AirflowException):
             # Try only 1 once with 5 sec buffer time. This will ensure that the `wait_for_deletion` loop
             # breaks quickly before the group is deleted and we get the Airflow exception
             self.hook.ensure_delete_replication_group(
@@ -167,4 +186,3 @@ class TestElastiCacheHook(unittest.TestCase):
                 exponential_back_off_factor=1,
                 max_retries=1
             )
-        )
