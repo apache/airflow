ad52c52e160a0b9480e92e95f3599c77bea33d32
mikaelfr <miksu.fredriksson@gmail.com>
2020-05-29 17:25:49 +0300

    Add tags parameter and member variable to BaseOperator.

--- airflow/models/baseoperator.py
+++ airflow/models/baseoperator.py
@@ -389,6 +389,7 @@ class BaseOperator(Operator, LoggingMixin, metaclass=BaseOperatorMeta):
         self.email = email
         self.email_on_retry = email_on_retry
         self.email_on_failure = email_on_failure
+
         # mypy doesn't yet understand differing getter and setter types
         self.task_tags = task_tags  # type: ignore
 
