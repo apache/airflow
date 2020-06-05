f1ad8925c546e7c37704fa813947815bf93aad86
mikaelfr <miksu.fredriksson@gmail.com>
2020-06-05 13:34:48 +0300

    Add example dag for task tags

---
+++ airflow/example_dags/example_task_tags.py
@@ -0,0 +1,62 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+""" Example dag demonstrating the usage of tags in tasks """
+
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.utils.dates import days_ago
+
+args = {
+    'owner': 'airflow',
+    'start_date': days_ago(2),
+}
+
+dag = DAG(
+    dag_id='example_task_tags',
+    default_args=args,
+    schedule_interval="@daily",
+    tags=['example']
+)
+
+init = DummyOperator(
+    task_id='initialize',
+    dag=dag
+)
+
+extract = []
+for i in range(3):
+    extract.append(DummyOperator(
+        task_id='extract_account_' + str(i),
+        tags=['extract'],
+        dag=dag
+    ))
+
+transform = DummyOperator(
+    task_id='transform',
+    tags=['transform'],
+    dag=dag
+)
+
+load = DummyOperator(
+    task_id='load',
+    tags=['load'],
+    dag=dag
+)
+
+init >> extract >> transform >> load
