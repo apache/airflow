ef9463547264a211eef9ca31f16f0ff5e11d18bd
Jarek Potiuk <jarek@potiuk.com>
2020-06-07 23:19:25 +0200

    fixup! Improved cloud tool available in the trimmed down CI container

--- scripts/ci/prepare_tool_scripts.sh
+++ scripts/ci/prepare_tool_scripts.sh
@@ -30,14 +30,16 @@ function prepare_tool_script() {
     cat >"${TARGET_TOOL_PATH}" <<EOF
 #!/usr/bin/env bash
 docker run --rm -it \
+    -v "\${HOST_AIRFLOW_SOURCES}/files:/files" \
     -v "\${HOST_AIRFLOW_SOURCES}:/opt/airflow" \
     -v "\${HOST_HOME}/${VOLUME}:/root/${VOLUME}" \
     "${IMAGE}" ${COMMAND} "\$@"
 RES=\$?
 docker run --rm \
+    -v "\${HOST_AIRFLOW_SOURCES}/files:/files" \
     -v "\${HOST_HOME}/${VOLUME}:/root/${VOLUME}" \
     "\${AIRFLOW_CI_IMAGE}" bash -c \
-    "find '/root/${VOLUME}' -user root -print0 | xargs --null chown '\${HOST_USER_ID}.\${HOST_GROUP_ID}' --no-dereference" >/dev/null 2>&1
+    "find '/files/' '/root/${VOLUME}' -user root -print0 | xargs --null chown '\${HOST_USER_ID}.\${HOST_GROUP_ID}' --no-dereference" >/dev/null 2>&1
 exit \${RES}
 EOF
 
