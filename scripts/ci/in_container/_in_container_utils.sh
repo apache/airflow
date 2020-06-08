90e55c693847b9a70fd8b6bb650674f39380bf91
Jarek Potiuk <jarek@potiuk.com>
2020-06-08 07:21:55 +0200

    fixup! fixup! fixup! fixup! fixup! Improved cloud tool available in the trimmed down CI container

--- scripts/ci/in_container/_in_container_utils.sh
+++ scripts/ci/in_container/_in_container_utils.sh
@@ -99,7 +99,7 @@ function in_container_fix_ownership() {
         sudo find "${AIRFLOW_SOURCES}" -print0 -user root \
         | sudo xargs --null chown "${HOST_USER_ID}.${HOST_GROUP_ID}" --no-dereference >/dev/null 2>&1
         sudo find "/root/.aws" "/root/.azure" "/root/.config" "/root/.docker" -print0 -user root \
-        | sudo xargs --null chown "${HOST_USER_ID}.${HOST_GROUP_ID}" --no-dereference >/dev/null 2>&1
+        | sudo xargs --null chown "${HOST_USER_ID}.${HOST_GROUP_ID}" --no-dereference || true >/dev/null 2>&1
         set -o pipefail
     fi
 }
