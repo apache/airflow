24580d80e0c942c1da14549a827264fce6908bde
Kamil Breguła <kamil.bregula@polidea.com>
2020-06-15 15:22:16 +0200

    fixup! fixup! Fix failing master - Invalid assertion in conf

--- tests/www/test_views.py
+++ tests/www/test_views.py
@@ -630,6 +630,7 @@ class TestAirflowBaseViews(TestBase):
         ("hello\nworld", r'\"conf\":{\"abc\":\"hello\\nworld\"}}'),
         ("hello'world", r'\"conf\":{\"abc\":\"hello\\u0027world\"}}'),
         ("<script>", r'\"conf\":{\"abc\":\"\\u003cscript\\u003e\"}}'),
+        ("\"", r'\"conf\":{\"abc\":\"\\\"\"}}'),
     ])
     def test_escape_in_tree_view(self, test_str, expected_text):
         dag = self.dagbag.dags['test_tree_view']
