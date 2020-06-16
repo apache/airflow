38a74715a8e9fbd4aefc683edc6994c72a178ee0
EphraimBuddy <splendidzigy24@gmail.com>
2020-06-16 14:36:33 +0100

    add log endpoint

--- airflow/api_connexion/endpoints/log_endpoint.py
+++ airflow/api_connexion/endpoints/log_endpoint.py
@@ -14,13 +14,80 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+import logging
+import os
+from io import BytesIO
 
-# TODO(mik-laj): We have to implement it.
-#     Do you want to help? Please look at: https://github.com/apache/airflow/issues/8135
+from flask import request, send_file
+
+from airflow import models, settings
+from airflow.api_connexion.exceptions import NotFound
+from airflow.configuration import conf
+from airflow.models import DagRun
+from airflow.settings import STORE_SERIALIZED_DAGS
+from airflow.utils.helpers import render_log_filename
+from airflow.utils.session import provide_session
+
+if os.environ.get('SKIP_DAGS_PARSING') != 'True':
+    dagbag = models.DagBag(settings.DAGS_FOLDER, store_serialized_dags=STORE_SERIALIZED_DAGS)
+else:
+    dagbag = models.DagBag(os.devnull, include_examples=False)
 
 
-def get_log():
+@provide_session
+def get_log(session, dag_id, dag_run_id, task_id, task_try_number,
+            full_content=False, token=None):
     """
     Get logs for specific task instance
     """
-    raise NotImplementedError("Not implemented yet.")
+    if not token:
+        token = {}
+    # Todo use itsdangerous to decrypt metadata
+
+    metadata = token
+    if metadata.get('download_logs', None) and not full_content:
+        full_content = True
+    if full_content:
+        metadata['download_logs'] = True
+    query = session.query(DagRun).filter(DagRun.dag_id == dag_id)
+    dag_run = query.filter(DagRun.run_id == dag_run_id).first()
+    if not dag_run:
+        raise NotFound("Specified DagRun not found")
+
+    logger = logging.getLogger('airflow.task')
+    task_log_reader = conf.get('logging', 'task_log_reader')
+    handler = next((handler for handler in logger.handlers
+                    if handler.name == task_log_reader), None)
+
+    ti = dag_run.get_task_instance(task_id, session)
+    try:
+        if ti is None:
+            logs = ["*** Task instance did not exist in the DB\n"]
+            metadata['end_of_log'] = True
+        else:
+            dag = dagbag.get_dag(dag_id)
+            ti.task = dag.get_task(ti.task_id)
+            logs, metadatas = handler.read(ti, task_try_number, metadata=metadata)
+            metadata = metadatas[0]
+
+        if request.headers['Content-Type'] == 'application/json':
+            return dict(continuation_token=str(metadata),
+                        content=str(logs))
+
+        file_obj = BytesIO(b'\n'.join(
+            log.encode('utf-8') for log in logs
+        ))
+        filename_template = conf.get('core', 'LOG_FILENAME_TEMPLATE')
+        attachment_filename = render_log_filename(
+            ti=ti,
+            try_number="all" if task_try_number is None else task_try_number,
+            filename_template=filename_template)
+        return send_file(file_obj, as_attachment=True,
+                         attachment_filename=attachment_filename)
+
+    except AttributeError as err:
+        error_message = ["Task log handler {} does not support read logs.\n{}\n"
+                         .format(task_log_reader, str(err))]
+        metadata['end_of_log'] = True
+        return dict(continuation_token=str(metadata),
+                    content=str(error_message))
--- tests/api_connexion/endpoints/test_log_endpoint.py
+++ tests/api_connexion/endpoints/test_log_endpoint.py
@@ -14,25 +14,110 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+import copy
+import logging.config
+import os
+import shutil
+import sys
+import tempfile
 import unittest
 
-import pytest
-
+from airflow import DAG, settings
+from airflow.config_templates.airflow_local_settings import DEFAULT_LOGGING_CONFIG
+from airflow.models import DagRun, TaskInstance
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.utils import timezone
+from airflow.utils.session import create_session, provide_session
+from airflow.utils.types import DagRunType
 from airflow.www import app
+from tests.test_utils.config import conf_vars
+from tests.test_utils.db import clear_db_runs
 
 
 class TestGetLog(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls) -> None:
-        super().setUpClass()
-        cls.app = app.create_app(testing=True)  # type:ignore
+    DAG_ID = 'dag_for_testing_log_endpoint'
+    DAG_ID_REMOVED = 'removed_dag_for_testing_log_endpoint'
+    TASK_ID = 'task_for_testing_log_endpoint'
 
     def setUp(self) -> None:
-        self.client = self.app.test_client()  # type:ignore
+        self.default_time = "2020-06-10T20:00:00+00:00"
 
-    @pytest.mark.skip(reason="Not implemented yet")
-    def test_should_response_200(self):
+        # Make sure that the configure_logging is not cached
+        self.old_modules = dict(sys.modules)
+
+        # Create a custom logging configuration
+        logging_config = copy.deepcopy(DEFAULT_LOGGING_CONFIG)
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        logging_config['handlers']['task']['base_log_folder'] = os.path.normpath(
+            os.path.join(current_dir, 'test_logs'))
+
+        logging_config['handlers']['task']['filename_template'] = \
+            '{{ ti.dag_id }}/{{ ti.task_id }}/' \
+            '{{ ts | replace(":", ".") }}/{{ try_number }}.log'
+
+        # Write the custom logging configuration to a file
+        self.settings_folder = tempfile.mkdtemp()
+        settings_file = os.path.join(self.settings_folder, "airflow_local_settings.py")
+        new_logging_file = "LOGGING_CONFIG = {}".format(logging_config)
+        with open(settings_file, 'w') as handle:
+            handle.writelines(new_logging_file)
+        sys.path.append(self.settings_folder)
+
+        with conf_vars({('logging', 'logging_config_class'): 'airflow_local_settings.LOGGING_CONFIG'}):
+            self.app = app.create_app(testing=True)  # type:ignore
+            self.client = self.app.test_client()  # type:ignore
+            settings.configure_orm()
+            from airflow.api_connexion.endpoints.log_endpoint import dagbag
+            dag = DAG(self.DAG_ID, start_date=timezone.parse(self.default_time))
+            dag.sync_to_db()
+            dag_removed = DAG(self.DAG_ID_REMOVED, start_date=timezone.parse(self.default_time))
+            dag_removed.sync_to_db()
+            dagbag.bag_dag(dag, parent_dag=dag, root_dag=dag)
+            with create_session() as session:
+                self.ti = TaskInstance(
+                    task=DummyOperator(task_id=self.TASK_ID, dag=dag),
+                    execution_date=timezone.parse(self.default_time)
+                )
+                self.ti.try_number = 1
+                self.ti_removed_dag = TaskInstance(
+                    task=DummyOperator(task_id=self.TASK_ID, dag=dag_removed),
+                    execution_date=timezone.parse(self.default_time)
+                )
+                self.ti_removed_dag.try_number = 1
+
+                session.merge(self.ti)
+                session.merge(self.ti_removed_dag)
+
+    def tearDown(self):
+        logging.config.dictConfig(DEFAULT_LOGGING_CONFIG)
+        clear_db_runs()
+
+        # Remove any new modules imported during the test run. This lets us
+        # import the same source files for more than one test.
+        for mod in [m for m in sys.modules if m not in self.old_modules]:
+            del sys.modules[mod]
+
+        sys.path.remove(self.settings_folder)
+        shutil.rmtree(self.settings_folder)
+
+        super().tearDown()
+
+    @provide_session
+    def test_should_response_200(self, session):
+        dagrun_model = DagRun(
+            dag_id=self.DAG_ID,
+            run_id='TEST_DAG_RUN_ID',
+            run_type=DagRunType.MANUAL.value,
+            execution_date=timezone.parse(self.default_time),
+            start_date=timezone.parse(self.default_time),
+            external_trigger=True,
+        )
+        session.add(dagrun_model)
+        session.commit()
+        headers = {'Content-Type': 'application/json'}
         response = self.client.get(
-            "/dags/TEST_DG_ID/dagRuns/TEST_DAG_RUN_ID/taskInstances/TEST_TASK_ID/logs/3"
+            f"api/v1/dags/{self.DAG_ID}/dagRuns/TEST_DAG_RUN_ID/"
+            f"taskInstances/{self.TASK_ID}/logs/1",
+            headers=headers
         )
         assert response.status_code == 200
