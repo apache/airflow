e17430c585b8572aed10627ca7f87d43a856ba95
Aneesh Joseph <admin@coderplus.com>
2020-06-19 17:04:58 +0530

    add more tests

--- scripts/ci/ci_run_helm_testing.sh
+++ scripts/ci/ci_run_helm_testing.sh
@@ -24,5 +24,5 @@ echo "Chart directory is $CHART_DIR"
 
 docker run -w /airflow-chart -v "$CHART_DIR":/airflow-chart \
   --entrypoint /bin/sh \
-  divya12/helm-unittest \
+  aneeshkj/helm-unittest \
   -c "helm repo add stable https://kubernetes-charts.storage.googleapis.com; helm dependency update ; helm unittest ."
