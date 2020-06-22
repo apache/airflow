37533585c5b673d4ebb8cee89aa03de5e5adc316
Kaxil Naik <kaxilnaik@gmail.com>
2020-06-22 01:33:32 +0100

    fixup! fixup! Add __init__ methods to Variable class

--- airflow/models/variable.py
+++ airflow/models/variable.py
@@ -44,7 +44,7 @@ class Variable(Base, LoggingMixin):
     _val = Column('val', Text)
     is_encrypted = Column(Boolean, unique=False, default=False)
 
-    def __init__(self, key: str = None, val: Any = None):
+    def __init__(self, key=None, val=None):
         super().__init__()
         self.key = key
         self.val = val
