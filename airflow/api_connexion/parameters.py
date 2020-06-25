ed11790a5a95cd5a6efd4c1470464b79d1d4e678
EphraimBuddy <splendidzigy24@gmail.com>
2020-06-25 03:05:04 +0100

    Revert "add rfc339-validator for date-time validator"
    This reverts commit e8bd3240074b1a299a0540a019c166c25de10c62.

--- airflow/api_connexion/parameters.py
+++ airflow/api_connexion/parameters.py
@@ -17,8 +17,32 @@
 from functools import wraps
 from typing import Callable, Dict
 
+from jsonschema import draft4_format_checker
+from pendulum.parsing import ParserError
+
 from airflow.api_connexion.exceptions import BadRequest
 from airflow.configuration import conf
+from airflow.utils import timezone
+
+# Database entity fields
+dag_id = "dag_id"
+pool_id = "pool_id"
+
+
+@draft4_format_checker.checks('timezone-datetime')
+def is_custom_date(val):
+    """ Custom datetime format"""
+    if val is None:  # this avoids validating nullable fields
+        return True
+    if not isinstance(val, str):
+        return False
+    if ' ' in val:
+        return False
+    try:
+        _ = timezone.parse(val)
+    except (ParserError, TypeError):
+        return False
+    return True
 
 
 def check_limit(value: int):
