d838092abb612bcad8184a872d23f656408346af
Jakob Engl <jakob.engl@hotmail.de>
2020-06-26 22:02:45 +0100

    fixed wrong classs name also in code not just in import

--- tests/providers/microsoft/mssql/operators/test_mssql_to_oracle.py
+++ tests/providers/microsoft/mssql/operators/test_mssql_to_oracle.py
@@ -48,7 +48,7 @@ class TestMsSqlToOracleOperator(unittest.TestCase):
         mock_cursor.description.__iter__.return_value = cursor_description
         mock_cursor.fetchmany.side_effect = [cursor_rows, []]
 
-        op = MSSQLToOracleTransferOperator(
+        op = MsSqlToOracleOperator(
             task_id='copy_data',
             oracle_destination_conn_id=oracle_destination_conn_id,
             destination_table=destination_table,
