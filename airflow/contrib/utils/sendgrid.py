bf61086f24033a0dac5912dfc2610056d72bc874
Kamil Breguła <kamil.bregula@polidea.com>
2020-06-28 11:21:37 +0200

    fixup! Move out sendgrid email from airflow.contrib

--- airflow/contrib/utils/sendgrid.py
+++ airflow/contrib/utils/sendgrid.py
@@ -1,3 +1,4 @@
+#
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -17,8 +18,8 @@
 
 """
 This module is deprecated.
-Please use `airflow.providers.sendgrid.utils.emailer`
-."""
+Please use `airflow.providers.sendgrid.utils.emailer`.
+"""
 
 import warnings
 
