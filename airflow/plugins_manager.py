813b1aae93fca16717951a365a3501b68c153049
j-y-matsubara <j.y.matsubara.git@gmail.com>
2020-07-02 10:22:39 +0900

    Retest

--- airflow/plugins_manager.py
+++ airflow/plugins_manager.py
@@ -159,7 +159,7 @@ def load_entrypoint_plugins():
 
 def load_plugins_from_plugin_directory():
     """
-    Load and register Airflow Plugins from plugins directory.
+    Load and register Airflow Plugins from plugins directory
     """
     global import_errors  # pylint: disable=global-statement
     global plugins  # pylint: disable=global-statement
