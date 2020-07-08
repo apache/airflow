47c09359c1986fec7ec9c1eec637f233df8ef75e
EphraimBuddy <splendidzigy24@gmail.com>
2020-07-08 21:08:08 +0100

    increase typing coverage for apache provider

--- airflow/hooks/base_hook.py
+++ airflow/hooks/base_hook.py
@@ -21,7 +21,7 @@ import random
 from typing import List
 
 from airflow import secrets
-from airflow.models import Connection
+from airflow.models.connection import Connection
 from airflow.utils.log.logging_mixin import LoggingMixin
 
 log = logging.getLogger(__name__)
@@ -82,6 +82,6 @@ class BaseHook(LoggingMixin):
         connection = cls.get_connection(conn_id)
         return connection.get_hook()
 
-    def get_conn(self):
+    def get_conn(self) -> None:
         """Returns connection for the hook."""
         raise NotImplementedError()
