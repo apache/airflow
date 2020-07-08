2462ed58141d5b05867afc707409cc1952f876bc
vanka56 <venkateshbtechit@gmail.com>
2020-07-08 16:57:48 -0700

    Update test_hive.py

--- tests/providers/apache/hive/hooks/test_hive.py
+++ tests/providers/apache/hive/hooks/test_hive.py
@@ -559,7 +559,7 @@ class TestHiveMetastoreHook(TestHiveEnvironment):
 
     mock.patch('hmsclient.genthrift.hive_metastore.ThriftHiveMetastore.Client.drop_partition')
     def test_drop_partition(self, thrift_mock):
-        self.assertTrue(self.hook.drop_partitions(self.table, db=self.database, 
+        self.assertTrue(self.hook.drop_partitions(self.table, db=self.database,
                                                   part_vals=[DEFAULT_DATE_DS]))
         thrift_mock.assert_called_once_with(table=self.table, db=self.database)
 
