dfb5892118406c932e1c9765562eca8128c18d9f
usharma <usharma@akamai.com>
2020-07-23 01:56:39 +0530

    Added conflict dispositon feature

--- airflow/cli/cli_parser.py
+++ airflow/cli/cli_parser.py
@@ -626,10 +626,19 @@ ARG_INCLUDE_SECRETS = Arg(
     ),
     action="store_true",
     default=False)
-ARG_CONN_FILE_PATH = Arg(
-    ('--file-path',),
-    help='Import connections from a file. Acceptable file formats .json or .env',
+ARG_CONN_IMPORT = Arg(
+    ('file',),
+    help='Import connections from a file. Acceptable file formats .json, .yaml, .env',
     type=str)
+ARG_CONFLICT_DISPOSITION = Arg(
+    ('--conflict-disposition',),
+    help=("Specifies the action that occurs if the connection already exists. Default value : restrict\n"
+          "Values -\n"
+          "\toverwrite : Overwrites the connection\n"
+          "\tignore : Skip this connection\n"
+          "\trestrict : Raise an exception\n"),
+    type=str)
+
 
 # users
 ARG_USERNAME = Arg(
@@ -1102,7 +1111,7 @@ CONNECTIONS_COMMANDS = (
         name='import',
         help='Import connections',
         func=lazy_load_command('airflow.cli.commands.connection_command.connections_import'),
-        args=(ARG_CONN_FILE_PATH,),
+        args=(ARG_CONN_IMPORT, ARG_CONFLICT_DISPOSITION),
     ),
 )
 USERS_COMMANDS = (
