8aad44ef6eaac74325fa97359f1d3af917adda21
HasanJ <hasan_sg@hotmail.com>
2020-07-27 18:08:00 +0300

    Make conn_id as non-nullable

--- airflow/migrations/versions/8d48763f6d53_add_unique_constraint_to_conn_id.py
+++ airflow/migrations/versions/8d48763f6d53_add_unique_constraint_to_conn_id.py
@@ -35,21 +35,33 @@ depends_on = None
 
 
 def upgrade():
-    """Apply add unique constraint to conn_id"""
+    """Apply add unique constraint to conn_id and set it as non-nullable"""
     try:
         with op.batch_alter_table('connection') as batch_op:
             batch_op.create_unique_constraint(
                 constraint_name="unique_conn_id",
                 columns=["conn_id"]
             )
+
+            batch_op.alter_column(
+                "conn_id",
+                nullable=False,
+                existing_type=sa.String
+            )
     except sa.exc.IntegrityError:
-        raise Exception("Make sure there are no duplicate connections with the same conn_id")
+        raise Exception("Make sure there are no duplicate connections with the same conn_id or null values")
 
 
 def downgrade():
-    """Unapply add unique constraint to conn_id"""
+    """Unapply add unique constraint to conn_id and set it as non-nullable"""
     with op.batch_alter_table('connection') as batch_op:
         batch_op.drop_constraint(
             constraint_name="unique_conn_id",
             type_="unique"
+        )
+
+        batch_op.alter_column(
+            "conn_id",
+            nullable=True,
+            existing_type=sa.String
         )
