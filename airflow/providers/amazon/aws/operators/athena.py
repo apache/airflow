59c2b053f31c22d4e0defdde23f44a907a11eda3
coopergillan <cooper.gillan@gmail.com>
2020-07-31 20:15:50 +0000

    Add type annotations to AWSAthenaOperator
    Remove hook as an attribute and get it locally as needed to ensure mypy
    knows the type being called. Default query_execution_id to an empty
    string since it must be a string.

--- airflow/providers/amazon/aws/operators/athena.py
+++ airflow/providers/amazon/aws/operators/athena.py
@@ -17,6 +17,7 @@
 # under the License.
 #
 
+from typing import Any, Dict, Mapping, Optional
 from uuid import uuid4
 
 from airflow.models import BaseOperator
@@ -39,7 +40,7 @@ class AWSAthenaOperator(BaseOperator):
     :param sleep_time: Time to wait between two consecutive call to check query status on athena
     :type sleep_time: int
     :param max_tries: Number of times to poll for query state before function exits
-    :type max_triex: int
+    :type max_tries: int
     """
 
     ui_color = '#44b5e2'
@@ -49,16 +50,16 @@ class AWSAthenaOperator(BaseOperator):
     @apply_defaults
     def __init__(  # pylint: disable=too-many-arguments
         self,
-        query,
-        database,
-        output_location,
-        aws_conn_id="aws_default",
-        client_request_token=None,
-        workgroup="primary",
-        query_execution_context=None,
-        result_configuration=None,
-        sleep_time=30,
-        max_tries=None,
+        query: str,
+        database: str,
+        output_location: str,
+        aws_conn_id: str = "aws_default",
+        client_request_token: Optional[str] = None,
+        workgroup: str = "primary",
+        query_execution_context: Optional[Dict[str, str]] = None,
+        result_configuration: Optional[Dict[Any, Any]] = None,
+        sleep_time: int = 30,
+        max_tries: int = 0,
         *args,
         **kwargs
     ):
@@ -69,32 +70,31 @@ class AWSAthenaOperator(BaseOperator):
         self.aws_conn_id = aws_conn_id
         self.client_request_token = client_request_token or str(uuid4())
         self.workgroup = workgroup
-        self.query_execution_context = query_execution_context or {}
-        self.result_configuration = result_configuration or {}
+        self.query_execution_context = query_execution_context or {'Database': self.database}
+        self.result_configuration = result_configuration or {'OutputLocation': self.output_location}
         self.sleep_time = sleep_time
         self.max_tries = max_tries
-        self.query_execution_id = None
-        self.hook = None
+        self.query_execution_id: str = ""
 
-    def get_hook(self):
+    def get_hook(self) -> AWSAthenaHook:
         """Create and return an AWSAthenaHook."""
         return AWSAthenaHook(self.aws_conn_id, sleep_time=self.sleep_time)
 
-    def execute(self, context):
+    def execute(self, context: Mapping[Any, Any]) -> str:
         """
         Run Presto Query on Athena
         """
-        self.hook = self.get_hook()
+        hook = self.get_hook()
 
         self.query_execution_context['Database'] = self.database
         self.result_configuration['OutputLocation'] = self.output_location
-        self.query_execution_id = self.hook.run_query(self.query, self.query_execution_context,
-                                                      self.result_configuration, self.client_request_token,
-                                                      self.workgroup)
-        query_status = self.hook.poll_query_status(self.query_execution_id, self.max_tries)
+        self.query_execution_id = hook.run_query(self.query, self.query_execution_context,
+                                                 self.result_configuration, self.client_request_token,
+                                                 self.workgroup)
+        query_status = hook.poll_query_status(self.query_execution_id, self.max_tries)
 
         if query_status in AWSAthenaHook.FAILURE_STATES:
-            error_message = self.hook.get_state_change_reason(self.query_execution_id)
+            error_message = hook.get_state_change_reason(self.query_execution_id)
             raise Exception(
                 'Final state of Athena job is {}, query_execution_id is {}. Error: {}'
                 .format(query_status, self.query_execution_id, error_message))
@@ -106,16 +106,17 @@ class AWSAthenaOperator(BaseOperator):
 
         return self.query_execution_id
 
-    def on_kill(self):
+    def on_kill(self) -> None:
         """
         Cancel the submitted athena query
         """
+        hook = self.get_hook()
         if self.query_execution_id:
             self.log.info('⚰️⚰️⚰️ Received a kill Signal. Time to Die')
             self.log.info(
                 'Stopping Query with executionId - %s', self.query_execution_id
             )
-            response = self.hook.stop_query(self.query_execution_id)
+            response = hook.stop_query(self.query_execution_id)
             http_status_code = None
             try:
                 http_status_code = response['ResponseMetadata']['HTTPStatusCode']
@@ -128,4 +129,4 @@ class AWSAthenaOperator(BaseOperator):
                     self.log.info(
                         'Polling Athena for query with id %s to reach final state', self.query_execution_id
                     )
-                    self.hook.poll_query_status(self.query_execution_id)
+                    hook.poll_query_status(self.query_execution_id)
