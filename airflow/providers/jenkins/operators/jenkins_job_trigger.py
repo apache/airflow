510ee46634f04a587f81508353958e54800bf887
coopergillan <cooper.gillan@gmail.com>
2020-08-02 14:24:16 +0000

    Update data type in docstring

--- airflow/providers/jenkins/operators/jenkins_job_trigger.py
+++ airflow/providers/jenkins/operators/jenkins_job_trigger.py
@@ -86,7 +86,7 @@ class JenkinsJobTriggerOperator(BaseOperator):
     :param job_name: The name of the job to trigger
     :type job_name: str
     :param parameters: The parameters block to provide to jenkins. (templated)
-    :type parameters: str
+    :type parameters: JSON blob, path to JSON file, Dict, or List
     :param sleep_time: How long will the operator sleep between each status
         request for the job (min 1, default 10)
     :type sleep_time: int
