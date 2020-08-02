01a13e8ff8501b618fc1682f3ae0aa2c6e3d9251
coopergillan <cooper.gillan@gmail.com>
2020-08-02 04:30:39 +0000

    Fix import linting error

--- airflow/providers/amazon/aws/sensors/athena.py
+++ airflow/providers/amazon/aws/sensors/athena.py
@@ -16,7 +16,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
-from typing import Any, Mapping, Optional
+from typing import Optional
 
 from airflow.exceptions import AirflowException
 from airflow.providers.amazon.aws.hooks.athena import AWSAthenaHook
