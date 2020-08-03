fd2a5a329754e2602d75a4d2cdc25f67d7c43f23
EphraimBuddy <splendidzigy24@gmail.com>
2020-08-03 21:26:58 +0100

    revert previous commits

--- airflow/config_templates/default_webserver_config.py
+++ airflow/config_templates/default_webserver_config.py
@@ -70,15 +70,15 @@ AUTH_TYPE = AUTH_DB
 #     'token_key':'access_token',
 #     'icon':'fa-google',
 #         'remote_app': {
-#             'api_base_url':'https://www.googleapis.com/oauth2/v2/',
-#             'client_kwargs':{
+#             'base_url':'https://www.googleapis.com/oauth2/v2/',
+#             'request_token_params':{
 #                 'scope': 'email profile'
 #             },
 #             'access_token_url':'https://accounts.google.com/o/oauth2/token',
 #             'authorize_url':'https://accounts.google.com/o/oauth2/auth',
 #             'request_token_url': None,
-#             'client_id': GOOGLE_KEY,
-#             'client_secret': GOOGLE_SECRET_KEY,
+#             'consumer_key': CONSUMER_KEY,
+#             'consumer_secret': SECRET_KEY,
 #         }
 # }]
 
