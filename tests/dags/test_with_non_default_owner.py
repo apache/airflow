733066f4a181748ae6eeb1a60477627aec88d459
Jacob Ferriero <jferriero@google.com>
2020-08-12 12:37:51 -0700

    fixup license headers

--- tests/dags/test_with_non_default_owner.py
+++ tests/dags/test_with_non_default_owner.py
@@ -11,25 +11,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Copyright 2020 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-#
-# This software is provided as-is,
-# without warranty or representation for any use or purpose.
-# Your use of it is subject to your agreement with Google.
-
 from datetime import timedelta
 
 from airflow import DAG
