8e6624807f9b31ac0a2f705d088f250046a41fb1
Jacob Ferriero <jferriero@google.com>
2020-08-12 12:21:50 -0700

    fixup license headers

--- airflow/exceptions.py
+++ airflow/exceptions.py
@@ -1,3 +1,4 @@
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -9,6 +10,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 # Note: Any AirflowException raised is expected to cause the TaskInstance
 #       to be marked in an ERROR state
 """Exceptions used by Airflow"""
