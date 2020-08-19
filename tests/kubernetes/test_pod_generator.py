f45a64d63e0cd2a393f99e0a030ef6175f2f0bbe
Daniel Imberman <daniel.imberman@gmail.com>
2020-08-19 10:35:15 -0700

    static tests

--- tests/kubernetes/test_pod_generator.py
+++ tests/kubernetes/test_pod_generator.py
@@ -14,9 +14,9 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+import sys
 import unittest
 import uuid
-import sys
 from unittest import mock
 
 from kubernetes.client import ApiClient, models as k8s
