507fc28c5d96b6b6954efbf0d3b344a431916148
Marco Aguiar <marco@DESKTOP-8IVSCHM.localdomain>
2020-08-29 19:21:09 -0300

    Fix trailing whitespace

--- airflow/www/utils.py
+++ airflow/www/utils.py
@@ -268,7 +268,7 @@ def datetime_f(attr_name):
     return dt
 # pylint: enable=invalid-name
 
- 
+
 def json_f(attr_name):
     """Returns a formatted string with HTML for given JSON serializable"""
     def json_(attr):
@@ -276,7 +276,7 @@ def json_f(attr_name):
         serialized = json.dumps(f)
         return Markup('<nobr>{}</nobr>').format(serialized)  # noqa
     return json_
- 
+
 
 def dag_link(attr):
     """Generates a URL to the Graph View for a Dag."""
