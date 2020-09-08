b9a8692bc0326ccf328b1328862a493eb0b501cb
Jarek Potiuk <jarek.potiuk@polidea.com>
2020-09-08 10:07:10 +0200

    Move dev docker images to airflow registry (#9652)
    Part of #9401
    
    (cherry picked from commit 4de67a673130111b0a27eb628af6121d415ba3cc)

--- tests/kubernetes/test_pod_generator.py
+++ tests/kubernetes/test_pod_generator.py
@@ -40,7 +40,7 @@ class TestPodGenerator(unittest.TestCase):
                 'containers': [{
                     'args': ['--vm', '1', '--vm-bytes', '150M', '--vm-hang', '1'],
                     'command': ['stress'],
-                    'image': 'polinux/stress',
+                    'image': 'apache/airflow:stress-2020.07.10-1.0.4',
                     'name': 'memory-demo-ctr',
                     'resources': {
                         'limits': {'memory': '200Mi'},
@@ -1060,7 +1060,7 @@ metadata:
 spec:
   containers:
     - name: memory-demo-ctr
-      image: polinux/stress
+      image: apache/airflow:stress-2020.07.10-1.0.4
       resources:
         limits:
           memory: "200Mi"
