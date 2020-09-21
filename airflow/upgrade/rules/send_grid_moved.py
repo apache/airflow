325b196d64fd1b06b7fd26ceef4491ef64cd7129
Patrick <p.cando80@gmail.com>
2020-09-21 22:56:32 +0100

    Add SendGrid emailer rule

---
+++ airflow/upgrade/rules/send_grid_moved.py
@@ -0,0 +1,37 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from __future__ import absolute_import
+
+from airflow.configuration import conf
+from airflow.upgrade.rules.base_rule import BaseRule
+
+
+class SendGridEmailerMovedRule(BaseRule):
+    title = "SendGrid email uses old airflow.contrib module"
+
+    description = """
+Removes the need for SendGrid email code to be in contrib package. The SendGrid email function \
+has been moved to airflow.providers.
+    """
+
+    def check(self, session=None):
+        email_conf = conf.get(section="email", key="email_backend")
+        if email_conf.startswith("airflow.contrib"):
+            email_backend_path = "airflow.providers.sendgrid.utils.emailer"
+            return "Email backend option uses airflow.contrib module. Please use new module: {}".format(
+                email_backend_path)
