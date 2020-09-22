apache/spark

commit fbd994a4cff32be42474c5a121ec6fb888d05cd8

Add permissions for stable API (#10594)
Related Github Issue: https://github.com/apache/airflow/issues/8112

airflow/api_connexion/security.py
@@ -16,25 +16,76 @@
 # under the License.
 
 from functools import wraps
-from typing import Callable, TypeVar, cast
+from typing import Callable, Optional, Sequence, Tuple, TypeVar, cast
 
-from flask import Response, current_app
+from flask import Response, current_app, g
 
-from airflow.api_connexion.exceptions import Unauthenticated
+from airflow.api_connexion.exceptions import PermissionDenied, Unauthenticated
 
 T = TypeVar("T", bound=Callable)  # pylint: disable=invalid-name
 
 
-def requires_authentication(function: T):
-    """Decorator for functions that require authentication"""
+def check_authentication() -> None:
+    """Checks that the request has valid authorization information."""
+    response = current_app.api_auth.requires_authentication(Response)()
+    if response.status_code != 200:
+        # since this handler only checks authentication, not authorization,
+        # we should always return 401
+        raise Unauthenticated(headers=response.headers)
 
-    @wraps(function)
-    def decorated(*args, **kwargs):
-        response = current_app.api_auth.requires_authentication(Response)()
-        if response.status_code != 200:
-            # since this handler only checks authentication, not authorization,
-            # we should always return 401
-            raise Unauthenticated(headers=response.headers)
-        return function(*args, **kwargs)
 
-    return cast(T, decorated)
+def can_access_any_dags(action: str, dag_id: Optional[int] = None) -> bool:
+    """Checks if user has read or write access to some dags."""
+    appbuilder = current_app.appbuilder
+    if dag_id and dag_id != '~':
+        return appbuilder.sm.has_access(action, dag_id)
+
+    user = g.user
+    if action == 'can_read':
+        return any(appbuilder.sm.get_readable_dags(user))
+    return any(appbuilder.sm.get_editable_dags(user))
+
+
+def check_authorization(
+    permissions: Optional[Sequence[Tuple[str, str]]] = None, dag_id: Optional[int] = None
+) -> None:
+    """Checks that the logged in user has the specified permissions."""
+
+    if not permissions:
+        return
+    appbuilder = current_app.appbuilder
+    for permission in permissions:
+        if permission in (('can_read', 'Dag'), ('can_edit', 'Dag')):
+            can_access_all_dags = appbuilder.sm.has_access(*permission)
+            if can_access_all_dags:
+                continue
+
+            action = permission[0]
+            if can_access_any_dags(action, dag_id):
+                continue
+
+            raise PermissionDenied()
+
+        elif not appbuilder.sm.has_access(*permission):
+            raise PermissionDenied()
+
+
+def requires_access(permissions: Optional[Sequence[Tuple[str, str]]] = None) -> Callable[[T], T]:
+    """
+    Factory for decorator that checks current user's permissions against required permissions.
+    """
+    appbuilder = current_app.appbuilder
+    appbuilder.sm.sync_resource_permissions(permissions)
+
+    def requires_access_decorator(func: T):
+        @wraps(func)
+        def decorated(*args, **kwargs):
+
+            check_authentication()
+            check_authorization(permissions, kwargs.get('dag_id', None))
+
+            return func(*args, **kwargs)
+
+        return cast(T, decorated)
+
+    return requires_access_decorator

airflow/www/security.py
@@ -24,6 +24,7 @@ from sqlalchemy import and_, or_
 
 from airflow import models
 from airflow.exceptions import AirflowException
+from airflow.models import DagModel
 from airflow.utils.log.logging_mixin import LoggingMixin
 from airflow.utils.session import provide_session
 from airflow.www.utils import CustomSQLAInterface
@@ -35,6 +36,11 @@ EXISTING_ROLES = {
     'Op',
     'Public',
 }
+
+CAN_CREATE = 'can_create'
+CAN_READ = 'can_read'
+CAN_EDIT = 'can_edit'
+CAN_DELETE = 'can_delete'
 
 
 class AirflowSecurityManager(SecurityManager, LoggingMixin):
@@ -48,6 +54,7 @@ class AirflowSecurityManager(SecurityManager, LoggingMixin):
         'Airflow',
         'DagModelView',
         'Browse',
+        'Config',
         'DAG Runs',
         'DagRunModelView',
         'Task Instances',
@@ -114,7 +121,7 @@ class AirflowSecurityManager(SecurityManager, LoggingMixin):
         'can_blocked',
         'can_rendered',
         'can_version',
-        'can_extra_links'
+        'can_extra_links',
     }
     # [END security_viewer_perms]
 
@@ -147,16 +154,16 @@ class AirflowSecurityManager(SecurityManager, LoggingMixin):
     # [END security_op_perms]
 
     # global view-menu for dag-level access
-    DAG_VMS = {
-        'all_dags'
-    }
+    DAG_VMS = {'all_dags'}
 
     WRITE_DAG_PERMS = {
         'can_dag_edit',
+        'can_edit',
     }
 
     READ_DAG_PERMS = {
         'can_dag_read',
+        'can_read',
     }
 
     DAG_PERMS = WRITE_DAG_PERMS | READ_DAG_PERMS
@@ -166,11 +173,7 @@ class AirflowSecurityManager(SecurityManager, LoggingMixin):
     ###########################################################################
 
     ROLE_CONFIGS = [
-        {
-            'role': 'Viewer',
-            'perms': VIEWER_PERMS | READ_DAG_PERMS,
-            'vms': VIEWER_VMS | DAG_VMS
-        },
+        {'role': 'Viewer', 'perms': VIEWER_PERMS | READ_DAG_PERMS, 'vms': VIEWER_VMS | DAG_VMS},
         {
             'role': 'User',
             'perms': VIEWER_PERMS | USER_PERMS | DAG_PERMS,
@@ -224,25 +227,24 @@ class AirflowSecurityManager(SecurityManager, LoggingMixin):
             self.get_session.merge(role)
             self.get_session.commit()
         else:
-            self.log.debug('Existing permissions for the role:%s '
-                           'within the database will persist.', role_name)
+            self.log.debug(
+                'Existing permissions for the role:%s ' 'within the database will persist.', role_name
+            )
 
     def delete_role(self, role_name):
-        """Delete the given Role
+        """
+        Delete the given Role
 
         :param role_name: the name of a role in the ab_role table
         """
         session = self.get_session
-        role = session.query(sqla_models.Role)\
-                      .filter(sqla_models.Role.name == role_name)\
-                      .first()
+        role = session.query(sqla_models.Role).filter(sqla_models.Role.name == role_name).first()
         if role:
             self.log.info("Deleting role '%s'", role_name)
             session.delete(role)
             session.commit()
         else:
-            raise AirflowException("Role named '{}' does not exist".format(
-                role_name))
+            raise AirflowException("Role named '{}' does not exist".format(role_name))
 
     @staticmethod
     def get_user_roles(user=None):
@@ -256,8 +258,7 @@ class AirflowSecurityManager(SecurityManager, LoggingMixin):
             user = g.user
         if user.is_anonymous:
             public_role = current_app.appbuilder.config.get('AUTH_ROLE_PUBLIC')
-            return [current_app.appbuilder.security_manager.find_role(public_role)] \
-                if public_role else []
+            return [current_app.appbuilder.security_manager.find_role(public_role)] if public_role else []
         return user.roles
 
     def get_all_permissions_views(self):
@@ -266,9 +267,45 @@ class AirflowSecurityManager(SecurityManager, LoggingMixin):
         """
         perms_views = set()
         for role in self.get_user_roles():
-            perms_views.update({(perm_view.permission.name, perm_view.view_menu.name)
-                                for perm_view in role.permissions})
+            perms_views.update(
+                {(perm_view.permission.name, perm_view.view_menu.name) for perm_view in role.permissions}
+            )
         return perms_views
+
+    def get_readable_dags(self, user):
+        """Gets the DAGs readable by authenticated user."""
+        return self.get_accessible_dags(CAN_READ, user)
+
+    def get_editable_dags(self, user):
+        """Gets the DAGs editable by authenticated user."""
+        return self.get_accessible_dags(CAN_EDIT, user)
+
+    def get_readable_dag_ids(self, user):
+        """Gets the DAG IDs readable by authenticated user."""
+        return [dag.dag_id for dag in self.get_readable_dags(user)]
+
+    def get_editable_dag_ids(self, user):
+        """Gets the DAG IDs editable by authenticated user."""
+        return [dag.dag_id for dag in self.get_editable_dags(user)]
+
+    @provide_session
+    def get_accessible_dags(self, user_action, user, session=None):
+        """Generic function to get readable or writable DAGs for authenticated user."""
+
+        if user.is_anonymous:
+            return set()
+
+        resources = set()
+        for role in user.roles:
+            for permission in role.permissions:
+                resource = permission.view_menu.name
+                action = permission.permission.name
+                if action == user_action:
+                    resources.add(resource)
+        if 'Dag' in resources:
+            return session.query(DagModel)
+
+        return session.query(DagModel).filter(DagModel.dag_id.in_(resources))
 
     def get_accessible_dag_ids(self, username=None):
         """
@@ -324,8 +361,7 @@ class AirflowSecurityManager(SecurityManager, LoggingMixin):
         """
         if not isinstance(role_name_or_list, list):
             role_name_or_list = [role_name_or_list]
-        return any(
-            r.name in role_name_or_list for r in self.get_user_roles())
+        return any(r.name in role_name_or_list for r in self.get_user_roles())
 
     def _has_perm(self, permission_name, view_menu_name):
         """
@@ -346,9 +382,10 @@ class AirflowSecurityManager(SecurityManager, LoggingMixin):
         3. Has can_dag_edit permission on all_dags view.
         """
         return (
-            self._has_role(['Admin', 'Viewer', 'Op', 'User']) or
-            self._has_perm('can_dag_read', 'all_dags') or
-            self._has_perm('can_dag_edit', 'all_dags'))
+            self._has_role(['Admin', 'Viewer', 'Op', 'User'])
+            or self._has_perm('can_dag_read', 'all_dags')
+            or self._has_perm('can_dag_edit', 'all_dags')
+        )
 
     def clean_perms(self):
         """
@@ -356,12 +393,11 @@ class AirflowSecurityManager(SecurityManager, LoggingMixin):
         """
         self.log.debug('Cleaning faulty perms')
         sesh = self.get_session
-        pvms = (
-            sesh.query(sqla_models.PermissionView)
-            .filter(or_(
+        pvms = sesh.query(sqla_models.PermissionView).filter(
+            or_(
                 sqla_models.PermissionView.permission == None,  # noqa pylint: disable=singleton-comparison
                 sqla_models.PermissionView.view_menu == None,  # noqa pylint: disable=singleton-comparison
-            ))
+            )
         )
         # Since FAB doesn't define ON DELETE CASCADE on these tables, we need
         # to delete the _object_ so that SQLA knows to delete the many-to-many
@@ -391,8 +427,11 @@ class AirflowSecurityManager(SecurityManager, LoggingMixin):
         view_menu = self.find_view_menu(view_menu_name)
         permission_view = None
         if permission and view_menu:
-            permission_view = self.get_session.query(self.permissionview_model).filter_by(
-                permission=permission, view_menu=view_menu).first()
+            permission_view = (
+                self.get_session.query(self.permissionview_model)
+                .filter_by(permission=permission, view_menu=view_menu)
+                .first()
+            )
         if not permission_view and permission_name and view_menu_name:
             self.add_permission_view_menu(permission_name, view_menu_name)
 
@@ -423,8 +462,11 @@ class AirflowSecurityManager(SecurityManager, LoggingMixin):
                 all_permission_views.add((permission_view.permission.name, permission_view.view_menu.name))
 
         # Get all the active / paused dags and insert them into a set
-        all_dags_models = session.query(models.DagModel)\
-            .filter(or_(models.DagModel.is_active, models.DagModel.is_paused)).all()
+        all_dags_models = (
+            session.query(models.DagModel)
+            .filter(or_(models.DagModel.is_active, models.DagModel.is_paused))
+            .all()
+        )
 
         # create can_dag_edit and can_dag_read permissions for every dag(vm)
         for dag in all_dags_models:
@@ -449,7 +491,7 @@ class AirflowSecurityManager(SecurityManager, LoggingMixin):
             session.query(ab_perm_view_role)
             .join(
                 perm_view,
-                perm_view.id == ab_perm_view_role.columns.permission_view_id  # pylint: disable=no-member
+                perm_view.id == ab_perm_view_role.columns.permission_view_id,  # pylint: disable=no-member
             )
             .filter(ab_perm_view_role.columns.role_id == user_role.id)  # pylint: disable=no-member
             .join(view_menu)
@@ -460,20 +502,19 @@ class AirflowSecurityManager(SecurityManager, LoggingMixin):
         for role in dag_role:
             # pylint: disable=no-member
             # Get all the perm-view of the role
-            existing_perm_view_by_user = self.get_session.query(ab_perm_view_role)\
-                .filter(ab_perm_view_role.columns.role_id == role.id)
+            existing_perm_view_by_user = self.get_session.query(ab_perm_view_role).filter(
+                ab_perm_view_role.columns.role_id == role.id
+            )
 
             existing_perms_views = {pv.permission_view_id for pv in existing_perm_view_by_user}
             missing_perm_views = all_perm_views - existing_perms_views
 
             for perm_view_id in missing_perm_views:
-                update_perm_views.append({'permission_view_id': perm_view_id,
-                                          'role_id': role.id})
+                update_perm_views.append({'permission_view_id': perm_view_id, 'role_id': role.id})
 
         if update_perm_views:
             self.get_session.execute(
-                ab_perm_view_role.insert(),  # pylint: disable=no-value-for-parameter
-                update_perm_views
+                ab_perm_view_role.insert(), update_perm_views  # pylint: disable=no-value-for-parameter
             )
         self.get_session.commit()
 
@@ -487,10 +528,16 @@ class AirflowSecurityManager(SecurityManager, LoggingMixin):
         """
         all_dag_view = self.find_view_menu('all_dags')
         dag_perm_ids = [self.find_permission('can_dag_edit').id, self.find_permission('can_dag_read').id]
-        pvms = self.get_session.query(sqla_models.PermissionView).filter(~and_(
-            sqla_models.PermissionView.permission_id.in_(dag_perm_ids),
-            sqla_models.PermissionView.view_menu_id != all_dag_view.id)
-        ).all()
+        pvms = (
+            self.get_session.query(sqla_models.PermissionView)
+            .filter(
+                ~and_(
+                    sqla_models.PermissionView.permission_id.in_(dag_perm_ids),
+                    sqla_models.PermissionView.view_menu_id != all_dag_view.id,
+                )
+            )
+            .all()
+        )
 
         pvms = [p for p in pvms if p.permission and p.view_menu]
 
@@ -523,6 +570,17 @@ class AirflowSecurityManager(SecurityManager, LoggingMixin):
         self.update_admin_perm_view()
         self.clean_perms()
 
+    def sync_resource_permissions(self, permissions=None):
+        """
+        Populates resource-based permissions.
+        """
+        if not permissions:
+            return
+
+        for action, resource in permissions:
+            self.add_view_menu(resource)
+            self.add_permission_view_menu(action, resource)
+
     def sync_perm_for_dag(self, dag_id, access_control=None):
         """
         Sync permissions for given dag id. The dag id surely exists in our dag bag
@@ -554,13 +612,11 @@ class AirflowSecurityManager(SecurityManager, LoggingMixin):
             {'can_dag_read'}
         :type access_control: dict
         """
+
         def _get_or_create_dag_permission(perm_name):
             dag_perm = self.find_permission_view_menu(perm_name, dag_id)
             if not dag_perm:
-                self.log.info(
-                    "Creating new permission '%s' on view '%s'",
-                    perm_name, dag_id
-                )
+                self.log.info("Creating new permission '%s' on view '%s'", perm_name, dag_id)
                 dag_perm = self.add_permission_view_menu(perm_name, dag_id)
 
             return dag_perm
@@ -568,14 +624,12 @@ class AirflowSecurityManager(SecurityManager, LoggingMixin):
         def _revoke_stale_permissions(dag_view):
             existing_dag_perms = self.find_permissions_view_menu(dag_view)
             for perm in existing_dag_perms:
-                non_admin_roles = [role for role in perm.role
-                                   if role.name != 'Admin']
+                non_admin_roles = [role for role in perm.role if role.name != 'Admin']
                 for role in non_admin_roles:
                     target_perms_for_role = access_control.get(role.name, {})
                     if perm.permission.name not in target_perms_for_role:
                         self.log.info(
-                            "Revoking '%s' on DAG '%s' for role '%s'",
-                            perm.permission, dag_id, role.name
+                            "Revoking '%s' on DAG '%s' for role '%s'", perm.permission, dag_id, role.name
                         )
                         self.del_permission_role(role, perm)
 
@@ -588,9 +642,8 @@ class AirflowSecurityManager(SecurityManager, LoggingMixin):
             if not role:
                 raise AirflowException(
                     "The access_control mapping for DAG '{}' includes a role "
-                    "named '{}', but that role does not exist".format(
-                        dag_id,
-                        rolename))
+                    "named '{}', but that role does not exist".format(dag_id, rolename)
+                )
 
             perms = set(perms)
             invalid_perms = perms - self.DAG_PERMS
@@ -598,9 +651,8 @@ class AirflowSecurityManager(SecurityManager, LoggingMixin):
                 raise AirflowException(
                     "The access_control map for DAG '{}' includes the following "
                     "invalid permissions: {}; The set of valid permissions "
-                    "is: {}".format(dag_id,
-                                    (perms - self.DAG_PERMS),
-                                    self.DAG_PERMS))
+                    "is: {}".format(dag_id, (perms - self.DAG_PERMS), self.DAG_PERMS)
+                )
 
             for perm_name in perms:
                 dag_perm = _get_or_create_dag_permission(perm_name)
@@ -613,5 +665,4 @@ class AirflowSecurityManager(SecurityManager, LoggingMixin):
         # create perm for global logical dag
         for dag_vm in self.DAG_VMS:
             for perm in self.DAG_PERMS:
-                self._merge_perm(permission_name=perm,
-                                 view_menu_name=dag_vm)
+                self._merge_perm(permission_name=perm, view_menu_name=dag_vm)

airflow/api_connexion/endpoints/config_endpoint.py
@@ -61,7 +61,7 @@ def _config_to_json(config: Config) -> str:
     return json.dumps(config_schema.dump(config), indent=4)
 
 
-@security.requires_authentication
+@security.requires_access([("can_read", "Config")])
 def get_config() -> Response:
     """
     Get current configuration.

airflow/api_connexion/endpoints/connection_endpoint.py
@@ -33,7 +33,7 @@ from airflow.models import Connection
 from airflow.utils.session import provide_session
 
 
-@security.requires_authentication
+@security.requires_access([("can_delete", "Connection")])
 @provide_session
 def delete_connection(connection_id, session):
     """
@@ -49,7 +49,7 @@ def delete_connection(connection_id, session):
     return NoContent, 204
 
 
-@security.requires_authentication
+@security.requires_access([("can_read", "Connection")])
 @provide_session
 def get_connection(connection_id, session):
     """
@@ -64,7 +64,7 @@ def get_connection(connection_id, session):
     return connection_collection_item_schema.dump(connection)
 
 
-@security.requires_authentication
+@security.requires_access([("can_read", "Connection")])
 @format_parameters({'limit': check_limit})
 @provide_session
 def get_connections(session, limit, offset=0):
@@ -79,7 +79,7 @@ def get_connections(session, limit, offset=0):
     )
 
 
-@security.requires_authentication
+@security.requires_access([("can_edit", "Connection")])
 @provide_session
 def patch_connection(connection_id, session, update_mask=None):
     """
@@ -115,7 +115,7 @@ def patch_connection(connection_id, session, update_mask=None):
     return connection_schema.dump(connection)
 
 
-@security.requires_authentication
+@security.requires_access([("can_create", "Connection")])
 @provide_session
 def post_connection(session):
     """

airflow/api_connexion/endpoints/dag_endpoint.py
@@ -14,9 +14,8 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-from flask import current_app, request
+from flask import current_app, g, request
 from marshmallow import ValidationError
-from sqlalchemy import func
 
 from airflow import DAG
 from airflow.api_connexion import security
@@ -32,7 +31,7 @@ from airflow.models.dag import DagModel
 from airflow.utils.session import provide_session
 
 
-@security.requires_authentication
+@security.requires_access([("can_read", "Dag")])
 @provide_session
 def get_dag(dag_id, session):
     """
@@ -46,7 +45,7 @@ def get_dag(dag_id, session):
     return dag_schema.dump(dag)
 
 
-@security.requires_authentication
+@security.requires_access([("can_read", "Dag")])
 def get_dag_details(dag_id):
     """
     Get details of DAG.
@@ -57,21 +56,20 @@ def get_dag_details(dag_id):
     return dag_detail_schema.dump(dag)
 
 
-@security.requires_authentication
+@security.requires_access([("can_read", "Dag")])
 @format_parameters({'limit': check_limit})
-@provide_session
-def get_dags(session, limit, offset=0):
+def get_dags(limit, offset=0):
     """
     Get all DAGs.
     """
-    dags = session.query(DagModel).order_by(DagModel.dag_id).offset(offset).limit(limit).all()
-
-    total_entries = session.query(func.count(DagModel.dag_id)).scalar()
+    readable_dags = current_app.appbuilder.sm.get_readable_dags(g.user)
+    dags = readable_dags.order_by(DagModel.dag_id).offset(offset).limit(limit).all()
+    total_entries = readable_dags.count()
 
     return dags_collection_schema.dump(DAGCollection(dags=dags, total_entries=total_entries))
 
 
-@security.requires_authentication
+@security.requires_access([("can_edit", "Dag")])
 @provide_session
 def patch_dag(session, dag_id, update_mask=None):
     """

airflow/api_connexion/endpoints/dag_run_endpoint.py
@@ -15,9 +15,8 @@
 # specific language governing permissions and limitations
 # under the License.
 from connexion import NoContent
-from flask import request
+from flask import g, request, current_app
 from marshmallow import ValidationError
-from sqlalchemy import func
 
 from airflow.api_connexion import security
 from airflow.api_connexion.exceptions import AlreadyExists, BadRequest, NotFound
@@ -33,7 +32,7 @@ from airflow.utils.session import provide_session
 from airflow.utils.types import DagRunType
 
 
-@security.requires_authentication
+@security.requires_access([("can_read", "Dag"), ("can_delete", "DagRun")])
 @provide_session
 def delete_dag_run(dag_id, dag_run_id, session):
     """
@@ -44,7 +43,7 @@ def delete_dag_run(dag_id, dag_run_id, session):
     return NoContent, 204
 
 
-@security.requires_authentication
+@security.requires_access([("can_read", "Dag"), ("can_read", "DagRun")])
 @provide_session
 def get_dag_run(dag_id, dag_run_id, session):
     """
@@ -59,7 +58,7 @@ def get_dag_run(dag_id, dag_run_id, session):
     return dagrun_schema.dump(dag_run)
 
 
-@security.requires_authentication
+@security.requires_access([("can_read", "Dag"), ("can_read", "DagRun")])
 @format_parameters(
     {
         'start_date_gte': format_datetime,
@@ -90,12 +89,14 @@ def get_dag_runs(
     query = session.query(DagRun)
 
     #  This endpoint allows specifying ~ as the dag_id to retrieve DAG Runs for all DAGs.
-    if dag_id != "~":
+    if dag_id == "~":
+        appbuilder = current_app.appbuilder
+        query = query.filter(DagRun.dag_id.in_(appbuilder.sm.get_readable_dag_ids(g.user)))
+    else:
         query = query.filter(DagRun.dag_id == dag_id)
 
     dag_run, total_entries = _fetch_dag_runs(
         query,
-        session,
         end_date_gte,
         end_date_lte,
         execution_date_gte,
@@ -111,7 +112,6 @@ def get_dag_runs(
 
 def _fetch_dag_runs(
     query,
-    session,
     end_date_gte,
     end_date_lte,
     execution_date_gte,
@@ -121,6 +121,7 @@ def _fetch_dag_runs(
     limit,
     offset,
 ):
+    total_entries = query.count()
     query = _apply_date_filters_to_query(
         query,
         end_date_gte,
@@ -132,7 +133,6 @@ def _fetch_dag_runs(
     )
     # apply offset and limit
     dag_run = query.order_by(DagRun.id).offset(offset).limit(limit).all()
-    total_entries = session.query(func.count(DagRun.id)).scalar()
     return dag_run, total_entries
 
 
@@ -157,7 +157,7 @@ def _apply_date_filters_to_query(
     return query
 
 
-@security.requires_authentication
+@security.requires_access([("can_read", "Dag"), ("can_read", "DagRun")])
 @provide_session
 def get_dag_runs_batch(session):
     """
@@ -169,14 +169,17 @@ def get_dag_runs_batch(session):
     except ValidationError as err:
         raise BadRequest(detail=str(err.messages))
 
+    appbuilder = current_app.appbuilder
+    readable_dag_ids = appbuilder.sm.get_readable_dag_ids(g.user)
     query = session.query(DagRun)
-
-    if data["dag_ids"]:
-        query = query.filter(DagRun.dag_id.in_(data["dag_ids"]))
+    if data.get("dag_ids"):
+        dag_ids = set(data["dag_ids"]) & set(readable_dag_ids)
+        query = query.filter(DagRun.dag_id.in_(dag_ids))
+    else:
+        query = query.filter(DagRun.dag_id.in_(readable_dag_ids))
 
     dag_runs, total_entries = _fetch_dag_runs(
         query,
-        session,
         data["end_date_gte"],
         data["end_date_lte"],
         data["execution_date_gte"],
@@ -190,7 +193,7 @@ def get_dag_runs_batch(session):
     return dagrun_collection_schema.dump(DAGRunCollection(dag_runs=dag_runs, total_entries=total_entries))
 
 
-@security.requires_authentication
+@security.requires_access([("can_read", "Dag"), ("can_create", "DagRun")])
 @provide_session
 def post_dag_run(dag_id, session):
     """

airflow/api_connexion/endpoints/dag_source_endpoint.py
@@ -27,7 +27,7 @@ from airflow.models.dagcode import DagCode
 log = logging.getLogger(__name__)
 
 
-@security.requires_authentication
+@security.requires_access([("can_read", "DagCode")])
 def get_dag_source(file_token: str):
     """
     Get source code using file token

airflow/api_connexion/endpoints/event_log_endpoint.py
@@ -30,7 +30,7 @@ from airflow.models import Log
 from airflow.utils.session import provide_session
 
 
-@security.requires_authentication
+@security.requires_access([('can_read', 'Log')])
 @provide_session
 def get_event_log(event_log_id, session):
     """
@@ -42,7 +42,7 @@ def get_event_log(event_log_id, session):
     return event_log_schema.dump(event_log)
 
 
-@security.requires_authentication
+@security.requires_access([('can_read', 'Log')])
 @format_parameters({'limit': check_limit})
 @provide_session
 def get_event_logs(session, limit, offset=None):

apache/airflow

commit c1318f65c7e315429bd95bbb95220f5486d9a5d6

Fix for pydocstyle D202
'issues' introduced in https://github.com/apache/airflow/pull/10594

airflow/api_connexion/security.py
@@ -50,7 +50,6 @@ def check_authorization(
     permissions: Optional[Sequence[Tuple[str, str]]] = None, dag_id: Optional[int] = None
 ) -> None:
     """Checks that the logged in user has the specified permissions."""
-
     if not permissions:
         return
     appbuilder = current_app.appbuilder

airflow/www/security.py
@@ -291,7 +291,6 @@ class AirflowSecurityManager(SecurityManager, LoggingMixin):
     @provide_session
     def get_accessible_dags(self, user_action, user, session=None):
         """Generic function to get readable or writable DAGs for authenticated user."""
-
         if user.is_anonymous:
             return set()
 