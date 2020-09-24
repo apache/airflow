0542779f92370a3818ca73599176846714d27b1e
Nadim Younes <nyounes@kobo.com>
2020-09-24 18:11:09 -0400

    added comma as per suggestion to fix static check

--- tests/providers/ssh/hooks/test_ssh.py
+++ tests/providers/ssh/hooks/test_ssh.py
@@ -69,7 +69,7 @@ class TestSSHHook(unittest.TestCase):
         with create_session() as session:
             conns_to_reset = [
                 cls.CONN_SSH_WITH_PRIVATE_KEY_EXTRA,
-                cls.CONN_SSH_WITH_PRIVATE_KEY_PASSPHRASE_EXTRA
+                cls.CONN_SSH_WITH_PRIVATE_KEY_PASSPHRASE_EXTRA,
             ]
             connections = session.query(Connection).filter(Connection.conn_id.in_(conns_to_reset))
             connections.delete(synchronize_session=False)
