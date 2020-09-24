ae7883217b2ac56cca17b3f4b120261ac0e0d4d9
Ben Ryves <benjamin.ryves@getyourguide.com>
2020-09-24 18:05:07 +0200

    improve error messaging for duplicate task_ids in a DAG (#11125)

--- airflow/models/dag.py
+++ airflow/models/dag.py
@@ -1335,10 +1335,10 @@ class DAG(BaseDag, LoggingMixin):
         if task.task_id in self.task_dict and self.task_dict[task.task_id] is not task:
             # TODO: raise an error in Airflow 2.0
             warnings.warn(
-                'The requested task could not be added to the DAG because a '
+                'The requested task could not be added to the DAG with dag_id {} because a '
                 'task with task_id {} is already in the DAG. Starting in '
                 'Airflow 2.0, trying to overwrite a task will raise an '
-                'exception.'.format(task.task_id),
+                'exception.'.format(self.dag_id, task.task_id),
                 category=PendingDeprecationWarning)
         else:
             self.task_dict[task.task_id] = task
