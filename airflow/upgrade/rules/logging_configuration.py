d282e567d86e5f72b8556f5765afd5acbf061acb
sangarshanan <sangarshanan1998@gmail.com>
2020-09-28 00:51:30 +0530

    Include the logging keys that need to be moved

--- airflow/upgrade/rules/logging_configuration.py
+++ airflow/upgrade/rules/logging_configuration.py
@@ -28,4 +28,24 @@ class LoggingConfigurationRule(BaseRule):
 
     def check(self):
         if not conf.has_option("logging", "logging_level"):
-            return "The logging configurations have been to moved from [core] to the new [logging] section."
+            return (
+                "The following configurations have been to moved from [core] to the new [logging] section. \n"
+                "- base_log_folder \n"
+                "- remote_logging \n"
+                "- remote_log_conn_id \n"
+                "- remote_base_log_folder \n"
+                "- encrypt_s3_logs \n"
+                "- logging_level \n"
+                "- fab_logging_level \n"
+                "- logging_config_class \n"
+                "- colored_console_log \n"
+                "- colored_log_format \n"
+                "- colored_formatter_class \n"
+                "- log_format \n"
+                "- simple_log_format \n"
+                "- task_log_prefix_template \n"
+                "- log_filename_template \n"
+                "- log_processor_filename_template \n"
+                "- dag_processor_manager_log_location \n"
+                "- task_log_reader \n"
+            )
