13173ad82f41b4b6a333cc173aada2aa15a9969a
James Timmins <james@astronomer.io>
2020-09-28 16:10:03 -0700

    Add updated permissions.

--- airflow/cli/commands/role_command.py
+++ airflow/cli/commands/role_command.py
@@ -42,8 +42,7 @@ def roles_create(args):
 
 
 mapping = {
-    ("About", "menu_access"): [],
-    ("Admin", "menu_access"): [],
+    # ========================= Airflow Views
     ("Airflow", "can_blocked"): [("Dag", "can_read"), ("DagRun", "can_read")],
     ("Airflow", "can_clear"): [("Dag", "can_read"), ("Task", "can_read"), ("TaskInstance", "can_delete")],
     ("Airflow", "can_code"): [("Dag", "can_read"), ("DagCode", "can_read")],
@@ -107,128 +106,80 @@ mapping = {
     ("Airflow", "can_tries"): [("Dag", "can_read"), ("Task", "can_read"), ("TaskInstance", "can_read")],
     ("Airflow", "can_trigger"): [("Dag", "can_read"), ("DagRun", "can_create")],
     ("Airflow", "can_xcom"): [("Dag", "can_read"), ("TaskInstance", "can_read"), ("XCom", "can_read")],
-    ("Base Permissions", "menu_access"): [],
-    ("Browse", "menu_access"): [],
-    ("Config", "can_read"): [],
-    ("ConfigurationView", "can_conf"): [],
-    ("Configurations", "menu_access"): [],
-    ("Connection", "can_create"): [],
-    ("Connection", "can_delete"): [],
-    ("Connection", "can_edit"): [],
-    ("Connection", "can_read"): [],
-    ("ConnectionModelView", "can_add"): [("Connection", "can_create")],
-    ("ConnectionModelView", "can_delete"): [("Connection", "can_delete")],
-    ("ConnectionModelView", "can_edit"): [("Connection", "can_edit")],
-    ("ConnectionModelView", "can_list"): [("Connection", "can_read")],
-    ("ConnectionModelView", "muldelete"): [("Connection", "can_delete")],
-    ("Connections", "menu_access"): [],
-    ("DAG Runs", "menu_access"): [],
-    ("Dag", "can_edit"): [],
-    ("Dag", "can_read"): [],
-    ("DagBag", "can_read"): [],
-    ("DagCode", "can_read"): [],
-    ("DagModelView", "can_list"): [("Dag", "can_read")],
-    ("DagModelView", "can_show"): [],
-    ("DagRun", "can_create"): [],
-    ("DagRun", "can_delete"): [],
-    ("DagRun", "can_edit"): [],
-    ("DagRun", "can_read"): [],
-    ("DagRunModelView", "can_add"): [("DagRun", "can_create")],
-    ("DagRunModelView", "can_list"): [("DagRun", "can_read")],
-    ("DagRunModelView", "muldelete"): [("DagRun", "can_delete")],
-    ("DagRunModelView", "set_failed"): [("DagRun", "can_edit")],
-    ("DagRunModelView", "set_running"): [("DagRun", "can_edit")],
-    ("DagRunModelView", "set_success"): [("DagRun", "can_edit")],
-    ("Docs", "menu_access"): [],
-    ("Documentation", "menu_access"): [],
-    ("GitHub", "menu_access"): [],
-    ("ImportError", "can_read"): [],
-    ("JobModelView", "can_list"): [("Job", "can_read")],
-    ("Jobs", "menu_access"): [],
-    ("List Roles", "menu_access"): [],
-    ("List Users", "menu_access"): [],
-    ("Log", "can_read"): [],
-    ("LogModelView", "can_list"): [("Log", "can_read")],
-    ("Logs", "menu_access"): [],
-    ("MenuApi", "can_get"): [],
-    ("Permission on Views/Menus", "menu_access"): [],
-    ("PermissionModelView", "can_list"): [("Permission", "can_read")],
-    ("PermissionViewModelView", "can_list"): [("PermissionView", "can_read")],
-    ("Pool", "can_create"): [],
-    ("Pool", "can_delete"): [],
-    ("Pool", "can_edit"): [],
-    ("Pool", "can_read"): [],
-    ("PoolModelView", "can_add"): [("Pool", "can_create")],
-    ("PoolModelView", "can_delete"): [("Pool", "can_delete")],
-    ("PoolModelView", "can_edit"): [("Pool", "can_edit")],
-    ("PoolModelView", "can_list"): [("Pool", "can_read")],
-    ("PoolModelView", "muldelete"): [("Pool", "can_delete")],
-    ("Pools", "menu_access"): [],
-    ("REST API Reference (Redoc)", "menu_access"): [],
-    ("REST API Reference (Swagger UI)", "menu_access"): [],
-    ("ResetMyPasswordView", "can_this_form_get"): [],
-    ("ResetMyPasswordView", "can_this_form_post"): [],
-    ("ResetPasswordView", "can_this_form_get"): [],
-    ("ResetPasswordView", "can_this_form_post"): [],
-    ("RoleModelView", "can_add"): [("Role", "can_create")],
-    ("RoleModelView", "can_delete"): [("Role", "can_delete")],
-    ("RoleModelView", "can_download"): [],
-    ("RoleModelView", "can_edit"): [("Role", "can_edit")],
-    ("RoleModelView", "can_list"): [("Role", "can_read")],
-    ("RoleModelView", "can_show"): [],
-    ("RoleModelView", "copyrole"): [],
-    ("SLA Misses", "menu_access"): [],
-    ("Security", "menu_access"): [],
-    ("SlaMissModelView", "can_list"): [("SlaMiss", "can_read")],
-    ("Task Instances", "menu_access"): [],
-    ("Task Reschedules", "menu_access"): [],
-    ("Task", "can_edit"): [],
-    ("Task", "can_read"): [],
-    ("TaskInstance", "can_edit"): [],
-    ("TaskInstance", "can_read"): [],
-    ("TaskInstanceModelView", "can_list"): [("TaskInstance", "can_read")],
-    ("TaskInstanceModelView", "clear"): [],
-    ("TaskInstanceModelView", "set_failed"): [("TaskInstance", "can_edit")],
-    ("TaskInstanceModelView", "set_retry"): [("TaskInstance", "can_edit")],
-    ("TaskInstanceModelView", "set_running"): [("TaskInstance", "can_edit")],
-    ("TaskInstanceModelView", "set_success"): [("TaskInstance", "can_edit")],
-    ("TaskRescheduleModelView", "can_list"): [("TaskReschedule", "can_read")],
-    ("User's Statistics", "menu_access"): [],
-    ("UserDBModelView", "can_add"): [("UserDB", "can_create")],
-    ("UserDBModelView", "can_delete"): [("UserDB", "can_delete")],
-    ("UserDBModelView", "can_download"): [],
-    ("UserDBModelView", "can_edit"): [("UserDB", "can_edit")],
-    ("UserDBModelView", "can_list"): [("UserDB", "can_read")],
-    ("UserDBModelView", "can_show"): [],
-    ("UserDBModelView", "can_userinfo"): [],
-    ("UserDBModelView", "resetmypassword"): [],
-    ("UserDBModelView", "resetpasswords"): [],
-    ("UserDBModelView", "userinfoedit"): [],
-    ("UserInfoEditView", "can_this_form_get"): [],
-    ("UserInfoEditView", "can_this_form_post"): [],
-    ("UserStatsChartView", "can_chart"): [],
-    ("Variable", "can_create"): [],
-    ("Variable", "can_delete"): [],
-    ("Variable", "can_edit"): [],
-    ("Variable", "can_read"): [],
-    ("VariableModelView", "can_add"): [("Variable", "can_create")],
-    ("VariableModelView", "can_delete"): [("Variable", "can_delete")],
-    ("VariableModelView", "can_edit"): [("Variable", "can_edit")],
-    ("VariableModelView", "can_list"): [("Variable", "can_read")],
-    ("VariableModelView", "can_varimport"): [],
-    ("VariableModelView", "muldelete"): [("Variable", "can_delete")],
-    ("VariableModelView", "varexport"): [],
-    ("Variables", "menu_access"): [],
-    ("Version", "menu_access"): [],
-    ("VersionView", "can_version"): [],
-    ("ViewMenuModelView", "can_list"): [("ViewMenu", "can_read")],
-    ("Views/Menus", "menu_access"): [],
-    ("Website", "menu_access"): [],
-    ("XCom", "can_read"): [],
-    ("XComModelView", "can_delete"): [("XCom", "can_delete")],
-    ("XComModelView", "can_list"): [("XCom", "can_read")],
-    ("XComModelView", "muldelete"): [("XCom", "can_delete")],
-    ("XComs", "menu_access"): [],
+    # =========================
+    ("Config", "can_read"): [("Config", "can_read")],  # config_endpoint
+    ("ConfigurationView", "can_conf"): [],  # views.py
+    ("ConnectionModelView", "can_add"): [("Connection", "can_create")],  # views.py
+    ("ConnectionModelView", "can_delete"): [("Connection", "can_delete")],  # views.py
+    ("ConnectionModelView", "can_edit"): [("Connection", "can_edit")],  # views.py
+    ("ConnectionModelView", "can_list"): [("Connection", "can_read")],  # views.py
+    ("ConnectionModelView", "muldelete"): [("Connection", "can_delete")],  # views.py
+    ("DagModelView", "can_list"): [("Dag", "can_read")],  # views.py
+    ("DagModelView", "can_show"): [("Dag", "can_read")],  # views.py
+    ("DagRunModelView", "can_add"): [("DagRun", "can_create")],  # views.py
+    ("DagRunModelView", "can_list"): [("DagRun", "can_read")],  # views.py
+    ("DagRunModelView", "muldelete"): [("DagRun", "can_delete")],  # views.py
+    ("DagRunModelView", "set_failed"): [("DagRun", "can_edit")],  # views.py
+    ("DagRunModelView", "set_running"): [("DagRun", "can_edit")],  # views.py
+    ("DagRunModelView", "set_success"): [("DagRun", "can_edit")],  # views.py
+    ("ImportError", "can_read"): [],  # import_error_endpoint
+    ("JobModelView", "can_list"): [("Job", "can_read")],  # views.py
+    ("Log", "can_read"): [],  # event_log_endpoint.py
+    ("LogModelView", "can_list"): [("Log", "can_read")],  # views.py
+    ("MenuApi", "can_get"): [],  # FAB menu.py
+    ("PermissionModelView", "can_list"): [("Permission", "can_read")],  # FAB
+    ("PermissionViewModelView", "can_list"): [("PermissionView", "can_read")],  # FAB
+    ("PoolModelView", "can_add"): [("Pool", "can_create")],  # views.py
+    ("PoolModelView", "can_delete"): [("Pool", "can_delete")],  # views.py
+    ("PoolModelView", "can_edit"): [("Pool", "can_edit")],  # views.py
+    ("PoolModelView", "can_list"): [("Pool", "can_read")],  # views.py
+    ("PoolModelView", "muldelete"): [("Pool", "can_delete")],  # views.py
+    ("ResetMyPasswordView", "can_this_form_get"): [],  # FAB
+    ("ResetMyPasswordView", "can_this_form_post"): [],  # FAB
+    ("ResetPasswordView", "can_this_form_get"): [],  # FAB
+    ("ResetPasswordView", "can_this_form_post"): [],  # FAB
+    ("RoleModelView", "can_add"): [("Role", "can_create")],  # FAB
+    ("RoleModelView", "can_delete"): [("Role", "can_delete")],  # FAB
+    ("RoleModelView", "can_download"): [("Role", "can_read")],  # FAB
+    ("RoleModelView", "can_edit"): [("Role", "can_edit")],  # FAB
+    ("RoleModelView", "can_list"): [("Role", "can_read")],  # FAB
+    ("RoleModelView", "can_show"): [("Role", "can_read")],  # FAB
+    ("RoleModelView", "copyrole"): [("Role", "can_create")],  # FAB
+    ("SlaMissModelView", "can_list"): [("SlaMiss", "can_read")],  # views.py
+    ("Task", "can_edit"): [("Task", "can_edit")],  # task_endpoint.py
+    ("Task", "can_read"): [("Task", "can_read")],  # task_endpoint.py
+    ("TaskInstanceModelView", "can_list"): [("TaskInstance", "can_read")],  # views.py
+    ("TaskInstanceModelView", "clear"): [],  # views.py
+    ("TaskInstanceModelView", "set_failed"): [("TaskInstance", "can_edit")],  # views.py
+    ("TaskInstanceModelView", "set_retry"): [("TaskInstance", "can_edit")],  # views.py
+    ("TaskInstanceModelView", "set_running"): [("TaskInstance", "can_edit")],  # views.py
+    ("TaskInstanceModelView", "set_success"): [("TaskInstance", "can_edit")],  # views.py
+    ("TaskRescheduleModelView", "can_list"): [("TaskReschedule", "can_read")],  # views.py
+    ("UserDBModelView", "can_add"): [("UserDB", "can_create")],  # FAB
+    ("UserDBModelView", "can_delete"): [("UserDB", "can_delete")],  # FAB
+    ("UserDBModelView", "can_download"): [],  # FAB
+    ("UserDBModelView", "can_edit"): [("UserDB", "can_edit")],  # FAB
+    ("UserDBModelView", "can_list"): [("UserDB", "can_read")],  # FAB
+    ("UserDBModelView", "can_show"): [],  # FAB
+    ("UserDBModelView", "can_userinfo"): [],  # FAB
+    ("UserDBModelView", "resetmypassword"): [],  # FAB
+    ("UserDBModelView", "resetpasswords"): [],  # FAB
+    ("UserDBModelView", "userinfoedit"): [],  # FAB
+    ("UserInfoEditView", "can_this_form_get"): [],  # FAB
+    ("UserInfoEditView", "can_this_form_post"): [],  # FAB
+    ("UserStatsChartView", "can_chart"): [],  # FAB
+    ("VariableModelView", "can_add"): [("Variable", "can_create")],  # views.py
+    ("VariableModelView", "can_delete"): [("Variable", "can_delete")],  # views.py
+    ("VariableModelView", "can_edit"): [("Variable", "can_edit")],  # views.py
+    ("VariableModelView", "can_list"): [("Variable", "can_read")],  # views.py
+    ("VariableModelView", "can_varimport"): [],  # views.py
+    ("VariableModelView", "muldelete"): [("Variable", "can_delete")],  # views.py
+    ("VariableModelView", "varexport"): [],  # views.py
+    ("VersionView", "can_version"): [],  # views.py
+    ("ViewMenuModelView", "can_list"): [("ViewMenu", "can_read")],  # FAB
+    ("XComModelView", "can_delete"): [("XCom", "can_delete")],  # views.py
+    ("XComModelView", "can_list"): [("XCom", "can_read")],  # views.py
+    ("XComModelView", "muldelete"): [("XCom", "can_delete")],  # views.py
     ("all_dags", "can_dag_edit"): [("Dag", "can_edit")],
     ("all_dags", "can_dag_read"): [("Dag", "can_read")],
     ("all_dags", "can_edit"): [("Dag", "can_edit")],
