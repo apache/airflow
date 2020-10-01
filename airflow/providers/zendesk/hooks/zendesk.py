5d84df5cb39732feb6c468b755285268992f2c5b
Cooper Gillan <cooper.gillan@gmail.com>
2020-10-01 13:33:31 +0000

    Remove useless return

--- airflow/providers/zendesk/hooks/zendesk.py
+++ airflow/providers/zendesk/hooks/zendesk.py
@@ -49,7 +49,6 @@ class ZendeskHook(BaseHook):
         retry_after = int(rate_limit_exception.response.headers.get('Retry-After', 60))
         self.log.info("Hit Zendesk API rate limit. Pausing for %s seconds", retry_after)
         time.sleep(retry_after)
-        return None
 
     def call(
         self,
