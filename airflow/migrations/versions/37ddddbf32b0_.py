ec4def9ed4db7a7e2a63710a0199be9ea44d7b7e
James Timmins <james@astronomer.io>
2020-10-05 00:00:20 -0700

    Merge migrations.

---
+++ airflow/migrations/versions/37ddddbf32b0_.py
@@ -0,0 +1,41 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+"""empty message
+
+Revision ID: 37ddddbf32b0
+Revises: 849da589634d, bef4f3d11e8b
+Create Date: 2020-10-04 23:58:43.373419
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '37ddddbf32b0'
+down_revision = ('849da589634d', 'bef4f3d11e8b')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    """Apply empty message"""
+    pass
+
+
+def downgrade():
+    """Unapply empty message"""
+    pass
