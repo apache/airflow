09cf30f4d9f1c0abe7cc4292db11d800e8d75ffe
Cosine <cooosine@outlook.com>
2020-10-14 18:43:16 +0800

    [ADD] use_cluster config

--- airflow/contrib/hooks/redis_hook.py
+++ airflow/contrib/hooks/redis_hook.py
@@ -44,6 +44,7 @@ class RedisHook(BaseHook):
         self.password = None
         self.db = None
         self.health_check_interval = health_check_interval
+        self.use_cluster = False
         self.startup_nodes = []
 
     def get_conn(self) -> Redis:
@@ -55,19 +56,29 @@ class RedisHook(BaseHook):
         self.port = conn.port
         self.password = None if str(conn.password).lower() in ['none', 'false', ''] else conn.password
         self.db = conn.extra_dejson.get('db', None)
+        self.use_cluster = conn.extra_dejson.get('use_cluster', False)
         self.startup_nodes = conn.extra_dejson.get('startup_nodes', [])
 
         if not self.redis:
             self.log.debug(
-                'Initializing redis object for conn_id "%s" on %s:%s:%s',
-                self.redis_conn_id, self.host, self.port, self.db
+                'Initializing redis object (cluster: {}) for conn_id "{}" on {}:{}:{}'
+                    .format(self.use_cluster, self.redis_conn_id, self.host, self.port, self.db)
             )
-            self.redis = RedisCluster(
-                health_check_interval=self.health_check_interval,
-                host=self.host,
-                port=self.port,
-                password=self.password,
-                db=self.db,
-                startup_nodes=self.startup_nodes)
+            if not self.use_cluster or self.use_cluster == 'False':
+                self.redis = Redis(
+                    health_check_interval=self.health_check_interval,
+                    host=self.host,
+                    port=self.port,
+                    password=self.password,
+                    db=self.db)
+            else:
+                self.redis = RedisCluster(
+                    host=self.host,
+                    port=self.port,
+                    startup_nodes=self.startup_nodes,
+                    health_check_interval=self.health_check_interval,
+                    password=self.password,
+                    db=self.db
+                )
 
         return self.redis
