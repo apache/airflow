apache/spark

commit 9237338f7553a3a6dc6d1ceccf05abcbd2bf9ba3

Optimizes CI builds heavily with selective checks (#11541)
* Images are not built if the change is not touching code or docs.
* In case we have no need for CI images we run stripped-down
  pre-commit checks which skip the long checks and only run for
  changed files
* If none of the CLI/Providers/Kubernetes/WWW files changed
  the relevant tests are skipped, unless some of the core files
  changed as well.
* The selective checks logic is explained and documented.

.github/workflows/build-images-workflow-run.yml
@@ -185,13 +185,26 @@ jobs:
     env:
       GITHUB_CONTEXT: ${{ toJson(github) }}
     outputs:
-      pythonVersions: ${{ steps.versions.outputs.python-versions }}
-      allPythonVersions: ${{ steps.versions.outputs.all-python-versions }}
-      defaultPythonVersion: ${{ steps.versions.outputs.default-python-version }}
+      pythonVersions: ${{ steps.selective-checks.python-versions }}
+      allPythonVersions: ${{ steps.selective-checks.outputs.all-python-versions }}
+      defaultPythonVersion: ${{ steps.selective-checks.outputs.default-python-version }}
+      run-tests: ${{ steps.selective-checks.outputs.run-tests }}
+      run-kubernetes-tests: ${{ steps.selective-checks.outputs.run-kubernetes-tests }}
+      basic-checks-only: ${{ steps.selective-checks.outputs.basic-checks-only }}
     if: >
       needs.cancel-workflow-runs.outputs.buildImages == 'true' &&
       (github.repository == 'apache/airflow' || github.event.workflow_run.event != 'schedule')
     steps:
+      # First fetch the sha of merge commit in case it is pull request so that we can
+      # Run selective tests
+      - name: >
+          Fetch merge commit ${{ github.ref }} ( ${{ github.sha }}:
+          merge_commit ${{ needs.cancel-workflow-runs.outputs.mergeCommitSha }} )
+        uses: actions/checkout@v2
+        with:
+          ref: ${{ needs.cancel-workflow-runs.outputs.mergeCommitSha }}
+          fetch-depth: 2
+        if: needs.cancel-workflow-runs.outputs.sourceEvent  == 'pull_request'
       - name: "Checkout ${{ github.ref }} ( ${{ github.sha }} )"
         uses: actions/checkout@v2
       - name: >
@@ -204,19 +217,20 @@ jobs:
           Source Sha: ${{ needs.cancel-workflow-runs.outputs.sourceHeadSha }}
           Merge commit Sha: ${{ needs.cancel-workflow-runs.outputs.mergeCommitSha }}
           Target commit Sha: ${{ needs.cancel-workflow-runs.outputs.targetCommitSha }}
+        run: printenv
+      - name: Selective checks
+        id: selective-checks
+        env:
+          EVENT_NAME: ${{ needs.cancel-workflow-runs.outputs.sourceEvent }}
+          MERGE_COMMIT_SHA: ${{ needs.cancel-workflow-runs.outputs.mergeCommitSha }}
         run: |
-          printenv
-      - name: Set versions
-        id: versions
-        run: |
-          . ./scripts/ci/libraries/_script_init.sh
-
-          initialization::ga_output python-versions \
-              "$(initialization::parameters_to_json "${CURRENT_PYTHON_MAJOR_MINOR_VERSIONS[@]}")"
-          initialization::ga_output default-python-version "${CURRENT_PYTHON_MAJOR_MINOR_VERSIONS[0]}"
-
-          initialization::ga_output all-python-versions \
-              "$(initialization::parameters_to_json "${ALL_PYTHON_MAJOR_MINOR_VERSIONS[@]}")"
+          if [[ ${EVENT_NAME} == "pull_request" ]]; then
+            # Run selective checks
+            ./scripts/ci/selective_ci_checks.sh "${MERGE_COMMIT_SHA}"
+          else
+            # Run all checks
+            ./scripts/ci/selective_ci_checks.sh
+          fi
 
   build-images:
     timeout-minutes: 80
@@ -231,6 +245,7 @@ jobs:
         image-type: [CI, PROD]
       fail-fast: true
     if: >
+      needs.build-info.outputs.basic-checks-only == 'false' &&
       needs.cancel-workflow-runs.outputs.buildImages == 'true' &&
       (github.repository == 'apache/airflow' || github.event.workflow_run.event != 'schedule')
     env:

scripts/ci/selective_ci_checks.sh
@@ -18,12 +18,21 @@
 # shellcheck source=scripts/ci/libraries/_script_init.sh
 . ./scripts/ci/libraries/_script_init.sh
 
+# Parameter:
+#
+# $1 - Merge commit SHA. If this parameter is missing, this script does not check anything, it simply
+#      sets all the version outputs that determine that all tests should be run. This happens in case
+#      the even triggering the workflow is 'schedule' or 'push'. Merge commit is only
+#      available in case of 'pull_request' triggered runs.
+#
 declare -a pattern_array
 
 function output_all_basic_variables() {
     initialization::ga_output python-versions \
         "$(initialization::parameters_to_json "${CURRENT_PYTHON_MAJOR_MINOR_VERSIONS[@]}")"
     initialization::ga_output default-python-version "${DEFAULT_PYTHON_MAJOR_MINOR_VERSION}"
+    initialization::ga_output all-python-versions \
+        "$(initialization::parameters_to_json "${ALL_PYTHON_MAJOR_MINOR_VERSIONS[@]}")"
 
     initialization::ga_output kubernetes-versions \
         "$(initialization::parameters_to_json "${CURRENT_KUBERNETES_VERSIONS[@]}")"
@@ -58,28 +67,64 @@ function output_all_basic_variables() {
     initialization::ga_output kubernetes-exclude '[]'
 }
 
+function run_tests() {
+    initialization::ga_output run-tests "${@}"
+}
+
+function run_kubernetes_tests() {
+    initialization::ga_output run-kubernetes-tests "${@}"
+}
+
+function needs_helm_tests() {
+    initialization::ga_output needs-helm-tests "${@}"
+}
+
+function needs_api_tests() {
+    initialization::ga_output needs-api-tests "${@}"
+}
+
+function set_test_types() {
+    initialization::ga_output test-types "${@}"
+}
+
+function set_basic_checks_only() {
+    initialization::ga_output basic-checks-only "${@}"
+}
+
+ALL_TESTS="Core Other API CLI Providers WWW Integration Heisentests"
+readonly ALL_TESTS
+
+function set_outputs_run_everything_and_exit() {
+    needs_api_tests "true"
+    needs_helm_tests "true"
+    run_tests "true"
+    run_kubernetes_tests "true"
+    set_test_types "${ALL_TESTS}"
+    set_basic_checks_only "false"
+    exit
+}
+
 function set_outputs_run_all_tests() {
-    initialization::ga_output run-tests "true"
-    initialization::ga_output run-kubernetes-tests "true"
-    initialization::ga_output test-types "Core Other API CLI Providers WWW Integration Heisentests"
+    run_tests "true"
+    run_kubernetes_tests "true"
+    set_test_types "${ALL_TESTS}"
+    set_basic_checks_only "false"
 }
 
-function set_output_skip_all_tests() {
-    initialization::ga_output run-tests "false"
-    initialization::ga_output run-kubernetes-tests "false"
-    initialization::ga_output test-types ""
+function set_output_skip_all_tests_and_exit() {
+    run_tests "false"
+    run_kubernetes_tests "false"
+    set_test_types ""
+    set_basic_checks_only "true"
+    exit
 }
 
-function initialize_git_repo() {
-    git remote add target "https://github.com/${CI_TARGET_REPO}"
-    git fetch target "${CI_TARGET_BRANCH}:${CI_TARGET_BRANCH}" --depth=1
+function get_changed_files() {
+    local commit_sha=${1}
     echo
-    echo "My commit SHA: ${COMMIT_SHA}"
+    echo "Retrieved changed files from ${commit_sha}"
     echo
-    echo
-    echo "Retrieved changed files from ${COMMIT_SHA} comparing to ${CI_TARGET_BRANCH} in ${CI_TARGET_REPO}"
-    echo
-    CHANGED_FILES=$(git diff-tree --no-commit-id --name-only -r "${COMMIT_SHA}" "${CI_TARGET_BRANCH}" || true)
+    CHANGED_FILES=$(git diff-tree --no-commit-id --name-only -r "${commit_sha}" "${commit_sha}^" || true)
     echo
     echo "Changed files:"
     echo
@@ -121,76 +166,93 @@ function show_changed_files() {
 # Output:
 #    Count of changed files matching the patterns
 function count_changed_files() {
+    local count_changed_files
     count_changed_files=$(echo "${CHANGED_FILES}" | grep -c -E "$(get_regexp_from_patterns)" || true)
     echo "${count_changed_files}"
 }
 
-function run_all_tests_when_push_or_schedule() {
-    if [[ ${GITHUB_EVENT_NAME} == "push" || ${GITHUB_EVENT_NAME} == "schedule" ]]; then
-        echo
-        echo "Always run all tests in case of push/schedule events"
-        echo
-        set_outputs_run_all_tests
-        exit
+function check_if_api_tests_should_be_run() {
+    local pattern_array=(
+        "^airflow/api"
+    )
+    show_changed_files
+
+    if [[ $(count_changed_files) == "0" ]]; then
+        needs_api_tests "false"
+    else
+        needs_api_tests "true"
     fi
 }
 
-function check_if_tests_should_be_run_at_all() {
-    TEST_TRIGGERING_PATTERNS=(
-        "^airflow"
-        "^.github/workflows/"
-        "^Dockerfile"
-        "^scripts"
+function check_if_helm_tests_should_be_run() {
+    local pattern_array=(
         "^chart"
-        "^setup.py"
+    )
+    show_changed_files
+
+    if [[ $(count_changed_files) == "0" ]]; then
+        needs_helm_tests "false"
+    else
+        needs_helm_tests "true"
+    fi
+}
+
+function check_if_docs_should_be_generated() {
+    local pattern_array=(
+        "^docs$"
+        "\.py$"
+        "^CHANGELOG\.txt"
+    )
+    show_changed_files
+
+    if [[ $(count_changed_files) == "0" ]]; then
+        echo "None of the docs changed"
+    else
+        image_build_needed="true"
+    fi
+}
+
+AIRFLOW_SOURCES_TRIGGERING_TESTS=(
+        "^airflow"
+        "^chart"
         "^tests"
         "^kubernetes_tests"
-    )
-    readonly TEST_TRIGGERING_PATTERNS
+)
+readonly AIRFLOW_SOURCES_TRIGGERING_TESTS
 
-    pattern_array=("${TEST_TRIGGERING_PATTERNS[@]}")
-    show_changed_files "$(get_regexp_from_patterns)"
+function check_if_tests_are_needed_at_all() {
+    local pattern_array=("${AIRFLOW_SOURCES_TRIGGERING_TESTS[@]}")
+    show_changed_files
 
     if [[ $(count_changed_files) == "0" ]]; then
         echo "None of the important files changed, Skipping tests"
-        set_output_skip_all_tests
-        exit
+        set_output_skip_all_tests_and_exit
     else
-        initialization::ga_output run-tests "true"
+        image_build_needed="true"
+        tests_needed="true"
     fi
 }
 
-function check_if_environment_files_changed() {
-    ENVIRONMENT_TRIGGERING_PATTERNS=(
+function run_all_tests_if_environment_files_changed() {
+    local pattern_array=(
         "^.github/workflows/"
         "^Dockerfile"
         "^scripts"
         "^setup.py"
     )
-    readonly ENVIRONMENT_TRIGGERING_PATTERNS
-
-    pattern_array=("${ENVIRONMENT_TRIGGERING_PATTERNS[@]}")
-    show_changed_files "$(get_regexp_from_patterns)"
+    show_changed_files
 
     if [[ $(count_changed_files) != "0" ]]; then
-        echo "Important environment files changed. Running all tests"
-        set_outputs_run_all_tests
-        exit
+        echo "Important environment files changed. Running everything"
+        set_outputs_run_everything_and_exit
     fi
 }
 
 function get_count_all_files() {
     echo
-    echo "Count All files"
+    echo "Count All airflow source files"
     echo
-    ALL_TRIGGERING_PATTERNS=(
-        "^airflow"
-        "^chart"
-        "^tests"
-        "^kubernetes_tests"
-    )
-    readonly ALL_TRIGGERING_PATTERNS
-    pattern_array=("${ALL_TRIGGERING_PATTERNS[@]}")
+    local pattern_array=("${AIRFLOW_SOURCES_TRIGGERING_TESTS[@]}")
     show_changed_files
     COUNT_ALL_CHANGED_FILES=$(count_changed_files)
     echo "Files count: ${COUNT_ALL_CHANGED_FILES}"
@@ -201,14 +263,12 @@ function get_count_api_files() {
     echo
     echo "Count API files"
     echo
-    API_TRIGGERING_PATTERNS=(
+    local pattern_array=(
         "^airflow/api"
         "^airflow/api_connexion"
         "^tests/api"
         "^tests/api_connexion"
     )
-    readonly API_TRIGGERING_PATTERNS
-    pattern_array=("${API_TRIGGERING_PATTERNS[@]}")
     show_changed_files
     COUNT_API_CHANGED_FILES=$(count_changed_files)
     echo "Files count: ${COUNT_API_CHANGED_FILES}"
@@ -219,12 +279,10 @@ function get_count_cli_files() {
     echo
     echo "Count CLI files"
     echo
-    CLI_TRIGGERING_PATTERNS=(
+    local pattern_array=(
         "^airflow/cli"
         "^tests/cli"
     )
-    readonly CLI_TRIGGERING_PATTERNS
-    pattern_array=("${CLI_TRIGGERING_PATTERNS[@]}")
     show_changed_files
     COUNT_CLI_CHANGED_FILES=$(count_changed_files)
     echo "Files count: ${COUNT_CLI_CHANGED_FILES}"
@@ -235,12 +293,10 @@ function get_count_providers_files() {
     echo
     echo "Count Providers files"
     echo
-    PROVIDERS_TRIGGERING_PATTERNS=(
+    local pattern_array=(
         "^airflow/providers"
         "^tests/providers"
     )
-    readonly PROVIDERS_TRIGGERING_PATTERNS
-    pattern_array=("${PROVIDERS_TRIGGERING_PATTERNS[@]}")
     show_changed_files
     COUNT_PROVIDERS_CHANGED_FILES=$(count_changed_files)
     echo "Files count: ${COUNT_PROVIDERS_CHANGED_FILES}"
@@ -251,12 +307,10 @@ function get_count_www_files() {
     echo
     echo "Count WWW files"
     echo
-    WWW_TRIGGERING_PATTERNS=(
+    local pattern_array=(
         "^airflow/www"
         "^tests/www"
     )
-    readonly WWW_TRIGGERING_PATTERNS
-    pattern_array=("${WWW_TRIGGERING_PATTERNS[@]}")
     show_changed_files
     COUNT_WWW_CHANGED_FILES=$(count_changed_files)
     echo "Files count: ${COUNT_WWW_CHANGED_FILES}"
@@ -267,13 +321,11 @@ function get_count_kubernetes_files() {
     echo
     echo "Count Kubernetes files"
     echo
-    KUBERNETES_TRIGGERING_PATTERNS=(
+    local pattern_array=(
         "^airflow/kubernetes"
         "^chart"
         "^tests/kubernetes_tests"
     )
-    readonly KUBERNETES_TRIGGERING_PATTERNS
-    pattern_array=("${KUBERNETES_TRIGGERING_PATTERNS[@]}")
     show_changed_files
     COUNT_KUBERNETES_CHANGED_FILES=$(count_changed_files)
     echo "Files count: ${COUNT_KUBERNETES_CHANGED_FILES}"
@@ -284,9 +336,7 @@ function calculate_test_types_to_run() {
     echo
     echo "Count Core/Other files"
     echo
-    COUNT_CORE_OTHER_CHANGED_FILES=$((COUNT_ALL_CHANGED_FILES - COUNT_WWW_CHANGED_FILES - \
-        COUNT_PROVIDERS_CHANGED_FILES - COUNT_CLI_CHANGED_FILES - COUNT_API_CHANGED_FILES - \
-        COUNT_KUBERNETES_CHANGED_FILES))
+    COUNT_CORE_OTHER_CHANGED_FILES=$((COUNT_ALL_CHANGED_FILES - COUNT_WWW_CHANGED_FILES - COUNT_PROVIDERS_CHANGED_FILES - COUNT_CLI_CHANGED_FILES - COUNT_API_CHANGED_FILES - COUNT_KUBERNETES_CHANGED_FILES))
 
     readonly COUNT_CORE_OTHER_CHANGED_FILES
     echo
@@ -301,11 +351,9 @@ function calculate_test_types_to_run() {
         set_outputs_run_all_tests
     else
         if [[ ${COUNT_KUBERNETES_CHANGED_FILES} != "0" ]]; then
-            initialization::ga_output run-kubernetes-tests "true"
-        else
-            initialization::ga_output run-kubernetes-tests "false"
+            kubernetes_tests_needed="true"
         fi
-        initialization::ga_output run-tests "true"
+        tests_needed="true"
         SELECTED_TESTS=""
         if [[ ${COUNT_API_CHANGED_FILES} != "0" ]]; then
             echo
@@ -336,10 +384,31 @@ function calculate_test_types_to_run() {
 }
 
 output_all_basic_variables
-run_all_tests_when_push_or_schedule
-initialize_git_repo
-check_if_tests_should_be_run_at_all
-check_if_environment_files_changed
+
+if (($# < 1)); then
+    echo
+    echo "No merge commit SHA - running all tests!"
+    echo
+    set_outputs_run_everything_and_exit
+fi
+
+MERGE_COMMIT_SHA="${1}"
+readonly MERGE_COMMIT_SHA
+
+echo
+echo "Merge commit SHA: ${MERGE_COMMIT_SHA}"
+echo
+
+image_build_needed="false"
+tests_needed="false"
+kubernetes_tests_needed="false"
+
+get_changed_files "${MERGE_COMMIT_SHA}"
+run_all_tests_if_environment_files_changed
+check_if_docs_should_be_generated
+check_if_helm_tests_should_be_run
+check_if_api_tests_should_be_run
+check_if_tests_are_needed_at_all
 get_count_all_files
 get_count_api_files
 get_count_cli_files
@@ -347,3 +416,21 @@ get_count_providers_files
 get_count_www_files
 get_count_kubernetes_files
 calculate_test_types_to_run
+
+if [[ ${image_build_needed} == "true" ]]; then
+    set_basic_checks_only "false"
+else
+    set_basic_checks_only "true"
+fi
+
+if [[ ${tests_needed} == "true" ]]; then
+    run_tests
+else
+    skip_running_tests
+fi
+
+if [[ ${kubernetes_tests_needed} == "true" ]]; then
+    run_kubernetes_tests
+else
+    skip_running_kubernetes_tests
+fi

.github/workflows/codeql-analysis.yml
@@ -33,7 +33,7 @@ jobs:
       # If this run was triggered by a pull request event, then checkout
       # the head of the pull request instead of the merge commit.
       - run: git checkout HEAD^2
-        if: ${{ github.event_name == 'pull_request' }}
+        if: github.event_name == 'pull_request'
 
       # Initializes the CodeQL tools for scanning.
       - name: Initialize CodeQL

apache/airflow

commit 363cf49781637da0c86938984343ffd10cc69890

Fixed an error introduced in selective checks
A few remnants of earlier version of the script caused occasional
errors. Error introduced in #11541

.github/workflows/build-images-workflow-run.yml
@@ -222,11 +222,11 @@ jobs:
         id: selective-checks
         env:
           EVENT_NAME: ${{ needs.cancel-workflow-runs.outputs.sourceEvent }}
-          MERGE_COMMIT_SHA: ${{ needs.cancel-workflow-runs.outputs.mergeCommitSha }}
+          COMMIT_SHA: ${{ needs.cancel-workflow-runs.outputs.mergeCommitSha }}
         run: |
           if [[ ${EVENT_NAME} == "pull_request" ]]; then
             # Run selective checks
-            ./scripts/ci/selective_ci_checks.sh "${MERGE_COMMIT_SHA}"
+            ./scripts/ci/selective_ci_checks.sh "${COMMIT_SHA}"
           else
             # Run all checks
             ./scripts/ci/selective_ci_checks.sh

.github/workflows/ci.yml
@@ -99,14 +99,13 @@ jobs:
       # First fetch the sha of merge commit in case it is pull request so that we can
       # Run selective tests
       - name: >
-          Fetch merge commit ${{ github.ref }} ( ${{ github.sha }}:
-          merge_commit ${{ github.event.pull_request.merge_commit_sha }} )
+          Fetch merge commit ${{ github.ref }} ( ${{ github.sha }} )
         uses: actions/checkout@v2
         with:
-          ref: ${{ github.event.pull_request.merge_commit_sha }}
+          ref: ${{ github.sha }}
           fetch-depth: 2
         if: github.event_name  == 'pull_request'
-      - name: "Checkout ${{ github.ref }} ( ${{ github.sha }} : merge commit ${{ github.merge_commit_sha }} )"
+      - name: "Checkout ${{ github.ref }} ( ${{ github.sha }})"
         uses: actions/checkout@v2
       - name: >
           Event: ${{ github.event_name }}
@@ -114,7 +113,6 @@ jobs:
           Branch: ${{ github.head_ref }}
           Run id: ${{ github.run_id }}
           Sha: ${{ github.sha }}
-          Merge commit sha: ${{ github.merge_commit_sha }}
           Ref: ${{ github.ref }}
         run: printenv
       - name: Set wait for image
@@ -129,11 +127,11 @@ jobs:
         id: selective-checks
         env:
           EVENT_NAME: ${{ github.event_name }}
-          MERGE_COMMIT_SHA: ${{ github.event.pull_request.merge_commit_sha }}
+          COMMIT_SHA: ${{ github.sha }}
         run: |
           if [[ ${EVENT_NAME} == "pull_request" ]]; then
             # Run selective checks
-            ./scripts/ci/selective_ci_checks.sh "${MERGE_COMMIT_SHA}"
+            ./scripts/ci/selective_ci_checks.sh "${COMMIT_SHA}"
           else
             # Run all checks
             ./scripts/ci/selective_ci_checks.sh
@@ -148,7 +146,7 @@ jobs:
       needs.build-info.outputs.needs-helm-tests == 'true' &&
       (github.repository == 'apache/airflow' || github.event_name != 'schedule')
     steps:
-      - name: "Checkout ${{ github.ref }} ( ${{ github.sha }} : merge commit ${{ github.merge_commit_sha }} )"
+      - name: "Checkout ${{ github.ref }} ( ${{ github.sha }} )"
         uses: actions/checkout@v2
       - name: "Helm Tests"
         run: ./scripts/ci/kubernetes/ci_run_helm_testing.sh
@@ -160,7 +158,7 @@ jobs:
       needs.build-info.outputs.needs-api-tests == 'true' &&
       (github.repository == 'apache/airflow' || github.event_name != 'schedule')
     steps:
-      - name: "Checkout ${{ github.ref }} ( ${{ github.sha }} : merge commit ${{ github.merge_commit_sha }})"
+      - name: "Checkout ${{ github.ref }} ( ${{ github.sha }} )"
         uses: actions/checkout@v2
       - name: "Generate client codegen diff"
         run: ./scripts/ci/openapi/client_codegen_diff.sh
@@ -176,7 +174,7 @@ jobs:
     env:
       BACKEND: sqlite
     steps:
-      - name: "Checkout ${{ github.ref }} ( ${{ github.sha }} : merge commit ${{ github.merge_commit_sha }} )"
+      - name: "Checkout ${{ github.ref }} ( ${{ github.sha }} )"
         uses: actions/checkout@v2
         if: needs.build-info.outputs.waitForImage == 'true'
       - name: "Setup python"
@@ -211,7 +209,7 @@ jobs:
       needs.build-info.outputs.basic-checks-only == 'false' &&
       (github.repository == 'apache/airflow' || github.event_name != 'schedule')
     steps:
-      - name: "Checkout ${{ github.ref }} ( ${{ github.sha }} : merge commit ${{ github.merge_commit_sha }} )"
+      - name: "Checkout ${{ github.ref }} ( ${{ github.sha }} )"
         uses: actions/checkout@v2
       - name: "Setup python"
         uses: actions/setup-python@v2
@@ -251,13 +249,11 @@ jobs:
     steps:
       # First fetch the sha of merge commit. The basic-checks-only can only be true in pull_request event!
       - name: >
-          Fetch merge commit ${{ github.ref }} ( ${{ github.sha }}:
-          merge_commit ${{ github.event.pull_request.merge_commit_sha }} )
-        uses: actions/checkout@v2
+          Fetch merge commit ${{ github.ref }} ( ${{ github.sha }} )
         with:
-          ref: ${{ github.event.pull_request.merge_commit_sha }}
+          ref: ${{ github.sha }}
           fetch-depth: 2
-      - name: "Checkout ${{ github.ref }} ( ${{ github.sha }} : merge commit ${{ github.merge_commit_sha }} )"
+      - name: "Checkout ${{ github.ref }} ( ${{ github.sha }} )"
         uses: actions/checkout@v2
         with:
           fetch-depth: 2
@@ -276,8 +272,7 @@ jobs:
         run: ./scripts/ci/tools/ci_free_space_on_ci.sh
       - name: "Static checks: basic checks only"
         run: |
-          ./scripts/ci/static_checks/run_basic_static_checks.sh \
-              "${{ github.event.pull_request.merge_commit_sha }}"
+          ./scripts/ci/static_checks/run_basic_static_checks.sh "${{ github.sha }}"
         env:
           VERBOSE: false
 
@@ -296,7 +291,7 @@ jobs:
       MOUNT_LOCAL_SOURCES: "true"
       PYTHON_MAJOR_MINOR_VERSION: ${{needs.build-info.outputs.defaultPythonVersion}}
     steps:
-      - name: "Checkout ${{ github.ref }} ( ${{ github.sha }} : merge commit ${{ github.merge_commit_sha }})"
+      - name: "Checkout ${{ github.ref }} (${{ github.sha }} )"
         uses: actions/checkout@v2
       - name: "Setup python"
         uses: actions/setup-python@v2
@@ -327,7 +322,7 @@ jobs:
       needs.build-info.outputs.basic-checks-only == 'false' &&
       (github.repository == 'apache/airflow' || github.event_name != 'schedule')
     steps:
-      - name: "Checkout ${{ github.ref }} ( ${{ github.sha }} : merge commit ${{ github.merge_commit_sha }})"
+      - name: "Checkout ${{ github.ref }} (${{ github.sha }} )"
         uses: actions/checkout@v2
       - name: "Prepare CI image ${{env.PYTHON_MAJOR_MINOR_VERSION}}:${{ env.GITHUB_REGISTRY_PULL_IMAGE_TAG }}"
         run: ./scripts/ci/images/ci_prepare_ci_image_on_ci.sh
@@ -351,7 +346,7 @@ jobs:
       needs.build-info.outputs.basic-checks-only == 'false' &&
       (github.repository == 'apache/airflow' || github.event_name != 'schedule')
     steps:
-      - name: "Checkout ${{ github.ref }} ( ${{ github.sha }} : merge commit ${{ github.merge_commit_sha }} )"
+      - name: "Checkout ${{ github.ref }} ( ${{ github.sha }} : )"
         uses: actions/checkout@v2
       - name: "Prepare CI image ${{env.PYTHON_MAJOR_MINOR_VERSION}}:${{ env.GITHUB_REGISTRY_PULL_IMAGE_TAG }}"
         run: ./scripts/ci/images/ci_prepare_ci_image_on_ci.sh
@@ -371,7 +366,7 @@ jobs:
       needs.build-info.outputs.basic-checks-only == 'false' &&
       (github.repository == 'apache/airflow' || github.event_name != 'schedule')
     steps:
-      - name: "Checkout ${{ github.ref }} ( ${{ github.sha }} : merge commit ${{ github.merge_commit_sha }})"
+      - name: "Checkout ${{ github.ref }} (${{ github.sha }} )"
         uses: actions/checkout@v2
       - name: "Setup python"
         uses: actions/setup-python@v2
@@ -408,7 +403,7 @@ jobs:
       needs.build-info.outputs.basic-checks-only == 'false' &&
       (github.repository == 'apache/airflow' || github.event_name != 'schedule')
     steps:
-      - name: "Checkout ${{ github.ref }} ( ${{ github.sha }} : merge commit ${{ github.merge_commit_sha }})"
+      - name: "Checkout ${{ github.ref }} (${{ github.sha }} )"
         uses: actions/checkout@v2
       - name: "Setup python"
         uses: actions/setup-python@v2
@@ -455,7 +450,7 @@ jobs:
         needs.build-info.outputs.run-tests == 'true' &&
         (github.repository == 'apache/airflow' || github.event_name != 'schedule')
     steps:
-      - name: "Checkout ${{ github.ref }} ( ${{ github.sha }} : merge commit ${{ github.merge_commit_sha }})"
+      - name: "Checkout ${{ github.ref }} (${{ github.sha }} )"
         uses: actions/checkout@v2
       - name: "Setup python"
         uses: actions/setup-python@v2
@@ -501,7 +496,7 @@ jobs:
         needs.build-info.outputs.run-tests == 'true' &&
         (github.repository == 'apache/airflow' || github.event_name != 'schedule')
     steps:
-      - name: "Checkout ${{ github.ref }} ( ${{ github.sha }} : merge commit ${{ github.merge_commit_sha }})"
+      - name: "Checkout ${{ github.ref }} (${{ github.sha }} )"
         uses: actions/checkout@v2
       - name: "Setup python"
         uses: actions/setup-python@v2
@@ -544,7 +539,7 @@ jobs:
         needs.build-info.outputs.run-tests == 'true' &&
         (github.repository == 'apache/airflow' || github.event_name != 'schedule')
     steps:
-      - name: "Checkout ${{ github.ref }} ( ${{ github.sha }} : merge commit ${{ github.merge_commit_sha }})"
+      - name: "Checkout ${{ github.ref }} (${{ github.sha }} )"
         uses: actions/checkout@v2
       - name: "Setup python"
         uses: actions/setup-python@v2
@@ -593,7 +588,7 @@ jobs:
       needs.build-info.outputs.run-tests == 'true' &&
       (github.repository == 'apache/airflow' || github.event_name != 'schedule')
     steps:
-      - name: "Checkout ${{ github.ref }} ( ${{ github.sha }} : merge commit ${{ github.merge_commit_sha }})"
+      - name: "Checkout ${{ github.ref }} (${{ github.sha }} )"
         uses: actions/checkout@v2
       - name: "Setup python"
         uses: actions/setup-python@v2
@@ -714,7 +709,7 @@ jobs:
       needs.build-info.outputs.run-kubernetes-tests == 'true' &&
       (github.repository == 'apache/airflow' || github.event_name != 'schedule')
     steps:
-      - name: "Checkout ${{ github.ref }} ( ${{ github.sha }} : merge commit ${{ github.merge_commit_sha }})"
+      - name: "Checkout ${{ github.ref }} (${{ github.sha }} )"
         uses: actions/checkout@v2
       - name: "Setup python"
         uses: actions/setup-python@v2
@@ -839,7 +834,7 @@ jobs:
       PYTHON_MAJOR_MINOR_VERSION: ${{ matrix.python-version }}
       GITHUB_REGISTRY_PUSH_IMAGE_TAG: "latest"
     steps:
-      - name: "Checkout ${{ github.ref }} ( ${{ github.sha }} : merge commit ${{ github.merge_commit_sha }})"
+      - name: "Checkout ${{ github.ref }} (${{ github.sha }} )"
         uses: actions/checkout@v2
       - name: "Setup python"
         uses: actions/setup-python@v2
@@ -876,7 +871,7 @@ jobs:
       github.event_name != 'pull' &&
       (github.repository == 'apache/airflow' || github.event_name != 'schedule')
     steps:
-      - name: "Checkout ${{ github.ref }} ( ${{ github.sha }} : merge commit ${{ github.merge_commit_sha }})"
+      - name: "Checkout ${{ github.ref }} (${{ github.sha }} )"
         uses: actions/checkout@v2
       - name: "Setup python"
         uses: actions/setup-python@v2
@@ -948,7 +943,7 @@ jobs:
       needs.build-info.outputs.basic-checks-only == 'false' &&
       github.event_name == 'schedule' &&  github.repository == 'apache/airflow'
     steps:
-      - name: "Checkout ${{ github.ref }} ( ${{ github.sha }} : merge commit ${{ github.merge_commit_sha }})"
+      - name: "Checkout ${{ github.ref }} (${{ github.sha }} )"
         uses: actions/checkout@v2
       - name: "Free space"
         run: ./scripts/ci/tools/ci_free_space_on_ci.sh

scripts/ci/selective_ci_checks.sh
@@ -392,18 +392,18 @@ if (($# < 1)); then
     set_outputs_run_everything_and_exit
 fi
 
-MERGE_COMMIT_SHA="${1}"
-readonly MERGE_COMMIT_SHA
+COMMIT_SHA="${1}"
+readonly COMMIT_SHA
 
 echo
-echo "Merge commit SHA: ${MERGE_COMMIT_SHA}"
+echo "Merge commit SHA: ${COMMIT_SHA}"
 echo
 
 image_build_needed="false"
 tests_needed="false"
 kubernetes_tests_needed="false"
 
-get_changed_files "${MERGE_COMMIT_SHA}"
+get_changed_files "${COMMIT_SHA}"
 run_all_tests_if_environment_files_changed
 check_if_docs_should_be_generated
 check_if_helm_tests_should_be_run
@@ -424,13 +424,13 @@ else
 fi
 
 if [[ ${tests_needed} == "true" ]]; then
-    run_tests
+    run_tests "true"
 else
-    skip_running_tests
+    run_tests "false"
 fi
 
 if [[ ${kubernetes_tests_needed} == "true" ]]; then
-    run_kubernetes_tests
+    run_kubernetes_tests "true"
 else
-    skip_running_kubernetes_tests
+    run_kubernetes_tests "false"
 fi