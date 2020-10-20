apache/spark

commit 68f647a4e1dd8740a06ff961b382a1ecc89c414a

Dumps more logs in case of CI failure (#11614)
We do not dump airflow logs on success any more, but we dump them
and all the container logs in case of failure, so that we can
better investigate cases like #11543 - that includes enabling
full deadlock information dumping in our mysql database.

scripts/ci/libraries/_start_end.sh
@@ -44,6 +44,20 @@ function start_end::script_start {
     START_SCRIPT_TIME=$(date +%s)
 }
 
+function start_end::dump_container_logs() {
+    local container="${1}"
+    local dump_file
+    dump_file=${AIRFLOW_SOURCES}/files/container_logs_${container}_$(date "+%Y-%m-%d")_${CI_BUILD_ID}_${CI_JOB_ID}.log
+    echo "###########################################################################################"
+    echo "                   Dumping logs from ${container} container"
+    echo "###########################################################################################"
+    docker logs "${container}" > "${dump_file}"
+    echo "                   Container ${container} logs dumped to ${dump_file}"
+    popd || exit 1
+    echo "###########################################################################################"
+}
+
+
 #
 # Trap function executed always at the end of the script. In case of verbose output it also
 # Prints the exit code that the script exits with. Removes verbosity of commands in case it was run with
@@ -52,15 +66,20 @@ function start_end::script_start {
 #
 function start_end::script_end {
     #shellcheck disable=2181
-    EXIT_CODE=$?
-    if [[ ${EXIT_CODE} != 0 ]]; then
+    local exit_code=$?
+    if [[ ${exit_code} != 0 ]]; then
+        local container
+        for container in $(docker ps --format '{{.Names}}')
+        do
+            start_end::dump_container_logs "${container}"
+        done
         # Cat output log in case we exit with error but only if we do not PRINT_INFO_FROM_SCRIPTS
         # Because it will be printed immediately by "tee"
         if [[ -f "${OUTPUT_LOG}" && ${PRINT_INFO_FROM_SCRIPTS} == "false" ]]; then
             cat "${OUTPUT_LOG}"
         fi
         verbosity::print_info "###########################################################################################"
-        verbosity::print_info "                   EXITING WITH STATUS CODE ${EXIT_CODE}"
+        verbosity::print_info "                   EXITING WITH STATUS CODE ${exit_code}"
         verbosity::print_info "###########################################################################################"
     fi
     if [[ ${VERBOSE_COMMANDS:="false"} == "true" ]]; then
@@ -77,7 +96,7 @@ function start_end::script_end {
         verbosity::print_info
         verbosity::print_info "Finished the script $(basename "$0")"
         verbosity::print_info "Elapsed time spent in the script: ${RUN_SCRIPT_TIME} seconds"
-        verbosity::print_info "Exit code ${EXIT_CODE}"
+        verbosity::print_info "Exit code ${exit_code}"
         verbosity::print_info
     fi
 }

.github/workflows/ci.yml
@@ -379,10 +379,16 @@ jobs:
         run: ./scripts/ci/testing/ci_run_airflow_testing.sh
       - name: "Upload airflow logs"
         uses: actions/upload-artifact@v2
-        if: always()
+        if: failure()
         with:
           name: airflow-logs-${{matrix.python-version}}-${{matrix.postgres-version}}
           path: "./files/airflow_logs*"
+      - name: "Upload container logs"
+        uses: actions/upload-artifact@v2
+        if: failure()
+        with:
+          name: container-logs-postgres-${{matrix.python-version}}-${{matrix.postgres-version}}
+          path: "./files/container_logs*"
       - name: "Upload artifact for coverage"
         uses: actions/upload-artifact@v2
         with:
@@ -425,10 +431,16 @@ jobs:
         run: ./scripts/ci/testing/ci_run_airflow_testing.sh
       - name: "Upload airflow logs"
         uses: actions/upload-artifact@v2
-        if: always()
+        if: failure()
         with:
           name: airflow-logs-${{matrix.python-version}}-${{matrix.mysql-version}}
           path: "./files/airflow_logs*"
+      - name: "Upload container logs"
+        uses: actions/upload-artifact@v2
+        if: failure()
+        with:
+          name: container-logs-mysql-${{matrix.python-version}}-${{matrix.mysql-version}}
+          path: "./files/container_logs*"
       - name: "Upload artifact for coverage"
         uses: actions/upload-artifact@v2
         with:
@@ -468,10 +480,16 @@ jobs:
         run: ./scripts/ci/testing/ci_run_airflow_testing.sh
       - name: "Upload airflow logs"
         uses: actions/upload-artifact@v2
-        if: always()
+        if: failure()
         with:
           name: airflow-logs-${{matrix.python-version}}
           path: './files/airflow_logs*'
+      - name: "Upload container logs"
+        uses: actions/upload-artifact@v2
+        if: failure()
+        with:
+          name: container-logs-sqlite-${{matrix.python-version}}
+          path: "./files/container_logs*"
       - name: "Upload artifact for coverage"
         uses: actions/upload-artifact@v2
         with:
@@ -535,14 +553,20 @@ jobs:
           path: "files/test_result.xml"
       - name: "Upload airflow logs"
         uses: actions/upload-artifact@v2
-        if: always()
+        if: failure()
         with:
-          name: airflow-logs-quarantined-${{matrix.python-version}}-${{ matrix.postgres-version }}
+          name: airflow-logs-quarantined-${{ matrix.backend }}
           path: "./files/airflow_logs*"
+      - name: "Upload container logs"
+        uses: actions/upload-artifact@v2
+        if: failure()
+        with:
+          name: container-logs-quarantined-${{ matrix.backend }}
+          path: "./files/container_logs*"
       - name: "Upload artifact for coverage"
         uses: actions/upload-artifact@v2
         with:
-          name: coverage-quarantined-${{matrix.python-version}}
+          name: coverage-quarantined-${{ matrix.backend }}
           path: "./files/coverage.xml"
 
   upload-coverage:

scripts/ci/libraries/_verbosity.sh
@@ -117,6 +117,7 @@ function kind {
     ${KIND_BINARY_PATH} "${@}"
 }
 
+
 # Prints verbose information in case VERBOSE variable is set
 function verbosity::print_info() {
     if [[ ${VERBOSE:="false"} == "true" && ${PRINT_INFO_FROM_SCRIPTS} == "true" ]]; then

scripts/ci/mysql/conf.d/airflow.cnf
@@ -21,3 +21,4 @@
 explicit_defaults_for_timestamp = 1
 secure_file_priv = "/var/lib/mysql"
 local_infile = 1
+innodb_print_all_deadlocks = 1

scripts/in_container/_in_container_utils.sh
@@ -274,17 +274,17 @@ function setup_kerberos() {
 }
 
 function dump_airflow_logs() {
-    DUMP_FILE=/files/airflow_logs_$(date "+%Y-%m-%d")_${CI_BUILD_ID}_${CI_JOB_ID}.log.tar.gz
+    local dump_file
+    dump_file=/files/airflow_logs_$(date "+%Y-%m-%d")_${CI_BUILD_ID}_${CI_JOB_ID}.log.tar.gz
     echo "###########################################################################################"
     echo "                   Dumping logs from all the airflow tasks"
     echo "###########################################################################################"
     pushd "${AIRFLOW_HOME}" || exit 1
-    tar -czf "${DUMP_FILE}" logs
-    echo "                   Logs dumped to ${DUMP_FILE}"
+    tar -czf "${dump_file}" logs
+    echo "                   Logs dumped to ${dump_file}"
     popd || exit 1
     echo "###########################################################################################"
 }
-
 
 function install_released_airflow_version() {
     pip uninstall -y apache-airflow || true

scripts/in_container/run_ci_tests.sh
@@ -98,7 +98,12 @@ if [[ ${TEST_TYPE:=} == "Quarantined" ]]; then
 fi
 
 if [[ ${CI:=} == "true" ]]; then
-    dump_airflow_logs
+    if [[ ${RES} != "0" ]]; then
+        echo
+        echo "Dumping logs on error"
+        echo
+        dump_airflow_logs
+    fi
 fi
 
 exit "${RES}"

apache/airflow

commit eec2845e45e40f429f3df1c52207858e649846bd

Fixing problem with missing output in pre-commits in some cases
Dumping logs from container should only be done in CI.

Problem was introduced in #11614

scripts/ci/libraries/_start_end.sh
@@ -68,15 +68,17 @@ function start_end::script_end {
     #shellcheck disable=2181
     local exit_code=$?
     if [[ ${exit_code} != 0 ]]; then
-        local container
-        for container in $(docker ps --format '{{.Names}}')
-        do
-            start_end::dump_container_logs "${container}"
-        done
         # Cat output log in case we exit with error but only if we do not PRINT_INFO_FROM_SCRIPTS
         # Because it will be printed immediately by "tee"
         if [[ -f "${OUTPUT_LOG}" && ${PRINT_INFO_FROM_SCRIPTS} == "false" ]]; then
             cat "${OUTPUT_LOG}"
+        fi
+        if [[ ${CI} == "true" ]]; then
+            local container
+            for container in $(docker ps --format '{{.Names}}')
+            do
+                start_end::dump_container_logs "${container}"
+            done
         fi
         verbosity::print_info "###########################################################################################"
         verbosity::print_info "                   EXITING WITH STATUS CODE ${exit_code}"