apache/spark

commit 1fb3c28e1a4fd54c9d83dccd413659a7a87c7315

Add support for setting ciphers for SFTPHook (#11720)

airflow/providers/sftp/hooks/sftp.py
@@ -51,6 +51,7 @@ class SFTPHook(SSHHook):
 
         self.conn = None
         self.private_key_pass = None
+        self.ciphers = None
 
         # Fail for unverified hosts, unless this is explicitly allowed
         self.no_host_key_check = False
@@ -80,6 +81,9 @@ class SFTPHook(SSHHook):
 
                 if 'no_host_key_check' in extra_options:
                     self.no_host_key_check = str(extra_options['no_host_key_check']).lower() == 'true'
+                    
+                if 'ciphers' in extra_options:
+                    self.ciphers = extra_options['ciphers']
 
                 if 'private_key' in extra_options:
                     warnings.warn(
@@ -98,6 +102,7 @@ class SFTPHook(SSHHook):
             if self.no_host_key_check:
                 cnopts.hostkeys = None
             cnopts.compression = self.compress
+            cnopts.ciphers = self.ciphers
             conn_params = {
                 'host': self.remote_host,
                 'port': self.port,

tests/providers/sftp/hooks/test_sftp.py
@@ -145,6 +145,14 @@ class TestSFTPHook(unittest.TestCase):
         get_connection.return_value = connection
         hook = SFTPHook()
         self.assertEqual(hook.no_host_key_check, False)
+        
+    @mock.patch('airflow.providers.sftp.hooks.sftp.SFTPHook.get_connection')
+    def test_ciphers(self, get_connection):
+        connection = Connection(login='login', host='host', extra='{"ciphers": ["A", "B", "C"]}')
+
+        get_connection.return_value = connection
+        hook = SFTPHook()
+        self.assertEqual(hook.ciphers, ["A", "B", "C"])
 
     @mock.patch('airflow.providers.sftp.hooks.sftp.SFTPHook.get_connection')
     def test_no_host_key_check_disabled_for_all_but_true(self, get_connection):

apache/airflow

commit cc9c8ed91170be9c738e9c6e2e98230cc2eafd0e

Fix formatting errors introduced in #11720

airflow/providers/sftp/hooks/sftp.py
@@ -81,7 +81,7 @@ class SFTPHook(SSHHook):
 
                 if 'no_host_key_check' in extra_options:
                     self.no_host_key_check = str(extra_options['no_host_key_check']).lower() == 'true'
-                    
+
                 if 'ciphers' in extra_options:
                     self.ciphers = extra_options['ciphers']
 

tests/providers/sftp/hooks/test_sftp.py
@@ -145,7 +145,7 @@ class TestSFTPHook(unittest.TestCase):
         get_connection.return_value = connection
         hook = SFTPHook()
         self.assertEqual(hook.no_host_key_check, False)
-        
+
     @mock.patch('airflow.providers.sftp.hooks.sftp.SFTPHook.get_connection')
     def test_ciphers(self, get_connection):
         connection = Connection(login='login', host='host', extra='{"ciphers": ["A", "B", "C"]}')