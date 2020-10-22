50401022475db291fd7740a84745c4d911e593f5
Michał Misiewicz <misiewicz.michall@gmail.com>
2020-10-22 19:58:14 +0200

    modify should_emit_deprecation_warnings test

--- tests/www/test_app.py
+++ tests/www/test_app.py
@@ -234,10 +234,8 @@ class TestApp(unittest.TestCase):
     def test_should_emit_deprecation_warnings(self):
         with pytest.warns(DeprecationWarning) as warns:
             application.cached_app(testing=True)
-        app_path = str(Path(__file__).parents[2].joinpath('airflow', 'www', 'app.py'))
-        warns = [w for w in warns if w.filename == app_path]
-        self.assertEqual(len(warns), 2)
-        self.assertEqual(warns[0].message.args[0],
-                         'SESSION_LIFETIME_DAYS option is deprecated. Please use `SESSION_LIFETIME_MINUTES`')
-        self.assertEqual(warns[1].message.args[0],
-                         'FORCE_LOG_OUT_AFTER option is deprecated. Please use `SESSION_LIFETIME_MINUTES`')
+        msg1 = 'SESSION_LIFETIME_DAYS option is deprecated. Please use `SESSION_LIFETIME_MINUTES`'
+        msg2 = 'FORCE_LOG_OUT_AFTER option is deprecated. Please use `SESSION_LIFETIME_MINUTES`'
+
+        warns = [w.message.args[0] for w in warns]
+        self.assertEqual({msg1, msg2}.issubset(warns), True)
