316fc57bc5986b44c9b2ab01de519dc2949379f6
Szymon Nieradka <szymon[]nieradka.net>
2020-10-25 16:19:00 +0100

    Less strict comparison between setup.py and installation.rst

--- scripts/ci/pre_commit/pre_commit_check_setup_installation.rst.py
+++ scripts/ci/pre_commit/pre_commit_check_setup_installation.rst.py
@@ -37,8 +37,8 @@ def get_file_content(*file_path: str) -> str:
 
 def get_extras_from_setup() -> {str: bool}:
     """
-    Returns an array of extras from setup.py file in format:
-    {'package name': is_deprecated, ...]
+    Returns an array EXTRAS_REQUIREMENTS with aliases from setup.py file in format:
+    {'package name': ['alias1', 'alias2], ...}
     """
     setup_content = get_file_content(SETUP_PY_FILE)
     
@@ -48,44 +48,54 @@ def get_extras_from_setup() -> {str: bool}:
 
     extras_regex = re.compile(
         f'^\s+[\"\']([a-zA-Z_][a-zA-Z0-9_\.]*)[\"\']:\s*([a-zA-Z_][a-zA-Z0-9_\.]*)[^#\n]*(#\s*TODO.*)?$', re.MULTILINE)
-    extras = extras_regex.findall(extras_section)
 
-    extras = list(filter(lambda entry: entry[0] == entry[1], extras))
-
-    return {x[0]: ' remove ' in x[2] for x in extras}
+    extras_dict = {}
+    for extras in extras_regex.findall(extras_section):
+        package = extras[1]
+        alias = extras[0]
+        if not extras_dict.get(package):
+            extras_dict[package] = []
+        extras_dict[package].append(alias)
+    return extras_dict
 
 def get_extras_from_docs() -> {str: bool}:
     """
-    Returns an array of extras from installation.rst file in format:
-    {'package name': is_deprecated, ...]
+    Returns an array of install packages names from installation.rst
+    file in format.
     """
     docs_content = get_file_content('docs', DOCS_FILE)
 
-    extras_section_regex = re.compile(f'^\|([^|]+)\|.*pip install .apache-airflow\[({PY_IDENTIFIER})\].', re.MULTILINE)
-    extras = extras_section_regex.findall(docs_content)
-
-    return {x[1]: 'deprecated' in x[0] for x in extras}
+    extras_section_regex = re.compile(f'^\|[^|]+\|.*pip install .apache-airflow\[({PY_IDENTIFIER})\].', re.MULTILINE)
+    return extras_section_regex.findall(docs_content)
 
 if __name__ == '__main__':
     setup_packages = get_extras_from_setup()
     docs_packages = get_extras_from_docs()
-    all_packages = {**setup_packages, **docs_packages}
 
-    def format_extras(extras):
-        return 
+    output_table = ""
 
-    differences = {item: (setup_packages.get(item, ''), docs_packages.get(item, '')) for item in all_packages}
-
-    if len(differences) == 0:
-        exit(0)
+    for extras in sorted(setup_packages.keys()):
+        if not set(setup_packages[extras]).intersection(docs_packages):
+            output_table += "| {:20} | {:^10} | {:^10} |\n".format(extras, "V", "")
     
-    print(".{:_^22}.{:_^12}.{:_^12}.".format("EXTRAS", "SETUP", "INSTALLATION"))
-    for extras in sorted(differences.keys()):
-        if(differences[extras][0] != differences[extras][1]):
-            setup_label = differences[extras][0]
-            setup_label  = "remove" if setup_label else ("V" if setup_label == False else "!")
-            docs_label = differences[extras][1]
-            docs_label  = "deprecated" if docs_label else ("V" if docs_label == False else "!")
-            print(f"| {extras:20} | {setup_label:^10} | {docs_label:^10} |")
+    setup_packages_str = str(setup_packages)
+    for extras in sorted(docs_packages):
+        if not f"'{extras}'" in setup_packages_str:
+            output_table += "| {:20} | {:^10} | {:^10} |\n".format(extras, "", "V")
+
+    if(output_table == ""):
+        exit(0)
+
+    print(f"""
+ERROR
+
+"EXTRAS_REQUIREMENTS" section in {SETUP_PY_FILE} should be synchronized
+with "Extra Packages" section in documentation file doc/{DOCS_FILE}.
+
+here is a list of packages that are used but are not documented, or
+documented although not used.
+    """)
+    print(".{:_^22}.{:_^12}.{:_^12}.".format("NAME", "SETUP", "INSTALLATION"))
+    print(output_table)
 
     exit(1)
