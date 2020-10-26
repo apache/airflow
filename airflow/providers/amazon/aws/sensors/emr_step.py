6b2ae705448dad3f4bdc5532cad10b16e0e33067
pedresnyman <pedresnyman@capitecbank.co.za>
2020-10-26 11:26:47 +0200

    AWS EmrStepSensor to auto detect steps (#11877)

--- airflow/providers/amazon/aws/sensors/emr_step.py
+++ airflow/providers/amazon/aws/sensors/emr_step.py
@@ -21,10 +21,16 @@ from typing import Any, Dict, Iterable, Optional
 from airflow.providers.amazon.aws.sensors.emr_base import EmrBaseSensor
 from airflow.utils.decorators import apply_defaults
 
+from airflow.exceptions import AirflowException
+from airflow.models.xcom import XCOM_RETURN_KEY
+
 
 class EmrStepSensor(EmrBaseSensor):
     """
     Asks for the state of the step until it reaches any of the target states.
+    If no step Id has been provided the sensor retrieves all steps and current states
+    and will continue to ask until all steps reaches any of the target states.
+
     If it fails the sensor errors, failing the task.
 
     With the default target states, sensor waits step to be completed.
@@ -32,7 +38,7 @@ class EmrStepSensor(EmrBaseSensor):
     :param job_flow_id: job_flow_id which contains the step check the state of
     :type job_flow_id: str
     :param step_id: step to check the state of
-    :type step_id: str
+    :type step_id: str or list
     :param target_states: the target states, sensor waits until
         step reaches any of these states
     :type target_states: list[str]
@@ -59,6 +65,50 @@ class EmrStepSensor(EmrBaseSensor):
         self.step_id = step_id
         self.target_states = target_states or ['COMPLETED']
         self.failed_states = failed_states or ['CANCELLED', 'FAILED', 'INTERRUPTED']
+
+    def poke(self, context):
+        """
+        If only one step_id is provided we want to run the base poke.
+
+        If no step_id is provided we want to run list_steps() and return a single response with
+        all the steps on specified cluster.
+        """
+        if self.step_id:
+            return super().poke(context)
+
+        emr_client = self.get_hook().get_conn()
+        self.log.info('Poking steps on cluster %s', self.job_flow_id)
+        response = emr_client.list_steps(ClusterId=self.job_flow_id)
+
+        if not response['ResponseMetadata']['HTTPStatusCode'] == 200:
+            self.log.info('Bad HTTP response: %s', response)
+            return False
+
+        step_states = [step['Status']['State'] for step in response['Steps']]
+        state_dict = {}
+        for steps in response['Steps']:
+            state_dict[steps['Id']] = steps['Status']['State']
+        self.log.info('Step state = %s', state_dict)
+
+        if self.do_xcom_push:
+            context['ti'].xcom_push(key=XCOM_RETURN_KEY, value=state_dict)
+
+        if all(step_state in self.target_states for step_state in step_states):
+            return True
+
+        if any(step_state in self.failed_states for step_state in step_states):
+            fail_details = [fail_msg['Status'].get('FailureDetails') for fail_msg in response['Steps']]
+            failure_messages = [
+                'for reason {} with message {} and log file {}'.format(
+                    details.get('Reason'), details.get('Message'), details.get('LogFile')
+                )
+                for details in fail_details
+            ]
+            if failure_messages:
+                final_message = 'EMR job failed ' + ' '.join(failure_messages)
+            raise AirflowException(final_message)
+
+        return False
 
     def get_emr_response(self) -> Dict[str, Any]:
         """
