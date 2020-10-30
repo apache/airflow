6b41e9541d445261bd8073750e047cbfaf03a624
Kaxil Naik <kaxilnaik@gmail.com>
2020-10-30 15:14:49 +0000

    Bump attrs and cattrs dependencies (#11969)
    `cattrs` now depends on `attrs >= 20.1.0`, because of `attr.resolve_types`.
    
    Source: https://github.com/Tinche/cattrs/blob/master/HISTORY.rst#110-2020-10-29
    
    closes https://github.com/apache/airflow/issues/11965
    
    (cherry picked from commit 3ad037872e54ec617f1b2734781c61640c7528ca)

--- setup.py
+++ setup.py
@@ -575,9 +575,11 @@ EXTRAS_REQUIREMENTS = {
 INSTALL_REQUIREMENTS = [
     'alembic>=1.0, <2.0',
     'argcomplete~=1.10',
-    'attrs~=20.0',
+    'attrs>=20.0, <21.0',
     'cached_property~=1.5',
-    'cattrs~=1.0',
+    # cattrs >= 1.1.0 dropped support for Python 3.6
+    'cattrs>=1.0, <1.1.0;python_version<="3.6"',
+    'cattrs>=1.0, <2.0;python_version>"3.7"',
     'colorlog==4.0.2',
     'configparser>=3.5.0, <3.6.0',
     'croniter>=0.3.17, <0.4',
