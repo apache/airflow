a990d37ad2c1924cc9e8e1bfaaac2fb7c4d86909
jfmolano <jose.francisco.molano@gmail.com>
2020-11-02 15:42:59 -0500

    Adding MySql howto-documentation and example DAG #11918

--- airflow/providers/mysql/example_dags/example_mysql_operator.py
+++ airflow/providers/mysql/example_dags/example_mysql_operator.py
@@ -23,10 +23,12 @@ from datetime import timedelta
 
 from airflow import DAG
 from airflow.operators.mysql_operator import MySqlOperator
+from airflow.utils.dates import days_ago
 
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
+    'start_date': days_ago(2),
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
