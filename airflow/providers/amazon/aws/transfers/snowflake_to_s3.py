5492e76568fd7d894755a96b1ae494b2cab5b087
javier.lopez <javier.lopez@promocionesfarma.com>
2020-11-02 13:11:38 +0100

    changes

--- airflow/providers/amazon/aws/transfers/snowflake_to_s3.py
+++ airflow/providers/amazon/aws/transfers/snowflake_to_s3.py
@@ -24,21 +24,24 @@ from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
 
 class SnowflakeToS3Operator(BaseOperator):
     """
-    UNLOAD a query or a table from Snowflake to S3
+    UNLOAD a query or a table from Snowflake to S3. It is not necessary a conn to S3 because
+    Snowflake handles it. You have to follow one of the setups in:
+    https://docs.snowflake.com/en/user-guide/data-load-s3-config.html
 
-    :param schema: reference to a specific schema in snowflake database
-    :type schema: str
-    :param table: reference to a specific table in snowflake database
-    :type table: str
-    :param s3_bucket: reference to a specific S3 bucket where the data will be saved
+    :param stage: Copy the data into a Snowflake Stage. This allows you to use the method
+        'Configuring AWS IAM User Credentials' for unloading data to s3. If this is true,
+        s3_bucket and file_format are not necessaries.
+    :type stage: bool
+    :param s3_bucket: reference to a specific S3 bucket where the data will be saved. For using it, you
+        should have done the one-time setup 'Configuring a Snowflake Storage Integration'
     :type s3_bucket: str
     :param s3_key: reference to a specific S3 key within the previous bucket
     :type s3_key: str
-    :param file_format: can be either a previous file format created in Snowflake console
+    :param file_format: can be either a previous file format created in Snowflake
         or hardcoded one like ``type = csv field_delimiter = ',' skip_header = 1``
     :type file_format: str
-    :param sql: optional parameter to unload a customized query instead an entire table
-    :type sql: str
+    :param query_or_table: query or full table to unload. If table, it must include the schema like ``schema.table`
+    :type query_or_table: str
     :param snowflake_conn_id: reference to a specific snowflake database
     :type snowflake_conn_id: str
     :param unload_options: reference to a list of UNLOAD options (SINGLE, MAX_FILE_SIZE,
@@ -49,8 +52,7 @@ class SnowflakeToS3Operator(BaseOperator):
     template_fields = (
         's3_key',
         's3_bucket',
-        'sql',
-        'table',
+        'table_or_query',
         'snowflake_conn_id',
     )
     template_ext = ('.sql',)
@@ -60,12 +62,11 @@ class SnowflakeToS3Operator(BaseOperator):
     def __init__(
         self,
         *,
-        s3_bucket: str,
+        stage: bool = False,
+        s3_bucket: Optional[str],
         s3_key: str,
-        file_format: str,
-        schema: Optional[str] = None,
-        table: Optional[str] = None,
-        sql: Optional[str] = None,
+        query_or_table: str,
+        file_format: Optional[str],
         snowflake_conn_id: str = "snowflake_default",
         unload_options: Optional[list] = None,
         autocommit: bool = True,
@@ -73,13 +74,12 @@ class SnowflakeToS3Operator(BaseOperator):
         **kwargs,
     ) -> None:
         super().__init__(**kwargs)
+        self.stage = stage
         self.s3_bucket = s3_bucket
         self.s3_key = s3_key
         self.file_format = file_format
         self.snowflake_conn_id = snowflake_conn_id
-        self.schema = schema
-        self.table = table
-        self.sql = sql
+        self.query_or_table = query_or_table
         self.unload_options = unload_options or []  # type: List
         self.autocommit = autocommit
         self.include_header = include_header
@@ -88,17 +88,25 @@ class SnowflakeToS3Operator(BaseOperator):
         snowflake_hook = SnowflakeHook(snowflake_conn_id=self.snowflake_conn_id)
         unload_options = '\n\t'.join(self.unload_options)
 
-        if not self.sql:
-            self.sql = self.schema + '.' + self.table
+        if self.stage:
+            unload_query = f"""
+            COPY INTO @{self.stage}/{self.s3_key}
+            FROM ({self.query_or_table})
+            """
 
-        unload_query = f"""
-        COPY INTO 's3://{self.s3_bucket}/{self.s3_key}'
-        FROM ({self.sql})
-        STORAGE_INTEGRATION = S3
-        FILE_FORMAT = ({self.file_format})
-        {unload_options}
-        HEADER = {self.include_header};
-        """
+            if self.file_format:
+                unload_query += f'FILE_FORMAT = ({self.file_format})'
+
+        else:
+            unload_query = f"""
+            COPY INTO 's3://{self.s3_bucket}/{self.s3_key}'
+            FROM ({self.query_or_table})
+            STORAGE_INTEGRATION = S3
+            FILE_FORMAT = ({self.file_format})
+            """
+
+        unload_query += f"""{unload_options}
+                            HEADER = {self.include_header};"""
 
         self.log.info('Executing UNLOAD command...')
         snowflake_hook.run(unload_query, self.autocommit)
