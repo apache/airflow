65646252e7e09c9779c116314601c3da040eb3dd
javier.lopez <javier.lopez@promocionesfarma.com>
2020-11-02 11:51:31 +0100

    AllowDiskUse parameter and docs in MongotoS3Operator

--- airflow/providers/amazon/aws/transfers/mongo_to_s3.py
+++ airflow/providers/amazon/aws/transfers/mongo_to_s3.py
@@ -36,44 +36,66 @@ class MongoToS3Operator(BaseOperator):
                 .execute() is written to depend on .transform()
                 .transform() is meant to be extended by child classes
                 to perform transformations unique to those operators needs
+
+    :param mongo_conn_id: reference to a specific schema in redshift database
+    :type mongo_conn_id: str
+    :param aws_conn_id: reference to a specific S3 connection
+    :type aws_conn_id: str
+    :param mongo_collection: reference to a specific collection in your mongo db
+    :type mongo_collection: str
+    :param mongo_query: query to execute. A list including a dict of the query
+    :type mongo_query: list
+    :param s3_bucket: reference to a specific S3 bucket to store the data
+    :type s3_bucket: str
+    :param s3_key: in which S3 key the file will be stored
+    :type s3_key: str
+    :param mongo_db: reference to a specific mongo database
+    :type mongo_db: str
+    :param replace: whether or not to replace the file in S3 if it previously existed
+    :param replace: bool
+    :param allow_disk_use: in the case you are retrieving a lot of data, you may have
+    to use the disk to save it instead of saving all in the RAM
+    :param allow_disk_use: bool
     """
 
+<<<<<<< HEAD
     template_fields = ['s3_key', 'mongo_query', 'mongo_collection']
+    template_fields = ('s3_bucket', 's3_key', 'mongo_query', 'mongo_collection')
     # pylint: disable=too-many-instance-attributes
 
     @apply_defaults
     def __init__(
         self,
         *,
-        mongo_conn_id: str,
-        s3_conn_id: str,
+        mongo_conn_id: str = 'mongo_default',
+        aws_conn_id: str = 'aws_default',
         mongo_collection: str,
         mongo_query: Union[list, dict],
         s3_bucket: str,
         s3_key: str,
         mongo_db: Optional[str] = None,
         replace: bool = False,
+        allow_disk_use: bool = False,
         **kwargs,
     ) -> None:
         super().__init__(**kwargs)
-        # Conn Ids
         self.mongo_conn_id = mongo_conn_id
-        self.s3_conn_id = s3_conn_id
-        # Mongo Query Settings
+        self.s3_conn_id = aws_conn_id
         self.mongo_db = mongo_db
         self.mongo_collection = mongo_collection
+
         # Grab query and determine if we need to run an aggregate pipeline
         self.mongo_query = mongo_query
         self.is_pipeline = isinstance(self.mongo_query, list)
 
-        # S3 Settings
         self.s3_bucket = s3_bucket
         self.s3_key = s3_key
         self.replace = replace
+        self.allow_disk_use = allow_disk_use
 
     def execute(self, context) -> bool:
         """Executed by task_instance at runtime"""
-        s3_conn = S3Hook(self.s3_conn_id)
+        s3_conn = S3Hook(self.aws_conn_id)
 
         # Grab collection and execute query according to whether or not it is a pipeline
         if self.is_pipeline:
@@ -81,6 +103,7 @@ class MongoToS3Operator(BaseOperator):
                 mongo_collection=self.mongo_collection,
                 aggregate_query=cast(list, self.mongo_query),
                 mongo_db=self.mongo_db,
+                allowDiskUse=self.allow_disk_use
             )
 
         else:
@@ -88,6 +111,7 @@ class MongoToS3Operator(BaseOperator):
                 mongo_collection=self.mongo_collection,
                 query=cast(dict, self.mongo_query),
                 mongo_db=self.mongo_db,
+                allowDiskUse=self.allow_disk_use
             )
 
         # Performs transform then stringifies the docs results into json format
@@ -97,8 +121,6 @@ class MongoToS3Operator(BaseOperator):
         s3_conn.load_string(
             string_data=docs_str, key=self.s3_key, bucket_name=self.s3_bucket, replace=self.replace
         )
-
-        return True
 
     @staticmethod
     def _stringify(iterable: Iterable, joinable: str = '\n') -> str:
