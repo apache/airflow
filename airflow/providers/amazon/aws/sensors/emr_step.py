230d0e6c05405c13231475d0126cfa68a1d81b79
pedresnyman <pedresnyman@capitecbank.co.za>
2020-11-03 11:05:09 +0200

    removed line

--- airflow/providers/amazon/aws/sensors/emr_step.py
+++ airflow/providers/amazon/aws/sensors/emr_step.py
@@ -14,7 +14,7 @@
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
-# under the License.Ek gaan
+# under the License.
 
 from typing import Any, Dict, Iterable, Optional
 
