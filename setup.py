59f2f3511bf70794778356a31d49f13ec7d04f2b
Jarek Potiuk <jarek@potiuk.com>
2020-11-15 01:10:47 +0100

    Limit version of marshmallow-sqlalchemy
    The marshmallow-sqlalchemy 0.24.0 requires marshmallow > 3.0.0 even
    if they say they require > 2.15.2 at their page.
    
    https://pypi.org/project/marshmallow-sqlalchemy/
    
    Issue raised:
    
    https://github.com/marshmallow-code/marshmallow-sqlalchemy/issues/352

--- setup.py
+++ setup.py
@@ -604,6 +604,7 @@ INSTALL_REQUIREMENTS = [
     'jsonschema~=3.0',
     'lazy_object_proxy~=1.3',
     'markdown>=2.5.2, <3.0',
+    'marshmallow-sqlalchemy>=0.16.1, <0.24.0;python_version>="3.6"',
     'marshmallow-sqlalchemy>=0.16.1, <0.19.0;python_version<"3.6"',
     'pandas>=0.17.1, <2.0',
     'pendulum==1.4.4',
