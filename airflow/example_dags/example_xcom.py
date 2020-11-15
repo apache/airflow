c05aa0903689d617641af40d54836df81fe53233
Ehsan Poursaeed <hsnprsd@gmail.com>
2020-11-16 00:11:45 +0330

    Use new syntax for xcom pull in example xcom dag

--- airflow/example_dags/example_xcom.py
+++ airflow/example_dags/example_xcom.py
@@ -17,6 +17,7 @@
 # under the License.
 
 """Example DAG demonstrating the usage of XComs."""
+
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
@@ -43,27 +44,17 @@ def push_by_returning(**kwargs):
     return value_2
 
 
-def puller(**kwargs):
+def puller(pulled_value_1, pulled_value_2, **kwargs):
     """Pull all previously pushed XComs and check if the pushed values match the pulled values."""
     ti = kwargs['ti']
 
-    # get value_1
-    pulled_value_1 = ti.xcom_pull(key=None, task_ids='push')
+    # check pulled value 1
     if pulled_value_1 != value_1:
         raise ValueError(f'The two values differ {pulled_value_1} and {value_1}')
 
-    # get value_2
-    pulled_value_2 = ti.xcom_pull(task_ids='push_by_returning')
+    # check pulled value 2
     if pulled_value_2 != value_2:
         raise ValueError(f'The two values differ {pulled_value_2} and {value_2}')
-
-    # get both value_1 and value_2
-    pulled_value_1, pulled_value_2 = ti.xcom_pull(key=None, task_ids=['push', 'push_by_returning'])
-    if pulled_value_1 != value_1:
-        raise ValueError(f'The two values differ {pulled_value_1} and {value_1}')
-    if pulled_value_2 != value_2:
-        raise ValueError(f'The two values differ {pulled_value_2} and {value_2}')
-
 
 push1 = PythonOperator(
     task_id='push',
@@ -81,6 +72,10 @@ pull = PythonOperator(
     task_id='puller',
     dag=dag,
     python_callable=puller,
+    op_args=[
+        push1.output['value_from_pusher 1'],
+        push2.output,
+    ],
 )
 
 pull << [push1, push2]
