3c59dda7caa1d89dffe24381a2866570c49a3ade
Dmitriy Synkov <dmitriysynkov@gmail.com>
2020-11-16 21:06:10 -0500

    [AIRFLOW-5115] Add mock.patch S3Hook in S3KeySensor tests

--- tests/providers/amazon/aws/sensors/test_s3_key.py
+++ tests/providers/amazon/aws/sensors/test_s3_key.py
@@ -26,6 +26,8 @@ from airflow.providers.amazon.aws.sensors.s3_key import S3KeySensor
 
 
 class TestS3KeySensor(unittest.TestCase):
+
+    @mock.patch('airflow.providers.amazon.aws.sensors.s3_key.S3Hook')
     def test_bucket_name_none_and_bucket_key_as_relative_path(self):
         """
         Test if exception is raised when bucket_name is None
@@ -36,6 +38,7 @@ class TestS3KeySensor(unittest.TestCase):
         with self.assertRaises(AirflowException):
             op.poke(None)
 
+    @mock.patch('airflow.providers.amazon.aws.sensors.s3_key.S3Hook')
     def test_bucket_name_provided_and_bucket_key_is_s3_url(self):
         """
         Test if exception is raised when bucket_name is provided
@@ -54,6 +57,7 @@ class TestS3KeySensor(unittest.TestCase):
             ['key', 'bucket', 'key', 'bucket'],
         ]
     )
+    @mock.patch('airflow.providers.amazon.aws.sensors.s3_key.S3Hook')
     def test_parse_bucket_key(self, key, bucket, parsed_key, parsed_bucket):
         op = S3KeySensor(
             task_id='s3_key_sensor',
