37da5a1dd5d0b5bffb69f1c923086bf6ad30c1e8
Daniel Imberman <daniel.imberman@gmail.com>
2020-11-20 07:19:10 -0800

    Merge invalid_kubernetes_config rule and pod_template_file rule

--- airflow/upgrade/rules/pod_template_file_rule.py
+++ airflow/upgrade/rules/pod_template_file_rule.py
@@ -20,14 +20,36 @@ from __future__ import absolute_import
 from airflow.upgrade.rules.base_rule import BaseRule
 from airflow.configuration import conf
 
+invalid_config_keys = {
+    "airflow_configmap",
+    "airflow_local_settings_configmap",
+    "dags_in_image",
+    "dags_volume_subpath",
+    "dags_volume_mount_point", "dags_volume_claim",
+    "logs_volume_subpath", "logs_volume_claim",
+    "dags_volume_host", "logs_volume_host",
+    "env_from_configmap_ref", "env_from_secret_ref", "git_repo",
+    "git_branch", "git_sync_depth", "git_subpath",
+    "git_sync_rev", "git_user", "git_password",
+    "git_sync_root", "git_sync_dest",
+    "git_dags_folder_mount_point", "git_ssh_key_secret_name",
+    "git_ssh_known_hosts_configmap_name", "git_sync_credentials_secret",
+    "git_sync_container_repository",
+    "git_sync_container_tag", "git_sync_init_container_name",
+    "git_sync_run_as_user",
+    "worker_service_account_name", "image_pull_secrets",
+    "gcp_service_account_keys", "affinity",
+    "tolerations", "run_as_user", "fs_group"
+}
+
 
 class PodTemplateFileRule(BaseRule):
-
     title = "Users must set a kubernetes.pod_template_file value"
 
     description = """\
 In Airflow 2.0, KubernetesExecutor Users need to set a pod_template_file as a base
-value for all pods launched by the KubernetesExecutor
+value for all pods launched by the KubernetesExecutor. Many Kubernetes configs are no longer
+needed once this pod_template_file has been generated.
 """
 
     def check(self):
@@ -37,3 +59,19 @@ value for all pods launched by the KubernetesExecutor
                 "Please create a pod_template_file by running `airflow generate_pod_template`.\n"
                 "This will generate a pod using your aiflow.cfg settings"
             )
+
+        conf_dict = conf.as_dict(display_sensitive=True)
+        kube_conf = conf_dict['kubernetes']
+        keys = kube_conf.keys()
+        resp = [k for k in keys if k in invalid_config_keys]
+        if conf_dict['kubernetes_labels']:
+            resp.append("kubernetes_labels")
+        if conf_dict['kubernetes_secrets']:
+            resp.append("kubernetes_secrets")
+
+        if resp:
+            resp_string = "\n".join(resp)
+            return "The following invalid keys were found in your airflow.cfg: \
+                   \n\n{resp_string}\n\n \
+                   Now that you have a pod_template_file, these keys no longer do anything.\n\
+                   Please delete these keys.".format(resp_string=resp_string)
