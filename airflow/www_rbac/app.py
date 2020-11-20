apache/spark

Unify user session lifetime configuration (#11970)
* Unify user session lifetime configuration

* align with new linting rules

* exit app when removed args are provided in conf

* add one more test

* extract stopping gunicorn to method

* add docstring to stop_webserver method

* use lazy formatting

* exit webserver when removed options are provided

* align with markdown lint

* Move unify user session lifetime configuration section to master

* add new line

* remove quotes

airflow/www/app.py
@@ -16,7 +16,8 @@
 # specific language governing permissions and limitations
 # under the License.
 #
-
+import logging
+import sys
 from datetime import timedelta
 from typing import Optional
 
@@ -35,7 +36,7 @@ from airflow.www.extensions.init_dagbag import init_dagbag
 from airflow.www.extensions.init_jinja_globals import init_jinja_globals
 from airflow.www.extensions.init_manifest_files import configure_manifest_files
 from airflow.www.extensions.init_security import init_api_experimental_auth, init_xframe_protection
-from airflow.www.extensions.init_session import init_logout_timeout, init_permanent_session
+from airflow.www.extensions.init_session import init_permanent_session
 from airflow.www.extensions.init_views import (
     init_api_connexion,
     init_api_experimental,
@@ -70,8 +71,22 @@ def create_app(config=None, testing=False, app_name="Airflow"):
     flask_app = Flask(__name__)
     flask_app.secret_key = conf.get('webserver', 'SECRET_KEY')
 
-    session_lifetime_days = conf.getint('webserver', 'SESSION_LIFETIME_DAYS', fallback=30)
-    flask_app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=session_lifetime_days)
+    if conf.has_option('webserver', 'SESSION_LIFETIME_DAYS') or conf.has_option(
+        'webserver', 'FORCE_LOG_OUT_AFTER'
+    ):
+        logging.error(
+            '`SESSION_LIFETIME_DAYS` option from `webserver` section has been '
+            'renamed to `SESSION_LIFETIME_MINUTES`. New option allows to configure '
+            'session lifetime in minutes. FORCE_LOG_OUT_AFTER option has been removed '
+            'from `webserver` section. Please update your configuration.'
+        )
+        # Stop gunicorn server https://github.com/benoitc/gunicorn/blob/20.0.4/gunicorn/arbiter.py#L526
+        sys.exit(4)
+    else:
+        session_lifetime_minutes = conf.getint('webserver', 'SESSION_LIFETIME_MINUTES', fallback=43200)
+        logging.info('User session lifetime is set to %s minutes.', session_lifetime_minutes)
+
+    flask_app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=session_lifetime_minutes)
 
     flask_app.config.from_pyfile(settings.WEBSERVER_CONFIG, silent=True)
     flask_app.config['APP_NAME'] = app_name
@@ -124,7 +139,6 @@ def create_app(config=None, testing=False, app_name="Airflow"):
         sync_appbuilder_roles(flask_app)
 
         init_jinja_globals(flask_app)
-        init_logout_timeout(flask_app)
         init_xframe_protection(flask_app)
         init_permanent_session(flask_app)
 

tests/www/test_app.py
@@ -17,6 +17,7 @@
 # under the License.
 
 import unittest
+from datetime import timedelta
 from unittest import mock
 
 import pytest
@@ -24,6 +25,7 @@ from werkzeug.routing import Rule
 from werkzeug.test import create_environ
 from werkzeug.wrappers import Response
 
+from airflow.configuration import conf
 from airflow.www import app as application
 from tests.test_utils.config import conf_vars
 
@@ -221,3 +223,26 @@ class TestApp(unittest.TestCase):
         app = application.cached_app(testing=True)
         engine_params = {'pool_size': 3, 'pool_recycle': 120, 'pool_pre_ping': True, 'max_overflow': 5}
         self.assertEqual(app.config['SQLALCHEMY_ENGINE_OPTIONS'], engine_params)
+
+    @conf_vars(
+        {
+            ('webserver', 'session_lifetime_minutes'): '3600',
+        }
+    )
+    @mock.patch("airflow.www.app.app", None)
+    def test_should_set_permanent_session_timeout(self):
+        app = application.cached_app(testing=True)
+        self.assertEqual(app.config['PERMANENT_SESSION_LIFETIME'], timedelta(minutes=3600))
+
+    @conf_vars(
+        {
+            ('webserver', 'session_lifetime_days'): '30',
+            ('webserver', 'force_log_out_after'): '30',
+        }
+    )
+    @mock.patch("airflow.www.app.app", None)
+    def test_should_stop_app_when_removed_options_are_provided(self):
+        with self.assertRaises(SystemExit) as e:
+            conf.remove_option('webserver', 'session_lifetime_minutes')
+            application.cached_app(testing=True)
+        self.assertEqual(e.exception.code, 4)

apache/airflow

airflow/www_rbac/app.py
@@ -18,6 +18,7 @@
 # under the License.
 #
 import logging
+import sys
 import socket
 from datetime import timedelta
 from typing import Any
@@ -62,8 +63,22 @@ def create_app(config=None, session=None, testing=False, app_name="Airflow"):
         )
     app.secret_key = conf.get('webserver', 'SECRET_KEY')
 
-    session_lifetime_days = conf.getint('webserver', 'SESSION_LIFETIME_DAYS', fallback=30)
-    app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=session_lifetime_days)
+    if conf.has_option('webserver', 'SESSION_LIFETIME_DAYS') or conf.has_option(
+        'webserver', 'FORCE_LOG_OUT_AFTER'
+    ):
+        logging.error(
+            '`SESSION_LIFETIME_DAYS` option from `webserver` section has been '
+            'renamed to `SESSION_LIFETIME_MINUTES`. New option allows to configure '
+            'session lifetime in minutes. FORCE_LOG_OUT_AFTER option has been removed '
+            'from `webserver` section. Please update your configuration.'
+        )
+        # Stop gunicorn server https://github.com/benoitc/gunicorn/blob/20.0.4/gunicorn/arbiter.py#L526
+        # sys.exit(4)
+    else:
+        session_lifetime_minutes = conf.getint('webserver', 'SESSION_LIFETIME_MINUTES', fallback=43200)
+        logging.info('User session lifetime is set to %s minutes.', session_lifetime_minutes)
+
+    app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=session_lifetime_minutes)
 
     app.config.from_pyfile(settings.WEBSERVER_CONFIG, silent=True)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -267,15 +282,6 @@ def create_app(config=None, session=None, testing=False, app_name="Airflow"):
         @app.teardown_appcontext
         def shutdown_session(exception=None):
             settings.Session.remove()
-
-        @app.before_request
-        def before_request():
-            _force_log_out_after = conf.getint('webserver', 'FORCE_LOG_OUT_AFTER', fallback=0)
-            if _force_log_out_after > 0:
-                flask.session.permanent = True
-                app.permanent_session_lifetime = timedelta(minutes=_force_log_out_after)
-                flask.session.modified = True
-                flask.g.user = flask_login.current_user
 
         @app.after_request
         def apply_caching(response):

tests/www_rbac/test_app.py
@@ -18,11 +18,13 @@
 # under the License.
 import json
 import unittest
+from datetime import timedelta
 
 import pytest
 import six
 from werkzeug.middleware.proxy_fix import ProxyFix
 
+from airflow.configuration import conf
 from airflow.settings import Session
 from airflow.www_rbac import app as application
 from tests.compat import mock
@@ -80,3 +82,26 @@ class TestApp(unittest.TestCase):
         if six.PY2:
             engine_params = json.dumps(engine_params)
         self.assertEqual(app.config['SQLALCHEMY_ENGINE_OPTIONS'], engine_params)
+
+    @conf_vars(
+        {
+            ('webserver', 'session_lifetime_minutes'): '3600',
+        }
+    )
+    @mock.patch("airflow.www_rbac.app.app", None)
+    def test_should_set_permanent_session_timeout(self):
+        app, _ = application.cached_appbuilder(testing=True)
+        self.assertEqual(app.config['PERMANENT_SESSION_LIFETIME'], timedelta(minutes=3600))
+
+    @conf_vars(
+        {
+            ('webserver', 'session_lifetime_days'): '30',
+            ('webserver', 'force_log_out_after'): '30',
+        }
+    )
+    @mock.patch("airflow.www_rbac.app.app", None)
+    def test_should_stop_app_when_removed_options_are_provided(self):
+        with self.assertRaises(SystemExit) as e:
+            conf.remove_option('webserver', 'session_lifetime_minutes')
+            application.cached_appbuilder(testing=True)
+        self.assertEqual(e.exception.code, 4)