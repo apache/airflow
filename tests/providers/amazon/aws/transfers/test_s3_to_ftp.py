40b89b70153fadbe07f5cd87c4a4c4d5cada9f13
JavierLopezT <javierlopeztomas@gmail.com>
2020-11-26 14:21:34 +0100

    Update tests/providers/amazon/aws/transfers/test_s3_to_ftp.py
    Co-authored-by: Felix Uellendall <feluelle@users.noreply.github.com>

--- tests/providers/amazon/aws/transfers/test_s3_to_ftp.py
+++ tests/providers/amazon/aws/transfers/test_s3_to_ftp.py
@@ -29,12 +29,15 @@ FTP_CONN_ID = 'ftp_default'
 
 
 class TestS3ToSFTPOperator(unittest.TestCase):
-    @mock.patch("airflow.providers.amazon.aws.hooks.s3.S3Hook")
-    @mock.patch("airflow.providers.ftp.hooks.ftp.FTPHook")
-    def test_execute(self, ftp_hook, s3_hook):
+    @mock.patch("airflow.providers.ftp.hooks.ftp.FTPHook.store_file")
+    @mock.patch("airflow.providers.amazon.aws.hooks.s3.S3Hook.get_key")
+    @mock.patch("airflow.providers.amazon.aws.transfers.s3_to_ftp.NamedTemporaryFile")
+    def test_execute(self, mock_local_tmp_file, mock_s3_hook_get_key, mock_ftp_hook_store_file):
         operator = S3ToFTPOperator(task_id=TASK_ID, s3_bucket=BUCKET, s3_key=S3_KEY, ftp_path=FTP_PATH)
         operator.execute(None)
 
-        s3_hook.assert_called_once_with(AWS_CONN_ID)
+        mock_s3_hook_get_key.assert_called_once_with(operator.s3_key, operator.s3_bucket)
 
-        ftp_hook.assert_called_once_with(FTP_CONN_ID)
+        mock_local_tmp_file_value = mock_local_tmp_file.__enter__.return_value
+        mock_s3_hook_get_key.return_value.download_fileobj.assert_called_once_with(mock_local_tmp_file_value)
+        mock_ftp_hook_store_file.assert_called_once_with(operator.ftp_path, mock_local_tmp_file_value.name)
