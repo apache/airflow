e6f21ecc64b4f4f6b5358ea6180806f8655f3fec
Kamil Breguła <kamil.bregula@polidea.com>
2020-11-28 10:54:48 +0100

    Validate JSON schema files with JSON Schema

--- BREEZE.rst
+++ BREEZE.rst
@@ -2008,15 +2008,15 @@ This is the current syntax for  `./breeze <./breeze>`_:
         Run selected static checks for currently changed files. You should specify static check that
         you would like to run or 'all' to run all checks. One of:
 
-                 all all-but-pylint airflow-config-yaml airflow-providers-available
-                 airflow-provider-yaml-files-ok base-operator bats-tests bats-in-container-tests
-                 black build build-providers-dependencies check-apache-license check-builtin-literals
+                 airflow-config-yaml airflow-provider-yaml-files-ok airflow-providers-available all
+                 all-but-pylint base-operator bats-in-container-tests bats-tests black build
+                 build-providers-dependencies check-apache-license check-builtin-literals
                  check-executables-have-shebangs check-hooks-apply check-integrations
                  check-merge-conflict check-xml consistent-pylint daysago-import-check
                  debug-statements detect-private-key doctoc dont-use-safe-filter end-of-file-fixer
                  fix-encoding-pragma flake8 forbid-tabs helm-lint incorrect-use-of-LoggingMixin
-                 insert-license isort language-matters lint-dockerfile lint-openapi markdownlint
-                 mermaid mixed-line-ending mypy mypy-helm no-providers-in-core-examples
+                 insert-license isort json-schema language-matters lint-dockerfile lint-openapi
+                 markdownlint mermaid mixed-line-ending mypy mypy-helm no-providers-in-core-examples
                  no-relative-imports pre-commit-descriptions provide-create-sessions
                  providers-init-file provider-yamls pydevd pydocstyle pylint pylint-tests
                  python-no-log-warn pyupgrade restrict-start_date rst-backticks setup-order
--- scripts/ci/pre_commit/pre_commit_json_schema.py
+++ scripts/ci/pre_commit/pre_commit_json_schema.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python3
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 import argparse
 import hashlib
 import json
 import os
 import re
 import sys
-import tempfile
+from typing import Dict, List, Optional
 
-import jsonschema
 import requests
-from typing import Dict
-
 import yaml
 from jsonschema.validators import validator_for
 
@@ -20,18 +34,13 @@ if __name__ != "__main__":
         "To run this script, run the ./build_docs.py command"
     )
 
+AIRFLOW_SOURCES_DIR = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, os.pardir)
 
-def _user_cache_dir(appname=None):
+
+def _cache_dir():
     """Return full path to the user-specific cache dir for this application"""
-    if sys.platform == "win32":
-        # Windows has a complex procedure to download the App Dir directory because this directory can be
-        # changed in window registry, so we use temporary directory for cache
-        path = os.path.join(tempfile.gettempdir(), appname)
-    elif sys.platform == 'darwin':
-        path = os.path.expanduser('~/Library/Caches')
-    else:
-        path = os.getenv('XDG_CACHE_HOME', os.path.expanduser('~/.cache'))
-    path = os.path.join(path, appname)
+    path = os.path.join(AIRFLOW_SOURCES_DIR, ".build", "cache")
+    os.makedirs(path, exist_ok=True)
     return path
 
 
@@ -43,7 +52,7 @@ def _gethash(string: str):
 def fetch_and_cache(url: str, output_filename: str):
     """Fetch URL to local cache and returns path."""
     cache_key = _gethash(url)
-    cache_dir = _user_cache_dir("pre-commmit-json-schema")
+    cache_dir = _cache_dir()
     cache_metadata_filepath = os.path.join(cache_dir, "cache-metadata.json")
     cache_filepath = os.path.join(cache_dir, f"{cache_key}-{output_filename}")
     # Create cache directory
@@ -81,27 +90,22 @@ def fetch_and_cache(url: str, output_filename: str):
     return cache_filepath
 
 
-class ValidatorError(Exception):
+class _ValidatorError(Exception):
     pass
 
 
-def load_file(file_path):
+def load_file(file_path: str):
+    """Loads a file using a serializer which guesses based on the file extension"""
     if file_path.lower().endswith('.json'):
         with open(file_path) as input_file:
             return json.load(input_file)
     elif file_path.lower().endswith('.yaml') or file_path.lower().endswith('.yml'):
         with open(file_path) as input_file:
             return yaml.load(input_file)
-    raise ValidatorError("Unknown file format. Supported extension: '.yaml', '.json'")
+    raise _ValidatorError("Unknown file format. Supported extension: '.yaml', '.json'")
 
 
-def validate_spec(validator, instance):
-    error = jsonschema.exceptions.best_match(validator.iter_errors(instance))
-    if error is not None:
-        raise ValidatorError(str(error))
-
-
-def get_parser():
+def _get_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(description='Validates the file using JSON Schema specifications')
 
     group = parser.add_mutually_exclusive_group(required=True)
@@ -109,35 +113,33 @@ def get_parser():
     group.add_argument('--spec-url', help="The URL to specification")
 
     parser.add_argument('file', nargs='+')
+
     return parser
 
 
-def process_files(validator, file_paths):
+def _process_files(validator, file_paths: List[str]):
     exit_code = 0
     for input_path in file_paths:
         print("Processing file: ", input_path)
         try:
             instance = load_file(input_path)
-            validate_spec(validator, instance)
-        except ValidatorError as ex:
+            validator.is_valid(instance)
+        except _ValidatorError as ex:
             print('Problem processing the file.')
             print(ex)
             exit_code = 1
     return exit_code
 
 
-def create_validator(schema):
+def _create_validator(schema):
     cls = validator_for(schema)
     cls.check_schema(schema)
     return cls(schema)
 
 
-def load_spec(spec_file, spec_url):
+def _load_spec(spec_file: Optional[str], spec_url: Optional[str]):
     if spec_url:
-        spec_file = fetch_and_cache(
-            url=spec_url,
-            output_filename=re.sub(r"[^a-zA-Z0-9]", "-", spec_url)
-        )
+        spec_file = fetch_and_cache(url=spec_url, output_filename=re.sub(r"[^a-zA-Z0-9]", "-", spec_url))
     else:
         spec_file = spec_file
     with open(spec_file) as schema_file:
@@ -146,17 +148,18 @@ def load_spec(spec_file, spec_url):
 
 
 def main() -> int:
-    parser = get_parser()
+    """Main code"""
+    parser = _get_parser()
     args = parser.parse_args()
     spec_url = args.spec_url
     spec_file = args.spec_file
 
-    schema = load_spec(spec_file, spec_url)
+    schema = _load_spec(spec_file, spec_url)
 
-    validator = create_validator(schema)
+    validator = _create_validator(schema)
 
     file_paths = args.file
-    exit_code = process_files(validator, file_paths)
+    exit_code = _process_files(validator, file_paths)
 
     return exit_code
 
