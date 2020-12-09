31f9fb3e3aaabafa2daba936ca42a26aafe957f3
Ash Berlin-Taylor <ash_github@firemirror.com>
2020-12-09 13:32:33 +0000

    fixup! Rename airflow.operators.dagrun_operator to airflow.operators.trigger_dagrun

--- airflow/serialization/serialized_objects.py
+++ airflow/serialization/serialized_objects.py
@@ -65,13 +65,8 @@ if TYPE_CHECKING:
 log = logging.getLogger(__name__)
 
 _OPERATOR_EXTRA_LINKS: Set[str] = {
-<<<<<<< HEAD
-    "airflow.operators.dagrun_operator.TriggerDagRunLink",
-    "airflow.sensors.external_task.ExternalTaskSensorLink",
-=======
     "airflow.operators.trigger_dagrun.TriggerDagRunLink",
-    "airflow.sensors.external_task_sensor.ExternalTaskSensorLink",
->>>>>>> 33ea01611... Rename airflow.operators.dagrun_operator to airflow.operators.trigger_dagrun
+    "airflow.sensors.external_task.ExternalTaskSensorLink",
 }
 
 
