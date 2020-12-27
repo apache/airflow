204f58bf9533d157d5e4e165ffbeb004b1442667
Kanthi Subramanian <subkanthi@gmail.com>
2020-12-27 12:54:50 -0500

    Fixed pylint error with initializing self.connection

--- tests/providers/neo4j/hooks/test_neo4j.py
+++ tests/providers/neo4j/hooks/test_neo4j.py
@@ -15,36 +15,27 @@
 # specific language governing permissions and limitations
 # under the License.
 #
-
 import json
-import os
 import unittest
-import uuid
 from unittest import mock
 
-import pytest
-from parameterized import parameterized
-
 from airflow.models import Connection
-from airflow.models.dag import DAG
 from airflow.providers.neo4j.hooks.neo4j import Neo4jHook
-from airflow.utils import timezone
-
 
 class TestNeo4jHookConn(unittest.TestCase):
     def setUp(self):
         super().setUp()
         self.neo4j_hook = Neo4jHook()
-
-    def test_get_uri_neo4j_scheme(self):
         self.connection = Connection(
             conn_type='neo4j',
             login='login',
             password='password',
             host='host',
-            schema='schema',
-            extra=json.dumps({"tenant": "tenant", "account_name": "accountname"}),
+            schema='schema'
         )
+
+    def test_get_uri_neo4j_scheme(self):
+
         self.neo4j_hook.get_connection = mock.Mock()
         self.neo4j_hook.get_connection.return_value = self.connection
         uri = self.neo4j_hook.get_uri(self.connection)
@@ -52,14 +43,8 @@ class TestNeo4jHookConn(unittest.TestCase):
         self.assertEqual(uri, "neo4j://host:7687")
 
     def test_get_uri_bolt_scheme(self):
-        self.connection = Connection(
-            conn_type='neo4j',
-            login='login',
-            password='password',
-            host='host',
-            schema='schema',
-            extra=json.dumps({"bolt_scheme": True}),
-        )
+
+        self.connection.extra = json.dumps({"bolt_scheme": True})
         self.neo4j_hook.get_connection = mock.Mock()
         self.neo4j_hook.get_connection.return_value = self.connection
         uri = self.neo4j_hook.get_uri(self.connection)
@@ -67,14 +52,7 @@ class TestNeo4jHookConn(unittest.TestCase):
         self.assertEqual(uri, "bolt://host:7687")
 
     def test_get_uri_bolt_ssc_scheme(self):
-        self.connection = Connection(
-            conn_type='neo4j',
-            login='login',
-            password='password',
-            host='host',
-            schema='schema',
-            extra=json.dumps({"certs_self_signed": True, "bolt_scheme": True}),
-        )
+        self.connection.extra = json.dumps({"certs_self_signed": True, "bolt_scheme": True})
         self.neo4j_hook.get_connection = mock.Mock()
         self.neo4j_hook.get_connection.return_value = self.connection
         uri = self.neo4j_hook.get_uri(self.connection)
@@ -82,14 +60,7 @@ class TestNeo4jHookConn(unittest.TestCase):
         self.assertEqual(uri, "bolt+ssc://host:7687")
 
     def test_get_uri_bolt_trusted_ca_scheme(self):
-        self.connection = Connection(
-            conn_type='neo4j',
-            login='login',
-            password='password',
-            host='host',
-            schema='schema',
-            extra=json.dumps({"certs_trusted_ca": True, "bolt_scheme": True}),
-        )
+        self.connection.extra = json.dumps({"certs_trusted_ca": True, "bolt_scheme": True})
         self.neo4j_hook.get_connection = mock.Mock()
         self.neo4j_hook.get_connection.return_value = self.connection
         uri = self.neo4j_hook.get_uri(self.connection)
