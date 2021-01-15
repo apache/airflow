da2cc182be23579a37474c7f505db726dd03383c
javier.lopez <javier.lopez@promocionesfarma.com>
2021-01-16 00:40:31 +0100

    changes

--- tests/providers/amazon/aws/transfers/test_s3_to_ftp.py
+++ tests/providers/amazon/aws/transfers/test_s3_to_ftp.py
@@ -28,12 +28,12 @@ AWS_CONN_ID = 'aws_default'
 FTP_CONN_ID = 'ftp_default'
 
 
-class TestFTPToS3Operator(unittest.TestCase):
+class TestS3ToFTPOperator(unittest.TestCase):
     @mock.patch("airflow.providers.ftp.hooks.ftp.FTPHook.store_file")
     @mock.patch("airflow.providers.amazon.aws.hooks.s3.S3Hook.get_key")
     @mock.patch("airflow.providers.amazon.aws.transfers.s3_to_ftp.NamedTemporaryFile")
     def test_execute(self, mock_local_tmp_file, mock_s3_hook_get_key, mock_ftp_hook_store_file):
-        operator = FTPToS3Operator(task_id=TASK_ID, s3_bucket=BUCKET, s3_key=S3_KEY, ftp_path=FTP_PATH)
+        operator = S3ToFTPperator(task_id=TASK_ID, s3_bucket=BUCKET, s3_key=S3_KEY, ftp_path=FTP_PATH)
         operator.execute(None)
 
         mock_s3_hook_get_key.assert_called_once_with(operator.s3_key, operator.s3_bucket)
