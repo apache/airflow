763aa332a81da3fcbe224635806d9fb250679d8e
javier.lopez <javier.lopez@promocionesfarma.com>
2021-01-16 00:38:51 +0100

    pylint and static

--- airflow/providers/amazon/aws/transfers/ftp_to_s3.py
+++ airflow/providers/amazon/aws/transfers/ftp_to_s3.py
@@ -1,3 +1,20 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
 from tempfile import NamedTemporaryFile
 
 from airflow.models import BaseOperator
@@ -36,21 +53,27 @@ class FTPToS3Operator(BaseOperator):
         uploaded to the S3 bucket.
     :type acl_policy: str
     """
-    template_fields = ('s3_bucket', 's3_key', 'ftp_path',)
+    template_fields = (
+        's3_bucket',
+        's3_key',
+        'ftp_path',
+    )
 
     @apply_defaults
-    def __init__(self,
-                 s3_bucket,
-                 s3_key,
-                 ftp_path,
-                 aws_conn_id='aws_default',
-                 ftp_conn_id='ftp_default',
-                 replace=False,
-                 encrypt=False,
-                 gzip=False,
-                 acl_policy=None,
-                 *args,
-                 **kwargs):
+    def __init__(
+        self,
+        s3_bucket,
+        s3_key,
+        ftp_path,
+        aws_conn_id='aws_default',
+        ftp_conn_id='ftp_default',
+        replace=False,
+        encrypt=False,
+        gzip=False,
+        acl_policy=None,
+        *args,
+        **kwargs
+    ):
         super().__init__(*args, **kwargs)
         self.s3_bucket = s3_bucket
         self.s3_key = s3_key
@@ -63,17 +86,21 @@ class FTPToS3Operator(BaseOperator):
         self.acl_policy = acl_policy
 
     def execute(self, context):
-        s3 = S3Hook(self.aws_conn_id)
+        s3_hook = S3Hook(self.aws_conn_id)
         ftp_hook = FTPHook(ftp_conn_id=self.ftp_conn_id)
 
         with NamedTemporaryFile() as local_tmp_file:
-            ftp_hook.retrieve_file(remote_full_path=self.ftp_path,
-                                   local_full_path_or_buffer=local_tmp_file.name)
+            ftp_hook.retrieve_file(
+                remote_full_path=self.ftp_path,
+                local_full_path_or_buffer=local_tmp_file.name
+            )
 
-            s3.load_file(filename=local_tmp_file.name,
-                         key=self.s3_key,
-                         bucket_name=self.s3_bucket,
-                         replace=self.replace,
-                         encrypt=self.encrypt,
-                         gzip=self.gzip,
-                         acl_policy=self.acl_policy)
+            s3_hook.load_file(
+                filename=local_tmp_file.name,
+                key=self.s3_key,
+                bucket_name=self.s3_bucket,
+                replace=self.replace,
+                encrypt=self.encrypt,
+                gzip=self.gzip,
+                acl_policy=self.acl_policy
+            )
