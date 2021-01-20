5aff1102da6588c90c094b357472ad5cf02c96cd
Dennis Akpenyi <dennisakpenyi@gmail.com>
2021-01-20 01:05:35 +0100

    Created rule for SparkJDBCOperator class conn_id
    This PR implements an upgrade check for SparkJDBCOperator.
    
    In Airflow 1.10.x, SparkJDBCOperator class conn_id is 'spark-default'.
    In Airflow 2.0, it has been changed to 'spark_default' to conform with
    the naming conventions of all other connection names.

---
+++ tests/upgrade/rules/test_spark_operator_default_conn_id_check.py
@@ -0,0 +1,60 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from unittest import TestCase
+
+from airflow.models import Connection
+from airflow.upgrade.rules.spark_operator_default_conn_id_check import SparkOperatorDefaultConnIdRule
+from airflow.utils.db import create_session
+from tests.test_utils.db import clear_db_connections
+
+
+class TestSparkOperatorDefaultConnIdRule(TestCase):
+    def tearDown(self):
+        clear_db_connections()
+
+    def test_check(self):
+        rule = SparkOperatorDefaultConnIdRule()
+
+        assert isinstance(rule.title, str)
+        assert isinstance(rule.description, str)
+
+        with create_session() as session:
+            conn = Connection(conn_id='spark_default')
+            session.merge(conn)
+
+        msgs = rule.check(session=session)
+        assert msgs is None
+
+    def test_invalid_check(self):
+        rule = SparkOperatorDefaultConnIdRule()
+
+        assert isinstance(rule.title, str)
+        assert isinstance(rule.description, str)
+
+        with create_session() as session:
+            conn = Connection(conn_id='spark-default')
+            session.merge(conn)
+
+        expected = (
+            "DeprecationWarning: "
+            "In Airflow 2.0, SparkJDBCOperator class 'conn_id' value has been changed "
+            "to 'spark_default' to conform with the naming conventions of all other connection names."
+        )
+
+        msgs = rule.check(session=session)
+        assert msgs == expected
