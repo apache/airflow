5cc245b5d3c540023cc312731073e8a5875a4b07
javier.lopez <javier.lopez@promocionesfarma.com>
2021-01-20 20:20:20 +0100

    Fix S3ToFTPOperator

--- airflow/providers/amazon/aws/transfers/s3_to_ftp.py
+++ airflow/providers/amazon/aws/transfers/s3_to_ftp.py
@@ -26,12 +26,11 @@ from airflow.providers.ftp.hooks.ftp import FTPHook
 class S3ToFTPOperator(BaseOperator):
     """
     This operator enables the transferring of files from S3 to a FTP server.
-
     :param ftp_conn_id: The ftp connection id. The name or identifier for
         establishing a connection to the FTP server.
     :type ftp_conn_id: str
-    :param ftp_path: The ftp remote path. This is the specified file path for
-        uploading file to the FTP server.
+    :param ftp_path: The ftp remote path in where the file will be stored.
+        The desired filename bust be specified here.
     :type ftp_path: str
     :param s3_bucket: The targeted s3 bucket. This is the S3 bucket from
         where the file is downloaded.
@@ -41,17 +40,17 @@ class S3ToFTPOperator(BaseOperator):
     :type s3_key: str
     """
 
-    template_fields = ('s3_bucket', 's3_key')
+    template_fields = ('s3_bucket', 's3_key', 'ftp_path')
 
     def __init__(
-        self,
-        *,
-        s3_bucket,
-        s3_key,
-        ftp_path,
-        aws_conn_id='aws_default',
-        ftp_conn_id='ftp_default',
-        **kwargs,
+            self,
+            *,
+            s3_bucket,
+            s3_key,
+            ftp_path,
+            aws_conn_id='aws_default',
+            ftp_conn_id='ftp_default',
+            **kwargs,
     ) -> None:
         super().__init__(**kwargs)
         self.s3_bucket = s3_bucket
@@ -64,8 +63,8 @@ class S3ToFTPOperator(BaseOperator):
         s3_hook = S3Hook(self.aws_conn_id)
         ftp_hook = FTPHook(ftp_conn_id=self.ftp_conn_id)
 
-        s3_obj = s3_hook.get_key(self.s3_key, self.s3_bucket)
+        s3_client = s3_hook.get_conn()
 
         with NamedTemporaryFile() as local_tmp_file:
-            s3_obj.download_fileobj(local_tmp_file)
+            s3_client.download_file(self.s3_bucket, self.s3_key, local_tmp_file.name)
             ftp_hook.store_file(self.ftp_path, local_tmp_file.name)
