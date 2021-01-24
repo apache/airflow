dd6730f0b1a1a0f4b91376597161f530d872bcb0
Vivek Bhojawala <vbhojawala@gmail.com>
2021-01-24 10:46:55 +0530

    Added test cases for copy connection

--- tests/www/test_views.py
+++ tests/www/test_views.py
@@ -238,6 +238,28 @@ class TestConnectionModelView(TestBase):
             'password': 'admin',
         }
 
+        conn1 = Connection(
+            conn_id='aws_mongo',
+            conn_type='FTP',
+            description='MongoDB',
+            host='192.168.55.11',
+            schema='airflow',
+            port='5566',
+        )
+
+        # Already copied and modified
+        conn2 = Connection(
+            conn_id='aws_mongo__1',
+            conn_type='FTP',
+            description='MongoDB2',
+            host='192.168.55.12',
+            schema='airflow',
+            port='5567',
+        )
+
+        self.session.add_all([conn1, conn2])
+        self.session.commit()
+
     def tearDown(self):
         self.clear_table(Connection)
         super().tearDown()
@@ -252,6 +274,19 @@ class TestConnectionModelView(TestBase):
 
         cmv = ConnectionModelView()
         cmv.prefill_form(form=mock_form, pk=1)
+
+    def test_copy_connection(self):
+        """ Test copy multiple connection with suffix(already copied )"""
+
+        mock_form = mock.Mock()
+        mock_form.data = {"action": "mulcopy", "rowid": [1, 2]}
+        resp = self.client.post('/connection/action_post', data=mock_form.data, follow_redirects=True)
+
+        expected_result = {'aws_mongo', 'aws_mongo__1', 'aws_mongo__2', 'aws_mongo__3'}
+        response = {conn[0] for conn in self.session.query(Connection.conn_id).all()}
+
+        assert resp.status_code == 200
+        assert expected_result == response
 
 
 class TestVariableModelView(TestBase):
