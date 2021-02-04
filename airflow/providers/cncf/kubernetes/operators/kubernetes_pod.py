f6bb5ec7132d604833c3705e14c03e56bb4ff192
Daniel Imberman <daniel.imberman@gmail.com>
2021-02-04 10:51:24 -0800

    Allow users of the KPO to template environment variables
    Addresses https://github.com/apache/airflow/issues/13348
    
    Adds a "templated_fields" variable to k8s.V1EnvVar so jinja templating
    will work when passed by users``

--- airflow/providers/cncf/kubernetes/operators/kubernetes_pod.py
+++ airflow/providers/cncf/kubernetes/operators/kubernetes_pod.py
@@ -228,6 +228,8 @@ class KubernetesPodOperator(BaseOperator):  # pylint: disable=too-many-instance-
         self.env_vars = convert_env_vars(env_vars) if env_vars else []
         if pod_runtime_info_envs:
             self.env_vars.extend([convert_pod_runtime_info_env(p) for p in pod_runtime_info_envs])
+        env_vars = self.add_template_fields_to_env_vars(env_vars)
+        self.env_vars = env_vars
         self.env_from = env_from or []
         if configmaps:
             self.env_from.extend([convert_configmap(c) for c in configmaps])
@@ -270,6 +272,22 @@ class KubernetesPodOperator(BaseOperator):  # pylint: disable=too-many-instance-
         self.termination_grace_period = termination_grace_period
         self.client: CoreV1Api = None
         self.pod: k8s.V1Pod = None
+
+    def add_template_fields_to_env_vars(self, env_vars):
+        """
+        Adds the field `templated_fields` to V1EnvVar so that Airflow can apply jinja templating
+        to both the name and value of an environment variable.
+
+        Addresses https://github.com/apache/airflow/issues/13348
+        @param env_vars:
+        @return:
+        """
+        env_vars = []
+        for env_var in self.env_vars:
+            if not hasattr(env_var, 'template_fields'):
+                env_var.template_fields = ('value', 'name')
+                env_vars.append(env_var)
+        return env_vars
 
     @staticmethod
     def create_labels_for_pod(context) -> dict:
