3c8b3ecfd4f7bbdba625af3c4305dcde82ec35c7
Arati Nagmal <nagmalarati6@gmail.com>
2021-02-08 20:57:49 +0530

    Update error message
    Co-authored-by: Kaxil Naik <kaxilnaik@gmail.com>

--- airflow/providers/amazon/aws/transfers/redshift_to_s3.py
+++ airflow/providers/amazon/aws/transfers/redshift_to_s3.py
@@ -109,7 +109,7 @@ class RedshiftToS3Operator(BaseOperator):
         elif self.schema and self.table:
             self._select_query = f"SELECT * FROM {self.schema}.{self.table}"
         else:
-            raise ValueError('Please provide both `schema` and `table` params to fetch the data.')
+            raise ValueError('Please provide both `schema` and `table` params or `select_query` to fetch the data.')
 
         if self.include_header and 'HEADER' not in [uo.upper().strip() for uo in self.unload_options]:
             self.unload_options = list(self.unload_options) + [
