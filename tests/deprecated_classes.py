95286ac16d604690e53dd69b9b364993a6d6d7b3
morrme <morrme@users.noreply.github.com>
2021-02-10 11:55:12 -0600

    correct class name and add to deprecated classes

--- tests/deprecated_classes.py
+++ tests/deprecated_classes.py
@@ -1416,8 +1416,12 @@ SENSORS = [
         "airflow.contrib.sensors.gcs_sensor.GoogleCloudStorageObjectUpdatedSensor",
     ),
     (
-        "airflow.providers.google.cloud.sensors.gcs.GCSObjectsWtihPrefixExistenceSensor",
+        "airflow.providers.google.cloud.sensors.gcs.GCSObjectsWithPrefixExistenceSensor",
         "airflow.contrib.sensors.gcs_sensor.GoogleCloudStoragePrefixSensor",
+    ),
+    (
+        "airflow.providers.google.cloud.sensors.gcs.GCSObjectsWithPrefixExistenceSensor",
+        "airflow.providers.google.cloud.sensors.gcs.GCSObjectsWtihPrefixExistenceSensor",
     ),
     (
         "airflow.providers.google.cloud.sensors.gcs.GCSUploadSessionCompleteSensor",
