ab59eaadc078d0a02480dea57e06b4d2a731bd9c
morrme <morrme@users.noreply.github.com>
2021-02-10 16:12:59 -0600

    correct deprecated_classes tuple

--- tests/deprecated_classes.py
+++ tests/deprecated_classes.py
@@ -1420,8 +1420,8 @@ SENSORS = [
         "airflow.contrib.sensors.gcs_sensor.GoogleCloudStoragePrefixSensor",
     ),
     (
-        "airflow.providers.google.cloud.sensors.gcs.GCSObjectsWithPrefixExistenceSensor",
         "airflow.providers.google.cloud.sensors.gcs.GCSObjectsWtihPrefixExistenceSensor",
+        "airflow.providers.google.cloud.sensors.gcs.GCSObjectsWithPrefixExistenceSensor",
     ),
     (
         "airflow.providers.google.cloud.sensors.gcs.GCSUploadSessionCompleteSensor",
