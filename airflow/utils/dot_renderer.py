cd33225c146254d0d68563c341f66629e3162fc1
Qian Yu <yuqian1990@gmail.com>
2021-02-25 13:28:04 +0800

    Refactor into functions

--- airflow/utils/dot_renderer.py
+++ airflow/utils/dot_renderer.py
@@ -26,6 +26,7 @@ from airflow.models.baseoperator import BaseOperator
 from airflow.models.dag import DAG
 from airflow.models.taskmixin import TaskMixin
 from airflow.utils.state import State
+from airflow.utils.task_group import TaskGroup
 from airflow.www.views import dag_edges
 
 
@@ -45,67 +46,75 @@ def _refine_color(color: str):
     return color
 
 
+def _draw_task(task: BaseOperator, parent_graph: graphviz.Digraph, states_by_task_id: Dict[str, str]) -> None:
+    """Draw a single task on the given parent_graph"""
+    if states_by_task_id:
+        state = states_by_task_id.get(task.task_id, State.NONE)
+        color = State.color_fg(state)
+        fill_color = State.color(state)
+    else:
+        color = task.ui_fgcolor
+        fill_color = task.ui_color
+
+    parent_graph.node(
+        task.task_id,
+        _attributes={
+            "label": task.label,
+            "shape": "rectangle",
+            "style": "filled,rounded",
+            "color": _refine_color(color),
+            "fillcolor": _refine_color(fill_color),
+        },
+    )
+
+
+def _draw_task_group(
+    task_group: TaskGroup, parent_graph: graphviz.Digraph, states_by_task_id: Dict[str, str]
+) -> None:
+    """Draw the given task_group and its children on the given parent_graph"""
+    # Draw joins
+    if task_group.upstream_group_ids or task_group.upstream_task_ids:
+        parent_graph.node(
+            task_group.upstream_join_id,
+            _attributes={
+                "label": "",
+                "shape": "circle",
+                "style": "filled,rounded",
+                "color": _refine_color(task_group.ui_fgcolor),
+                "fillcolor": _refine_color(task_group.ui_color),
+                "width": "0.2",
+                "height": "0.2",
+            },
+        )
+
+    if task_group.downstream_group_ids or task_group.downstream_task_ids:
+        parent_graph.node(
+            task_group.downstream_join_id,
+            _attributes={
+                "label": "",
+                "shape": "circle",
+                "style": "filled,rounded",
+                "color": _refine_color(task_group.ui_fgcolor),
+                "fillcolor": _refine_color(task_group.ui_color),
+                "width": "0.2",
+                "height": "0.2",
+            },
+        )
+
+    # Draw children
+    for child in sorted(task_group.children.values(), key=lambda t: t.label):
+        _draw_nodes(child, parent_graph, states_by_task_id)
+
+
 def _draw_nodes(node: TaskMixin, parent_graph: graphviz.Digraph, states_by_task_id: Dict[str, str]) -> None:
     """Draw the node and its children on the given parent_graph recursively."""
     if isinstance(node, BaseOperator):
-        # Draw task
-        if states_by_task_id:
-            state = states_by_task_id.get(node.task_id, State.NONE)
-            color = State.color_fg(state)
-            fill_color = State.color(state)
-        else:
-            color = node.ui_fgcolor
-            fill_color = node.ui_color
-
-        parent_graph.node(
-            node.task_id,
-            _attributes={
-                "label": node.label,
-                "shape": "rectangle",
-                "style": "filled,rounded",
-                "color": _refine_color(color),
-                "fillcolor": _refine_color(fill_color),
-            },
-        )
+        _draw_task(node, parent_graph, states_by_task_id)
     else:
-
-        def draw_task_group(parent):
-            # Draw joins
-            if node.upstream_group_ids or node.upstream_task_ids:
-                parent.node(
-                    node.upstream_join_id,
-                    _attributes={
-                        "label": "",
-                        "shape": "circle",
-                        "style": "filled,rounded",
-                        "color": _refine_color(node.ui_fgcolor),
-                        "fillcolor": _refine_color(node.ui_color),
-                        "width": "0.2",
-                        "height": "0.2",
-                    },
-                )
-
-            if node.downstream_group_ids or node.downstream_task_ids:
-                parent.node(
-                    node.downstream_join_id,
-                    _attributes={
-                        "label": "",
-                        "shape": "circle",
-                        "style": "filled,rounded",
-                        "color": _refine_color(node.ui_fgcolor),
-                        "fillcolor": _refine_color(node.ui_color),
-                        "width": "0.2",
-                        "height": "0.2",
-                    },
-                )
-
-            for child in sorted(node.children.values(), key=lambda t: t.label):
-                _draw_nodes(child, parent, states_by_task_id)
-
         # Draw TaskGroup
         if node.is_root:
             # No need to draw background for root TaskGroup.
-            draw_task_group(parent_graph)
+            _draw_task_group(node, parent_graph, states_by_task_id)
         else:
             with parent_graph.subgraph(name=f"cluster_{node.group_id}") as sub:
                 sub.attr(
@@ -116,7 +125,7 @@ def _draw_nodes(node: TaskMixin, parent_graph: graphviz.Digraph, states_by_task_
                     fillcolor="#6495ed7f",
                     label=node.label,
                 )
-                draw_task_group(sub)
+                _draw_task_group(node, sub, states_by_task_id)
 
 
 def render_dag(dag: DAG, tis: Optional[List[TaskInstance]] = None) -> graphviz.Digraph:
