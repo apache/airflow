apache/airflow

commit 3fc89bd67ff469940d97d182ce09dabef5634516

[AIRFLOW-1467] Allow tasks to use more than one pool slot (#7160)

airflow/models/taskinstance.py
@@ -152,6 +152,7 @@ class TaskInstance(Base, LoggingMixin):
     unixname = Column(String(1000))
     job_id = Column(Integer)
     pool = Column(String(50), nullable=False)
+    pool_slots = Column(Integer, default=1)
     queue = Column(String(256))
     priority_weight = Column(Integer)
     operator = Column(String(1000))
@@ -192,6 +193,7 @@ class TaskInstance(Base, LoggingMixin):
 
         self.queue = task.queue
         self.pool = task.pool
+        self.pool_slots = task.pool_slots
         self.priority_weight = task.priority_weight_total
         self.try_number = 0
         self.max_tries = self.task.retries
@@ -486,6 +488,7 @@ class TaskInstance(Base, LoggingMixin):
             self.unixname = ti.unixname
             self.job_id = ti.job_id
             self.pool = ti.pool
+            self.pool_slots = ti.pool_slots
             self.queue = ti.queue
             self.priority_weight = ti.priority_weight
             self.operator = ti.operator
@@ -793,6 +796,7 @@ class TaskInstance(Base, LoggingMixin):
         """
         task = self.task
         self.pool = pool or task.pool
+        self.pool_slots = task.pool_slots
         self.test_mode = test_mode
         self.refresh_from_db(session=session, lock_for_update=True)
         self.job_id = job_id
@@ -908,6 +912,7 @@ class TaskInstance(Base, LoggingMixin):
 
         task = self.task
         self.pool = pool or task.pool
+        self.pool_slots = task.pool_slots
         self.test_mode = test_mode
         self.refresh_from_db(session=session)
         self.job_id = job_id

airflow/migrations/versions/a4c2fd67d16b_add_pool_slots_field_to_task_instance.py
@@ -1,0 +1,42 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+"""add pool_slots field to task_instance
+
+Revision ID: a4c2fd67d16b
+Revises: 7939bcff74ba
+Create Date: 2020-01-14 03:35:01.161519
+
+"""
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = 'a4c2fd67d16b'
+down_revision = '7939bcff74ba'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column('task_instance', sa.Column('pool_slots', sa.Integer, default=1))
+
+
+def downgrade():
+    op.drop_column('task_instance', 'pool_slots')

airflow/models/baseoperator.py
@@ -172,6 +172,9 @@ class BaseOperator(LoggingMixin):
     :param pool: the slot pool this task should run in, slot pools are a
         way to limit concurrency for certain tasks
     :type pool: str
+    :param pool_slots: the number of pool slots this task should use (>= 1)
+        Values less than 1 are not allowed.
+    :type pool_slots: int
     :param sla: time by which the job is expected to succeed. Note that
         this represents the ``timedelta`` after the period is closed. For
         example if you set an SLA of 1 hour, the scheduler would send an email
@@ -306,6 +309,7 @@ class BaseOperator(LoggingMixin):
         weight_rule=WeightRule.DOWNSTREAM,  # type: str
         queue=conf.get('celery', 'default_queue'),  # type: str
         pool=Pool.DEFAULT_POOL_NAME,  # type: str
+        pool_slots=1,  # type: int
         sla=None,  # type: Optional[timedelta]
         execution_timeout=None,  # type: Optional[timedelta]
         on_failure_callback=None,  # type: Optional[Callable]
@@ -375,6 +379,10 @@ class BaseOperator(LoggingMixin):
         self.retries = retries
         self.queue = queue
         self.pool = pool
+        self.pool_slots = pool_slots
+        if self.pool_slots < 1:
+            raise AirflowException("pool slots for %s in dag %s cannot be less than 1"
+                                   % (self.task_id, self.dag_id))
         self.sla = sla
         self.execution_timeout = execution_timeout
         self.on_failure_callback = on_failure_callback

airflow/models/pool.py
@@ -65,11 +65,11 @@ class Pool(Base):
         from airflow.models.taskinstance import TaskInstance  # Avoid circular import
         return (
             session
-            .query(func.count())
+            .query(func.sum(TaskInstance.pool_slots))
             .filter(TaskInstance.pool == self.pool)
             .filter(TaskInstance.state.in_(STATES_TO_COUNT_AS_RUNNING))
             .scalar()
-        )
+        ) or 0
 
     @provide_session
     def used_slots(self, session):
@@ -80,11 +80,11 @@ class Pool(Base):
 
         running = (
             session
-            .query(func.count())
+            .query(func.sum(TaskInstance.pool_slots))
             .filter(TaskInstance.pool == self.pool)
             .filter(TaskInstance.state == State.RUNNING)
             .scalar()
-        )
+        ) or 0
         return running
 
     @provide_session
@@ -96,11 +96,11 @@ class Pool(Base):
 
         return (
             session
-            .query(func.count())
+            .query(func.sum(TaskInstance.pool_slots))
             .filter(TaskInstance.pool == self.pool)
             .filter(TaskInstance.state == State.QUEUED)
             .scalar()
-        )
+        ) or 0
 
     @provide_session
     def open_slots(self, session):

airflow/ti_deps/deps/pool_slots_available_dep.py
@@ -62,12 +62,13 @@ class PoolSlotsAvailableDep(BaseTIDep):
             open_slots = pools[0].open_slots()
 
         if ti.state in STATES_TO_COUNT_AS_RUNNING:
-            open_slots += 1
+            open_slots += ti.pool_slots
 
-        if open_slots <= 0:
+        if open_slots <= (ti.pool_slots - 1):
             yield self._failing_status(
-                reason=("Not scheduling since there are %s open slots in pool %s",
-                        open_slots, pool_name)
+                reason=("Not scheduling since there are %s open slots in pool %s "
+                        "and require %s pool slots",
+                        open_slots, pool_name, ti.pool_slots)
             )
         else:
             yield self._passing_status(

tests/models/test_pool.py
@@ -97,7 +97,7 @@ class PoolTest(unittest.TestCase):
             dag_id='test_default_pool_open_slots',
             start_date=DEFAULT_DATE, )
         t1 = DummyOperator(task_id='dummy1', dag=dag)
-        t2 = DummyOperator(task_id='dummy2', dag=dag)
+        t2 = DummyOperator(task_id='dummy2', dag=dag, pool_slots=2)
         ti1 = TI(task=t1, execution_date=DEFAULT_DATE)
         ti2 = TI(task=t2, execution_date=DEFAULT_DATE)
         ti1.state = State.RUNNING
@@ -109,4 +109,4 @@ class PoolTest(unittest.TestCase):
         session.commit()
         session.close()
 
-        self.assertEqual(3, Pool.get_default_pool().open_slots())
+        self.assertEqual(2, Pool.get_default_pool().open_slots())

tests/models/test_taskinstance.py
@@ -344,6 +344,19 @@ class TaskInstanceTest(unittest.TestCase):
         db.clear_db_pools()
         self.assertEqual(ti.state, State.SUCCESS)
 
+    def test_pool_slots_property(self):
+        """
+        test that try to create a task with pool_slots less than 1
+        """
+        def create_task_instance():
+            dag = models.DAG(dag_id='test_run_pooling_task')
+            task = DummyOperator(task_id='test_run_pooling_task_op', dag=dag,
+                                 pool='test_pool', pool_slots=0, owner='airflow',
+                                 start_date=timezone.datetime(2016, 2, 1, 0, 0, 0))
+            return TI(task=task, execution_date=timezone.utcnow())
+
+        self.assertRaises(AirflowException, create_task_instance)
+
     @provide_session
     def test_ti_updates_with_task(self, session=None):
         """

tests/serialization/test_dag_serialization.py
@@ -582,6 +582,7 @@ class TestStringifiedDAGs(unittest.TestCase):
                           'owner': 'airflow',
                           'params': {},
                           'pool': 'default_pool',
+                          'pool_slots': 1,
                           'priority_weight': 1,
                           'queue': 'default',
                           'resources': None,

commit 6793eee26eebb986bf58d7482d57909560fc234f

Make TaskInstance.pool_slots not nullable with a default of 1
closes https://github.com/apache/airflow/issues/13799

Without it the migration from 1.10.14 to 2.0.0 can fail with following error for old TIs:

```
Traceback (most recent call last):
  File "/usr/local/lib/python3.6/dist-packages/airflow/jobs/scheduler_job.py", line 1275, in _execute
    self._run_scheduler_loop()
  File "/usr/local/lib/python3.6/dist-packages/airflow/jobs/scheduler_job.py", line 1377, in _run_scheduler_loop
    num_queued_tis = self._do_scheduling(session)
  File "/usr/local/lib/python3.6/dist-packages/airflow/jobs/scheduler_job.py", line 1533, in _do_scheduling
    num_queued_tis = self._critical_section_execute_task_instances(session=session)
  File "/usr/local/lib/python3.6/dist-packages/airflow/jobs/scheduler_job.py", line 1132, in _critical_section_execute_task_instances
    queued_tis = self._executable_task_instances_to_queued(max_tis, session=session)
  File "/usr/local/lib/python3.6/dist-packages/airflow/utils/session.py", line 62, in wrapper
    return func(*args, **kwargs)
  File "/usr/local/lib/python3.6/dist-packages/airflow/jobs/scheduler_job.py", line 1034, in _executable_task_instances_to_queued
    if task_instance.pool_slots > open_slots:
TypeError: '>' not supported between instances of 'NoneType' and 'int'
```

Workaround was to run manually:

```
UPDATE task_instance SET pool_slots = 1 WHERE pool_slots IS NULL;
```

This commit makes adds a DB migration to change the value to 1 for records with NULL value. And makes the column NOT NULLABLE.

This bug was caused by https://github.com/apache/airflow/pull/7160

airflow/migrations/versions/8646922c8a04_change_default_pool_slots_to_1.py
@@ -1,0 +1,93 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+"""Change default pool_slots to 1
+
+Revision ID: 8646922c8a04
+Revises: 449b4072c2da
+Create Date: 2021-02-23 23:19:22.409973
+
+"""
+
+import dill
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy import Column, Float, Integer, PickleType, String
+
+# revision identifiers, used by Alembic.
+from sqlalchemy.ext.declarative import declarative_base
+
+from airflow.models.base import COLLATION_ARGS
+from airflow.utils.sqlalchemy import UtcDateTime
+
+revision = '8646922c8a04'
+down_revision = '449b4072c2da'
+branch_labels = None
+depends_on = None
+
+Base = declarative_base()
+BATCH_SIZE = 5000
+ID_LEN = 250
+
+
+class TaskInstance(Base):  # noqa: D101  # type: ignore
+    __tablename__ = "task_instance"
+
+    task_id = Column(String(ID_LEN, **COLLATION_ARGS), primary_key=True)
+    dag_id = Column(String(ID_LEN, **COLLATION_ARGS), primary_key=True)
+    execution_date = Column(UtcDateTime, primary_key=True)
+    start_date = Column(UtcDateTime)
+    end_date = Column(UtcDateTime)
+    duration = Column(Float)
+    state = Column(String(20))
+    _try_number = Column('try_number', Integer, default=0)
+    max_tries = Column(Integer)
+    hostname = Column(String(1000))
+    unixname = Column(String(1000))
+    job_id = Column(Integer)
+    pool = Column(String(50), nullable=False)
+    pool_slots = Column(Integer, default=1)
+    queue = Column(String(256))
+    priority_weight = Column(Integer)
+    operator = Column(String(1000))
+    queued_dttm = Column(UtcDateTime)
+    queued_by_job_id = Column(Integer)
+    pid = Column(Integer)
+    executor_config = Column(PickleType(pickler=dill))
+    external_executor_id = Column(String(ID_LEN, **COLLATION_ARGS))
+
+
+def upgrade():
+    """Change default pool_slots to 1 and make pool_slots not nullable"""
+    connection = op.get_bind()
+    sessionmaker = sa.orm.sessionmaker()
+    session = sessionmaker(bind=connection)
+
+    session.query(TaskInstance).filter(TaskInstance.pool_slots.is_(None)).update(
+        {TaskInstance.pool_slots: 1}, synchronize_session=False
+    )
+    session.commit()
+
+    with op.batch_alter_table("task_instance", schema=None) as batch_op:
+        batch_op.alter_column("pool_slots", existing_type=sa.Integer, nullable=False)
+
+
+def downgrade():
+    """Unapply Change default pool_slots to 1"""
+    with op.batch_alter_table("task_instance", schema=None) as batch_op:
+        batch_op.alter_column("pool_slots", existing_type=sa.Integer, nullable=True)

airflow/models/taskinstance.py
@@ -273,7 +273,7 @@ class TaskInstance(Base, LoggingMixin):  # pylint: disable=R0902,R0904
     unixname = Column(String(1000))
     job_id = Column(Integer)
     pool = Column(String(50), nullable=False)
-    pool_slots = Column(Integer, default=1)
+    pool_slots = Column(Integer, default=1, nullable=False)
     queue = Column(String(256))
     priority_weight = Column(Integer)
     operator = Column(String(1000))