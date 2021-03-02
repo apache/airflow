6ce3244c0fc827d9f41d0b6d77cb7f065043dde5
Manuel Zaforas <mzaforas@gmail.com>
2021-03-02 12:47:14 +0100

    Refactor, parametrize using mock and simplify tests

--- tests/providers/docker/operators/test_docker.py
+++ tests/providers/docker/operators/test_docker.py
@@ -262,26 +262,11 @@ class TestDockerOperator(unittest.TestCase):
         assert 'extra_hosts' in self.client_mock.create_host_config.call_args[1]
         assert hosts_obj is self.client_mock.create_host_config.call_args[1]['extra_hosts']
 
-    def test_privileged_true(self):
-        operator = DockerOperator(task_id='test', image='test', privileged=True)
+    def test_privileged(self):
+        privileged = mock.Mock()
+        operator = DockerOperator(task_id='test', image='test', privileged=privileged)
         operator.execute(None)
         self.client_mock.create_container.assert_called_once()
         assert 'host_config' in self.client_mock.create_container.call_args[1]
         assert 'privileged' in self.client_mock.create_host_config.call_args[1]
-        assert True is self.client_mock.create_host_config.call_args[1]['privileged']
-
-    def test_privileged_false(self):
-        operator = DockerOperator(task_id='test', image='test', privileged=False)
-        operator.execute(None)
-        self.client_mock.create_container.assert_called_once()
-        assert 'host_config' in self.client_mock.create_container.call_args[1]
-        assert 'privileged' in self.client_mock.create_host_config.call_args[1]
-        assert False is self.client_mock.create_host_config.call_args[1]['privileged']
-
-    def test_privileged_missing(self):
-        operator = DockerOperator(task_id='test', image='test')
-        operator.execute(None)
-        self.client_mock.create_container.assert_called_once()
-        assert 'host_config' in self.client_mock.create_container.call_args[1]
-        assert 'privileged' in self.client_mock.create_host_config.call_args[1]
-        assert False is self.client_mock.create_host_config.call_args[1]['privileged']
+        assert privileged is self.client_mock.create_host_config.call_args[1]['privileged']
