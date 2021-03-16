d5f6f7d15b85a77da18d26fc8f2a903ba3bf7dfc
Dmitriy Synkov <dmitriysynkov@gmail.com>
2021-03-15 21:18:23 -0400

    Add extra_options to LivySensor as well

--- airflow/providers/apache/livy/operators/livy.py
+++ airflow/providers/apache/livy/operators/livy.py
@@ -66,6 +66,8 @@ class LivyOperator(BaseOperator):
     :type livy_conn_id: str
     :param polling_interval: time in seconds between polling for job completion. Don't poll for values >=0
     :type polling_interval: int
+    :type extra_options: A dictionary of options, where key is string and value
+    depends on the option that's being modified.
     """
 
     template_fields = ('spark_params',)
