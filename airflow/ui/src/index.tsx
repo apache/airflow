50d1be90edb685dcd522a32d81ed0a7a737d1406
Ryan Hamilton <ryan@ryanahamilton.com>
2021-03-17 16:19:32 -0400

    Adds initial router, routes, and placeholder views

--- airflow/ui/src/index.tsx
+++ airflow/ui/src/index.tsx
@@ -19,14 +19,17 @@
 
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 
 import App from './App';
 import theme from './theme';
 
 render(
-  <ChakraProvider theme={theme}>
-    <App />
-  </ChakraProvider>,
+  <BrowserRouter basename="/">
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </BrowserRouter>,
   document.getElementById('root'),
 );
