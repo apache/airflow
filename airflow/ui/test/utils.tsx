5696117da9d7c6103a242e70b77b2065655f726d
Brent Bovenzi <brent.bovenzi@gmail.com>
2021-03-24 14:42:59 -0400

    more strict allow origin

--- airflow/ui/test/utils.tsx
+++ airflow/ui/test/utils.tsx
@@ -20,14 +20,14 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
+export const url: string = process.env.API_URL || '';
+
 export const defaultHeaders = {
-  'access-control-allow-origin': '*',
+  'access-control-allow-origin': url,
   'access-control-allow-credentials': 'true',
   'access-control-allow-headers': '*',
   'access-control-allow-methods': '*',
 };
-
-export const url: string = process.env.API_URL || '';
 
 export const QueryWrapper: React.FC<{}> = ({ children }) => {
   const queryClient = new QueryClient();
