f2045d00c939af973832000b60ce91b7bd9c19ab
Davide Riva <73470084+nttdriva@users.noreply.github.com>
2021-03-25 17:41:29 +0100

    PostgreSQL operator returns query result

--- airflow/providers/postgres/operators/postgres.py
+++ airflow/providers/postgres/operators/postgres.py
@@ -68,6 +68,7 @@ class PostgresOperator(BaseOperator):
     def execute(self, context):
         self.log.info('Executing: %s', self.sql)
         self.hook = PostgresHook(postgres_conn_id=self.postgres_conn_id, schema=self.database)
-        self.hook.run(self.sql, self.autocommit, parameters=self.parameters)
+        result = self.hook.run(self.sql, self.autocommit, parameters=self.parameters)
         for output in self.hook.conn.notices:
             self.log.info(output)
+        return result
