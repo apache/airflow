apache/spark

commit badd890675d3cb3dfc088bff6a1d73dfdc275f31

Extend the same keyword args callable support in PythonOperator to some other sensors/operators (#11922)
This PR Standardises the callable signatures in PythonOperator, PythonSensor, ExternalTaskSensor, SimpleHttpOperator and HttpSensor.

The callable facilities in PythonOperator have been refactored into airflow.utils.helper.make_kwargs_callable. And it's used in those other places to make them work the same way.


Co-authored-by: Ash Berlin-Taylor <ash_github@firemirror.com>

airflow/providers/http/operators/http.py
@@ -17,8 +17,6 @@
 # under the License.
 from typing import Any, Callable, Dict, Optional
 
-import requests
-
 from airflow.exceptions import AirflowException
 from airflow.models import BaseOperator
 from airflow.providers.http.hooks.http import HttpHook
@@ -46,10 +44,14 @@ class SimpleHttpOperator(BaseOperator):
     :param headers: The HTTP headers to be added to the GET request
     :type headers: a dictionary of string key/value pairs
     :param response_check: A check against the 'requests' response object.
-        Returns True for 'pass' and False otherwise.
+        The callable takes the response object as the first positional argument
+        and optionally any number of keyword arguments available in the context dictionary.
+        It should return True for 'pass' and False otherwise.
     :type response_check: A lambda or defined function.
     :param response_filter: A function allowing you to manipulate the response
-        text. e.g response_filter=lambda response: json.loads(response.text)
+        text. e.g response_filter=lambda response: json.loads(response.text).
+        The callable takes the response object as the first positional argument
+        and optionally any number of keyword arguments available in the context dictionary.
     :type response_filter: A lambda or defined function.
     :param extra_options: Extra options for the 'requests' library, see the
         'requests' documentation (options to modify timeout, ssl, etc.)
@@ -76,8 +78,8 @@ class SimpleHttpOperator(BaseOperator):
         method: str = 'POST',
         data: Any = None,
         headers: Optional[Dict[str, str]] = None,
-        response_check: Optional[Callable[..., Any]] = None,
-        response_filter: Optional[Callable[[requests.Response], Any]] = None,
+        response_check: Optional[Callable[..., bool]] = None,
+        response_filter: Optional[Callable[..., Any]] = None,
         extra_options: Optional[Dict[str, Any]] = None,
         http_conn_id: str = 'http_default',
         log_response: bool = False,
@@ -97,6 +99,8 @@ class SimpleHttpOperator(BaseOperator):
             raise AirflowException("'xcom_push' was deprecated, use 'BaseOperator.do_xcom_push' instead")
 
     def execute(self, context: Dict[str, Any]) -> Any:
+        from airflow.utils.operator_helpers import make_kwargs_callable
+
         http = HttpHook(self.method, http_conn_id=self.http_conn_id)
 
         self.log.info("Calling HTTP method")
@@ -105,8 +109,10 @@ class SimpleHttpOperator(BaseOperator):
         if self.log_response:
             self.log.info(response.text)
         if self.response_check:
-            if not self.response_check(response):
+            kwargs_callable = make_kwargs_callable(self.response_check)
+            if not kwargs_callable(response, **context):
                 raise AirflowException("Response check returned False.")
         if self.response_filter:
-            return self.response_filter(response)
+            kwargs_callable = make_kwargs_callable(self.response_filter)
+            return kwargs_callable(response, **context)
         return response.text

airflow/providers/http/sensors/http.py
@@ -18,7 +18,6 @@
 from typing import Any, Callable, Dict, Optional
 
 from airflow.exceptions import AirflowException
-from airflow.operators.python import PythonOperator
 from airflow.providers.http.hooks.http import HttpHook
 from airflow.sensors.base_sensor_operator import BaseSensorOperator
 from airflow.utils.decorators import apply_defaults
@@ -59,7 +58,9 @@ class HttpSensor(BaseSensorOperator):
     :param headers: The HTTP headers to be added to the GET request
     :type headers: a dictionary of string key/value pairs
     :param response_check: A check against the 'requests' response object.
-        Returns True for 'pass' and False otherwise.
+        The callable takes the response object as the first positional argument
+        and optionally any number of keyword arguments available in the context dictionary.
+        It should return True for 'pass' and False otherwise.
     :type response_check: A lambda or defined function.
     :param extra_options: Extra options for the 'requests' library, see the
         'requests' documentation (options to modify timeout, ssl, etc.)
@@ -78,7 +79,7 @@ class HttpSensor(BaseSensorOperator):
         method: str = 'GET',
         request_params: Optional[Dict[str, Any]] = None,
         headers: Optional[Dict[str, Any]] = None,
-        response_check: Optional[Callable[..., Any]] = None,
+        response_check: Optional[Callable[..., bool]] = None,
         extra_options: Optional[Dict[str, Any]] = None,
         **kwargs: Any,
     ) -> None:
@@ -93,6 +94,8 @@ class HttpSensor(BaseSensorOperator):
         self.hook = HttpHook(method=method, http_conn_id=http_conn_id)
 
     def poke(self, context: Dict[Any, Any]) -> bool:
+        from airflow.utils.operator_helpers import make_kwargs_callable
+
         self.log.info('Poking: %s', self.endpoint)
         try:
             response = self.hook.run(
@@ -102,8 +105,8 @@ class HttpSensor(BaseSensorOperator):
                 extra_options=self.extra_options,
             )
             if self.response_check:
-                op_kwargs = PythonOperator.determine_op_kwargs(self.response_check, context)
-                return self.response_check(response, **op_kwargs)
+                kwargs_callable = make_kwargs_callable(self.response_check)
+                return kwargs_callable(response, **context)
 
         except AirflowException as exc:
             if str(exc).startswith("404"):

airflow/operators/python.py
@@ -24,7 +24,6 @@ import sys
 import types
 import warnings
 from inspect import signature
-from itertools import islice
 from tempfile import TemporaryDirectory
 from textwrap import dedent
 from typing import Any, Callable, Dict, Iterable, List, Optional, Tuple, TypeVar, Union, cast
@@ -38,6 +37,7 @@ from airflow.models.skipmixin import SkipMixin
 from airflow.models.taskinstance import _CURRENT_CONTEXT
 from airflow.models.xcom_arg import XComArg
 from airflow.utils.decorators import apply_defaults
+from airflow.utils.operator_helpers import determine_kwargs
 from airflow.utils.process_utils import execute_in_subprocess
 from airflow.utils.python_virtualenv import prepare_virtualenv, write_python_script
 
@@ -106,45 +106,11 @@ class PythonOperator(BaseOperator):
         if templates_exts:
             self.template_ext = templates_exts
 
-    @staticmethod
-    def determine_op_kwargs(python_callable: Callable, context: Dict, num_op_args: int = 0) -> Dict:
-        """
-        Function that will inspect the signature of a python_callable to determine which
-        values need to be passed to the function.
-
-        :param python_callable: The function that you want to invoke
-        :param context: The context provided by the execute method of the Operator/Sensor
-        :param num_op_args: The number of op_args provided, so we know how many to skip
-        :return: The op_args dictionary which contains the values that are compatible with the Callable
-        """
-        context_keys = context.keys()
-        sig = signature(python_callable).parameters.items()
-        op_args_names = islice(sig, num_op_args)
-        for name, _ in op_args_names:
-            # Check if it is part of the context
-            if name in context_keys:
-                # Raise an exception to let the user know that the keyword is reserved
-                raise ValueError(
-                    f"The key {name} in the op_args is part of the context, and therefore reserved"
-                )
-
-        if any(str(param).startswith("**") for _, param in sig):
-            # If there is a ** argument then just dump everything.
-            op_kwargs = context
-        else:
-            # If there is only for example, an execution_date, then pass only these in :-)
-            op_kwargs = {
-                name: context[name]
-                for name, _ in sig
-                if name in context  # If it isn't available on the context, then ignore
-            }
-        return op_kwargs
-
     def execute(self, context: Dict):
         context.update(self.op_kwargs)
         context['templates_dict'] = self.templates_dict
 
-        self.op_kwargs = PythonOperator.determine_op_kwargs(self.python_callable, context, len(self.op_args))
+        self.op_kwargs = determine_kwargs(self.python_callable, self.op_args, context)
 
         return_value = self.execute_callable()
         self.log.info("Done. Returned value was: %s", return_value)

airflow/sensors/external_task_sensor.py
@@ -65,9 +65,11 @@ class ExternalTaskSensor(BaseSensorOperator):
         execution_delta or execution_date_fn can be passed to
         ExternalTaskSensor, but not both.
     :type execution_delta: Optional[datetime.timedelta]
-    :param execution_date_fn: function that receives the current execution date
-        and returns the desired execution dates to query. Either execution_delta
-        or execution_date_fn can be passed to ExternalTaskSensor, but not both.
+    :param execution_date_fn: function that receives the current execution date as the first
+        positional argument and optionally any number of keyword arguments available in the
+        context dictionary, and returns the desired execution dates to query.
+        Either execution_delta or execution_date_fn can be passed to ExternalTaskSensor,
+        but not both.
     :type execution_date_fn: Optional[Callable]
     :param check_existence: Set to `True` to check if the external task exists (when
         external_task_id is not None) or check if the DAG to wait for exists (when
@@ -232,19 +234,19 @@ class ExternalTaskSensor(BaseSensorOperator):
         """
         This function is to handle backwards compatibility with how this operator was
         previously where it only passes the execution date, but also allow for the newer
-        implementation to pass all context through as well, to allow for more sophisticated
-        returns of dates to return.
-        Namely, this function check the number of arguments in the execution_date_fn
-        signature and if its 1, treat the legacy way, if it's 2, pass the context as
-        the 2nd argument, and if its more, throw an exception.
+        implementation to pass all context variables as keyword arguments, to allow
+        for more sophisticated returns of dates to return.
         """
-        num_fxn_params = self.execution_date_fn.__code__.co_argcount
-        if num_fxn_params == 1:
-            return self.execution_date_fn(context['execution_date'])
-        if num_fxn_params == 2:
-            return self.execution_date_fn(context['execution_date'], context)
+        from airflow.utils.operator_helpers import make_kwargs_callable
 
-        raise AirflowException(f'execution_date_fn passed {num_fxn_params} args but only allowed up to 2')
+        # Remove "execution_date" because it is already a mandatory positional argument
+        execution_date = context["execution_date"]
+        kwargs = {k: v for k, v in context.items() if k != "execution_date"}
+        # Add "context" in the kwargs for backward compatibility (because context used to be
+        # an acceptable argument of execution_date_fn)
+        kwargs["context"] = context
+        kwargs_callable = make_kwargs_callable(self.execution_date_fn)
+        return kwargs_callable(execution_date, **kwargs)
 
 
 class ExternalTaskMarker(DummyOperator):

airflow/sensors/python.py
@@ -17,9 +17,9 @@
 # under the License.
 from typing import Callable, Dict, List, Optional
 
-from airflow.operators.python import PythonOperator
 from airflow.sensors.base_sensor_operator import BaseSensorOperator
 from airflow.utils.decorators import apply_defaults
+from airflow.utils.operator_helpers import determine_kwargs
 
 
 class PythonSensor(BaseSensorOperator):
@@ -67,7 +67,7 @@ class PythonSensor(BaseSensorOperator):
     def poke(self, context: Dict):
         context.update(self.op_kwargs)
         context['templates_dict'] = self.templates_dict
-        self.op_kwargs = PythonOperator.determine_op_kwargs(self.python_callable, context, len(self.op_args))
+        self.op_kwargs = determine_kwargs(self.python_callable, self.op_args, context)
 
         self.log.info("Poking callable: %s", str(self.python_callable))
         return_value = self.python_callable(*self.op_args, **self.op_kwargs)

airflow/utils/operator_helpers.py
@@ -16,6 +16,7 @@
 # specific language governing permissions and limitations
 # under the License.
 #
+from typing import Callable, Dict, List, Tuple, Union
 
 AIRFLOW_VAR_NAME_FORMAT_MAPPING = {
     'AIRFLOW_CONTEXT_DAG_ID': {'default': 'airflow.ctx.dag_id', 'env_var_format': 'AIRFLOW_CTX_DAG_ID'},
@@ -89,3 +90,48 @@ def context_to_airflow_vars(context, in_env_var_format=False):
     if dag_run and dag_run.run_id:
         params[AIRFLOW_VAR_NAME_FORMAT_MAPPING['AIRFLOW_CONTEXT_DAG_RUN_ID'][name_format]] = dag_run.run_id
     return params
+
+
+def determine_kwargs(func: Callable, args: Union[Tuple, List], kwargs: Dict) -> Dict:
+    """
+    Inspect the signature of a given callable to determine which arguments in kwargs need
+    to be passed to the callable.
+
+    :param func: The callable that you want to invoke
+    :param args: The positional arguments that needs to be passed to the callable, so we
+        know how many to skip.
+    :param kwargs: The keyword arguments that need to be filtered before passing to the callable.
+    :return: A dictionary which contains the keyword arguments that are compatible with the callable.
+    """
+    import inspect
+    import itertools
+
+    signature = inspect.signature(func)
+    has_kwargs = any(p.kind == p.VAR_KEYWORD for p in signature.parameters.values())
+
+    for name in itertools.islice(signature.parameters.keys(), len(args)):
+        # Check if args conflict with names in kwargs
+        if name in kwargs:
+            raise ValueError(f"The key {name} in args is part of kwargs and therefore reserved.")
+
+    if has_kwargs:
+        # If the callable has a **kwargs argument, it's ready to accept all the kwargs.
+        return kwargs
+
+    # If the callable has no **kwargs argument, it only wants the arguments it requested.
+    return {key: kwargs[key] for key in signature.parameters if key in kwargs}
+
+
+def make_kwargs_callable(func: Callable) -> Callable:
+    """
+    Make a new callable that can accept any number of positional or keyword arguments
+    but only forwards those required by the given callable func.
+    """
+    import functools
+
+    @functools.wraps(func)
+    def kwargs_func(*args, **kwargs):
+        kwargs = determine_kwargs(func, args, kwargs)
+        return func(*args, **kwargs)
+
+    return kwargs_func

tests/providers/http/operators/test_http.py
@@ -69,7 +69,7 @@ class TestSimpleHttpOp(unittest.TestCase):
         )
 
         with mock.patch.object(operator.log, 'info') as mock_info:
-            self.assertRaises(AirflowException, operator.execute, None)
+            self.assertRaises(AirflowException, operator.execute, {})
             calls = [mock.call('Calling HTTP method'), mock.call('invalid response')]
             mock_info.assert_has_calls(calls, any_order=True)
 
@@ -83,5 +83,5 @@ class TestSimpleHttpOp(unittest.TestCase):
             http_conn_id='HTTP_EXAMPLE',
             response_filter=lambda response: response.json(),
         )
-        result = operator.execute(None)
+        result = operator.execute({})
         assert result == {'value': 5}

tests/sensors/test_external_task_sensor.py
@@ -264,6 +264,25 @@ exit 0
         )
         op1.run(start_date=DEFAULT_DATE, end_date=DEFAULT_DATE, ignore_ti_state=True)
 
+    def test_external_task_sensor_fn_kwargs(self):
+        """Check this task sensor passes multiple args with full context. If no failure, means clean run."""
+        self.test_time_sensor()
+
+        def my_func(dt, ds_nodash, tomorrow_ds_nodash):
+            assert ds_nodash == dt.strftime("%Y%m%d")
+            assert tomorrow_ds_nodash == (dt + timedelta(days=1)).strftime("%Y%m%d")
+            return dt + timedelta(0)
+
+        op1 = ExternalTaskSensor(
+            task_id='test_external_task_sensor_fn_kwargs',
+            external_dag_id=TEST_DAG_ID,
+            external_task_id=TEST_TASK_ID,
+            execution_date_fn=my_func,
+            allowed_states=['success'],
+            dag=self.dag,
+        )
+        op1.run(start_date=DEFAULT_DATE, end_date=DEFAULT_DATE, ignore_ti_state=True)
+
     def test_external_task_sensor_error_delta_and_fn(self):
         self.test_time_sensor()
         # Test that providing execution_delta and a function raises an error

apache/airflow

commit 0d2ec4262bc60218ea4595bb6a694283f68f28f4

Fix make_kwargs_callable import for http provider (out of bands)
Fixes failing import problem of the http backport
provider with Airflow 1.10.* series.

A problem was introduced in #11922 which cause the http provider
to stop working (local import was not caught at the review time
and as local import it has not been caught by the test harness).

Since the http provider is defunct and is very popular, we
decided to release an out-of-band release of the http provider
even if backport providers reached end-of-life.

This PR copies implementation of make_kwargs_callable into http
backport provider to deal with the incompatibility.

Fixes: #15198

airflow/providers/http/__init__.py
@@ -15,3 +15,49 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+#
+from typing import Callable, Dict, List, Tuple, Union
+
+
+def determine_kwargs(func: Callable, args: Union[Tuple, List], kwargs: Dict) -> Dict:
+    """
+    Inspect the signature of a given callable to determine which arguments in kwargs need
+    to be passed to the callable.
+    :param func: The callable that you want to invoke
+    :param args: The positional arguments that needs to be passed to the callable, so we
+        know how many to skip.
+    :param kwargs: The keyword arguments that need to be filtered before passing to the callable.
+    :return: A dictionary which contains the keyword arguments that are compatible with the callable.
+    """
+    import inspect
+    import itertools
+
+    signature = inspect.signature(func)
+    has_kwargs = any(p.kind == p.VAR_KEYWORD for p in signature.parameters.values())
+
+    for name in itertools.islice(signature.parameters.keys(), len(args)):
+        # Check if args conflict with names in kwargs
+        if name in kwargs:
+            raise ValueError(f"The key {name} in args is part of kwargs and therefore reserved.")
+
+    if has_kwargs:
+        # If the callable has a **kwargs argument, it's ready to accept all the kwargs.
+        return kwargs
+
+    # If the callable has no **kwargs argument, it only wants the arguments it requested.
+    return {key: kwargs[key] for key in signature.parameters if key in kwargs}
+
+
+def make_kwargs_callable(func: Callable) -> Callable:
+    """
+    Make a new callable that can accept any number of positional or keyword arguments
+    but only forwards those required by the given callable func.
+    """
+    import functools
+
+    @functools.wraps(func)
+    def kwargs_func(*args, **kwargs):
+        kwargs = determine_kwargs(func, args, kwargs)
+        return func(*args, **kwargs)
+
+    return kwargs_func

airflow/providers/http/operators/http.py
@@ -19,6 +19,7 @@ from typing import Any, Callable, Dict, Optional
 
 from airflow.exceptions import AirflowException
 from airflow.models import BaseOperator
+from airflow.providers.http import make_kwargs_callable
 from airflow.providers.http.hooks.http import HttpHook
 from airflow.utils.decorators import apply_defaults
 
@@ -99,8 +100,6 @@ class SimpleHttpOperator(BaseOperator):
             raise AirflowException("'xcom_push' was deprecated, use 'BaseOperator.do_xcom_push' instead")
 
     def execute(self, context: Dict[str, Any]) -> Any:
-        from airflow.utils.operator_helpers import make_kwargs_callable
-
         http = HttpHook(self.method, http_conn_id=self.http_conn_id)
 
         self.log.info("Calling HTTP method")

airflow/providers/http/sensors/http.py
@@ -18,6 +18,7 @@
 from typing import Any, Callable, Dict, Optional
 
 from airflow.exceptions import AirflowException
+from airflow.providers.http import make_kwargs_callable
 from airflow.providers.http.hooks.http import HttpHook
 from airflow.sensors.base import BaseSensorOperator
 from airflow.utils.decorators import apply_defaults
@@ -97,8 +98,6 @@ class HttpSensor(BaseSensorOperator):
         self.hook = HttpHook(method=method, http_conn_id=http_conn_id)
 
     def poke(self, context: Dict[Any, Any]) -> bool:
-        from airflow.utils.operator_helpers import make_kwargs_callable
-
         self.log.info('Poking: %s', self.endpoint)
         try:
             response = self.hook.run(

scripts/ci/libraries/_initialization.sh
@@ -157,7 +157,7 @@ function initialization::initialize_base_variables() {
 function initialization::initialize_branch_variables() {
     # Default branch used - this will be different in different branches
     export DEFAULT_BRANCH=${DEFAULT_BRANCH="master"}
-    export DEFAULT_CONSTRAINTS_BRANCH=${DEFAULT_CONSTRAINTS_BRANCH="constraints-master"}
+    export DEFAULT_CONSTRAINTS_BRANCH=${DEFAULT_CONSTRAINTS_BRANCH="constraints-2.0.0"}
     readonly DEFAULT_BRANCH
     readonly DEFAULT_CONSTRAINTS_BRANCH
 

setup.py
@@ -298,7 +298,7 @@ google = [
     'google-cloud-language>=1.1.1,<2.0.0',
     'google-cloud-logging>=2.1.1,<3.0.0',
     'google-cloud-memcache>=0.2.0',
-    'google-cloud-monitoring>=2.0.0,<3.0.0',
+    'google-cloud-monitoring>=2.0.0,<2.1.0',
     'google-cloud-os-login>=2.0.0,<3.0.0',
     'google-cloud-pubsub>=2.0.0,<3.0.0',
     'google-cloud-redis>=2.0.0,<3.0.0',