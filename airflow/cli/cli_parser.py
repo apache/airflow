84166794cf21b5365b5d00280afa929de11c545d
Xiaodong DENG <xddeng@apache.org>
2021-04-08 12:11:08 +0200

    BugFix: CLI 'kubernetes cleanup-pods' should only clean up Airflow-created Pods (#15204)
    closes: #15193
    
    Currently condition if the pod is created by Airflow is not considered. This commit fixes this.
    
    We decide if the Pod is created by Airflow via checking if it has all the labels added in PodGenerator.construct_pod() or KubernetesPodOperator.create_labels_for_pod().
    
    (cherry picked from commit c594d9cfb32bbcfe30af3f5dcb452c6053cacc95)

--- airflow/cli/cli_parser.py
+++ airflow/cli/cli_parser.py
@@ -1341,7 +1341,11 @@ CONFIG_COMMANDS = (
 KUBERNETES_COMMANDS = (
     ActionCommand(
         name='cleanup-pods',
-        help="Clean up Kubernetes pods in evicted/failed/succeeded states",
+        help=(
+            "Clean up Kubernetes pods "
+            "(created by KubernetesExecutor/KubernetesPodOperator) "
+            "in evicted/failed/succeeded states"
+        ),
         func=lazy_load_command('airflow.cli.commands.kubernetes_command.cleanup_pods'),
         args=(ARG_NAMESPACE,),
     ),
