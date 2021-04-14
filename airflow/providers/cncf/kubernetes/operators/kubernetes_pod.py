3c319672c226e63a84c810cafdd1e8552191942a
Jed Cunningham <66968678+jedcunningham@users.noreply.github.com>
2021-04-14 11:21:13 -0600

    Require `name` with KubernetesPodOperator
    We will require a `name` when using KPO without `pod_template_file` or
    `full_pod_spec`. Previously it would use `name` literally
    without being randomized, which is likely to confuse users during
    runtime. This emits an exception during parsing instead.
    
    This also adds test coverage around `pod_template_file` and
    `full_pod_spec`.

--- airflow/providers/cncf/kubernetes/operators/kubernetes_pod.py
+++ airflow/providers/cncf/kubernetes/operators/kubernetes_pod.py
@@ -415,8 +415,11 @@ class KubernetesPodOperator(BaseOperator):  # pylint: disable=too-many-instance-
         return pod.metadata.labels['try_number'] == context['ti'].try_number
 
     def _set_name(self, name):
-        if name is None:
+        if name is None and (self.pod_template_file or self.full_pod_spec):
             return None
+        if name is None:
+            raise AirflowException("`name` is required unless `pod_template_file` or `full_pod_spec` is set")
+
         validate_key(name, max_length=220)
         return re.sub(r'[^a-z0-9.-]+', '-', name.lower())
 
