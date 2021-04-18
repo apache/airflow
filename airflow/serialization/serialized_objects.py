916fb22126bc927eeff714bc57cdfc43cc8f3e13
Marcin Szymanski <ms32035@gmail.com>
2021-04-18 20:49:00 +0100

    fix tuple generation

--- airflow/serialization/serialized_objects.py
+++ airflow/serialization/serialized_objects.py
@@ -293,7 +293,7 @@ class BaseSerialization:
         elif type_ == DAT.SET:
             return {cls._deserialize(v) for v in var}
         elif type_ == DAT.TUPLE:
-            return tuple([cls._deserialize(v) for v in var])
+            return tuple(cls._deserialize(v) for v in var)
         else:
             raise TypeError(f'Invalid type {type_!s} in deserialization.')
 
