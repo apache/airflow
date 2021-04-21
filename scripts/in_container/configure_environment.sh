9e13ab4358f405fa834f72ad5c3e243419db6602
Ashish Patel <ashishpatel0720@gmail.com>
2021-04-21 17:36:06 +0530

    #15416 - log changed

--- scripts/in_container/configure_environment.sh
+++ scripts/in_container/configure_environment.sh
@@ -63,6 +63,6 @@ if [[ -d "${AIRFLOW_BREEZE_CONFIG_DIR}" && \
 else
     echo
     echo "You can add ${AIRFLOW_BREEZE_CONFIG_DIR} directory and place ${TMUX_CONF_FILE}"
-    echo "In it to make breeze use your local ${TMUX_CONF_FILE} for tmux"
+    echo "in it to make breeze use your local ${TMUX_CONF_FILE} for tmux"
     echo
 fi
