8d2ff5d96bad2d7f06301b66914c60bc1e9b7aa1
khilawar4 <38088179+khilawar4@users.noreply.github.com>
2021-05-07 11:16:40 -0700

    templatize aws_conn_id param
    adding `aws_conn_id` param to template_fields so that it can be overriden using jinja template from Dags.
    This feature can be useful if user wants to update/override the aws_conn_id for some kind of Dag Isolation etc)

--- airflow/providers/amazon/aws/operators/emr_create_job_flow.py
+++ airflow/providers/amazon/aws/operators/emr_create_job_flow.py
@@ -31,6 +31,8 @@ class EmrCreateJobFlowOperator(BaseOperator):
 
     :param aws_conn_id: aws connection to uses
     :type aws_conn_id: str
+       (adding this param to template_fields so that it can be overriden using jinja template from Dags
+       This feature can be useful if user wants to update/override the aws_conn_id for some kind of Dag Isolation etc) 
     :param emr_conn_id: emr connection to use
     :type emr_conn_id: str
     :param job_flow_overrides: boto3 style arguments or reference to an arguments file
@@ -40,7 +42,7 @@ class EmrCreateJobFlowOperator(BaseOperator):
     :type region_name: Optional[str]
     """
 
-    template_fields = ['job_flow_overrides']
+    template_fields = ['job_flow_overrides' , 'aws_conn_id']
     template_ext = ('.json',)
     template_fields_renderers = {"job_flow_overrides": "json"}
     ui_color = '#f9c915'
