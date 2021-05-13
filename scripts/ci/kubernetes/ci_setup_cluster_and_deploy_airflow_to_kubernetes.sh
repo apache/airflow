6fab1e3b7ea6fdb4ba30e66675ba61b8906c392e
EphraimBuddy <splendidzigy24@gmail.com>
2021-05-13 20:40:34 +0100

    Use matrix strategy for k8s test

--- scripts/ci/kubernetes/ci_setup_cluster_and_deploy_airflow_to_kubernetes.sh
+++ scripts/ci/kubernetes/ci_setup_cluster_and_deploy_airflow_to_kubernetes.sh
@@ -31,12 +31,8 @@ build_images::prepare_prod_build
 build_images::build_prod_images
 kind::build_image_for_kubernetes_tests
 kind::load_image_to_kind_cluster
-for mode in KubernetesExecutor CeleryExecutor LocalExecutor
-do
-    export EXECUTOR="${mode}"
-    kind::deploy_airflow_with_helm
-    kind::deploy_test_kubernetes_resources
-    kind::wait_for_webserver_healthy
-    export CLUSTER_FORWARDED_PORT="${FORWARDED_PORT_NUMBER}"
-    "$( dirname "${BASH_SOURCE[0]}" )/ci_run_kubernetes_tests.sh"
-done
+kind::deploy_airflow_with_helm
+kind::deploy_test_kubernetes_resources
+kind::wait_for_webserver_healthy
+export CLUSTER_FORWARDED_PORT="${FORWARDED_PORT_NUMBER}"
+"$( dirname "${BASH_SOURCE[0]}" )/ci_run_kubernetes_tests.sh"
