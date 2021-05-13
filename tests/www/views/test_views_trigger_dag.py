05408292a1a433eb6c9fa5474c719b13c362728d
Jens Scheffler <j_scheffler@gmx.de>
2021-05-13 21:01:37 +0200

    do not check response code in HTML view, fix unit test.

--- tests/www/views/test_views_trigger_dag.py
+++ tests/www/views/test_views_trigger_dag.py
@@ -82,7 +82,7 @@ def test_trigger_dag_conf_not_dict(admin_client):
     test_dag_id = "example_bash_operator"
 
     response = admin_client.post(f'trigger?dag_id={test_dag_id}', data={'conf': 'string and not a dict'})
-    check_content_in_response('must be a dict', response, status_code=400)
+    check_content_in_response('must be a dict', response)
 
     with create_session() as session:
         run = session.query(DagRun).filter(DagRun.dag_id == test_dag_id).first()
