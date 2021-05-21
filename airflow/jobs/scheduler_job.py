51fdcff119ade4af09b5e3ef0b6af579d42584e8
qian <yuqian1990@gmail.com>
2021-05-21 21:26:46 +0800

    Use ProcessPoolExecutor instead of multiprocessing.Pool

--- airflow/jobs/scheduler_job.py
+++ airflow/jobs/scheduler_job.py
@@ -671,6 +671,14 @@ class DagFileProcessor(LoggingMixin):
         return len(dagbag.dags), len(dagbag.import_errors)
 
 
+def _is_parent_process():
+    """
+    Returns True if the current process is the parent process. False if the current process is a child
+    process started by multiprocessing.
+    """
+    return multiprocessing.current_process().name == 'MainProcess'
+
+
 class SchedulerJob(BaseJob):  # pylint: disable=too-many-instance-attributes
     """
     This SchedulerJob runs for a specific time interval and schedules the jobs
@@ -746,21 +754,24 @@ class SchedulerJob(BaseJob):  # pylint: disable=too-many-instance-attributes
 
     def _exit_gracefully(self, signum, frame) -> None:  # pylint: disable=unused-argument
         """Helper method to clean up processor_agent to avoid leaving orphan processes."""
-        self.log.info("Exiting gracefully upon receiving signal %s", signum)
-        if self.processor_agent:
-            self.processor_agent.end()
-        sys.exit(os.EX_OK)
+        if _is_parent_process():
+            # Only the parent process should perform the cleanup.
+            self.log.info("Exiting gracefully upon receiving signal %s", signum)
+            if self.processor_agent:
+                self.processor_agent.end()
+            sys.exit(os.EX_OK)
 
     def _debug_dump(self, signum, frame):  # pylint: disable=unused-argument
-        try:
-            sig_name = signal.Signals(signum).name  # pylint: disable=no-member
-        except Exception:  # pylint: disable=broad-except
-            sig_name = str(signum)
+        if _is_parent_process():
+            try:
+                sig_name = signal.Signals(signum).name  # pylint: disable=no-member
+            except Exception:  # pylint: disable=broad-except
+                sig_name = str(signum)
 
-        self.log.info("%s\n%s received, printing debug\n%s", "-" * 80, sig_name, "-" * 80)
+            self.log.info("%s\n%s received, printing debug\n%s", "-" * 80, sig_name, "-" * 80)
 
-        self.executor.debug_dump()
-        self.log.info("-" * 80)
+            self.executor.debug_dump()
+            self.log.info("-" * 80)
 
     def is_alive(self, grace_multiplier: Optional[float] = None) -> bool:
         """
