b03b0b875fe04c2fd29b93bd7f6610732f0b78b5
jpyen <>
2021-05-26 23:39:01 -0700

    Remove pod specs from test, since they are set by default from k8s

--- kubernetes_tests/test_kubernetes_pod_operator.py
+++ kubernetes_tests/test_kubernetes_pod_operator.py
@@ -95,7 +95,6 @@ class TestKubernetesPodOperatorSystem(unittest.TestCase):
                 'containers': [
                     {
                         'image': 'ubuntu:16.04',
-                        'imagePullPolicy': 'IfNotPresent',
                         'args': ["echo 10"],
                         'command': ["bash", "-cx"],
                         'env': [],
@@ -112,7 +111,6 @@ class TestKubernetesPodOperatorSystem(unittest.TestCase):
                 'nodeSelector': {},
                 'restartPolicy': 'Never',
                 'securityContext': {},
-                'serviceAccountName': 'default',
                 'tolerations': [],
                 'volumes': [],
             },
