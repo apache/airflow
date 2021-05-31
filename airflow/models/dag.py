apache/spark

commit 38ecbd6769ecee1136b653c17c2c5c2b17937818

Move common pitfall documentation to Airflow docs (#15183)
Reviewed Common Pitfall page and migrated to Airflow documentation. I also added more common pitfalls.

closes: #10180

airflow/config_templates/config.yml
@@ -158,17 +158,19 @@
       default: ~
     - name: parallelism
       description: |
-        The amount of parallelism as a setting to the executor. This defines
-        the max number of task instances that should run simultaneously
-        on this airflow installation
+        This defines the maximum number of task instances that can run concurrently in Airflow
+        regardless of scheduler count and worker count. Generally, this value is reflective of
+        the number of task instances with the running state in the metadata database.
       version_added: ~
       type: string
       example: ~
       default: "32"
     - name: dag_concurrency
       description: |
-        The number of task instances allowed to run concurrently by the scheduler
-        in one DAG. Can be overridden by ``concurrency`` on DAG level.
+        The maximum number of task instances allowed to run concurrently in each DAG. To calculate
+        the number of tasks that is running concurrently for a DAG, add up the number of running
+        tasks for all DAG runs of the DAG. This is configurable at the DAG level with ``concurrency``,
+        which is defaulted as ``dag_concurrency``.
       version_added: ~
       type: string
       example: ~
@@ -182,7 +184,9 @@
       default: "True"
     - name: max_active_runs_per_dag
       description: |
-        The maximum number of active DAG runs per DAG
+        The maximum number of active DAG runs per DAG. The scheduler will not create more DAG runs
+        if it reaches the limit. This is configurable at the DAG level with ``max_active_runs``,
+        which is defaulted as ``max_active_runs_per_dag``.
       version_added: ~
       type: string
       example: ~

airflow/config_templates/default_airflow.cfg
@@ -108,19 +108,23 @@ sql_alchemy_schema =
 # See https://docs.sqlalchemy.org/en/13/core/engines.html#sqlalchemy.create_engine.params.connect_args
 # sql_alchemy_connect_args =
 
-# The amount of parallelism as a setting to the executor. This defines
-# the max number of task instances that should run simultaneously
-# on this airflow installation
+# This defines the maximum number of task instances that can run concurrently in Airflow
+# regardless of scheduler count and worker count. Generally, this value is reflective of
+# the number of task instances with the running state in the metadata database.
 parallelism = 32
 
-# The number of task instances allowed to run concurrently by the scheduler
-# in one DAG. Can be overridden by ``concurrency`` on DAG level.
+# The maximum number of task instances allowed to run concurrently in each DAG. To calculate
+# the number of tasks that is running concurrently for a DAG, add up the number of running
+# tasks for all DAG runs of the DAG. This is configurable at the DAG level with ``concurrency``,
+# which is defaulted as ``dag_concurrency``.
 dag_concurrency = 16
 
 # Are DAGs paused by default at creation
 dags_are_paused_at_creation = True
 
-# The maximum number of active DAG runs per DAG
+# The maximum number of active DAG runs per DAG. The scheduler will not create more DAG runs
+# if it reaches the limit. This is configurable at the DAG level with ``max_active_runs``,
+# which is defaulted as ``max_active_runs_per_dag``.
 max_active_runs_per_dag = 16
 
 # Whether to load the DAG examples that ship with Airflow. It's good to

apache/airflow

commit aae99669c80062f8444e861fad595aad335de119

Fix docs for ``dag_concurrency``
The docs were incorrect. Bug introduced in https://github.com/apache/airflow/pull/15183/
unfortunately.

airflow/config_templates/config.yml
@@ -167,10 +167,8 @@
       default: "32"
     - name: dag_concurrency
       description: |
-        The maximum number of task instances allowed to run concurrently in each DAG. To calculate
-        the number of tasks that is running concurrently for a DAG, add up the number of running
-        tasks for all DAG runs of the DAG. This is configurable at the DAG level with ``concurrency``,
-        which is defaulted as ``dag_concurrency``.
+        The maximum number of task instances allowed to run concurrently in each DAG Run for that
+        DAG. Can be overridden by ``concurrency`` on DAG level.
       version_added: ~
       type: string
       example: ~

airflow/config_templates/default_airflow.cfg
@@ -113,10 +113,8 @@ sql_alchemy_schema =
 # the number of task instances with the running state in the metadata database.
 parallelism = 32
 
-# The maximum number of task instances allowed to run concurrently in each DAG. To calculate
-# the number of tasks that is running concurrently for a DAG, add up the number of running
-# tasks for all DAG runs of the DAG. This is configurable at the DAG level with ``concurrency``,
-# which is defaulted as ``dag_concurrency``.
+# The maximum number of task instances allowed to run concurrently in each DAG Run for that
+# DAG. Can be overridden by ``concurrency`` on DAG level.
 dag_concurrency = 16
 
 # Are DAGs paused by default at creation

airflow/models/dag.py
@@ -167,7 +167,7 @@ class DAG(LoggingMixin):
         params can be overridden at the task level.
     :type params: dict
     :param concurrency: the number of task instances allowed to run
-        concurrently
+        concurrently in a single DagRun
     :type concurrency: int
     :param max_active_runs: maximum number of active DAG runs, beyond this
         number of DAG runs in a running state, the scheduler won't create