5644d9c095566afe9701e82774aa727978e30833
Sumit Maheshwari <sumit@astronomer.io>
2021-06-04 14:55:59 +0530

    Fix broken CI

--- tests/providers/airbyte/hooks/test_airbyte.py
+++ tests/providers/airbyte/hooks/test_airbyte.py
@@ -128,7 +128,10 @@ class TestAirbyteHook(unittest.TestCase):
 
     @requests_mock.mock()
     def test_connection_success(self, m):
-        m.get(self.health_endpoint, status_code=200,)
+        m.get(
+            self.health_endpoint,
+            status_code=200,
+        )
 
         status, msg = self.hook.test_connection()
         assert status is True
@@ -136,7 +139,11 @@ class TestAirbyteHook(unittest.TestCase):
 
     @requests_mock.mock()
     def test_connection_failure(self, m):
-        m.get(self.health_endpoint, status_code=500, json={"message": "internal server error"})
+        m.get(
+            self.health_endpoint,
+            status_code=500,
+            json={"message": "internal server error"}
+        )
 
         status, msg = self.hook.test_connection()
         assert status is False
