fca490f0aba1e912625c1769f5baa9df3d93044f
javier.lopez <javier.lopez@promocionesfarma.com>
2021-06-11 19:13:17 +0200

    ash suggestion

--- airflow/providers/amazon/aws/transfers/s3_to_ftp.py
+++ airflow/providers/amazon/aws/transfers/s3_to_ftp.py
@@ -27,17 +27,20 @@ class S3ToFTPOperator(BaseOperator):
     """
     This operator enables the transferring of files from S3 to a FTP server.
 
+    :param s3_bucket: The targeted s3 bucket. This is the S3 bucket from
+    where the file is downloaded.
+    :type s3_bucket: str
+    :param s3_key: The targeted s3 key. This is the specified file path for
+    downloading the file from S3.
+    :type s3_key: str
+    :param ftp_path: The ftp remote path. This is the specified file path for
+    uploading file to the FTP server.
+    :type ftp_path: str
+    :param aws_conn_id: reference to a specific AWS connection
+    :type aws_conn_id: str
     :param ftp_conn_id: The ftp connection id. The name or identifier for
         establishing a connection to the FTP server.
     :type ftp_conn_id: str
-    :param ftp_path: The ftp remote path where the file will be stored, inclusive of filename.
-    :type ftp_path: str
-    :param s3_bucket: The targeted s3 bucket. This is the S3 bucket from
-        where the file is downloaded.
-    :type s3_bucket: str
-    :param s3_key: The targeted s3 key. This is the specified file path for
-        downloading the file from S3.
-    :type s3_key: str
     """
 
     template_fields = ('s3_bucket', 's3_key', 'ftp_path')
@@ -63,8 +66,11 @@ class S3ToFTPOperator(BaseOperator):
         s3_hook = S3Hook(self.aws_conn_id)
         ftp_hook = FTPHook(ftp_conn_id=self.ftp_conn_id)
 
-        s3_client = s3_hook.get_conn()
+        s3_obj = s3_hook.get_key(self.s3_key, self.s3_bucket)
 
         with NamedTemporaryFile() as local_tmp_file:
-            s3_client.download_file(self.s3_bucket, self.s3_key, local_tmp_file.name)
+            self.log.info(f'Downloading file from {self.s3_key}')
+            s3_obj.download_fileobj(local_tmp_file)
+            local_tmp_file.seek(0)
             ftp_hook.store_file(self.ftp_path, local_tmp_file.name)
+            self.log.info(f'File stored in {self.ftp_path}')
--- tests/providers/amazon/aws/transfers/test_s3_to_ftp.py
+++ tests/providers/amazon/aws/transfers/test_s3_to_ftp.py
@@ -16,9 +16,8 @@
 # specific language governing permissions and limitations
 # under the License.
 import unittest
-from unittest import mock, Mock
+from unittest import mock
 
-from airflow.provider.amazon.aws.hooks.s3 import S3Hook
 from airflow.providers.amazon.aws.transfers.s3_to_ftp import S3ToFTPOperator
 
 TASK_ID = 'test_s3_to_ftp'
@@ -31,19 +30,14 @@ FTP_CONN_ID = 'ftp_default'
 
 class TestS3ToFTPOperator(unittest.TestCase):
     @mock.patch("airflow.providers.ftp.hooks.ftp.FTPHook.store_file")
+    @mock.patch("airflow.providers.amazon.aws.hooks.s3.S3Hook.get_key")
     @mock.patch("airflow.providers.amazon.aws.transfers.s3_to_ftp.NamedTemporaryFile")
-    @mock.patch('airflow.providers.amazon.aws.transfers.s3_to_ftp.S3Hook')
-    def test_execute(self, mock_s3_hook, mock_local_tmp_file, mock_ftp_hook_store_file):
+    def test_execute(self, mock_local_tmp_file, mock_s3_hook_get_key, mock_ftp_hook_store_file):
         operator = S3ToFTPOperator(task_id=TASK_ID, s3_bucket=BUCKET, s3_key=S3_KEY, ftp_path=FTP_PATH)
         operator.execute(None)
 
+        mock_s3_hook_get_key.assert_called_once_with(operator.s3_key, operator.s3_bucket)
+
         mock_local_tmp_file_value = mock_local_tmp_file.return_value.__enter__.return_value
-        key = operator.s3_key
-        bucket = operator.s3_bucket
-
-        s3_client = mock_s3_hook.get_conn()
-
-        s3_client.download_file(bucket, key, mock_local_tmp_file.name)
-        s3_client.download_file.assert_called_once_with(bucket, key, mock_local_tmp_file.name)
-
+        mock_s3_hook_get_key.return_value.download_fileobj.assert_called_once_with(mock_local_tmp_file_value)
         mock_ftp_hook_store_file.assert_called_once_with(operator.ftp_path, mock_local_tmp_file_value.name)
