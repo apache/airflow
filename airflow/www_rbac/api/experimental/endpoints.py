44d72ce195b754f73a59846ed34f0aa8c7dd0883
WangGuoxi <wang918562230@qq.com>
2021-06-15 17:14:44 +0800

    [IMP](spc)spc增加正态分布曲线图

--- airflow/www_rbac/api/experimental/endpoints.py
+++ airflow/www_rbac/api/experimental/endpoints.py
@@ -46,6 +46,7 @@ import datetime
 from random import choices
 import pendulum
 from airflow.utils.spc.lexen_spc.chart import covert2dArray, xbar_rbar, rbar, xbar_sbar, sbar, cpk
+from airflow.utils.spc.lexen_spc.plot import histogram, normal
 import math
 from airflow.settings import TIMEZONE
 from flask_login import current_user
@@ -399,9 +400,12 @@ mm_min = {TORQUE: 'torque_min', ANGLE: 'angle_min'}
 @profile(os.path.join(PROFILE_DIR, 'spc.profile'))
 def get_spc_by_entity_id():
     spc = {'x-r': {"title": u"Xbar-R 控制图", "data": {TORQUE: {}, ANGLE: {}}},
-           'x-s': {"title": u"Xbar-S 控制图", "data": {TORQUE: {}, ANGLE: {}}}}
+           'x-s': {"title": u"Xbar-S 控制图", "data": {TORQUE: {}, ANGLE: {}}},
+           'n-d': {"title": u"正态分布 图", "data": {TORQUE: {}, ANGLE: {}}},
+           }
     x_r_entry = spc.get('x-r').get('data')
     x_s_entry = spc.get('x-s').get('data')
+    n_d_entry = spc.get('n-d').get('data')
     try:
         vals = request.args.get('entity_ids')
         tType = request.args.get('type', 'torque')  # 默认是扭矩图
@@ -427,8 +431,8 @@ def get_spc_by_entity_id():
                 raise AirflowException(u'SPC 数据格式不正确!')
             xr_xbar_part = xbar_rbar(data, SPC_SIZE)
             xr_r_part = rbar(data, SPC_SIZE)
-            xs_xbar_part = xbar_sbar(data, SPC_SIZE)
-            xs_s_part = sbar(data, SPC_SIZE)
+            xs_xbar_part = xbar_sbar(data, SPC_SIZE, None)
+            xs_s_part = sbar(data, SPC_SIZE, None)
             cpk_data = cpk(entry, get_first_valid_data(results, mm_max.get(key)), get_first_valid_data(results,mm_min.get(key)))
             # todo: SPC包
             # xbar-r chart
@@ -451,6 +455,41 @@ def get_spc_by_entity_id():
                 's': xs_s_part,
                 'cpk': cpk_data
             })
+
+        # 正态分布图
+        nd_data = origin_data.get(tType)
+        if len(nd_data) > 0:
+            first_result = results[0]
+            # 正常是使用target值的正负百分之三
+            # _target = first_result.get(tType+"_target")
+            # if not _target:
+            #     _target = (first_result.get(tType+"_min") + first_result.get(tType+"_max")) / 2
+            # usl = _target * (1+0.03)
+            # lsl = _target * (1-0.03)
+            usl = first_result.get(tType+"_max")
+            lsl = first_result.get(tType+"_min")
+            spc_step = 1
+            his = histogram(nd_data, usl, lsl, spc_step)
+            nor = normal(nd_data, usl, lsl, spc_step)
+            x1, y1, y2 = [], [], []
+            for i, val in enumerate(his[0]):
+                if i + 1 < len(his[0]):
+                    x1.append('(%.1f,%.1f)' % (val, his[0][i + 1]))
+            for i, val in enumerate(his[1]):
+                if np.isnan(val):
+                    val = 0
+                y1.append(round(val * 100, 2))
+            for i, val in enumerate(nor[1]):
+                if np.isnan(val):
+                    val = 0
+                y2.append(round(val * 100, 2))
+            n_d_entry.get(tType).update(
+                {
+                    'x1': x1,
+                    'y1': y1,
+                    'y2': y2
+                }
+            )
         _log.info(spc)
         return jsonify(spc=spc)
     except AirflowException as e:
