8e6f300f6d5e1cf8e0355fb4b0eb65a6f06ec00e
Josh Fell <josh.d.fell@astronomer.io>
2021-07-08 23:40:36 -0400

    Removing S3ToSnowflakeOperator task from example DAG

--- airflow/providers/amazon/aws/example_dags/example_salesforce_to_s3.py
+++ airflow/providers/amazon/aws/example_dags/example_salesforce_to_s3.py
@@ -16,19 +16,13 @@
 # under the License.
 """
 This is a basic example DAG for using `SalesforceToS3Operator` to retrieve Salesforce customer
-data, upload to an S3 data lake, and copy into a Snowflake data warehouse.
+data and upload to an S3 bucket.
 """
 
 from datetime import datetime
 
 from airflow import DAG
 from airflow.providers.amazon.aws.transfers.salesforce_to_s3 import SalesforceToS3Operator
-from airflow.providers.snowflake.transfers.s3_to_snowflake import S3ToSnowflakeOperator
-
-
-BASE_PATH = "salesforce/customers"
-DATE_PREFIXES = "{{ execution_date.strftime('%Y/%m/%d') }}"
-FILE_NAME = "customer_extract_{{ ds_nodash }}.json"
 
 with DAG(
     dag_id="example_salesforce_to_s3_transfer",
@@ -38,26 +32,18 @@ with DAG(
     tags=["example"],
 ) as dag:
     # [START howto_operator_salesforce_to_s3_transfer]
+    base_path = "salesforce/customers"
+    date_prefixes = "{{ execution_date.strftime('%Y/%m/%d') }}"
+    file_name = "customer_extract_{{ ds_nodash }}.json"
+
     upload_salesforce_data_to_s3 = SalesforceToS3Operator(
         task_id="upload_salesforce_data_to_s3",
         query="SELECT Id, Name, Company, Phone, Email, Region, CreatedDate, LastModifiedDate, IsActive FROM Customers",
         s3_bucket_name="data-lake-raw",
-        s3_key=f"{BASE_PATH}/{DATE_PREFIXES}/{FILE_NAME}",
+        s3_key=f"{base_path}/{date_prefixes}/{file_name}",
         salesforce_conn_id="salesforce",
         export_format="json",
         aws_conn_id="s3",
         replace=True,
     )
     # [END howto_operator_salesforce_to_s3_transfer]
-
-    copy_from_s3_to_snowflake = S3ToSnowflakeOperator(
-        task_id="copy_from_s3_to_snowflake",
-        snowflake_conn_id="snowflake",
-        prefix=f"{BASE_PATH}/{DATE_PREFIXES}/",
-        table="customers",
-        schema="ingest",
-        stage="s3_data_lake_raw",
-        file_format="(type = 'JSON')",
-    )
-
-    upload_salesforce_data_to_s3 >> copy_from_s3_to_snowflake
