bd67107a3fe008d3377e97c77b482f1bf85502c5
Cosine <cooosine@outlook.com>
2021-07-09 15:15:38 +0800

    [FEAT](plugins): 将基础数据移动到新的仓库，通过submodule添加到plugin中

--- plugins/default_data/create_default_data.py
+++ plugins/default_data/create_default_data.py
@@ -3,17 +3,22 @@ import os
 from airflow.utils.log.logging_mixin import LoggingMixin
 from airflow.plugins_manager import AirflowPlugin
 from airflow.utils.load_data_from_csv import load_data_from_csv
-
+from airflow.configuration import conf
+from plugins.factory_code.factory_code import get_factory_code
 
 log = LoggingMixin().log
 
 
 @provide_session
-def load_default_controller(session=None):
+def load_default_controller(file_dir, session=None):
     log.info("Loading default controllers")
-    from airflow.models import TighteningController
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    val = load_data_from_csv(os.path.join(current_dir, 'default_controllers.csv'), {
+    file_path = os.path.join(current_dir, 'data/{}/default_controllers.csv'.format(file_dir))
+    if not os.path.exists(file_path):
+        log.error("导入控制器目录不存在：{}".format(file_path))
+        return
+    from airflow.models import TighteningController
+    val = load_data_from_csv(file_path, {
         'controller_name': '控制器名称',
         'line_code': '工段编号',
         'work_center_code': '工位编号',
@@ -35,29 +40,6 @@ def load_default_controller(session=None):
         )
 
 
-default_error_tags = {
-    '1': '曲线异常（未知原因）',
-    '101': '螺栓粘滑',
-    '102': '扭矩异常下落',
-    '103': '重复拧紧',
-    '104': '提前松手',
-    '105': '角度过大',
-    # '100': '提前松手',
-    # '101': '螺栓放偏',
-    # '102': '螺栓空转，没办法旋入',
-    # '103': '螺栓被错误的预拧紧',
-    # '104': '螺纹胶涂胶识别有无',
-    # '105': '螺纹胶涂覆位置错误-前后',
-    # '106': '螺钉太长',
-    # '107': '螺钉太短',
-    # '108': '工件开裂',
-    # '109': '尖叫螺栓',
-    # '110': '提前进入屈服阶段',
-    # '111': '转角法监控扭矩小于下限值',
-    # '112': '转角法监控扭矩大于上限值-或者临界上限值'
-}
-
-
 @provide_session
 def merge_data(model, data, is_exist, session=None):
     if not session.query(model).filter(is_exist).first():
@@ -67,58 +49,66 @@ def merge_data(model, data, is_exist, session=None):
 
 @provide_session
 def create_default_error_tags(session=None):
+    log.info("Loading default error_tags")
     from airflow.models import ErrorTag
-    for key, value in default_error_tags.items():
-        data = ErrorTag(lable=value, value=key)
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    error_tags = load_data_from_csv(os.path.join(current_dir, 'data/error_tags.csv'), {
+        'value': 'value',
+        'label': 'label'
+    })
+    for error_tag in error_tags:
+        data = ErrorTag(lable=error_tag.get('label'), value=error_tag.get('value'))
         merge_data(model=ErrorTag, data=data, is_exist=ErrorTag.label == data.label, session=session)
-
-
-support_device_types = {
-    '拧紧工具': '{}',
-    '压机': '{}',
-}
 
 
 @provide_session
 def create_device_type_support(session=None):
+    log.info("Loading default device types")
+    current_dir = os.path.dirname(os.path.abspath(__file__))
     if not session:
         return
+    device_types = load_data_from_csv(os.path.join(current_dir, 'data/device_types.csv'), {
+        'name': 'name',
+        'view_config': 'view_config'
+    })
     from airflow.models.tightening_controller import DeviceTypeModel
-    for name, view_config in support_device_types.items():
-        data = DeviceTypeModel(name=name, view_config=view_config)
+    for device_type in device_types:
+        data = DeviceTypeModel(name=device_type.get('name'), view_config=device_type.get('view_config'))
         merge_data(model=DeviceTypeModel, data=data, is_exist=DeviceTypeModel.name == data.name, session=session)
 
 
-def create_default_users():
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    default_users = load_data_from_csv(os.path.join(current_dir, 'default_users.csv'), {
-        'username': 'username',
-        'email': 'email',
-        'lastname': 'lastname',
-        'firstname': 'firstname',
-        'password': 'password',
-        'role': 'role'
-    })
-    from airflow.www_rbac.app import cached_appbuilder
-    appbuilder = cached_appbuilder()
-    for user in default_users:
-        try:
-            role = appbuilder.sm.find_role(user['role'])
-            if not role:
-                raise SystemExit('{} is not a valid role.'.format(user['role']))
-            user_created = appbuilder.sm.add_user(
-                user['username'],
-                user['firstname'],
-                user['lastname'],
-                user['email'],
-                role,
-                user['password'])
-            if user_created:
-                log.info('{} user {} created.'.format(user['role'], user['username']))
-            else:
-                raise SystemExit('Failed to create user.')
-        except Exception as e:
-            log.error(e)
+# fixme: 该方法在1.10版本中不可用，2.x版本中可用，需要在升级后稍作修改
+# def create_default_users():
+#     current_dir = os.path.dirname(os.path.abspath(__file__))
+#     default_users = load_data_from_csv(os.path.join(current_dir, 'data/nd/default_users.csv'), {
+#         'username': 'username',
+#         'email': 'email',
+#         'lastname': 'lastname',
+#         'firstname': 'firstname',
+#         'password': 'password',
+#         'role': 'role'
+#     })
+#     from airflow.www_rbac.app import cached_appbuilder
+#     appbuilder = cached_appbuilder()
+#     for user in default_users:
+#         try:
+#             role = appbuilder.sm.find_role(user['role'])
+#             if not role:
+#                 raise SystemExit('{} is not a valid role.'.format(user['role']))
+#             user_created = appbuilder.sm.add_user(
+#                 user['username'],
+#                 user['firstname'],
+#                 user['lastname'],
+#                 user['email'],
+#                 role,
+#                 user['password'])
+#             if user_created:
+#                 log.info('{} user {} created.'.format(user['role'], user['username']))
+#             else:
+#                 raise SystemExit('Failed to create user.')
+#         except Exception as e:
+#             log.error(e)
+
 
 # Defining the plugin class
 class LoadDefaultDataPlugin(AirflowPlugin):
@@ -126,7 +116,12 @@ class LoadDefaultDataPlugin(AirflowPlugin):
 
     @classmethod
     def on_load(cls, *args, **kwargs):
-        create_default_users()
-        load_default_controller()
-        create_device_type_support()
-        create_default_error_tags()
+        # create_default_users()
+        if conf.getboolean('core', 'LOAD_DEFAULT_ERROR_TAG', fallback=True):
+            create_default_error_tags()
+
+        if conf.getboolean('core', 'LOAD_DEFAULT_DEVICE_TYPE', fallback=True):
+            create_device_type_support()
+
+        factory_code = get_factory_code()
+        load_default_controller(factory_code)
---
+++ plugins/factory_code/factory_code.py
@@ -0,0 +1,14 @@
+import os
+
+factory_code_map = {
+    'nd': ['nd', '7200', 'ND'],
+    'lg': ['lg', '2200', 'LG'],
+}
+
+
+def get_factory_code():
+    code = os.environ.get('FACTORY_CODE', '')
+    for key, values in factory_code_map.items():
+        if code in values:
+            return key
+    return None
