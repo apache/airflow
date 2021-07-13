bcbb8e9955a39d1f168040f7454ec27fa2fd0d3e
pyerbiz <39149075+pyerbiz@users.noreply.github.com>
2021-07-13 14:21:44 +0530

    Added docs and ref for aws transfer operators sftp_to_s2 and s3_to_sftp #16730 (#1)
    * created files
    
    * added s3tosftp and examples
    
    * vault var removed
    
    * updated s3_to_sftp example and doc
    
    * fixed directives
    
    * ref error fixed

--- airflow/providers/amazon/aws/example_dags/example_s3_to_sftp.py
+++ airflow/providers/amazon/aws/example_dags/example_s3_to_sftp.py
@@ -39,5 +39,6 @@ with models.DAG(
         s3_conn_id="s3_conn_id",
         s3_bucket=S3_BUCKET,
         s3_key=S3_KEY,
+        dag=dag
     )
     # [END howto_s3_transfer_data_to_sftp]
--- airflow/providers/amazon/aws/example_dags/example_sftp_to_s3.py
+++ airflow/providers/amazon/aws/example_dags/example_sftp_to_s3.py
@@ -38,5 +38,6 @@ with models.DAG(
         s3_conn_id="s3_conn_id",
         s3_bucket=S3_BUCKET,
         s3_key=S3_KEY,
+        dag=dag
     )
     # [END howto_sftp_transfer_data_to_s3]
