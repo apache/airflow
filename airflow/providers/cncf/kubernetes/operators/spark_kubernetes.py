f2a4bec37ad57ca2792e9acbecb052957cf8bd03
john-jac <75442233+john-jac@users.noreply.github.com>
2021-07-16 07:40:18 -0700

    Update spark_kubernetes.py
    Clarified application_file param definition

--- airflow/providers/cncf/kubernetes/operators/spark_kubernetes.py
+++ airflow/providers/cncf/kubernetes/operators/spark_kubernetes.py
@@ -30,8 +30,7 @@ class SparkKubernetesOperator(BaseOperator):
         For more detail about Spark Application Object have a look at the reference:
         https://github.com/GoogleCloudPlatform/spark-on-k8s-operator/blob/v1beta2-1.1.0-2.4.5/docs/api-docs.md#sparkapplication
 
-    :param application_file: either 1) a file ending in '.yaml', '.yml', or '.json' that will be converted to the file's string contents by the templating, or
-                                    2) a JSON string defining kubernetes custom_resource_definition of sparkApplication
+    :param application_file: Defines Kubernetes 'custom_resource_definition' of 'sparkApplication' as either a path to a '.json' file or a JSON string.
     :type application_file:  str
     :param namespace: kubernetes namespace to put sparkApplication
     :type namespace: str
