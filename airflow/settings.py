a1cc08974f9bebaceddceb81e67697257c893642
Witold Baryluk <witold.baryluk@gmail.com>
2021-07-23 18:58:22 +0200

    Do not ignore transitive import failures from airflow_local_settings
    A case of `airflow_local_settings.py` importing a module
    that does not exist, will also throw `ModuleNotFoundError`,
    but it should not be silently ignored.

--- airflow/settings.py
+++ airflow/settings.py
@@ -433,8 +433,11 @@ def import_local_settings():
         log.info("Loaded airflow_local_settings from %s .", airflow_local_settings.__file__)
     except ImportError:
         log.warning("Failed to import airflow_local_settings.", exc_info=True)
-    except ModuleNotFoundError:
-        log.debug("No airflow_local_settings to import.", exc_info=True)
+    except ModuleNotFoundError as e:
+        if e.name == "airflow_local_settings":
+            log.debug("No airflow_local_settings to import.", exc_info=True)
+        else:
+            log.warning("Failed to import airflow_local_settings due to a transitive module not found error.", exc_info=True)
 
 
 def initialize():
