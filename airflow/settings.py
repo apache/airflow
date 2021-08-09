7572a39261bd4eb4491f709d054604e7ad8248d1
Lionel <yongliang.zhao@happyelements.com>
2021-08-09 17:47:06 +0800

    localize the dag_run

--- airflow/settings.py
+++ airflow/settings.py
@@ -51,6 +51,8 @@ except Exception:
     pass
 log.info("Configured default timezone %s", TIMEZONE)
 
+LOCALIZE_RUN_ID = conf.get("core", "localize_dag_run_id", fallback=False)
+
 
 HEADER = '\n'.join(
     [
