4d4d1fff0db6d0a7c40ae445c7496959f9607be8
Lionel <yongliang.zhao@happyelements.com>
2021-08-10 17:34:27 +0800

    rollback the changes in settings.py

--- airflow/settings.py
+++ airflow/settings.py
@@ -50,7 +50,6 @@ except Exception:
     pass
 log.info("Configured default timezone %s", TIMEZONE)
 
-LOCALIZE_RUN_ID = conf.get("core", "localize_dag_run_id", fallback=False)
 
 
 HEADER = '\n'.join(
