acf3af427d6d8f83aa757995279e7a590151bad0
Jarek Potiuk <potiuk@apache.org>
2021-08-10 09:57:44 +0200

    Update airflow/providers/ssh/operators/ssh.py
    Co-authored-by: Tzu-ping Chung <uranusjr@gmail.com>

--- airflow/providers/ssh/operators/ssh.py
+++ airflow/providers/ssh/operators/ssh.py
@@ -151,11 +151,11 @@ class SSHOperator(BaseOperator):
                         stdout.channel.shutdown_read()
                         try:
                             stdout.channel.close()
-                        except Exception as e:
+                        except Exception:
                             # there is a race that when shutdown_read has been called and when
                             # you try to close the connection, the socket is already closed
                             # We should ignore such errors (but we should log them with warning)
-                            self.log.warning("Ignoring exception on close: ", e)
+                            self.log.warning("Ignoring exception on close", exc_info=True)
                         break
 
                 stdout.close()
