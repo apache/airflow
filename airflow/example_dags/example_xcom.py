be7fb19a123c8dd7478011a4eef04346224b9a8b
LionelZhao28 <yongliang.zhao@happyelements.com>
2021-08-20 10:51:23 +0800

    add bash examples in the demo example_xcom

--- airflow/example_dags/example_xcom.py
+++ airflow/example_dags/example_xcom.py
@@ -17,6 +17,8 @@
 # under the License.
 
 """Example DAG demonstrating the usage of XComs."""
+from airflow.operators.bash import BashOperator
+
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
