df9291c821e55633d49a8809d254912562a002b6
Ephraim Anierobi <splendidzigy24@gmail.com>
2021-09-15 10:32:46 +0100

    Experiment: Reduce password lenghth got create_user

--- tests/www/views/conftest.py
+++ tests/www/views/conftest.py
@@ -119,7 +119,7 @@ def client_factory(app):
     def factory(name, role_name, permissions):
         create_user(app, username=name, role_name=role_name, permissions=permissions)
         client = app.test_client()
-        resp = client.post("/login/", data={"username": name, "password": name})
+        resp = client.post("/login/", data={"username": name, "password": 'mypassword'})
         assert resp.status_code == 302
         return client
 
