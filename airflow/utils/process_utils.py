apache/spark

commit e25eea052fd54c94b490a377de05c6bae4c24dbb

Inclusive Language (#18349)

airflow/utils/process_utils.py
@@ -159,19 +159,19 @@ def execute_interactive(cmd: List[str], **kwargs):
     tty.setraw(sys.stdin.fileno())
 
     # open pseudo-terminal to interact with subprocess
-    master_fd, slave_fd = pty.openpty()
+    primary_fd, secondary_fd = pty.openpty()
     try:
         # use os.setsid() make it run in a new process group, or bash job control will not be enabled
         with subprocess.Popen(
-            cmd, stdin=slave_fd, stdout=slave_fd, stderr=slave_fd, universal_newlines=True, **kwargs
+            cmd, stdin=secondary_fd, stdout=secondary_fd, stderr=secondary_fd, universal_newlines=True, **kwargs
         ) as proc:
             while proc.poll() is None:
-                readable_fbs, _, _ = select.select([sys.stdin, master_fd], [], [])
+                readable_fbs, _, _ = select.select([sys.stdin, primary_fd], [], [])
                 if sys.stdin in readable_fbs:
                     input_data = os.read(sys.stdin.fileno(), 10240)
-                    os.write(master_fd, input_data)
-                if master_fd in readable_fbs:
-                    output_data = os.read(master_fd, 10240)
+                    os.write(primary_fd, input_data)
+                if primary_fd in readable_fbs:
+                    output_data = os.read(primary_fd, 10240)
                     if output_data:
                         os.write(sys.stdout.fileno(), output_data)
     finally:

airflow/models/serialized_dag.py
@@ -160,7 +160,7 @@ class SerializedDagModel(Base):
             log.debug("Deserializing DAG: %s", row.dag_id)
             dag = row.dag
 
-            # Sanity check.
+            # Coherence check
             if dag.dag_id == row.dag_id:
                 dags[row.dag_id] = dag
             else:

airflow/providers/amazon/aws/example_dags/example_emr_job_flow_automatic_steps.py
@@ -44,7 +44,7 @@ JOB_FLOW_OVERRIDES = {
     'Instances': {
         'InstanceGroups': [
             {
-                'Name': 'Master node',
+                'Name': 'Primary node',
                 'Market': 'SPOT',
                 'InstanceRole': 'MASTER',
                 'InstanceType': 'm1.medium',

airflow/providers/amazon/aws/example_dags/example_emr_job_flow_manual_steps.py
@@ -48,7 +48,7 @@ JOB_FLOW_OVERRIDES = {
     'Instances': {
         'InstanceGroups': [
             {
-                'Name': 'Master node',
+                'Name': 'Primary node',
                 'Market': 'SPOT',
                 'InstanceRole': 'MASTER',
                 'InstanceType': 'm1.medium',

airflow/providers/google/cloud/hooks/cloud_memorystore.py
@@ -291,7 +291,7 @@ class CloudMemorystoreHook(GoogleBaseHook):
         metadata: Optional[Sequence[Tuple[str, str]]] = None,
     ):
         """
-        Initiates a failover of the master node to current replica node for a specific STANDARD tier Cloud
+        Initiates a failover of the primary node to current replica node for a specific STANDARD tier Cloud
         Memorystore for Redis instance.
 
         :param location: The location of the Cloud Memorystore instance (for example europe-west1)

airflow/providers/google/cloud/operators/cloud_memorystore.py
@@ -315,7 +315,7 @@ class CloudMemorystoreExportInstanceOperator(BaseOperator):
 
 class CloudMemorystoreFailoverInstanceOperator(BaseOperator):
     """
-    Initiates a failover of the master node to current replica node for a specific STANDARD tier Cloud
+    Initiates a failover of the primary node to current replica node for a specific STANDARD tier Cloud
     Memorystore for Redis instance.
 
     .. seealso::

airflow/providers/google/cloud/operators/dataproc.py
@@ -135,14 +135,14 @@ class ClusterGenerator:
     :type optional_components: list[str]
     :param num_masters: The # of master nodes to spin up
     :type num_masters: int
-    :param master_machine_type: Compute engine machine type to use for the master node
+    :param master_machine_type: Compute engine machine type to use for the primary node
     :type master_machine_type: str
-    :param master_disk_type: Type of the boot disk for the master node
+    :param master_disk_type: Type of the boot disk for the primary node
         (default is ``pd-standard``).
         Valid values: ``pd-ssd`` (Persistent Disk Solid State Drive) or
         ``pd-standard`` (Persistent Disk Hard Disk Drive).
     :type master_disk_type: str
-    :param master_disk_size: Disk size for the master node
+    :param master_disk_size: Disk size for the primary node
     :type master_disk_size: int
     :param worker_machine_type: Compute engine machine type to use for the worker nodes
     :type worker_machine_type: str

airflow/providers/yandex/operators/yandexcloud_dataproc.py
@@ -49,7 +49,7 @@ class DataprocCreateClusterOperator(BaseOperator):
                                Service account can be created inside the folder.
     :type service_account_id: Optional[str]
     :param masternode_resource_preset: Resources preset (CPU+RAM configuration)
-                                       for the master node of the cluster.
+                                       for the primary node of the cluster.
     :type masternode_resource_preset: str
     :param masternode_disk_size: Masternode storage size in GiB.
     :type masternode_disk_size: int

apache/airflow

commit 204edee4057112f32e265ba40964cc17dcd46913

Fix broken static check on main
This got broken in https://github.com/apache/airflow/pull/18349

airflow/utils/process_utils.py
@@ -163,7 +163,12 @@ def execute_interactive(cmd: List[str], **kwargs):
     try:
         # use os.setsid() make it run in a new process group, or bash job control will not be enabled
         with subprocess.Popen(
-            cmd, stdin=secondary_fd, stdout=secondary_fd, stderr=secondary_fd, universal_newlines=True, **kwargs
+            cmd,
+            stdin=secondary_fd,
+            stdout=secondary_fd,
+            stderr=secondary_fd,
+            universal_newlines=True,
+            **kwargs,
         ) as proc:
             while proc.poll() is None:
                 readable_fbs, _, _ = select.select([sys.stdin, primary_fd], [], [])