a23c11034b24d0b2a9310ca5b066d019f6490026
James Timmins <james@astronomer.io>
2021-09-20 18:01:14 -0700

    Add line to docblock so doc build passes.

--- airflow/www/fab_security/sqla/manager.py
+++ airflow/www/fab_security/sqla/manager.py
@@ -401,6 +401,7 @@ class SecurityManager(BaseSecurityManager):
     def add_view_menu(self, name):
         """
         Adds a view or menu to the backend, model view_menu
+
         :param name:
             name of the view menu to add
         """
