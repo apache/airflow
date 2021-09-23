8b0da7701f55626439960614d0f1f7e9866c19fe
Weichung Shaw <weichung.shaw@gmail.com>
2021-09-23 14:17:40 +0100

    allow per-component custom pod annotations

--- chart/tests/test_annotations.py
+++ chart/tests/test_annotations.py
@@ -45,6 +45,14 @@ CUSTOM_ANNOTATION_VALUES = (
     "redis",
 )
 
+COMPONENTS_SUPPORTING_CUSTOM_POD_ANNOTATIONS = (
+    "scheduler",
+    "webserver",
+    "worker",
+    "flower",
+    "triggerer",
+)
+
 
 class AnnotationsTest(unittest.TestCase):
     def test_service_account_annotations(self):
@@ -136,3 +144,112 @@ class AnnotationsTest(unittest.TestCase):
             list_of_annotation_values_in_objects,
             CUSTOM_ANNOTATION_VALUES,
         )
+
+    def test_per_component_custom_annotations(self):
+        release_name = "RELEASE_NAME"
+
+        k8s_objects = render_chart(
+            name=release_name,
+            values={
+                "scheduler": {
+                    "podAnnotations": {
+                        "example": "scheduler",
+                    },
+                },
+                "webserver": {
+                    "podAnnotations": {
+                        "example": "webserver",
+                    },
+                },
+                "workers": {
+                    "podAnnotations": {
+                        "example": "worker",
+                    },
+                },
+                "flower": {
+                    "podAnnotations": {
+                        "example": "flower",
+                    },
+                },
+                "airflowVersion": "2.2.0",  # Needed for triggerer to be enabled.
+                "triggerer": {
+                    "podAnnotations": {
+                        "example": "triggerer",
+                    },
+                },
+            },
+            show_only=[
+                "templates/scheduler/scheduler-deployment.yaml",
+                "templates/workers/worker-deployment.yaml",
+                "templates/webserver/webserver-deployment.yaml",
+                "templates/flower/flower-deployment.yaml",
+                "templates/triggerer/triggerer-deployment.yaml",
+            ],
+        )
+
+        # The test relies on the convention that the Deployment name
+        # is always `{ Release.Name }-<component_name>`.
+        obj_by_component_name = {
+            obj["metadata"]["name"].replace(release_name + "-", ""): obj for obj in k8s_objects
+        }
+
+        self.assertCountEqual(obj_by_component_name, COMPONENTS_SUPPORTING_CUSTOM_POD_ANNOTATIONS)
+
+        for component_name, obj in obj_by_component_name.items():
+            self.assertIn("example", obj["spec"]["template"]["metadata"]["annotations"])
+            self.assertEqual(component_name, obj["spec"]["template"]["metadata"]["annotations"]["example"])
+
+    def test_per_component_custom_annotations_precedence(self):
+        release_name = "RELEASE_NAME"
+
+        k8s_objects = render_chart(
+            name=release_name,
+            values={
+                "airflowPodAnnotations": {"example": "GLOBAL"},
+                "scheduler": {
+                    "podAnnotations": {
+                        "example": "scheduler",
+                    },
+                },
+                "webserver": {
+                    "podAnnotations": {
+                        "example": "webserver",
+                    },
+                },
+                "workers": {
+                    "podAnnotations": {
+                        "example": "worker",
+                    },
+                },
+                "flower": {
+                    "podAnnotations": {
+                        "example": "flower",
+                    },
+                },
+                "airflowVersion": "2.2.0",  # Needed for triggerer to be enabled.
+                "triggerer": {
+                    "podAnnotations": {
+                        "example": "triggerer",
+                    },
+                },
+            },
+            show_only=[
+                "templates/scheduler/scheduler-deployment.yaml",
+                "templates/workers/worker-deployment.yaml",
+                "templates/webserver/webserver-deployment.yaml",
+                "templates/flower/flower-deployment.yaml",
+                "templates/triggerer/triggerer-deployment.yaml",
+            ],
+        )
+
+        # The test relies on the convention that the Deployment name
+        # is always `{ Release.Name }-<component_name>`.
+        obj_by_component_name = {
+            obj["metadata"]["name"].replace(release_name + "-", ""): obj for obj in k8s_objects
+        }
+
+        self.assertCountEqual(obj_by_component_name, COMPONENTS_SUPPORTING_CUSTOM_POD_ANNOTATIONS)
+
+        for component_name, obj in obj_by_component_name.items():
+            self.assertIn("example", obj["spec"]["template"]["metadata"]["annotations"])
+            self.assertEqual(component_name, obj["spec"]["template"]["metadata"]["annotations"]["example"])
