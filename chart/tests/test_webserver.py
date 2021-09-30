74191160e80d3852fb70b1bfc71963de060be220
Kimyen Ladia <kimyen@github.com>
2021-09-30 15:21:18 -0700

    Update chart/tests/test_webserver.py
    Co-authored-by: Jed Cunningham <66968678+jedcunningham@users.noreply.github.com>

--- chart/tests/test_webserver.py
+++ chart/tests/test_webserver.py
@@ -258,11 +258,11 @@ class WebserverDeploymentTest(unittest.TestCase):
 
         # pod_template_file config
         assert pod_template_file_expected == (
-                {
-                    "name": "config",
-                    "mountPath": "/opt/airflow/pod_template_file.yaml",
-                }
-                in jmespath.search("spec.template.spec.containers[0].volumeMounts", docs[0])
+            {
+                "name": "config",
+                "mountPath": "/opt/airflow/pod_template_file.yaml",
+            }
+            in jmespath.search("spec.template.spec.containers[0].volumeMounts", docs[0])
         )
 
     def test_webserver_resources_are_configurable(self):
