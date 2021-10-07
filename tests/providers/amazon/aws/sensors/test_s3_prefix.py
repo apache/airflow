fe55301f735f5370ef5e143a56d774ca816bf39c
Anay Nayak <anayak007@gmail.com>
2021-10-07 19:06:44 +0530

    Add test to validate s3prefixsensor with multiple prefixes

--- tests/providers/amazon/aws/sensors/test_s3_prefix.py
+++ tests/providers/amazon/aws/sensors/test_s3_prefix.py
@@ -18,6 +18,7 @@
 
 import unittest
 from unittest import mock
+from unittest.mock import call
 
 from airflow.providers.amazon.aws.sensors.s3_prefix import S3PrefixSensor
 
@@ -35,3 +36,30 @@ class TestS3PrefixSensor(unittest.TestCase):
 
         mock_hook.return_value.check_for_prefix.return_value = True
         assert op.poke({})
+
+
+@mock.patch('airflow.providers.amazon.aws.sensors.s3_prefix.S3Hook')
+def test_poke_should_check_multiple_prefixes(mock_hook):
+    op = S3PrefixSensor(task_id='s3_prefix', bucket_name='bucket', prefix=['prefix1', 'prefix2'])
+
+    mock_hook.return_value.check_for_prefix.return_value = False
+    assert not op.poke({}), "poke returns false when the prefixes do not exist"
+
+    mock_hook.return_value.check_for_prefix.assert_has_calls(
+        calls=[
+            call(prefix='prefix1', delimiter='/', bucket_name='bucket'),
+        ]
+    )
+
+    mock_hook.return_value.check_for_prefix.side_effect = [True, False]
+    assert not op.poke({}), "poke returns false when only some of the prefixes exist"
+
+    mock_hook.return_value.check_for_prefix.side_effect = [True, True]
+    assert op.poke({}), "poke returns true when both prefixes exist"
+
+    mock_hook.return_value.check_for_prefix.assert_has_calls(
+        calls=[
+            call(prefix='prefix1', delimiter='/', bucket_name='bucket'),
+            call(prefix='prefix2', delimiter='/', bucket_name='bucket'),
+        ]
+    )
