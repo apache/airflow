cd5e8abc4835eae857d4bdf6c2e9b87d9d2a5d0e
xuans <xuan_shen@outlook.com>
2021-10-22 10:51:50 -0700

    pass timeout to CassandraToGCSOperator

--- tests/providers/google/cloud/transfers/test_cassandra_to_gcs.py
+++ tests/providers/google/cloud/transfers/test_cassandra_to_gcs.py
@@ -34,6 +34,7 @@ class TestCassandraToGCS(unittest.TestCase):
         schema = "schema.json"
         filename = "data.json"
         gzip = True
+        query_timeout = 20
         mock_tempfile.return_value.name = TMP_FILE_NAME
 
         operator = CassandraToGCSOperator(
@@ -43,6 +44,7 @@ class TestCassandraToGCS(unittest.TestCase):
             filename=filename,
             schema_filename=schema,
             gzip=gzip,
+            query_timeout=query_timeout,
         )
         operator.execute(None)
         mock_hook.return_value.get_conn.assert_called_once_with()
