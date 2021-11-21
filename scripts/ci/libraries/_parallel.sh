2f48f8fd467ca05cde8c44f36775bf5e36a44434
khalidmammadov <xmamedov@hotmail.com>
2021-11-21 15:25:41 +0000

    Absolute address

--- scripts/ci/libraries/_parallel.sh
+++ scripts/ci/libraries/_parallel.sh
@@ -21,7 +21,7 @@
 
 
 function parallel::initialize_monitoring() {
-    PARALLEL_MONITORED_DIR="${AIRFLOW_SOURCES}/files/container_logs$(mktemp -d)"
+    PARALLEL_MONITORED_DIR="$(pwd)/files/container_logs$(mktemp -d)"
     export PARALLEL_MONITORED_DIR
 
     PARALLEL_TAIL_LENGTH=${PARALLEL_TAIL_LENGTH:=2}
