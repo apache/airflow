apache/spark

commit 621d17bb77e3160c1a927803e5d190c0e2aade3c

Tests for Docker images in Python (#19737)

scripts/ci/libraries/_build_images.sh
@@ -942,7 +942,8 @@ function build_images::build_prod_images_from_locally_built_airflow_packages() {
     build_images::cleanup_docker_context_files
 
     # Build necessary provider packages
-    runs::run_prepare_provider_packages "${INSTALLED_PROVIDERS[@]}"
+    IFS=$'\n' read -d '' -r -a installed_providers < "${AIRFLOW_SOURCES}/scripts/ci/installed_providers.txt"
+    runs::run_prepare_provider_packages "${installed_providers[@]}"
     mv "${AIRFLOW_SOURCES}/dist/"* "${AIRFLOW_SOURCES}/docker-context-files/"
 
     # Build apache airflow packages

.github/CODEOWNERS
@@ -50,8 +50,9 @@
 /.github/workflows/ @potiuk @ashb @kaxil
 breeze @potiuk
 breeze-complete @potiuk
-Dockerfile @potiuk @ashb
+Dockerfile @potiuk @ashb @mik-laj
 Dockerfile.ci @potiuk @ashb
 /dev/ @potiuk @ashb @kaxil
 /provider_packages/ @potiuk @ashb
-/scripts/ @potiuk @ashb
+/scripts/ @potiuk @ashb @mik-laj
+/docker_tests/ @potiuk @ashb @mik-laj

.github/boring-cyborg.yml
@@ -190,8 +190,8 @@ labelPRBasedOnFilePath:
     - Dockerfile
     - docs/docker-stack/**/*
     - scripts/in_container/prod/*
-    - scripts/ci/tools/verify_docker_image.sh
     - scripts/ci/libraries/_verify_image.sh
+    - docker_tests/**/*
 
 # Various Flags to control behaviour of the "Labeler"
 labelerFlags:

.pre-commit-config.yaml
@@ -238,6 +238,7 @@ repos:
           ^scripts/.*\.py$|
           ^dev|
           ^provider_packages|
+          ^docker_tests|
           ^kubernetes_tests|
           .*example_dags/.*|
           ^chart/.*\.py$|

docker_tests/__init__.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -15,15 +14,3 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-
-function verify_image::verify_prod_image {
-    DOCKER_IMAGE="${1}"
-    export DOCKER_IMAGE
-    python3 "${SCRIPTS_CI_DIR}/images/ci_run_docker_tests.py" "${AIRFLOW_SOURCES}/docker_tests/prod_image.py"
-}
-
-function verify_image::verify_ci_image {
-    DOCKER_IMAGE="${1}"
-    export DOCKER_IMAGE
-    python3 "${SCRIPTS_CI_DIR}/images/ci_run_docker_tests.py" "${AIRFLOW_SOURCES}/docker_tests/ci_image.py"
-}

docker_tests/ci_image.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -16,14 +15,27 @@
 # specific language governing permissions and limitations
 # under the License.
 
-function verify_image::verify_prod_image {
-    DOCKER_IMAGE="${1}"
-    export DOCKER_IMAGE
-    python3 "${SCRIPTS_CI_DIR}/images/ci_run_docker_tests.py" "${AIRFLOW_SOURCES}/docker_tests/prod_image.py"
-}
+import subprocess
 
-function verify_image::verify_ci_image {
-    DOCKER_IMAGE="${1}"
-    export DOCKER_IMAGE
-    python3 "${SCRIPTS_CI_DIR}/images/ci_run_docker_tests.py" "${AIRFLOW_SOURCES}/docker_tests/ci_image.py"
-}
+from docker_tests.docker_tests_utils import (
+    display_dependency_conflict_message,
+    docker_image,
+    run_bash_in_docker,
+    run_command,
+)
+
+
+class TestFiles:
+    def test_dist_folder_should_exists(self):
+        run_bash_in_docker('[ -f /opt/airflow/airflow/www/static/dist/manifest.json ] || exit 1')
+
+
+class TestPythonPackages:
+    def test_pip_dependencies_conflict(self):
+        try:
+            run_command(
+                ["docker", "run", "--rm", "--entrypoint", "/bin/bash", docker_image, "-c", 'pip check']
+            )
+        except subprocess.CalledProcessError as ex:
+            display_dependency_conflict_message()
+            raise ex

docker_tests/docker_tests_utils.py
@@ -1,0 +1,116 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import os
+import shlex
+import subprocess
+from pathlib import Path
+from typing import List
+
+docker_image = os.environ.get('DOCKER_IMAGE')
+SOURCE_ROOT = Path(__file__).resolve().parents[1]
+
+if not docker_image:
+    raise Exception("The DOCKER_IMAGE environment variable is required")
+
+
+def run_command(cmd: List[str], print_output_on_error: bool = True, **kwargs):
+    print(f"$ {' '.join(shlex.quote(c) for c in cmd)}")
+    try:
+        return subprocess.check_output(cmd, **kwargs).decode()
+    except subprocess.CalledProcessError as ex:
+        if print_output_on_error:
+            print("========================= OUTPUT start ============================")
+            print(ex.stderr)
+            print(ex.stdout)
+            print("========================= OUTPUT end ============================")
+        raise
+
+
+def run_bash_in_docker(bash_script, **kwargs):
+    docker_command = [
+        "docker",
+        "run",
+        "--rm",
+        "-e",
+        "COLUMNS=180",
+        "--entrypoint",
+        "/bin/bash",
+        docker_image,
+        "-c",
+        bash_script,
+    ]
+    return run_command(docker_command, **kwargs)
+
+
+def run_python_in_docker(python_script, **kwargs):
+    docker_command = [
+        "docker",
+        "run",
+        "--rm",
+        "-e",
+        "COLUMNS=180",
+        "-e",
+        "PYTHONDONTWRITEBYTECODE=true",
+        docker_image,
+        "python",
+        "-c",
+        python_script,
+    ]
+    return run_command(docker_command, **kwargs)
+
+
+def display_dependency_conflict_message():
+    print(
+        """
+***** Beginning of the instructions ****
+
+The image did not pass 'pip check' verification. This means that there are some conflicting dependencies
+in the image.
+
+It can mean one of those:
+
+1) The main is currently broken (other PRs will fail with the same error)
+2) You changed some dependencies in setup.py or setup.cfg and they are conflicting.
+
+
+
+In case 1) - apologies for the trouble.Please let committers know and they will fix it. You might
+be asked to rebase to the latest main after the problem is fixed.
+
+In case 2) - Follow the steps below:
+
+* try to build CI and then PROD image locally with breeze, adding --upgrade-to-newer-dependencies flag
+  (repeat it for all python versions)
+
+CI image:
+
+     ./breeze build-image --upgrade-to-newer-dependencies --python 3.6
+
+Production image:
+
+     ./breeze build-image --production-image --upgrade-to-newer-dependencies --python 3.6
+
+* You will see error messages there telling which requirements are conflicting and which packages caused the
+  conflict. Add the limitation that caused the conflict to EAGER_UPGRADE_ADDITIONAL_REQUIREMENTS
+  variable in Dockerfile.ci. Note that the limitations might be different for Dockerfile.ci and Dockerfile
+  because not all packages are installed by default in the PROD Dockerfile. So you might find that you
+  only need to add the limitation to the Dockerfile.ci
+
+***** End of the instructions ****
+"""
+    )

docker_tests/prod_image.py
@@ -1,0 +1,206 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import json
+import subprocess
+import tempfile
+from pathlib import Path
+
+import pytest
+
+from docker_tests.docker_tests_utils import (
+    SOURCE_ROOT,
+    display_dependency_conflict_message,
+    docker_image,
+    run_bash_in_docker,
+    run_command,
+    run_python_in_docker,
+)
+
+INSTALLED_PROVIDER_PATH = SOURCE_ROOT / "scripts" / "ci" / "installed_providers.txt"
+
+
+class TestCommands:
+    def test_without_command(self):
+        """Checking the image without a command. It should return non-zero exit code."""
+        with pytest.raises(subprocess.CalledProcessError) as ctx:
+            run_command(["docker", "run", "--rm", "-e", "COLUMNS=180", docker_image])
+        assert 2 == ctx.value.returncode
+
+    def test_airflow_command(self):
+        """Checking 'airflow' command  It should return non-zero exit code."""
+        with pytest.raises(subprocess.CalledProcessError) as ctx:
+            run_command(["docker", "run", "--rm", "-e", "COLUMNS=180", docker_image, "airflow"])
+        assert 2 == ctx.value.returncode
+
+    def test_airflow_version(self):
+        """Checking 'airflow version' command  It should return zero exit code."""
+        output = run_command(
+            ["docker", "run", "--rm", "-e", "COLUMNS=180", docker_image, "airflow", "version"]
+        )
+        assert "2." in output
+
+    def test_python_version(self):
+        """Checking 'python --version' command  It should return zero exit code."""
+        output = run_command(
+            ["docker", "run", "--rm", "-e", "COLUMNS=180", docker_image, "python", "--version"]
+        )
+        assert "Python 3." in output
+
+    def test_bash_version(self):
+        """Checking 'bash --version' command  It should return zero exit code."""
+        output = run_command(
+            ["docker", "run", "--rm", "-e", "COLUMNS=180", docker_image, "bash", "--version"]
+        )
+        assert "GNU bash," in output
+
+
+class TestPythonPackages:
+    def test_required_providers_are_installed(self):
+        lines = (d.strip() for d in INSTALLED_PROVIDER_PATH.read_text().splitlines())
+        lines = (d for d in lines)
+        packages_to_install = {f"apache-airflow-providers-{d.replace('.', '-')}" for d in lines}
+        assert len(packages_to_install) != 0
+
+        output = run_bash_in_docker("airflow providers list --output json", stderr=subprocess.DEVNULL)
+        providers = json.loads(output)
+        packages_installed = {d['package_name'] for d in providers}
+        assert len(packages_installed) != 0
+
+        assert packages_to_install == packages_installed, (
+            f"List of expected installed packages and image content mismatch. "
+            f"Check {INSTALLED_PROVIDER_PATH} file."
+        )
+
+    def test_pip_dependencies_conflict(self):
+        try:
+            run_bash_in_docker("pip check")
+        except subprocess.CalledProcessError as ex:
+            display_dependency_conflict_message()
+            raise ex
+
+    PACKAGE_IMPORTS = {
+        "amazon": ["boto3", "botocore", "watchtower"],
+        "async": ["gevent", "eventlet", "greenlet"],
+        "azure": [
+            'azure.batch',
+            'azure.cosmos',
+            'azure.datalake.store',
+            'azure.identity',
+            'azure.keyvault',
+            'azure.kusto.data',
+            'azure.mgmt.containerinstance',
+            'azure.mgmt.datalake.store',
+            'azure.mgmt.resource',
+            'azure.storage',
+        ],
+        "celery": ["celery", "flower", "vine"],
+        "cncf.kubernetes": ["kubernetes", "cryptography"],
+        "dask": ["cloudpickle", "distributed"],
+        "docker": ["docker"],
+        "elasticsearch": ["elasticsearch", "es.elastic", "elasticsearch_dsl"],
+        "google": [
+            'OpenSSL',
+            'google.ads',
+            'googleapiclient',
+            'google.auth',
+            'google_auth_httplib2',
+            'google.cloud.automl',
+            'google.cloud.bigquery_datatransfer',
+            'google.cloud.bigtable',
+            'google.cloud.container',
+            'google.cloud.datacatalog',
+            'google.cloud.dataproc',
+            'google.cloud.dlp',
+            'google.cloud.kms',
+            'google.cloud.language',
+            'google.cloud.logging',
+            'google.cloud.memcache',
+            'google.cloud.monitoring',
+            'google.cloud.oslogin',
+            'google.cloud.pubsub',
+            'google.cloud.redis',
+            'google.cloud.secretmanager',
+            'google.cloud.spanner',
+            'google.cloud.speech',
+            'google.cloud.storage',
+            'google.cloud.tasks',
+            'google.cloud.texttospeech',
+            'google.cloud.translate',
+            'google.cloud.videointelligence',
+            'google.cloud.vision',
+        ],
+        "grpc": ["grpc", "google.auth", "google_auth_httplib2"],
+        "hashicorp": ["hvac"],
+        "ldap": ["ldap"],
+        "mysql": ["mysql"],
+        "postgres": ["psycopg2"],
+        "pyodbc": ["pyodbc"],
+        "redis": ["redis"],
+        "sendgrid": ["sendgrid"],
+        "sftp/ssh": ["paramiko", "pysftp", "sshtunnel"],
+        "slack": ["slack_sdk"],
+        "statsd": ["statsd"],
+        "virtualenv": ["virtualenv"],
+    }
+
+    @pytest.mark.parametrize("package_name,import_names", PACKAGE_IMPORTS.items())
+    def test_check_dependencies_imports(self, package_name, import_names):
+        run_python_in_docker(f"import {','.join(import_names)}")
+
+
+class TestExecuteAsRoot:
+    def test_execute_airflow_as_root(self):
+        run_command(
+            [
+                "docker",
+                "run",
+                "--rm",
+                "--user",
+                "0",
+                "-e",
+                "PYTHONDONTWRITEBYTECODE=true",
+                docker_image,
+                "airflow",
+                "info",
+            ]
+        )
+
+    def test_run_custom_python_packages_as_root(self):
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            (Path(tmp_dir) / "__init__.py").write_text('')
+            (Path(tmp_dir) / "awesome.py").write_text('print("Awesome")')
+
+            run_command(
+                [
+                    "docker",
+                    "run",
+                    "--rm",
+                    "-e",
+                    f"PYTHONPATH={tmp_dir}",
+                    "-e",
+                    "PYTHONDONTWRITEBYTECODE=true",
+                    "-v",
+                    f"{tmp_dir}:{tmp_dir}",
+                    "--user",
+                    "0",
+                    docker_image,
+                    "python",
+                    "-c",
+                    "import awesome",
+                ]
+            )

apache/airflow

commit c9ad3ef663c421f241cf62986c9ffcc2616812ee

Fixes failure of image building
The read command introduced in #19737 returned non zer error code
on encountering EOF, and our bash script fail on that. This PR
makes sure that the return code is not taken into account for that
command.

scripts/ci/libraries/_build_images.sh
@@ -942,7 +942,7 @@ function build_images::build_prod_images_from_locally_built_airflow_packages() {
     build_images::cleanup_docker_context_files
 
     # Build necessary provider packages
-    IFS=$'\n' read -d '' -r -a installed_providers < "${AIRFLOW_SOURCES}/scripts/ci/installed_providers.txt"
+    IFS=$'\n' read -d '' -r -a installed_providers < "${AIRFLOW_SOURCES}/scripts/ci/installed_providers.txt" || true
     runs::run_prepare_provider_packages "${installed_providers[@]}"
     mv "${AIRFLOW_SOURCES}/dist/"* "${AIRFLOW_SOURCES}/docker-context-files/"
 