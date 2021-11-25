2bd0420654cafeadeff2470b8320c3ffa4c4d5d4
Kamil Breguła <kamilbregula@apache.org>
2021-11-25 03:49:41 +0100

    Rewrite image building tests to Python

--- docker_tests/command_utils.py
+++ docker_tests/command_utils.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -16,14 +15,22 @@
 # specific language governing permissions and limitations
 # under the License.
 
-function verify_image::verify_prod_image {
-    DOCKER_IMAGE="${1}"
-    export DOCKER_IMAGE
-    python3 "${SCRIPTS_CI_DIR}/images/ci_run_docker_tests.py" "${AIRFLOW_SOURCES}/docker_tests/test_prod_image.py"
-}
+import shlex
+import subprocess
+from typing import List
 
-function verify_image::verify_ci_image {
-    DOCKER_IMAGE="${1}"
-    export DOCKER_IMAGE
-    python3 "${SCRIPTS_CI_DIR}/images/ci_run_docker_tests.py" "${AIRFLOW_SOURCES}/docker_tests/test_ci_image.py"
-}
+
+def run_command(cmd: List[str], *, print_output_on_error: bool = True, return_output: bool = False, **kwargs):
+    print(f"$ {' '.join(shlex.quote(c) for c in cmd)}")
+    try:
+        if return_output:
+            return subprocess.check_output(cmd, **kwargs).decode()
+        else:
+            subprocess.run(cmd, check=True, **kwargs)
+    except subprocess.CalledProcessError as ex:
+        if print_output_on_error:
+            print("========================= OUTPUT start ============================")
+            print(ex.stderr)
+            print(ex.stdout)
+            print("========================= OUTPUT end ============================")
+        raise
--- docker_tests/constants.py
+++ docker_tests/constants.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -16,14 +15,6 @@
 # specific language governing permissions and limitations
 # under the License.
 
-function verify_image::verify_prod_image {
-    DOCKER_IMAGE="${1}"
-    export DOCKER_IMAGE
-    python3 "${SCRIPTS_CI_DIR}/images/ci_run_docker_tests.py" "${AIRFLOW_SOURCES}/docker_tests/test_prod_image.py"
-}
+from pathlib import Path
 
-function verify_image::verify_ci_image {
-    DOCKER_IMAGE="${1}"
-    export DOCKER_IMAGE
-    python3 "${SCRIPTS_CI_DIR}/images/ci_run_docker_tests.py" "${AIRFLOW_SOURCES}/docker_tests/test_ci_image.py"
-}
+SOURCE_ROOT = Path(__file__).resolve().parents[1]
