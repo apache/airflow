cb403a50bccb1c1cb28f3bc8c312980f32b7569c
Anudeep Konaboina <anudeep_konaboina@epam.com>
2021-12-02 15:47:06 +0530

    Added logic for SFTPFilePatternSenor

--- airflow/providers/sftp/hooks/sftp.py
+++ airflow/providers/sftp/hooks/sftp.py
@@ -17,7 +17,9 @@
 # under the License.
 """This module contains SFTP hook."""
 import datetime
+import re
 import stat
+import fnmatch
 from typing import Dict, List, Optional, Tuple
 
 import pysftp
@@ -259,6 +261,20 @@ class SFTPHook(SSHHook):
         conn = self.get_conn()
         ftp_mdtm = conn.stat(path).st_mtime
         return datetime.datetime.fromtimestamp(ftp_mdtm).strftime('%Y%m%d%H%M%S')
+
+    def get_file_name_using_pattern(self, path: str, pattern: str) -> str:
+        """
+        Returns True if a file with the given pattern exists
+        :param path: full path to the remote directory
+        :type path: str
+        :param pattern: full path to the remote directory
+        :type pattern: str
+        """
+        conn = self.get_conn()
+        list_of_files = conn.open_sftp().listdir(path)
+        for file in list_of_files:
+            if re.search(file, pattern):
+                return file
 
     def path_exists(self, path: str) -> bool:
         """
--- airflow/providers/sftp/sensors/sftp.py
+++ airflow/providers/sftp/sensors/sftp.py
@@ -54,3 +54,38 @@ class SFTPSensor(BaseSensorOperator):
             return False
         self.hook.close_conn()
         return True
+
+
+class SFTPFilePatternSensor(BaseSensorOperator):
+    """
+        Waits for a file or directory to be present on SFTP.
+
+        :param path: Remote file or directory path
+        :type path: str
+        :param sftp_conn_id: The connection to run the sensor against
+        :type sftp_conn_id: str
+        """
+
+    template_fields = ('path',)
+    file_pattern: Optional[str] = None
+
+    def __init__(self, *, file_path: str, file_pattern: str, sftp_conn_id: str = 'sftp_default', **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.path = file_path
+        self.hook: Optional[SFTPHook] = None
+        self.sftp_conn_id = sftp_conn_id
+        self.file_pattern = file_pattern
+
+    def poke(self, context: dict) -> bool:
+        self.hook = SFTPHook(self.sftp_conn_id)
+        self.log.info('Poking for %s', self.path)
+
+        try:
+            file_name = self.hook.get_file_name_using_pattern(self.path,self.file_pattern)
+            self.log.info('Found File %s', str(file_name))
+        except OSError as e:
+            if e.errno != SFTP_NO_SUCH_FILE:
+                raise e
+            return False
+        self.hook.close_conn()
+        return True
