00a2f86f27585b45ec152e9e45aa77f5edb3713a
khalidmammadov <xmamedov@hotmail.com>
2021-12-05 20:31:12 +0000

    Fixing mypy issues in providers elasticsearch, ADF and mongo hook

--- tests/providers/elasticsearch/log/elasticmock/__init__.py
+++ tests/providers/elasticsearch/log/elasticmock/__init__.py
@@ -42,7 +42,7 @@ from functools import wraps
 from typing import Dict
 from unittest.mock import patch
 
-from elasticsearch.client import _normalize_hosts
+from elasticsearch.client import _normalize_hosts  # type: ignore
 
 from .fake_elasticsearch import FakeElasticsearch
 
