d606d7a58fa4ea2d357685f216818af848e1719c
eskarimov <eskarimov@gmail.com>
2021-12-06 09:04:43 +0100

    Use original task's `start_date` if task continues after deferral

--- airflow/models/taskinstance.py
+++ airflow/models/taskinstance.py
@@ -1207,7 +1207,8 @@ class TaskInstance(Base, LoggingMixin):
             # Attempt 0 for the first attempt).
             # Set the task start date. In case it was re-scheduled use the initial
             # start date that is recorded in task_reschedule table
-            self.start_date = timezone.utcnow()
+            # If the task continues after being deferred (next_method is set), use the original start_date
+            self.start_date = self.start_date if self.next_method else timezone.utcnow()
             if self.state == State.UP_FOR_RESCHEDULE:
                 task_reschedule: TR = TR.query_for_task_instance(self, session=session).first()
                 if task_reschedule:
