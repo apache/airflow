4d5276d6feb8af30eca14cc17b55b435c4582644
Ash Berlin-Taylor <ash@apache.org>
2021-12-08 12:53:17 +0000

    Fix tests

--- tests/task/task_runner/test_base_task_runner.py
+++ tests/task/task_runner/test_base_task_runner.py
@@ -25,9 +25,11 @@ from airflow.task.task_runner.base_task_runner import BaseTaskRunner
 
 
 @pytest.mark.parametrize(["impersonation"], (("nobody",), (None,)))
-@mock.patch('subprocess.call')
+@mock.patch('subprocess.check_call')
 @mock.patch('airflow.task.task_runner.base_task_runner.tmp_configuration_copy')
 def test_config_copy_mode(tmp_configuration_copy, subprocess_call, dag_maker, impersonation):
+    tmp_configuration_copy.return_value = "/tmp/some-string"
+
     with dag_maker("test"):
         BaseOperator(task_id="task_1", run_as_user=impersonation)
 
@@ -45,7 +47,7 @@ def test_config_copy_mode(tmp_configuration_copy, subprocess_call, dag_maker, im
 
     if impersonation:
         subprocess_call.assert_called_with(
-            ['sudo', 'chown', impersonation, tmp_configuration_copy.return_value, runner._error_file], close_fds=True
+            ['sudo', 'chown', impersonation, "/tmp/some-string", runner._error_file.name], close_fds=True
         )
     else:
         subprocess_call.not_assert_called()
