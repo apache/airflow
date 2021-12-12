c805d1015c8e3d046831fe315cfc06704a54a3f0
subkanthi <subkanthi@gmail.com>
2021-12-12 12:54:34 -0500

    Fixed mypy errors in providers/docker

--- airflow/providers/docker/decorators/docker.py
+++ airflow/providers/docker/decorators/docker.py
@@ -62,7 +62,7 @@ class _DockerDecoratedOperator(DecoratedOperator, DockerOperator):
     :type multiple_outputs: bool
     """
 
-    template_fields = ('op_args', 'op_kwargs')
+    template_fields: Iterable[str] = ('op_args', 'op_kwargs')
 
     # since we won't mutate the arguments, we should just do the shallow copy
     # there are some cases we can't deepcopy the objects (e.g protobuf).
@@ -144,7 +144,7 @@ def docker_task(
     return task_decorator_factory(
         python_callable=python_callable,
         multiple_outputs=multiple_outputs,
-        decorated_operator_class=_DockerDecoratedOperator,
+        decorated_operator_class=_DockerDecoratedOperator, # type: ignore
         **kwargs,
     )
 
