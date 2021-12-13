86942ade635bd65c236e7f4f047d0678d1a53f00
Daniel Standish <15932138+dstandish@users.noreply.github.com>
2021-12-13 07:52:12 -0800

    remove unused import

--- tests/utils/test_helpers.py
+++ tests/utils/test_helpers.py
@@ -16,7 +16,7 @@
 # specific language governing permissions and limitations
 # under the License.
 import re
-from itertools import combinations, product
+from itertools import product
 
 import pytest
 
