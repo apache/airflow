03e6e8b9de14f41c021b46397badfa5a34365667
Tzu-ping Chung <tp@astronomer.io>
2021-12-16 19:24:16 +0800

    Use logical date directly in ShortCircuitOperator

--- airflow/operators/python.py
+++ airflow/operators/python.py
@@ -252,11 +252,11 @@ class ShortCircuitOperator(PythonOperator, SkipMixin):
 
         self.log.info('Skipping downstream tasks...')
 
-        downstream_tasks = context['task'].get_flat_relatives(upstream=False)
+        downstream_tasks = context["task"].get_flat_relatives(upstream=False)
         self.log.debug("Downstream task_ids %s", downstream_tasks)
 
         if downstream_tasks:
-            self.skip(context['dag_run'], context['ti'].execution_date, downstream_tasks)
+            self.skip(context["dag_run"], context["logical_date"], downstream_tasks)
 
         self.log.info("Done.")
 
