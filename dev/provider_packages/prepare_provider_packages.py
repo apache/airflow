8be5a75326456285880b52810f41d54cabadfecd
Dmitry Romanenko <Dmitry@Romanenko.in>
2021-12-16 18:25:45 -0500

    Add exception for new deprecated warnings

--- dev/provider_packages/prepare_provider_packages.py
+++ dev/provider_packages/prepare_provider_packages.py
@@ -2166,6 +2166,7 @@ KNOWN_DEPRECATED_DIRECT_IMPORTS: Set[str] = {
     "This module is deprecated. Please use `kubernetes.client.models.V1Volume`.",
     "This module is deprecated. Please use `kubernetes.client.models.V1VolumeMount`.",
     "This module is deprecated. Please use `kubernetes.client.models.V1ResourceRequirements` and `kubernetes.client.models.V1ContainerPort`.",
+    "This module is deprecated. Please use `kubernetes.client.models.V1EnvVar`.",
     'numpy.ufunc size changed, may indicate binary incompatibility. Expected 192 from C header,'
     ' got 216 from PyObject',
     "This module is deprecated. Please use `airflow.providers.amazon.aws.sensors.step_function`.",
