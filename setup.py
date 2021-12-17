61bfe94328a137f3f2f0a4743a8fbf323556b760
Josh Fell <josh.d.fell@astronomer.io>
2021-12-17 16:34:12 -0500

    Fix MyPy errors in airflow/api and airflow/api_connexion

--- setup.py
+++ setup.py
@@ -524,6 +524,7 @@ mypy_dependencies = [
     'types-boto',
     'types-certifi',
     'types-croniter',
+    'types-Deprecated',
     'types-docutils',
     'types-freezegun',
     'types-paramiko',
