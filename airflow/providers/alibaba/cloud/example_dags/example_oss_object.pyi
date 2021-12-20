1340053dece8355e633707ab89813e8bef94f1f0
Josh Fell <josh.d.fell@astronomer.io>
2021-12-20 11:17:49 -0500

    Add stub files for example DAGs

---
+++ airflow/providers/alibaba/cloud/example_dags/example_oss_object.pyi
@@ -0,0 +1,51 @@
+from typing import Optional
+
+
+class OSSUploadObjectOperator:
+    def __init__(
+        self,
+        key: str = ...,
+        file: str = ...,
+        region: str = ...,
+        bucket_name: Optional[str] = None,
+        oss_conn_id: str = 'oss_default',
+        **kwargs,
+    ) -> None:
+        ...
+
+
+class OSSDownloadObjectOperator:
+    def __init__(
+        self,
+        key: str = ...,
+        file: str = ...,
+        region: str = ...,
+        bucket_name: Optional[str] = None,
+        oss_conn_id: str = 'oss_default',
+        **kwargs,
+    ) -> None:
+        ...
+
+
+class OSSDeleteBatchObjectOperator:
+    def __init__(
+        self,
+        keys: list = ...,
+        region: str = ...,
+        bucket_name: Optional[str] = None,
+        oss_conn_id: str = 'oss_default',
+        **kwargs,
+    ) -> None:
+        ...
+
+
+class OSSDeleteObjectOperator:
+    def __init__(
+        self,
+        key: str = ...,
+        region: str = ...,
+        bucket_name: Optional[str] = None,
+        oss_conn_id: str = 'oss_default',
+        **kwargs,
+    ) -> None:
+        ...
