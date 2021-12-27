02e3cd7b9d2753c89eb4a5ffc735111a591b44f8
Sonins <gmlrhks95@gmail.com>
2021-12-27 10:06:48 +0900

    days_ago localized.  * Resolved issue at https://github.com/apache/airflow/issues/17720  * Set the timezone of days_ago return to localized timezone using make_aware.

--- airflow/utils/dates.py
+++ airflow/utils/dates.py
@@ -257,7 +257,8 @@ def days_ago(n, hour=0, minute=0, second=0, microsecond=0):
     Get a datetime object representing `n` days ago. By default the time is
     set to midnight.
     """
-    today = timezone.utcnow().replace(hour=hour, minute=minute, second=second, microsecond=microsecond)
+    today = datetime.utcnow()
+    today = timezone.make_aware(today).replace(hour=hour, minute=minute, second=second, microsecond=microsecond)
     return today - timedelta(days=n)
 
 
