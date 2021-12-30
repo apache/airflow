e8d6499ba62103349c4a66a07c7a275379a6aedd
Sonins <gmlrhks95@gmail.com>
2021-12-30 11:29:30 +0900

    Replace datetime.utcnow() and timezone.make_aware() with datetime.now(timezone.TIMEZONE).

--- airflow/utils/dates.py
+++ airflow/utils/dates.py
@@ -257,8 +257,7 @@ def days_ago(n, hour=0, minute=0, second=0, microsecond=0):
     Get a datetime object representing `n` days ago. By default the time is
     set to midnight.
     """
-    today = datetime.utcnow()
-    today = timezone.make_aware(today).replace(
+    today = datetime.now(timezone.TIMEZONE).replace(
         hour=hour, minute=minute, second=second, microsecond=microsecond
     )
     return today - timedelta(days=n)
