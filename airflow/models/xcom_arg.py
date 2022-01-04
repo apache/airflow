16df4abeda17b4262f9bb44f25c7fad0a3edf324
Ash Berlin-Taylor <ash@apache.org>
2022-01-04 14:52:25 +0000

    fixup! Add mapping and partial support to TaskFlow tasks

--- airflow/models/xcom_arg.py
+++ airflow/models/xcom_arg.py
@@ -14,7 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-from typing import Any, List, Optional, Sequence, Union
+from typing import TYPE_CHECKING, Any, List, Optional, Sequence, Union
 
 from airflow.exceptions import AirflowException
 from airflow.models.baseoperator import BaseOperator, MappedOperator
