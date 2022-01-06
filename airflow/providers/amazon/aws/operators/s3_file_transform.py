8debbd441ebbf2e330a5c784b2bae4b4b894489c
Ankit Chaurasia <8670962+sunank200@users.noreply.github.com>
2022-01-06 19:03:12 +0545

    Fix mypy errors for providers amazon s3 operators

--- airflow/providers/amazon/aws/operators/s3_file_transform.py
+++ airflow/providers/amazon/aws/operators/s3_file_transform.py
@@ -19,7 +19,7 @@
 
 import warnings
 
-from airflow.providers.amazon.aws.operators.s3 import S3FileTransformOperator  # noqa
+from airflow.providers.amazon.aws.operators.s3 import S3FileTransformOperator  # type: ignore # noqa
 
 warnings.warn(
     "This module is deprecated. Please use `airflow.providers.amazon.aws.operators.s3`.",
--- airflow/providers/amazon/aws/operators/s3_list_prefixes.py
+++ airflow/providers/amazon/aws/operators/s3_list_prefixes.py
@@ -19,7 +19,7 @@
 
 import warnings
 
-from airflow.providers.amazon.aws.operators.s3 import S3ListPrefixesOperator  # noqa
+from airflow.providers.amazon.aws.operators.s3 import S3ListPrefixesOperator  # type: ignore # noqa
 
 warnings.warn(
     "This module is deprecated. Please use `airflow.providers.amazon.aws.operators.s3`.",
