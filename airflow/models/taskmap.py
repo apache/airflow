02b6bb44867f06c14d4b1f555c5fde92e75f72d8
Tzu-ping Chung <uranusjr@gmail.com>
2022-01-14 13:17:28 +0800

    Clarify TaskMap costring
    Co-authored-by: Ash Berlin-Taylor <ash_github@firemirror.com>

--- airflow/models/taskmap.py
+++ airflow/models/taskmap.py
@@ -16,7 +16,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
-"""Table to store "mapped" task instances (AIP-42)."""
+"""Table to store information about mapped task instances (AIP-42)."""
 
 import collections.abc
 import enum
