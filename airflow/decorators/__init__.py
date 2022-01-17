b04f69471f931ade85d4ec56357665546fc660df
Tzu-ping Chung <tp@astronomer.io>
2022-01-17 15:04:07 +0800

    Reduce autocompletion hacks
    Hard-code declarations into pyi instead fo simply things.

--- airflow/decorators/__init__.py
+++ airflow/decorators/__init__.py
@@ -15,8 +15,6 @@
 # specific language governing permissions and limitations
 # under the License.
 
-from typing import TYPE_CHECKING, Callable, Optional, TypeVar, overload
-
 from airflow.decorators.base import TaskDecorator
 from airflow.decorators.python import python_task
 from airflow.decorators.python_virtualenv import virtualenv_task
@@ -26,45 +24,23 @@ from airflow.providers_manager import ProvidersManager
 
 __all__ = ["dag", "task", "task_group", "python_task", "virtualenv_task"]
 
-T = TypeVar("T", bound=Callable)
 
+class _TaskDecoratorFactory:
+    """Implementation to provide the ``@task`` syntax."""
 
-class _TaskDecoratorMeta(type):
-    def __getattr__(self, name: str) -> "TaskDecorator":
+    python = staticmethod(python_task)
+    virtualenv = staticmethod(virtualenv_task)
+
+    __call__ = python  # Alias '@task' to '@task.python'.
+
+    def __getattr__(self, name: str) -> TaskDecorator:
         """Dynamically get provider-registered task decorators, e.g. ``@task.docker``."""
         if name.startswith("__"):
-            raise AttributeError(f'{self.__name__} has no attribute {name!r}')
+            raise AttributeError(f'{type(self).__name__} has no attribute {name!r}')
         decorators = ProvidersManager().taskflow_decorators
         if name not in decorators:
             raise AttributeError(f"task decorator {name!r} not found")
         return decorators[name]
 
 
-class _TaskDecoratorAutocompletion(metaclass=_TaskDecoratorMeta):
-    # [START mixin_for_autocomplete]
-    if TYPE_CHECKING:
-        from airflow.providers.docker.decorators.docker import docker_task_autocomplete as docker
-    # [END mixin_for_autocomplete]
-
-
-class task(_TaskDecoratorAutocompletion):
-    """Implementation to provide the ``@task`` syntax."""
-
-    python = python_task
-    virtualenv = virtualenv_task
-
-    @overload
-    def __new__(  # type: ignore[misc]
-        cls,
-        multiple_outputs: Optional[bool] = None,
-        **kwargs,
-    ) -> TaskDecorator:
-        """For the decorator factory ``@task()`` case."""
-
-    @overload
-    def __new__(cls, python_callable: T) -> T:  # type: ignore[misc]
-        """For the "bare decorator" ``@task`` case."""
-
-    def __new__(cls, python_callable, multiple_outputs, **kwargs):
-        """Alias ``@task`` to ``@task.python``."""
-        return python_task(python_callable, multiple_outputs, **kwargs)
+task = _TaskDecoratorFactory()
