2a932657cc988ff523f8b01dd286b79f6eb47e38
Josh Fell <josh.d.fell@astronomer.io>
2022-01-18 10:29:42 -0500

    Fix Mypy "no attribute" errors in `airflow/contrib/sensors`

--- airflow/providers/microsoft/azure/sensors/wasb.pyi
+++ airflow/providers/microsoft/azure/sensors/wasb.pyi
@@ -24,6 +24,7 @@
 
 from typing import Optional
 
+from airflow.providers.sensors.wasb import WasbPrefixSensor
 from airflow.sensors.base import BaseSensorOperator
 
 class WasbBlobSensor(BaseSensorOperator):
@@ -41,3 +42,7 @@ class WasbBlobSensor(BaseSensorOperator):
         check_options: Optional[dict] = None,
         **kwargs,
     ) -> None: ...
+
+# Adding this public module override to suppress Mypy errors stating
+# "airflow.providers.microsoft.azure.sensors.wasb has no attribute 'WasbPrefixSensor'".
+__all__ = ["WasbBlobSensor", "WasbPrefixSensor"]
