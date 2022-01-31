ee880c2135dda966cdcf4551ed3ccc3ef58a71c4
edithturn <edypuclla@gmail.com>
2022-01-31 12:05:43 -0500

    Checked PR_LABELS

--- scripts/ci/selective_ci_checks.py
+++ scripts/ci/selective_ci_checks.py
@@ -19,11 +19,25 @@
 
 import os
 
-PR_LABELS = os.environ.get('PR_LABELS=')
+PR_LABELS = os.environ.get('PR_LABELS')
+GITHUB_EVENT_NAME = os.environ.get('GITHUB_EVENT_NAME')
 
-if os.environ.get('PR_LABELS=') == "full tests needed":
+
+if os.environ.get("PR_LABELS") == "full tests needed":
     print(f"Found the right PR labels in ${PR_LABELS} : 'full tests needed'")
     FULL_TESTS_NEEDED_LABEL = "true"
 else:
     print(f"Did not find the right PR labels in ${PR_LABELS}: 'full tests needed'")
     FULL_TESTS_NEEDED_LABEL = "false"
+
+
+# def check_upgrade_to_newer_dependencies_needed():
+#     RANDOM = os.environ.get('RANDOM')
+#     if GITHUB_EVENT_NAME == "push" or GITHUB_EVENT_NAME == "scheduled":
+#         # Trigger upgrading to latest constraints when we are in push or schedule event. We use the
+#         # random string so that it always triggers rebuilding layer in the docker image
+#         # Each build that upgrades to latest constraints will get truly latest constraints, not those
+#         # cached in the image because docker layer will get invalidated.
+#         # This upgrade_to_newer_dependencies variable can later be overridden
+#         # in case we find that any of the setup.* files changed (see below)
+#         upgrade_to_newer_dependencies = RANDOM
