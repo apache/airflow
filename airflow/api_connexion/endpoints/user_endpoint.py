2f8cc699477a2f9ad7891c586449ea287bcf6d9c
Ash Berlin-Taylor <ash@apache.org>
2022-02-03 15:38:50 +0000

    Code review

--- airflow/api_connexion/endpoints/user_endpoint.py
+++ airflow/api_connexion/endpoints/user_endpoint.py
@@ -53,7 +53,7 @@ def get_users(*, limit: int, order_by: str = "id", offset: Optional[str] = None)
     appbuilder = current_app.appbuilder
     session = appbuilder.get_session
     total_entries = session.query(func.count(User.id)).scalar()
-    direction = "desc" if order_by.startswith("-") else "asc"
+    direction = desc if order_by.startswith("-") else asc
     to_replace = {"user_id": "id"}
     order_param = order_by.strip("-")
     order_param = to_replace.get(order_param, order_param)
