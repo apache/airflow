96e726b3d4c8f084d41a49c24e3aa51777d84f30
eladkal <45845474+eladkal@users.noreply.github.com>
2022-02-06 12:20:19 +0200

    Update tests/providers/google/suite/hooks/test_drive.py

--- tests/providers/google/suite/hooks/test_drive.py
+++ tests/providers/google/suite/hooks/test_drive.py
@@ -275,7 +275,7 @@ class TestGoogleDriveHook(unittest.TestCase):
                     body={"name": "remote_path", "parents": ["PARENT_ID"]},
                     fields="id",
                     media_body=mock_media_file_upload.return_value,
-                    supportsAllDrives=True
+                    supportsAllDrives=True,
                 )
             ]
         )
