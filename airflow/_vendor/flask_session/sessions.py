1a67c19b1f050df82f884f474fad9457d92c231e
blag <blag@users.noreply.github.com>
2022-02-07 22:01:05 -0800

    Add database backend for www sessions

--- airflow/_vendor/flask_session/sessions.py
+++ airflow/_vendor/flask_session/sessions.py
@@ -16,6 +16,8 @@ from flask.sessions import SessionMixin as FlaskSessionMixin
 from werkzeug.datastructures import CallbackDict
 from itsdangerous import Signer, BadSignature, want_bytes
 
+from airflow.utils.session import create_session
+
 from .db_models import Session as SessionModel
 
 
@@ -91,63 +93,60 @@ class SQLAlchemySessionInterface(SessionInterface):
                 return self.session_class(sid=sid, permanent=self.permanent)
 
         store_id = self.key_prefix + sid
-        db_session = self.db.session()
-        saved_session = db_session.query(self.sql_session_model).filter_by(
-            session_id=store_id).first()
-        if saved_session and saved_session.expiry <= datetime.utcnow():
-            # Delete expired session
-            db_session.delete(saved_session)
-            db_session.commit()
-            saved_session = None
-        if saved_session:
-            try:
-                val = saved_session.data
-                data = self.serializer.loads(val)
-                return self.session_class(data, sid=sid)
-            except:
-                return self.session_class(sid=sid, permanent=self.permanent)
-        return self.session_class(sid=sid, permanent=self.permanent)
+        with create_session() as db_session:
+            saved_session = db_session.query(self.sql_session_model).filter_by(
+                session_id=store_id).first()
+            if saved_session and saved_session.expiry <= datetime.utcnow():
+                # Delete expired session
+                db_session.delete(saved_session)
+                saved_session = None
+            if saved_session:
+                try:
+                    val = saved_session.data
+                    data = self.serializer.loads(val)
+                    return self.session_class(data, sid=sid)
+                except:
+                    return self.session_class(sid=sid, permanent=self.permanent)
+            return self.session_class(sid=sid, permanent=self.permanent)
 
     def save_session(self, app, session, response):
         domain = self.get_cookie_domain(app)
         path = self.get_cookie_path(app)
         store_id = self.key_prefix + session.sid
         db_session = self.db.session()
-        saved_session = db_session.query(self.sql_session_model).filter_by(
-            session_id=store_id).first()
+        with create_session() as db_session:
+            saved_session = db_session.query(self.sql_session_model).filter_by(
+                session_id=store_id).first()
 
-        # The session will evalutate to false once it has been modified to be empty
-        if not session:
-            # If the session is modified to be empty, remove the cookie.
-            if session.modified:
-                if saved_session:
-                    db_session.delete(saved_session)
-                    db_session.commit()
-                response.delete_cookie(app.session_cookie_name,
-                                       domain=domain, path=path)
-            # If the session is empty, return without setting the cookie.
-            return
+            # The session will evalutate to false once it has been modified to be empty
+            if not session:
+                # If the session is modified to be empty, remove the cookie.
+                if session.modified:
+                    if saved_session:
+                        db_session.delete(saved_session)
+                    response.delete_cookie(app.session_cookie_name,
+                                           domain=domain, path=path)
+                # If the session is empty, return without setting the cookie.
+                return
 
-        conditional_cookie_kwargs = {}
-        httponly = self.get_cookie_httponly(app)
-        secure = self.get_cookie_secure(app)
-        if self.has_same_site_capability:
-            conditional_cookie_kwargs["samesite"] = self.get_cookie_samesite(app)
-        expires = self.get_expiration_time(app, session)
-        val = self.serializer.dumps(dict(session))
-        if saved_session:
-            saved_session.data = val
-            saved_session.expiry = expires
-            db_session.commit()
-        else:
-            new_session = self.sql_session_model(store_id, val, expires)
-            db_session.add(new_session)
-            db_session.commit()
-        if self.use_signer:
-            session_id = self._get_signer(app).sign(want_bytes(session.sid))
-        else:
-            session_id = session.sid
-        response.set_cookie(app.session_cookie_name, session_id,
-                            expires=expires, httponly=httponly,
-                            domain=domain, path=path, secure=secure,
-                            **conditional_cookie_kwargs)
+            conditional_cookie_kwargs = {}
+            httponly = self.get_cookie_httponly(app)
+            secure = self.get_cookie_secure(app)
+            if self.has_same_site_capability:
+                conditional_cookie_kwargs["samesite"] = self.get_cookie_samesite(app)
+            expires = self.get_expiration_time(app, session)
+            val = self.serializer.dumps(dict(session))
+            if saved_session:
+                saved_session.data = val
+                saved_session.expiry = expires
+            else:
+                new_session = self.sql_session_model(store_id, val, expires)
+                db_session.add(new_session)
+            if self.use_signer:
+                session_id = self._get_signer(app).sign(want_bytes(session.sid))
+            else:
+                session_id = session.sid
+            response.set_cookie(app.session_cookie_name, session_id,
+                                expires=expires, httponly=httponly,
+                                domain=domain, path=path, secure=secure,
+                                **conditional_cookie_kwargs)
