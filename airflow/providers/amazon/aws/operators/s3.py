e5e2b1041f188aa60a5a2d01fd54bb21d0719165
stevensn2h <stevens.nicholas@abc.net.au>
2022-02-09 23:08:12 +1100

    Bug Fix - S3DeleteObjectsOperator will try and delete all keys If a templated rendered empty list of key is passed into S3DeleteObjectsOperator without any prefix it will deleted everything on the bucket

--- airflow/providers/amazon/aws/operators/s3.py
+++ airflow/providers/amazon/aws/operators/s3.py
@@ -364,9 +364,6 @@ class S3DeleteObjectsOperator(BaseOperator):
         **kwargs,
     ):
 
-        if not bool(keys) ^ bool(prefix):
-            raise ValueError("Either keys or prefix should be set.")
-
         super().__init__(**kwargs)
         self.bucket = bucket
         self.keys = keys
@@ -375,6 +372,8 @@ class S3DeleteObjectsOperator(BaseOperator):
         self.verify = verify
 
     def execute(self, context: 'Context'):
+        if not bool(self.keys) ^ bool(self.prefix):
+            raise AirflowException("Either keys or prefix should be set.")        
         s3_hook = S3Hook(aws_conn_id=self.aws_conn_id, verify=self.verify)
 
         keys = self.keys or s3_hook.list_keys(bucket_name=self.bucket, prefix=self.prefix)
