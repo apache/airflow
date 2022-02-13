6bb0857df94c0f959e7ebe421a00b942fd60b199
張泰瑋(Chang Tai Wei) <davidtnfsh@gmail.com>
2022-02-14 03:03:47 +0800

    (providers_google) add a location check in bigquery (#19571)

--- airflow/providers/google/cloud/hooks/bigquery.py
+++ airflow/providers/google/cloud/hooks/bigquery.py
@@ -189,6 +189,11 @@ class BigQueryHook(GoogleBaseHook, DbApiHook):
                 "and extra__google_cloud_platform__keyfile_dict"
             )
 
+    def get_records(self, sql, parameters=None):
+        if self.location is None:
+            raise AirflowException("Need to specify 'location' to use BigQueryHook.get_records()")
+        return super().get_records(sql, parameters=parameters)
+
     @staticmethod
     def _resolve_table_reference(
         table_resource: Dict[str, Any],
