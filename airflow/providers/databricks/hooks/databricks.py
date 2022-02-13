c12120e61cc5a125d88cee6b956fc069ff94a786
Alex Ott <alexott@gmail.com>
2022-02-13 20:05:31 +0100

    Revert change that is covered by another PR

--- airflow/providers/databricks/hooks/databricks.py
+++ airflow/providers/databricks/hooks/databricks.py
@@ -120,7 +120,7 @@ class DatabricksHook(BaseDatabricksHook):
 
     def run_now(self, json: dict) -> int:
         """
-        Utility function to call the ``api/2.1/jobs/run-now`` endpoint.
+        Utility function to call the ``api/2.0/jobs/run-now`` endpoint.
 
         :param json: The data used in the body of the request to the ``run-now`` endpoint.
         :return: the run_id as an int
@@ -131,7 +131,7 @@ class DatabricksHook(BaseDatabricksHook):
 
     def submit_run(self, json: dict) -> int:
         """
-        Utility function to call the ``api/2.1/jobs/runs/submit`` endpoint.
+        Utility function to call the ``api/2.0/jobs/runs/submit`` endpoint.
 
         :param json: The data used in the body of the request to the ``submit`` endpoint.
         :return: the run_id as an int
