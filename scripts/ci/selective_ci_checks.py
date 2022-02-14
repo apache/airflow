1c4dc012bcbdd441661f6d191c40063042719d65
edithturn <edypuclla@gmail.com>
2022-02-14 10:20:04 -0500

    add _script_init

--- scripts/ci/selective_ci_checks.py
+++ scripts/ci/selective_ci_checks.py
@@ -19,7 +19,10 @@
 
 import json
 import os
+import subprocess
 import sys
+
+subprocess.call("../../scripts/ci/libraries/_script_init.sh")
 
 # Forcing PR
 PR_LABELS = "full tests needed"  # os.environ.get('PR_LABELS')
@@ -54,7 +57,7 @@ def ga_output(parameter_name: str, parameter_value: str):
 
 
 def output_all_basic_variables():
-    CURRENT_PYTHON_MAJOR_MINOR_VERSIONS = os.environ.get('CURRENT_PYTHON_MAJOR_MINOR_VERSIONS')
+    CURRENT_PYTHON_MAJOR_MINOR_VERSIONS = os.environ('CURRENT_PYTHON_MAJOR_MINOR_VERSIONS')
     print(f"CPMMV + {CURRENT_PYTHON_MAJOR_MINOR_VERSIONS}")
     ALL_PYTHON_MAJOR_MINOR_VERSIONS = os.environ.get('ALL_PYTHON_MAJOR_MINOR_VERSIONS')
     print(f" HERE >>  + {FULL_TESTS_NEEDED_LABEL}")
