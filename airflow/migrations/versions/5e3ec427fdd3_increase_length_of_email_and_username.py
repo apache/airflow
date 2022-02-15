137c3cb929ca6fb0f37c4d865c1746f70d2226a6
Jed Cunningham <jedcunningham@apache.org>
2022-02-15 13:56:45 -0700

    Reorder migrations to include bugfix in 2.2.4

--- airflow/migrations/versions/5e3ec427fdd3_increase_length_of_email_and_username.py
+++ airflow/migrations/versions/5e3ec427fdd3_increase_length_of_email_and_username.py
@@ -19,7 +19,7 @@
 """Increase length of email and username
 
 Revision ID: 5e3ec427fdd3
-Revises: c381b21cb7e4
+Revises: 587bdf053233
 Create Date: 2021-12-01 11:49:26.390210
 
 """
@@ -29,7 +29,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = '5e3ec427fdd3'
-down_revision = 'c381b21cb7e4'
+down_revision = '587bdf053233'
 branch_labels = None
 depends_on = None
 
