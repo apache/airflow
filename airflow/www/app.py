1c2909f8d69ade70803f10653e4845319ae99c0e
Jed Cunningham <66968678+jedcunningham@users.noreply.github.com>
2022-02-15 10:57:46 -0700

    Add a session backend to store session data in the database (#21478)
    Co-authored-by: Jed Cunningham <jedcunningham@apache.org>
    (cherry picked from commit da9d0863c7ff121c111a455708163b026943bdf1)

--- airflow/www/app.py
+++ airflow/www/app.py
@@ -36,7 +36,7 @@ from airflow.www.extensions.init_jinja_globals import init_jinja_globals
 from airflow.www.extensions.init_manifest_files import configure_manifest_files
 from airflow.www.extensions.init_robots import init_robots
 from airflow.www.extensions.init_security import init_api_experimental_auth, init_xframe_protection
-from airflow.www.extensions.init_session import init_airflow_session_interface, init_permanent_session
+from airflow.www.extensions.init_session import init_airflow_session_interface
 from airflow.www.extensions.init_views import (
     init_api_connexion,
     init_api_experimental,
@@ -135,7 +135,6 @@ def create_app(config=None, testing=False):
 
         init_jinja_globals(flask_app)
         init_xframe_protection(flask_app)
-        init_permanent_session(flask_app)
         init_airflow_session_interface(flask_app)
     return flask_app
 
