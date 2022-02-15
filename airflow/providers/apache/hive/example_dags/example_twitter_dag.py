041babb0608a102fd9d83e77b7fab5d9831ec2b4
Jed Cunningham <66968678+jedcunningham@users.noreply.github.com>
2022-02-15 03:15:18 -0700

    Fix mypy issues in `example_twitter_dag` (#21571)

--- airflow/providers/apache/hive/example_dags/example_twitter_dag.py
+++ airflow/providers/apache/hive/example_dags/example_twitter_dag.py
@@ -78,12 +78,12 @@ with DAG(
     tags=['example'],
     catchup=False,
 ) as dag:
-    fetch_tweets = fetch_tweets()
-    clean_tweets = clean_tweets()
-    analyze_tweets = analyze_tweets()
+    fetch = fetch_tweets()
+    clean = clean_tweets()
+    analyze = analyze_tweets()
     hive_to_mysql = transfer_to_db()
 
-    fetch_tweets >> clean_tweets >> analyze_tweets
+    fetch >> clean >> analyze
 
     # --------------------------------------------------------------------------------
     # The following tasks are generated using for loop. The first task puts the eight
@@ -122,7 +122,7 @@ with DAG(
             ),
         )
 
-        analyze_tweets >> load_to_hdfs >> load_to_hive >> hive_to_mysql
+        analyze >> load_to_hdfs >> load_to_hive >> hive_to_mysql
 
     for channel in from_channels:
         file_name = f"from_{channel}_{dt}.csv"
@@ -142,4 +142,4 @@ with DAG(
             ),
         )
 
-        analyze_tweets >> load_to_hdfs >> load_to_hive >> hive_to_mysql
+        analyze >> load_to_hdfs >> load_to_hive >> hive_to_mysql
