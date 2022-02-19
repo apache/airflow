apache/airflow

commit fd5558fe9eb4c82f1aa49244deb75a6d500334bc

Fixing ses email backend (#18042)
(cherry picked from commit 1543dc28f4a2f1631dfaedd948e646a181ccf7ee)

airflow/providers/amazon/aws/utils/emailer.py
@@ -23,6 +23,7 @@ from airflow.providers.amazon.aws.hooks.ses import SESHook
 
 
 def send_email(
+    from_email: str,
     to: Union[List[str], str],
     subject: str,
     html_content: str,
@@ -37,7 +38,7 @@ def send_email(
     """Email backend for SES."""
     hook = SESHook(aws_conn_id=conn_id)
     hook.send_email(
-        mail_from=None,
+        mail_from=from_email,
         to=to,
         subject=subject,
         html_content=html_content,

tests/providers/amazon/aws/utils/test_emailer.py
@@ -16,27 +16,29 @@
 # specific language governing permissions and limitations
 # under the License.
 #
-
-from unittest import mock
+from unittest import TestCase, mock
 
 from airflow.providers.amazon.aws.utils.emailer import send_email
 
 
-@mock.patch("airflow.providers.amazon.aws.utils.emailer.SESHook")
-def test_send_email(mock_hook):
-    send_email(
-        to="to@test.com",
-        subject="subject",
-        html_content="content",
-    )
-    mock_hook.return_value.send_email.assert_called_once_with(
-        mail_from=None,
-        to="to@test.com",
-        subject="subject",
-        html_content="content",
-        bcc=None,
-        cc=None,
-        files=None,
-        mime_charset="utf-8",
-        mime_subtype="mixed",
-    )
+class TestSendEmailSes(TestCase):
+    @mock.patch("airflow.providers.amazon.aws.utils.emailer.SESHook")
+    def test_send_ses_email(self, mock_hook):
+        send_email(
+            from_email="From Test <from@test.com>",
+            to="to@test.com",
+            subject="subject",
+            html_content="content",
+        )
+
+        mock_hook.return_value.send_email.assert_called_once_with(
+            mail_from="From Test <from@test.com>",
+            to="to@test.com",
+            subject="subject",
+            html_content="content",
+            bcc=None,
+            cc=None,
+            files=None,
+            mime_charset="utf-8",
+            mime_subtype="mixed",
+        )

airflow/config_templates/config.yml
@@ -1353,6 +1353,14 @@
       example: "/path/to/my_html_content_template_file"
       default: ~
       see_also: ":doc:`Email Configuration </howto/email-config>`"
+    - name: from_email
+      description: |
+        Email address that will be used as sender address.
+        It can either be raw email or the complete address in a format ``Sender Name <sender@email.com>``
+      version_added: 2.3.0
+      type: string
+      example: "Airflow <airflow@example.com>"
+      default: ~
 
 - name: smtp
   description: |

airflow/config_templates/default_airflow.cfg
@@ -681,6 +681,11 @@ default_email_on_failure = True
 # Example: html_content_template = /path/to/my_html_content_template_file
 # html_content_template =
 
+# Email address that will be used as sender address.
+# It can either be raw email or the complete address in a format ``Sender Name <sender@email.com>``
+# Example: from_email = Airflow <airflow@example.com>
+# from_email =
+
 [smtp]
 
 # If you want airflow to send emails on retries, failure, and you want to use

airflow/utils/email.py
@@ -49,6 +49,8 @@ def send_email(
     """Send email using backend specified in EMAIL_BACKEND."""
     backend = conf.getimport('email', 'EMAIL_BACKEND')
     backend_conn_id = conn_id or conf.get("email", "EMAIL_CONN_ID")
+    from_email = conf.get('email', 'from_email', fallback=None)
+
     to_list = get_email_address_list(to)
     to_comma_separated = ", ".join(to_list)
 
@@ -63,6 +65,7 @@ def send_email(
         mime_subtype=mime_subtype,
         mime_charset=mime_charset,
         conn_id=backend_conn_id,
+        from_email=from_email,
         **kwargs,
     )
 
@@ -78,6 +81,7 @@ def send_email_smtp(
     mime_subtype: str = 'mixed',
     mime_charset: str = 'utf-8',
     conn_id: str = "smtp_default",
+    from_email: str = None,
     **kwargs,
 ):
     """
@@ -87,8 +91,10 @@ def send_email_smtp(
     """
     smtp_mail_from = conf.get('smtp', 'SMTP_MAIL_FROM')
 
+    mail_from = smtp_mail_from or from_email
+
     msg, recipients = build_mime_message(
-        mail_from=smtp_mail_from,
+        mail_from=mail_from,
         to=to,
         subject=subject,
         html_content=html_content,
@@ -99,7 +105,7 @@ def send_email_smtp(
         mime_charset=mime_charset,
     )
 
-    send_mime_email(e_from=smtp_mail_from, e_to=recipients, mime_msg=msg, conn_id=conn_id, dryrun=dryrun)
+    send_mime_email(e_from=mail_from, e_to=recipients, mime_msg=msg, conn_id=conn_id, dryrun=dryrun)
 
 
 def build_mime_message(

tests/utils/test_email.py
@@ -99,8 +99,22 @@ class TestEmail(unittest.TestCase):
             mime_charset='utf-8',
             mime_subtype='mixed',
             conn_id='smtp_default',
+            from_email=None,
         )
         assert not mock_send_email.called
+
+    @mock.patch('airflow.utils.email.send_email_smtp')
+    @conf_vars(
+        {
+            ('email', 'email_backend'): 'tests.utils.test_email.send_email_test',
+            ('email', 'from_email'): 'from@test.com',
+        }
+    )
+    def test_custom_backend_sender(self, mock_send_email_smtp):
+        utils.email.send_email('to', 'subject', 'content')
+        _, call_kwargs = send_email_test.call_args
+        assert call_kwargs['from_email'] == 'from@test.com'
+        assert not mock_send_email_smtp.called
 
     def test_build_mime_message(self):
         mail_from = 'from@example.com'

commit a723825aca6299e653e768e63b862113822ac45a

Fix Amazon SES emailer signature
Amazon SES Had a wrong signature to become a mailer in Airflow
introduced in #18042. As the result setting SES emailer as
email backend resulted in:

```
TypeError: send_email() missing 1 required positional argument:
'html_content'
```

Fixes: #21671

airflow/providers/amazon/aws/utils/emailer.py
@@ -16,14 +16,12 @@
 # specific language governing permissions and limitations
 # under the License.
 """Airflow module for email backend using AWS SES"""
-
 from typing import List, Optional, Union
 
 from airflow.providers.amazon.aws.hooks.ses import SesHook
 
 
 def send_email(
-    from_email: str,
     to: Union[List[str], str],
     subject: str,
     html_content: str,
@@ -33,9 +31,12 @@ def send_email(
     mime_subtype: str = 'mixed',
     mime_charset: str = 'utf-8',
     conn_id: str = 'aws_default',
+    from_email: Optional[str] = None,
     **kwargs,
 ) -> None:
     """Email backend for SES."""
+    if from_email is None:
+        raise RuntimeError("The `from_email' configuration has to be set for the SES emailer.")
     hook = SesHook(aws_conn_id=conn_id)
     hook.send_email(
         mail_from=from_email,

tests/providers/amazon/aws/utils/test_emailer.py
@@ -18,6 +18,8 @@
 #
 from unittest import TestCase, mock
 
+import pytest
+
 from airflow.providers.amazon.aws.utils.emailer import send_email
 
 
@@ -25,10 +27,10 @@ class TestSendEmailSes(TestCase):
     @mock.patch("airflow.providers.amazon.aws.utils.emailer.SesHook")
     def test_send_ses_email(self, mock_hook):
         send_email(
-            from_email="From Test <from@test.com>",
             to="to@test.com",
             subject="subject",
             html_content="content",
+            from_email="From Test <from@test.com>",
         )
 
         mock_hook.return_value.send_email.assert_called_once_with(
@@ -42,3 +44,10 @@ class TestSendEmailSes(TestCase):
             mime_charset="utf-8",
             mime_subtype="mixed",
         )
+
+    @mock.patch("airflow.providers.amazon.aws.utils.emailer.SesHook")
+    def test_send_ses_email_no_from_mail(self, mock_hook):
+        with pytest.raises(
+            RuntimeError, match="The `from_email' configuration has to be set for the SES emailer."
+        ):
+            send_email(to="to@test.com", subject="subject", html_content="content")