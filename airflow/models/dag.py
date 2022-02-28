7ce123b72899c25641219506378257b60ae622f8
Ephraim Anierobi <splendidzigy24@gmail.com>
2022-02-28 16:50:37 +0100

    Update airflow/models/dag.py
    Co-authored-by: Tzu-ping Chung <uranusjr@gmail.com>

--- airflow/models/dag.py
+++ airflow/models/dag.py
@@ -2639,7 +2639,7 @@ class DAG(LoggingMixin):
         task_pools = {task.pool for task in self.tasks}
         diff = task_pools - pools
         if diff:
-            raise PoolNotFound(f"The following pools: `{list(diff)}` does not exist in the database")
+            raise PoolNotFound(f"The following pools: `{sorted(diff)}` do not exist in the database")
 
 
 class DagTag(Base):
