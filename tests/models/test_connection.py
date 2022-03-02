77a118e95844d2f68f3d359a7b6561c65cce9c55
Ashish Patel <ashishpatel0720@gmail.com>
2022-03-02 19:29:40 +0000

    21720 - pre-commit fixes

--- tests/models/test_connection.py
+++ tests/models/test_connection.py
@@ -185,8 +185,8 @@ class TestConnection(unittest.TestCase):
         ),
         UriTestCaseConfig(
             test_conn_uri='scheme://user:password@host%2Flocation:1234/schema?'
-            '__extra__=%7B%22my_val%22%3A+%5B%22list%22%2C+%22of%22%2C+%22values%22%5D%2C+%22extra%22%3A+%7B%22nested%22%3A+%7B%22json%22%3A+%22val%22%7D%7D%7D',
-
+            '__extra__=%7B%22my_val%22%3A+%5B%22list%22%2C+%22of%22%2C+%22values%22%5D%2C+%22'
+            'extra%22%3A+%7B%22nested%22%3A+%7B%22json%22%3A+%22val%22%7D%7D%7D',
             test_conn_attributes=dict(
                 conn_type='scheme',
                 host='host/location',
