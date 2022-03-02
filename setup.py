9b40c028f716948321d0f41e84afd8446e982724
Eugene Karimov <eskarimov@gmail.com>
2022-03-02 22:01:47 +0100

    Add Deferrable Databricks operators

--- setup.py
+++ setup.py
@@ -265,6 +265,7 @@ dask = [
 databricks = [
     'requests>=2.26.0, <3',
     'databricks-sql-connector>=2.0.0, <3.0.0',
+    'aiohttp>=3.6.3, <4',
 ]
 datadog = [
     'datadog>=0.14.0',
@@ -602,6 +603,7 @@ mypy_dependencies = [
 
 # Dependencies needed for development only
 devel_only = [
+    'asynctest~=0.13',
     'aws_xray_sdk',
     'beautifulsoup4>=4.7.1',
     'black',
