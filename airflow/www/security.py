92e17390587f824f06a2b06bad4ff00a95b5ebdc
blag <blag@users.noreply.github.com>
2022-03-02 04:22:19 -0700

    Add per-DAG delete permissions

--- airflow/www/security.py
+++ airflow/www/security.py
@@ -325,6 +325,11 @@ class AirflowSecurityManager(SecurityManager, LoggingMixin):
         dag_resource_name = permissions.resource_name_for_dag(dag_id)
         return self.has_access(permissions.ACTION_CAN_EDIT, dag_resource_name, user=user)
 
+    def can_delete_dag(self, dag_id, user=None) -> bool:
+        """Determines whether a user has DAG delete access."""
+        dag_resource_name = permissions.resource_name_for_dag(dag_id)
+        return self.has_access(permissions.ACTION_CAN_DELETE, dag_resource_name, user=user)
+
     def prefixed_dag_id(self, dag_id):
         """Returns the permission name for a DAG id."""
         warnings.warn(
@@ -479,8 +484,8 @@ class AirflowSecurityManager(SecurityManager, LoggingMixin):
 
     def create_dag_specific_permissions(self) -> None:
         """
-        Creates 'can_read' and 'can_edit' permissions for all DAGs,
-        along with any `access_control` permissions provided in them.
+        Creates 'can_read', 'can_edit', and 'can_delete' permissions for all
+        DAGs, along with any `access_control` permissions provided in them.
 
         This does iterate through ALL the DAGs, which can be slow. See `sync_perm_for_dag`
         if you only need to sync a single DAG.
@@ -647,6 +652,7 @@ class AirflowSecurityManager(SecurityManager, LoggingMixin):
             if perm in (
                 (permissions.ACTION_CAN_READ, permissions.RESOURCE_DAG),
                 (permissions.ACTION_CAN_EDIT, permissions.RESOURCE_DAG),
+                (permissions.ACTION_CAN_DELETE, permissions.RESOURCE_DAG),
             ):
                 can_access_all_dags = self.has_access(*perm)
                 if can_access_all_dags:
