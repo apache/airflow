7bd8b2d7f3bca39a919cf0aeef91da1c476d792d
Eric Gao <ericgao.apache@gmail.com>
2022-03-11 20:48:40 +0800

    Add oss_task_handler into alibaba-provider and enable remote logging to OSS (#21785)

--- tests/providers/alibaba/cloud/log/__init__.py
+++ tests/providers/alibaba/cloud/log/__init__.py
@@ -1,4 +1,3 @@
-#
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -15,23 +14,3 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-import json
-
-from airflow.models import Connection
-
-OSS_PROJECT_ID_HOOK_UNIT_TEST = 'example-project'
-
-
-def mock_oss_hook_default_project_id(self, oss_conn_id='mock_oss_default', region='mock_region'):
-    self.oss_conn_id = oss_conn_id
-    self.oss_conn = Connection(
-        extra=json.dumps(
-            {
-                'auth_type': 'AK',
-                'access_key_id': 'mock_access_key_id',
-                'access_key_secret': 'mock_access_key_secret',
-                'region': 'mock_region',
-            }
-        )
-    )
-    self.region = region
