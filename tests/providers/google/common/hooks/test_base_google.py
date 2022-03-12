6cb47d957b32030792e813ac29f5cf0eb5105dc6
Harish Kesava Rao <harish.kesavarao@gmail.com>
2022-03-12 10:03:10 -0800

    Pushing black linter changes for base google and dataproc files

--- tests/providers/google/common/hooks/test_base_google.py
+++ tests/providers/google/common/hooks/test_base_google.py
@@ -684,9 +684,11 @@ class TestProvideAuthorizedGcloud(unittest.TestCase):
         with self.instance.provide_authorized_gcloud():
             assert os.environ[CREDENTIALS] == key_path
 
-        calls = [mock.call(['gcloud', 'config', 'set', 'core/project', 'PROJECT_ID']),
-            mock.call(['gcloud', 'auth', 'activate-service-account', '--key-file=/test/key-path'])]
-        mock_check_output.assert_has_calls(calls,any_order=True)
+        calls = [
+            mock.call(['gcloud', 'config', 'set', 'core/project', 'PROJECT_ID']),
+            mock.call(['gcloud', 'auth', 'activate-service-account', '--key-file=/test/key-path']),
+        ]
+        mock_check_output.assert_has_calls(calls, any_order=True)
 
     @mock.patch(
         'airflow.providers.google.common.hooks.base_google.GoogleBaseHook.project_id',
@@ -707,9 +709,11 @@ class TestProvideAuthorizedGcloud(unittest.TestCase):
         with self.instance.provide_authorized_gcloud():
             assert os.environ[CREDENTIALS] == file_name
 
-        calls = [mock.call(['gcloud', 'config', 'set', 'core/project', 'PROJECT_ID']),
-                mock.call(['gcloud', 'auth', 'activate-service-account', '--key-file=/test/mock-file'])]
-        mock_check_output.assert_has_calls(calls,any_order=True)
+        calls = [
+            mock.call(['gcloud', 'config', 'set', 'core/project', 'PROJECT_ID']),
+            mock.call(['gcloud', 'auth', 'activate-service-account', '--key-file=/test/mock-file']),
+        ]
+        mock_check_output.assert_has_calls(calls, any_order=True)
 
     @mock.patch(
         'airflow.providers.google.common.hooks.base_google.GoogleBaseHook.project_id',
