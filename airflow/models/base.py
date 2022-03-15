5c2a6919389feb8197540c6ff53e539cd9bfe036
Ephraim Anierobi <splendidzigy24@gmail.com>
2022-03-15 20:24:32 +0100

    drop support for mssql

--- airflow/models/base.py
+++ airflow/models/base.py
@@ -25,8 +25,19 @@ from airflow.configuration import conf
 
 SQL_ALCHEMY_SCHEMA = conf.get("core", "SQL_ALCHEMY_SCHEMA")
 
+naming_convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+
 metadata = (
-    None if not SQL_ALCHEMY_SCHEMA or SQL_ALCHEMY_SCHEMA.isspace() else MetaData(schema=SQL_ALCHEMY_SCHEMA)
+    None
+    if not SQL_ALCHEMY_SCHEMA or SQL_ALCHEMY_SCHEMA.isspace()
+    else MetaData(schema=SQL_ALCHEMY_SCHEMA, naming_convention=naming_convention)
 )
 Base: Any = declarative_base(metadata=metadata)
 
