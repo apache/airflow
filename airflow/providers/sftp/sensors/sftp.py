8647041cd861f45575dde237ec7fe04c83474bc1
paslawsk <Adampaslawski@gmail.com>
2022-03-19 10:33:37 -0400

    assume file creation time in UTC

--- airflow/providers/sftp/sensors/sftp.py
+++ airflow/providers/sftp/sensors/sftp.py
@@ -23,7 +23,7 @@ from paramiko.sftp import SFTP_NO_SUCH_FILE
 
 from airflow.providers.sftp.hooks.sftp import SFTPHook
 from airflow.sensors.base import BaseSensorOperator
-from airflow.utils.timezone import is_naive, make_naive
+from airflow.utils.timezone import convert_to_utc
 
 if TYPE_CHECKING:
     from airflow.utils.context import Context
@@ -44,7 +44,7 @@ class SFTPSensor(BaseSensorOperator):
     )
 
     def __init__(
-        self, *, path: str, newer_than: datetime, sftp_conn_id: str = 'sftp_default', **kwargs
+        self, *, path: str, newer_than: datetime = None, sftp_conn_id: str = 'sftp_default', **kwargs
     ) -> None:
         super().__init__(**kwargs)
         self.path = path
@@ -64,8 +64,8 @@ class SFTPSensor(BaseSensorOperator):
             return False
         self.hook.close_conn()
         if self.newer_than:
-            _mod_time = datetime.strptime(mod_time, '%Y%m%d%H%M%S')
-            _newer_than = make_naive(self.newer_than) if not is_naive(self.newer_than) else self.newer_than
+            _mod_time = convert_to_utc(datetime.strptime(mod_time, '%Y%m%d%H%M%S'))
+            _newer_than = convert_to_utc(self.newer_than)
             return _newer_than <= _mod_time
         else:
             return True
