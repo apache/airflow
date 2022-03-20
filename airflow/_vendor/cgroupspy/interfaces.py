76d6089887919765c1d431c07e53a33f2c16de01
Jarek Potiuk <jarek@potiuk.com>
2022-03-20 17:51:34 +0100

    Fix python 3.10 support in vendored in cgroupspy (#22208)
    The cgrouppspy is not Python 3.10 compliant due to Iterable
    being imported directly from collections.
    
    This is captured in https://github.com/cloudsigma/cgroupspy/issues/13
    
    We fix it in our vendored-in version of cgroupspy until
    hopefully new version of it is released.
    
    This is part of the effort needed to implement Python 3.10
    compatibility: #22050

--- airflow/_vendor/cgroupspy/interfaces.py
+++ airflow/_vendor/cgroupspy/interfaces.py
@@ -24,7 +24,7 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
-from collections import Iterable
+from collections.abc import Iterable
 from airflow._vendor.cgroupspy.contenttypes import BaseContentType
 
 
