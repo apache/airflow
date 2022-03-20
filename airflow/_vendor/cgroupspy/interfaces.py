172df9ee247af62e9417cebb2e2a3bc2c261a204
Jarek Potiuk <jarek@potiuk.com>
2022-03-20 12:19:36 +0100

    Apply import fixes to vendored cgroupspy library (#22207)
    This commit applies import fixes to vendored cgroupspy library
    so that the library uses the vendored version internally.
    
    This commit will need to be re-applied if we upgrade the
    vendored cgroupspy library.
    
    This is part of the effort needed to implement Python 3.10
    compatibility: #22050

--- airflow/_vendor/cgroupspy/interfaces.py
+++ airflow/_vendor/cgroupspy/interfaces.py
@@ -25,7 +25,7 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 from collections import Iterable
-from cgroupspy.contenttypes import BaseContentType
+from airflow._vendor.cgroupspy.contenttypes import BaseContentType
 
 
 class BaseFileInterface(object):
