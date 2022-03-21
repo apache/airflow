aee0e6e5a65526cb5d1bb9fb01003eaccf1158d1
Ephraim Anierobi <splendidzigy24@gmail.com>
2022-03-21 15:37:08 +0100

    fixup! Use Airflow.Base.metadata in FAB models

--- airflow/migrations/versions/0106_909884dea523_update_migration_for_fab_tables_to_add_missing_constraints.py
+++ airflow/migrations/versions/0106_909884dea523_update_migration_for_fab_tables_to_add_missing_constraints.py
@@ -42,43 +42,49 @@ def upgrade():
     """Apply Update migration for FAB tables to add missing constraints"""
     conn = op.get_bind()
     if conn.dialect.name == 'sqlite':
+        op.execute('PRAGMA foreign_keys=OFF')
         with op.batch_alter_table('ab_view_menu', schema=None) as batch_op:
             batch_op.create_unique_constraint(batch_op.f('ab_view_menu_name_uq'), ['name'])
+        op.execute('PRAGMA foreign_keys=ON')
     elif conn.dialect.name == 'mysql':
         with op.batch_alter_table('ab_register_user', schema=None) as batch_op:
-            batch_op.alter_column('username', existing_type=sa.String(256), nullable=False, unique=True)
-            batch_op.alter_column('email', existing_type=sa.String(256), nullable=False, unique=True)
-
+            batch_op.alter_column('username', existing_type=sa.String(256), nullable=False)
+            batch_op.alter_column('email', existing_type=sa.String(256), nullable=False)
         with op.batch_alter_table('ab_user', schema=None) as batch_op:
-            batch_op.alter_column('username', existing_type=sa.String(256), nullable=False, unique=True)
-            batch_op.alter_column('email', existing_type=sa.String(256), nullable=False, unique=True)
+            batch_op.alter_column('username', existing_type=sa.String(256), nullable=False)
+            batch_op.alter_column('email', existing_type=sa.String(256), nullable=False)
     elif conn.dialect.name == 'mssql':
         with op.batch_alter_table('ab_register_user') as batch_op:
             # Drop the unique constraint on username and email
             constraints = get_mssql_table_constraints(conn, 'ab_register_user')
             for k, _ in constraints.get('UNIQUE').items():
                 batch_op.drop_constraint(k, type_='unique')
-            batch_op.alter_column('username', existing_type=sa.String(256), nullable=False, unique=True)
-            batch_op.alter_column('email', existing_type=sa.String(256), nullable=False, unique=True)
-
+            batch_op.alter_column('username', existing_type=sa.String(256), nullable=False)
+            batch_op.create_unique_constraint(None, ['username'])
+            batch_op.alter_column('email', existing_type=sa.String(256), nullable=False)
+            batch_op.create_unique_constraint(None, ['email'])
         with op.batch_alter_table('ab_user') as batch_op:
             # Drop the unique constraint on username and email
             constraints = get_mssql_table_constraints(conn, 'ab_user')
             for k, _ in constraints.get('UNIQUE').items():
                 batch_op.drop_constraint(k, type_='unique')
-            batch_op.alter_column('username', existing_type=sa.String(256), nullable=False, unique=True)
-            batch_op.alter_column('email', existing_type=sa.String(256), nullable=False, unique=True)
+            batch_op.alter_column('username', existing_type=sa.String(256), nullable=False)
+            batch_op.create_unique_constraint(None, ['username'])
+            batch_op.alter_column('email', existing_type=sa.String(256), nullable=False)
+            batch_op.create_unique_constraint(None, ['email'])
 
 
 def downgrade():
     """Unapply Update migration for FAB tables to add missing constraints"""
     conn = op.get_bind()
     if conn.dialect.name == 'sqlite':
+        op.execute('PRAGMA foreign_keys=OFF')
         with op.batch_alter_table('ab_view_menu', schema=None) as batch_op:
             batch_op.drop_constraint('ab_view_menu_name_uq', type_='unique')
+        op.execute('PRAGMA foreign_keys=ON')
     elif conn.dialect.name == 'mysql':
         with op.batch_alter_table('ab_user', schema=None) as batch_op:
-            batch_op.alter_column('email', existing_type=sa.String(256), nullable=True, unique=True)
+            batch_op.alter_column('email', existing_type=sa.String(256), nullable=True)
             batch_op.alter_column('username', existing_type=sa.String(256), nullable=True, unique=True)
         with op.batch_alter_table('ab_register_user', schema=None) as batch_op:
             batch_op.alter_column('email', existing_type=sa.String(256), nullable=True)
@@ -90,12 +96,16 @@ def downgrade():
             for k, _ in constraints.get('UNIQUE').items():
                 batch_op.drop_constraint(k, type_='unique')
             batch_op.alter_column('username', existing_type=sa.String(256), nullable=False, unique=True)
+            batch_op.create_unique_constraint(None, ['username'])
             batch_op.alter_column('email', existing_type=sa.String(256), nullable=False, unique=True)
+            batch_op.create_unique_constraint(None, ['email'])
 
         with op.batch_alter_table('ab_user') as batch_op:
             # Drop the unique constraint on username and email
             constraints = get_mssql_table_constraints(conn, 'ab_user')
             for k, _ in constraints.get('UNIQUE').items():
                 batch_op.drop_constraint(k, type_='unique')
-            batch_op.alter_column('username', existing_type=sa.String(256), nullable=True, unique=True)
+            batch_op.alter_column('username', existing_type=sa.String(256), nullable=True)
+            batch_op.create_unique_constraint(None, ['username'])
             batch_op.alter_column('email', existing_type=sa.String(256), nullable=True, unique=True)
+            batch_op.create_unique_constraint(None, ['email'])
