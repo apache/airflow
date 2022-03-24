c623a918564cec844f66a0b6ab729e7b14a15098
Matt Rixman <MatrixManAtYrService@users.noreply.github.com>
2022-03-24 09:02:42 -0600

    sorted imports

--- airflow/utils/db.py
+++ airflow/utils/db.py
@@ -21,10 +21,10 @@ import logging
 import os
 import sys
 import time
+import warnings
 from tempfile import gettempdir
 from typing import TYPE_CHECKING, Any, Callable, Iterable, List, Optional, Tuple
 
-import warnings
 from sqlalchemy import Table, exc, func, inspect, or_, text
 from sqlalchemy.orm.session import Session
 
