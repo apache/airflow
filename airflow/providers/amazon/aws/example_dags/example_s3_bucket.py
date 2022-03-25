927ff66f324a95633f70d96dedb8b9e69fa9c812
caxefaizan <63395276+caxefaizan@users.noreply.github.com>
2022-03-25 10:28:17 +0530

    Update example_s3_bucket.py
    DAG  doesnt show up with `from airflow.models.dag import DAG`

--- airflow/providers/amazon/aws/example_dags/example_s3_bucket.py
+++ airflow/providers/amazon/aws/example_dags/example_s3_bucket.py
@@ -19,7 +19,7 @@ import os
 from datetime import datetime
 
 from airflow.models.baseoperator import chain
-from airflow.models.dag import DAG
+from airflow import DAG
 from airflow.providers.amazon.aws.operators.s3 import (
     S3CreateBucketOperator,
     S3DeleteBucketOperator,
