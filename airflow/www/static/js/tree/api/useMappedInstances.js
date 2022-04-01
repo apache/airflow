5c965d8be4d0a9ba598053d0ffd83fa66b61663c
Brent Bovenzi <brent.bovenzi@gmail.com>
2022-04-01 10:29:45 -0400

    Add table of mapped instances to grid view

---
+++ airflow/www/static/js/tree/api/useMappedInstances.js
@@ -0,0 +1,33 @@
+/*!
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import axios from 'axios';
+import { useQuery } from 'react-query';
+
+export default function useMappedInstances({
+  dagId, runId, taskId, limit, offset, order,
+}) {
+  const orderParam = order && order !== 'map_index' ? { order_by: order } : {};
+  return useQuery(
+    ['mappedInstances', dagId, runId, taskId, offset, order],
+    () => axios.get(`/api/v1/dags/${dagId}/dagRuns/${runId}/taskInstances/${taskId}/listMapped`, {
+      params: { offset, limit, ...orderParam },
+    }),
+  );
+}
