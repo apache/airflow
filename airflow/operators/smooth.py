15b40d08d07c00353c27572a9367a844faca79fc
Tomasz Urbaszek <turbaszek@gmail.com>
2022-04-07 14:10:28 +0200

    Add SmoothOperator
    Easter is coming so I just came with idea of an easter egg.

---
+++ airflow/operators/smooth.py
@@ -0,0 +1,38 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+from airflow.operators.dummy import DummyOperator
+from airflow.utils.context import Context
+
+
+class SmoothOperator(DummyOperator):
+    """
+    Operator that does literally nothing but it logs YouTube link to
+    Sade song "Smooth Operator".
+
+    The task is evaluated by the scheduler but never processed by the executor.
+    """
+
+    ui_color = '#e8f7e4'
+    inherits_from_dummy_operator = True
+    yt_link: str = "https://www.youtube.com/watch?v=4TYv2PhG89A"
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+
+    def execute(self, context: Context):
+        self.log.info("Enjoy Sade - Smooth Operator: %s", self.yt_link)
