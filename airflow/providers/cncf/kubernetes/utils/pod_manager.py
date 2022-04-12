acf266e9066b634e5b8567bff551784eccdd9e05
karunpoudel <poudel.karun@gmail.com>
2022-04-12 16:47:12 -0500

    fix for pre-commit

--- airflow/providers/cncf/kubernetes/utils/pod_manager.py
+++ airflow/providers/cncf/kubernetes/utils/pod_manager.py
@@ -86,7 +86,7 @@ def get_container_termination_message(pod: V1Pod, container_name: str):
     try:
         container_statuses = pod.status.container_statuses
         container_status = next(iter([x for x in container_statuses if x.name == container_name]), None)
-        return container_status.state.terminated.message
+        return container_status.state.terminated.message if container_status else None
     except AttributeError:
         return None
 
