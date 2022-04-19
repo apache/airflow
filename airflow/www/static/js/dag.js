4874068d99444289f361fdbeb9e2ab9326e2537d
Brent Bovenzi <brent.bovenzi@gmail.com>
2022-04-19 13:27:50 -0500

    Improve Graph view task actions for Dynamic Tasks (#23064)
    * Include state in mapped task dropdown
    
    * Rearrange task action IA
    
    * List Instances button all runs vs current run

--- airflow/www/static/js/dag.js
+++ airflow/www/static/js/dag.js
@@ -54,8 +54,7 @@ let executionDate = '';
 let subdagId = '';
 let dagRunId = '';
 let mapIndex;
-let showBack = false;
-let mapLength = 0;
+let mapStates = [];
 const showExternalLogRedirect = getMetaValue('show_external_log_redirect') === 'True';
 
 const buttons = Array.from(document.querySelectorAll('a[id^="btn_"][data-base-url]')).reduce((obj, elm) => {
@@ -95,6 +94,13 @@ function updateModalUrls() {
     map_index: mapIndex,
   });
 
+  updateButtonUrl(buttons.mapped, {
+    _flt_3_dag_id: dagId,
+    _flt_3_task_id: taskId,
+    _flt_3_run_id: dagRunId,
+    _oc_TaskInstanceModelView: 'map_index',
+  });
+
   if (buttons.rendered_k8s) {
     updateButtonUrl(buttons.rendered_k8s, {
       dag_id: dagId,
@@ -104,19 +110,14 @@ function updateModalUrls() {
     });
   }
 
-  updateButtonUrl(buttons.ti, {
+  const tiButtonParams = {
     _flt_3_dag_id: dagId,
     _flt_3_task_id: taskId,
-    _flt_3_map_index: mapIndex,
     _oc_TaskInstanceModelView: 'dag_run.execution_date',
-  });
-
-  updateButtonUrl(buttons.mapped, {
-    _flt_3_dag_id: dagId,
-    _flt_3_task_id: taskId,
-    _flt_3_run_id: dagRunId,
-    _oc_TaskInstanceModelView: 'map_index',
-  });
+  };
+  // eslint-disable-next-line no-underscore-dangle
+  if (mapIndex >= 0) tiButtonParams._flt_0_map_index = mapIndex;
+  updateButtonUrl(buttons.ti, tiButtonParams);
 
   updateButtonUrl(buttons.log, {
     dag_id: dagId,
@@ -149,7 +150,7 @@ export function callModal({
   dagRunId: drID,
   mapIndex: mi,
   isMapped = false,
-  mappedLength = 0,
+  mappedStates = [],
 }) {
   taskId = t;
   const location = String(window.location);
@@ -160,12 +161,7 @@ export function callModal({
   dagRunId = drID;
   mapIndex = mi;
   if (isMapped) {
-    mapLength = mappedLength;
-  }
-  if (showBack) {
-    $('#btn_back').show();
-  } else {
-    $('#btn_back').hide();
+    mapStates = mappedStates;
   }
   $('#dag_run_id').text(drID);
   $('#task_id').text(t);
@@ -175,15 +171,11 @@ export function callModal({
   $('#extra_links').prev('hr').hide();
   $('#extra_links').empty().hide();
   if (mi >= 0) {
-    $('#modal_map_index').show();
-    $('#modal_map_index .value').text(mi);
     // Marking state and clear are not yet supported for mapped instances
     $('#success_action').hide();
     $('#failed_action').hide();
     $('#clear_action').hide();
   } else {
-    $('#modal_map_index').hide();
-    $('#modal_map_index .value').text('');
     $('#success_action').show();
     $('#failed_action').show();
     $('#clear_action').show();
@@ -196,29 +188,42 @@ export function callModal({
     subdagId = undefined;
   }
 
-  if (isMapped || mapIndex >= 0) {
-    $('#mapped_instances').show();
+  // Show a span or dropdown for mapIndex
+  if (mi >= 0 && !mapStates.length) {
+    $('#modal_map_index').show();
+    $('#modal_map_index .value').text(mi);
+    $('#mapped_dropdown').hide();
+  } else if (mi >= 0 || isMapped) {
+    $('#modal_map_index').show();
+    $('#modal_map_index .value').text('');
+    $('#mapped_dropdown').show();
+
+    const dropdownText = mapIndex > -1 ? mapIndex : `All  ${mapStates.length} Mapped Instances`;
+    $('#mapped_dropdown #dropdown-label').text(dropdownText);
+    $('#mapped_dropdown .dropdown-menu').empty();
+    $('#mapped_dropdown .dropdown-menu').append(`<li><a href="#" class="map_index_item" data-mapIndex="all">All ${mapStates.length} Mapped Instances</a></li>`);
+    mapStates.forEach((state, i) => {
+      $('#mapped_dropdown .dropdown-menu').append(`<li><a href="#" class="map_index_item" data-mapIndex="${i}">${i} - ${state}</a></li>`);
+    });
   } else {
-    $('#mapped_instances').hide();
+    $('#modal_map_index').hide();
+    $('#modal_map_index .value').text('');
+    $('#mapped_dropdown').hide();
   }
 
   if (isMapped) {
-    $('#mapped_dropdown #dropdown-label').text(`Mapped Instances [${mappedLength}]`);
-    $('#mapped_dropdown .dropdown-menu').empty();
-    [...Array(mappedLength)].forEach((_, i) => {
-      $('#mapped_dropdown .dropdown-menu').append(`<li><a href="#" class="map_index_item" data-mapIndex="${i}">${i}</a></li>`);
-    });
+    $('#task_actions').text(`Task Actions for all ${mappedStates.length} instances`);
     $('#btn_mapped').show();
     $('#mapped_dropdown').css('display', 'inline-block');
     $('#btn_rendered').hide();
     $('#btn_xcom').hide();
     $('#btn_log').hide();
   } else {
+    $('#task_actions').text('Task Actions');
     $('#btn_rendered').show();
     $('#btn_xcom').show();
     $('#btn_log').show();
     $('#btn_mapped').hide();
-    $('#mapped_dropdown').hide();
   }
 
   $('#dag_dl_logs').hide();
@@ -309,55 +314,29 @@ export function callModal({
 // Switch the modal from a mapped task summary to a specific mapped task instance
 $(document).on('click', '.map_index_item', function mapItem() {
   const mi = $(this).attr('data-mapIndex');
-  showBack = true;
-  callModal({
-    taskId,
-    executionDate,
-    dagRunId,
-    mapIndex: mi,
-  });
-});
-
-// Switch from a mapped task instance back to a mapped task summary
-$(document).on('click', '#btn_back', () => {
-  showBack = false;
-  callModal({
-    taskId,
-    executionDate,
-    dagRunId,
-    mapIndex: -1,
-    isMapped: true,
-    mappedLength: mapLength,
-  });
+  if (mi === 'all') {
+    callModal({
+      taskId,
+      executionDate,
+      dagRunId,
+      mapIndex: -1,
+      isMapped: true,
+      mappedStates: mapStates,
+    });
+  } else {
+    callModal({
+      taskId,
+      executionDate,
+      dagRunId,
+      mapIndex: mi,
+    });
+  }
 });
 
 // Task Instance Modal actions
 $('form[data-action]').on('submit', function submit(e) {
   e.preventDefault();
   const form = $(this).get(0);
-  // Somehow submit is fired twice. Only once is the executionDate/dagRunId valid
-  if (dagRunId || executionDate) {
-    if (form.dag_run_id) {
-      form.dag_run_id.value = dagRunId;
-    }
-    if (form.execution_date) {
-      form.execution_date.value = executionDate;
-    }
-    form.origin.value = window.location;
-    if (form.task_id) {
-      form.task_id.value = taskId;
-    }
-    if (form.map_index) {
-      form.map_index.value = mapIndex === undefined ? '' : mapIndex;
-    }
-    form.action = $(this).data('action');
-    form.submit();
-  }
-});
-
-// DAG Modal actions
-$('form button[data-action]').on('click', function onClick() {
-  const form = $(this).closest('form').get(0);
   // Somehow submit is fired twice. Only once is the executionDate/dagRunId valid
   if (dagRunId || executionDate) {
     if (form.dag_run_id) {
