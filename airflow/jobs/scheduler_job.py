27b60ddc170fbe7b7d2a127383bd29543ad300f2
Tanel Kiis <tanelk@users.noreply.github.com>
2022-04-21 12:51:38 +0300

    Update airflow/jobs/scheduler_job.py
    Co-authored-by: Tzu-ping Chung <uranusjr@gmail.com>

--- airflow/jobs/scheduler_job.py
+++ airflow/jobs/scheduler_job.py
@@ -273,7 +273,7 @@ class SchedulerJob(BaseJob):
 
         # If the pools are full, there is no point doing anything!
         # If _somehow_ the pool is overfull, don't let the limit go negative - it breaks SQL
-        pool_slots_free = sum(pool['open'] for pool in pools.values() if pool['open'] >= 0)
+        pool_slots_free = sum(max(0, pool['open']) for pool in pools.values())
 
         if pool_slots_free == 0:
             self.log.debug("All pools are full!")
