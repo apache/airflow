7bc1b829ee37d80d07b90493e8525389cc397ec5
Brent Bovenzi <brent.bovenzi@gmail.com>
2022-04-21 12:19:04 -0400

    Fix incorrect rebase

--- airflow/www/static/js/dag.js
+++ airflow/www/static/js/dag.js
@@ -352,9 +352,6 @@ $('form[data-action]').on('submit', function submit(e) {
     } else if (form.map_index) {
       form.map_index.remove();
     }
-    if (form.map_indexes) {
-      form.map_indexes.value = mapIndex === undefined ? '' : mapIndex;
-    }
     form.action = $(this).data('action');
     form.submit();
   }
