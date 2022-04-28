ad9f65518fbe708e15887cd296ebf5281e66dcc7
Ryan Hatter <ryan.hatter@astronomer.io>
2022-04-28 17:59:40 -0400

    fix cli airflow show dags for mapped operator

--- tests/utils/test_dot_renderer.py
+++ tests/utils/test_dot_renderer.py
@@ -109,16 +109,15 @@ class TestDotRenderer:
 
     def test_should_render_dag_with_abstract_mapped_operator(self, session, dag_maker):
         with dag_maker(dag_id="DAG_ID", session=session) as dag:
-            task_1 = BashOperator.partial(task_id="first").expand(
-                bash_command=["echo hello", "echo world"]
-            )
+            BashOperator.partial(task_id="first").expand(bash_command=["echo hello", "echo world"])
 
         dot = dot_renderer.render_dag(dag)
         source = dot.source
         # Should render DAG title
         assert "label=DAG_ID" in source
         assert (
-            'first [color="#000000" fillcolor="#f0ede4" label=first shape=rectangle style="filled,rounded"]' in source
+            'first [color="#000000" fillcolor="#f0ede4" label=first shape=rectangle style="filled,rounded"]'
+            in source
         )
 
     def test_should_render_dag_orientation(self, session, dag_maker):
