b0abeedfd052ce38c4eeec57e8f6b3642a2f5944
Jarek Potiuk <jarek@potiuk.com>
2022-04-30 22:45:03 +0200

    Extract "extra" labeling of the image to breeze from Dockerfile
    Fixes: #21046

--- dev/breeze/src/airflow_breeze/build_image/prod/build_prod_params.py
+++ dev/breeze/src/airflow_breeze/build_image/prod/build_prod_params.py
@@ -53,7 +53,6 @@ class BuildProdParams:
     airflow_is_in_context: bool = False
     install_packages_from_context: bool = False
     upgrade_to_newer_dependencies: bool = False
-    airflow_version: str = get_airflow_version()
     python: str = "3.7"
     airflow_branch_for_pypi_preloading: str = AIRFLOW_BRANCH
     install_airflow_reference: str = ""
@@ -86,6 +85,13 @@ class BuildProdParams:
     installation_method: str = "."
     debian_version: str = "bullseye"
     answer: Optional[str] = None
+
+    @property
+    def airflow_version(self) -> str:
+        if self.install_airflow_version:
+            return self.install_airflow_version
+        else:
+            return get_airflow_version()
 
     @property
     def airflow_branch(self) -> str:
@@ -170,7 +176,6 @@ class BuildProdParams:
                 ]
             )
             extra_build_flags.extend(self.args_for_remote_install)
-            self.airflow_version = self.install_airflow_reference
         elif len(self.install_airflow_version) > 0:
             if not re.match(r'^[0-9\.]+((a|b|rc|alpha|beta|pre)[0-9]+)?$', self.install_airflow_version):
                 get_console().print(
@@ -183,8 +188,15 @@ class BuildProdParams:
                 ["--build-arg", f"AIRFLOW_VERSION_SPECIFICATION==={self.install_airflow_version}"]
             )
             extra_build_flags.extend(["--build-arg", f"AIRFLOW_VERSION={self.install_airflow_version}"])
+            constraints_base = (
+                f"https://raw.githubusercontent.com/{self.github_repository}/"
+                f"{self.airflow_constraints_reference}"
+            )
+            constraints_location = (
+                f"{constraints_base}/constraints-{self.install_airflow_version}/constraints-{self.python}.txt"
+            )
+            self.airflow_constraints_location = constraints_location
             extra_build_flags.extend(self.args_for_remote_install)
-            self.airflow_version = self.install_airflow_version
         else:
             extra_build_flags.extend(
                 [
@@ -202,6 +214,15 @@ class BuildProdParams:
                     f"AIRFLOW_CONSTRAINTS_REFERENCE={self.default_constraints_branch}",
                 ]
             )
+        extra_build_flags.extend(
+            [
+                "--label",
+                "io.artifacthub.package.license=Apache-2.0",
+                "--label",
+                "io.artifacthub.package.readme-url=https://raw.githubusercontent.com/"
+                "apache/airflow/main/docs/docker-stack/README.md",
+            ]
+        )
         return extra_build_flags
 
     @property
