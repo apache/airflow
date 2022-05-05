03eeec1d33d4c19c069f1454361e31a6e3b44203
Daniel Standish <15932138+dstandish@users.noreply.github.com>
2022-05-04 17:44:10 -0700

    fix bind

--- airflow/utils/db.py
+++ airflow/utils/db.py
@@ -1067,7 +1067,7 @@ def _move_dangling_data_to_new_table(
     first_moved_row = session.execute(moved_rows_exist_query).all()
     if not first_moved_row:
         # no bad rows were found; drop moved rows table.
-        target_table.drop(bind=session.execute)
+        target_table.drop(bind=settings.engine)
     else:
         # purge the bad rows
         delete = source_table.delete().where(
