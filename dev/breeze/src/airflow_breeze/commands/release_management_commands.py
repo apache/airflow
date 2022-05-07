6e5955831672c71bfc0424dd50c8e72f6fd5b2a7
Jarek Potiuk <jarek@potiuk.com>
2022-05-07 23:53:11 +0200

    Add slim images to release process (#23391)
    This PR adds slim images to release process of Airflow.
    
    Those images are small as they do not contain any extras.
    
    Fixes: #20849

--- dev/breeze/src/airflow_breeze/commands/release_management_commands.py
+++ dev/breeze/src/airflow_breeze/commands/release_management_commands.py
@@ -17,18 +17,24 @@
 import multiprocessing as mp
 import shlex
 import sys
+import time
+from copy import deepcopy
+from re import match
 from subprocess import CalledProcessError, CompletedProcess
-from typing import IO, List, Optional, Tuple, Union
+from typing import IO, Dict, List, Optional, Tuple, Union
 
 import click
 
 from airflow_breeze.commands.main_command import main
 from airflow_breeze.global_constants import (
     ALLOWED_GENERATE_CONSTRAINTS_MODES,
+    ALLOWED_PLATFORMS,
+    CURRENT_PYTHON_MAJOR_MINOR_VERSIONS,
     DEFAULT_PYTHON_MAJOR_MINOR_VERSION,
     MOUNT_ALL,
     MOUNT_NONE,
     MOUNT_SELECTED,
+    MULTI_PLATFORM,
 )
 from airflow_breeze.params.shell_params import ShellParams
 from airflow_breeze.utils.common_options import (
@@ -97,7 +103,6 @@ RELEASE_MANAGEMENT_PARAMETERS = {
         {
             "name": "Provider documentation preparation flags",
             "options": [
-                "--skip-package-verification",
                 "--debug",
             ],
         }
@@ -121,13 +126,26 @@ RELEASE_MANAGEMENT_PARAMETERS = {
             ],
         },
     ],
+    "breeze release-prod-images": [
+        {
+            "name": "Release PROD IMAGE flags",
+            "options": [
+                "--airflow-version",
+                "--dockerhub-repo",
+                "--slim-images",
+                "--limit-python",
+                "--limit-platform",
+                "--skip-latest",
+            ],
+        }
+    ],
     "breeze find-newer-dependencies": [
         {
             "name": "Find newer dependencies flags",
             "options": [
                 "--python",
                 "--timezone",
-                "--constraints-reference",
+                "--constraints-branch",
                 "--updated-on-or-after",
                 "--max-age",
             ],
@@ -142,6 +160,7 @@ RELEASE_MANAGEMENT_COMMANDS = {
         "prepare-provider-documentation",
         "prepare-provider-packages",
         "prepare-airflow-package",
+        "release-prod-images",
         "generate-constraints",
         "find-newer-dependencies",
     ],
@@ -517,6 +536,205 @@ def verify_provider_packages(
         debug=debug,
     )
     sys.exit(result_command.returncode)
+
+
+def convert_build_args_dict_to_array_of_args(build_args: Dict[str, str]) -> List[str]:
+    array_of_args = []
+    for key, value in build_args.items():
+        array_of_args.append("--build-arg")
+        array_of_args.append(f'{key}={value}')
+    return array_of_args
+
+
+def alias_image(image_from: str, image_to: str, dry_run: bool, verbose: bool):
+    get_console().print(f"[info]Creating {image_to} alias for {image_from}[/]")
+    run_command(
+        ["regctl", "image", "copy", "--force-recursive", "--digest-tags", image_from, image_to],
+        dry_run=dry_run,
+        verbose=verbose,
+    )
+
+
+@main.command(
+    name="release-prod-images", help="Release production images to DockerHub (needs DockerHub permissions)."
+)
+@click.option('--airflow-version', required=True, help="Airflow version to release (2.3.0, 2.3.0rc1 etc.)")
+@click.option(
+    '--dockerhub-repo',
+    default="apache/airflow",
+    show_default=True,
+    help="DockerHub repository for the images",
+)
+@click.option(
+    '--slim-images',
+    is_flag=True,
+    help='Whether to prepare slim images instead of the regular ones.',
+)
+@click.option(
+    '--limit-python',
+    type=BetterChoice(CURRENT_PYTHON_MAJOR_MINOR_VERSIONS),
+    help="Specific python to build slim images for (if not specified - the images are built for all"
+    " available python versions)",
+)
+@click.option(
+    '--limit-platform',
+    type=BetterChoice(ALLOWED_PLATFORMS),
+    default=MULTI_PLATFORM,
+    show_default=True,
+    help="Specific platform to build images for (if not specified, multiplatform images will be built.",
+)
+@click.option(
+    '--skip-latest',
+    is_flag=True,
+    help="Whether to skip publishing the latest images (so that 'latest' images are not updated). "
+    "This should only be used if you release image for previous branches. Automatically set when "
+    "rc/alpha/beta images are built.",
+)
+@option_verbose
+@option_dry_run
+def release_prod_images(
+    airflow_version: str,
+    dockerhub_repo: str,
+    slim_images: bool,
+    limit_platform: str,
+    limit_python: Optional[str],
+    skip_latest: bool,
+    verbose: bool,
+    dry_run: bool,
+):
+    if not match(r"^\d*\.\d*\.\d*$", airflow_version):
+        get_console().print(
+            f"[warning]Skipping latest image tagging as this is a pre-release version: {airflow_version}"
+        )
+        skip_latest = True
+    else:
+        if skip_latest:
+            get_console().print("[info]Skipping latest image tagging as user requested it.[/]")
+        else:
+            get_console().print(
+                "[info]Also tagging the images with latest tags as this is release version.[/]"
+            )
+    result_docker_buildx = run_command(
+        ["docker", 'buildx', 'version'], check=False, dry_run=dry_run, verbose=verbose
+    )
+    if result_docker_buildx.returncode != 0:
+        get_console().print("[error]Docker buildx plugin must be installed to release the images[/]")
+        get_console().print()
+        get_console().print("See https://docs.docker.com/buildx/working-with-buildx/ for installation info.")
+        sys.exit(1)
+    result_inspect_builder = run_command(
+        ["docker", 'buildx', 'inspect', 'airflow_cache'], check=False, dry_run=dry_run, verbose=verbose
+    )
+    if result_inspect_builder.returncode != 0:
+        get_console().print("[error]Regctl must be installed and on PATH to release the images[/]")
+        get_console().print()
+        get_console().print(
+            "See https://github.com/apache/airflow/blob/main/dev/README_RELEASE_AIRFLOW.md"
+            "#setting-up-cache-refreshing-with-hardware-armamd-support for "
+            "instructions on setting it up."
+        )
+        sys.exit(1)
+    result_regctl = run_command(["regctl", 'version'], check=False, dry_run=dry_run, verbose=verbose)
+    if result_regctl.returncode != 0:
+        get_console().print("[error]Regctl must be installed and on PATH to release the images[/]")
+        get_console().print()
+        get_console().print(
+            "See https://github.com/regclient/regclient/blob/main/docs/regctl.md for installation info."
+        )
+        sys.exit(1)
+    python_versions = CURRENT_PYTHON_MAJOR_MINOR_VERSIONS if limit_python is None else [limit_python]
+    for python in python_versions:
+        if slim_images:
+            slim_build_args = {
+                "AIRFLOW_EXTRAS": "",
+                "AIRFLOW_CONSTRAINTS": "constraints-no-providers",
+                "PYTHON_BASE_IMAGE": f"python:{python}-slim-bullseye",
+                "AIRFLOW_VERSION": airflow_version,
+            }
+            get_console().print(f"[info]Building slim {airflow_version} image for Python {python}[/]")
+            python_build_args = deepcopy(slim_build_args)
+            slim_image_name = f"{dockerhub_repo}:slim-{airflow_version}-python{python}"
+            docker_buildx_command = [
+                "docker",
+                "buildx",
+                "build",
+                "--builder",
+                "airflow_cache",
+                *convert_build_args_dict_to_array_of_args(build_args=python_build_args),
+                "--platform",
+                limit_platform,
+                ".",
+                "-t",
+                slim_image_name,
+                "--push",
+            ]
+            run_command(docker_buildx_command, verbose=verbose, dry_run=dry_run)
+            if python == DEFAULT_PYTHON_MAJOR_MINOR_VERSION:
+                alias_image(
+                    slim_image_name,
+                    f"{dockerhub_repo}:slim-{airflow_version}",
+                    verbose=verbose,
+                    dry_run=dry_run,
+                )
+        else:
+            get_console().print(f"[info]Building regular {airflow_version} image for Python {python}[/]")
+            image_name = f"{dockerhub_repo}:{airflow_version}-python{python}"
+            regular_build_args = {
+                "PYTHON_BASE_IMAGE": f"python:{python}-slim-bullseye",
+                "AIRFLOW_VERSION": airflow_version,
+            }
+            docker_buildx_command = [
+                "docker",
+                "buildx",
+                "build",
+                "--builder",
+                "airflow_cache",
+                *convert_build_args_dict_to_array_of_args(build_args=regular_build_args),
+                "--platform",
+                limit_platform,
+                ".",
+                "-t",
+                image_name,
+                "--push",
+            ]
+            run_command(docker_buildx_command, verbose=verbose, dry_run=dry_run)
+            if python == DEFAULT_PYTHON_MAJOR_MINOR_VERSION:
+                alias_image(
+                    image_name, f"{dockerhub_repo}:{airflow_version}", verbose=verbose, dry_run=dry_run
+                )
+    # in case of re-tagging the images might need few seconds to refresh multi-platform images in DockerHub
+    time.sleep(10)
+    if not skip_latest:
+        get_console().print("[info]Replacing latest images with links to the newly created images.[/]")
+        for python in python_versions:
+            if slim_images:
+                alias_image(
+                    f"{dockerhub_repo}:slim-{airflow_version}-python{python}",
+                    f"{dockerhub_repo}:slim-latest-python{python}",
+                    verbose=verbose,
+                    dry_run=dry_run,
+                )
+            else:
+                alias_image(
+                    f"{dockerhub_repo}:{airflow_version}-python{python}",
+                    f"{dockerhub_repo}:latest-python{python}",
+                    verbose=verbose,
+                    dry_run=dry_run,
+                )
+        if slim_images:
+            alias_image(
+                f"{dockerhub_repo}:slim-{airflow_version}",
+                f"{dockerhub_repo}:slim-latest",
+                verbose=verbose,
+                dry_run=dry_run,
+            )
+        else:
+            alias_image(
+                f"{dockerhub_repo}:{airflow_version}",
+                f"{dockerhub_repo}:latest",
+                verbose=verbose,
+                dry_run=dry_run,
+            )
 
 
 @main.command(name="find-newer-dependencies", help="Finds which dependencies are being upgraded.")
