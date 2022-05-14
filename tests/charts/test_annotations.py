cda702f13b598bbf4dded9846afe08a083fde14a
moshederri <moshe.derri@appsflyer.com>
2022-05-14 21:05:06 +0300

    adding tests

--- tests/charts/test_annotations.py
+++ tests/charts/test_annotations.py
@@ -309,6 +309,19 @@ class TestServiceAccountAnnotations:
                 "example": "cleanup",
             },
         ),
+        (
+            {
+                "redis": {
+                    "podAnnotations": {
+                        "example": "redis",
+                    },
+                },
+            },
+            "templates/redis/redis-statefulset.yaml",
+            {
+                "example": "redis",
+            },
+        ),
     ],
 )
 class TestPerComponentPodAnnotations:
