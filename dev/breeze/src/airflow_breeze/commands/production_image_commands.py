b1ec576f85a3c5be45b9627d2fd231c538e7e918
Jarek Potiuk <jarek@potiuk.com>
2022-05-18 16:21:47 +0200

    Add tagging image as latest for CI image wait (#23775)
    The "wait for image" step lacked --tag-as-latest which made the
    subsequent "fix-ownership" step run sometimes far longer than
    needed - because it rebuilt the image for fix-ownership case.
    
    Also the "fix-ownership" command has been changed to just pull
    the image if one is missing locally rather than build. This
    command might be run in an environment where the image is missing
    or any other image was build (for example in jobs where an image
    was build for different Python version) in this case the command
    will simply use whatever Python version is available (it does
    not matter), or in case no image is available, it will pull the image
    as the last resort.
    
    (cherry picked from commit 5e3f652397005c5fac6c6b0099de345b5c39148d)

--- dev/breeze/src/airflow_breeze/commands/production_image_commands.py
+++ dev/breeze/src/airflow_breeze/commands/production_image_commands.py
@@ -71,8 +71,8 @@ from airflow_breeze.utils.docker_command_utils import (
     prepare_docker_build_command,
     prepare_empty_docker_build_command,
 )
+from airflow_breeze.utils.image import run_pull_image, run_pull_in_parallel
 from airflow_breeze.utils.path_utils import AIRFLOW_SOURCES_ROOT, DOCKER_CONTEXT_DIR
-from airflow_breeze.utils.pulll_image import run_pull_image, run_pull_in_parallel
 from airflow_breeze.utils.python_versions import get_python_version_list
 from airflow_breeze.utils.registry import login_to_github_docker_registry
 from airflow_breeze.utils.run_tests import verify_an_image
