b62586257faa5ca8db40dad82ead8b5433aa807c
Ephraim Anierobi <splendidzigy24@gmail.com>
2022-05-19 16:34:48 +0100

    fixup! Add TaskInstance State 'REMOVED' to finished states and success states

--- airflow/utils/state.py
+++ airflow/utils/state.py
@@ -193,7 +193,7 @@ class State:
     """
 
     success_states: FrozenSet[TaskInstanceState] = frozenset(
-        [TaskInstanceState.SUCCESS, TaskInstanceState.SKIPPED, TaskInstanceState.REMOVED]
+        [TaskInstanceState.SUCCESS, TaskInstanceState.SKIPPED]
     )
     """
     A list of states indicating that a task or dag is a success state.
