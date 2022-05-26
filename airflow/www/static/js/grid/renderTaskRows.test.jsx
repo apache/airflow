e0b833b0f1f9d8502cc8cf55113ae0df5af5b822
Brent Bovenzi <brent.bovenzi@gmail.com>
2022-05-26 17:31:41 +0200

    fix test data

--- airflow/www/static/js/grid/renderTaskRows.test.jsx
+++ airflow/www/static/js/grid/renderTaskRows.test.jsx
@@ -25,33 +25,16 @@ import { render } from '@testing-library/react';
 import renderTaskRows from './renderTaskRows';
 import { TableWrapper } from './utils/testUtils';
 
-const mockGroup = {
-  id: null,
-  label: null,
-  children: [
-    {
-      extraLinks: [],
-      id: 'group_1',
-      label: 'group_1',
-      instances: [
-        {
-          dagId: 'dagId',
-          duration: 0,
-          endDate: '2021-10-26T15:42:03.391939+00:00',
-          executionDate: '2021-10-25T15:41:09.726436+00:00',
-          operator: 'DummyOperator',
-          runId: 'run1',
-          startDate: '2021-10-26T15:42:03.391917+00:00',
-          state: 'success',
-          taskId: 'group_1',
-          tryNumber: 1,
-        },
-      ],
+describe('Test renderTaskRows', () => {
+  test('Renders name and task instance', () => {
+    const task = {
+      id: null,
+      label: null,
       children: [
         {
-          id: 'group_1.task_1',
-          label: 'group_1.task_1',
           extraLinks: [],
+          id: 'group_1',
+          label: 'group_1',
           instances: [
             {
               dagId: 'dagId',
@@ -62,42 +45,35 @@ const mockGroup = {
               runId: 'run1',
               startDate: '2021-10-26T15:42:03.391917+00:00',
               state: 'success',
-              taskId: 'group_1.task_1',
+              taskId: 'group_1',
               tryNumber: 1,
+            },
+          ],
+          children: [
+            {
+              id: 'group_1.task_1',
+              label: 'group_1.task_1',
+              extraLinks: [],
+              instances: [
+                {
+                  dagId: 'dagId',
+                  duration: 0,
+                  endDate: '2021-10-26T15:42:03.391939+00:00',
+                  executionDate: '2021-10-25T15:41:09.726436+00:00',
+                  operator: 'DummyOperator',
+                  runId: 'run1',
+                  startDate: '2021-10-26T15:42:03.391917+00:00',
+                  state: 'success',
+                  taskId: 'group_1.task_1',
+                  tryNumber: 1,
+                },
+              ],
             },
           ],
         },
       ],
-    },
-  ],
-  instances: [],
-};
-
-const nullMockGroup = {
-  id: null,
-  label: null,
-  children: [
-    {
-      extraLinks: [],
-      id: 'group_1',
-      label: 'group_1',
-      instances: [null],
-      children: [
-        {
-          id: 'group_1.task_1',
-          label: 'group_1.task_1',
-          extraLinks: [],
-          instances: [null],
-        },
-      ],
-    },
-  ],
-  instances: [null],
-};
-
-describe('Test renderTaskRows', () => {
-  test('Renders name and task instance', () => {
-    const task = mockGroup;
+      instances: [],
+    };
 
     const { queryByTestId, getByText } = render(
       <>{renderTaskRows({ task, dagRunIds: ['run1'] })}</>,
@@ -110,7 +86,19 @@ describe('Test renderTaskRows', () => {
   });
 
   test('Still renders names if there are no instances', () => {
-    const task = mockGroup;
+    const task = {
+      id: null,
+      label: null,
+      children: [
+        {
+          extraLinks: [],
+          id: 'group_1',
+          label: 'group_1',
+          instances: [],
+        },
+      ],
+      instances: [],
+    };
 
     const { queryByTestId, getByText } = render(
       <>{renderTaskRows({ task, dagRunIds: [] })}</>,
@@ -122,7 +110,27 @@ describe('Test renderTaskRows', () => {
   });
 
   test('Still renders correctly if task instance is null', () => {
-    const task = nullMockGroup;
+    const task = {
+      id: null,
+      label: null,
+      children: [
+        {
+          extraLinks: [],
+          id: 'group_1',
+          label: 'group_1',
+          instances: [null],
+          children: [
+            {
+              id: 'group_1.task_1',
+              label: 'group_1.task_1',
+              extraLinks: [],
+              instances: [null],
+            },
+          ],
+        },
+      ],
+      instances: [null],
+    };
 
     const { queryByTestId, getByText } = render(
       <>{renderTaskRows({ task, dagRunIds: ['run1'] })}</>,
