53725b879481d8b8ab6a2c3c03713193d2124f29
bharanidharan14 <bharanidharan.manimuthu@astronomer.io>
2022-05-27 14:28:46 +0530

    Fix mypy issue

--- airflow/providers/microsoft/azure/hooks/asb_message.py
+++ airflow/providers/microsoft/azure/hooks/asb_message.py
@@ -79,7 +79,7 @@ class ServiceBusMessageHook(BaseAzureServiceBusHook):
 
     @staticmethod
     def send_list_messages(sender: ServiceBusSender, messages: List[str]):
-        list_messages = [ServiceBusMessage(message) for message in messages]
+        list_messages: Union[List[ServiceBusMessage]] = [ServiceBusMessage(message) for message in messages]
         sender.send_messages(list_messages)
 
     @staticmethod
