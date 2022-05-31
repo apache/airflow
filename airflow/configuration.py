51b03eb4fafb426cfbb4d0cc05cb4a9081f3f4ec
Kamil Breguła <kamilbregula@apache.org>
2022-05-31 07:38:47 +0200

    Add enum validation for [webserver]analytics_tool

--- airflow/configuration.py
+++ airflow/configuration.py
@@ -254,6 +254,7 @@ class AirflowConfigParser(ConfigParser):
         ("logging", "fab_logging_level"): _available_logging_levels,
         # celery_logging_level can be empty, which uses logging_level as fallback
         ("logging", "celery_logging_level"): _available_logging_levels + [''],
+        ("webserver", "analytical_tool"): ['google_analytics', 'metarouter', 'segment', ''],
     }
 
     upgraded_values: Dict[Tuple[str, str], str]
