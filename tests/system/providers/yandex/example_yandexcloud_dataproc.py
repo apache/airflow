ae3836835c03fb75ccb44fc2c235ee014c09c88b
Chenglong Yan <alanx.yan@gmail.com>
2022-06-02 00:05:03 +0800

    Migrate Yandex example DAGs to new design AIP-47
    closes: #22470

--- tests/system/providers/yandex/example_yandexcloud_dataproc.py
+++ tests/system/providers/yandex/example_yandexcloud_dataproc.py
@@ -14,6 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+import os
 import uuid
 from datetime import datetime
 
@@ -35,9 +36,11 @@ AVAILABILITY_ZONE_ID = 'ru-central1-c'
 # Dataproc cluster jobs will produce logs in specified s3 bucket
 S3_BUCKET_NAME_FOR_JOB_LOGS = ''
 
+ENV_ID = os.environ.get("SYSTEM_TESTS_ENV_ID")
+DAG_ID = 'example_yandexcloud_dataproc_operator'
 
 with DAG(
-    'example_yandexcloud_dataproc_operator',
+    DAG_ID,
     schedule_interval=None,
     start_date=datetime(2021, 1, 1),
     tags=['example'],
@@ -153,3 +156,8 @@ with DAG(
 
     create_cluster >> create_mapreduce_job >> create_hive_query >> create_hive_query_from_file
     create_hive_query_from_file >> create_spark_job >> create_pyspark_job >> delete_cluster
+
+from tests.system.utils import get_test_run  # noqa: E402
+
+# Needed to run the example DAG with pytest (see: tests/system/README.md#run_via_pytest)
+test_run = get_test_run(dag)
