4aa685c24b10ab1540966e4d3ed0cfe65f698d52
Chethan UK <chethan.umesha@tessian.com>
2022-06-02 23:45:11 +0100

    Migrate Zendesk example DAGs to new design #22471

--- tests/system/providers/zendesk/example_zendesk_custom_get.py
+++ tests/system/providers/zendesk/example_zendesk_custom_get.py
@@ -15,12 +15,16 @@
 # specific language governing permissions and limitations
 # under the License.
 
+import os
 from datetime import datetime
 from typing import Dict, List
 
 from airflow import DAG
 from airflow.decorators import task
 from airflow.providers.zendesk.hooks.zendesk import ZendeskHook
+
+ENV_ID = os.environ.get("SYSTEM_TESTS_ENV_ID")
+DAG_ID = "zendesk_custom_get_dag"
 
 
 @task
@@ -33,9 +37,14 @@ def fetch_organizations() -> List[Dict]:
 
 
 with DAG(
-    dag_id="zendesk_custom_get_dag",
+    dag_id=DAG_ID,
     schedule_interval=None,
     start_date=datetime(2021, 1, 1),
     catchup=False,
 ) as dag:
     fetch_organizations()
+
+from tests.system.utils import get_test_run  # noqa: E402
+
+# Needed to run the example DAG with pytest (see: tests/system/README.md#run_via_pytest)
+test_run = get_test_run(dag)
