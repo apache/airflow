0e11fa32c522773ae1a1e7b6a72ef61e7876731d
Chethan UK <chethan.umesha@tessian.com>
2022-06-03 01:16:14 +0100

    Migrate Plexus example DAGs to new design #22457

--- tests/system/providers/plexus/example_plexus.py
+++ tests/system/providers/plexus/example_plexus.py
@@ -15,6 +15,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
+import os
 from datetime import datetime
 
 from airflow import DAG
@@ -22,26 +23,33 @@ from airflow.providers.plexus.operators.job import PlexusJobOperator
 
 HOME = '/home/acc'
 T3_PRERUN_SCRIPT = 'cp {home}/imdb/run_scripts/mlflow.sh {home}/ && chmod +x mlflow.sh'.format(home=HOME)
+ENV_ID = os.environ.get("SYSTEM_TESTS_ENV_ID")
+DAG_ID = "test"
 
-
-dag = DAG(
-    'test',
+with DAG(
+    DAG_ID,
     default_args={'owner': 'core scientific', 'retries': 1},
     description='testing plexus operator',
     start_date=datetime(2021, 1, 1),
     schedule_interval='@once',
     catchup=False,
-)
+) as dag:
+    # [START plexus_job_op]
+    t1 = PlexusJobOperator(
+        task_id='test',
+        job_params={
+            'name': 'test',
+            'app': 'MLFlow Pipeline 01',
+            'queue': 'DGX-2 (gpu:Tesla V100-SXM3-32GB)',
+            'num_nodes': 1,
+            'num_cores': 1,
+            'prerun_script': T3_PRERUN_SCRIPT,
+        },
+    )
+    # [END plexus_job_op]
 
-t1 = PlexusJobOperator(
-    task_id='test',
-    job_params={
-        'name': 'test',
-        'app': 'MLFlow Pipeline 01',
-        'queue': 'DGX-2 (gpu:Tesla V100-SXM3-32GB)',
-        'num_nodes': 1,
-        'num_cores': 1,
-        'prerun_script': T3_PRERUN_SCRIPT,
-    },
-    dag=dag,
-)
+
+from tests.system.utils import get_test_run  # noqa: E402
+
+# Needed to run the example DAG with pytest (see: tests/system/README.md#run_via_pytest)
+test_run = get_test_run(dag)
