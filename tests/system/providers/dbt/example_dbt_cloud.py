8f2762d142e75afa257e60e007281a960ee4ac10
Chethan UK <chethan.umesha@tessian.com>
2022-06-05 00:36:28 +0100

    AIP-47 - Migrate dbt DAGs to new design #22472

--- tests/system/providers/dbt/example_dbt_cloud.py
+++ tests/system/providers/dbt/example_dbt_cloud.py
@@ -17,21 +17,26 @@
 
 from datetime import datetime
 
-from airflow.models import DAG, BaseOperator
+from airflow.models import DAG
 
 try:
     from airflow.operators.empty import EmptyOperator
 except ModuleNotFoundError:
     from airflow.operators.dummy import DummyOperator as EmptyOperator  # type: ignore
+
 from airflow.providers.dbt.cloud.operators.dbt import (
     DbtCloudGetJobRunArtifactOperator,
     DbtCloudRunJobOperator,
 )
 from airflow.providers.dbt.cloud.sensors.dbt import DbtCloudJobRunSensor
 from airflow.utils.edgemodifier import Label
+from tests.system.utils import get_test_env_id
+
+ENV_ID = get_test_env_id()
+DAG_ID = "example_dbt_cloud"
 
 with DAG(
-    dag_id="example_dbt_cloud",
+    dag_id=DAG_ID,
     default_args={"dbt_cloud_conn_id": "dbt", "account_id": 39151},
     start_date=datetime(2021, 1, 1),
     schedule_interval=None,
@@ -50,7 +55,7 @@ with DAG(
     # [END howto_operator_dbt_cloud_run_job]
 
     # [START howto_operator_dbt_cloud_get_artifact]
-    get_run_results_artifact: BaseOperator = DbtCloudGetJobRunArtifactOperator(
+    get_run_results_artifact = DbtCloudGetJobRunArtifactOperator(
         task_id="get_run_results_artifact", run_id=trigger_job_run1.output, path="run_results.json"
     )
     # [END howto_operator_dbt_cloud_get_artifact]
@@ -65,7 +70,7 @@ with DAG(
     # [END howto_operator_dbt_cloud_run_job_async]
 
     # [START howto_operator_dbt_cloud_run_job_sensor]
-    job_run_sensor: BaseOperator = DbtCloudJobRunSensor(
+    job_run_sensor = DbtCloudJobRunSensor(
         task_id="job_run_sensor", run_id=trigger_job_run2.output, timeout=20
     )
     # [END howto_operator_dbt_cloud_run_job_sensor]
@@ -77,3 +82,15 @@ with DAG(
     # Task dependency created via `XComArgs`:
     # trigger_job_run1 >> get_run_results_artifact
     # trigger_job_run2 >> job_run_sensor
+
+    from tests.system.utils.watcher import watcher
+
+    # This test needs watcher in order to properly mark success/failure
+    # when "tearDown" task with trigger rule is part of the DAG
+    list(dag.tasks) >> watcher()
+
+
+from tests.system.utils import get_test_run  # noqa: E402
+
+# Needed to run the example DAG with pytest (see: tests/system/README.md#run_via_pytest)
+test_run = get_test_run(dag)
