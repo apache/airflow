0f0ddc9d7408ef3a0a9e50354f0c228cf4919872
Jarek Potiuk <jarek@potiuk.com>
2022-06-05 18:49:17 +0200

    Fix backwards-compatibility introduced by fixing mypy problems
    There was a backwards-incompatibility introduced by #23716 in
    two providers by using get_mandatory_value config method.
    
    This PR corrects that backwards compatibility and updates 2.1
    compatibility pre-commit to check for forbidden usage of
    get_mandatory_value.

--- airflow/providers/apache/spark/hooks/spark_submit.py
+++ airflow/providers/apache/spark/hooks/spark_submit.py
@@ -632,7 +632,10 @@ class SparkSubmitHook(BaseHook, LoggingMixin):
                     # we still attempt to kill the yarn application
                     renew_from_kt(self._principal, self._keytab, exit_on_fail=False)
                     env = os.environ.copy()
-                    env["KRB5CCNAME"] = airflow_conf.get_mandatory_value('kerberos', 'ccache')
+                    ccacche = airflow_conf.get('kerberos', 'ccache')
+                    if ccacche is None:
+                        raise ValueError("The kerberos/ccache config should be set here!")
+                    env["KRB5CCNAME"] = ccacche
 
                 with subprocess.Popen(
                     kill_cmd, env=env, stdout=subprocess.PIPE, stderr=subprocess.PIPE
