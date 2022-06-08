b6fc1c6e06ee680fa4f32560c79a3feec3de0c21
Chenglong Yan <alanx.yan@gmail.com>
2022-06-09 00:03:43 +0800

    Migrate Google calendar example DAG to new design AIP-47
    related: #22447

--- tests/system/providers/google/calendar/example_calendar_to_gcs.py
+++ tests/system/providers/google/calendar/example_calendar_to_gcs.py
@@ -20,24 +20,58 @@ import os
 from datetime import datetime
 
 from airflow import models
+from airflow.providers.google.cloud.operators.gcs import GCSCreateBucketOperator, GCSDeleteBucketOperator
 from airflow.providers.google.cloud.transfers.calendar_to_gcs import GoogleCalendarToGCSOperator
+from airflow.utils.trigger_rule import TriggerRule
 
-BUCKET = os.environ.get("GCP_GCS_BUCKET", "test28397yeo")
+ENV_ID = os.environ.get("SYSTEM_TESTS_ENV_ID")
+PROJECT_ID = os.environ.get("SYSTEM_TESTS_GCP_PROJECT")
+DAG_ID = "example_calendar_to_gcs"
+
+BUCKET_NAME = f"bucket_{DAG_ID}_{ENV_ID}"
 CALENDAR_ID = os.environ.get("CALENDAR_ID", "1234567890qwerty")
 API_VERSION = "v3"
 
 with models.DAG(
-    "example_calendar_to_gcs",
+    DAG_ID,
     schedule_interval='@once',  # Override to match your needs
-    start_date=datetime(2022, 1, 1),
+    start_date=datetime(2021, 1, 1),
     catchup=False,
-    tags=["example"],
+    tags=["example", "calendar"],
 ) as dag:
+    create_bucket = GCSCreateBucketOperator(
+        task_id="create_bucket", bucket_name=BUCKET_NAME, project_id=PROJECT_ID
+    )
+
     # [START upload_calendar_to_gcs]
     upload_calendar_to_gcs = GoogleCalendarToGCSOperator(
         task_id="upload_calendar_to_gcs",
-        destination_bucket=BUCKET,
+        destination_bucket=BUCKET_NAME,
         calendar_id=CALENDAR_ID,
         api_version=API_VERSION,
     )
     # [END upload_calendar_to_gcs]
+
+    delete_bucket = GCSDeleteBucketOperator(
+        task_id="delete_bucket", bucket_name=BUCKET_NAME, trigger_rule=TriggerRule.ALL_DONE
+    )
+
+    (
+        # TEST SETUP
+        create_bucket
+        # TEST BODY
+        >> upload_calendar_to_gcs
+        # TEST TEARDOWN
+        >> delete_bucket
+    )
+
+    from tests.system.utils.watcher import watcher
+
+    # This test needs watcher in order to properly mark success/failure
+    # when "tearDown" task with trigger rule is part of the DAG
+    list(dag.tasks) >> watcher()
+
+from tests.system.utils import get_test_run  # noqa: E402
+
+# Needed to run the example DAG with pytest (see: tests/system/README.md#run_via_pytest)
+test_run = get_test_run(dag)
