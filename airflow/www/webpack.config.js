127f8f4de02422ade8f2c84f84d3262d6efde185
Brent Bovenzi <brent.bovenzi@gmail.com>
2022-06-16 14:35:32 -0400

    Upgrade to webpack 5 (#24485)
    * update webpack-cli, eslint, stylelint, babel
    
    * revert stylelint changes
    
    * update more plugins
    
    * update to webpack 5
    
    * remove all resolutions

--- airflow/www/webpack.config.js
+++ airflow/www/webpack.config.js
@@ -19,12 +19,12 @@
 
 const webpack = require('webpack');
 const path = require('path');
-const ManifestPlugin = require('webpack-manifest-plugin');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 const cwplg = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const LicensePlugin = require('webpack-license-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
@@ -84,6 +84,7 @@ const config = {
     chunkFilename: '[name].[chunkhash].js',
     library: ['Airflow', '[name]'],
     libraryTarget: 'umd',
+    publicPath: '',
   },
   resolve: {
     extensions: [
@@ -98,15 +99,23 @@ const config = {
     rules: [
       {
         test: /datatables\.net.*/,
-        loader: 'imports-loader?define=>false',
+        use: [
+          {
+            loader: 'imports-loader?define=>false',
+          },
+        ],
       },
       {
         test: /\.[j|t]sx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-react', '@babel/preset-typescript'],
-        },
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react', '@babel/preset-typescript'],
+            },
+          },
+        ],
       },
       // Extract css files
       {
@@ -149,12 +158,19 @@ const config = {
       },
       {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader',
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
       },
     ],
   },
   plugins: [
-    new ManifestPlugin(),
+    new WebpackManifestPlugin({
+      // d3-tip is named index.js in its dist folder which was confusing the manifest
+      map: (file) => (file.path === 'd3-tip.js' ? { ...file, name: 'd3-tip.js' } : file),
+    }),
     new cwplg.CleanWebpackPlugin({
       verbose: true,
     }),
@@ -252,7 +268,7 @@ const config = {
   optimization: {
     minimize: process.env.NODE_ENV === 'production',
     minimizer: [
-      new OptimizeCSSAssetsPlugin({}),
+      new CssMinimizerPlugin({}),
       new TerserPlugin(),
     ],
   },
