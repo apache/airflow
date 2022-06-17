dbf56f33587a8dcabd7df9a0dbe088952c612c66
Daniel Standish <15932138+dstandish@users.noreply.github.com>
2022-06-17 09:05:25 -0700

    better warning

--- airflow/dag_processing/processor.py
+++ airflow/dag_processing/processor.py
@@ -573,7 +573,9 @@ class DagFileProcessor(LoggingMixin):
             task_pools = {task.pool for task in dag.tasks}
             nonexistent_pools = task_pools - pools
             if nonexistent_pools:
-                return f"Dag {dag.dag_id} references non-existent pools: {list(sorted(nonexistent_pools))!r}"
+                return (
+                    f"Dag '{dag.dag_id}' references non-existent pools: {list(sorted(nonexistent_pools))!r}"
+                )
 
         pools = {p.pool for p in Pool.get_pools(session)}
         for dag in dagbag.dags.values():
