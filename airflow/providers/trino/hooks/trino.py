122d2f69bbf39c445ddcaa5a9c09a9ea86434a55
Phani Kumar <94376113+phanikumv@users.noreply.github.com>
2022-06-21 23:39:15 +0530

    Add test_connection method to Trino hook (#24583)

--- airflow/providers/trino/hooks/trino.py
+++ airflow/providers/trino/hooks/trino.py
@@ -350,3 +350,19 @@ class TrinoHook(DbApiHook):
             commit_every = 0
 
         super().insert_rows(table, rows, target_fields, commit_every, replace)
+
+    def test_connection(self):
+        """Tests the connection from UI using Trino specific query"""
+        status, message = False, ''
+        try:
+            with closing(self.get_conn()) as conn:
+                with closing(conn.cursor()) as cur:
+                    cur.execute("select 1")
+                    if cur.fetchone():
+                        status = True
+                        message = 'Connection successfully tested'
+        except Exception as e:
+            status = False
+            message = str(e)
+
+        return status, message
