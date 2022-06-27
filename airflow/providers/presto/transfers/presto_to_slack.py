aac9fe10092b3037151e87cd7e719a6ce4e652ef
alexkruc <alexkru11@gmail.com>
2022-06-27 15:39:26 +0300

    removing typo in `PrestoToSlackOperator` documentation

--- airflow/providers/presto/transfers/presto_to_slack.py
+++ airflow/providers/presto/transfers/presto_to_slack.py
@@ -45,7 +45,7 @@ class PrestoToSlackOperator(SqlToSlackOperator):
     :param results_df_name: The name of the JINJA template's dataframe variable, default is 'results_df'
     :param parameters: The parameters to pass to the SQL query
     :param slack_token: The token to use to authenticate to Slack. If this is not provided, the
-        'webhook_token' attribute needs to be specified in the 'Extra' JSON field against the slack_conn_id.py
+        'webhook_token' attribute needs to be specified in the 'Extra' JSON field against the slack_conn_id
         Mutually exclusive with 'slack_conn_id'
     :param slack_channel: The channel to send message. Override default from Slack connection.
     """
