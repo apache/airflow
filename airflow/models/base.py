65bc228a4d50aa2c301462e992c0025716a799db
Ephraim Anierobi <splendidzigy24@gmail.com>
2022-07-08 09:05:58 +0100

    mirror existing naming convention

--- airflow/models/base.py
+++ airflow/models/base.py
@@ -32,7 +32,7 @@ naming_convention = {
     "ix": "idx_%(column_0_N_label)s",
     "uq": "%(table_name)s_%(column_0_N_name)s_uq",
     "ck": "ck_%(table_name)s_%(constraint_name)s",
-    "fk": "%(table_name)s_%(column_0_name)s_%(referred_table_name)s_fkey",
+    "fk": "%(table_name)s_%(column_0_name)s_fkey",
     "pk": "%(table_name)s_pkey",
 }
 
