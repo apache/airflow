20bb05e8df44f8cf71da5f65658bd52298f89e79
frankcash <fcash@astronomer.io>
2022-07-14 13:35:03 -0400

    removes comments

--- airflow/www/static/js/graph.js
+++ airflow/www/static/js/graph.js
@@ -598,13 +598,6 @@ function getNodeState(nodeId, tis) {
       childrenStates.add(state == null ? 'no_status' : state);
     }
   });
-
-  // In this order, if any of these states appeared in childrenStates, return it as
-  // the group state.
-  // const priority = ['failed', 'upstream_failed', 'up_for_retry', 'up_for_reschedule',
-  //   'queued', 'scheduled', 'sensing', 'running', 'shutdown', 'restarting', 'removed',
-  //   'no_status', 'success', 'skipped'];
-
   return priority.find((state) => childrenStates.has(state)) || 'no_status';
 }
 
