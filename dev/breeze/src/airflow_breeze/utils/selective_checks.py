32a7251b61f1028816c6107ffe7c736241de9779
Jarek Potiuk <jarek@potiuk.com>
2022-07-14 11:21:04 +0200

    Retrieve airflow branch/constraints from env variables
    We are using the "main" breeze to build even v2-3 images and
    it has to retrieve the branch and constraints not from the
    Python constants but from environment variables that are
    set by build-image.yaml. Otherwise "main" is used to push/pull
    image and constraints.
    
    This pr changes the retrieval in build image to retrieve
    branch, constraints branch (and debian version) from env
    variables if they are set.

--- dev/breeze/src/airflow_breeze/utils/selective_checks.py
+++ dev/breeze/src/airflow_breeze/utils/selective_checks.py
@@ -252,12 +252,14 @@ class SelectiveChecks:
         self,
         files: tuple[str, ...] = (),
         default_branch="main",
+        default_constraints_branch="constraints-main",
         commit_ref: str | None = None,
         pr_labels: tuple[str, ...] = (),
         github_event: GithubEvents = GithubEvents.PULL_REQUEST,
     ):
         self._files = files
         self._default_branch = default_branch
+        self._default_constraints_branch = default_constraints_branch
         self._commit_ref = commit_ref
         self._pr_labels = pr_labels
         self._github_event = github_event
@@ -292,6 +294,10 @@ class SelectiveChecks:
     @cached_property
     def default_branch(self) -> str:
         return self._default_branch
+
+    @cached_property
+    def default_constraints_branch(self) -> str:
+        return self._default_constraints_branch
 
     @cached_property
     def _full_tests_needed(self) -> bool:
