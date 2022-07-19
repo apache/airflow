436fe2d84877501b21e21450cf1167ec2fbd5fce
Jarek Potiuk <jarek@potiuk.com>
2022-07-19 12:25:17 +0200

    Fix PR label detection in CI (#25148)
    Label detection for incoming PR had few bugs:
    
    * wrong name of output was used for Build Info
    * assumption in selective checks was that PR labels are
      space separated, but they were really array formatted.
    * there was a $ typo in build-images.yaml
    
    This PR fixes all that:
    * output name and typo is corrected
    * we use ast.literal_eval now to parse the PR labels.
    
    (cherry picked from commit 9a939150c3bb02180e68edcc5ba62f50a0a219f7)

--- dev/breeze/src/airflow_breeze/commands/ci_commands.py
+++ dev/breeze/src/airflow_breeze/commands/ci_commands.py
@@ -14,6 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+import ast
 import os
 import platform
 import subprocess
@@ -244,7 +245,7 @@ def get_changed_files(commit_ref: Optional[str], dry_run: bool, verbose: bool) -
 )
 @click.option(
     '--pr-labels',
-    help="Space-separate list of labels which are valid for the PR",
+    help="Python array formatted PR labels assigned to the PR",
     default="",
     envvar="PR_LABELS",
 )
@@ -296,7 +297,7 @@ def selective_check(
         default_branch=default_branch,
         default_constraints_branch=default_constraints_branch,
         debian_version=debian_version,
-        pr_labels=tuple(" ".split(pr_labels)) if pr_labels else (),
+        pr_labels=tuple(ast.literal_eval(pr_labels)) if pr_labels else (),
         github_event=github_event,
     )
     print(str(sc))
