a3ec16592770ff1094eb1e89fac5526048cf0c8e
Brent Bovenzi <brent.bovenzi@gmail.com>
2022-07-21 16:34:02 -0400

    Dataset details view (#25208)
    * dataset details view
    
    * Fix sorting and naming
    
    * update table tests
    
    * update comments

--- airflow/www/static/js/api/index.ts
+++ airflow/www/static/js/api/index.ts
@@ -34,6 +34,8 @@ import useConfirmMarkTask from './useConfirmMarkTask';
 import useGridData from './useGridData';
 import useMappedInstances from './useMappedInstances';
 import useDatasets from './useDatasets';
+import useDataset from './useDataset';
+import useDatasetEvents from './useDatasetEvents';
 
 axios.interceptors.response.use(
   (res: AxiosResponse) => (res.data ? camelcaseKeys(res.data, { deep: true }) : res),
@@ -56,4 +58,6 @@ export {
   useGridData,
   useMappedInstances,
   useDatasets,
+  useDataset,
+  useDatasetEvents,
 };
