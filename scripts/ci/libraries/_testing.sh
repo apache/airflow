b246b2a4c6c0d28a0260c9b0a29d81ec16d4331f
Jarek Potiuk <jarek@potiuk.com>
2022-07-27 15:03:20 +0200

    Fix MsSQL failing on long list of providers. (#25338)
    The #25301 fixed too long names for files in case of long list
    of providers but there is one more place it needs to be fixed -
    in the volume name of MsSQL tests.

--- scripts/ci/libraries/_testing.sh
+++ scripts/ci/libraries/_testing.sh
@@ -143,7 +143,7 @@ function testing::setup_docker_compose_backend() {
             # so we need to mount an external volume for its db location
             # the external db must allow for parallel testing so TEST_TYPE
             # is added to the volume name
-            export MSSQL_DATA_VOLUME="${HOME}/tmp-mssql-volume-${TEST_TYPE}-${MSSQL_VERSION}"
+            export MSSQL_DATA_VOLUME="${HOME}/tmp-mssql-volume-${TEST_TYPE/\[*\]/}-${MSSQL_VERSION}"
             mkdir -p "${MSSQL_DATA_VOLUME}"
             # MSSQL 2019 runs with non-root user by default so we have to make the volumes world-writeable
             # This is a bit scary and we could get by making it group-writeable but the group would have
