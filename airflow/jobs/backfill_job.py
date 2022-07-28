221aa50b2c77518ea15aca9816a08793a86284ff
Kimyen Ladia <kimyen@github.com>
2022-07-28 16:55:04 -0700

    add debugging

--- airflow/jobs/backfill_job.py
+++ airflow/jobs/backfill_job.py
@@ -211,32 +211,38 @@ class BackfillJob(BaseJob):
         if filter_for_tis is not None:
             refreshed_tis = session.query(TI).filter(filter_for_tis).all()
 
+        self.log.debug("ti_status: %s", ti_status)
         for ti in refreshed_tis:
             # Here we remake the key by subtracting 1 to match in memory information
             reduced_key = ti.key.reduced
             if ti.state == State.SUCCESS:
                 ti_status.succeeded.add(reduced_key)
                 self.log.debug("Task instance %s succeeded. Don't rerun.", ti)
+                self.log.debug("reduced_key: %s", reduced_key)
                 _pop_running_ti(ti_status, reduced_key)
                 continue
             if ti.state == State.SKIPPED:
                 ti_status.skipped.add(reduced_key)
                 self.log.debug("Task instance %s skipped. Don't rerun.", ti)
+                self.log.debug("reduced_key: %s", reduced_key)
                 _pop_running_ti(ti_status, reduced_key)
                 continue
             if ti.state == State.FAILED:
                 self.log.error("Task instance %s failed", ti)
+                self.log.debug("reduced_key: %s", reduced_key)
                 ti_status.failed.add(reduced_key)
                 _pop_running_ti(ti_status, reduced_key)
                 continue
             # special case: if the task needs to run again put it back
             if ti.state == State.UP_FOR_RETRY:
                 self.log.warning("Task instance %s is up for retry", ti)
+                self.log.debug("reduced_key: %s", reduced_key)
                 _pop_running_ti(ti_status, reduced_key)
                 ti_status.to_run[ti.key] = ti
             # special case: if the task needs to be rescheduled put it back
             elif ti.state == State.UP_FOR_RESCHEDULE:
                 self.log.warning("Task instance %s is up for reschedule", ti)
+                self.log.debug("reduced_key: %s", reduced_key)
                 _pop_running_ti(ti_status, reduced_key)
                 ti_status.to_run[ti.key] = ti
             # special case: The state of the task can be set to NONE by the task itself
@@ -250,6 +256,7 @@ class BackfillJob(BaseJob):
                     "reaching concurrency limits. Re-adding task to queue.",
                     ti,
                 )
+                self.log.debug("reduced_key: %s", reduced_key)
                 tis_to_be_scheduled.append(ti)
                 _pop_running_ti(ti_status, reduced_key)
                 ti_status.to_run[ti.key] = ti
