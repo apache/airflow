a81c1c3bde25f05813bb8e8169f3aad3a8b7d9a9
Josh Fell <josh.d.fell@astronomer.io>
2022-07-29 16:44:44 -0400

    Use fixtures to handle teardowns

--- tests/utils/test_compression.py
+++ tests/utils/test_compression.py
@@ -20,74 +20,75 @@ import bz2
 import errno
 import filecmp
 import gzip
-import logging
 import shutil
 import tempfile
-import unittest
 
 import pytest
 
 from airflow.utils import compression
 
 
-class TestCompression(unittest.TestCase):
-    def setUp(self):
-        self.file_names = {}
-        try:
-            header = b"Sno\tSome,Text \n"
-            line1 = b"1\tAirflow Test\n"
-            line2 = b"2\tCompressionUtil\n"
-            self.tmp_dir = tempfile.mkdtemp(prefix='test_utils_compression_')
-            # create sample txt, gz and bz2 files
-            with tempfile.NamedTemporaryFile(mode='wb+', dir=self.tmp_dir, delete=False) as f_txt:
-                self._set_fn(f_txt.name, '.txt')
-                f_txt.writelines([header, line1, line2])
+@pytest.fixture
+def tmp_dir():
+    return tempfile.mkdtemp(prefix='test_utils_compression_')
 
-            fn_gz = self._get_fn('.txt') + ".gz"
-            with gzip.GzipFile(filename=fn_gz, mode="wb") as f_gz:
-                self._set_fn(fn_gz, '.gz')
-                f_gz.writelines([header, line1, line2])
 
-            fn_bz2 = self._get_fn('.txt') + '.bz2'
-            with bz2.BZ2File(filename=fn_bz2, mode="wb") as f_bz2:
-                self._set_fn(fn_bz2, '.bz2')
-                f_bz2.writelines([header, line1, line2])
-
-        # Base Exception so it catches Keyboard Interrupt
-        except BaseException:
-            logging.exception("An exception has occurred.")
-            self.tearDown()
-
-    def tearDown(self):
-        try:
-            shutil.rmtree(self.tmp_dir)
-        except OSError as e:
-            # ENOENT - no such file or directory
-            if e.errno != errno.ENOENT:
-                raise e
+@pytest.fixture
+def file_names(tmp_dir):
+    file_names = {}
 
     # Helper method to create a dictionary of file names and
     # file extension
-    def _set_fn(self, fn, ext):
-        self.file_names[ext] = fn
+    def _set_fn(fn, ext):
+        file_names[ext] = fn
 
     # Helper method to fetch a file of a
     # certain extension
-    def _get_fn(self, ext):
-        return self.file_names[ext]
+    def _get_fn(ext):
+        return file_names[ext]
 
-    def test_uncompress_file(self):
+    header = b"Sno\tSome,Text \n"
+    line1 = b"1\tAirflow Test\n"
+    line2 = b"2\tCompressionUtil\n"
+
+    # create sample txt, gz and bz2 files
+    with tempfile.NamedTemporaryFile(mode='wb+', dir=tmp_dir, delete=False) as f_txt:
+        _set_fn(f_txt.name, '.txt')
+        f_txt.writelines([header, line1, line2])
+
+    fn_gz = _get_fn('.txt') + ".gz"
+    with gzip.GzipFile(filename=fn_gz, mode="wb") as f_gz:
+        _set_fn(fn_gz, '.gz')
+        f_gz.writelines([header, line1, line2])
+
+    fn_bz2 = _get_fn('.txt') + '.bz2'
+    with bz2.BZ2File(filename=fn_bz2, mode="wb") as f_bz2:
+        _set_fn(fn_bz2, '.bz2')
+        f_bz2.writelines([header, line1, line2])
+
+    yield file_names
+
+    try:
+        shutil.rmtree(tmp_dir)
+    except OSError as e:
+        # ENOENT - no such file or directory
+        if e.errno != errno.ENOENT:
+            raise e
+
+
+class TestCompression:
+    def test_uncompress_file(self, tmp_dir, file_names):
         # Testing txt file type
         with pytest.raises(NotImplementedError, match="^Received .txt format. Only gz and bz2.*"):
             compression.uncompress_file(
                 **{'input_file_name': None, 'file_extension': '.txt', 'dest_dir': None},
             )
         # Testing gz file type
-        fn_txt = self._get_fn('.txt')
-        fn_gz = self._get_fn('.gz')
-        txt_gz = compression.uncompress_file(fn_gz, '.gz', self.tmp_dir)
+        fn_txt = file_names['.txt']
+        fn_gz = file_names['.gz']
+        txt_gz = compression.uncompress_file(fn_gz, '.gz', tmp_dir)
         assert filecmp.cmp(txt_gz, fn_txt, shallow=False), "Uncompressed file doest match original"
         # Testing bz2 file type
-        fn_bz2 = self._get_fn('.bz2')
-        txt_bz2 = compression.uncompress_file(fn_bz2, '.bz2', self.tmp_dir)
+        fn_bz2 = file_names['.bz2']
+        txt_bz2 = compression.uncompress_file(fn_bz2, '.bz2', tmp_dir)
         assert filecmp.cmp(txt_bz2, fn_txt, shallow=False), "Uncompressed file doest match original"
