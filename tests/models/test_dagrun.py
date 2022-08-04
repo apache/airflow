b7873901f553d7da5a9f0df626ca09a690d6a0a3
Ephraim Anierobi <splendidzigy24@gmail.com>
2022-08-04 14:12:32 +0100

    Apply suggestions from code review

--- tests/models/test_dagrun.py
+++ tests/models/test_dagrun.py
@@ -1228,7 +1228,10 @@ def test_mapped_length_increase_at_runtime_adds_additional_tis(dag_maker, sessio
 
 
 def test_mapped_literal_length_reduction_at_runtime_adds_removed_state(dag_maker, session):
-    """Test that when the length of mapped literal increases at runtime, additional ti is added"""
+    """
+    Test that when the length of mapped literal reduces at runtime, the missing task instances
+    are marked as removed
+    """
     from airflow.models import Variable
 
     Variable.set(key='arg1', value=[1, 2, 3])
