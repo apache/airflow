c60edf13db479ac214332e2d6e7bc00443807213
Syed Hussain <syedahsn@amazon.com>
2022-08-04 19:33:49 -0600

    Add template fields to EmrServerlessStartJobOperator

--- airflow/providers/amazon/aws/operators/emr.py
+++ airflow/providers/amazon/aws/operators/emr.py
@@ -520,7 +520,12 @@ class EmrServerlessStartJobOperator(BaseOperator):
     :param aws_conn_id: AWS connection to use
     """
 
-    template_fields: Sequence[str] = ('application_id',)
+    template_fields: Sequence[str] = (
+        'application_id',
+        'execution_role_arn',
+        'job_driver',
+        'configuration_overrides',
+    )
 
     def __init__(
         self,
