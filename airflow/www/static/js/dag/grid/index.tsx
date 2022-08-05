a01f6547ad2d892c923c06e205944f7043dafe27
pierrejeambrun <pierrejbrun@gmail.com>
2022-08-05 18:46:59 +0200

    Add right padding (#25554)

--- airflow/www/static/js/dag/grid/index.tsx
+++ airflow/www/static/js/dag/grid/index.tsx
@@ -126,6 +126,7 @@ const Grid = ({ isPanelOpen = false, onPanelToggle, hoveredTaskState }: Props) =
         ref={scrollRef}
         maxHeight="900px"
         position="relative"
+        pr={4}
       >
         <Table pr="10px">
           <Thead>
