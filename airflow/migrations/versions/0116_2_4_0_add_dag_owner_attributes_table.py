6a60a5b5cad7dba22a5a7e18857e32d03b7afffe
Jarek Potiuk <jarek@potiuk.com>
2022-08-08 09:08:36 +0200

    Attempt to reproduce error on migration when bad collation used

--- airflow/migrations/versions/0116_2_4_0_add_dag_owner_attributes_table.py
+++ airflow/migrations/versions/0116_2_4_0_add_dag_owner_attributes_table.py
@@ -27,8 +27,6 @@ Create Date: 2022-08-04 16:59:45.406589
 import sqlalchemy as sa
 from alembic import op
 
-from airflow.migrations.db_types import StringID
-
 # revision identifiers, used by Alembic.
 revision = '1486deb605b4'
 down_revision = 'f4ff391becb5'
@@ -41,7 +39,7 @@ def upgrade():
     """Apply Add ``DagOwnerAttributes`` table"""
     op.create_table(
         'dag_owner_attributes',
-        sa.Column('dag_id', StringID(), nullable=False),
+        sa.Column('dag_id', sa.String(length=250), nullable=False),
         sa.Column('owner', sa.String(length=500), nullable=False),
         sa.Column('link', sa.String(length=500), nullable=False),
         sa.ForeignKeyConstraint(['dag_id'], ['dag.dag_id'], ondelete='CASCADE'),
