d21011ae6e2dd12d8c84b40abe976b191619a6ac
vinit payal <vinit@tribes.ai>
2022-08-10 10:59:59 +0530

    - Documentation addition for google cloud tasks sensor - Fix the existing documentation for cloud tasks

---
+++ airflow/providers/google/cloud/example_dags/example_cloud_task_sensor.py
@@ -0,0 +1,57 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+"""
+Example Airflow DAG that sense a cloud task queue being empty.
+
+This DAG relies on the following OS environment variables
+
+* GCP_PROJECT_ID - Google Cloud project where the Compute Engine instance exists.
+* GCE_ZONE - Google Cloud zone where the instance exists.
+* GCE_INSTANCE - Name of the Compute Engine instance.
+* GCE_SHORT_MACHINE_TYPE_NAME - Machine type resource name to set, e.g. 'n1-standard-1'.
+    See https://cloud.google.com/compute/docs/machine-types
+"""
+
+import os
+
+from airflow import models
+from airflow.operators.dummy import DummyOperator
+from airflow.providers.google.cloud.sensors.tasks import TaskQueueEmptySensor
+
+GCP_PROJECT_ID = os.environ.get('GCP_PROJECT_ID', 'example-project')
+GCP_ZONE = os.environ.get('GCE_ZONE', 'europe-west1-b')
+QUEUE_NAME = os.environ.get('GCP_QUEUE_NAME', 'testqueue')
+
+
+with models.DAG(
+    'example_gcp_cloud_tasks_sensor',
+    schedule_interval=None,  # Override to match your needs
+    tags=['example'],
+) as dag:
+    start = DummyOperator(task_id="start")
+    # [START cloud_tasks_empty_sensor]
+    gcp_cloud_tasks_sensor = TaskQueueEmptySensor(
+        project_id=GCP_PROJECT_ID,
+        location=GCP_ZONE,
+        task_id='gcp_sense_cloud_tasks_empty',
+        queue_name=QUEUE_NAME
+    )
+    # [END cloud_tasks_empty_sensor]
+    end = DummyOperator(task_id="end")
+
