2ce0620b91e18a20c99e280f728d3cbfe5a41838
Daniel Hoherd <daniel.hoherd@gmail.com>
2022-08-11 17:46:59 -0400

    Add 'executor' label to airflow scheduler deployment

--- tests/charts/test_scheduler.py
+++ tests/charts/test_scheduler.py
@@ -628,3 +628,22 @@ class SchedulerTest(unittest.TestCase):
             show_only=["templates/scheduler/scheduler-deployment.yaml"],
         )
         assert {"foo": "bar"} == jmespath.search("spec.volumeClaimTemplates[0].metadata.annotations", docs[0])
+
+    @parameterized.expand(
+        "executor",
+        [
+            ("LocalExecutor"),
+            ("LocalKubernetesExecutor"),
+            ("CeleryExecutor"),
+            ("KubernetesExecutor"),
+            ("CeleryKubernetesExecutor"),
+        ],
+    )
+    def test_scheduler_deployment_has_executor_label(self, executor):
+        docs = render_chart(
+            values={"executor": executor},
+            show_only=["templates/scheduler/scheduler-deployment.yaml"],
+        )
+
+        assert 1 == len(docs)
+        assert executor == docs[0]['metadata']['labels'].get('executor')
