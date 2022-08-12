cebf2aceb831522ac31abda4b95c8408eff40af8
Chenglong Yan <alanx.yan@gmail.com>
2022-08-12 11:35:16 +0800

    Migrate Google example automl_video_intelligence_classification to new design AIP-47
    related: #22447, #22430

--- tests/system/providers/google/cloud/automl/example_automl_video_intelligence_classification.py
+++ tests/system/providers/google/cloud/automl/example_automl_video_intelligence_classification.py
@@ -32,6 +32,9 @@ from airflow.providers.google.cloud.operators.automl import (
     AutoMLTrainModelOperator,
 )
 
+ENV_ID = os.environ.get("SYSTEM_TESTS_ENV_ID")
+DAG_ID = "example_automl_video"
+
 GCP_PROJECT_ID = os.environ.get("GCP_PROJECT_ID", "your-project-id")
 GCP_AUTOML_LOCATION = os.environ.get("GCP_AUTOML_LOCATION", "us-central1")
 GCP_AUTOML_VIDEO_BUCKET = os.environ.get(
@@ -61,12 +64,13 @@ extract_object_id = CloudAutoMLHook.extract_object_id
 
 # Example DAG for AutoML Video Intelligence Classification
 with models.DAG(
-    "example_automl_video",
+    DAG_ID,
+    schedule="@once",  # Override to match your needs
     start_date=datetime(2021, 1, 1),
     catchup=False,
     user_defined_macros={"extract_object_id": extract_object_id},
-    tags=['example'],
-) as example_dag:
+    tags=['example', 'automl'],
+) as dag:
     create_dataset_task = AutoMLCreateDatasetOperator(
         task_id="create_dataset_task", dataset=DATASET, location=GCP_AUTOML_LOCATION
     )
@@ -100,11 +104,24 @@ with models.DAG(
         project_id=GCP_PROJECT_ID,
     )
 
-    import_dataset_task >> create_model
-    delete_model_task >> delete_datasets_task
+    (
+        # TEST SETUP
+        create_dataset_task
+        >> import_dataset_task
+        # TEST BODY
+        >> create_model
+        # TEST TEARDOWN
+        >> delete_model_task
+        >> delete_datasets_task
+    )
 
-    # Task dependencies created via `XComArgs`:
-    #   create_dataset_task >> import_dataset_task
-    #   create_dataset_task >> create_model
-    #   create_model >> delete_model_task
-    #   create_dataset_task >> delete_datasets_task
+    from tests.system.utils.watcher import watcher
+
+    # This test needs watcher in order to properly mark success/failure
+    # when "tearDown" task with trigger rule is part of the DAG
+    list(dag.tasks) >> watcher()
+
+from tests.system.utils import get_test_run  # noqa: E402
+
+# Needed to run the example DAG with pytest (see: tests/system/README.md#run_via_pytest)
+test_run = get_test_run(dag)
