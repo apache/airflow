a0d771bf4d5df07e05c30a4457c82181cf33a6b8
Tzu-ping Chung <uranusjr@gmail.com>
2022-08-12 16:35:49 +0800

    Slightly simplifies code and add type hints

--- airflow/providers/cncf/kubernetes/decorators/kubernetes.py
+++ airflow/providers/cncf/kubernetes/decorators/kubernetes.py
@@ -21,7 +21,7 @@ import pickle
 import uuid
 from tempfile import TemporaryDirectory
 from textwrap import dedent
-from typing import TYPE_CHECKING, Callable, Optional, Sequence, TypeVar
+from typing import TYPE_CHECKING, Callable, Optional, Sequence
 
 from kubernetes.client import models as k8s
 
@@ -35,13 +35,16 @@ from airflow.providers.cncf.kubernetes.python_kubernetes_script import (
 if TYPE_CHECKING:
     from airflow.utils.context import Context
 
+_PYTHON_SCRIPT_ENV = "__PYTHON_SCRIPT"
 
-def _generate_decode_command(env_var, file):
-    # We don't need `f.close()` as the interpreter is about to exit anyway
+_FILENAME_IN_CONTAINER = "/tmp/script.py"
+
+
+def _generate_decode_command() -> str:
     return (
         f'python -c "import base64, os;'
-        rf'x = os.environ[\"{env_var}\"];'
-        rf'f = open(\"{file}\", \"w\"); f.write(x);"'
+        rf'x = os.environ[\"{_PYTHON_SCRIPT_ENV}\"];'
+        rf'f = open(\"{_FILENAME_IN_CONTAINER}\", \"w\"); f.write(x); f.close()"'
     )
 
 
@@ -51,72 +54,21 @@ def _read_file_contents(filename):
 
 
 class _KubernetesDecoratedOperator(DecoratedOperator, KubernetesPodOperator):
-    """
-    Wraps a Python callable and executes in a kubernetes pod
-
-    :param python_callable: A reference to an object that is callable
-    :param op_kwargs: a dictionary of keyword arguments that will get unpacked
-        in your function (templated)
-    :param op_args: a list of positional arguments that will get unpacked when
-        calling your callable (templated)
-    :param multiple_outputs: if set, function return value will be
-        unrolled to multiple XCom values. Dict will unroll to xcom values with keys as keys.
-        Defaults to False.
-    """
-
     template_fields: Sequence[str] = ('op_args', 'op_kwargs')
 
     # since we won't mutate the arguments, we should just do the shallow copy
     # there are some cases we can't deepcopy the objects (e.g protobuf).
     shallow_copy_attrs: Sequence[str] = ('python_callable',)
 
-    def __init__(
-        self,
-        **kwargs,
-    ) -> None:
+    def __init__(self, namespace: str = "default", **kwargs) -> None:
         self.pickling_library = pickle
-
-        # Set defaults for name and namespace.
-        if 'name' not in kwargs:
-            kwargs['name'] = f'k8s_airflow_pod_{uuid.uuid4().hex}'
-
-        if 'namespace' not in kwargs:
-            kwargs['namespace'] = 'default'
-
-        super().__init__(**kwargs)
-
-    def execute(self, context: 'Context'):
-
-        with TemporaryDirectory(prefix='venv') as tmp_dir:
-            script_filename = os.path.join(tmp_dir, 'script.py')
-            py_source = self._get_python_source()
-
-            jinja_context = dict(
-                op_args=self.op_args,
-                op_kwargs=self.op_kwargs,
-                pickling_library=self.pickling_library.__name__,
-                python_callable=self.python_callable.__name__,
-                python_callable_source=py_source,
-                string_args_global=False,
-            )
-            write_python_script(
-                jinja_context=jinja_context,
-                filename=script_filename,
-                # template_file='python_kubernetes_script.jinja2',
-            )
-
-            self.env_vars.append(
-                k8s.V1EnvVar(name="__PYTHON_SCRIPT", value=_read_file_contents(script_filename))
-            )
-
-            self.cmds.append("bash")
-
-            self.arguments.append("-cx")
-            self.arguments.append(
-                f'{_generate_decode_command("__PYTHON_SCRIPT", "/tmp/script.py")} && python /tmp/script.py'
-            )
-
-            return super().execute(context)
+        super().__init__(
+            namespace=namespace,
+            name=kwargs.pop("name", f"k8s_airflow_pod_{uuid.uuid4().hex}"),
+            cmds=["bash"],
+            arguments=["-cx", f"{_generate_decode_command()} && python {_FILENAME_IN_CONTAINER}"],
+            **kwargs,
+        )
 
     def _get_python_source(self):
         raw_source = inspect.getsource(self.python_callable)
@@ -124,21 +76,43 @@ class _KubernetesDecoratedOperator(DecoratedOperator, KubernetesPodOperator):
         res = remove_task_decorator(res, "@task.kubernetes")
         return res
 
+    def execute(self, context: 'Context'):
+        with TemporaryDirectory(prefix="venv") as tmp_dir:
+            script_filename = os.path.join(tmp_dir, 'script.py')
+            py_source = self._get_python_source()
 
-T = TypeVar("T", bound=Callable)
+            jinja_context = {
+                "op_args": self.op_args,
+                "op_kwargs": self.op_kwargs,
+                "pickling_library": self.pickling_library.__name__,
+                "python_callable": self.python_callable.__name__,
+                "python_callable_source": py_source,
+                "string_args_global": False,
+            }
+            write_python_script(jinja_context=jinja_context, filename=script_filename)
+
+            self.env_vars = [
+                *self.env_vars,
+                k8s.V1EnvVar(name=_PYTHON_SCRIPT_ENV, value=_read_file_contents(script_filename)),
+            ]
+            return super().execute(context)
 
 
 def kubernetes_task(
-    python_callable: Optional[Callable] = None, multiple_outputs: Optional[bool] = None, **kwargs
+    python_callable: Optional[Callable] = None,
+    multiple_outputs: Optional[bool] = None,
+    **kwargs,
 ) -> TaskDecorator:
-    """
-    Kubernetes operator decorator. Wraps a function to be executed in K8s using KubernetesPodOperator.
-    Also accepts any argument that DockerOperator will via ``kwargs``. Can be reused in a single DAG.
+    """Kubernetes operator decorator.
+
+    This wraps a function to be executed in K8s using KubernetesPodOperator.
+    Also accepts any argument that DockerOperator will via ``kwargs``. Can be
+    reused in a single DAG.
 
     :param python_callable: Function to decorate
     :param multiple_outputs: if set, function return value will be
-        unrolled to multiple XCom values. Dict will unroll to xcom values with keys as XCom keys.
-        Defaults to False.
+        unrolled to multiple XCom values. Dict will unroll to xcom values with
+        keys as XCom keys. Defaults to False.
     """
     return task_decorator_factory(
         python_callable=python_callable,
