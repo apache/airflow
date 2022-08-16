11cf73b37b1c3b212c343ba6ace413ac0a66da3a
Phani Kumar <phani.kumar@astronomer.io>
2022-08-16 15:03:49 +0530

    Apply review suggestion

--- scripts/in_container/bin/install_aws.sh
+++ scripts/in_container/bin/install_aws.sh
@@ -35,7 +35,8 @@ if command -v aws; then
     exit 1
 fi
 
-if [[ $(uname -m) == "arm64" || $(uname -m) == "aarch64" ]]; then
+HOST=$(uname -m)
+if [[ ${HOST} == "arm64" || ${HOST} == "aarch64" ]]; then
     DOWNLOAD_URL="https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip"
 else
     DOWNLOAD_URL="https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
