6decd2563d17545c49ca865191b7695b7a51520c
blag <blag@users.noreply.github.com>
2022-08-17 23:19:54 -0700

    Import the database layer lately

--- airflow/datasets/manager.py
+++ airflow/datasets/manager.py
@@ -17,7 +17,6 @@
 # under the License.
 from sqlalchemy.orm.session import Session
 
-from airflow.models.dataset import DatasetDagRunQueue, DatasetEvent
 from airflow.utils.log.logging_mixin import LoggingMixin
 from airflow.utils.session import NEW_SESSION
 
@@ -38,7 +37,7 @@ class DatasetEventManager(LoggingMixin):
         the dataset event
         """
         from airflow.datasets import Dataset
-        from airflow.models.dataset import DatasetModel
+        from airflow.models.dataset import DatasetEvent, DatasetModel
 
         if isinstance(dataset, Dataset):
             dataset = session.query(DatasetModel).filter(DatasetModel.uri == dataset.uri).one_or_none()
@@ -58,6 +57,8 @@ class DatasetEventManager(LoggingMixin):
             self._queue_dagruns(dataset, session)
 
     def _queue_dagruns(self, dataset, session: Session) -> None:
+        from airflow.models.dataset import DatasetDagRunQueue
+
         consuming_dag_ids = [x.dag_id for x in dataset.consuming_dags]
         self.log.debug("consuming dag ids %s", consuming_dag_ids)
         for dag_id in consuming_dag_ids:
