6eb29d4e15320ce0ddb84aca9421ef1dd1f0204e
Pankaj <pankaj.singh@astronomer.io>
2022-08-23 22:56:31 +0530

    Fix test

--- tests/providers/amazon/aws/operators/test_redshift_cluster.py
+++ tests/providers/amazon/aws/operators/test_redshift_cluster.py
@@ -123,7 +123,7 @@ class TestRedshiftCreateClusterSnapshotOperator:
             ManualSnapshotRetentionPeriod=1,
         )
 
-        mock_get_conn.return_value.get_waiter.wait.assert_not_called()
+        mock_get_conn.return_value.get_waiter.assert_not_called()
 
     @mock.patch("airflow.providers.amazon.aws.hooks.redshift_cluster.RedshiftHook.cluster_status")
     def test_raise_exception_when_cluster_is_not_available(self, mock_cluster_status):
