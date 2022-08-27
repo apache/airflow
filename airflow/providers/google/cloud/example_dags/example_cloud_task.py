8345f34b14832e1a9ace866384d38e491b2b17fc
vinit payal <vinitpayal@gmail.com>
2022-08-27 06:43:39 +0530

    Google Cloud Tasks Sensor for queue being empty (#25622)

---
+++ airflow/providers/google/cloud/example_dags/example_cloud_task.py
@@ -0,0 +1,53 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+"""
+Example Airflow DAG that sense a cloud task queue being empty.
+
+This DAG relies on the following OS environment variables
+
+* GCP_PROJECT_ID - Google Cloud project where the Compute Engine instance exists.
+* GCP_ZONE - Google Cloud zone where the cloud task queue exists.
+* QUEUE_NAME - Name of the cloud task queue.
+"""
+
+import os
+from datetime import datetime
+
+from airflow import models
+from airflow.providers.google.cloud.sensors.tasks import TaskQueueEmptySensor
+
+GCP_PROJECT_ID = os.environ.get('GCP_PROJECT_ID', 'example-project')
+GCP_ZONE = os.environ.get('GCE_ZONE', 'europe-west1-b')
+QUEUE_NAME = os.environ.get('GCP_QUEUE_NAME', 'testqueue')
+
+
+with models.DAG(
+    'example_gcp_cloud_tasks_sensor',
+    start_date=datetime(2022, 8, 8),
+    catchup=False,
+    tags=['example'],
+) as dag:
+    # [START cloud_tasks_empty_sensor]
+    gcp_cloud_tasks_sensor = TaskQueueEmptySensor(
+        project_id=GCP_PROJECT_ID,
+        location=GCP_ZONE,
+        task_id='gcp_sense_cloud_tasks_empty',
+        queue_name=QUEUE_NAME,
+    )
+    # [END cloud_tasks_empty_sensor]
---
+++ airflow/providers/google/cloud/sensors/tasks.py
@@ -0,0 +1,87 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""This module contains a Google Cloud Task sensor."""
+from typing import TYPE_CHECKING, Optional, Sequence, Union
+
+from airflow.providers.google.cloud.hooks.tasks import CloudTasksHook
+from airflow.sensors.base import BaseSensorOperator
+
+if TYPE_CHECKING:
+    from airflow.utils.context import Context
+
+
+class TaskQueueEmptySensor(BaseSensorOperator):
+    """
+    Pulls tasks count from a cloud task queue.
+    Always waits for queue returning tasks count as 0.
+
+    :param project_id: the Google Cloud project ID for the subscription (templated)
+    :param gcp_conn_id: The connection ID to use connecting to Google Cloud.
+    :param queue_name: The queue name to for which task empty sensing is required.
+    :param impersonation_chain: Optional service account to impersonate using short-term
+        credentials, or chained list of accounts required to get the access_token
+        of the last account in the list, which will be impersonated in the request.
+        If set as a string, the account must grant the originating account
+        the Service Account Token Creator IAM role.
+        If set as a sequence, the identities from the list must grant
+        Service Account Token Creator IAM role to the directly preceding identity, with first
+        account from the list granting this role to the originating account (templated).
+    """
+
+    template_fields: Sequence[str] = (
+        "project_id",
+        "location",
+        "queue_name",
+        "gcp_conn_id",
+        "impersonation_chain",
+    )
+
+    def __init__(
+        self,
+        *,
+        location: str,
+        project_id: Optional[str] = None,
+        queue_name: Optional[str] = None,
+        gcp_conn_id: str = "google_cloud_default",
+        impersonation_chain: Optional[Union[str, Sequence[str]]] = None,
+        **kwargs,
+    ) -> None:
+        super().__init__(**kwargs)
+        self.location = location
+        self.project_id = project_id
+        self.queue_name = queue_name
+        self.gcp_conn_id = gcp_conn_id
+        self.impersonation_chain = impersonation_chain
+
+    def poke(self, context: "Context") -> bool:
+
+        hook = CloudTasksHook(
+            gcp_conn_id=self.gcp_conn_id,
+            impersonation_chain=self.impersonation_chain,
+        )
+
+        # TODO uncomment page_size once https://issuetracker.google.com/issues/155978649?pli=1 gets fixed
+        tasks = hook.list_tasks(
+            location=self.location,
+            queue_name=self.queue_name,
+            # page_size=1
+        )
+
+        self.log.info("tasks exhausted in cloud task queue?: %s" % (len(tasks) == 0))
+
+        return len(tasks) == 0
