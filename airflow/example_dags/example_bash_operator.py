58473fb265ff48fbfac0af7b459a4b76754f6141
Jarek Potiuk <jarek@potiuk.com>
2022-08-31 22:47:59 +0200

    Change the template to use human readable task_instance description (#25960)
    The task_instance_key_str description is a better template
    example as a human-readable description of the task.

--- airflow/example_dags/example_bash_operator.py
+++ airflow/example_dags/example_bash_operator.py
@@ -58,7 +58,7 @@ with DAG(
     # [START howto_operator_bash_template]
     also_run_this = BashOperator(
         task_id='also_run_this',
-        bash_command='echo "run_id={{ run_id }} | dag_run={{ dag_run }}"',
+        bash_command='echo "ti_key={{ task_instance_key_str }}"',
     )
     # [END howto_operator_bash_template]
     also_run_this >> run_this_last
