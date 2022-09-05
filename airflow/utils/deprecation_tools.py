5264b63a8327a6c7918b9eee412efc2aabb4f3ce
Jarek Potiuk <jarek@potiuk.com>
2022-09-05 17:24:26 +0200

    Removed deprecated contrib files and replace them with PEP-562 getattr (#26153)
    The conrib modules are carried from Airflow 1.10 and we cannot
    remove the classes that were used there without bumping Airlfow
    to 3, however we can replace the files with dynamic attribute
    retrieval following PEP-562. This way we will have far less number
    of files in contrib and users will not get auto-import and
    autocomplete when they will be developing their DAGs.
    
    This does not break compatibility but it providers much stronger
    signal to the users that they should switch to new classes:
    
    * they will not see the classes in autocomplete and autoimport
      any more
    * it will seem that the classes are not existing in contrib until
      they are used
    * if anyone performs static analysis on the DAGs, the analysis
      will fail (and this is an intended behaviour)

---
+++ airflow/utils/deprecation_tools.py
@@ -0,0 +1,47 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import functools
+import importlib
+import sys
+import warnings
+from types import ModuleType
+from typing import Dict
+
+
+def getattr_with_deprecation(imports: Dict[str, str], module: str, name: str):
+    target_class_full_name = imports.get(name)
+    if not target_class_full_name:
+        raise AttributeError(f"The module `{module!r}` has no attribute `{name!r}`")
+    warnings.warn(
+        f"The `{module}.{name}` class is deprecated. Please use `{target_class_full_name!r}`.",
+        DeprecationWarning,
+        stacklevel=2,
+    )
+    new_module, new_class_name = target_class_full_name.rsplit('.', 1)
+    return getattr(importlib.import_module(new_module), new_class_name)
+
+
+def add_deprecated_classes(module_imports: Dict[str, Dict[str, str]], package: str):
+    for module_name, imports in module_imports.items():
+        full_module_name = f"{package}.{module_name}"
+        module_type = ModuleType(full_module_name)
+        # Mypy is not able to derive the right function signature https://github.com/python/mypy/issues/2427
+        module_type.__getattr__ = functools.partial(  # type: ignore[assignment]
+            getattr_with_deprecation, imports, full_module_name
+        )
+        sys.modules.setdefault(full_module_name, module_type)
