65bbcee52484b80b4fb7c5957b59c2b3f983d029
Peter Debelak <pdebelak@gmail.com>
2022-09-07 19:35:43 -0500

    Undo secrets backend config caching (#26223)
    Revert "Fix unhashable issue with secrets.backend_kwargs and caching (#25970)"

--- airflow/configuration.py
+++ airflow/configuration.py
@@ -1544,20 +1544,16 @@ def ensure_secrets_loaded() -> List[BaseSecretsBackend]:
 def get_custom_secret_backend() -> Optional[BaseSecretsBackend]:
     """Get Secret Backend if defined in airflow.cfg"""
     secrets_backend_cls = conf.getimport(section='secrets', key='backend')
+
     if secrets_backend_cls:
-        backends: Any = conf.get(section='secrets', key='backend_kwargs', fallback='{}')
-        return _custom_secrets_backend(secrets_backend_cls, backends)
+        try:
+            backends: Any = conf.get(section='secrets', key='backend_kwargs', fallback='{}')
+            alternative_secrets_config_dict = json.loads(backends)
+        except JSONDecodeError:
+            alternative_secrets_config_dict = {}
+
+        return secrets_backend_cls(**alternative_secrets_config_dict)
     return None
-
-
-@functools.lru_cache(maxsize=2)
-def _custom_secrets_backend(secrets_backend_cls, backend_kwargs):
-    """Separate function to create secrets backend instance to allow caching"""
-    try:
-        alternative_secrets_config_dict = json.loads(backend_kwargs)
-    except JSONDecodeError:
-        alternative_secrets_config_dict = {}
-    return secrets_backend_cls(**alternative_secrets_config_dict)
 
 
 def initialize_secrets_backends() -> List[BaseSecretsBackend]:
