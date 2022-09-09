cceeaadbddb7c6a9ac4afbeecdb2fe96f14935e2
Igor <408698+cra@users.noreply.github.com>
2022-09-09 04:05:52 +0200

    Add more weekday operator and sensor examples #26071 (#26098)

--- airflow/example_dags/example_branch_day_of_week_operator.py
+++ airflow/example_dags/example_branch_day_of_week_operator.py
@@ -24,6 +24,7 @@ import pendulum
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 from airflow.operators.weekday import BranchDayOfWeekOperator
+from airflow.utils.weekday import WeekDay
 
 with DAG(
     dag_id="example_weekday_branch_operator",
@@ -35,6 +36,8 @@ with DAG(
     # [START howto_operator_day_of_week_branch]
     empty_task_1 = EmptyOperator(task_id='branch_true')
     empty_task_2 = EmptyOperator(task_id='branch_false')
+    empty_task_3 = EmptyOperator(task_id='branch_weekend')
+    empty_task_4 = EmptyOperator(task_id='branch_mid_week')
 
     branch = BranchDayOfWeekOperator(
         task_id="make_choice",
@@ -42,7 +45,15 @@ with DAG(
         follow_task_ids_if_false="branch_false",
         week_day="Monday",
     )
+    branch_weekend = BranchDayOfWeekOperator(
+        task_id="make_weekend_choice",
+        follow_task_ids_if_true="branch_weekend",
+        follow_task_ids_if_false="branch_mid_week",
+        week_day={WeekDay.SATURDAY, WeekDay.SUNDAY},
+    )
 
-    # Run empty_task_1 if branch executes on Monday
+    # Run empty_task_1 if branch executes on Monday, empty_task_2 otherwise
     branch >> [empty_task_1, empty_task_2]
+    # Run empty_task_3 if it's a weekend, empty_task_4 otherwise
+    empty_task_2 >> branch_weekend >> [empty_task_3, empty_task_4]
     # [END howto_operator_day_of_week_branch]
