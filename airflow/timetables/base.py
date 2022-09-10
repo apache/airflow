7f88e0d928977b10aa26aed1e9fb0d5076067c13
Jarek Potiuk <jarek@potiuk.com>
2022-09-10 19:09:24 +0200

    Convert airflow/timetables to __future__.annotations

--- airflow/timetables/base.py
+++ airflow/timetables/base.py
@@ -14,8 +14,9 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Dict, NamedTuple, Optional, Sequence
+from typing import TYPE_CHECKING, Any, NamedTuple, Sequence
 
 from pendulum import DateTime
 
@@ -36,7 +37,7 @@ class DataInterval(NamedTuple):
     end: DateTime
 
     @classmethod
-    def exact(cls, at: DateTime) -> "DataInterval":
+    def exact(cls, at: DateTime) -> DataInterval:
         """Represent an "interval" containing only an exact time."""
         return cls(start=at, end=at)
 
@@ -57,8 +58,8 @@ class TimeRestriction(NamedTuple):
     created by Airflow.
     """
 
-    earliest: Optional[DateTime]
-    latest: Optional[DateTime]
+    earliest: DateTime | None
+    latest: DateTime | None
     catchup: bool
 
 
@@ -79,12 +80,12 @@ class DagRunInfo(NamedTuple):
     """The data interval this DagRun to operate over."""
 
     @classmethod
-    def exact(cls, at: DateTime) -> "DagRunInfo":
+    def exact(cls, at: DateTime) -> DagRunInfo:
         """Represent a run on an exact time."""
         return cls(run_after=at, data_interval=DataInterval.exact(at))
 
     @classmethod
-    def interval(cls, start: DateTime, end: DateTime) -> "DagRunInfo":
+    def interval(cls, start: DateTime, end: DateTime) -> DagRunInfo:
         """Represent a run on a continuous schedule.
 
         In such a schedule, each data interval starts right after the previous
@@ -94,7 +95,7 @@ class DagRunInfo(NamedTuple):
         return cls(run_after=end, data_interval=DataInterval(start, end))
 
     @property
-    def logical_date(self: "DagRunInfo") -> DateTime:
+    def logical_date(self: DagRunInfo) -> DateTime:
         """Infer the logical date to represent a DagRun.
 
         This replaces ``execution_date`` in Airflow 2.1 and prior. The idea is
@@ -135,7 +136,7 @@ class Timetable(Protocol):
     """
 
     @classmethod
-    def deserialize(cls, data: Dict[str, Any]) -> "Timetable":
+    def deserialize(cls, data: dict[str, Any]) -> Timetable:
         """Deserialize a timetable from data.
 
         This is called when a serialized DAG is deserialized. ``data`` will be
@@ -144,7 +145,7 @@ class Timetable(Protocol):
         """
         return cls()
 
-    def serialize(self) -> Dict[str, Any]:
+    def serialize(self) -> dict[str, Any]:
         """Serialize the timetable for JSON encoding.
 
         This is called during DAG serialization to store timetable information
@@ -185,9 +186,9 @@ class Timetable(Protocol):
     def next_dagrun_info(
         self,
         *,
-        last_automated_data_interval: Optional[DataInterval],
+        last_automated_data_interval: DataInterval | None,
         restriction: TimeRestriction,
-    ) -> Optional[DagRunInfo]:
+    ) -> DagRunInfo | None:
         """Provide information to schedule the next DagRun.
 
         The default implementation raises ``NotImplementedError``.
@@ -207,9 +208,9 @@ class Timetable(Protocol):
     def generate_run_id(
         self,
         *,
-        run_type: "DagRunType",
+        run_type: DagRunType,
         logical_date: DateTime,
-        data_interval: Optional[DataInterval],
+        data_interval: DataInterval | None,
         **extra,
     ) -> str:
         return run_type.generate_run_id(logical_date)
