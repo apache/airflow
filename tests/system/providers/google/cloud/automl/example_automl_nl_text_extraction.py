3791ab097129ae302b11dbcad4f83ea8d104d92d
Jarek Potiuk <jarek@potiuk.com>
2022-09-10 19:46:57 +0200

    Convert tests/system to __future__.annotations

--- tests/system/providers/google/cloud/automl/example_automl_nl_text_extraction.py
+++ tests/system/providers/google/cloud/automl/example_automl_nl_text_extraction.py
@@ -15,10 +15,11 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-
 """
 Example Airflow DAG that uses Google AutoML services.
 """
+from __future__ import annotations
+
 import os
 from datetime import datetime
 from typing import cast
