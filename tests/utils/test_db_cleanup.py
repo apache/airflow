9c9c65c10c0936b381af6014ff3528eba513f997
Jarek Potiuk <jarek@potiuk.com>
2022-09-10 20:05:43 +0200

    Convert all airflow tests to __future__.annotations

--- tests/utils/test_db_cleanup.py
+++ tests/utils/test_db_cleanup.py
@@ -15,6 +15,8 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+from __future__ import annotations
+
 from contextlib import suppress
 from importlib import import_module
 from pathlib import Path
