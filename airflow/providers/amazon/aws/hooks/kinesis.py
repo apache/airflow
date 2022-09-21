0ccbb74b4764a17e608e9d16cd44df0f6c9ceda1
Paul Stanton <paul.stanton@selectquote.com>
2022-09-20 20:03:23 -0500

    Added a basic hook for AWS Kinesis data streams to easily put records from Airflow.

--- airflow/providers/amazon/aws/hooks/kinesis.py
+++ airflow/providers/amazon/aws/hooks/kinesis.py
@@ -15,7 +15,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-"""This module contains AWS Firehose hook"""
+"""This module contains AWS Kinesis hooks"""
 from __future__ import annotations
 
 from typing import Iterable
@@ -44,3 +44,41 @@ class FirehoseHook(AwsBaseHook):
     def put_records(self, records: Iterable):
         """Write batch records to Kinesis Firehose"""
         return self.get_conn().put_record_batch(DeliveryStreamName=self.delivery_stream, Records=records)
+
+
+class KinesisHook(AwsBaseHook):
+    """
+    Interact with AWS Kinesis Data Stream.
+
+    Additional arguments (such as ``aws_conn_id``) may be specified and
+    are passed down to the underlying AwsBaseHook.
+
+    :param delivery_stream: Name of the data stream
+
+    AWS Kinesis docs: https://docs.aws.amazon.com/streams/latest/dev/introduction.html
+
+    .. seealso::
+        :class:`~airflow.providers.amazon.aws.hooks.base_aws.AwsBaseHook`
+    """
+
+    def __init__(self, delivery_stream: str, *args, **kwargs) -> None:
+        self.delivery_stream = delivery_stream
+        kwargs["client_type"] = "kinesis"
+        super().__init__(*args, **kwargs)
+
+    def put_records(self, records: list[dict[str, str| bytes]]):
+        """
+        Write records to Kinesis Data Stream
+
+        Each record may have these keys:
+            Data: bytes -
+                Data blob of up to 1 MB.
+            PartitionKey: str -
+                A partition key is used to group data by shard within a stream.
+                Partition keys are Unicode strings with a maximum length limit of 256 characters for each key.
+            ExplicitHashKey: str (optional) -
+                Overrides the partition key to shard mapping.
+
+        AWS Kinesis API reference: https://docs.aws.amazon.com/kinesis/latest/APIReference/API_PutRecords.html
+        """
+        return self.get_conn().put_records(StreamName=self.delivery_stream, Records=records)
