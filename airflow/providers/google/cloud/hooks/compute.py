0db01197e67bb8cd387b8507a392fc835087f460
VladaZakharova <vladazaharova0@gmail.com>
2022-09-22 12:26:17 +0200

    change doc string in hook

--- airflow/providers/google/cloud/hooks/compute.py
+++ airflow/providers/google/cloud/hooks/compute.py
@@ -271,10 +271,9 @@ class ComputeEngineHook(GoogleBaseHook):
             and network_interfaces and etc will be passed to Instance as they are specified
             in the Instance Template.
             Full or partial URL and can be represented as examples below:
-            1. "https://www.googleapis.com/compute/v1/projects/your-project-name/global/instanceTemplates/"
-"your-instanceTemplate-name"
-            2. "projects/your-project-name/global/instanceTemplates/your-instanceTemplate-name"
-            3. "global/instanceTemplates/your-instanceTemplate-name"
+            1. "https://www.googleapis.com/compute/v1/projects/your-project/global/instanceTemplates/temp"
+            2. "projects/your-project/global/instanceTemplates/temp"
+            3. "global/instanceTemplates/temp"
         :param request_id: Unique request_id that you might add to achieve
             full idempotence (for example when client call times out repeating the request
             with the same request id will not create a new instance template again)
