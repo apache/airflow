1a44249266f1b42075ef5405c8c9999169ba91d8
Andrey Anshin <Andrey.Anshin@taragol.is>
2022-09-27 18:41:20 +0400

    Refactor `SlackWebhookOperator`: Get rid of mandatory http-provider dependency (#26648)
    * Refactor `SlackWebhookOperator`: Get rid of mandatory http-provider dependency
    
    * Fix tests selective tests for slack

--- tests/providers/slack/operators/test_slack_webhook.py
+++ tests/providers/slack/operators/test_slack_webhook.py
@@ -17,55 +17,97 @@
 # under the License.
 from __future__ import annotations
 
-import unittest
-from typing import Sequence
+from unittest import mock
 
-from airflow.models.dag import DAG
+import pytest
+
+from airflow.exceptions import AirflowException
 from airflow.providers.slack.operators.slack_webhook import SlackWebhookOperator
-from airflow.utils import timezone
-
-DEFAULT_DATE = timezone.datetime(2017, 1, 1)
 
 
-class TestSlackWebhookOperator(unittest.TestCase):
-    _config = {
-        'http_conn_id': 'slack-webhook-default',
-        'webhook_token': 'manual_token',
-        'message': 'your message here',
-        'attachments': [{'fallback': 'Required plain-text summary'}],
-        'blocks': [{'type': 'section', 'text': {'type': 'mrkdwn', 'text': '*bold text*'}}],
-        'channel': '#general',
-        'username': 'SlackMcSlackFace',
-        'icon_emoji': ':hankey',
-        'icon_url': 'https://airflow.apache.org/_images/pin_large.png',
-        'link_names': True,
-        'proxy': 'https://my-horrible-proxy.proxyist.com:8080',
-    }
+class TestSlackWebhookOperator:
+    def setup_method(self):
+        self.default_op_kwargs = {
+            "channel": None,
+            "username": None,
+            "icon_emoji": None,
+            "icon_url": None,
+        }
 
-    def setUp(self):
-        args = {'owner': 'airflow', 'start_date': DEFAULT_DATE}
-        self.dag = DAG('test_dag_id', default_args=args)
+    @pytest.mark.parametrize(
+        "simple_http_op_attr",
+        [
+            "endpoint",
+            "method",
+            "data",
+            "headers",
+            "response_check",
+            "response_filter",
+            "extra_options",
+            "log_response",
+            "auth_type",
+            "tcp_keep_alive",
+            "tcp_keep_alive_idle",
+            "tcp_keep_alive_count",
+            "tcp_keep_alive_interval",
+        ],
+    )
+    def test_unused_deprecated_http_operator_kwargs(self, simple_http_op_attr):
+        """
+        Test remove deprecated (and unused) SimpleHttpOperator keyword arguments.
+        No error should happen if provide any of attribute, unless operator allow to provide this attributes.
+        """
+        kw = {simple_http_op_attr: "foo-bar"}
+        warning_message = fr"Provide '{simple_http_op_attr}' is deprecated and as has no affect"
+        with pytest.warns(DeprecationWarning, match=warning_message):
+            SlackWebhookOperator(task_id="test_unused_args", **kw)
 
-    def test_execute(self):
-        # Given / When
-        operator = SlackWebhookOperator(task_id='slack_webhook_job', dag=self.dag, **self._config)
+    def test_deprecated_http_conn_id(self):
+        """Test resolve deprecated http_conn_id."""
+        warning_message = (
+            r"Parameter `http_conn_id` is deprecated. Please use `slack_webhook_conn_id` instead."
+        )
+        with pytest.warns(DeprecationWarning, match=warning_message):
+            op = SlackWebhookOperator(
+                task_id='test_deprecated_http_conn_id', slack_webhook_conn_id=None, http_conn_id="http_conn"
+            )
+        assert op.slack_webhook_conn_id == "http_conn"
 
-        assert self._config['http_conn_id'] == operator.http_conn_id
-        assert self._config['webhook_token'] == operator.webhook_token
-        assert self._config['message'] == operator.message
-        assert self._config['attachments'] == operator.attachments
-        assert self._config['blocks'] == operator.blocks
-        assert self._config['channel'] == operator.channel
-        assert self._config['username'] == operator.username
-        assert self._config['icon_emoji'] == operator.icon_emoji
-        assert self._config['icon_url'] == operator.icon_url
-        assert self._config['link_names'] == operator.link_names
-        assert self._config['proxy'] == operator.proxy
+        error_message = "You cannot provide both `slack_webhook_conn_id` and `http_conn_id`."
+        with pytest.raises(AirflowException, match=error_message):
+            with pytest.warns(DeprecationWarning, match=warning_message):
+                SlackWebhookOperator(
+                    task_id='test_both_conn_ids',
+                    slack_webhook_conn_id="slack_webhook_conn_id",
+                    http_conn_id="http_conn",
+                )
+
+    @pytest.mark.parametrize(
+        "slack_webhook_conn_id,webhook_token",
+        [
+            ("test_conn_id", None),
+            (None, "https://hooks.slack.com/services/T000/B000/XXX"),
+            ("test_conn_id", "https://hooks.slack.com/services/T000/B000/XXX"),
+        ],
+    )
+    @pytest.mark.parametrize("proxy", [None, "https://localhost:9999"])
+    @mock.patch("airflow.providers.slack.operators.slack_webhook.SlackWebhookHook")
+    def test_hook(self, mock_slackwebhook_cls, slack_webhook_conn_id, webhook_token, proxy):
+        """Test get cached ``SlackWebhookHook`` hook."""
+        op_kw = {
+            "slack_webhook_conn_id": slack_webhook_conn_id,
+            "proxy": proxy,
+            "webhook_token": webhook_token,
+        }
+        op = SlackWebhookOperator(task_id='test_hook', **op_kw)
+        hook = op.hook
+        assert hook is op.hook, "Expected cached hook"
+        mock_slackwebhook_cls.assert_called_once_with(**op_kw)
 
     def test_assert_templated_fields(self):
-        operator = SlackWebhookOperator(task_id='slack_webhook_job', dag=self.dag, **self._config)
-
-        template_fields: Sequence[str] = (
+        """Test expected templated fields."""
+        operator = SlackWebhookOperator(task_id='test_assert_templated_fields', **self.default_op_kwargs)
+        template_fields = (
             'webhook_token',
             'message',
             'attachments',
@@ -74,5 +116,52 @@ class TestSlackWebhookOperator(unittest.TestCase):
             'username',
             'proxy',
         )
-
         assert operator.template_fields == template_fields
+
+    @pytest.mark.parametrize(
+        "message,blocks,attachments",
+        [
+            ("Test Text", ["Dummy Block"], ["Test Attachments"]),
+            ("Test Text", ["Dummy Block"], None),
+            ("Test Text", None, None),
+            (None, ["Dummy Block"], None),
+            (None, ["Dummy Block"], ["Test Attachments"]),
+            (None, None, ["Test Attachments"]),
+        ],
+    )
+    @pytest.mark.parametrize(
+        "channel,username,icon_emoji,icon_url",
+        [
+            (None, None, None, None),
+            ("legacy-channel", "legacy-username", "legacy-icon_emoji", "legacy-icon-url"),
+        ],
+        ids=["webhook-attrs", "legacy-webhook-attrs"],
+    )
+    @mock.patch("airflow.providers.slack.operators.slack_webhook.SlackWebhookHook")
+    def test_execute_operator(
+        self, mock_slackwebhook_cls, message, blocks, attachments, channel, username, icon_emoji, icon_url
+    ):
+        mock_slackwebhook = mock_slackwebhook_cls.return_value
+        mock_slackwebhook_send = mock_slackwebhook.send
+        op = SlackWebhookOperator(
+            task_id="test_execute",
+            slack_webhook_conn_id="test_conn_id",
+            message=message,
+            blocks=blocks,
+            attachments=attachments,
+            channel=channel,
+            username=username,
+            icon_emoji=icon_emoji,
+            icon_url=icon_url,
+        )
+        op.execute(mock.MagicMock())
+        mock_slackwebhook_send.assert_called_once_with(
+            text=message,
+            blocks=blocks,
+            attachments=attachments,
+            channel=channel,
+            username=username,
+            icon_emoji=icon_emoji,
+            icon_url=icon_url,
+            link_names=mock.ANY,
+        )
