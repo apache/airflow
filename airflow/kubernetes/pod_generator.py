d99d23dbfeac474ef65b7e6a71440aff90278a7e
Dov Benyomin Sohacheski <b@kloud.email>
2022-09-30 11:58:03 +0300

    added docs and removed old changes

--- airflow/kubernetes/pod_generator.py
+++ airflow/kubernetes/pod_generator.py
@@ -36,7 +36,6 @@ from dateutil import parser
 from kubernetes.client import models as k8s
 from kubernetes.client.api_client import ApiClient
 
-from airflow.configuration import conf
 from airflow.exceptions import AirflowConfigException, PodReconciliationError, RemovedInAirflow3Warning
 from airflow.kubernetes.pod_generator_deprecated import PodDefaults, PodGenerator as PodGeneratorDeprecated
 from airflow.utils import yaml
@@ -147,10 +146,7 @@ class PodGenerator:
         pod_cp.spec.volumes.insert(0, PodDefaults.VOLUME)
         pod_cp.spec.containers[0].volume_mounts = pod_cp.spec.containers[0].volume_mounts or []
         pod_cp.spec.containers[0].volume_mounts.insert(0, PodDefaults.VOLUME_MOUNT)
-
-        sidecar = copy.deepcopy(PodDefaults.SIDECAR_CONTAINER)
-        sidecar.image = conf.get('kubernetes', 'sidecar_container_image', fallback=sidecar.image)
-        pod_cp.spec.containers.append(sidecar)
+        pod_cp.spec.containers.append(PodDefaults.SIDECAR_CONTAINER)
 
         return pod_cp
 
