ba7371d2856afbf1ce77aea768165f0fcb4774e1
VladaZakharova <vladazaharova0@gmail.com>
2022-10-07 22:55:26 +0200

    Update gcs_to_bigquery.py

--- airflow/providers/google/cloud/transfers/gcs_to_bigquery.py
+++ airflow/providers/google/cloud/transfers/gcs_to_bigquery.py
@@ -75,12 +75,12 @@ class GCSToBigQueryOperator(BaseOperator):
     :param skip_leading_rows: The number of rows at the top of a CSV file that BigQuery
         will skip when loading the data.
         When autodetect is on, the behavior is the following:
-        skipLeadingRows unspecified - Autodetect tries to detect headers in the first row.
+        skip_leading_rows unspecified - Autodetect tries to detect headers in the first row.
         If they are not detected, the row is read as data. Otherwise, data is read starting
         from the second row.
-        skipLeadingRows is 0 - Instructs autodetect that there are no headers and data
+        skip_leading_rows is 0 - Instructs autodetect that there are no headers and data
         should be read starting from the first row.
-        skipLeadingRows = N > 0 - Autodetect skips N-1 rows and tries to detect headers
+        skip_leading_rows = N > 0 - Autodetect skips N-1 rows and tries to detect headers
         in row N. If headers are not detected, row N is just skipped. Otherwise, row N is
         used to extract column names for the detected schema.
         Default value set to None so that autodetect option can detect schema fields.
