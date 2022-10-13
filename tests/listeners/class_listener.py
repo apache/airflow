79b0163b0f8bd1c6e5d5bd0c6193f066a2854d72
Maciej Obuchowski <obuchowski.maciej@gmail.com>
2022-10-13 11:24:20 +0200

    notification: add dag status notification system
    Signed-off-by: Maciej Obuchowski <obuchowski.maciej@gmail.com>

--- tests/listeners/class_listener.py
+++ tests/listeners/class_listener.py
@@ -1,4 +1,3 @@
-#
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -15,45 +14,36 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+
 from __future__ import annotations
 
 from airflow.listeners import hookimpl
 from airflow.utils.state import State
 
-started_component = None
-stopped_component = None
-state = []
 
+class ClassBasedListener:
+    def __init__(self):
+        self.started_component = None
+        self.stopped_component = None
+        self.state = []
 
-@hookimpl
-def on_starting(component):
-    global started_component
-    started_component = component
+    @hookimpl
+    def on_starting(self, component):
+        self.started_component = component
 
+    @hookimpl
+    def before_stopping(self, component):
+        global stopped_component
+        stopped_component = component
 
-@hookimpl
-def before_stopping(component):
-    global stopped_component
-    stopped_component = component
+    @hookimpl
+    def on_task_instance_running(self, previous_state, task_instance, session):
+        self.state.append(State.RUNNING)
 
+    @hookimpl
+    def on_task_instance_success(self, previous_state, task_instance, session):
+        self.state.append(State.SUCCESS)
 
-@hookimpl
-def on_task_instance_running(previous_state, task_instance, session):
-    state.append(State.RUNNING)
-
-
-@hookimpl
-def on_task_instance_success(previous_state, task_instance, session):
-    state.append(State.SUCCESS)
-
-
-@hookimpl
-def on_task_instance_failed(previous_state, task_instance, session):
-    state.append(State.FAILED)
-
-
-def clear():
-    global started_component, stopped_component, state
-    started_component = None
-    stopped_component = None
-    state = []
+    @hookimpl
+    def on_task_instance_failed(self, previous_state, task_instance, session):
+        self.state.append(State.FAILED)
