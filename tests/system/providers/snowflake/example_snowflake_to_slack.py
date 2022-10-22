5d49ee66bb98e63e771ee56312ceef4d21f0b1e9
Kamil Breguła <kamilbregula@apache.org>
2022-10-22 22:30:47 +0100

    Fix system tests for SnowflakeOperator

--- tests/system/providers/snowflake/example_snowflake_to_slack.py
+++ tests/system/providers/snowflake/example_snowflake_to_slack.py
@@ -24,21 +24,21 @@ import os
 from datetime import datetime
 
 from airflow import DAG
-from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from airflow.providers.snowflake.transfers.snowflake_to_slack import SnowflakeToSlackOperator
 
 SNOWFLAKE_CONN_ID = 'my_snowflake_conn'
+SLACK_CONN_ID = 'my_slack_conn'
 SNOWFLAKE_SAMPLE_TABLE = 'sample_table'
 
 # SQL commands
-CREATE_TABLE_SQL_STRING = (
-    f"CREATE OR REPLACE TRANSIENT TABLE {SNOWFLAKE_SAMPLE_TABLE} (name VARCHAR(250), id INT);"
+SNOWFLAKE_SLACK_SQL = f"SELECT name, id FROM {SNOWFLAKE_SAMPLE_TABLE} LIMIT 10;"
+SNOWFLAKE_SLACK_MESSAGE = (
+    "Results in an ASCII table:\n```{{ results_df | tabulate(tablefmt='pretty', headers='keys') }}```"
 )
-SQL_INSERT_STATEMENT = f"INSERT INTO {SNOWFLAKE_SAMPLE_TABLE} VALUES ('name', %(id)s)"
-SQL_LIST = [SQL_INSERT_STATEMENT % {"id": n} for n in range(0, 10)]
-SQL_MULTIPLE_STMTS = "; ".join(SQL_LIST)
 ENV_ID = os.environ.get("SYSTEM_TESTS_ENV_ID")
-DAG_ID = "example_snowflake"
+DAG_ID = "example_snowflake_to_slack"
 
+# [START howto_operator_snowflake]
 
 with DAG(
     DAG_ID,
@@ -48,38 +48,16 @@ with DAG(
     schedule="@once",
     catchup=False,
 ) as dag:
-    # [START howto_operator_snowflake]
-    snowflake_op_sql_str = SnowflakeOperator(task_id='snowflake_op_sql_str', sql=CREATE_TABLE_SQL_STRING)
+    # [START howto_operator_snowflake_to_slack]
 
-    snowflake_op_with_params = SnowflakeOperator(
-        task_id='snowflake_op_with_params',
-        sql=SQL_INSERT_STATEMENT,
-        parameters={"id": 56},
+    slack_report = SnowflakeToSlackOperator(
+        task_id="slack_report",
+        sql=SNOWFLAKE_SLACK_SQL,
+        slack_message=SNOWFLAKE_SLACK_MESSAGE,
+        slack_conn_id=SLACK_CONN_ID,
     )
 
-    snowflake_op_sql_list = SnowflakeOperator(task_id='snowflake_op_sql_list', sql=SQL_LIST)
-
-    snowflake_op_sql_multiple_stmts = SnowflakeOperator(
-        task_id='snowflake_op_sql_multiple_stmts',
-        sql=SQL_MULTIPLE_STMTS,
-    )
-
-    snowflake_op_template_file = SnowflakeOperator(
-        task_id='snowflake_op_template_file',
-        sql='example_snowflake_snowflake_op_template_file.sql',
-    )
-
-    # [END howto_operator_snowflake]
-
-    (
-        snowflake_op_sql_str
-        >> [
-            snowflake_op_with_params,
-            snowflake_op_sql_list,
-            snowflake_op_template_file,
-            snowflake_op_sql_multiple_stmts,
-        ]
-    )
+    # [END howto_operator_snowflake_to_slack]
 
 
 from tests.system.utils import get_test_run  # noqa: E402
