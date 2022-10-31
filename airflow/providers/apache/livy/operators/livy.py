9de79b92bf098ae3b00826dd8a5828bb3a15f535
Dov Benyomin Sohacheski <b@kloud.email>
2022-10-31 07:43:21 +0200

    Add Spark's `appId` to xcom output (#27376)

--- airflow/providers/apache/livy/operators/livy.py
+++ airflow/providers/apache/livy/operators/livy.py
@@ -142,6 +142,8 @@ class LivyOperator(BaseOperator):
         if self._polling_interval > 0:
             self.poll_for_termination(self._batch_id)
 
+        context["ti"].xcom_push(key="app_id", value=self.get_hook().get_batch(self._batch_id)["appId"])
+
         return self._batch_id
 
     def poll_for_termination(self, batch_id: int | str) -> None:
