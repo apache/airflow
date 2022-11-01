f4b8234cb7854b1f6f46b6c04addbefe53eca069
Jed Cunningham <66968678+jedcunningham@users.noreply.github.com>
2022-11-01 16:55:02 -0700

    Fix behavior of `_` when searching for DAGs (#27448)
    On the homepage, when you search for DAGs, `_` wasn't behaving as most
    people might expect since it is a single character wildcard in SQL. We
    will automatically escape it now.

--- airflow/www/views.py
+++ airflow/www/views.py
@@ -654,9 +654,10 @@ class Airflow(AirflowBaseView):
             dags_query = session.query(DagModel).filter(~DagModel.is_subdag, DagModel.is_active)
 
             if arg_search_query:
+                escaped_arg_search_query = arg_search_query.replace("_", r"\_")
                 dags_query = dags_query.filter(
-                    DagModel.dag_id.ilike('%' + arg_search_query + '%')
-                    | DagModel.owners.ilike('%' + arg_search_query + '%')
+                    DagModel.dag_id.ilike("%" + escaped_arg_search_query + "%", escape="\\")
+                    | DagModel.owners.ilike("%" + escaped_arg_search_query + "%", escape="\\")
                 )
 
             if arg_tags_filter:
