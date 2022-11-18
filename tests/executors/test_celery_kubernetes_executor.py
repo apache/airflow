66bdea0f37fd1b841ebf029e0ef05215c2bbcf97
Hussein Awala <houssein.awala.96@gmail.com>
2022-11-18 10:24:56 +0100

    fix executors tests

--- tests/executors/test_celery_kubernetes_executor.py
+++ tests/executors/test_celery_kubernetes_executor.py
@@ -123,13 +123,12 @@ class TestCeleryKubernetesExecutor:
             pool=None,
             cfg_path=None,
         )
-        kwarg_values = kwargs.values()
         cke.queue_task_instance(**kwargs)
         if test_queue == KUBERNETES_QUEUE:
-            k8s_executor_mock.queue_task_instance.assert_called_once_with(*kwarg_values)
+            k8s_executor_mock.queue_task_instance.assert_called_once_with(**kwargs)
             celery_executor_mock.queue_task_instance.assert_not_called()
         else:
-            celery_executor_mock.queue_task_instance.assert_called_once_with(*kwarg_values)
+            celery_executor_mock.queue_task_instance.assert_called_once_with(**kwargs)
             k8s_executor_mock.queue_task_instance.assert_not_called()
 
     @parameterized.expand(
