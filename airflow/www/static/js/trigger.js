a0724a5a3d05d9d57227e1bed1c9df3ec68a0d68
Jack Hu <jackhu242@gmail.com>
2022-11-19 23:52:54 -0500

    feat: automatically save and allow restore of recent DAG run configs

--- airflow/www/static/js/trigger.js
+++ airflow/www/static/js/trigger.js
@@ -20,13 +20,55 @@
 /* global document, CodeMirror, window */
 
 const textArea = document.getElementById('json');
+const form = document.getElementById('dag-trigger');
+const recentConfigList = document.getElementById('recent_configs');
 const minHeight = 300;
 const maxHeight = window.innerHeight - 450;
 const height = maxHeight > minHeight ? maxHeight : minHeight;
 
-CodeMirror.fromTextArea(textArea, {
+const editor = CodeMirror.fromTextArea(textArea, {
   lineNumbers: true,
-  mode: { name: 'javascript', json: true },
+  mode: {
+    name: 'javascript',
+    json: true,
+  },
   gutters: ['CodeMirror-lint-markers'],
   lint: true,
-}).setSize(null, height);
+})
+  .setSize(null, height);
+
+function saveConfigJson(e) {
+  const { localStorage } = window;
+  if (e.preventDefault) e.preventDefault();
+
+  const recentConfigString = localStorage.getItem('recentConfigs') ?? '{}';
+  const configsFromStorage = JSON.parse(recentConfigString);
+  const jsonString = JSON.stringify(JSON.parse(textArea.value));
+  configsFromStorage[jsonString] = Date.now();
+  window.localStorage.setItem('recentConfigs', JSON.stringify(configsFromStorage));
+
+  form.submit();
+}
+
+function loadRecentConfigs() {
+  const { localStorage } = window;
+  const recentConfigString = localStorage.getItem('recentConfigs') ?? '{}';
+  const configsFromStorage = JSON.parse(recentConfigString);
+  const configsToLoad = Object.entries(configsFromStorage)
+    .sort(([, a], [, b]) => b - a);
+
+  configsToLoad.forEach((config) => {
+    const opt = document.createElement('option');
+    opt.value = config[0];
+    opt.innerHTML = config[0].replaceAll('"', '&quot;');
+    recentConfigList.appendChild(opt);
+  });
+}
+
+function setRecentConfig(e) {
+  document.querySelector('.CodeMirror').CodeMirror.setValue(e.target.value);
+}
+
+form.addEventListener('submit', saveConfigJson);
+recentConfigList.addEventListener('change', setRecentConfig);
+loadRecentConfigs();
