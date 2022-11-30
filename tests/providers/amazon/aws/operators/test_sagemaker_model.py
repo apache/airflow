419e6158d6735f6ee5a010f1ad01d7b97d68eeae
Raphaël Vandon <vandonr@amazon.com>
2022-11-30 15:06:06 -0800

    move utests to their own file for consistency

--- tests/providers/amazon/aws/operators/test_sagemaker_model.py
+++ tests/providers/amazon/aws/operators/test_sagemaker_model.py
@@ -27,8 +27,6 @@ from airflow.providers.amazon.aws.hooks.sagemaker import SageMakerHook
 from airflow.providers.amazon.aws.operators.sagemaker import (
     SageMakerDeleteModelOperator,
     SageMakerModelOperator,
-    SageMakerStartPipelineOperator,
-    SageMakerStopPipelineOperator,
 )
 
 CREATE_MODEL_PARAMS: dict = {
@@ -70,46 +68,3 @@ class TestSageMakerDeleteModelOperator(unittest.TestCase):
         )
         op.execute(None)
         delete_model.assert_called_once_with(model_name="model_name")
-
-
-class TestSageMakerStartPipelineOperator:
-    @mock.patch.object(SageMakerHook, "start_pipeline")
-    def test_execute(self, start_pipeline):
-        op = SageMakerStartPipelineOperator(
-            task_id="test_sagemaker_operator",
-            pipeline_name="my_pipeline",
-            display_name="test_disp_name",
-            pipeline_params={"is_a_test": "yes"},
-            wait_for_completion=True,
-            check_interval=12,
-            verbose=False,
-        )
-
-        op.execute(None)
-
-        start_pipeline.assert_called_once_with(
-            pipeline_name="my_pipeline",
-            display_name="test_disp_name",
-            pipeline_params={"is_a_test": "yes"},
-            wait_for_completion=True,
-            check_interval=12,
-            verbose=False,
-        )
-
-
-class TestSageMakerStopPipelineOperator:
-    @mock.patch.object(SageMakerHook, "stop_pipeline")
-    def test_execute(self, stop_pipeline):
-        op = SageMakerStopPipelineOperator(
-            task_id="test_sagemaker_operator", pipeline_exec_arn="pipeline_arn"
-        )
-
-        op.execute(None)
-
-        stop_pipeline.assert_called_once_with(
-            pipeline_exec_arn="pipeline_arn",
-            wait_for_completion=False,
-            check_interval=30,
-            fail_if_not_running=False,
-            verbose=True,
-        )
