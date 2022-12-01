cf9f74d0d1901c71881c38b2d810e258a6f2eb7a
Aaron Abraham <aaronabraham311@gmail.com>
2022-12-01 17:03:16 -0500

    Automatically save and allow restore of recent DAG run configs (#27805)
    * feat: automatically save and allow restore of recent DAG run configs
    
    * Added a "none" option
    
    * Fixed lint issues
    
    * Changed approach--now loading recent configs from DAGRuns in database
    
    * removed API call in favour of querying recent DAGRuns from DB and passing to template
    
    * implemented suggestions
    
    * feat: automatically save and allow restore of recent DAG run configs
    
    * Added a "none" option
    
    * Fixed lint issues
    
    * Changed approach--now loading recent configs from DAGRuns in database
    
    * removed API call in favour of querying recent DAGRuns from DB and passing to template
    
    * implemented suggestions
    
    * added return type
    
    * added client-side JSON formatting, distinct configs and should fix failing test
    
    * changed query to use group by
    
    * allowed non-json-parseable configs to stay as strings
    
    Co-authored-by: Jack Hu <jackhu242@gmail.com>
    Co-authored-by: Jack Hu <34525842+jh242@users.noreply.github.com>

--- airflow/www/utils.py
+++ airflow/www/utils.py
@@ -133,17 +133,24 @@ def get_mapped_summary(parent_instance, task_instances):
     }
 
 
+def get_dag_run_conf(dag_run_conf: Any) -> tuple[str | None, bool]:
+    conf: str | None = None
+
+    conf_is_json: bool = False
+    if isinstance(dag_run_conf, str):
+        conf = dag_run_conf
+    elif isinstance(dag_run_conf, (dict, list)) and any(dag_run_conf):
+        conf = json.dumps(dag_run_conf, sort_keys=True)
+        conf_is_json = True
+
+    return conf, conf_is_json
+
+
 def encode_dag_run(dag_run: DagRun | None) -> dict[str, Any] | None:
     if not dag_run:
         return None
 
-    conf: str | None = None
-    conf_is_json: bool = False
-    if isinstance(dag_run.conf, str):
-        conf = dag_run.conf
-    elif isinstance(dag_run.conf, (dict, list)) and any(dag_run.conf):
-        conf = json.dumps(dag_run.conf, sort_keys=True)
-        conf_is_json = True
+    conf, conf_is_json = get_dag_run_conf(dag_run.conf)
 
     return {
         "run_id": dag_run.run_id,
