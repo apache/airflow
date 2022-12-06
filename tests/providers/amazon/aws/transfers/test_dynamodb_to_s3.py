17487302c286295177b861c31ed0327a8a192f82
sanjay pillai <sanjay.pillai@gmail.com>
2022-12-06 19:00:02 +0530

    add test for JSONEncoder

--- tests/providers/amazon/aws/transfers/test_dynamodb_to_s3.py
+++ tests/providers/amazon/aws/transfers/test_dynamodb_to_s3.py
@@ -22,7 +22,18 @@ import unittest
 from decimal import Decimal
 from unittest.mock import MagicMock, patch
 
-from airflow.providers.amazon.aws.transfers.dynamodb_to_s3 import DynamoDBToS3Operator
+from airflow.providers.amazon.aws.transfers.dynamodb_to_s3 import DynamoDBToS3Operator, JSONEncoder
+
+
+class JSONEncoderTest(unittest.TestCase):
+    def test_jsonencode_with_decimal(self):
+        """Test JSONEncoder correctly encodes decimal to float"""
+
+        for i in ["102938.3043847474", 1.010001, 10, "100", "1E-128", 1e-128]:
+            org = Decimal(i)
+            encoded = json.dumps(org, cls=JSONEncoder)
+            decoded = json.loads(encoded, parse_float=Decimal)
+            self.assertAlmostEqual(decoded, org)
 
 
 class DynamodbToS3Test(unittest.TestCase):
