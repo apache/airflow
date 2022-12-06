2d86a123da8deea139164094b9bdd31cb063817d
Daniel Standish <15932138+dstandish@users.noreply.github.com>
2022-12-06 12:38:07 -0800

    Don't check rosetta on i386 (#28169)

--- dev/breeze/src/airflow_breeze/commands/main_command.py
+++ dev/breeze/src/airflow_breeze/commands/main_command.py
@@ -153,7 +153,7 @@ def check_for_python_emulation():
 
 
 def check_for_rosetta_environment():
-    if sys.platform != "darwin":
+    if sys.platform != "darwin" or platform.processor() == "i386":
         return
 
     from inputimeout import TimeoutOccurred, inputimeout
