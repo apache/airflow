99de0b0c6e7878ea54cee6c20f9b129bf03eea1f
sanjay pillai <sanjay.pillai@hitachivantara.com>
2022-12-08 04:04:59 +0530

    fix static check error

--- tests/providers/amazon/aws/transfers/test_dynamodb_to_s3.py
+++ tests/providers/amazon/aws/transfers/test_dynamodb_to_s3.py
@@ -20,14 +20,15 @@ from __future__ import annotations
 import json
 from decimal import Decimal
 from unittest.mock import MagicMock, patch
+
 import pytest
+
 from airflow.providers.amazon.aws.transfers.dynamodb_to_s3 import DynamoDBToS3Operator, JSONEncoder
 
 
 class TestJSONEncoder:
-
-    @pytest.mark.parametrize('value', ["102938.3043847474", 1.010001, 10, "100", "1E-128", 1e-128])
-    def test_jsonencoder_with_decimal(self,value):
+    @pytest.mark.parametrize("value", ["102938.3043847474", 1.010001, 10, "100", "1E-128", 1e-128])
+    def test_jsonencoder_with_decimal(self, value):
         """Test JSONEncoder correctly encodes and decodes decimal values."""
 
         org = Decimal(value)
