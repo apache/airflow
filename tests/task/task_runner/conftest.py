57fd64d75d130e8273a6c75f3f31e8afe3904b32
Andrey Anshin <Andrey.Anshin@taragol.is>
2022-12-08 16:55:27 +0400

    Migrate remaining core sensors tests to `pytest` (#28204)
    (cherry picked from commit e93b669d841828b5f912edf40f255299b1833521)

--- tests/task/task_runner/conftest.py
+++ tests/task/task_runner/conftest.py
@@ -1,4 +1,3 @@
-#
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -15,3 +14,26 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+
+from __future__ import annotations
+
+import logging
+from logging.config import dictConfig
+
+import pytest
+
+from airflow.config_templates.airflow_local_settings import DEFAULT_LOGGING_CONFIG
+from airflow.listeners.listener import get_listener_manager
+
+
+@pytest.fixture(scope="module", autouse=True)
+def reset_to_default_logging():
+    """
+    Initialize ``BaseTaskRunner`` might have side effect to another tests.
+    This fixture reset back logging to default after execution of separate module  in this test package.
+    """
+    yield
+    airflow_logger = logging.getLogger("airflow")
+    airflow_logger.handlers = []
+    dictConfig(DEFAULT_LOGGING_CONFIG)
+    get_listener_manager().clear()
