bdc3d2e6474f7f23f75683fd072b4a07ef5aaeaa
Daniel Standish <15932138+dstandish@users.noreply.github.com>
2022-12-08 13:11:33 -0800

    Keep pod name for k8s executor under 63 characters (#28237)
    Because of the way that the task log handler reads from running k8s executor pods, we must keep pod name <= 63 characters.  The handler gets pod name from ti.hostname. TI hostname is derived from the container hostname, which is truncated to 63 characters. We could lift this limit by using label selectors instead of pod name to find the pod. But for now, easy enough to keep limited to 63.
    
    Since we limit to 63 in the code, we can remove the logic to find the matching pod when length is >= 63.

--- airflow/utils/log/file_task_handler.py
+++ airflow/utils/log/file_task_handler.py
@@ -196,20 +196,6 @@ class FileTaskHandler(logging.Handler):
 
                 kube_client = get_kube_client()
 
-                if len(ti.hostname) >= 63:
-                    # Kubernetes takes the pod name and truncates it for the hostname. This truncated hostname
-                    # is returned for the fqdn to comply with the 63 character limit imposed by DNS standards
-                    # on any label of a FQDN.
-                    pod_list = kube_client.list_namespaced_pod(conf.get("kubernetes_executor", "namespace"))
-                    matches = [
-                        pod.metadata.name
-                        for pod in pod_list.items
-                        if pod.metadata.name.startswith(ti.hostname)
-                    ]
-                    if len(matches) == 1:
-                        if len(matches[0]) > len(ti.hostname):
-                            ti.hostname = matches[0]
-
                 log += f"*** Trying to get logs (last 100 lines) from worker pod {ti.hostname} ***\n\n"
 
                 res = kube_client.read_namespaced_pod_log(
