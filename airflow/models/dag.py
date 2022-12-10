b8b1baad63cc6764ce17c463ad219cdc752df9aa
Miky Schreiber <61650261+miky-schreiber@users.noreply.github.com>
2022-12-10 19:56:40 +0200

    update dag.py with Black modifications

--- airflow/models/dag.py
+++ airflow/models/dag.py
@@ -3205,7 +3205,7 @@ class DagModel(Base):
 
     @property
     def schedule_description(self) -> str | None:
-        if isinstance(self.schedule_interval, str) and self.schedule_interval != '@once':
+        if isinstance(self.schedule_interval, str) and self.schedule_interval != "@once":
             return str(self.schedule_interval) + " " + str(self.timezone.name)
         else:
             return str(self.schedule_interval)
