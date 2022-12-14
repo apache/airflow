c6b9aff0d7042280a39ae08e64813310f2f75247
Amogh <adesai@cloudera.com>
2022-12-14 15:28:39 +0530

    Fixing build failures

--- airflow/example_dags/example_dynamic_task_mapping.py
+++ airflow/example_dags/example_dynamic_task_mapping.py
@@ -23,7 +23,7 @@ from datetime import datetime
 from airflow import DAG
 from airflow.decorators import task
 
-with DAG(dag_id="simple_mapping", start_date=datetime(2022, 3, 4)) as dag:
+with DAG(dag_id="example_dynamic_task_mapping", start_date=datetime(2022, 3, 4)) as dag:
 
     @task
     def add_one(x: int):
