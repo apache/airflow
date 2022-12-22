89219f33cb44cc887f8ce372a2e6bbcaaa63473c
Pankaj Singh <98807258+pankajastro@users.noreply.github.com>
2022-12-22 05:47:32 +0530

    Set bigquery ``use_legacy_sql`` param in job config correctly (#28522)

--- airflow/providers/google/cloud/operators/bigquery.py
+++ airflow/providers/google/cloud/operators/bigquery.py
@@ -205,7 +205,7 @@ class BigQueryCheckOperator(_BigQueryDbHookMixin, SQLCheckOperator):
         job_id: str,
     ) -> BigQueryJob:
         """Submit a new job and get the job id for polling the status using Trigger."""
-        configuration = {"query": {"query": self.sql}}
+        configuration = {"query": {"query": self.sql, "useLegacySql": self.use_legacy_sql}}
 
         return hook.insert_job(
             configuration=configuration,
@@ -319,11 +319,9 @@ class BigQueryValueCheckOperator(_BigQueryDbHookMixin, SQLValueCheckOperator):
         configuration = {
             "query": {
                 "query": self.sql,
-                "useLegacySql": False,
-            }
+                "useLegacySql": self.use_legacy_sql,
+            },
         }
-        if self.use_legacy_sql:
-            configuration["query"]["useLegacySql"] = self.use_legacy_sql
 
         return hook.insert_job(
             configuration=configuration,
@@ -453,7 +451,7 @@ class BigQueryIntervalCheckOperator(_BigQueryDbHookMixin, SQLIntervalCheckOperat
         job_id: str,
     ) -> BigQueryJob:
         """Submit a new job and get the job id for polling the status using Triggerer."""
-        configuration = {"query": {"query": sql}}
+        configuration = {"query": {"query": sql, "useLegacySql": self.use_legacy_sql}}
         return hook.insert_job(
             configuration=configuration,
             project_id=hook.project_id,
@@ -577,7 +575,7 @@ class BigQueryColumnCheckOperator(_BigQueryDbHookMixin, SQLColumnCheckOperator):
         job_id: str,
     ) -> BigQueryJob:
         """Submit a new job and get the job id for polling the status using Trigger."""
-        configuration = {"query": {"query": self.sql}}
+        configuration = {"query": {"query": self.sql, "useLegacySql": self.use_legacy_sql}}
 
         return hook.insert_job(
             configuration=configuration,
@@ -689,7 +687,7 @@ class BigQueryTableCheckOperator(_BigQueryDbHookMixin, SQLTableCheckOperator):
         job_id: str,
     ) -> BigQueryJob:
         """Submit a new job and get the job id for polling the status using Trigger."""
-        configuration = {"query": {"query": self.sql}}
+        configuration = {"query": {"query": self.sql, "useLegacySql": self.use_legacy_sql}}
 
         return hook.insert_job(
             configuration=configuration,
