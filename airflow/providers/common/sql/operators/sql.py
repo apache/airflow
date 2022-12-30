2e7b9f550403cc6937b3210aaaf9e80e3e944445
Daniel Standish <15932138+dstandish@users.noreply.github.com>
2022-12-29 22:04:13 -0800

    Defer to hook setting for split_statements in SQLExecuteQueryOperator (#28635)
    Some databases, such as snowflake, require you to split statements in order to submit multi-statement sql.  For such databases, splitting is the natural default, and we should defer to the hook to control that.

--- airflow/providers/common/sql/operators/sql.py
+++ airflow/providers/common/sql/operators/sql.py
@@ -198,7 +198,8 @@ class SQLExecuteQueryOperator(BaseSQLOperator):
     :param autocommit: (optional) if True, each command is automatically committed (default: False).
     :param parameters: (optional) the parameters to render the SQL query with.
     :param handler: (optional) the function that will be applied to the cursor (default: fetch_all_handler).
-    :param split_statements: (optional) if split single SQL string into statements (default: False).
+    :param split_statements: (optional) if split single SQL string into statements. By default, defers
+        to the default value in the ``run`` method of the configured hook.
     :param return_last: (optional) return the result of only last statement (default: True).
 
     .. seealso::
@@ -218,7 +219,7 @@ class SQLExecuteQueryOperator(BaseSQLOperator):
         autocommit: bool = False,
         parameters: Mapping | Iterable | None = None,
         handler: Callable[[Any], Any] = fetch_all_handler,
-        split_statements: bool = False,
+        split_statements: bool | None = None,
         return_last: bool = True,
         **kwargs,
     ) -> None:
@@ -252,13 +253,17 @@ class SQLExecuteQueryOperator(BaseSQLOperator):
     def execute(self, context):
         self.log.info("Executing: %s", self.sql)
         hook = self.get_db_hook()
+        if self.split_statements is not None:
+            extra_kwargs = {"split_statements": self.split_statements}
+        else:
+            extra_kwargs = {}
         output = hook.run(
             sql=self.sql,
             autocommit=self.autocommit,
             parameters=self.parameters,
             handler=self.handler if self.do_xcom_push else None,
-            split_statements=self.split_statements,
             return_last=self.return_last,
+            **extra_kwargs,
         )
         if return_single_query_results(self.sql, self.return_last, self.split_statements):
             # For simplicity, we pass always list as input to _process_output, regardless if
