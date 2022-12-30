5012093007e3f3a89e394c8d48d07c0cceddf246
Daniel Standish <15932138+dstandish@users.noreply.github.com>
2022-12-30 00:29:19 -0800

    Allow generation of connection URI to work when no conn type (#26765)
    Previously if get_uri was called it would fail with `NoneType not iterable`, because of the check `if '-' in conn_type`.
    
    (cherry picked from commit b124d6cac1180e21d21408f74729e77f75a46cb9)

--- tests/always/test_connection.py
+++ tests/always/test_connection.py
@@ -735,3 +735,11 @@ class TestConnection(unittest.TestCase):
     def test_extra_warnings_non_dict_json(self):
         with pytest.warns(DeprecationWarning, match="not parse as a dictionary"):
             Connection(conn_id="test_extra", conn_type="none", extra='"hi"')
+
+    def test_get_uri_no_conn_type(self):
+        # no conn type --> scheme-relative URI
+        assert Connection().get_uri() == "//"
+        # with host, still works
+        assert Connection(host="abc").get_uri() == "//abc"
+        # parsing back as conn still works
+        assert Connection(uri="//abc").host == "abc"
