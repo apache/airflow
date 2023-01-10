262ab3354c639aeac4b0bfbed3671cb0160a04e5
Max Ho <maxhowl86@gmail.com>
2023-01-11 07:25:29 +0800

    Update CSRF token to expire with session (#28730)

--- airflow/config_templates/default_webserver_config.py
+++ airflow/config_templates/default_webserver_config.py
@@ -32,6 +32,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Flask-WTF flag for CSRF
 WTF_CSRF_ENABLED = True
+WTF_CSRF_TIME_LIMIT = None
 
 # ----------------------------------------------------
 # AUTHENTICATION CONFIG
