e659595b9f1346fd918defe7a18b5a34fe889cc8
Josh Goldman <jgoldman110@gmail.com>
2023-01-10 05:24:17 -0500

    Remove swagger-ui extra from connexion and install swagger-ui-dist via npm package (#28788)
    (cherry picked from commit 35ad16dc0f6b764322b1eb289709e493fbbb0ae0)

--- airflow/www/extensions/init_views.py
+++ airflow/www/extensions/init_views.py
@@ -208,7 +208,12 @@ def init_api_connexion(app: Flask) -> None:
             return views.method_not_allowed(ex)
 
     spec_dir = path.join(ROOT_APP_DIR, "api_connexion", "openapi")
-    connexion_app = App(__name__, specification_dir=spec_dir, skip_error_handlers=True)
+    swagger_ui_dir = path.join(ROOT_APP_DIR, "www", "static", "dist", "swagger-ui")
+    options = {
+        "swagger_ui": conf.getboolean("webserver", "enable_swagger_ui", fallback=True),
+        "swagger_path": swagger_ui_dir,
+    }
+    connexion_app = App(__name__, specification_dir=spec_dir, skip_error_handlers=True, options=options)
     connexion_app.app = app
     api_bp = connexion_app.add_api(
         specification="v1.yaml", base_path=base_path, validate_responses=True, strict_validation=True
