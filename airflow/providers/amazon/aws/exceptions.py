4ffcea8054a7ec723c8d254cda10b3332c8f24f2
Daniel Standish <15932138+dstandish@users.noreply.github.com>
2023-01-12 20:01:00 -0800

    Avoid circular import from S3HookUriParseFailure (#28908)
    Otherwise, webserver fails to start when s3 configured.

--- airflow/providers/amazon/aws/exceptions.py
+++ airflow/providers/amazon/aws/exceptions.py
@@ -17,7 +17,7 @@
 # under the License.
 from __future__ import annotations
 
-from airflow import AirflowException
+from airflow.exceptions import AirflowException
 
 # Note: Any AirflowException raised is expected to cause the TaskInstance
 #       to be marked in an ERROR state
