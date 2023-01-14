d8374538ed02877a1d0f7ecf673194a376ca6097
Andrey Anshin <Andrey.Anshin@taragol.is>
2023-01-14 04:35:27 +0400

    Fix examples for Amazon notifiers

--- docs/apache-airflow-providers-amazon/notifications/sns.rst
+++ docs/apache-airflow-providers-amazon/notifications/sns.rst
@@ -17,12 +17,12 @@
 
 .. _howto/notifier:SnsNotifier:
 
-How-to Guide for SNS notifications
-==================================
+How-to Guide for Amazon Simple Notification Service (Amazon SNS) notifications
+==============================================================================
 
 Introduction
 ------------
-`AWS SNS <https://aws.amazon.com/sns/>`__ notifier :class:`~airflow.providers.amazon.aws.notifications.sns.SnsNotifier`
+`Amazon SNS <https://aws.amazon.com/sns/>`__ notifier :class:`~airflow.providers.amazon.aws.notifications.sns.SnsNotifier`
 allows users to push messages to a SNS Topic using the various ``on_*_callbacks`` at both the DAG level and Task level.
 
 You can also use a notifier with ``sla_miss_callback``.
@@ -38,7 +38,7 @@ Example Code:
 
     from datetime import datetime
     from airflow import DAG
-    from airflow.operators.python import PythonOperator
+    from airflow.operators.bash import BashOperator
     from airflow.providers.amazon.aws.notifications.sns import send_sns_notification
 
     dag_failure_sns_notification = send_sns_notification(
@@ -54,7 +54,13 @@ Example Code:
         target_arn="arn:aws:sns:us-west-2:123456789098:AnotherTopicName",
     )
 
-    with DAG(start_date=datetime(2023, 1, 1), on_failure_callback=[dag_failure_sns_notification]):
+    with DAG(
+        dag_id="mydag",
+        schedule="@once",
+        start_date=datetime(2023, 1, 1),
+        on_failure_callback=[dag_failure_sns_notification],
+        catchup=False,
+    ):
         BashOperator(
             task_id="mytask",
             on_failure_callback=[task_failure_sns_notification],
--- tests/providers/amazon/aws/notifications/test_sns.py
+++ tests/providers/amazon/aws/notifications/test_sns.py
@@ -20,6 +20,7 @@ from unittest import mock
 
 import pytest
 
+from airflow.operators.empty import EmptyOperator
 from airflow.providers.amazon.aws.notifications.sns import SnsNotifier, send_sns_notification
 from airflow.utils.types import NOTSET
 
@@ -34,20 +35,60 @@ class TestSnsNotifier:
     @pytest.mark.parametrize("region_name", ["eu-west-2", None, PARAM_DEFAULT_VALUE])
     def test_parameters_propagate_to_hook(self, aws_conn_id, region_name):
         """Test notifier attributes propagate to SnsHook."""
-        notifier_kwargs = {
+        publish_kwargs = {
             "target_arn": "arn:aws:sns:us-west-2:123456789098:TopicName",
             "message": "foo-bar",
+            "subject": "spam-egg",
+            "message_attributes": {},
         }
+        notifier_kwargs = {}
         if aws_conn_id is not NOTSET:
             notifier_kwargs["aws_conn_id"] = aws_conn_id
         if region_name is not NOTSET:
             notifier_kwargs["region_name"] = region_name
 
-        notifier = SnsNotifier(**notifier_kwargs)
+        notifier = SnsNotifier(**notifier_kwargs, **publish_kwargs)
         with mock.patch("airflow.providers.amazon.aws.notifications.sns.SnsHook") as mock_hook:
             hook = notifier.hook
             assert hook is notifier.hook, "Hook property not cached"
             mock_hook.assert_called_once_with(
                 aws_conn_id=(aws_conn_id if aws_conn_id is not NOTSET else "aws_default"),
                 region_name=(region_name if region_name is not NOTSET else None),
+            )
+
+            # Basic check for notifier
+            notifier.notify({})
+            mock_hook.return_value.publish_to_target.assert_called_once_with(**publish_kwargs)
+
+    def test_sns_notifier_templated(self, dag_maker):
+        with dag_maker("test_sns_notifier_templated") as dag:
+            EmptyOperator(task_id="task1")
+
+        notifier = SnsNotifier(
+            aws_conn_id="{{ dag.dag_id }}",
+            target_arn="arn:aws:sns:{{ var_region }}:{{ var_account }}:{{ var_topic }}",
+            message="I, {{ var_username }}",
+            subject="{{ var_subject }}",
+            message_attributes={"foo": "{{ dag.dag_id }}"},
+            region_name="{{ var_region }}",
+        )
+        with mock.patch("airflow.providers.amazon.aws.notifications.sns.SnsHook") as m:
+            notifier(
+                {
+                    "dag": dag,
+                    "var_username": "Robot",
+                    "var_region": "us-west-1",
+                    "var_account": "000000000000",
+                    "var_topic": "AwesomeTopic",
+                    "var_subject": "spam-egg",
+                }
+            )
+            # Hook initialisation
+            m.assert_called_once_with(aws_conn_id="test_sns_notifier_templated", region_name="us-west-1")
+            # Publish message
+            m.return_value.publish_to_target.assert_called_once_with(
+                target_arn="arn:aws:sns:us-west-1:000000000000:AwesomeTopic",
+                message="I, Robot",
+                subject="spam-egg",
+                message_attributes={"foo": "test_sns_notifier_templated"},
             )
