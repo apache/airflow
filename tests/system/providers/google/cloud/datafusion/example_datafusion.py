e926bb9bb0ce97b4bb32e49279bde237ba8935ed
VladaZakharova <80038284+VladaZakharova@users.noreply.github.com>
2023-01-20 21:57:19 +0100

    Add deferrable mode to DataFusionStartPipelineOperator (#28690)

--- tests/system/providers/google/cloud/datafusion/example_datafusion.py
+++ tests/system/providers/google/cloud/datafusion/example_datafusion.py
@@ -36,10 +36,12 @@ from airflow.providers.google.cloud.operators.datafusion import (
     CloudDataFusionStopPipelineOperator,
     CloudDataFusionUpdateInstanceOperator,
 )
+from airflow.providers.google.cloud.operators.gcs import GCSCreateBucketOperator
 from airflow.providers.google.cloud.sensors.datafusion import CloudDataFusionPipelineStateSensor
 
 # [START howto_data_fusion_env_variables]
 SERVICE_ACCOUNT = os.environ.get("GCP_DATAFUSION_SERVICE_ACCOUNT")
+PROJECT_ID = os.environ.get("SYSTEM_TESTS_GCP_PROJECT")
 LOCATION = "europe-north1"
 INSTANCE_NAME = "airflow-test-instance"
 INSTANCE = {
@@ -48,19 +50,17 @@ INSTANCE = {
     "dataprocServiceAccount": SERVICE_ACCOUNT,
 }
 
-BUCKET_1 = os.environ.get("GCP_DATAFUSION_BUCKET_1", "test-datafusion-bucket-1")
-BUCKET_2 = os.environ.get("GCP_DATAFUSION_BUCKET_2", "test-datafusion-bucket-2")
-
+BUCKET_1 = "test-datafusion-1"
+BUCKET_2 = "test-datafusion-2"
 BUCKET_1_URI = f"gs://{BUCKET_1}"
 BUCKET_2_URI = f"gs://{BUCKET_2}"
 
-PIPELINE_NAME = os.environ.get("GCP_DATAFUSION_PIPELINE_NAME", "airflow_test")
+PIPELINE_NAME = "test-pipe"
 PIPELINE = {
     "artifact": {
         "name": "cdap-data-pipeline",
-        "version": "6.5.1",
+        "version": "6.7.2",
         "scope": "SYSTEM",
-        "label": "Data Pipeline - System Test",
     },
     "description": "Data Pipeline Application",
     "name": "test-pipe",
@@ -80,7 +80,7 @@ PIPELINE = {
                     "name": "GCSFile",
                     "type": "batchsource",
                     "label": "GCS",
-                    "artifact": {"name": "google-cloud", "version": "0.18.1", "scope": "SYSTEM"},
+                    "artifact": {"name": "google-cloud", "version": "0.20.3", "scope": "SYSTEM"},
                     "properties": {
                         "project": "auto-detect",
                         "format": "text",
@@ -109,7 +109,7 @@ PIPELINE = {
                     "name": "GCS",
                     "type": "batchsink",
                     "label": "GCS2",
-                    "artifact": {"name": "google-cloud", "version": "0.18.1", "scope": "SYSTEM"},
+                    "artifact": {"name": "google-cloud", "version": "0.20.3", "scope": "SYSTEM"},
                     "properties": {
                         "project": "auto-detect",
                         "suffix": "yyyy-MM-dd-HH-mm",
@@ -145,12 +145,23 @@ PIPELINE = {
 }
 # [END howto_data_fusion_env_variables]
 
-
 with models.DAG(
     "example_data_fusion",
     start_date=datetime(2021, 1, 1),
     catchup=False,
 ) as dag:
+    create_bucket1 = GCSCreateBucketOperator(
+        task_id="create_bucket1",
+        bucket_name=BUCKET_1,
+        project_id=PROJECT_ID,
+    )
+
+    create_bucket2 = GCSCreateBucketOperator(
+        task_id="create_bucket2",
+        bucket_name=BUCKET_2,
+        project_id=PROJECT_ID,
+    )
+
     # [START howto_cloud_data_fusion_create_instance_operator]
     create_instance = CloudDataFusionCreateInstanceOperator(
         location=LOCATION,
@@ -215,7 +226,6 @@ with models.DAG(
         asynchronous=True,
         task_id="start_pipeline_async",
     )
-
     # [END howto_cloud_data_fusion_start_pipeline_async]
 
     # [START howto_cloud_data_fusion_start_pipeline_sensor]
@@ -257,19 +267,35 @@ with models.DAG(
     # Add sleep before creating pipeline
     sleep = BashOperator(task_id="sleep", bash_command="sleep 60")
 
-    create_instance >> get_instance >> restart_instance >> update_instance >> sleep
+    # Add sleep before creating pipeline
+    sleep_30 = BashOperator(task_id="sleep_30", bash_command="sleep 30")
+
     (
-        sleep
+        create_bucket1
+        >> create_bucket2
+        >> create_instance
+        >> get_instance
+        >> restart_instance
+        >> update_instance
+        >> sleep
         >> create_pipeline
         >> list_pipelines
         >> start_pipeline_async
         >> start_pipeline_sensor
         >> start_pipeline
         >> stop_pipeline
+        >> sleep_30
         >> delete_pipeline
+        >> delete_instance
     )
-    delete_pipeline >> delete_instance
 
-if __name__ == "__main__":
-    dag.clear()
-    dag.run()
+    from tests.system.utils.watcher import watcher
+
+# This test needs watcher in order to properly mark success/failure
+# when "tearDown" task with trigger rule is part of the DAG
+list(dag.tasks) >> watcher()
+
+from tests.system.utils import get_test_run  # noqa: E402
+
+# Needed to run the example DAG with pytest (see: tests/system/README.md#run_via_pytest)
+test_run = get_test_run(dag)
