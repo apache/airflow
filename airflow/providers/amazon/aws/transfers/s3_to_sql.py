6785cf68892dc0b2b56f755f2c3a4545cdcaa3a6
max <memax@live.de>
2023-01-21 14:58:21 +0100

    fixed formatting

--- airflow/providers/amazon/aws/transfers/s3_to_sql.py
+++ airflow/providers/amazon/aws/transfers/s3_to_sql.py
@@ -26,7 +26,6 @@ from airflow.models import BaseOperator
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.providers.common.sql.hooks.sql import DbApiHook
 
-
 if TYPE_CHECKING:
     from airflow.utils.context import Context
 
