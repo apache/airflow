7272f67b971a2f88074d062c3392ba6623fe6dc8
sanjayp <sanjaypillai11@gmail.com>
2023-01-24 14:38:49 -0600

    AIP-51 - Executor Coupling in Logging (#28161)
    Executors may now implement a method to vend task logs

--- tests/executors/test_kubernetes_executor.py
+++ tests/executors/test_kubernetes_executor.py
@@ -34,6 +34,7 @@ from airflow import AirflowException
 from airflow.exceptions import PodReconciliationError
 from airflow.models.taskinstance import TaskInstanceKey
 from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
 from airflow.utils import timezone
 from tests.test_utils.config import conf_vars
 
@@ -1214,6 +1215,33 @@ class TestKubernetesExecutor:
         ti1.refresh_from_db()
         assert ti0.state == State.SCHEDULED
         assert ti1.state == State.QUEUED
+
+    @mock.patch("airflow.executors.kubernetes_executor.get_kube_client")
+    def test_get_task_log(self, mock_get_kube_client, create_task_instance_of_operator):
+        """fetch task log from pod"""
+        mock_kube_client = mock_get_kube_client.return_value
+
+        mock_kube_client.read_namespaced_pod_log.return_value = [b"a_", b"b_", b"c_"]
+        mock_pod = mock.Mock()
+        mock_pod.metadata.name = "x"
+        mock_kube_client.list_namespaced_pod.return_value.items = [mock_pod]
+        ti = create_task_instance_of_operator(EmptyOperator, dag_id="test_k8s_log_dag", task_id="test_task")
+
+        executor = KubernetesExecutor()
+        log = executor.get_task_log(ti=ti, log="test_init_log")
+
+        mock_kube_client.read_namespaced_pod_log.assert_called_once()
+        assert "test_init_log" in log
+        assert "Trying to get logs (last 100 lines) from worker pod" in log
+        assert "a_b_c" in log
+
+        mock_kube_client.reset_mock()
+        mock_kube_client.read_namespaced_pod_log.side_effect = Exception("error_fetching_pod_log")
+
+        log = executor.get_task_log(ti=ti, log="test_init_log")
+        assert len(log) == 2
+        assert "error_fetching_pod_log" in log[0]
+        assert log[1]["end_of_log"]
 
     def test_supports_pickling(self):
         assert KubernetesExecutor.supports_pickling
