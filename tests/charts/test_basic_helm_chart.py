5c2c55f237cb33954e6c1c6cc81e11e2444a655d
Amogh <adesai@cloudera.com>
2023-01-29 20:32:43 +0530

    Adding basic helm chart tests

--- tests/charts/test_basic_helm_chart.py
+++ tests/charts/test_basic_helm_chart.py
@@ -508,3 +508,35 @@ class TestBaseChartTest:
     def test_namespace_names(self, namespace):
         """Test various namespace names to make sure they render correctly in templates"""
         render_chart(namespace=namespace)
+
+    @pytest.mark.parametrize()
+    def test_postgres_connection_url_no_override(self):
+        # no nameoverride provided
+        assert "postgresql://postgres:postgres@my-release-postgresql.default:5432/postgres?sslmode=disable" == \
+               render_chart(
+                   "my-release",
+                   values={
+                       "postgresql": {
+                           "enabled": True,
+                       }
+                   },
+                   show_only=[
+                       "templates/secrets/metadata-connection-secret.yaml"
+                   ]
+               )["data"]["connection"]
+
+    @pytest.mark.parametrize()
+    def test_postgres_connection_url_name_override(self):
+        assert "postgresql://postgres:postgres@overrideName:5432/postgres?sslmode=disable" == \
+               render_chart(
+                   "my-release",
+                   values={
+                       "postgresql": {
+                           "enabled": True,
+                           "nameOverride": "overrideName"
+                       }
+                   },
+                   show_only=[
+                       "templates/secrets/metadata-connection-secret.yaml"
+                   ]
+               )["data"]["connection"]
