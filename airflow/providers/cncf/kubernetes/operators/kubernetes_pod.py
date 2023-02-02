068370ec867705a1159a61faea38cd94cc89b11a
Phani Kumar <phani.kumar@astronomer.io>
2023-02-02 14:40:52 +0530

    Enable perioding resuming to fetch logs for KPOA

--- airflow/providers/cncf/kubernetes/operators/kubernetes_pod.py
+++ airflow/providers/cncf/kubernetes/operators/kubernetes_pod.py
@@ -294,6 +294,7 @@ class KubernetesPodOperator(BaseOperator):
         base_container_name: str | None = None,
         deferrable: bool = False,
         poll_interval: float = 2,
+        logging_interval: int | None = None,
         **kwargs,
     ) -> None:
         # TODO: remove in provider 6.0.0 release. This is a mitigate step to advise users to switch to the
@@ -363,6 +364,7 @@ class KubernetesPodOperator(BaseOperator):
         self.base_container_name = base_container_name or self.BASE_CONTAINER_NAME
         self.deferrable = deferrable
         self.poll_interval = poll_interval
+        self.logging_interval = logging_interval
         self.remote_pod: k8s.V1Pod | None = None
 
         self._config_dict: dict | None = None
@@ -565,7 +567,103 @@ class KubernetesPodOperator(BaseOperator):
             context=context,
         )
         self.convert_config_file_to_dict()
-        self.invoke_defer_method()
+        self.defer()
+
+    def defer(self, last_log_time: DateTime | None = None, **kwargs: Any) -> None:
+        """Defers to ``WaitContainerTrigger`` optionally with last log time."""
+        if kwargs:
+            raise ValueError(
+                f"Received keyword arguments {list(kwargs.keys())} but "
+                f"they are not used in this implementation of `defer`."
+            )
+        super().defer(
+            trigger=WaitContainerTrigger(
+                kubernetes_conn_id=self.kubernetes_conn_id,
+                hook_params={
+                    "cluster_context": self.cluster_context,
+                    "config_file": self.config_file,
+                    "in_cluster": self.in_cluster,
+                },
+                pod_name=self.pod.metadata.name,
+                container_name=self.BASE_CONTAINER_NAME,
+                pod_namespace=self.pod.metadata.namespace,
+                pending_phase_timeout=self.startup_timeout_seconds,
+                poll_interval=self.poll_interval,
+                logging_interval=self.logging_interval,
+                last_log_time=last_log_time,
+            ),
+            method_name=self.trigger_reentry.__name__,
+        )
+
+    @staticmethod
+    def raise_for_trigger_status(event: dict[str, Any]) -> None:
+        """Raise exception if pod is not in expected state."""
+        if event["status"] == "error":
+            error_type = event["error_type"]
+            description = event["description"]
+            if error_type == "PodLaunchTimeoutException":
+                raise PodLaunchTimeoutException(description)
+            else:
+                raise AirflowException(description)
+
+    def trigger_reentry(self, context: Context, event: dict[str, Any]) -> Any:
+        """
+        Point of re-entry from trigger.
+
+        If ``logging_interval`` is None, then at this point the pod should be done and we'll just fetch
+        the logs and exit.
+
+        If ``logging_interval`` is not None, it could be that the pod is still running and we'll just
+        grab the latest logs and defer back to the trigger again.
+        """
+        remote_pod = None
+        try:
+            self.pod_request_obj = self.build_pod_request_obj(context)
+            self.pod = self.find_pod(
+                namespace=self.namespace or self.pod_request_obj.metadata.namespace,
+                context=context,
+            )
+
+            # we try to find pod before possibly raising so that on_kill will have `pod` attr
+            self.raise_for_trigger_status(event)
+
+            if not self.pod:
+                raise PodNotFoundException("Could not find pod after resuming from deferral")
+
+            if self.get_logs:
+                last_log_time = event and event.get("last_log_time")
+                if last_log_time:
+                    self.log.info("Resuming logs read from time %r", last_log_time)
+                pod_log_status = self.pod_manager.fetch_container_logs(
+                    pod=self.pod,
+                    container_name=self.BASE_CONTAINER_NAME,
+                    follow=self.logging_interval is None,
+                    since_time=last_log_time,
+                )
+                if pod_log_status.running:
+                    self.log.info("Container still running; deferring again.")
+                    self.defer(pod_log_status.last_log_time)
+
+            if self.do_xcom_push:
+                result = self.extract_xcom(pod=self.pod)
+            remote_pod = self.pod_manager.await_pod_completion(self.pod)
+        except TaskDeferred:
+            raise
+        except Exception:
+            self.cleanup(
+                pod=self.pod or self.pod_request_obj,
+                remote_pod=remote_pod,
+            )
+            raise
+        self.cleanup(
+            pod=self.pod or self.pod_request_obj,
+            remote_pod=remote_pod,
+        )
+        ti = context["ti"]
+        ti.xcom_push(key="pod_name", value=self.pod.metadata.name)
+        ti.xcom_push(key="pod_namespace", value=self.pod.metadata.namespace)
+        if self.do_xcom_push:
+            return result
 
     def convert_config_file_to_dict(self):
         """Converts passed config_file to dict format."""
@@ -576,65 +674,13 @@ class KubernetesPodOperator(BaseOperator):
         else:
             self._config_dict = None
 
-    def invoke_defer_method(self):
-        """Method to easily redefine triggers which are being used in child classes."""
-        trigger_start_time = utcnow()
-        self.defer(
-            trigger=KubernetesPodTrigger(
-                pod_name=self.pod.metadata.name,
-                pod_namespace=self.pod.metadata.namespace,
-                trigger_start_time=trigger_start_time,
-                kubernetes_conn_id=self.kubernetes_conn_id,
-                cluster_context=self.cluster_context,
-                config_dict=self._config_dict,
-                in_cluster=self.in_cluster,
-                poll_interval=self.poll_interval,
-                should_delete_pod=self.is_delete_operator_pod,
-                get_logs=self.get_logs,
-                startup_timeout=self.startup_timeout_seconds,
-                base_container_name=self.base_container_name,
-            ),
-            method_name="execute_complete",
+    def execute_complete(self, context: Context, event: dict[str, Any]) -> Any:
+        """Deprecated; replaced by trigger_reentry."""
+        warnings.warn(
+            "Method `execute_complete` is deprecated and replaced with method `trigger_reentry`.",
+            DeprecationWarning,
         )
-
-    def execute_complete(self, context: Context, event: dict, **kwargs):
-        pod = None
-        remote_pod = None
-        try:
-            pod = self.hook.get_pod(
-                event["name"],
-                event["namespace"],
-            )
-            # It is done to coincide with the current implementation of the general logic of the cleanup
-            # method. If it's going to be remade in future then it must be changed
-            remote_pod = pod
-            if event["status"] in ("error", "failed", "timeout"):
-                # fetch some logs when pod is failed
-                if self.get_logs:
-                    self.write_logs(pod)
-                raise AirflowException(event["message"])
-            elif event["status"] == "success":
-                ti = context["ti"]
-                ti.xcom_push(key="pod_name", value=pod.metadata.name)
-                ti.xcom_push(key="pod_namespace", value=pod.metadata.namespace)
-
-                # fetch some logs when pod is executed successfully
-                if self.get_logs:
-                    self.write_logs(pod)
-
-                if self.do_xcom_push:
-                    xcom_sidecar_output = self.extract_xcom(pod=pod)
-                    pod = self.pod_manager.await_pod_completion(pod)
-                    # It is done to coincide with the current implementation of the general logic of
-                    # the cleanup method. If it's going to be remade in future then it must be changed
-                    remote_pod = pod
-                    return xcom_sidecar_output
-        finally:
-            if pod is not None and remote_pod is not None:
-                self.post_complete_action(
-                    pod=pod,
-                    remote_pod=remote_pod,
-                )
+        self.trigger_reentry(context=context, event=event)
 
     def write_logs(self, pod: k8s.V1Pod):
         try:
@@ -883,131 +929,4 @@ class PodNotFoundException(AirflowException):
     """Expected pod does not exist in kube-api."""
 
 
-class KubernetesPodAsyncOperator(KubernetesPodOperator):
-    """
-    Async (deferring) version of KubernetesPodOperator
 
-    .. warning::
-        By default, logs will not be available in the Airflow Webserver until the task completes. However,
-        you can configure ``KubernetesPodAsyncOperator`` to periodically resume and fetch logs.  This behavior
-        is controlled by param ``logging_interval``.
-
-    :param poll_interval: interval in seconds to sleep between checking pod status
-    :param logging_interval: max time in seconds that task should be in deferred state before
-        resuming to fetch latest logs. If ``None``, then the task will remain in deferred state until pod
-        is done, and no logs will be visible until that time.
-    """
-
-    def __init__(self, *, poll_interval: int = 5, logging_interval: int | None = None, **kwargs: Any):
-        self.poll_interval = poll_interval
-        self.logging_interval = logging_interval
-        super().__init__(**kwargs)
-
-    @staticmethod
-    def raise_for_trigger_status(event: dict[str, Any]) -> None:
-        """Raise exception if pod is not in expected state."""
-        if event["status"] == "error":
-            error_type = event["error_type"]
-            description = event["description"]
-            if error_type == "PodLaunchTimeoutException":
-                raise PodLaunchTimeoutException(description)
-            else:
-                raise AirflowException(description)
-
-    def defer(self, last_log_time: DateTime | None = None, **kwargs: Any) -> None:
-        """Defers to ``WaitContainerTrigger`` optionally with last log time."""
-        if kwargs:
-            raise ValueError(
-                f"Received keyword arguments {list(kwargs.keys())} but "
-                f"they are not used in this implementation of `defer`."
-            )
-        super().defer(
-            trigger=WaitContainerTrigger(
-                kubernetes_conn_id=self.kubernetes_conn_id,
-                hook_params={
-                    "cluster_context": self.cluster_context,
-                    "config_file": self.config_file,
-                    "in_cluster": self.in_cluster,
-                },
-                pod_name=self.pod.metadata.name,
-                container_name=self.BASE_CONTAINER_NAME,
-                pod_namespace=self.pod.metadata.namespace,
-                pending_phase_timeout=self.startup_timeout_seconds,
-                poll_interval=self.poll_interval,
-                logging_interval=self.logging_interval,
-                last_log_time=last_log_time,
-            ),
-            method_name=self.trigger_reentry.__name__,
-        )
-
-    def execute(self, context: Context) -> None:
-        self.pod_request_obj = self.build_pod_request_obj(context)
-        self.pod: k8s.V1Pod = self.get_or_create_pod(self.pod_request_obj, context)
-        self.defer()
-
-    def execute_complete(self, context: Context, event: dict[str, Any]) -> Any:
-        """Deprecated; replaced by trigger_reentry."""
-        warnings.warn(
-            "Method `execute_complete` is deprecated and replaced with method `trigger_reentry`.",
-            DeprecationWarning,
-        )
-        self.trigger_reentry(context=context, event=event)
-
-    def trigger_reentry(self, context: Context, event: dict[str, Any]) -> Any:
-        """
-        Point of re-entry from trigger.
-
-        If ``logging_interval`` is None, then at this point the pod should be done and we'll just fetch
-        the logs and exit.
-
-        If ``logging_interval`` is not None, it could be that the pod is still running and we'll just
-        grab the latest logs and defer back to the trigger again.
-        """
-        remote_pod = None
-        try:
-            self.pod_request_obj = self.build_pod_request_obj(context)
-            self.pod = self.find_pod(
-                namespace=self.namespace or self.pod_request_obj.metadata.namespace,
-                context=context,
-            )
-
-            # we try to find pod before possibly raising so that on_kill will have `pod` attr
-            self.raise_for_trigger_status(event)
-
-            if not self.pod:
-                raise PodNotFoundException("Could not find pod after resuming from deferral")
-
-            if self.get_logs:
-                last_log_time = event and event.get("last_log_time")
-                if last_log_time:
-                    self.log.info("Resuming logs read from time %r", last_log_time)
-                pod_log_status = self.pod_manager.fetch_container_logs(
-                    pod=self.pod,
-                    container_name=self.BASE_CONTAINER_NAME,
-                    follow=self.logging_interval is None,
-                    since_time=last_log_time,
-                )
-                if pod_log_status.running:
-                    self.log.info("Container still running; deferring again.")
-                    self.defer(pod_log_status.last_log_time)
-
-            if self.do_xcom_push:
-                result = self.extract_xcom(pod=self.pod)
-            remote_pod = self.pod_manager.await_pod_completion(self.pod)
-        except TaskDeferred:
-            raise
-        except Exception:
-            self.cleanup(
-                pod=self.pod or self.pod_request_obj,
-                remote_pod=remote_pod,
-            )
-            raise
-        self.cleanup(
-            pod=self.pod or self.pod_request_obj,
-            remote_pod=remote_pod,
-        )
-        ti = context["ti"]
-        ti.xcom_push(key="pod_name", value=self.pod.metadata.name)
-        ti.xcom_push(key="pod_namespace", value=self.pod.metadata.namespace)
-        if self.do_xcom_push:
-            return result
