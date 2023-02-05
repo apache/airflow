2ef6fa899d4695b7b6a1882dc383c3718f00bd93
max rieger <max.rieger@idealo.de>
2023-02-05 19:48:28 +0100

    Fixing get_task_instances_before method and adding a unit test
    With this new fix, the determination of the min_date is done on a set of
    execution_date values - via querying - limited by `num`and taking the
    last element of it. the fallback value if none are found is simply the
    base_date. this way, the invoked get_task_instances cannot default to
    30 days prior to the current time.
    
    Furthermore, the query itself is no longer confined to non-manual runs,
    but includes all DAGRun types.
    
    More context:
    
    During writing the tests, it became evident
    that the current implementation of the get_task_instances_before
    method has two problems:
    1) With the implementation of a limit + offset determination of the
    min_date, no results can occur:
      * Either due to less DAGRuns in the DB than `num` or
      * Because of the constraining to non-manual runs (see more below).
    Consequently, the fallback in the invoked get_task_instances is then a
    -30d from datetime.now(), so an entirely different time dimension than
    the one selected in the UI (execution_date).
    2) With the explicit omission of manual runs in the query, in case of n
    manual runs and 1 scheduled one before the base_date (e.g.
    S-M-M-...-M-M-BD), the scheduled one + n manual runs are returned. in
    the case of mainly triggered/manual DAGRuns, this leads to more results
    displayed than expected. in case of solely manual DAGRuns, the method
    will return no results and hence default to the above mentioned
    fallback, which operates on a different timeline. prior tp this fix,
    there was even a fallback to the start of the current epochal period,
    i.e. showing all DAGRuns in the DB.
    
    With the previously suggested non-breaking fix, it was not possible to
    avoid this scenario, with any scheduled run found, the fallback was
    never reached.
    
    The suggested changes may be considered
    breaking. the changes are confined to the scope of the
    get_task_instances_before method and usage is only in the views.py for
    the duration, tries and landing times, so where the changes  meant to
    fix the existing behavior.

--- airflow/models/dag.py
+++ airflow/models/dag.py
@@ -43,6 +43,7 @@ from typing import (
     Iterator,
     List,
     Sequence,
+    TypeVar,
     Union,
     cast,
     overload,
@@ -111,6 +112,8 @@ if TYPE_CHECKING:
 
 
 log = logging.getLogger(__name__)
+
+_T = TypeVar("_T")
 
 DEFAULT_VIEW_PRESETS = ["grid", "graph", "duration", "gantt", "landing_times"]
 ORIENTATION_PRESETS = ["LR", "TB", "RL", "BT"]
@@ -1501,44 +1504,27 @@ class DAG(LoggingMixin):
     ) -> list[TaskInstance]:
         """Get ``num`` task instances before (including) ``base_date``.
 
-        The returned list may contain exactly ``num`` task instances. It can
-        have less if there are less than ``num`` scheduled DAG runs before
-        ``base_date``, or more if there are manual task runs between the
-        requested period, which does not count toward ``num``.
-
-        When there are only manual DAG runs found, the ``num`` of task 
-        instances will include manual DAG run types before ``base_date``.
-        If neither are found, it will return an empty list.
+        The returned list may contain exactly ``num`` task instances
+        corresponding to any DagRunType. It can have less if there are
+        less than ``num`` scheduled DAG runs before ``base_date``.
         """
-        min_date: datetime | None = (
+        execution_dates: list[Any] = (
             session.query(DagRun.execution_date)
             .filter(
                 DagRun.dag_id == self.dag_id,
                 DagRun.execution_date <= base_date,
-                DagRun.run_type != DagRunType.MANUAL
             )
             .order_by(DagRun.execution_date.desc())
-            .offset(num)
-            .limit(1)
-            .scalar()
+            .limit(num)
+            .all()
         )
 
-        if min_date is None:
-            min_date: datetime | None = (
-            session.query(DagRun.execution_date)
-            .filter(
-                DagRun.dag_id == self.dag_id,
-                DagRun.execution_date <= base_date
-            )
-            .order_by(DagRun.execution_date.desc())
-            .offset(num)
-            .limit(1)
-            .scalar()
-        )
+        if len(execution_dates) == 0:
+            return self.get_task_instances(start_date=base_date, end_date=base_date, session=session)
 
-        if min_date is None:
-            # fast return as there are no DAG runs ``num`` before ``base_date`` to be found.
-            return list()
+        min_date: datetime | None = execution_dates[-1]._mapping.get(
+            "execution_date"
+        )  # getting the last value from the list
 
         return self.get_task_instances(start_date=min_date, end_date=base_date, session=session)
 
@@ -1554,6 +1540,8 @@ class DAG(LoggingMixin):
             start_date = (timezone.utcnow() - timedelta(30)).replace(
                 hour=0, minute=0, second=0, microsecond=0
             )
+
+        print("=S=", start_date)
         query = self._get_task_instances(
             task_ids=None,
             start_date=start_date,
