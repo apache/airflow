6ae0a80cbaf1d33343b763c7f82612b4522afc40
Ash Berlin-Taylor <ash@apache.org>
2023-02-10 18:25:39 +0000

    Upgrade mypy to 0.991 (#28926)
    Co-authored-by: Tzu-ping Chung <uranusjr@gmail.com>

--- airflow/providers/google/cloud/hooks/cloud_sql.py
+++ airflow/providers/google/cloud/hooks/cloud_sql.py
@@ -691,8 +691,6 @@ class CloudSQLDatabaseHook(BaseHook):
     conn_type = "gcpcloudsqldb"
     hook_name = "Google Cloud SQL Database"
 
-    _conn = None
-
     def __init__(
         self,
         gcp_cloudsql_conn_id: str = "google_cloud_sql_default",
