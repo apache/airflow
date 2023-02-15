b01f5db713ef4cf68e9a99e7fa554243a94a2eae
mhenc <mhenc@google.com>
2023-02-15 01:13:44 +0100

    Migrate  DagWarning.purge_inactive_dag_warnings and DagModel.deactivate_deleted_dags (#29534)

--- airflow/api_internal/endpoints/rpc_api_endpoint.py
+++ airflow/api_internal/endpoints/rpc_api_endpoint.py
@@ -26,6 +26,7 @@ from flask import Response
 
 from airflow.api_connexion.types import APIResponse
 from airflow.models import Variable, XCom
+from airflow.models.dagwarning import DagWarning
 from airflow.serialization.serialized_objects import BaseSerialization
 
 log = logging.getLogger(__name__)
@@ -41,8 +42,10 @@ def _initialize_map() -> dict[str, Callable]:
         DagFileProcessor.update_import_errors,
         DagFileProcessor.manage_slas,
         DagFileProcessorManager.deactivate_stale_dags,
+        DagModel.deactivate_deleted_dags,
         DagModel.get_paused_dag_ids,
         DagFileProcessorManager.clear_nonexistent_import_errors,
+        DagWarning.purge_inactive_dag_warnings,
         XCom.get_value,
         XCom.get_one,
         XCom.get_many,
