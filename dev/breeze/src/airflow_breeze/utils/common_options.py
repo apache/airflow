5627522c6a11c7e9f3c1f7f2ddc721fd84cae198
Jarek Potiuk <jarek@potiuk.com>
2023-02-17 18:08:45 +0100

    Add "all-testable" integration alias (#29520)
    The "all" alias outgrew original meaning with adding statsd integration
    in #29449 which resulted in a problem fixed by #29517. The "all" tests
    as result was not "all" but "all that could be tested".
    
    This PR adds "all-testable" alias which as opposed to "all" does not
    contain "statsd" - it also renames some of the vars/arrays to reflect
    it.7

--- dev/breeze/src/airflow_breeze/utils/common_options.py
+++ dev/breeze/src/airflow_breeze/utils/common_options.py
@@ -27,7 +27,6 @@ from airflow_breeze.global_constants import (
     ALLOWED_CONSTRAINTS_MODES_CI,
     ALLOWED_CONSTRAINTS_MODES_PROD,
     ALLOWED_INSTALLATION_PACKAGE_FORMATS,
-    ALLOWED_INTEGRATIONS,
     ALLOWED_MOUNT_OPTIONS,
     ALLOWED_MSSQL_VERSIONS,
     ALLOWED_MYSQL_VERSIONS,
@@ -37,6 +36,7 @@ from airflow_breeze.global_constants import (
     ALLOWED_PYTHON_MAJOR_MINOR_VERSIONS,
     ALLOWED_USE_AIRFLOW_VERSIONS,
     APACHE_AIRFLOW_GITHUB_REPOSITORY,
+    AUTOCOMPLETE_INTEGRATIONS,
     SINGLE_PLATFORMS,
     get_available_documentation_packages,
 )
@@ -131,7 +131,7 @@ option_backend = click.option(
 option_integration = click.option(
     "--integration",
     help="Integration(s) to enable when running (can be more than one).",
-    type=BetterChoice(ALLOWED_INTEGRATIONS),
+    type=BetterChoice(AUTOCOMPLETE_INTEGRATIONS),
     multiple=True,
 )
 option_postgres_version = click.option(
