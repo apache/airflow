6f9efbd0537944102cd4a1cfef06e11fe0a3d03d
Michael Petro <40223998+michaelmicheal@users.noreply.github.com>
2023-02-20 03:42:42 -0500

    datasets, next_run_datasets, remove unnecessary timestamp filter (#29441)
    * datasets, next_run_datasets, remove unnecessary timestamp filter
    
    * remove redundant `_and`

--- airflow/www/views.py
+++ airflow/www/views.py
@@ -3706,10 +3706,7 @@ class Airflow(AirflowBaseView):
                 )
                 .join(
                     DatasetEvent,
-                    and_(
-                        DatasetEvent.dataset_id == DatasetModel.id,
-                        DatasetEvent.timestamp > DatasetDagRunQueue.created_at,
-                    ),
+                    DatasetEvent.dataset_id == DatasetModel.id,
                     isouter=True,
                 )
                 .filter(DagScheduleDatasetReference.dag_id == dag_id, ~DatasetModel.is_orphaned)
