a3e029d99f8ca391c92eba9845ae456e24a84545
Andrey Anshin <Andrey.Anshin@taragol.is>
2023-02-21 21:16:13 +0400

    Quarantine `test_cli_internal_api_background` (#29665)

--- tests/cli/commands/test_internal_api_command.py
+++ tests/cli/commands/test_internal_api_command.py
@@ -131,6 +131,7 @@ class TestCliInternalAPi:
                     raise
                 time.sleep(1)
 
+    @pytest.mark.quarantined
     @pytest.mark.execution_timeout(210)
     def test_cli_internal_api_background(self):
         with tempfile.TemporaryDirectory(prefix="gunicorn") as tmpdir:
