54897ff7b4d6e7c4ffd17fe6394315cc7a4adeab
r-richmond <rrichmond.gh@gmail.com>
2023-02-20 20:45:46 -0800

    sort and appease mypy

--- airflow/providers/google/cloud/operators/bigtable.py
+++ airflow/providers/google/cloud/operators/bigtable.py
@@ -22,8 +22,8 @@ import enum
 from typing import TYPE_CHECKING, Iterable, Sequence
 
 import google.api_core.exceptions
-from google.cloud.bigtable.column_family import GarbageCollectionRule
 from google.cloud.bigtable import enums
+from google.cloud.bigtable.column_family import GarbageCollectionRule
 
 from airflow.exceptions import AirflowException
 from airflow.models import BaseOperator
