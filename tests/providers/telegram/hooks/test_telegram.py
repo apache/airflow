57289b6dc6b02a4e8ba245aeae5e4f4430db5f53
eladkal <45845474+eladkal@users.noreply.github.com>
2023-02-22 18:59:13 +0200

    fix static checks

--- tests/providers/telegram/hooks/test_telegram.py
+++ tests/providers/telegram/hooks/test_telegram.py
@@ -34,6 +34,7 @@ class AsyncMock(mock.MagicMock):
     async def __call__(self, *args, **kwargs):
         return super(AsyncMock, self).__call__(*args, **kwargs)
 
+
 class TestTelegramHook:
     def setup_method(self):
         db.merge_conn(
