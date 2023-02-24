b0100c7baf2d91edeca4c8f46ebd1a8280c48f45
Ephraim Anierobi <splendidzigy24@gmail.com>
2023-02-24 16:01:09 +0100

    Fix on_failure_callback when task receives a SIGTERM
    This fixes on_failure_callback when task receives a SIGTERM by
    raising a different exception in the handler and catching the
    exception during task execution so we can directly run the failure
    callback

--- airflow/exceptions.py
+++ airflow/exceptions.py
@@ -29,6 +29,12 @@ if TYPE_CHECKING:
     from airflow.models import DagRun
 
 
+class AirflowKillSignal(Exception):
+    """Raise when there's kill signal"""
+
+    status_code = HTTPStatus.INTERNAL_SERVER_ERROR
+
+
 class AirflowException(Exception):
     """
     Base class for all Airflow's errors.
