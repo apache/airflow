842b75dd226cba91cc8e7e0414d68f95be4fed1d
pcc <pierre@data-fullstack.com>
2023-02-28 16:33:11 +0100

    add bigquery reservation hook

---
+++ airflow/providers/google/cloud/hooks/bigquery_reservation.py
@@ -0,0 +1,129 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""This module contains a BigQuery Reservation Hook."""
+from __future__ import annotations
+
+from typing import Sequence
+
+from google.cloud.bigquery_reservation_v1 import ReservationServiceClient
+
+from airflow.exceptions import AirflowException
+from airflow.providers.google.common.consts import CLIENT_INFO
+from airflow.providers.google.common.hooks.base_google import GoogleBaseHook
+
+
+class BigQueryReservationServiceHook(GoogleBaseHook):
+    """
+    Hook for Google Bigquery Reservation API.
+
+    :param gcp_conn_id: The Airflow connection used for GCP credentials.
+    :param location: The location of the BigQuery resource.
+    :param delegate_to: This performs a task on one host with reference to other hosts.
+    :param impersonation_chain: This is the optional service account to impersonate using short term
+        credentials.
+    """
+
+    conn_name_attr = "gcp_conn_id"
+    default_conn_name = "google_cloud_bigquery_reservation_default"
+    conn_type = "gcpbigqueryreservation"
+    hook_name = "Google Bigquery Reservation"
+
+    def __init__(
+        self,
+        gcp_conn_id: str = GoogleBaseHook.default_conn_name,
+        location: str | None = None,
+        delegate_to: str | None = None,
+        impersonation_chain: str | Sequence[str] | None = None,
+    ) -> None:
+        super().__init__(
+            gcp_conn_id=gcp_conn_id,
+            delegate_to=delegate_to,
+            impersonation_chain=impersonation_chain,
+        )
+        self.location = location
+        self._client: ReservationServiceClient | None = None
+
+    @staticmethod
+    def _convert_gb_to_kb(value: int) -> int:
+        """
+        Convert GB value to KB.
+
+        :param value: Value to convert
+        """
+        return value * 1073741824
+
+    def get_client(self) -> ReservationServiceClient:
+        """
+        Get reservation service client.
+
+        :return: Google Bigquery Reservation client
+        """
+        if not self._client:
+            self._client = ReservationServiceClient(
+                credentials=self.get_credentials(), client_info=CLIENT_INFO
+            )
+        return self._client
+
+    @GoogleBaseHook.fallback_to_default_project_id
+    def create_bi_reservation(self, project_id: str, size: int) -> None:
+        """
+        Create BI Engine reservation
+
+        :param project_id: The name of the project where we want to create/update
+            the BI Engine reservation.
+        :param size: The BI Engine reservation size in Gb.
+        """
+        parent = f"projects/{project_id}/locations/{self.location}/biReservation"
+        client = self.get_client()
+        size = self._convert_gb_to_kb(value=size)
+
+        try:
+            bi_reservation = client.get_bi_reservation(name=parent)
+            bi_reservation.size = size + bi_reservation.size
+
+            client.update_bi_reservation(bi_reservation=bi_reservation)
+
+            self.log.info("BI Engine reservation {parent} have been updated to {bi_reservation.size}Kb.")
+        except Exception as e:
+            self.log.error(e)
+            raise AirflowException(f"Failed to create BI engine reservation of {size}.")
+
+    @GoogleBaseHook.fallback_to_default_project_id
+    def delete_bi_reservation(self, project_id: str, size: int | None = None) -> None:
+        """
+        Delete/Update BI Engine reservation with the specified memory size
+
+        :param project_id: The name of the project where we want to delete/update
+            the BI Engine reservation.
+        :param size: The BI Engine reservation size in Gb.
+        """
+        parent = f"projects/{project_id}/locations/{self.location}/biReservation"
+        client = self.get_client()
+        try:
+            bi_reservation = client.get_bi_reservation(name=parent)
+            if size is not None:
+                size = self._convert_gb_to_kb(size)
+                bi_reservation.size = max(bi_reservation.size - size, 0)
+            else:
+                bi_reservation.size = 0
+
+            client.update_bi_reservation(bi_reservation=bi_reservation)
+            self.log.info("BI Engine reservation {parent} have been updated to {bi_reservation.size}Kb.")
+        except Exception as e:
+            self.log.error(e)
+            raise AirflowException(f"Failed to delete BI engine reservation of {size}.")
---
+++ tests/providers/google/cloud/hooks/test_bigquery_reservation.py
@@ -0,0 +1,166 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+from __future__ import annotations
+
+from random import randint
+from unittest import mock
+
+import pytest
+from google.cloud.bigquery_reservation_v1 import BiReservation, ReservationServiceClient
+
+from airflow.exceptions import AirflowException
+from airflow.providers.google.cloud.hooks.bigquery_reservation import (
+    BigQueryReservationServiceHook,
+)
+from airflow.providers.google.common.consts import CLIENT_INFO
+from tests.providers.google.cloud.utils.base_gcp_mock import mock_base_gcp_hook_no_default_project_id
+
+CREDENTIALS = "test-creds"
+PROJECT_ID = "test-project"
+LOCATION = "US"
+SIZE = 100
+CONSTANT_GO_TO_KO = 1073741824
+SIZE_KO = SIZE * CONSTANT_GO_TO_KO
+PARENT = f"projects/{PROJECT_ID}/locations/{LOCATION}/biReservation"
+
+
+class TestBigQueryReservationHook:
+    def setup_method(self):
+        with mock.patch(
+            "airflow.providers.google.cloud.hooks.bigquery_reservation.GoogleBaseHook.__init__",
+            new=mock_base_gcp_hook_no_default_project_id,
+        ):
+            self.hook = BigQueryReservationServiceHook(location=LOCATION)
+            self.hook.get_credentials = mock.MagicMock(return_value=CREDENTIALS)
+            self.location = LOCATION
+
+    def test_convert_gb_to_kb(self):
+        value = randint(1, 1000)
+        assert self.hook._convert_gb_to_kb(value) == value * 1073741824
+
+    @mock.patch("google.cloud.bigquery_reservation_v1.ReservationServiceClient")
+    def test_get_client_already_exist(self, reservation_client_mock):
+        expected = reservation_client_mock(credentials=self.hook.get_credentials(), client_info=CLIENT_INFO)
+        self.hook._client = expected
+        assert self.hook.get_client() == expected
+
+    # Create BI Reservation
+    @mock.patch(
+        "airflow.providers.google.cloud.hooks.bigquery_reservation.BigQueryReservationServiceHook.get_client"
+    )
+    def test_create_bi_reservation_existing_reservation_success(self, client_mock):
+        requested_size_gb = SIZE
+        initial_size = SIZE_KO
+        expected_size = initial_size + requested_size_gb * CONSTANT_GO_TO_KO
+
+        client_mock.return_value.get_bi_reservation.return_value = BiReservation(
+            name=PARENT, size=initial_size
+        )
+        self.hook.create_bi_reservation(project_id=PROJECT_ID, size=requested_size_gb)
+        client_mock.return_value.get_bi_reservation.assert_called_once_with(name=PARENT)
+        client_mock.return_value.update_bi_reservation.assert_called_once_with(
+            bi_reservation=BiReservation(name=PARENT, size=expected_size)
+        )
+
+    @mock.patch(
+        "airflow.providers.google.cloud.hooks.bigquery_reservation.BigQueryReservationServiceHook.get_client"
+    )
+    def test_create_bi_reservation_none_existing_reservation_success(self, client_mock):
+        requested_size_gb = SIZE
+        initial_size = 0
+        expected_size = initial_size + requested_size_gb * CONSTANT_GO_TO_KO
+
+        client_mock.return_value.get_bi_reservation.return_value = BiReservation(
+            name=PARENT, size=initial_size
+        )
+        self.hook.create_bi_reservation(project_id=PROJECT_ID, size=requested_size_gb)
+        client_mock.return_value.get_bi_reservation.assert_called_once_with(name=PARENT)
+
+        client_mock.return_value.update_bi_reservation.assert_called_once_with(
+            bi_reservation=BiReservation(name=PARENT, size=expected_size)
+        )
+
+    @mock.patch.object(ReservationServiceClient, "get_bi_reservation", side_effect=Exception("Test"))
+    def test_create_bi_reservation_get_failure(self, call_failure):
+        with pytest.raises(AirflowException):
+            self.hook.create_bi_reservation(project_id=PROJECT_ID, size=SIZE)
+
+    @mock.patch.object(ReservationServiceClient, "get_bi_reservation")
+    @mock.patch.object(ReservationServiceClient, "update_bi_reservation", side_effect=Exception("Test"))
+    def test_create_bi_reservation_update_failure(self, call_failure, get_bi_reservation_mock):
+        with pytest.raises(AirflowException):
+            self.hook.create_bi_reservation(project_id=PROJECT_ID, size=SIZE)
+
+    # Delete BI Reservation
+    @mock.patch(
+        "airflow.providers.google.cloud.hooks.bigquery_reservation.BigQueryReservationServiceHook.get_client"
+    )
+    def test_delete_bi_reservation_size_none_success(self, client_mock):
+        initial_size = SIZE_KO
+        expected_size = 0
+        client_mock.return_value.get_bi_reservation.return_value = BiReservation(
+            name=PARENT, size=initial_size
+        )
+        self.hook.delete_bi_reservation(project_id=PROJECT_ID)
+        client_mock.return_value.get_bi_reservation.assert_called_once_with(name=PARENT)
+        client_mock.return_value.update_bi_reservation.assert_called_once_with(
+            bi_reservation=BiReservation(name=PARENT, size=expected_size)
+        )
+
+    @mock.patch(
+        "airflow.providers.google.cloud.hooks.bigquery_reservation.BigQueryReservationServiceHook.get_client"
+    )
+    def test_delete_bi_reservation_size_filled_update_non_negative_success(self, client_mock):
+        initial_size = 100 * CONSTANT_GO_TO_KO
+        requeted_deleted_size_gb = 50
+        expected_size = initial_size - requeted_deleted_size_gb * CONSTANT_GO_TO_KO
+        client_mock.return_value.get_bi_reservation.return_value = BiReservation(
+            name=PARENT, size=initial_size
+        )
+        self.hook.delete_bi_reservation(project_id=PROJECT_ID, size=requeted_deleted_size_gb)
+        client_mock.return_value.get_bi_reservation.assert_called_once_with(name=PARENT)
+        client_mock.return_value.update_bi_reservation.assert_called_once_with(
+            bi_reservation=BiReservation(name=PARENT, size=expected_size)
+        )
+
+    @mock.patch(
+        "airflow.providers.google.cloud.hooks.bigquery_reservation.BigQueryReservationServiceHook.get_client"
+    )
+    def test_delete_bi_reservation_size_filled_update_negative_success(self, client_mock):
+        initial_size = 100 * CONSTANT_GO_TO_KO
+        requeted_deleted_size_gb = 200
+        expected_size = 0
+        client_mock.return_value.get_bi_reservation.return_value = BiReservation(
+            name=PARENT, size=initial_size
+        )
+        self.hook.delete_bi_reservation(project_id=PROJECT_ID, size=requeted_deleted_size_gb)
+        client_mock.return_value.get_bi_reservation.assert_called_once_with(name=PARENT)
+        client_mock.return_value.update_bi_reservation.assert_called_once_with(
+            bi_reservation=BiReservation(name=PARENT, size=expected_size)
+        )
+
+    @mock.patch.object(ReservationServiceClient, "get_bi_reservation", side_effect=Exception("Test"))
+    def test_delete_bi_reservation_get_failure(self, call_failure):
+        with pytest.raises(AirflowException):
+            self.hook.delete_bi_reservation(project_id=PROJECT_ID, size=SIZE)
+
+    @mock.patch.object(ReservationServiceClient, "get_bi_reservation")
+    @mock.patch.object(ReservationServiceClient, "update_bi_reservation", side_effect=Exception("Test"))
+    def test_delete_bi_reservation_update_failure(self, call_failure, get_bi_reservation_mock):
+        with pytest.raises(AirflowException):
+            self.hook.delete_bi_reservation(project_id=PROJECT_ID, size=SIZE)
