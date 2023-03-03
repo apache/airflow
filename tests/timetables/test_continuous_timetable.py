b1b82fb1f3e06f197f2281cd3c275fb6d30199bd
Sam Wheating <samwheating@gmail.com>
2023-03-03 14:54:41 -0800

    Adding tests for continuous timetable

---
+++ tests/timetables/test_continuous_timetable.py
@@ -0,0 +1,61 @@
+from airflow.timetables.simple import ContinuousTimetable
+from airflow.timetables.base import DagRunInfo, DataInterval, TimeRestriction
+import pendulum
+
+import pytest
+
+import time_machine
+
+START_DATE = pendulum.datetime(2023, 3, 3, tz="UTC")
+DURING_DATE = pendulum.datetime(2023, 3, 6, tz="UTC")
+END_DATE = pendulum.datetime(2023, 3, 10, tz="UTC")
+AFTER_DATE = pendulum.datetime(2023, 3, 12, tz="UTC")
+
+
+@pytest.fixture()
+def restriction():
+    return TimeRestriction(earliest=START_DATE, latest=END_DATE, catchup=True)
+
+
+@pytest.fixture()
+def timetable():
+    return ContinuousTimetable()
+
+
+def test_no_runs_without_start_date(timetable):
+    next_info = timetable.next_dagrun_info(
+        last_automated_data_interval=None,
+        restriction=TimeRestriction(earliest=None, latest=None, catchup=False),
+    )
+    assert next_info is None
+
+
+@time_machine.travel(DURING_DATE)
+def test_first_run_matches_start_date(timetable, restriction):
+    next_info = timetable.next_dagrun_info(
+        last_automated_data_interval=None,
+        restriction=restriction,
+    )
+    assert next_info.run_after == START_DATE
+
+
+@time_machine.travel(DURING_DATE)
+def test_run_uses_utcnow(timetable, restriction):
+
+    next_info = timetable.next_dagrun_info(
+        last_automated_data_interval=START_DATE,
+        restriction=restriction,
+    )
+
+    assert next_info.run_after == DURING_DATE
+
+
+@time_machine.travel(AFTER_DATE)
+def test_no_runs_after_end_date(timetable, restriction):
+
+    next_info = timetable.next_dagrun_info(
+        last_automated_data_interval=START_DATE,
+        restriction=restriction,
+    )
+
+    assert next_info is None
