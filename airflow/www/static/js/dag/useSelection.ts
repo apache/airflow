12e0dcdeb8a406b11572a2d428a7108c2ef29282
Brent Bovenzi <brent.bovenzi@gmail.com>
2023-03-03 00:10:31 -0500

    store-grid-tabs-in-url-params

--- airflow/www/static/js/dag/useSelection.ts
+++ airflow/www/static/js/dag/useSelection.ts
@@ -18,7 +18,6 @@
  */
 
 import { useSearchParams } from "react-router-dom";
-import URLSearchParamsWrapper from "src/utils/URLSearchParamWrapper";
 
 const RUN_ID = "dag_run_id";
 const TASK_ID = "task_id";
@@ -42,7 +41,7 @@ const useSelection = () => {
   };
 
   const onSelect = ({ runId, taskId, mapIndex }: SelectionProps) => {
-    const params = new URLSearchParamsWrapper(searchParams);
+    const params = new URLSearchParams(window.location.search);
 
     if (runId) params.set(RUN_ID, runId);
     else params.delete(RUN_ID);
