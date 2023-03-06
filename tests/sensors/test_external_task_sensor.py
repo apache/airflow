6f1510e599de3e368237c879b034506f49dbb776
Andrew Gibbs <github@andrew.gibbs.io>
2023-03-06 10:00:31 +0000

    PR feedback - use session fixture

--- tests/sensors/test_external_task_sensor.py
+++ tests/sensors/test_external_task_sensor.py
@@ -307,7 +307,7 @@ class TestExternalTaskSensor:
         assert len(task_instances) == 1, "Unexpected number of task instances"
         assert task_instances[0].state == State.SKIPPED, "Unexpected external task state"
 
-    def test_external_task_sensor_skipped_states_as_skipped(self):
+    def test_external_task_sensor_skipped_states_as_skipped(self, session):
         self.add_time_sensor()
         op = ExternalTaskSensor(
             task_id="test_external_task_sensor_check",
@@ -322,7 +322,6 @@ class TestExternalTaskSensor:
         op.run(start_date=DEFAULT_DATE, end_date=DEFAULT_DATE, ignore_ti_state=True)
 
         # then
-        session = settings.Session()
         TI = TaskInstance
         task_instances: list[TI] = session.query(TI).filter(TI.task_id == op.task_id).all()
         assert len(task_instances) == 1, "Unexpected number of task instances"
