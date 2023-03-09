e505564ed23d62efd587dd2299a4071003a2fa30
Dmytro Yurchenko <1inuxoid@whoyz.com>
2023-03-09 13:11:35 +0100

    Update airflow/providers/amazon/aws/transfers/dynamodb_to_s3.py
    Co-authored-by: Andrey Anshin <Andrey.Anshin@taragol.is>

--- airflow/providers/amazon/aws/transfers/dynamodb_to_s3.py
+++ airflow/providers/amazon/aws/transfers/dynamodb_to_s3.py
@@ -63,7 +63,7 @@ def _upload_file_to_s3(
     file_obj: IO,
     bucket_name: str,
     s3_key_prefix: str,
-    aws_conn_id: str | None | ArgNotSet = AwsBaseHook.default_conn_name,
+    aws_conn_id: str | None = AwsBaseHook.default_conn_name,
 ) -> None:
     if isinstance(aws_conn_id, str) or aws_conn_id is None:
         s3_client = S3Hook(aws_conn_id=aws_conn_id).get_conn()
