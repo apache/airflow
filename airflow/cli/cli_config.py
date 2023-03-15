0cc3b0f1d7e089a3894de8828ac62553f1efe018
Niko Oliveira <onikolas@amazon.com>
2023-03-15 10:28:34 -0700

    Update airflow/cli/cli_config.py
    Co-authored-by: Tzu-ping Chung <uranusjr@gmail.com>

--- airflow/cli/cli_config.py
+++ airflow/cli/cli_config.py
@@ -16,9 +16,8 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-"""This modules contains the explicit configuration and definition of Airflow
-CLI commands
-"""
+"""Explicit configuration and definition of Airflow CLI commands."""
+
 from __future__ import annotations
 
 import argparse
