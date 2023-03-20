82cb293c7b11a254129b2db8c1ce90e91449fd46
Hussein Awala <houssein.awala.96@gmail.com>
2023-03-20 19:59:59 +0100

    replace execution_date by run_id

--- airflow/jobs/triggerer_job.py
+++ airflow/jobs/triggerer_job.py
@@ -485,12 +485,12 @@ class TriggerRunner(threading.Thread, LoggingMixin):
             if trigger_id not in self.triggers:
                 task_instance: TaskInstance = trigger_instance.task_instance
                 dag_id = task_instance.dag_id
-                execution_date = task_instance.execution_date
+                run_id = task_instance.run_id
                 task_id = task_instance.task_id
                 map_index = task_instance.map_index
                 self.triggers[trigger_id] = {
                     "task": asyncio.create_task(self.run_trigger(trigger_id, trigger_instance)),
-                    "name": f"{dag_id}/{execution_date}/{task_id}/{map_index} (ID {trigger_id})",
+                    "name": f"{dag_id}/{run_id}/{task_id}/{map_index} (ID {trigger_id})",
                     "events": 0,
                 }
             else:
