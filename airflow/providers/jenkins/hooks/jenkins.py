db02510d00398a1b50000a9deb4254d6ba0cbd98
Victor Chiapaikeo <vchiapaikeo@gmail.com>
2023-03-25 15:37:53 -0400

    `JenkinsHook` fix connection intake on https (#30301)

--- airflow/providers/jenkins/hooks/jenkins.py
+++ airflow/providers/jenkins/hooks/jenkins.py
@@ -17,10 +17,40 @@
 # under the License.
 from __future__ import annotations
 
+from functools import wraps
+from typing import Any
+
 import jenkins
 
 from airflow.hooks.base import BaseHook
-from airflow.utils.strings import to_boolean
+
+
+def _ensure_prefixes(conn_type):
+    """
+    Remove when provider min airflow version >= 2.5.0 since this is handled by
+    provider manager from that version.
+    """
+
+    def dec(func):
+        @wraps(func)
+        def inner():
+            field_behaviors = func()
+            conn_attrs = {"host", "schema", "login", "password", "port", "extra"}
+
+            def _ensure_prefix(field):
+                if field not in conn_attrs and not field.startswith("extra__"):
+                    return f"extra__{conn_type}__{field}"
+                else:
+                    return field
+
+            if "placeholders" in field_behaviors:
+                placeholders = field_behaviors["placeholders"]
+                field_behaviors["placeholders"] = {_ensure_prefix(k): v for k, v in placeholders.items()}
+            return field_behaviors
+
+        return inner
+
+    return dec
 
 
 class JenkinsHook(BaseHook):
@@ -31,13 +61,41 @@ class JenkinsHook(BaseHook):
     conn_type = "jenkins"
     hook_name = "Jenkins"
 
+    @staticmethod
+    def get_connection_form_widgets() -> dict[str, Any]:
+        """Returns connection widgets to add to connection form"""
+        from flask_babel import lazy_gettext
+        from wtforms import BooleanField
+
+        return {
+            "use_https": BooleanField(
+                label=lazy_gettext("Use Https"),
+                description="Specifies whether to use https scheme. Defaults to http",
+            ),
+        }
+
+    @staticmethod
+    @_ensure_prefixes(conn_type="jenkins")
+    def get_ui_field_behaviour() -> dict[str, Any]:
+        """Returns custom field behaviour"""
+        return {
+            "hidden_fields": ["schema", "extra"],
+            "relabeling": {},
+            "placeholders": {
+                "login": "Login for the Jenkins service you would like to connect to",
+                "password": "Password for the Jenkins service you would like to connect too",
+                "host": "Host for your Jenkins server. Should NOT contain scheme (http:// or https://)",
+                "port": "Specify a port number",
+            },
+        }
+
     def __init__(self, conn_id: str = default_conn_name) -> None:
         super().__init__()
         connection = self.get_connection(conn_id)
         self.connection = connection
         connection_prefix = "http"
         # connection.extra contains info about using https (true) or http (false)
-        if to_boolean(connection.extra):
+        if connection.extra_dejson.get("use_https"):
             connection_prefix = "https"
         url = f"{connection_prefix}://{connection.host}:{connection.port}/{connection.schema}"
         self.log.info("Trying to connect to %s", url)
