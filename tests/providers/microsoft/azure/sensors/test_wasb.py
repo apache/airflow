7a66c331f5590233f03e6e952227579224cb63c4
Phani Kumar <phani.kumar@astronomer.io>
2023-03-30 11:39:40 +0530

    Fix tests

--- tests/providers/microsoft/azure/sensors/test_wasb.py
+++ tests/providers/microsoft/azure/sensors/test_wasb.py
@@ -133,7 +133,7 @@ class TestWasbBlobAsyncSensor:
         with pytest.raises(TaskDeferred) as exc:
             self.SENSOR.execute(self.create_context(self.SENSOR))
         assert isinstance(exc.value.trigger, WasbBlobSensorTrigger), "Trigger is not a WasbBlobSensorTrigger"
-        assert exc.value.timeout == 5
+        assert exc.value.timeout == datetime.timedelta(seconds=5)
 
     @pytest.mark.parametrize(
         "event",
