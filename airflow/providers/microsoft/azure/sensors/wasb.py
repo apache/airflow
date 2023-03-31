9648049f62d124091fd66ae08392bd45c18c89ee
Phani Kumar <94376113+phanikumv@users.noreply.github.com>
2023-03-31 22:31:53 +0530

    Add `WasbBlobAsyncSensor` (#30197)
    * Add WasbBlobAsyncSensor
    
    * Add WasbBlobAsyncSensor
    
    * Add WasbBlobAsyncSensor
    
    * add tests and example DAG
    
    * Fix failure during deferral
    
    * Apply review suggestions
    
    * Add wasb async conn
    
    * Specify timeout in the tests
    
    * Fix tests

--- airflow/providers/microsoft/azure/sensors/wasb.py
+++ airflow/providers/microsoft/azure/sensors/wasb.py
@@ -17,9 +17,12 @@
 # under the License.
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Sequence
+from datetime import timedelta
+from typing import TYPE_CHECKING, Any, Sequence
 
+from airflow.exceptions import AirflowException
 from airflow.providers.microsoft.azure.hooks.wasb import WasbHook
+from airflow.providers.microsoft.azure.triggers.wasb import WasbBlobSensorTrigger
 from airflow.sensors.base import BaseSensorOperator
 
 if TYPE_CHECKING:
@@ -60,6 +63,65 @@ class WasbBlobSensor(BaseSensorOperator):
         self.log.info("Poking for blob: %s\n in wasb://%s", self.blob_name, self.container_name)
         hook = WasbHook(wasb_conn_id=self.wasb_conn_id)
         return hook.check_for_blob(self.container_name, self.blob_name, **self.check_options)
+
+
+class WasbBlobAsyncSensor(WasbBlobSensor):
+    """
+    Polls asynchronously for the existence of a blob in a WASB container.
+
+    :param container_name: name of the container in which the blob should be searched for
+    :param blob_name: name of the blob to check existence for
+    :param wasb_conn_id: the connection identifier for connecting to Azure WASB
+    :param poke_interval:  polling period in seconds to check for the status
+    :param public_read: whether an anonymous public read access should be used. Default is False
+    :param timeout: Time, in seconds before the task times out and fails.
+    """
+
+    def __init__(
+        self,
+        *,
+        container_name: str,
+        blob_name: str,
+        wasb_conn_id: str = "wasb_default",
+        public_read: bool = False,
+        poke_interval: float = 5.0,
+        **kwargs: Any,
+    ):
+        self.container_name = container_name
+        self.blob_name = blob_name
+        self.poke_interval = poke_interval
+        super().__init__(container_name=container_name, blob_name=blob_name, **kwargs)
+        self.wasb_conn_id = wasb_conn_id
+        self.public_read = public_read
+
+    def execute(self, context: Context) -> None:
+        """Defers trigger class to poll for state of the job run until it reaches
+        a failure state or success state
+        """
+        self.defer(
+            timeout=timedelta(seconds=self.timeout),
+            trigger=WasbBlobSensorTrigger(
+                container_name=self.container_name,
+                blob_name=self.blob_name,
+                wasb_conn_id=self.wasb_conn_id,
+                public_read=self.public_read,
+                poke_interval=self.poke_interval,
+            ),
+            method_name="execute_complete",
+        )
+
+    def execute_complete(self, context: Context, event: dict[str, str]) -> None:
+        """
+        Callback for when the trigger fires - returns immediately.
+        Relies on trigger to throw an exception, otherwise it assumes execution was
+        successful.
+        """
+        if event:
+            if event["status"] == "error":
+                raise AirflowException(event["message"])
+            self.log.info(event["message"])
+        else:
+            raise AirflowException("Did not receive valid event from the triggerer")
 
 
 class WasbPrefixSensor(BaseSensorOperator):
