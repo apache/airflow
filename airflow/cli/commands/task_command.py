a59b0bd22eceee1b2d7ec58e976c3c6704d46c62
agalsty&outlook.fr <agalsty&outlook.fr>
2023-03-31 18:15:26 +0000

    Remove str

--- airflow/cli/commands/task_command.py
+++ airflow/cli/commands/task_command.py
@@ -217,9 +217,9 @@ def _run_task_by_executor(args, dag, ti):
                 pickle = DagPickle(dag)
                 session.add(pickle)
             pickle_id = pickle.id
-            log.info("Pickled dag %s as pickle_id: %s", str(dag), str(pickle_id))
+            log.info("Pickled dag %s as pickle_id: %s", dag, pickle_id)
         except Exception as e:
-            log.error("Could not pickle the DAG: %s", str(e))
+            log.error("Could not pickle the DAG: %s", e)
             raise e
     executor = ExecutorLoader.get_default_executor()
     executor.job_id = None
@@ -389,7 +389,7 @@ def task_run(args, dag=None):
     get_listener_manager().hook.on_starting(component=TaskCommandMarker())
 
     if args.pickle:
-        log.info("Loading pickle id: %s", str(args.pickle))
+        log.info("Loading pickle id: %s", args.pickle)
         dag = get_dag_by_pickle(args.pickle)
     elif not dag:
         dag = get_dag(args.subdir, args.dag_id)
