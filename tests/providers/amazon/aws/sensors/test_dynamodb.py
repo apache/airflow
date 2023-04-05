b9cdc05d3fc21084cb32835d4caead88ccfa633f
Mark Richman <mrichman@users.noreply.github.com>
2023-04-04 20:17:08 -0400

    Unit test working

--- tests/providers/amazon/aws/sensors/test_dynamodb.py
+++ tests/providers/amazon/aws/sensors/test_dynamodb.py
@@ -26,31 +26,36 @@ from moto import mock_dynamodb
 
 class TestDynamoDBValueSensor:
     def setup_method(self):
+        self.table_name = "test_airflow"
+        self.key_name = "PK"
+        self.key_value = "Test"
+        self.attribute_name = "Foo"
+        self.attribute_value = "Bar"
+
         self.sensor = DynamoDBValueSensor(
             task_id="dynamodb_value_sensor",
-            poke_interval=30,
-            timeout=120,
-            soft_fail=False,
-            retries=10,
-            table_name="test_airflow",
-            partition_key_name="PK",
-            partition_key_value="Test",
-            sort_key_name="SK",
-            sort_key_value="2023-03-28T11:11:25-0400",
-            attribute_name="Foo",
-            attribute_value="Bar",
+            table_name=self.table_name,
+            partition_key_name=self.key_name,
+            partition_key_value=self.key_value,
+            attribute_name=self.attribute_name,
+            attribute_value=self.attribute_value,
         )
 
-        self.ddb_hook = DynamoDBHook()
-        self.sensor.hook = self.ddb_hook
-
     @mock_dynamodb
-    @patch("airflow.providers.amazon.aws.hooks.dynamodb.DynamoDBHook.conn", new_callable=PropertyMock)
-    def test_sensor_with_pk_and_sk(self, mock_conn):
-        response = {
-            "ResponseMetadata": {"HTTPStatusCode": 200},
-            "Item": {"PK": "Test", "SK": "2023-03-28T11:11:25-0400", "Foo": "Bar"},
-        }
+    def test_sensor_with_pk(self):
+        hook = DynamoDBHook(table_name=self.table_name, table_keys=[self.key_name])
+
+        hook.conn.create_table(
+            TableName=self.table_name,
+            KeySchema=[{"AttributeName": self.key_name, "KeyType": "HASH"}],
+            AttributeDefinitions=[{"AttributeName": self.key_name, "AttributeType": "S"}],
+            ProvisionedThroughput={"ReadCapacityUnits": 10, "WriteCapacityUnits": 10},
+        )
+
+        items = [{self.key_name: self.key_value, self.attribute_name: self.attribute_value}]
+        hook.write_batch_data(items)
+
+        assert self.sensor.poke({})
 
         mock_conn.Table = Mock()
         mock_conn.Table.get_item = Mock(return_value=response)
