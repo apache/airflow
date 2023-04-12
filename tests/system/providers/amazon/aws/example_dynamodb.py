c6589db2cdb5ca3463c3704e4f2242ed7377c2d6
Mark Richman <mrkrchm@amazon.com>
2023-04-12 19:20:22 +0000

    Added docs for DynamoDBValueSensor

--- tests/system/providers/amazon/aws/example_dynamodb.py
+++ tests/system/providers/amazon/aws/example_dynamodb.py
@@ -16,19 +16,65 @@
 # under the License.
 from __future__ import annotations
 
-import os
+from datetime import datetime
 
+import boto3
+from airflow.decorators import task
+from airflow.models.baseoperator import chain
 from airflow import DAG
-from airflow.utils.dates import days_ago
 
 from airflow.providers.amazon.aws.sensors.dynamodb import DynamoDBValueSensor
+from airflow.utils.trigger_rule import TriggerRule
+from tests.system.providers.amazon.aws.utils import ENV_ID_KEY, SystemTestContextBuilder
+
+
+DAG_ID = "example_dynamodbvaluesensor"
+sys_test_context_task = SystemTestContextBuilder().build()
+
+TABLE_ATTRIBUTES = [
+    {"AttributeName": "PK", "AttributeType": "S"},
+    {"AttributeName": "SK", "AttributeType": "S"},
+]
+TABLE_KEY_SCHEMA = [
+    {"AttributeName": "PK", "KeyType": "HASH"},
+    {"AttributeName": "SK", "KeyType": "RANGE"},
+]
+TABLE_THROUGHPUT = {"ReadCapacityUnits": 10, "WriteCapacityUnits": 10}
+
+
+@task(trigger_rule=TriggerRule.ALL_DONE)
+def create_table(table_name: str):
+    ddb = boto3.resource("dynamodb")
+    table = ddb.create_table(
+        AttributeDefinitions=TABLE_ATTRIBUTES,
+        TableName=table_name,
+        KeySchema=TABLE_KEY_SCHEMA,
+        ProvisionedThroughput=TABLE_THROUGHPUT,
+    )
+    boto3.client("dynamodb").get_waiter("table_exists").wait()
+    table.put_item(Item={"PK": "Test", "SK": "2022-07-12T11:11:25-0400", "Value": "Testing"})
+
+
+@task
+def delete_table(table_name: str):
+    ddb = boto3.resource("dynamodb")
+    table = ddb.delete_table(TableName=table_name)
+    boto3.client("dynamodb").get_waiter("table_not_exists").wait()
+
 
 with DAG(
-    dag_id=os.path.basename(__file__).replace(".py", ""),
-    schedule_interval=None,
+    dag_id=DAG_ID,
+    schedule="@once",
+    start_date=datetime(2021, 1, 1),
     catchup=False,
-    start_date=days_ago(1),
+    tags=["example"],
 ) as dag:
+    test_context = sys_test_context_task()
+    env_id = test_context[ENV_ID_KEY]
+    table_name = f"{env_id}-dynamodb-table"
+    create_table = create_table(table_name=table_name)
+
+    # [START howto_sensor_dynamodb]
     dynamodb_sensor = DynamoDBValueSensor(
         task_id="waiting_for_dynamodb_item_value",
         poke_interval=30,
@@ -40,6 +86,29 @@ with DAG(
         partition_key_value="Test",
         sort_key_name="SK",
         sort_key_value="2022-07-12T11:11:25-0400",
-        attribute_name="Foo",
-        attribute_value="Bar",
+        attribute_name="Value",
+        attribute_value="Testing",
     )
+    # [END howto_sensor_dynamodb]
+
+    chain(
+        # TEST SETUP
+        test_context,
+        # TEST BODY
+        create_table,
+        dynamodb_sensor,
+        # TEST TEARDOWN
+        delete_table,
+    )
+
+    from tests.system.utils.watcher import watcher
+
+    # This test needs watcher in order to properly mark success/failure
+    # when "tearDown" task with trigger rule is part of the DAG
+    list(dag.tasks) >> watcher()
+
+
+from tests.system.utils import get_test_run  # noqa: E402
+
+# Needed to run the example DAG with pytest (see: tests/system/README.md#run_via_pytest)
+test_run = get_test_run(dag)
