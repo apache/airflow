d7141542676141216f2a8cd99f1ce22d19e57411
Jarek Potiuk <jarek@potiuk.com>
2023-04-17 22:38:55 +0200

    Upgrade to MyPy 1.2.0 (#30687)
    Upgrading to latest (released a week ago) MyPy in the hopes it
    will fix some more problem with attrs after upgrading new packages,
    but it seems that even the latest MyPy does not know about the
    new typing changes introduced in attrs (traditionally mypy has
    attrs plugin that injects appropriate typing but apparently it
    needs to catch up with those changes.

--- setup.py
+++ setup.py
@@ -332,7 +332,7 @@ mypy_dependencies = [
     # TODO: upgrade to newer versions of MyPy continuously as they are released
     # Make sure to upgrade the mypy version in update-common-sql-api-stubs in .pre-commit-config.yaml
     # when you upgrade it here !!!!
-    "mypy==1.0.0",
+    "mypy==1.2.0",
     "types-boto",
     "types-certifi",
     "types-croniter",
