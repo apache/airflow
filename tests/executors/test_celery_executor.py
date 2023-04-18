7e1dace0fd3eed2bbec2ae1e55ac586c9e3dd8cb
D. Ferruzzi <ferruzzi@amazon.com>
2023-04-17 17:58:46 -0700

    [OTel Integration] Add tagging to existing stats (#30496)

--- tests/executors/test_celery_executor.py
+++ tests/executors/test_celery_executor.py
@@ -124,9 +124,15 @@ class TestCeleryExecutor:
         executor = celery_executor.CeleryExecutor()
         executor.heartbeat()
         calls = [
-            mock.call("executor.open_slots", mock.ANY),
-            mock.call("executor.queued_tasks", mock.ANY),
-            mock.call("executor.running_tasks", mock.ANY),
+            mock.call(
+                "executor.open_slots", value=mock.ANY, tags={"status": "open", "name": "CeleryExecutor"}
+            ),
+            mock.call(
+                "executor.queued_tasks", value=mock.ANY, tags={"status": "queued", "name": "CeleryExecutor"}
+            ),
+            mock.call(
+                "executor.running_tasks", value=mock.ANY, tags={"status": "running", "name": "CeleryExecutor"}
+            ),
         ]
         mock_stats_gauge.assert_has_calls(calls)
 
