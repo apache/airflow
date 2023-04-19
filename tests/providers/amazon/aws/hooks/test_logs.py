bf283af160a62c8f22f16bacd678fc16ab85ff72
Vincent Beck <vincbeck@amazon.com>
2023-04-19 16:09:50 -0600

    AWS logs. Exit fast when 3 consecutive responses are returned from AWS Cloudwatch logs

--- tests/providers/amazon/aws/hooks/test_logs.py
+++ tests/providers/amazon/aws/hooks/test_logs.py
@@ -17,8 +17,10 @@
 # under the License.
 from __future__ import annotations
 
-import time
+from unittest import mock
+from unittest.mock import patch
 
+import pytest
 from moto import mock_logs
 
 from airflow.providers.amazon.aws.hooks.logs import AwsLogsHook
@@ -26,37 +28,46 @@ from airflow.providers.amazon.aws.hooks.logs import AwsLogsHook
 
 @mock_logs
 class TestAwsLogsHook:
-    def test_get_conn_returns_a_boto3_connection(self):
+    @pytest.mark.parametrize(
+        "get_log_events_response, num_skip_events, expected_num_events",
+        [
+            # No event
+            ({"events": []}, 0, 0),
+            # 2 events in each call with no token, skip 2
+            ({"events": [{}, {}]}, 2, 4),
+            # 2 events in each call with no token
+            ({"events": [{}, {}]}, 0, 6),
+            # 2 events in each call. The third as a token
+            (
+                [
+                    {"events": [{}, {}]},
+                    {"events": [{}, {}]},
+                    {"nextForwardToken": "token", "events": [{}, {}]},
+                    {"events": [{}, {}]},
+                    {"events": [{}, {}]},
+                    {"events": [{}, {}]},
+                ],
+                0,
+                12,
+            ),
+            # 2 responses with same nextForwardToken
+            ({"nextForwardToken": "token", "events": [{}, {}]}, 0, 4),
+        ],
+    )
+    @patch("airflow.providers.amazon.aws.hooks.logs.AwsLogsHook.conn", new_callable=mock.PropertyMock)
+    def test_get_log_events(self, mock_conn, get_log_events_response, num_skip_events, expected_num_events):
+        if isinstance(get_log_events_response, list):
+            mock_conn().get_log_events.side_effect = get_log_events_response
+        else:
+            mock_conn().get_log_events.return_value = get_log_events_response
+
         hook = AwsLogsHook(aws_conn_id="aws_default", region_name="us-east-1")
-        assert hook.get_conn() is not None
-
-    def test_get_log_events(self):
-        # moto.logs does not support proper pagination so we cannot test that yet
-        # https://github.com/spulec/moto/issues/2259
-        # ToDo: seems like mock.logs support since https://github.com/spulec/moto/pull/2361
-
-        log_group_name = "example-group"
-        log_stream_name = "example-log-stream"
-
-        hook = AwsLogsHook(aws_conn_id="aws_default", region_name="us-east-1")
-
-        # First we create some log events
-        conn = hook.get_conn()
-        conn.create_log_group(logGroupName=log_group_name)
-        conn.create_log_stream(logGroupName=log_group_name, logStreamName=log_stream_name)
-
-        input_events = [{"timestamp": int(time.time()) * 1000, "message": "Test Message 1"}]
-
-        conn.put_log_events(
-            logGroupName=log_group_name, logStreamName=log_stream_name, logEvents=input_events
+        events = hook.get_log_events(
+            log_group="example-group",
+            log_stream_name="example-log-stream",
+            skip=num_skip_events,
         )
 
-        events = hook.get_log_events(log_group=log_group_name, log_stream_name=log_stream_name)
-
-        # Iterate through entire generator
         events = list(events)
-        count = len(events)
 
-        assert count == 1
-        assert events[0]["timestamp"] == input_events[0]["timestamp"]
-        assert events[0]["message"] == input_events[0]["message"]
+        assert len(events) == expected_num_events
