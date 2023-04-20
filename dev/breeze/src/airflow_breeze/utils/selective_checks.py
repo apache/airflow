b04894e5125e682d9ad043602d4babe5b0b39b0c
Niko Oliveira <onikolas@amazon.com>
2023-04-20 16:44:25 -0700

    Export SUSPENDED_PROVIDERS_FOLDERS for breeze testing commands (#30780)
    Export the SUSPENDED_PROVIDERS_FOLDERS env var in breeze directly
    instead of in Airflow CI workflows. This will fix the issue for users
    executing `breeze testing ...` commands locally.

--- dev/breeze/src/airflow_breeze/utils/selective_checks.py
+++ dev/breeze/src/airflow_breeze/utils/selective_checks.py
@@ -30,7 +30,6 @@ from airflow_breeze.utils.path_utils import (
     SYSTEM_TESTS_PROVIDERS_ROOT,
     TESTS_PROVIDERS_ROOT,
 )
-from airflow_breeze.utils.suspended_providers import get_suspended_providers_folders
 
 if sys.version_info >= (3, 8):
     from functools import cached_property
@@ -684,10 +683,6 @@ class SelectiveChecks:
     @cached_property
     def debug_resources(self) -> bool:
         return DEBUG_CI_RESOURCES_LABEL in self._pr_labels
-
-    @cached_property
-    def suspended_providers_folders(self) -> str:
-        return " ".join(get_suspended_providers_folders())
 
     @cached_property
     def helm_test_packages(self) -> str:
