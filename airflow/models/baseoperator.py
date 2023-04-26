61624241a9a5da23423b5e0b83df9d3faf30236d
Daniel Standish <15932138+dstandish@users.noreply.github.com>
2023-04-26 13:02:04 -0700

    make trigger rule non-private

--- airflow/models/baseoperator.py
+++ airflow/models/baseoperator.py
@@ -981,7 +981,7 @@ class BaseOperator(AbstractOperator, metaclass=BaseOperatorMeta):
         on_failure_fail_dagrun = kwargs.pop("on_failure_fail_dagrun", False)
         if "trigger_rule" in kwargs:
             raise ValueError("Cannot set trigger rule for teardown tasks.")
-        op = cls(*args, **kwargs, trigger_rule=TriggerRule._ALL_DONE_SETUP_SUCCESS)
+        op = cls(*args, **kwargs, trigger_rule=TriggerRule.ALL_DONE_SETUP_SUCCESS)
         op._is_teardown = True
         op._on_failure_fail_dagrun = on_failure_fail_dagrun
         return op
