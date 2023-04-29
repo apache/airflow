apache/airflow

commit 886d9bd6c9ea4e97b74e7143d77246ddea084260

Speed up most Users/Role CLI commands (#28259)
Originally those command were initializing whole Airflow Flask
webserver, which was much more than what was needed - we only
need to initialize Flask Appblication Builder. In case you have
slow DB this limits significantly not only a number of imported
classses but also a number of DB connections.

In some cases the speed of those CLI commands will visibly
go down from 10s of seonds to individual seconds (3x - 5x times).

Follow up after #28242, #28244

airflow/cli/commands/role_command.py
@@ -23,23 +23,19 @@ import itertools
 import json
 import os
 
-from flask import Flask
-
 from airflow.cli.simple_table import AirflowConsole
 from airflow.utils import cli as cli_utils
 from airflow.utils.cli import suppress_logs_and_warning
-from airflow.www.app import cached_app
-from airflow.www.extensions.init_appbuilder import init_appbuilder
 from airflow.www.fab_security.sqla.models import Action, Permission, Resource, Role
-from airflow.www.security import EXISTING_ROLES, AirflowSecurityManager
+from airflow.www.security import EXISTING_ROLES
 
 
 @suppress_logs_and_warning
 def roles_list(args):
     """Lists all existing roles."""
-    flask_app = Flask(__name__)
-    with flask_app.app_context():
-        appbuilder = init_appbuilder(flask_app)
+    from airflow.utils.cli_app_builder import get_application_builder
+
+    with get_application_builder() as appbuilder:
         roles = appbuilder.sm.get_all_roles()
 
     if not args.permission:
@@ -64,9 +60,11 @@ def roles_list(args):
 @suppress_logs_and_warning
 def roles_create(args):
     """Creates new empty role in DB."""
-    appbuilder = cached_app().appbuilder
-    for role_name in args.role:
-        appbuilder.sm.add_role(role_name)
+    from airflow.utils.cli_app_builder import get_application_builder
+
+    with get_application_builder() as appbuilder:
+        for role_name in args.role:
+            appbuilder.sm.add_role(role_name)
     print(f"Added {len(args.role)} role(s)")
 
 
@@ -74,65 +72,68 @@ def roles_create(args):
 @suppress_logs_and_warning
 def roles_delete(args):
     """Deletes role in DB."""
-    appbuilder = cached_app().appbuilder
+    from airflow.utils.cli_app_builder import get_application_builder
 
-    for role_name in args.role:
-        role = appbuilder.sm.find_role(role_name)
-        if not role:
-            print(f"Role named '{role_name}' does not exist")
-            exit(1)
-
-    for role_name in args.role:
-        appbuilder.sm.delete_role(role_name)
+    with get_application_builder() as appbuilder:
+        for role_name in args.role:
+            role = appbuilder.sm.find_role(role_name)
+            if not role:
+                print(f"Role named '{role_name}' does not exist")
+                exit(1)
+        for role_name in args.role:
+            appbuilder.sm.delete_role(role_name)
     print(f"Deleted {len(args.role)} role(s)")
 
 
 def __roles_add_or_remove_permissions(args):
-    asm: AirflowSecurityManager = cached_app().appbuilder.sm
-    is_add: bool = args.subcommand.startswith("add")
+    from airflow.utils.cli_app_builder import get_application_builder
 
-    role_map = {}
-    perm_map: dict[tuple[str, str], set[str]] = collections.defaultdict(set)
-    for name in args.role:
-        role: Role | None = asm.find_role(name)
-        if not role:
-            print(f"Role named '{name}' does not exist")
-            exit(1)
+    with get_application_builder() as appbuilder:
+        is_add: bool = args.subcommand.startswith("add")
 
-        role_map[name] = role
-        for permission in role.permissions:
-            perm_map[(name, permission.resource.name)].add(permission.action.name)
+        role_map = {}
+        perm_map: dict[tuple[str, str], set[str]] = collections.defaultdict(set)
+        asm = appbuilder.sm
+        for name in args.role:
+            role: Role | None = asm.find_role(name)
+            if not role:
+                print(f"Role named '{name}' does not exist")
+                exit(1)
 
-    for name in args.resource:
-        resource: Resource | None = asm.get_resource(name)
-        if not resource:
-            print(f"Resource named '{name}' does not exist")
-            exit(1)
+            role_map[name] = role
+            for permission in role.permissions:
+                perm_map[(name, permission.resource.name)].add(permission.action.name)
 
-    for name in args.action or []:
-        action: Action | None = asm.get_action(name)
-        if not action:
-            print(f"Action named '{name}' does not exist")
-            exit(1)
+        for name in args.resource:
+            resource: Resource | None = asm.get_resource(name)
+            if not resource:
+                print(f"Resource named '{name}' does not exist")
+                exit(1)
 
-    permission_count = 0
-    for (role_name, resource_name, action_name) in list(
-        itertools.product(args.role, args.resource, args.action or [None])
-    ):
-        res_key = (role_name, resource_name)
-        if is_add and action_name not in perm_map[res_key]:
-            perm: Permission | None = asm.create_permission(action_name, resource_name)
-            asm.add_permission_to_role(role_map[role_name], perm)
-            print(f"Added {perm} to role {role_name}")
-            permission_count += 1
-        elif not is_add and res_key in perm_map:
-            for _action_name in perm_map[res_key] if action_name is None else [action_name]:
-                perm: Permission | None = asm.get_permission(_action_name, resource_name)
-                asm.remove_permission_from_role(role_map[role_name], perm)
-                print(f"Deleted {perm} from role {role_name}")
+        for name in args.action or []:
+            action: Action | None = asm.get_action(name)
+            if not action:
+                print(f"Action named '{name}' does not exist")
+                exit(1)
+
+        permission_count = 0
+        for (role_name, resource_name, action_name) in list(
+            itertools.product(args.role, args.resource, args.action or [None])
+        ):
+            res_key = (role_name, resource_name)
+            if is_add and action_name not in perm_map[res_key]:
+                perm: Permission | None = asm.create_permission(action_name, resource_name)
+                asm.add_permission_to_role(role_map[role_name], perm)
+                print(f"Added {perm} to role {role_name}")
                 permission_count += 1
+            elif not is_add and res_key in perm_map:
+                for _action_name in perm_map[res_key] if action_name is None else [action_name]:
+                    perm: Permission | None = asm.get_permission(_action_name, resource_name)
+                    asm.remove_permission_from_role(role_map[role_name], perm)
+                    print(f"Deleted {perm} from role {role_name}")
+                    permission_count += 1
 
-    print(f"{'Added' if is_add else 'Deleted'} {permission_count} permission(s)")
+        print(f"{'Added' if is_add else 'Deleted'} {permission_count} permission(s)")
 
 
 @cli_utils.action_cli
@@ -157,9 +158,11 @@ def roles_export(args):
     Note, this function does not export the permissions associated for each role.
     Strictly, it exports the role names into the passed role json file.
     """
-    appbuilder = cached_app().appbuilder
-    roles = appbuilder.sm.get_all_roles()
-    exporting_roles = [role.name for role in roles if role.name not in EXISTING_ROLES]
+    from airflow.utils.cli_app_builder import get_application_builder
+
+    with get_application_builder() as appbuilder:
+        roles = appbuilder.sm.get_all_roles()
+        exporting_roles = [role.name for role in roles if role.name not in EXISTING_ROLES]
     filename = os.path.expanduser(args.file)
     kwargs = {} if not args.pretty else {"sort_keys": True, "indent": 4}
     with open(filename, "w", encoding="utf-8") as f:
@@ -186,9 +189,11 @@ def roles_import(args):
     except ValueError as e:
         print(f"File '{json_file}' is not a valid JSON file. Error: {e}")
         exit(1)
-    appbuilder = cached_app().appbuilder
-    existing_roles = [role.name for role in appbuilder.sm.get_all_roles()]
-    roles_to_import = [role for role in role_list if role not in existing_roles]
-    for role_name in roles_to_import:
-        appbuilder.sm.add_role(role_name)
+    from airflow.utils.cli_app_builder import get_application_builder
+
+    with get_application_builder() as appbuilder:
+        existing_roles = [role.name for role in appbuilder.sm.get_all_roles()]
+        roles_to_import = [role for role in role_list if role not in existing_roles]
+        for role_name in roles_to_import:
+            appbuilder.sm.add_role(role_name)
     print(f"roles '{roles_to_import}' successfully imported")

airflow/cli/commands/standalone_command.py
@@ -33,7 +33,6 @@ from airflow.executors.executor_loader import ExecutorLoader
 from airflow.jobs.scheduler_job import SchedulerJob
 from airflow.jobs.triggerer_job import TriggererJob
 from airflow.utils import db
-from airflow.www.app import cached_app
 
 
 class StandaloneCommand:
@@ -180,8 +179,10 @@ class StandaloneCommand:
         # server. Thus, we make a random password and store it in AIRFLOW_HOME,
         # with the reasoning that if you can read that directory, you can see
         # the database credentials anyway.
-        appbuilder = cached_app().appbuilder
-        user_exists = appbuilder.sm.find_user("admin")
+        from airflow.utils.cli_app_builder import get_application_builder
+
+        with get_application_builder() as appbuilder:
+            user_exists = appbuilder.sm.find_user("admin")
         password_path = os.path.join(AIRFLOW_HOME, "standalone_admin_password.txt")
         we_know_password = os.path.isfile(password_path)
         # If the user does not exist, make a random password and make it

airflow/cli/commands/user_command.py
@@ -32,7 +32,6 @@ from marshmallow.exceptions import ValidationError
 from airflow.cli.simple_table import AirflowConsole
 from airflow.utils import cli as cli_utils
 from airflow.utils.cli import suppress_logs_and_warning
-from airflow.www.app import cached_app
 
 
 class UserSchema(Schema):
@@ -49,42 +48,47 @@ class UserSchema(Schema):
 @suppress_logs_and_warning
 def users_list(args):
     """Lists users at the command line."""
-    appbuilder = cached_app().appbuilder
-    users = appbuilder.sm.get_all_users()
-    fields = ["id", "username", "email", "first_name", "last_name", "roles"]
+    from airflow.utils.cli_app_builder import get_application_builder
 
-    AirflowConsole().print_as(
-        data=users, output=args.output, mapper=lambda x: {f: x.__getattribute__(f) for f in fields}
-    )
+    with get_application_builder() as appbuilder:
+        users = appbuilder.sm.get_all_users()
+        fields = ["id", "username", "email", "first_name", "last_name", "roles"]
+
+        AirflowConsole().print_as(
+            data=users, output=args.output, mapper=lambda x: {f: x.__getattribute__(f) for f in fields}
+        )
 
 
 @cli_utils.action_cli(check_db=True)
 def users_create(args):
     """Creates new user in the DB."""
-    appbuilder = cached_app().appbuilder
-    role = appbuilder.sm.find_role(args.role)
-    if not role:
-        valid_roles = appbuilder.sm.get_all_roles()
-        raise SystemExit(f"{args.role} is not a valid role. Valid roles are: {valid_roles}")
+    from airflow.utils.cli_app_builder import get_application_builder
 
-    if args.use_random_password:
-        password = "".join(random.choice(string.printable) for _ in range(16))
-    elif args.password:
-        password = args.password
-    else:
-        password = getpass.getpass("Password:")
-        password_confirmation = getpass.getpass("Repeat for confirmation:")
-        if password != password_confirmation:
-            raise SystemExit("Passwords did not match")
+    with get_application_builder() as appbuilder:
+        role = appbuilder.sm.find_role(args.role)
+        if not role:
+            valid_roles = appbuilder.sm.get_all_roles()
+            raise SystemExit(f"{args.role} is not a valid role. Valid roles are: {valid_roles}")
+        if args.use_random_password:
+            password = "".join(random.choice(string.printable) for _ in range(16))
+        elif args.password:
+            password = args.password
+        else:
+            password = getpass.getpass("Password:")
+            password_confirmation = getpass.getpass("Repeat for confirmation:")
+            if password != password_confirmation:
+                raise SystemExit("Passwords did not match")
 
-    if appbuilder.sm.find_user(args.username):
-        print(f"{args.username} already exist in the db")
-        return
-    user = appbuilder.sm.add_user(args.username, args.firstname, args.lastname, args.email, role, password)
-    if user:
-        print(f'User "{args.username}" created with role "{args.role}"')
-    else:
-        raise SystemExit("Failed to create user")
+        if appbuilder.sm.find_user(args.username):
+            print(f"{args.username} already exist in the db")
+            return
+        user = appbuilder.sm.add_user(
+            args.username, args.firstname, args.lastname, args.email, role, password
+        )
+        if user:
+            print(f'User "{args.username}" created with role "{args.role}"')
+        else:
+            raise SystemExit("Failed to create user")
 
 
 def _find_user(args):
@@ -94,11 +98,12 @@ def _find_user(args):
     if args.username and args.email:
         raise SystemExit("Conflicting args: must supply either --username or --email, but not both")
 
-    appbuilder = cached_app().appbuilder
+    from airflow.utils.cli_app_builder import get_application_builder
 
-    user = appbuilder.sm.find_user(username=args.username, email=args.email)
-    if not user:
-        raise SystemExit(f'User "{args.username or args.email}" does not exist')
+    with get_application_builder() as appbuilder:
+        user = appbuilder.sm.find_user(username=args.username, email=args.email)
+        if not user:
+            raise SystemExit(f'User "{args.username or args.email}" does not exist')
     return user
 
 
@@ -107,12 +112,13 @@ def users_delete(args):
     """Deletes user from DB."""
     user = _find_user(args)
 
-    appbuilder = cached_app().appbuilder
+    from airflow.utils.cli_app_builder import get_application_builder
 
-    if appbuilder.sm.del_register_user(user):
-        print(f'User "{user.username}" deleted')
-    else:
-        raise SystemExit("Failed to delete user")
+    with get_application_builder() as appbuilder:
+        if appbuilder.sm.del_register_user(user):
+            print(f'User "{user.username}" deleted')
+        else:
+            raise SystemExit("Failed to delete user")
 
 
 @cli_utils.action_cli
@@ -120,53 +126,56 @@ def users_manage_role(args, remove=False):
     """Deletes or appends user roles."""
     user = _find_user(args)
 
-    appbuilder = cached_app().appbuilder
+    from airflow.utils.cli_app_builder import get_application_builder
 
-    role = appbuilder.sm.find_role(args.role)
-    if not role:
-        valid_roles = appbuilder.sm.get_all_roles()
-        raise SystemExit(f'"{args.role}" is not a valid role. Valid roles are: {valid_roles}')
+    with get_application_builder() as appbuilder:
+        role = appbuilder.sm.find_role(args.role)
+        if not role:
+            valid_roles = appbuilder.sm.get_all_roles()
+            raise SystemExit(f'"{args.role}" is not a valid role. Valid roles are: {valid_roles}')
 
-    if remove:
-        if role not in user.roles:
-            raise SystemExit(f'User "{user.username}" is not a member of role "{args.role}"')
+        if remove:
+            if role not in user.roles:
+                raise SystemExit(f'User "{user.username}" is not a member of role "{args.role}"')
 
-        user.roles = [r for r in user.roles if r != role]
-        appbuilder.sm.update_user(user)
-        print(f'User "{user.username}" removed from role "{args.role}"')
-    else:
-        if role in user.roles:
-            raise SystemExit(f'User "{user.username}" is already a member of role "{args.role}"')
+            user.roles = [r for r in user.roles if r != role]
+            appbuilder.sm.update_user(user)
+            print(f'User "{user.username}" removed from role "{args.role}"')
+        else:
+            if role in user.roles:
+                raise SystemExit(f'User "{user.username}" is already a member of role "{args.role}"')
 
-        user.roles.append(role)
-        appbuilder.sm.update_user(user)
-        print(f'User "{user.username}" added to role "{args.role}"')
+            user.roles.append(role)
+            appbuilder.sm.update_user(user)
+            print(f'User "{user.username}" added to role "{args.role}"')
 
 
 def users_export(args):
     """Exports all users to the json file."""
-    appbuilder = cached_app().appbuilder
-    users = appbuilder.sm.get_all_users()
-    fields = ["id", "username", "email", "first_name", "last_name", "roles"]
+    from airflow.utils.cli_app_builder import get_application_builder
 
-    # In the User model the first and last name fields have underscores,
-    # but the corresponding parameters in the CLI don't
-    def remove_underscores(s):
-        return re.sub("_", "", s)
+    with get_application_builder() as appbuilder:
+        users = appbuilder.sm.get_all_users()
+        fields = ["id", "username", "email", "first_name", "last_name", "roles"]
 
-    users = [
-        {
-            remove_underscores(field): user.__getattribute__(field)
-            if field != "roles"
-            else [r.name for r in user.roles]
-            for field in fields
-        }
-        for user in users
-    ]
+        # In the User model the first and last name fields have underscores,
+        # but the corresponding parameters in the CLI don't
+        def remove_underscores(s):
+            return re.sub("_", "", s)
 
-    with open(args.export, "w") as file:
-        file.write(json.dumps(users, sort_keys=True, indent=4))
-        print(f"{len(users)} users successfully exported to {file.name}")
+        users = [
+            {
+                remove_underscores(field): user.__getattribute__(field)
+                if field != "roles"
+                else [r.name for r in user.roles]
+                for field in fields
+            }
+            for user in users
+        ]
+
+        with open(args.export, "w") as file:
+            file.write(json.dumps(users, sort_keys=True, indent=4))
+            print(f"{len(users)} users successfully exported to {file.name}")
 
 
 @cli_utils.action_cli
@@ -192,58 +201,62 @@ def users_import(args):
 
 
 def _import_users(users_list: list[dict[str, Any]]):
-    appbuilder = cached_app().appbuilder
-    users_created = []
-    users_updated = []
+    from airflow.utils.cli_app_builder import get_application_builder
 
-    try:
-        UserSchema(many=True).load(users_list)
-    except ValidationError as e:
-        msg = []
-        for row_num, failure in e.normalized_messages().items():
-            msg.append(f"[Item {row_num}]")
-            for key, value in failure.items():
-                msg.append(f"\t{key}: {value}")
-        raise SystemExit("Error: Input file didn't pass validation. See below:\n{}".format("\n".join(msg)))
+    with get_application_builder() as appbuilder:
+        users_created = []
+        users_updated = []
 
-    for user in users_list:
-
-        roles = []
-        for rolename in user["roles"]:
-            role = appbuilder.sm.find_role(rolename)
-            if not role:
-                valid_roles = appbuilder.sm.get_all_roles()
-                raise SystemExit(f'Error: "{rolename}" is not a valid role. Valid roles are: {valid_roles}')
-
-            roles.append(role)
-
-        existing_user = appbuilder.sm.find_user(email=user["email"])
-        if existing_user:
-            print(f"Found existing user with email '{user['email']}'")
-            if existing_user.username != user["username"]:
-                raise SystemExit(
-                    f"Error: Changing the username is not allowed - please delete and recreate the user with"
-                    f" email {user['email']!r}"
-                )
-
-            existing_user.roles = roles
-            existing_user.first_name = user["firstname"]
-            existing_user.last_name = user["lastname"]
-            appbuilder.sm.update_user(existing_user)
-            users_updated.append(user["email"])
-        else:
-            print(f"Creating new user with email '{user['email']}'")
-            appbuilder.sm.add_user(
-                username=user["username"],
-                first_name=user["firstname"],
-                last_name=user["lastname"],
-                email=user["email"],
-                role=roles,
+        try:
+            UserSchema(many=True).load(users_list)
+        except ValidationError as e:
+            msg = []
+            for row_num, failure in e.normalized_messages().items():
+                msg.append(f"[Item {row_num}]")
+                for key, value in failure.items():
+                    msg.append(f"\t{key}: {value}")
+            raise SystemExit(
+                "Error: Input file didn't pass validation. See below:\n{}".format("\n".join(msg))
             )
 
-            users_created.append(user["email"])
+        for user in users_list:
+            roles = []
+            for rolename in user["roles"]:
+                role = appbuilder.sm.find_role(rolename)
+                if not role:
+                    valid_roles = appbuilder.sm.get_all_roles()
+                    raise SystemExit(
+                        f'Error: "{rolename}" is not a valid role. Valid roles are: {valid_roles}'
+                    )
+                roles.append(role)
 
-    return users_created, users_updated
+            existing_user = appbuilder.sm.find_user(email=user["email"])
+            if existing_user:
+                print(f"Found existing user with email '{user['email']}'")
+                if existing_user.username != user["username"]:
+                    raise SystemExit(
+                        f"Error: Changing the username is not allowed - "
+                        f"please delete and recreate the user with email {user['email']!r}"
+                    )
+
+                existing_user.roles = roles
+                existing_user.first_name = user["firstname"]
+                existing_user.last_name = user["lastname"]
+                appbuilder.sm.update_user(existing_user)
+                users_updated.append(user["email"])
+            else:
+                print(f"Creating new user with email '{user['email']}'")
+                appbuilder.sm.add_user(
+                    username=user["username"],
+                    first_name=user["firstname"],
+                    last_name=user["lastname"],
+                    email=user["email"],
+                    role=roles,
+                )
+
+                users_created.append(user["email"])
+
+        return users_created, users_updated
 
 
 add_role = functools.partial(users_manage_role, remove=False)

airflow/utils/cli_app_builder.py
@@ -1,4 +1,3 @@
-#
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -15,22 +14,26 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-"""Sync permission command."""
+
 from __future__ import annotations
 
-from airflow.utils import cli as cli_utils
+from contextlib import contextmanager
+from functools import lru_cache
+from typing import Generator
+
+from flask import Flask
+
+from airflow.www.extensions.init_appbuilder import AirflowAppBuilder, init_appbuilder
 
 
-@cli_utils.action_cli
-def sync_perm(args):
-    """Updates permissions for existing roles and DAGs."""
-    from airflow.utils.cli_app_builder import get_application_builder
+@lru_cache(maxsize=None)
+def _return_appbuilder(app: Flask) -> AirflowAppBuilder:
+    """Returns an appbuilder instance for the given app"""
+    return init_appbuilder(app)
 
-    with get_application_builder() as appbuilder:
-        print("Updating actions and resources for all existing roles")
-        # Add missing permissions for all the Base Views _before_ syncing/creating roles
-        appbuilder.add_permissions(update_perms=True)
-        appbuilder.sm.sync_roles()
-        if args.include_dags:
-            print("Updating permission on all DAG views")
-            appbuilder.sm.create_dag_specific_permissions()
+
+@contextmanager
+def get_application_builder() -> Generator[AirflowAppBuilder, None, None]:
+    flask_app = Flask(__name__)
+    with flask_app.app_context():
+        yield _return_appbuilder(flask_app)

tests/cli/commands/test_role_command.py
@@ -25,6 +25,7 @@ import pytest
 
 from airflow.cli.commands import role_command
 from airflow.security import permissions
+from airflow.utils.cli_app_builder import get_application_builder
 from airflow.www.fab_security.sqla.models import Role
 
 TEST_USER1_EMAIL = "test-user1@example.com"
@@ -33,14 +34,14 @@ TEST_USER2_EMAIL = "test-user2@example.com"
 
 class TestCliRoles:
     @pytest.fixture(autouse=True)
-    def _set_attrs(self, app, dagbag, parser):
-        self.app = app
+    def _set_attrs(self, dagbag, parser):
         self.dagbag = dagbag
         self.parser = parser
-        self.appbuilder = self.app.appbuilder
-        self.clear_roles_and_roles()
-        yield
-        self.clear_roles_and_roles()
+        with get_application_builder() as appbuilder:
+            self.appbuilder = appbuilder
+            self.clear_roles_and_roles()
+            yield
+            self.clear_roles_and_roles()
 
     def clear_roles_and_roles(self):
         for email in [TEST_USER1_EMAIL, TEST_USER2_EMAIL]:
@@ -108,6 +109,8 @@ class TestCliRoles:
 
         role_command.roles_create(self.parser.parse_args(["roles", "create", "FakeTeamC"]))
         assert self.appbuilder.sm.find_role("FakeTeamC") is not None
+        role: Role = self.appbuilder.sm.find_role("FakeTeamC")
+        assert len(role.permissions) == 0
 
         role_command.roles_add_perms(
             self.parser.parse_args(

commit 557f5bb3f49f0977969965222659346efbdd3af8

Add unit test for static path location for flask blueprints
The location has been broken by #28259 and fixed by #30952.

Adding unit tests should avoid regressions (though are not likely
any more).

tests/plugins/test_plugins_manager.py
@@ -22,6 +22,7 @@ import logging
 import os
 import sys
 import tempfile
+from pathlib import Path
 from unittest import mock
 
 import pytest
@@ -30,6 +31,7 @@ from airflow.hooks.base import BaseHook
 from airflow.listeners.listener import get_listener_manager
 from airflow.plugins_manager import AirflowPlugin
 from airflow.www import app as application
+from setup import AIRFLOW_SOURCES_ROOT
 from tests.test_utils.config import conf_vars
 from tests.test_utils.mock_plugins import mock_plugin_manager
 
@@ -134,6 +136,11 @@ class TestPluginsRBAC:
         # Blueprint should be present in the app
         assert "test_plugin" in self.app.blueprints
         assert self.app.blueprints["test_plugin"].name == bp.name
+
+    def test_app_static_folder(self):
+
+        # Blueprint static folder should be properly set
+        assert AIRFLOW_SOURCES_ROOT / "airflow" / "www" / "static" == Path(self.app.static_folder).resolve()
 
 
 def test_flaskappbuilder_nomenu_views():