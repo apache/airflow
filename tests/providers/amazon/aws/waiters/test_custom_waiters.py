e8dd42746e57093a161bd040c48ea743006ae469
ferruzzi <ferruzzi@amazon.com>
2023-05-08 14:49:37 -0700

    Revert "DynamoDBToS3Operator - Add feature to export table to a point in time (#30501)"
    This reverts commit fc4166127a1d2099d358fee1ea10662838cf9cf3.

--- tests/providers/amazon/aws/waiters/test_custom_waiters.py
+++ tests/providers/amazon/aws/waiters/test_custom_waiters.py
@@ -26,7 +26,6 @@ from botocore.exceptions import WaiterError
 from botocore.waiter import WaiterModel
 from moto import mock_eks
 
-from airflow.providers.amazon.aws.hooks.dynamodb import DynamoDBHook
 from airflow.providers.amazon.aws.hooks.ecs import EcsClusterStates, EcsHook, EcsTaskDefinitionStates
 from airflow.providers.amazon.aws.hooks.eks import EksHook
 from airflow.providers.amazon.aws.waiters.base_waiter import BaseBotoWaiter
@@ -250,59 +249,3 @@ class TestCustomECSServiceWaiters:
         ]
         waiter = EcsHook(aws_conn_id=None).get_waiter("task_definition_inactive")
         waiter.wait(taskDefinition="spam-egg", WaiterConfig={"Delay": 0.01, "MaxAttempts": 3})
-
-
-class TestCustomDynamoDBServiceWaiters:
-    """Test waiters from ``amazon/aws/waiters/dynamodb.json``."""
-
-    STATUS_COMPLETED = "COMPLETED"
-    STATUS_FAILED = "FAILED"
-    STATUS_IN_PROGRESS = "IN_PROGRESS"
-
-    @pytest.fixture(autouse=True)
-    def setup_test_cases(self, monkeypatch):
-        self.client = boto3.client("dynamodb", region_name="eu-west-3")
-        monkeypatch.setattr(DynamoDBHook, "conn", self.client)
-
-    @pytest.fixture
-    def mock_describe_export(self):
-        """Mock ``DynamoDBHook.Client.describe_export`` method."""
-        with mock.patch.object(self.client, "describe_export") as m:
-            yield m
-
-    def test_service_waiters(self):
-        hook_waiters = DynamoDBHook(aws_conn_id=None).list_waiters()
-        assert "export_table" in hook_waiters
-
-    @staticmethod
-    def describe_export(status: str):
-        """
-        Helper function for generate minimal DescribeExport response for single job.
-        https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_DescribeExport.html
-        """
-        return {"ExportDescription": {"ExportStatus": status}}
-
-    def test_export_table_to_point_in_time_completed(self, mock_describe_export):
-        """Test state transition from `in progress` to `completed` during init."""
-        with mock.patch("boto3.client") as client:
-            client.return_value = self.client
-            waiter = DynamoDBHook(aws_conn_id=None).get_waiter("export_table", client=self.client)
-            mock_describe_export.side_effect = [
-                self.describe_export(self.STATUS_IN_PROGRESS),
-                self.describe_export(self.STATUS_COMPLETED),
-            ]
-            waiter.wait(
-                ExportArn="LoremIpsumissimplydummytextoftheprintingandtypesettingindustry",
-            )
-
-    def test_export_table_to_point_in_time_failed(self, mock_describe_export):
-        """Test state transition from `in progress` to `failed` during init."""
-        with mock.patch("boto3.client") as client:
-            client.return_value = self.client
-            mock_describe_export.side_effect = [
-                self.describe_export(self.STATUS_IN_PROGRESS),
-                self.describe_export(self.STATUS_FAILED),
-            ]
-            waiter = DynamoDBHook(aws_conn_id=None).get_waiter("export_table", client=self.client)
-            with pytest.raises(WaiterError, match='we matched expected path: "FAILED"'):
-                waiter.wait(ExportArn="LoremIpsumissimplydummytextoftheprintingandtypesettingindustry")
