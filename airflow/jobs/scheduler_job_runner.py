b33665a62eedfd444e4aadac19fbaaccce3cac5e
Daniel Standish <15932138+dstandish@users.noreply.github.com>
2023-05-09 15:26:03 -0700

    tweak language

--- airflow/jobs/scheduler_job_runner.py
+++ airflow/jobs/scheduler_job_runner.py
@@ -704,16 +704,7 @@ class SchedulerJobRunner(BaseJobRunner, LoggingMixin):
             # We create map (dag_id, task_id, execution_date) -> in-memory try_number
             ti_primary_key_to_try_number_map[ti_key.primary] = ti_key.try_number
 
-            self.log.info(
-                "Executor received event with state %s for task instance "
-                "dag_id=%s task_id=%s run_id=%s try_number=%s map_index=%s",
-                state,
-                ti_key.dag_id,
-                ti_key.task_id,
-                ti_key.run_id,
-                ti_key.try_number,
-                ti_key.map_index,
-            )
+            self.log.info("Received executor event with state %s for task instance %s", state, ti_key)
             if state in (State.FAILED, State.SUCCESS, State.QUEUED):
                 tis_with_right_state.append(ti_key)
 
