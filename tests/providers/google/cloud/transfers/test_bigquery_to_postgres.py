cf1e26b04669ad2b232618fee623e6bc7797a13a
Jean-Baptiste Braun <8479617+jbbqqf@users.noreply.github.com>
2023-05-16 13:20:57 +0200

    Add BigQueryToPostgresOperator (#30658)
    Co-authored-by: eladkal <45845474+eladkal@users.noreply.github.com>

--- tests/providers/google/cloud/transfers/test_bigquery_to_postgres.py
+++ tests/providers/google/cloud/transfers/test_bigquery_to_postgres.py
@@ -19,29 +19,21 @@ from __future__ import annotations
 
 from unittest import mock
 
-import pytest
-
-try:
-    from airflow.providers.google.cloud.transfers.bigquery_to_mssql import BigQueryToMsSqlOperator
-except ImportError:
-    pytest.skip("MSSQL not available", allow_module_level=True)
-
+from airflow.providers.google.cloud.transfers.bigquery_to_postgres import BigQueryToPostgresOperator
 
 TASK_ID = "test-bq-create-table-operator"
-TEST_PROJECT_ID = "test-project"
 TEST_DATASET = "test-dataset"
 TEST_TABLE_ID = "test-table-id"
 TEST_DAG_ID = "test-bigquery-operators"
 
 
-@pytest.mark.backend("mssql")
-class TestBigQueryToMsSqlOperator:
+class TestBigQueryToPostgresOperator:
     @mock.patch("airflow.providers.google.cloud.transfers.bigquery_to_sql.BigQueryHook")
     def test_execute_good_request_to_bq(self, mock_hook):
         destination_table = "table"
-        operator = BigQueryToMsSqlOperator(
+        operator = BigQueryToPostgresOperator(
             task_id=TASK_ID,
-            source_project_dataset_table=f"{TEST_PROJECT_ID}.{TEST_DATASET}.{TEST_TABLE_ID}",
+            dataset_table=f"{TEST_DATASET}.{TEST_TABLE_ID}",
             target_table_name=destination_table,
             replace=False,
         )
