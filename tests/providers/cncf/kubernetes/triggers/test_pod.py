002edd1903239a788d3f9d4923f13497c11ce680
Hussein Awala <hussein@awala.fr>
2023-05-17 13:28:03 +0200

    add a unit test for the new waiting condition

--- tests/providers/cncf/kubernetes/triggers/test_pod.py
+++ tests/providers/cncf/kubernetes/triggers/test_pod.py
@@ -119,6 +119,35 @@ class TestKubernetesPodTrigger:
     @pytest.mark.asyncio
     @mock.patch(f"{TRIGGER_PATH}.define_container_state")
     @mock.patch(f"{TRIGGER_PATH}._get_async_hook")
+    async def test_run_loop_wait_pod_termination_before_returning_success_event(
+        self, mock_hook, mock_method, trigger
+    ):
+        running_state = mock.MagicMock(**{"status.phase": "Running"})
+        succeeded_state = mock.MagicMock(**{"status.phase": "Succeeded"})
+        mock_hook.return_value.get_pod.side_effect = [
+            self._mock_pod_result(running_state),
+            self._mock_pod_result(running_state),
+            self._mock_pod_result(succeeded_state),
+        ]
+        mock_method.return_value = ContainerState.TERMINATED
+
+        expected_event = TriggerEvent(
+            {
+                "name": POD_NAME,
+                "namespace": NAMESPACE,
+                "status": "success",
+                "message": "All containers inside pod have started successfully.",
+            }
+        )
+        with mock.patch.object(asyncio, "sleep") as mock_sleep:
+            actual_event = await (trigger.run()).asend(None)
+
+        assert actual_event == expected_event
+        assert mock_sleep.call_count == 2
+
+    @pytest.mark.asyncio
+    @mock.patch(f"{TRIGGER_PATH}.define_container_state")
+    @mock.patch(f"{TRIGGER_PATH}._get_async_hook")
     async def test_run_loop_return_failed_event(self, mock_hook, mock_method, trigger):
         mock_hook.return_value.get_pod.return_value = self._mock_pod_result(
             mock.MagicMock(
