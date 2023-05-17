d984fdea2781bb2c996a96fb8a0922208d655735
utkarsh sharma <utkarsharma2@gmail.com>
2023-05-17 15:32:53 +0530

    Add test_connection method to PagerdutyHook

--- airflow/providers/pagerduty/hooks/pagerduty.py
+++ airflow/providers/pagerduty/hooks/pagerduty.py
@@ -159,3 +159,11 @@ class PagerdutyHook(BaseHook):
             images=images,
             links=links,
         )
+
+    def test_connection(self):
+        try:
+            session = pdpyras.APISession(self.token)
+            session.list_all("services", params={"query": "some_non_existing_service"})
+        except Exception as e:
+            return False, str(e)
+        return True, "connection tested successfully"
