8b992f842de8b16cb61767bfbf9ebc2ab68b405a
Vincent Beck <vincbeck@amazon.com>
2023-05-23 14:09:29 -0400

    Add back spelling exceptions

--- airflow/providers/microsoft/azure/hooks/data_factory.py
+++ airflow/providers/microsoft/azure/hooks/data_factory.py
@@ -14,6 +14,20 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+"""Spelling exceptions.
+
+.. spelling::
+    CreateRunResponse
+    DatasetResource
+    LinkedServiceResource
+    LROPoller
+    PipelineResource
+    PipelineRun
+    TriggerResource
+    datafactory
+    DataFlow
+    mgmt
+"""
 from __future__ import annotations
 
 import inspect
