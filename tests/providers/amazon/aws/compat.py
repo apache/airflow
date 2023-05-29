c4be0daf1fc59b179d08c503eb3ca1a92c44b5cd
Ankit Chaurasia <8670962+sunank200@users.noreply.github.com>
2023-05-29 22:04:28 +0530

    remove test for Python 3.7 in Airflow

--- tests/providers/amazon/aws/compat.py
+++ tests/providers/amazon/aws/compat.py
@@ -19,19 +19,5 @@ from __future__ import annotations
 
 __all__ = ["async_mock", "AsyncMock"]
 
-import sys
-
-if sys.version_info < (3, 8):
-    # For compatibility with Python 3.7
-    from asynctest import mock as async_mock
-
-    # ``asynctest.mock.CoroutineMock`` which provide compatibility not working well with autospec=True
-    # as result "TypeError: object MagicMock can't be used in 'await' expression" could be raised.
-    # Best solution in this case provide as spec actual awaitable object
-    # >>> from tests.providers.apache.livy.compat import AsyncMock
-    # >>> from foo.bar import SpamEgg
-    # >>> mock_something = AsyncMock(SpamEgg)
-    from asynctest.mock import CoroutineMock as AsyncMock
-else:
-    from unittest import mock as async_mock
-    from unittest.mock import AsyncMock
+from unittest import mock as async_mock
+from unittest.mock import AsyncMock
