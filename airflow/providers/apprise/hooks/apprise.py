e77462286098db9b466575403ac0dc94301a4914
Utkarsh Sharma <utkarsharma2@gmail.com>
2023-06-01 09:23:00 +0530

    Update airflow/providers/apprise/hooks/apprise.py
    Co-authored-by: Jed Cunningham <66968678+jedcunningham@users.noreply.github.com>

--- airflow/providers/apprise/hooks/apprise.py
+++ airflow/providers/apprise/hooks/apprise.py
@@ -57,7 +57,7 @@ class AppriseHook(BaseHook):
         elif isinstance(config_object, dict):
             apprise_obj.add(config_object["path"], tag=config_object.get("tag", None))
         else:
-            raise ValueError("only dict / list[dict] type of values are expected")
+            raise ValueError(f"Only types of dict or list[dict] are expected in Apprise connections, got {type(config_object)}")
 
     def notify(
         self,
