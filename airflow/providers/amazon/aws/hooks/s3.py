545a26bd68c417514c230192b6b4c7c6a1de2abb
Ankit Chaurasia <8670962+sunank200@users.noreply.github.com>
2023-06-07 15:53:07 +0530

    add type checking

--- airflow/providers/amazon/aws/hooks/s3.py
+++ airflow/providers/amazon/aws/hooks/s3.py
@@ -39,10 +39,12 @@ from typing import TYPE_CHECKING, Any, Callable, TypeVar, cast
 from urllib.parse import urlsplit
 from uuid import uuid4
 
-try:
-    from aiobotocore.client import AioBaseClient
-except ImportError:
-    pass
+if TYPE_CHECKING:
+    try:
+        from aiobotocore.client import AioBaseClient
+    except ImportError:
+        pass
+
 from asgiref.sync import sync_to_async
 from boto3.s3.transfer import TransferConfig
 from botocore.exceptions import ClientError
