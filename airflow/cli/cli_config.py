f632a1aa20e1a18fbb95cf9328244e6d93494ee8
vedantlodha <vedant.lodha@hotmail.com>
2023-06-12 16:59:10 +0530

    run pre-commit on the changes.

--- airflow/cli/cli_config.py
+++ airflow/cli/cli_config.py
@@ -544,10 +544,9 @@ ARG_DEFAULT = Arg(
 ARG_DESERIALIZE_JSON = Arg(("-j", "--json"), help="Deserialize JSON variable", action="store_true")
 ARG_SERIALIZE_JSON = Arg(("-j", "--json"), help="Serialize JSON variable", action="store_true")
 ARG_VAR_IMPORT = Arg(("file",), help="Import variables from JSON file")
-ARG_VAR_EXPORT = Arg(("file",),
-                     help="Export all variables to JSON file",
-                     type=argparse.FileType("w", encoding="UTF-8")
-                     )
+ARG_VAR_EXPORT = Arg(
+    ("file",), help="Export all variables to JSON file", type=argparse.FileType("w", encoding="UTF-8")
+)
 # kerberos
 ARG_PRINCIPAL = Arg(("principal",), help="kerberos principal", nargs="?")
 ARG_KEYTAB = Arg(("-k", "--keytab"), help="keytab", nargs="?", default=conf.get("kerberos", "keytab"))
