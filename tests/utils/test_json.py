1570d9a1f48487a8fae1ebdafd9d332b0ef1c3a7
utkarsh sharma <utkarsharma2@gmail.com>
2023-06-15 11:34:56 +0530

    Remove unwanted changes

--- tests/utils/test_json.py
+++ tests/utils/test_json.py
@@ -22,7 +22,7 @@ from dataclasses import dataclass
 from datetime import date, datetime
 from typing import ClassVar
 
-# import numpy as np
+import numpy as np
 import pendulum
 import pytest
 
@@ -67,14 +67,14 @@ class TestWebEncoder:
     def test_encode_date(self):
         assert json.dumps(date(2017, 5, 21), cls=utils_json.WebEncoder) == '"2017-05-21"'
 
-    # def test_encode_numpy_int(self):
-    #     assert json.dumps(np.int32(5), cls=utils_json.WebEncoder) == "5"
-    #
-    # def test_encode_numpy_bool(self):
-    #     assert json.dumps(np.bool_(True), cls=utils_json.WebEncoder) == "true"
-    #
-    # def test_encode_numpy_float(self):
-    #     assert json.dumps(np.float16(3.76953125), cls=utils_json.WebEncoder) == "3.76953125"
+    def test_encode_numpy_int(self):
+        assert json.dumps(np.int32(5), cls=utils_json.WebEncoder) == "5"
+
+    def test_encode_numpy_bool(self):
+        assert json.dumps(np.bool_(True), cls=utils_json.WebEncoder) == "true"
+
+    def test_encode_numpy_float(self):
+        assert json.dumps(np.float16(3.76953125), cls=utils_json.WebEncoder) == "3.76953125"
 
 
 class TestXComEncoder:
