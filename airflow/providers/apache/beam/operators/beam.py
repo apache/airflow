152c8d7addc4b70f8c7027806bec5e538efd0bf7
Hussein Awala <hussein@awala.fr>
2023-06-18 23:03:24 +0200

    Update airflow/providers/apache/beam/operators/beam.py

--- airflow/providers/apache/beam/operators/beam.py
+++ airflow/providers/apache/beam/operators/beam.py
@@ -301,7 +301,7 @@ class BeamRunPythonPipelineOperator(BeamBasePipelineOperator):
             if snake_case_pipeline_options.get("requirements_file", "").startswith("gs://"):
                 tmp_req_file = exit_stack.enter_context(
                     gcs_hook.provide_file(object_url=snake_case_pipeline_options["requirements_file"])
-                 )
+                )
                 snake_case_pipeline_options["requirements_file"] = tmp_req_file.name
 
             if is_dataflow and self.dataflow_hook:
