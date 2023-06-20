f5cd910cb74275b79699d434d51eeefd2acfb86f
darkag <darkag@free.fr>
2023-06-20 19:19:21 +0200

    Add binding to all options
    Add binding from extra to vertica connection parameters

--- airflow/providers/vertica/hooks/vertica.py
+++ airflow/providers/vertica/hooks/vertica.py
@@ -51,6 +51,45 @@ class VerticaHook(DbApiHook):
             
         if conn.extra_dejson.get("session_label", False):
             conn_config["session_label"] = conn.extra_dejson["session_label"]
+
+        if conn.extra_dejson.get("backup_server_node", False):
+            conn_config["backup_server_node"] = conn.extra_dejson["backup_server_node"]
+
+        if conn.extra_dejson.get("binary_transfer", False):
+            conn_config["binary_transfer"] = bool(conn.extra_dejson["binary_transfer"])
+
+        if conn.extra_dejson.get("connection_timeout", False):
+            conn_config["connection_timeout"] = int(conn.extra_dejson["connection_timeout"])
+
+        if conn.extra_dejson.get("disable_copy_local", False):
+            conn_config["disable_copy_local"] = bool(conn.extra_dejson["disable_copy_local"])
+
+        if conn.extra_dejson.get("kerberos_host_name", False):
+            conn_config["kerberos_host_name"] = conn.extra_dejson["kerberos_host_name"]
+
+        if conn.extra_dejson.get("kerberos_service_name", False):
+            conn_config["kerberos_service_name"] = conn.extra_dejson["kerberos_service_name"]
+
+        if conn.extra_dejson.get("log_level", False):
+            conn_config["log_level"] = conn.extra_dejson["log_level"]
             
+        if conn.extra_dejson.get("log_path", False):
+            conn_config["log_path"] = conn.extra_dejson["log_path"]
+
+        if conn.extra_dejson.get("request_complex_types", False):
+            conn_config["request_complex_types"] = bool(conn.extra_dejson["request_complex_types"])
+
+        if conn.extra_dejson.get("use_prepared_statements", False):
+            conn_config["use_prepared_statements"] = bool(conn.extra_dejson["use_prepared_statements"])
+
+        if conn.extra_dejson.get("unicode_error", False):
+            conn_config["unicode_error"] = conn.extra_dejson["unicode_error"]
+
+        if conn.extra_dejson.get("workload", False):
+            conn_config["workload"] = conn.extra_dejson["workload"]
+
+        if conn.extra_dejson.get("ssl", False):
+            conn_config["ssl"] = conn.extra_dejson["ssl"]
+        
         conn = connect(**conn_config)
         return conn
