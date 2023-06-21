b39b57b8bb5099e324165ffccd392d0f2e22a3fe
utkarsh sharma <utkarsharma2@gmail.com>
2023-06-21 09:30:47 +0530

    Fix tests

--- tests/providers/snowflake/triggers/test_snowflake.py
+++ tests/providers/snowflake/triggers/test_snowflake.py
@@ -57,13 +57,13 @@ class TestSnowflakeSqlApiTrigger:
         }
 
     @pytest.mark.asyncio
-    @mock.patch(f"{MODULE}.triggers.snowflake_trigger.SnowflakeSqlApiTrigger.is_still_running")
+    @mock.patch(f"{MODULE}.triggers.snowflake_trigger.SnowflakeSqlApiTrigger.get_query_status")
     @mock.patch(f"{MODULE}.hooks.snowflake_sql_api.SnowflakeSqlApiHook.get_sql_api_query_status_async")
     async def test_snowflake_sql_trigger_running(
-        self, mock_get_sql_api_query_status_async, mock_is_still_running
+        self, mock_get_sql_api_query_status_async, mock_get_query_status
     ):
-        """Tests that the SnowflakeSqlApiTrigger in running by mocking is_still_running to true"""
-        mock_is_still_running.return_value = True
+        """Tests that the SnowflakeSqlApiTrigger in running by mocking get_query_status to true"""
+        mock_get_query_status.return_value = {"status": "running"}
 
         task = asyncio.create_task(self.TRIGGER.run().__anext__())
         await asyncio.sleep(0.5)
@@ -73,16 +73,16 @@ class TestSnowflakeSqlApiTrigger:
         asyncio.get_event_loop().stop()
 
     @pytest.mark.asyncio
-    @mock.patch(f"{MODULE}.triggers.snowflake_trigger.SnowflakeSqlApiTrigger.is_still_running")
+    @mock.patch(f"{MODULE}.triggers.snowflake_trigger.SnowflakeSqlApiTrigger.get_query_status")
     @mock.patch(f"{MODULE}.hooks.snowflake_sql_api.SnowflakeSqlApiHook.get_sql_api_query_status_async")
     async def test_snowflake_sql_trigger_completed(
-        self, mock_get_sql_api_query_status_async, mock_is_still_running
+        self, mock_get_sql_api_query_status_async, mock_get_query_status
     ):
         """
         Test SnowflakeSqlApiTrigger run method with success status and mock the get_sql_api_query_status
-         result and  is_still_running to False.
+         result and  get_query_status to False.
         """
-        mock_is_still_running.return_value = False
+        mock_get_query_status.return_value = {"status": "success", "statement_handles": ["uuid", "uuid1"]}
         statement_query_ids = ["uuid", "uuid1"]
         mock_get_sql_api_query_status_async.return_value = {
             "message": "Statement executed successfully.",
@@ -95,13 +95,9 @@ class TestSnowflakeSqlApiTrigger:
         assert TriggerEvent({"status": "success", "statement_query_ids": statement_query_ids}) == actual
 
     @pytest.mark.asyncio
-    @mock.patch(f"{MODULE}.triggers.snowflake_trigger.SnowflakeSqlApiTrigger.is_still_running")
     @mock.patch(f"{MODULE}.hooks.snowflake_sql_api.SnowflakeSqlApiHook.get_sql_api_query_status_async")
-    async def test_snowflake_sql_trigger_failure_status(
-        self, mock_get_sql_api_query_status_async, mock_is_still_running
-    ):
+    async def test_snowflake_sql_trigger_failure_status(self, mock_get_sql_api_query_status_async):
         """Test SnowflakeSqlApiTrigger task is executed and triggered with failure status."""
-        mock_is_still_running.return_value = False
         mock_response = {
             "status": "error",
             "message": "An error occurred when executing the statement. Check "
@@ -114,33 +110,11 @@ class TestSnowflakeSqlApiTrigger:
         assert TriggerEvent(mock_response) == actual
 
     @pytest.mark.asyncio
-    @mock.patch(f"{MODULE}.triggers.snowflake_trigger.SnowflakeSqlApiTrigger.is_still_running")
     @mock.patch(f"{MODULE}.hooks.snowflake_sql_api.SnowflakeSqlApiHook.get_sql_api_query_status_async")
-    async def test_snowflake_sql_trigger_exception(
-        self, mock_get_sql_api_query_status_async, mock_is_still_running
-    ):
+    async def test_snowflake_sql_trigger_exception(self, mock_get_sql_api_query_status_async):
         """Tests the SnowflakeSqlApiTrigger does not fire if there is an exception."""
-        mock_is_still_running.return_value = False
         mock_get_sql_api_query_status_async.side_effect = Exception("Test exception")
 
         task = [i async for i in self.TRIGGER.run()]
         assert len(task) == 1
         assert TriggerEvent({"status": "error", "message": "Test exception"}) in task
-
-    @pytest.mark.asyncio
-    @pytest.mark.parametrize(
-        "mock_response, expected_status",
-        [
-            ({"status": "success"}, False),
-            ({"status": "error"}, False),
-            ({"status": "running"}, True),
-        ],
-    )
-    @mock.patch(f"{MODULE}.hooks.snowflake_sql_api.SnowflakeSqlApiHook.get_sql_api_query_status_async")
-    async def test_snowflake_sql_trigger_is_still_running(
-        self, mock_get_sql_api_query_status_async, mock_response, expected_status
-    ):
-        mock_get_sql_api_query_status_async.return_value = mock_response
-
-        response = await self.TRIGGER.is_still_running(["uuid"])
-        assert response == expected_status
