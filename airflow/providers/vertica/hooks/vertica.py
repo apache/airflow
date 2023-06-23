60da8c355bf6e8d9cb97fb0728ffb34994b95f9f
darkag <darkag@free.fr>
2023-06-23 21:22:41 +0200

    Update vertica.py
    Simplification of code, better handling of log level parameter, correction for "request_complex_types" which is true by default (it would have been impossible to pass false with the previous version)

--- airflow/providers/vertica/hooks/vertica.py
+++ airflow/providers/vertica/hooks/vertica.py
@@ -46,50 +46,57 @@ class VerticaHook(DbApiHook):
         else:
             conn_config["port"] = int(conn.port)
             
-        if conn.extra_dejson.get("connection_load_balance", False):
-            conn_config["connection_load_balance"] = bool(conn.extra_dejson["connection_load_balance"])
+        conn_extra = conn.extra_dejson        
+        if "connection_load_balance" in conn_extra:
+            conn_config["connection_load_balance"] = bool(conn_extra["connection_load_balance"])
             
-        if conn.extra_dejson.get("session_label", False):
-            conn_config["session_label"] = conn.extra_dejson["session_label"]
+        conn_config["session_label"] = conn_extra.get("session_label")
+        conn_config["backup_server_node"] = conn_extra.get("backup_server_node")
 
-        if conn.extra_dejson.get("backup_server_node", False):
-            conn_config["backup_server_node"] = conn.extra_dejson["backup_server_node"]
+        if "binary_transfer" in conn_extra:
+            conn_config["binary_transfer"] = bool(conn_extra["binary_transfer"])
 
-        if conn.extra_dejson.get("binary_transfer", False):
-            conn_config["binary_transfer"] = bool(conn.extra_dejson["binary_transfer"])
+        if "connection_timeout" in conn_extra:
+            conn_config["connection_timeout"] = int(conn_extra["connection_timeout"])
 
-        if conn.extra_dejson.get("connection_timeout", False):
-            conn_config["connection_timeout"] = int(conn.extra_dejson["connection_timeout"])
+        if "disable_copy_local" in conn_extra:
+            conn_config["disable_copy_local"] = bool(conn_extra["disable_copy_local"])
 
-        if conn.extra_dejson.get("disable_copy_local", False):
-            conn_config["disable_copy_local"] = bool(conn.extra_dejson["disable_copy_local"])
+        conn_config["kerberos_host_name"] = conn_extra.get("kerberos_host_name")
+        conn_config["kerberos_service_name"] = conn_extra.get("kerberos_service_name")
 
-        if conn.extra_dejson.get("kerberos_host_name", False):
-            conn_config["kerberos_host_name"] = conn.extra_dejson["kerberos_host_name"]
-
-        if conn.extra_dejson.get("kerberos_service_name", False):
-            conn_config["kerberos_service_name"] = conn.extra_dejson["kerberos_service_name"]
-
-        if conn.extra_dejson.get("log_level", False):
-            conn_config["log_level"] = conn.extra_dejson["log_level"]
+        if "log_level" in conn_extra:
+            import logging
+            log_lvl = conn_extra["log_level"]
+            if isinstance(log_lvl, str):
+                log_lvl = log_lvl.lower()
+                if log_lvl == "critical":
+                    conn_config["log_level"] = logging.CRITICAL
+                elif log_lvl == "error":
+                    conn_config["log_level"] = logging.ERROR
+                elif log_lvl == "warning":
+                    conn_config["log_level"] = logging.WARNING
+                elif log_lvl == "info":
+                    conn_config["log_level"] = logging.INFO
+                elif log_lvl == "debug":
+                    conn_config["log_level"] = logging.DEBUG
+                elif log_lvl == "notset":
+                    conn_config["log_level"] = logging.NOTSET
+            else:
+                conn_config["log_level"] = int(conn_extra["log_level"])
             
-        if conn.extra_dejson.get("log_path", False):
-            conn_config["log_path"] = conn.extra_dejson["log_path"]
+        if "log_path" in conn_extra:
+            conn_config["log_path"] = conn_extra["log_path"]
 
-        if conn.extra_dejson.get("request_complex_types", False):
-            conn_config["request_complex_types"] = bool(conn.extra_dejson["request_complex_types"])
+        if "request_complex_types" in conn_extra:
+            conn_config["request_complex_types"] = bool(conn_extra["request_complex_types"])
 
-        if conn.extra_dejson.get("use_prepared_statements", False):
-            conn_config["use_prepared_statements"] = bool(conn.extra_dejson["use_prepared_statements"])
+        if "use_prepared_statements" in conn_extra:
+            conn_config["use_prepared_statements"] = bool(conn_extra["use_prepared_statements"])
 
-        if conn.extra_dejson.get("unicode_error", False):
-            conn_config["unicode_error"] = conn.extra_dejson["unicode_error"]
-
-        if conn.extra_dejson.get("workload", False):
-            conn_config["workload"] = conn.extra_dejson["workload"]
-
-        if conn.extra_dejson.get("ssl", False):
-            conn_config["ssl"] = conn.extra_dejson["ssl"]
+        conn_config["unicode_error"] = conn_extra.get("unicode_error")
+        conn_config["workload"] = conn_extra.get("workload")
+        conn_config["ssl"] = conn_extra.get("ssl")
         
         conn = connect(**conn_config)
         return conn
