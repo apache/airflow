a3bbefea80bec5c0f69df393fdf580636daa630f
Daniel Standish <15932138+dstandish@users.noreply.github.com>
2023-06-26 14:48:34 -0700

    fix test

--- tests/utils/test_task_group.py
+++ tests/utils/test_task_group.py
@@ -24,7 +24,8 @@ import pytest
 
 from airflow.decorators import dag, task_group as task_group_decorator
 from airflow.exceptions import TaskAlreadyInTaskGroup
-from airflow.models.baseoperator import DAG, BaseOperator
+from airflow.models.baseoperator import BaseOperator
+from airflow.models.dag import DAG
 from airflow.models.xcom_arg import XComArg
 from airflow.operators.bash import BashOperator
 from airflow.operators.empty import EmptyOperator
