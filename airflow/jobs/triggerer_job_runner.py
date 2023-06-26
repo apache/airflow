3aeb2ca271fe92efba42ed83c1d4c52d6300736c
Artur Piotr Izaak Laskowski <artur.pi.laskowski@gmail.com>
2023-06-26 20:17:27 +0200

    Update airflow/jobs/triggerer_job_runner.py
    Co-authored-by: D. Ferruzzi <ferruzzi@amazon.com>

--- airflow/jobs/triggerer_job_runner.py
+++ airflow/jobs/triggerer_job_runner.py
@@ -402,6 +402,7 @@ class TriggererJobRunner(BaseJobRunner["Job | JobPydantic"], LoggingMixin):
 
     def emit_metrics(self):
         Stats.gauge(f"triggers.running.{self.job.hostname}", len(self.trigger_runner.triggers))
+        Stats.gauge(f"triggers.running", len(self.trigger_runner.triggers), tags={"hostname":self.job.hostname})
 
 
 class TriggerDetails(TypedDict):
