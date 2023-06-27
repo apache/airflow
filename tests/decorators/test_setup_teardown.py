e5f93ed82a00de260dae373e81af13d08bbbe089
Ephraim Anierobi <splendidzigy24@gmail.com>
2023-06-27 08:50:21 +0100

    correctly import AirflowException

--- tests/decorators/test_setup_teardown.py
+++ tests/decorators/test_setup_teardown.py
@@ -19,9 +19,9 @@ from __future__ import annotations
 
 import pytest
 
-from airflow import AirflowException
 from airflow.decorators import setup, task, task_group, teardown
 from airflow.decorators.setup_teardown import context_wrapper
+from airflow.exceptions import AirflowException
 from airflow.operators.bash import BashOperator
 
 
