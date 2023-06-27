215a031f611d04d551ca5f95bb52496b4d5bc123
Artur Piotr Izaak Laskowski <apilaskowski@google.com>
2023-06-27 19:37:06 +0200

    Add hostname to triggerer metric [triggers.running] (#32050)
    ---------
    
    Co-authored-by: D. Ferruzzi <ferruzzi@amazon.com>

--- airflow/jobs/triggerer_job_runner.py
+++ airflow/jobs/triggerer_job_runner.py
@@ -401,7 +401,10 @@ class TriggererJobRunner(BaseJobRunner["Job | JobPydantic"], LoggingMixin):
             Stats.incr("triggers.failed")
 
     def emit_metrics(self):
-        Stats.gauge("triggers.running", len(self.trigger_runner.triggers))
+        Stats.gauge(f"triggers.running.{self.job.hostname}", len(self.trigger_runner.triggers))
+        Stats.gauge(
+            "triggers.running", len(self.trigger_runner.triggers), tags={"hostname": self.job.hostname}
+        )
 
 
 class TriggerDetails(TypedDict):
