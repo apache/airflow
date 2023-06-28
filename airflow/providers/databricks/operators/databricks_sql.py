84740e1db9bbe5e1e26b766e2d4b75af0c9b8b0b
ferruzzi <ferruzzi@amazon.com>
2023-06-28 09:27:21 -0700

    D205 Support - Providers: Databricks to Github (inclusive)

--- airflow/providers/databricks/operators/databricks_sql.py
+++ airflow/providers/databricks/operators/databricks_sql.py
@@ -172,6 +172,7 @@ COPY_INTO_APPROVED_FORMATS = ["CSV", "JSON", "AVRO", "ORC", "PARQUET", "TEXT", "
 class DatabricksCopyIntoOperator(BaseOperator):
     """
     Executes COPY INTO command in a Databricks SQL endpoint or a Databricks cluster.
+
     COPY INTO command is constructed from individual pieces, that are described in
     `documentation <https://docs.databricks.com/sql/language-manual/delta-copy-into.html>`_.
 
