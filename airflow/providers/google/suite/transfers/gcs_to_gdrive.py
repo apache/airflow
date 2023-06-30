8348e1f50fb14db40ccf0f2c0886d52d05f3627d
Pat Leamon <patrick@redbubble.com>
2023-06-30 10:23:14 +1000

    Fixup: remove reference to source object In this example the name of the source object is irrelevant

--- airflow/providers/google/suite/transfers/gcs_to_gdrive.py
+++ airflow/providers/google/suite/transfers/gcs_to_gdrive.py
@@ -64,8 +64,8 @@ class GCSToGoogleDriveOperator(BaseOperator):
         which case the copied file will be named ``blah/foo/baz``.
     :param destination_folder_id: The folder ID where the destination objects will be placed.  It is
         an additive prefix for anything specified in destination_object.
-        For example, if folder ID ``xXyYzZ`` is called ``foo``, destination is ``bar/baz`` and source object
-         is ``qux``, the file will end up in `foo/bar/baz`.
+        For example if folder ID ``xXyYzZ`` is called ``foo`` and the destination is ``bar/baz``, the file
+        will end up in `foo/bar/baz`.
         This can be used to target an existing folder that is already visible to other users.  The credentials
         provided must have access to this folder.
     :param move_object: When move object is True, the object is moved instead of copied to the new location.
