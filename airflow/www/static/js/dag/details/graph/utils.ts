00bf2ab2191fd7ebce34583007a4c06b7561b360
Brent Bovenzi <brent@astronomer.io>
2023-07-02 11:47:29 -0400

    Use reactflow for datasets graph (#31775)

--- airflow/www/static/js/dag/details/graph/utils.ts
+++ airflow/www/static/js/dag/details/graph/utils.ts
@@ -23,8 +23,7 @@ import type { ElkExtendedEdge } from "elkjs";
 
 import type { SelectionProps } from "src/dag/useSelection";
 import { getTask } from "src/utils";
-import type { Task, TaskInstance } from "src/types";
-import type { NodeType } from "src/datasets/Graph/Node";
+import type { Task, TaskInstance, NodeType } from "src/types";
 
 import type { CustomNodeProps } from "./Node";
 
