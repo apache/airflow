0150c188ecf4d7f6c5a66623e240c9f2ccb1b6ed
Wei Lee <weilee.rx@gmail.com>
2023-07-04 13:01:18 +0800

    feat(providers/azure): consider the case that cancel_pipeline_run might raise an exception

--- airflow/providers/microsoft/azure/triggers/data_factory.py
+++ airflow/providers/microsoft/azure/triggers/data_factory.py
@@ -222,10 +222,13 @@ class AzureDataFactoryTrigger(BaseTrigger):
                 )
         except Exception as e:
             if self.run_id:
-                await hook.cancel_pipeline_run(
-                    run_id=self.run_id,
-                    resource_group_name=self.resource_group_name,
-                    factory_name=self.factory_name,
-                )
-                self.log.info("Unexpected error %s caught. Cancel pipeline run %s", str(e), self.run_id)
+                try:
+                    await hook.cancel_pipeline_run(
+                        run_id=self.run_id,
+                        resource_group_name=self.resource_group_name,
+                        factory_name=self.factory_name,
+                    )
+                    self.log.info("Unexpected error %s caught. Cancel pipeline run %s", str(e), self.run_id)
+                except Exception as err:
+                    yield TriggerEvent({"status": "error", "message": str(err), "run_id": self.run_id})
             yield TriggerEvent({"status": "error", "message": str(e), "run_id": self.run_id})
