4c3e8cda1932baf69cda7099ea40b744a94f2ccb
adaverse <adaverse@LAPTOP-JD3LRTNF>
2023-07-10 11:01:33 +0530

    name fix

--- airflow/providers/sftp/decorators/sensors/sftp.py
+++ airflow/providers/sftp/decorators/sensors/sftp.py
@@ -23,7 +23,7 @@ from airflow.decorators.base import TaskDecorator, get_unique_task_id, task_deco
 from airflow.providers.sftp.sensors.sftp import SFTPSensor
 
 
-class _DecoratedSFTPSensor(SFTPSensor):
+class _DecoratedSftpSensor(SFTPSensor):
     """
     Wraps a Python callable and captures args/kwargs when called for execution.
 
@@ -67,6 +67,6 @@ def sftp_sensor_task(python_callable: Callable | None = None, **kwargs) -> TaskD
     return task_decorator_factory(
         python_callable=python_callable,
         multiple_outputs=False,
-        decorated_operator_class=_DecoratedSFTPSensor,
+        decorated_operator_class=_DecoratedSftpSensor,
         **kwargs,
     )
