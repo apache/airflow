bb19a3318248eff7c0c10f6adb790c255c75d3db
Brenda Pham <bloop@google.com>
2023-07-12 20:47:20 +0000

    Raise exception if data_pipeline_name not given

--- airflow/providers/google/cloud/operators/datapipeline.py
+++ airflow/providers/google/cloud/operators/datapipeline.py
@@ -118,6 +118,11 @@ class RunDataPipelineOperator(GoogleCloudBaseOperator):
 
     def execute(self, context: Context):
         self.data_pipeline_hook = DataPipelineHook(gcp_conn_id=self.gcp_conn_id)
+        
+        if self.data_pipeline_name is None:
+            raise AirflowException(
+                "Data Pipeline name not given; cannot run unspecified pipeline."
+            )
 
 
         if self.data_pipeline_name is None:
