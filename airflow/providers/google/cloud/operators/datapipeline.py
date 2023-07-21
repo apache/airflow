b6f57266786e1eff2cc69071279a35d60be9e9bc
Brenda Pham <bloop@google.com>
2023-07-21 23:42:02 +0000

    update

--- airflow/providers/google/cloud/operators/datapipeline.py
+++ airflow/providers/google/cloud/operators/datapipeline.py
@@ -16,7 +16,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-"""This module contains Google DataPipelines operators."""
+"""This module contains Google Data Pipelines operators."""
 from __future__ import annotations
 
 import copy
