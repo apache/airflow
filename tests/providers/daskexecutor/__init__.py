bfafbe513a2f08f754aecc8e709c10fb21306d93
Jarek Potiuk <jarek@potiuk.com>
2023-07-22 13:44:07 +0200

    Extract Dask executor to new daskexecutor provider

--- tests/providers/daskexecutor/__init__.py
+++ tests/providers/daskexecutor/__init__.py
@@ -14,23 +14,3 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-"""Executors."""
-from __future__ import annotations
-
-from airflow.utils.deprecation_tools import add_deprecated_classes
-
-__deprecated_classes = {
-    "celery_executor": {
-        "app": "airflow.providers.celery.executors.celery_executor_utils.app",
-        "CeleryExecutor": "airflow.providers.celery.executors.celery_executor.CeleryExecutor",
-    },
-    "celery_kubernetes_executor": {
-        "CeleryKubernetesExecutor": "airflow.providers.celery.executors."
-        "celery_kubernetes_executor.CeleryKubernetesExecutor",
-    },
-    "dask_executor": {
-        "DaskExecutor": "airflow.providers.daskexecutor.executors.dask_executor.DaskExecutor",
-    },
-}
-
-add_deprecated_classes(__deprecated_classes, __name__)
