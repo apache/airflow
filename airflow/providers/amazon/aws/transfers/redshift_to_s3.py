4d211c0953b4f4c9e42df6e6ea1035bb5064906e
Ashwaq Reheman <ashwaq@MSI>
2023-07-28 03:38:08 +0530

    Fix handling of single quotes in RedshiftToS3Operator

--- airflow/providers/amazon/aws/transfers/redshift_to_s3.py
+++ airflow/providers/amazon/aws/transfers/redshift_to_s3.py
@@ -142,7 +142,7 @@ class RedshiftToS3Operator(BaseOperator):
         self, credentials_block: str, select_query: str, s3_key: str, unload_options: str
     ) -> str:
         return f"""
-                    UNLOAD ('{select_query}')
+                    UNLOAD ($${select_query}$$)
                     TO 's3://{self.s3_bucket}/{s3_key}'
                     credentials
                     '{credentials_block}'
