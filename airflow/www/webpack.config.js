045d907e642af930f5d7dc70e6044296fb87dde5
Brent Bovenzi <brent.bovenzi@gmail.com>
2023-07-28 17:01:29 +0800

    remove old graph view

--- airflow/www/webpack.config.js
+++ airflow/www/webpack.config.js
@@ -65,7 +65,6 @@ const config = {
     dagDependencies: `${JS_DIR}/dag_dependencies.js`,
     dags: [`${CSS_DIR}/dags.css`, `${JS_DIR}/dags.js`],
     flash: `${CSS_DIR}/flash.css`,
-    graph: [`${CSS_DIR}/graph.css`, `${JS_DIR}/graph.js`],
     loadingDots: `${CSS_DIR}/loading-dots.css`,
     main: [`${CSS_DIR}/main.css`, `${JS_DIR}/main.js`],
     materialIcons: `${CSS_DIR}/material-icons.css`,
