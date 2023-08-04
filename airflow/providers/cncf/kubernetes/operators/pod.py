d31c77510cc9141011c65c513d9f07580c639717
Hussein Awala <hussein@awala.fr>
2023-08-04 20:17:59 +0200

    Fix waiting the base container when reading the logs of other containers (#33092)
    * Fix waiting the base container when reading the logs of other containers
    
    * add condition for await_container_completion to fix unit tests

--- airflow/providers/cncf/kubernetes/operators/pod.py
+++ airflow/providers/cncf/kubernetes/operators/pod.py
@@ -590,7 +590,9 @@ class KubernetesPodOperator(BaseOperator):
                     container_logs=self.container_logs,
                     follow_logs=True,
                 )
-            else:
+            if not self.get_logs or (
+                self.container_logs is not True and self.base_container_name not in self.container_logs
+            ):
                 self.pod_manager.await_container_completion(
                     pod=self.pod, container_name=self.base_container_name
                 )
