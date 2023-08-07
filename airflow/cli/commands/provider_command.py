99aaacbd64c40dbbb825cb2cf9d1db911d5ce180
Miroslav Šedivý <6774676+eumiro@users.noreply.github.com>
2023-08-07 18:52:13 +0000

    Refactor: Simplify dict manipulation in airflow/cli (#33159)
    (cherry picked from commit c074a1ecd946b2ad0f85be62aa977c269e2ffb83)

--- airflow/cli/commands/provider_command.py
+++ airflow/cli/commands/provider_command.py
@@ -106,7 +106,7 @@ def triggers_list(args):
 def connection_form_widget_list(args):
     """Lists all custom connection form fields at the command line."""
     AirflowConsole().print_as(
-        data=list(sorted(ProvidersManager().connection_form_widgets.items())),
+        data=sorted(ProvidersManager().connection_form_widgets.items()),
         output=args.output,
         mapper=lambda x: {
             "connection_parameter_name": x[0],
@@ -122,7 +122,7 @@ def connection_form_widget_list(args):
 def connection_field_behaviours(args):
     """Lists field behaviours."""
     AirflowConsole().print_as(
-        data=list(ProvidersManager().field_behaviours.keys()),
+        data=list(ProvidersManager().field_behaviours),
         output=args.output,
         mapper=lambda x: {
             "field_behaviours": x,
