741fb27f50a9fee7fca776abf24c32e22783aa03
Maham Ali <50532268+mahammi@users.noreply.github.com>
2023-08-08 13:29:50 -0700

    Implement EventBridge enable and disable rule operators (#33226)
    Co-authored-by: Maham Ali <maahaam@amazon.com>

--- tests/providers/amazon/aws/hooks/test_eventbridge.py
+++ tests/providers/amazon/aws/hooks/test_eventbridge.py
@@ -30,10 +30,18 @@ class TestEventBridgeHook:
 
     def test_put_rule(self):
         hook = EventBridgeHook(aws_conn_id="aws_default")
-        response = hook.put_rule(name="test", event_pattern='{"source": ["aws.s3"]}', state="ENABLED")
+        response = hook.put_rule(
+            name="test",
+            event_pattern='{"source": ["aws.s3"]}',
+            state="ENABLED",
+        )
         assert "RuleArn" in response
 
     def test_put_rule_with_bad_json_fails(self):
         hook = EventBridgeHook(aws_conn_id="aws_default")
         with pytest.raises(ValueError):
-            hook.put_rule(name="test", event_pattern="invalid json", state="ENABLED")
+            hook.put_rule(
+                name="test",
+                event_pattern="invalid json",
+                state="ENABLED",
+            )
