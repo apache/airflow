2ff8d4d6133300b1c8017ed8f5d9e981921b95c9
Jarek Potiuk <jarek@potiuk.com>
2023-08-09 23:00:21 +0200

    Change type of defualts for include_deferred (#33271)
    We should change the defaults to client defaults for MSSQL compatibility
    The change in #32709 was not liked by MSSQL
    
    Co-authored-by: Ephraim Anierobi <splendidzigy24@gmail.com>
    (cherry picked from commit d989e9dba5899e87780df9a8c5994ed1e3f8a776)

--- airflow/migrations/versions/0128_2_7_0_add_include_deferred_column_to_pool.py
+++ airflow/migrations/versions/0128_2_7_0_add_include_deferred_column_to_pool.py
@@ -39,9 +39,7 @@ airflow_version = "2.7.0"
 def upgrade():
     """Apply add include_deferred column to pool"""
     with op.batch_alter_table("slot_pool") as batch_op:
-        batch_op.add_column(
-            sa.Column("include_deferred", sa.Boolean, nullable=False, server_default=sa.false())
-        )
+        batch_op.add_column(sa.Column("include_deferred", sa.Boolean, default=False))
 
 
 def downgrade():
