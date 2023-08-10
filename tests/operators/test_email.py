8420a26aec616de3e10e942d377ad0ec3d001835
Shakeel Ansari <Shakeel.Ansari@Gmail.com>
2023-08-10 22:15:06 +0530

    Updated test_email for verifying from_email param

--- tests/operators/test_email.py
+++ tests/operators/test_email.py
@@ -55,10 +55,46 @@ class TestEmailOperator:
         task.run(start_date=DEFAULT_DATE, end_date=DEFAULT_DATE)
         self.dag.clear()
 
+    def _run_as_operator_with_from(self, **kwargs):
+        task = EmailOperator(
+            to="airflow@example.com",
+            subject="Test Run",
+            html_content="The quick brown fox jumps over the lazy dog",
+            task_id="task2",
+            dag=self.dag,
+            files=["/tmp/Report-A-{{ ds }}.csv"],
+            custom_headers={"Reply-To": "reply_to@example.com"},
+            from_email="noreply@example.com",
+            **kwargs,
+        )
+        task.run(start_date=DEFAULT_DATE, end_date=DEFAULT_DATE)
+        self.dag.clear()
+
     def test_execute(self):
-        with conf_vars({("email", "email_backend"): "tests.operators.test_email.send_email_test"}):
+        with conf_vars(
+            {
+                ("email", "email_backend"): "tests.operators.test_email.send_email_test",
+                ("email", "from_email"): "noreply-config@example.com",
+            }
+        ):
             self._run_as_operator()
         assert send_email_test.call_count == 1
         call_args = send_email_test.call_args.kwargs
         assert call_args["files"] == ["/tmp/Report-A-2016-01-01.csv"]
         assert call_args["custom_headers"] == {"Reply-To": "reply_to@example.com"}
+        # When there is no email provided, it should get the from_email from config as usual
+        assert call_args["from_email"] == "noreply-config@example.com"
+
+        # Add test for from email
+        with conf_vars(
+            {
+                ("email", "email_backend"): "tests.operators.test_email.send_email_test",
+                ("email", "from_email"): "noreply-config@example.com",
+            }
+        ):
+            self._run_as_operator_with_from()
+        assert send_email_test.call_count == 2
+        call_args = send_email_test.call_args.kwargs
+        # When Email is provided, it should pick the provided email
+        assert call_args["from_email"] == "noreply@example.com"
+        assert call_args["from_email"] != "noreply-config@example.com"
