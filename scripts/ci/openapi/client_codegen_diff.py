7c5a2fe6501424a178c739eb72f8589aa23ff225
royno <royno@wix.com>
2023-08-13 20:14:53 +0300

    ci

--- scripts/ci/openapi/client_codegen_diff.py
+++ scripts/ci/openapi/client_codegen_diff.py
@@ -1,8 +1,26 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+from __future__ import annotations
+
 import subprocess
 
 
 def run_command(commands: list):
-    result = subprocess.run(commands, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    result = subprocess.run(commands, check=True, capture_output=True, text=True)
     if result.returncode != 0:
         print(f"Error running command: {result.stderr}")
         exit(1)
@@ -11,14 +29,8 @@ def run_command(commands: list):
 
 # HEAD^1 says the "first" parent. For PR merge commits, or main commits, this is the "right" commit.
 #
-# In this example, 9c532b6 is the PR commit (HEAD^2), 4840892 is the head GitHub checks-out for us, and db121f7 is the
-# "merge target" (HEAD^1) -- i.e. mainline
-#
-# *   4840892 (HEAD, pull/11906/merge) Merge 9c532b6a2c56cd5d4c2a80ecbed60f9dfd1f5fe6 into db121f726b3c7a37aca1ea05eb4714f884456005 [Ephraim Anierobi]
-# |\
-# | * 9c532b6 (grafted) make taskinstances pid and duration nullable [EphraimBuddy]
-# * db121f7 (grafted) Add truncate table (before copy) option to S3ToRedshiftOperator (#9246) [JavierLopezT]
-
+# In this example, 9c532b6 is the PR commit (HEAD^2), 4840892 is the head GitHub checks-out for us,
+# and db121f7 is the "merge target" (HEAD^1) -- i.e. mainline
 
 def client_codegen_diff():
     previous_mainline_commit = run_command(["git", "rev-parse", "--short", "HEAD^1"]).strip()
@@ -43,5 +55,5 @@ def client_codegen_diff():
     run_command(["diff", "-u", f"{GO_TARGET_CLIENT_PATH}", f"{GO_CLIENT_PATH}"])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     client_codegen_diff()
