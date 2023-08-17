1421ca9cfedc7e04f6d78f85bd170dce8560f6fd
Brenda Pham <blpham21@gmail.com>
2023-08-17 12:37:40 -0700

    merge conflicts, hooks

--- airflow/providers/google/cloud/hooks/datapipeline.py
+++ airflow/providers/google/cloud/hooks/datapipeline.py
@@ -52,9 +52,37 @@ class DataPipelineHook(GoogleBaseHook):
         http_authorized = self._authorize()
         return build("datapipelines", "v1", http=http_authorized, cache_discovery=False)
 
-    @staticmethod
-    def build_parent_name(project_id: str, location: str):
-        return f"projects/{project_id}/locations/{location}"
+    @GoogleBaseHook.fallback_to_default_project_id
+    def create_data_pipeline(
+        self,
+        body: dict,
+        project_id: str,
+        location: str = DEFAULT_DATAPIPELINE_LOCATION,
+    ) -> None:
+        """
+        Creates a new Data Pipelines instance from the Data Pipelines API.
+
+        :param body: The request body (contains instance of Pipeline). See:
+            https://cloud.google.com/dataflow/docs/reference/data-pipelines/rest/v1/projects.locations.pipelines/create#request-body
+        :param project_id: The ID of the GCP project that owns the job.
+        :param location: The location to direct the Data Pipelines instance to (for example us-central1).
+
+        Returns the created Data Pipelines instance in JSON representation.
+        """
+        parent = self.build_parent_name(project_id, location)
+        service = self.get_conn()
+        self.log.info(dir(service.projects().locations()))
+        request = (
+            service.projects()
+            .locations()
+            .pipelines()
+            .create(
+                parent=parent,
+                body=body,
+            )
+        )
+        response = request.execute(num_retries=self.num_retries)
+        return response
 
     @GoogleBaseHook.fallback_to_default_project_id
     def run_data_pipeline(
@@ -86,3 +114,7 @@ class DataPipelineHook(GoogleBaseHook):
         )
         response = request.execute(num_retries=self.num_retries)
         return response
+
+    @staticmethod
+    def build_parent_name(project_id: str, location: str):
+        return f"projects/{project_id}/locations/{location}"
