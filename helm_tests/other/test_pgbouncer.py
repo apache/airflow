2c0fa0c93e550e74f91e7faf45b3d481e179c9e7
Mikaël Ducharme <mducharme@mozilla.com>
2023-08-18 14:46:43 -0400

    Chart: Add existing secret support for PGBouncer metrics exporter (#32724)

--- helm_tests/other/test_pgbouncer.py
+++ helm_tests/other/test_pgbouncer.py
@@ -568,6 +568,78 @@ class TestPgbouncerExporter:
             "/pgbouncer?sslmode=require" == connection
         )
 
+    def test_no_existing_secret(self):
+        docs = render_chart(
+            "test-pgbouncer-stats",
+            values={
+                "pgbouncer": {"enabled": True},
+            },
+            show_only=["templates/pgbouncer/pgbouncer-deployment.yaml"],
+        )
+
+        assert {
+            "name": "test-pgbouncer-stats-pgbouncer-stats",
+            "key": "connection",
+        } == jmespath.search("spec.template.spec.containers[1].env[0].valueFrom.secretKeyRef", docs[0])
+
+    def test_existing_secret(self):
+        docs = render_chart(
+            "test-pgbouncer-stats",
+            values={
+                "pgbouncer": {
+                    "enabled": True,
+                    "metricsExporterSidecar": {
+                        "statsSecretName": "existing-stats-secret",
+                    },
+                },
+            },
+            show_only=["templates/pgbouncer/pgbouncer-deployment.yaml"],
+        )
+
+        assert {
+            "name": "existing-stats-secret",
+            "key": "connection",
+        } == jmespath.search("spec.template.spec.containers[1].env[0].valueFrom.secretKeyRef", docs[0])
+
+    def test_existing_secret_existing_key(self):
+        docs = render_chart(
+            "test-pgbouncer-stats",
+            values={
+                "pgbouncer": {
+                    "enabled": True,
+                    "metricsExporterSidecar": {
+                        "statsSecretName": "existing-stats-secret",
+                        "statsSecretKey": "exisiting-stats-secret-key",
+                    },
+                },
+            },
+            show_only=["templates/pgbouncer/pgbouncer-deployment.yaml"],
+        )
+
+        assert {
+            "name": "existing-stats-secret",
+            "key": "exisiting-stats-secret-key",
+        } == jmespath.search("spec.template.spec.containers[1].env[0].valueFrom.secretKeyRef", docs[0])
+
+    def test_unused_secret_key(self):
+        docs = render_chart(
+            "test-pgbouncer-stats",
+            values={
+                "pgbouncer": {
+                    "enabled": True,
+                    "metricsExporterSidecar": {
+                        "statsSecretKey": "unused",
+                    },
+                },
+            },
+            show_only=["templates/pgbouncer/pgbouncer-deployment.yaml"],
+        )
+
+        assert {
+            "name": "test-pgbouncer-stats-pgbouncer-stats",
+            "key": "connection",
+        } == jmespath.search("spec.template.spec.containers[1].env[0].valueFrom.secretKeyRef", docs[0])
+
 
 class TestPgBouncerServiceAccount:
     """Tests PgBouncer Service Account."""
