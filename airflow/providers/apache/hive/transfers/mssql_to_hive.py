70106033b0f172c49a13e444cf87569331118907
Miroslav Šedivý <6774676+eumiro@users.noreply.github.com>
2023-08-18 19:34:34 +0200

    Simplify code around enumerate

--- airflow/providers/apache/hive/transfers/mssql_to_hive.py
+++ airflow/providers/apache/hive/transfers/mssql_to_hive.py
@@ -118,9 +118,8 @@ class MsSqlToHiveOperator(BaseOperator):
                 with NamedTemporaryFile(mode="w", encoding="utf-8") as tmp_file:
                     csv_writer = csv.writer(tmp_file, delimiter=self.delimiter)
                     field_dict = OrderedDict()
-                    for col_count, field in enumerate(cursor.description, start=1):
-                        col_position = f"Column{col_count}"
-                        field_dict[col_position if field[0] == "" else field[0]] = self.type_map(field[1])
+                    for col_count, (key, val) in enumerate(cursor.description, start=1):
+                        field_dict[key or f"Column{col_count}"] = self.type_map(val)
                     csv_writer.writerows(cursor)
                     tmp_file.flush()
 
