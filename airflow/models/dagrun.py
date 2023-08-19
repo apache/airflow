2f6f95c61d89b8eaaa669471a84adea4f4fb425d
Sung Yun <syun64@bloomberg.net>
2023-08-19 13:31:33 -0400

    clean up

--- airflow/models/dagrun.py
+++ airflow/models/dagrun.py
@@ -705,12 +705,11 @@ class DagRun(Base, LoggingMixin):
                         unfinished = unfinished.recalculate()
 
         tis_for_dagrun_state = self._tis_for_dagrun_state(dag=dag, tis=tis)
-
         # check if sla has just been missed for this dagrun
         if dag.sla and not self.sla_missed and not self.run_type == DagRunType.BACKFILL_JOB:
             start_time = self.start_date
             ts = timezone.utcnow()
-            if dag.sla > ts - start_time:
+            if dag.sla < ts - start_time:
                 self.sla_missed = True
 
                 if execute_callbacks:
@@ -718,7 +717,6 @@ class DagRun(Base, LoggingMixin):
                         self,
                         dagrun_state=DagRunState.RUNNING,
                         sla_miss=True,
-                        reason="sla_missed",
                         session=session,
                     )
                 elif dag.has_on_sla_miss_callback:
@@ -732,7 +730,6 @@ class DagRun(Base, LoggingMixin):
                         dagrun_state=DagRunState.RUNNING,
                         sla_miss=True,
                         processor_subdir=None if dag_model is None else dag_model.processor_subdir,
-                        msg="sla_missed",
                     )
         # if all tasks finished and at least one failed, the run failed
         if not unfinished.tis and any(x.state in State.failed_states for x in tis_for_dagrun_state):
@@ -810,7 +807,7 @@ class DagRun(Base, LoggingMixin):
                     full_filepath=dag.fileloc,
                     dag_id=self.dag_id,
                     run_id=self.run_id,
-                    is_failure_callback=True,
+                    dagrun_state=DagRunState.FAILED,
                     processor_subdir=None if dag_model is None else dag_model.processor_subdir,
                     msg="all_tasks_deadlocked",
                 )
