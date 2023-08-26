8754b509bd09f21c8395aa4ded5edaf2e5f2868c
Wei Lee <weilee.rx@gmail.com>
2023-08-26 22:28:24 +0800

    Respect "soft_fail" for core async sensors (#33403)
    * fix(sensors): ensure that DateTimeSensorAsync, TimeDeltaSensorAsync, TimeSensorAsync respect soft_fail
    
    * refactor(sensors): move the soft_fail checking logic from DateTimeSensorAsync, TimeDeltaSensorAsync, TimeSensorAsync to DateTimeTrigger
    
    * test(triggers/temporal): add test case for DateTimeSensorAsync respects soft_fail
    
    * fix(triggers/temporal): use the original error message with skipping postfix as message for AirflowSkipException
    
    * Revert "fix(triggers/temporal): use the original error message with skipping postfix as message for AirflowSkipException"
    
    This reverts commit a6d803303bf71a84e9e59e94d9c088e3120bedb5.
    
    * Revert "test(triggers/temporal): add test case for DateTimeSensorAsync respects soft_fail"
    
    This reverts commit 50e39e08a415685ace788ae728397a199c21e82b.
    
    * Revert "refactor(sensors): move the soft_fail checking logic from DateTimeSensorAsync, TimeDeltaSensorAsync, TimeSensorAsync to DateTimeTrigger"
    
    This reverts commit 985981a269cea68da719d6fd1c60bedd9a7e5225.
    
    * Revert "fix(sensors): ensure that DateTimeSensorAsync, TimeDeltaSensorAsync, TimeSensorAsync respect soft_fail"
    
    This reverts commit b2f2662ae1a11ea928aad57acd2892c763c2db25.
    
    * fix(sensors): move core async sensor trigger initialization to __init__ if possible
    
    (cherry picked from commit 9ce76e321f9792690a8d93d5ecb4df9bdaf8fac9)

--- airflow/sensors/time_sensor.py
+++ airflow/sensors/time_sensor.py
@@ -68,10 +68,11 @@ class TimeSensorAsync(BaseSensorOperator):
         )
 
         self.target_datetime = timezone.convert_to_utc(aware_time)
+        self.trigger = DateTimeTrigger(moment=self.target_datetime)
 
     def execute(self, context: Context):
         self.defer(
-            trigger=DateTimeTrigger(moment=self.target_datetime),
+            trigger=self.trigger,
             method_name="execute_complete",
         )
 
