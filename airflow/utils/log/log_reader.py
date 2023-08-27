58d8577f3528e7e143addefb03711bdc82cb1782
Hussein Awala <hussein@awala.fr>
2023-08-27 22:00:10 +0200

    Improve importing the module in Airflow utils package (#33803)

--- airflow/utils/log/log_reader.py
+++ airflow/utils/log/log_reader.py
@@ -19,16 +19,18 @@ from __future__ import annotations
 import logging
 import time
 from functools import cached_property
-from typing import Iterator
-
-from sqlalchemy.orm.session import Session
+from typing import TYPE_CHECKING, Iterator
 
 from airflow.configuration import conf
-from airflow.models.taskinstance import TaskInstance
 from airflow.utils.helpers import render_log_filename
 from airflow.utils.log.logging_mixin import ExternalLoggingMixin
 from airflow.utils.session import NEW_SESSION, provide_session
 from airflow.utils.state import TaskInstanceState
+
+if TYPE_CHECKING:
+    from sqlalchemy.orm.session import Session
+
+    from airflow.models.taskinstance import TaskInstance
 
 
 class TaskLogReader:
