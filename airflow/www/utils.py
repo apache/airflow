4618ae4a45f6ff76df986e41477016ad78eb8681
Daniel Dyląg <Bisk1@users.noreply.github.com>
2023-08-28 12:40:52 +0200

    Refactor: remove unused state - SHUTDOWN (#33746)
    Co-authored-by: daniel.dylag <danieldylag1990@gmail.com>
    (cherry picked from commit 7faa72795185c1af4b21f207ce7e0735c4365d46)

--- airflow/www/utils.py
+++ airflow/www/utils.py
@@ -108,7 +108,6 @@ priority: list[None | TaskInstanceState] = [
     TaskInstanceState.SCHEDULED,
     TaskInstanceState.DEFERRED,
     TaskInstanceState.RUNNING,
-    TaskInstanceState.SHUTDOWN,
     TaskInstanceState.RESTARTING,
     None,
     TaskInstanceState.SUCCESS,
