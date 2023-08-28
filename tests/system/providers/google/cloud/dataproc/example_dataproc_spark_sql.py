9a0da56296464360a6e377607ad6106b0017df8f
Maksim Moiseenkov <maksim_moiseenkov@epam.com>
2023-08-28 11:47:14 +0000

    Decrease disk size for Dataproc cluster nodes in system tests

--- tests/system/providers/google/cloud/dataproc/example_dataproc_spark_sql.py
+++ tests/system/providers/google/cloud/dataproc/example_dataproc_spark_sql.py
@@ -43,12 +43,12 @@ CLUSTER_CONFIG = {
     "master_config": {
         "num_instances": 1,
         "machine_type_uri": "n1-standard-4",
-        "disk_config": {"boot_disk_type": "pd-standard", "boot_disk_size_gb": 1024},
+        "disk_config": {"boot_disk_type": "pd-standard", "boot_disk_size_gb": 32},
     },
     "worker_config": {
         "num_instances": 2,
         "machine_type_uri": "n1-standard-4",
-        "disk_config": {"boot_disk_type": "pd-standard", "boot_disk_size_gb": 1024},
+        "disk_config": {"boot_disk_type": "pd-standard", "boot_disk_size_gb": 32},
     },
 }
 
