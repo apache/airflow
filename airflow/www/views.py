b3e2e4cb91784456ce3df45ffcffbc8d38bd0d1c
Rafael Carrasco <rafacarrasco07@gmail.com>
2023-09-02 08:56:11 -0500

    Hide Irrelevant Dag Processor from Cluster Activity Page (#33611)
    * first commit
    
    * refactoring
    
    * adding undefined default
    
    * added logic to check if defined
    
    * refactored component
    
    * refactored not equal sign
    
    * refactored code for boolean type instead of string
    
    * refactored global variable. refactored conditional
    
    * pre-commit and linting fixes
    
    * updated jest-setup
    
    * updated jest-setup
    
    * precommit fix
    
    * Update Health.tsx
    
    replacing bottom margin with a top margin
    
    * Update Health.tsx
    
    removed bottom margin from triggerer
    
    (cherry picked from commit c055e1da0b50e98820ffff8f8d10d0882f753384)

--- airflow/www/views.py
+++ airflow/www/views.py
@@ -1062,10 +1062,12 @@ class Airflow(AirflowBaseView):
         """Cluster Activity view."""
         state_color_mapping = State.state_color.copy()
         state_color_mapping["no_status"] = state_color_mapping.pop(None)
+        standalone_dag_processor = conf.getboolean("scheduler", "standalone_dag_processor")
         return self.render_template(
             "airflow/cluster_activity.html",
             auto_refresh_interval=conf.getint("webserver", "auto_refresh_interval"),
             state_color_mapping=state_color_mapping,
+            standalone_dag_processor=standalone_dag_processor,
         )
 
     @expose("/next_run_datasets_summary", methods=["POST"])
