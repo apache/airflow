0af9a5b21cf84adfe4107945c0f68bada12eb359
Hussein Awala <hussein@awala.fr>
2023-09-03 23:46:14 +0200

    Move the try outside the loop when this is possible in Airflow core (#33975)
    * Move the try outside the loop when this is possible in Airflow core
    
    * Use supress instead of except pass
    
    (cherry picked from commit 8918b435be8c683bbd6bb2ffa871dbd31d476f48)

--- airflow/triggers/external_task.py
+++ airflow/triggers/external_task.py
@@ -90,8 +90,8 @@ class TaskStateTrigger(BaseTrigger):
         If dag with specified name was not in the running state after _timeout_sec seconds
         after starting execution process of the trigger, terminate with status 'timeout'.
         """
-        while True:
-            try:
+        try:
+            while True:
                 delta = utcnow() - self.trigger_start_time
                 if delta.total_seconds() < self._timeout_sec:
                     # mypy confuses typing here
@@ -107,9 +107,8 @@ class TaskStateTrigger(BaseTrigger):
                     return
                 self.log.info("Task is still running, sleeping for %s seconds...", self.poll_interval)
                 await asyncio.sleep(self.poll_interval)
-            except Exception:
-                yield TriggerEvent({"status": "failed"})
-                return
+        except Exception:
+            yield TriggerEvent({"status": "failed"})
 
     @sync_to_async
     @provide_session
