988632fd67abc10375ad9fe2cbd8c9edccc609a5
Hussein Awala <hussein@awala.fr>
2023-09-05 09:49:56 +0200

    Add missing audit logs for Flask actions add, edit and delete (#34090)

--- airflow/www/views.py
+++ airflow/www/views.py
@@ -5247,6 +5247,8 @@ class DagRunModelView(AirflowPrivilegeVerifierModelView):
 
     class_permission_name = permissions.RESOURCE_DAG_RUN
     method_permission_name = {
+        "delete": "delete",
+        "edit": "edit",
         "list": "read",
         "action_clear": "edit",
         "action_muldelete": "delete",
@@ -5625,6 +5627,7 @@ class TaskInstanceModelView(AirflowPrivilegeVerifierModelView):
     class_permission_name = permissions.RESOURCE_TASK_INSTANCE
     method_permission_name = {
         "list": "read",
+        "delete": "delete",
         "action_clear": "edit",
         "action_muldelete": "delete",
         "action_set_running": "edit",
