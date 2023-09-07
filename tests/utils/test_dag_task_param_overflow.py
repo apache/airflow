0dc84ad84fe271771da26e0c0bf729ad949fda55
tiranux <23462892+tiranux@users.noreply.github.com>
2023-09-07 08:31:18 +0000

    adding overflow tests

--- tests/utils/test_dag_task_param_overflow.py
+++ tests/utils/test_dag_task_param_overflow.py
@@ -16,70 +16,56 @@
 # under the License.
 from __future__ import annotations
 
+import pytest
+
+from airflow import DAG
+from airflow.exceptions import AirflowDagTaskOutOfBoundsValue
+from airflow.operators.empty import EmptyOperator
+from airflow.utils.dag_parameters_overflow import check_values_overflow
+from tests.models import DEFAULT_DATE
+
 
 class TestDagTaskParameterOverflow:
-    # Pending to create tests
-    """def test_cycle_empty(self):
-        # test empty
+    def test_priority_weight_empty(self):
+        # test empty DAG no overflow
         dag = DAG("dag", start_date=DEFAULT_DATE, default_args={"owner": "owner1"})
 
-        assert not check_cycle(dag)
+        assert not check_values_overflow(dag)
 
-    def test_cycle_single_task(self):
-        # test single task
+    def test_priority_weight_single_task(self):
+        # test single task with no specific priority weight (no overflow)
         dag = DAG("dag", start_date=DEFAULT_DATE, default_args={"owner": "owner1"})
 
         with dag:
-            EmptyOperator(task_id="A")
+            EmptyOperator(task_id="stage1")
 
-        assert not check_cycle(dag)
+        assert not check_values_overflow(dag)
 
-    def test_semi_complex(self):
+    @pytest.mark.backend("postgres")
+    def test_priority_weight_sum_up_overflow(self):
+        # Test that priority_weight_total sum up overflows
         dag = DAG("dag", start_date=DEFAULT_DATE, default_args={"owner": "owner1"})
-
-        # A -> B -> C
-        #      B -> D
-        # E -> F
         with dag:
-            create_cluster = EmptyOperator(task_id="c")
-            pod_task = EmptyOperator(task_id="p")
-            pod_task_xcom = EmptyOperator(task_id="x")
-            delete_cluster = EmptyOperator(task_id="d")
-            pod_task_xcom_result = EmptyOperator(task_id="r")
-            create_cluster >> pod_task >> delete_cluster
-            create_cluster >> pod_task_xcom >> delete_cluster
-            pod_task_xcom >> pod_task_xcom_result
-
-    def test_cycle_no_cycle(self):
-        # test no cycle
-        dag = DAG("dag", start_date=DEFAULT_DATE, default_args={"owner": "owner1"})
-
-        # A -> B -> C
-        #      B -> D
-        # E -> F
-        with dag:
-            op1 = EmptyOperator(task_id="A")
-            op2 = EmptyOperator(task_id="B")
-            op3 = EmptyOperator(task_id="C")
-            op4 = EmptyOperator(task_id="D")
-            op5 = EmptyOperator(task_id="E")
-            op6 = EmptyOperator(task_id="F")
+            op1 = EmptyOperator(task_id="stage1", priority_weight=10)
+            op2 = EmptyOperator(task_id="stage2", priority_weight=2147483647)
             op1.set_downstream(op2)
-            op2.set_downstream(op3)
-            op2.set_downstream(op4)
-            op5.set_downstream(op6)
+        with pytest.raises(AirflowDagTaskOutOfBoundsValue):
+            assert not check_values_overflow(dag)
 
-        assert not check_cycle(dag)
-
-    def test_cycle_loop(self):
-        # test self loop
+    @pytest.mark.backend("postgres")
+    def test_cycle_no_test_priority_weight_negative_overflow(self):
+        # Test that priority_weight_total overflows
         dag = DAG("dag", start_date=DEFAULT_DATE, default_args={"owner": "owner1"})
-
-        # A -> A
         with dag:
-            op1 = EmptyOperator(task_id="A")
-            op1.set_downstream(op1)
+            EmptyOperator(task_id="stage1", priority_weight=-3147483648)
+        with pytest.raises(AirflowDagTaskOutOfBoundsValue):
+            assert not check_values_overflow(dag)
 
-        with pytest.raises(AirflowDagCycleException):
-            assert not check_cycle(dag)
-    """
+    @pytest.mark.backend("postgres")
+    def test_cycle_no_test_priority_weight_positive_overflow(self):
+        # Test that priority_weight_total overflows
+        dag = DAG("dag", start_date=DEFAULT_DATE, default_args={"owner": "owner1"})
+        with dag:
+            EmptyOperator(task_id="stage1", priority_weight=2147483648)
+        with pytest.raises(AirflowDagTaskOutOfBoundsValue):
+            assert not check_values_overflow(dag)
