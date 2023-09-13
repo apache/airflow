62a99152d71e41bb1cca612d982f0a090464dbec
abhishekbhakat <abhishek.bhakat@hotmail.com>
2023-09-13 17:54:05 +0000

    refactor clear_number column enabling mssql drop

--- airflow/migrations/versions/0129_2_8_0_add_clear_number_to_dag_run.py
+++ airflow/migrations/versions/0129_2_8_0_add_clear_number_to_dag_run.py
@@ -39,14 +39,11 @@ airflow_version = "2.8.0"
 def upgrade():
     """Apply add cleared column to dagrun"""
     with op.batch_alter_table("dag_run") as batch_op:
-        batch_op.add_column(
-            sa.Column(
-                "clear_number",
-                sa.Integer,
-                server_default="0",
-                nullable=False,
-            )
-        )
+        batch_op.add_column(sa.Column("clear_number", sa.Integer))
+    op.execute(sa.text("UPDATE dag_run SET clear_number = 0"))
+
+    with op.batch_alter_table("dag_run") as batch_op:
+        batch_op.alter_column("clear_number", existing_type=sa.Integer, nullable=False)
 
 
 def downgrade():
