9f436ce4790fb2672ed80d4f05552911bb931844
Richard Hsu <richsuca@users.noreply.github.com>
2023-09-21 13:14:07 -0400

    Fix typing for get_provider_name_from_short_hand and its usages
    Removed the type cast

--- dev/breeze/src/airflow_breeze/utils/general_utils.py
+++ dev/breeze/src/airflow_breeze/utils/general_utils.py
@@ -29,4 +29,4 @@ def get_provider_name_from_short_hand(short_form_providers: tuple[str]):
         short_form_provider.split(".")
         parts = "-".join(short_form_provider.split("."))
         providers.append(providers_prefix + parts)
-    return tuple(providers)
+    return providers
