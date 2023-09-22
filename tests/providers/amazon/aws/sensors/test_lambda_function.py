eb57244cbf11cd9d3f8777940ae99e0f0f97ac67
utkarsh sharma <utkarsharma2@gmail.com>
2023-09-22 16:21:27 +0530

    Respect soft_fail parameter in LambdaFunctionStateSensor

--- tests/providers/amazon/aws/sensors/test_lambda_function.py
+++ tests/providers/amazon/aws/sensors/test_lambda_function.py
@@ -20,7 +20,7 @@ from unittest import mock
 
 import pytest
 
-from airflow.exceptions import AirflowException
+from airflow.exceptions import AirflowException, AirflowSkipException
 from airflow.providers.amazon.aws.hooks.lambda_function import LambdaHook
 from airflow.providers.amazon.aws.sensors.lambda_function import LambdaFunctionStateSensor
 
@@ -69,3 +69,16 @@ class TestLambdaFunctionStateSensor:
             mock_conn.get_function.assert_called_once_with(
                 FunctionName=FUNCTION_NAME,
             )
+
+    @pytest.mark.parametrize(
+        "soft_fail, expected_exception", ((False, AirflowException), (True, AirflowSkipException))
+    )
+    def test_fail_poke(self, soft_fail, expected_exception):
+        sensor = LambdaFunctionStateSensor(
+            task_id="test_sensor",
+            function_name=FUNCTION_NAME,
+        )
+        sensor.soft_fail = soft_fail
+        message = "Lambda function state sensor failed because the Lambda is in a failed state"
+        with pytest.raises(expected_exception, match=message):
+            sensor.poke(context={})
