2b57f39f48d1d7145b78cf2a4f557d4e8c831989
Andrey Anshin <Andrey.Anshin@taragol.is>
2023-09-26 17:16:45 +0400

    Use `airflow.models.dag.DAG` in dev (#34615)

--- dev/perf/dags/perf_dag_2.py
+++ dev/perf/dags/perf_dag_2.py
@@ -22,7 +22,7 @@ from __future__ import annotations
 
 import datetime
 
-from airflow.models import DAG
+from airflow.models.dag import DAG
 from airflow.operators.bash import BashOperator
 
 args = {
