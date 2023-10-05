7449ba200f3d5975c711760178e6327a34e0fc2f
cjames23 <hawkinscary23@gmail.com>
2023-10-05 15:00:09 -0700

    Fix formatting of opensearch hook tests

--- tests/providers/opensearch/hooks/test_opensearch.py
+++ tests/providers/opensearch/hooks/test_opensearch.py
@@ -23,16 +23,15 @@ MOCK_SEARCH_RETURN = {"status": "test"}
 
 class TestOpenSearchHook:
     def test_hook_search(self, mock_hook):
-        hook = OpenSearchHook(open_search_conn_id="opensearch_default",  log_query=True)
-        result = hook.search(
-            index_name="testIndex", 
+        self.hook = OpenSearchHook(open_search_conn_id="opensearch_default", log_query=True)
+        result = self.hook.search(
+            index_name="testIndex",
             query={"size": 1, "query": {"multi_match": {"query": "test", "fields": ["testField"]}}},
         )
 
         assert result == MOCK_SEARCH_RETURN
 
     def test_hook_index(self, mock_hook):
-        result = OpenSearchHook(open_search_conn_id="opensearch_default", log_query=True).index(
-            index_name="test_index", document={"title": "Monty Python"}, doc_id=3
-        )
+        self.hook = OpenSearchHook(open_search_conn_id="opensearch_default", log_query=True)
+        result = self.hook.index(index_name="test_index", document={"title": "Monty Python"}, doc_id=3)
         assert result == 3
