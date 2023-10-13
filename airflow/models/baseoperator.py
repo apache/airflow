3dc0b818e25027ae89f40e301e0c99b64fad0581
Joffrey Bienvenu <joffrey.bienvenu@infrabel.be>
2023-10-13 18:27:30 +0200

    feat: Implement customizable logger name

--- airflow/models/baseoperator.py
+++ airflow/models/baseoperator.py
@@ -676,6 +676,7 @@ class BaseOperator(AbstractOperator, metaclass=BaseOperatorMeta):
         "user_defined_macros",
         "user_defined_filters",
         "params",
+        "_log",
     )
 
     # each operator should override this class attr for shallow copy attrs.
