1f1709b17e034d74c3c562d0f2376abd4d5e753e
Vincent Beck <vincbeck@amazon.com>
2023-10-13 16:26:50 -0400

    Fix tests

--- tests/auth/managers/fab/test_fab_auth_manager.py
+++ tests/auth/managers/fab/test_fab_auth_manager.py
@@ -261,12 +261,32 @@ class TestFabAuthManager:
                 [(ACTION_CAN_READ, RESOURCE_TASK_INSTANCE)],
                 False,
             ),
-            # With read permissions on a specific DAG
+            # With read permissions on a specific DAG but not on the DAG run
             (
                 "GET",
                 DagAccessEntity.TASK_INSTANCE,
                 DagDetails(id="test_dag_id"),
                 [(ACTION_CAN_READ, "DAG:test_dag_id"), (ACTION_CAN_READ, RESOURCE_TASK_INSTANCE)],
+                False,
+            ),
+            # With read permissions on a specific DAG but not on the DAG run
+            (
+                "GET",
+                DagAccessEntity.TASK_INSTANCE,
+                DagDetails(id="test_dag_id"),
+                [
+                    (ACTION_CAN_READ, "DAG:test_dag_id"),
+                    (ACTION_CAN_READ, RESOURCE_TASK_INSTANCE),
+                    (ACTION_CAN_READ, RESOURCE_DAG_RUN),
+                ],
+                True,
+            ),
+            # With edit permissions on a specific DAG and read on the DAG access entity
+            (
+                "DELETE",
+                DagAccessEntity.TASK,
+                DagDetails(id="test_dag_id"),
+                [(ACTION_CAN_EDIT, "DAG:test_dag_id"), (ACTION_CAN_DELETE, RESOURCE_TASK_INSTANCE)],
                 True,
             ),
             # With edit permissions on a specific DAG and read on the DAG access entity
