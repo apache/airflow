9151a4bc31bd800aa9e60cf13a55aaba9541b4e1
Jakub Dardzinski <kuba0221@gmail.com>
2023-10-18 15:48:58 +0200

    Fix `get_plugin_info` for class based listeners.
    Signed-off-by: Jakub Dardzinski <kuba0221@gmail.com>

--- tests/plugins/test_plugins_manager.py
+++ tests/plugins/test_plugins_manager.py
@@ -18,6 +18,7 @@
 from __future__ import annotations
 
 import importlib
+import inspect
 import logging
 import os
 import sys
@@ -29,6 +30,7 @@ import pytest
 from airflow.hooks.base import BaseHook
 from airflow.listeners.listener import get_listener_manager
 from airflow.plugins_manager import AirflowPlugin
+from airflow.utils.module_loading import qualname
 from airflow.www import app as application
 from setup import AIRFLOW_SOURCES_ROOT
 from tests.test_utils.config import conf_vars
@@ -379,7 +381,13 @@ class TestPluginsManager:
             plugins_manager.integrate_listener_plugins(get_listener_manager())
 
             assert get_listener_manager().has_listeners
-            assert get_listener_manager().pm.get_plugins().pop().__name__ == "tests.listeners.empty_listener"
+            listeners = get_listener_manager().pm.get_plugins()
+            listener_names = [el.__name__ if inspect.ismodule(el) else qualname(el) for el in listeners]
+            # sort names as order of listeners is not guaranteed
+            assert [
+                "tests.listeners.class_listener.ClassBasedListener",
+                "tests.listeners.empty_listener",
+            ] == sorted(listener_names)
 
     def test_should_import_plugin_from_providers(self):
         from airflow import plugins_manager
