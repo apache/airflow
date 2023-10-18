c0b933e49bb7cb3940449726524f880cac29096f
Xiroo <kh6312@naver.com>
2023-10-18 23:03:43 +0900

    print timeut

--- airflow/sensors/external_task.py
+++ airflow/sensors/external_task.py
@@ -352,7 +352,7 @@ class ExternalTaskSensor(BaseSensorOperator):
             self.log.info("External task %s has executed successfully.", self.external_task_id)
             return None
         elif event["status"] == "timeout":
-            raise AirflowException("Dag was not started within timeout, assuming fail.")
+            raise AirflowException(f"Dag was not started within {self.timeout}, assuming fail.")
         else:
             raise AirflowException(
                 "Error occurred while trying to retrieve task status. Please, check the "
