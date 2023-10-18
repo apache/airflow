e8be9b9e4a362f4532d8fc452cd1813c0ef7777c
Vincent Beck <vincbeck@amazon.com>
2023-10-18 15:17:45 -0400

    Keep `deny_all` and `kerberos` in core Airflow

--- airflow/api/auth/backend/kerberos_auth.py
+++ airflow/api/auth/backend/kerberos_auth.py
@@ -19,6 +19,7 @@ from __future__ import annotations
 import warnings
 
 from airflow.exceptions import RemovedInAirflow3Warning
+from airflow.utils.airflow_flask_app import get_airflow_app
 
 #
 # Copyright (c) 2013, Michael Komitee
@@ -44,30 +45,124 @@ from airflow.exceptions import RemovedInAirflow3Warning
 # ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-"""
-This module is deprecated.
+"""Kerberos authentication module"""
+import logging
+import os
+from functools import wraps
+from typing import TYPE_CHECKING, Any, Callable, TypeVar, cast
 
-Please use :mod:`airflow.auth.managers.fab.api.auth.backend.kerberos_auth` instead.
-"""
-from typing import Any, Callable
-
+import kerberos
+from flask import Response, _request_ctx_stack as stack, g, make_response, request  # type: ignore
 from requests_kerberos import HTTPKerberosAuth
 
-import airflow.auth.managers.fab.api.auth.backend.kerberos_auth as fab_kerberos_auth
+from airflow.configuration import conf
+from airflow.utils.net import getfqdn
+
+if TYPE_CHECKING:
+    from airflow.auth.managers.models.base_user import BaseUser
+
+log = logging.getLogger(__name__)
+
 
 CLIENT_AUTH: tuple[str, str] | Any | None = HTTPKerberosAuth(service="airflow")
 
-warnings.warn(
-    "This module is deprecated. Please use `airflow.auth.managers.fab.api.auth.backend.kerberos_auth` "
-    "instead.",
-    RemovedInAirflow3Warning,
-    stacklevel=2,
-)
+
+class KerberosService:
+    """Class to keep information about the Kerberos Service initialized."""
+
+    def __init__(self):
+        self.service_name = None
+
+
+# Stores currently initialized Kerberos Service
+_KERBEROS_SERVICE = KerberosService()
 
 
 def init_app(app):
-    fab_kerberos_auth.init_app(app)
+    """Initialize application with kerberos."""
+    hostname = app.config.get("SERVER_NAME")
+    if not hostname:
+        hostname = getfqdn()
+    log.info("Kerberos: hostname %s", hostname)
+
+    service = "airflow"
+
+    _KERBEROS_SERVICE.service_name = f"{service}@{hostname}"
+
+    if "KRB5_KTNAME" not in os.environ:
+        os.environ["KRB5_KTNAME"] = conf.get("kerberos", "keytab")
+
+    try:
+        log.info("Kerberos init: %s %s", service, hostname)
+        principal = kerberos.getServerPrincipalDetails(service, hostname)
+    except kerberos.KrbError as err:
+        log.warning("Kerberos: %s", err)
+    else:
+        log.info("Kerberos API: server is %s", principal)
 
 
-def requires_authentication(function: Callable):
-    return fab_kerberos_auth.requires_authentication(function)
+def _unauthorized():
+    """Indicate that authorization is required."""
+    return Response("Unauthorized", 401, {"WWW-Authenticate": "Negotiate"})
+
+
+def _forbidden():
+    return Response("Forbidden", 403)
+
+
+def _gssapi_authenticate(token):
+    state = None
+    ctx = stack.top
+    try:
+        return_code, state = kerberos.authGSSServerInit(_KERBEROS_SERVICE.service_name)
+        if return_code != kerberos.AUTH_GSS_COMPLETE:
+            return None
+        return_code = kerberos.authGSSServerStep(state, token)
+        if return_code == kerberos.AUTH_GSS_COMPLETE:
+            ctx.kerberos_token = kerberos.authGSSServerResponse(state)
+            ctx.kerberos_user = kerberos.authGSSServerUserName(state)
+            return return_code
+        if return_code == kerberos.AUTH_GSS_CONTINUE:
+            return kerberos.AUTH_GSS_CONTINUE
+        return None
+    except kerberos.GSSError:
+        return None
+    finally:
+        if state:
+            kerberos.authGSSServerClean(state)
+
+
+T = TypeVar("T", bound=Callable)
+
+
+def requires_authentication(function: T, find_user: Callable[[str], BaseUser] | None = None):
+    """Decorate functions that require authentication with Kerberos."""
+    if not find_user:
+        warnings.warn(
+            "This module is deprecated. Please use "
+            "`airflow.auth.managers.fab.api.auth.backend.kerberos_auth` instead.",
+            RemovedInAirflow3Warning,
+            stacklevel=2,
+        )
+        find_user = get_airflow_app().appbuilder.sm.find_user
+
+    @wraps(function)
+    def decorated(*args, **kwargs):
+        header = request.headers.get("Authorization")
+        if header:
+            ctx = stack.top
+            token = "".join(header.split()[1:])
+            return_code = _gssapi_authenticate(token)
+            if return_code == kerberos.AUTH_GSS_COMPLETE:
+                g.user = find_user(ctx.kerberos_user)
+                response = function(*args, **kwargs)
+                response = make_response(response)
+                if ctx.kerberos_token is not None:
+                    response.headers["WWW-Authenticate"] = f"negotiate {ctx.kerberos_token}"
+
+                return response
+            if return_code != kerberos.AUTH_GSS_CONTINUE:
+                return _forbidden()
+        return _unauthorized()
+
+    return cast(T, decorated)
