551886eb263c8df0b2eee847dd6725de78bc25fc
Jarek Potiuk <jarek@potiuk.com>
2023-10-20 11:26:17 +0200

    Handle "all-providers" when building documentation (#35069)

--- dev/breeze/src/airflow_breeze/utils/general_utils.py
+++ dev/breeze/src/airflow_breeze/utils/general_utils.py
@@ -16,7 +16,7 @@
 # under the License.
 from __future__ import annotations
 
-from airflow_breeze.global_constants import ALL_SPECIAL_DOC_KEYS
+from airflow_breeze.global_constants import ALL_SPECIAL_DOC_KEYS, get_available_documentation_packages
 
 providers_prefix = "apache-airflow-providers-"
 
@@ -32,3 +32,11 @@ def get_provider_name_from_short_hand(short_form_providers: tuple[str]):
         parts = "-".join(short_form_provider.split("."))
         providers.append(providers_prefix + parts)
     return tuple(providers)
+
+
+def expand_all_providers(short_doc_packages: tuple[str, ...]) -> tuple[str, ...]:
+    if "all-providers" in short_doc_packages:
+        packages = [package for package in short_doc_packages if package != "all-providers"]
+        packages.extend(get_available_documentation_packages(only_providers=True, short_version=True))
+        short_doc_packages = tuple(set(packages))
+    return short_doc_packages
