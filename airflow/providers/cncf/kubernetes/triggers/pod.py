029b079ef1f945053ffbd6ecfbc5ceb9e1596b81
anukrati15 <anukrati.saxena@cloudera.com>
2023-10-20 15:26:03 +0530

    fixing suggestions

--- airflow/providers/cncf/kubernetes/triggers/pod.py
+++ airflow/providers/cncf/kubernetes/triggers/pod.py
@@ -102,7 +102,7 @@ class KubernetesPodTrigger(BaseTrigger):
         if should_delete_pod is not None:
             warnings.warn(
                 "`should_delete_pod` parameter is deprecated, please use `on_finish_action`",
-                AirflowProviderDeprecationWarning,
+                category=AirflowProviderDeprecationWarning,
                 stacklevel=2
             )
             self.on_finish_action = (
