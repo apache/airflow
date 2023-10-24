b18ce3e1acd611e03b8d511c7467db6b30766ed5
Andrey Anshin <Andrey.Anshin@taragol.is>
2023-10-24 18:38:39 +0400

    Drop MySQL 5.7 support in dev/ci (#35152)

--- dev/breeze/src/airflow_breeze/global_constants.py
+++ dev/breeze/src/airflow_breeze/global_constants.py
@@ -93,7 +93,7 @@ ALLOWED_POSTGRES_VERSIONS = ["11", "12", "13", "14", "15", "16"]
 # - Innovations: Shot living releases with short support cycle - only until next Innovation/LTS release.
 # See: https://dev.mysql.com/blog-archive/introducing-mysql-innovation-and-long-term-support-lts-versions/
 MYSQL_LTS_RELEASES: list[str] = []
-MYSQL_OLD_RELEASES = ["5.7", "8.0"]
+MYSQL_OLD_RELEASES = ["8.0"]
 MYSQL_INNOVATION_RELEASE = "8.1"
 ALLOWED_MYSQL_VERSIONS = [*MYSQL_OLD_RELEASES, *MYSQL_LTS_RELEASES]
 if MYSQL_INNOVATION_RELEASE:
