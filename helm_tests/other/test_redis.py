550ebe8176a7f54c036e3ae2d25b5b390a4ba7a9
hugo-syn <61210734+hugo-syn@users.noreply.github.com>
2023-10-24 12:22:52 +0200

    Fix typo s/overriden/overridden/ (#35128)
    (cherry picked from commit 6316025b7c160006ef57d3463868394c3f9e8d97)

--- helm_tests/other/test_redis.py
+++ helm_tests/other/test_redis.py
@@ -344,7 +344,7 @@ class TestRedisServiceAccount:
         )
         assert jmespath.search("automountServiceAccountToken", docs[0]) is True
 
-    def test_overriden_automount_service_account_token(self):
+    def test_overridden_automount_service_account_token(self):
         docs = render_chart(
             values={
                 "redis": {
