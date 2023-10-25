b99d7d73ec5182aee9cd218eec539a1956056008
SimonPrydden <simon.prydden@sykescottages.co.uk>
2023-10-25 19:19:05 +0100

    add unit tests and fix static checks

--- tests/providers/amazon/aws/transfers/test_http_to_s3.py
+++ tests/providers/amazon/aws/transfers/test_http_to_s3.py
@@ -18,26 +18,32 @@
 from __future__ import annotations
 
 import datetime
+from unittest import mock
 
 import boto3
-import pytest
 from moto import mock_s3
 
-from airflow.providers.amazon.aws.transfers.http_to_s3 import HttpToS3Operator
 from airflow.models.dag import DAG
+from airflow.providers.amazon.aws.transfers.http_to_s3 import HttpToS3Operator
+
+EXAMPLE_URL = "http://www.example.com"
 
 
+@mock.patch.dict("os.environ", AIRFLOW_CONN_HTTP_EXAMPLE=EXAMPLE_URL)
 class TestHttpToS3Operator:
     def setup_method(self):
         args = {"owner": "airflow", "start_date": datetime.datetime(2017, 1, 1)}
         self.dag = DAG("test_dag_id", default_args=args)
-        self.endpoint = "/example_report"
+        self.http_conn_id = "HTTP_EXAMPLE"
+        self.response = b"Example.com fake response"
+        self.endpoint = "/"
         self.s3_key = "test/test1.csv"
         self.s3_bucket = "dummy"
 
     def test_init(self):
         operator = HttpToS3Operator(
             task_id="http_to_s3_operator",
+            http_conn_id=self.http_conn_id,
             endpoint=self.endpoint,
             s3_key=self.s3_key,
             s3_bucket=self.s3_bucket,
@@ -46,24 +52,16 @@ class TestHttpToS3Operator:
         assert operator.endpoint == self.endpoint
         assert operator.s3_key == self.s3_key
         assert operator.s3_bucket == self.s3_bucket
-
-    def test_execute_exception(self):
-        operator = HttpToS3Operator(
-            task_id="http_to_s3_operator_exception",
-            endpoint=self.endpoint,
-            s3_key=self.s3_key,
-            s3_bucket=self.s3_bucket,
-            dag=self.dag,
-        )
-        with pytest.raises(TypeError):
-            operator.execute(None)
+        assert operator.http_conn_id == self.http_conn_id
 
     @mock_s3
-    def test_execute(self):
+    def test_execute(self, requests_mock):
+        requests_mock.register_uri("GET", EXAMPLE_URL, content=self.response)
         conn = boto3.client("s3")
         conn.create_bucket(Bucket=self.s3_bucket)
         operator = HttpToS3Operator(
             task_id="s3_to_file_sensor",
+            http_conn_id=self.http_conn_id,
             endpoint=self.endpoint,
             s3_key=self.s3_key,
             s3_bucket=self.s3_bucket,
