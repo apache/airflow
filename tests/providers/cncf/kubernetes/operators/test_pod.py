19889be10939e687526aaccb1acc4d603f6d20b3
Mark Scannell <scannell@google.com>
2023-10-25 09:34:33 +0100

    Cleaned up Python syntax

--- tests/providers/cncf/kubernetes/operators/test_pod.py
+++ tests/providers/cncf/kubernetes/operators/test_pod.py
@@ -132,11 +132,13 @@ class TestKubernetesPodOperator:
                 requests={"memory": "{{ dag.dag_id }}", "cpu": "{{ dag.dag_id }}"},
                 limits={"memory": "{{ dag.dag_id }}", "cpu": "{{ dag.dag_id }}"},
             ),
-            volume_mounts=[k8s.V1VolumeMount(
-                name="{{ dag.dag_id }}",
-                mount_path="mount_path",
-                sub_path="{{ dag.dag_id }}",
-            )],
+            volume_mounts=[
+                k8s.V1VolumeMount(
+                    name="{{ dag.dag_id }}",
+                    mount_path="mount_path",
+                    sub_path="{{ dag.dag_id }}",
+                )
+            ],
             pod_template_file="{{ dag.dag_id }}",
             config_file="{{ dag.dag_id }}",
             labels="{{ dag.dag_id }}",
