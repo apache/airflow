090e532703fa7c9353806da36cd201b2a476d3cb
bolkedebruin <bolkedebruin@users.noreply.github.com>
2023-11-05 20:27:07 +0100

    Add support for serialization of iceberg tables (#35456)
    This adds lightweight support for serialization of
    Apache Iceberg tables. This means that references
    are captured and tables are re-instantiated with their
    catalog information.

--- tests/serialization/serializers/test_serializers.py
+++ tests/serialization/serializers/test_serializers.py
@@ -18,12 +18,16 @@ from __future__ import annotations
 
 import datetime
 import decimal
+from unittest.mock import patch
 
 import numpy as np
 import pendulum.tz
 import pytest
 from dateutil.tz import tzutc
 from pendulum import DateTime
+from pyiceberg.catalog import Catalog
+from pyiceberg.io import FileIO
+from pyiceberg.table import Table
 
 from airflow import PY39
 from airflow.models.param import Param, ParamsDict
@@ -175,3 +179,22 @@ class TestSerializers:
         e = serialize(i)
         d = deserialize(e)
         assert i.equals(d)
+
+    @patch.object(Catalog, "__abstractmethods__", set())
+    @patch.object(FileIO, "__abstractmethods__", set())
+    @patch("pyiceberg.catalog.Catalog.load_table")
+    @patch("pyiceberg.catalog.load_catalog")
+    def test_iceberg(self, mock_load_catalog, mock_load_table):
+        uri = "http://rest.no.where"
+        catalog = Catalog("catalog", uri=uri)
+        identifier = ("catalog", "schema", "table")
+        mock_load_catalog.return_value = catalog
+
+        i = Table(identifier, "bar", catalog=catalog, metadata_location="", io=FileIO())
+        mock_load_table.return_value = i
+
+        e = serialize(i)
+        d = deserialize(e)
+        assert i == d
+        mock_load_catalog.assert_called_with("catalog", uri=uri)
+        mock_load_table.assert_called_with((identifier[1], identifier[2]))
