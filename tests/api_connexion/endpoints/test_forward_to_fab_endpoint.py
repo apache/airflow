213ef2fb528b95dff991742036ab7df57043b12a
Ephraim Anierobi <splendidzigy24@gmail.com>
2023-11-06 13:51:04 +0100

    Fix patch & post endpoints forwarding to FAB

--- tests/api_connexion/endpoints/test_forward_to_fab_endpoint.py
+++ tests/api_connexion/endpoints/test_forward_to_fab_endpoint.py
@@ -24,10 +24,58 @@ from airflow.auth.managers.base_auth_manager import BaseAuthManager
 from airflow.auth.managers.fab.models import Role, User
 from airflow.security import permissions
 from airflow.utils import timezone
+from airflow.utils.session import create_session
 from airflow.www.security import EXISTING_ROLES
 from tests.test_utils.api_connexion_utils import create_role, create_user, delete_role, delete_user
 
 DEFAULT_TIME = "2020-06-11T18:00:00+00:00"
+
+EXAMPLE_USER_NAME = "example_user"
+
+EXAMPLE_USER_EMAIL = "example_user@example.com"
+
+
+def _delete_user(**filters):
+    with create_session() as session:
+        user = session.query(User).filter_by(**filters).first()
+        if user is None:
+            return
+        user.roles = []
+        session.delete(user)
+
+
+@pytest.fixture()
+def autoclean_user_payload(autoclean_username, autoclean_email):
+    return {
+        "username": autoclean_username,
+        "password": "resutsop",
+        "email": autoclean_email,
+        "first_name": "Tester",
+        "last_name": "",
+    }
+
+
+@pytest.fixture()
+def autoclean_admin_user(configured_app, autoclean_user_payload):
+    security_manager = configured_app.appbuilder.sm
+    return security_manager.add_user(
+        role=security_manager.find_role("Admin"),
+        **autoclean_user_payload,
+    )
+
+
+@pytest.fixture()
+def autoclean_username():
+    _delete_user(username=EXAMPLE_USER_NAME)
+    yield EXAMPLE_USER_NAME
+    _delete_user(username=EXAMPLE_USER_NAME)
+
+
+@pytest.fixture()
+def autoclean_email():
+    _delete_user(email=EXAMPLE_USER_EMAIL)
+    yield EXAMPLE_USER_EMAIL
+    _delete_user(email=EXAMPLE_USER_EMAIL)
 
 
 @pytest.fixture(scope="module")
@@ -76,24 +124,6 @@ class TestFABforwarding:
         users.delete(synchronize_session=False)
         session.commit()
 
-    def _create_users(self, count, roles=None):
-        # create users with defined created_on and changed_on date
-        # for easy testing
-        if roles is None:
-            roles = []
-        return [
-            User(
-                first_name=f"test{i}",
-                last_name=f"test{i}",
-                username=f"TEST_USER{i}",
-                email=f"mytest@test{i}.org",
-                roles=roles or [],
-                created_on=timezone.parse(DEFAULT_TIME),
-                changed_on=timezone.parse(DEFAULT_TIME),
-            )
-            for i in range(1, count + 1)
-        ]
-
 
 class TestFABRoleForwarding(TestFABforwarding):
     @mock.patch("airflow.api_connexion.endpoints.forward_to_fab_endpoint.get_auth_manager")
@@ -138,6 +168,26 @@ class TestFABRoleForwarding(TestFABforwarding):
         resp = self.client.get("api/v1/permissions", environ_overrides={"REMOTE_USER": "test"})
         assert resp.status_code == 200
 
+
+class TestFABUserForwarding(TestFABforwarding):
+    def _create_users(self, count, roles=None):
+        # create users with defined created_on and changed_on date
+        # for easy testing
+        if roles is None:
+            roles = []
+        return [
+            User(
+                first_name=f"test{i}",
+                last_name=f"test{i}",
+                username=f"TEST_USER{i}",
+                email=f"mytest@test{i}.org",
+                roles=roles or [],
+                created_on=timezone.parse(DEFAULT_TIME),
+                changed_on=timezone.parse(DEFAULT_TIME),
+            )
+            for i in range(1, count + 1)
+        ]
+
     def test_get_user_forwards_to_fab(self):
         users = self._create_users(1)
         session = self.app.appbuilder.get_session
@@ -154,29 +204,28 @@ class TestFABRoleForwarding(TestFABforwarding):
         resp = self.client.get("api/v1/users", environ_overrides={"REMOTE_USER": "test"})
         assert resp.status_code == 200
 
-    def test_post_user_forwards_to_fab(self):
-        payload = {
-            "username": "TEST_USER1",
-            "email": "testemail@test.com",
-            "roles": [{"name": "Test"}],
-        }
-        resp = self.client.post("api/v1/users", json=payload, environ_overrides={"REMOTE_USER": "test"})
-        assert resp.status_code == 200
-
-    def test_patch_user_forwards_to_fab(self):
-        users = self._create_users(1)
-        session = self.app.appbuilder.get_session
-        session.add_all(users)
-        session.commit()
-        payload = {
-            "username": "TEST_USER1",
-            "email": "testemail@test.com",
-            "roles": [{"name": "Test"}],
-        }
-        resp = self.client.patch(
-            "api/v1/users/TEST_USER1", json=payload, environ_overrides={"REMOTE_USER": "test"}
+    def test_post_user_forwards_to_fab(self, autoclean_username, autoclean_user_payload):
+        response = self.client.post(
+            "/api/v1/users",
+            json=autoclean_user_payload,
+            environ_overrides={"REMOTE_USER": "test"},
         )
-        assert resp.status_code == 200
+        assert response.status_code == 200, response.json
+
+        security_manager = self.app.appbuilder.sm
+        user = security_manager.find_user(autoclean_username)
+        assert user is not None
+        assert user.roles == [security_manager.find_role("Public")]
+
+    @pytest.mark.usefixtures("autoclean_admin_user")
+    def test_patch_user_forwards_to_fab(self, autoclean_username, autoclean_user_payload):
+        autoclean_user_payload["first_name"] = "Changed"
+        response = self.client.patch(
+            f"/api/v1/users/{autoclean_username}",
+            json=autoclean_user_payload,
+            environ_overrides={"REMOTE_USER": "test"},
+        )
+        assert response.status_code == 200, response.json
 
     def test_delete_user_forwards_to_fab(self):
         users = self._create_users(1)
