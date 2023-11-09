957fd288be35c1c00323e72d8f7102ed7bdd8862
Eric Mendes <42689328+Eric-Mendes@users.noreply.github.com>
2023-11-09 17:21:22 -0300

    Update tests/system/providers/opsgenie/example_opsgenie_notifier.py
    Co-authored-by: Andrey Anshin <Andrey.Anshin@taragol.is>

--- tests/system/providers/opsgenie/example_opsgenie_notifier.py
+++ tests/system/providers/opsgenie/example_opsgenie_notifier.py
@@ -13,6 +13,7 @@
 # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
+# [START howto_notifier_opsgenie]
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.bash import BashOperator
