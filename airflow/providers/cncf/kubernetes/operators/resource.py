2966fb699414ab4a2052d5cfbcac942e99eaf20c
raphaelauv <raphaelauv@gmail.com>
2023-11-13 13:58:45 +0100

    feat: K8S resource operator - CRD

--- airflow/providers/cncf/kubernetes/operators/resource.py
+++ airflow/providers/cncf/kubernetes/operators/resource.py
@@ -22,14 +22,18 @@ from functools import cached_property
 from typing import TYPE_CHECKING
 
 import yaml
-from kubernetes.utils import create_from_yaml
+from kubernetes.client import ApiClient, CustomObjectsApi
+from kubernetes.dynamic import DynamicClient, ResourceInstance
+from kubernetes.utils import FailToCreateError, create_from_yaml
 
+from airflow.exceptions import AirflowFailException
 from airflow.models import BaseOperator
 from airflow.providers.cncf.kubernetes.hooks.kubernetes import KubernetesHook
 from airflow.providers.cncf.kubernetes.utils.delete_from import delete_from_yaml
+from airflow.sensors.base import BaseSensorOperator
 
 if TYPE_CHECKING:
-    from kubernetes.client import ApiClient
+    from airflow.utils.context import Context
 
 __all__ = ["KubernetesCreateResourceOperator", "KubernetesDeleteResourceOperator"]
 
@@ -47,6 +51,116 @@ class KubernetesResourceBaseOperator(BaseOperator):
         for the Kubernetes cluster.
     """
 
+    template_fields = ("yaml_conf",)
+    template_fields_renderers = {"yaml_conf": "yaml"}
+
+    def __init__(
+        self,
+        *,
+        yaml_conf: str,
+        namespace: str | None = None,
+        kubernetes_conn_id: str | None = KubernetesHook.default_conn_name,
+        custom_resource_definition: bool = False,
+        **kwargs,
+    ) -> None:
+        super().__init__(**kwargs)
+        self._namespace = namespace
+        self.kubernetes_conn_id = kubernetes_conn_id
+        self.yaml_conf = yaml_conf
+        self.custom_resource_definition = custom_resource_definition
+
+    @cached_property
+    def client(self) -> ApiClient:
+        return self.hook.api_client
+
+    @cached_property
+    def hook(self) -> KubernetesHook:
+        hook = KubernetesHook(conn_id=self.kubernetes_conn_id)
+        return hook
+
+    def get_namespace(self) -> str:
+        if self._namespace:
+            return self._namespace
+        else:
+            return self.hook.get_namespace() or "default"
+
+    def wrap_errors(self, objects, callback):
+        failures = []
+        for yml_document in objects:
+            if yml_document is None:
+                continue
+            try:
+                getattr(self, callback)(yml_document)
+            except FailToCreateError as failure:
+                failures.extend(failure.api_exceptions)
+        if failures:
+            raise FailToCreateError(failures)
+
+
+class KubernetesCreateResourceOperator(KubernetesResourceBaseOperator):
+    """Create a resource in a kubernetes."""
+
+    def create_custom_from_yaml_object(self, body: dict):
+        api_version = body["apiVersion"]
+        group = api_version[0 : api_version.find("/")]
+        version = api_version[api_version.find("/") + 1 :]
+
+        namespace = None
+        if body.get("metadata"):
+            metadata: dict = body.get("metadata", None)
+            namespace = metadata.get("namespace", None)
+        if namespace is None:
+            namespace = self.get_namespace()
+
+        plural = body["kind"].lower() + "s"
+        CustomObjectsApi(self.client).create_namespaced_custom_object(group, version, namespace, plural, body)
+
+    def execute(self, context) -> None:
+        resources = yaml.safe_load_all(self.yaml_conf)
+        if not self.custom_resource_definition:
+            create_from_yaml(
+                k8s_client=self.client,
+                yaml_objects=resources,
+                namespace=self.get_namespace(),
+            )
+        else:
+            self.wrap_errors(objects=resources, callback="create_custom_from_yaml_object")
+
+
+class KubernetesDeleteResourceOperator(KubernetesResourceBaseOperator):
+    """Delete a resource in a kubernetes."""
+
+    def delete_custom_from_yaml_object(self, body: dict):
+        name = body["metadata"]["name"]
+
+        api_version = body["apiVersion"]
+        group = api_version[0 : api_version.find("/")]
+        version = api_version[api_version.find("/") + 1 :]
+
+        namespace = None
+        if body.get("metadata"):
+            metadata: dict = body.get("metadata", None)
+            namespace = metadata.get("namespace", None)
+        if namespace is None:
+            namespace = self.get_namespace()
+
+        plural = body["kind"].lower() + "s"
+
+        CustomObjectsApi(self.client).delete_namespaced_custom_object(group, version, namespace, plural, name)
+
+    def execute(self, context) -> None:
+        resources = yaml.safe_load_all(self.yaml_conf)
+        if not self.custom_resource_definition:
+            delete_from_yaml(
+                k8s_client=self.client,
+                yaml_objects=resources,
+                namespace=self.get_namespace(),
+            )
+        else:
+            self.wrap_errors(objects=resources, callback="delete_custom_from_yaml_object")
+
+
+class BaseKubernetesResourceSensor(BaseSensorOperator):
     template_fields = ("yaml_conf",)
     template_fields_renderers = {"yaml_conf": "yaml"}
 
@@ -78,24 +192,30 @@ class KubernetesResourceBaseOperator(BaseOperator):
         else:
             return self.hook.get_namespace() or "default"
 
+    def get_state(self) -> dict:
+        body = next(yaml.safe_load_all(self.yaml_conf))  # only first element
 
-class KubernetesCreateResourceOperator(KubernetesResourceBaseOperator):
-    """Create a resource in a kubernetes."""
+        dynamic_client = DynamicClient(self.client)
+        api_version = body.get("apiVersion")
+        kind = body.get("kind")
+        namespace = body.get("metadata").get("namespace")
+        if namespace is None:
+            namespace = self.get_namespace()
 
-    def execute(self, context) -> None:
-        create_from_yaml(
-            k8s_client=self.client,
-            yaml_objects=yaml.safe_load_all(self.yaml_conf),
-            namespace=self.get_namespace(),
-        )
+        crd_api = dynamic_client.resources.get(api_version=api_version, kind=kind)
+        rst: ResourceInstance = crd_api.get(body=body, namespace=namespace)
+        return rst.to_dict()
 
 
-class KubernetesDeleteResourceOperator(KubernetesResourceBaseOperator):
-    """Delete a resource in a kubernetes."""
+class JobKubernetesResourceSensor(BaseKubernetesResourceSensor):
+    def poke(self, context: Context) -> bool:
+        state = self.get_state()
+        job_status = state["items"][0]["status"]["jobStatus"]
 
-    def execute(self, context) -> None:
-        delete_from_yaml(
-            k8s_client=self.client,
-            yaml_objects=yaml.safe_load_all(self.yaml_conf),
-            namespace=self.get_namespace(),
-        )
+        self.log.info("jobStatus : %s", job_status)
+
+        if job_status == "SUCCEEDED":
+            return True
+        if job_status in ["STOPPED", "FAILED"]:
+            raise AirflowFailException("Job error, status is : %s", job_status)
+        return False
