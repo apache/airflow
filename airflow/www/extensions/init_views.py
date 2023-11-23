e0bb249be496cb804368f657372f6c6fd076638d
Ulada Zakharava <Vlada_Zakharava@epam.com>
2023-11-23 14:33:45 +0000

    Update methods to use Connexion v3, Ginucorn command and encoding

--- airflow/www/extensions/init_views.py
+++ airflow/www/extensions/init_views.py
@@ -24,6 +24,7 @@ from pathlib import Path
 from typing import TYPE_CHECKING
 
 import connexion
+import starlette.exceptions
 from connexion import ProblemException, Resolver
 from connexion.options import SwaggerUIOptions
 from connexion.problem import problem
