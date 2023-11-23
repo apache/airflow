cf016922601c60238ce48d5baa251445f7087159
Ulada Zakharava <Vlada_Zakharava@epam.com>
2023-11-23 14:33:45 +0000

    Update methods to use Connexion v3, Ginucorn command and encoding

--- airflow/auth/managers/base_auth_manager.py
+++ airflow/auth/managers/base_auth_manager.py
@@ -33,7 +33,7 @@ from airflow.utils.log.logging_mixin import LoggingMixin
 from airflow.utils.session import NEW_SESSION, provide_session
 
 if TYPE_CHECKING:
-    from flask import Blueprint
+    import connexion
     from flask_appbuilder.menu import MenuItem
     from sqlalchemy.orm import Session
 
@@ -81,7 +81,7 @@ class BaseAuthManager(LoggingMixin):
         """
         return []
 
-    def get_api_endpoints(self) -> None | Blueprint:
+    def get_api_endpoints(self, connexion_app: connexion.FlaskApp) -> connexion.apps.flask.FlaskApi:
         """Return API endpoint(s) definition for the auth manager."""
         return None
 
