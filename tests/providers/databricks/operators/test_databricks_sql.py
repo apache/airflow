c653798ad3180bbcfada20b0b440c519593882ed
Joffrey Bienvenu <joffrey.bienvenu@infrabel.be>
2023-12-13 22:38:04 +0100

    fix: add `return_serializable` to databricks operator tests

--- tests/providers/databricks/operators/test_databricks_sql.py
+++ tests/providers/databricks/operators/test_databricks_sql.py
@@ -133,6 +133,7 @@ def test_exec_success(sql, return_last, split_statement, hook_results, hook_desc
         db_mock_class.assert_called_once_with(
             DEFAULT_CONN_ID,
             http_path=None,
+            return_serializable=True,
             session_configuration=None,
             sql_endpoint_name=None,
             http_headers=None,
@@ -276,6 +277,7 @@ def test_exec_write_file(
         db_mock_class.assert_called_once_with(
             DEFAULT_CONN_ID,
             http_path=None,
+            return_serializable=True,
             session_configuration=None,
             sql_endpoint_name=None,
             http_headers=None,
