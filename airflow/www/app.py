ce71896deba21d09b6ef77ed2a5dddd17d6e16f6
y.yoshida5 <39612448+yo1956@users.noreply.github.com>
2023-12-24 14:56:16 +0900

    add flask config: max content length

--- airflow/www/app.py
+++ airflow/www/app.py
@@ -75,6 +75,9 @@ def create_app(config=None, testing=False):
 
     flask_app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(minutes=settings.get_session_lifetime_config())
 
+    limit_in_mb = 1
+    flask_app.config["MAX_CONTENT_LENGTH"] = limit_in_mb * 1024 * 1024
+
     webserver_config = conf.get_mandatory_value("webserver", "config_file")
     # Enable customizations in webserver_config.py to be applied via Flask.current_app.
     with flask_app.app_context():
