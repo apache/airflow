863737ed6e8663a2a916467c793afd3e5dc86bb3
Rahul Vats <rah.sharma11@gmail.com>
2024-01-04 17:38:15 +0530

    fixing static check

--- tests/system/providers/weaviate/example_weaviate_openai.py
+++ tests/system/providers/weaviate/example_weaviate_openai.py
@@ -1,7 +1,26 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+from __future__ import annotations
+
 import json
 from pathlib import Path
 
 import pendulum
+
 from airflow.decorators import dag, setup, task, teardown
 from airflow.providers.openai.operators.openai import OpenAIEmbeddingOperator
 from airflow.providers.weaviate.hooks.weaviate import WeaviateHook
@@ -106,3 +125,9 @@ def example_weaviate_openai():
 
 
 example_weaviate_openai()
+
+
+from tests.system.utils import get_test_run  # noqa: E402
+
+# Needed to run the example DAG with pytest (see: tests/system/README.md#run_via_pytest)
+test_run = get_test_run(dag)
