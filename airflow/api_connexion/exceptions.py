823c5fbd3e19856c92c59dac3daac3b9bb2c88d9
Maksim Yermakou <maksimy@google.com>
2024-01-09 15:00:43 +0000

    Fix problem with static checks

--- airflow/api_connexion/exceptions.py
+++ airflow/api_connexion/exceptions.py
@@ -17,7 +17,7 @@
 from __future__ import annotations
 
 from http import HTTPStatus
-from typing import Any, TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 from connexion import ProblemException, problem
 
