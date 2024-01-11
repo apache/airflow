8f14f45b10437ee349c1ea842445ca184179f01b
gaurav7261 <142777151+gaurav7261@users.noreply.github.com>
2024-01-11 20:21:59 +0530

    Update airflow/providers/databricks/operators/databricks.py
    Co-authored-by: Andrey Anshin <Andrey.Anshin@taragol.is>

--- airflow/providers/databricks/operators/databricks.py
+++ airflow/providers/databricks/operators/databricks.py
@@ -91,8 +91,8 @@ def _handle_databricks_operator_execution(operator, hook, log, context) -> None:
                     if isinstance(operator, DatabricksRunNowOperator) and operator.repair_run:
                         operator.repair_run = False
                         log.warning(
-                            error_message + "but since repair run is set, repairing the run with all "
-                            "failed tasks"
+                            "%s but since repair run is set, repairing the run with all failed tasks",
+                            error_message
                         )
 
                         latest_repair_id = hook.get_latest_repair_id(operator.run_id)
