e1d407f05d75a5803ffc80a64adb6b034c6873cf
Anush008 <anushshetty90@gmail.com>
2024-01-16 11:54:58 +0530

    feat: Qdrant Provider

---
+++ docs/apache-airflow-providers-qdrant/index.rst
@@ -0,0 +1,98 @@
+
+ .. Licensed to the Apache Software Foundation (ASF) under one
+    or more contributor license agreements.  See the NOTICE file
+    distributed with this work for additional information
+    regarding copyright ownership.  The ASF licenses this file
+    to you under the Apache License, Version 2.0 (the
+    "License"); you may not use this file except in compliance
+    with the License.  You may obtain a copy of the License at
+
+ ..   http://www.apache.org/licenses/LICENSE-2.0
+
+.. Unless required by applicable law or agreed to in writing,
+    software distributed under the License is distributed on an
+    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, either express or implied.  See the License for the
+    specific language governing permissions and limitations
+    under the License.
+
+``apache-airflow-providers-qdrant``
+======================================
+
+
+.. toctree::
+    :hidden:
+    :maxdepth: 1
+    :caption: Basics
+
+    Home <self>
+    Changelog <changelog>
+    Security <security>
+
+.. toctree::
+    :hidden:
+    :maxdepth: 1
+    :caption: Guides
+
+    Connection types <connections>
+    Operators <operators/qdrant>
+
+
+.. toctree::
+    :hidden:
+    :maxdepth: 1
+    :caption: Commits
+
+    Detailed list of commits <commits>
+
+
+.. toctree::
+    :hidden:
+    :maxdepth: 1
+    :caption: Resources
+
+    Python API <_api/airflow/providers/qdrant/index>
+    PyPI Repository <https://pypi.org/project/apache-airflow-providers-qdrant/>
+    Installing from sources <installing-providers-from-sources>
+
+.. toctree::
+    :hidden:
+    :maxdepth: 1
+    :caption: System tests
+
+    System Tests <_api/tests/system/providers/qdrant/index>
+
+Package apache-airflow-providers-qdrant
+-----------------------------------------
+
+`Qdrant <https://qdrant.tech/>`__
+
+
+Release: 1.0.0
+
+Provider package
+----------------
+
+This is a provider package for ``Qdrant`` APIs. All classes for this provider package
+are in ``airflow.providers.qdrant`` python module.
+
+Installation
+------------
+
+You can install this package on top of an existing Airflow 2 installation (see ``Requirements`` below)
+for the minimum Airflow version supported) via
+``pip install apache-airflow-providers-qdrant``
+
+
+Requirements
+------------
+
+The minimum Apache Airflow version supported by this provider package is ``2.5.0``.
+
+
+===================  ==================
+PIP package          Version required
+===================  ==================
+``apache-airflow``    ``>=2.5.0``
+``qdrant_client``     ``>=1.7.0``
+===================  ==================
---
+++ tests/providers/qdrant/operators/test_qdrant.py
@@ -0,0 +1,84 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+from __future__ import annotations
+
+from datetime import datetime
+from unittest.mock import Mock, patch
+
+import pytest
+
+from airflow.models import DAG
+from airflow.providers.qdrant.operators.qdrant import QdrantIngestOperator
+
+
+class MockQdrantHook:
+    """Mocking QdrantHook to avoid actual external calls"""
+
+    def create_collection(self, *args, **kwargs):
+        pass
+
+    @staticmethod
+    def upsert(*args, **kwargs):
+        return Mock()
+
+
+@pytest.fixture
+def dummy_dag():
+    """Fixture to provide a dummy Airflow DAG for testing."""
+    return DAG(dag_id="test_dag", start_date=datetime(2023, 9, 29))
+
+
+class TestQdrantIngestOperator:
+    def test_operator_execution(self, dummy_dag):
+        """
+        Test the execution of the QdrantIngestOperator.
+        Ensures that the upsert method on the hook is correctly called.
+        """
+        vectors = [[0.732, 0.611, 0.289], [0.217, 0.526, 0.416], [0.326, 0.483, 0.376]]
+        ids = [32, 21, "b626f6a9-b14d-4af9-b7c3-43d8deb719a6"]
+        payload = [{"meta": "data"}, {"meta": "data_2"}, {"meta": "data_3", "extra": "data"}]
+
+        task = QdrantIngestOperator(
+            task_id="ingest_vectors",
+            collection_name="test_collection",
+            vectors=vectors,
+            ids=ids,
+            payload=payload,
+            wait=False,
+            max_retries=1,
+            parallel=3,
+            dag=dummy_dag,
+        )
+
+        with patch(
+            "airflow.providers.qdrant.operators.qdrant.QdrantIngestOperator.hook",
+            new_callable=MockQdrantHook,
+        ) as mock_hook_instance:
+            mock_hook_instance.upsert = Mock()
+
+            task.execute(context={})
+            mock_hook_instance.upsert.assert_called_once_with(
+                collection_name="test_collection",
+                vectors=vectors,
+                ids=ids,
+                payload=payload,
+                batch_size=64,
+                wait=False,
+                max_retries=1,
+                parallel=3,
+                method=None,
+            )
