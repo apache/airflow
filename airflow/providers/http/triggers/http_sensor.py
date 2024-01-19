facb5eee81c1badfda10a4007903444db739e12f
vatsrahul1001 <rah.sharma11@gmail.com>
2024-01-19 20:13:35 +0530

    adding http sensor triggerer

--- airflow/providers/http/triggers/http_sensor.py
+++ airflow/providers/http/triggers/http_sensor.py
@@ -1,22 +1,38 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+from __future__ import annotations
+
 import asyncio
-from typing import Any, AsyncIterator, Dict, Optional, Tuple, Union
+from typing import Any, AsyncIterator
 
 from airflow.exceptions import AirflowException
-from airflow.triggers.base import BaseTrigger, TriggerEvent
-
 from airflow.providers.http.hooks.http import HttpAsyncHook
+from airflow.triggers.base import BaseTrigger, TriggerEvent
 
 
 class HttpSensorTrigger(BaseTrigger):
     """
-    A trigger that fires when the request to a URL returns a non-404 status code
+    A trigger that fires when the request to a URL returns a non-404 status code.
 
     :param endpoint: The relative part of the full url
     :param http_conn_id: The HTTP Connection ID to run the sensor against
     :param method: The HTTP request method to use
     :param data: payload to be uploaded or aiohttp parameters
     :param headers: The HTTP headers to be added to the GET request
-    :type headers: a dictionary of string key/value pairs
     :param extra_options: Additional kwargs to pass when creating a request.
         For example, ``run(json=obj)`` is passed as ``aiohttp.ClientSession().get(json=obj)``
     :param poke_interval: Time to sleep using asyncio
@@ -24,12 +40,12 @@ class HttpSensorTrigger(BaseTrigger):
 
     def __init__(
         self,
-        endpoint: str,
+        endpoint: str | None = None,
         http_conn_id: str = "http_default",
         method: str = "GET",
-        data: Optional[Union[Dict[str, Any], str]] = None,
-        headers: Optional[Dict[str, Any]] = None,
-        extra_options: Optional[Dict[str, Any]] = None,
+        data: dict[str, Any] | str | None = None,
+        headers: dict[str, str] | None = None,
+        extra_options: dict[str, Any] | None = None,
         poke_interval: float = 5.0,
     ):
         super().__init__()
@@ -41,7 +57,7 @@ class HttpSensorTrigger(BaseTrigger):
         self.http_conn_id = http_conn_id
         self.poke_interval = poke_interval
 
-    def serialize(self) -> Tuple[str, Dict[str, Any]]:
+    def serialize(self) -> tuple[str, dict[str, Any]]:
         """Serializes HttpTrigger arguments and classpath."""
         return (
             "airflow.providers.http.triggers.http_sensor.HttpSensorTrigger",
@@ -55,12 +71,8 @@ class HttpSensorTrigger(BaseTrigger):
             },
         )
 
-    async def run(self) -> AsyncIterator["TriggerEvent"]:
-        """
-        Makes a series of asynchronous http calls via an http hook. It yields a Trigger if
-        response is a 200 and run_state is successful, will retry the call up to the retry limit
-        if the error is 'retryable', otherwise it throws an exception.
-        """
+    async def run(self) -> AsyncIterator[TriggerEvent]:
+        """Makes a series of asynchronous http calls via an http hook."""
         hook = self._get_async_hook()
         while True:
             try:
