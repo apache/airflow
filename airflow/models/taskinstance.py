fa4126750151f8d8b8359c0df376e25b0016020e
Jake Kramer <jkramer@ginkgobioworks.com>
2024-01-24 16:33:07 -0500

    Remove superfluous `@Sentry.enrich_errors` from `TaskInstance._schedule_downstream_tasks`
    A recent refactoring broke the contract `enrich_errors` expect. This decorator
    is being removed because it's not desired to log Sentry errors for scheduling issues.

--- airflow/models/taskinstance.py
+++ airflow/models/taskinstance.py
@@ -3227,7 +3227,6 @@ class TaskInstance(Base, LoggingMixin):
 
     @classmethod
     @internal_api_call
-    @Sentry.enrich_errors
     @provide_session
     def _schedule_downstream_tasks(
         cls,
