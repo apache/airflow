5d60cce1c8762b864c69486437545e1025f7ce33
Tzu-ping Chung <uranusjr@gmail.com>
2024-01-25 09:44:29 +0800

    Add URI declarations to providers

--- airflow/datasets/__init__.py
+++ airflow/datasets/__init__.py
@@ -14,35 +14,58 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+
 from __future__ import annotations
 
 import os
-from typing import Any, ClassVar
+from typing import TYPE_CHECKING, Any, Callable, ClassVar
 from urllib.parse import urlsplit
 
 import attr
+
+from airflow.providers_manager import ProvidersManager
+
+if TYPE_CHECKING:
+    from urllib.parse import SplitResult
+
+
+def normalize_noop(parts: SplitResult) -> SplitResult:
+    return parts
+
+
+def _get_uri_normalizer(scheme: str) -> Callable[[SplitResult], SplitResult] | None:
+    if scheme == "file":
+        return normalize_noop
+    return ProvidersManager().dataset_uri_handlers.get(scheme)
+
+
+def _sanitize_uri(uri: str) -> str:
+    if uri.isspace():
+        raise ValueError("Dataset URI cannot be just whitespace")
+    try:
+        uri.encode("ascii")
+    except UnicodeEncodeError:
+        raise ValueError("Dataset URI must only consist of ASCII characters") from None
+    parsed = urlsplit(uri)
+    if (normalized_scheme := parsed.scheme.lower()) == "airflow":
+        raise ValueError("Dataset scheme 'airflow' is reserved")
+    parsed = parsed._replace(scheme=normalized_scheme, fragment="")
+    if (normalizer := _get_uri_normalizer(normalized_scheme)) is not None:
+        parsed = normalizer(parsed)
+    return parsed.geturl()
 
 
 @attr.define()
 class Dataset(os.PathLike):
     """A Dataset is used for marking data dependencies between workflows."""
 
-    uri: str = attr.field(validator=[attr.validators.min_len(1), attr.validators.max_len(3000)])
+    uri: str = attr.field(
+        converter=_sanitize_uri,
+        validator=[attr.validators.min_len(1), attr.validators.max_len(3000)],
+    )
     extra: dict[str, Any] | None = None
 
     __version__: ClassVar[int] = 1
-
-    @uri.validator
-    def _check_uri(self, attr, uri: str):
-        if uri.isspace():
-            raise ValueError(f"{attr.name} cannot be just whitespace")
-        try:
-            uri.encode("ascii")
-        except UnicodeEncodeError:
-            raise ValueError(f"{attr.name!r} must be ascii")
-        parsed = urlsplit(uri)
-        if parsed.scheme and parsed.scheme.lower() == "airflow":
-            raise ValueError(f"{attr.name!r} scheme `airflow` is reserved")
 
     def __fspath__(self):
         return self.uri
