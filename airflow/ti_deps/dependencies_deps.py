13811aa8f14fbd9b14dcd3e7d20bcb8291154077
Xiaodong DENG <xddeng@apache.org>
2024-02-01 21:00:51 -0800

    Fixup

--- airflow/ti_deps/dependencies_deps.py
+++ airflow/ti_deps/dependencies_deps.py
@@ -20,7 +20,6 @@ from airflow.ti_deps.dependencies_states import (
     BACKFILL_QUEUEABLE_STATES,
     QUEUEABLE_STATES,
     RUNNABLE_STATES,
-    SCHEDULEABLE_STATES,
 )
 from airflow.ti_deps.deps.dag_ti_slots_available_dep import DagTISlotsAvailableDep
 from airflow.ti_deps.deps.dag_unpaused_dep import DagUnpausedDep
