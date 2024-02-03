fca30bb5c632c53ad688ec9d6a06c76ad726b02d
Utkarsh Sharma <utkarsharma2@gmail.com>
2024-02-04 05:27:54 +0530

    Add deferrable param in SFTPSensor (#37117)
    * Add deferrable param in SFTPSensor
    
    * Fix static checks
    
    * Fixed duplicate task_id issue
    
    * Add missing tests
    
    * Fix failing test cases
    
    * Fix provider.yaml static check
    
    * Update airflow/providers/sftp/sensors/sftp.py
    
    Co-authored-by: Pankaj Koti <pankajkoti699@gmail.com>
    
    * Update airflow/providers/sftp/sensors/sftp.py
    
    * Removed get_files_by_pattern
    
    * Change the name for rst marker - howto_sensor_sftp_deferrable
    
    * Handle missing file case for triggers
    
    ---------
    
    Co-authored-by: Pankaj Koti <pankajkoti699@gmail.com>
    (cherry picked from commit f24a03709eecbda87ed794cee567806e51c3a21f)

--- tests/system/providers/sftp/example_sftp_sensor.py
+++ tests/system/providers/sftp/example_sftp_sensor.py
@@ -76,8 +76,18 @@ with DAG(
     sftp_with_operator = SFTPSensor(task_id="sftp_operator", path=FULL_FILE_PATH, poke_interval=10)
     # [END howto_operator_sftp_sensor]
 
+    # [START howto_sensor_sftp_deferrable]
+    sftp_sensor_with_async = SFTPSensor(
+        task_id="sftp_operator_async", path=FULL_FILE_PATH, poke_interval=10, deferrable=True
+    )
+    # [END howto_sensor_sftp_deferrable]
+
     remove_file_task_start >> sleep_task >> create_decoy_file_task
-    remove_file_task_start >> [sftp_with_operator, sftp_with_sensor] >> remove_file_task_end
+    (
+        remove_file_task_start
+        >> [sftp_with_operator, sftp_sensor_with_async, sftp_with_sensor]
+        >> remove_file_task_end
+    )
 
     from tests.system.utils.watcher import watcher
 
