96b0cfe3887ccf87ea16bf40b31dc5d7e1af08a4
Jarek Potiuk <jarek@potiuk.com>
2024-02-07 22:43:10 +0100

    Upgrade to FAB 4.3.11 (#37233)
    (cherry picked from commit e99cfbbd51515fa947c16912acebbaa7ed816e8a)

--- dev/breeze/tests/test_packages.py
+++ dev/breeze/tests/test_packages.py
@@ -114,12 +114,12 @@ def test_get_removed_providers():
 
 def test_get_suspended_provider_ids():
     # Modify it every time we suspend/resume provider
-    assert ["apache.sqoop", "daskexecutor", "plexus"] == get_suspended_provider_ids()
+    assert ["daskexecutor", "plexus"] == get_suspended_provider_ids()
 
 
 def test_get_suspended_provider_folders():
     # Modify it every time we suspend/resume provider
-    assert ["apache/sqoop", "daskexecutor", "plexus"] == get_suspended_provider_folders()
+    assert ["daskexecutor", "plexus"] == get_suspended_provider_folders()
 
 
 @pytest.mark.parametrize(
