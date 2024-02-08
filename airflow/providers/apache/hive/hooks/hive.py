08036e5df5ae3ec9f600219361f86a1a3e8e9d19
Andrey Anshin <Andrey.Anshin@taragol.is>
2024-02-09 02:50:09 +0400

    D401 Support in Providers (simple) (#37258)

--- airflow/providers/apache/hive/hooks/hive.py
+++ airflow/providers/apache/hive/hooks/hive.py
@@ -119,7 +119,7 @@ class HiveCliHook(BaseHook):
 
     @classmethod
     def get_connection_form_widgets(cls) -> dict[str, Any]:
-        """Returns connection widgets to add to connection form."""
+        """Return connection widgets to add to Hive Client Wrapper connection form."""
         from flask_appbuilder.fieldwidgets import BS3TextFieldWidget
         from flask_babel import lazy_gettext
         from wtforms import BooleanField, StringField
@@ -134,7 +134,7 @@ class HiveCliHook(BaseHook):
 
     @classmethod
     def get_ui_field_behaviour(cls) -> dict[str, Any]:
-        """Returns custom field behaviour."""
+        """Return custom UI field behaviour for Hive Client Wrapper connection."""
         return {
             "hidden_fields": ["extra"],
             "relabeling": {},
