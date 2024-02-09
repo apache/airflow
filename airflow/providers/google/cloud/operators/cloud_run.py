34f741c6ef0dd875f06214e59837db1cb2410b21
Jostein Gogstad <jostein.gogstad@gmail.com>
2024-02-09 22:34:11 +0100

    cloud run uses the configured project id when deferred

--- airflow/providers/google/cloud/operators/cloud_run.py
+++ airflow/providers/google/cloud/operators/cloud_run.py
@@ -333,7 +333,7 @@ class CloudRunExecuteJobOperator(GoogleCloudBaseOperator):
 
         hook: CloudRunHook = CloudRunHook(self.gcp_conn_id, self.impersonation_chain)
 
-        job = hook.get_job(job_name=event["job_name"], region=self.region)
+        job = hook.get_job(job_name=event["job_name"], region=self.region, project_id=self.project_id)
         return Job.to_dict(job)
 
     def _fail_if_execution_failed(self, execution: Execution):
