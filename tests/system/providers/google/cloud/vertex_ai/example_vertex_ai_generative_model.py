77d54b5c5d85919fba8f293bd1822e82c6db016a
Christian Yarros <yarros@google.com>
2024-02-22 15:30:46 +0000

    update system test to standardize gcp project/region usage

--- tests/system/providers/google/cloud/vertex_ai/example_vertex_ai_generative_model.py
+++ tests/system/providers/google/cloud/vertex_ai/example_vertex_ai_generative_model.py
@@ -21,16 +21,24 @@ Example Airflow DAG for Google Vertex AI Generative Model prompting.
 """
 from __future__ import annotations
 
+import os
 from datetime import datetime
 
-from airflow import models
+from airflow.models.dag import DAG
 from airflow.providers.google.cloud.operators.vertex_ai.generative_model import (
     PromptLanguageModelOperator,
     PromptMultimodalModelOperator,
 )
 
-with models.DAG(
-    dag_id="example_vertex_ai_generative_model_dag",
+PROJECT_ID = os.environ.get("SYSTEM_TESTS_GCP_PROJECT", "default")
+DAG_ID = "example_vertex_ai_generative_model_dag"
+REGION = "us-central1"
+PROMPT = "In 10 words or less, why is Apache Airflow amazing?"
+LANGUAGE_MODEL = "text-bison"
+MULTIMODAL_MODAL = "gemini-pro"
+
+with DAG(
+    dag_id=DAG_ID,
     description="Sample DAG with generative models.",
     schedule="@once",
     start_date=datetime(2024, 1, 1),
@@ -40,25 +48,25 @@ with models.DAG(
     # [START how_to_cloud_vertex_ai_prompt_language_model_operator]
     prompt_language_model_task = PromptLanguageModelOperator(
         task_id="prompt_language_model_task",
-        project_id="your-project",
-        location="us-central1",
-        prompt="In 10 words or less, what is Apache Airflow?",
-        pretrained_model="text-bison",
+        project_id=PROJECT_ID,
+        location=REGION,
+        prompt=PROMPT,
+        pretrained_model=LANGUAGE_MODEL,
     )
     # [END how_to_cloud_vertex_ai_prompt_language_model_operator]
 
     # [START how_to_cloud_vertex_ai_prompt_multimodal_model_operator]
     prompt_multimodal_model_task = PromptMultimodalModelOperator(
         task_id="generative_model_task",
-        project_id="your-project",
-        location="us-central1",
-        prompt="In 10 words or less, what is Apache Airflow?",
-        pretrained_model="gemini-pro",
+        project_id=PROJECT_ID,
+        location=REGION,
+        prompt=PROMPT,
+        pretrained_model=MULTIMODAL_MODAL,
     )
     # [END how_to_cloud_vertex_ai_prompt_multimodal_model_operator]
 
 
-from tests.system.utils import get_test_run
+from tests.system.utils import get_test_run  # noqa: E402
 
 # Needed to run the example DAG with pytest (see: tests/system/README.md#run_via_pytest)
 test_run = get_test_run(dag)
