90d5dd75af1b90256c853c5f24f5a7d5f18c4047
David Blain <david.blain@infrabel.be>
2024-03-04 13:51:03 +0100

    fix: Re-added wrongly removed unit test

--- tests/providers/microsoft/azure/transfers/test_local_to_adls.py
+++ tests/providers/microsoft/azure/transfers/test_local_to_adls.py
@@ -56,3 +56,22 @@ class TestADLSUploadOperator:
         with pytest.raises(AirflowException) as ctx:
             operator.execute(None)
         assert str(ctx.value) == "Recursive glob patterns using `**` are not supported"
+
+    @mock.patch("airflow.providers.microsoft.azure.transfers.local_to_adls.AzureDataLakeHook")
+    def test_extra_options_is_passed(self, mock_hook):
+        operator = LocalFilesystemToADLSOperator(
+            task_id=TASK_ID,
+            local_path=LOCAL_PATH,
+            remote_path=REMOTE_PATH,
+            extra_upload_options={"run": False},
+        )
+        operator.execute(None)
+        mock_hook.return_value.upload_file.assert_called_once_with(
+            local_path=LOCAL_PATH,
+            remote_path=REMOTE_PATH,
+            nthreads=64,
+            overwrite=True,
+            buffersize=4194304,
+            blocksize=4194304,
+            run=False,  # extra upload options
+        )
