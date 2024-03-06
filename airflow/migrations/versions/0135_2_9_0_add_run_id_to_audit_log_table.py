305c29b7fe56893180a580c2be11bb614452ec66
Brent Bovenzi <brent.bovenzi@gmail.com>
2024-03-06 16:24:23 -0500

    Merge Log migration files

--- airflow/migrations/versions/0135_2_9_0_add_run_id_to_audit_log_table.py
+++ airflow/migrations/versions/0135_2_9_0_add_run_id_to_audit_log_table.py
@@ -16,7 +16,7 @@
 # specific language governing permissions and limitations
 # under the License.
 
-"""Add run_id to (Audit) log table
+"""Add run_id to (Audit) log table and increase event name length
 
 Revision ID: d75389605139
 Revises: 1fd565369930
@@ -24,6 +24,7 @@ Create Date: 2024-02-29 17:50:03.759967
 
 """
 
+from airflow.migrations.db_types import StringID
 import sqlalchemy as sa
 from alembic import op
 
@@ -35,18 +36,29 @@ branch_labels = None
 depends_on = None
 airflow_version = '2.9.0'
 
-from airflow.migrations.db_types import StringID
 
 def upgrade():
-    """Apply Add run_id to Log."""
+    """Apply Add run_id to Log and increase Log event name length."""
 
     # Note: we could repopulate the run_id of old runs via a join with DagRun on date + dag_id,
     # But this would incur a potentially heavy migration for non-essential changes.
     # Instead, we've chosen to only populate this column from 2.9.0 onwards.
     with op.batch_alter_table("log") as batch_op:
         batch_op.add_column(sa.Column("run_id", StringID(), nullable=True))
+        batch_op.alter_column("event", type_=sa.String(60))
+
 
 def downgrade():
-    """Unapply Add run_id to Log."""
+    """Unapply Add run_id to Log and increase Log event name length."""
     with op.batch_alter_table("log") as batch_op:
         batch_op.drop_column("run_id")
+
+    conn = op.get_bind()
+    if conn.dialect.name == "mssql":
+        with op.batch_alter_table("log") as batch_op:
+            batch_op.drop_index("idx_log_event")
+            batch_op.alter_column("event", type_=sa.String(30), nullable=False)
+            batch_op.create_index("idx_log_event", ["event"])
+    else:
+        with op.batch_alter_table("log") as batch_op:
+            batch_op.alter_column("event", type_=sa.String(30), nullable=False)
