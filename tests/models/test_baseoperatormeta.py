b230f7fdf53605926023200a56618689910fefa2
David Blain <david.blain@infrabel.be>
2024-03-07 13:39:04 +0100

    refactor: Added test case where HelloWorldOperator is called from within an PythonOperator

--- tests/models/test_baseoperatormeta.py
+++ tests/models/test_baseoperatormeta.py
@@ -9,6 +9,7 @@ from airflow import DAG
 from airflow.exceptions import AirflowException
 from airflow.models import TaskInstance, DagRun
 from airflow.models.baseoperator import BaseOperator
+from airflow.operators.python import PythonOperator, task
 from airflow.utils.context import Context
 from airflow.utils.state import DagRunState
 from tests.test_utils.config import conf_vars
@@ -29,16 +30,16 @@ class IterableSession(Session):
 
 class TestBaseOperatorMeta:
     @conf_vars({("core", "unit_test_mode"): "False"})
-    def test_executor_safeguard_when_unauthorized(self):
+    def test_executor_when_called_directly(self):
         with raises(AirflowException, match="Method execute cannot be called from inner!"):
             dag = DAG(dag_id="hello_world")
             context = MagicMock(spec=Context)
 
-            HelloWorldOperator(task_id="task_id", dag=dag).execute(context=context)
+            HelloWorldOperator(task_id="task_id", retries=0, dag=dag).execute(context=context)
 
     @patch("sqlalchemy.orm.Session.__init__")
     @conf_vars({("core", "unit_test_mode"): "False"})
-    def test_executor_safeguard_when_authorized(self, mock_session: MagicMock):
+    def test_executor_when_called_from_decorated_task(self, mock_session: MagicMock):
         session = MagicMock(spec=IterableSession)
         mock_session.return_value = session
         session.__iter__.return_value = iter({})
@@ -51,14 +52,48 @@ class TestBaseOperatorMeta:
         TaskInstance.clear_xcom_data = Mock(return_value=None)
         TaskInstance.get_task_instance = Mock(return_value=None)
 
-        operator = HelloWorldOperator(task_id="hello_operator", dag=dag)
+        operator = HelloWorldOperator(task_id="hello_operator", retries=0, dag=dag)
+
+        @task(task_id="task_id", dag=dag)
+        def say_hello(**context):
+            return operator.execute(context=context)
+
+        assert not operator.called
+
+        task_instance = TaskInstance(task=say_hello(context=context).operator, run_id="run_id")
+        task_instance.task_id = "hello_operator"
+        task_instance.dag_id = "hello_world"
+        task_instance.dag_run = DagRun(run_id="run_id", dag_id="hello_world",
+                                       execution_date=pendulum.now(),
+                                       state=DagRunState.RUNNING)
+        task_instance._run_raw_task(test_mode=True, session=session)
+
+        assert not operator.called
+
+    @patch("sqlalchemy.orm.Session.__init__")
+    @conf_vars({("core", "unit_test_mode"): "False"})
+    def test_executor_when_called_from_task_instance(self, mock_session: MagicMock):
+        session = MagicMock(spec=IterableSession)
+        mock_session.return_value = session
+        session.__iter__.return_value = iter({})
+
+        dag = DAG(dag_id="hello_world")
+        context = {"params": {}}
+        TaskInstance.get_task_instance = Mock(return_value=None)
+        TaskInstance.get_template_context = Mock(return_value=context)
+        TaskInstance.render_templates = Mock(return_value=None)
+        TaskInstance.clear_xcom_data = Mock(return_value=None)
+        TaskInstance.get_task_instance = Mock(return_value=None)
+
+        operator = HelloWorldOperator(task_id="hello_operator", retries=0, dag=dag)
 
         assert not operator.called
 
         task_instance = TaskInstance(task=operator, run_id="run_id")
         task_instance.task_id = "hello_operator"
         task_instance.dag_id = "hello_world"
-        task_instance.dag_run = DagRun(run_id="run_id", dag_id="hello_world", execution_date=pendulum.now(), state=DagRunState.RUNNING)
+        task_instance.dag_run = DagRun(run_id="run_id", dag_id="hello_world", execution_date=pendulum.now(),
+                                       state=DagRunState.RUNNING)
         task_instance._run_raw_task(test_mode=True, session=session)
 
         assert operator.called
