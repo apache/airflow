1783b89f2d5839b46944b827c654ae9e17c48bc6
David Blain <david.blain@infrabel.be>
2024-03-07 19:40:40 +0100

    refactor: Refactored TestBaseOperatorMeta by having a dedicated fixture for TaskInstance

--- tests/models/test_baseoperatormeta.py
+++ tests/models/test_baseoperatormeta.py
@@ -1,15 +1,15 @@
 from typing import Any
-from unittest.mock import Mock, patch, MagicMock
+from unittest.mock import patch, MagicMock
 
 import pendulum
-from pytest import raises
+import pytest
 from sqlalchemy.orm import Session
 
 from airflow import DAG
 from airflow.exceptions import AirflowException
-from airflow.models import TaskInstance, DagRun
+from airflow.models import TaskInstance, DagRun, Operator
 from airflow.models.baseoperator import BaseOperator
-from airflow.operators.python import PythonOperator, task
+from airflow.operators.python import task
 from airflow.utils.context import Context
 from airflow.utils.state import DagRunState
 from tests.test_utils.config import conf_vars
@@ -28,10 +28,35 @@ class IterableSession(Session):
         pass
 
 
+@pytest.fixture
+def mock_task_instance_methods(mocker):
+    mocker.patch.object(TaskInstance, "get_task_instance", return_value=None)
+    mocker.patch.object(TaskInstance, "get_template_context", return_value={"params": {}})
+    mocker.patch.object(TaskInstance, "render_templates", return_value=None)
+    mocker.patch.object(TaskInstance, "clear_xcom_data", return_value=None)
+    mocker.patch.object(TaskInstance, "check_and_change_state_before_execution", return_value=True)
+
+
 class TestBaseOperatorMeta:
+    def setup_method(self):
+        HelloWorldOperator.called = False
+
+    @classmethod
+    def create_task_instance(cls, operator: Operator) -> TaskInstance:
+        task_instance = TaskInstance(task=operator, run_id="run_id")
+        task_instance.task_id = "hello_operator"
+        task_instance.dag_id = "hello_world"
+        task_instance.dag_run = DagRun(
+            run_id="run_id",
+            dag_id="hello_world",
+            execution_date=pendulum.now(),
+            state=DagRunState.RUNNING,
+        )
+        return task_instance
+
     @conf_vars({("core", "unit_test_mode"): "False"})
     def test_executor_when_called_directly(self):
-        with raises(AirflowException, match="Method execute cannot be called from inner!"):
+        with pytest.raises(AirflowException, match="Method execute cannot be called from inner!"):
             dag = DAG(dag_id="hello_world")
             context = MagicMock(spec=Context)
 
@@ -39,19 +64,13 @@ class TestBaseOperatorMeta:
 
     @patch("sqlalchemy.orm.Session.__init__")
     @conf_vars({("core", "unit_test_mode"): "False"})
-    def test_executor_when_called_from_decorated_task(self, mock_session: MagicMock):
+    def test_executor_when_called_from_decorated_task(self, mock_session: MagicMock, mock_task_instance_methods):
         session = MagicMock(spec=IterableSession)
         mock_session.return_value = session
         session.__iter__.return_value = iter({})
 
         dag = DAG(dag_id="hello_world")
-        context = {"params": {}}
-        TaskInstance.get_task_instance = Mock(return_value=None)
-        TaskInstance.get_template_context = Mock(return_value=context)
-        TaskInstance.render_templates = Mock(return_value=None)
-        TaskInstance.clear_xcom_data = Mock(return_value=None)
-        TaskInstance.get_task_instance = Mock(return_value=None)
-
+        context = MagicMock(spec=Context)
         operator = HelloWorldOperator(task_id="hello_operator", retries=0, dag=dag)
 
         @task(task_id="task_id", dag=dag)
@@ -60,40 +79,24 @@ class TestBaseOperatorMeta:
 
         assert not operator.called
 
-        task_instance = TaskInstance(task=say_hello(context=context).operator, run_id="run_id")
-        task_instance.task_id = "hello_operator"
-        task_instance.dag_id = "hello_world"
-        task_instance.dag_run = DagRun(run_id="run_id", dag_id="hello_world",
-                                       execution_date=pendulum.now(),
-                                       state=DagRunState.RUNNING)
-        task_instance._run_raw_task(test_mode=True, session=session)
+        task_instance = self.create_task_instance(operator=say_hello(context=context).operator)
+        task_instance.run(test_mode=True, session=session)
 
         assert not operator.called
 
     @patch("sqlalchemy.orm.Session.__init__")
     @conf_vars({("core", "unit_test_mode"): "False"})
-    def test_executor_when_called_from_task_instance(self, mock_session: MagicMock):
+    def test_executor_when_called_from_task_instance(self, mock_session: MagicMock, mock_task_instance_methods):
         session = MagicMock(spec=IterableSession)
         mock_session.return_value = session
         session.__iter__.return_value = iter({})
 
         dag = DAG(dag_id="hello_world")
-        context = {"params": {}}
-        TaskInstance.get_task_instance = Mock(return_value=None)
-        TaskInstance.get_template_context = Mock(return_value=context)
-        TaskInstance.render_templates = Mock(return_value=None)
-        TaskInstance.clear_xcom_data = Mock(return_value=None)
-        TaskInstance.get_task_instance = Mock(return_value=None)
-
         operator = HelloWorldOperator(task_id="hello_operator", retries=0, dag=dag)
 
         assert not operator.called
 
-        task_instance = TaskInstance(task=operator, run_id="run_id")
-        task_instance.task_id = "hello_operator"
-        task_instance.dag_id = "hello_world"
-        task_instance.dag_run = DagRun(run_id="run_id", dag_id="hello_world", execution_date=pendulum.now(),
-                                       state=DagRunState.RUNNING)
-        task_instance._run_raw_task(test_mode=True, session=session)
+        task_instance = self.create_task_instance(operator=operator)
+        task_instance.run(test_mode=True, session=session)
 
         assert operator.called
