c45ae985f1659183b7b121420c836dc877111e11
Pankaj <pankaj.singh@astronomer.io>
2024-03-12 23:04:04 +0530

    Update tests

--- tests/providers/cncf/kubernetes/triggers/test_pod.py
+++ tests/providers/cncf/kubernetes/triggers/test_pod.py
@@ -187,6 +187,7 @@ class TestKubernetesPodTrigger:
                 "namespace": "default",
                 "name": "test-pod-name",
                 "message": "Container state failed",
+                "last_log_time": None,
             }
         )
         actual_event = await trigger.run().asend(None)
