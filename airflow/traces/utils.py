782cf1cc8f67dbc7d2d7018b195b4c3f0f56283e
howardyoo <howardyoo@gmail.com>
2024-03-15 00:11:30 -0500

    fixes on generated static files

--- airflow/traces/utils.py
+++ airflow/traces/utils.py
@@ -84,9 +84,9 @@ def parse_tracestate(tracestate_str: str | None = None) -> dict:
 
 def is_valid_trace_id(trace_id: str) -> bool:
     """Check whether trace id is valid."""
-    return trace_id and len(trace_id) == 34 and int(trace_id, 16) != 0
+    return trace_id is not None and len(trace_id) == 34 and int(trace_id, 16) != 0
 
 
 def is_valid_span_id(span_id: str) -> bool:
     """Check whether span id is valid."""
-    return span_id and len(span_id) == 18 and int(span_id, 16) != 0
+    return span_id is not None and len(span_id) == 18 and int(span_id, 16) != 0
