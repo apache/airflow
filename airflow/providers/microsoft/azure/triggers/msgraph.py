6a14ebe01936ca31ab188ab0fcbb40ba1960c3ba
David Blain <david.blain@infrabel.be>
2024-03-15 14:13:15 +0100

    refactor: Removed docstring from CallableResponseHandler

--- airflow/providers/microsoft/azure/triggers/msgraph.py
+++ airflow/providers/microsoft/azure/triggers/msgraph.py
@@ -88,12 +88,6 @@ class ResponseSerializer:
 
 
 class CallableResponseHandler(ResponseHandler):
-    """
-    CallableResponseHandler executes the passed callable_function with response as parameter.
-
-    :param callable_function: Function which allows you to handle the response before returning.
-    """
-
     def __init__(
         self,
         callable_function: Callable[[NativeResponseType, dict[str, ParsableFactory | None] | None], Any],
