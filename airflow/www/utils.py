apache/airflow

commit 43d7f6d3da0192c3abd07c9f0dab54e6da605621

Fix task instances list link (#38096)

airflow/www/utils.py
@@ -422,6 +422,7 @@ def task_instance_link(attr):
     """Generate a URL to the Graph view for a TaskInstance."""
     dag_id = attr.get("dag_id")
     task_id = attr.get("task_id")
+    run_id = attr.get("run_id")
     execution_date = attr.get("dag_run.execution_date") or attr.get("execution_date") or timezone.utcnow()
     url = url_for(
         "Airflow.task",
@@ -431,13 +432,18 @@ def task_instance_link(attr):
         map_index=attr.get("map_index", -1),
     )
     url_root = url_for(
-        "Airflow.graph", dag_id=dag_id, root=task_id, execution_date=execution_date.isoformat()
+        "Airflow.grid",
+        dag_id=dag_id,
+        task_id=task_id,
+        dag_run_id=run_id,
+        tab="graph",
+        map_index=attr.get("map_index", -1),
     )
     return Markup(
         """
         <span style="white-space: nowrap;">
         <a href="{url}">{task_id}</a>
-        <a href="{url_root}" title="Filter on this task and upstream">
+        <a href="{url_root}" title="Filter on this task">
         <span class="material-icons" style="margin-left:0;"
             aria-hidden="true">filter_alt</span>
         </a>

commit 12aa181c069d3e61d8589e0bee6334f0e0cde707

Fix filtered TI links
We have links to the graph, filtered down to a specific TI. This was
accidentally broken in #38096.

airflow/www/utils.py
@@ -435,6 +435,7 @@ def task_instance_link(attr):
         "Airflow.grid",
         dag_id=dag_id,
         task_id=task_id,
+        root=task_id,
         dag_run_id=run_id,
         tab="graph",
         map_index=attr.get("map_index", -1),