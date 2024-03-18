96f38f1f6a66e0c73643852842048087fff4e38b
satoshi-sh <satoss1108@gmail.com>
2024-03-18 18:37:23 -0500

    Reveted auto-formatted code.

--- airflow/models/dagrun.py
+++ airflow/models/dagrun.py
@@ -897,11 +897,9 @@ class DagRun(Base, LoggingMixin):
                 self.run_id,
                 self.start_date,
                 self.end_date,
-                (
-                    (self.end_date - self.start_date).total_seconds()
-                    if self.start_date and self.end_date
-                    else None
-                ),
+                (self.end_date - self.start_date).total_seconds()
+                if self.start_date and self.end_date
+                else None,
                 self._state,
                 self.external_trigger,
                 self.run_type,
