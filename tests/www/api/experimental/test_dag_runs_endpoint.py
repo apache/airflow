f065f1f7eff31c73acaf92c35a3ef0c7f939bc8a
sudipto baral <sudiptobaral.me@gmail.com>
2024-03-20 16:25:09 -0400

    fix: unit tests of experimental/test_dag_runs_endpoint.py.
    Signed-off-by: sudipto baral <sudiptobaral.me@gmail.com>

--- tests/www/api/experimental/test_dag_runs_endpoint.py
+++ tests/www/api/experimental/test_dag_runs_endpoint.py
@@ -17,8 +17,6 @@
 # under the License.
 from __future__ import annotations
 
-import json
-
 import pytest
 
 from airflow.api.common.trigger_dag import trigger_dag
@@ -58,7 +56,7 @@ class TestDagRunsEndpoint:
 
         response = self.app.get(url_template.format(dag_id))
         assert 200 == response.status_code
-        data = json.loads(response.data.decode("utf-8"))
+        data = response.json()
 
         assert isinstance(data, list)
         assert len(data) == 1
@@ -73,7 +71,7 @@ class TestDagRunsEndpoint:
 
         response = self.app.get(url_template.format(dag_id))
         assert 200 == response.status_code
-        data = json.loads(response.data.decode("utf-8"))
+        data = response.json()
 
         assert isinstance(data, list)
         assert len(data) == 1
@@ -88,7 +86,7 @@ class TestDagRunsEndpoint:
 
         response = self.app.get(url_template.format(dag_id))
         assert 200 == response.status_code
-        data = json.loads(response.data.decode("utf-8"))
+        data = response.json()
 
         assert isinstance(data, list)
         assert len(data) == 1
@@ -110,7 +108,7 @@ class TestDagRunsEndpoint:
 
         response = self.app.get(url_template.format(dag_id))
         assert 400 == response.status_code
-        data = json.loads(response.data.decode("utf-8"))
+        data = response.json()
 
         assert not isinstance(data, list)
 
@@ -120,7 +118,7 @@ class TestDagRunsEndpoint:
 
         response = self.app.get(url_template.format(dag_id))
         assert 200 == response.status_code
-        data = json.loads(response.data.decode("utf-8"))
+        data = response.json()
 
         assert isinstance(data, list)
         assert len(data) == 0
