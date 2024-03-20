0eb14056172f49890ae238bb8300d71c64da573e
Daniel Standish <15932138+dstandish@users.noreply.github.com>
2024-03-20 00:41:49 -0700

    Implement clear_xcom_data on TaskInstancePydantic (#38302)

--- airflow/api_internal/endpoints/rpc_api_endpoint.py
+++ airflow/api_internal/endpoints/rpc_api_endpoint.py
@@ -83,6 +83,7 @@ def _initialize_map() -> dict[str, Callable]:
         TaskInstance.fetch_handle_failure_context,
         TaskInstance.save_to_db,
         TaskInstance._schedule_downstream_tasks,
+        TaskInstance._clear_xcom_data,
         Trigger.from_object,
         Trigger.bulk_fetch,
         Trigger.clean_unused,
