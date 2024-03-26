77985e30b8c533f8cabf855810bec8e39ac125b2
Daniel Standish <15932138+dstandish@users.noreply.github.com>
2024-03-25 17:47:17 -0700

    Implement _run_execute_callback on TaskInstancePydantic

--- airflow/serialization/pydantic/taskinstance.py
+++ airflow/serialization/pydantic/taskinstance.py
@@ -126,6 +126,9 @@ class TaskInstancePydantic(BaseModelPydantic, LoggingMixin):
     def set_state(self, state, session: Session | None = None) -> bool:
         return TaskInstance._set_state(ti=self, state=state, session=session)
 
+    def _run_execute_callback(self, context, task):
+        TaskInstance._run_execute_callback(self=self, context=context, task=task)  # type: ignore[arg-type]
+
     def init_run_context(self, raw: bool = False) -> None:
         """Set the log context."""
         self.raw = raw
