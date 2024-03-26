a697bb4f6ecff01158b9471539abc510ff1aab21
Jarek Potiuk <jarek@potiuk.com>
2024-03-26 09:51:41 +0100

    Fix image cache optimizations - speeding up the build (#38442) (#38473)
    The recent refactors in workflows broke the way how cache had been used
    in the CI builds. This PR brings back the optimizations by using the
    cache and rebuilding it.

--- dev/breeze/src/airflow_breeze/utils/docker_command_utils.py
+++ dev/breeze/src/airflow_breeze/utils/docker_command_utils.py
@@ -381,12 +381,15 @@ def prepare_base_build_command(image_params: CommonBuildParams) -> list[str]:
             ]
         )
         if not image_params.docker_host:
+            builder = get_and_use_docker_context(image_params.builder)
             build_command_param.extend(
                 [
                     "--builder",
-                    get_and_use_docker_context(image_params.builder),
+                    builder,
                 ]
             )
+            if builder != "default":
+                build_command_param.append("--load")
     else:
         build_command_param.append("build")
     return build_command_param
@@ -658,6 +661,7 @@ def autodetect_docker_context():
 def get_and_use_docker_context(context: str):
     if context == "autodetect":
         context = autodetect_docker_context()
+    run_command(["docker", "context", "create", context], check=False)
     output = run_command(["docker", "context", "use", context], check=False)
     if output.returncode != 0:
         get_console().print(
