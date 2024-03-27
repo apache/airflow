b69ea858dd8f3886fce03d78aa4f08ea23137c01
Pankaj Singh <98807258+pankajastro@users.noreply.github.com>
2024-03-27 17:09:01 +0530

    Update airflow/providers/cncf/kubernetes/operators/pod.py

--- airflow/providers/cncf/kubernetes/operators/pod.py
+++ airflow/providers/cncf/kubernetes/operators/pod.py
@@ -781,7 +781,7 @@ class KubernetesPodOperator(BaseOperator):
                 follow=False,
             )
             for raw_line in logs:
-                line = raw_line.decode("utf-8", errors="backslashreplace").strip()
+                line = raw_line.decode("utf-8", errors="backslashreplace").rstrip("\n")
                 if line:
                     self.log.info("Container logs: %s", line)
         except HTTPError as e:
