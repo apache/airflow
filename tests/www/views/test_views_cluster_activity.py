8434a5b6f6d35c95d8906fd10bcff1374863de11
sudipto baral <sudiptobaral.me@gmail.com>
2024-03-28 19:44:06 -0400

    fix: does not green the test but fix the attribute error.
    Signed-off-by: sudipto baral <sudiptobaral.me@gmail.com>

--- tests/www/views/test_views_cluster_activity.py
+++ tests/www/views/test_views_cluster_activity.py
@@ -104,7 +104,7 @@ def test_historical_metrics_data(admin_client, session, time_machine):
         follow_redirects=True,
     )
     assert resp.status_code == 200
-    assert resp.json == {
+    assert resp.json() == {
         "dag_run_states": {"failed": 1, "queued": 0, "running": 1, "success": 1},
         "dag_run_types": {"backfill": 0, "dataset_triggered": 1, "manual": 0, "scheduled": 2},
         "task_instance_states": {
@@ -133,7 +133,7 @@ def test_historical_metrics_data_date_filters(admin_client, session):
         follow_redirects=True,
     )
     assert resp.status_code == 200
-    assert resp.json == {
+    assert resp.json() == {
         "dag_run_states": {"failed": 1, "queued": 0, "running": 0, "success": 0},
         "dag_run_types": {"backfill": 0, "dataset_triggered": 1, "manual": 0, "scheduled": 0},
         "task_instance_states": {
