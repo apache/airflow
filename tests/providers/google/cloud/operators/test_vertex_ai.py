45d98570e23eb64488c75caf6473bdd75f143a5e
Jarek Potiuk <jarek@potiuk.com>
2024-03-29 15:36:53 +0100

    Conditionally skip few more tests for "Pydantic Removed" test
    Turns out that we need to conditionally skip a few more tests in order to make
    "Pydantic Removed" tests happy after #38617

--- tests/providers/google/cloud/operators/test_vertex_ai.py
+++ tests/providers/google/cloud/operators/test_vertex_ai.py
@@ -20,6 +20,10 @@ from unittest import mock
 from unittest.mock import MagicMock, call
 
 import pytest
+
+# For no Pydantic environment, we need to skip the tests
+pytest.importorskip("google.cloud.aiplatform_v1")
+
 from google.api_core.gapic_v1.method import DEFAULT
 from google.api_core.retry import Retry
 
