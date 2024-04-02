423eba6c8b5a7a458420babf8add65c85325b565
Syed Hussain <syedahsn@amazon.com>
2024-04-02 15:20:53 -0700

    Missed return type

--- tests/system/conftest.py
+++ tests/system/conftest.py
@@ -46,7 +46,7 @@ def skip_if_env_var_not_set(provider_env_vars: list[str]) -> None:
             return
 
 
-def pytest_collection_modifyitems(config: pytest.Config, items: list[pytest.Item]):
+def pytest_collection_modifyitems(config: pytest.Config, items: list[pytest.Item]) -> None:
     """Add @pytest.mark.system(provider_name) for every system test."""
     rootdir = config.rootpath
     for item in items:
