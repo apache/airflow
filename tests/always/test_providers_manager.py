b313232fdbd6f9582bf1ea036f5df7eb1f4bd2a6
Andrey Anshin <Andrey.Anshin@taragol.is>
2024-04-04 03:38:05 +0400

    Fixup tests

--- tests/always/test_providers_manager.py
+++ tests/always/test_providers_manager.py
@@ -477,17 +477,20 @@ def test_lazy_cache_dict_raises_error():
 def test_lazy_cache_dict_del_item():
     lazy_cache_dict = LazyDictWithCache()
 
-    lazy_cache_dict["spam"] = "egg"
+    def answer():
+        return 42
+
+    lazy_cache_dict["spam"] = answer
     assert "spam" in lazy_cache_dict._raw_dict
     assert "spam" not in lazy_cache_dict._resolved  # Not resoled yet
-    assert lazy_cache_dict["spam"] == "egg"
+    assert lazy_cache_dict["spam"] == 42
     assert "spam" in lazy_cache_dict._resolved
     del lazy_cache_dict["spam"]
     assert "spam" not in lazy_cache_dict._raw_dict
     assert "spam" not in lazy_cache_dict._resolved
 
-    lazy_cache_dict["foo"] = "bar"
-    assert lazy_cache_dict["foo"] == "bar"
+    lazy_cache_dict["foo"] = answer
+    assert lazy_cache_dict["foo"] == 42
     assert "foo" in lazy_cache_dict._resolved
     # Emulate some mess in data, e.g. value from `_raw_dict` deleted but not from `_resolved`
     del lazy_cache_dict._raw_dict["foo"]
@@ -497,7 +500,7 @@ def test_lazy_cache_dict_del_item():
         del lazy_cache_dict["foo"]
     assert "foo" not in lazy_cache_dict._resolved
 
-    lazy_cache_dict["baz"] = "qux"
+    lazy_cache_dict["baz"] = answer
     # Key in `_resolved` not created yet
     assert "baz" in lazy_cache_dict._raw_dict
     assert "baz" not in lazy_cache_dict._resolved
@@ -507,16 +510,19 @@ def test_lazy_cache_dict_del_item():
 
 
 def test_lazy_cache_dict_clear():
+    def answer():
+        return 42
+
     lazy_cache_dict = LazyDictWithCache()
     assert len(lazy_cache_dict) == 0
-    lazy_cache_dict["spam"] = "egg"
-    lazy_cache_dict["foo"] = "bar"
-    lazy_cache_dict["baz"] = "qux"
+    lazy_cache_dict["spam"] = answer
+    lazy_cache_dict["foo"] = answer
+    lazy_cache_dict["baz"] = answer
 
     assert len(lazy_cache_dict) == 3
     assert len(lazy_cache_dict._raw_dict) == 3
     assert not lazy_cache_dict._resolved
-    assert lazy_cache_dict["spam"] == "egg"
+    assert lazy_cache_dict["spam"] == 42
     assert len(lazy_cache_dict._resolved) == 1
     # Emulate some mess in data, contain some data into the `_resolved`
     lazy_cache_dict._resolved.add("biz")
