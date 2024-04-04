cc901cdaab287742e966c23d9bbe9b198aef2f46
Andrey Anshin <Andrey.Anshin@taragol.is>
2024-04-05 00:27:56 +0400

    Replace to broad exceptions into the dev (#38489)
    * Replace to broad exceptions into the Dev
    
    * Add changes according to the review
    
    Co-authored-by: Jarek Potiuk <jarek@potiuk.com>
    Co-authored-by: Tzu-ping Chung <uranusjr@gmail.com>
    
    * RuntimeError -> SystemExit if detect it run in __main__
    
    Co-authored-by: Tzu-ping Chung <uranusjr@gmail.com>
    
    ---------
    
    Co-authored-by: Jarek Potiuk <jarek@potiuk.com>
    Co-authored-by: Tzu-ping Chung <uranusjr@gmail.com>

--- dev/breeze/src/airflow_breeze/global_constants.py
+++ dev/breeze/src/airflow_breeze/global_constants.py
@@ -364,7 +364,7 @@ def get_airflow_version():
                 airflow_version = line.split()[2][1:-1]
                 break
     if airflow_version == "unknown":
-        raise Exception("Unable to determine Airflow version")
+        raise RuntimeError("Unable to determine Airflow version")
     return airflow_version
 
 
