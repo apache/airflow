17c920ce8d551fdb9c4d751b092b8b98893a292b
Amogh <amoghrajesh1999@gmail.com>
2024-04-05 15:56:22 +0530

    fixing uts

--- tests/providers/apache/hive/hooks/test_hive.py
+++ tests/providers/apache/hive/hooks/test_hive.py
@@ -873,14 +873,14 @@ class TestHiveCli:
     def test_default_values(self):
         hook = MockHiveCliHook()
 
-        assert hook.use_beeline == True
+        assert hook.use_beeline
         assert hook.auth is None
         assert hook.sub_process is None
         assert hook.mapred_queue == "airflow"
         assert hook.mapred_queue_priority is None
         assert hook.mapred_job_name is None
         assert hook.proxy_user is None
-        assert hook.high_availability == False
+        assert not hook.high_availability
 
     @pytest.mark.parametrize(
         "extra_dejson, correct_proxy_user, proxy_user",
