c8dc5ce163e61d92b947d89611824f66b8385682
kalyanr <kalyan.ben10@live.com>
2024-04-09 20:56:42 +0530

    Add sqlalchemy_url property to DbApiHook
    Signed-off-by: kalyanr <kalyan.ben10@live.com>

--- airflow/providers/common/sql/hooks/sql.py
+++ airflow/providers/common/sql/hooks/sql.py
@@ -39,6 +39,7 @@ from urllib.parse import urlparse
 import sqlparse
 from more_itertools import chunked
 from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
 
 from airflow.exceptions import (
     AirflowException,
@@ -192,6 +193,17 @@ class DbApiHook(BaseHook):
         )
         return self._placeholder
 
+    @property
+    def drivername(self) -> str:
+        """
+        Return the drivername for the connection.
+
+        Should be implemented in the derived class to return database backend and drivername.
+
+        :return: the drivername for the connection.
+        """
+        raise NotImplementedError("drivername property should be implemented in the provider subclass.")
+
     def get_conn(self):
         """Return a connection object."""
         db = self.get_connection(getattr(self, cast(str, self.conn_name_attr)))
@@ -207,6 +219,24 @@ class DbApiHook(BaseHook):
         conn.schema = self.__schema or conn.schema
         return conn.get_uri()
 
+    def get_sqlalchemy_url(self) -> URL:
+        """
+        Extract the SQLAlchemy URI from the connection.
+
+        :return: the extracted uri.
+        """
+        conn = self.get_connection(getattr(self, self.conn_name_attr))
+        conn.schema = self.__schema or conn.schema
+        conn.port = conn.port or 5432
+        return URL.create(
+            drivername=self.drivername,
+            username=conn.login,
+            password=conn.password,
+            host=conn.host,
+            port=conn.port,
+            database=conn.schema,
+        )
+
     def get_sqlalchemy_engine(self, engine_kwargs=None):
         """
         Get an sqlalchemy_engine object.
@@ -216,7 +246,8 @@ class DbApiHook(BaseHook):
         """
         if engine_kwargs is None:
             engine_kwargs = {}
-        return create_engine(self.get_uri(), **engine_kwargs)
+        sa_uri = self.get_sqlalchemy_url().render_as_string(hide_password=False)
+        return create_engine(sa_uri, **engine_kwargs)
 
     def get_pandas_df(
         self,
