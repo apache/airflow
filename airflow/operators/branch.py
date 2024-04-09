911e37af25b142e0c8dcf9783a042bbecb788ea9
boraberke <boraberkesahin@gmail.com>
2024-04-09 22:46:17 +0300

    Allow `Task Group Id`s to be passed as branches in BranchMixIn

--- airflow/operators/branch.py
+++ airflow/operators/branch.py
@@ -21,8 +21,10 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Iterable
 
+from airflow.models import TaskInstance
 from airflow.models.baseoperator import BaseOperator
 from airflow.models.skipmixin import SkipMixin
+from airflow.serialization.pydantic.taskinstance import TaskInstancePydantic
 
 if TYPE_CHECKING:
     from airflow.utils.context import Context
@@ -34,8 +36,33 @@ class BranchMixIn(SkipMixin):
     def do_branch(self, context: Context, branches_to_execute: str | Iterable[str]) -> str | Iterable[str]:
         """Implement the handling of branching including logging."""
         self.log.info("Branch into %s", branches_to_execute)
-        self.skip_all_except(context["ti"], branches_to_execute)
-        return branches_to_execute
+        branch_task_ids = self.expand_task_group_roots(context["ti"], branches_to_execute)
+        self.skip_all_except(context["ti"], branch_task_ids)
+        return branch_task_ids
+
+    def expand_task_group_roots(
+        self, ti: TaskInstance | TaskInstancePydantic, branches_to_execute: str | Iterable[str]
+    ) -> str | Iterable[str]:
+        """Replace any task group with the root task ids."""
+        task = ti.task
+        dag = task.dag
+        task_ids = []
+        if isinstance(branches_to_execute, str):
+            branches_to_execute = [branches_to_execute]
+
+        for branch in branches_to_execute:
+            if branch in dag.task_group_dict:
+                tg = dag.task_group_dict[branch]
+                root_ids = [root.task_id for root in tg.roots]
+                self.log.info("Expanding task group %s into %s", tg.group_id, root_ids)
+                task_ids.extend(root_ids)
+            else:
+                task_ids.append(branch)
+
+        if len(task_ids) == 1:
+            return task_ids[0]
+        else:
+            return task_ids
 
 
 class BaseBranchOperator(BaseOperator, BranchMixIn):
