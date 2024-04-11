732a13bf6b6268e0bfb36f3a0406f6279e831741
David Blain <david.blain@infrabel.be>
2024-04-11 14:07:37 +0200

    refactor: Reformatted global_constants.py

--- dev/breeze/src/airflow_breeze/global_constants.py
+++ dev/breeze/src/airflow_breeze/global_constants.py
@@ -471,7 +471,9 @@ BASE_PROVIDERS_COMPATIBILITY_CHECKS: list[dict[str, str]] = [
     {
         "python-version": "3.8",
         "airflow-version": "2.6.0",
-        "remove-providers": _exclusion(["openlineage", "common.io", "cohere", "fab", "qdrant", "microsoft-azure"]),
+        "remove-providers": _exclusion(
+            ["openlineage", "common.io", "cohere", "fab", "qdrant", "microsoft-azure"]
+        ),
     },
     {
         "python-version": "3.8",
