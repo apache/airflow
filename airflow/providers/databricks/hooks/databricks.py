2dcdf428c482f01b580e9f04640a056aaa0465e3
subham611 <subhamsinghal@sharechat.co>
2024-04-11 13:25:39 +0530

    Remove extra slash from update permission endpoint

--- airflow/providers/databricks/hooks/databricks.py
+++ airflow/providers/databricks/hooks/databricks.py
@@ -51,7 +51,7 @@ DELETE_RUN_ENDPOINT = ("POST", "api/2.1/jobs/runs/delete")
 REPAIR_RUN_ENDPOINT = ("POST", "api/2.1/jobs/runs/repair")
 OUTPUT_RUNS_JOB_ENDPOINT = ("GET", "api/2.1/jobs/runs/get-output")
 CANCEL_ALL_RUNS_ENDPOINT = ("POST", "api/2.1/jobs/runs/cancel-all")
-UPDATE_PERMISSION_ENDPOINT = ("PATCH", "/api/2.0/permissions/jobs")
+UPDATE_PERMISSION_ENDPOINT = ("PATCH", "api/2.0/permissions/jobs")
 
 INSTALL_LIBS_ENDPOINT = ("POST", "api/2.0/libraries/install")
 UNINSTALL_LIBS_ENDPOINT = ("POST", "api/2.0/libraries/uninstall")
