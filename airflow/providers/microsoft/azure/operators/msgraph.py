f7045933f97c299c1f85bec3479ffa7a86a5896e
David Blain <david.blain@infrabel.be>
2024-04-11 09:33:58 +0200

    Revert "refactor: Added noqa check on pull_execute_complete method of MSGraphOperator"
    This reverts commit ca6f92cae94edeed190df1c2c807324e510bbae3.

--- airflow/providers/microsoft/azure/operators/msgraph.py
+++ airflow/providers/microsoft/azure/operators/msgraph.py
@@ -229,7 +229,7 @@ class MSGraphAsyncOperator(BaseOperator):
                 dag_id=self.dag_id,
                 key=self.key,
             )
-            or []  # noqa: W503
+            or []
         )
         self.log.info(
             "Pulled XCom with task_id '%s' and dag_id '%s' and key '%s': %s",
