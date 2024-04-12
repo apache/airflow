d03ba594b3158c127c1c1b3f1d0c31fb93104367
Daniel Standish <15932138+dstandish@users.noreply.github.com>
2024-04-12 16:04:16 -0700

    Use debug level for minischeduler skip (#38976)
    Now that we are using nowait, it will be more common that the minicheduler skips.  Leaving it at info level will cause unnecessary alarm to users.

--- airflow/models/taskinstance.py
+++ airflow/models/taskinstance.py
@@ -3563,7 +3563,7 @@ class TaskInstance(Base, LoggingMixin):
 
         except OperationalError as e:
             # Any kind of DB error here is _non fatal_ as this block is just an optimisation.
-            cls.logger().info(
+            cls.logger().debug(
                 "Skipping mini scheduling run due to exception: %s",
                 e.statement,
                 exc_info=True,
