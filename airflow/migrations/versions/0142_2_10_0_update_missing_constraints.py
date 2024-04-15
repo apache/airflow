a71a5e3bc0dd338dbf8a2e34961d76c95cea6170
Ephraim Anierobi <splendidzigy24@gmail.com>
2024-04-15 18:49:57 +0100

    fixup! fixup! fixup! Fix alembic autogeneration and rename mismatching constraints

--- airflow/migrations/versions/0142_2_10_0_update_missing_constraints.py
+++ airflow/migrations/versions/0142_2_10_0_update_missing_constraints.py
@@ -38,8 +38,10 @@ airflow_version = '2.10.0'
 
 def upgrade():
     """Apply Update missing constraints"""
+    op.execute("ALTER TABLE connection DROP CONSTRAINT IF EXISTS unique_conn_id")
+    # Dropping and recreating cause there's no IF NOT EXISTS
+    op.execute("ALTER TABLE connection DROP CONSTRAINT IF EXISTS connection_conn_id_uq")
     with op.batch_alter_table('connection') as batch_op:
-        batch_op.drop_constraint('unique_conn_id', type_='unique')
         batch_op.create_unique_constraint(batch_op.f('connection_conn_id_uq'), ['conn_id'])
 
     max_cons = sa.table('dag', sa.column('max_consecutive_failed_dag_runs'))
@@ -52,9 +54,12 @@ def upgrade():
     with op.batch_alter_table("task_reschedule") as batch_op:
         batch_op.drop_constraint("task_reschedule_dr_fkey", type_="foreignkey")
 
+    op.execute("ALTER TABLE dag_run DROP CONSTRAINT IF EXISTS dag_run_dag_id_execution_date_uq")
+    op.execute("ALTER TABLE dag_run DROP CONSTRAINT IF EXISTS dag_run_dag_id_run_id_uq")
+    # below we drop and recreate the constraints because there's no IF NOT EXISTS
+    op.execute("ALTER TABLE dag_run DROP CONSTRAINT IF EXISTS dag_run_dag_id_execution_date_key")
+    op.execute("ALTER TABLE dag_run DROP CONSTRAINT IF EXISTS dag_run_dag_id_run_id_key")
     with op.batch_alter_table('dag_run') as batch_op:
-        batch_op.drop_constraint('dag_run_dag_id_execution_date_uq', type_='unique')
-        batch_op.drop_constraint('dag_run_dag_id_run_id_uq', type_='unique')
         batch_op.create_unique_constraint('dag_run_dag_id_execution_date_key', ['dag_id', 'execution_date'])
         batch_op.create_unique_constraint('dag_run_dag_id_run_id_key', ['dag_id', 'run_id'])
     with op.batch_alter_table("task_instance") as batch_op:
@@ -70,33 +75,4 @@ def upgrade():
 
 
 def downgrade():
-    """Unapply Update missing constraints"""
-    with op.batch_alter_table("task_instance") as batch_op:
-        batch_op.drop_constraint("task_instance_dag_run_fkey", type_="foreignkey")
-    with op.batch_alter_table("task_reschedule") as batch_op:
-        batch_op.drop_constraint("task_reschedule_dr_fkey", type_="foreignkey")
-    with op.batch_alter_table('dag_run') as batch_op:
-        batch_op.drop_constraint('dag_run_dag_id_run_id_key', type_='unique')
-        batch_op.drop_constraint('dag_run_dag_id_execution_date_key', type_='unique')
-        batch_op.create_unique_constraint('dag_run_dag_id_run_id_uq', ['dag_id', 'run_id'])
-        batch_op.create_unique_constraint('dag_run_dag_id_execution_date_uq', ['dag_id', 'execution_date'])
-
-    with op.batch_alter_table("task_instance") as batch_op:
-        batch_op.create_foreign_key("task_instance_dag_run_fkey", "dag_run", ["dag_id", "run_id"], ["dag_id", "run_id"])
-    with op.batch_alter_table("task_reschedule") as batch_op:
-        batch_op.create_foreign_key(
-            "task_reschedule_dr_fkey",
-            "dag_run",
-            ["dag_id", "run_id"],
-            ["dag_id", "run_id"],
-            ondelete="CASCADE",
-        )
-
-    with op.batch_alter_table('dag') as batch_op:
-        batch_op.alter_column('max_consecutive_failed_dag_runs',
-               existing_type=sa.Integer(),
-               nullable=True)
-
-    with op.batch_alter_table('connection') as batch_op:
-        batch_op.drop_constraint(batch_op.f('connection_conn_id_uq'), type_='unique')
-        batch_op.create_unique_constraint('unique_conn_id', ['conn_id'])
+    """NO downgrade because this is to make ORM consistent with the database."""
