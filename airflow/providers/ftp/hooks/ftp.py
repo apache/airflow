fa50ba0af6f21e6770bdbd31973ea18bf40ebf89
kerlion <kerlion.he@hotmail.com>
2024-04-16 08:47:09 +0800

    enable none-default ftp port

--- airflow/providers/ftp/hooks/ftp.py
+++ airflow/providers/ftp/hooks/ftp.py
@@ -58,7 +58,9 @@ class FTPHook(BaseHook):
         if self.conn is None:
             params = self.get_connection(self.ftp_conn_id)
             pasv = params.extra_dejson.get("passive", True)
-            self.conn = ftplib.FTP(params.host, params.login, params.password)  # nosec: B321
+            self.conn = ftplib.FTP()
+            self.conn.connect(params.host, params.port)
+            self.conn.login(params.login, params.password)
             self.conn.set_pasv(pasv)
 
         return self.conn
