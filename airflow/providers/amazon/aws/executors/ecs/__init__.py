9da925e82b374b50bf5b8f0b90a089d1514cb06a
Niko Oliveira <onikolas@amazon.com>
2024-04-17 11:34:00 -0700

    Add comments to explain precommit shenanigans

--- airflow/providers/amazon/aws/executors/ecs/__init__.py
+++ airflow/providers/amazon/aws/executors/ecs/__init__.py
@@ -14,8 +14,11 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-from __future__ import annotations
+from __future__ import annotations  # Added by precommit hooks
 
 from airflow.providers.amazon.aws.executors.ecs.ecs_executor import AwsEcsExecutor
 
+# precommit hooks (rust as of the time of commit) throws F401 - "Module imported but unused"
+# One of the solutions it suggests is using a "redundant alias". This is used below instead of doing a #no-qa
+# type ignore of the issue.
 AwsEcsExecutor = AwsEcsExecutor
