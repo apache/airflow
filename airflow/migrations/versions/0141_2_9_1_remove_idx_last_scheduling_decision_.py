61e0fa2012ca70963a41cd46328bf0a4368b24e8
Pankaj Koti <pankajkoti699@gmail.com>
2024-04-30 17:22:16 +0530

    Address review comments

--- airflow/migrations/versions/0141_2_9_1_remove_idx_last_scheduling_decision_.py
+++ airflow/migrations/versions/0141_2_9_1_remove_idx_last_scheduling_decision_.py
@@ -24,10 +24,8 @@ Create Date: 2024-04-26 12:58:00.594762
 
 """
 
-import sqlalchemy as sa
 from alembic import op
 
-from contextlib import suppress
 
 # revision identifiers, used by Alembic.
 revision = "bff083ad727d"
@@ -39,23 +37,10 @@ airflow_version = "2.9.1"
 
 def upgrade():
     """Apply Remove idx_last_scheduling_decision index on last_scheduling_decision in dag_run table"""
-    try:
-        op.drop_index("idx_last_scheduling_decision", table_name="dag_run", if_exists=True)
-    except sa.exc.DatabaseError:
-        # MySQL does not support drop if exists index. Try to drop the index directly instead.
-        with suppress(sa.exc.DatabaseError):
-            # Suppress the error if the index does not exist.
-            op.drop_index("idx_last_scheduling_decision", table_name="dag_run")
+    op.drop_index("idx_last_scheduling_decision", table_name="dag_run")
 
 
 def downgrade():
     """Unapply Remove idx_last_scheduling_decision index on last_scheduling_decision in dag_run table"""
-
-    try:
-        op.create_index("idx_last_scheduling_decision", "dag_run", ["last_scheduling_decision"],
-                        unique=False, if_not_exists=True)
-    except sa.exc.DatabaseError:
-        # MySQL does not support create if not exists index. Try to create the index directly instead.
-        with suppress(sa.exc.DatabaseError):
-            # Suppress the error if the index already exists.
-            op.create_index("idx_last_scheduling_decision", "dag_run", ["last_scheduling_decision"], unique=False)
+    op.create_index("idx_last_scheduling_decision", "dag_run", ["last_scheduling_decision"],
+                    unique=False)
