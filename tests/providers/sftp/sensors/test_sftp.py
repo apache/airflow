ef0dbc146f409bbe83138a9b3983f7b88e55176f
Grégoire Rolland <gregoire.rolland@mymoneybank.com>
2024-05-06 09:15:26 +0200

    Fix utc timezone in unit tests

--- tests/providers/sftp/sensors/test_sftp.py
+++ tests/providers/sftp/sensors/test_sftp.py
@@ -18,6 +18,7 @@
 from __future__ import annotations
 
 from datetime import datetime
+from datetime import timezone as stdlib_timezone
 from unittest import mock
 from unittest.mock import Mock, call, patch
 
@@ -100,7 +101,7 @@ class TestSFTPSensor:
     @pytest.mark.parametrize(
         "newer_than", (
             datetime(2020, 1, 2),
-            datetime(2020, 1, 2, tzinfo=timezone.utc),
+            datetime(2020, 1, 2, tzinfo=stdlib_timezone.utc),
             "2020-01-02",
             "2020-01-02 00:00:00+00:00",
             "2020-01-02 00:00:00.001+00:00",
