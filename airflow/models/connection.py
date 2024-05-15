d7f455289e1b02adbc6ecb4d348fdc85c3fedc1d
Michael Trossbach <mptrossbach@gmail.com>
2024-05-15 12:22:56 -0700

    Remove RFC3986 conn_type warning in get_uri method

--- airflow/models/connection.py
+++ airflow/models/connection.py
@@ -271,12 +271,6 @@ class Connection(Base, LoggingMixin):
 
     def get_uri(self) -> str:
         """Return connection in URI format."""
-        if self.conn_type and "_" in self.conn_type:
-            self.log.warning(
-                "Connection schemes (type: %s) shall not contain '_' according to RFC3986.",
-                self.conn_type,
-            )
-
         if self.conn_type:
             uri = f"{self.conn_type.lower().replace('_', '-')}://"
         else:
