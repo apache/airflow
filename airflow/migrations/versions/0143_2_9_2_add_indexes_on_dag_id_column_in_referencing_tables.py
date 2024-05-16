0114f04e7ee77e89b0e738414de2e58b15c25064
Pankaj Koti <pankajkoti699@gmail.com>
2024-05-16 19:33:02 +0530

    Address @ephraimbuddy's comment

--- airflow/migrations/versions/0143_2_9_2_add_indexes_on_dag_id_column_in_referencing_tables.py
+++ airflow/migrations/versions/0143_2_9_2_add_indexes_on_dag_id_column_in_referencing_tables.py
@@ -38,19 +38,19 @@ airflow_version = "2.9.2"
 
 def upgrade():
     """Apply Add indexes on dag_id column in referencing tables."""
-    with op.batch_alter_table("dag_schedule_dataset_reference", schema=None) as batch_op:
+    with op.batch_alter_table("dag_schedule_dataset_reference") as batch_op:
         batch_op.create_index("idx_dag_schedule_dataset_reference_dag_id", ["dag_id"], unique=False)
 
-    with op.batch_alter_table("dag_tag", schema=None) as batch_op:
+    with op.batch_alter_table("dag_tag") as batch_op:
         batch_op.create_index("idx_dag_tag_dag_id", ["dag_id"], unique=False)
 
-    with op.batch_alter_table("dag_warning", schema=None) as batch_op:
+    with op.batch_alter_table("dag_warning") as batch_op:
         batch_op.create_index("idx_dag_warning_dag_id", ["dag_id"], unique=False)
 
-    with op.batch_alter_table("dataset_dag_run_queue", schema=None) as batch_op:
+    with op.batch_alter_table("dataset_dag_run_queue") as batch_op:
         batch_op.create_index("idx_dataset_dag_run_queue_target_dag_id", ["target_dag_id"], unique=False)
 
-    with op.batch_alter_table("task_outlet_dataset_reference", schema=None) as batch_op:
+    with op.batch_alter_table("task_outlet_dataset_reference") as batch_op:
         batch_op.create_index("idx_task_outlet_dataset_reference_dag_id", ["dag_id"], unique=False)
 
 
@@ -64,13 +64,13 @@ def _handle_foreign_key_constraint_index_deletion(
         batch_op.create_foreign_key(
             constraint_name, "dag", [local_fk_column_name], ["dag_id"], ondelete="CASCADE"
         )
-    elif conn.dialect.name in ("postgresql", "sqlite"):
+    else:
         batch_op.drop_index(index_name)
 
 
 def downgrade():
     """Unapply Add indexes on dag_id column in referencing tables."""
-    with op.batch_alter_table("dag_schedule_dataset_reference", schema=None) as batch_op:
+    with op.batch_alter_table("dag_schedule_dataset_reference") as batch_op:
         _handle_foreign_key_constraint_index_deletion(
             batch_op,
             "dsdr_dag_id_fkey",
@@ -78,17 +78,17 @@ def downgrade():
             "dag_id",
         )
 
-    with op.batch_alter_table("dag_tag", schema=None) as batch_op:
+    with op.batch_alter_table("dag_tag") as batch_op:
         _handle_foreign_key_constraint_index_deletion(
             batch_op, "dag_tag_dag_id_fkey", "idx_dag_tag_dag_id", "dag_id"
         )
 
-    with op.batch_alter_table("dag_warning", schema=None) as batch_op:
+    with op.batch_alter_table("dag_warning") as batch_op:
         _handle_foreign_key_constraint_index_deletion(
             batch_op, "dcw_dag_id_fkey", "idx_dag_warning_dag_id", "dag_id"
         )
 
-    with op.batch_alter_table("dataset_dag_run_queue", schema=None) as batch_op:
+    with op.batch_alter_table("dataset_dag_run_queue") as batch_op:
         _handle_foreign_key_constraint_index_deletion(
             batch_op,
             "ddrq_dag_fkey",
@@ -96,7 +96,7 @@ def downgrade():
             "target_dag_id",
         )
 
-    with op.batch_alter_table("task_outlet_dataset_reference", schema=None) as batch_op:
+    with op.batch_alter_table("task_outlet_dataset_reference") as batch_op:
         _handle_foreign_key_constraint_index_deletion(
             batch_op,
             "todr_dag_id_fkey",
