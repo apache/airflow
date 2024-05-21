732896d79b71cfb3842714d47f104548e091c295
hsyoun <hsyoun@sj.co.kr>
2024-05-21 14:06:54 +0900

    fix lint

--- airflow/www/static/js/toggle_theme.js
+++ airflow/www/static/js/toggle_theme.js
@@ -19,36 +19,34 @@
 
 /* global document, localStorage, window */
 
-const STORAGE_THEME_KEY = 'darkTheme';
-const HTML_THEME_DATASET_KEY = 'data-color-scheme';
+const STORAGE_THEME_KEY = "darkTheme";
+const HTML_THEME_DATASET_KEY = "data-color-scheme";
 const HTML = document.documentElement;
-const TOGGLE_BUTTON_ID = 'themeToggleButton';
+const TOGGLE_BUTTON_ID = "themeToggleButton";
 
-const getJsonFromStorage = (key)=>JSON.parse(localStorage.getItem(key));
-const updateTheme = (isDark)=>{
-    localStorage.setItem(STORAGE_THEME_KEY, isDark);
-    HTML.setAttribute(HTML_THEME_DATASET_KEY, isDark ? 'dark' : 'light');
-}
-;
-const initTheme = ()=>{
-    const isDark = getJsonFromStorage(STORAGE_THEME_KEY) ?? window.matchMedia('(prefers-color-scheme: dark)').matches;
-    updateTheme(isDark);
-}
-;
-const toggleTheme = ()=>{
-    const isDark = getJsonFromStorage(STORAGE_THEME_KEY);
-    updateTheme(!isDark);
-}
-;
-window.addEventListener('click', (e)=>{
-    if (e.target.id !== TOGGLE_BUTTON_ID)
-        return;
-    toggleTheme();
-}
-);
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e)=>{
+const getJsonFromStorage = (key) => JSON.parse(localStorage.getItem(key));
+const updateTheme = (isDark) => {
+  localStorage.setItem(STORAGE_THEME_KEY, isDark);
+  HTML.setAttribute(HTML_THEME_DATASET_KEY, isDark ? "dark" : "light");
+};
+const initTheme = () => {
+  const isDark =
+    getJsonFromStorage(STORAGE_THEME_KEY) ??
+    window.matchMedia("(prefers-color-scheme: dark)").matches;
+  updateTheme(isDark);
+};
+const toggleTheme = () => {
+  const isDark = getJsonFromStorage(STORAGE_THEME_KEY);
+  updateTheme(!isDark);
+};
+window.addEventListener("click", (e) => {
+  if (e.target.id !== TOGGLE_BUTTON_ID) return;
+  toggleTheme();
+});
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", (e) => {
     const isDark = e.matches;
     updateTheme(isDark);
-}
-);
+});
 initTheme();
