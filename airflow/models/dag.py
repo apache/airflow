1892c4dad9aa9f7318f974593bc4decf03b2ba89
Mayank Soni <41102134+msoni1369@users.noreply.github.com>
2024-05-22 22:52:04 +0530

    Update dag.py

--- airflow/models/dag.py
+++ airflow/models/dag.py
@@ -160,9 +160,6 @@ if TYPE_CHECKING:
     # This is a workaround because mypy doesn't work with hybrid_property
     # TODO: remove this hack and move hybrid_property back to main import block
     # See https://github.com/python/mypy/issues/4430
-#     hybrid_property = property
-# else:
-    # from sqlalchemy.ext.hybrid import hybrid_property
 
 log = logging.getLogger(__name__)
 
