59866b0fefc8aeea75326d0d68914e9f8184116a
dirrao <dirisala.gopal@gmail.com>
2024-05-22 19:25:50 +0530

    Resolving emr deprecated warnings

--- tests/always/test_example_dags.py
+++ tests/always/test_example_dags.py
@@ -49,7 +49,6 @@ IGNORE_AIRFLOW_PROVIDER_DEPRECATION_WARNING: tuple[str, ...] = (
     # and a corresponding Issue should be created on GitHub.
     "tests/system/providers/amazon/aws/example_ecs_fargate.py",
     "tests/system/providers/amazon/aws/example_eks_with_nodegroups.py",
-    "tests/system/providers/amazon/aws/example_emr.py",
     "tests/system/providers/amazon/aws/example_emr_notebook_execution.py",
     "tests/system/providers/google/cloud/bigquery/example_bigquery_operations.py",
     "tests/system/providers/google/cloud/bigquery/example_bigquery_sensors.py",
