9b82f7a06caef7cd255f5e8794019c2085e79410
dirrao <dirisala.gopal@gmail.com>
2024-05-26 13:41:49 +0530

    Resolving google bigquery deprecated warnings

--- tests/always/test_example_dags.py
+++ tests/always/test_example_dags.py
@@ -50,7 +50,6 @@ IGNORE_AIRFLOW_PROVIDER_DEPRECATION_WARNING: tuple[str, ...] = (
     "tests/system/providers/amazon/aws/example_ecs_fargate.py",
     "tests/system/providers/amazon/aws/example_emr_notebook_execution.py",
     "tests/system/providers/google/cloud/bigquery/example_bigquery_operations.py",
-    "tests/system/providers/google/cloud/bigquery/example_bigquery_sensors.py",
     "tests/system/providers/google/cloud/dataproc/example_dataproc_gke.py",
     "tests/system/providers/google/cloud/gcs/example_gcs_sensor.py",
     "tests/system/providers/google/cloud/gcs/example_gcs_to_gcs.py",
