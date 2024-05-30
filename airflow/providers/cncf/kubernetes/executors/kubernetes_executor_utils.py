a8584dc4c9b2181ff993e0716ac7a3f355d6aabe
Alan Ma <alanma0907@gmail.com>
2024-05-30 12:55:44 -0700

    Add else statement for when there are reasons but no fatal reasons

--- airflow/providers/cncf/kubernetes/executors/kubernetes_executor_utils.py
+++ airflow/providers/cncf/kubernetes/executors/kubernetes_executor_utils.py
@@ -249,13 +249,17 @@ class KubernetesJobWatcher(multiprocessing.Process, LoggingMixin):
                                 and container_status_state["waiting"]["message"] == "pull QPS exceeded"
                             ):
                                 continue
-                            self.log.error("Event: %s has container with fatal reason %s",
-                                          pod_name, container_status_state["waiting"]["reason"])
+                            self.log.error("Event: %s has container %s with fatal reason %s",
+                                    pod_name,
+                                    container_status["name"],
+                                    container_status_state["waiting"]["reason"]
+                            )
                             self.watcher_queue.put(
                                 (pod_name, namespace, TaskInstanceState.FAILED, annotations, resource_version)
                             )
                             break
-                self.log.info("Event: %s Pending, annotations: %s", pod_name, annotations_string)
+                else:
+                    self.log.info("Event: %s Pending, annotations: %s", pod_name, annotations_string)
             else:
                 self.log.debug("Event: %s Pending, annotations: %s", pod_name, annotations_string)
         elif status == "Failed":
