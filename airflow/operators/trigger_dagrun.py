133f5e4738fe33e69fefb9d57359570cd6807aa1
Josh Fell <48934154+josh-fell@users.noreply.github.com>
2024-06-01 23:02:40 -0400

    Pass triggered or existing DAG Run logical date to DagStateTrigger (#39960)
    Closes: #38353
    
    When using the TriggerDagRunOperator in `deferrable=True` mode, the DagStateTrigger is being passed the incorrect logical date to poll for. The trigger is using a logical date that is calculated on every execution rather than the logical from either the triggered DAG run or an existing DAG run (if the task is configured to not fail for existing DAG runs).
    
    This change corrects the logical date being used by the DagStateTrigger to poll for the triggered (or reset) DAG run.
    
    (cherry picked from commit ae648e6884a91d200ee63418563e32a2f78874c3)

--- airflow/operators/trigger_dagrun.py
+++ airflow/operators/trigger_dagrun.py
@@ -184,7 +184,8 @@ class TriggerDagRunOperator(BaseOperator):
 
         except DagRunAlreadyExists as e:
             if self.reset_dag_run:
-                self.log.info("Clearing %s on %s", self.trigger_dag_id, parsed_logical_date)
+                dag_run = e.dag_run
+                self.log.info("Clearing %s on %s", self.trigger_dag_id, dag_run.logical_date)
 
                 # Get target dag object and call clear()
                 dag_model = DagModel.get_current(self.trigger_dag_id)
@@ -193,7 +194,6 @@ class TriggerDagRunOperator(BaseOperator):
 
                 dag_bag = DagBag(dag_folder=dag_model.fileloc, read_dags_from_db=True)
                 dag = dag_bag.get_dag(self.trigger_dag_id)
-                dag_run = e.dag_run
                 dag.clear(start_date=dag_run.logical_date, end_date=dag_run.logical_date)
             else:
                 raise e
@@ -212,7 +212,7 @@ class TriggerDagRunOperator(BaseOperator):
                     trigger=DagStateTrigger(
                         dag_id=self.trigger_dag_id,
                         states=self.allowed_states + self.failed_states,
-                        execution_dates=[parsed_logical_date],
+                        execution_dates=[dag_run.logical_date],
                         poll_interval=self.poke_interval,
                     ),
                     method_name="execute_complete",
