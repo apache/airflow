adf0720d1f653d2ac4beecccc0df0017fca12ab2
boraberke <boraberkesahin@gmail.com>
2024-06-11 19:31:14 +0300

    Resolve deprecations in `LatestOnlyOperator`

--- tests/operators/test_latest_only_operator.py
+++ tests/operators/test_latest_only_operator.py
@@ -91,25 +91,33 @@ class TestLatestOnlyOperator:
         downstream_task2.set_upstream(downstream_task)
         downstream_task3.set_upstream(downstream_task)
 
+        data_interval = self.dag.infer_automated_data_interval(timezone.coerce_datetime(DEFAULT_DATE))
         self.dag.create_dagrun(
             run_type=DagRunType.SCHEDULED,
             start_date=timezone.utcnow(),
             execution_date=DEFAULT_DATE,
             state=State.RUNNING,
+            data_interval=data_interval,
         )
 
+        data_interval = self.dag.infer_automated_data_interval(
+            timezone.coerce_datetime(timezone.datetime(2016, 1, 1, 12))
+        )
         self.dag.create_dagrun(
             run_type=DagRunType.SCHEDULED,
             start_date=timezone.utcnow(),
             execution_date=timezone.datetime(2016, 1, 1, 12),
             state=State.RUNNING,
+            data_interval=data_interval,
         )
 
+        data_interval = self.dag.infer_automated_data_interval(timezone.coerce_datetime(END_DATE))
         self.dag.create_dagrun(
             run_type=DagRunType.SCHEDULED,
             start_date=timezone.utcnow(),
             execution_date=END_DATE,
             state=State.RUNNING,
+            data_interval=data_interval,
         )
 
         latest_task.run(start_date=DEFAULT_DATE, end_date=END_DATE)
