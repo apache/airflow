519838075334dc4303740a94e3cac2aa68cea524
João Rocha <joaonolascorocha@tecnico.ulisboa.pt>
2024-06-14 11:52:15 +0100

    Updated: rebase

--- airflow/www/static/js/dag/details/graph/DagNode.tsx
+++ airflow/www/static/js/dag/details/graph/DagNode.tsx
@@ -139,17 +139,10 @@ const DagNode = ({
           isOpen={isOpen}
           isGroup={!!childCount}
           onClick={(e) => {
-<<<<<<< HEAD
-              if (!!childCount) {
-                  e.stopPropagation();
-                  onToggleCollapse();
-              }
-=======
             if (childCount) {
               e.stopPropagation();
               onToggleCollapse();
             }
->>>>>>> ed7479e036 (Added missing changes)
           }}
           setupTeardownType={setupTeardownType}
           isZoomedOut={isZoomedOut}
