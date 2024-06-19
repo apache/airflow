5d746bc76d222427c0a04cfc0d811a1c0ca28cd0
Jake Roach <jroachgolf84@jroachgolf84-macbook-m3pro.local>
2024-06-19 13:23:52 -0400

    Stashing changes to unit test and operator

--- airflow/providers/microsoft/azure/transfers/s3_to_wasb.py
+++ airflow/providers/microsoft/azure/transfers/s3_to_wasb.py
@@ -65,7 +65,7 @@ class S3ToAzureBlobStorageOperator(BaseOperator):
         s3_verify: bool = False,
         s3_extra_args: dict | None = None,
         wasb_extra_args: dict | None = None,
-        **kwargs
+        **kwargs,
     ):
         # Call to constructor of the inherited BaseOperator class
         super().__init__(**kwargs)
@@ -74,16 +74,16 @@ class S3ToAzureBlobStorageOperator(BaseOperator):
         self.wasb_conn_id = wasb_conn_id
         self.s3_bucket = s3_bucket
         self.container_name = container_name
-        self.s3_prefix = s3_prefix if s3_prefix.endswith("/") else s3_prefix + "/"
+        self.s3_prefix = s3_prefix
         self.s3_key = s3_key
-        self.blob_prefix = blob_prefix if blob_prefix.endswith("/") else blob_prefix + "/"
+        self.blob_prefix = blob_prefix
         self.blob_name = blob_name
         self.delimiter = delimiter
         self.create_container = create_container
         self.replace = replace
         self.s3_verify = s3_verify
-        self.s3_extra_args = s3_extra_args
-        self.wasb_extra_args = wasb_extra_args
+        self.s3_extra_args = s3_extra_args or {}
+        self.wasb_extra_args = wasb_extra_args or {}
 
     def execute(self, context: Context) -> List[str]:
         """
@@ -98,7 +98,8 @@ class S3ToAzureBlobStorageOperator(BaseOperator):
         Returns:
             None
         """
-        # First, create a WasbHook and an S3Hook using the conn's that were provided
+        # First, create a WasbHook and an S3Hook using the conn's that were provided. These hooks will be
+        # passed to the methods we use to "help" with execution
         wasb_hook: WasbHook = WasbHook(wasb_conn_id=self.wasb_conn_id, **self.wasb_extra_args)
         s3_hook: S3Hook = S3Hook(
             aws_conn_id=self.aws_conn_id,
@@ -111,75 +112,107 @@ class S3ToAzureBlobStorageOperator(BaseOperator):
             f"Getting file: {self.s3_key}" if self.s3_key else f"Getting files from: {self.s3_prefix}"
         )
 
-        # First, try to use the S3 key that is passed in. Otherwise, pull all files from the S3 bucket/path
-        # prefix and remove the prefix
-        if self.s3_key:
-            # Only pull the file name from the s3_key
-            files_to_move: List[str] = [self.s3_key.split("/")[-1]]
-        else:
-            # Pull the keys from the s3_bucket using the provided prefix. Remove the prefix from the file
-            # name, and add to the list of files to move
-            s3_keys: List[str] = s3_hook.list_keys(
-                bucket_name=self.s3_bucket,
-                prefix=self.s3_prefix,
-                delimiter=self.delimiter  # Optional filter by the "extension" of the file
-            )
-            files_to_move = [s3_key.replace(f"{self.s3_prefix}/", "", 1) for s3_key in s3_keys]
+        # Pull a list of files to move from S3 to Azure Blob storage
+        files_to_move: List[str] = self.get_files_to_move(s3_hook, wasb_hook)
 
-        if not self.replace:
-            # Only grab the files from S3 that are not in Azure Blob already. This will prevent any files that
-            # exist in both S3 and Azure Blob from being overwritten. If a blob_name is provided, create a
-            # list with only this value
-            azure_blob_files: List[str] = [self.blob_name] if self.blob_name else \
-                wasb_hook.get_blobs_list_recursive(
-                container_name=self.container_name,
-                prefix=self.blob_prefix,
-                endswith=self.delimiter  # Optional filter by the "extension" of the file
-            )
-            existing_files = azure_blob_files if azure_blob_files else []  # TODO: Clean blob names
-            files_to_move = list(set(files_to_move) - set(existing_files))
-
+        # Check to see if there are indeed files to move. If so, move each of these files. Otherwise, output
+        # a logging message that denotes there are no files to move
         if files_to_move:
-            for file in files_to_move:
-                with tempfile.NamedTemporaryFile() as temp_file:
-                    # If using an s3_key, this creates a scenario where the only file in the files_to_move
-                    # list is going to be the name pulled from the s3_key. It's not verbose, but provides
-                    # standard implementation across the operator
-                    source_s3_key: str = self.s3_key if self.s3_key else self.s3_prefix + file
-                    s3_hook.download_file(
-                        local_path=temp_file.name,  # Make sure to look at this
-                        bucket_name=self.s3_bucket,
-                        key=source_s3_key
-                    )
+            for file_name in files_to_move:
+                self.move_file(file_name, s3_hook, wasb_hook)
 
-                    # Load the file to Azure Blob using either the key that has been passed in, or the key
-                    # from the list of files present in the s3_prefix, plus the blob_prefix
-                    destination_azure_blob_name: str = self.blob_name if self.blob_name \
-                        else self.blob_prefix + file
-                    wasb_hook.load_file(
-                        file_path=temp_file.name,
-                        container_name=self.container_name,
-                        blob_name=destination_azure_blob_name,
-                        create_container=self.create_container,
-                        **self.wasb_extra_args
-                    )
-
+            # Assuming that files_to_move is a list (which it always should be), this will get "hit" after the
+            # last file is moved from S3 -> Azure Blob
             self.log.info(f"All done, uploaded {len(files_to_move)} to Azure Blob.")
 
         else:
+            # If there are no files to move, a message will be printed. May want to consider alternative
+            # functionality (should an exception instead be raised?)
             self.log.info("There are no files to move!")
 
         # Return a list of the files that were moved
         return files_to_move
 
-    def move_file(self) -> None:
+    def get_files_to_move(self, s3_hook: S3Hook, wasb_hook: WasbHook) -> List[str]:
         """
-        move_files
+        get_files_to_move
 
         Description:
-            This "helper" method will take a file name, and move it from S3 to Azure Blob storage
+            This "helper" method will determine the list of files that need to be moved, and return the name
+            of each of these files
 
-        Params: None
-        Returns None
+        Params:
+            s3_hook (S3Hook)
+            wasb_hook (WasbHook)
+
+        Returns:
+            files_to_move (List[str])
         """
-        pass
+        if self.s3_key:
+            # Only pull the file name from the s3_key, drop the rest of the key
+            files_to_move: List[str] = [self.s3_key.split("/")[-1]]
+        else:
+            # Pull the keys from the s3_bucket using the provided prefix. Remove the prefix from the file
+            # name, and add to the list of files to move
+            s3_keys: List[str] = s3_hook.list_keys(bucket_name=self.s3_bucket, prefix=self.s3_prefix)
+            files_to_move = [s3_key.replace(f"{self.s3_prefix}/", "", 1) for s3_key in s3_keys]
+
+        if not self.replace:
+            # Only grab the files from S3 that are not in Azure Blob already. This will prevent any files that
+            # exist in both S3 and Azure Blob from being overwritten. If a blob_name is provided, check to
+            # see if that blob exists
+            if self.blob_name:
+                azure_blob_files = [self.blob_name.split("/")[-1]] if \
+                    wasb_hook.check_for_blob(self.container_name, self.blob_name) else \
+                    []
+            else:
+                azure_blob_files: List[str] = wasb_hook.get_blobs_list_recursive(
+                    container_name=self.container_name,
+                    prefix=self.blob_prefix,
+                    endswith=self.delimiter  # Optional filter by the "extension" of the file
+                )
+            # This conditional block only does one thing - it alters the elements in the files_to_move list.
+            # This list is being trimmed to remove the existing files in the Azure Blob (as mentioned above)
+            existing_files = azure_blob_files if azure_blob_files else []
+            files_to_move = list(set(files_to_move) - set(existing_files))
+
+        return files_to_move
+
+    def move_file(self, file_name: str, s3_hook: S3Hook, wasb_hook: WasbHook) -> None:
+        """
+        move_file
+
+        Description:
+            Move file from S3 to Azure Blob storage
+
+        Params:
+            file_name (str)
+            s3_hook (str)
+            wasb_hook ()
+
+        Returns: None
+        """
+        with tempfile.NamedTemporaryFile() as temp_file:
+            # If using an s3_key, this creates a scenario where the only file in the files_to_move
+            # list is going to be the name pulled from the s3_key. It's not verbose, but provides
+            # standard implementation across the operator
+            source_s3_key: str = self.s3_key if self.s3_key else self.s3_prefix + "/" + file_name
+            s3_hook.download_file(
+                local_path=temp_file.name,  # Make sure to look at this
+                bucket_name=self.s3_bucket,
+                key=source_s3_key
+            )
+
+            # Load the file to Azure Blob using either the key that has been passed in, or the key
+            # from the list of files present in the s3_prefix, plus the blob_prefix. There may be
+            # desire to only pass in an S3 key, in which case, the blob_name should be derived from
+            # the S3 key
+            destination_azure_blob_name: str = self.blob_name if self.blob_name \
+                else self.blob_prefix + "/" + file_name
+            wasb_hook.load_file(
+                file_path=temp_file.name,
+                container_name=self.container_name,
+                blob_name=destination_azure_blob_name,
+                create_container=self.create_container,
+                **self.wasb_extra_args
+            )
--- tests/providers/microsoft/azure/transfers/test_s3_to_wasb.py
+++ tests/providers/microsoft/azure/transfers/test_s3_to_wasb.py
@@ -23,45 +23,181 @@ from unittest import mock
 
 from moto import mock_aws
 
-from airflow.providers.amazon.aws.hooks.s3 import S3Hook
-from airflow.providers.microsoft.azure.hooks.wasb import WasbHook
 from airflow.providers.microsoft.azure.transfers.s3_to_wasb import S3ToAzureBlobStorageOperator
 
-from tests.test_utils.azure_system_helpers import WASB_CONNECTION_ID
-
 TASK_ID = "test-s3-to-azure-blob-operator"
+AWS_CONN_ID = "test-conn-id"
+S3_BUCKET = "s3://test-bucket/"
 CONTAINER_NAME = "test-container"
 DELIMITER = ".csv"
 PREFIX = "TEST"
-S3_BUCKET = "s3://test-bucket/"
+TEMPFILE_NAME = "test-tempfile"
 MOCK_FILES = ["TEST1.csv", "TEST2.csv", "TEST3.csv"]
-S3_ACL_POLICY = "private-read"
+
+# Here are some of the tests that need to be run (for the execute() function)
+# 1. Prefix with no existing files, without replace [DONE]
+# 2. Prefix with existing files, without replace [DONE]
+# 3. Prefix with existing files, with replace [DONE]
+# 4. Two keys without existing files, without replace [DONE]
+# 5. Two keys with existing files, without replace
+# 6. Two keys with existing files, with replace
+# 7. S3 key with Azure prefix, without existing files, without replace
+# 8. S3 key with Azure prefix, with existing files, without replace
+# 9. S3 key with Azure prefix, with existing files, with replace
+
+# Other tests that need to be run
+# - Test __init__
+# - Test get_files_to_move
 
 
 @mock_aws
 class TestS3ToAzureBlobStorageOperator:
+    def test__init__(self):
+        # Create a mock operator with a single set of parameters that are used to test the __init__()
+        # constructor. Not every parameter needs to be provided, as this code will also be used to test the
+        # default parameters that are configured
+        operator = S3ToAzureBlobStorageOperator(
+            task_id=TASK_ID,
+            aws_conn_id="test-conn-id",
+            s3_bucket=S3_BUCKET,
+            s3_prefix=PREFIX,
+            container_name=CONTAINER_NAME,
+            blob_prefix=PREFIX,
+            replace=True,
+            delimiter=DELIMITER
+        )
 
-    @mock.patch("airflow.providers.amazon.aws.transfers.azure_blob_to_s3.S3Hook")
-    @mock.patch("airflow.providers.amazon.microsoft.azure.transfers.s3_to_wasb.WasbHook")
-    def test_operators_prefix(self, s3_mock_hook, wasb_mock_hook):
-        # Set the list files that the S3Hook should return
+        # ... is None is used to validate if a value is None, while not ... is used to evaluate if a value
+        # is False
+        assert operator.task_id == TASK_ID
+        assert operator.aws_conn_id == AWS_CONN_ID
+        assert operator.wasb_conn_id == "wasb_default"
+        assert operator.s3_bucket == S3_BUCKET
+        assert operator.container_name == CONTAINER_NAME
+        assert operator.s3_prefix == PREFIX
+        assert operator.s3_key is None
+        assert operator.blob_prefix == PREFIX
+        assert operator.blob_name is None
+        assert operator.delimiter == DELIMITER
+        assert not operator.create_container  # Should be false (match default value in constructor)
+        assert operator.replace
+        assert not operator.s3_verify  # Should be false (match default value in constructor)
+        assert operator.s3_extra_args == {}
+        assert operator.wasb_extra_args == {}
+
+    # There are a number of very similar tests that use the same mocking, and for the most part, the same
+    # logic
+    @mock.patch("airflow.providers.microsoft.azure.transfers.s3_to_wasb.S3Hook")
+    @mock.patch("airflow.providers.microsoft.azure.transfers.s3_to_wasb.WasbHook")
+    @mock.patch("tempfile.NamedTemporaryFile")
+    def test_execute_prefix_without_replace_empty_destination(
+        self, tempfile_mock, wasb_mock_hook, s3_mock_hook
+    ):
+        # TODO: Ideally, I'd like to rewrite this to use the get_files_to_move() method, rather than execute
+        # Set the list files that the S3Hook should return, as well as the list of files that are returned
+        # when the get_blobs_list_recursive method is called using the WasbHook. In this scenario, the
+        # destination is empty, meaning that the full list should be returned
         s3_mock_hook.return_value.list_keys.return_value = MOCK_FILES
         wasb_mock_hook.return_value.get_blobs_list_recursive.return_value = []
 
+        s3_mock_hook.return_value.download_file.return_value = RawIOBase(b"test file contents")
+        tempfile_mock.return_value.__enter__.return_value.name = TEMPFILE_NAME
+
+        # For testing, we're using a few default values. The most notable are the s3_conn_id and the
+        # wasb_conn_id
         operator = S3ToAzureBlobStorageOperator(
             task_id=TASK_ID,
             s3_bucket=S3_BUCKET,
-            s3_prefix="TEST",
+            s3_prefix=PREFIX,
             container_name=CONTAINER_NAME,
-            blob_prefix="TEST"
+            blob_prefix=PREFIX,
         )
-        # Need to create a container here
+        # When the execute() method is called in the operator, it first creates two hooks (S3Hook and
+        # WasbHook). The return values from any calls that use these hooks are mocked above, allowing the
+        # tests to run in a test setting
         uploaded_files = operator.execute(None)
 
         assert sorted(uploaded_files) == sorted(MOCK_FILES)
 
+    @mock.patch("airflow.providers.microsoft.azure.transfers.s3_to_wasb.S3Hook")
+    @mock.patch("airflow.providers.microsoft.azure.transfers.s3_to_wasb.WasbHook")
+    @mock.patch("tempfile.NamedTemporaryFile")
+    def test_execute_prefix_without_replace_populated_destination(
+        self, tempfile_mock, wasb_mock_hook, s3_mock_hook
+    ):
+        # Set the list files that the S3Hook should return
+        s3_mock_hook.return_value.list_keys.return_value = MOCK_FILES
+        wasb_mock_hook.return_value.get_blobs_list_recursive.return_value = MOCK_FILES[1:]
+
+        s3_mock_hook.return_value.download_file.return_value = RawIOBase(b"test file contents")
+        tempfile_mock.return_value.__enter__.return_value.name = TEMPFILE_NAME
+
+        operator = S3ToAzureBlobStorageOperator(
+            task_id=TASK_ID,
+            s3_bucket=S3_BUCKET,
+            s3_prefix=PREFIX,
+            container_name=CONTAINER_NAME,
+            blob_prefix=PREFIX,
+        )
+        # Placing an empty "context" object here (using None)
+        uploaded_files = operator.execute(None)
+
+        assert sorted(uploaded_files) == sorted([MOCK_FILES[0]])
+
+    @mock.patch("airflow.providers.microsoft.azure.transfers.s3_to_wasb.S3Hook")
+    @mock.patch("airflow.providers.microsoft.azure.transfers.s3_to_wasb.WasbHook")
+    @mock.patch("tempfile.NamedTemporaryFile")
+    def test_execute_prefix_with_replace_populated_destination(
+        self, tempfile_mock, wasb_mock_hook, s3_mock_hook
+    ):
+        # Set the list files that the S3Hook should return
+        s3_mock_hook.return_value.list_keys.return_value = MOCK_FILES
+        wasb_mock_hook.return_value.get_blobs_list_recursive.return_value = MOCK_FILES[1:]
+
+        s3_mock_hook.return_value.download_file.return_value = RawIOBase(b"test file contents")
+        tempfile_mock.return_value.__enter__.return_value.name = TEMPFILE_NAME
+
+        operator = S3ToAzureBlobStorageOperator(
+            task_id=TASK_ID,
+            s3_bucket=S3_BUCKET,
+            s3_prefix=PREFIX,
+            container_name=CONTAINER_NAME,
+            blob_prefix=PREFIX,
+            replace=True,
+        )
+        # Placing an empty "context" object here (using None)
+        uploaded_files = operator.execute(None)
+
+        # Since the replace parameter is being set to True, all the files that are present in the S3 bucket
+        # will be moved to the Azure Blob, even though there are existing files in the Azure Blob
+        assert sorted(uploaded_files) == sorted(MOCK_FILES)
+
+    @mock.patch("airflow.providers.microsoft.azure.transfers.s3_to_wasb.S3Hook")
+    @mock.patch("airflow.providers.microsoft.azure.transfers.s3_to_wasb.WasbHook")
+    @mock.patch("tempfile.NamedTemporaryFile")
+    def test_execute_key_without_replace_empty_destination(
+        self, tempfile_mock, wasb_mock_hook, s3_mock_hook
+    ):
+        # Different than above, able to remove the mocking of the list_keys method for the S3 hook (since a
+        # single key is being passed, rather than a prefix). Here, there is no file present in the container,
+        # so the file can be moved
+        wasb_mock_hook.return_value.check_for_blob.return_value = False
+
+        s3_mock_hook.return_value.download_file.return_value = RawIOBase(b"test file contents")
+        tempfile_mock.return_value.__enter__.return_value.name = TEMPFILE_NAME
+
+        operator = S3ToAzureBlobStorageOperator(
+            task_id=TASK_ID,
+            s3_bucket=S3_BUCKET,
+            s3_key="TEST/TEST1.csv",
+            container_name=CONTAINER_NAME,
+            blob_name="TEST/TEST1.csv",
+        )
+        # Placing an empty "context" object here (using None)
+        uploaded_files = operator.execute(None)
+
+        # Only the file name should be returned, rather than the entire blob name
+        assert sorted(uploaded_files) == sorted(["TEST1.csv"])
+
 
 # Test helpers
-def _create_container():
-    hook = WasbHook(WASB_CONNECTION_ID)
-    return hook
