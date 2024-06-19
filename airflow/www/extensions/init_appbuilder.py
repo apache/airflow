62cdd307cc8b10aa42ae0720a9518d0a2976ff11
vincbeck <vincbeck@amazon.com>
2024-06-19 09:16:39 -0400

    Use `deprecated_options` to deprecate some webserver related config options

--- airflow/www/extensions/init_appbuilder.py
+++ airflow/www/extensions/init_appbuilder.py
@@ -131,19 +131,9 @@ class AirflowAppBuilder:
         base_template="airflow/main.html",
         static_folder="static/appbuilder",
         static_url_path="/appbuilder",
-        update_perms=conf.getboolean(
-            "fab", "UPDATE_FAB_PERMS", fallback=conf.getboolean("webserver", "UPDATE_FAB_PERMS")
-        ),
-        auth_rate_limited=conf.getboolean(
-            "fab",
-            "AUTH_RATE_LIMITED",
-            fallback=conf.getboolean("webserver", "AUTH_RATE_LIMITED", fallback=True),
-        ),
-        auth_rate_limit=conf.get(
-            "fab",
-            "AUTH_RATE_LIMIT",
-            fallback=conf.get("webserver", "AUTH_RATE_LIMIT", fallback="5 per 40 second"),
-        ),
+        update_perms=conf.getboolean("fab", "UPDATE_FAB_PERMS"),
+        auth_rate_limited=conf.getboolean("fab", "AUTH_RATE_LIMITED"),
+        auth_rate_limit=conf.get("fab", "AUTH_RATE_LIMIT"),
     ):
         """
         App-builder constructor.
@@ -677,17 +667,7 @@ def init_appbuilder(app: Flask) -> AirflowAppBuilder:
         app=app,
         session=settings.Session,
         base_template="airflow/main.html",
-        update_perms=conf.getboolean(
-            "fab", "UPDATE_FAB_PERMS", fallback=conf.getboolean("webserver", "UPDATE_FAB_PERMS")
-        ),
-        auth_rate_limited=conf.getboolean(
-            "fab",
-            "AUTH_RATE_LIMITED",
-            fallback=conf.getboolean("webserver", "AUTH_RATE_LIMITED", fallback=True),
-        ),
-        auth_rate_limit=conf.get(
-            "fab",
-            "AUTH_RATE_LIMIT",
-            fallback=conf.get("webserver", "AUTH_RATE_LIMIT", fallback="5 per 40 second"),
-        ),
+        update_perms=conf.getboolean("fab", "UPDATE_FAB_PERMS"),
+        auth_rate_limited=conf.getboolean("fab", "AUTH_RATE_LIMITED"),
+        auth_rate_limit=conf.get("fab", "AUTH_RATE_LIMIT"),
     )
