69d2dce3c41728c29b9ad38aea6fa3ef178dd9d4
Satish Ch <satishchinthanippu@gmail.com>
2024-06-21 03:53:16 -0700

    UI changes

--- .github/workflows/system-tests-files/report_generate_index_html.py
+++ .github/workflows/system-tests-files/report_generate_index_html.py
@@ -1,5 +1,6 @@
 import os
 import re
+from datetime import datetime
 
 import pandas as pd
 from jinja2 import Template
@@ -17,6 +18,7 @@ class Record:
 
 pd.options.display.max_rows = 9999
 fileName = 'reporttest.csv'
+# Read test report
 df = pd.read_csv(fileName, delimiter=',', header=None)
 
 # Read system test files under system tests directory
@@ -32,7 +34,7 @@ system_test_files = [file_name for file_name in file_names if
 liRecords = []
 items = []
 
-
+# Getting each system test history from report and preparing UI for github page
 for system_test_file in system_test_files:
     record = Record('', 0, 0,[], '', '')
     system_test_file = system_test_file[:-3]
@@ -42,10 +44,11 @@ for system_test_file in system_test_files:
     for index, row in df.iterrows():
         if row[0].strip() == system_test_file:
             record.time = row[2]
-            record.rundate = row[3]
+            record.rundate = row[3] # Got Date in str format - YYYY-MM-DD
             if row[3] is not None:
                 try:
-                    record.rundate = row[3].strftime("%m/%d/%Y")
+                    datetime_object = datetime.strptime(row[3], '%Y-%m-%d') # Getting date object from date str
+                    record.rundate = datetime_object.strftime('%m/%d/%Y') # Converting to US - MM-DD-YYYY format
                 except ValueError:
                     print(ValueError)
             if row[1] == 'S':
