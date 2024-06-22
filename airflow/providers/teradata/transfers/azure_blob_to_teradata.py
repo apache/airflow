33a14198c106be4b0a89d67bd711b560b0f4251d
Satish Chinthanippu <satishchinthanippu@gmail.com>
2024-06-22 02:50:41 -0700

    Updates to Teradata Provider (#40378)
    Added support of teradata authorization object for cloud transfer operators to teradata. (#46)
    
    1. Added teradata authorization object for authorization in transfer operators
    2. Added security token support in s3toteradata transfer operator

--- airflow/providers/teradata/transfers/azure_blob_to_teradata.py
+++ airflow/providers/teradata/transfers/azure_blob_to_teradata.py
@@ -48,10 +48,17 @@ class AzureBlobStorageToTeradataOperator(BaseOperator):
         The URI format is `/az/YOUR-STORAGE-ACCOUNT.blob.core.windows.net/YOUR-CONTAINER/YOUR-BLOB-LOCATION`.
         Refer to
         https://docs.teradata.com/search/documents?query=native+object+store&sort=last_update&virtual-field=title_only&content-lang=en-US
+    :param public_bucket: Specifies whether the provided blob container is public. If the blob container is public,
+        it means that anyone can access the objects within it via a URL without requiring authentication.
+        If the bucket is private and authentication is not provided, the operator will throw an exception.
     :param azure_conn_id: The Airflow WASB connection used for azure blob credentials.
     :param teradata_table: The name of the teradata table to which the data is transferred.(templated)
     :param teradata_conn_id: The connection ID used to connect to Teradata
         :ref:`Teradata connection <howto/connection:Teradata>`
+    :param teradata_authorization_name: The name of Teradata Authorization Database Object,
+        is used to control who can access an Azure Blob object store.
+        Refer to
+        https://docs.teradata.com/r/Enterprise_IntelliFlex_VMware/Teradata-VantageTM-Native-Object-Store-Getting-Started-Guide-17.20/Setting-Up-Access/Controlling-Foreign-Table-Access-with-an-AUTHORIZATION-Object
 
     Note that ``blob_source_key`` and ``teradata_table`` are
     templated, so you can use variables in them if you wish.
@@ -64,37 +71,48 @@ class AzureBlobStorageToTeradataOperator(BaseOperator):
         self,
         *,
         blob_source_key: str,
+        public_bucket: bool = False,
         azure_conn_id: str = "azure_default",
         teradata_table: str,
         teradata_conn_id: str = "teradata_default",
+        teradata_authorization_name: str = "",
         **kwargs,
     ) -> None:
         super().__init__(**kwargs)
         self.blob_source_key = blob_source_key
+        self.public_bucket = public_bucket
         self.azure_conn_id = azure_conn_id
         self.teradata_table = teradata_table
         self.teradata_conn_id = teradata_conn_id
+        self.teradata_authorization_name = teradata_authorization_name
 
     def execute(self, context: Context) -> None:
         self.log.info(
             "transferring data from %s to teradata table %s...", self.blob_source_key, self.teradata_table
         )
-        azure_hook = WasbHook(wasb_conn_id=self.azure_conn_id)
-        conn = azure_hook.get_connection(self.azure_conn_id)
-        # Obtaining the Azure client ID and Azure secret in order to access a specified Blob container
-        access_id = conn.login if conn.login is not None else ""
-        access_secret = conn.password if conn.password is not None else ""
         teradata_hook = TeradataHook(teradata_conn_id=self.teradata_conn_id)
+        credentials_part = "ACCESS_ID= '' ACCESS_KEY= ''"
+        if not self.public_bucket:
+            # Accessing data directly from the Azure Blob Storage and creating permanent table inside the
+            # database
+            if self.teradata_authorization_name:
+                credentials_part = f"AUTHORIZATION={self.teradata_authorization_name}"
+            else:
+                # Obtaining the Azure client ID and Azure secret in order to access a specified Blob container
+                azure_hook = WasbHook(wasb_conn_id=self.azure_conn_id)
+                conn = azure_hook.get_connection(self.azure_conn_id)
+                access_id = conn.login
+                access_secret = conn.password
+                credentials_part = f"ACCESS_ID= '{access_id}' ACCESS_KEY= '{access_secret}'"
         sql = dedent(f"""
-                    CREATE MULTISET TABLE {self.teradata_table}  AS
-                    (
-                        SELECT * FROM (
-                            LOCATION = '{self.blob_source_key}'
-                            ACCESS_ID= '{access_id}'
-                            ACCESS_KEY= '{access_secret}'
-                    ) AS d
-                    ) WITH DATA
-                """).rstrip()
+                        CREATE MULTISET TABLE {self.teradata_table} AS
+                        (
+                            SELECT * FROM (
+                                LOCATION = '{self.blob_source_key}'
+                                {credentials_part}
+                            ) AS d
+                        ) WITH DATA
+                        """).rstrip()
         try:
             teradata_hook.run(sql, True)
         except Exception as ex:
