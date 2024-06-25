efa1f5be266d91f1c482ff053f7c3355b602b55e
Ephraim Anierobi <splendidzigy24@gmail.com>
2024-06-25 17:06:00 +0100

    Flush session after updating dagrun in scheduler log

--- airflow/jobs/scheduler_job_runner.py
+++ airflow/jobs/scheduler_job_runner.py
@@ -1871,4 +1871,6 @@ class SchedulerJobRunner(BaseJobRunner, LoggingMixin):
                 continue
             if not ti.is_premature:
                 ti.dag_run.activate_scheduling()
+                session.merge(ti.dag_run)
                 drs.append(ti.dag_run)
+        session.flush()
