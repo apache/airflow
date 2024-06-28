42e255763d3eaaad377db31b1eb21f4102b679a9
Christian Yarros <yarros@google.com>
2024-06-28 18:18:36 +0000

    refactor to match vertex ai api, add contents and tools

--- tests/providers/google/cloud/hooks/vertex_ai/test_generative_model.py
+++ tests/providers/google/cloud/hooks/vertex_ai/test_generative_model.py
@@ -24,7 +24,7 @@ import pytest
 # For no Pydantic environment, we need to skip the tests
 pytest.importorskip("google.cloud.aiplatform_v1")
 vertexai = pytest.importorskip("vertexai.generative_models")
-from vertexai.generative_models import HarmBlockThreshold, HarmCategory
+from vertexai.generative_models import HarmBlockThreshold, HarmCategory, Tool, grounding
 
 from airflow.providers.google.cloud.hooks.vertex_ai.generative_model import (
     GenerativeModelHook,
@@ -38,6 +38,7 @@ GCP_PROJECT = "test-project"
 GCP_LOCATION = "us-central1"
 
 TEST_PROMPT = "In 10 words or less, what is apache airflow?"
+TEST_CONTENTS = [TEST_PROMPT]
 TEST_LANGUAGE_PRETRAINED_MODEL = "text-bison"
 TEST_TEMPERATURE = 0.0
 TEST_MAX_OUTPUT_TOKENS = 256
@@ -58,11 +59,7 @@ TEST_GENERATION_CONFIG = {
     "top_p": TEST_TOP_P,
     "temperature": TEST_TEMPERATURE,
 }
-
-TEST_MULTIMODAL_VISION_MODEL = "gemini-pro-vision"
-TEST_VISION_PROMPT = "In 10 words or less, describe this content."
-TEST_MEDIA_GCS_PATH = "gs://download.tensorflow.org/example_images/320px-Felis_catus-cat_on_snow.jpg"
-TEST_MIME_TYPE = "image/jpeg"
+TEST_TOOLS = [Tool.from_google_search_retrieval(grounding.GoogleSearchRetrieval())]
 
 BASE_STRING = "airflow.providers.google.common.hooks.base_google.{}"
 GENERATIVE_MODEL_STRING = "airflow.providers.google.cloud.hooks.vertex_ai.generative_model.{}"
@@ -80,8 +77,8 @@ class TestGenerativeModelWithDefaultProjectIdHook:
             self.hook.get_credentials = self.dummy_get_credentials
 
     @mock.patch(GENERATIVE_MODEL_STRING.format("GenerativeModelHook.get_text_generation_model"))
-    def test_prompt_language_model(self, mock_model) -> None:
-        self.hook.prompt_language_model(
+    def test_text_generation_model_predict(self, mock_model) -> None:
+        self.hook.text_generation_model_predict(
             project_id=GCP_PROJECT,
             location=GCP_LOCATION,
             prompt=TEST_PROMPT,
@@ -101,8 +98,8 @@ class TestGenerativeModelWithDefaultProjectIdHook:
         )
 
     @mock.patch(GENERATIVE_MODEL_STRING.format("GenerativeModelHook.get_text_embedding_model"))
-    def test_generate_text_embeddings(self, mock_model) -> None:
-        self.hook.generate_text_embeddings(
+    def test_text_embedding_model_get_embeddings(self, mock_model) -> None:
+        self.hook.text_embedding_model_get_embeddings(
             project_id=GCP_PROJECT,
             location=GCP_LOCATION,
             prompt=TEST_PROMPT,
@@ -112,40 +109,20 @@ class TestGenerativeModelWithDefaultProjectIdHook:
         mock_model.return_value.get_embeddings.assert_called_once_with([TEST_PROMPT])
 
     @mock.patch(GENERATIVE_MODEL_STRING.format("GenerativeModelHook.get_generative_model"))
-    def test_prompt_multimodal_model(self, mock_model) -> None:
-        self.hook.prompt_multimodal_model(
+    def test_generative_model_generate_content(self, mock_model) -> None:
+        self.hook.generative_model_generate_content(
             project_id=GCP_PROJECT,
+            contents=TEST_CONTENTS,
             location=GCP_LOCATION,
-            prompt=TEST_PROMPT,
+            tools=TEST_TOOLS,
             generation_config=TEST_GENERATION_CONFIG,
             safety_settings=TEST_SAFETY_SETTINGS,
             pretrained_model=TEST_MULTIMODAL_PRETRAINED_MODEL,
         )
         mock_model.assert_called_once_with(TEST_MULTIMODAL_PRETRAINED_MODEL)
         mock_model.return_value.generate_content.assert_called_once_with(
-            contents=[TEST_PROMPT],
-            generation_config=TEST_GENERATION_CONFIG,
-            safety_settings=TEST_SAFETY_SETTINGS,
-        )
-
-    @mock.patch(GENERATIVE_MODEL_STRING.format("GenerativeModelHook.get_generative_model_part"))
-    @mock.patch(GENERATIVE_MODEL_STRING.format("GenerativeModelHook.get_generative_model"))
-    def test_prompt_multimodal_model_with_media(self, mock_model, mock_part) -> None:
-        self.hook.prompt_multimodal_model_with_media(
-            project_id=GCP_PROJECT,
-            location=GCP_LOCATION,
-            prompt=TEST_VISION_PROMPT,
-            generation_config=TEST_GENERATION_CONFIG,
-            safety_settings=TEST_SAFETY_SETTINGS,
-            pretrained_model=TEST_MULTIMODAL_VISION_MODEL,
-            media_gcs_path=TEST_MEDIA_GCS_PATH,
-            mime_type=TEST_MIME_TYPE,
-        )
-        mock_model.assert_called_once_with(TEST_MULTIMODAL_VISION_MODEL)
-        mock_part.assert_called_once_with(TEST_MEDIA_GCS_PATH, TEST_MIME_TYPE)
-
-        mock_model.return_value.generate_content.assert_called_once_with(
-            contents=[TEST_VISION_PROMPT, mock_part.return_value],
+            contents=TEST_CONTENTS,
+            tools=TEST_TOOLS,
             generation_config=TEST_GENERATION_CONFIG,
             safety_settings=TEST_SAFETY_SETTINGS,
         )
