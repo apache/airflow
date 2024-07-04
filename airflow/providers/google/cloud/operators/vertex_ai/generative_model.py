9c9706730c06c1f4eecd5e72efa5cac1a8d95d83
Christian Yarros <yarros@google.com>
2024-07-04 11:55:22 -0400

    Update Google Cloud Generative Model Hooks/Operators to bring parity with Vertex AI API (#40484)

--- airflow/providers/google/cloud/operators/vertex_ai/generative_model.py
+++ airflow/providers/google/cloud/operators/vertex_ai/generative_model.py
@@ -21,6 +21,9 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Sequence
 
+from deprecated import deprecated
+
+from airflow.exceptions import AirflowProviderDeprecationWarning
 from airflow.providers.google.cloud.hooks.vertex_ai.generative_model import GenerativeModelHook
 from airflow.providers.google.cloud.operators.cloud_base import GoogleCloudBaseOperator
 
@@ -28,6 +31,10 @@ if TYPE_CHECKING:
     from airflow.utils.context import Context
 
 
+@deprecated(
+    reason="This operator is deprecated and will be removed after 01.01.2025, please use `TextGenerationModelPredictOperator`.",
+    category=AirflowProviderDeprecationWarning,
+)
 class PromptLanguageModelOperator(GoogleCloudBaseOperator):
     """
     Uses the Vertex AI PaLM API to generate natural language text.
@@ -113,6 +120,10 @@ class PromptLanguageModelOperator(GoogleCloudBaseOperator):
         return response
 
 
+@deprecated(
+    reason="This operator is deprecated and will be removed after 01.01.2025, please use `TextEmbeddingModelGetEmbeddingsOperator`.",
+    category=AirflowProviderDeprecationWarning,
+)
 class GenerateTextEmbeddingsOperator(GoogleCloudBaseOperator):
     """
     Uses the Vertex AI PaLM API to generate natural language text.
@@ -177,6 +188,10 @@ class GenerateTextEmbeddingsOperator(GoogleCloudBaseOperator):
         return response
 
 
+@deprecated(
+    reason="This operator is deprecated and will be removed after 01.01.2025, please use `GenerativeModelGenerateContentOperator`.",
+    category=AirflowProviderDeprecationWarning,
+)
 class PromptMultimodalModelOperator(GoogleCloudBaseOperator):
     """
     Use the Vertex AI Gemini Pro foundation model to generate natural language text.
@@ -249,6 +264,10 @@ class PromptMultimodalModelOperator(GoogleCloudBaseOperator):
         return response
 
 
+@deprecated(
+    reason="This operator is deprecated and will be removed after 01.01.2025, please use `GenerativeModelGenerateContentOperator`.",
+    category=AirflowProviderDeprecationWarning,
+)
 class PromptMultimodalModelWithMediaOperator(GoogleCloudBaseOperator):
     """
     Use the Vertex AI Gemini Pro foundation model to generate natural language text.
@@ -326,5 +345,229 @@ class PromptMultimodalModelWithMediaOperator(GoogleCloudBaseOperator):
 
         self.log.info("Model response: %s", response)
         self.xcom_push(context, key="prompt_response", value=response)
+
+        return response
+
+
+class TextGenerationModelPredictOperator(GoogleCloudBaseOperator):
+    """
+    Uses the Vertex AI PaLM API to generate natural language text.
+
+    :param project_id: Required. The ID of the Google Cloud project that the
+        service belongs to (templated).
+    :param location: Required. The ID of the Google Cloud location that the
+        service belongs to (templated).
+    :param prompt: Required. Inputs or queries that a user or a program gives
+        to the Vertex AI PaLM API, in order to elicit a specific response (templated).
+    :param pretrained_model: By default uses the pre-trained model `text-bison`,
+        optimized for performing natural language tasks such as classification,
+        summarization, extraction, content creation, and ideation.
+    :param temperature: Temperature controls the degree of randomness in token
+        selection. Defaults to 0.0.
+    :param max_output_tokens: Token limit determines the maximum amount of text
+        output. Defaults to 256.
+    :param top_p: Tokens are selected from most probable to least until the sum
+        of their probabilities equals the top_p value. Defaults to 0.8.
+    :param top_k: A top_k of 1 means the selected token is the most probable
+        among all tokens. Defaults to 0.4.
+    :param gcp_conn_id: The connection ID to use connecting to Google Cloud.
+    :param impersonation_chain: Optional service account to impersonate using short-term
+        credentials, or chained list of accounts required to get the access_token
+        of the last account in the list, which will be impersonated in the request.
+        If set as a string, the account must grant the originating account
+        the Service Account Token Creator IAM role.
+        If set as a sequence, the identities from the list must grant
+        Service Account Token Creator IAM role to the directly preceding identity, with first
+        account from the list granting this role to the originating account (templated).
+    """
+
+    template_fields = ("location", "project_id", "impersonation_chain", "prompt")
+
+    def __init__(
+        self,
+        *,
+        project_id: str,
+        location: str,
+        prompt: str,
+        pretrained_model: str = "text-bison",
+        temperature: float = 0.0,
+        max_output_tokens: int = 256,
+        top_p: float = 0.8,
+        top_k: int = 40,
+        gcp_conn_id: str = "google_cloud_default",
+        impersonation_chain: str | Sequence[str] | None = None,
+        **kwargs,
+    ) -> None:
+        super().__init__(**kwargs)
+        self.project_id = project_id
+        self.location = location
+        self.prompt = prompt
+        self.pretrained_model = pretrained_model
+        self.temperature = temperature
+        self.max_output_tokens = max_output_tokens
+        self.top_p = top_p
+        self.top_k = top_k
+        self.gcp_conn_id = gcp_conn_id
+        self.impersonation_chain = impersonation_chain
+
+    def execute(self, context: Context):
+        self.hook = GenerativeModelHook(
+            gcp_conn_id=self.gcp_conn_id,
+            impersonation_chain=self.impersonation_chain,
+        )
+
+        self.log.info("Submitting prompt")
+        response = self.hook.text_generation_model_predict(
+            project_id=self.project_id,
+            location=self.location,
+            prompt=self.prompt,
+            pretrained_model=self.pretrained_model,
+            temperature=self.temperature,
+            max_output_tokens=self.max_output_tokens,
+            top_p=self.top_p,
+            top_k=self.top_k,
+        )
+
+        self.log.info("Model response: %s", response)
+        self.xcom_push(context, key="model_response", value=response)
+
+        return response
+
+
+class TextEmbeddingModelGetEmbeddingsOperator(GoogleCloudBaseOperator):
+    """
+    Uses the Vertex AI Embeddings API to generate embeddings based on prompt.
+
+    :param project_id: Required. The ID of the Google Cloud project that the
+        service belongs to (templated).
+    :param location: Required. The ID of the Google Cloud location that the
+        service belongs to (templated).
+    :param prompt: Required. Inputs or queries that a user or a program gives
+        to the Vertex AI PaLM API, in order to elicit a specific response (templated).
+    :param pretrained_model: By default uses the pre-trained model `textembedding-gecko`,
+        optimized for performing text embeddings.
+    :param gcp_conn_id: The connection ID to use connecting to Google Cloud.
+    :param impersonation_chain: Optional service account to impersonate using short-term
+        credentials, or chained list of accounts required to get the access_token
+        of the last account in the list, which will be impersonated in the request.
+        If set as a string, the account must grant the originating account
+        the Service Account Token Creator IAM role.
+        If set as a sequence, the identities from the list must grant
+        Service Account Token Creator IAM role to the directly preceding identity, with first
+        account from the list granting this role to the originating account (templated).
+    """
+
+    template_fields = ("location", "project_id", "impersonation_chain", "prompt")
+
+    def __init__(
+        self,
+        *,
+        project_id: str,
+        location: str,
+        prompt: str,
+        pretrained_model: str = "textembedding-gecko",
+        gcp_conn_id: str = "google_cloud_default",
+        impersonation_chain: str | Sequence[str] | None = None,
+        **kwargs,
+    ) -> None:
+        super().__init__(**kwargs)
+        self.project_id = project_id
+        self.location = location
+        self.prompt = prompt
+        self.pretrained_model = pretrained_model
+        self.gcp_conn_id = gcp_conn_id
+        self.impersonation_chain = impersonation_chain
+
+    def execute(self, context: Context):
+        self.hook = GenerativeModelHook(
+            gcp_conn_id=self.gcp_conn_id,
+            impersonation_chain=self.impersonation_chain,
+        )
+
+        self.log.info("Generating text embeddings")
+        response = self.hook.text_embedding_model_get_embeddings(
+            project_id=self.project_id,
+            location=self.location,
+            prompt=self.prompt,
+            pretrained_model=self.pretrained_model,
+        )
+
+        self.log.info("Model response: %s", response)
+        self.xcom_push(context, key="model_response", value=response)
+
+        return response
+
+
+class GenerativeModelGenerateContentOperator(GoogleCloudBaseOperator):
+    """
+    Use the Vertex AI Gemini Pro foundation model to generate content.
+
+    :param project_id: Required. The ID of the Google Cloud project that the
+        service belongs to (templated).
+    :param contents: Required. The multi-part content of a message that a user or a program
+        gives to the generative model, in order to elicit a specific response.
+    :param location: Required. The ID of the Google Cloud location that the
+        service belongs to (templated).
+    :param generation_config: Optional. Generation configuration settings.
+    :param safety_settings: Optional. Per request settings for blocking unsafe content.
+    :param pretrained_model: By default uses the pre-trained model `gemini-pro`,
+        supporting prompts with text-only input, including natural language
+        tasks, multi-turn text and code chat, and code generation. It can
+        output text and code.
+    :param gcp_conn_id: The connection ID to use connecting to Google Cloud.
+    :param impersonation_chain: Optional service account to impersonate using short-term
+        credentials, or chained list of accounts required to get the access_token
+        of the last account in the list, which will be impersonated in the request.
+        If set as a string, the account must grant the originating account
+        the Service Account Token Creator IAM role.
+        If set as a sequence, the identities from the list must grant
+        Service Account Token Creator IAM role to the directly preceding identity, with first
+        account from the list granting this role to the originating account (templated).
+    """
+
+    template_fields = ("location", "project_id", "impersonation_chain", "contents")
+
+    def __init__(
+        self,
+        *,
+        project_id: str,
+        contents: list,
+        location: str,
+        tools: list | None = None,
+        generation_config: dict | None = None,
+        safety_settings: dict | None = None,
+        pretrained_model: str = "gemini-pro",
+        gcp_conn_id: str = "google_cloud_default",
+        impersonation_chain: str | Sequence[str] | None = None,
+        **kwargs,
+    ) -> None:
+        super().__init__(**kwargs)
+        self.project_id = project_id
+        self.location = location
+        self.contents = contents
+        self.tools = tools
+        self.generation_config = generation_config
+        self.safety_settings = safety_settings
+        self.pretrained_model = pretrained_model
+        self.gcp_conn_id = gcp_conn_id
+        self.impersonation_chain = impersonation_chain
+
+    def execute(self, context: Context):
+        self.hook = GenerativeModelHook(
+            gcp_conn_id=self.gcp_conn_id,
+            impersonation_chain=self.impersonation_chain,
+        )
+        response = self.hook.generative_model_generate_content(
+            project_id=self.project_id,
+            location=self.location,
+            contents=self.contents,
+            tools=self.tools,
+            generation_config=self.generation_config,
+            safety_settings=self.safety_settings,
+            pretrained_model=self.pretrained_model,
+        )
+
+        self.log.info("Model response: %s", response)
+        self.xcom_push(context, key="model_response", value=response)
 
         return response
