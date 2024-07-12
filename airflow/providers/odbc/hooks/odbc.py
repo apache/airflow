93ef22b7e55e4e7d4f71d5a3689543d4ececfe21
David Blain <david.blain@infrabel.be>
2024-07-12 19:37:27 +0200

    refactor: Fixed static check

--- airflow/providers/odbc/hooks/odbc.py
+++ airflow/providers/odbc/hooks/odbc.py
@@ -150,9 +150,7 @@ class OdbcHook(DbApiHook):
                 conn_str += f"PORT={self.connection.port};"
 
             extra_exclude = {"driver", "dsn", "connect_kwargs", "sqlalchemy_scheme", "placeholder"}
-            extra_params = {
-                k: v for k, v in self.connection_extra.items() if k.lower() not in extra_exclude
-            }
+            extra_params = {k: v for k, v in self.connection_extra.items() if k.lower() not in extra_exclude}
             for k, v in extra_params.items():
                 conn_str += f"{k}={v};"
 
