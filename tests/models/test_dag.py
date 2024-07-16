598ac237b90f887c086efa85d3f2b89ef96976cb
Wei Lee <weilee.rx@gmail.com>
2024-07-16 15:06:02 +0800

    test(dag): add test case test_dags_needing_dagruns_dataset_aliases

--- tests/models/test_dag.py
+++ tests/models/test_dag.py
@@ -3024,6 +3024,54 @@ class TestDagModel:
         dag_models = query.all()
         assert dag_models == [dag_model]
 
+    def test_dags_needing_dagruns_dataset_aliases(self, dag_maker, session):
+        # link dataset_alias hello_alias to dataset hello
+        dataset_model = DatasetModel(uri="hello")
+        dataset_alias_model = DatasetAliasModel(name="hello_alias")
+        dataset_alias_model.datasets.append(dataset_model)
+        session.add_all([dataset_model, dataset_alias_model])
+        session.commit()
+
+        with dag_maker(
+            session=session,
+            dag_id="my_dag",
+            max_active_runs=1,
+            schedule=[DatasetAlias(name="hello_alias")],
+            start_date=pendulum.now().add(days=-2),
+        ) as dag:
+            EmptyOperator(task_id="dummy")
+
+        # there's no queue record yet, so no runs needed at this time.
+        query, _ = DagModel.dags_needing_dagruns(session)
+        dag_models = query.all()
+        assert dag_models == []
+
+        # add queue records so we'll need a run
+        dag_model = session.query(DagModel).filter(DagModel.dag_id == dag.dag_id).one()
+        dataset_model: DatasetModel = dag_model.schedule_datasets[0]
+        session.add(DatasetDagRunQueue(dataset_id=dataset_model.id, target_dag_id=dag_model.dag_id))
+        session.flush()
+        query, _ = DagModel.dags_needing_dagruns(session)
+        dag_models = query.all()
+        assert dag_models == [dag_model]
+
+        # create run so we don't need a run anymore (due to max active runs)
+        dag_maker.create_dagrun(
+            run_type=DagRunType.DATASET_TRIGGERED,
+            state=DagRunState.QUEUED,
+            execution_date=pendulum.now("UTC"),
+        )
+        query, _ = DagModel.dags_needing_dagruns(session)
+        dag_models = query.all()
+        assert dag_models == []
+
+        # increase max active runs and we should now need another run
+        dag_maker.dag_model.max_active_runs = 2
+        session.flush()
+        query, _ = DagModel.dags_needing_dagruns(session)
+        dag_models = query.all()
+        assert dag_models == [dag_model]
+
     def test_max_active_runs_not_none(self):
         dag = DAG(dag_id="test_max_active_runs_not_none", start_date=timezone.datetime(2038, 1, 1))
         EmptyOperator(task_id="dummy", dag=dag, owner="airflow")
