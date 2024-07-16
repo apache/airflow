37d0167b2b8a5c188b9baa5197db3d23ac20b875
Shoaib Akhtar <starvoltz173@gmail.com>
2024-07-17 00:10:35 +0530

    modified is_venv_installed

--- airflow/operators/python.py
+++ airflow/operators/python.py
@@ -74,7 +74,7 @@ def is_venv_installed() -> bool:
     try:
         subprocess.check_output(["python", "-c", "import virtualenv"], stderr=subprocess.STDOUT)
         return True
-    except subprocess.CalledProcessError:
+    except (subprocess.CalledProcessError, FileNotFoundError):
         return False
 
 
