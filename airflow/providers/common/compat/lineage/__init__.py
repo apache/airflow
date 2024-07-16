63662044583031fc27d98af02f2913d324245db0
Maciej Obuchowski <obuchowski.maciej@gmail.com>
2024-07-17 01:22:16 +0200

    openlineage: add method to common.compat to not force hooks to try/except every 2.10 hook lineage call (#40812)
    Signed-off-by: Maciej Obuchowski <obuchowski.maciej@gmail.com>

--- airflow/providers/common/compat/lineage/__init__.py
+++ airflow/providers/common/compat/lineage/__init__.py
@@ -14,8 +14,3 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-from __future__ import annotations
-
-
-def test_empty():
-    assert True
