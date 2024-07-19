2b567c10c298e7d261b17b11192177cb87d05091
Wei Lee <weilee.rx@gmail.com>
2024-07-19 18:27:24 +0800

    refactor(www): refactor dataset_dependencies logic

--- airflow/www/views.py
+++ airflow/www/views.py
@@ -3504,21 +3504,26 @@ class Airflow(AirflowBaseView):
             if dag_node_id not in nodes_dict:
                 for dep in dependencies:
                     if dep.dependency_type in ("dag", "dataset", "dataset-alias"):
+                        # add node
                         nodes_dict[dag_node_id] = node_dict(dag_node_id, dag, "dag")
                         if dep.node_id not in nodes_dict:
                             nodes_dict[dep.node_id] = node_dict(
                                 dep.node_id, dep.dependency_id, dep.dependency_type
                             )
+
+                        # add edge
+
+                        # not start dep
                         if dep.source != dep.dependency_type:
-                            if ":" not in dep.source:
-                                edge_tuples.add((f"dag:{dep.source}", dep.node_id))
-                            else:
-                                edge_tuples.add((dep.source, dep.node_id))
-                        if dep.target != dep.dependency_type and ":" not in dep.target:
-                            if ":" not in dep.target:
-                                edge_tuples.add((dep.node_id, f"dag:{dep.target}"))
-                            else:
-                                edge_tuples.add((dep.node_id, dep.target))
+                            source = dep.source if ":" in dep.source else f"dag:{dep.source}"
+                            target = dep.node_id
+                            edge_tuples.add((source, target))
+
+                        # not end dep
+                        if dep.target != dep.dependency_type:
+                            source = dep.node_id
+                            target = dep.target if ":" in dep.target else f"dag:{dep.target}"
+                            edge_tuples.add((source, target))
 
         nodes = list(nodes_dict.values())
         edges = [{"source": source, "target": target} for source, target in edge_tuples]
