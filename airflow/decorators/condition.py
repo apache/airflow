682cbdda725cedf2c9d181dde61bb36d12f96caf
phi <phi.friday@gmail.com>
2024-07-30 20:54:47 +0900

    fix

--- airflow/decorators/condition.py
+++ airflow/decorators/condition.py
@@ -1,3 +1,19 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
 from __future__ import annotations
 
 from functools import wraps
@@ -31,7 +47,7 @@ def run_if(condition: Callable[[Context], bool]) -> Callable[[_T], _T]:
         pre_execute: TaskPreExecuteHook | None = task.kwargs.get("pre_execute")
         new_pre_execute = combine_hooks(pre_execute, wrapped_condition)
         task.kwargs["pre_execute"] = new_pre_execute
-        return task
+        return task  # type: ignore[return-value]
 
     return decorator
 
@@ -52,7 +68,7 @@ def skip_if(condition: Callable[[Context], bool]) -> Callable[[_T], _T]:
         pre_execute: TaskPreExecuteHook | None = task.kwargs.get("pre_execute")
         new_pre_execute = combine_hooks(pre_execute, wrapped_condition)
         task.kwargs["pre_execute"] = new_pre_execute
-        return task
+        return task  # type: ignore[return-value]
 
     return decorator
 
