87d37e15b423c4475321afb94fe61d6950ab5550
David Blain <david.blain@infrabel.be>
2024-08-08 15:03:24 +0200

    refactored: Moved handlers to dedicated module to avoid circular dependencies issues

--- airflow/providers/common/sql/hooks/sql.py
+++ airflow/providers/common/sql/hooks/sql.py
@@ -52,6 +52,11 @@ from airflow.exceptions import (
 from airflow.hooks.base import BaseHook
 from airflow.providers.common.sql.dialects.dialect import Dialect
 from airflow.providers.common.sql.dialects.mssql import MsSqlDialect
+from airflow.providers.common.sql.hooks.handlers import (
+    fetch_all_handler,
+    fetch_one_handler,
+    return_single_query_results,
+)
 
 if TYPE_CHECKING:
     from pandas import DataFrame
@@ -63,60 +68,7 @@ if TYPE_CHECKING:
 
 T = TypeVar("T")
 SQL_PLACEHOLDERS = frozenset({"%s", "?"})
-
-
-def return_single_query_results(sql: str | Iterable[str], return_last: bool, split_statements: bool):
-    """
-    Determine when results of single query only should be returned.
-
-    For compatibility reasons, the behaviour of the DBAPIHook is somewhat confusing.
-    In some cases, when multiple queries are run, the return value will be an iterable (list) of results
-    -- one for each query. However, in other cases, when single query is run, the return value will be just
-    the result of that single query without wrapping the results in a list.
-
-    The cases when single query results are returned without wrapping them in a list are as follows:
-
-    a) sql is string and ``return_last`` is True (regardless what ``split_statements`` value is)
-    b) sql is string and ``split_statements`` is False
-
-    In all other cases, the results are wrapped in a list, even if there is only one statement to process.
-    In particular, the return value will be a list of query results in the following circumstances:
-
-    a) when ``sql`` is an iterable of string statements (regardless what ``return_last`` value is)
-    b) when ``sql`` is string, ``split_statements`` is True and ``return_last`` is False
-
-    :param sql: sql to run (either string or list of strings)
-    :param return_last: whether last statement output should only be returned
-    :param split_statements: whether to split string statements.
-    :return: True if the hook should return single query results
-    """
-    return isinstance(sql, str) and (return_last or not split_statements)
-
-
-def fetch_all_handler(cursor) -> list[tuple] | None:
-    """Return results for DbApiHook.run()."""
-    if not hasattr(cursor, "description"):
-        raise RuntimeError(
-            "The database we interact with does not support DBAPI 2.0. Use operator and "
-            "handlers that are specifically designed for your database."
-        )
-    if cursor.description is not None:
-        return cursor.fetchall()
-    else:
-        return None
-
-
-def fetch_one_handler(cursor) -> list[tuple] | None:
-    """Return first result for DbApiHook.run()."""
-    if not hasattr(cursor, "description"):
-        raise RuntimeError(
-            "The database we interact with does not support DBAPI 2.0. Use operator and "
-            "handlers that are specifically designed for your database."
-        )
-    if cursor.description is not None:
-        return cursor.fetchone()
-    else:
-        return None
+__all__ = ["fetch_all_handler", "fetch_one_handler", "return_single_query_results", ...]
 
 
 class ConnectorProtocol(Protocol):
