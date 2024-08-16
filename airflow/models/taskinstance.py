fbe8318dd9fdb669da96ff83e4928f4e13a92d79
romsharon98 <rom.sharon98@gmail.com>
2024-08-16 17:04:35 +0300

    convert list to set for optimization

--- airflow/models/taskinstance.py
+++ airflow/models/taskinstance.py
@@ -4030,13 +4030,11 @@ class TaskInstance(Base, LoggingMixin):
                 )
             )
 
-    def get_upstream_task_ids_by_state(self, state: list[TaskInstanceState], session: Session) -> list[str]:
+    def get_upstream_task_ids_by_state(self, state: list[TaskInstanceState], session: Session) -> set[str]:
         """Get direct upstream tasks ids by state."""
         task_instances = self.dag_run.get_task_instances(state=state, session=session)
-        filtered_task_ids = [
-            task.task_id for task in task_instances if task.task_id in self.task.upstream_task_ids
-        ]
-        return filtered_task_ids
+        dag_task_instances = set(task_instance.task_id for task_instance in task_instances)
+        return dag_task_instances & self.task.upstream_task_ids
 
 
 def _find_common_ancestor_mapped_group(node1: Operator, node2: Operator) -> MappedTaskGroup | None:
