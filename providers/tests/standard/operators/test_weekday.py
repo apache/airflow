00c769ec1b45876d1161199bfe3b55bd25881c34
romsharon98 <rom.sharon98@gmail.com>
2024-08-19 23:55:22 +0300

    change core to essentials

--- providers/tests/standard/operators/test_weekday.py
+++ providers/tests/standard/operators/test_weekday.py
@@ -28,10 +28,14 @@ from airflow.models.taskinstance import TaskInstance as TI
 from airflow.models.xcom import XCom
 from airflow.operators.empty import EmptyOperator
 <<<<<<<< HEAD:tests/providers/standard/operators/test_weekday.py
+<<<<<<<< HEAD:tests/providers/standard/operators/test_weekday.py
 from airflow.providers.standard.operators.weekday import BranchDayOfWeekOperator
 ========
 from airflow.providers.core.time.operators.weekday import BranchDayOfWeekOperator
 >>>>>>>> 6f86e128e4 (add core.time provider):tests/providers/core/time/operators/test_weekday.py
+========
+from airflow.providers.essentials.time.operators.weekday import BranchDayOfWeekOperator
+>>>>>>>> 149f5ecb0d (change core to essentials):tests/providers/essentials/time/operators/test_weekday.py
 from airflow.utils import timezone
 from airflow.utils.session import create_session
 from airflow.utils.state import State
