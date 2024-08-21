250c59ed119aa49e4e2fee4a3450eba5c3ad6c04
Ephraim Anierobi <splendidzigy24@gmail.com>
2024-08-21 16:37:35 +0100

    Fix backcompat for fab provider

--- tests/providers/fab/auth_manager/models/test_db.py
+++ tests/providers/fab/auth_manager/models/test_db.py
@@ -24,7 +24,6 @@ from alembic.migration import MigrationContext
 from sqlalchemy import MetaData
 
 import airflow
-from airflow.providers.fab.auth_manager.models.db import FABDBManager
 from airflow.settings import engine
 from airflow.utils.db import (
     compare_server_default,
@@ -32,43 +31,46 @@ from airflow.utils.db import (
 )
 
 pytestmark = [pytest.mark.db_test]
+try:
+    from airflow.providers.fab.auth_manager.models.db import FABDBManager
 
+    class TestFABDBManager:
+        def setup_method(self, session):
+            self.airflow_dir = os.path.dirname(airflow.__file__)
+            self.db_manager = FABDBManager(session=session)
 
-class TestFABDBManager:
-    def setup_method(self, session):
-        self.airflow_dir = os.path.dirname(airflow.__file__)
-        self.db_manager = FABDBManager(session=session)
+        def test_version_table_name_set(self):
+            assert self.db_manager.version_table_name == "fab_alembic_version"
 
-    def test_version_table_name_set(self):
-        assert self.db_manager.version_table_name == "fab_alembic_version"
+        def test_migration_dir_set(self):
+            assert self.db_manager.migration_dir == f"{self.airflow_dir}/providers/fab/migrations"
 
-    def test_migration_dir_set(self):
-        assert self.db_manager.migration_dir == f"{self.airflow_dir}/providers/fab/migrations"
+        def test_alembic_file_set(self):
+            assert self.db_manager.alembic_file == f"{self.airflow_dir}/providers/fab/alembic.ini"
 
-    def test_alembic_file_set(self):
-        assert self.db_manager.alembic_file == f"{self.airflow_dir}/providers/fab/alembic.ini"
+        def test_supports_table_dropping_set(self):
+            assert self.db_manager.supports_table_dropping is True
 
-    def test_supports_table_dropping_set(self):
-        assert self.db_manager.supports_table_dropping is True
+        def test_database_schema_and_sqlalchemy_model_are_in_sync(self):
+            def include_object(_, name, type_, *args):
+                if type_ == "table" and name not in self.db_manager.metadata.tables:
+                    return False
+                return True
 
-    def test_database_schema_and_sqlalchemy_model_are_in_sync(self):
-        def include_object(_, name, type_, *args):
-            if type_ == "table" and name not in self.db_manager.metadata.tables:
-                return False
-            return True
+            all_meta_data = MetaData()
+            for table_name, table in self.db_manager.metadata.tables.items():
+                all_meta_data._add_table(table_name, table.schema, table)
+            # create diff between database schema and SQLAlchemy model
+            mctx = MigrationContext.configure(
+                engine.connect(),
+                opts={
+                    "compare_type": compare_type,
+                    "compare_server_default": compare_server_default,
+                    "include_object": include_object,
+                },
+            )
+            diff = compare_metadata(mctx, all_meta_data)
 
-        all_meta_data = MetaData()
-        for table_name, table in self.db_manager.metadata.tables.items():
-            all_meta_data._add_table(table_name, table.schema, table)
-        # create diff between database schema and SQLAlchemy model
-        mctx = MigrationContext.configure(
-            engine.connect(),
-            opts={
-                "compare_type": compare_type,
-                "compare_server_default": compare_server_default,
-                "include_object": include_object,
-            },
-        )
-        diff = compare_metadata(mctx, all_meta_data)
-
-        assert not diff, "Database schema and SQLAlchemy model are not in sync: " + str(diff)
+            assert not diff, "Database schema and SQLAlchemy model are not in sync: " + str(diff)
+except ModuleNotFoundError:
+    pass
