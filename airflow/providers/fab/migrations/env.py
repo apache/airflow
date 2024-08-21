2dcf266b601557604747ea7d651a66b1b784a17c
Ephraim Anierobi <splendidzigy24@gmail.com>
2024-08-21 08:02:44 +0100

    Don't disable existing loggers

--- airflow/providers/fab/migrations/env.py
+++ airflow/providers/fab/migrations/env.py
@@ -33,7 +33,7 @@ version_table = FABDBManager.version_table_name
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+    fileConfig(config.config_file_name, disable_existing_loggers=False)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
