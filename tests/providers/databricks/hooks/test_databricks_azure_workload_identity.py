29de4a27735b699c2a84fb9ab62b3f05a62e5ad1
Bas van Driel <bas.van.driel@nl.abnamro.com>
2024-08-21 11:14:51 +0200

    add kubernetes check

--- tests/providers/databricks/hooks/test_databricks_azure_workload_identity.py
+++ tests/providers/databricks/hooks/test_databricks_azure_workload_identity.py
@@ -8,6 +8,7 @@ import pytest
 import tenacity
 from azure.core.credentials import AccessToken
 
+from airflow.exceptions import AirflowException
 from airflow.models import Connection
 from airflow.providers.databricks.hooks.databricks import DatabricksHook
 from airflow.providers.databricks.hooks.databricks_base import WORKLOAD_IDENTITY_SETTING_KEY
@@ -20,8 +21,9 @@ def create_successful_response_mock(content):
     response.status_code = 200
     return response
 
+
 def create_aad_token_for_resource() -> AccessToken:
-    return AccessToken(expires_on=1575500666, token='sample-token')
+    return AccessToken(expires_on=1575500666, token="sample-token")
 
 
 HOST = "xx.cloud.databricks.com"
@@ -31,6 +33,7 @@ DEFAULT_RETRY_ARGS = dict(
     wait=tenacity.wait_none(),
     stop=tenacity.stop_after_attempt(DEFAULT_RETRY_NUMBER),
 )
+
 
 @pytest.mark.db_test
 class TestDatabricksHookAadTokenManagedIdentity:
@@ -54,8 +57,25 @@ class TestDatabricksHookAadTokenManagedIdentity:
         # This will use the default connection id (databricks_default)
         self._hook = DatabricksHook(retry_args=DEFAULT_RETRY_ARGS)
 
-    @mock.patch.dict(os.environ, {"AZURE_CLIENT_ID": "fake-client-id", "AZURE_TENANT_ID": "fake-tenant-id", "AZURE_FEDERATED_TOKEN_FILE": '/badpath'})
-    @mock.patch('azure.identity.WorkloadIdentityCredential.get_token', return_value=create_aad_token_for_resource())
+
+    def test_not_running_in_kubernetes(self):
+        with pytest.raises(AirflowException) as e:
+            self._hook.list_jobs()
+
+        assert str(e.value) == "Workload identity authentication is only supporting when running in an Kubernetes cluster"
+
+    @mock.patch.dict(
+        os.environ,
+        {
+            "AZURE_CLIENT_ID": "fake-client-id",
+            "AZURE_TENANT_ID": "fake-tenant-id",
+            "AZURE_FEDERATED_TOKEN_FILE": "/badpath",
+            "KUBERNETES_SERVICE_HOST": "fakeip"
+        },
+    )
+    @mock.patch(
+        "azure.identity.WorkloadIdentityCredential.get_token", return_value=create_aad_token_for_resource()
+    )
     @mock.patch("airflow.providers.databricks.hooks.databricks_base.requests.get")
     def test_one(self, requests_mock, get_token_mock: mock.MagicMock):
         # get_token_mock.return_value = create_aad_token_for_resource()
