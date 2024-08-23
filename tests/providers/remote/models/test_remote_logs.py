3d0caed88c52afdc59456ffec224ff80b622fbfb
Jens Scheffler <jens.scheffler@de.bosch.com>
2024-08-24 00:07:47 +0200

    Second batch of pytests

--- tests/providers/remote/models/test_remote_logs.py
+++ tests/providers/remote/models/test_remote_logs.py
@@ -14,3 +14,30 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+from __future__ import annotations
+
+from airflow.providers.remote.models.remote_logs import RemoteLogs, RemoteLogsModel
+from airflow.utils import timezone
+
+
+def test_serializing_pydantic_remote_logs():
+    rlm = RemoteLogsModel(
+        dag_id="test_dag",
+        task_id="test_task",
+        run_id="test_run",
+        map_index=-1,
+        try_number=1,
+        log_chunk_time=timezone.utcnow(),
+        log_chunk_data="some logs captured",
+    )
+
+    pydantic_logs = RemoteLogs.model_validate(rlm)
+
+    json_string = pydantic_logs.model_dump_json()
+    print(json_string)
+
+    deserialized_model = RemoteLogs.model_validate_json(json_string)
+    assert deserialized_model.dag_id == rlm.dag_id
+    assert deserialized_model.try_number == rlm.try_number
+    assert deserialized_model.log_chunk_time == rlm.log_chunk_time
+    assert deserialized_model.log_chunk_data == rlm.log_chunk_data
