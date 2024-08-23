ca0275d20b922bc01e20134e11324f78b41158c7
Jens Scheffler <jens.scheffler@de.bosch.com>
2024-08-24 00:07:47 +0200

    Second batch of pytests

--- tests/providers/remote/models/test_remote_job.py
+++ tests/providers/remote/models/test_remote_job.py
@@ -14,3 +14,83 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import pytest
+
+from airflow.providers.remote.models.remote_job import RemoteJob, RemoteJobModel
+from airflow.utils import timezone
+from airflow.utils.state import TaskInstanceState
+
+if TYPE_CHECKING:
+    from sqlalchemy.orm import Session
+
+
+class TestRemoteJob:
+    @pytest.fixture(autouse=True)
+    def setup_test_cases(self, session: Session):
+        session.query(RemoteJobModel).delete()
+
+    def test_reserve_task_no_job(self):
+        job = RemoteJob.reserve_task("worker")
+        assert job is None
+
+    def test_reserve_task_has_one(self, session: Session):
+        rjm = RemoteJobModel(
+            dag_id="test_dag",
+            task_id="test_task",
+            run_id="test_run",
+            map_index=-1,
+            try_number=1,
+            state=TaskInstanceState.QUEUED,
+            queue="default",
+            command=str(["hello", "world"]),
+            queued_dttm=timezone.utcnow(),
+        )
+        session.add(rjm)
+        session.commit()
+
+        job = RemoteJob.reserve_task("worker")
+        assert job
+        assert job.remote_worker == "worker"
+        assert job.queue == "default"
+        assert job.dag_id == "test_dag"
+        assert job.task_id == "test_task"
+        assert job.run_id == "test_run"
+
+        jobs: list[RemoteJobModel] = session.query(RemoteJobModel).all()
+        assert len(jobs) == 1
+        assert jobs[0].state == TaskInstanceState.RUNNING
+        assert jobs[0].remote_worker == "worker"
+        assert jobs[0].queue == "default"
+        assert jobs[0].dag_id == "test_dag"
+        assert jobs[0].task_id == "test_task"
+        assert jobs[0].run_id == "test_run"
+
+    def test_set_state(self, session: Session):
+        rjm = RemoteJobModel(
+            dag_id="test_dag",
+            task_id="test_task",
+            run_id="test_run",
+            map_index=-1,
+            try_number=1,
+            state=TaskInstanceState.RUNNING,
+            queue="default",
+            command=str(["hello", "world"]),
+            queued_dttm=timezone.utcnow(),
+        )
+        session.add(rjm)
+        session.commit()
+
+        RemoteJob.set_state(rjm.key, TaskInstanceState.FAILED)
+
+        jobs: list[RemoteJobModel] = session.query(RemoteJobModel).all()
+        assert len(jobs) == 1
+        assert jobs[0].state == TaskInstanceState.FAILED
+        assert jobs[0].last_update
+        assert jobs[0].queue == "default"
+        assert jobs[0].dag_id == "test_dag"
+        assert jobs[0].task_id == "test_task"
+        assert jobs[0].run_id == "test_run"
--- tests/providers/remote/plugins/test_remote_executor_plugin.py
+++ tests/providers/remote/plugins/test_remote_executor_plugin.py
@@ -14,3 +14,53 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+from __future__ import annotations
+
+import importlib
+
+import pytest
+
+from airflow.plugins_manager import AirflowPlugin
+from airflow.providers.remote.plugins import remote_executor_plugin
+from tests.test_utils.config import conf_vars
+
+
+def test_plugin_inactive():
+    with conf_vars({("remote", "api_enabled"): "false"}):
+        importlib.reload(remote_executor_plugin)
+
+        from airflow.providers.remote.plugins.remote_executor_plugin import (
+            REMOTE_EXECUTOR_ACTIVE,
+            RemoteExecutorPlugin,
+        )
+
+        rep = RemoteExecutorPlugin()
+        assert not REMOTE_EXECUTOR_ACTIVE
+        assert len(rep.flask_blueprints) == 0
+        assert len(rep.appbuilder_views) == 0
+
+
+def test_plugin_active():
+    with conf_vars({("remote", "api_enabled"): "true"}):
+        importlib.reload(remote_executor_plugin)
+
+        from airflow.providers.remote.plugins.remote_executor_plugin import (
+            REMOTE_EXECUTOR_ACTIVE,
+            RemoteExecutorPlugin,
+        )
+
+        rep = RemoteExecutorPlugin()
+        assert REMOTE_EXECUTOR_ACTIVE
+        assert len(rep.flask_blueprints) == 2
+        assert len(rep.appbuilder_views) == 2
+
+
+@pytest.fixture
+def plugin():
+    from airflow.providers.remote.plugins.remote_executor_plugin import RemoteExecutorPlugin
+
+    return RemoteExecutorPlugin()
+
+
+def test_plugin_is_airflow_plugin(plugin):
+    assert isinstance(plugin, AirflowPlugin)
