bfce97964839b54c41193ee4e5da1a5054d80056
Jens Scheffler <jens.scheffler@de.bosch.com>
2024-08-25 17:08:23 +0200

    Breeze adjustments for introduction of AIP-69 Remote Executor

--- dev/breeze/src/airflow_breeze/params/shell_params.py
+++ dev/breeze/src/airflow_breeze/params/shell_params.py
@@ -35,6 +35,7 @@ from airflow_breeze.global_constants import (
     ALLOWED_PYTHON_MAJOR_MINOR_VERSIONS,
     APACHE_AIRFLOW_GITHUB_REPOSITORY,
     CELERY_BROKER_URLS_MAP,
+    CELERY_EXECUTOR,
     DEFAULT_CELERY_BROKER,
     DEFAULT_UV_HTTP_TIMEOUT,
     DOCKER_DEFAULT_PLATFORM,
@@ -50,6 +51,7 @@ from airflow_breeze.global_constants import (
     MYSQL_HOST_PORT,
     POSTGRES_HOST_PORT,
     REDIS_HOST_PORT,
+    REMOTE_EXECUTOR,
     SSH_PORT,
     START_AIRFLOW_DEFAULT_ALLOWED_EXECUTOR,
     TESTABLE_INTEGRATIONS,
@@ -324,7 +326,7 @@ class ShellParams:
             for backend in ALLOWED_BACKENDS:
                 backend_files.extend(self.get_backend_compose_files(backend))
 
-        if self.executor == "CeleryExecutor":
+        if self.executor == CELERY_EXECUTOR:
             compose_file_list.append(DOCKER_COMPOSE_DIR / "integration-celery.yml")
             if self.use_airflow_version:
                 current_extras = self.airflow_extras
@@ -492,6 +494,9 @@ class ShellParams:
         _set_var(_env, "AIRFLOW_VERSION", self.airflow_version)
         _set_var(_env, "AIRFLOW__CELERY__BROKER_URL", self.airflow_celery_broker_url)
         _set_var(_env, "AIRFLOW__CORE__EXECUTOR", self.executor)
+        if self.executor == REMOTE_EXECUTOR:
+            _set_var(_env, "AIRFLOW__REMOTE__API_ENABLED", "true")
+            _set_var(_env, "AIRFLOW__REMOTE__API_URL", "http://localhost:8080/remote_worker/v1/rpcapi")
         _set_var(_env, "ANSWER", get_forced_answer() or "")
         _set_var(_env, "BACKEND", self.backend)
         _set_var(_env, "BASE_BRANCH", self.base_branch, "main")
