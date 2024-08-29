45d559330e7628a8dd0d795c8fd4a9296185c003
Jens Scheffler <jens.scheffler@de.bosch.com>
2024-08-30 00:18:35 +0200

    Rename Remote Executor to Edge Executor

--- tests/providers/edge/executors/test_edge_executor.py
+++ tests/providers/edge/executors/test_edge_executor.py
@@ -21,8 +21,8 @@ from unittest.mock import patch
 import pytest
 
 from airflow.models.taskinstancekey import TaskInstanceKey
-from airflow.providers.remote.executors.remote_executor import RemoteExecutor
-from airflow.providers.remote.models.remote_job import RemoteJobModel
+from airflow.providers.edge.executors.edge_executor import EdgeExecutor
+from airflow.providers.edge.models.edge_job import EdgeJobModel
 from airflow.utils import timezone
 from airflow.utils.session import create_session
 from airflow.utils.state import TaskInstanceState
@@ -30,14 +30,14 @@ from airflow.utils.state import TaskInstanceState
 pytestmark = pytest.mark.db_test
 
 
-class TestRemoteExecutor:
+class TestEdgeExecutor:
     @pytest.fixture(autouse=True)
     def setup_test_cases(self):
         with create_session() as session:
-            session.query(RemoteJobModel).delete()
+            session.query(EdgeJobModel).delete()
 
     def test_execute_async_bad_command(self):
-        executor = RemoteExecutor()
+        executor = EdgeExecutor()
         with pytest.raises(ValueError):
             executor.execute_async(
                 TaskInstanceKey(
@@ -47,7 +47,7 @@ class TestRemoteExecutor:
             )
 
     def test_execute_async_ok_command(self):
-        executor = RemoteExecutor()
+        executor = EdgeExecutor()
         executor.execute_async(
             TaskInstanceKey(
                 dag_id="test_dag", run_id="test_run", task_id="test_task", map_index=-1, try_number=1
@@ -55,17 +55,17 @@ class TestRemoteExecutor:
             command=["airflow", "tasks", "run", "hello", "world"],
         )
         with create_session() as session:
-            jobs: list[RemoteJobModel] = session.query(RemoteJobModel).all()
+            jobs: list[EdgeJobModel] = session.query(EdgeJobModel).all()
         assert len(jobs) == 1
         assert jobs[0].dag_id == "test_dag"
         assert jobs[0].run_id == "test_run"
         assert jobs[0].task_id == "test_task"
 
-    @patch("airflow.providers.remote.executors.remote_executor.RemoteExecutor.running_state")
-    @patch("airflow.providers.remote.executors.remote_executor.RemoteExecutor.success")
-    @patch("airflow.providers.remote.executors.remote_executor.RemoteExecutor.fail")
+    @patch("airflow.providers.edge.executors.edge_executor.EdgeExecutor.running_state")
+    @patch("airflow.providers.edge.executors.edge_executor.EdgeExecutor.success")
+    @patch("airflow.providers.edge.executors.edge_executor.EdgeExecutor.fail")
     def test_sync(self, mock_fail, mock_success, mock_running_state):
-        executor = RemoteExecutor()
+        executor = EdgeExecutor()
 
         def remove_from_running(key: TaskInstanceKey):
             executor.running.remove(key)
@@ -81,7 +81,7 @@ class TestRemoteExecutor:
                 ("started_failed", TaskInstanceState.FAILED),
             ]:
                 session.add(
-                    RemoteJobModel(
+                    EdgeJobModel(
                         dag_id="test_dag",
                         task_id=task_id,
                         run_id="test_run",
@@ -116,7 +116,7 @@ class TestRemoteExecutor:
             task_id = "started_running2"
             state = TaskInstanceState.RUNNING
             session.add(
-                RemoteJobModel(
+                EdgeJobModel(
                     dag_id="test_dag",
                     task_id=task_id,
                     run_id="test_run",
