4c3710da30da092c26c93022bc097a2f77e14fe4
Jens Scheffler <jens.scheffler@de.bosch.com>
2024-08-30 00:18:35 +0200

    Rename Remote Executor to Edge Executor

--- airflow/providers/edge/executors/__init__.py
+++ airflow/providers/edge/executors/__init__.py
@@ -14,3 +14,9 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+
+from __future__ import annotations
+
+from airflow.providers.edge.executors.edge_executor import EdgeExecutor
+
+__all__ = ["EdgeExecutor"]
