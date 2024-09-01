d32f89db51d21f649d2774e2c58a87e3c3b33cb0
Ephraim Anierobi <splendidzigy24@gmail.com>
2024-09-01 08:12:28 +0100

    set airflow_db as false

--- airflow/providers/fab/auth_manager/cli_commands/db_command.py
+++ airflow/providers/fab/auth_manager/cli_commands/db_command.py
@@ -38,7 +38,7 @@ def migratedb(args):
     """Migrates the metadata database."""
     session = settings.Session()
     upgrade_command = FABDBManager(session).upgradedb
-    run_db_migrate_command(args, upgrade_command, revision_heads_map=_REVISION_HEADS_MAP)
+    run_db_migrate_command(args, upgrade_command, revision_heads_map=_REVISION_HEADS_MAP, airflow_db=False)
 
 
 @cli_utils.action_cli(check_db=False)
