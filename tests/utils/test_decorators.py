f21d8d602ec448d642ebd1dc0b2465d137127ab0
phi <phi.friday@gmail.com>
2024-09-04 20:42:38 +0900

    fix: use fixture

--- tests/utils/test_decorators.py
+++ tests/utils/test_decorators.py
@@ -16,7 +16,6 @@
 # under the License.
 from __future__ import annotations
 
-import ast
 from typing import TYPE_CHECKING, Any
 
 import pytest
@@ -24,133 +23,107 @@ import pytest
 from airflow.decorators import task
 
 if TYPE_CHECKING:
-    from airflow.decorators.base import Task
+    from airflow.decorators.base import Task, TaskDecorator
 DECORATORS = sorted(set(x for x in dir(task) if not x.startswith("_")) - {"skip_if", "run_if"})
+DECORATORS_USING_SOURCE = ("external_python", "virtualenv", "branch_virtualenv", "branch_external_python")
 
 
-class TestDecoratorSource:
-    @staticmethod
-    def update_custom_operator_name(operator: Any, custom_operator_name: str):
-        custom_operator_name = (
-            custom_operator_name if custom_operator_name.startswith("@") else f"@{custom_operator_name}"
-        )
-        operator.__dict__["custom_operator_name"] = custom_operator_name
+@pytest.fixture
+def decorator(request: pytest.FixtureRequest) -> TaskDecorator:
+    decorator_factory = getattr(task, request.param)
 
-    @staticmethod
-    def init_decorator(decorator_name: str):
-        decorator_factory = getattr(task, decorator_name)
+    kwargs = {}
+    if "external" in request.param:
+        kwargs["python"] = "python3"
+    return decorator_factory(**kwargs)
 
-        kwargs = {}
-        if "external" in decorator_name:
-            kwargs["python"] = "python3"
-        return decorator_factory(**kwargs)
 
-    @classmethod
-    def parse_python_source(cls, task: Task, custom_operator_name: str | None = None) -> str:
-        operator = task().operator
-        if not hasattr(operator, "get_python_source"):
-            pytest.skip(f"Operator {operator} does not have get_python_source method")
-        if custom_operator_name:
-            cls.update_custom_operator_name(operator, custom_operator_name)
-        return operator.get_python_source()
+@pytest.mark.parametrize("decorator", DECORATORS_USING_SOURCE, indirect=["decorator"])
+def test_task_decorator_using_source(decorator: TaskDecorator):
+    @decorator
+    def f():
+        return ["some_task"]
 
-    @classmethod
-    def parse_decorator_names(cls, source: Task | str) -> list[str]:
-        if not isinstance(source, str):
-            source = cls.parse_python_source(source)
-        node = ast.parse(source)
-        func: ast.FunctionDef = node.body[0]  # type: ignore[assignment]
-        decorators: list[ast.Name] = func.decorator_list  # type: ignore[assignment]
-        return [decorator.id for decorator in decorators]
+    assert parse_python_source(f, "decorator") == 'def f():\n    return ["some_task"]\n'
 
-    def test_branch_external_python(self):
-        @task.branch_virtualenv()
-        def f():
-            return ["some_task"]
 
-        assert not self.parse_decorator_names(f)
-        assert self.parse_python_source(f) == 'def f():\n    return ["some_task"]\n'
+@pytest.mark.parametrize("decorator", DECORATORS, indirect=["decorator"])
+def test_skip_if(decorator: TaskDecorator):
+    @task.skip_if(lambda context: True)
+    @decorator
+    def f():
+        return "hello world"
 
-    def test_branch_virtualenv(self):
-        @task.external_python(python="python3")
-        def f():
-            return "hello world"
+    assert parse_python_source(f, "decorator") == 'def f():\n    return "hello world"\n'
 
-        assert not self.parse_decorator_names(f)
-        assert self.parse_python_source(f) == 'def f():\n    return "hello world"\n'
 
-    def test_virtualenv(self):
-        @task.virtualenv()
-        def f():
-            return "hello world"
+@pytest.mark.parametrize("decorator", DECORATORS, indirect=["decorator"])
+def test_run_if(decorator: TaskDecorator):
+    @task.run_if(lambda context: True)
+    @decorator
+    def f():
+        return "hello world"
 
-        assert not self.parse_decorator_names(f)
-        assert self.parse_python_source(f) == 'def f():\n    return "hello world"\n'
+    assert parse_python_source(f, "decorator") == 'def f():\n    return "hello world"\n'
 
-    @pytest.mark.parametrize("decorator_name", DECORATORS)
-    def test_skip_if(self, decorator_name):
-        decorator = self.init_decorator(decorator_name)
 
-        @task.skip_if(lambda context: True)
-        @decorator
-        def f():
-            return "hello world"
+def test_skip_if_and_run_if():
+    @task.skip_if(lambda context: True)
+    @task.run_if(lambda context: True)
+    @task.virtualenv()
+    def f():
+        return "hello world"
 
-        source = self.parse_python_source(f, "decorator")
-        assert source == 'def f():\n    return "hello world"\n'
+    assert parse_python_source(f) == 'def f():\n    return "hello world"\n'
 
-    @pytest.mark.parametrize("decorator_name", DECORATORS)
-    def test_run_if(self, decorator_name):
-        decorator = self.init_decorator(decorator_name)
 
-        @task.run_if(lambda context: True)
-        @decorator
-        def f():
-            return "hello world"
+def test_run_if_and_skip_if():
+    @task.run_if(lambda context: True)
+    @task.skip_if(lambda context: True)
+    @task.virtualenv()
+    def f():
+        return "hello world"
 
-        source = self.parse_python_source(f, "decorator")
-        assert source == 'def f():\n    return "hello world"\n'
+    assert parse_python_source(f) == 'def f():\n    return "hello world"\n'
 
-    def test_skip_if_and_run_if(self):
-        @task.skip_if(lambda context: True)
-        @task.run_if(lambda context: True)
-        @task.virtualenv()
-        def f():
-            return "hello world"
 
-        assert self.parse_python_source(f) == 'def f():\n    return "hello world"\n'
+def test_skip_if_allow_decorator():
+    def non_task_decorator(func):
+        return func
 
-    def test_run_if_and_skip_if(self):
-        @task.run_if(lambda context: True)
-        @task.skip_if(lambda context: True)
-        @task.virtualenv()
-        def f():
-            return "hello world"
+    @task.skip_if(lambda context: True)
+    @task.virtualenv()
+    @non_task_decorator
+    def f():
+        return "hello world"
 
-        assert self.parse_python_source(f) == 'def f():\n    return "hello world"\n'
+    assert parse_python_source(f) == '@non_task_decorator\ndef f():\n    return "hello world"\n'
 
-    def test_skip_if_allow_decorator(self):
-        def decorator(func):
-            return func
 
-        @task.skip_if(lambda context: True)
-        @task.virtualenv()
-        @decorator
-        def f():
-            return "hello world"
+def test_run_if_allow_decorator():
+    def non_task_decorator(func):
+        return func
 
-        assert self.parse_decorator_names(f) == ["decorator"]
-        assert self.parse_python_source(f) == '@decorator\ndef f():\n    return "hello world"\n'
+    @task.run_if(lambda context: True)
+    @task.virtualenv()
+    @non_task_decorator
+    def f():
+        return "hello world"
 
-    def test_run_if_allow_decorator(self):
-        def decorator(func):
-            return func
+    assert parse_python_source(f) == '@non_task_decorator\ndef f():\n    return "hello world"\n'
 
-        @task.run_if(lambda context: True)
-        @task.virtualenv()
-        @decorator
-        def f():
-            return "hello world"
 
-        assert self.parse_decorator_names(f) == ["decorator"]
-        assert self.parse_python_source(f) == '@decorator\ndef f():\n    return "hello world"\n'
+def parse_python_source(task: Task, custom_operator_name: str | None = None) -> str:
+    operator = task().operator
+    if not hasattr(operator, "get_python_source"):
+        pytest.skip(f"Operator {operator} does not have get_python_source method")
+    if custom_operator_name:
+        update_custom_operator_name(operator, custom_operator_name)
+    return operator.get_python_source()
+
+
+def update_custom_operator_name(operator: Any, custom_operator_name: str):
+    custom_operator_name = (
+        custom_operator_name if custom_operator_name.startswith("@") else f"@{custom_operator_name}"
+    )
+    operator.__dict__["custom_operator_name"] = custom_operator_name
