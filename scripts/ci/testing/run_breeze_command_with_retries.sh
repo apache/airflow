a0fd326d44ae2477c01c3155a8a8dfa460ce443d
Bugra Ozturk <ozturkbugra93@gmail.com>
2024-09-07 17:19:17 +0200

    Move script to testing/ directory

--- scripts/ci/testing/run_breeze_command_with_retries.sh
+++ scripts/ci/testing/run_breeze_command_with_retries.sh
@@ -18,19 +18,50 @@
 
 # This script should be only used commands without waiting interactive inputs.
 # For example: 'apt-get install [package_name]' will ask you to input 'y' while installing.
-# This means you cannot interactively take input while using this script.
+# This is an interactive input which you cannot provide.
+(ies)
+export COLOR_RED=$'\e[31m'
+export COLOR_YELLOW=$'\e[33m'
+export COLOR_RESET=$'\e[0m'
+
+if [ ! "$#" -eq 2 ]; then
+    echo "${COLOR_RED}You must provide exactly two argument!.${COLOR_RESET}"
+    exit 1
+fi
+
+# Param 1: Breeze Command
+# Param 2: Number of Retry
 COMMAND_TO_RETRY=$1
 NUMBER_OF_RETRY=$2
 NUMBER_OF_RETRY="${NUMBER_OF_RETRY:-1}"
-CURRENT_RETRY=1
+CURRENT_RETRY=2
 SUCCESS=false
 
-while [ "$SUCCESS" = false ] && [ "$CURRENT_RETRY" -le "$NUMBER_OF_RETRY" ]; do
-  if CURRENT_OUTPUT=$($COMMAND_TO_RETRY); then
-    SUCCESS=true
-  else
-    echo "Current # of Attempt: $CURRENT_RETRY. Trying again..."
+breeze down
+set +e
+CURRENT_OUTPUT=$($COMMAND_TO_RETRY)
+RESULT=$?
+echo "$CURRENT_OUTPUT"
+set -e
+
+while [ "${SUCCESS}" = false ] && [ "${CURRENT_RETRY}" -le "${NUMBER_OF_RETRY}" ]; do
+  if [ "${RESULT}" != "0" ]; then
+    ATTEMPT_LEFT=$((NUMBER_OF_RETRY-CURRENT_RETRY))
+    echo
+    echo "${COLOR_YELLOW}Breeze Command failed. Retrying once${COLOR_RESET}"
+    echo
+    echo "This could be due to a flaky test, re-running once to re-check it After restarting docker."
+    echo "Current Attempt: $CURRENT_RETRY, Attempt Left: ${ATTEMPT_LEFT}"
+    echo
+    sudo service docker restart
+    breeze down
+    set +e
+    CURRENT_OUTPUT=$($COMMAND_TO_RETRY)
+    RESULT=$?
+    echo "$CURRENT_OUTPUT"
+    set -e
     CURRENT_RETRY=$(( CURRENT_RETRY + 1 ))
+  else
+    SUCCESS=true
   fi
-  echo "$CURRENT_OUTPUT"
 done
