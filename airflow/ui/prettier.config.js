cf29ca2a0cec9cdb17bf42f32937de392686763c
Lou Cyx <git@lou.cx>
2024-09-12 01:13:30 -0500

    🔧 add new import sorting for prettier.

--- airflow/ui/prettier.config.js
+++ airflow/ui/prettier.config.js
@@ -1,13 +1,17 @@
 /**
  * @import { Config } from "prettier";
+ * @import { PluginConfig } from "@trivago/prettier-plugin-sort-imports";
  */
 
 /**
  * Prettier configuration.
  */
-export default /** @type {const} @satisfies {Config} */ ({
+export default /** @type {const} @satisfies {Config & PluginConfig} */ ({
   endOfLine: "lf",
+  importOrder: ["<THIRD_PARTY_MODULES>", "^(src|openapi)/", "^[./]"],
+  importOrderSeparation: true,
   jsxSingleQuote: false,
+  plugins: ["@trivago/prettier-plugin-sort-imports"],
   printWidth: 80,
   singleQuote: false,
   tabWidth: 2,
