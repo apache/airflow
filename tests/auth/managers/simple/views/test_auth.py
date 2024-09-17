b599d81121f3b9103658fba9c33d6f3bebe33cf6
Vincent <97131062+vincbeck@users.noreply.github.com>
2024-09-17 09:24:25 -0700

    Implement `SimpleAuthManager` (#42004)

---
+++ tests/auth/managers/simple/views/test_auth.py
@@ -0,0 +1,76 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+from __future__ import annotations
+
+import json
+
+import pytest
+from flask import session, url_for
+
+from airflow.auth.managers.simple.simple_auth_manager import SimpleAuthManager
+from airflow.www import app as application
+from tests.test_utils.config import conf_vars
+
+
+@pytest.fixture
+def simple_app():
+    with conf_vars(
+        {
+            (
+                "core",
+                "auth_manager",
+            ): "airflow.auth.managers.simple.simple_auth_manager.SimpleAuthManager",
+        }
+    ):
+        with open(SimpleAuthManager.GENERATED_PASSWORDS_FILE, "w") as file:
+            user = {"test": "test"}
+            file.write(json.dumps(user))
+
+        return application.create_app(
+            testing=True,
+            config={
+                "SIMPLE_AUTH_MANAGER_USERS": [
+                    {
+                        "username": "test",
+                        "role": "admin",
+                    }
+                ]
+            },
+        )
+
+
+@pytest.mark.db_test
+class TestSimpleAuthManagerAuthenticationViews:
+    def test_logout_redirects_to_login_and_clear_user(self, simple_app):
+        with simple_app.test_client() as client:
+            response = client.get("/logout")
+            assert response.status_code == 302
+            assert response.location == "/login"
+            assert session.get("user") is None
+
+    @pytest.mark.parametrize(
+        "username, password, is_successful",
+        [("test", "test", True), ("test", "test2", False), ("", "", False)],
+    )
+    def test_login_submit(self, simple_app, username, password, is_successful):
+        with simple_app.test_client() as client:
+            response = client.post("/login_submit", data={"username": username, "password": password})
+            assert response.status_code == 302
+            if is_successful:
+                assert response.location == url_for("Airflow.index")
+            else:
+                assert response.location == url_for("SimpleAuthManagerAuthenticationViews.login", error=["1"])
