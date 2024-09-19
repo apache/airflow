fad0a5974af9a64ac248f64c5869ac3233d1b368
vincbeck <vincbeck@amazon.com>
2024-09-19 11:59:24 -0400

    Make `is_logged_in` backward compatible

--- airflow/providers/fab/auth_manager/fab_auth_manager.py
+++ airflow/providers/fab/auth_manager/fab_auth_manager.py
@@ -24,6 +24,7 @@ from typing import TYPE_CHECKING, Container
 
 from connexion import FlaskApi
 from flask import Blueprint, url_for
+from packaging.version import Version
 from sqlalchemy import select
 from sqlalchemy.orm import Session, joinedload
 
@@ -81,6 +82,7 @@ from airflow.security.permissions import (
 )
 from airflow.utils.session import NEW_SESSION, provide_session
 from airflow.utils.yaml import safe_load
+from airflow.version import version
 from airflow.www.constants import SWAGGER_BUNDLE, SWAGGER_ENABLED
 from airflow.www.extensions.init_views import _CustomErrorRequestBodyValidator, _LazyResolver
 
@@ -180,9 +182,12 @@ class FabAuthManager(BaseAuthManager):
     def is_logged_in(self) -> bool:
         """Return whether the user is logged in."""
         user = self.get_user()
-        return self.appbuilder.get_app.config.get("AUTH_ROLE_PUBLIC", None) or (
-            not user.is_anonymous and user.is_active
-        )
+        if Version(Version(version).base_version) < Version("3.0.0"):
+            return not user.is_anonymous and user.is_active
+        else:
+            return self.appbuilder.get_app.config.get("AUTH_ROLE_PUBLIC", None) or (
+                not user.is_anonymous and user.is_active
+            )
 
     def is_authorized_configuration(
         self,
@@ -527,10 +532,6 @@ class FabAuthManager(BaseAuthManager):
         # Otherwise, when the name of a view or menu is changed, the framework
         # will add the new Views and Menus names to the backend, but will not
         # delete the old ones.
-        from packaging.version import Version
-
-        from airflow.version import version
-
         if Version(Version(version).base_version) >= Version("3.0.0"):
             fallback = None
         else:
