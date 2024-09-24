464ca049d5680ef29fec9c5348dbbdb2557c5490
Daniel Standish <15932138+dstandish@users.noreply.github.com>
2024-09-24 08:42:15 -0700

    fix tests

--- tests/jobs/test_scheduler_job.py
+++ tests/jobs/test_scheduler_job.py
@@ -6014,7 +6014,9 @@ class TestSchedulerJobQueriesCount:
             self.job_runner.processor_agent = mock_agent
 
             with assert_queries_count(expected_query_count, margin=15):
-                with mock.patch.object(DagRun, "running_dag_runs_to_examine") as mock_dagruns:
+                with mock.patch.object(
+                    DagRun, DagRun.get_running_dag_runs_to_examine.__name__
+                ) as mock_dagruns:
                     query = MagicMock()
                     query.all.return_value = dagruns
                     mock_dagruns.return_value = query
