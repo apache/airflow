9a03242a57f78fdd66f22e1472d27f506d03f81b
Daniel Standish <15932138+dstandish@users.noreply.github.com>
2024-09-24 14:07:25 -0700

    Remove dag run creation logic

--- airflow/api_connexion/endpoints/backfill_endpoint.py
+++ airflow/api_connexion/endpoints/backfill_endpoint.py
@@ -22,7 +22,7 @@ from typing import TYPE_CHECKING
 
 import pendulum
 from flask import g
-from sqlalchemy import func, select
+from sqlalchemy import select
 
 from airflow.api_connexion import security
 from airflow.api_connexion.exceptions import Conflict, NotFound, PermissionDenied
@@ -37,7 +37,6 @@ from airflow.models.serialized_dag import SerializedDagModel
 from airflow.utils import timezone
 from airflow.utils.session import NEW_SESSION, provide_session
 from airflow.utils.state import DagRunState
-from airflow.utils.types import DagRunType
 from airflow.www.decorators import action_logging
 from airflow.www.extensions.init_auth_manager import get_auth_manager
 
@@ -66,12 +65,6 @@ def _create_backfill(
     if not serdag:
         raise NotFound(f"Could not find dag {dag_id}")
 
-    num_active = session.scalar(
-        select(func.count()).where(Backfill.dag_id == dag_id, Backfill.completed_at.is_(None))
-    )
-    if num_active > 0:
-        return None
-
     from_date = pendulum.parse(from_date)
     to_date = pendulum.parse(to_date)
     br = Backfill(
@@ -83,49 +76,6 @@ def _create_backfill(
     )
     session.add(br)
     session.commit()
-    # todo: should we preserve `ignore_first_depends_on_past`?
-
-    dag = serdag.dag
-    depends_on_past = any(x.depends_on_past for x in dag.tasks)
-    if depends_on_past:
-        # todo: AIP-78 verify that depends on past works ok
-        if reverse is True:  # todo: AIP-78 test_reverse_and_depends_on_past_fails
-            raise ValueError(
-                "Backfill cannot be run in reverse when the dag has tasks where depends_on_past=True"
-            )
-
-    backfill_sort_ordinal = 0
-    dagrun_info_list = dag.iter_dagrun_infos_between(from_date, to_date)
-    if reverse:
-        dagrun_info_list = reversed([x for x in dag.iter_dagrun_infos_between(from_date, to_date)])
-    for info in dagrun_info_list:
-        backfill_sort_ordinal += 1
-        log.info("creating backfill dag run %s dag_id=%s backfill_id=%s, info=", dag.dag_id, br.id, info)
-        dr = None
-        try:
-            dr = dag.create_dagrun(
-                execution_date=info.logical_date,
-                data_interval=info.data_interval,
-                start_date=timezone.utcnow(),
-                state=DagRunState.QUEUED,
-                external_trigger=False,
-                conf=br.dag_run_conf,
-                run_type=DagRunType.BACKFILL_JOB,
-                creating_job_id=None,  # todo: what to do here? what does webserver do when triggering?
-                session=session,
-                backfill_id=br.id,
-            )
-        except Exception:
-            dag.log.exception("something failed")
-            session.rollback()
-        session.add(
-            BackfillDagRun(
-                backfill_id=br.id,
-                dag_run_id=dr.id if dr else None,  # this means we failed to create the dag run
-                sort_ordinal=backfill_sort_ordinal,
-            )
-        )
-        session.commit()
     return br
 
 
