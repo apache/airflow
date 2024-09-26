568162263d0bd0a92483d97c439ef4b2e3a8b782
Vincent <97131062+vincbeck@users.noreply.github.com>
2024-09-26 08:40:23 -0700

    Remove deprecated stuff from Amazon provider package (#42450)

--- airflow/providers/amazon/aws/sensors/sagemaker.py
+++ airflow/providers/amazon/aws/sensors/sagemaker.py
@@ -20,9 +20,7 @@ import time
 from functools import cached_property
 from typing import TYPE_CHECKING, Sequence
 
-from deprecated import deprecated
-
-from airflow.exceptions import AirflowException, AirflowProviderDeprecationWarning
+from airflow.exceptions import AirflowException
 from airflow.providers.amazon.aws.hooks.sagemaker import LogState, SageMakerHook
 from airflow.sensors.base import BaseSensorOperator
 
@@ -44,11 +42,6 @@ class SageMakerBaseSensor(BaseSensorOperator):
         super().__init__(**kwargs)
         self.aws_conn_id = aws_conn_id
         self.resource_type = resource_type  # only used for logs, to say what kind of resource we are sensing
-
-    @deprecated(reason="use `hook` property instead.", category=AirflowProviderDeprecationWarning)
-    def get_hook(self) -> SageMakerHook:
-        """Get SageMakerHook."""
-        return self.hook
 
     @cached_property
     def hook(self) -> SageMakerHook:
