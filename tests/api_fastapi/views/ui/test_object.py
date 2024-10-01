e5b9cf2f32d54656ba7cd0daab33a3dc6a93f456
Bugra Ozturk <ozturkbugra93@gmail.com>
2024-10-01 21:35:17 +0200

    Use pyfixture session

--- tests/api_fastapi/views/ui/test_object.py
+++ tests/api_fastapi/views/ui/test_object.py
@@ -24,7 +24,6 @@ import pytest
 
 from airflow.models import DagBag
 from airflow.operators.empty import EmptyOperator
-from airflow.utils.session import provide_session
 from airflow.utils.state import DagRunState, TaskInstanceState
 from airflow.utils.types import DagRunType
 from tests.test_utils.db import clear_db_runs
@@ -53,7 +52,6 @@ def freeze_time_for_dagruns(time_machine):
 
 
 @pytest.fixture
-@provide_session
 def make_dag_runs(dag_maker, session, time_machine):
     with dag_maker(
         dag_id="test_dag_id",
