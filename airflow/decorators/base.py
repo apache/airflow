e18b0662facd3f382062d61f94e246b3a5d0365c
David Blain <david.blain@infrabel.be>
2024-10-02 17:11:09 +0200

    refactor: Added stream method to _TaskDecorator class

--- airflow/decorators/base.py
+++ airflow/decorators/base.py
@@ -56,7 +56,7 @@ from airflow.models.expandinput import (
     ListOfDictsExpandInput,
     is_mappable,
 )
-from airflow.models.mappedoperator import MappedOperator, ensure_xcomarg_return_value
+from airflow.models.mappedoperator import MappedOperator, ensure_xcomarg_return_value, validate_mapping_kwargs
 from airflow.models.pool import Pool
 from airflow.models.xcom_arg import XComArg
 from airflow.typing_compat import ParamSpec, Protocol
@@ -529,6 +529,39 @@ class _TaskDecorator(ExpandableFactory, Generic[FParams, FReturn, OperatorSubcla
             expand_input_attr="op_kwargs_expand_input",
             start_trigger_args=self.operator_class.start_trigger_args,
             start_from_trigger=self.operator_class.start_from_trigger,
+        )
+        return XComArg(operator=operator)
+
+    def stream(self, **mapped_kwargs: OperatorExpandArgument) -> XComArg:
+        from airflow.models.streamedoperator import StreamedOperator
+
+        if not mapped_kwargs:
+            raise TypeError("no arguments to expand against")
+        validate_mapping_kwargs(self.operator_class, "stream", mapped_kwargs)
+        prevent_duplicates(self.kwargs, mapped_kwargs, fail_reason="unmappable or already specified")
+
+        expand_input = DictOfListsExpandInput(mapped_kwargs)
+        ensure_xcomarg_return_value(expand_input.value)
+
+        partial_kwargs = self.kwargs.copy()
+        task_id = partial_kwargs.pop("task_id")
+        dag = partial_kwargs.pop("dag")
+        task_group = partial_kwargs.pop("task_group")
+        start_date = partial_kwargs.pop("start_date")
+        end_date = partial_kwargs.pop("end_date")
+        max_active_tis_per_dag = partial_kwargs.pop("max_active_tis_per_dag", None)
+
+        operator = StreamedOperator(
+            task_id=task_id,
+            dag=dag,
+            task_group=task_group,
+            start_date=start_date,
+            end_date=end_date,
+            max_active_tis_per_dag=max_active_tis_per_dag,
+            operator_class=self.operator_class,
+            expand_input=expand_input,
+            retries=0,
+            partial_kwargs=self.kwargs.copy(),
         )
         return XComArg(operator=operator)
 
