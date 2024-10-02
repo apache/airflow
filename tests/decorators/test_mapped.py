fd37e9c1a1b0044dae92be12d4044682e9ef25c9
phi-friday <phi.friday@gmail.com>
2024-10-02 10:13:38 +0900

    fix: task flow dynamic mapping with default_args (#41592)

--- tests/decorators/test_mapped.py
+++ tests/decorators/test_mapped.py
@@ -17,6 +17,9 @@
 # under the License.
 from __future__ import annotations
 
+import pytest
+
+from airflow.decorators import task
 from airflow.models.dag import DAG
 from airflow.utils.task_group import TaskGroup
 from tests.models import DEFAULT_DATE
@@ -36,3 +39,24 @@ def test_mapped_task_group_id_prefix_task_id():
 
     dag.get_task("t1") == x1.operator
     dag.get_task("g.t2") == x2.operator
+
+
+@pytest.mark.db_test
+def test_mapped_task_with_arbitrary_default_args(dag_maker, session):
+    default_args = {"some": "value", "not": "in", "the": "task", "or": "dag"}
+    with dag_maker(session=session, default_args=default_args):
+
+        @task.python(do_xcom_push=True)
+        def f(x: int, y: int) -> int:
+            return x + y
+
+        f.partial(y=10).expand(x=[1, 2, 3])
+
+    dag_run = dag_maker.create_dagrun(session=session)
+    decision = dag_run.task_instance_scheduling_decisions(session=session)
+    xcoms = set()
+    for ti in decision.schedulable_tis:
+        ti.run(session=session)
+        xcoms.add(ti.xcom_pull(session=session, task_ids=ti.task_id, map_indexes=ti.map_index))
+
+    assert xcoms == {11, 12, 13}
