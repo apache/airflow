36fbd55e60e1a98f937b92c9f6a7f40378d69c11
David Blain <david.blain@infrabel.be>
2024-10-04 16:19:20 +0200

    refactor: Reformatted dialect method in DbApiHook

--- airflow/providers/common/sql/hooks/sql.py
+++ airflow/providers/common/sql/hooks/sql.py
@@ -271,7 +271,7 @@ class DbApiHook(BaseHook):
                 raise AirflowOptionalProviderFeatureException(
                     f"{dialect_info.dialect_class_name} not found, run: pip install "
                     f"'{dialect_info.provider_name}'."
-                    )
+                )
         raise AirflowException(f"Dialect '{self.dialect_name}' not found")
 
     def get_pandas_df(
