76442b73aca6d9d53cca655c7ae014055948165d
Pavan Kumar <gopidesupavan@gmail.com>
2024-10-04 21:26:45 +0100

    remove version from hatch build

--- hatch_build.py
+++ hatch_build.py
@@ -49,7 +49,7 @@ PRE_INSTALLED_PROVIDERS = [
     "imap",
     "smtp",
     "sqlite",
-    "standard>=1.0.0.dev0",
+    "standard",
 ]
 
 # Those extras are dynamically added by hatch in the build hook to metadata optional dependencies
