d002ffe86984e2f5b4cc83857c96238b8ea334be
David Blain <david.blain@infrabel.be>
2024-10-07 13:58:22 +0200

    refactor: Incremented version of apache-airflow-providers-common-sql in test_get_install_requirements

--- dev/breeze/tests/test_packages.py
+++ dev/breeze/tests/test_packages.py
@@ -207,7 +207,7 @@ def test_get_documentation_package_path():
             "postgres",
             "beta0",
             """
-    "apache-airflow-providers-common-sql>=1.17.0b0",
+    "apache-airflow-providers-common-sql>=1.17.1b0",
     "apache-airflow>=2.8.0b0",
     "psycopg2-binary>=2.9.4",
     """,
@@ -217,7 +217,7 @@ def test_get_documentation_package_path():
             "postgres",
             "",
             """
-    "apache-airflow-providers-common-sql>=1.17.0",
+    "apache-airflow-providers-common-sql>=1.17.1",
     "apache-airflow>=2.8.0",
     "psycopg2-binary>=2.9.4",
     """,
