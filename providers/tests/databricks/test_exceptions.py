2d64d32598051a6b2c595753c735fc1b19a9108f
Lorin <lorin@databricks.com>
2024-10-07 12:22:46 -0600

    add tests for new exceptions

--- providers/tests/databricks/test_exceptions.py
+++ providers/tests/databricks/test_exceptions.py
@@ -1,33 +1,14 @@
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-from __future__ import annotations
-
 import pytest
+from airflow.providers.databricks.exceptions import AirflowTaskExecutionError, AirflowTaskExecutionTimeout
 
-from airflow.providers.databricks.exceptions import DatabricksSqlExecutionError, DatabricksSqlExecutionTimeout
 
-
-def test_databricks_sql_execution_error():
+def test_airflow_task_execution_error():
     """Test if AirflowTaskExecutionError can be raised correctly."""
-    with pytest.raises(DatabricksSqlExecutionError, match="Task execution failed"):
-        raise DatabricksSqlExecutionError("Task execution failed")
+    with pytest.raises(AirflowTaskExecutionError, match="Task execution failed"):
+        raise AirflowTaskExecutionError("Task execution failed")
 
 
-def test_databricks_sql_execution_timeout():
+def test_airflow_task_execution_timeout():
     """Test if AirflowTaskExecutionTimeout can be raised correctly."""
-    with pytest.raises(DatabricksSqlExecutionTimeout, match="Task execution timed out"):
-        raise DatabricksSqlExecutionTimeout("Task execution timed out")
+    with pytest.raises(AirflowTaskExecutionTimeout, match="Task execution timed out"):
+        raise AirflowTaskExecutionTimeout("Task execution timed out")
