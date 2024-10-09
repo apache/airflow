2a2165762ea12ffd901877ac9efd758235dd07c2
Tzu-ping Chung <uranusjr@gmail.com>
2024-10-09 13:19:53 +0900

    Fix some provider/cncf compat on logical date
    We should continue to use execution_date if the provider is run against 2.

--- providers/tests/cncf/kubernetes/executors/test_kubernetes_executor.py
+++ providers/tests/cncf/kubernetes/executors/test_kubernetes_executor.py
@@ -28,6 +28,7 @@ from kubernetes.client import models as k8s
 from kubernetes.client.rest import ApiException
 from urllib3 import HTTPResponse
 
+from airflow import __version__
 from airflow.exceptions import AirflowException, AirflowProviderDeprecationWarning
 from airflow.models.taskinstancekey import TaskInstanceKey
 from airflow.operators.empty import EmptyOperator
@@ -59,6 +60,11 @@ from tests_common.test_utils.compat import BashOperator
 from tests_common.test_utils.config import conf_vars
 
 pytestmark = pytest.mark.skip_if_database_isolation_mode
+
+if __version__.startswith("2."):
+    LOGICAL_DATE_KEY = "execution_date"
+else:
+    LOGICAL_DATE_KEY = "logical_date"
 
 
 class TestAirflowKubernetesScheduler:
@@ -1609,7 +1615,7 @@ class TestKubernetesJobWatcher:
             "task_id": "task",
             "run_id": "run_id",
             "try_number": "1",
-            "execution_date": None,
+            LOGICAL_DATE_KEY: None,
         }
         self.pod = k8s.V1Pod(
             metadata=k8s.V1ObjectMeta(
