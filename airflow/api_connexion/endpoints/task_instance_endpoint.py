8928a34139682c5b6e8f2cf2faf7b9ed01b51bd6
kalyanr <kalyan.ben10@live.com>
2024-10-10 01:00:04 +0530

    add offset and limit

--- airflow/api_connexion/endpoints/task_instance_endpoint.py
+++ airflow/api_connexion/endpoints/task_instance_endpoint.py
@@ -464,15 +464,21 @@ def get_task_instances_batch(session: Session = NEW_SESSION) -> APIResponse:
     # Count elements before joining extra columns
     total_entries = get_query_count(base_query, session=session)
     # Add join
-    base_query = base_query.join(
-        SlaMiss,
-        and_(
-            SlaMiss.dag_id == TI.dag_id,
-            SlaMiss.task_id == TI.task_id,
-            SlaMiss.execution_date == DR.execution_date,
-        ),
-        isouter=True,
-    ).add_columns(SlaMiss)
+    base_query = (
+        base_query.join(
+            SlaMiss,
+            and_(
+                SlaMiss.dag_id == TI.dag_id,
+                SlaMiss.task_id == TI.task_id,
+                SlaMiss.execution_date == DR.execution_date,
+            ),
+            isouter=True,
+        )
+        .add_columns(SlaMiss)
+        .offset(data["offset"])
+        .limit(data["limit"])
+    )
+
     ti_query = base_query.options(
         joinedload(TI.rendered_task_instance_fields), joinedload(TI.task_instance_note)
     )
