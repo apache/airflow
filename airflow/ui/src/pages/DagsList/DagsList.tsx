62dc6b1fb616cb8a95fe43f3ef95f986498b9218
Luyang Liu <luyang.l@proton.me>
2024-10-10 19:47:13 +1100

    Update airflow/ui/src/pages/DagsList/DagsList.tsx
    Co-authored-by: Brent Bovenzi <brent.bovenzi@gmail.com>

--- airflow/ui/src/pages/DagsList/DagsList.tsx
+++ airflow/ui/src/pages/DagsList/DagsList.tsx
@@ -133,7 +133,9 @@ export const DagsList = () => {
 
   const { data, error, isFetching, isLoading } = useDagServiceGetDags({
     lastDagRunState,
-    dagDisplayNamePattern: dagDisplayNamePattern !== "" ? dagDisplayNamePattern : undefined,
+    dagDisplayNamePattern: Boolean(dagDisplayNamePattern)
+        ? `%${dagDisplayNamePattern}%`
+        : undefined,
     limit: pagination.pageSize,
     offset: pagination.pageIndex * pagination.pageSize,
     onlyActive: true,
