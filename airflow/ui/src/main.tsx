8fe8010084f653891b7f12bf6e5caee7f4cf8559
Brent Bovenzi <brent@astronomer.io>
2024-10-11 13:53:26 +0200

    Render errors when getting a list of dags (#42897)
    * Render errors when getting a list of dags
    
    * Restore axios, prettierignore pnpm-store
    
    * Add pnpm-store to prettier ignore

--- airflow/ui/src/main.tsx
+++ airflow/ui/src/main.tsx
@@ -18,7 +18,7 @@
  */
 import { ChakraProvider } from "@chakra-ui/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import axios, { type AxiosError, type AxiosResponse } from "axios";
+import axios, { type AxiosError } from "axios";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
@@ -45,7 +45,7 @@ const queryClient = new QueryClient({
 
 // redirect to login page if the API responds with unauthorized or forbidden errors
 axios.interceptors.response.use(
-  (response: AxiosResponse) => response,
+  (response) => response,
   (error: AxiosError) => {
     if (error.response?.status === 403 || error.response?.status === 401) {
       const params = new URLSearchParams();
@@ -53,6 +53,8 @@ axios.interceptors.response.use(
       params.set("next", globalThis.location.href);
       globalThis.location.replace(`/login?${params.toString()}`);
     }
+
+    return Promise.reject(error);
   },
 );
 
