2f65937035d87911741f8bedd494feb3e4b223a9
ferruzzi <ferruzzi@amazon.com>
2024-10-11 15:21:18 -0700

    Rephrase relevant comments

--- airflow/__init__.py
+++ airflow/__init__.py
@@ -17,7 +17,9 @@
 # under the License.
 from __future__ import annotations
 
-# Explicit namespaces are required by some IDEs to resolve the import paths.
+# Make `airflow` a namespace package, supporting installing
+# airflow.providers.* in different locations (i.e. one in site, and one in user
+# lib.)  This is required by some IDEs to resolve the import paths.
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
 
 __version__ = "3.0.0.dev0"
--- providers/src/airflow/providers/__init__.py
+++ providers/src/airflow/providers/__init__.py
@@ -18,7 +18,9 @@
 
 from __future__ import annotations
 
-# Explicit namespaces are required by some IDEs to resolve the import paths.
+# Make `airflow` a namespace package, supporting installing
+# airflow.providers.* in different locations (i.e. one in site, and one in user
+# lib.)  This is required by some IDEs to resolve the import paths.
 #
 # Note: this file is not installed or distributed in any distribution!
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
