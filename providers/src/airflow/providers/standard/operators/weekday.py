9effa55a7a7d54f0ffcc9dfb030cf3eb50e7fb6e
Sam Firke <sfirke@users.noreply.github.com>
2024-10-15 15:26:49 -0400

    restore whitespace line

--- providers/src/airflow/providers/standard/operators/weekday.py
+++ providers/src/airflow/providers/standard/operators/weekday.py
@@ -40,6 +40,7 @@ class BranchDayOfWeekOperator(BaseBranchOperator):
 
         from airflow.operators.empty import EmptyOperator
         from airflow.operators.weekday import BranchDayOfWeekOperator
+        
         monday = EmptyOperator(task_id="monday")
         other_day = EmptyOperator(task_id="other_day")
 
