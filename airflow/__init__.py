apache/airflow

commit 857ca4c06c9008593674cabdd28d3c30e3e7f97b

Split providers out of the main "airflow/" tree into a UV workspace project (#42505)
This is only a partial split so far. It moves all the code and tests, but
leaves the creation of `core/` to a separate PR as this is already large
enough.

In addition to the straight file rename the other changes I had to make here
are:

- Some mypy/typing fixes.

  Mypy can be fragile about what it picks up when, so maybe some of those
  changes were caused by that. But the typing changes aren't large.

- Improve typing in common.sql type stub

  Again, likely a mypy file oddity, but the types should be safe

- Removed the `check-providers-init-file-missing` check

  This isn't needed now that airflow/providers shouldn't exist at all in the
  main tree.

- Create a "dev.tests_common" package that contains helper files and common
  pytest fixtures

  Since the provider tests are no longer under tests/ they don't automatically
  share the fixtures from the parent `tests/conftest.py` so they needed
  extracted.

  Ditto for `tests.test_utils` -- they can't be easily imported in provider
  tests anymore, so they are moved to a more explicit shared location.

In future we should switch how the CI image is built to make better use of UV
caching than our own approach as that would remvoe a lot of custom code.

providers/__init__.py
@@ -1,0 +1,23 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+# This exists so that pytest doesn't get confused about namespace packages
+# and think that `tests/conftest.py` and `providers/tests/conftest.py` are
+# both "tests.conftest"
+#
+# This is a temporary solution until https://github.com/apache/airflow/issues/42632
+# is done

pyproject.toml
@@ -205,7 +205,6 @@ include = [
     "/airflow/git_version"
 ]
 exclude = [
-    "/airflow/providers/",
     "/airflow/www/node_modules/"
 ]
 artifacts = [
@@ -218,9 +217,6 @@ artifacts = [
 [tool.hatch.build.targets.wheel]
 include = [
     "/airflow",
-]
-exclude = [
-    "/airflow/providers/",
 ]
 artifacts = [
     "/airflow/www/static/dist/",
@@ -240,9 +236,6 @@ line-length = 110
 extend-exclude = [
     ".eggs",
     "*/_vendor/*",
-    # The files generated by stubgen aren't 100% valid syntax it turns out, and we don't ship them, so we can
-    # ignore them in ruff
-    "airflow/providers/common/sql/*/*.pyi",
     "tests/dags/test_imports.py",
 ]
 
@@ -337,7 +330,21 @@ docstring-code-format = true
 required-imports = ["from __future__ import annotations"]
 combine-as-imports = true
 
-[tool.ruff.lint.per-file-ignores]
+
+section-order = [
+  "future",
+  "standard-library",
+  "third-party",
+  "first-party",
+  "local-folder",
+  "testing"
+]
+
+# Make sure we put the "dev" imports at the end, not as a third-party module
+[tool.ruff.lint.isort.sections]
+testing = ["dev", "providers.tests"]
+
+[tool.ruff.lint.extend-per-file-ignores]
 "airflow/__init__.py" = ["F401", "TCH004"]
 "airflow/models/__init__.py" = ["F401", "TCH004"]
 "airflow/models/sqla_models.py" = ["F401"]
@@ -370,6 +377,7 @@ combine-as-imports = true
 # TRY002: Use `raise Exception`
 "dev/perf/*" = ["TID253"]
 "dev/check_files.py" = ["S101"]
+"dev/tests_common/*" = ["S101", "TRY002"]
 "dev/breeze/tests/*" = ["TID253", "S101", "TRY002"]
 "tests/*" = ["D", "TID253", "S101", "TRY002"]
 "docker_tests/*" = ["D", "TID253", "S101", "TRY002"]
@@ -382,47 +390,12 @@ combine-as-imports = true
 "airflow/api/auth/backend/kerberos_auth.py" = ["E402"]
 "airflow/security/kerberos.py" = ["E402"]
 "airflow/security/utils.py" = ["E402"]
-"tests/providers/amazon/aws/auth_manager/security_manager/test_aws_security_manager_override.py" = ["E402"]
-"tests/providers/common/io/xcom/test_backend.py" = ["E402"]
-"tests/providers/elasticsearch/log/elasticmock/__init__.py" = ["E402"]
-"tests/providers/elasticsearch/log/elasticmock/utilities/__init__.py" = ["E402"]
-"tests/providers/google/cloud/hooks/vertex_ai/test_batch_prediction_job.py" = ["E402"]
-"tests/providers/google/cloud/hooks/vertex_ai/test_hyperparameter_tuning_job.py" = ["E402"]
-"tests/providers/google/cloud/hooks/vertex_ai/test_auto_ml.py" = ["E402"]
-"tests/providers/google/cloud/hooks/vertex_ai/test_custom_job.py" = ["E402"]
-"tests/providers/google/cloud/hooks/vertex_ai/test_dataset.py" = ["E402"]
-"tests/providers/google/cloud/hooks/vertex_ai/test_endpoint_service.py" = ["E402"]
-"tests/providers/google/cloud/hooks/vertex_ai/test_generative_model.py" = ["E402"]
-"tests/providers/google/cloud/hooks/vertex_ai/test_model_service.py" = ["E402"]
-"tests/providers/google/cloud/hooks/vertex_ai/test_pipeline_job.py" = ["E402"]
-"tests/providers/google/cloud/hooks/vertex_ai/test_prediction_service.py" = ["E402"]
-"tests/providers/google/cloud/links/test_translate.py" = ["E402"]
-"tests/providers/google/cloud/operators/test_automl.py"= ["E402"]
-"tests/providers/google/cloud/operators/test_vertex_ai.py" = ["E402"]
-"tests/providers/google/cloud/operators/vertex_ai/test_generative_model.py" = ["E402"]
-"tests/providers/google/cloud/triggers/test_vertex_ai.py" = ["E402"]
-"tests/providers/openai/hooks/test_openai.py" = ["E402"]
-"tests/providers/openai/operators/test_openai.py" = ["E402"]
-"tests/providers/openai/triggers/test_openai.py" = ["E402"]
-"tests/providers/opensearch/conftest.py" = ["E402"]
-"tests/providers/opensearch/hooks/test_opensearch.py" = ["E402"]
-"tests/providers/opensearch/log/test_os_json_formatter.py" = ["E402"]
-"tests/providers/opensearch/log/test_os_response.py" = ["E402"]
-"tests/providers/opensearch/log/test_os_task_handler.py" = ["E402"]
-"tests/providers/opensearch/operators/test_opensearch.py" = ["E402"]
-"tests/providers/qdrant/hooks/test_qdrant.py" = ["E402"]
-"tests/providers/qdrant/operators/test_qdrant.py" = ["E402"]
-"tests/providers/snowflake/operators/test_snowflake_sql.py" = ["E402"]
-"tests/providers/yandex/*/*.py" = ["E402"]
 
 # All the modules which do not follow B028 yet: https://docs.astral.sh/ruff/rules/no-explicit-stacklevel/
 "helm_tests/airflow_aux/test_basic_helm_chart.py" = ["B028"]
 
-# https://github.com/apache/airflow/issues/39252
-"airflow/providers/amazon/aws/hooks/eks.py" = ["W605"]
-
 # Test compat imports banned imports to allow testing against older airflow versions
-"tests/test_utils/compat.py" = ["TID251", "F401"]
+"dev/tests_common/test_utils/compat.py" = ["TID251", "F401"]
 
 [tool.ruff.lint.flake8-tidy-imports]
 # Disallow all relative imports.
@@ -512,11 +485,10 @@ norecursedirs = [
     "airflow",
     "tests/_internals",
     "tests/dags_with_system_exit",
-    "tests/test_utils",
     "tests/dags_corrupted",
     "tests/dags",
-    "tests/system/providers/google/cloud/dataproc/resources",
-    "tests/system/providers/google/cloud/gcs/resources",
+    "providers/tests/system/google/cloud/dataproc/resources",
+    "providers/tests/system/google/cloud/gcs/resources",
 ]
 log_level = "INFO"
 filterwarnings = [
@@ -576,9 +548,9 @@ omit = [
     "airflow/contrib/**",
     "airflow/example_dags/**",
     "airflow/migrations/**",
-    "airflow/providers/**/example_dags/**",
     "airflow/www/node_modules/**",
-    "airflow/providers/google/ads/_vendor/**",
+    "providers/src/airflow/providers/**/example_dags/**",
+    "providers/src/airflow/providers/google/ads/_vendor/**",
 ]
 
 [tool.coverage.report]
@@ -608,6 +580,16 @@ pretty = true
 show_error_codes = true
 disable_error_code = [
     "annotation-unchecked",
+]
+# Since there are no __init__.py files in
+# providers/src/apache/airflow/providers we need to tell MyPy where the "base"
+# is, otherwise when it sees
+# providers/src/apache/airflow/providers/redis/__init__.py, it thinks this is
+# the toplevel `redis` folder.
+explicit_package_bases = true
+mypy_path = [
+  "$MYPY_CONFIG_FILE_DIR",
+  "$MYPY_CONFIG_FILE_DIR/providers/src",
 ]
 
 [[tool.mypy.overrides]]
@@ -640,3 +622,16 @@ module=[
     "apache_beam.*"
 ]
 ignore_errors = true
+
+[tool.uv]
+dev-dependencies = [
+  "local-providers",
+]
+
+[tool.uv.sources]
+# These names must match the names as defined in the pyproject.toml of the workspace items,
+# *not* the workspace folder paths
+local-providers = { workspace = true }
+
+[tool.uv.workspace]
+members = ["providers"]

scripts/ci/pre_commit/update_common_sql_api_stubs.py
@@ -39,10 +39,12 @@ sys.path.insert(0, str(Path(__file__).parent.resolve()))  # make sure common_pre
 from common_precommit_black_utils import black_format
 from common_precommit_utils import AIRFLOW_SOURCES_ROOT_PATH
 
-PROVIDERS_ROOT = (AIRFLOW_SOURCES_ROOT_PATH / "airflow" / "providers").resolve(strict=True)
+PROVIDERS_ROOT = (AIRFLOW_SOURCES_ROOT_PATH / "providers" / "src" / "airflow" / "providers").resolve(
+    strict=True
+)
 COMMON_SQL_ROOT = (PROVIDERS_ROOT / "common" / "sql").resolve(strict=True)
 OUT_DIR = AIRFLOW_SOURCES_ROOT_PATH / "out"
-OUT_DIR_PROVIDERS = OUT_DIR / "airflow" / "providers"
+OUT_DIR_PROVIDERS = OUT_DIR / PROVIDERS_ROOT.relative_to(AIRFLOW_SOURCES_ROOT_PATH)
 
 COMMON_SQL_PACKAGE_PREFIX = "airflow.providers.common.sql."
 
@@ -317,7 +319,7 @@ if __name__ == "__main__":
     shutil.rmtree(OUT_DIR, ignore_errors=True)
 
     subprocess.run(
-        ["stubgen", *[os.fspath(path) for path in COMMON_SQL_ROOT.rglob("*.py")]],
+        ["stubgen", f"--out={ OUT_DIR }", COMMON_SQL_ROOT],
         cwd=AIRFLOW_SOURCES_ROOT_PATH,
     )
     total_removals, total_additions = 0, 0

providers/tests/openlineage/extractors/test_base.py
@@ -1,0 +1,321 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any
+from unittest import mock
+
+import pytest
+from attrs import Factory, define, field
+from openlineage.client.event_v2 import Dataset
+from openlineage.client.facet_v2 import BaseFacet, JobFacet, parent_run, sql_job
+
+from airflow.models.baseoperator import BaseOperator
+from airflow.models.taskinstance import TaskInstanceState
+from airflow.operators.python import PythonOperator
+from airflow.providers.openlineage.extractors.base import (
+    BaseExtractor,
+    DefaultExtractor,
+    OperatorLineage,
+)
+from airflow.providers.openlineage.extractors.manager import ExtractorManager
+from airflow.providers.openlineage.extractors.python import PythonExtractor
+
+if TYPE_CHECKING:
+    from openlineage.client.facet_v2 import RunFacet
+pytestmark = pytest.mark.db_test
+
+
+INPUTS = [Dataset(namespace="database://host:port", name="inputtable")]
+OUTPUTS = [Dataset(namespace="database://host:port", name="inputtable")]
+RUN_FACETS: dict[str, RunFacet] = {
+    "parent": parent_run.ParentRunFacet(
+        run=parent_run.Run(runId="3bb703d1-09c1-4a42-8da5-35a0b3216072"),
+        job=parent_run.Job(namespace="namespace", name="parentjob"),
+    )
+}
+JOB_FACETS: dict[str, JobFacet] = {"sql": sql_job.SQLJobFacet(query="SELECT * FROM inputtable")}
+
+
+@define
+class CompleteRunFacet(JobFacet):
+    finished: bool = field(default=False)
+
+
+FINISHED_FACETS: dict[str, JobFacet] = {"complete": CompleteRunFacet(True)}
+
+
+class ExampleExtractor(BaseExtractor):
+    @classmethod
+    def get_operator_classnames(cls):
+        return ["ExampleOperator"]
+
+
+class ExampleOperator(BaseOperator):
+    def execute(self, context) -> Any:
+        pass
+
+    def get_openlineage_facets_on_start(self) -> OperatorLineage:
+        return OperatorLineage(
+            inputs=INPUTS,
+            outputs=OUTPUTS,
+            run_facets=RUN_FACETS,
+            job_facets=JOB_FACETS,
+        )
+
+    def get_openlineage_facets_on_complete(self, task_instance) -> OperatorLineage:
+        return OperatorLineage(
+            inputs=INPUTS,
+            outputs=OUTPUTS,
+            run_facets=RUN_FACETS,
+            job_facets=FINISHED_FACETS,
+        )
+
+
+class OperatorWithoutComplete(BaseOperator):
+    def execute(self, context) -> Any:
+        pass
+
+    def get_openlineage_facets_on_start(self) -> OperatorLineage:
+        return OperatorLineage(
+            inputs=INPUTS,
+            outputs=OUTPUTS,
+            run_facets=RUN_FACETS,
+            job_facets=JOB_FACETS,
+        )
+
+
+class OperatorWithoutStart(BaseOperator):
+    def execute(self, context) -> Any:
+        pass
+
+    def get_openlineage_facets_on_complete(self, task_instance) -> OperatorLineage:
+        return OperatorLineage(
+            inputs=INPUTS,
+            outputs=OUTPUTS,
+            run_facets=RUN_FACETS,
+            job_facets=FINISHED_FACETS,
+        )
+
+
+class OperatorDifferentOperatorLineageClass(BaseOperator):
+    def execute(self, context) -> Any:
+        pass
+
+    def get_openlineage_facets_on_start(self):
+        @define
+        class DifferentOperatorLineage:
+            name: str = ""
+            inputs: list[Dataset] = Factory(list)
+            outputs: list[Dataset] = Factory(list)
+            run_facets: dict[str, BaseFacet] = Factory(dict)
+            job_facets: dict[str, BaseFacet] = Factory(dict)
+            some_other_param: dict = Factory(dict)
+
+        return DifferentOperatorLineage(  # type: ignore
+            name="unused",
+            inputs=INPUTS,
+            outputs=OUTPUTS,
+            run_facets=RUN_FACETS,
+            job_facets=JOB_FACETS,
+            some_other_param={"asdf": "fdsa"},
+        )
+
+
+class OperatorWrongOperatorLineageClass(BaseOperator):
+    def execute(self, context) -> Any:
+        pass
+
+    def get_openlineage_facets_on_start(self):
+        @define
+        class WrongOperatorLineage:
+            inputs: list[Dataset] = Factory(list)
+            outputs: list[Dataset] = Factory(list)
+            some_other_param: dict = Factory(dict)
+
+        return WrongOperatorLineage(  # type: ignore
+            inputs=INPUTS,
+            outputs=OUTPUTS,
+            some_other_param={"asdf": "fdsa"},
+        )
+
+
+class BrokenOperator(BaseOperator):
+    get_openlineage_facets: list[BaseFacet] = []
+
+    def execute(self, context) -> Any:
+        pass
+
+
+def test_default_extraction():
+    extractor = ExtractorManager().get_extractor_class(ExampleOperator)
+    assert extractor is DefaultExtractor
+
+    metadata = extractor(ExampleOperator(task_id="test")).extract()
+
+    task_instance = mock.MagicMock()
+
+    metadata_on_complete = extractor(ExampleOperator(task_id="test")).extract_on_complete(
+        task_instance=task_instance
+    )
+
+    assert metadata == OperatorLineage(
+        inputs=INPUTS,
+        outputs=OUTPUTS,
+        run_facets=RUN_FACETS,
+        job_facets=JOB_FACETS,
+    )
+
+    assert metadata_on_complete == OperatorLineage(
+        inputs=INPUTS,
+        outputs=OUTPUTS,
+        run_facets=RUN_FACETS,
+        job_facets=FINISHED_FACETS,
+    )
+
+
+def test_extraction_without_on_complete():
+    extractor = ExtractorManager().get_extractor_class(OperatorWithoutComplete)
+    assert extractor is DefaultExtractor
+
+    metadata = extractor(OperatorWithoutComplete(task_id="test")).extract()
+
+    task_instance = mock.MagicMock()
+
+    metadata_on_complete = extractor(OperatorWithoutComplete(task_id="test")).extract_on_complete(
+        task_instance=task_instance
+    )
+
+    expected_task_metadata = OperatorLineage(
+        inputs=INPUTS,
+        outputs=OUTPUTS,
+        run_facets=RUN_FACETS,
+        job_facets=JOB_FACETS,
+    )
+
+    assert metadata == expected_task_metadata
+
+    assert metadata_on_complete == expected_task_metadata
+
+
+def test_extraction_without_on_start():
+    extractor = ExtractorManager().get_extractor_class(OperatorWithoutStart)
+    assert extractor is DefaultExtractor
+
+    metadata = extractor(OperatorWithoutStart(task_id="test")).extract()
+
+    task_instance = mock.MagicMock()
+
+    metadata_on_complete = extractor(OperatorWithoutStart(task_id="test")).extract_on_complete(
+        task_instance=task_instance
+    )
+
+    assert metadata == OperatorLineage()
+
+    assert metadata_on_complete == OperatorLineage(
+        inputs=INPUTS,
+        outputs=OUTPUTS,
+        run_facets=RUN_FACETS,
+        job_facets=FINISHED_FACETS,
+    )
+
+
+@pytest.mark.parametrize(
+    "task_state, is_airflow_2_10_or_higher, should_call_on_failure",
+    (
+        # Airflow >= 2.10
+        (TaskInstanceState.FAILED, True, True),
+        (TaskInstanceState.UP_FOR_RETRY, True, True),
+        (TaskInstanceState.RUNNING, True, False),
+        (TaskInstanceState.SUCCESS, True, False),
+        # Airflow < 2.10
+        (TaskInstanceState.RUNNING, False, True),
+        (TaskInstanceState.SUCCESS, False, False),
+        (TaskInstanceState.FAILED, False, False),  # should never happen, fixed in #41053
+        (TaskInstanceState.UP_FOR_RETRY, False, False),  # should never happen, fixed in #41053
+    ),
+)
+def test_extract_on_failure(task_state, is_airflow_2_10_or_higher, should_call_on_failure):
+    task_instance = mock.Mock(state=task_state)
+    operator = mock.Mock()
+    operator.get_openlineage_facets_on_failure = mock.Mock(
+        return_value=OperatorLineage(run_facets={"failed": True})
+    )
+    operator.get_openlineage_facets_on_complete = mock.Mock(return_value=None)
+
+    extractor = DefaultExtractor(operator=operator)
+
+    with mock.patch(
+        "airflow.providers.openlineage.extractors.base.IS_AIRFLOW_2_10_OR_HIGHER", is_airflow_2_10_or_higher
+    ):
+        result = extractor.extract_on_complete(task_instance)
+
+        if should_call_on_failure:
+            operator.get_openlineage_facets_on_failure.assert_called_once_with(task_instance)
+            operator.get_openlineage_facets_on_complete.assert_not_called()
+            assert isinstance(result, OperatorLineage)
+            assert result.run_facets == {"failed": True}
+        else:
+            operator.get_openlineage_facets_on_failure.assert_not_called()
+            operator.get_openlineage_facets_on_complete.assert_called_once_with(task_instance)
+            assert result is None
+
+
+@mock.patch("airflow.providers.openlineage.conf.custom_extractors")
+def test_extractors_env_var(custom_extractors):
+    custom_extractors.return_value = {"providers.tests.openlineage.extractors.test_base.ExampleExtractor"}
+    extractor = ExtractorManager().get_extractor_class(ExampleOperator(task_id="example"))
+    assert extractor is ExampleExtractor
+
+
+def test_does_not_use_default_extractor_when_not_a_method():
+    extractor_class = ExtractorManager().get_extractor_class(BrokenOperator(task_id="a"))
+    assert extractor_class is None
+
+
+def test_does_not_use_default_extractor_when_no_get_openlineage_facets():
+    extractor_class = ExtractorManager().get_extractor_class(BaseOperator(task_id="b"))
+    assert extractor_class is None
+
+
+def test_does_not_use_default_extractor_when_explicite_extractor():
+    extractor_class = ExtractorManager().get_extractor_class(
+        PythonOperator(task_id="c", python_callable=lambda: 7)
+    )
+    assert extractor_class is PythonExtractor
+
+
+def test_default_extractor_uses_different_operatorlineage_class():
+    operator = OperatorDifferentOperatorLineageClass(task_id="task_id")
+    extractor_class = ExtractorManager().get_extractor_class(operator)
+    assert extractor_class is DefaultExtractor
+    extractor = extractor_class(operator)
+    assert extractor.extract() == OperatorLineage(
+        inputs=INPUTS,
+        outputs=OUTPUTS,
+        run_facets=RUN_FACETS,
+        job_facets=JOB_FACETS,
+    )
+
+
+def test_default_extractor_uses_wrong_operatorlineage_class():
+    operator = OperatorWrongOperatorLineageClass(task_id="task_id")
+    # If extractor returns lineage class that can't be changed into OperatorLineage, just return
+    # empty OperatorLineage
+    assert (
+        ExtractorManager().extract_metadata(mock.MagicMock(), operator, complete=False) == OperatorLineage()
+    )

commit 7f84059996e16ec23507c1f7e802d2abfe8c9655

Remove "from __future__" from airflow/providers __init__.py
Cleans-up airflow and providers `__init__.py" files in order to
get providers import work again.

This is done by excluding the two `__init__.py` files from
automated ruff isort rules adding `from __future__ import annotations`.

Also removed the `__init__.py` file from "providers" directory,
it is not needed there, because "providers" is just a folder where
we keep provider files, it's not a Python package.

That should finally get rid of the Intellij teething import
problem that has been introduced in #42505.

There were earlier - unsuccesful - attempts to fix it in
the #43116 and #43081 that followed #42951 - but the key is that Pycharm
requires the namespace's extend_path to be first "real" line
of code in the `__init__.py` to understand that the package
is an "explicit" namespace package - and it conflicts with
the requirement of "from __future__ import annotations" to be
the first line of Python code.

Also this PR fixes a few other teething problems with setup of
tests that were introcuded in #42505 and #43802 "masked" by having
`__init__.py` added in providers package:

* common.sql interface pre-commit used wrong path to generated files
* openlineage extractor test that should not expect "providers.tests.*"
  but "tests.*" package
* common_sql_api_stubs wrongly calculating generated path for
  stub-generated files
* pytest_plugin expecting .asf.yml in "airflow" sources - even during
  compatibility tests with older version of airflow (where the
  .asf.yml is not present)

airflow/__init__.py
@@ -15,7 +15,10 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-from __future__ import annotations
+
+# We do not use "from __future__ import annotations" here because it is not supported
+# by Pycharm when we want to make sure all imports in airflow work from namespace packages
+# Adding it automatically is excluded in pyproject.toml via I002 ruff rule exclusion
 
 # Make `airflow` a namespace package, supporting installing
 # airflow.providers.* in different locations (i.e. one in site, and one in user

providers/__init__.py
@@ -1,23 +1,0 @@
-# Licensed to the Apache Software Foundation (ASF) under one
-# or more contributor license agreements.  See the NOTICE file
-# distributed with this work for additional information
-# regarding copyright ownership.  The ASF licenses this file
-# to you under the Apache License, Version 2.0 (the
-# "License"); you may not use this file except in compliance
-# with the License.  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing,
-# software distributed under the License is distributed on an
-# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-# KIND, either express or implied.  See the License for the
-# specific language governing permissions and limitations
-# under the License.
-
-# This exists so that pytest doesn't get confused about namespace packages
-# and think that `tests/conftest.py` and `providers/tests/conftest.py` are
-# both "tests.conftest"
-#
-# This is a temporary solution until https://github.com/apache/airflow/issues/42632
-# is done

providers/src/airflow/providers/__init__.py
@@ -16,7 +16,9 @@
 # specific language governing permissions and limitations
 # under the License.
 
-from __future__ import annotations
+# We do not use "from __future__ import annotations" here because it is not supported
+# by Pycharm when we want to make sure all imports in airflow work from namespace packages
+# Adding it automatically is excluded in pyproject.toml via I002 ruff rule exclusion
 
 # Make `airflow` a namespace package, supporting installing
 # airflow.providers.* in different locations (i.e. one in site, and one in user

pyproject.toml
@@ -346,9 +346,10 @@ section-order = [
 testing = ["dev", "providers.tests", "task_sdk.tests", "tests_common", "tests"]
 
 [tool.ruff.lint.extend-per-file-ignores]
-"airflow/__init__.py" = ["F401", "TCH004"]
+"airflow/__init__.py" = ["F401", "TCH004", "I002"]
 "airflow/models/__init__.py" = ["F401", "TCH004"]
 "airflow/models/sqla_models.py" = ["F401"]
+"providers/src/airflow/providers/__init__.py" = ["I002"]
 
 # The test_python.py is needed because adding __future__.annotations breaks runtime checks that are
 # needed for the test to work

scripts/ci/pre_commit/update_common_sql_api_stubs.py
@@ -44,7 +44,7 @@ PROVIDERS_ROOT = (AIRFLOW_SOURCES_ROOT_PATH / "providers" / "src" / "airflow" / 
 )
 COMMON_SQL_ROOT = (PROVIDERS_ROOT / "common" / "sql").resolve(strict=True)
 OUT_DIR = AIRFLOW_SOURCES_ROOT_PATH / "out"
-OUT_DIR_PROVIDERS = OUT_DIR / PROVIDERS_ROOT.relative_to(AIRFLOW_SOURCES_ROOT_PATH)
+OUT_DIR_PROVIDERS = OUT_DIR / "providers"
 
 COMMON_SQL_PACKAGE_PREFIX = "airflow.providers.common.sql."
 
@@ -337,7 +337,7 @@ if __name__ == "__main__":
         generated_path = OUT_DIR_PROVIDERS / target_path.relative_to(PROVIDERS_ROOT)
         if not generated_path.exists():
             console.print(
-                f"[red]The {target_path} file is missing in generated files:. "
+                f"[red]The {generated_path} file is missing in generated files:. "
                 f"This is treated as breaking change."
             )
             total_removals += 1

tests_common/pytest_plugin.py
@@ -371,21 +371,24 @@ def initialize_airflow_tests(request):
 def pytest_configure(config: pytest.Config) -> None:
     # Ensure that the airflow sources dir is at the end of the sys path if it's not already there. Needed to
     # run import from `providers/tests/`
-    desired = AIRFLOW_SOURCES_ROOT_DIR.as_posix()
-    for path in sys.path:
-        if path == desired:
-            break
-    else:
-        # This "desired" path should be the Airflow source directory (repo root)
-        assert (AIRFLOW_SOURCES_ROOT_DIR / ".asf.yaml").exists(), f"Path {desired} is not Airflow root"
-        sys.path.append(desired)
+    if os.environ.get("USE_AIRFLOW_VERSION") == "":
+        # if USE_AIRFLOW_VERSION is not empty, we are running tests against the installed version of Airflow
+        # and providers so there is no need to add the sources directory to the path
+        desired = AIRFLOW_SOURCES_ROOT_DIR.as_posix()
+        for path in sys.path:
+            if path == desired:
+                break
+        else:
+            # This "desired" path should be the Airflow source directory (repo root)
+            assert (AIRFLOW_SOURCES_ROOT_DIR / ".asf.yaml").exists(), f"Path {desired} is not Airflow root"
+            sys.path.append(desired)
 
-    if (backend := config.getoption("backend", default=None)) and backend not in SUPPORTED_DB_BACKENDS:
-        msg = (
-            f"Provided DB backend {backend!r} not supported, "
-            f"expected one of: {', '.join(map(repr, SUPPORTED_DB_BACKENDS))}"
-        )
-        pytest.exit(msg, returncode=6)
+        if (backend := config.getoption("backend", default=None)) and backend not in SUPPORTED_DB_BACKENDS:
+            msg = (
+                f"Provided DB backend {backend!r} not supported, "
+                f"expected one of: {', '.join(map(repr, SUPPORTED_DB_BACKENDS))}"
+            )
+            pytest.exit(msg, returncode=6)
 
     config.addinivalue_line("markers", "integration(name): mark test to run with named integration")
     config.addinivalue_line("markers", "backend(name): mark test to run with named backend")

providers/tests/openlineage/extractors/test_base.py
@@ -277,7 +277,7 @@ def test_extract_on_failure(task_state, is_airflow_2_10_or_higher, should_call_o
 
 @mock.patch("airflow.providers.openlineage.conf.custom_extractors")
 def test_extractors_env_var(custom_extractors):
-    custom_extractors.return_value = {"providers.tests.openlineage.extractors.test_base.ExampleExtractor"}
+    custom_extractors.return_value = {"tests.openlineage.extractors.test_base.ExampleExtractor"}
     extractor = ExtractorManager().get_extractor_class(ExampleOperator(task_id="example"))
     assert extractor is ExampleExtractor
 