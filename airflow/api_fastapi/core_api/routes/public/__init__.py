f76d3b21f9789db2542da0355cbcf5b077e65e30
Pierre Jeambrun <pierrejbrun@gmail.com>
2024-10-21 15:56:53 +0800

    AIP-84 Delete Pool (#43165)

--- airflow/api_fastapi/core_api/routes/public/__init__.py
+++ airflow/api_fastapi/core_api/routes/public/__init__.py
@@ -22,6 +22,7 @@ from airflow.api_fastapi.core_api.routes.public.connections import connections_r
 from airflow.api_fastapi.core_api.routes.public.dag_run import dag_run_router
 from airflow.api_fastapi.core_api.routes.public.dags import dags_router
 from airflow.api_fastapi.core_api.routes.public.monitor import monitor_router
+from airflow.api_fastapi.core_api.routes.public.pools import pools_router
 from airflow.api_fastapi.core_api.routes.public.variables import variables_router
 
 public_router = AirflowRouter(prefix="/public")
@@ -32,3 +33,4 @@ public_router.include_router(connections_router)
 public_router.include_router(variables_router)
 public_router.include_router(dag_run_router)
 public_router.include_router(monitor_router)
+public_router.include_router(pools_router)
