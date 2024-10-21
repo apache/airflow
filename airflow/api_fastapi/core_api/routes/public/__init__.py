f37f29bc7ed50f9709606e4f6fed66136e8a05c7
Pierre Jeambrun <pierrejbrun@gmail.com>
2024-10-21 17:22:14 +0800

    AIP-84 Get Providers (#43159)

--- airflow/api_fastapi/core_api/routes/public/__init__.py
+++ airflow/api_fastapi/core_api/routes/public/__init__.py
@@ -23,6 +23,7 @@ from airflow.api_fastapi.core_api.routes.public.dag_run import dag_run_router
 from airflow.api_fastapi.core_api.routes.public.dags import dags_router
 from airflow.api_fastapi.core_api.routes.public.monitor import monitor_router
 from airflow.api_fastapi.core_api.routes.public.pools import pools_router
+from airflow.api_fastapi.core_api.routes.public.providers import providers_router
 from airflow.api_fastapi.core_api.routes.public.variables import variables_router
 
 public_router = AirflowRouter(prefix="/public")
@@ -34,3 +35,4 @@ public_router.include_router(variables_router)
 public_router.include_router(dag_run_router)
 public_router.include_router(monitor_router)
 public_router.include_router(pools_router)
+public_router.include_router(providers_router)
--- airflow/ui/openapi-gen/queries/common.ts
+++ airflow/ui/openapi-gen/queries/common.ts
@@ -9,6 +9,7 @@ import {
   DashboardService,
   MonitorService,
   PoolService,
+  ProviderService,
   VariableService,
 } from "../requests/services.gen";
 import { DagRunState } from "../requests/types.gen";
@@ -246,6 +247,24 @@ export const UseMonitorServiceGetHealthKeyFn = (queryKey?: Array<unknown>) => [
   useMonitorServiceGetHealthKey,
   ...(queryKey ?? []),
 ];
+export type ProviderServiceGetProvidersDefaultResponse = Awaited<
+  ReturnType<typeof ProviderService.getProviders>
+>;
+export type ProviderServiceGetProvidersQueryResult<
+  TData = ProviderServiceGetProvidersDefaultResponse,
+  TError = unknown,
+> = UseQueryResult<TData, TError>;
+export const useProviderServiceGetProvidersKey = "ProviderServiceGetProviders";
+export const UseProviderServiceGetProvidersKeyFn = (
+  {
+    limit,
+    offset,
+  }: {
+    limit?: number;
+    offset?: number;
+  } = {},
+  queryKey?: Array<unknown>,
+) => [useProviderServiceGetProvidersKey, ...(queryKey ?? [{ limit, offset }])];
 export type VariableServicePostVariableMutationResult = Awaited<
   ReturnType<typeof VariableService.postVariable>
 >;
