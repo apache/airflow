1e2f92e2c4a93072846ded0c0c52ef0edf788d20
Wei Lee <weilee.rx@gmail.com>
2024-10-22 22:44:16 +0800

    fix up

--- airflow/__init__.py
+++ airflow/__init__.py
@@ -91,7 +91,7 @@ if TYPE_CHECKING:
     # These objects are imported by PEP-562, however, static analyzers and IDE's
     # have no idea about typing of these objects.
     # Add it under TYPE_CHECKING block should help with it.
-    from airflow.models.asset import Asset, Dataset
+    from airflow.assets import Asset, Dataset
     from airflow.models.dag import DAG
     from airflow.models.xcom_arg import XComArg
 
