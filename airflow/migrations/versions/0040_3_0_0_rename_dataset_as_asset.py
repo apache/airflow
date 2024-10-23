d9361118f6830d92969f63723057c085574dc67c
Wei Lee <weilee.rx@gmail.com>
2024-10-23 08:21:45 +0800

    fix(migration): fix dataset to asset migration typo

--- airflow/migrations/versions/0040_3_0_0_rename_dataset_as_asset.py
+++ airflow/migrations/versions/0040_3_0_0_rename_dataset_as_asset.py
@@ -193,7 +193,7 @@ def upgrade():
         )
 
     with op.batch_alter_table("dag_schedule_asset_alias_reference", schema=None) as batch_op:
-        batch_op.drop_constraint("dsdar_dataset_fkey", type_="foreignkey")
+        batch_op.drop_constraint("dsdar_dataset_alias_fkey", type_="foreignkey")
         batch_op.drop_constraint("dsdar_dag_id_fkey", type_="foreignkey")
 
         _rename_pk_constraint(
@@ -512,7 +512,7 @@ def downgrade():
         )
 
         batch_op.create_foreign_key(
-            constraint_name="dsdar_dataset_fkey",
+            constraint_name="dsdar_dataset_alias_fkey",
             referent_table="dataset_alias",
             local_cols=["alias_id"],
             remote_cols=["id"],
