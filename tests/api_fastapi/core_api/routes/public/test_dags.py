5b99f038beada24f3e18a1a47bf3dad0c9c90390
Bugra Ozturk <ozturkbugra93@gmail.com>
2024-10-24 20:50:21 +0200

    Amend update_mask as array

--- tests/api_fastapi/core_api/routes/public/test_dags.py
+++ tests/api_fastapi/core_api/routes/public/test_dags.py
@@ -193,7 +193,7 @@ class TestPatchDag(TestDagEndpoint):
         "query_params, dag_id, body, expected_status_code, expected_is_paused",
         [
             ({}, "fake_dag_id", {"is_paused": True}, 404, None),
-            ({"update_mask": "field_1, is_paused"}, DAG1_ID, {"is_paused": True}, 400, None),
+            ({"update_mask": ["field_1", "is_paused"]}, DAG1_ID, {"is_paused": True}, 400, None),
             ({}, DAG1_ID, {"is_paused": True}, 200, True),
             ({}, DAG1_ID, {"is_paused": False}, 200, False),
             ({"update_mask": ["is_paused"]}, DAG1_ID, {"is_paused": True}, 200, True),
