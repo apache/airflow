5ccfe5c4c7093984e56c79e3ac02f5677ff39af4
Kaxil Naik <kaxilnaik@gmail.com>
2024-11-04 23:24:07 +0000

    Bump `ruff` to `0.7.2` (#43668)
    updates:
    - [github.com/astral-sh/ruff-pre-commit: v0.7.1 → v0.7.2](https://github.com/astral-sh/ruff-pre-commit/compare/v0.7.1...v0.7.2)

--- hatch_build.py
+++ hatch_build.py
@@ -246,7 +246,7 @@ DEVEL_EXTRAS: dict[str, list[str]] = {
     "devel-static-checks": [
         "black>=23.12.0",
         "pre-commit>=3.5.0",
-        "ruff==0.7.1",
+        "ruff==0.7.2",
         "yamllint>=1.33.0",
     ],
     "devel-tests": [
