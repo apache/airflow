969da8d9529f59a64aa7ed27ba82103364192253
Pavan Kumar <gopidesupavan@gmail.com>
2024-11-04 17:48:47 +0000

    fix ruff format

--- providers/tests/standard/test_module_redirect_finder.py
+++ providers/tests/standard/test_module_redirect_finder.py
@@ -25,22 +25,25 @@ from tests_common.test_utils.compat import AIRFLOW_V_3_0_PLUS
 
 @pytest.mark.skipif(not AIRFLOW_V_3_0_PLUS, reason="Valid for Airflow 3.0+ only")
 class TestModuleRedirectFinder:
-    @pytest.mark.parametrize("old_module, new_module", [
-        ("airflow.operators.python", "airflow.providers.standard.operators.python"),
-        ("airflow.operators.bash", "airflow.providers.standard.operators.bash"),
-        ("airflow.operators.datetime", "airflow.providers.standard.operators.datetime"),
-        ("airflow.operators.weekday", "airflow.providers.standard.operators.weekday"),
-        ("airflow.sensors.bash", "airflow.providers.standard.sensors.bash"),
-        ("airflow.sensors.date_time", "airflow.providers.standard.sensors.date_time"),
-        ("airflow.sensors.python", "airflow.providers.standard.sensors.python"),
-        ("airflow.sensors.time", "airflow.providers.standard.sensors.time"),
-        ("airflow.sensors.time_delta", "airflow.providers.standard.sensors.time_delta"),
-        ("airflow.sensors.weekday", "airflow.providers.standard.sensors.weekday"),
-        ("airflow.hooks.filesystem", "airflow.providers.standard.hooks.filesystem"),
-        ("airflow.hooks.package_index", "airflow.providers.standard.hooks.package_index"),
-        ("airflow.hooks.subprocess", "airflow.providers.standard.hooks.subprocess"),
-        ("airflow.utils.python_virtualenv", "airflow.providers.standard.utils.python_virtualenv"),
-    ])
+    @pytest.mark.parametrize(
+        "old_module, new_module",
+        [
+            ("airflow.operators.python", "airflow.providers.standard.operators.python"),
+            ("airflow.operators.bash", "airflow.providers.standard.operators.bash"),
+            ("airflow.operators.datetime", "airflow.providers.standard.operators.datetime"),
+            ("airflow.operators.weekday", "airflow.providers.standard.operators.weekday"),
+            ("airflow.sensors.bash", "airflow.providers.standard.sensors.bash"),
+            ("airflow.sensors.date_time", "airflow.providers.standard.sensors.date_time"),
+            ("airflow.sensors.python", "airflow.providers.standard.sensors.python"),
+            ("airflow.sensors.time", "airflow.providers.standard.sensors.time"),
+            ("airflow.sensors.time_delta", "airflow.providers.standard.sensors.time_delta"),
+            ("airflow.sensors.weekday", "airflow.providers.standard.sensors.weekday"),
+            ("airflow.hooks.filesystem", "airflow.providers.standard.hooks.filesystem"),
+            ("airflow.hooks.package_index", "airflow.providers.standard.hooks.package_index"),
+            ("airflow.hooks.subprocess", "airflow.providers.standard.hooks.subprocess"),
+            ("airflow.utils.python_virtualenv", "airflow.providers.standard.utils.python_virtualenv"),
+        ],
+    )
     def test_module_redirect_finder_for_old_location(self, old_module, new_module):
         try:
             assert importlib.import_module(old_module).__name__ == new_module
