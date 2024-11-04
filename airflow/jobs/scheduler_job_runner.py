11de1e90db0bb056eb77aca072f134e8920177e9
Ephraim Anierobi <splendidzigy24@gmail.com>
2024-11-04 11:05:46 +0100

    use if TYPE_CHECKING

--- airflow/jobs/scheduler_job_runner.py
+++ airflow/jobs/scheduler_job_runner.py
@@ -1751,8 +1751,9 @@ class SchedulerJobRunner(BaseJobRunner, LoggingMixin):
         Return True if we determine that DAG still exists.
         """
         latest_dag_version = DagVersion.get_latest_version(dag_run.dag_id, session=session)
-        latest_dag_version_id = latest_dag_version.id if latest_dag_version else None
-        if dag_run.dag_version_id == latest_dag_version_id:
+        if TYPE_CHECKING:
+            assert latest_dag_version
+        if dag_run.dag_version_id == latest_dag_version.id:
             self.log.debug("DAG %s not changed structure, skipping dagrun.verify_integrity", dag_run.dag_id)
             return True
 
