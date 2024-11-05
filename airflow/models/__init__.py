1116f286eec3c233fe45fe66a3e4515c42501b75
Ephraim Anierobi <splendidzigy24@gmail.com>
2024-11-05 15:19:24 +0100

    AIP-65: Add DAG versioning support (#42913)
    * AIP-65: Add DAG versioning support
    
    This commit introduces versioning for DAGs
    
    Changes:
    - Introduced DagVersion model to handle versioning of DAGs.
    - Added version_name field to DAG for use in tracking the dagversion by users
    - Added support for version retrieval in the get_dag_source API endpoint
    - Modified DAG execution logic to reference dag_version_id instead of the
    dag_hash to ensure DAG runs are linked to specific versions.
    
    Fix tests
    
    revert RESTAPI changes
    
    * fixup! AIP-65: Add DAG versioning support
    
    * fixup! fixup! AIP-65: Add DAG versioning support
    
    * fix migration
    
    * fix test
    
    * more test fixes
    
    * update query count
    
    * fix static checks
    
    * Fix query and add created_at to dag_version table
    
    * improve code
    
    * Change to using UUID for primary keys
    
    * DagCode.bulk_write_code is no longer used
    
    * fixup! Change to using UUID for primary keys
    
    * fix tests
    
    * fixup! fix tests
    
    * use uuid for version_name
    
    * fixup! use uuid for version_name
    
    * use row lock when writing dag version
    
    * use row lock when writing dag version
    
    * fixup! use row lock when writing dag version
    
    * deactivating dag should not remove serialized dags
    
    * save version_name as string not uuid
    
    * Make dag_version_id unique
    
    * fixup! Make dag_version_id unique
    
    * Fix tests
    
    * Use uuid7
    
    * fix test
    
    * fixup! fix test
    
    * use binary=False for uuid field to fix sqlite issue
    
    * apply suggestions from code review
    
    * Remove unnecessary version_name on dagmodel
    
    * Fix sqlalchemy 2 warning
    
    * Fix conflicts
    
    * Apply suggestions from code review
    
    Co-authored-by: Jed Cunningham <66968678+jedcunningham@users.noreply.github.com>
    
    * fixup! Apply suggestions from code review
    
    * fixup! fixup! Apply suggestions from code review
    
    * add test for dagversion model and make version_name, number and dag_id unique
    
    * Remove commented test as serdag can no longer disappear
    
    * Add SQLAlchemy-utils to requirements
    
    * mark test_dag_version.py as db_test
    
    * make version_name nullable
    
    * Apply suggestions from code review
    
    * fixup! Apply suggestions from code review
    
    * remove file_updater
    
    * Use dag_version for creating dagruns instead of dag_version_id
    
    * fix conflicts
    
    * use if TYPE_CHECKING
    
    * Add docstrings to methods
    
    * Move getting latest serdags to SerializedDagModel

--- airflow/models/__init__.py
+++ airflow/models/__init__.py
@@ -57,6 +57,7 @@ def import_all_models():
 
     import airflow.models.asset
     import airflow.models.backfill
+    import airflow.models.dag_version
     import airflow.models.dagwarning
     import airflow.models.errors
     import airflow.models.serialized_dag
