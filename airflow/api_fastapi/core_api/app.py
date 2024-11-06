080ab10ae21c0d236a23defd8624dee37805140d
Karthikeyan Singaravelan <tir.karthi@gmail.com>
2024-11-06 17:59:50 +0530

    Add comment about compression parameters.

--- airflow/api_fastapi/core_api/app.py
+++ airflow/api_fastapi/core_api/app.py
@@ -114,5 +114,9 @@ def init_config(app: FastAPI) -> None:
             allow_headers=allow_headers,
         )
 
-    app.add_middleware(GZipMiddleware, minimum_size=1000, compresslevel=5)
+    # Compress responses greater than 1kB with optimal compression level as 5
+    # with level ranging from 1 to 9 with 1 (fastest, least compression)
+    # and 9 (slowest, most compression)
+    app.add_middleware(GZipMiddleware, minimum_size=1024, compresslevel=5)
+
     app.state.secret_key = conf.get("webserver", "secret_key")
