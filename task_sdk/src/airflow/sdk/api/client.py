a81f69c344edd9ccf25f669062f3bb31620b1373
Ash Berlin-Taylor <ash@apache.org>
2024-11-07 22:40:04 +0000

    Start building the replacement task runner for Task Execution SDK
    The eventual goal if this "airflow.sdk.exeuction_time" package is to replace
    LocalTaskJob and StandardTaskRunner, but at this stage it co-exists with it's
    replacement.
    
    As this PR is not a complete re-implementation of all the features that exist
    currently (no handling of task level callbacks yet, no AirflowSkipException
    etc.) the current tests are skeleton at best. Once we get closer to feature
    parity (in future PRs) the tests will grow to match.
    
    This supervisor and task runner operates slightly differently to the current
    classes in these ways
    
    **Logs from the subprocess are send over a different channel to stdout/stderr**
    
    This makes the task supervisor a little bit more complex as it now has to
    read stdout, stderr and a logs channel. The advantage of this approach is
    that it makes the logs setup in the task process itself markedly simpler --
    all it has to do is write logs output to the custom file handle as JSON and
    it will show up "natively" as logs.
    
    structlog has been chosen as the logging engine over stdlib's own logging as
    the ability to have structured fields in the logs is nice, and stdlib is
    configured to send logs to a stuctlog processor.
    
    **Direct database access is replaced with an HTTP API client**
    
    This is the crux of this feature and of AIP-72 in general -- tasks run via
    this runner can no longer access DB models or DB session directly. This PR
    doesn't yet implement the code/shims to make `Connection.get_connection_from_secrets`
    use this client yet - that will be future work.
    
    The reason tasks don't speak directly to the API server is primarily for two
    reasons:
    
    1. The supervisor process already needs to maintain an http session in order
       to report the task as started, to heart beat it, and to mark it as
       finished; and so because of that
    2. Reduce the number of active HTTP connections for tasks to 1 per task
       (instead of 2 per task).
    
    THe other reason we have this interface is that DAG parsing code will very
    soon need to be updated to not have direct DB access either, and having this
    "in process" interface ability already means that we can support commands like
    `airflow dags reserialize` without having a running API server.
    
    The API client itself is not auto-generated: I tried a number of different
    client generates based on the OpenAPI spec and found them all lacking or buggy
    in different ways, and the http client side itself is very simple, the only
    interesting/difficult bit is the generation of the datamodels from the OpenAPI
    spec which I found one that
    
    msgspec was chosen over Pydantic as it is much lighter weight (and thus
    quicker), especially on a client side when we have next to no validation
    requirements of response data. I admit that I have not benchmarked it
    specifically though.

---
+++ task_sdk/src/airflow/sdk/api/client.py
@@ -0,0 +1,215 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from __future__ import annotations
+
+import uuid
+from typing import TYPE_CHECKING, Any
+
+import httpx
+import methodtools
+import msgspec
+import structlog
+from uuid6 import uuid7
+
+from airflow.sdk.api.datamodels._generated import (
+    ConnectionResponse,
+    State1 as TerminalState,
+    TaskInstanceState,
+    TIEnterRunningPayload,
+    TITerminalStatePayload,
+    ValidationError as RemoteValidationError,
+)
+from airflow.utils.net import get_hostname
+from airflow.utils.platform import getuser
+
+if TYPE_CHECKING:
+    from datetime import datetime
+
+
+log = structlog.get_logger(logger_name=__name__)
+
+__all__ = [
+    "Client",
+    "ConnectionOperations",
+    "ErrorBody",
+    "ServerResponseError",
+    "TaskInstanceOperations",
+]
+
+
+def get_json_error(response: httpx.Response):
+    """Raise a ServerResponseError if we can extract error info from the error."""
+    err = ServerResponseError.from_response(response)
+    if err:
+        log.warning("Server error", detail=err.detail)
+        raise err
+
+
+def raise_on_4xx_5xx(response: httpx.Response):
+    return get_json_error(response) or response.raise_for_status()
+
+
+# Py 3.11+ version
+def raise_on_4xx_5xx_with_note(response: httpx.Response):
+    try:
+        return get_json_error(response) or response.raise_for_status()
+    except httpx.HTTPStatusError as e:
+        if TYPE_CHECKING:
+            assert hasattr(e, "add_note")
+        e.add_note(
+            f"Correlation-id={response.headers.get('correlation-id', None) or response.request.headers.get('correlation-id', 'no-correlction-id')}"
+        )
+        raise
+
+
+if hasattr(BaseException, "add_note"):
+    # Py 3.11+
+    raise_on_4xx_5xx = raise_on_4xx_5xx_with_note
+
+
+def add_correlation_id(request: httpx.Request):
+    request.headers["correlation-id"] = str(uuid7())
+
+
+class TaskInstanceOperations:
+    __slots__ = ("client",)
+
+    def __init__(self, client: Client):
+        self.client = client
+
+    def start(self, id: uuid.UUID, pid: int, when: datetime):
+        """Tell the API server that this TI has started running."""
+        body = TIEnterRunningPayload(pid=pid, hostname=get_hostname(), unixname=getuser(), start_date=when)
+
+        self.client.patch(f"task_instance/{id}/state", content=self.client.encoder.encode(body))
+
+    def finish(self, id: uuid.UUID, state: TaskInstanceState, when: datetime):
+        """Tell the API server that this TI has reached a terminal state."""
+        body = TITerminalStatePayload(end_date=when, state=TerminalState(state))
+
+        self.client.patch(f"task_instance/{id}/state", content=self.client.encoder.encode(body))
+
+    def heartbeat(self, id: uuid.UUID):
+        self.client.put(f"task_instance/{id}/heartbeat")
+
+
+class ConnectionOperations:
+    __slots__ = ("client", "decoder")
+
+    def __init__(self, client: Client):
+        self.client = client
+        self.decoder: msgspec.json.Decoder[ConnectionResponse] = msgspec.json.Decoder(type=ConnectionResponse)
+
+    def get(self, id: str) -> ConnectionResponse:
+        """Get a connection from the API server."""
+        resp = self.client.get(f"connection/{id}")
+        return self.decoder.decode(resp.read())
+
+
+class BearerAuth(httpx.Auth):
+    def __init__(self, token: str):
+        self.token: str = token
+
+    def auth_flow(self, request: httpx.Request):
+        if self.token:
+            request.headers["Authorization"] = "Bearer " + self.token
+        yield request
+
+
+def noop_handler(request: httpx.Request) -> httpx.Response:
+    log.debug("Dry-run request", method=request.method, path=request.url.path)
+    return httpx.Response(200, json={"text": "Hello, world!"})
+
+
+class Client(httpx.Client):
+    encoder: msgspec.json.Encoder
+
+    def __init__(self, *, base_url: str | None, dry_run: bool = False, token: str, **kwargs: Any):
+        if (not base_url) ^ dry_run:
+            raise ValueError(f"Can only specify one of {base_url=} or {dry_run=}")
+        auth = BearerAuth(token)
+
+        self.encoder = msgspec.json.Encoder()
+        if dry_run:
+            # If dry run is requests, install a no op handler so that simple tasks can "heartbeat" using a
+            # real client, but just don't make any HTTP requests
+            kwargs["transport"] = httpx.MockTransport(noop_handler)
+            kwargs["base_url"] = "dry-run://server"
+        else:
+            kwargs["base_url"] = base_url
+        super().__init__(
+            auth=auth,
+            headers={"airflow-api-version": "2024-07-30"},
+            event_hooks={"response": [raise_on_4xx_5xx], "request": [add_correlation_id]},
+            **kwargs,
+        )
+
+    # We "group" or "namespace" operations by what they operate on, rather than a flat namespace with all
+    # methods on one object prefixed with the object type (`.task_instances.update` rather than
+    # `task_instance_update` etc.)
+
+    @methodtools.lru_cache()  # type: ignore[misc]
+    @property
+    def task_instances(self) -> TaskInstanceOperations:
+        """Operations related to TaskInstances."""
+        return TaskInstanceOperations(self)
+
+    @methodtools.lru_cache()  # type: ignore[misc]
+    @property
+    def connections(self) -> ConnectionOperations:
+        """Operations related to TaskInstances."""
+        return ConnectionOperations(self)
+
+
+class ErrorBody(msgspec.Struct):
+    detail: list[RemoteValidationError] | dict[str, Any]
+
+    def __repr__(self):
+        return repr(self.detail)
+
+
+class ServerResponseError(httpx.HTTPStatusError):
+    def __init__(self, message: str, *, request: httpx.Request, response: httpx.Response):
+        super().__init__(message, request=request, response=response)
+
+    detail: ErrorBody
+
+    @classmethod
+    def from_response(cls, response: httpx.Response) -> ServerResponseError | None:
+        if response.is_success:
+            return None
+        # 4xx or 5xx error?
+        if 400 < (response.status_code // 100) >= 600:
+            return None
+
+        if response.headers.get("content-type") != "application/json":
+            return None
+
+        try:
+            err = msgspec.json.decode(response.read(), type=ErrorBody)
+            if isinstance(err.detail, list):
+                msg = "Remote server returned validation error"
+            else:
+                msg = err.detail.get("message", "") or "Un-parseable error"
+        except Exception:
+            err = msgspec.json.decode(response.content)
+            msg = "Server returned error"
+
+        self = cls(msg, request=response.request, response=response)
+        self.detail = err
+        return self
---
+++ task_sdk/src/airflow/sdk/execution_time/comms.py
@@ -0,0 +1,103 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+r"""
+Communication protocol between the Supervisor and the task process
+==================================================================
+
+* All communication is done over stdout/stdin in the form of "JSON lines" (each
+  message is a single JSON document terminated by `\n` character)
+* Messages from the subprocess are all log messages and are sent directly to the log
+* No messages are sent to task process except in response to a request. (This is because the task process will
+  be running user's code, so we can't read from stdin until we enter our code, such as when requesting an XCom
+  value etc.)
+
+The reason this communication protocol exists, rather than the task process speaking directly to the Task
+Execution API server is because:
+
+1. To reduce the number of concurrent HTTP connections on the API server.
+
+   The supervisor already has to speak to that to heartbeat the running Task, so having the task speak to its
+   parent process and having all API traffic go through that means that the number of HTTP connections is
+   "halved". (Not every task will make API calls, so it's not always halved, but it is reduced.)
+
+2. This means that the user Task code doesn't ever directly see the task identity JWT token.
+
+   This is a short lived token tied to one specific task instance try, so it being leaked/exfiltrated is not a
+   large risk, but it's easy to not give it to the user code, so lets do that.
+"""  # noqa: D400, D205
+
+from __future__ import annotations
+
+from typing import Any, Union
+
+import msgspec
+
+from airflow.sdk.api.datamodels._generated import TaskInstanceState  # noqa: TCH001
+from airflow.sdk.api.datamodels.ti import TaskInstance  # noqa: TCH001
+
+
+class StartupDetails(msgspec.Struct, omit_defaults=True, tag=True):
+    ti: TaskInstance
+    file: str
+    requests_fd: int
+    """
+    The channel for the task to send requests over.
+
+    Responses will come back on stdin
+    """
+
+
+class XComResponse(msgspec.Struct, omit_defaults=True, tag=True):
+    """Response to ReadXCom request."""
+
+    key: str
+    value: Any
+
+
+class ConnectionResponse(msgspec.Struct, omit_defaults=True, tag="connection"):
+    conn: Any
+
+
+ToTask = Union[StartupDetails, XComResponse, ConnectionResponse]
+
+
+class TaskState(msgspec.Struct, omit_defaults=True, tag=True):
+    """
+    Update a task's state.
+
+    If a process exits without sending one of these the state will be derived from the exit code:
+    - 0 = SUCCESS
+    - anything else = FAILED
+    """
+
+    state: TaskInstanceState
+
+
+class ReadXCom(msgspec.Struct, tag=True):
+    key: str
+
+
+class GetConnection(msgspec.Struct, tag=True):
+    id: str
+
+
+class GetVariable(msgspec.Struct, tag=True):
+    id: str
+
+
+ToSupervisor = Union[TaskState, ReadXCom, GetConnection, GetVariable]
