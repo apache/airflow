4a9a1f59d2ec4315c30299f4e8591020a778966c
yangyulely <yule.yang@samsung.com>
2024-11-13 20:22:04 +0800

    Add missing test for apache cassandra hooks (#43951)

--- tests/always/test_project_structure.py
+++ tests/always/test_project_structure.py
@@ -75,7 +75,6 @@ class TestProjectStructure:
             "providers/tests/amazon/aws/utils/test_rds.py",
             "providers/tests/amazon/aws/utils/test_sagemaker.py",
             "providers/tests/amazon/aws/waiters/test_base_waiter.py",
-            "providers/tests/apache/cassandra/hooks/test_cassandra.py",
             "providers/tests/apache/drill/operators/test_drill.py",
             "providers/tests/apache/druid/operators/test_druid_check.py",
             "providers/tests/apache/hdfs/hooks/test_hdfs.py",
