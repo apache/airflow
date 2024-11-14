2d1b0ae51929eb4cd71b963ec4377a6aa9441ff2
Amogh <amoghrajesh1999@gmail.com>
2024-11-14 14:41:05 +0530

    adding unit tests

--- tests/api_fastapi/core_api/routes/public/test_dags.py
+++ tests/api_fastapi/core_api/routes/public/test_dags.py
@@ -627,3 +627,38 @@ class TestGetDagAssetQueuedEvents(TestQueuedEventEndpoint):
 
         assert response.status_code == 404
         assert response.json()["detail"] == "Queue event with dag_id: `not_exists` was not found"
+
+
+class TestGetDagAssetQueuedEvent(TestQueuedEventEndpoint):
+    @pytest.mark.usefixtures("time_freezer")
+    def test_should_respond_200(self, test_client, session, create_dummy_dag):
+        dag, _ = create_dummy_dag()
+        dag_id = dag.dag_id
+        asset_id = self._create_asset(session).id
+        self._create_asset_dag_run_queues(dag_id, asset_id, session)
+        asset_uri = "s3://bucket/key"
+
+        response = test_client.get(
+            f"/public/dags/{dag_id}/assets/queuedEvent/{asset_uri}",
+        )
+
+        assert response.status_code == 200
+        assert response.json() == {
+            "created_at": self.default_time.replace("+00:00", "Z"),
+            "uri": "s3://bucket/key",
+            "dag_id": "dag",
+        }
+
+    def test_should_respond_404(self, test_client):
+        dag_id = "not_exists"
+        asset_uri = "not_exists"
+
+        response = test_client.get(
+            f"/public/dags/{dag_id}/assets/queuedEvent/{asset_uri}",
+        )
+
+        assert response.status_code == 404
+        assert (
+            response.json()["detail"]
+            == "Queue event with dag_id: `not_exists` and asset uri: `not_exists` was not found"
+        )
