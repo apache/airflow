a42cf9cf7f2ba753ad94f05559c3f7162131734d
Jarek Potiuk <jarek@potiuk.com>
2024-11-16 06:08:07 +0000

    Revert "AIP-84: Migrating GET queued asset events for DAG to fastAPI (#43934)"
    This reverts commit 391773018747b37cfa5dab0dcfc8b22d763e38e6.

--- airflow/ui/openapi-gen/requests/types.gen.ts
+++ airflow/ui/openapi-gen/requests/types.gen.ts
@@ -713,23 +713,6 @@ export type ProviderResponse = {
 };
 
 /**
- * Queued Event Collection serializer for responses.
- */
-export type QueuedEventCollectionResponse = {
-  queued_events: Array<QueuedEventResponse>;
-  total_entries: number;
-};
-
-/**
- * Queued Event serializer for responses..
- */
-export type QueuedEventResponse = {
-  uri: string;
-  dag_id: string;
-  created_at: string;
-};
-
-/**
  * Internal enum for setting reprocess behavior in a backfill.
  *
  * :meta private:
@@ -1061,13 +1044,6 @@ export type GetAssetData = {
 };
 
 export type GetAssetResponse = AssetResponse;
-
-export type GetDagAssetQueuedEventsData = {
-  before?: string | null;
-  dagId: string;
-};
-
-export type GetDagAssetQueuedEventsResponse = QueuedEventCollectionResponse;
 
 export type HistoricalMetricsData = {
   endDate: string;
@@ -1559,7 +1535,7 @@ export type $OpenApiTs = {
       };
     };
   };
-  "/public/assets": {
+  "/public/assets/": {
     get: {
       req: GetAssetsData;
       res: {
@@ -1612,8 +1588,6 @@ export type $OpenApiTs = {
         422: HTTPValidationError;
       };
     };
-  };
-  "/public/events": {
     post: {
       req: CreateAssetEventData;
       res: {
@@ -1648,33 +1622,6 @@ export type $OpenApiTs = {
          * Successful Response
          */
         200: AssetResponse;
-        /**
-         * Unauthorized
-         */
-        401: HTTPExceptionResponse;
-        /**
-         * Forbidden
-         */
-        403: HTTPExceptionResponse;
-        /**
-         * Not Found
-         */
-        404: HTTPExceptionResponse;
-        /**
-         * Validation Error
-         */
-        422: HTTPValidationError;
-      };
-    };
-  };
-  "/public/dags/{dag_id}/assets/queuedEvent": {
-    get: {
-      req: GetDagAssetQueuedEventsData;
-      res: {
-        /**
-         * Successful Response
-         */
-        200: QueuedEventCollectionResponse;
         /**
          * Unauthorized
          */
