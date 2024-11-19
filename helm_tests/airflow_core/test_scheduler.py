265e3c4eddfd5deed3ea68d4a069a6ed6ebe22d2
romsharon98 <rom.sharon98@gmail.com>
2024-11-19 08:52:08 +0200

    remove scheduler automate serviceaccount token

--- helm_tests/airflow_core/test_scheduler.py
+++ helm_tests/airflow_core/test_scheduler.py
@@ -988,28 +988,6 @@ class TestSchedulerServiceAccount:
         assert "test_label" in jmespath.search("metadata.labels", docs[0])
         assert jmespath.search("metadata.labels", docs[0])["test_label"] == "test_label_value"
 
-    def test_default_automount_service_account_token(self):
-        docs = render_chart(
-            values={
-                "scheduler": {
-                    "serviceAccount": {"create": True},
-                },
-            },
-            show_only=["templates/scheduler/scheduler-serviceaccount.yaml"],
-        )
-        assert jmespath.search("automountServiceAccountToken", docs[0]) is True
-
-    def test_overridden_automount_service_account_token(self):
-        docs = render_chart(
-            values={
-                "scheduler": {
-                    "serviceAccount": {"create": True, "automountServiceAccountToken": False},
-                },
-            },
-            show_only=["templates/scheduler/scheduler-serviceaccount.yaml"],
-        )
-        assert jmespath.search("automountServiceAccountToken", docs[0]) is False
-
 
 class TestSchedulerCreation:
     """Tests scheduler deployment creation."""
