15cbd0a4a6cb76082b9d435befce9dba378cba54
Amogh <amoghrajesh1999@gmail.com>
2024-11-21 09:52:02 +0530

    Fixing static check due to faulty entry in openapi spec

--- airflow/ui/openapi-gen/requests/schemas.gen.ts
+++ airflow/ui/openapi-gen/requests/schemas.gen.ts
@@ -4290,7 +4290,7 @@ export const $TimeDelta = {
   properties: {
     __type: {
       type: "string",
-      title: "  Type",
+      title: "Type",
       default: "TimeDelta",
     },
     days: {
