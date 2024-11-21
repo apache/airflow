655d5ed96b9612940d281aa9207f65dbc7725186
David Blain <david.blain@infrabel.be>
2024-11-21 10:41:38 +0100

    fix: AttributeError: '_thread._local' object has no attribute 'callers' in ExecutorSafeguard when executed in multithreaded environment

--- airflow/models/baseoperator.py
+++ airflow/models/baseoperator.py
@@ -352,7 +352,13 @@ class ExecutorSafeguard:
 
     test_mode = conf.getboolean("core", "unit_test_mode")
     _sentinel = local()
-    _sentinel.callers = {}
+
+    @classmethod
+    def get_callers(cls):
+        # Ensure _sentinel.callers exists for the current thread
+        if not hasattr(cls._sentinel, "callers"):
+            cls._sentinel.callers = {}
+        return cls._sentinel.callers
 
     @classmethod
     def decorator(cls, func):
@@ -362,13 +368,18 @@ class ExecutorSafeguard:
 
             sentinel_key = f"{self.__class__.__name__}__sentinel"
             sentinel = kwargs.pop(sentinel_key, None)
+            callers = cls.get_callers()
 
             if sentinel:
-                cls._sentinel.callers[sentinel_key] = sentinel
+                callers[sentinel_key] = sentinel
             else:
-                sentinel = cls._sentinel.callers.pop(f"{func.__qualname__.split('.')[0]}__sentinel", None)
+                sentinel = callers.pop(f"{func.__qualname__.split('.')[0]}__sentinel", None)
 
-            if not cls.test_mode and not sentinel == _sentinel and not isinstance(self, DecoratedOperator):
+            if (
+                not cls.test_mode
+                and not sentinel == _sentinel
+                and not isinstance(self, DecoratedOperator)
+            ):
                 message = f"{self.__class__.__name__}.{func.__name__} cannot be called outside TaskInstance!"
                 if not self.allow_nested_operators:
                     raise AirflowException(message)
