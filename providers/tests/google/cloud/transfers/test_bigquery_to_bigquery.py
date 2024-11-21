a82122b846151249d46edf3e505963fc6cbcd207
Salome Papiashvili <spapiashvili@google.com>
2024-11-21 14:00:19 +0100

    add unit test for get_openlineage_facets_on_complete

--- providers/tests/google/cloud/transfers/test_bigquery_to_bigquery.py
+++ providers/tests/google/cloud/transfers/test_bigquery_to_bigquery.py
@@ -20,6 +20,7 @@ from __future__ import annotations
 from unittest import mock
 
 from airflow.providers.google.cloud.transfers.bigquery_to_bigquery import BigQueryToBigQueryOperator
+from google.cloud.bigquery.table import Table
 
 BQ_HOOK_PATH = "airflow.providers.google.cloud.transfers.bigquery_to_bigquery.BigQueryHook"
 TASK_ID = "test-bq-create-table-operator"
@@ -48,6 +49,26 @@ def split_tablename_side_effect(*args, **kwargs):
             TEST_DATASET + "_new",
             TEST_TABLE_ID,
         )
+
+
+def get_table_side_effect(*args, **kwargs):
+    dataset_id = ""
+    if args[0] == SOURCE_PROJECT_DATASET_TABLES:
+        dataset_id = TEST_DATASET
+    elif args[0] == DESTINATION_PROJECT_DATASET_TABLE:
+        dataset_id = TEST_DATASET + '_new'
+    return Table.from_api_repr(
+    {
+        "tableReference": {"projectId": TEST_GCP_PROJECT_ID, "datasetId": dataset_id, "tableId": TEST_TABLE_ID},
+        "description": "table description",
+        "schema": {
+            "fields": [
+                {"name": "field1", "type": "STRING", "description": "field1 description"},
+                {"name": "field2", "type": "INTEGER", "description": "field2 description"},
+            ]
+        },
+    }
+)
 
 
 class TestBigQueryToBigQueryOperator:
@@ -110,3 +131,23 @@ class TestBigQueryToBigQueryOperator:
             job_id=mock_hook.return_value.insert_job.return_value.job_id,
             location=location,
         )
+
+    @mock.patch(BQ_HOOK_PATH)
+    def test_get_openlineage_facets_on_complete(self, mock_hook):
+        operator = BigQueryToBigQueryOperator(
+            task_id=TASK_ID,
+            source_project_dataset_tables=SOURCE_PROJECT_DATASET_TABLES,
+            destination_project_dataset_table=DESTINATION_PROJECT_DATASET_TABLE,
+        )
+        mock_hook.return_value.split_tablename.side_effect = split_tablename_side_effect
+        mock_hook.return_value.get_client.return_value.get_table.side_effect = get_table_side_effect
+
+        operator.execute(context=mock.MagicMock())
+
+        lineage = operator.get_openlineage_facets_on_complete(None)
+        assert len(lineage.inputs) == 1
+        assert lineage.inputs[0].name == SOURCE_PROJECT_DATASET_TABLES
+        assert lineage.inputs[0].namespace == "bigquery"
+        assert len(lineage.outputs) == 1
+        assert lineage.outputs[0].name == DESTINATION_PROJECT_DATASET_TABLE
+        assert lineage.outputs[0].namespace == "bigquery"
