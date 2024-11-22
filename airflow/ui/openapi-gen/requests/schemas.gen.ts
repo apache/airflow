ad4763756947781c02d4814f8794722ca737ae20
Jarek Potiuk <jarek@potiuk.com>
2024-11-22 09:14:13 +0000

    Lower-bind pydantic to 2.10.1
    The new pydantic 2.10 release produces different output for the
    OpenAPI specification for the same classes. In order to stabilize
    it, we should make sure Pydantic is not less than 2.10.1.

--- airflow/ui/openapi-gen/requests/schemas.gen.ts
+++ airflow/ui/openapi-gen/requests/schemas.gen.ts
@@ -4307,7 +4307,7 @@ export const $TimeDelta = {
   properties: {
     __type: {
       type: "string",
-      title: "  Type",
+      title: "Type",
       default: "TimeDelta",
     },
     days: {
