f0e262e14f1815dfb8cfbdd41ef614ffb7a34427
Daniel Standish <15932138+dstandish@users.noreply.github.com>
2024-11-22 07:18:16 -0800

    fix tests

--- airflow/api_fastapi/core_api/datamodels/event_logs.py
+++ airflow/api_fastapi/core_api/datamodels/event_logs.py
@@ -19,13 +19,15 @@ from __future__ import annotations
 
 from datetime import datetime
 
-from pydantic import Field
+from pydantic import ConfigDict, Field
 
 from airflow.api_fastapi.core_api.base import BaseModel
 
 
 class EventLogResponse(BaseModel):
     """Event Log Response."""
+
+    model_config = ConfigDict(populate_by_name=True, from_attributes=True)
 
     id: int = Field(alias="event_log_id")
     dttm: datetime = Field(alias="when")
--- airflow/api_fastapi/core_api/datamodels/import_error.py
+++ airflow/api_fastapi/core_api/datamodels/import_error.py
@@ -18,13 +18,15 @@ from __future__ import annotations
 
 from datetime import datetime
 
-from pydantic import Field
+from pydantic import ConfigDict, Field
 
 from airflow.api_fastapi.core_api.base import BaseModel
 
 
 class ImportErrorResponse(BaseModel):
     """Import Error Response."""
+
+    model_config = ConfigDict(populate_by_name=True, from_attributes=True)
 
     id: int = Field(alias="import_error_id")
     timestamp: datetime
