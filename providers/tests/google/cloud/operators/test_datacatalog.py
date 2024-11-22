426d5fe8db0beb92491cf4a51794dd71f35ffec4
GPK <gopidesupavan@gmail.com>
2024-11-22 09:58:23 +0000

    fix google datacatalog operator tests (#44281)
    * fix google datacatalog operator tests
    
    * bump google-cloud-datacatalog

--- providers/tests/google/cloud/operators/test_datacatalog.py
+++ providers/tests/google/cloud/operators/test_datacatalog.py
@@ -102,9 +102,20 @@ TEST_ENTRY_DICT: dict = {
     "name": TEST_ENTRY_PATH,
 }
 TEST_ENTRY_GROUP: EntryGroup = EntryGroup(name=TEST_ENTRY_GROUP_PATH)
-TEST_ENTRY_GROUP_DICT: dict = {"description": "", "display_name": "", "name": TEST_ENTRY_GROUP_PATH}
+TEST_ENTRY_GROUP_DICT: dict = {
+    "description": "",
+    "display_name": "",
+    "name": TEST_ENTRY_GROUP_PATH,
+    "transferred_to_dataplex": False,
+}
 TEST_TAG: Tag = Tag(name=TEST_TAG_PATH)
-TEST_TAG_DICT: dict = {"fields": {}, "name": TEST_TAG_PATH, "template": "", "template_display_name": ""}
+TEST_TAG_DICT: dict = {
+    "fields": {},
+    "name": TEST_TAG_PATH,
+    "template": "",
+    "template_display_name": "",
+    "dataplex_transfer_status": 0,
+}
 TEST_TAG_TEMPLATE: TagTemplate = TagTemplate(name=TEST_TAG_TEMPLATE_PATH)
 TEST_TAG_TEMPLATE_DICT: dict = {
     "display_name": "",
@@ -172,6 +183,7 @@ class TestCloudDataCatalogCreateEntryOperator:
                 "project_id": TEST_PROJECT_ID,
             },
         )
+
         assert TEST_ENTRY_DICT == result
 
     @mock.patch("airflow.providers.google.cloud.operators.datacatalog.CloudDataCatalogHook")
