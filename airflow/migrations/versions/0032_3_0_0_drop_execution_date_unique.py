4c6e9837c5e75c5fc43502c7f449df942524bdcc
Ephraim Anierobi <splendidzigy24@gmail.com>
2024-11-22 13:37:02 +0100

    fixup! fix for sqlite

--- airflow/migrations/versions/0032_3_0_0_drop_execution_date_unique.py
+++ airflow/migrations/versions/0032_3_0_0_drop_execution_date_unique.py
@@ -31,8 +31,9 @@ Create Date: 2024-08-28 08:35:26.634475
 
 from __future__ import annotations
 
-import sqlalchemy as sa
 from alembic import op
+
+from airflow.utils.sqlalchemy import UtcDateTime
 
 # revision identifiers, used by Alembic.
 revision = "1cdc775ca98f"
@@ -47,7 +48,7 @@ def upgrade():
         batch_op.alter_column(
             "execution_date",
             new_column_name="logical_date",
-            existing_type=sa.TIMESTAMP,
+            existing_type=UtcDateTime,
             existing_nullable=False,
         )
     with op.batch_alter_table("dag_run", schema=None) as batch_op:
@@ -59,7 +60,7 @@ def downgrade():
         batch_op.alter_column(
             "logical_date",
             new_column_name="execution_date",
-            existing_type=sa.TIMESTAMP,
+            existing_type=UtcDateTime,
             existing_nullable=False,
         )
     with op.batch_alter_table("dag_run", schema=None) as batch_op:
