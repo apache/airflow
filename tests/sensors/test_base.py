5f79752d80ef5f956622aeccec78b2023277438e
Daniel Standish <15932138+dstandish@users.noreply.github.com>
2024-11-25 10:53:26 -0800

    fix test

--- tests/sensors/test_base.py
+++ tests/sensors/test_base.py
@@ -45,7 +45,7 @@ from airflow.executors.executor_constants import (
 from airflow.executors.local_executor import LocalExecutor
 from airflow.executors.sequential_executor import SequentialExecutor
 from airflow.models import TaskInstance, TaskReschedule
-from airflow.models.baseoperator import TRIGGER_TIMEOUT_REPR
+from airflow.models.trigger import TriggerFailureReason
 from airflow.models.xcom import XCom
 from airflow.operators.empty import EmptyOperator
 from airflow.providers.celery.executors.celery_executor import CeleryExecutor
@@ -1067,7 +1067,7 @@ class TestBaseSensor:
         with pytest.raises(AirflowSensorTimeout):
             op.resume_execution(
                 next_method="__fail__",
-                next_kwargs={"error": TRIGGER_TIMEOUT_REPR},
+                next_kwargs={"error": TriggerFailureReason.TRIGGER_TIMEOUT},
                 context={},
             )
 
