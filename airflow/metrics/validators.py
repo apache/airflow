f9984670a9d5b4c8f64fe77b466c88a1e0169b7f
Arshia Zakeri Rad <zrr.arshia@gmail.com>
2024-11-26 14:14:29 -0500

    Removed BACK_COMPAT_METRIC_NAMES as it's not beeing used anymore

--- airflow/metrics/validators.py
+++ airflow/metrics/validators.py
@@ -24,7 +24,7 @@ import logging
 import string
 import warnings
 from functools import partial, wraps
-from typing import Callable, Iterable, Pattern, cast
+from typing import Callable, Iterable, cast
 
 import re2
 
@@ -80,7 +80,6 @@ BACK_COMPAT_METRIC_NAME_PATTERNS: set[str] = {
     r"^dagrun\.schedule_delay\.(?P<dag_id>.*)$",
     r"^dagrun\.(?P<dag_id>.*)\.first_task_scheduling_delay$",
 }
-BACK_COMPAT_METRIC_NAMES: set[Pattern[str]] = {re2.compile(name) for name in BACK_COMPAT_METRIC_NAME_PATTERNS}
 
 OTEL_NAME_MAX_LENGTH = 63
 DEFAULT_VALIDATOR_TYPE = "allow"
