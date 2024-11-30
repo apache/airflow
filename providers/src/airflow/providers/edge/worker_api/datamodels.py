4080945092766656f54f6f312325bfc9d58922e0
Jens Scheffler <jens.scheffler@de.bosch.com>
2024-11-30 15:50:59 +0100

    Migrate Edge calls for Worker to FastAPI part 1 - Worker routes

--- providers/src/airflow/providers/edge/worker_api/datamodels.py
+++ providers/src/airflow/providers/edge/worker_api/datamodels.py
@@ -16,17 +16,73 @@
 # under the License.
 from __future__ import annotations
 
-from typing import Any, Optional
+from typing import (  # noqa: UP035 - prevent pytest failing in back-compat
+    Annotated,
+    Any,
+    Dict,
+    List,
+    Optional,
+    Union,
+)
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
+from airflow.providers.edge.models.edge_worker import EdgeWorkerState  # noqa: TCH001
 
 
-class JsonRpcRequest(BaseModel):
+class JsonRpcRequestBase(BaseModel):
+    """Base JSON RPC request model to define just the method."""
+
+    method: Annotated[
+        str,
+        Field(description="Fully qualified python module method name that is called via JSON RPC."),
+    ]
+
+
+class JsonRpcRequest(JsonRpcRequestBase):
     """JSON RPC request model."""
 
-    method: str
-    """Fully qualified python module method name that is called via JSON RPC."""
-    jsonrpc: str
-    """JSON RPC version."""
-    params: Optional[dict[str, Any]] = None  # noqa: UP007 - prevent pytest failing in back-compat
-    """Parameters passed to the method."""
+    jsonrpc: Annotated[str, Field(description="JSON RPC Version", examples=["2.0"])]
+    params: Annotated[
+        Optional[Dict[str, Any]],  # noqa: UP006, UP007 - prevent pytest failing in back-compat
+        Field(description="Dictionary of parameters passed to the method."),
+    ]
+
+
+class WorkerStateBody(BaseModel):
+    """Details of the worker state sent to the scheduler."""
+
+    state: Annotated[EdgeWorkerState, Field(description="State of the worker from the view of the worker.")]
+    jobs_active: Annotated[int, Field(description="Number of active jobs the worker is running.")] = 0
+    queues: Annotated[
+        Optional[List[str]],  # noqa: UP006, UP007 - prevent pytest failing in back-compat
+        Field(
+            description="List of queues the worker is pulling jobs from. If not provided, worker pulls from all queues."
+        ),
+    ] = None
+    sysinfo: Annotated[
+        Dict[str, Union[str, int]],  # noqa: UP006, UP007 - prevent pytest failing in back-compat
+        Field(
+            description="System information of the worker.",
+            examples=[
+                {
+                    "concurrency": 4,
+                    "airflow_version": "2.0.0",
+                    "edge_provider_version": "1.0.0",
+                }
+            ],
+        ),
+    ]
+
+
+class WorkerQueueUpdateBody(BaseModel):
+    """Changed queues for the worker."""
+
+    new_queues: Annotated[
+        Optional[List[str]],  # noqa: UP006, UP007 - prevent pytest failing in back-compat
+        Field(description="Additional queues to be added to worker."),
+    ]
+    remove_queues: Annotated[
+        Optional[List[str]],  # noqa: UP006, UP007 - prevent pytest failing in back-compat
+        Field(description="Queues to remove from worker."),
+    ]
