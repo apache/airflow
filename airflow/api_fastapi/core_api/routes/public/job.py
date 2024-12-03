31ba41e0244d2507832021fde09923db007bebb9
LIU ZHE YOU <68415893+jason810496@users.noreply.github.com>
2024-12-04 00:55:19 +0800

    AIP-84 Refactor Filter Query Parameters (#43947)
    * Add FilterParam and type_filter_param_factory
    
    * Refactor Get Event Logs with filter_param_factory
    
    * Refactor add type option for filter_param_factory
    
    * Fix Get Event Logs with latest paginated_select
    
    * Refactor Get Assets Event
    
    * Refactor List Dag Warnings
    
    * Refactor DagRun related
    
    - QueryLastDagRunStateFilter
    - dag_ids of get_dag_stats
    
    * Remove unused parameters
    
    * Refactor on Dag parameters
    
    * Add any_equal to FilterParam
    
    * Refactor Task Instance
    
    * Fix Get Event Logs type
    
    * Fix after rebase
    
    * Refactor with search_param_factory
    
    * Refactor QueryLastDagRunStateFilter
    
    * Fix get_list_dag_runs_batch

--- airflow/api_fastapi/core_api/routes/public/job.py
+++ airflow/api_fastapi/core_api/routes/public/job.py
@@ -26,15 +26,13 @@ from airflow.api_fastapi.common.db.common import (
     paginated_select,
 )
 from airflow.api_fastapi.common.parameters import (
-    QueryJobExecutorClassFilter,
-    QueryJobHostnameFilter,
-    QueryJobStateFilter,
-    QueryJobTypeFilter,
+    FilterParam,
     QueryLimit,
     QueryOffset,
     RangeFilter,
     SortParam,
     datetime_range_filter_factory,
+    filter_param_factory,
 )
 from airflow.api_fastapi.common.router import AirflowRouter
 from airflow.api_fastapi.core_api.datamodels.job import (
@@ -84,15 +82,25 @@ def get_jobs(
         ),
     ],
     session: SessionDep,
-    state: QueryJobStateFilter,
-    job_type: QueryJobTypeFilter,
-    hostname: QueryJobHostnameFilter,
-    executor_class: QueryJobExecutorClassFilter,
+    state: Annotated[
+        FilterParam[str | None], Depends(filter_param_factory(Job.state, str | None, filter_name="job_state"))
+    ],
+    job_type: Annotated[
+        FilterParam[str | None],
+        Depends(filter_param_factory(Job.job_type, str | None, filter_name="job_type")),
+    ],
+    hostname: Annotated[
+        FilterParam[str | None],
+        Depends(filter_param_factory(Job.hostname, str | None, filter_name="hostname")),
+    ],
+    executor_class: Annotated[
+        FilterParam[str | None],
+        Depends(filter_param_factory(Job.executor_class, str | None, filter_name="executor_class")),
+    ],
     is_alive: bool | None = None,
 ) -> JobCollectionResponse:
     """Get all jobs."""
     base_select = select(Job).where(Job.state == JobState.RUNNING).order_by(Job.latest_heartbeat.desc())
-    # TODO: Refactor using the `FilterParam` class in commit `574b72e41cc5ed175a2bbf4356522589b836bb11`
 
     jobs_select, total_entries = paginated_select(
         statement=base_select,
