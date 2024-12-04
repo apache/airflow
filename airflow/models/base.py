d3895c85390c220cbad1fe15489134e9aa751e59
rahulgoyal2987 <rahulgoyal338@gmail.com>
2024-12-04 19:42:19 +0530

    Update base.py

--- airflow/models/base.py
+++ airflow/models/base.py
@@ -47,7 +47,7 @@ def _get_schema():
 metadata = MetaData(schema=_get_schema(), naming_convention=naming_convention)
 mapper_registry = registry(metadata=metadata)
 _sentinel = type('DefaultSentinel', (object,), {})
-
+_sentinel.callers = {}
 if TYPE_CHECKING:
     Base = Any
 else:
