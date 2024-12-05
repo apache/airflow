ed301844bc31a4141e5eaea5af2956c6ec09f393
LIU ZHE YOU <68415893+jason810496@users.noreply.github.com>
2024-12-06 04:00:00 +0800

    Remove Provider Deprecations in Common SQL (#44645)
    * Remove Provider Deprecations in Common SQL
    
    * Fix Changelog header
    
    Co-authored-by: Jens Scheffler <95105677+jscheffl@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Jens Scheffler <95105677+jscheffl@users.noreply.github.com>

--- providers/src/airflow/providers/common/sql/hooks/sql.py
+++ providers/src/airflow/providers/common/sql/hooks/sql.py
@@ -41,7 +41,6 @@ from sqlalchemy.engine import Inspector
 from airflow.exceptions import (
     AirflowException,
     AirflowOptionalProviderFeatureException,
-    AirflowProviderDeprecationWarning,
 )
 from airflow.hooks.base import BaseHook
 
@@ -502,17 +501,6 @@ class DbApiHook(BaseHook):
         If this method is not overridden, the result data is returned as-is. If the output of the cursor
         is already a common data structure, this method should be ignored.
         """
-        # Back-compatibility call for providers implementing old ´_make_serializable' method.
-        with contextlib.suppress(AttributeError):
-            result = self._make_serializable(result=result)  # type: ignore[attr-defined]
-            warnings.warn(
-                "The `_make_serializable` method is deprecated and support will be removed in a future "
-                f"version of the common.sql provider. Please update the {self.__class__.__name__}'s provider "
-                "to a version based on common.sql >= 1.9.1.",
-                AirflowProviderDeprecationWarning,
-                stacklevel=2,
-            )
-
         if isinstance(result, Sequence):
             return cast(list[tuple], result)
         return cast(tuple, result)
