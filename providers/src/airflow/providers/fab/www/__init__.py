662f6e2d9473eef3384fb55990e7a58daabbd212
Vincent <97131062+vincbeck@users.noreply.github.com>
2024-12-06 13:41:21 -0500

    AIP-79 Support Airflow 2.x plugins in fast api. Embed a minimal version of the Flask application in fastapi application (#44464)

--- providers/src/airflow/providers/fab/www/__init__.py
+++ providers/src/airflow/providers/fab/www/__init__.py
@@ -1,3 +1,4 @@
+#
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -14,15 +15,3 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-from __future__ import annotations
-
-import pytest
-
-pytestmark = pytest.mark.db_test
-
-
-def test_health(test_client):
-    response = test_client.get("/execution/health")
-
-    assert response.status_code == 200
-    assert response.json() == {"status": "healthy"}
