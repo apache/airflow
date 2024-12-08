c41e5a032a5d8f1248f8f9175a4d6d3478e3cb4a
jason810496 <zhu424.dev@gmail.com>
2024-12-08 16:35:23 +0800

    Add multiple columns, parsing error cases in test

--- tests/api_fastapi/common/test_exceptions.py
+++ tests/api_fastapi/common/test_exceptions.py
@@ -22,10 +22,11 @@ from sqlalchemy.exc import IntegrityError
 
 from airflow.api_fastapi.common.exceptions import _DatabaseDialect, _UniqueConstraintErrorHandler
 from airflow.configuration import conf
-from airflow.models import Pool, Variable
+from airflow.models import DagRun, Pool, Variable
 from airflow.utils.session import provide_session
+from airflow.utils.state import DagRunState
 
-from tests_common.test_utils.db import clear_db_connections, clear_db_pools
+from tests_common.test_utils.db import clear_db_connections, clear_db_dags, clear_db_pools, clear_db_runs
 
 pytestmark = pytest.mark.db_test
 
@@ -41,49 +42,54 @@ EXPECTED_EXCEPTION_VARIABLE = HTTPException(
     status_code=status.HTTP_409_CONFLICT,
     detail="Unique constraint violation: variable with key=test_key already exists",
 )
-
-# Take Pool and Variable tables as test cases
-TEST_CASES_TABLES = [
-    "Pool",
-    "Variable",
-]
-TEST_CASES_EXCEPTIONS = [
-    EXPECTED_EXCEPTION_POOL,
-    EXPECTED_EXCEPTION_VARIABLE,
-]
-TEST_CASE_DB_DIALECTS_SKIP = [
+EXPECTED_EXCEPTION_DAG_RUN = HTTPException(
+    status_code=status.HTTP_409_CONFLICT,
+    detail="Unique constraint violation: dag_run with dag_id=test_dag_id, run_id=test_run_id already exists",
+)
+PYTEST_MARKS_DB_DIALECT = [
     {
         "condition": CURRENT_DATABASE_DIALECT.startswith(_DatabaseDialect.MYSQL.value)
-        or CURRENT_DATABASE_DIALECT.startswith(_DatabaseDialect.POSTGRESQL.value),
+        or CURRENT_DATABASE_DIALECT.startswith(_DatabaseDialect.POSTGRES.value),
         "reason": f"Test for {_DatabaseDialect.SQLITE.value} only",
     },
     {
         "condition": CURRENT_DATABASE_DIALECT.startswith(_DatabaseDialect.SQLITE.value)
-        or CURRENT_DATABASE_DIALECT.startswith(_DatabaseDialect.POSTGRESQL.value),
+        or CURRENT_DATABASE_DIALECT.startswith(_DatabaseDialect.POSTGRES.value),
         "reason": f"Test for {_DatabaseDialect.MYSQL.value} only",
     },
     {
         "condition": CURRENT_DATABASE_DIALECT.startswith(_DatabaseDialect.SQLITE.value)
         or CURRENT_DATABASE_DIALECT.startswith(_DatabaseDialect.MYSQL.value),
-        "reason": f"Test for {_DatabaseDialect.POSTGRESQL.value} only",
+        "reason": f"Test for {_DatabaseDialect.POSTGRES.value} only",
     },
 ]
 
 
-def generate_test_cases_parametrize():
-    """Generate cross product of test cases for parametrize."""
-    test_cases = []
-    for table, expected_exception in zip(TEST_CASES_TABLES, TEST_CASES_EXCEPTIONS):
-        for test_case in TEST_CASE_DB_DIALECTS_SKIP:
-            test_cases.append(
+def generate_test_cases_parametrize(test_cases: list[str], expected_exceptions: list[HTTPException]):
+    """Generate cross product of test cases for parametrize with different database dialects."""
+    generated_test_cases = []
+    for test_case, expected_exception in zip(test_cases, expected_exceptions):
+        for mark in PYTEST_MARKS_DB_DIALECT:
+            generated_test_cases.append(
                 pytest.param(
-                    table,
+                    test_case,
                     expected_exception,
-                    id=f"{test_case['reason']} - Inserting duplicate entry in {table}",
-                    marks=pytest.mark.skipif(**test_case),
+                    id=f"{mark['reason']} - {test_case}",
+                    marks=pytest.mark.skipif(**mark),  # type: ignore
                 )
             )
-    return test_cases
+    return generated_test_cases
+
+
+def get_unique_constraint_error_prefix():
+    """Get unique constraint error prefix based on current database dialect."""
+    if CURRENT_DATABASE_DIALECT.startswith(_DatabaseDialect.SQLITE.value):
+        return _UniqueConstraintErrorHandler.unique_constraint_error_prefix_dict[_DatabaseDialect.SQLITE]
+    if CURRENT_DATABASE_DIALECT.startswith(_DatabaseDialect.MYSQL.value):
+        return _UniqueConstraintErrorHandler.unique_constraint_error_prefix_dict[_DatabaseDialect.MYSQL]
+    if CURRENT_DATABASE_DIALECT.startswith(_DatabaseDialect.POSTGRES.value):
+        return _UniqueConstraintErrorHandler.unique_constraint_error_prefix_dict[_DatabaseDialect.POSTGRES]
+    return ""
 
 
 class TestUniqueConstraintErrorHandler:
@@ -93,17 +99,28 @@ class TestUniqueConstraintErrorHandler:
     def setup(self) -> None:
         clear_db_connections(add_default_connections_back=False)
         clear_db_pools()
+        clear_db_runs()
+        clear_db_dags()
 
     def teardown_method(self) -> None:
         clear_db_connections()
         clear_db_pools()
+        clear_db_runs()
+        clear_db_dags()
 
     @pytest.mark.parametrize(
         "table, expected_exception",
-        generate_test_cases_parametrize(),
+        generate_test_cases_parametrize(
+            ["Pool", "Variable"],
+            [
+                EXPECTED_EXCEPTION_POOL,
+                EXPECTED_EXCEPTION_VARIABLE,
+            ],
+        ),
     )
     @provide_session
-    def test_handle_unique_constraint_error(self, session, table, expected_exception) -> None:
+    def test_handle_single_column_unique_constraint_error(self, session, table, expected_exception) -> None:
+        # Take Pool and Variable tables as test cases
         if table == "Pool":
             session.add(Pool(pool=TEST_POOL, slots=1, description="test pool", include_deferred=False))
             session.add(Pool(pool=TEST_POOL, slots=1, description="test pool", include_deferred=False))
@@ -116,5 +133,68 @@ class TestUniqueConstraintErrorHandler:
 
         with pytest.raises(HTTPException) as exeinfo_response_error:
             self.unique_constraint_error_handler.exception_handler(None, exeinfo_integrity_error.value)  # type: ignore
+
+        assert str(exeinfo_response_error.value) == str(expected_exception)
+
+    @pytest.mark.parametrize(
+        "table, expected_exception",
+        generate_test_cases_parametrize(
+            ["DagRun"],
+            [EXPECTED_EXCEPTION_DAG_RUN],
+        ),
+    )
+    @provide_session
+    def test_handle_multiple_columns_unique_constraint_error(
+        self, session, table, expected_exception
+    ) -> None:
+        if table == "DagRun":
+            session.add(
+                DagRun(
+                    dag_id="test_dag_id", run_id="test_run_id", run_type="manual", state=DagRunState.RUNNING
+                )
+            )
+            session.add(
+                DagRun(
+                    dag_id="test_dag_id", run_id="test_run_id", run_type="manual", state=DagRunState.RUNNING
+                )
+            )
+
+        with pytest.raises(IntegrityError) as exeinfo_integrity_error:
+            session.commit()
+
+        with pytest.raises(HTTPException) as exeinfo_response_error:
+            self.unique_constraint_error_handler.exception_handler(None, exeinfo_integrity_error.value)  # type: ignore
+
+        assert str(exeinfo_response_error.value) == str(expected_exception)
+
+    @pytest.mark.parametrize(
+        "mock_error_orig, expected_exception",
+        generate_test_cases_parametrize(
+            [
+                f"{get_unique_constraint_error_prefix()} This is a string that will cause parse error in exception handler",
+                f"{get_unique_constraint_error_prefix()} This_is_another_string_that_will_cause_parse_error_in_exception_handler",
+            ],
+            [
+                HTTPException(
+                    status_code=status.HTTP_409_CONFLICT,
+                    detail=f"Unique constraint violation: {get_unique_constraint_error_prefix()} This is a string that will cause parse error in exception handler",
+                ),
+                HTTPException(
+                    status_code=status.HTTP_409_CONFLICT,
+                    detail=f"Unique constraint violation: {get_unique_constraint_error_prefix()} This_is_another_string_that_will_cause_parse_error_in_exception_handler",
+                ),
+            ],
+        ),
+    )
+    def test_handle_error_parsing_message_unique_constraint_error(
+        self, mock_error_orig, expected_exception
+    ) -> None:
+        mock_integrity_error = IntegrityError(
+            statement="mock statement",
+            params={},
+            orig=Exception(mock_error_orig),
+        )
+        with pytest.raises(HTTPException) as exeinfo_response_error:
+            self.unique_constraint_error_handler.exception_handler(None, mock_integrity_error)  # type: ignore
 
         assert str(exeinfo_response_error.value) == str(expected_exception)
