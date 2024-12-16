70ce5dfad2dfacaec42387a8c306567b82843a52
vatsrahul1001 <rah.sharma11@gmail.com>
2024-12-17 00:14:12 +0530

    refactor approach to migrate values in conf

--- airflow/migrations/versions/0052_3_0_0_remove_pickled_data_from_dagrun_table.py
+++ airflow/migrations/versions/0052_3_0_0_remove_pickled_data_from_dagrun_table.py
@@ -27,8 +27,13 @@ Create Date: 2024-12-01 08:33:15.425141
 
 from __future__ import annotations
 
+import json
+import pickle
+from textwrap import dedent
+
 import sqlalchemy as sa
-from alembic import op
+from alembic import context, op
+from sqlalchemy import text
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
@@ -41,21 +46,105 @@ airflow_version = "3.0.0"
 
 def upgrade():
     """Apply remove pickled data from dagrun table."""
+    conn = op.get_bind()
     conf_type = sa.JSON().with_variant(postgresql.JSONB, "postgresql")
+    op.add_column("dag_run", sa.Column("conf_json", conf_type, nullable=True))
 
-    with op.batch_alter_table("dag_run", schema=None) as batch_op:
-        # Drop the existing column for SQLite (due to its limitations)
-        batch_op.drop_column("conf")
-        # Add the new column with the correct type for the all dialect
-        batch_op.add_column(sa.Column("conf", conf_type, nullable=True))
+    if context.is_offline_mode():
+        # Update the dag_run.conf column value to NULL
+        print(
+            dedent("""
+             ------------
+             --  WARNING: Unable to migrate data of conf column in offline mode!
+             --  Column conf will be set to NULL in offline mode!.
+             --  DO not use Offline mode if data if you want to keep conf values.
+             ------------
+             """)
+        )
+        conn.execute(text("UPDATE dag_run set conf=null WHERE conf IS NOT NULL"))
+    else:
+        BATCH_SIZE = 2
+        total_processed = 0
+        offset = 0
+        while True:
+            rows = conn.execute(
+                text(
+                    f"SELECT id,conf FROM dag_run WHERE conf IS not NULL order by id LIMIT {BATCH_SIZE} OFFSET {offset}"
+                )
+            ).fetchall()
+            if not rows:
+                break
+            for row in rows:
+                row_id, pickle_data = row
+
+                try:
+                    original_data = pickle.loads(pickle_data)
+                    json_data = json.dumps(original_data)
+                    conn.execute(text(f"UPDATE dag_run SET conf_json ='{json_data}' WHERE id = {row_id}"))
+                except Exception as e:
+                    print(f"Error processing row ID {row_id}: {e}")
+                    continue
+            offset += BATCH_SIZE
+
+            # Update total processed count
+            total_processed += len(rows)
+
+    op.drop_column("dag_run", "conf")
+
+    op.alter_column("dag_run", "conf_json", existing_type=conf_type, new_column_name="conf")
 
 
 def downgrade():
     """Unapply Remove pickled data from dagrun table."""
+    conn = op.get_bind()
     conf_type = sa.LargeBinary().with_variant(postgresql.BYTEA, "postgresql")
+    op.add_column("dag_run", sa.Column("conf_pickle", conf_type, nullable=True))
 
-    # Apply the same logic for all dialects, including SQLite
-    with op.batch_alter_table("dag_run", schema=None) as batch_op:
-        # Drop the existing column for SQLite (due to its limitations)
-        batch_op.drop_column("conf")
-        batch_op.add_column(sa.Column("conf", conf_type, nullable=True))
+    if context.is_offline_mode():
+        # Update the dag_run.conf column value to NULL
+        print(
+            dedent("""
+             ------------
+             --  WARNING: Unable to migrate data of conf column in offline mode!
+             --  Column conf will be set to NULL in offline mode!.
+             --  DO not use Offline mode if data if you want to keep conf values.
+             ------------
+             """)
+        )
+        conn.execute(text("UPDATE dag_run set conf=null WHERE conf IS NOT NULL"))
+    else:
+        BATCH_SIZE = 2
+        total_processed = 0
+        offset = 0
+        while True:
+            rows = conn.execute(
+                text(
+                    f"SELECT id,conf FROM dag_run WHERE conf IS not NULL order by id LIMIT {BATCH_SIZE} OFFSET {offset}"
+                )
+            ).fetchall()
+            if not rows:
+                break  #
+            for row in rows:
+                row_id, json_data = row
+
+                try:
+                    pickled_data = pickle.dumps(json_data, protocol=pickle.HIGHEST_PROTOCOL)
+                    conn.execute(
+                        text("""
+                            UPDATE dag_run
+                            SET conf_pickle = :pickle_data
+                            WHERE id = :id
+                        """),
+                        {"pickle_data": pickled_data, "id": row_id},
+                    )
+                except Exception as e:
+                    print(f"Error processing row ID {row_id}: {e}")
+                    continue
+            offset += BATCH_SIZE
+
+            # Update total processed count
+            total_processed += len(rows)
+
+    op.drop_column("dag_run", "conf")
+
+    op.alter_column("dag_run", "conf_pickle", existing_type=conf_type, new_column_name="conf")
