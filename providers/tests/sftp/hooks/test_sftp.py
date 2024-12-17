84d07e5556f09a749da4945fa610861ea341ffc7
David Blain <david.blain@infrabel.be>
2024-12-17 20:55:10 +0100

    refactor: Fixed test_context_manager in TestSFTPHook

--- providers/tests/sftp/hooks/test_sftp.py
+++ providers/tests/sftp/hooks/test_sftp.py
@@ -23,17 +23,18 @@ import os
 import shutil
 from io import BytesIO, StringIO
 from unittest import mock
-from unittest.mock import AsyncMock, patch
+from unittest.mock import AsyncMock, patch, MagicMock
 
 import paramiko
 import pytest
-from asyncssh import SFTPAttrs, SFTPNoSuchFile
-from asyncssh.sftp import SFTPName
-
 from airflow.exceptions import AirflowException
 from airflow.models import Connection
 from airflow.providers.sftp.hooks.sftp import SFTPHook, SFTPHookAsync
 from airflow.utils.session import provide_session
+from asyncssh import SFTPAttrs, SFTPNoSuchFile
+from asyncssh.sftp import SFTPName
+from paramiko.client import SSHClient
+from paramiko.sftp_client import SFTPClient
 
 pytestmark = pytest.mark.db_test
 
@@ -397,33 +398,30 @@ class TestSFTPHook:
     @pytest.mark.parametrize(
         "test_connection_side_effect",
         [
-            (lambda: (True, "Connection successfully tested")),
-            RuntimeError("Test connection failed"),
+            (lambda arg: (True, "Connection successfully tested")),
+            (lambda arg: RuntimeError("Test connection failed")),
         ],
     )
-    @mock.patch("airflow.providers.sftp.hooks.sftp.SFTPHook.get_connection")
-    def test_context_manager(self, mock_get_connection, test_connection_side_effect):
-        connection = Connection(login="login", host="host")
-        mock_get_connection.return_value = connection
+    @mock.patch("airflow.providers.sftp.hooks.sftp.SFTPHook.get_conn")
+    def test_context_manager(self, mock_get_conn, test_connection_side_effect):
+        ssh_conn = MagicMock(spec=SSHClient)
+        mock_get_conn.return_value = ssh_conn
+        sftp_conn = MagicMock(spec=SFTPClient)
+        ssh_conn.open_sftp.return_value = sftp_conn
+        sftp_conn.normalize.side_effect = test_connection_side_effect
 
-        with (
-            mock.patch.object(SFTPHook, "get_conn") as mock_get_conn,
-            mock.patch.object(SFTPHook, "close_conn") as mock_close_conn,
-        ):
-            mock_get_conn.return_value.pwd = "/home/someuser"
+        hook = SFTPHook()
+        if isinstance(test_connection_side_effect, RuntimeError):
+            with pytest.raises(RuntimeError, match="Test connection failed"):
+                hook.test_connection()
+        else:
+            status, msg = hook.test_connection()
 
-            with SFTPHook() as hook:
-                with mock.patch.object(hook, "test_connection", side_effect=test_connection_side_effect):
-                    try:
-                        status, msg = hook.test_connection()
+            assert status is True
+            assert msg == "Connection successfully tested"
 
-                        assert status is True
-                        assert msg == "Connection successfully tested"
-                    except RuntimeError as e:
-                        assert str(e) == "Test connection failed"
-
-            mock_get_conn.assert_called_once()
-            mock_close_conn.assert_called_once()
+        ssh_conn.open_sftp.assert_called_once()
+        sftp_conn.close.assert_called()
 
     def test_get_suffix_pattern_match(self):
         output = self.hook.get_file_by_pattern(self.temp_dir, "*.txt")
