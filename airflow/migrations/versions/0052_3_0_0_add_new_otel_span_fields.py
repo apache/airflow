1d19534218cc51b7c21dc1f938c5d7d5d43fd2ac
Christos Bisias <christosbis@gmail.com>
2024-12-18 15:31:18 +0200

    trigger er diagram generation

--- airflow/migrations/versions/0052_3_0_0_add_new_otel_span_fields.py
+++ airflow/migrations/versions/0052_3_0_0_add_new_otel_span_fields.py
@@ -29,7 +29,8 @@ from __future__ import annotations
 
 import sqlalchemy as sa
 from alembic import op
-from sqlalchemy import JSON
+
+from airflow.utils.sqlalchemy import ExtendedJSON
 
 # revision identifiers, used by Alembic.
 revision = "0eb040b3eb12"
@@ -42,10 +43,10 @@ airflow_version = "3.0.0"
 def upgrade():
     """Apply add new otel span fields."""
     op.add_column("dag_run", sa.Column("scheduled_by_job_id", sa.Integer, nullable=True))
-    op.add_column("dag_run", sa.Column("context_carrier", JSON, nullable=True))
+    op.add_column("dag_run", sa.Column("context_carrier", ExtendedJSON, nullable=True))
     op.add_column("dag_run", sa.Column("span_status", sa.String(250), nullable=False))
 
-    op.add_column("task_instance", sa.Column("context_carrier", JSON, nullable=True))
+    op.add_column("task_instance", sa.Column("context_carrier", ExtendedJSON, nullable=True))
     op.add_column("task_instance", sa.Column("span_status", sa.String(250), nullable=False))
 
 
