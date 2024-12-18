26aab6c96b37c949e25e25ffed007f2789671cdf
Christopher P. Anderson <48180628+topherinternational@users.noreply.github.com>
2024-12-18 11:27:53 -0600

    more ES tests

--- helm_tests/airflow_aux/test_remote_logging.py
+++ helm_tests/airflow_aux/test_remote_logging.py
@@ -25,8 +25,8 @@ import pytest
 from tests.charts.helm_template_generator import render_chart
 
 
-class TestElasticsearchSecret:
-    """Tests elasticsearch secret."""
+class TestElasticsearchConfig:
+    """Tests elasticsearch configuration behaviors."""
 
     def test_should_not_generate_a_document_if_elasticsearch_disabled(self):
         docs = render_chart(
@@ -72,93 +72,3 @@ class TestElasticsearchSecret:
             in ex_ctx.value.stderr.decode()
         )
 
-    def _get_connection(self, values: dict) -> str:
-        docs = render_chart(
-            values=values,
-            show_only=["templates/secrets/elasticsearch-secret.yaml"],
-        )
-        encoded_connection = jmespath.search("data.connection", docs[0])
-        return base64.b64decode(encoded_connection).decode()
-
-    def test_should_correctly_handle_password_with_special_characters(self):
-        connection = self._get_connection(
-            {
-                "elasticsearch": {
-                    "enabled": True,
-                    "connection": {
-                        "user": "username!@#$%%^&*()",
-                        "pass": "password!@#$%%^&*()",
-                        "host": "elastichostname",
-                    },
-                }
-            }
-        )
-
-        assert (
-            connection
-            == "http://username%21%40%23$%25%25%5E&%2A%28%29:password%21%40%23$%25%25%5E&%2A%28%29@"
-            "elastichostname:9200"
-        )
-
-    def test_should_generate_secret_with_specified_port(self):
-        connection = self._get_connection(
-            {
-                "elasticsearch": {
-                    "enabled": True,
-                    "connection": {
-                        "user": "username",
-                        "pass": "password",
-                        "host": "elastichostname",
-                        "port": 2222,
-                    },
-                }
-            }
-        )
-
-        assert connection == "http://username:password@elastichostname:2222"
-
-    @pytest.mark.parametrize("scheme", ["http", "https"])
-    def test_should_generate_secret_with_specified_schemes(self, scheme):
-        connection = self._get_connection(
-            {
-                "elasticsearch": {
-                    "enabled": True,
-                    "connection": {
-                        "scheme": scheme,
-                        "user": "username",
-                        "pass": "password",
-                        "host": "elastichostname",
-                    },
-                }
-            }
-        )
-
-        assert f"{scheme}://username:password@elastichostname:9200" == connection
-
-    @pytest.mark.parametrize(
-        "extra_conn_kwargs, expected_user_info",
-        [
-            # When both user and password are empty.
-            ({}, ""),
-            # When password is empty
-            ({"user": "admin"}, ""),
-            # When user is empty
-            ({"pass": "password"}, ""),
-            # Valid username/password
-            ({"user": "admin", "pass": "password"}, "admin:password"),
-        ],
-    )
-    def test_url_generated_when_user_pass_empty_combinations(self, extra_conn_kwargs, expected_user_info):
-        connection = self._get_connection(
-            {
-                "elasticsearch": {
-                    "enabled": True,
-                    "connection": {"host": "elastichostname", "port": 8080, **extra_conn_kwargs},
-                }
-            }
-        )
-
-        if not expected_user_info:
-            assert connection == "http://elastichostname:8080"
-        else:
-            assert f"http://{expected_user_info}@elastichostname:8080" == connection
