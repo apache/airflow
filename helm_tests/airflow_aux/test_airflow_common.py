b54829edddd5fe45e65f2f1089f091a7bc77e907
Christopher P. Anderson <48180628+topherinternational@users.noreply.github.com>
2024-12-19 09:57:01 -0600

    nit and comment

--- helm_tests/airflow_aux/test_airflow_common.py
+++ helm_tests/airflow_aux/test_airflow_common.py
@@ -324,6 +324,7 @@ class TestAirflowCommon:
                     # the following vars only appear if remote logging is set, so disabling them in this test is kind of a no-op
                     "AIRFLOW__ELASTICSEARCH__HOST": False,
                     "AIRFLOW__ELASTICSEARCH__ELASTICSEARCH_HOST": False,
+                    "AIRFLOW__OPENSEARCH__HOST": False,
                 },
             },
             show_only=[
