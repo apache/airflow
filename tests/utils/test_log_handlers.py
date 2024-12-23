607b4e472fc19a3318997b9d6a701cb990f45d1b
jason810496 <zhu424.dev@gmail.com>
2024-12-23 14:25:51 +0800

    Fix test_log_reader, minor fix test_log_handlers
    - Fix mock_read to new stream-based reading in test_log_reader
    - Fix test for expecting stdout of callable should be in log lines

--- tests/utils/test_log_handlers.py
+++ tests/utils/test_log_handlers.py
@@ -20,7 +20,6 @@ from __future__ import annotations
 import logging
 import logging.config
 import os
-import re
 from contextlib import suppress
 from http import HTTPStatus
 from importlib import reload
@@ -160,7 +159,7 @@ class TestFileTaskLogHandler:
 
     def test_file_task_handler(self, dag_maker, session):
         def task_callable(ti):
-            ti.log.info("test")
+            ti.log.info("test_log_stdout_in_callable")
 
         with dag_maker("dag_for_testing_file_task_handler", schedule=None, session=session):
             PythonOperator(
@@ -203,13 +202,11 @@ class TestFileTaskLogHandler:
         assert len(log_streams) == len(metadata_array)
         self._assert_is_log_stream_type(log_streams[0])
         assert isinstance(metadata_array[0], dict)
-        target_re = r"\n\[[^\]]+\] {test_log_handlers.py:\d+} INFO - test\n"
 
         # We should expect our log line from the callable above to appear in
         # the logs we read back
         log_str = "\n".join(line for line in log_streams[0])
-        log_lines = log_str.splitlines()
-        assert re.search(target_re, log_lines[-2]), "Logs were " + log_str
+        assert "test_log_stdout_in_callable" in log_str
 
         # Remove the generated tmp log file.
         os.remove(log_filename)
