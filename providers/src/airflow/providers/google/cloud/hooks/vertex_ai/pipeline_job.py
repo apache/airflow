2242ddeec51433078a7ac1e98a4554c9f17fc81f
Oleg Kachur <kachur@google.com>
2024-12-28 01:37:26 +0000

    Introduce operation helper class and refactor
    - Refactor google cloud hooks, that used generic 'wait_for_operation'
      methods.

--- providers/src/airflow/providers/google/cloud/hooks/vertex_ai/pipeline_job.py
+++ providers/src/airflow/providers/google/cloud/hooks/vertex_ai/pipeline_job.py
@@ -42,6 +42,7 @@ from google.cloud.aiplatform_v1 import (
 from airflow.exceptions import AirflowException
 from airflow.providers.google.common.consts import CLIENT_INFO
 from airflow.providers.google.common.hooks.base_google import GoogleBaseAsyncHook, GoogleBaseHook
+from airflow.providers.google.common.hooks.operation_helpers import OperationHelper
 
 if TYPE_CHECKING:
     from google.api_core.operation import Operation
@@ -51,7 +52,7 @@ if TYPE_CHECKING:
     from google.cloud.aiplatform_v1.services.pipeline_service.pagers import ListPipelineJobsPager
 
 
-class PipelineJobHook(GoogleBaseHook):
+class PipelineJobHook(GoogleBaseHook, OperationHelper):
     """Hook for Google Cloud Vertex AI Pipeline Job APIs."""
 
     def __init__(
@@ -111,14 +112,6 @@ class PipelineJobHook(GoogleBaseHook):
             location=location,
             failure_policy=failure_policy,
         )
-
-    def wait_for_operation(self, operation: Operation, timeout: float | None = None):
-        """Wait for long-lasting operation to complete."""
-        try:
-            return operation.result(timeout=timeout)
-        except Exception:
-            error = operation.exception(timeout=timeout)
-            raise AirflowException(error)
 
     def cancel_pipeline_job(self) -> None:
         """Cancel PipelineJob."""
