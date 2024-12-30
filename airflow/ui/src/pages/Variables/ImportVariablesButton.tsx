3e1a44ab8357c0c36fdfc9cc8dca0f5c20189e33
shubhamraj-git <shubhamraj.osc@gmail.com>
2024-12-30 12:40:40 +0530

    add button

---
+++ airflow/ui/src/pages/Variables/ImportVariablesButton.tsx
@@ -0,0 +1,34 @@
+/*!
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+import { useDisclosure } from "@chakra-ui/react";
+import { FiUploadCloud } from "react-icons/fi";
+
+import { Button } from "src/components/ui";
+
+const ImportVariablesButton = () => {
+  const { onClose, onOpen, open } = useDisclosure();
+
+  return (
+    <Button colorPalette="blue" onClick={onOpen}>
+      <FiUploadCloud /> Import Variables
+    </Button>
+  );
+};
+
+export default ImportVariablesButton;
