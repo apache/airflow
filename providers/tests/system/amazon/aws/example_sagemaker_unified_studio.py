a1e0766dcd187e1c1b90a9679a07fcd3af90ca87
Arunav Gupta <garunav@amazon.com>
2025-01-07 22:07:36 +0000

    Add more specifics to SUS system test

--- providers/tests/system/amazon/aws/example_sagemaker_unified_studio.py
+++ providers/tests/system/amazon/aws/example_sagemaker_unified_studio.py
@@ -34,9 +34,15 @@ Prerequisites: The account which runs this test must manually have the following
 1. An IAM IDC organization set up in the testing region with the following user initialized:
     Username: airflowTestUser
     Password: airflowSystemTestP@ssword1!
+
+Essentially, this test will emulate a DAG run in the shared MWAA environment inside a SageMaker Unified Studio Project.
+The setup tasks will set up the project and configure the test runnner to emulate an MWAA instance.
+Then, the SageMakerNotebookOperator will run a test notebook. This should spin up a SageMaker training job, run the notebook, and exit successfully.
+The teardown tasks will finally delete the project and domain that was set up for this test run.
 """
 
-pytestmark = pytest.mark.skipif(not AIRFLOW_V_2_10_PLUS, reason="Test requires Airflow 2.10+")
+pytestmark = pytest.mark.skipif(
+    not AIRFLOW_V_2_10_PLUS, reason="Test requires Airflow 2.10+")
 
 DAG_ID = "example_sagemaker_unified_studio"
 
@@ -48,13 +54,16 @@ def create_roles_and_network_stack():
     """
     Creates SageMakerUnifiedStudio VPC and IAM roles for a quick-setup SageMaker Unified Studio
     domain and project. Does so by running a CloudFormation template.
-    Returns domainExecutionRole for domain creation.
+    Returns domainExecutionRole, manageAccessRole, provisioningRole for domain creation.
     """
+    # TODO: do we still need the CFN template or will roles be created automatically now?
     pass
 
 
 @task
-def create_sus_domain(env_id: str, domain_execution_role: str):
+def create_sus_domain(
+    env_id: str, domain_execution_role: str, manage_access_role: str, provisioning_role: str
+):
     """
     Creates a SageMaker Unified Studio Domain in the testing account.
     This assumes network and IDC prerequisites have been fulfilled.
@@ -73,34 +82,180 @@ def create_sus_domain(env_id: str, domain_execution_role: str):
     domain_id = create_domain_response["id"]
 
     retries = 0
-    while status := datazone_client.get_domain(identifier=domain_id) != "AVAILABLE":
+    while status := datazone_client.get_domain(identifier=domain_id)["status"] != "AVAILABLE":
         retries += 1
         if retries > 3:
             raise RuntimeError("Create domain timed out")
         print(f"Creating DataZone domain {domain_id}, Status = {status}")
         time.sleep(10)  # polling every 10 seconds
 
-    # 2. Add Environment blueprints
-    # TODO: get the VPC/subnets for SUS and add them to blueprints
-    # TODO: create the data analytics profile with tooling blueprint (only one we need?)
-    create_project_profile_response = datazone_client.create_project_profile(data_analytics_profile)
+    # 2. Add Environment blueprints and create project profile
+    data_analytics_profile = construct_data_analytics_profile(
+        datazone_client,
+        domain_id,
+        manage_access_role_arn=manage_access_role,
+        provisioning_role_arn=provisioning_role,
+    )
+    create_project_profile_response = datazone_client.create_project_profile(
+        **data_analytics_profile)
 
-    # TODO: figure out how to get environment id
-    # TODO: disambiguate between the env_id for this run and the datazone environment_id
     return {"domain_id": domain_id}
 
 
+def get_common_config(blueprint):
+    return {
+        "awsAccount": {"awsAccountId": account},
+        "awsRegion": {"regionName": region},
+        "configurationParameters": {},  # user default parameters
+        "description": blueprint["description"],
+        "environmentBlueprintId": blueprint["id"],
+        "name": blueprint["name"],
+    }
+
+
+def construct_data_analytics_profile(
+    datazone_client: boto3.Client, domain_id: str, manage_access_role_arn: str, provisioning_role_arn: str
+):
+    # 1. get blueprints
+    environment_blueprints = datazone_client.list_environment_blueprints(
+        domainIdentifier=domain_id,
+        managed=True,
+    )["items"]
+    environment_blueprints = list(
+        filter(lambda x: x["provider"] ==
+               "Amazon SageMaker", environment_blueprints)
+    )
+    # TODO: 2. get subnet data and regional params
+    # 3. update blueprint with regional params
+    for blueprint in environment_blueprints:
+        datazone_client.put_environment_blueprint_configurations(
+            domainIdentifier=domain_id,
+            environmentBlueprintIdentifier=blueprint["id"],
+            enabledRegions=enabled_regions,  # from network step 2
+            manageAccessRoleArn=manage_access_role_arn,
+            provisioningRoleArn=provisioning_role_arn,
+            regionalParameters=regional_params,  # from network step 2
+        )
+    # 4. construct tooling configs
+    tooling_blueprint = next(
+        (x for x in environment_blueprints if x["name"] == "Tooling"), None)
+    datalake_blueprint = next(
+        (x for x in environment_blueprints if x["name"] == "DataLake"), None)
+    lakehouse_catalog_blueprint = next(
+        (x for x in environment_blueprints if x["name"]
+         == "LakehouseCatalog"), None
+    )
+    redshift_serverless_blueprint = next(
+        (x for x in environment_blueprints if x["name"]
+         == "RedshiftServerless"), None
+    )
+    emr_serverless_blueprint = next(
+        (x for x in environment_blueprints if x["name"] == "EmrServerless"), None)
+    emr_on_ec2_blueprint = next(
+        (x for x in environment_blueprints if x["name"] == "EmrOnEc2"), None)
+
+    tooling_config = {
+        **get_common_config(tooling_blueprint),
+        "configurationParameters": {},
+        "deploymentMode": "ON_CREATE",
+        "deploymentOrder": 0,
+    }
+    datalake_config = {
+        **get_common_config(datalake_blueprint), "deploymentMode": "ON_CREATE"}
+
+    lakehouse_catalog_config = {
+        **get_common_config(lakehouse_catalog_blueprint),
+        "deploymentMode": "ON_DEMAND",
+    }
+
+    redshift_serverless_on_create_config = {
+        **get_common_config(redshift_serverless_blueprint),
+        "deploymentMode": "ON_CREATE",
+    }
+
+    redshift_serverless_on_demand_config = {
+        **redshift_serverless_on_create_config,
+        "configurationParameters": {
+            "parameterOverrides": [
+                {"name": "connectionName", "isEditable": True},
+                {"name": "connectionDescription", "isEditable": True},
+            ]
+        },
+        "deploymentMode": "ON_DEMAND",
+    }
+
+    emr_serverless_config = {
+        **get_common_config(emr_serverless_blueprint), "deploymentMode": "ON_DEMAND"}
+
+    emr_on_ec2_config = {
+        **get_common_config(emr_on_ec2_blueprint), "deploymentMode": "ON_DEMAND"}
+    # 5. place required configs in data analytics profile
+    data_analytics_profile = {
+        # TODO: pull this name suffix from somewhere (check original)
+        "name": f"Data analytics project profile{profile_name_suffix}",
+        "description": "This sample project profile creates environments with necessary AWS resources and tooling for data lake activities. It includes AWS Glue databases, Amazon Athena workgroups, Lakehouse catalog, Amazon Redshift Serverless workgroups, and more.",
+        "domainIdentifier": domain_id,
+        "environmentConfigurations": [
+            tooling_config,
+            datalake_config,
+            redshift_serverless_on_create_config,
+            emr_on_ec2_config,
+            redshift_serverless_on_demand_config,
+            lakehouse_catalog_config,
+            emr_serverless_config,
+        ],
+        "status": "ENABLED",
+    }
+
+    return data_analytics_profile
+
+
 @task
-def create_sus_project(domain_id: str):
+def create_sus_project(domain_id: str, project_profile_id: str):
     """
     Creates a project within a given SageMaker Unified Studio Domain.
     Returns project id.
     """
-    datazone_client = boto3.client("datazone")
-    create_project_response = datazone_client.create_project(domainIdentifier=domain_id)
-    # TODO: figure out how to create this project with the project profile we need
+    import time
 
-    return {"project_id": create_project_response["id"]}
+    datazone_client = boto3.client("datazone")
+    create_project_response = datazone_client.create_project(
+        domainIdentifier=domain_id,
+        name="testProject",
+        projectProfileId=project_profile_id,
+    )
+    project_id = create_project_response["id"]
+    # Wait for project to be created. Can take up to 10 mins
+    retries = 0
+    while (
+        project_status := datazone_client.get_project(domainIdentifier=domain_id, identifier=project_id)[
+            "projectStatus"
+        ]
+        != "ACTIVE"
+    ) | (
+        environment_status := datazone_client.get_project(domainIdentifier=domain_id, identifier=project_id)[
+            "environmentDeploymentDetails"
+        ]["overallDeploymentStatus"]
+        != "SUCCESSFUL"
+    ):
+        retries += 1
+        if retries > 20:
+            raise RuntimeError("Create project timed out")
+        print(
+            f"Creating DataZone project {project_id}, project status = {
+                project_status} and environment status = {environment_status}"
+        )
+        time.sleep(30)  # polling every 10 seconds
+
+    return {
+        "project_id": project_id,
+        "environment_id": min(
+            datazone_client.list_environments(domainIdentifier=domain_id, projectIdentifier=project_id)[
+                "items"
+            ],
+            key=lambda x: x["deploymentOrder"],
+        )["id"],
+    }
 
 
 @task
@@ -119,7 +274,8 @@ def delete_sus_domain(env_id: str, domain_id: str):
     This also deletes any projects hosted within that domain.
     """
     datazone_client = boto3.client("datazone")
-    delete_domain_response = datazone_client.delete_domain(client_token=env_id, identifier=domain_id)
+    delete_domain_response = datazone_client.delete_domain(
+        client_token=env_id, identifier=domain_id)
 
     # TODO: check for successful deletion
     return delete_domain_response
@@ -139,7 +295,8 @@ with DAG(
 
     setup_sus_domain = create_sus_domain(env_id=env_id)
 
-    setup_sus_project = create_sus_project(domain_id=setup_sus_domain["domain_id"])
+    setup_sus_project = create_sus_project(
+        domain_id=setup_sus_domain["domain_id"])
 
     setup_mock_mwaa_environment = emulate_mwaa_environment()
 
@@ -151,7 +308,8 @@ with DAG(
         poll_interval=5,
     )
 
-    teardown_sus_domain = delete_sus_domain(domain_id=setup_sus_domain["domain_id"])
+    teardown_sus_domain = delete_sus_domain(
+        domain_id=setup_sus_domain["domain_id"])
 
     chain(
         # TEST SETUP
@@ -170,6 +328,7 @@ with DAG(
     # This test needs watcher in order to properly mark success/failure
     # when "tearDown" task with trigger rule is part of the DAG
     list(dag.tasks) >> watcher()
+
 
 from tests_common.test_utils.system_tests import get_test_run  # noqa: E402
 
