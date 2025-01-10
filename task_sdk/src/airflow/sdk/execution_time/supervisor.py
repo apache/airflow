5aed9b02986914a611bfbdf2c49613cbc2f369c0
Jed Cunningham <jedcunningham@apache.org>
2025-01-09 22:26:06 -0700

    Test a theory...

--- task_sdk/src/airflow/sdk/execution_time/supervisor.py
+++ task_sdk/src/airflow/sdk/execution_time/supervisor.py
@@ -254,6 +254,10 @@ def _fork_main(
                 atexit._run_exitfuncs()
                 base_exit(n)
 
+    from airflow import settings
+
+    settings.configure_orm()
+
     try:
         target()
         exit(0)
