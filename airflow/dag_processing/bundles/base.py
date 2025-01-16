c033a3ae5371ebb8f637f6cbdcacf0fed856829e
Daniel Standish <15932138+dstandish@users.noreply.github.com>
2025-01-16 10:18:27 -0800

    Add default for refresh_interval in dag bundles

--- airflow/dag_processing/bundles/base.py
+++ airflow/dag_processing/bundles/base.py
@@ -46,7 +46,7 @@ class BaseDagBundle(ABC):
 
     supports_versioning: bool = False
 
-    def __init__(self, *, name: str, refresh_interval: int, version: str | None = None) -> None:
+    def __init__(self, *, name: str, refresh_interval: int = 300, version: str | None = None) -> None:
         self.name = name
         self.version = version
         self.refresh_interval = refresh_interval
