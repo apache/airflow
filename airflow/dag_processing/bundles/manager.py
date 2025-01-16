e044c5fcf995926ed11965d6c8c75251d50968af
Jed Cunningham <jedcunningham@apache.org>
2025-01-16 13:02:58 -0700

    Move dags-folder bundle to LocalDagBundle; rename kwarg
    This moves the dags-folder bundle to use LocalDagBundle, instead of
    having its own class. A follow up will remove the `[core] dags_folder`
    config as well, but that will be much larger reaching.
    
    I've also renamed the `local_folder` kwarg to `directory` - just a nicer
    name for it.

--- airflow/dag_processing/bundles/manager.py
+++ airflow/dag_processing/bundles/manager.py
@@ -81,7 +81,7 @@ class DagBundlesManager(LoggingMixin):
                     "name": "example_dags",
                     "classpath": "airflow.dag_processing.bundles.local.LocalDagBundle",
                     "kwargs": {
-                        "local_folder": example_dag_folder,
+                        "directory": example_dag_folder,
                     },
                 }
             )
