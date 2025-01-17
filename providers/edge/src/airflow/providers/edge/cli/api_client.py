cca82b054b09774174626799610866abea47c90d
Majoros Donat (XC-DX/EET2-Bp) <donat.majoros2@hu.bosch.com>
2025-01-17 11:35:11 +0100

    try-except

--- providers/edge/src/airflow/providers/edge/cli/api_client.py
+++ providers/edge/src/airflow/providers/edge/cli/api_client.py
@@ -83,7 +83,10 @@ def _make_generic_request(method: str, rest_path: str, data: str | None = None) 
     response = requests.request(method, url=api_endpoint, data=data, headers=headers)
     logger.info("Response text: %s", response.text)
     logger.info("Response headers: %s", response.headers)
-    logger.info("Response json: %s", response.json())
+    try:
+        logger.info("Response json: %s", response.json())
+    except:
+        pass
 
     response.raise_for_status()
     if response.status_code == HTTPStatus.NO_CONTENT:
