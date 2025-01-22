903e4fb0bce035ce3853a54c9649e16891b2e29e
Maksim Moiseenkov <moiseenkov@google.com>
2025-01-22 11:57:41 +0000

    Add config option [secrets]backends_order

--- airflow-core/src/airflow/ui/src/pages/Variables/Variables.tsx
+++ airflow-core/src/airflow/ui/src/pages/Variables/Variables.tsx
@@ -38,6 +38,7 @@ import { SearchParamsKeys, type SearchParamsKeysType } from "src/constants/searc
 import { useConfig } from "src/queries/useConfig.tsx";
 import { TrimText } from "src/utils/TrimText";
 
+import { BackendsOrderCard } from "./BackendsOrderCard";
 import DeleteVariablesButton from "./DeleteVariablesButton";
 import ImportVariablesButton from "./ImportVariablesButton";
 import AddVariableButton from "./ManageVariable/AddVariableButton";
@@ -208,6 +209,7 @@ export const Variables = () => {
         />
         <HStack gap={4} mt={2}>
           <ImportVariablesButton disabled={selectedRows.size > 0} />
+          <BackendsOrderCard />
           <Spacer />
           <ExpandCollapseButtons
             collapseLabel={translate("common:expand.collapse")}
