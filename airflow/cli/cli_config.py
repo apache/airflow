019e06f7663c9ffed998e043a7469560af0454e6
Ambika Garg <v-ambikagarg@microsoft.com>
2025-01-21 23:07:08 -0500

    Add latest-bundle-version argument to list_dags command and display bundle fields in the CLI

--- airflow/cli/cli_config.py
+++ airflow/cli/cli_config.py
@@ -172,7 +172,11 @@ ARG_BUNDLE_NAME = Arg(
         "--bundle-name",
     ),
     help=("The name of the DAG bundle to use."),
+    type=str,
     default=None,
+)
+ARG_LATEST_BUNDLE_VERSION = Arg(
+    ("-bv", "--latest-bundle-version"), help=("The latest version of the bundle."), type=str, default=None
 )
 ARG_START_DATE = Arg(("-s", "--start-date"), help="Override start_date YYYY-MM-DD", type=parsedate)
 ARG_END_DATE = Arg(("-e", "--end-date"), help="Override end_date YYYY-MM-DD", type=parsedate)
@@ -930,7 +934,7 @@ ARG_DAG_LIST_COLUMNS = Arg(
     ("--columns",),
     type=string_list_type,
     help="List of columns to render. (default: ['dag_id', 'fileloc', 'owner', 'is_paused'])",
-    default=("dag_id", "fileloc", "owners", "is_paused"),
+    default=("dag_id", "fileloc", "owners", "is_paused", "bundle_name", "latest_bundle_version"),
 )
 
 ARG_ASSET_LIST_COLUMNS = Arg(
@@ -1028,7 +1032,7 @@ DAGS_COMMANDS = (
         name="list",
         help="List all the DAGs",
         func=lazy_load_command("airflow.cli.commands.remote_commands.dag_command.dag_list_dags"),
-        args=(ARG_SUBDIR, ARG_OUTPUT, ARG_VERBOSE, ARG_DAG_LIST_COLUMNS, ARG_BUNDLE_NAME),
+        args=(ARG_OUTPUT, ARG_VERBOSE, ARG_DAG_LIST_COLUMNS, ARG_BUNDLE_NAME, ARG_LATEST_BUNDLE_VERSION),
     ),
     ActionCommand(
         name="list-import-errors",
