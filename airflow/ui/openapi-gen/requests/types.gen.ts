9285cc7b1d252a7cc4189b7b8e3022c4c67e3fc2
Pierre Jeambrun <pierrejbrun@gmail.com>
2025-01-27 13:39:38 +0100

    AIP-84 Fix dag reports tests outside of breeze (#46125)

--- airflow/ui/openapi-gen/requests/types.gen.ts
+++ airflow/ui/openapi-gen/requests/types.gen.ts
@@ -1877,11 +1877,11 @@ export type GetDagStatsData = {
 
 export type GetDagStatsResponse = DagStatsCollectionResponse;
 
-export type GetDagReportData = {
+export type GetDagReportsData = {
   subdir: string;
 };
 
-export type GetDagReportResponse = unknown;
+export type GetDagReportsResponse = unknown;
 
 export type ListDagWarningsData = {
   dagId?: string | null;
@@ -3551,7 +3551,7 @@ export type $OpenApiTs = {
   };
   "/public/dagReports": {
     get: {
-      req: GetDagReportData;
+      req: GetDagReportsData;
       res: {
         /**
          * Successful Response
