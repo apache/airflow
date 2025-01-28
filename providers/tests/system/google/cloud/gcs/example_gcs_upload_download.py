15d2e200d8651cf5dbc55d5f06bc01f5457fe9a9
vatsrahul1001 <43964496+vatsrahul1001@users.noreply.github.com>
2025-01-29 00:34:28 +0530

    Fixing OPENLINEAGE system tests import failure after new structure changes (#46204)
    * fix OL system tests
    
    * fixing testx

--- providers/tests/system/google/cloud/gcs/example_gcs_upload_download.py
+++ providers/tests/system/google/cloud/gcs/example_gcs_upload_download.py
@@ -29,10 +29,10 @@ from airflow.models.dag import DAG
 from airflow.providers.google.cloud.operators.gcs import GCSCreateBucketOperator, GCSDeleteBucketOperator
 from airflow.providers.google.cloud.transfers.gcs_to_local import GCSToLocalFilesystemOperator
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+from airflow.providers.openlineage.tests.system.openlineage.operator import OpenLineageTestOperator
 from airflow.utils.trigger_rule import TriggerRule
 
 from providers.tests.system.google import DEFAULT_GCP_SYSTEM_TEST_PROJECT_ID
-from providers.tests.system.openlineage.operator import OpenLineageTestOperator
 
 ENV_ID = os.environ.get("SYSTEM_TESTS_ENV_ID", "default")
 PROJECT_ID = os.environ.get("SYSTEM_TESTS_GCP_PROJECT") or DEFAULT_GCP_SYSTEM_TEST_PROJECT_ID
