b1c31e88d152b4cddbd1b65959ad1a9eaa921053
Daniel Standish <15932138+dstandish@users.noreply.github.com>
2025-01-29 15:47:38 -0800

    fix moretests

--- tests_common/pytest_plugin.py
+++ tests_common/pytest_plugin.py
@@ -836,7 +836,6 @@ def dag_maker(request) -> Generator[DagMaker, None, None]:
             dag.clear(session=self.session)
             if AIRFLOW_V_3_0_PLUS:
                 dag.bulk_write_to_db(self.bundle_name, None, [dag], session=self.session)
-                dag.relative_fileloc = dag.fileloc
             else:
                 dag.sync_to_db(session=self.session)
 
