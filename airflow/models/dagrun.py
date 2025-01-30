70497c93520a48859008e1c1f249b89b3ab210fc
AutomationDev85 <96178949+AutomationDev85@users.noreply.github.com>
2025-01-30 20:59:49 +0100

    Enable already existing "task.scheduled_duration" metric (#46009)
    * Enable scheduled_dttm field in task_instance
    
    * Fixed unit tests
    
    * Fixed Unit test
    
    * Removed comment
    
    * Fixed unit tests
    
    ---------
    
    Co-authored-by: Marco Küttelwesch <marco.kuettelwesch@de.bosch.com>

--- airflow/models/dagrun.py
+++ airflow/models/dagrun.py
@@ -1662,6 +1662,7 @@ class DagRun(Base, LoggingMixin):
                     )
                     .values(
                         state=TaskInstanceState.SCHEDULED,
+                        scheduled_dttm=timezone.utcnow(),
                         try_number=case(
                             (
                                 or_(TI.state.is_(None), TI.state != TaskInstanceState.UP_FOR_RESCHEDULE),
