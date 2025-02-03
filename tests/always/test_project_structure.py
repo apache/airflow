56c5a55be9f301d5602ab8a07120d87f294d3df8
insomnes <denginm@gmail.com>
2025-02-03 13:57:58 +0100

    Remove delted deprecated k8s modules from tests files check

--- tests/always/test_project_structure.py
+++ tests/always/test_project_structure.py
@@ -92,8 +92,6 @@ class TestProjectStructure:
             "providers/tests/cncf/kubernetes/test_k8s_model.py",
             "providers/tests/cncf/kubernetes/test_kube_client.py",
             "providers/tests/cncf/kubernetes/test_kube_config.py",
-            "providers/tests/cncf/kubernetes/test_pod_generator_deprecated.py",
-            "providers/tests/cncf/kubernetes/test_pod_launcher_deprecated.py",
             "providers/tests/cncf/kubernetes/test_python_kubernetes_script.py",
             "providers/tests/cncf/kubernetes/test_secret.py",
             "providers/tests/cncf/kubernetes/triggers/test_kubernetes_pod.py",
