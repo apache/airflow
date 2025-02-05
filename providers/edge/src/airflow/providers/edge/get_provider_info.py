4842f680880da61ec8be1e860800bac5a86a11d4
Amogh Desai <amoghrajesh1999@gmail.com>
2025-02-05 20:28:26 +0530

    Undoing the retryhttp limitation (<1.3.0) (#46470)
    * Undoing the retryhttp limitation (<1.3.0)
    
    * adding !=1.3.0

--- providers/edge/src/airflow/providers/edge/get_provider_info.py
+++ providers/edge/src/airflow/providers/edge/get_provider_info.py
@@ -99,5 +99,5 @@ def get_provider_info():
                 },
             }
         },
-        "dependencies": ["apache-airflow>=2.10.0", "pydantic>=2.10.2", "retryhttp>=1.2.0,<1.3.0"],
+        "dependencies": ["apache-airflow>=2.10.0", "pydantic>=2.10.2", "retryhttp>=1.2.0,!=1.3.0"],
     }
