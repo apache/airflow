d2037acba08d989e6449913aff321aca494a0b8a
David Blain <info@dabla.be>
2025-02-05 20:40:59 +0100

    Fixed generated SQL MSSQL dialect when all columns are also primary keys (#46380)
    * refactor: Fixed case when all columns are part of primary key when generated replace sql statement for MSSQL
    
    * refactor: Reformatted TestMsSqlDialect
    
    * refactor: Reformatted create_db_api_hook
    
    ---------
    
    Co-authored-by: David Blain <david.blain@infrabel.be>

--- providers/microsoft/mssql/src/airflow/providers/microsoft/mssql/dialects/mssql.py
+++ providers/microsoft/mssql/src/airflow/providers/microsoft/mssql/dialects/mssql.py
@@ -55,10 +55,15 @@ class MsSqlDialect(Dialect):
         self.log.debug("primary_keys: %s", primary_keys)
         self.log.debug("columns: %s", columns)
 
-        return f"""MERGE INTO {table} WITH (ROWLOCK) AS target
+        sql = f"""MERGE INTO {table} WITH (ROWLOCK) AS target
             USING (SELECT {', '.join(map(lambda column: f'{self.placeholder} AS {self.escape_word(column)}', target_fields))}) AS source
-            ON {' AND '.join(map(lambda column: f'target.{self.escape_word(column)} = source.{self.escape_word(column)}', primary_keys))}
+            ON {' AND '.join(map(lambda column: f'target.{self.escape_word(column)} = source.{self.escape_word(column)}', primary_keys))}"""
+
+        if columns:
+            sql = f"""{sql}
             WHEN MATCHED THEN
-                UPDATE SET {', '.join(map(lambda column: f'target.{column} = source.{column}', columns))}
+                UPDATE SET {', '.join(map(lambda column: f'target.{column} = source.{column}', columns))}"""
+
+        return f"""{sql}
             WHEN NOT MATCHED THEN
                 INSERT ({', '.join(map(self.escape_word, target_fields))}) VALUES ({', '.join(map(lambda column: f'source.{self.escape_word(column)}', target_fields))});"""
