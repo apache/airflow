aaaea355d3adf430204d01f8fdb3bfafbd7c2bd9
Ash Berlin-Taylor <ash@apache.org>
2025-02-06 17:58:13 +0000

    Add dynamic task mapping into TaskSDK runtime (#46032)
    The big change here (other than just moving code around) is to introduce a
    conceptual separation between Definition/Execution time and Scheduler time.
    
    This means that the expansion of tasks (creating the TaskInstance rows with
    different map_index values) is now done on the scheduler, and we now
    deserialize to different classes. For example, when we deserialize the
    `DictOfListsExpandInput` it gets turned into an instance of
    SchedulerDictOfListsExpandInput. This is primarily designed so that DB access
    is kept 100% out of the TaskSDK.
    
    Some of the changes here are on the "wat" side of the scale, and this is
    mostly designed to not break 100% of our tests, and we have #45549 to look at
    that more holistically.
    
    To support the "reduce" style task which takes as input a sequence of all the
    pushed (mapped) XCom values, and to keep the previous behaviour of not loading
    all values in to memory at once, we have added a new HEAD route to the Task
    Execution interface that returns the number of mapped XCom values so that it
    is possible to implement `__len__` on the new LazyXComSequence class.
    
    I have deleted a tranche of tests from tests/models that were to do with
    runtime behavoir and and now tested in the TaskSDK instead.

--- airflow/api_fastapi/execution_api/datamodels/taskinstance.py
+++ airflow/api_fastapi/execution_api/datamodels/taskinstance.py
@@ -14,11 +14,11 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-
 from __future__ import annotations
 
 import uuid
 from datetime import timedelta
+from enum import Enum
 from typing import Annotated, Any, Literal, Union
 
 from pydantic import (
@@ -60,15 +60,20 @@ class TIEnterRunningPayload(StrictBaseModel):
     """When the task started executing"""
 
 
+# Create an enum to give a nice name in the generated datamodels
+class TerminalStateNonSuccess(str, Enum):
+    """TaskInstance states that can be reported without extra information."""
+
+    FAILED = TerminalTIState.FAILED
+    SKIPPED = TerminalTIState.SKIPPED
+    REMOVED = TerminalTIState.REMOVED
+    FAIL_WITHOUT_RETRY = TerminalTIState.FAIL_WITHOUT_RETRY
+
+
 class TITerminalStatePayload(StrictBaseModel):
     """Schema for updating TaskInstance to a terminal state except SUCCESS state."""
 
-    state: Literal[
-        TerminalTIState.FAILED,
-        TerminalTIState.SKIPPED,
-        TerminalTIState.REMOVED,
-        TerminalTIState.FAIL_WITHOUT_RETRY,
-    ]
+    state: TerminalStateNonSuccess
 
     end_date: UtcDateTime
     """When the task completed executing"""
@@ -241,6 +246,8 @@ class TIRunContext(BaseModel):
 
     connections: Annotated[list[ConnectionResponse], Field(default_factory=list)]
     """Connections that can be accessed by the task instance."""
+
+    upstream_map_indexes: dict[str, int] | None = None
 
 
 class PrevSuccessfulDagRunResponse(BaseModel):
