1a73d6e2ab567aa5ef2d659560720e6a418fe12f
Majoros Donat (XC-DX/EET2-Bp) <donat.majoros2@hu.bosch.com>
2025-02-10 12:53:27 +0100

    try session.commit

--- providers/edge/src/airflow/providers/edge/executors/edge_executor.py
+++ providers/edge/src/airflow/providers/edge/executors/edge_executor.py
@@ -86,9 +86,10 @@ class EdgeExecutor(BaseExecutor):
 
         # version 0.14.0pre0 added new column maintenance_comment
         if edge_worker_columns and "maintenance_comment" not in edge_worker_columns:
-            connection = engine.connect().execution_options(isolation_level="AUTOCOMMIT")
+            connection = engine.connect()
             query = "ALTER TABLE edge_worker ADD maintenance_comment VARCHAR(1024);"
             connection.execute(text(query))
+            session.commit()
 
     @provide_session
     def start(self, session: Session = NEW_SESSION):
