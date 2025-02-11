7b1436d9760fd59093493930dce5ea559d6980aa
Wei Lee <weilee.rx@gmail.com>
2025-02-11 23:26:25 +0800

    test(pytest_plugin): add DagRun.DATASET_TRIGERED for backward compat

--- tests_common/pytest_plugin.py
+++ tests_common/pytest_plugin.py
@@ -893,6 +893,8 @@ def dag_maker(request) -> Generator[DagMaker, None, None]:
 
             if AIRFLOW_V_3_0_PLUS:
                 from airflow.utils.types import DagRunTriggeredByType
+            else:
+                DagRunType.ASSET_TRIGGERED = DagRunType.DATASET_TRIGGERED
 
             if "execution_date" in kwargs:
                 raise TypeError("use logical_date instead")
