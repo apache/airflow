d5b9d630c4d10267e7ead291da8850deece3ef9f
Tzu-ping Chung <uranusjr@gmail.com>
2025-02-17 14:33:26 +0800

    Add REST API endpoint to materialize asset (#46718)
    This is done by triggering the DAG that has the given asset as a task
    outlet. 409 is returned if more than one DAG uses the asset as outlet.

--- airflow/ui/openapi-gen/queries/common.ts
+++ airflow/ui/openapi-gen/queries/common.ts
@@ -1704,6 +1704,9 @@ export const UseLoginServiceLoginKeyFn = (
 export type AssetServiceCreateAssetEventMutationResult = Awaited<
   ReturnType<typeof AssetService.createAssetEvent>
 >;
+export type AssetServiceMaterializeAssetMutationResult = Awaited<
+  ReturnType<typeof AssetService.materializeAsset>
+>;
 export type BackfillServiceCreateBackfillMutationResult = Awaited<
   ReturnType<typeof BackfillService.createBackfill>
 >;
