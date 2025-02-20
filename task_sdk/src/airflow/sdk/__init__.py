f0524db873e2c0c68c8cd6f4d0cc0bf8165caa85
Jed Cunningham <jedcunningham@apache.org>
2025-02-20 14:33:45 -0700

    Bump task-sdk version to 1.0.0.b1

--- task_sdk/src/airflow/sdk/__init__.py
+++ task_sdk/src/airflow/sdk/__init__.py
@@ -43,7 +43,7 @@ __all__ = [
     "literal",
 ]
 
-__version__ = "1.0.0.alpha1"
+__version__ = "1.0.0.b1"
 
 if TYPE_CHECKING:
     from airflow.sdk.definitions.asset import Asset, AssetAlias, AssetAll, AssetAny, AssetWatcher
