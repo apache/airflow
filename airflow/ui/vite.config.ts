29590324a91b4389152663c963e3c7514b76339a
pierrejeambrun <pierrejbrun@gmail.com>
2025-02-21 16:20:57 +0100

    Fix pin icon

--- airflow/ui/vite.config.ts
+++ airflow/ui/vite.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
     {
       name: "transform-url-src",
       transformIndexHtml: (html) =>
-        html.replace(`src="./assets/`, `src="./static/assets/`).replace(`href="/`, `href="/webapp/`),
+        html.replace(`src="./assets/`, `src="./static/assets/`).replace(`href="/`, `href="./`),
     },
     cssInjectedByJsPlugin(),
   ],
