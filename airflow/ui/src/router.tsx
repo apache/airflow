cfcb7bd59dba4ccd7117dcb621dddee8875b887f
Jed Cunningham <jedcunningham@apache.org>
2025-02-24 16:33:03 -0700

    Fix react tests

--- airflow/ui/src/router.tsx
+++ airflow/ui/src/router.tsx
@@ -168,7 +168,7 @@ export const routerConfig = [
   },
 ];
 
-const baseUrl = document.querySelector("base")!.href;
+const baseUrl = document.querySelector("base")?.href ?? "http://localhost:9091/";
 const basename = new URL(baseUrl).pathname;
 
 export const router = createBrowserRouter(routerConfig, { basename });
