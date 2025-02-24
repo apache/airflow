32ec8a6e59fa038c08b7c7a91a4d18e712eb232b
Yusuf Quazi <111078670+yquaziii@users.noreply.github.com>
2025-02-25 01:11:46 +0530

    Update test_list_failed_runs.py
    updated to logical_date

--- tests/cli/commands/test_list_failed_runs.py
+++ tests/cli/commands/test_list_failed_runs.py
@@ -16,9 +16,9 @@ class TestListFailedRuns(unittest.TestCase):
         mock_utcnow.return_value = fixed_now
         since_24h_ago = fixed_now - timedelta(hours=24)
 
-        mock_dag_run1 = Mock(dag_id="dag1", execution_date=fixed_now - timedelta(hours=1), state="failed")
-        mock_dag_run3 = Mock(dag_id="dag2", execution_date=fixed_now - timedelta(hours=2), state="failed")
-        mock_dag_run5 = Mock(dag_id="dag4", execution_date=fixed_now - timedelta(minutes=10), state="failed")
+        mock_dag_run1 = Mock(dag_id="dag1", logical_date=fixed_now - timedelta(hours=1), state="failed")
+        mock_dag_run3 = Mock(dag_id="dag2", logical_date=fixed_now - timedelta(hours=2), state="failed")
+        mock_dag_run5 = Mock(dag_id="dag4", logical_date=fixed_now - timedelta(minutes=10), state="failed")
 
         mock_dag_run_find.return_value = [mock_dag_run1, mock_dag_run3, mock_dag_run5]
 
