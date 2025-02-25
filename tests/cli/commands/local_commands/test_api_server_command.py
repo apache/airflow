4a5dd8557d44552603073f9250c89d311ef856da
Jed Cunningham <jedcunningham@apache.org>
2025-02-25 10:45:10 -0700

    Move `fastapi-api` command to `api-server`

--- tests/cli/commands/local_commands/test_api_server_command.py
+++ tests/cli/commands/local_commands/test_api_server_command.py
@@ -21,7 +21,7 @@ from unittest import mock
 import pytest
 from rich.console import Console
 
-from airflow.cli.commands.local_commands import fastapi_api_command
+from airflow.cli.commands.local_commands import api_server_command
 from airflow.exceptions import AirflowConfigException
 
 from tests.cli.commands._common_cli_classes import _CommonCLIGunicornTestClass
@@ -31,13 +31,13 @@ console = Console(width=400, color_system="standard")
 
 @pytest.mark.db_test
 class TestCliFastAPI(_CommonCLIGunicornTestClass):
-    main_process_regexp = r"airflow fastapi-api"
+    main_process_regexp = r"airflow api-server"
 
     @pytest.mark.parametrize(
         "args, expected_command",
         [
             (
-                ["fastapi-api", "--port", "9092", "--hostname", "somehost", "--debug"],
+                ["api-server", "--port", "9092", "--hostname", "somehost", "--debug"],
                 [
                     "fastapi",
                     "dev",
@@ -49,7 +49,7 @@ class TestCliFastAPI(_CommonCLIGunicornTestClass):
                 ],
             ),
             (
-                ["fastapi-api", "--port", "9092", "--hostname", "somehost", "--debug", "--proxy-headers"],
+                ["api-server", "--port", "9092", "--hostname", "somehost", "--debug", "--proxy-headers"],
                 [
                     "fastapi",
                     "dev",
@@ -68,7 +68,7 @@ class TestCliFastAPI(_CommonCLIGunicornTestClass):
             mock.patch("subprocess.Popen") as Popen,
         ):
             args = self.parser.parse_args(args)
-            fastapi_api_command.fastapi_api(args)
+            api_server_command.api_server(args)
 
             Popen.assert_called_with(
                 expected_command,
@@ -78,7 +78,7 @@ class TestCliFastAPI(_CommonCLIGunicornTestClass):
     def test_cli_fastapi_api_env_var_set_unset(self, app):
         """
         Test that AIRFLOW_API_APPS is set and unset in the environment when
-        calling the airflow fastapi-api command
+        calling the airflow api-server command
         """
         with (
             mock.patch("subprocess.Popen") as Popen,
@@ -90,14 +90,14 @@ class TestCliFastAPI(_CommonCLIGunicornTestClass):
 
             # Parse the command line arguments
             args = self.parser.parse_args(
-                ["fastapi-api", "--port", port, "--hostname", hostname, "--apps", apps_value, "--debug"]
+                ["api-server", "--port", port, "--hostname", hostname, "--apps", apps_value, "--debug"]
             )
 
             # Ensure AIRFLOW_API_APPS is not set initially
             mock_environ.get.return_value = None
 
             # Call the fastapi_api command
-            fastapi_api_command.fastapi_api(args)
+            api_server_command.api_server(args)
 
             # Assert that AIRFLOW_API_APPS was set in the environment before subprocess
             mock_environ.__setitem__.assert_called_with("AIRFLOW_API_APPS", apps_value)
@@ -127,7 +127,7 @@ class TestCliFastAPI(_CommonCLIGunicornTestClass):
         ):
             args = self.parser.parse_args(
                 [
-                    "fastapi-api",
+                    "api-server",
                     "--access-logformat",
                     "custom_log_format",
                     "--pid",
@@ -140,7 +140,7 @@ class TestCliFastAPI(_CommonCLIGunicornTestClass):
                     "core",
                 ]
             )
-            fastapi_api_command.fastapi_api(args)
+            api_server_command.api_server(args)
 
             mock_run.assert_called_with(
                 "airflow.api_fastapi.main:app",
@@ -164,17 +164,17 @@ class TestCliFastAPI(_CommonCLIGunicornTestClass):
         ],
     )
     def test_get_ssl_cert_and_key_filepaths_with_incorrect_usage(self, ssl_arguments, error_pattern):
-        args = self.parser.parse_args(["fastapi-api"] + ssl_arguments)
+        args = self.parser.parse_args(["api-server"] + ssl_arguments)
         with pytest.raises(AirflowConfigException, match=error_pattern):
-            fastapi_api_command._get_ssl_cert_and_key_filepaths(args)
+            api_server_command._get_ssl_cert_and_key_filepaths(args)
 
     def test_get_ssl_cert_and_key_filepaths_with_correct_usage(self, ssl_cert_and_key):
         cert_path, key_path = ssl_cert_and_key
 
         args = self.parser.parse_args(
-            ["fastapi-api"] + ["--ssl-cert", str(cert_path), "--ssl-key", str(key_path)]
+            ["api-server"] + ["--ssl-cert", str(cert_path), "--ssl-key", str(key_path)]
         )
-        assert fastapi_api_command._get_ssl_cert_and_key_filepaths(args) == (str(cert_path), str(key_path))
+        assert api_server_command._get_ssl_cert_and_key_filepaths(args) == (str(cert_path), str(key_path))
 
     @pytest.fixture
     def ssl_cert_and_key(self, tmp_path):
