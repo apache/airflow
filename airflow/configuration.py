4b171befbf35f22870dc14c3388cfe0ee78d67f5
LIU ZHE YOU <zhu424.dev@gmail.com>
2025-02-26 15:28:24 +0800

    fixup! Add docstring for lookup_from_deprecated

--- airflow/configuration.py
+++ airflow/configuration.py
@@ -1260,6 +1260,7 @@ class AirflowConfigParser(ConfigParser):
 
         :param section: section to get option from
         :param option: option to get
+        :param lookup_from_deprecated: If True, check if the option is defined in deprecated sections
         :return:
         """
         try:
