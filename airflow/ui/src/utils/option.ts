0025835a8b80bf36010bc2c6037cf53095714cdd
pierrejeambrun <pierrejbrun@gmail.com>
2025-02-26 19:54:22 +0100

    AIP-38 Factorize Option type

--- airflow/ui/src/utils/option.ts
+++ airflow/ui/src/utils/option.ts
@@ -16,14 +16,9 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-import { chakraComponents } from "chakra-react-select";
-import type { DropdownIndicatorProps } from "chakra-react-select";
-import { FiSearch } from "react-icons/fi";
 
-import type { Option } from "src/utils/option";
-
-export const DropdownIndicator: React.FC<DropdownIndicatorProps<Option, false>> = (props) => (
-  <chakraComponents.DropdownIndicator {...props}>
-    <FiSearch />
-  </chakraComponents.DropdownIndicator>
-);
+export type Option = {
+  readonly disabled?: boolean;
+  readonly label: string;
+  readonly value: string;
+};
