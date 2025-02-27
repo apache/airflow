fdb0023d834f990cb16f1d89df43d321a553fca0
Ephraim Anierobi <splendidzigy24@gmail.com>
2025-02-27 18:05:26 +0100

    fixup! fixup! fixup! fixup! Make try_id a primary key of task_instance_history table

--- airflow/models/taskinstancehistory.py
+++ airflow/models/taskinstancehistory.py
@@ -61,12 +61,12 @@ class TaskInstanceHistory(Base):
     """
 
     __tablename__ = "task_instance_history"
-    try_id = Column(UUIDType(binary=False), nullable=False, primary_key=True)
-    id = Column(
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    task_instance_id = Column(
         String(36).with_variant(postgresql.UUID(as_uuid=False), "postgresql"),
         nullable=False,
     )
-
+    try_id = Column(UUIDType(binary=False), nullable=False, unique=True)
     task_id = Column(StringID(), nullable=False)
     dag_id = Column(StringID(), nullable=False)
     run_id = Column(StringID(), nullable=False)
@@ -117,6 +117,11 @@ class TaskInstanceHistory(Base):
     ):
         super().__init__()
         for column in self.__table__.columns:
+            if column.name == "id":
+                continue
+            if column.name == "task_instance_id":
+                setattr(self, column.name, ti.id)
+                continue
             setattr(self, column.name, getattr(ti, column.name))
 
         if state:
@@ -151,7 +156,11 @@ class TaskInstanceHistory(Base):
         """Record a TaskInstance to TaskInstanceHistory."""
         exists_q = session.scalar(
             select(func.count(TaskInstanceHistory.task_id)).where(
-                TaskInstanceHistory.try_id == ti.try_id,
+                TaskInstanceHistory.dag_id == ti.dag_id,
+                TaskInstanceHistory.task_id == ti.task_id,
+                TaskInstanceHistory.run_id == ti.run_id,
+                TaskInstanceHistory.map_index == ti.map_index,
+                TaskInstanceHistory.try_number == ti.try_number,
             )
         )
         if exists_q:
