aa61371a071c92214bcc1bd29842fc8d4b0e33a2
Ash Berlin-Taylor <ash@apache.org>
2025-02-27 10:53:29 +0000

    Render structured logs in the new UI rather than showing raw JSON (#46827)
    * Render structured logs in the new UI rather than showing raw JSON
    
    There are multiple parts to this PR;
    
    First off: the log reader interface was _a mess_ There was some odd+old code
    do deal with reading from multiple hosts that made the message confusing. This
      was added for smart sensors (which went away in v2.4 or v2.5) but this mess
      remained, and reading from multiple hosts is handled differently now.
    
    This PR keeps the current "parse+interleave" behaviour (though it's debatable
    if the interleave feature is needed specifically, or if we could get away with
    a simpler concat instead. Future work there if anyone wants to think about and
    tackle this.) but changes the JSON resposne type from a single string (the
    value of which was previoulsy a mess of double encoded JSON and repr of a
    python tuple making it impossible to do anything but display at) to a list of
    either strings (when it can't be parsed) or a list of
    dicts/StructuredLogMessage.
    
    I have also done some cursory rendering/displaying of these structured log
    messages in the UI, but they could be greatly improved by adding colors to
    various components of the log.
    
    The current rendered HTML looks like this:
    
    ```html
    <p><span class="event">::group::Log message source details</span> <span class="log-key sources">sources=["/root/airflow/logs/dag_id=trigger_test/run_id=manual__2025-02-16T12:23:29.118614+00:00_gOTl0Qub/task_id=waiter/attempt=1.log","/root/airflow/logs/dag_id=trigger_test/run_id=manual__2025-02-16T12:23:29.118614+00:00_gOTl0Qub/task_id=waiter/attempt=1.log.trigger.14.log"]</span></p>
    <p><span class="event">::endgroup::</span></p>
    <p>[<time datetime="2025-02-16T12:23:30.033308">2025-02-16T12:23:30.033308</time>] <span class="log-level debug">DEBUG</span> - <span class="event">Hook impls: []</span> <span class="log-key logger">logger="airflow.listeners.listener"</span></p>
    ```
    
    Although not used by the UI, the non-application/json content type is now
    updated to a) include the continuation token as a header, and to set the
    content type as application/x-ndjson
    
    * Fix typescript useLogs
    
    * style: group metadata pop
    
    * style: reduce if-else and directly use bool for assigning metadata["download_logs"]
    
    * style: improve type annotation
    
    * test(test_log_reader): fix existing unit tests
    
    * test(api_fastapi): fix existing test_log unit tests
    
    * feat(api_connexion/log): update v1 api to the latest log format
    
    * test(providers/elasticsearch): fix part of the existing unit test
    
    * test(providers/amazon): fix TestCloudwatchTaskHandler::test_read
    
    * feat(providers/amazon): add airflow 3 compat logic
    
    * feat(providers/google): add airflow 3 task handler log handling logic
    
    * feat(providers/elasticsearch): add airflow 3 task handler log handling logic
    
    * feat(providers/microsoft): add airflow 3 task handler log handling logic
    
    * feat(providers/redis): add airflow 3 task handler log handling logic
    
    * feat(providers/opensearch): add airflow 3 task handler log handling logic
    
    * test: ignore unneeded tests
    
    * test(log_handlers): fix pendulum.tz version imcompat
    
    * feat: force StructuredLogMessage check when initialing
    
    ---------
    
    Co-authored-by: Brent Bovenzi <brent.bovenzi@gmail.com>
    Co-authored-by: Wei Lee <weilee.rx@gmail.com>

--- airflow/ui/src/queries/useLogs.tsx
+++ airflow/ui/src/queries/useLogs.tsx
@@ -19,7 +19,11 @@
 import dayjs from "dayjs";
 
 import { useTaskInstanceServiceGetLog } from "openapi/queries";
-import type { TaskInstanceResponse } from "openapi/requests/types.gen";
+import type {
+  StructuredLogMessage,
+  TaskInstanceResponse,
+  TaskInstancesLogResponse,
+} from "openapi/requests/types.gen";
 import { isStatePending, useAutoRefresh } from "src/utils";
 
 type Props = {
@@ -29,28 +33,57 @@ type Props = {
 };
 
 type ParseLogsProps = {
-  data: string | undefined;
+  data: TaskInstancesLogResponse["content"];
+};
+
+const renderStructuredLog = (logMessage: string | StructuredLogMessage, index: number) => {
+  if (typeof logMessage === "string") {
+    return <p key={index}>{logMessage}</p>;
+  }
+
+  const { event, level = undefined, timestamp, ...structured } = logMessage;
+  const elements = [];
+
+  if (Boolean(timestamp)) {
+    elements.push("[", <time dateTime={timestamp}>{timestamp}</time>, "] ");
+  }
+
+  if (typeof level === "string") {
+    elements.push(<span className={`log-level ${level}`}>{level.toUpperCase()}</span>, " - ");
+  }
+
+  elements.push(<span className="event">{event}</span>);
+
+  for (const key in structured) {
+    if (Object.hasOwn(structured, key)) {
+      elements.push(
+        " ",
+        <span className={`log-key ${key}`}>
+          {key}={JSON.stringify(structured[key])}
+        </span>,
+      );
+    }
+  }
+
+  return <p key={index}>{elements}</p>;
 };
 
 // TODO: add support for log groups, colors, formats, filters
 const parseLogs = ({ data }: ParseLogsProps) => {
-  if (data === undefined) {
-    return {};
-  }
-  let lines;
-
   let warning;
+  let parsedLines;
 
   try {
-    lines = data.split("\\n");
-  } catch {
+    parsedLines = data.map((datum, index) => renderStructuredLog(datum, index));
+  } catch (error) {
+    const errorMessage = error instanceof Error ? error.message : "An error occurred.";
+
+    // eslint-disable-next-line no-console
+    console.warn(`Error parsing logs: ${errorMessage}`);
     warning = "Unable to show logs. There was an error parsing logs.";
 
     return { data, warning };
   }
-
-  // eslint-disable-next-line react/no-array-index-key
-  const parsedLines = lines.map((line, index) => <p key={index}>{line}</p>);
 
   return {
     fileSources: [],
@@ -82,7 +115,7 @@ export const useLogs = ({ dagId, taskInstance, tryNumber = 1 }: Props) => {
   );
 
   const parsedData = parseLogs({
-    data: data?.content,
+    data: data?.content ?? [],
   });
 
   return { data: parsedData, ...rest };
