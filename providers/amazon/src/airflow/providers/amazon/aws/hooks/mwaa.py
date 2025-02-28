7fe8c87f2d8a8d39081a6cd2db68fceed62e0106
Ramit Kataria <ramitkat@amazon.com>
2025-02-28 14:48:31 -0800

    Add fallback parameter and remove storing of sessions in hook

--- providers/amazon/src/airflow/providers/amazon/aws/hooks/mwaa.py
+++ providers/amazon/src/airflow/providers/amazon/aws/hooks/mwaa.py
@@ -18,8 +18,6 @@
 
 from __future__ import annotations
 
-from typing import Any
-
 import requests
 from botocore.exceptions import ClientError
 
@@ -32,8 +30,10 @@ class MwaaHook(AwsBaseHook):
 
     Provide thin wrapper around :external+boto3:py:class:`boto3.client("mwaa") <MWAA.Client>`
 
-    If your IAM policy doesn't have `airflow:InvokeRestApi` permission or if you reach throttling capacity, the
-    hook will use a session token to make the requests. Learn more here:
+    If your IAM policy doesn't have `airflow:InvokeRestApi` permission, the hook will use a fallback method
+    that uses the AWS credential to create a web login token for the Airflow Web UI and then directly make
+    requests to the Airflow API. This fallback method can be set as the default (and only) method used by
+    setting `only_use_web_login` to True.  Learn more here:
     https://docs.aws.amazon.com/mwaa/latest/userguide/access-mwaa-apache-airflow-rest-api.html#granting-access-MWAA-Enhanced-REST-API
 
     Additional arguments (such as ``aws_conn_id``) may be specified and
@@ -46,7 +46,6 @@ class MwaaHook(AwsBaseHook):
     def __init__(self, *args, **kwargs) -> None:
         kwargs["client_type"] = "mwaa"
         super().__init__(*args, **kwargs)
-        self._env_to_session_conn_map: dict[str, dict[str, Any]] = {}
 
     def invoke_rest_api(
         self,
@@ -55,6 +54,7 @@ class MwaaHook(AwsBaseHook):
         method: str,
         body: dict | None = None,
         query_params: dict | None = None,
+        only_use_web_login: bool = False,
     ) -> dict:
         """
         Invoke the REST API on the Airflow webserver with the specified inputs.
@@ -67,6 +67,8 @@ class MwaaHook(AwsBaseHook):
         :param method: HTTP method used for making Airflow REST API calls: 'GET'|'PUT'|'POST'|'PATCH'|'DELETE'
         :param body: Request body for the Apache Airflow REST API call
         :param query_params: Query parameters to be included in the Apache Airflow REST API call
+        :param only_use_web_login: If True, only the web login method is used without trying boto's
+            invoke_rest_api first
         """
         # Filter out keys with None values because Airflow REST API doesn't accept requests otherwise
         body = {k: v for k, v in body.items() if v is not None} if body else {}
@@ -78,6 +80,10 @@ class MwaaHook(AwsBaseHook):
             "Body": body,
             "QueryParameters": query_params,
         }
+
+        if only_use_web_login:
+            return self._invoke_rest_api_using_web_login(**api_kwargs)
+
         try:
             response = self.conn.invoke_rest_api(**api_kwargs)
             # ResponseMetadata is removed because it contains data that is either very unlikely to be useful
@@ -89,9 +95,9 @@ class MwaaHook(AwsBaseHook):
             if e.response["Error"]["Code"] == "AccessDeniedException":
                 self.log.info(
                     "Access Denied, possibly due to missing airflow:InvokeRestApi in IAM policy. "
-                    "Trying again with session token..."
+                    "Trying again using web login..."
                 )
-                return self._invoke_rest_api_using_session_token(env_name, path, method, body, query_params)
+                return self._invoke_rest_api_using_web_login(**api_kwargs)
             else:
                 to_log = e.response
                 # ResponseMetadata is removed because it contains data that is either very unlikely to be
@@ -100,31 +106,20 @@ class MwaaHook(AwsBaseHook):
                 self.log.error(to_log)
                 raise e
 
-    def _invoke_rest_api_using_session_token(
+    def _invoke_rest_api_using_web_login(
         self,
-        env_name: str,
-        path: str,
-        method: str,
-        body: dict | None = None,
-        query_params: dict | None = None,
+        **api_kwargs,
     ) -> dict:
-        def try_request():
-            conn_info = self._env_to_session_conn_map[env_name]
-            response = conn_info["session"].request(
-                method=method,
-                url=f"https://{conn_info['hostname']}/api/v1{path}",
-                params=query_params,
-                json=body,
-                timeout=10,
-            )
-            response.raise_for_status()
-            return response
+        session, hostname = self._get_session_conn(api_kwargs["Name"])
 
-        try:
-            response = try_request()
-        except (requests.exceptions.HTTPError, KeyError):
-            self._update_session_conn(env_name)
-            response = try_request()
+        response = session.request(
+            method=api_kwargs["Method"],
+            url=f"https://{hostname}/api/v1{api_kwargs['Path']}",
+            params=api_kwargs["QueryParameters"],
+            json=api_kwargs["Body"],
+            timeout=10,
+        )
+        response.raise_for_status()
 
         return {
             "RestApiStatusCode": response.status_code,
@@ -132,7 +127,7 @@ class MwaaHook(AwsBaseHook):
         }
 
     # Based on: https://docs.aws.amazon.com/mwaa/latest/userguide/access-mwaa-apache-airflow-rest-api.html#create-web-server-session-token
-    def _update_session_conn(self, env_name: str):
+    def _get_session_conn(self, env_name: str) -> tuple:
         create_token_response = self.conn.create_web_login_token(Name=env_name)
         web_server_hostname = create_token_response["WebServerHostname"]
         web_token = create_token_response["WebToken"]
@@ -143,4 +138,4 @@ class MwaaHook(AwsBaseHook):
         login_response = session.post(login_url, data=login_payload, timeout=10)
         login_response.raise_for_status()
 
-        self._env_to_session_conn_map[env_name] = {"session": session, "hostname": web_server_hostname}
+        return session, web_server_hostname
