d7a932e0d0c59e2aa4b6355dd11728f0999d8cae
kalyanr <kalyan.ben10@live.com>
2025-03-01 19:01:08 +0530

    add dag_stats

--- airflow/api_fastapi/core_api/routes/public/dag_stats.py
+++ airflow/api_fastapi/core_api/routes/public/dag_stats.py
@@ -21,6 +21,7 @@ from typing import Annotated
 
 from fastapi import Depends, status
 
+from airflow.api_fastapi.auth.managers.models.resource_details import DagAccessEntity
 from airflow.api_fastapi.common.db.common import (
     SessionDep,
     paginated_select,
@@ -38,6 +39,7 @@ from airflow.api_fastapi.core_api.datamodels.dag_stats import (
     DagStatsStateResponse,
 )
 from airflow.api_fastapi.core_api.openapi.exceptions import create_openapi_http_exception_doc
+from airflow.api_fastapi.core_api.security import requires_access_dag
 from airflow.models.dagrun import DagRun
 from airflow.utils.state import DagRunState
 
@@ -52,6 +54,7 @@ dag_stats_router = AirflowRouter(tags=["DagStats"], prefix="/dagStats")
             status.HTTP_404_NOT_FOUND,
         ]
     ),
+    dependencies=[Depends(requires_access_dag(method="GET", access_entity=DagAccessEntity.RUN))],
 )
 def get_dag_stats(
     session: SessionDep,
