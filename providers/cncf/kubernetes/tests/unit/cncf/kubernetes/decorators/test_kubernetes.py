7555ee63664eab14e0697152c1e858acf25a85c7
insomnes <denginm@gmail.com>
2025-03-02 19:11:02 +0100

    Finaliaze decorator changes

--- providers/cncf/kubernetes/tests/unit/cncf/kubernetes/decorators/test_kubernetes.py
+++ providers/cncf/kubernetes/tests/unit/cncf/kubernetes/decorators/test_kubernetes.py
@@ -18,116 +18,104 @@ from __future__ import annotations
 
 import base64
 import pickle
-from unittest import mock
-
-import pytest
 
 from airflow.decorators import task
-
-pytestmark = pytest.mark.db_test
-
+from unit.cncf.kubernetes.decorators.test_kubernetes_commons import TestKubernetesDecoratorsBase
 
 XCOM_IMAGE = "XCOM_IMAGE"
 
 
-def test_basic_kubernetes(dag_maker, session, mock_create_pod: mock.Mock, mock_hook: mock.Mock) -> None:
-    with dag_maker(session=session) as dag:
+class TestKubernetesDecorator(TestKubernetesDecoratorsBase):
+    def test_basic_kubernetes(self):
+        """Test basic proper KubernetesPodOperator creation from @task.kubernetes decorator"""
+        with self.dag:
 
-        @task.kubernetes(
-            image="python:3.10-slim-buster",
+            @task.kubernetes(
+                image="python:3.10-slim-buster",
+                in_cluster=False,
+                cluster_context="default",
+                config_file="/tmp/fake_file",
+                namespace="default",
+            )
+            def f():
+                import random
+
+                return [random.random() for _ in range(100)]
+
+            k8s_task = f()
+
+        self.execute_task(k8s_task)
+
+        self.mock_hook.assert_called_once_with(
+            conn_id="kubernetes_default",
             in_cluster=False,
             cluster_context="default",
             config_file="/tmp/fake_file",
-            namespace="default",
         )
-        def f():
-            import random
+        assert self.mock_create_pod.call_count == 1
 
-            return [random.random() for _ in range(100)]
+        containers = self.mock_create_pod.call_args.kwargs["pod"].spec.containers
+        assert len(containers) == 1
+        assert containers[0].command[0] == "bash"
+        assert len(containers[0].args) == 0
+        assert containers[0].env[0].name == "__PYTHON_SCRIPT"
+        assert containers[0].env[0].value
+        assert containers[0].env[1].name == "__PYTHON_INPUT"
 
-        f()
+        # Ensure we pass input through a b64 encoded env var
+        decoded_input = pickle.loads(base64.b64decode(containers[0].env[1].value))
+        assert decoded_input == {"args": [], "kwargs": {}}
 
-    dr = dag_maker.create_dagrun()
-    (ti,) = dr.task_instances
-    session.add(ti)
-    session.commit()
-    dag.get_task("f").execute(context=ti.get_template_context(session=session))
-    mock_hook.assert_called_once_with(
-        conn_id="kubernetes_default",
-        in_cluster=False,
-        cluster_context="default",
-        config_file="/tmp/fake_file",
-    )
-    assert mock_create_pod.call_count == 1
+    def test_kubernetes_with_input_output(self):
+        """Verify @task.kubernetes will run XCom container if do_xcom_push is set."""
+        with self.dag:
 
-    containers = mock_create_pod.call_args.kwargs["pod"].spec.containers
-    assert len(containers) == 1
-    assert containers[0].command[0] == "bash"
-    assert len(containers[0].args) == 0
-    assert containers[0].env[0].name == "__PYTHON_SCRIPT"
-    assert containers[0].env[0].value
-    assert containers[0].env[1].name == "__PYTHON_INPUT"
+            @task.kubernetes(
+                image="python:3.10-slim-buster",
+                in_cluster=False,
+                cluster_context="default",
+                config_file="/tmp/fake_file",
+                namespace="default",
+            )
+            def f(arg1, arg2, kwarg1=None, kwarg2=None):
+                return {"key1": "value1", "key2": "value2"}
 
-    # Ensure we pass input through a b64 encoded env var
-    decoded_input = pickle.loads(base64.b64decode(containers[0].env[1].value))
-    assert decoded_input == {"args": [], "kwargs": {}}
+            k8s_task = f.override(task_id="my_task_id", do_xcom_push=True)("arg1", "arg2", kwarg1="kwarg1")
 
+        self.mock_hook.return_value.get_xcom_sidecar_container_image.return_value = XCOM_IMAGE
+        self.mock_hook.return_value.get_xcom_sidecar_container_resources.return_value = {
+            "requests": {"cpu": "1m", "memory": "10Mi"},
+            "limits": {"cpu": "1m", "memory": "50Mi"},
+        }
 
-def test_kubernetes_with_input_output(
-    dag_maker, session, mock_create_pod: mock.Mock, mock_hook: mock.Mock
-) -> None:
-    with dag_maker(session=session) as dag:
+        self.execute_task(k8s_task)
+        assert self.mock_create_pod.call_count == 1
 
-        @task.kubernetes(
-            image="python:3.10-slim-buster",
+        self.mock_hook.assert_called_once_with(
+            conn_id="kubernetes_default",
             in_cluster=False,
             cluster_context="default",
             config_file="/tmp/fake_file",
-            namespace="default",
         )
-        def f(arg1, arg2, kwarg1=None, kwarg2=None):
-            return {"key1": "value1", "key2": "value2"}
+        assert self.mock_hook.return_value.get_xcom_sidecar_container_image.call_count == 1
+        assert self.mock_hook.return_value.get_xcom_sidecar_container_resources.call_count == 1
 
-        f.override(task_id="my_task_id", do_xcom_push=True)("arg1", "arg2", kwarg1="kwarg1")
+        containers = self.mock_create_pod.call_args.kwargs["pod"].spec.containers
 
-    mock_hook.return_value.get_xcom_sidecar_container_image.return_value = XCOM_IMAGE
-    mock_hook.return_value.get_xcom_sidecar_container_resources.return_value = {
-        "requests": {"cpu": "1m", "memory": "10Mi"},
-        "limits": {"cpu": "1m", "memory": "50Mi"},
-    }
+        # First container is Python script
+        assert len(containers) == 2
+        assert containers[0].command[0] == "bash"
+        assert len(containers[0].args) == 0
 
-    dr = dag_maker.create_dagrun()
-    (ti,) = dr.task_instances
-    session.add(dr)
-    session.commit()
-    dag.get_task("my_task_id").execute(context=ti.get_template_context(session=session))
+        assert containers[0].env[0].name == "__PYTHON_SCRIPT"
+        assert containers[0].env[0].value
+        assert containers[0].env[1].name == "__PYTHON_INPUT"
+        assert containers[0].env[1].value
 
-    mock_hook.assert_called_once_with(
-        conn_id="kubernetes_default",
-        in_cluster=False,
-        cluster_context="default",
-        config_file="/tmp/fake_file",
-    )
-    assert mock_create_pod.call_count == 1
-    assert mock_hook.return_value.get_xcom_sidecar_container_image.call_count == 1
-    assert mock_hook.return_value.get_xcom_sidecar_container_resources.call_count == 1
+        # Ensure we pass input through a b64 encoded env var
+        decoded_input = pickle.loads(base64.b64decode(containers[0].env[1].value))
+        assert decoded_input == {"args": ("arg1", "arg2"), "kwargs": {"kwarg1": "kwarg1"}}
 
-    containers = mock_create_pod.call_args.kwargs["pod"].spec.containers
-
-    # First container is Python script
-    assert len(containers) == 2
-    assert containers[0].command[0] == "bash"
-    assert len(containers[0].args) == 0
-
-    assert containers[0].env[0].name == "__PYTHON_SCRIPT"
-    assert containers[0].env[0].value
-    assert containers[0].env[1].name == "__PYTHON_INPUT"
-    assert containers[0].env[1].value
-
-    # Ensure we pass input through a b64 encoded env var
-    decoded_input = pickle.loads(base64.b64decode(containers[0].env[1].value))
-    assert decoded_input == {"args": ("arg1", "arg2"), "kwargs": {"kwarg1": "kwarg1"}}
-
-    # Second container is xcom image
-    assert containers[1].image == XCOM_IMAGE
-    assert containers[1].volume_mounts[0].mount_path == "/airflow/xcom"
+        # Second container is xcom image
+        assert containers[1].image == XCOM_IMAGE
+        assert containers[1].volume_mounts[0].mount_path == "/airflow/xcom"
