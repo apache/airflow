2fde54264c9becd30c5819bd957af5d1bb0dd949
insomnes <denginm@gmail.com>
2025-03-03 18:32:56 +0100

    Revert _get_async_hook delete

--- providers/cncf/kubernetes/src/airflow/providers/cncf/kubernetes/triggers/pod.py
+++ providers/cncf/kubernetes/src/airflow/providers/cncf/kubernetes/triggers/pod.py
@@ -274,14 +274,18 @@ class KubernetesPodTrigger(BaseTrigger):
             pod = cast(V1Pod, pod)
         return pod
 
-    @cached_property
-    def hook(self) -> AsyncKubernetesHook:
+    def _get_async_hook(self) -> AsyncKubernetesHook:
+        # TODO: Remove this method when the min version of kubernetes provider is 7.12.0 in Google provider.
         return AsyncKubernetesHook(
             conn_id=self.kubernetes_conn_id,
             in_cluster=self.in_cluster,
             config_dict=self.config_dict,
             cluster_context=self.cluster_context,
         )
+
+    @cached_property
+    def hook(self) -> AsyncKubernetesHook:
+        return self._get_async_hook()
 
     def define_container_state(self, pod: V1Pod) -> ContainerState:
         pod_containers = pod.status.container_statuses
