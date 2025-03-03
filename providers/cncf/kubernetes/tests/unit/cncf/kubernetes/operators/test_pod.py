4f39b8766686aedde576127315a7355b98c874c4
insomnes <denginm@gmail.com>
2025-03-03 19:36:19 +0100

    Log on any error status

--- providers/cncf/kubernetes/tests/unit/cncf/kubernetes/operators/test_pod.py
+++ providers/cncf/kubernetes/tests/unit/cncf/kubernetes/operators/test_pod.py
@@ -2091,10 +2091,11 @@ class TestKubernetesPodOperatorAsync:
         ti_mock.xcom_push.assert_any_call(key="pod_namespace", value=TEST_NAMESPACE)
         assert isinstance(exc.value.trigger, KubernetesPodTrigger)
 
+    @pytest.mark.parametrize("status", ["error", "failed", "timeout"])
     @patch(KUB_OP_PATH.format("log"))
     @patch(KUB_OP_PATH.format("cleanup"))
     @patch(HOOK_CLASS)
-    def test_async_create_pod_should_throw_exception(self, mocked_hook, mocked_cleanup, mocked_log):
+    def test_async_create_pod_should_throw_exception(self, mocked_hook, mocked_cleanup, mocked_log, status):
         """Tests that an AirflowException is raised in case of error event and event is logged"""
 
         mocked_hook.return_value.get_pod.return_value = MagicMock()
@@ -2112,20 +2113,20 @@ class TestKubernetesPodOperatorAsync:
             deferrable=True,
         )
 
-        error_message = "Some error"
+        message = "Some message"
         with pytest.raises(AirflowException):
             k.trigger_reentry(
                 context=None,
                 event={
-                    "status": "error",
-                    "message": error_message,
+                    "status": status,
+                    "message": message,
                     "name": TEST_NAME,
                     "namespace": TEST_NAMESPACE,
                 },
             )
 
-        log_message = "Trigger emitted an error event, failing the task: %s"
-        mocked_log.error.assert_called_once_with(log_message, error_message)
+        log_message = "Trigger emitted an %s event, failing the task: %s"
+        mocked_log.error.assert_called_once_with(log_message, status, message)
 
     @pytest.mark.parametrize(
         "kwargs, actual_exit_code, expected_exc, pod_status, event_status",
