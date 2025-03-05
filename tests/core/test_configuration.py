efb260fd64e1d8e1f6cf554b0efc3507603f6f0a
Bugra Ozturk <ozturkbugra93@gmail.com>
2025-03-05 23:42:45 +0100

    Include dedent for indentation of env file

--- tests/core/test_configuration.py
+++ tests/core/test_configuration.py
@@ -641,11 +641,11 @@ key3 = value3
 
     def test_command_from_env(self):
         test_cmdenv_config = """[testcmdenv]
-            itsacommand = NOT OK
-            notacommand = OK
+            itsacommand=NOT OK
+            notacommand=OK
         """
         test_cmdenv_conf = AirflowConfigParser()
-        test_cmdenv_conf.read_string(test_cmdenv_config)
+        test_cmdenv_conf.read_string(textwrap.dedent(test_cmdenv_config))
         test_cmdenv_conf.sensitive_config_values.add(("testcmdenv", "itsacommand"))
         with mock.patch.dict("os.environ"):
             # AIRFLOW__TESTCMDENV__ITSACOMMAND_CMD maps to ('testcmdenv', 'itsacommand') in
