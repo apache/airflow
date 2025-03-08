628d9c1e93f62c6e12734f02db036c28d84558f8
Maciej Obuchowski <maciej.obuchowski@datadoghq.com>
2025-03-08 16:19:32 +0100

    add OpenLineage configuration injection to LivyOperator
    Signed-off-by: Maciej Obuchowski <maciej.obuchowski@datadoghq.com>

--- providers/apache/livy/src/airflow/providers/apache/livy/get_provider_info.py
+++ providers/apache/livy/src/airflow/providers/apache/livy/get_provider_info.py
@@ -104,6 +104,7 @@ def get_provider_info():
         "dependencies": [
             "apache-airflow>=2.9.0",
             "apache-airflow-providers-http>=5.1.0",
+            "apache-airflow-providers-common-compat>=1.5.0",
             "aiohttp>=3.9.2",
             "asgiref>=2.3.0",
         ],
