1ce5b20abd7a2d73194a998463ddc4bb59f76ad3
Amogh Desai <amoghrajesh1999@gmail.com>
2025-03-10 15:57:27 +0530

    lazy loading for execution time xcoms

--- task-sdk/src/airflow/sdk/execution_time/__init__.py
+++ task-sdk/src/airflow/sdk/execution_time/__init__.py
@@ -15,3 +15,15 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+
+def __getattr__(name: str):
+    if name == "BaseXCom":
+        import importlib
+
+        mod = importlib.import_module(".xcom", __name__)
+        val = getattr(mod, name)
+
+        globals()[name] = val
+        return val
+
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
