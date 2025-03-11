b17f3c80dce555dab2f92fad459097d7f8c028b5
Kaxil Naik <kaxilnaik@gmail.com>
2025-03-11 23:35:53 +0530

    Move `chain`, `chain_linear` & `cross_downstream` to Task SDK
    This functions are for DAG Authors to define relationship between multiple tasks in batch

--- tests/system/example_empty.py
+++ tests/system/example_empty.py
@@ -18,9 +18,9 @@ from __future__ import annotations
 
 from datetime import datetime
 
-from airflow.models.baseoperator import chain
 from airflow.models.dag import DAG
 from airflow.providers.standard.operators.empty import EmptyOperator
+from airflow.sdk import chain
 
 DAG_ID = "example_empty"
 
