dce6fd581fa0703af6e506141721b83446c81471
Wei Lee <weilee.rx@gmail.com>
2025-03-12 14:57:33 +0800

    test(AIP-84): add test cases for backfill auth

--- providers/fab/tests/unit/fab/auth_manager/test_fab_auth_manager.py
+++ providers/fab/tests/unit/fab/auth_manager/test_fab_auth_manager.py
@@ -46,7 +46,11 @@ with ignore_provider_compatibility_error("2.9.0+", __file__):
     from airflow.providers.fab.auth_manager.fab_auth_manager import FabAuthManager
     from airflow.providers.fab.auth_manager.security_manager.override import FabAirflowSecurityManagerOverride
 
-from airflow.providers.common.compat.security.permissions import RESOURCE_ASSET, RESOURCE_ASSET_ALIAS
+from airflow.providers.common.compat.security.permissions import (
+    RESOURCE_ASSET,
+    RESOURCE_ASSET_ALIAS,
+    RESOURCE_BACKFILL,
+)
 from airflow.providers.fab.www.security.permissions import (
     ACTION_CAN_ACCESS_MENU,
     ACTION_CAN_CREATE,
@@ -76,6 +80,7 @@ IS_AUTHORIZED_METHODS_SIMPLE = {
     "is_authorized_connection": RESOURCE_CONNECTION,
     "is_authorized_asset": RESOURCE_ASSET,
     "is_authorized_asset_alias": RESOURCE_ASSET_ALIAS,
+    "is_authorized_backfill": RESOURCE_BACKFILL,
     "is_authorized_variable": RESOURCE_VARIABLE,
 }
 
