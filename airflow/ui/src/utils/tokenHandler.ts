78c8f130515813a062c7aef40fb13bd045b047d1
Pavan Kumar <gopidesupavan@gmail.com>
2025-03-12 15:08:25 +0000

    Fix tests for tokenHandler

--- airflow/ui/src/utils/tokenHandler.ts
+++ airflow/ui/src/utils/tokenHandler.ts
@@ -19,8 +19,6 @@
 import type { InternalAxiosRequestConfig } from "axios";
 
 export const TOKEN_STORAGE_KEY = "token";
-export const TOKEN_QUERY_PARAM_NAME = "token";
-
 const getTokenFromCookies = (): string | undefined => {
   const cookies = document.cookie.split(";");
 
