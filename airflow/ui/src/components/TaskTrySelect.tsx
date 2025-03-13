d91665688fc3c7b920240da7be9402f887923f19
Brent Bovenzi <brent@astronomer.io>
2025-03-13 12:30:40 -0400

    Add DagRunSelect to Graph view + other UI improvements (#47734)

--- airflow/ui/src/components/TaskTrySelect.tsx
+++ airflow/ui/src/components/TaskTrySelect.tsx
@@ -135,6 +135,7 @@ export const TaskTrySelect = ({ onSelectTryNumber, selectedTryNumber, taskInstan
                     onSelectTryNumber(ti.try_number);
                   }
                 }}
+                size="sm"
                 variant={selectedTryNumber === ti.try_number ? "surface" : "outline"}
               >
                 {ti.try_number}
