a10e041ac2e15feab85bce874df384191bf21a85
Brent Bovenzi <brent@astronomer.io>
2025-03-13 11:17:53 -0400

    Load dag run in grid if outside of initial fetch (#47695)
    * Add useGrid to handle dag runs outside of the initial data fetch
    
    * Fix selected version bug

--- airflow/ui/src/layouts/Details/Graph/Graph.tsx
+++ airflow/ui/src/layouts/Details/Graph/Graph.tsx
@@ -23,6 +23,7 @@ import { useParams } from "react-router-dom";
 import { useLocalStorage } from "usehooks-ts";
 
 import {
+  useDagRunServiceGetDagRun,
   useDependenciesServiceGetDependencies,
   useGridServiceGridData,
   useStructureServiceStructureData,
@@ -108,6 +109,15 @@ export const Graph = () => {
     { enabled: dependencies === "all" },
   );
 
+  const { data: dagRun } = useDagRunServiceGetDagRun(
+    {
+      dagId,
+      dagRunId: runId ?? "",
+    },
+    undefined,
+    { enabled: runId !== "" },
+  );
+
   const dagDepEdges = dependencies === "all" ? dagDependencies.edges : [];
   const dagDepNodes = dependencies === "all" ? dagDependencies.nodes : [];
 
@@ -124,29 +134,31 @@ export const Graph = () => {
     versionNumber: selectedVersion,
   });
 
+  // Filter grid data to get only a single dag run
   const { data: gridData } = useGridServiceGridData(
     {
       dagId,
-      limit: 25,
+      limit: 1,
       offset: 0,
-      orderBy: "-run_after",
+      runAfterGte: dagRun?.run_after,
+      runAfterLte: dagRun?.run_after,
     },
     undefined,
     {
-      enabled: Boolean(runId),
+      enabled: dagRun !== undefined,
       refetchInterval: (query) =>
         query.state.data?.dag_runs.some((dr) => isStatePending(dr.state)) && refetchInterval,
     },
   );
 
-  const dagRun = gridData?.dag_runs.find((dr) => dr.dag_run_id === runId);
+  const gridRun = gridData?.dag_runs.find((dr) => dr.dag_run_id === runId);
 
   // Add task instances to the node data but without having to recalculate how the graph is laid out
   const nodes =
-    dagRun?.task_instances === undefined
+    gridRun?.task_instances === undefined
       ? data?.nodes
       : data?.nodes.map((node) => {
-          const taskInstance = dagRun.task_instances.find((ti) => ti.task_id === node.id);
+          const taskInstance = gridRun.task_instances.find((ti) => ti.task_id === node.id);
 
           return {
             ...node,
