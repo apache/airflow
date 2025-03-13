a4b1f0f959f1b989b76cf0899de493b21b59eded
Pavan Kumar <gopidesupavan@gmail.com>
2025-03-13 14:41:17 +0000

    Use react-cookie

--- airflow/api_fastapi/auth/managers/simple/ui/src/login/Login.tsx
+++ airflow/api_fastapi/auth/managers/simple/ui/src/login/Login.tsx
@@ -25,7 +25,7 @@ import {LoginForm} from "src/login/LoginForm";
 import type {ApiError} from "openapi-gen/requests/core/ApiError";
 import type {LoginResponse, HTTPExceptionResponse, HTTPValidationError} from "openapi-gen/requests/types.gen";
 import { useSearchParams } from "react-router-dom";
-import Cookies from "js-cookie";
+import { useCookies } from 'react-cookie';
 
 export type LoginBody = {
     username: string; password: string;
@@ -37,12 +37,13 @@ type ExpandedApiError = {
 
 export const Login = () => {
     const [searchParams, setSearchParams] = useSearchParams();
+    const [cookies, setCookie] = useCookies(['_token']);
 
     const onSuccess = (data: LoginResponse) => {
         // Redirect to appropriate page with the token
         const next = searchParams.get("next")
 
-        Cookies.set('_token', data.jwt_token, { secure: true, sameSite: 'lax' });
+        setCookie('_token', data.jwt_token, {path: "/", secure: true, sameSite: 'lax' });
 
         globalThis.location.replace(`${next ?? ""}`);
     }
