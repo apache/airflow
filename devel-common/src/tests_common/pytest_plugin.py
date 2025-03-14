78bf7dbf6f76ac1785d65c1c8a95c20294486d48
Amogh Desai <amoghrajesh1999@gmail.com>
2025-03-14 19:10:02 +0530

    adding backcompat to fixture

--- devel-common/src/tests_common/pytest_plugin.py
+++ devel-common/src/tests_common/pytest_plugin.py
@@ -1833,6 +1833,11 @@ def override_caplog(request):
 
 @pytest.fixture
 def mock_supervisor_comms():
+    # for back-compat
+    from tests_common.test_utils.version_compat import AIRFLOW_V_3_0_PLUS
+
+    if not AIRFLOW_V_3_0_PLUS:
+        return None
     with mock.patch(
         "airflow.sdk.execution_time.task_runner.SUPERVISOR_COMMS", create=True
     ) as supervisor_comms:
