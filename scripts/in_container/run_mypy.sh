672b6862469388f639f55db5e84620b0839dff21
Jens Scheffler <jscheffl@apache.org>
2025-03-14 23:36:19 +0100

    Fix static checks in v2-10-test branch

--- scripts/in_container/run_mypy.sh
+++ scripts/in_container/run_mypy.sh
@@ -20,19 +20,7 @@
 . "$( dirname "${BASH_SOURCE[0]}" )/_in_container_script_init.sh"
 export PYTHONPATH=${AIRFLOW_SOURCES}
 
-ADDITIONAL_MYPY_OPTIONS=()
-
 export MYPY_FORCE_COLOR=true
 export TERM=ansi
 
-if [[ ${SUSPENDED_PROVIDERS_FOLDERS=} != "" ]];
-then
-    for folder in ${SUSPENDED_PROVIDERS_FOLDERS=}
-    do
-        ADDITIONAL_MYPY_OPTIONS+=(
-            "--exclude" "airflow/providers/${folder}/*"
-            "--exclude" "tests/providers/${folder}/*"
-        )
-    done
-fi
-mypy "${ADDITIONAL_MYPY_OPTIONS[@]}" "${@}"
+mypy --exclude airflow/providers --exclude tests/providers "${@}"
