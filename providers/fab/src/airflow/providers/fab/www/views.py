0b322c0ce9c257edced12b59ac0630c4f3e436cb
Pierre Jeambrun <pierrejbrun@gmail.com>
2025-03-17 18:54:09 +0100

    AIP-38 Fix safari login loop in dev mode (#47859)

--- providers/fab/src/airflow/providers/fab/www/views.py
+++ providers/fab/src/airflow/providers/fab/www/views.py
@@ -69,7 +69,9 @@ class FabIndexView(IndexView):
         if g.user is not None and g.user.is_authenticated:
             token = get_auth_manager().generate_jwt(g.user)
             response = make_response(redirect(f"{conf.get('api', 'base_url')}", code=302))
-            response.set_cookie(COOKIE_NAME_JWT_TOKEN, token, secure=True)
+
+            secure = bool(conf.get("api", "ssl_cert"))
+            response.set_cookie(COOKIE_NAME_JWT_TOKEN, token, secure=secure)
 
             return response
         else:
