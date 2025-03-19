b1144ec4063e2f945a11aa3d9f3b0cacf2948ddd
Tzu-ping Chung <uranusjr@gmail.com>
2025-03-20 02:23:33 +0800

    Only return active assets in API by default (#47962)
    Similar to the list dags endpoint, the list assets endpoint now has a
    query param 'only_active' that defaults to true (i.e. only show active
    assets). You can set this to false for the endpoint to also show
    inactive (deleted from code) assets.

--- airflow/ui/openapi-gen/requests/types.gen.ts
+++ airflow/ui/openapi-gen/requests/types.gen.ts
@@ -1681,6 +1681,7 @@ export type GetAssetsData = {
   limit?: number;
   namePattern?: string | null;
   offset?: number;
+  onlyActive?: boolean;
   orderBy?: string;
   uriPattern?: string | null;
 };
