252734455906b135b8fd78ff47dd78bd999c1ede
Jed Cunningham <jedcunningham@apache.org>
2025-03-21 11:02:07 -0600

    Remove FAB db manager as an `external_db_managers` default
    Now that the FAB auth manager isn't the default auth manager, it doesn't
    makes sense to have the FAB db manager as a default in
    `external_db_managers`.

--- dev/breeze/src/airflow_breeze/params/shell_params.py
+++ dev/breeze/src/airflow_breeze/params/shell_params.py
@@ -522,7 +522,14 @@ class ShellParams:
         _set_var(_env, "AIRFLOW__CELERY__BROKER_URL", self.airflow_celery_broker_url)
         _set_var(_env, "AIRFLOW__CORE__AUTH_MANAGER", self.auth_manager_path)
         _set_var(_env, "AIRFLOW__CORE__EXECUTOR", self.executor)
-        _set_var(_env, "AIRFLOW__CORE__SIMPLE_AUTH_MANAGER_USERS", "admin:admin,viewer:viewer")
+        if self.auth_manager == FAB_AUTH_MANAGER:
+            _set_var(
+                _env,
+                "AIRFLOW__DATABASE__EXTERNAL_DB_MANAGERS",
+                "airflow.providers.fab.auth_manager.models.db.FABDBManager",
+            )
+        if self.auth_manager == SIMPLE_AUTH_MANAGER:
+            _set_var(_env, "AIRFLOW__CORE__SIMPLE_AUTH_MANAGER_USERS", "admin:admin,viewer:viewer")
         _set_var(
             _env,
             "AIRFLOW__CORE__SIMPLE_AUTH_MANAGER_PASSWORDS_FILE",
