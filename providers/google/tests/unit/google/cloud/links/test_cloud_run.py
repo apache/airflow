1c62fab3adb2e1a8354e2b4de3ea256e1e4388e4
Ramon Vermeulen <ramonvermeulen98@gmail.com>
2025-03-21 08:27:03 +0100

    fix: implemented feedback (added test for `get_link`)

--- providers/google/tests/unit/google/cloud/links/test_cloud_run.py
+++ providers/google/tests/unit/google/cloud/links/test_cloud_run.py
@@ -19,7 +19,12 @@ from __future__ import annotations
 
 from unittest import mock
 
+import pytest
+
 from airflow.providers.google.cloud.links.cloud_run import CloudRunJobLoggingLink
+from airflow.providers.google.cloud.operators.cloud_run import CloudRunExecuteJobOperator
+from airflow.providers.google.version_compat import AIRFLOW_V_3_0_PLUS
+from airflow.sdk.execution_time.comms import XComResult
 
 TEST_LOG_URI = (
     "https://console.cloud.google.com/run/jobs/logs?project=test-project&region=test-region&job=test-job"
@@ -45,3 +50,25 @@ class TestCloudRunJobLoggingLink:
             key=CloudRunJobLoggingLink.key,
             value=TEST_LOG_URI,
         )
+
+    @pytest.mark.db_test
+    def test_get_link(self, create_task_instance_of_operator, session, mock_supervisor_comms):
+        link = CloudRunJobLoggingLink()
+        ti = create_task_instance_of_operator(
+            CloudRunExecuteJobOperator,
+            dag_id="test_cloud_run_job_logging_link_dag",
+            task_id="test_cloud_run_job_logging_link_task",
+            job_name="test-job",
+            project_id="test-project",
+            region="test-region",
+        )
+        session.add(ti)
+        session.commit()
+        link.persist(context={"ti": ti}, task_instance=ti.task, log_uri=TEST_LOG_URI)
+        if AIRFLOW_V_3_0_PLUS and mock_supervisor_comms:
+            mock_supervisor_comms.get_message.return_value = XComResult(
+                key="key",
+                value=TEST_LOG_URI,
+            )
+        actual_url = link.get_link(operator=ti.task, ti_key=ti.key)
+        assert actual_url == TEST_LOG_URI
