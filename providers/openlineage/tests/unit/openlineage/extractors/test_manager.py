6d0e00c73c03ace0d0b94174272a3443cd83244a
Tzu-ping Chung <uranusjr@gmail.com>
2025-03-25 14:02:21 +0800

    Implement task-level “on” callbacks in sdk (#48002)
    This includes
    
    on_success_callback
    on_skipped_callback
    on_failure_callback
    on_retry_callback

--- providers/openlineage/tests/unit/openlineage/extractors/test_manager.py
+++ providers/openlineage/tests/unit/openlineage/extractors/test_manager.py
@@ -512,7 +512,7 @@ def test_extractor_manager_gets_data_from_pythonoperator_tasksdk(
     )
     ti = mocked_parse(what, "test_hookcollector_dag", task)
 
-    task_runner.run(ti, logging.getLogger(__name__))
+    task_runner.run(ti, ti.get_template_context(), logging.getLogger(__name__))
 
     datasets = hook_lineage_collector.collected_assets
 
