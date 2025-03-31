683cae4a7d08b402d98e6ffe2eaea08ae4f415d7
Pierre Jeambrun <pierrejbrun@gmail.com>
2025-03-31 15:52:11 +0200

    Enable Comms in the TriggerRunner to make Connections, Variables and XComs accessible to Triggers (#48239)
    * Init triggerer comms
    
    * Remove cache from in_process_api_server
    
    * Add lock for trigger threads to wait for their messages
    
    * Move comms init code to TriggerRunner class
    
    * Add TODO for empty line investigation
    
    * Update kind to type for common discriminator
    
    * Add test, add xcom handling
    
    * Address PR comments
    
    * Rename test supervisor class
    
    * Add XCom test and code
    
    * Fix following code review
    
    * Fix CI and add comment
    
    * Update task-sdk/src/airflow/sdk/api/client.py
    
    * Small adjustments
    
    * Remove unused fixture
    
    * Fix CI

--- task-sdk/src/airflow/sdk/execution_time/context.py
+++ task-sdk/src/airflow/sdk/execution_time/context.py
@@ -113,6 +113,7 @@ def _convert_variable_result_to_variable(var_result: VariableResult, deserialize
 
 def _get_connection(conn_id: str) -> Connection:
     from airflow.sdk.execution_time.supervisor import ensure_secrets_backend_loaded
+
     # TODO: check cache first
     # enabled only if SecretCache.init() has been called first
 
@@ -142,8 +143,14 @@ def _get_connection(conn_id: str) -> Connection:
     from airflow.sdk.execution_time.comms import ErrorResponse, GetConnection
     from airflow.sdk.execution_time.task_runner import SUPERVISOR_COMMS
 
-    SUPERVISOR_COMMS.send_request(log=log, msg=GetConnection(conn_id=conn_id))
-    msg = SUPERVISOR_COMMS.get_message()
+    # Since Triggers can hit this code path via `sync_to_async` (which uses threads internally)
+    # we need to make sure that we "atomically" send a request and get the response to that
+    # back so that two triggers don't end up interleaving requests and create a possible
+    # race condition where the wrong trigger reads the response.
+    with SUPERVISOR_COMMS.lock:
+        SUPERVISOR_COMMS.send_request(log=log, msg=GetConnection(conn_id=conn_id))
+        msg = SUPERVISOR_COMMS.get_message()
+
     if isinstance(msg, ErrorResponse):
         raise AirflowRuntimeError(msg)
 
@@ -183,8 +190,14 @@ def _get_variable(key: str, deserialize_json: bool) -> Any:
     from airflow.sdk.execution_time.comms import ErrorResponse, GetVariable
     from airflow.sdk.execution_time.task_runner import SUPERVISOR_COMMS
 
-    SUPERVISOR_COMMS.send_request(log=log, msg=GetVariable(key=key))
-    msg = SUPERVISOR_COMMS.get_message()
+    # Since Triggers can hit this code path via `sync_to_async` (which uses threads internally)
+    # we need to make sure that we "atomically" send a request and get the response to that
+    # back so that two triggers don't end up interleaving requests and create a possible
+    # race condition where the wrong trigger reads the response.
+    with SUPERVISOR_COMMS.lock:
+        SUPERVISOR_COMMS.send_request(log=log, msg=GetVariable(key=key))
+        msg = SUPERVISOR_COMMS.get_message()
+
     if isinstance(msg, ErrorResponse):
         raise AirflowRuntimeError(msg)
 
