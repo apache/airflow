4e32c20b92ce21bf2caee899998157007691e9eb
Pavan Kumar <gopidesupavan@gmail.com>
2025-04-03 16:50:53 +0100

    Remove print

--- providers/standard/src/airflow/providers/standard/triggers/external_task.py
+++ providers/standard/src/airflow/providers/standard/triggers/external_task.py
@@ -195,7 +195,6 @@ class DagStateTrigger(BaseTrigger):
                     states=self.states,  # type: ignore[arg-type]
                     logical_dates=self.execution_dates,
                 )
-                print(num_dags)
             else:
                 num_dags = await self.count_dags()  # type: ignore[call-arg]
 
