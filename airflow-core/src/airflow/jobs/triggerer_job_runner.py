7cfbaebaba56c9da5b7b7e2b08a28ae48e3553b1
Pavan Kumar <gopidesupavan@gmail.com>
2025-04-04 22:10:28 +0100

    Fix WorkflowStateTrigger

--- airflow-core/src/airflow/jobs/triggerer_job_runner.py
+++ airflow-core/src/airflow/jobs/triggerer_job_runner.py
@@ -48,8 +48,12 @@ from airflow.sdk.execution_time.comms import (
     GetConnection,
     GetVariable,
     GetXCom,
+GetTICount,
+GetDRCount,
     VariableResult,
     XComResult,
+    TICount,
+DRCount
 )
 from airflow.sdk.execution_time.supervisor import WatchedSubprocess, make_buffered_socket_reader
 from airflow.stats import Stats
@@ -213,6 +217,8 @@ ToTriggerRunner = Annotated[
         ConnectionResult,
         VariableResult,
         XComResult,
+        TICount,
+        DRCount,
         ErrorResponse,
     ],
     Field(discriminator="type"),
@@ -224,7 +230,7 @@ code).
 
 
 ToTriggerSupervisor = Annotated[
-    Union[messages.TriggerStateChanges, GetConnection, GetVariable, GetXCom],
+    Union[messages.TriggerStateChanges, GetConnection, GetVariable, GetXCom, GetTICount, GetDRCount],
     Field(discriminator="type"),
 ]
 """
@@ -374,6 +380,24 @@ class TriggerRunnerSupervisor(WatchedSubprocess):
                 resp = xcom_result.model_dump_json(exclude_unset=True).encode()
             else:
                 resp = xcom.model_dump_json().encode()
+        elif isinstance(msg, GetTICount):
+            ti_count = self.client.task_instances.get_count(
+                dag_id=msg.dag_id,
+                task_ids=msg.task_ids,
+                task_group_id=msg.task_group_id,
+                logical_dates=msg.logical_dates,
+                run_ids=msg.run_ids,
+                states=msg.states,
+            )
+            resp = ti_count.model_dump_json().encode()
+        elif isinstance(msg, GetDRCount):
+            dr_count = self.client.dag_runs.get_count(
+                dag_id=msg.dag_id,
+                logical_dates=msg.logical_dates,
+                run_ids=msg.run_ids,
+                states=msg.states,
+            )
+            resp = dr_count.model_dump_json().encode()
         else:
             raise ValueError(f"Unknown message type {type(msg)}")
 
--- providers/standard/src/airflow/providers/standard/sensors/external_task.py
+++ providers/standard/src/airflow/providers/standard/sensors/external_task.py
@@ -427,7 +427,7 @@ class ExternalTaskSensor(BaseSensorOperator):
                     external_dag_id=self.external_dag_id,
                     external_task_group_id=self.external_task_group_id,
                     external_task_ids=self.external_task_ids,
-                    logical_dates=self._get_dttm_filter(context),
+                    execution_dates=self._get_dttm_filter(context),
                     allowed_states=self.allowed_states,
                     poke_interval=self.poll_interval,
                     soft_fail=self.soft_fail,
