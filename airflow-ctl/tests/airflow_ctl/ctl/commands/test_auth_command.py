8e865426c0f614ca5476b1f1bb46f72761e692d9
Bugra Ozturk <ozturkbugra93@gmail.com>
2025-04-07 05:28:00 +0200

    feat (airflowctl): Add login with username and password to auth command
    Add operations unit test
    Fix unit test for auth command

--- airflow-ctl/tests/airflow_ctl/ctl/commands/test_auth_command.py
+++ airflow-ctl/tests/airflow_ctl/ctl/commands/test_auth_command.py
@@ -16,6 +16,7 @@
 # under the License.
 from __future__ import annotations
 
+import io
 import json
 import os
 from unittest import mock
@@ -37,12 +38,22 @@ class TestCliAuthCommands:
     @patch.dict(os.environ, {"AIRFLOW_CLI_TOKEN": "TEST_TOKEN"})
     @patch.dict(os.environ, {"AIRFLOW_CLI_ENVIRONMENT": "TEST_AUTH_LOGIN"})
     @patch("airflowctl.api.client.keyring")
-    def test_login(self, mock_keyring):
+    def test_login(self, mock_keyring, api_client_maker):
+        api_client = api_client_maker(
+            path="/auth/token/cli",
+            response_json=self.login_response.model_dump(),
+            expected_http_status_code=201,
+            kind="auth",
+        )
+
         mock_keyring.set_password = mock.MagicMock()
         mock_keyring.get_password.return_value = None
         env = "TEST_AUTH_LOGIN"
 
-        auth_command.login(self.parser.parse_args(["auth", "login", "--api-url", "http://localhost:8080"]))
+        auth_command.login(
+            self.parser.parse_args(["auth", "login", "--api-url", "http://localhost:8080"]),
+            api_client=api_client,
+        )
         default_config_dir = user_config_path("airflow", "Apache Software Foundation")
         assert os.path.exists(default_config_dir)
         with open(os.path.join(default_config_dir, f"{env}.json")) as f:
@@ -56,7 +67,7 @@ class TestCliAuthCommands:
 
     # Test auth login with username and password
     @patch("airflowctl.api.client.keyring")
-    def test_login_with_username_and_password(self, mock_keyring, api_client_maker):
+    def test_login_with_username_and_password(self, mock_keyring, api_client_maker, monkeypatch):
         api_client = api_client_maker(
             path="/auth/token/cli",
             response_json=self.login_response.model_dump(),
@@ -66,7 +77,7 @@ class TestCliAuthCommands:
 
         mock_keyring.set_password = mock.MagicMock()
         mock_keyring.get_password.return_value = None
-
+        monkeypatch.setattr("sys.stdin", io.StringIO("test_password"))
         auth_command.login(
             self.parser.parse_args(
                 [
@@ -77,7 +88,6 @@ class TestCliAuthCommands:
                     "--username",
                     "test_user",
                     "--password",
-                    "test_password",
                 ]
             ),
             api_client=api_client,
