5e3078f287f288632fa6e39180956073779dea25
Ash Berlin-Taylor <ash@apache.org>
2025-04-08 14:40:51 +0100

    Add response migration for `consumed_asset_events` events field
    And, like I should have done in #48888, add tests this time.
    
    The issue was caused by me misunderstanding how Cadwyn works, the
    `instructions_to_migrate_to_previous_version` only affect the OpenAPI schema,
    but since the change in this case results in an "invalid" model being
    returned, we have to write our own migration for the data.
    
    For those curious, the error without this migration function is:
    
    > fastapi.exceptions.ResponseValidationError: 1 validation errors:
    >  {'type': 'extra_forbidden', 'loc': ('response', 'dag_run', 'consumed_asset_events'), 'msg': 'Extra inputs are not permitted', 'input': []}

--- airflow-core/tests/unit/api_fastapi/execution_api/versions/v2025_03_26/__init__.py
+++ airflow-core/tests/unit/api_fastapi/execution_api/versions/v2025_03_26/__init__.py
@@ -14,21 +14,3 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-
-from __future__ import annotations
-
-from cadwyn import ResponseInfo, VersionChange, convert_response_to_previous_version_for, schema
-
-from airflow.api_fastapi.execution_api.datamodels.taskinstance import DagRun, TIRunContext
-
-
-class AddConsumedAssetEventsField(VersionChange):
-    """Add the `consumed_asset_events` to DagRun model."""
-
-    description = __doc__
-
-    instructions_to_migrate_to_previous_version = (schema(DagRun).field("consumed_asset_events").didnt_exist,)
-
-    @convert_response_to_previous_version_for(TIRunContext)  # type: ignore
-    def remove_consumed_asset_events(response: ResponseInfo):  # type: ignore
-        response.body["dag_run"].pop("consumed_asset_events")
