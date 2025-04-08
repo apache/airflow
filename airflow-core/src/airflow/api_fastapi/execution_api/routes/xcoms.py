1c2701fbe82b4c96665067d2ee106bba7b45a7a3
kiran2706 <kiran1997raj@gmail.com>
2025-04-08 20:46:53 +0530

    Fixed the conflict

--- airflow-core/src/airflow/api_fastapi/execution_api/routes/xcoms.py
+++ airflow-core/src/airflow/api_fastapi/execution_api/routes/xcoms.py
@@ -140,8 +140,6 @@ def get_xcom(
     session: SessionDep,
     params: Annotated[GetXcomFilterParams, Query()],
     key: Annotated[str, StringConstraints(min_length=1)],
-    xcom_query: Annotated[Select, Depends(xcom_query)],
-    map_index: Annotated[int, Query()] = -1,
 ) -> XComResponse:
     """Get an Airflow XCom from database - not other XCom Backends."""
     # The xcom_query allows no map_index to be passed. This endpoint should always return just a single item,
