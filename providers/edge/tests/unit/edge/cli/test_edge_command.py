4fbda4b6fbb12492041704acc22717c18737eaa3
Christos Bisias <christosbis@gmail.com>
2025-04-08 18:32:43 +0300

    fix test_edge_command.py

--- providers/edge/tests/unit/edge/cli/test_edge_command.py
+++ providers/edge/tests/unit/edge/cli/test_edge_command.py
@@ -26,6 +26,7 @@ from unittest.mock import MagicMock, call, patch
 import pytest
 import time_machine
 from requests import HTTPError, Response
+from uuid6 import uuid7
 
 from airflow.providers.edge.cli.dataclasses import Job
 from airflow.providers.edge.cli.edge_command import _EdgeWorkerCli, _write_pid_to_pidfile
@@ -52,6 +53,7 @@ MOCK_COMMAND = (
             "task_id": "mock",
             "dag_id": "mock",
             "run_id": "mock",
+            "try_id": uuid7(),
             "try_number": 1,
             "pool_slots": 1,
             "queue": "default",
