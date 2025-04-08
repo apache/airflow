apache/airflow

commit 5eca6c6aa7879cc20c1e284cd9773abcee7fc312

Calculate retry eligibility before task runs (#47996)
The task needs this information to know whether it should run the retry
or failed callback if the task fails, so we calculate it eagerly and
pass it to the runner, instead of lazily only after the task has failed.

The information is used by the task runner to decide what state to set
the ti to when an error happens. If the task is eligible for retry, the
next state is set to UP_FOR_RETRY; otherwise it's FAILED. This is then
handled accordingly in the API.

The FAIL_WITHOUT_RETRY state has been removed from the code base since
it is no longer needed. The state was introduced in 1283cc347f26678e7504
to tell the API server to not check retry eligibility on certain failure
cases, but now that eligibility is always checked and used in the task
runner, the API server no longer needs this information; a request to
update to FAILED always set the TI to FAILED.

task-sdk/src/airflow/sdk/api/client.py
@@ -484,6 +484,7 @@ def noop_handler(request: httpx.Request) -> httpx.Response:
                     "run_after": "2021-01-01T00:00:00Z",
                 },
                 "max_tries": 0,
+                "should_retry": False,
             },
         )
     return httpx.Response(200, json={"text": "Hello, world!"})

task-sdk/tests/task_sdk/api/test_client.py
@@ -56,6 +56,7 @@ class TestClient:
                         "run_after": "2021-01-01T00:00:00Z",
                     },
                     "max_tries": 0,
+                    "should_retry": False,
                 },
             ),
         ],

task-sdk/tests/task_sdk/execution_time/test_supervisor.py
@@ -728,23 +728,16 @@ class TestListenerOvertime:
 
         if expected_timeout:
             assert any(
-                [
-                    event["event"] == "Workload success overtime reached; terminating process"
-                    for event in captured_logs
-                ]
+                event["event"] == "Workload success overtime reached; terminating process"
+                for event in captured_logs
             )
             assert any(
-                [
-                    event["event"] == "Process exited" and event["signal"] == "SIGTERM"
-                    for event in captured_logs
-                ]
+                event["event"] == "Process exited" and event["signal"] == "SIGTERM" for event in captured_logs
             )
         else:
             assert all(
-                [
-                    event["event"] != "Workload success overtime reached; terminating process"
-                    for event in captured_logs
-                ]
+                event["event"] != "Workload success overtime reached; terminating process"
+                for event in captured_logs
             )
 
 

airflow-core/src/airflow/api_fastapi/execution_api/datamodels/taskinstance.py
@@ -66,7 +66,6 @@ class TerminalStateNonSuccess(str, Enum):
     FAILED = TerminalTIState.FAILED
     SKIPPED = TerminalTIState.SKIPPED
     REMOVED = TerminalTIState.REMOVED
-    FAIL_WITHOUT_RETRY = TerminalTIState.FAIL_WITHOUT_RETRY
 
 
 class TITerminalStatePayload(StrictBaseModel):
@@ -157,6 +156,23 @@ class TIRescheduleStatePayload(StrictBaseModel):
     end_date: UtcDateTime
 
 
+class TIRetryStatePayload(StrictBaseModel):
+    """Schema for updating TaskInstance to up_for_retry."""
+
+    state: Annotated[
+        Literal[IntermediateTIState.UP_FOR_RETRY],
+        # Specify a default in the schema, but not in code, so Pydantic marks it as required.
+        WithJsonSchema(
+            {
+                "type": "string",
+                "enum": [IntermediateTIState.UP_FOR_RETRY],
+                "default": IntermediateTIState.UP_FOR_RETRY,
+            }
+        ),
+    ]
+    end_date: UtcDateTime
+
+
 class TISkippedDownstreamTasksStatePayload(StrictBaseModel):
     """Schema for updating downstream tasks to a skipped state."""
 
@@ -185,6 +201,8 @@ def ti_state_discriminator(v: dict[str, str] | StrictBaseModel) -> str:
         return "deferred"
     elif state == TIState.UP_FOR_RESCHEDULE:
         return "up_for_reschedule"
+    elif state == TIState.UP_FOR_RETRY:
+        return "up_for_retry"
     return "_other_"
 
 
@@ -197,6 +215,7 @@ TIStateUpdate = Annotated[
         Annotated[TITargetStatePayload, Tag("_other_")],
         Annotated[TIDeferredStatePayload, Tag("deferred")],
         Annotated[TIRescheduleStatePayload, Tag("up_for_reschedule")],
+        Annotated[TIRetryStatePayload, Tag("up_for_retry")],
     ],
     Discriminator(ti_state_discriminator),
 ]
@@ -275,6 +294,9 @@ class TIRunContext(BaseModel):
 
     xcom_keys_to_clear: Annotated[list[str], Field(default_factory=list)]
     """List of Xcom keys that need to be cleared and purged on by the worker."""
+
+    should_retry: bool
+    """If the ti encounters an error, whether it should enter retry or failed state."""
 
 
 class PrevSuccessfulDagRunResponse(BaseModel):

airflow-core/src/airflow/api_fastapi/execution_api/routes/task_instances.py
@@ -36,6 +36,7 @@ from airflow.api_fastapi.execution_api.datamodels.taskinstance import (
     TIEnterRunningPayload,
     TIHeartbeatInfo,
     TIRescheduleStatePayload,
+    TIRetryStatePayload,
     TIRunContext,
     TIRuntimeCheckPayload,
     TISkippedDownstreamTasksStatePayload,
@@ -50,7 +51,7 @@ from airflow.models.taskreschedule import TaskReschedule
 from airflow.models.trigger import Trigger
 from airflow.models.xcom import XComModel
 from airflow.utils import timezone
-from airflow.utils.state import DagRunState, TaskInstanceState, TerminalTIState
+from airflow.utils.state import DagRunState, TaskInstanceState
 
 router = VersionedAPIRouter(
     dependencies=[
@@ -136,7 +137,7 @@ def ti_run(
         ti_run_payload.pid,
     ):
         log.info("Duplicate start request received from %s ", ti_run_payload.hostname)
-    elif previous_state != TaskInstanceState.QUEUED:
+    elif previous_state not in (TaskInstanceState.QUEUED, TaskInstanceState.RESTARTING):
         log.warning(
             "Can not start Task Instance ('%s') in invalid state: %s",
             ti_id_str,
@@ -226,6 +227,7 @@ def ti_run(
             variables=[],
             connections=[],
             xcom_keys_to_clear=xcom_keys,
+            should_retry=_is_eligible_to_retry(previous_state, ti.try_number, ti.max_tries),
         )
 
         # Only set if they are non-null
@@ -289,23 +291,18 @@ def ti_update_state(
     query = update(TI).where(TI.id == ti_id_str).values(data)
 
     if isinstance(ti_patch_payload, TITerminalStatePayload):
-        query = TI.duration_expression_update(ti_patch_payload.end_date, query, session.bind)
         updated_state = ti_patch_payload.state
-        # if we get failed, we should attempt to retry, as it is a more
-        # normal state. Tasks with retries are more frequent than without retries.
-        if ti_patch_payload.state == TerminalTIState.FAIL_WITHOUT_RETRY:
-            updated_state = TaskInstanceState.FAILED
-        elif ti_patch_payload.state == TaskInstanceState.FAILED:
-            if _is_eligible_to_retry(previous_state, try_number, max_tries):
-                from airflow.models.taskinstance import uuid7
-                from airflow.models.taskinstancehistory import TaskInstanceHistory
+        query = TI.duration_expression_update(ti_patch_payload.end_date, query, session.bind)
+        query = query.values(state=updated_state)
+    elif isinstance(ti_patch_payload, TIRetryStatePayload):
+        from airflow.models.taskinstance import uuid7
+        from airflow.models.taskinstancehistory import TaskInstanceHistory
 
-                ti = session.get(TI, ti_id_str)
-                TaskInstanceHistory.record_ti(ti, session=session)
-                ti.try_id = uuid7()
-                updated_state = TaskInstanceState.UP_FOR_RETRY
-            else:
-                updated_state = TaskInstanceState.FAILED
+        ti = session.get(TI, ti_id_str)
+        TaskInstanceHistory.record_ti(ti, session=session)
+        ti.try_id = uuid7()
+        updated_state = ti_patch_payload.state
+        query = TI.duration_expression_update(ti_patch_payload.end_date, query, session.bind)
         query = query.values(state=updated_state)
     elif isinstance(ti_patch_payload, TISuccessStatePayload):
         query = TI.duration_expression_update(ti_patch_payload.end_date, query, session.bind)

airflow-core/src/airflow/models/taskinstance.py
@@ -889,6 +889,7 @@ def _get_template_context(
     ti_context_from_server = TIRunContext(
         dag_run=DagRunSDK.model_validate(dag_run, from_attributes=True),
         max_tries=task_instance.max_tries,
+        should_retry=task_instance.is_eligible_to_retry(),
     )
     runtime_ti = task_instance.to_runtime_ti(context_from_server=ti_context_from_server)
 
@@ -3197,7 +3198,7 @@ class TaskInstance(Base, LoggingMixin):
             fail_fast=fail_fast,
         )
 
-    def is_eligible_to_retry(self):
+    def is_eligible_to_retry(self) -> bool:
         """Is task instance is eligible for retry."""
         return _is_eligible_to_retry(task_instance=self)
 

airflow-core/src/airflow/utils/state.py
@@ -39,7 +39,6 @@ class TerminalTIState(str, Enum):
     FAILED = "failed"
     SKIPPED = "skipped"  # A user can raise a AirflowSkipException from a task & it will be marked as skipped
     REMOVED = "removed"
-    FAIL_WITHOUT_RETRY = "fail_without_retry"
 
     def __str__(self) -> str:
         return self.value

airflow-core/tests/unit/api_fastapi/execution_api/routes/test_task_instances.py
@@ -116,7 +116,22 @@ class TestTIRunState:
     def teardown_method(self):
         clear_db_runs()
 
-    def test_ti_run_state_to_running(self, client, session, create_task_instance, time_machine):
+    @pytest.mark.parametrize(
+        "max_tries, should_retry",
+        [
+            pytest.param(0, False, id="max_retries=0"),
+            pytest.param(3, True, id="should_retry"),
+        ],
+    )
+    def test_ti_run_state_to_running(
+        self,
+        client,
+        session,
+        create_task_instance,
+        time_machine,
+        max_tries,
+        should_retry,
+    ):
         """
         Test that the Task Instance state is updated to running when the Task Instance is in a state where it can be
         marked as running.
@@ -131,6 +146,7 @@ class TestTIRunState:
             session=session,
             start_date=instant,
         )
+        ti.max_tries = max_tries
         session.commit()
 
         response = client.patch(
@@ -160,7 +176,8 @@ class TestTIRunState:
                 "conf": {},
             },
             "task_reschedule_count": 0,
-            "max_tries": 0,
+            "max_tries": max_tries,
+            "should_retry": should_retry,
             "variables": [],
             "connections": [],
             "xcom_keys_to_clear": [],
@@ -210,7 +227,7 @@ class TestTIRunState:
         time_machine.move_to(instant, tick=False)
 
         ti = create_task_instance(
-            task_id="test_ti_run_state_to_running",
+            task_id="test_next_kwargs_still_encoded",
             state=State.QUEUED,
             session=session,
             start_date=instant,
@@ -238,6 +255,7 @@ class TestTIRunState:
             "dag_run": mock.ANY,
             "task_reschedule_count": 0,
             "max_tries": 0,
+            "should_retry": False,
             "variables": [],
             "connections": [],
             "xcom_keys_to_clear": [],
@@ -248,7 +266,10 @@ class TestTIRunState:
             },
         }
 
-    @pytest.mark.parametrize("initial_ti_state", [s for s in TaskInstanceState if s != State.QUEUED])
+    @pytest.mark.parametrize(
+        "initial_ti_state",
+        [s for s in TaskInstanceState if s not in (TaskInstanceState.QUEUED, TaskInstanceState.RESTARTING)],
+    )
     def test_ti_run_state_conflict_if_not_queued(
         self, client, session, create_task_instance, initial_ti_state
     ):
@@ -691,30 +712,17 @@ class TestTIUpdateState:
         assert trs[0].task_instance.map_index == -1
         assert trs[0].duration == 129600
 
-    @pytest.mark.parametrize(
-        ("retries", "expected_state"),
-        [
-            (0, State.FAILED),
-            (None, State.FAILED),
-            (3, State.UP_FOR_RETRY),
-        ],
-    )
-    def test_ti_update_state_to_failed_with_retries(
-        self, client, session, create_task_instance, retries, expected_state
-    ):
+    def test_ti_update_state_handle_retry(self, client, session, create_task_instance):
         ti = create_task_instance(
             task_id="test_ti_update_state_to_retry",
             state=State.RUNNING,
         )
-
-        if retries is not None:
-            ti.max_tries = retries
         session.commit()
 
         response = client.patch(
             f"/execution/task-instances/{ti.id}/state",
             json={
-                "state": TerminalTIState.FAILED,
+                "state": State.UP_FOR_RETRY,
                 "end_date": DEFAULT_END_DATE.isoformat(),
             },
         )
@@ -725,80 +733,19 @@ class TestTIUpdateState:
         session.expire_all()
 
         ti = session.get(TaskInstance, ti.id)
-        assert ti.state == expected_state
-        assert ti.next_method is None
-        assert ti.next_kwargs is None
-
-        tih = session.query(TaskInstanceHistory).where(
-            TaskInstanceHistory.task_id == ti.task_id, TaskInstanceHistory.task_instance_id == ti.id
-        )
-        tih_count = tih.count()
-        assert tih_count == (1 if retries else 0)
-        if retries:
-            tih = tih.one()
-            assert tih.try_id
-            assert tih.try_id != ti.try_id
-
-    def test_ti_update_state_when_ti_is_restarting(self, client, session, create_task_instance):
-        ti = create_task_instance(
-            task_id="test_ti_update_state_when_ti_is_restarting",
-            state=State.RUNNING,
-        )
-        # update state to restarting
-        ti.state = State.RESTARTING
-        session.commit()
-
-        response = client.patch(
-            f"/execution/task-instances/{ti.id}/state",
-            json={
-                "state": TerminalTIState.FAILED,
-                "end_date": DEFAULT_END_DATE.isoformat(),
-            },
-        )
-
-        assert response.status_code == 204
-        assert response.text == ""
-
-        session.expire_all()
-
-        ti = session.get(TaskInstance, ti.id)
-        # restarting is always retried
         assert ti.state == State.UP_FOR_RETRY
         assert ti.next_method is None
         assert ti.next_kwargs is None
 
-    def test_ti_update_state_when_ti_has_higher_tries_than_retries(
-        self, client, session, create_task_instance
-    ):
-        ti = create_task_instance(
-            task_id="test_ti_update_state_when_ti_has_higher_tries_than_retries",
-            state=State.RUNNING,
+        tih = (
+            session.query(TaskInstanceHistory)
+            .where(TaskInstanceHistory.task_id == ti.task_id, TaskInstanceHistory.task_instance_id == ti.id)
+            .one()
         )
-        # two maximum tries defined, but third try going on
-        ti.max_tries = 2
-        ti.try_number = 3
-        session.commit()
+        assert tih.try_id
+        assert tih.try_id != ti.try_id
 
-        response = client.patch(
-            f"/execution/task-instances/{ti.id}/state",
-            json={
-                "state": TerminalTIState.FAILED,
-                "end_date": DEFAULT_END_DATE.isoformat(),
-            },
-        )
-
-        assert response.status_code == 204
-        assert response.text == ""
-
-        session.expire_all()
-
-        ti = session.get(TaskInstance, ti.id)
-        # all retries exhausted, marking as failed
-        assert ti.state == State.FAILED
-        assert ti.next_method is None
-        assert ti.next_kwargs is None
-
-    def test_ti_update_state_to_failed_without_retry_table_check(self, client, session, create_task_instance):
+    def test_ti_update_state_to_failed_table_check(self, client, session, create_task_instance):
         # we just want to fail in this test, no need to retry
         ti = create_task_instance(
             task_id="test_ti_update_state_to_failed_table_check",
@@ -810,7 +757,7 @@ class TestTIUpdateState:
         response = client.patch(
             f"/execution/task-instances/{ti.id}/state",
             json={
-                "state": TerminalTIState.FAIL_WITHOUT_RETRY,
+                "state": TerminalTIState.FAILED,
                 "end_date": DEFAULT_END_DATE.isoformat(),
             },
         )

commit 3f46988792752b0e0f29c6408525e8f2da930d63

AIP-72: Fix running task with retries
This was broken in https://github.com/apache/airflow/pull/47996

closes https://github.com/apache/airflow/issues/48927

task-sdk/src/airflow/sdk/api/client.py
@@ -47,6 +47,7 @@ from airflow.sdk.api.datamodels._generated import (
     TIEnterRunningPayload,
     TIHeartbeatInfo,
     TIRescheduleStatePayload,
+    TIRetryStatePayload,
     TIRunContext,
     TISkippedDownstreamTasksStatePayload,
     TISuccessStatePayload,
@@ -150,6 +151,11 @@ class TaskInstanceOperations:
             raise ValueError("Logic error. SUCCESS state should call the `succeed` function instead")
         # TODO: handle the naming better. finish sounds wrong as "even" deferred is essentially finishing.
         body = TITerminalStatePayload(end_date=when, state=TerminalStateNonSuccess(state))
+        self.client.patch(f"task-instances/{id}/state", content=body.model_dump_json())
+
+    def retry(self, id: uuid.UUID, end_date: datetime):
+        """Tell the API server that this TI has failed and reached a up_for_retry state."""
+        body = TIRetryStatePayload(end_date=end_date)
         self.client.patch(f"task-instances/{id}/state", content=body.model_dump_json())
 
     def succeed(self, id: uuid.UUID, when: datetime, task_outlets, outlet_events):

task-sdk/src/airflow/sdk/execution_time/supervisor.py
@@ -87,6 +87,7 @@ from airflow.sdk.execution_time.comms import (
     PrevSuccessfulDagRunResult,
     PutVariable,
     RescheduleTask,
+    RetryTask,
     SetRenderedFields,
     SetXCom,
     SkipDownstreamTasks,
@@ -125,6 +126,7 @@ MAX_FAILED_HEARTBEATS: int = conf.getint("workers", "max_failed_heartbeats")
 STATES_SENT_DIRECTLY = [
     IntermediateTIState.DEFERRED,
     IntermediateTIState.UP_FOR_RESCHEDULE,
+    IntermediateTIState.UP_FOR_RETRY,
     TerminalTIState.SUCCESS,
 ]
 
@@ -912,6 +914,13 @@ class ActivitySubprocess(WatchedSubprocess):
                 when=msg.end_date,
                 task_outlets=msg.task_outlets,
                 outlet_events=msg.outlet_events,
+            )
+        elif isinstance(msg, RetryTask):
+            self._terminal_state = msg.state
+            self._task_end_time_monotonic = time.monotonic()
+            self.client.task_instances.retry(
+                id=self.id,
+                end_date=msg.end_date,
             )
         elif isinstance(msg, GetConnection):
             conn = self.client.connections.get(msg.conn_id)

task-sdk/tests/task_sdk/api/test_client.py
@@ -375,6 +375,22 @@ class TestTaskInstanceOperations:
         )
         client.task_instances.reschedule(ti_id, msg)
 
+    def test_task_instance_up_for_retry(self):
+        ti_id = uuid6.uuid7()
+
+        def handle_request(request: httpx.Request) -> httpx.Response:
+            if request.url.path == f"/task-instances/{ti_id}/state":
+                actual_body = json.loads(request.read())
+                assert actual_body["state"] == "up_for_retry"
+                assert actual_body["end_date"] == "2024-10-31T12:00:00Z"
+                return httpx.Response(
+                    status_code=204,
+                )
+            return httpx.Response(status_code=400, json={"detail": "Bad Request"})
+
+        client = make_client(transport=httpx.MockTransport(handle_request))
+        client.task_instances.retry(ti_id, end_date=timezone.parse("2024-10-31T12:00:00Z"))
+
     @pytest.mark.parametrize(
         "rendered_fields",
         [

task-sdk/tests/task_sdk/execution_time/test_supervisor.py
@@ -78,6 +78,7 @@ from airflow.sdk.execution_time.comms import (
     PrevSuccessfulDagRunResult,
     PutVariable,
     RescheduleTask,
+    RetryTask,
     SetRenderedFields,
     SetXCom,
     SucceedTask,
@@ -1164,6 +1165,15 @@ class TestHandleRequest:
                 {},
                 "",
                 id="patch_task_instance_to_skipped",
+            ),
+            pytest.param(
+                RetryTask(end_date=timezone.parse("2024-10-31T12:00:00Z")),
+                b"",
+                "task_instances.retry",
+                (),
+                {"id": TI_ID, "end_date": timezone.parse("2024-10-31T12:00:00Z")},
+                "",
+                id="up_for_retry",
             ),
             pytest.param(
                 SetRenderedFields(rendered_fields={"field1": "rendered_value1", "field2": "rendered_value2"}),