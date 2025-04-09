bb13c60dad38f65459298b70686edcc8b0631ad3
Bugra Ozturk <ozturkbugra93@gmail.com>
2025-04-10 00:17:09 +0200

    feat (airflowctl): Initial airflowctl docs

--- devel-common/src/sphinx_exts/docs_build/docs_builder.py
+++ devel-common/src/sphinx_exts/docs_build/docs_builder.py
@@ -65,6 +65,8 @@ class AirflowDocsBuilder:
             self.is_docker_stack = True
         if self.package_name == "apache-airflow-providers":
             self.is_providers_summary = True
+        if self.package_name == "apache-airflow-ctl":
+            self.is_airflow_ctl = True
 
     @property
     def _doctree_dir(self) -> Path:
@@ -117,8 +119,11 @@ class AirflowDocsBuilder:
         if self.package_name.startswith("apache-airflow-providers-"):
             package_paths = self.package_name[len("apache-airflow-providers-") :].split("-")
             return (AIRFLOW_CONTENT_ROOT_PATH / "providers").joinpath(*package_paths) / "docs"
-        console.print(f"[red]Unknown package name: {self.package_name}")
-        sys.exit(1)
+        elif self.package_name == "apache-airflow-ctl":
+            return AIRFLOW_CONTENT_ROOT_PATH / "airflow-ctl" / "docs"
+        else:
+            console.print(f"[red]Unknown package name: {self.package_name}")
+            sys.exit(1)
 
     @property
     def pythonpath(self) -> list[Path]:
@@ -330,6 +335,7 @@ def get_available_packages(include_suspended: bool = False, short_form: bool = F
         "apache-airflow",
         *provider_names,
         "apache-airflow-providers",
+        "apache-airflow-ctl",
         "helm-chart",
         "docker-stack",
     ]
