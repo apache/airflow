bb5f6969ea2f5d2e9c7ec8217ea9ca57701a36e3
Tzu-ping Chung <uranusjr@gmail.com>
2025-04-09 16:57:24 +0800

    Move ObjectStoragePath tests to sdk

--- task-sdk/tests/task_sdk/io/__init__.py
+++ task-sdk/tests/task_sdk/io/__init__.py
@@ -14,10 +14,3 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-
-from __future__ import annotations
-
-from airflow.sdk.io.path import ObjectStoragePath
-from airflow.sdk.io.store import attach
-
-__all__ = ["ObjectStoragePath", "attach"]
