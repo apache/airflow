52302a186be384a9771f8129bec4d8e5afd60e95
Wei Lee <weilee.rx@gmail.com>
2025-04-15 18:07:54 +0800

    fix(serialized_objects): handle both inlet and outlet in v1 to v2 convert

--- airflow-core/src/airflow/serialization/serialized_objects.py
+++ airflow-core/src/airflow/serialization/serialized_objects.py
@@ -1903,15 +1903,15 @@ class SerializedDAG(DAG, BaseSerialization):
                 task_var.pop(k, None)
             for old, new in task_renames:
                 task_var[new] = task_var.pop(old)
-            for item in task_var.get("outlets", []):
+            for item in itertools.chain(*(task_var.get(key, []) for key in ("inlets", "outlets"))):
+                original_item_type = item["__type"]
                 if isinstance(item, dict) and "__type" in item:
-                    item["__type"] = replace_dataset_in_str(item["__type"])
-            for item in task_var.get("inlets", []):
-                if isinstance(item, dict) and "__type" in item:
-                    item["__type"] = replace_dataset_in_str(item["__type"])
+                    item["__type"] = replace_dataset_in_str(original_item_type)
+
                 var_ = item["__var"]
-                var_["name"] = None
-                var_["group"] = None
+                if original_item_type == "dataset":
+                    var_["name"] = var_["uri"]
+                var_["group"] = "asset"
 
         # Set on the root TG
         dag_dict["task_group"]["group_display_name"] = ""
