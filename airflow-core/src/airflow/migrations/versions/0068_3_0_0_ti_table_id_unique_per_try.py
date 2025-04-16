438220f6b5e6c686015fefddb73daba1a8a5f9d4
Rahul Vats <43964496+vatsrahul1001@users.noreply.github.com>
2025-04-16 13:53:18 +0530

    Fix MySQL migration (#49315)
    Fix MySQL migration (#49315)

--- airflow-core/src/airflow/migrations/versions/0068_3_0_0_ti_table_id_unique_per_try.py
+++ airflow-core/src/airflow/migrations/versions/0068_3_0_0_ti_table_id_unique_per_try.py
@@ -40,7 +40,7 @@ airflow_version = "3.0.0"
 
 
 def _get_uuid_type(dialect_name: str) -> sa.types.TypeEngine:
-    if dialect_name == "sqlite":
+    if dialect_name != "postgres":
         return sa.String(36)
     return UUIDType(binary=False)
 
