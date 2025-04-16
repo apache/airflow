73c9376ceceb1ea97fccbfe787dba3dde4af55ee
Jarek Potiuk <jarek@potiuk.com>
2025-04-16 15:26:51 +0200

    Add openlineage to chicken-egg providers so that we use latest RC

--- dev/breeze/src/airflow_breeze/global_constants.py
+++ dev/breeze/src/airflow_breeze/global_constants.py
@@ -635,7 +635,7 @@ DEFAULT_EXTRAS = [
     # END OF EXTRAS LIST UPDATED BY PRE COMMIT
 ]
 
-CHICKEN_EGG_PROVIDERS = " ".join(["git", "common.messaging", "fab", "standard"])
+CHICKEN_EGG_PROVIDERS = " ".join(["git", "common.messaging", "fab", "standard", "openlineage"])
 
 
 PROVIDERS_COMPATIBILITY_TESTS_MATRIX: list[dict[str, str | list[str]]] = [
