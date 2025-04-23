fffa9454747e92f9c42edd66a70617c1feaa018d
Bugra Ozturk <bugraoz93@users.noreply.github.com>
2025-04-23 19:03:28 +0200

    Fix SnowflakeSqlApiHook backwards compatibility for get_oauth_token method (#49482)
    * Fix SnowflakeSqlApiHook backwards compatibility
    
    * Update providers/snowflake/src/airflow/providers/snowflake/hooks/snowflake_sql_api.py
    
    ---------
    
    Co-authored-by: Elad Kalif <45845474+eladkal@users.noreply.github.com>

--- providers/snowflake/src/airflow/providers/snowflake/hooks/snowflake.py
+++ providers/snowflake/src/airflow/providers/snowflake/hooks/snowflake.py
@@ -189,9 +189,24 @@ class SnowflakeHook(DbApiHook):
             return extra_dict[field_name] or None
         return extra_dict.get(backcompat_key) or None
 
-    def get_oauth_token(self, conn_config: dict) -> str:
+    @property
+    def account_identifier(self) -> str:
+        """Returns snowflake account identifier."""
+        conn_config = self._get_conn_params
+        account_identifier = f"https://{conn_config['account']}"
+
+        if conn_config["region"]:
+            account_identifier += f".{conn_config['region']}"
+
+        return account_identifier
+
+    def get_oauth_token(self, conn_config: dict | None = None) -> str:
         """Generate temporary OAuth access token using refresh token in connection details."""
-        url = f"https://{conn_config['account']}.snowflakecomputing.com/oauth/token-request"
+        if conn_config is None:
+            conn_config = self._get_conn_params
+
+        url = f"{self.account_identifier}.snowflakecomputing.com/oauth/token-request"
+
         data = {
             "grant_type": "refresh_token",
             "refresh_token": conn_config["refresh_token"],
