92776cf65b9ad74b71e830828112a01a0c75b45f
Daniel Standish <15932138+dstandish@users.noreply.github.com>
2025-04-24 10:08:57 -0700

    fix test

--- airflow-core/tests/unit/cli/commands/test_asset_command.py
+++ airflow-core/tests/unit/cli/commands/test_asset_command.py
@@ -141,6 +141,7 @@ def test_cli_assets_materialize(parser: ArgumentParser) -> None:
 
     assert run_list[0] | undeterministic == undeterministic | {
         "conf": {},
+        "bundle_version": None,
         "dag_id": "asset1_producer",
         "end_date": None,
         "last_scheduling_decision": None,
