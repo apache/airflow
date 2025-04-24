cbc4b9700eaafb27bd4003f873d39c4a983f467f
GPK <gopidesupavan@gmail.com>
2025-04-24 23:09:09 +0100

    Set execution api url in breeze config environment (#49753)
    * Set execution api url in breeze config environment
    
    * Set execution api url from breeze shell params

--- dev/breeze/src/airflow_breeze/params/shell_params.py
+++ dev/breeze/src/airflow_breeze/params/shell_params.py
@@ -523,6 +523,7 @@ class ShellParams:
         _set_var(_env, "AIRFLOW__CELERY__BROKER_URL", self.airflow_celery_broker_url)
         _set_var(_env, "AIRFLOW__CORE__AUTH_MANAGER", self.auth_manager_path)
         _set_var(_env, "AIRFLOW__CORE__EXECUTOR", self.executor)
+        _set_var(_env, "AIRFLOW__CORE__EXECUTION_API_SERVER_URL", "http://localhost:8080/execution/")
         if self.auth_manager == SIMPLE_AUTH_MANAGER:
             _set_var(_env, "AIRFLOW__CORE__SIMPLE_AUTH_MANAGER_USERS", "admin:admin,viewer:viewer")
         _set_var(
