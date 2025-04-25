1c3cd1c2d17bd7ddaa7797e127a9f77df15cc484
Sneha Prabhu <snehaprabhu@Snehas-MacBook-Pro.local>
2025-04-25 12:20:43 +0530

    Revert "Fix imports"
    This reverts commit 6fecc928636625396141a543707a9aaeac446547.

--- airflow-core/src/airflow/models/connection.py
+++ airflow-core/src/airflow/models/connection.py
@@ -33,7 +33,7 @@ from airflow.configuration import ensure_secrets_loaded
 from airflow.exceptions import AirflowException, AirflowNotFoundException
 from airflow.models.base import ID_LEN, Base
 from airflow.models.crypto import get_fernet
-from airflow.sdk.execution_time.cache import SecretCache
+from airflow.sdk import SecretCache
 from airflow.sdk.execution_time.secrets_masker import mask_secret
 from airflow.utils.helpers import prune_dict
 from airflow.utils.log.logging_mixin import LoggingMixin
