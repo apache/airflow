1b232b90a3303ad730515b45ae6792030f44e2b1
Vincent <97131062+vincbeck@users.noreply.github.com>
2025-04-25 12:24:28 -0400

    Fix AWS system test names (#49791)

--- providers/amazon/tests/system/amazon/aws/example_redshift_s3_transfers.py
+++ providers/amazon/tests/system/amazon/aws/example_redshift_s3_transfers.py
@@ -38,7 +38,7 @@ from airflow.utils.trigger_rule import TriggerRule
 
 from system.amazon.aws.utils import ENV_ID_KEY, SystemTestContextBuilder
 
-DAG_ID = "example_redshift_to_s3"
+DAG_ID = "example_redshift_s3_transfers"
 
 # Externally fetched variables:
 SECURITY_GROUP_KEY = "SECURITY_GROUP"
