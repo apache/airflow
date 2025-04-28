0f573ee40bfb7d9145328afd55bc16ca8598d008
Elad Kalif <45845474+eladkal@users.noreply.github.com>
2025-04-28 12:36:41 +0300

    Bump min Airflow version in providers to 2.10 (#49843)
    * Bump min Airflow version in providers to 2.10
    
    * exclude msgraph test
    
    * fixes

--- dev/breeze/src/airflow_breeze/global_constants.py
+++ dev/breeze/src/airflow_breeze/global_constants.py
@@ -689,12 +689,6 @@ DEFAULT_EXTRAS = [
 PROVIDERS_COMPATIBILITY_TESTS_MATRIX: list[dict[str, str | list[str]]] = [
     {
         "python-version": "3.9",
-        "airflow-version": "2.9.3",
-        "remove-providers": "cloudant common.messaging fab edge3 git",
-        "run-tests": "true",
-    },
-    {
-        "python-version": "3.9",
         "airflow-version": "2.10.5",
         "remove-providers": "cloudant common.messaging fab git",
         "run-tests": "true",
--- providers/apache/impala/README.rst
+++ providers/apache/impala/README.rst
@@ -55,7 +55,7 @@ PIP package                              Version required
 =======================================  ==================
 ``impyla``                               ``>=0.18.0,<1.0``
 ``apache-airflow-providers-common-sql``  ``>=1.20.0``
-``apache-airflow``                       ``>=2.9.0``
+``apache-airflow``                       ``>=2.10.0``
 =======================================  ==================
 
 Cross provider package dependencies
