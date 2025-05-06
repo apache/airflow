3b8bfa3f36f3a3bbc080ff9114636d7baa812f99
Guangyang Li <mail@guangyangli.com>
2025-05-06 17:39:19 -0400

    add new revision

--- airflow-core/src/airflow/migrations/versions/0050_3_0_0_add_dagbundlemodel.py
+++ airflow-core/src/airflow/migrations/versions/0050_3_0_0_add_dagbundlemodel.py
@@ -28,7 +28,6 @@ from __future__ import annotations
 
 import sqlalchemy as sa
 from alembic import op
-from sqlalchemy.sql import text
 
 from airflow.migrations.db_types import StringID
 from airflow.utils.sqlalchemy import UtcDateTime
@@ -49,31 +48,12 @@ def upgrade():
         sa.Column("last_refreshed", UtcDateTime(timezone=True), nullable=True),
         sa.PrimaryKeyConstraint("name", name=op.f("dag_bundle_pkey")),
     )
-
     with op.batch_alter_table("dag", schema=None) as batch_op:
         batch_op.add_column(sa.Column("bundle_name", sa.String(length=250), nullable=True))
         batch_op.add_column(sa.Column("bundle_version", sa.String(length=200), nullable=True))
-
         batch_op.create_foreign_key(
             batch_op.f("dag_bundle_name_fkey"), "dag_bundle", ["bundle_name"], ["name"]
         )
-
-        conn = op.get_bind()
-        conn.execute(
-            text(
-                """
-                UPDATE dag
-                SET bundle_name =
-                    CASE
-                        WHEN fileloc LIKE '%/airflow/airflow/example_dags/%' THEN 'example_dags'
-                        ELSE 'dags-folder'
-                    END
-                WHERE bundle_name IS NULL
-                """
-            )
-        )
-
-        batch_op.alter_column("bundle_name", nullable=False, existing_type=sa.String(length=250))
 
     with op.batch_alter_table("dag_run", schema=None) as batch_op:
         batch_op.add_column(sa.Column("bundle_version", sa.String(length=250), nullable=True))
