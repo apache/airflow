785ae8d139d0c3c2d04d8e61125a20f2108f23bc
Ephraim Anierobi <splendidzigy24@gmail.com>
2025-05-08 15:07:40 +0100

    Raise exception if downgrade can't proceed due to no `ab_user` table (#50343)
    * Raise exception if downgrade can't proceed due to no `ab_user` table
    
    We only log the error that the downgrade can't proceed if the `ab_user`
    table is not found, but we should raise an exception instead.
    Currently, the CI migrations don't exit on error, which prevents the necessary tests from running.
    
    Also updated the CI migration tests to export DB manager at the shell
    because it seems that's not happening, hence why downgrade is running correctly.
    
    * result can be none so check before accessing row_count
    
    * dont raise in unittest mode

--- airflow-core/src/airflow/utils/db.py
+++ airflow-core/src/airflow/utils/db.py
@@ -1219,7 +1219,8 @@ def downgrade(*, to_revision, from_revision=None, show_sql_only=False, session: 
     config = _get_alembic_config()
     # Check if downgrade is less than 3.0.0 and requires that `ab_user` fab table is present
     if _revision_greater(config, _REVISION_HEADS_MAP["3.0.0"], to_revision):
-        if conf.getboolean("core", "unit_test_mode"):
+        unitest_mode = conf.getboolean("core", "unit_test_mode")
+        if unitest_mode:
             try:
                 from airflow.providers.fab.auth_manager.models.db import FABDBManager
 
@@ -1227,13 +1228,12 @@ def downgrade(*, to_revision, from_revision=None, show_sql_only=False, session: 
                 dbm.initdb()
             except ImportError:
                 log.warning("Import error occurred while importing FABDBManager. Skipping the check.")
-                pass
-        if not inspect(settings.engine).has_table("ab_user"):
-            log.error(
+                return
+        if not inspect(settings.engine).has_table("ab_user") and not unitest_mode:
+            raise AirflowException(
                 "Downgrade to revision less than 3.0.0 requires that `ab_user` table is present. "
                 "Please add FabDBManager to [core] external_db_managers and run fab migrations before proceeding"
             )
-            return
     with create_global_lock(session=session, lock=DBLocks.MIGRATIONS):
         if show_sql_only:
             log.warning("Generating sql scripts for manual migration.")
