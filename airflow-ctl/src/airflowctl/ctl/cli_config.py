23678a906a7785623557ee73a0489a378903b831
Yunchi Pang <yunchipang@gmail.com>
2025-05-12 21:00:34 -0700

    implement lint config command in airflowctl

--- airflow-ctl/src/airflowctl/ctl/cli_config.py
+++ airflow-ctl/src/airflowctl/ctl/cli_config.py
@@ -192,6 +192,39 @@ ARG_AUTH_PASSWORD = Arg(
     nargs="?",
 )
 
+# Config arguments
+ARG_CONFIG_SECTION = Arg(
+    flags=("--section",),
+    type=str,
+    dest="section",
+    help="The section of the configuration",
+)
+ARG_CONFIG_OPTION = Arg(
+    flags=("--option",),
+    type=str,
+    dest="option",
+    help="The option of the configuration",
+)
+ARG_CONFIG_IGNORE_SECTION = Arg(
+    flags=("--ignore-section",),
+    type=str,
+    dest="ignore_section",
+    help="The configuration section being ignored",
+)
+ARG_CONFIG_IGNORE_OPTION = Arg(
+    flags=("--ignore-option",),
+    type=str,
+    dest="ignore_option",
+    help="The configuration option being ignored",
+)
+ARG_CONFIG_VERBOSE = Arg(
+    flags=(
+        "-v",
+        "--verbose",
+    ),
+    help="Enables detailed output, including the list of ignored sections and options",
+)
+
 
 class ActionCommand(NamedTuple):
     """Single CLI command."""
@@ -546,6 +579,22 @@ AUTH_COMMANDS = (
     ),
 )
 
+CONFIG_COMMANDS = (
+    ActionCommand(
+        name="lint",
+        help="Lint options for the configuration changes while migrating from Airflow 2.x to Airflow 3.0",
+        description="Lint options for the configuration changes while migrating from Airflow 2.x to Airflow 3.0",
+        func=lazy_load_command("airflowctl.ctl.commands.config_command.lint"),
+        args=(
+            ARG_CONFIG_SECTION,
+            ARG_CONFIG_OPTION,
+            ARG_CONFIG_IGNORE_SECTION,
+            ARG_CONFIG_IGNORE_OPTION,
+            ARG_CONFIG_VERBOSE,
+        ),
+    ),
+)
+
 
 core_commands: list[CLICommand] = [
     GroupCommand(
@@ -553,6 +602,11 @@ core_commands: list[CLICommand] = [
         help="Manage authentication for CLI. "
         "Either pass token from environment variable/parameter or pass username and password.",
         subcommands=AUTH_COMMANDS,
+    ),
+    GroupCommand(
+        name="config",
+        help="View, lint and update configurations.",
+        subcommands=CONFIG_COMMANDS,
     ),
 ]
 # Add generated group commands
