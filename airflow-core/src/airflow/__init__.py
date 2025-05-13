6e487fce71796a1d18607c30ef2f77dd3edc7d46
Kaxil Naik <kaxilnaik@gmail.com>
2025-05-14 02:36:13 +0530

    Bump version to 3.0.2

--- airflow-core/src/airflow/__init__.py
+++ airflow-core/src/airflow/__init__.py
@@ -25,7 +25,7 @@
 # lib.)  This is required by some IDEs to resolve the import paths.
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
 
-__version__ = "3.0.1"
+__version__ = "3.0.2"
 
 
 import os
