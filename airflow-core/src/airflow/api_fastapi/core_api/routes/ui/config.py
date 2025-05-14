15a142f971e4d7d2865a2f301df821d2e8aeb50b
Jed Cunningham <66968678+jedcunningham@users.noreply.github.com>
2025-05-14 10:14:55 -0600

    Fix permission check on the ui config endpoint (#50564)
    This is just config for the UI, not the whole Airflow instances config,
    which is what the config permission for users controls. If the user
    doesn't have the config permission, without this change, the user cannot
    use the UI at all.
    
    So, instead we just check that the user is authenticated at all.
    
    Co-authored-by: LIU ZHE YOU <68415893+jason810496@users.noreply.github.com>

--- airflow-core/src/airflow/api_fastapi/core_api/routes/ui/config.py
+++ airflow-core/src/airflow/api_fastapi/core_api/routes/ui/config.py
@@ -23,7 +23,7 @@ from fastapi import Depends, status
 from airflow.api_fastapi.common.router import AirflowRouter
 from airflow.api_fastapi.core_api.datamodels.ui.config import ConfigResponse
 from airflow.api_fastapi.core_api.openapi.exceptions import create_openapi_http_exception_doc
-from airflow.api_fastapi.core_api.security import requires_access_configuration
+from airflow.api_fastapi.core_api.security import requires_authenticated
 from airflow.configuration import conf
 from airflow.settings import DASHBOARD_UIALERTS
 from airflow.utils.log.log_reader import TaskLogReader
@@ -49,7 +49,7 @@ WEBSERVER_CONFIG_KEYS = [
 @config_router.get(
     "/config",
     responses=create_openapi_http_exception_doc([status.HTTP_404_NOT_FOUND]),
-    dependencies=[Depends(requires_access_configuration("GET"))],
+    dependencies=[Depends(requires_authenticated())],
 )
 def get_configs() -> ConfigResponse:
     """Get configs for UI."""
