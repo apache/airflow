5c3a1ecb96f46fd59fdb3bf0b1a8be4b73c66283
Daniel Standish <15932138+dstandish@users.noreply.github.com>
2025-05-14 07:59:23 -0700

    Always create serdag in dagmaker fixture (#50359)
    This PR makes it so dag_maker always creates serialized dag and dagversion objects, along with dag model.
    
    It's not really sensible anymore to not have these other objects there; they are always there in production, and increasingly we need them there for code to work right, and their omission can leave some bugs hidden (and some of them are resolved as part of this).
    
    Initially, I was going to just remove the option, but, it also controls the type of dag object returned by dag_maker (serdag vs dag), so for now I leave that as is.
    
    ---------
    
    Co-authored-by: Tzu-ping Chung <uranusjr@gmail.com>

--- airflow-core/src/airflow/serialization/serialized_objects.py
+++ airflow-core/src/airflow/serialization/serialized_objects.py
@@ -230,12 +230,26 @@ class _PriorityWeightStrategyNotRegistered(AirflowException):
 
 
 def _encode_trigger(trigger: BaseEventTrigger | dict):
+    def _ensure_serialized(d):
+        """
+        Make sure the kwargs dict is JSON-serializable.
+
+        This is done with BaseSerialization logic. A simple check is added to
+        ensure we don't double-serialize, which is possible when a trigger goes
+        through multiple serialization layers.
+        """
+        if isinstance(d, dict) and Encoding.TYPE in d:
+            return d
+        return BaseSerialization.serialize(d)
+
     if isinstance(trigger, dict):
-        return trigger
-    classpath, kwargs = trigger.serialize()
+        classpath = trigger["classpath"]
+        kwargs = trigger["kwargs"]
+    else:
+        classpath, kwargs = trigger.serialize()
     return {
         "classpath": classpath,
-        "kwargs": kwargs,
+        "kwargs": {k: _ensure_serialized(v) for k, v in kwargs.items()},
     }
 
 
@@ -303,6 +317,18 @@ def decode_asset_condition(var: dict[str, Any]) -> BaseAsset:
 
 
 def decode_asset(var: dict[str, Any]):
+    def _smart_decode_trigger_kwargs(d):
+        """
+        Slightly clean up kwargs for display.
+
+        This detects one level of BaseSerialization and tries to deserialize the
+        content, removing some __type __var ugliness when the value is displayed
+        in UI to the user.
+        """
+        if not isinstance(d, dict) or Encoding.TYPE not in d:
+            return d
+        return BaseSerialization.deserialize(d)
+
     watchers = var.get("watchers", [])
     return Asset(
         name=var["name"],
@@ -310,7 +336,14 @@ def decode_asset(var: dict[str, Any]):
         group=var["group"],
         extra=var["extra"],
         watchers=[
-            SerializedAssetWatcher(name=watcher["name"], trigger=watcher["trigger"]) for watcher in watchers
+            SerializedAssetWatcher(
+                name=watcher["name"],
+                trigger={
+                    "classpath": watcher["trigger"]["classpath"],
+                    "kwargs": _smart_decode_trigger_kwargs(watcher["trigger"]["kwargs"]),
+                },
+            )
+            for watcher in watchers
         ],
     )
 
