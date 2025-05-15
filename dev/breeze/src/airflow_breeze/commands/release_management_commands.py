ea8eb1f1c769309b3a23d33af7ff24eb867134ef
GPK <gopidesupavan@gmail.com>
2025-05-15 20:43:36 +0100

    Add SKIP_WRITE_TO_STABLE_FOLDER to publish docs (#50670)

--- dev/breeze/src/airflow_breeze/commands/release_management_commands.py
+++ dev/breeze/src/airflow_breeze/commands/release_management_commands.py
@@ -3788,6 +3788,11 @@ def generate_issue_content(
     is_flag=True,
     help="Publish all the stable versions of the docs in the source directory.",
 )
+@click.option(
+    "--skip-write-to-stable-folder",
+    is_flag=True,
+    help="Skip writing stable versions folder.",
+)
 @option_parallelism
 def publish_docs_to_s3(
     source_dir_path: str,
@@ -3798,6 +3803,7 @@ def publish_docs_to_s3(
     parallelism: int,
     publish_all_docs: bool,
     stable_versions: bool,
+    skip_write_to_stable_folder: bool,
 ):
     from airflow_breeze.utils.publish_docs_to_s3 import S3DocsPublish
 
@@ -3820,6 +3826,7 @@ def publish_docs_to_s3(
         overwrite=overwrite,
         destination_location=destination_location,
         parallelism=parallelism,
+        skip_write_to_stable_folder=skip_write_to_stable_folder,
     )
     if publish_all_docs:
         docs_to_s3.publish_all_docs()
