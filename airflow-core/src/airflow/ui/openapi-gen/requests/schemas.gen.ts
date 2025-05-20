cd89cdd24de5149a40ed3080e29b591bc9ebddd4
ayush3singh <yesayushcan@gmail.com>
2025-05-20 14:49:54 +0530

    [v3-0-test] make dag_run nullable in Details page (#50719) (cherry picked from commit e6282e0e654dfb26b58ae741a5654fc31f4e08a5)
    Co-authored-by: ayush3singh <yesayushcan@gmail.com>

--- airflow-core/src/airflow/ui/openapi-gen/requests/schemas.gen.ts
+++ airflow-core/src/airflow/ui/openapi-gen/requests/schemas.gen.ts
@@ -2247,8 +2247,15 @@ export const $DAGRunResponse = {
       ],
     },
     conf: {
-      additionalProperties: true,
-      type: "object",
+      anyOf: [
+        {
+          additionalProperties: true,
+          type: "object",
+        },
+        {
+          type: "null",
+        },
+      ],
       title: "Conf",
     },
     note: {
