f4b6edc70e863035e95e9e07b0f84a5f9f8e5170
Vincent Kling <vkling@vinniict.nl>
2025-05-20 07:55:49 +0200

    Fix tests

--- kubernetes-tests/tests/kubernetes_tests/test_kubernetes_pod_operator.py
+++ kubernetes-tests/tests/kubernetes_tests/test_kubernetes_pod_operator.py
@@ -998,7 +998,6 @@ class TestKubernetesPodOperatorSystem:
             },
             "spec": {
                 "affinity": {},
-                "automountServiceAccountToken": True,
                 "containers": [
                     {
                         "args": ["--vm", "1", "--vm-bytes", "150M", "--vm-hang", "1"],
