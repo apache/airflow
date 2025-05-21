ad923b374fd568a14ec813411ccc36619c035d2e
Amogh Desai <amoghrajesh1999@gmail.com>
2025-05-21 16:04:07 +0530

    [v3-0-test] Support macros defined via plugins in Airflow 3 (#50642) (#50808)
    (cherry picked from commit f008411fbd9679c2abbdc471e2a003e4f65e705e)

--- airflow-core/src/airflow/plugins_manager.py
+++ airflow-core/src/airflow/plugins_manager.py
@@ -507,7 +507,7 @@ def integrate_macros_plugins() -> None:
     global plugins
     global macros_modules
 
-    from airflow import macros
+    from airflow.sdk.definitions import macros
 
     if macros_modules is not None:
         return
@@ -525,7 +525,7 @@ def integrate_macros_plugins() -> None:
         if plugin.name is None:
             raise AirflowPluginException("Invalid plugin name")
 
-        macros_module = make_module(f"airflow.macros.{plugin.name}", plugin.macros)
+        macros_module = make_module(f"airflow.sdk.definitions.macros.{plugin.name}", plugin.macros)
 
         if macros_module:
             macros_modules.append(macros_module)
