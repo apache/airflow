960f0ed3683d651980dd2d23594e2e11d1bee476
vincbeck <vincbeck@amazon.com>
2025-05-22 11:30:04 -0400

    Call `init_airflow_session_interface`

--- providers/fab/src/airflow/providers/fab/www/app.py
+++ providers/fab/src/airflow/providers/fab/www/app.py
@@ -33,6 +33,7 @@ from airflow.providers.fab.www.extensions.init_appbuilder import init_appbuilder
 from airflow.providers.fab.www.extensions.init_jinja_globals import init_jinja_globals
 from airflow.providers.fab.www.extensions.init_manifest_files import configure_manifest_files
 from airflow.providers.fab.www.extensions.init_security import init_api_auth
+from airflow.providers.fab.www.extensions.init_session import init_airflow_session_interface
 from airflow.providers.fab.www.extensions.init_views import (
     init_api_auth_provider,
     init_api_error_handlers,
@@ -95,7 +96,7 @@ def create_app(enable_plugins: bool):
         elif isinstance(get_auth_manager(), FabAuthManager):
             init_api_auth_provider(flask_app)
             init_api_error_handlers(flask_app)
-            # init_airflow_session_interface(flask_app)
+            init_airflow_session_interface(flask_app)
         init_jinja_globals(flask_app, enable_plugins=enable_plugins)
         init_wsgi_middleware(flask_app)
     return flask_app
