4eac5913e04915d66f9ff571ff93c41b1a1d137c
Ephraim Anierobi <splendidzigy24@gmail.com>
2025-05-25 20:20:03 +0100

    fixup! Update more tests

--- providers/snowflake/tests/unit/snowflake/operators/test_snowflake.py
+++ providers/snowflake/tests/unit/snowflake/operators/test_snowflake.py
@@ -73,6 +73,11 @@ class TestSnowflakeOperator:
         operator = SQLExecuteQueryOperator(
             task_id="basic_snowflake", sql=sql, dag=self.dag, do_xcom_push=False, conn_id="snowflake_default"
         )
+        if AIRFLOW_V_3_0_PLUS:
+            from airflow.models.serialized_dag import SerializedDagModel
+
+            self.dag.sync_to_db()
+            SerializedDagModel.write_dag(self.dag, bundle_name="testing")
         # do_xcom_push=False because otherwise the XCom test will fail due to the mocking (it actually works)
         operator.run(start_date=DEFAULT_DATE, end_date=DEFAULT_DATE, ignore_ti_state=True)
 
@@ -180,6 +185,13 @@ def create_context(task, dag=None):
     tzinfo = pendulum.timezone("UTC")
     logical_date = timezone.datetime(2022, 1, 1, 1, 0, 0, tzinfo=tzinfo)
     if AIRFLOW_V_3_0_PLUS:
+        from airflow.models.dag_version import DagVersion
+        from airflow.models.serialized_dag import SerializedDagModel
+
+        dag.sync_to_db()
+        SerializedDagModel.write_dag(dag, bundle_name="testing")
+        dag_version = DagVersion.get_latest_version(dag.dag_id)
+        task_instance = TaskInstance(task=task, run_id="test_run_id", dag_version_id=dag_version.id)
         dag_run = DagRun(
             dag_id=dag.dag_id,
             logical_date=logical_date,
@@ -194,7 +206,7 @@ def create_context(task, dag=None):
             run_id=DagRun.generate_run_id(DagRunType.MANUAL, logical_date),
         )
 
-    task_instance = TaskInstance(task=task)
+        task_instance = TaskInstance(task=task)
     task_instance.dag_run = dag_run
     task_instance.xcom_push = mock.Mock()
     date_key = "logical_date" if AIRFLOW_V_3_0_PLUS else "execution_date"
