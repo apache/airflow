2cd1b735d451b4b51ebeed6774a2838b52cf3190
Ephraim Anierobi <splendidzigy24@gmail.com>
2025-05-30 18:14:31 +0100

    Add cadwyn migration and fix some tests

--- task-sdk/src/airflow/sdk/api/datamodels/_generated.py
+++ task-sdk/src/airflow/sdk/api/datamodels/_generated.py
@@ -27,7 +27,7 @@ from uuid import UUID
 
 from pydantic import AwareDatetime, BaseModel, ConfigDict, Field, JsonValue
 
-API_VERSION: Final[str] = "2025-05-20"
+API_VERSION: Final[str] = "2025-08-10"
 
 
 class AssetAliasReferenceAssetEventDagRun(BaseModel):
