192c69eb86fb70548f750a0c80ad3ce7d4adc01e
Ephraim Anierobi <splendidzigy24@gmail.com>
2025-05-30 20:42:54 +0100

    fix some provider tests

--- providers/cncf/kubernetes/src/airflow/providers/cncf/kubernetes/cli/kubernetes_command.py
+++ providers/cncf/kubernetes/src/airflow/providers/cncf/kubernetes/cli/kubernetes_command.py
@@ -27,6 +27,7 @@ from kubernetes.client.api_client import ApiClient
 from kubernetes.client.rest import ApiException
 
 from airflow.models import DagModel, DagRun, TaskInstance
+from airflow.models.serialized_dag import SerializedDagModel
 from airflow.providers.cncf.kubernetes import pod_generator
 from airflow.providers.cncf.kubernetes.executors.kubernetes_executor import KubeConfig
 from airflow.providers.cncf.kubernetes.kube_client import get_kube_client
@@ -46,6 +47,8 @@ def generate_pod_yaml(args):
     logical_date = args.logical_date if AIRFLOW_V_3_0_PLUS else args.execution_date
     if AIRFLOW_V_3_0_PLUS:
         dag = get_dag(bundle_names=args.bundle_name, dag_id=args.dag_id)
+        dag.sync_to_db()
+        SerializedDagModel.write_dag(dag, bundle_name="testing")
     else:
         dag = get_dag(subdir=args.subdir, dag_id=args.dag_id)
     yaml_output_path = args.output_path
@@ -67,7 +70,10 @@ def generate_pod_yaml(args):
 
     for task in dag.tasks:
         if AIRFLOW_V_3_0_PLUS:
-            ti = TaskInstance(task, run_id=dr.run_id, dag_version_id=dr.created_dag_version_id)
+            from airflow.models.dag_version import DagVersion
+
+            dag_version = DagVersion.get_latest_version(dr.dag_id)
+            ti = TaskInstance(task, run_id=dr.run_id, dag_version_id=dag_version.id)
         else:
             ti = TaskInstance(task, run_id=dr.run_id)
         ti.dag_run = dr
