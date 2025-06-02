4b3309e1eb51e319b8cb74955809c6e672e8675c
Pierre Jeambrun <pierrejbrun@gmail.com>
2025-06-02 16:13:38 +0200

    Fix last dag run not showing on dag listing (#51115)
    * Fix last dag run not showing on dag listing
    
    * Small adjustments
    
    * Remove useless public getDags query invalidation

--- airflow-core/src/airflow/ui/src/queries/useTogglePause.ts
+++ airflow-core/src/airflow/ui/src/queries/useTogglePause.ts
@@ -22,9 +22,8 @@ import {
   UseDagRunServiceGetDagRunsKeyFn,
   UseDagServiceGetDagDetailsKeyFn,
   UseDagServiceGetDagKeyFn,
-  useDagServiceGetDagsKey,
   useDagServicePatchDag,
-  useDagServiceRecentDagRunsKey,
+  useDagServiceGetDagsUiKey,
   UseTaskInstanceServiceGetTaskInstancesKeyFn,
 } from "openapi/queries";
 
@@ -33,8 +32,7 @@ export const useTogglePause = ({ dagId }: { dagId: string }) => {
 
   const onSuccess = async () => {
     const queryKeys = [
-      [useDagServiceGetDagsKey],
-      [useDagServiceRecentDagRunsKey],
+      [useDagServiceGetDagsUiKey],
       UseDagServiceGetDagKeyFn({ dagId }, [{ dagId }]),
       UseDagServiceGetDagDetailsKeyFn({ dagId }, [{ dagId }]),
       UseDagRunServiceGetDagRunsKeyFn({ dagId }, [{ dagId }]),
