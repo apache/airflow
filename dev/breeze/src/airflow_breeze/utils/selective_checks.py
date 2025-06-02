7cb6c5c46333680e3cc0f99bdf64d6d4b9343e52
Bugra Ozturk <bugraoz93@users.noreply.github.com>
2025-06-02 21:48:24 +0200

    bugfix(breeze/selective-checks): Not Mocked Caplog Warning (#51272)
    * Fix search term
    
    * check one commit earlier

--- dev/breeze/src/airflow_breeze/utils/selective_checks.py
+++ dev/breeze/src/airflow_breeze/utils/selective_checks.py
@@ -1456,6 +1456,7 @@ class SelectiveChecks:
                         return True
                     line_counter += 1
             line_counter += 1
+        return None
 
     def _caplog_exists_in_added_lines(self) -> bool:
         """
@@ -1464,7 +1465,7 @@ class SelectiveChecks:
         :return: True if caplog is used in added lines else False
         """
         lines = run_command(
-            ["git", "diff", f"{self._commit_ref}"],
+            ["git", "diff", f"{self._commit_ref}^"],
             capture_output=True,
             text=True,
             cwd=AIRFLOW_ROOT_PATH,
@@ -1475,7 +1476,7 @@ class SelectiveChecks:
             return False
 
         added_caplog_lines = [
-            line.lstrip().lstrip("+ ") for line in lines.stdout.split("\n") if line.lstrip().startswith("+ ")
+            line.lstrip().lstrip("+") for line in lines.stdout.split("\n") if line.lstrip().startswith("+")
         ]
         return self._find_caplog_in_def(added_lines=added_caplog_lines)
 
