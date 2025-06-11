d8b7a5c03d430df588b354a30b9b8887a2e1364c
jj.lee <63435794+jx2lee@users.noreply.github.com>
2025-06-12 06:43:52 +0900

    airflowctl: convert to `BulkResponse` in variables.import (#51514)
    * to BulkResponse in variables.bulk
    
    * modified import cmd
    
    * unit tests
    
    * fix operation bulktest

--- airflow-ctl/tests/airflow_ctl/ctl/commands/test_variable_command.py
+++ airflow-ctl/tests/airflow_ctl/ctl/commands/test_variable_command.py
@@ -19,9 +19,12 @@ from __future__ import annotations
 import json
 import os
 
+import pytest
+
 from airflowctl.api.client import ClientKind
 from airflowctl.api.datamodels.generated import (
     BulkActionResponse,
+    BulkResponse,
     VariableCollectionResponse,
     VariableResponse,
 )
@@ -46,12 +49,24 @@ class TestCliVariableCommands:
         ],
         total_entries=1,
     )
-    bulk_action_response = BulkActionResponse(success=[key], errors=[])
+    bulk_response_success = BulkResponse(
+        create=BulkActionResponse(success=[key], errors=[]), update=None, delete=None
+    )
+    bulk_response_error = BulkResponse(
+        create=BulkActionResponse(
+            success=[],
+            errors=[
+                {"error": f"The variables with these keys: {{'{key}'}} already exist.", "status_code": 409}
+            ],
+        ),
+        update=None,
+        delete=None,
+    )
 
-    def test_import(self, api_client_maker, tmp_path, monkeypatch):
+    def test_import_success(self, api_client_maker, tmp_path, monkeypatch):
         api_client = api_client_maker(
             path="/api/v2/variables",
-            response_json=self.bulk_action_response.model_dump(),
+            response_json=self.bulk_response_success.model_dump(),
             expected_http_status_code=200,
             kind=ClientKind.CLI,
         )
@@ -67,7 +82,28 @@ class TestCliVariableCommands:
             self.parser.parse_args(["variables", "import", expected_json_path.as_posix()]),
             api_client=api_client,
         )
-        assert response == ([self.key], [])
+        assert response == [self.key]
+
+    def test_import_error(self, api_client_maker, tmp_path, monkeypatch):
+        api_client = api_client_maker(
+            path="/api/v2/variables",
+            response_json=self.bulk_response_error.model_dump(),
+            expected_http_status_code=200,
+            kind=ClientKind.CLI,
+        )
+
+        monkeypatch.chdir(tmp_path)
+        expected_json_path = tmp_path / self.export_file_name
+        variable_file = {
+            self.key: self.value,
+        }
+
+        expected_json_path.write_text(json.dumps(variable_file))
+        with pytest.raises(SystemExit):
+            variable_command.import_(
+                self.parser.parse_args(["variables", "import", expected_json_path.as_posix()]),
+                api_client=api_client,
+            )
 
     def test_export(self, api_client_maker, tmp_path, monkeypatch):
         api_client = api_client_maker(
