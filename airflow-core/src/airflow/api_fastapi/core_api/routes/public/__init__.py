aa016651d6f62f3ed2390708b5f78c1806be60c6
Wei Lee <weilee.rx@gmail.com>
2025-06-19 17:30:52 +0800

    refactor: rewrite content handling

--- airflow-core/src/airflow/api_fastapi/core_api/routes/public/__init__.py
+++ airflow-core/src/airflow/api_fastapi/core_api/routes/public/__init__.py
@@ -84,6 +84,10 @@ authenticated_router.include_router(dag_parsing_router)
 authenticated_router.include_router(dag_tags_router)
 authenticated_router.include_router(dag_versions_router)
 
+with contextlib.suppress(ModuleNotFoundError):
+    from airflow.providers.standard.api_fastapi.core_api.routes.interactive import interactive_router
+
+    authenticated_router.include_router(interactive_router)
 
 # Include authenticated router in public router
 public_router.include_router(authenticated_router)
