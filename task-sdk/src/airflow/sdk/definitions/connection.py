064490c7f159c7c803529fdce32536a2a4db4ad8
Amogh Desai <amoghrajesh1999@gmail.com>
2025-06-20 16:36:47 +0530

    Fix connection recovery from uri when host has protocol (#51953)

--- task-sdk/src/airflow/sdk/definitions/connection.py
+++ task-sdk/src/airflow/sdk/definitions/connection.py
@@ -74,10 +74,20 @@ class Connection:
 
         if self.host and "://" in self.host:
             protocol, host = self.host.split("://", 1)
+            # If the protocol in host matches the connection type, don't add it again
+            if protocol == self.conn_type:
+                host_to_use = self.host
+                protocol_to_add = None
+            else:
+                # Different protocol, add it to the URI
+                host_to_use = host
+                protocol_to_add = protocol
         else:
-            protocol, host = None, self.host or ""
-        if protocol:
-            uri += f"{protocol}://"
+            host_to_use = self.host  # type: ignore[assignment]
+            protocol_to_add = None
+
+        if protocol_to_add:
+            uri += f"{protocol_to_add}://"
 
         authority_block = ""
         if self.login is not None:
@@ -89,8 +99,8 @@ class Connection:
             uri += authority_block
 
         host_block = ""
-        if host != "":
-            host_block += quote(host, safe="")
+        if host_to_use:
+            host_block += quote(host_to_use, safe="")
         if self.port:
             if host_block == "" and authority_block == "":
                 host_block += f"@:{self.port}"
