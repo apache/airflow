6ef70ab1ccbe3ea047258cbffa571f7bfaff8d49
Jens Scheffler <jscheffl@apache.org>
2025-06-21 16:27:51 +0200

    Prevent problematic access to getuser() if not properly configured

--- airflow-core/src/airflow/cli/commands/task_command.py
+++ airflow-core/src/airflow/cli/commands/task_command.py
@@ -30,7 +30,7 @@ from typing import TYPE_CHECKING, Protocol, cast
 from airflow import settings
 from airflow.cli.simple_table import AirflowConsole
 from airflow.cli.utils import fetch_dag_run_from_run_id_or_logical_date_string
-from airflow.exceptions import DagRunNotFound, TaskInstanceNotFound
+from airflow.exceptions import AirflowConfigException, DagRunNotFound, TaskInstanceNotFound
 from airflow.models import TaskInstance
 from airflow.models.dag import DAG as SchedulerDAG, _get_or_create_dagrun
 from airflow.models.dagrun import DagRun
@@ -121,6 +121,11 @@ def _get_dag_run(
         else None
     )
     run_after = data_interval.end if data_interval else timezone.utcnow()
+    try:
+        user = getuser()
+    except AirflowConfigException as e:
+        log.warning("Failed to get user name from os: %s", e)
+        user = None
     if create_if_necessary == "memory":
         dag_run = DagRun(
             dag_id=dag.dag_id,
@@ -130,7 +135,7 @@ def _get_dag_run(
             data_interval=data_interval,
             run_after=run_after,
             triggered_by=DagRunTriggeredByType.CLI,
-            triggering_user=getuser(),
+            triggering_user=user,
             state=DagRunState.RUNNING,
         )
         return dag_run, True
@@ -143,7 +148,7 @@ def _get_dag_run(
             data_interval=data_interval,
             run_after=run_after,
             triggered_by=DagRunTriggeredByType.CLI,
-            triggering_user=getuser(),
+            triggering_user=user,
             session=session,
             start_date=logical_date or run_after,
             conf=None,
