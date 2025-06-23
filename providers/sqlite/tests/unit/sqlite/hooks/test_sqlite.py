1f3a706b2dd844aa11e2bd66ffff67dd4ea11969
Yeonguk <choo121600@gmail.com>
2025-06-23 14:49:29 +0900

    revert test_sqlite and add db_test

--- providers/sqlite/tests/unit/sqlite/hooks/test_sqlite.py
+++ providers/sqlite/tests/unit/sqlite/hooks/test_sqlite.py
@@ -47,13 +47,13 @@ class TestSqliteHookConn:
         ],
     )
     @patch("airflow.providers.sqlite.hooks.sqlite.sqlite3.connect")
-    def test_get_conn(self, mock_connect: mock.Mock, connection: Connection, uri: str):
+    def test_get_conn(self, mock_connect, connection, uri):
         self.db_hook.get_connection = mock.Mock(return_value=connection)
         self.db_hook.get_conn()
         mock_connect.assert_called_once_with(uri, uri=True)
 
     @patch("airflow.providers.sqlite.hooks.sqlite.sqlite3.connect")
-    def test_get_conn_non_default_id(self, mock_connect: mock.Mock):
+    def test_get_conn_non_default_id(self, mock_connect):
         self.db_hook.get_connection = mock.Mock(return_value=Connection(host="host"))
         self.db_hook.test_conn_id = "non_default"
         self.db_hook.get_conn()
@@ -76,8 +76,6 @@ class TestSqliteHook:
                 return conn
 
         self.db_hook = UnitTestSqliteHook()
-        self.db_hook.get_connection = mock.Mock(return_value=Connection(conn_id="test", extra="{}"))
-        self.db_hook.get_uri = mock.Mock(return_value="sqlite://")
 
     def test_get_first_record(self):
         statement = "SQL"
@@ -135,6 +133,7 @@ class TestSqliteHook:
         self.db_hook.run(statement)
         assert self.db_hook.log.info.call_count == 2
 
+    @pytest.mark.db_test
     def test_generate_insert_sql_replace_false(self):
         expected_sql = "INSERT INTO Customer (first_name, last_name) VALUES (?,?)"
         rows = ("James", "1")
@@ -145,6 +144,7 @@ class TestSqliteHook:
 
         assert sql == expected_sql
 
+    @pytest.mark.db_test
     def test_generate_insert_sql_replace_true(self):
         expected_sql = "REPLACE INTO Customer (first_name, last_name) VALUES (?,?)"
         rows = ("James", "1")
@@ -155,16 +155,13 @@ class TestSqliteHook:
 
         assert sql == expected_sql
 
+    @pytest.mark.db_test
     def test_sqlalchemy_engine(self):
         """Test that the sqlalchemy engine is initialized"""
         conn_id = "sqlite_default"
-        with patch("airflow.hooks.base.BaseHook.get_connection") as mock_get_connection:
-            mock_get_connection.return_value = Connection(
-                conn_id="sqlite_default", conn_type="sqlite", host="/tmp/test.db"
-            )
-            hook = SqliteHook(sqlite_conn_id=conn_id)
-            engine = hook.get_sqlalchemy_engine()
-            assert isinstance(engine, sqlalchemy.engine.Engine)
-            assert engine.name == "sqlite"
-            # Assert filepath of the sqliate DB is correct
-            assert engine.url.database == mock_get_connection.return_value.host
+        hook = SqliteHook(sqlite_conn_id=conn_id)
+        engine = hook.get_sqlalchemy_engine()
+        assert isinstance(engine, sqlalchemy.engine.Engine)
+        assert engine.name == "sqlite"
+        # Assert filepath of the sqliate DB is correct
+        assert engine.url.database == hook.get_connection(conn_id).host
