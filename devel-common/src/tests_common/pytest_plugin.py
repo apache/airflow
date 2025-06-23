c7659d51fa9034be34a4af388ff99dbfbbcf2f64
Amogh Desai <amoghrajesh1999@gmail.com>
2025-06-23 18:10:33 +0530

    remove autouse

--- devel-common/src/tests_common/pytest_plugin.py
+++ devel-common/src/tests_common/pytest_plugin.py
@@ -1993,7 +1993,6 @@ def override_caplog(request):
         airflow.logging_config.configure_logging()
 
 
-@pytest.fixture(autouse=True)
 def mock_supervisor_comms(monkeypatch):
     # for back-compat
     from tests_common.test_utils.version_compat import AIRFLOW_V_3_0_PLUS
