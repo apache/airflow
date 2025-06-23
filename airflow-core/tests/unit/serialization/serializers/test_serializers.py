6a731652cfcf2a0725257e3bbdaac61a4780dea3
Amogh Desai <amoghrajesh1999@gmail.com>
2025-06-23 16:49:43 +0530

    Update numpy serializer for np 2 compat (remove np.float_ and np.complex_) (#52084)

--- airflow-core/tests/unit/serialization/serializers/test_serializers.py
+++ airflow-core/tests/unit/serialization/serializers/test_serializers.py
@@ -225,7 +225,7 @@ class TestSerializers:
         numpy_version = metadata.version("numpy")
         is_numpy_2 = version.parse(numpy_version).major == 2
 
-        assert serialize(np.bool_(False)) == (True, "numpy.bool" if is_numpy_2 else "numpy.bool_", 1, True)
+        assert serialize(np.bool_(False)) == (False, "numpy.bool" if is_numpy_2 else "numpy.bool_", 1, True)
         if is_numpy_2:
             assert serialize(np.float64(3.14)) == (float(np.float64(3.14)), "numpy.float64", 1, True)
         else:
