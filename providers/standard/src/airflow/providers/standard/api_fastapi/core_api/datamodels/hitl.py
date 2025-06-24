b3866da0bd75f1b992be90a280ae679521381df5
Wei Lee <weilee.rx@gmail.com>
2025-06-24 18:16:21 +0800

    feat(api): draft htil input requests api

--- providers/standard/src/airflow/providers/standard/api_fastapi/core_api/datamodels/hitl.py
+++ providers/standard/src/airflow/providers/standard/api_fastapi/core_api/datamodels/hitl.py
@@ -16,9 +16,14 @@
 # under the License.
 from __future__ import annotations
 
+from collections.abc import MutableMapping
 from datetime import datetime
 
+from pydantic import field_validator
+
 from airflow.api_fastapi.core_api.base import BaseModel
+from airflow.models.taskinstance import TaskInstance as TI
+from airflow.providers.standard.operators.hitl import HITLOperator
 
 
 class AddHITLResponsePayload(BaseModel):
@@ -33,3 +38,51 @@ class HITLResponse(BaseModel):
     ti_id: str
     content: str
     created_at: datetime
+
+
+class HITLInputRequest(BaseModel):
+    """Schema for details from Human-in-the-loop task."""
+
+    ti_id: str
+    task_id: str
+    dag_id: str
+    run_id: str
+    try_number: int
+    map_index: int = -1
+
+    options: list[str]
+    subject: str
+    body: str | None = None
+    default: str | None = None
+    params: MutableMapping | None = None
+
+    @field_validator("params", mode="before")
+    @classmethod
+    def get_params(cls, params: MutableMapping | None) -> dict | None:
+        """Convert params attribute to dict representation."""
+        if params is None:
+            return None
+        return {k: v.dump() for k, v in params.items()}
+
+    @classmethod
+    def from_task_instance(cls, task_instance: TI, task: HITLOperator) -> HITLInputRequest:
+        return HITLInputRequest(
+            ti_id=task_instance.id,
+            task_id=task_instance.task_id,
+            dag_id=task_instance.dag_id,
+            run_id=task_instance.run_id,
+            try_number=task_instance.try_number,
+            map_index=task_instance.map_index,
+            options=task.options,
+            subject=task.subject,
+            body=task.body,
+            default=task.default,
+            params=task.params,
+        )
+
+
+class HITLInputRequestCollection(BaseModel):
+    """Schema for details from Human-in-the-loop tasks."""
+
+    input_requests: list[HITLInputRequest]
+    total_entries: int
