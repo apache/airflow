edc74082fe7e73b4306fe7f9f1e3e9270956c96a
David Blain <david.blain@infrabel.be>
2025-06-28 13:49:51 +0200

    refactor: Fixed breeze test_docs_filter for Common SQL provider package python files changed

--- dev/breeze/tests/test_selective_checks.py
+++ dev/breeze/tests/test_selective_checks.py
@@ -2253,7 +2253,7 @@ def test_upgrade_to_newer_dependencies(
         pytest.param(
             ("providers/common/sql/src/airflow/providers/common/sql/common_sql_python.py",),
             {
-                "docs-list-as-string": "amazon apache.drill apache.druid apache.hive "
+                "docs-list-as-string": "amazon apache.arrow apache.drill apache.druid apache.hive "
                 "apache.impala apache.pinot common.sql databricks elasticsearch "
                 "exasol google jdbc microsoft.mssql mysql odbc openlineage "
                 "oracle pgvector postgres presto slack snowflake sqlite teradata trino vertica ydb",
