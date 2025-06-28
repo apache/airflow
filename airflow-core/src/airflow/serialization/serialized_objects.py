a7165ff95754f2f3fc7a4bd9daecac763651c5af
pratiksha badheka <pratikshabadheka27@gmail.com>
2025-06-28 02:27:22 -0700

    fix static checks

--- airflow-core/src/airflow/serialization/serialized_objects.py
+++ airflow-core/src/airflow/serialization/serialized_objects.py
@@ -2403,8 +2403,7 @@ class XComOperatorLink(LoggingMixin):
     name: str
     xcom_key: str
 
-
-    def get_link(self, session: Session,operator: BaseOperator, *, ti_key: TaskInstanceKey) -> str:
+    def get_link(self, session: Session, operator: BaseOperator, *, ti_key: TaskInstanceKey) -> str:
         """
         Retrieve the link from the XComs.
 
