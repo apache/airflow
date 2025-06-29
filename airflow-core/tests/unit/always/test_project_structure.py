a0c26bb86b3459ccd30137464cd244fea66e646a
Farhan <52888929+ahmadtfarhan@users.noreply.github.com>
2025-06-29 10:33:19 +0100

    Replace `models.BaseOperator` to Task SDK one for Apache TinkerPop (#52400)
    Follow-up of #52292 for Apache TinkerPop

--- airflow-core/tests/unit/always/test_project_structure.py
+++ airflow-core/tests/unit/always/test_project_structure.py
@@ -96,6 +96,7 @@ class TestProjectStructure:
             "providers/apache/livy/tests/unit/apache/livy/test_version_compat.py",
             "providers/apache/pig/tests/unit/apache/pig/test_version_compat.py",
             "providers/apache/spark/tests/unit/apache/spark/test_version_compat.py",
+            "providers/apache/tinkerpop/tests/unit/apache/tinkerpop/test_version_compat.py",
             "providers/arangodb/tests/unit/arangodb/test_version_compat.py",
             "providers/asana/tests/unit/asana/test_version_compat.py",
             "providers/atlassian/jira/tests/unit/atlassian/jira/test_version_compat.py",
