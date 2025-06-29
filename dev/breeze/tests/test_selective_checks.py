1dde5762657844e09aff420b04ab425d5f64bd03
GPK <gopidesupavan@gmail.com>
2025-06-29 19:32:03 +0100

    Filter only provided integration paths for breeze integration testing (#52462)
    * Filter only provided integration paths
    
    * Fix tests
    
    * Rename gremline integration name to tinkerpop
    
    * Fix selective_checks test
    
    * Update @integration pytest marker with tinkerpop

--- dev/breeze/tests/test_selective_checks.py
+++ dev/breeze/tests/test_selective_checks.py
@@ -1153,7 +1153,7 @@ def assert_outputs_are_printed(expected_outputs: dict[str, str], stderr: str):
                     "core-test-types-list-as-strings-in-json": ALL_CI_SELECTIVE_TEST_TYPES_AS_JSON,
                     "providers-test-types-list-as-strings-in-json": ALL_PROVIDERS_SELECTIVE_TEST_TYPES_AS_JSON,
                     "testable-core-integrations": "['kerberos']",
-                    "testable-providers-integrations": "['celery', 'cassandra', 'drill', 'gremlin', 'kafka', "
+                    "testable-providers-integrations": "['celery', 'cassandra', 'drill', 'tinkerpop', 'kafka', "
                     "'mongo', 'pinot', 'qdrant', 'redis', 'trino', 'ydb']",
                     "needs-mypy": "true",
                     "mypy-checks": ALL_MYPY_CHECKS,
