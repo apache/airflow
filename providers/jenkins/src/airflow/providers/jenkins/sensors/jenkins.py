af6201df4fbfa4c8a962c92e34050955ddd28be2
Pavan Kumar <gopidesupavan@gmail.com>
2025-06-30 08:29:14 +0100

    migrate jenkins provider to sdk BaseOperator

--- providers/jenkins/src/airflow/providers/jenkins/sensors/jenkins.py
+++ providers/jenkins/src/airflow/providers/jenkins/sensors/jenkins.py
@@ -22,12 +22,7 @@ from typing import TYPE_CHECKING
 
 from airflow.exceptions import AirflowException
 from airflow.providers.jenkins.hooks.jenkins import JenkinsHook
-from airflow.providers.jenkins.version_compat import AIRFLOW_V_3_0_PLUS
-
-if AIRFLOW_V_3_0_PLUS:
-    from airflow.sdk import BaseSensorOperator
-else:
-    from airflow.sensors.base import BaseSensorOperator  # type: ignore[no-redef]
+from airflow.providers.jenkins.version_compat import BaseSensorOperator
 
 if TYPE_CHECKING:
     try:
