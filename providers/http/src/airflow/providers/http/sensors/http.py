af51d4fe622e35432a8b2eebd4b8e394bcf7c778
Elad Kalif <45845474+eladkal@users.noreply.github.com>
2025-06-30 10:57:02 +0300

    Revert "Replace models.BaseOperator to Task SDK for http (#52506)" (#52515)
    This reverts commit a9a7fcc5192946f0546f70053d6fd498de381f76.

--- providers/http/src/airflow/providers/http/sensors/http.py
+++ providers/http/src/airflow/providers/http/sensors/http.py
@@ -25,7 +25,12 @@ from airflow.configuration import conf
 from airflow.exceptions import AirflowException
 from airflow.providers.http.hooks.http import HttpHook
 from airflow.providers.http.triggers.http import HttpSensorTrigger
-from airflow.providers.http.version_compat import AIRFLOW_V_3_0_PLUS, BaseSensorOperator
+from airflow.providers.http.version_compat import AIRFLOW_V_3_0_PLUS
+
+if AIRFLOW_V_3_0_PLUS:
+    from airflow.sdk import BaseSensorOperator
+else:
+    from airflow.sensors.base import BaseSensorOperator  # type: ignore[no-redef]
 
 if TYPE_CHECKING:
     try:
