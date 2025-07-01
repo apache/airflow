fdc591f4c83f5b84048feadc07d244be045d1586
Kaxil Naik <kaxilnaik@gmail.com>
2025-07-01 19:43:49 +0530

    Update providers/tableau/src/airflow/providers/tableau/version_compat.py

--- providers/tableau/src/airflow/providers/tableau/version_compat.py
+++ providers/tableau/src/airflow/providers/tableau/version_compat.py
@@ -37,7 +37,7 @@ AIRFLOW_V_3_0_PLUS = get_base_airflow_version_tuple() >= (3, 0, 0)
 if AIRFLOW_V_3_0_PLUS:
     from airflow.sdk import BaseOperator, BaseSensorOperator
 else:
-    from airflow.models import BaseOperator
+    from airflow.models import BaseOperator  # type: ignore[no-redef]
     from airflow.sensors.base import BaseSensorOperator  # type: ignore[no-redef]
 
 __all__ = ["AIRFLOW_V_3_0_PLUS", "BaseOperator", "BaseSensorOperator"]
