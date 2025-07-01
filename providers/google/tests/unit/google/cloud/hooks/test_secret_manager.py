19a068c9777fd6cce9e805f67f79e5ef64e3a39c
Harikrishna D <64940963+harikrishna12524@users.noreply.github.com>
2025-07-01 10:27:42 +0530

    Add regional support for google secret manager hook (#52124)
    * Add regional support for google secret manager hook
    
    * Change property name from location_id to location
    
    * Remove backward compatibility comment.
    
    * Fix static check failing

--- providers/google/tests/unit/google/cloud/hooks/test_secret_manager.py
+++ providers/google/tests/unit/google/cloud/hooks/test_secret_manager.py
@@ -29,12 +29,60 @@ from unit.google.cloud.utils.base_gcp_mock import GCP_PROJECT_ID_HOOK_UNIT_TEST
 BASE_PACKAGE = "airflow.providers.google.common.hooks.base_google."
 SECRETS_HOOK_PACKAGE = "airflow.providers.google.cloud.hooks.secret_manager."
 SECRET_ID = "test-secret-id"
+REGIONAL_SECRET_LOCATION = "test-location"
+SECRET_VERSION = "test-secret-version"
 
 
 class TestGoogleCloudSecretManagerHook:
     def setup_method(self, method):
         with patch(f"{BASE_PACKAGE}GoogleBaseHook.get_connection", return_value=MagicMock()):
             self.hook = GoogleCloudSecretManagerHook()
+
+    @patch(f"{SECRETS_HOOK_PACKAGE}GoogleCloudSecretManagerHook.client", new_callable=PropertyMock)
+    def test__get_parent(self, mock_client):
+        expected_value = mock_client.return_value.common_project_path.return_value
+        parent = self.hook._get_parent(GCP_PROJECT_ID_HOOK_UNIT_TEST)
+        assert expected_value == parent
+        mock_client.assert_called_once()
+        mock_client.return_value.common_project_path.assert_called_once_with(GCP_PROJECT_ID_HOOK_UNIT_TEST)
+
+        expected_value = mock_client.return_value.common_location_path.return_value
+        parent = self.hook._get_parent(GCP_PROJECT_ID_HOOK_UNIT_TEST, REGIONAL_SECRET_LOCATION)
+        assert expected_value == parent
+        # mock_client.assert_called_once() # will fail as already client has been triggered due to previous case.
+        mock_client.return_value.common_location_path.assert_called_once_with(
+            GCP_PROJECT_ID_HOOK_UNIT_TEST, REGIONAL_SECRET_LOCATION
+        )
+
+    @patch(f"{SECRETS_HOOK_PACKAGE}GoogleCloudSecretManagerHook.client", new_callable=PropertyMock)
+    def test__get_secret_path(self, mock_client):
+        expected_value = mock_client.return_value.secret_path.return_value
+        secret_path = self.hook._get_secret_path(GCP_PROJECT_ID_HOOK_UNIT_TEST, SECRET_ID)
+        assert expected_value == secret_path
+        mock_client.assert_called_once()
+        mock_client.return_value.secret_path.assert_called_once_with(GCP_PROJECT_ID_HOOK_UNIT_TEST, SECRET_ID)
+
+        expected_value = f"projects/{GCP_PROJECT_ID_HOOK_UNIT_TEST}/locations/{REGIONAL_SECRET_LOCATION}/secrets/{SECRET_ID}"
+        parent = self.hook._get_secret_path(
+            GCP_PROJECT_ID_HOOK_UNIT_TEST, SECRET_ID, REGIONAL_SECRET_LOCATION
+        )
+        assert expected_value == parent
+
+    @patch(f"{SECRETS_HOOK_PACKAGE}GoogleCloudSecretManagerHook.client", new_callable=PropertyMock)
+    def test__get_secret_version_path(self, mock_client):
+        expected_value = mock_client.return_value.secret_version_path.return_value
+        parent = self.hook._get_secret_version_path(GCP_PROJECT_ID_HOOK_UNIT_TEST, SECRET_ID, SECRET_VERSION)
+        assert expected_value == parent
+        mock_client.assert_called_once()
+        mock_client.return_value.secret_version_path.assert_called_once_with(
+            GCP_PROJECT_ID_HOOK_UNIT_TEST, SECRET_ID, SECRET_VERSION
+        )
+
+        expected_value = f"projects/{GCP_PROJECT_ID_HOOK_UNIT_TEST}/locations/{REGIONAL_SECRET_LOCATION}/secrets/{SECRET_ID}/versions/{SECRET_VERSION}"
+        parent = self.hook._get_secret_version_path(
+            GCP_PROJECT_ID_HOOK_UNIT_TEST, SECRET_ID, SECRET_VERSION, REGIONAL_SECRET_LOCATION
+        )
+        assert expected_value == parent
 
     @patch(f"{SECRETS_HOOK_PACKAGE}GoogleCloudSecretManagerHook.get_credentials")
     @patch(f"{SECRETS_HOOK_PACKAGE}SecretManagerServiceClient")
@@ -66,9 +114,10 @@ class TestGoogleCloudSecretManagerHook:
             (mock_secret := MagicMock(), mock_secret),  # type: ignore[name-defined]
         ],
     )
+    @patch(f"{SECRETS_HOOK_PACKAGE}GoogleCloudSecretManagerHook._get_parent")
     @patch(f"{SECRETS_HOOK_PACKAGE}GoogleCloudSecretManagerHook.client", new_callable=PropertyMock)
-    def test_create_secret(self, mock_client, input_secret, expected_secret):
-        expected_parent = f"projects/{GCP_PROJECT_ID_HOOK_UNIT_TEST}"
+    def test_create_secret(self, mock_client, mock_get_parent, input_secret, expected_secret):
+        expected_parent = mock_get_parent.return_value  # f"projects/{GCP_PROJECT_ID_HOOK_UNIT_TEST}"
         expected_response = mock_client.return_value.create_secret.return_value
         mock_retry, mock_timeout, mock_metadata = MagicMock(), MagicMock(), MagicMock()
 
@@ -83,6 +132,7 @@ class TestGoogleCloudSecretManagerHook:
 
         assert actual_response == expected_response
         mock_client.assert_called_once()
+        mock_get_parent.assert_called_once_with(GCP_PROJECT_ID_HOOK_UNIT_TEST, None)
         mock_client.return_value.create_secret.assert_called_once_with(
             request={
                 "parent": expected_parent,
@@ -94,9 +144,10 @@ class TestGoogleCloudSecretManagerHook:
             metadata=mock_metadata,
         )
 
+    @patch(f"{SECRETS_HOOK_PACKAGE}GoogleCloudSecretManagerHook._get_secret_path")
     @patch(f"{SECRETS_HOOK_PACKAGE}GoogleCloudSecretManagerHook.client", new_callable=PropertyMock)
-    def test_add_secret_version(self, mock_client):
-        expected_parent = f"projects/{GCP_PROJECT_ID_HOOK_UNIT_TEST}/secrets/{SECRET_ID}"
+    def test_add_secret_version(self, mock_client, mock_get_secret_path):
+        expected_parent = mock_get_secret_path.return_value
         expected_response = mock_client.return_value.add_secret_version.return_value
         mock_payload, mock_retry, mock_timeout, mock_metadata = (MagicMock() for _ in range(4))
 
@@ -111,6 +162,7 @@ class TestGoogleCloudSecretManagerHook:
 
         assert actual_response == expected_response
         mock_client.assert_called_once()
+        mock_get_secret_path.assert_called_once_with(GCP_PROJECT_ID_HOOK_UNIT_TEST, SECRET_ID, None)
         mock_client.return_value.add_secret_version.assert_called_once_with(
             request={
                 "parent": expected_parent,
@@ -121,9 +173,10 @@ class TestGoogleCloudSecretManagerHook:
             metadata=mock_metadata,
         )
 
+    @patch(f"{SECRETS_HOOK_PACKAGE}GoogleCloudSecretManagerHook._get_parent")
     @patch(f"{SECRETS_HOOK_PACKAGE}GoogleCloudSecretManagerHook.client", new_callable=PropertyMock)
-    def test_list_secrets(self, mock_client):
-        expected_parent = f"projects/{GCP_PROJECT_ID_HOOK_UNIT_TEST}"
+    def test_list_secrets(self, mock_client, mock_get_parent):
+        expected_parent = mock_get_parent.return_value
         expected_response = mock_client.return_value.list_secrets.return_value
         mock_filter, mock_retry, mock_timeout, mock_metadata = (MagicMock() for _ in range(4))
         page_size, page_token = 20, "test-page-token"
@@ -140,6 +193,7 @@ class TestGoogleCloudSecretManagerHook:
 
         assert actual_response == expected_response
         mock_client.assert_called_once()
+        mock_get_parent.assert_called_once_with(GCP_PROJECT_ID_HOOK_UNIT_TEST, None)
         mock_client.return_value.list_secrets.assert_called_once_with(
             request={
                 "parent": expected_parent,
@@ -184,7 +238,10 @@ class TestGoogleCloudSecretManagerHook:
         assert secret_exists_actual == secret_exists_expected
         mock_client.return_value.secret_path.assert_called_once_with(GCP_PROJECT_ID_HOOK_UNIT_TEST, secret_id)
         mock_list_secrets.assert_called_once_with(
-            project_id=GCP_PROJECT_ID_HOOK_UNIT_TEST, page_size=100, secret_filter=secret_filter
+            project_id=GCP_PROJECT_ID_HOOK_UNIT_TEST,
+            page_size=100,
+            secret_filter=secret_filter,
+            location=None,
         )
 
     @patch(f"{SECRETS_HOOK_PACKAGE}GoogleCloudSecretManagerHook.client", new_callable=PropertyMock)
