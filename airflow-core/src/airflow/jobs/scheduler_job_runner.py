ea6776322a98e72b28b5aea005120e7ddeaf51b9
Collin McNulty <collin.mcnulty@gmail.com>
2025-07-01 00:55:15 -0500

    Correctly treat requeues on reschedule sensors as resetting after each reschedule (#51410)
    (cherry picked from commit a3621016840d8dba97777954333f6b214d4f3363)

--- airflow-core/src/airflow/jobs/scheduler_job_runner.py
+++ airflow-core/src/airflow/jobs/scheduler_job_runner.py
@@ -33,6 +33,7 @@ from itertools import groupby
 from typing import TYPE_CHECKING, Any, Callable
 
 from sqlalchemy import and_, delete, exists, func, select, text, tuple_, update
+from sqlalchemy import and_, delete, desc, exists, func, or_, select, text, tuple_, update
 from sqlalchemy.exc import OperationalError
 from sqlalchemy.orm import joinedload, lazyload, load_only, make_transient, selectinload
 from sqlalchemy.sql import expression
@@ -2043,18 +2044,33 @@ class SchedulerJobRunner(BaseJobRunner, LoggingMixin):
 
         We can then use this information to determine whether to reschedule a task or fail it.
         """
-        return (
-            session.query(Log)
+        last_running_time = session.scalar(
+            select(Log.dttm)
             .where(
-                Log.task_id == ti.task_id,
                 Log.dag_id == ti.dag_id,
+                Log.task_id == ti.task_id,
                 Log.run_id == ti.run_id,
                 Log.map_index == ti.map_index,
                 Log.try_number == ti.try_number,
-                Log.event == TASK_STUCK_IN_QUEUED_RESCHEDULE_EVENT,
+                Log.event == "running",
             )
-            .count()
+            .order_by(desc(Log.dttm))
+            .limit(1)
         )
+
+        query = session.query(Log).where(
+            Log.task_id == ti.task_id,
+            Log.dag_id == ti.dag_id,
+            Log.run_id == ti.run_id,
+            Log.map_index == ti.map_index,
+            Log.try_number == ti.try_number,
+            Log.event == TASK_STUCK_IN_QUEUED_RESCHEDULE_EVENT,
+        )
+
+        if last_running_time:
+            query = query.where(Log.dttm > last_running_time)
+
+        return query.count()
 
     previous_ti_running_metrics: dict[tuple[str, str, str], int] = {}
 
