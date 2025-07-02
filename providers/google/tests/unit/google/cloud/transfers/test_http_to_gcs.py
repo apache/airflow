c09f9995e5c5665114c1d27db7714718f85fec1e
Josue Velazquez <jvelazquezg6@gmail.com>
2025-07-02 13:50:34 -0600

    Added tests for HttpToGcs Operator

--- providers/google/tests/unit/google/cloud/transfers/test_http_to_gcs.py
+++ providers/google/tests/unit/google/cloud/transfers/test_http_to_gcs.py
@@ -22,6 +22,7 @@ from unittest import mock
 
 from airflow.providers.google.cloud.transfers.http_to_gcs import HttpToGCSOperator
 
+TASK_CONTEXT = None
 TASK_ID = "test-http-to-gcs-operator"
 GCP_CONN_ID = "GCP_CONN_ID"
 HTTP_CONN_ID = "HTTP_CONN_ID"
@@ -69,7 +70,7 @@ class TestHttpToGCSOperator:
             gcp_conn_id=GCP_CONN_ID,
             impersonation_chain=IMPERSONATION_CHAIN,
         )
-        task.execute(None)
+        task.execute(TASK_CONTEXT)
 
         # GCS
         gcs_hook.assert_called_once_with(gcp_conn_id=GCP_CONN_ID, impersonation_chain=IMPERSONATION_CHAIN)
@@ -100,4 +101,7 @@ class TestHttpToGCSOperator:
         )
         task.http_hook.run.assert_called_once_with(
             endpoint=ENDPOINT, headers=HEADERS, data=DATA, extra_options=EXTRA_OPTIONS
+        )
+        task.process_response.assert_called_once_with(
+            context=TASK_CONTEXT, response=task.http_hook.run.return_value
         )
