faa29ca5133c9ed113a3f9d3b173d0c0a041749b
Ankit Chaurasia <8670962+sunank200@users.noreply.github.com>
2025-07-03 19:39:55 +0545

    Move BaseHook, BaseOperator, and BaseSensorOperator imports to version_compat for Alibaba provider Airflow 3.0 compatibility

--- providers/alibaba/src/airflow/providers/alibaba/cloud/hooks/oss.py
+++ providers/alibaba/src/airflow/providers/alibaba/cloud/hooks/oss.py
@@ -27,11 +27,7 @@ import oss2
 from oss2.exceptions import ClientError
 
 from airflow.exceptions import AirflowException
-
-try:
-    from airflow.sdk import BaseHook
-except ImportError:
-    from airflow.hooks.base import BaseHook  # type: ignore[attr-defined,no-redef]
+from airflow.providers.alibaba.version_compat import BaseHook
 
 if TYPE_CHECKING:
     try:
