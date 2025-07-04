e57188867ccb3b8be26629eacb24bd2432d8009f
Wei Lee <weilee.rx@gmail.com>
2025-07-05 00:45:37 +0800

    fix: fix ci issue

--- airflow-core/src/airflow/migrations/versions/0076_3_1_0_add_human_in_the_loop_response.py
+++ airflow-core/src/airflow/migrations/versions/0076_3_1_0_add_human_in_the_loop_response.py
@@ -20,7 +20,7 @@
 Add Human In the Loop Response table.
 
 Revision ID: 40f7c30a228b
-Revises: 66a7743fe20e
+Revises: ffdb0566c7c0
 Create Date: 2025-07-04 15:05:19.459197
 
 """
@@ -37,10 +37,9 @@ from airflow.utils.sqlalchemy import UtcDateTime
 
 # revision identifiers, used by Alembic.
 revision = "40f7c30a228b"
-down_revision = "66a7743fe20e"
+down_revision = "ffdb0566c7c0"
 branch_labels = None
 depends_on = None
-
 airflow_version = "3.1.0"
 
 
