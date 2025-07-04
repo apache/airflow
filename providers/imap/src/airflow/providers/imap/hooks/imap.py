998151b39c6c20049dd50256d4fed11cc0300453
Ankit Chaurasia <8670962+sunank200@users.noreply.github.com>
2025-07-04 15:29:22 +0545

    Move all BaseHook usages to version_compat in IMAP (#52816)
    Part of https://github.com/apache/airflow/issues/52676

--- providers/imap/src/airflow/providers/imap/hooks/imap.py
+++ providers/imap/src/airflow/providers/imap/hooks/imap.py
@@ -32,11 +32,7 @@ from collections.abc import Iterable
 from typing import TYPE_CHECKING, Any
 
 from airflow.exceptions import AirflowException
-
-try:
-    from airflow.sdk import BaseHook
-except ImportError:
-    from airflow.hooks.base import BaseHook  # type: ignore[attr-defined,no-redef]
+from airflow.providers.imap.version_compat import BaseHook
 from airflow.utils.log.logging_mixin import LoggingMixin
 
 if TYPE_CHECKING:
