bd99c84bf5f809ce27d5b5d31d847e2bcae33adb
pratiksha badheka <pratikshabadheka27@gmail.com>
2025-07-05 10:39:53 -0700

    fix static checks

--- providers/common/io/tests/unit/common/io/xcom/test_backend.py
+++ providers/common/io/tests/unit/common/io/xcom/test_backend.py
@@ -139,7 +139,7 @@ class TestXComObjectStorageBackend:
                 run_id=task_instance.run_id,
             )
 
-            res = session.execute(
+            res = session.scalars(
                 XComModel.get_many(
                     key=XCOM_RETURN_KEY,
                     dag_ids=task_instance.dag_id,
@@ -185,7 +185,7 @@ class TestXComObjectStorageBackend:
                 run_id=task_instance.run_id,
                 session=session,
             )
-            assert str(p) == XComModel.deserialize_value(session.execute(qry).first())
+            assert str(p) == XComModel.deserialize_value(session.scalars(qry).first())
         else:
             qry = XCom.get_many(
                 key=XCOM_RETURN_KEY,
@@ -225,7 +225,7 @@ class TestXComObjectStorageBackend:
                 run_id=task_instance.run_id,
             )
 
-            res = session.execute(
+            res = session.scalars(
                 XComModel.get_many(
                     key=XCOM_RETURN_KEY,
                     dag_ids=task_instance.dag_id,
@@ -276,7 +276,7 @@ class TestXComObjectStorageBackend:
                 run_id=task_instance.run_id,
                 map_index=task_instance.map_index,
             )
-            value = session.execute(
+            value = session.scalars(
                 XComModel.get_many(
                     key=XCOM_RETURN_KEY,
                     dag_ids=task_instance.dag_id,
@@ -326,7 +326,7 @@ class TestXComObjectStorageBackend:
                 run_id=task_instance.run_id,
             )
 
-            res = session.execute(
+            res = session.scalars(
                 XComModel.get_many(
                     key=XCOM_RETURN_KEY,
                     dag_ids=task_instance.dag_id,
