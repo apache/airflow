5802e25abc03cd711e1ea5595c06b94470e54c47
fweilun <william20020503@gmail.com>
2025-07-06 23:34:54 +0800

    Replace BaseHook to Task SDK for google (#52809)
    Co-authored-by: Amogh Desai <amoghrajesh1999@gmail.com>

--- providers/google/src/airflow/providers/google/cloud/hooks/looker.py
+++ providers/google/src/airflow/providers/google/cloud/hooks/looker.py
@@ -29,11 +29,7 @@ from looker_sdk.sdk.api40 import methods as methods40
 from packaging.version import parse as parse_version
 
 from airflow.exceptions import AirflowException
-
-try:
-    from airflow.sdk import BaseHook
-except ImportError:
-    from airflow.hooks.base import BaseHook  # type: ignore[attr-defined,no-redef]
+from airflow.providers.google.version_compat import BaseHook
 from airflow.version import version
 
 if TYPE_CHECKING:
