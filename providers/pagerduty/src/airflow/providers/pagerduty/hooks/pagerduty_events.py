d4e65c39e1b660721a37b475fc44f073193aebb7
Amogh Desai <amoghrajesh1999@gmail.com>
2025-07-07 08:33:44 +0530

    Moving BaseHook usages to version_compat for pagerduty (#52946)

--- providers/pagerduty/src/airflow/providers/pagerduty/hooks/pagerduty_events.py
+++ providers/pagerduty/src/airflow/providers/pagerduty/hooks/pagerduty_events.py
@@ -24,11 +24,7 @@ from typing import TYPE_CHECKING, Any
 import pagerduty
 
 from airflow.exceptions import AirflowException
-
-try:
-    from airflow.sdk import BaseHook
-except ImportError:
-    from airflow.hooks.base import BaseHook  # type: ignore[attr-defined,no-redef]
+from airflow.providers.pagerduty.version_compat import BaseHook
 
 if TYPE_CHECKING:
     from datetime import datetime
