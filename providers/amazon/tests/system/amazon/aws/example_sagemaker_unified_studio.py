709353f98cdda906a2f4d87dd7ac5aba2fc1555e
Bowrna <mailbowrna@gmail.com>
2025-07-08 20:32:36 +0530

    Changing import path to `airflow.sdk` in Amazon provider package

--- providers/amazon/tests/system/amazon/aws/example_sagemaker_unified_studio.py
+++ providers/amazon/tests/system/amazon/aws/example_sagemaker_unified_studio.py
@@ -17,14 +17,26 @@
 from __future__ import annotations
 
 from datetime import datetime
+from typing import TYPE_CHECKING
 
-from airflow.decorators import task
-from airflow.models.baseoperator import chain
-from airflow.models.dag import DAG
 from airflow.providers.amazon.aws.operators.sagemaker_unified_studio import (
     SageMakerNotebookOperator,
 )
 
+from tests_common.test_utils.version_compat import AIRFLOW_V_3_0_PLUS
+
+if TYPE_CHECKING:
+    from airflow.decorators import task
+    from airflow.models.baseoperator import chain
+    from airflow.models.dag import DAG
+else:
+    if AIRFLOW_V_3_0_PLUS:
+        from airflow.sdk import DAG, chain, task
+    else:
+        # Airflow 2.10 compat
+        from airflow.decorators import task
+        from airflow.models.baseoperator import chain
+        from airflow.models.dag import DAG
 from system.amazon.aws.utils import ENV_ID_KEY, SystemTestContextBuilder
 
 """
