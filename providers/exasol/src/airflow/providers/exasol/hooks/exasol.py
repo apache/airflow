2bb78c16cdbc5f5e507cf8d99c584a404957707c
GPK <gopidesupavan@gmail.com>
2025-07-13 08:38:30 +0100

    Cleanup mypy ignore in exasol provider where possible (#53278)
    * Cleanup mypy ignore in exasol provider where possible
    
    * Fix ruff errors

--- providers/exasol/src/airflow/providers/exasol/hooks/exasol.py
+++ providers/exasol/src/airflow/providers/exasol/hooks/exasol.py
@@ -199,7 +199,7 @@ class ExasolHook(DbApiHook):
             )
         return cols
 
-    @overload  # type: ignore[override]
+    @overload
     def run(
         self,
         sql: str | Iterable[str],
@@ -268,9 +268,8 @@ class ExasolHook(DbApiHook):
                 self.log.info("Running statement: %s, parameters: %s", sql_statement, parameters)
                 with closing(conn.execute(sql_statement, parameters)) as exa_statement:
                     if handler is not None:
-                        result = self._make_common_data_structure(  # type: ignore[attr-defined]
-                            handler(exa_statement)
-                        )
+                        result = self._make_common_data_structure(handler(exa_statement))
+
                         if return_single_query_results(sql, return_last, split_statements):
                             _last_result = result
                             _last_columns = self.get_description(exa_statement)
