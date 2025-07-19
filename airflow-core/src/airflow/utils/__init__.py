4231a87ff2d331b6de83d810f85477654ca03b63
Amogh Desai <amoghrajesh1999@gmail.com>
2025-07-19 10:56:57 +0530

    Deprecate `airflow.utils.warnings` properly (#53339)

--- airflow-core/src/airflow/utils/__init__.py
+++ airflow-core/src/airflow/utils/__init__.py
@@ -20,24 +20,6 @@ from __future__ import annotations
 
 from airflow.utils.deprecation_tools import add_deprecated_classes
 
-
-def _deprecate_this_module(message: str, **shims: tuple[str, str]):
-    import warnings
-
-    from airflow.exceptions import RemovedInAirflow4Warning
-
-    warnings.warn(message, RemovedInAirflow4Warning, stacklevel=3)
-
-    def __getattr__(name: str):
-        try:
-            impa, attr = shims[name]
-        except KeyError:
-            raise AttributeError(f"module {__name__!r} has no attribute {name!r}") from None
-        return getattr(__import__(impa), attr)
-
-    return __getattr__
-
-
 __deprecated_classes = {
     "setup_teardown": {
         "BaseSetupTeardownContext": "airflow.sdk.definitions._internal.setup_teardown.BaseSetupTeardownContext",
@@ -47,4 +29,5 @@ __deprecated_classes = {
         "XCOM_RETURN_KEY": "airflow.models.xcom.XCOM_RETURN_KEY",
     },
 }
+
 add_deprecated_classes(__deprecated_classes, __name__)
