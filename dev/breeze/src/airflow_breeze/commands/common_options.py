4af6bb0fb14ebb328e334eb39622f0c18fd7011f
Kalyan R <kalyan.ben10@live.com>
2025-07-24 17:05:57 +0530

    use of debugpy to run airflow components via breeze (#51763)
    * debug support in breeze
    
    * fix celery env var
    
    * Add debug options for Airflow components and update related constants
    
    * Add debug options for Airflow components and update related constants
    
    * refactor
    
    * update documentation and add setup_vscode
    
    * Remove section on overriding default debug ports in VSCode setup documentation
    
    * fix base-ports
    
    * fix base-ports
    
    * update docs
    
    * add webserver details in docs
    
    * update docs
    
    * update docs
    
    * update docs

--- dev/breeze/src/airflow_breeze/commands/common_options.py
+++ dev/breeze/src/airflow_breeze/commands/common_options.py
@@ -477,6 +477,25 @@ option_install_airflow_with_constraints_default_true = click.option(
     envvar="INSTALL_AIRFLOW_WITH_CONSTRAINTS",
     help="Install airflow in a separate step, with constraints determined from package or airflow version.",
 )
+option_debug_components = click.option(
+    "--debug",
+    "debug_components",
+    help="Enable debugging for specific Airflow components. Can be one or more of: "
+    "scheduler, triggerer, api-server, dag-processor, edge-worker, celery-worker.",
+    type=BetterChoice(
+        ["scheduler", "triggerer", "api-server", "dag-processor", "edge-worker", "celery-worker"]
+    ),
+    multiple=True,
+    envvar="DEBUG_COMPONENTS",
+)
+option_debugger = click.option(
+    "--debugger",
+    help="Debugger to use for debugging Airflow components.",
+    type=BetterChoice(["debugpy", "pydevd-pycharm"]),
+    default="debugpy",
+    show_default=True,
+    envvar="DEBUGGER",
+)
 
 
 def _is_number_greater_than_expected(value: str) -> bool:
