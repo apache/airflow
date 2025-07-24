f4fcd5d94077a422bab03cebe04a4a96c804480a
Owen Leung <owen.leung2@gmail.com>
2025-07-24 18:54:06 +0800

    Fix ESTaskHandler read error

--- providers/elasticsearch/tests/unit/elasticsearch/log/test_es_task_handler.py
+++ providers/elasticsearch/tests/unit/elasticsearch/log/test_es_task_handler.py
@@ -652,7 +652,9 @@ class TestElasticsearchTaskHandler:
             logs = list(logs)
             assert logs[2].event == self.test_message
         else:
-            assert logs[0][0][1] == self.test_message
+            assert (
+                logs[0][0][1] == "[2020-12-24 19:25:00,962] {taskinstance.py:851} INFO - some random stuff - "
+            )
 
     @pytest.mark.db_test
     def test_read_with_custom_offset_and_host_fields(self, ti):
@@ -678,9 +680,7 @@ class TestElasticsearchTaskHandler:
         if AIRFLOW_V_3_0_PLUS:
             pass
         else:
-            assert (
-                logs[0][0][1] == "[2020-12-24 19:25:00,962] {taskinstance.py:851} INFO - some random stuff - "
-            )
+            assert logs[0][0][1] == "some random stuff"
 
     @pytest.mark.db_test
     def test_close(self, ti):
