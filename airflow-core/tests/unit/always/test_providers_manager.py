6d7d4aa9757b8f797343bc753d5d5d9e05946a82
Jarek Potiuk <jarek@potiuk.com>
2025-07-26 09:47:56 +0200

    Make branch tests more resilient to provider dependency changes (#53776)
    While we want to follow automated upgrades of dependencies and constraints
    on vX-Y-test branches, in order to pull latest providers from PyPI,
    this might have some undesired effects - for example some provider's
    dependencies might break tests from importing or produce false
    tests results if such tests depend on - for example - importing all
    provider example dags. This happens for example for serialization
    tests - which import all example dags to test that serialization
    works for them.
    
    Such tests should be simply skipped when target branch is different
    than main.
    
    This PR adds "skip_if_not_on_main" marker and uses it for two tests
    that are currently failing on v3-0-test branch due to weaviate
    dependency upgrades causing import error and some undesired warnings.

--- airflow-core/tests/unit/always/test_providers_manager.py
+++ airflow-core/tests/unit/always/test_providers_manager.py
@@ -38,7 +38,7 @@ from airflow.providers_manager import (
     ProvidersManager,
 )
 
-from tests_common.test_utils.markers import skip_if_force_lowest_dependencies_marker
+from tests_common.test_utils.markers import skip_if_force_lowest_dependencies_marker, skip_if_not_on_main
 from tests_common.test_utils.paths import AIRFLOW_ROOT_PATH
 
 
@@ -230,6 +230,7 @@ class TestProviderManager:
             raise AssertionError("There are warnings generated during hook imports. Please fix them")
         assert [w.message for w in warning_records if "hook-class-names" in str(w.message)] == []
 
+    @skip_if_not_on_main
     @pytest.mark.execution_timeout(150)
     def test_hook_values(self):
         provider_dependencies = json.loads(
