6f7b35e8cc6eaabfcd4e4a07f5ff89a1f6eae41d
Christos Bisias <christosbis@gmail.com>
2025-07-27 12:43:08 +0300

    remove all logical changes

--- airflow-core/src/airflow/jobs/scheduler_job_runner.py
+++ airflow-core/src/airflow/jobs/scheduler_job_runner.py
@@ -650,7 +650,8 @@ class SchedulerJobRunner(BaseJobRunner, LoggingMixin):
                 f"pool.starving_tasks.{pool_name}",
                 "pool.starving_tasks",
                 num_starving_tasks,
-                tags={"pool_name": pool_name},
+                tags={},
+                extra_tags={"pool_name": pool_name},
             )
 
         Stats.gauge("scheduler.tasks.starving", num_starving_tasks_total)
@@ -1821,7 +1822,8 @@ class SchedulerJobRunner(BaseJobRunner, LoggingMixin):
                     f"dagrun.schedule_delay.{dag.dag_id}",
                     "dagrun.schedule_delay",
                     schedule_delay,
-                    tags={"dag_id": dag.dag_id},
+                    tags={},
+                    extra_tags={"dag_id": dag.dag_id},
                 )
                 if span.is_recording():
                     span.add_event(
@@ -1973,7 +1975,8 @@ class SchedulerJobRunner(BaseJobRunner, LoggingMixin):
                     f"dagrun.duration.failed.{dag_run.dag_id}",
                     "dagrun.duration.failed",
                     duration,
-                    tags={"dag_id": dag_run.dag_id},
+                    tags={},
+                    extra_tags={"dag_id": dag_run.dag_id},
                 )
                 span.set_attribute("error", True)
                 if span.is_recording():
@@ -2242,7 +2245,8 @@ class SchedulerJobRunner(BaseJobRunner, LoggingMixin):
                 f"ti.running.{queue}.{dag_id}.{task_id}",
                 "ti.running",
                 count,
-                tags={"queue": queue, "dag_id": dag_id, "task_id": task_id},
+                tags={},
+                extra_tags={"queue": queue, "dag_id": dag_id, "task_id": task_id},
             )
 
         for prev_key in self.previous_ti_running_metrics:
@@ -2253,7 +2257,8 @@ class SchedulerJobRunner(BaseJobRunner, LoggingMixin):
                     f"ti.running.{queue}.{dag_id}.{task_id}",
                     "ti.running",
                     0,
-                    tags={"queue": queue, "dag_id": dag_id, "task_id": task_id},
+                    tags={},
+                    extra_tags={"queue": queue, "dag_id": dag_id, "task_id": task_id},
                 )
 
         self.previous_ti_running_metrics = ti_running_metrics
@@ -2271,31 +2276,36 @@ class SchedulerJobRunner(BaseJobRunner, LoggingMixin):
                     f"pool.open_slots.{pool_name}",
                     "pool.open_slots",
                     slot_stats["open"],
-                    tags={"pool_name": pool_name},
+                    tags={},
+                    extra_tags={"pool_name": pool_name},
                 )
                 DualStatsManager.gauge(
                     f"pool.queued_slots.{pool_name}",
                     "pool.queued_slots",
                     slot_stats["queued"],
-                    tags={"pool_name": pool_name},
+                    tags={},
+                    extra_tags={"pool_name": pool_name},
                 )
                 DualStatsManager.gauge(
                     f"pool.running_slots.{pool_name}",
                     "pool.running_slots",
                     slot_stats["running"],
-                    tags={"pool_name": pool_name},
+                    tags={},
+                    extra_tags={"pool_name": pool_name},
                 )
                 DualStatsManager.gauge(
                     f"pool.deferred_slots.{pool_name}",
                     "pool.deferred_slots",
                     slot_stats["deferred"],
-                    tags={"pool_name": pool_name},
+                    tags={},
+                    extra_tags={"pool_name": pool_name},
                 )
                 DualStatsManager.gauge(
                     f"pool.scheduled_slots.{pool_name}",
                     "pool.scheduled_slots",
                     slot_stats["scheduled"],
-                    tags={"pool_name": pool_name},
+                    tags={},
+                    extra_tags={"pool_name": pool_name},
                 )
 
                 span.set_attributes(
--- airflow-core/src/airflow/models/taskinstance.py
+++ airflow-core/src/airflow/models/taskinstance.py
@@ -1254,7 +1254,8 @@ class TaskInstance(Base, LoggingMixin):
             f"dag.{self.dag_id}.{self.task_id}.{metric_name}",
             f"task.{metric_name}",
             timing,
-            tags={"task_id": self.task_id, "dag_id": self.dag_id, "queue": self.queue},
+            tags={},
+            extra_tags={"task_id": self.task_id, "dag_id": self.dag_id, "queue": self.queue},
         )
 
     def clear_next_method_args(self) -> None:
@@ -1522,7 +1523,8 @@ class TaskInstance(Base, LoggingMixin):
         DualStatsManager.incr(
             f"operator_failures_{ti.operator}",
             "operator_failures",
-            tags={**ti.stats_tags, "operator": ti.operator},
+            tags=ti.stats_tags,
+            extra_tags={"operator": ti.operator},
         )
         Stats.incr("ti_failures", tags=ti.stats_tags)
 
