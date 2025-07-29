771b4f5c108b1c939cd281533843d900683d3659
pierrejeambrun <pierrejbrun@gmail.com>
2025-07-29 11:54:37 +0200

    Simplify poolbar total slots

--- airflow-core/src/airflow/ui/src/pages/Pools/PoolBarCard.tsx
+++ airflow-core/src/airflow/ui/src/pages/Pools/PoolBarCard.tsx
@@ -64,16 +64,7 @@ const PoolBarCard = ({ pool }: PoolBarCardProps) => {
 
       <Box margin={4}>
         <Flex bg="gray.muted" borderRadius="md" h="20px" overflow="hidden" w="100%">
-          <PoolBar
-            pool={pool}
-            totalSlots={
-              pool.running_slots +
-              pool.queued_slots +
-              pool.deferred_slots +
-              pool.scheduled_slots +
-              pool.open_slots
-            }
-          />
+          <PoolBar pool={pool} totalSlots={pool.slots} />
         </Flex>
       </Box>
     </Box>
