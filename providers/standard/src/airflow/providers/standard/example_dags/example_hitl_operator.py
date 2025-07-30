03ed86c1ae44543c770d9e6bacbc0583442a18fb
Wei-Yu Chen <60685934+Jasperora@users.noreply.github.com>
2025-07-30 19:19:13 +0800

    Documentation for Human-in-the-loop operator (#53694)

--- providers/standard/src/airflow/providers/standard/example_dags/example_hitl_operator.py
+++ providers/standard/src/airflow/providers/standard/example_dags/example_hitl_operator.py
@@ -17,6 +17,7 @@
 
 from __future__ import annotations
 
+# [START hitl_tutorial]
 import datetime
 
 import pendulum
@@ -35,18 +36,23 @@ with DAG(
     catchup=False,
     tags=["example", "HITL"],
 ):
+    # [START howto_hitl_entry_operator]
     wait_for_input = HITLEntryOperator(
         task_id="wait_for_input",
         subject="Please provide required information: ",
         params={"information": Param("", type="string")},
     )
+    # [END howto_hitl_entry_operator]
 
+    # [START howto_hitl_operator]
     wait_for_option = HITLOperator(
         task_id="wait_for_option",
-        subject="Please choose one option to proceeded: ",
+        subject="Please choose one option to proceed: ",
         options=["option 1", "option 2", "option 3"],
     )
+    # [END howto_hitl_operator]
 
+    # [START howto_hitl_approval_operator]
     valid_input_and_options = ApprovalOperator(
         task_id="valid_input_and_options",
         subject="Are the following input and options valid?",
@@ -57,13 +63,17 @@ with DAG(
         defaults="Reject",
         execution_timeout=datetime.timedelta(minutes=1),
     )
+    # [END howto_hitl_approval_operator]
 
+    # [START howto_hitl_branch_operator]
     choose_a_branch_to_run = HITLBranchOperator(
         task_id="choose_a_branch_to_run",
         subject="You're now allowed to proceeded. Please choose one task to run: ",
         options=["task_1", "task_2", "task_3"],
     )
+    # [END howto_hitl_branch_operator]
 
+    # [START howto_hitl_workflow]
     @task
     def task_1(): ...
 
@@ -79,3 +89,6 @@ with DAG(
         >> choose_a_branch_to_run
         >> [task_1(), task_2(), task_3()]
     )
+    # [END howto_hitl_workflow]
+
+# [END hitl_tutorial]
