6c29e6ba96bb0b1e0616022c55c5525c8a1199b9
Christos Bisias <christosbis@gmail.com>
2025-08-05 19:30:17 +0300

    fix version_compat imports

--- providers/edge3/src/airflow/providers/edge3/version_compat.py
+++ providers/edge3/src/airflow/providers/edge3/version_compat.py
@@ -35,9 +35,9 @@ def get_base_airflow_version_tuple() -> tuple[int, int, int]:
 AIRFLOW_V_3_0_PLUS = get_base_airflow_version_tuple() >= (3, 0, 0)
 AIRFLOW_V_3_1_PLUS = get_base_airflow_version_tuple() >= (3, 1, 0)
 
-if AIRFLOW_V_3_1_PLUS:
+try:
     from airflow.sdk import timezone
-else:
+except ImportError:
     from airflow.utils import timezone  # type: ignore[no-redef,attr-defined]
 
 __all__ = [
