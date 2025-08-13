5da6500cf049f236afb045587b6a3d597b187699
Kaxil Naik <kaxilnaik@gmail.com>
2025-08-13 14:27:50 +0100

    Bump Airflow to 3.0.5 and Task SDK to 1.0.5

--- airflow-core/src/airflow/__init__.py
+++ airflow-core/src/airflow/__init__.py
@@ -25,7 +25,7 @@
 # lib.)  This is required by some IDEs to resolve the import paths.
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
 
-__version__ = "3.0.4"
+__version__ = "3.0.5"
 
 
 import os
