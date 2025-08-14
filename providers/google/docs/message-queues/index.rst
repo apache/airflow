e819c2cf5d6b586b41b76c8664e8f20632cde5f1
Deji Ibrahim <deji.a.ibrahim@gmail.com>
2025-08-14 07:35:12 +0100

    AIP-82: implement Google Pub/Sub message queue provider

---
+++ providers/google/docs/message-queues/index.rst
@@ -0,0 +1,61 @@
+ .. Licensed to the Apache Software Foundation (ASF) under one
+    or more contributor license agreements.  See the NOTICE file
+    distributed with this work for additional information
+    regarding copyright ownership.  The ASF licenses this file
+    to you under the Apache License, Version 2.0 (the
+    "License"); you may not use this file except in compliance
+    with the License.  You may obtain a copy of the License at
+
+ ..   http://www.apache.org/licenses/LICENSE-2.0
+
+ .. Unless required by applicable law or agreed to in writing,
+    software distributed under the License is distributed on an
+    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, either express or implied.  See the License for the
+    specific language governing permissions and limitations
+    under the License.
+
+Google Cloud Messaging Queues
+==============================
+
+Google Cloud Pub/Sub Queue Provider
+------------------------------------
+
+Implemented by :class:`~airflow.providers.google.cloud.queues.pubsub.PubsubMessageQueueProvider`
+
+The Google Cloud Pub/Sub Queue Provider is a message queue provider that uses Google Cloud Pub/Sub as the underlying message queue system.
+
+The queue should match this regex:
+
+.. exampleinclude:: /../src/airflow/providers/google/cloud/queues/pubsub.py
+    :language: python
+    :dedent: 0
+    :start-after: [START queue_regexp]
+    :end-before: [END queue_regexp]
+
+Queue URI Format:
+
+.. code-block:: text
+
+    projects/{project_id}/subscriptions/{subscription_name}
+
+Where:
+
+- ``project_id``: Google Cloud project ID where the Pub/Sub subscription exists
+- ``subscription_name``: Name of the Pub/Sub subscription to consume messages from
+
+.. note::
+    The subscription name must follow Google Cloud naming conventions:
+    - Must start with a letter
+    - Can contain letters, numbers, hyphens, underscores, tildes, plus signs, and percent signs
+    - Must be between 3 and 255 characters long
+    - Cannot start with "goog"
+
+The queue parameter is used to configure the underlying
+:class:`~airflow.providers.google.cloud.triggers.pubsub.PubsubPullTrigger` class.
+The provider extracts the ``project_id`` and ``subscription`` from the queue URI format
+and passes them along with additional configuration to the trigger constructor.
+
+.. warning::
+    Do not provide ``project_id`` or ``subscription`` in kwargs as they are extracted
+    from the queue URI format and will raise a ValueError if provided.
---
+++ providers/google/src/airflow/providers/google/cloud/queues/pubsub.py
@@ -0,0 +1,73 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+from __future__ import annotations
+
+import re
+from typing import TYPE_CHECKING
+
+from airflow.exceptions import AirflowOptionalProviderFeatureException
+from airflow.providers.google.cloud.triggers.pubsub import PubsubPullTrigger
+
+try:
+    from airflow.providers.common.messaging.providers.base_provider import BaseMessageQueueProvider
+except ImportError:
+    raise AirflowOptionalProviderFeatureException(
+        "This feature requires the 'common.messaging' provider to be installed in version >= 1.0.1."
+    )
+
+if TYPE_CHECKING:
+    from airflow.triggers.base import BaseEventTrigger
+
+# [START queue_regexp]
+QUEUE_REGEXP = (
+    r"^projects/(?P<project_id>[^/]+)/subscriptions/"
+    r"(?P<subscription>(?!goog)[A-Za-z][A-Za-z0-9\-_.~+%]{2,254})$"
+)
+# [END queue_regexp]
+
+
+class PubsubMessageQueueProvider(BaseMessageQueueProvider):
+    """Configuration for PubSub integration with common-messaging."""
+
+    def queue_matches(self, queue: str) -> bool:
+        return bool(re.match(QUEUE_REGEXP, queue))
+
+    def trigger_class(self) -> type[BaseEventTrigger]:
+        return PubsubPullTrigger
+
+    def trigger_kwargs(self, queue: str, **kwargs) -> dict:
+        pattern = re.compile(QUEUE_REGEXP)
+        match = pattern.match(queue)
+        project_id = match.group("project_id")
+        subscription = match.group("subscription")
+
+        if "project_id" in kwargs or "subscription" in kwargs:
+            raise ValueError(
+                "project_id or subscription cannot be provided in kwargs, use the queue param instead"
+            )
+
+        return_kwargs = {
+            "project_id": project_id,
+            "subscription": subscription,
+            "ack_messages": True,
+        }
+        if "max_messages" not in kwargs:
+            return_kwargs["max_messages"] = 1
+        if "gcp_conn_id" not in kwargs:
+            return_kwargs["gcp_conn_id"] = "google_cloud_default"
+
+        return return_kwargs
