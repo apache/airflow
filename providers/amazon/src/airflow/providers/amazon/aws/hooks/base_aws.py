a3b3fc839b647df5051a818a9a7081ce4d9bd04c
viiccwen <viiccwen@gmail.com>
2025-08-17 13:07:50 +0800

    fix(aws): move config initialization inside get_session method
    Move config creation inside get_session for better lazy evaluation to avoid session issue

--- providers/amazon/src/airflow/providers/amazon/aws/hooks/base_aws.py
+++ providers/amazon/src/airflow/providers/amazon/aws/hooks/base_aws.py
@@ -688,9 +688,10 @@ class AwsGenericHook(BaseHook, Generic[BaseAwsConnection]):
 
     def get_session(self, region_name: str | None = None, deferrable: bool = False) -> boto3.session.Session:
         """Get the underlying boto3.session.Session(region_name=region_name)."""
-        return SessionFactory(
-            conn=self.conn_config, region_name=region_name, config=self.config
-        ).create_session(deferrable=deferrable)
+        config = self._config or botocore.config.Config()
+        return SessionFactory(conn=self.conn_config, region_name=region_name, config=config).create_session(
+            deferrable=deferrable
+        )
 
     def _get_config(self, config: Config | None = None) -> Config:
         """
