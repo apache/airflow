f8ab73efa82f2727d1e468a45a6d32fd755ba511
David Blain <info@dabla.be>
2025-08-18 11:39:51 +0200

    refactor: Re-added missing import HttpxRequestAdapter

--- providers/microsoft/azure/tests/unit/microsoft/azure/hooks/test_msgraph.py
+++ providers/microsoft/azure/tests/unit/microsoft/azure/hooks/test_msgraph.py
@@ -26,6 +26,7 @@ import pytest
 from httpx import Response
 from httpx._utils import URLPattern
 from kiota_abstractions.request_information import RequestInformation
+from kiota_http.httpx_request_adapter import HttpxRequestAdapter
 from kiota_serialization_text.text_parse_node import TextParseNode
 from msgraph_core import APIVersion, NationalClouds
 from opentelemetry.trace import Span
