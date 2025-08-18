6634187e910676601a9fcb5975073d7a50fad211
Amogh Desai <amoghrajesh1999@gmail.com>
2025-08-18 18:04:56 +0530

    fixing tests

--- task-sdk/tests/task_sdk/docs/test_public_api.py
+++ task-sdk/tests/task_sdk/docs/test_public_api.py
@@ -53,6 +53,7 @@ def test_airflow_sdk_no_unexpected_exports():
         "log",
         "exceptions",
         "timezone",
+        "module_loading",
     }
     unexpected = actual - public - ignore
     assert not unexpected, f"Unexpected exports in airflow.sdk: {sorted(unexpected)}"
