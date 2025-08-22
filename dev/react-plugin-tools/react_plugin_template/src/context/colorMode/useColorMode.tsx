45001842811f224b063f222fab60adb9885d1401
李喆宸 <leeggroy@gmail.com>
2025-08-22 13:39:36 -0400

    refactor(plugin-template): align useColorMode with core UI implementation

--- dev/react-plugin-tools/react_plugin_template/src/context/colorMode/useColorMode.tsx
+++ dev/react-plugin-tools/react_plugin_template/src/context/colorMode/useColorMode.tsx
@@ -20,15 +20,11 @@
 import { useTheme } from "next-themes";
 
 export const useColorMode = () => {
-  const { theme, resolvedTheme, setTheme } = useTheme();
-  const toggleColorMode = () => {
-    setTheme(resolvedTheme === "light" ? "dark" : "light");
-  };
+  const { resolvedTheme, setTheme, theme } = useTheme();
 
   return {
     colorMode: resolvedTheme as "dark" | "light" | undefined,
     selectedTheme: theme as "dark" | "light" | "system" | undefined,
     setColorMode: setTheme,
-    toggleColorMode,
   };
 };
