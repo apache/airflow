41c3dbdcc307d3b7d89bc24b2e4f7ea299dd2da0
Jens Scheffler <jscheffl@apache.org>
2025-08-24 16:45:01 +0200

    Create API service stubs

---
+++ providers/edge3/src/airflow/providers/edge3/plugins/www/openapi-gen/queries/common.ts
@@ -0,0 +1,25 @@
+// generated with @7nohe/openapi-react-query-codegen@1.6.2 
+
+import { UseQueryResult } from "@tanstack/react-query";
+import { JobsService, LogsService, MonitorService, WorkerService } from "../requests/services.gen";
+export type LogsServiceLogfilePathDefaultResponse = Awaited<ReturnType<typeof LogsService.logfilePath>>;
+export type LogsServiceLogfilePathQueryResult<TData = LogsServiceLogfilePathDefaultResponse, TError = unknown> = UseQueryResult<TData, TError>;
+export const useLogsServiceLogfilePathKey = "LogsServiceLogfilePath";
+export const UseLogsServiceLogfilePathKeyFn = ({ authorization, dagId, mapIndex, runId, taskId, tryNumber }: {
+  authorization: string;
+  dagId: string;
+  mapIndex: number;
+  runId: string;
+  taskId: string;
+  tryNumber: number;
+}, queryKey?: Array<unknown>) => [useLogsServiceLogfilePathKey, ...(queryKey ?? [{ authorization, dagId, mapIndex, runId, taskId, tryNumber }])];
+export type MonitorServiceHealthDefaultResponse = Awaited<ReturnType<typeof MonitorService.health>>;
+export type MonitorServiceHealthQueryResult<TData = MonitorServiceHealthDefaultResponse, TError = unknown> = UseQueryResult<TData, TError>;
+export const useMonitorServiceHealthKey = "MonitorServiceHealth";
+export const UseMonitorServiceHealthKeyFn = (queryKey?: Array<unknown>) => [useMonitorServiceHealthKey, ...(queryKey ?? [])];
+export type JobsServiceFetchMutationResult = Awaited<ReturnType<typeof JobsService.fetch>>;
+export type LogsServicePushLogsMutationResult = Awaited<ReturnType<typeof LogsService.pushLogs>>;
+export type WorkerServiceRegisterMutationResult = Awaited<ReturnType<typeof WorkerService.register>>;
+export type JobsServiceStateMutationResult = Awaited<ReturnType<typeof JobsService.state>>;
+export type WorkerServiceSetStateMutationResult = Awaited<ReturnType<typeof WorkerService.setState>>;
+export type WorkerServiceUpdateQueuesMutationResult = Awaited<ReturnType<typeof WorkerService.updateQueues>>;
