ef1f77e66554385caf3671b7e8e29c1a06c86214
Jens Scheffler <jscheffl@apache.org>
2025-08-24 16:45:01 +0200

    Create API service stubs

---
+++ providers/edge3/src/airflow/providers/edge3/plugins/www/openapi-gen/queries/ensureQueryData.ts
@@ -0,0 +1,14 @@
+// generated with @7nohe/openapi-react-query-codegen@1.6.2 
+
+import { type QueryClient } from "@tanstack/react-query";
+import { LogsService, MonitorService } from "../requests/services.gen";
+import * as Common from "./common";
+export const ensureUseLogsServiceLogfilePathData = (queryClient: QueryClient, { authorization, dagId, mapIndex, runId, taskId, tryNumber }: {
+  authorization: string;
+  dagId: string;
+  mapIndex: number;
+  runId: string;
+  taskId: string;
+  tryNumber: number;
+}) => queryClient.ensureQueryData({ queryKey: Common.UseLogsServiceLogfilePathKeyFn({ authorization, dagId, mapIndex, runId, taskId, tryNumber }), queryFn: () => LogsService.logfilePath({ authorization, dagId, mapIndex, runId, taskId, tryNumber }) });
+export const ensureUseMonitorServiceHealthData = (queryClient: QueryClient) => queryClient.ensureQueryData({ queryKey: Common.UseMonitorServiceHealthKeyFn(), queryFn: () => MonitorService.health() });
---
+++ providers/edge3/src/airflow/providers/edge3/plugins/www/openapi-gen/queries/prefetch.ts
@@ -0,0 +1,14 @@
+// generated with @7nohe/openapi-react-query-codegen@1.6.2 
+
+import { type QueryClient } from "@tanstack/react-query";
+import { LogsService, MonitorService } from "../requests/services.gen";
+import * as Common from "./common";
+export const prefetchUseLogsServiceLogfilePath = (queryClient: QueryClient, { authorization, dagId, mapIndex, runId, taskId, tryNumber }: {
+  authorization: string;
+  dagId: string;
+  mapIndex: number;
+  runId: string;
+  taskId: string;
+  tryNumber: number;
+}) => queryClient.prefetchQuery({ queryKey: Common.UseLogsServiceLogfilePathKeyFn({ authorization, dagId, mapIndex, runId, taskId, tryNumber }), queryFn: () => LogsService.logfilePath({ authorization, dagId, mapIndex, runId, taskId, tryNumber }) });
+export const prefetchUseMonitorServiceHealth = (queryClient: QueryClient) => queryClient.prefetchQuery({ queryKey: Common.UseMonitorServiceHealthKeyFn(), queryFn: () => MonitorService.health() });
