220785a9a24b845022ee8b1822ec95d0e1855fe1
hussein-awala <hussein@awala.fr>
2025-08-24 16:17:57 +0200

    Support templated extra in outlets assets

--- task-sdk/src/airflow/sdk/execution_time/task_runner.py
+++ task-sdk/src/airflow/sdk/execution_time/task_runner.py
@@ -1270,6 +1270,11 @@ def _execute_task(context: Context, ti: RuntimeTaskInstance, log: Logger):
 
     outlet_events = context_get_outlet_events(context)
 
+    for outlet in task.outlets or ():
+        if isinstance(outlet, Asset):
+            outlet.render_extra_field(context, jinja_env=task.dag.get_template_env())
+            outlet_events[outlet].extra.update(outlet.extra)
+
     if (pre_execute_hook := task._pre_execute_hook) is not None:
         create_executable_runner(pre_execute_hook, outlet_events, logger=log).run(context)
     if getattr(pre_execute_hook := task.pre_execute, "__func__", None) is not BaseOperator.pre_execute:
