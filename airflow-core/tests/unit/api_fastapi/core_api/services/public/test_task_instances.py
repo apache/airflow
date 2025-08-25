6ef69327ede65ff3784d557fc72714ef3c31e25d
Guan Ming(Wesley) Chiu <105915352+guan404ming@users.noreply.github.com>
2025-08-25 23:31:39 +0800

    Simplify task instance creation

--- airflow-core/tests/unit/api_fastapi/core_api/services/public/test_task_instances.py
+++ airflow-core/tests/unit/api_fastapi/core_api/services/public/test_task_instances.py
@@ -17,123 +17,27 @@
 
 from __future__ import annotations
 
-import datetime as dt
-from typing import TYPE_CHECKING
-
 import pytest
 
-from airflow._shared.timezones.timezone import datetime
 from airflow.api_fastapi.core_api.datamodels.common import BulkBody
 from airflow.api_fastapi.core_api.services.public.task_instances import BulkTaskInstanceService
-from airflow.models import DagRun, TaskInstance
-from airflow.models.dag_version import DagVersion
-from airflow.utils.state import DagRunState, State
-from airflow.utils.types import DagRunType
+from airflow.providers.standard.operators.bash import BashOperator
 
 from tests_common.test_utils.db import (
     clear_db_runs,
 )
 
 pytestmark = pytest.mark.db_test
-
-DEFAULT = datetime(2020, 1, 1)
-DEFAULT_DATETIME_STR_1 = "2020-01-01T00:00:00+00:00"
-DEFAULT_DATETIME_STR_2 = "2020-01-02T00:00:00+00:00"
-
-DEFAULT_DATETIME_1 = dt.datetime.fromisoformat(DEFAULT_DATETIME_STR_1)
-DEFAULT_DATETIME_2 = dt.datetime.fromisoformat(DEFAULT_DATETIME_STR_2)
+DAG_ID = "TEST_DAG"
+DAG_RUN_ID = "TEST_DAG_RUN"
+TASK_ID_1 = "TEST_TASK_1"
+TASK_ID_2 = "TEST_TASK_2"
 
 
 class TestTaskInstanceEndpoint:
     @staticmethod
     def clear_db():
         clear_db_runs()
-
-    def create_task_instances(
-        self,
-        session,
-        dagbag,
-        dag_id: str = "example_python_operator",
-        update_extras: bool = True,
-        task_instances=None,
-        dag_run_state=DagRunState.RUNNING,
-        with_ti_history=False,
-    ):
-        """Method to create task instances using kwargs and default arguments"""
-        default_time = DEFAULT
-        ti_init = {
-            "logical_date": default_time,
-            "state": State.RUNNING,
-        }
-        ti_extras = {
-            "start_date": default_time + dt.timedelta(days=1),
-            "end_date": default_time + dt.timedelta(days=2),
-            "pid": 100,
-            "duration": 10000,
-            "pool": "default_pool",
-            "queue": "default_queue",
-        }
-
-        dag = dagbag.get_latest_version_of_dag(dag_id, session=session)
-        tasks = dag.tasks
-        counter = len(tasks)
-        if task_instances is not None:
-            counter = min(len(task_instances), counter)
-
-        run_id = "TEST_DAG_RUN_ID"
-        logical_date = ti_init.pop("logical_date", default_time)
-        dr = None
-        dag_version = DagVersion.get_latest_version(dag.dag_id, session=session)
-        tis = []
-        for i in range(counter):
-            if task_instances is None:
-                pass
-            elif update_extras:
-                ti_extras.update(task_instances[i])
-            else:
-                ti_init.update(task_instances[i])
-
-            if "logical_date" in ti_init:
-                run_id = f"TEST_DAG_RUN_ID_{i}"
-                logical_date = ti_init.pop("logical_date")
-                dr = None
-
-            if not dr:
-                dr = DagRun(
-                    run_id=run_id,
-                    dag_id=dag_id,
-                    logical_date=logical_date,
-                    run_type=DagRunType.MANUAL,
-                    state=dag_run_state,
-                )
-                session.add(dr)
-                session.flush()
-            if TYPE_CHECKING:
-                assert dag_version
-            ti = TaskInstance(task=tasks[i], **ti_init, dag_version_id=dag_version.id)
-            session.add(ti)
-            ti.dag_run = dr
-            ti.note = "placeholder-note"
-
-            for key, value in ti_extras.items():
-                setattr(ti, key, value)
-            tis.append(ti)
-
-        session.flush()
-
-        if with_ti_history:
-            for ti in tis:
-                ti.try_number = 1
-                session.merge(ti)
-                session.flush()
-            dag.clear(session=session)
-            for ti in tis:
-                ti.try_number = 2
-                ti.queue = "default_queue"
-                session.merge(ti)
-                session.flush()
-        session.commit()
-        return tis
 
 
 class TestCategorizeTaskInstances(TestTaskInstanceEndpoint):
@@ -156,8 +60,8 @@ class TestCategorizeTaskInstances(TestTaskInstanceEndpoint):
         "task_keys, expected_matched_keys, expected_not_found_keys, expected_matched_count, expected_not_found_count",
         [
             pytest.param(
-                {("print_the_context", -1), ("log_sql_query", -1)},
-                {("print_the_context", -1), ("log_sql_query", -1)},
+                {(TASK_ID_1, -1), (TASK_ID_2, -1)},
+                {(TASK_ID_1, -1), (TASK_ID_2, -1)},
                 set(),
                 2,
                 0,
@@ -172,9 +76,9 @@ class TestCategorizeTaskInstances(TestTaskInstanceEndpoint):
                 id="none_found",
             ),
             pytest.param(
-                {("print_the_context", -1), ("print_the_context", 0)},
-                {("print_the_context", -1)},
-                {("print_the_context", 0)},
+                {(TASK_ID_1, -1), (TASK_ID_1, 0)},
+                {(TASK_ID_1, -1)},
+                {(TASK_ID_1, 0)},
                 1,
                 1,
                 id="mixed_found_and_not_found",
@@ -185,7 +89,7 @@ class TestCategorizeTaskInstances(TestTaskInstanceEndpoint):
     def test_categorize_task_instances(
         self,
         session,
-        dagbag,
+        dag_maker,
         task_keys,
         expected_matched_keys,
         expected_not_found_keys,
@@ -193,14 +97,23 @@ class TestCategorizeTaskInstances(TestTaskInstanceEndpoint):
         expected_not_found_count,
     ):
         """Test categorize_task_instances with various scenarios."""
-        self.create_task_instances(session, dagbag)
-        dag_id = "example_python_operator"
-        run_id = "TEST_DAG_RUN_ID"
+        with dag_maker(dag_id=DAG_ID, session=session):
+            BashOperator(task_id=TASK_ID_1, bash_command="echo 1")
+            BashOperator(task_id=TASK_ID_2, bash_command="echo 2")
+
+        dag_maker.create_dagrun(run_id=DAG_RUN_ID)
+
+        session.commit()
 
         user = self.MockUser()
-        request = BulkBody(actions=[])
+        bulk_request = BulkBody(actions=[])
         service = BulkTaskInstanceService(
-            session=session, request=request, dag_id=dag_id, dag_run_id=run_id, dag_bag=dagbag, user=user
+            session=session,
+            request=bulk_request,
+            dag_id=DAG_ID,
+            dag_run_id=DAG_RUN_ID,
+            dag_bag=dag_maker.dagbag,
+            user=user,
         )
 
         _, matched_task_keys, not_found_task_keys = service.categorize_task_instances(task_keys)
