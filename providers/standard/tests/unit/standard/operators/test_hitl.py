05772d953aeb516e8059cbc41acb4a54797901e7
Wei Lee <weilee.rx@gmail.com>
2025-08-26 23:08:52 +0800

    fixup! feat(hitl): add utility function generate_link_to_ui

--- providers/standard/tests/unit/standard/operators/test_hitl.py
+++ providers/standard/tests/unit/standard/operators/test_hitl.py
@@ -291,6 +291,7 @@ class TestHITLOperator:
         options: list[str] | None,
         params_input: dict[str, Any] | None,
         expected_query_string: str,
+        hitl_task_and_ti_for_generating_link: tuple[HITLOperator, TaskInstance],
     ) -> None:
         with dag_maker("test_dag"):
             task = HITLOperator(
