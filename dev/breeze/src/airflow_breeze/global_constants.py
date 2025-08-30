ae7634a9fedc73db47c52f75fad27661c619cd1f
Kaxil Naik <kaxilnaik@gmail.com>
2025-08-30 03:38:47 +0100

    Update main with Airflow 3.0.6 release details (#55089)
    - https://pypi.org/project/apache-airflow-core/3.0.6/
    - https://pypi.org/project/apache-airflow/3.0.6/
    - https://pypi.org/project/apache-airflow-task-sdk/1.0.6/

--- dev/breeze/src/airflow_breeze/global_constants.py
+++ dev/breeze/src/airflow_breeze/global_constants.py
@@ -777,7 +777,7 @@ PROVIDERS_COMPATIBILITY_TESTS_MATRIX: list[dict[str, str | list[str]]] = [
     },
     {
         "python-version": "3.10",
-        "airflow-version": "3.0.4",
+        "airflow-version": "3.0.6",
         "remove-providers": "",
         "run-unit-tests": "true",
     },
