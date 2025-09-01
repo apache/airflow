40bb5ab8cc95d3350ea99c51c0dff5b16df0f66e
Javier Herrer Torres <javierhe@microsoft.com>
2025-09-01 18:32:10 +0200

    Fix: pass extra headers in LivyAsyncHook API call

--- providers/apache/livy/src/airflow/providers/apache/livy/hooks/livy.py
+++ providers/apache/livy/src/airflow/providers/apache/livy/hooks/livy.py
@@ -629,7 +629,10 @@ class LivyAsyncHook(HttpAsyncHook):
         """
         self._validate_session_id(session_id)
         self.log.info("Fetching info for batch session %s", session_id)
-        result = await self.run_method(endpoint=f"{self.endpoint_prefix}/batches/{session_id}/state")
+        result = await self.run_method(
+            endpoint=f"{self.endpoint_prefix}/batches/{session_id}/state",
+            headers=self.extra_headers,
+        )
         if result["status"] == "error":
             self.log.info(result)
             return {"batch_state": "error", "response": result, "status": "error"}
