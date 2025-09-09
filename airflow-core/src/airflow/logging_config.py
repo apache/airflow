apache/airflow

commit 36fc820f27a25df2997cc2c85010d5caa02e8211

Switch all airflow logging to structlog (#52651)
This change continues the work that was started in Airflow 3.0 and AIP-72 to
use Structlog in place of stdlib logging in Airflow.

The primary change here is to make LoggingMixin return a customized
structlogger; customized to maintain surface compatability with logging.Logger
(surface meaning things like handlers and filters aren't preserved) and to
then capture/redirect all logging via stdlib via Structlog processors.

This is the first step in allowing all Airflow components to be able to
produce JSON logs natively.

Things of note in this implementation:

- We have a customized structlog filtering logger that has a "per-logger-tree"
  level concept.

  This is implemented using a prefix trie[1] to lookup the logging level to
  efficiently be able to look up the level for child loggers when configured
  only at the parent level.

- We have a custom `PercentFormatRender` class that renders non-JSON logs that
  understands the stdlib style format strings, meaning users custom logging
  config will be respected again for the daemon components.

  (Note though: this won't help with Task logs, as those are always JSON and
  the UI does the rendering of those.)

- There is no longer a need for a different log format for colored and plain -- using
  color format specifiers (`%(blue)s`, `%(log_level)s` etc) when colors are
  disabled/not available will output nothing in their place.

- Introduce an mechanism for users to easily set the log level for loggers --
  for instance if you are debugging the scheduler, it would be nice to be able
  to set the `airflow.jobs.scheduler_job_runner` logger to DEBUG while keeping
  everything else at info.

[1]: https://en.wikipedia.org/wiki/Trie


The reason for not using caplog has gone away with the switch to structlog,
and we already override the builtin `caplog` fixture to use our structlog
version

airflow-core/src/airflow/logging_config.py
@@ -20,7 +20,6 @@ from __future__ import annotations
 import logging
 import warnings
 from importlib import import_module
-from logging.config import dictConfig
 from typing import TYPE_CHECKING, Any
 
 from airflow.configuration import conf
@@ -86,33 +85,25 @@ def load_logging_config() -> tuple[dict[str, Any], str]:
 
 
 def configure_logging():
+    from airflow._shared.logging.structlog import configure_logging
+
     logging_config, logging_class_path = load_logging_config()
     try:
-        # Ensure that the password masking filter is applied to the 'task' handler
-        # no matter what the user did.
-        if "filters" in logging_config and "mask_secrets" in logging_config["filters"]:
-            # But if they replace the logging config _entirely_, don't try to set this, it won't work
-            task_handler_config = logging_config["handlers"]["task"]
-
-            task_handler_config.setdefault("filters", [])
-
-            if "mask_secrets" not in task_handler_config["filters"]:
-                task_handler_config["filters"].append("mask_secrets")
-
+        level: str = conf.get_mandatory_value("logging", "LOGGING_LEVEL").upper()
         # Try to init logging
-        dictConfig(logging_config)
+        configure_logging(log_level=level, stdlib_config=logging_config)
     except (ValueError, KeyError) as e:
         log.error("Unable to load the config, contains a configuration error.")
         # When there is an error in the config, escalate the exception
         # otherwise Airflow would silently fall back on the default config
         raise e
 
-    validate_logging_config(logging_config)
+    validate_logging_config()
 
     return logging_class_path
 
 
-def validate_logging_config(logging_config):
+def validate_logging_config():
     """Validate the provided Logging Config."""
     # Now lets validate the other logging-related settings
     task_log_reader = conf.get("logging", "task_log_reader")

shared/logging/src/airflow_shared/logging/__init__.py
@@ -1,0 +1,19 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+from __future__ import annotations
+
+from .structlog import configure_logging as configure_logging

commit a5eb23eac2a7072e400b122926143ba554cc488d

Fixup to structlog migration - logging folder not being created correctly.
This was broken in #52651 with our move away from FileTaskHandler, and hidden
when running in Breeze due to the default logs folder already existing.

airflow-core/src/airflow/logging_config.py
@@ -85,7 +85,7 @@ def load_logging_config() -> tuple[dict[str, Any], str]:
 
 
 def configure_logging():
-    from airflow._shared.logging.structlog import configure_logging
+    from airflow._shared.logging import configure_logging, init_log_folder
 
     logging_config, logging_class_path = load_logging_config()
     try:
@@ -99,6 +99,18 @@ def configure_logging():
         raise e
 
     validate_logging_config()
+
+    new_folder_permissions = int(
+        conf.get("logging", "file_task_handler_new_folder_permissions", fallback="0o775"),
+        8,
+    )
+
+    base_log_folder = conf.get("logging", "base_log_folder")
+
+    return init_log_folder(
+        base_log_folder,
+        new_folder_permissions=new_folder_permissions,
+    )
 
     return logging_class_path
 

shared/logging/src/airflow_shared/logging/__init__.py
@@ -16,4 +16,10 @@
 # under the License.
 from __future__ import annotations
 
-from .structlog import configure_logging as configure_logging
+__all__ = [
+    "configure_logging",
+    "init_log_file",
+    "init_log_folder",
+]
+
+from .structlog import configure_logging, init_log_file, init_log_folder

shared/logging/src/airflow_shared/logging/structlog.py
@@ -25,6 +25,7 @@ import re
 import sys
 from collections.abc import Callable, Mapping, Sequence
 from functools import cache, cached_property, partial
+from pathlib import Path
 from typing import TYPE_CHECKING, Any, BinaryIO, Generic, TextIO, TypeVar, cast
 
 import pygtrie
@@ -503,6 +504,62 @@ def configure_logging(
     }
 
     logging.config.dictConfig(config)
+
+
+def init_log_folder(directory: str | os.PathLike[str], new_folder_permissions: int):
+    """
+    Prepare the log folder and ensure its mode is as configured.
+
+    To handle log writing when tasks are impersonated, the log files need to
+    be writable by the user that runs the Airflow command and the user
+    that is impersonated. This is mainly to handle corner cases with the
+    SubDagOperator. When the SubDagOperator is run, all of the operators
+    run under the impersonated user and create appropriate log files
+    as the impersonated user. However, if the user manually runs tasks
+    of the SubDagOperator through the UI, then the log files are created
+    by the user that runs the Airflow command. For example, the Airflow
+    run command may be run by the `airflow_sudoable` user, but the Airflow
+    tasks may be run by the `airflow` user. If the log files are not
+    writable by both users, then it's possible that re-running a task
+    via the UI (or vice versa) results in a permission error as the task
+    tries to write to a log file created by the other user.
+
+    We leave it up to the user to manage their permissions by exposing configuration for both
+    new folders and new log files. Default is to make new log folders and files group-writeable
+    to handle most common impersonation use cases. The requirement in this case will be to make
+    sure that the same group is set as default group for both - impersonated user and main airflow
+    user.
+    """
+    directory = Path(directory)
+    for parent in reversed(Path(directory).parents):
+        parent.mkdir(mode=new_folder_permissions, exist_ok=True)
+    directory.mkdir(mode=new_folder_permissions, exist_ok=True)
+
+
+def init_log_file(
+    base_log_folder: str | os.PathLike[str],
+    local_relative_path: str | os.PathLike[str],
+    *,
+    new_folder_permissions: int = 0o775,
+    new_file_permissions: int = 0o664,
+) -> Path:
+    """
+    Ensure log file and parent directories are created with the correct permissions.
+
+    Any directories that are missing are created with the right permission bits.
+
+    See above ``init_log_folder`` method for more detailed explanation.
+    """
+    full_path = Path(base_log_folder, local_relative_path)
+    init_log_folder(full_path.parent, new_folder_permissions)
+
+    try:
+        full_path.touch(new_file_permissions)
+    except OSError as e:
+        log = structlog.get_logger(__name__)
+        log.warning("OSError while changing ownership of the log file. %s", e)
+
+    return full_path
 
 
 if __name__ == "__main__":

task-sdk/src/airflow/sdk/log.py
@@ -222,46 +222,14 @@ def logger_at_level(name: str, level: int) -> Logger:
     )
 
 
-def _prepare_log_folder(directory: Path, mode: int):
-    """
-    Prepare the log folder and ensure its mode is as configured.
-
-    To handle log writing when tasks are impersonated, the log files need to
-    be writable by the user that runs the Airflow command and the user
-    that is impersonated. This is mainly to handle corner cases with the
-    SubDagOperator. When the SubDagOperator is run, all of the operators
-    run under the impersonated user and create appropriate log files
-    as the impersonated user. However, if the user manually runs tasks
-    of the SubDagOperator through the UI, then the log files are created
-    by the user that runs the Airflow command. For example, the Airflow
-    run command may be run by the `airflow_sudoable` user, but the Airflow
-    tasks may be run by the `airflow` user. If the log files are not
-    writable by both users, then it's possible that re-running a task
-    via the UI (or vice versa) results in a permission error as the task
-    tries to write to a log file created by the other user.
-
-    We leave it up to the user to manage their permissions by exposing configuration for both
-    new folders and new log files. Default is to make new log folders and files group-writeable
-    to handle most common impersonation use cases. The requirement in this case will be to make
-    sure that the same group is set as default group for both - impersonated user and main airflow
-    user.
-    """
-    for parent in reversed(directory.parents):
-        parent.mkdir(mode=mode, exist_ok=True)
-    directory.mkdir(mode=mode, exist_ok=True)
-
-
 def init_log_file(local_relative_path: str) -> Path:
     """
     Ensure log file and parent directories are created.
 
     Any directories that are missing are created with the right permission bits.
-
-    See above ``_prepare_log_folder`` method for more detailed explanation.
     """
-    # NOTE: This is duplicated from airflow.utils.log.file_task_handler:FileTaskHandler._init_file, but we
-    # want to remove that
     from airflow.configuration import conf
+    from airflow.sdk._shared.logging import init_log_file
 
     new_file_permissions = int(
         conf.get("logging", "file_task_handler_new_file_permissions", fallback="0o664"),
@@ -273,17 +241,13 @@ def init_log_file(local_relative_path: str) -> Path:
     )
 
     base_log_folder = conf.get("logging", "base_log_folder")
-    full_path = Path(base_log_folder, local_relative_path)
 
-    _prepare_log_folder(full_path.parent, new_folder_permissions)
-
-    try:
-        full_path.touch(new_file_permissions)
-    except OSError as e:
-        log = structlog.get_logger(__name__)
-        log.warning("OSError while changing ownership of the log file. %s", e)
-
-    return full_path
+    return init_log_file(
+        base_log_folder,
+        local_relative_path,
+        new_folder_permissions=new_folder_permissions,
+        new_file_permissions=new_file_permissions,
+    )
 
 
 def load_remote_log_handler() -> RemoteLogIO | None: