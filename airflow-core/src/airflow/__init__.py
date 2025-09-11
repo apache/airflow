f676b065d2ef5464311f3bff9b295c2cf0f868c1
Kaxil Naik <kaxilnaik@gmail.com>
2025-09-11 01:07:04 -0600

    Make the main branch target to Airflow 3.2 & Task SDK 1.2
    Since 3.1 release branch is out and we are in beta phase, let's continue development in main for the next minor release.

--- airflow-core/src/airflow/__init__.py
+++ airflow-core/src/airflow/__init__.py
@@ -25,7 +25,7 @@
 # lib.)  This is required by some IDEs to resolve the import paths.
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)
 
-__version__ = "3.1.0"
+__version__ = "3.2.0"
 
 
 import os
--- task-sdk/src/airflow/sdk/__init__.py
+++ task-sdk/src/airflow/sdk/__init__.py
@@ -60,7 +60,7 @@ __all__ = [
     "teardown",
 ]
 
-__version__ = "1.1.0"
+__version__ = "1.2.0"
 
 if TYPE_CHECKING:
     from airflow.sdk.api.datamodels._generated import DagRunState, TaskInstanceState, TriggerRule
