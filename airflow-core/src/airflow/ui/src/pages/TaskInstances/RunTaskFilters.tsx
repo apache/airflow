38069abdf9c82b654e99cbdb75d3907f186125f1
Kavya Katal <kavyakatal09@gmail.com>
2025-09-15 02:09:45 +0530

    ui: switch DAG Run → Task Instances tab to FilterBar (pill UI)
    Signed-off-by: Kavya Katal <kavyakatal09@gmail.com>

--- airflow-core/src/airflow/ui/src/pages/TaskInstances/RunTaskFilters.tsx
+++ airflow-core/src/airflow/ui/src/pages/TaskInstances/RunTaskFilters.tsx
@@ -1,7 +1,23 @@
 /*!
- * Licensed to the Apache Software Foundation (ASF)...
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 import { useMemo } from "react";
+
 import { FilterBar, type FilterValue } from "src/components/FilterBar";
 import { SearchParamsKeys } from "src/constants/searchParams";
 import { useFiltersHandler, type FilterableSearchParamsKeys } from "src/utils";
@@ -11,8 +27,8 @@ export const RunTaskFilters = () => {
   const searchParamKeys = useMemo(
     (): Array<FilterableSearchParamsKeys> => [
       SearchParamsKeys.TASK_ID_PATTERN, // pill label "Task" (text)
-      SearchParamsKeys.START_DATE,   // date from
-      SearchParamsKeys.END_DATE,     // date to
+      SearchParamsKeys.START_DATE, // date from
+      SearchParamsKeys.END_DATE, // date to
     ],
     [],
   );
@@ -39,5 +55,7 @@ export const RunTaskFilters = () => {
     return values;
   }, [searchParams, filterConfigs]);
 
-  return <FilterBar configs={filterConfigs} initialValues={initialValues} onFiltersChange={handleFiltersChange} />;
+  return (
+    <FilterBar configs={filterConfigs} initialValues={initialValues} onFiltersChange={handleFiltersChange} />
+  );
 };
