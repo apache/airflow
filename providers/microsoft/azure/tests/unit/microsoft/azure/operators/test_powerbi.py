f835d3c6ae24305f95b66ea1c5670b78add12614
David Blain <info@dabla.be>
2025-09-15 17:28:31 +0200

    Avoid using extra_dejson method on connection in KiotaRequestAdapterHook to avoid AsyncToSync RuntimeError (#55179)
    * fix: do not use connection.extra_dejson until it's fixed in Airflow otherwise expect: RuntimeError: You cannot use AsyncToSync in the same thread as an async event loop.
    
    * refactor: Refactored get_async_conn to check whether BaseHook has an async aget_connection method or not, if not fallback to sync_to_async solution
    
    * refactor: Import Connection from version_compat
    
    * refactor: Moved render_template_fields from deferrable_operator to execute_operator
    
    * Revert "refactor: Moved render_template_fields from deferrable_operator to execute_operator"
    
    This reverts commit 7d57de174890af6efeb9a7262e1dcd96c7c28c8a.
    
    * refactor: Refactored patch_hook_and_request_adapter
    
    * refactor: Patch directly on BaseHook class
    
    * refactor: Extracted patch_hook from patch_hook_and_request_adapter
    
    * refactor: Try fixing static checks
    
    * refactor: Fixed test connection
    
    * refactor: Added test on get_conn in MSGraphTrigger
    
    * refactor: Changed match on test_get_conn in TestKiotaRequestAdapterHook
    
    * refactor: fixed static checks
    
    * refactor: Inlined connection side effects
    
    * fix: Also add method when triggering next page in MSGraphOperator, otherwise it will always fall back to GET and not the currently used
    
    * refactor: templated fields don't make sense in MSGraphTrigger
    
    * refactor: Removed unused import of Sequence
    
    * refactor: Refactored tests for msgraph
    
    * refactor: Applied same for powerbi
    
    * refactor: Fixed static checks
    
    * refactor: Fixed test powerbi list
    
    * refactor: cross compat import timezone
    
    * refactor: Added issue number in TODO
    
    * refactor: Moved Context import out of type checking
    
    * refactor: Fixed hook patches TestPowerBITrigger
    
    * refactor: Fixed classpath assertion in TestPowerBITrigger

--- providers/microsoft/azure/tests/unit/microsoft/azure/operators/test_powerbi.py
+++ providers/microsoft/azure/tests/unit/microsoft/azure/operators/test_powerbi.py
@@ -29,21 +29,17 @@ from airflow.providers.microsoft.azure.hooks.powerbi import (
 )
 from airflow.providers.microsoft.azure.operators.powerbi import PowerBIDatasetRefreshOperator
 from airflow.providers.microsoft.azure.triggers.powerbi import PowerBITrigger
-from airflow.utils import timezone
+from airflow.providers.microsoft.azure.version_compat import BaseHook
 
 from tests_common.test_utils.mock_context import mock_context
-from unit.microsoft.azure.base import Base
 from unit.microsoft.azure.test_utils import get_airflow_connection
 
 try:
-    import importlib.util
-
-    if not importlib.util.find_spec("airflow.sdk.bases.hook"):
-        raise ImportError
-
-    BASEHOOK_PATCH_PATH = "airflow.sdk.bases.hook.BaseHook"
+    from airflow.sdk import timezone
 except ImportError:
-    BASEHOOK_PATCH_PATH = "airflow.hooks.base.BaseHook"
+    from airflow.utils import timezone  # type: ignore[no-redef]
+
+
 DEFAULT_CONNECTION_CLIENT_SECRET = "powerbi_conn_id"
 TASK_ID = "run_powerbi_operator"
 GROUP_ID = "group_id"
@@ -101,8 +97,8 @@ IN_PROGRESS_REFRESH_DETAILS = {
 }
 
 
-class TestPowerBIDatasetRefreshOperator(Base):
-    @mock.patch(f"{BASEHOOK_PATCH_PATH}.get_connection", side_effect=get_airflow_connection)
+class TestPowerBIDatasetRefreshOperator:
+    @mock.patch.object(BaseHook, "get_connection", side_effect=get_airflow_connection)
     def test_execute_wait_for_termination_with_deferrable(self, connection):
         operator = PowerBIDatasetRefreshOperator(
             **CONFIG,
@@ -115,7 +111,7 @@ class TestPowerBIDatasetRefreshOperator(Base):
         assert isinstance(exc.value.trigger, PowerBITrigger)
         assert exc.value.trigger.dataset_refresh_id is None
 
-    @mock.patch(f"{BASEHOOK_PATCH_PATH}.get_connection", side_effect=get_airflow_connection)
+    @mock.patch.object(BaseHook, "get_connection", side_effect=get_airflow_connection)
     def test_powerbi_operator_async_get_refresh_status_success(self, connection):
         """Assert that get_refresh_status log success message"""
         operator = PowerBIDatasetRefreshOperator(
