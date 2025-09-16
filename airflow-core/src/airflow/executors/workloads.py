2d0ea745ec6ecd915956dab9d1c9ac03dbb95f9a
Ephraim Anierobi <splendidzigy24@gmail.com>
2025-09-16 12:27:44 +0100

    Do not require dag_version_id in workload TI model
    Older TIs won't have the dag_version_id, so we shouldn't require it
    in workload's TI model.

--- airflow-core/src/airflow/executors/workloads.py
+++ airflow-core/src/airflow/executors/workloads.py
@@ -55,7 +55,6 @@ class TaskInstance(BaseModel):
     """Schema for TaskInstance with minimal required fields needed for Executors and Task SDK."""
 
     id: uuid.UUID
-    dag_version_id: uuid.UUID
     task_id: str
     dag_id: str
     run_id: str
@@ -69,6 +68,7 @@ class TaskInstance(BaseModel):
 
     parent_context_carrier: dict | None = None
     context_carrier: dict | None = None
+    dag_version_id: uuid.UUID | None = None
 
     # TODO: Task-SDK: Can we replace TastInstanceKey with just the uuid across the codebase?
     @property
