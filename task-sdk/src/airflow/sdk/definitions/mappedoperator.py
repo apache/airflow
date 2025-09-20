22e63ccbd7cff54afcda40653af5977f472b6cb7
David Blain <info@dabla.be>
2025-09-20 22:54:09 +0200

    refactor: start_from_trigger and start_trigger_args could as well be defined on class level as on partial_kwargs in OperatorPartial

--- task-sdk/src/airflow/sdk/definitions/mappedoperator.py
+++ task-sdk/src/airflow/sdk/definitions/mappedoperator.py
@@ -227,6 +227,8 @@ class OperatorPartial:
         task_group = partial_kwargs.pop("task_group")
         start_date = partial_kwargs.pop("start_date", None)
         end_date = partial_kwargs.pop("end_date", None)
+        start_from_trigger = bool(partial_kwargs.pop("start_from_trigger", False) or getattr(self.operator_class, "start_from_trigger", False))
+        start_trigger_args = partial_kwargs.pop("start_trigger_args", None) or getattr(self.operator_class, "start_trigger_args", None)
 
         try:
             operator_name = self.operator_class.custom_operator_name  # type: ignore
@@ -260,8 +262,8 @@ class OperatorPartial:
             # to BaseOperator.expand() contribute to operator arguments.
             expand_input_attr="expand_input",
             # TODO: Move these to task SDK's BaseOperator and remove getattr
-            start_trigger_args=getattr(self.operator_class, "start_trigger_args", None),
-            start_from_trigger=bool(getattr(self.operator_class, "start_from_trigger", False)),
+            start_trigger_args=start_trigger_args,
+            start_from_trigger=start_from_trigger,
         )
         return op
 
