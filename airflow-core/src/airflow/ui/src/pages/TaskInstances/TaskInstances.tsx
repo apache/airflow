bcf485012342861e12b4d644967a8383055e51bb
Jens Scheffler <95105677+jscheffl@users.noreply.github.com>
2025-09-23 17:00:04 +0200

    Reduce default columns of Dag Run and Task Instance lists (#55968)
    (cherry picked from commit 350b6f7db484037ef4beeb262ba3c1b72ac91d52)

--- airflow-core/src/airflow/ui/src/pages/TaskInstances/TaskInstances.tsx
+++ airflow-core/src/airflow/ui/src/pages/TaskInstances/TaskInstances.tsx
@@ -215,8 +215,11 @@ export const TaskInstances = () => {
   const [searchParams] = useSearchParams();
   const { setTableURLState, tableURLState } = useTableURLState({
     columnVisibility: {
+      dag_version: false,
+      end_date: false,
       executor: false,
       hostname: false,
+      pool: false,
       queue: false,
     },
   });
