cc9826fdd5fa401499c5dcd081c0d6b8923d021b
Kavya Katal <kavyakatal09@gmail.com>
2025-09-25 10:52:31 +0530

    UI DAGs list: sort “Last run” by run_after

--- airflow-core/src/airflow/api_fastapi/core_api/routes/ui/dags.py
+++ airflow-core/src/airflow/api_fastapi/core_api/routes/ui/dags.py
@@ -104,7 +104,12 @@ def get_dags(
             SortParam(
                 ["dag_id", "dag_display_name", "next_dagrun", "state", "start_date"],
                 DagModel,
-                {"last_run_state": DagRun.state, "last_run_start_date": DagRun.start_date},
+                {
+                    "last_run_state": DagRun.state,
+                    "start_date": func.coalesce(DagRun.run_after, DagRun.start_date),
+                    "last_run_start_date": func.coalesce(DagRun.run_after, DagRun.start_date),
+                    "last_run_after": DagRun.run_after,
+                },
             ).dynamic_depends()
         ),
     ],
