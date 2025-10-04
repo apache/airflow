ed410c3d5bd4f921ac78d4927a48d88bb2296ad6
Karun Poudel <poudel.karun@gmail.com>
2025-10-04 06:25:33 +0000

    try

--- dev/breeze/tests/test_selective_checks.py
+++ dev/breeze/tests/test_selective_checks.py
@@ -645,7 +645,7 @@ def assert_outputs_are_printed(expected_outputs: dict[str, str], stderr: str):
                 ),
                 {
                     "selected-providers-list-as-string": "amazon common.sql google "
-                    "openlineage pgvector postgres",
+                    "microsoft.azure openlineage pgvector postgres",
                     "all-python-versions": f"['{DEFAULT_PYTHON_MAJOR_MINOR_VERSION}']",
                     "all-python-versions-list-as-string": DEFAULT_PYTHON_MAJOR_MINOR_VERSION,
                     "python-versions": f"['{DEFAULT_PYTHON_MAJOR_MINOR_VERSION}']",
