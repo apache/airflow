59b7e5f4aa2e80833a5e1af4c6191e4c04282f05
Vivek Nanda <vnanda@cloudera.com>
2025-10-04 19:46:27 +0530

    Fixed formatting

--- airflow-core/src/airflow/ui/src/pages/Dag/Dag.tsx
+++ airflow-core/src/airflow/ui/src/pages/Dag/Dag.tsx
@@ -108,7 +108,9 @@ export const Dag = () => {
       <DetailsLayout error={error ?? runsError} isLoading={isLoading || isLoadingRuns} tabs={displayTabs}>
         <Header
           dag={dag}
-          isRefreshing={latestRun ? Boolean(isStatePending(latestRun.state) && Boolean(refetchInterval)) : false}
+          isRefreshing={
+            latestRun ? Boolean(isStatePending(latestRun.state) && Boolean(refetchInterval)) : false
+          }
           latestRunInfo={latestRun ?? undefined}
         />
       </DetailsLayout>
