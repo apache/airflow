e9b9c92e48a93aabedee78b78d0267f43ae7e969
Karthikeyan Singaravelan <tir.karthi@gmail.com>
2025-10-07 17:59:23 +0530

    Add name to MultiAssetDefinition to skip instance checks.

--- task-sdk/src/airflow/sdk/definitions/asset/decorators.py
+++ task-sdk/src/airflow/sdk/definitions/asset/decorators.py
@@ -57,12 +57,6 @@ class _AssetMainOperator(PythonOperator):
     @classmethod
     def from_definition(cls, definition: AssetDefinition | MultiAssetDefinition) -> Self:
         _validate_asset_function_arguments(definition._function)
-
-        if isinstance(definition, AssetDefinition):
-            definition_name = definition.name
-        else:
-            definition_name = definition._function.__name__
-
         return cls(
             task_id=definition._function.__name__,
             inlets=[
@@ -72,7 +66,7 @@ class _AssetMainOperator(PythonOperator):
             ],
             outlets=[v for _, v in definition.iter_assets()],
             python_callable=definition._function,
-            definition_name=definition_name,
+            definition_name=definition.name,
         )
 
     def _iter_kwargs(self, context: Mapping[str, Any]) -> Iterator[tuple[str, Any]]:
@@ -143,6 +137,7 @@ class MultiAssetDefinition(BaseAsset):
     :meta private:
     """
 
+    name: str
     _function: Callable
     _source: asset.multi
 
@@ -237,7 +232,7 @@ class asset(_DAGFactory):
                 raise ValueError("nested function not supported")
             if not self.outlets:
                 raise ValueError("no outlets provided")
-            return MultiAssetDefinition(function=f, source=self)
+            return MultiAssetDefinition(function=f, source=self, name=f.__name__)
 
     def __call__(self, f: Callable) -> AssetDefinition:
         if f.__name__ != f.__qualname__:
