62565d292953afa5f91be82e873167d2868c4746
Tyrell H <73722828+TyrellHaywood@users.noreply.github.com>
2025-10-08 14:15:28 -0400

    Add comprehensive async message tests

--- providers/amazon/tests/unit/amazon/aws/hooks/test_sqs.py
+++ providers/amazon/tests/unit/amazon/aws/hooks/test_sqs.py
@@ -28,7 +28,25 @@ QUEUE_NAME = "test-queue"
 QUEUE_URL = "https://sqs.region.amazonaws.com/123456789/test-queue"
 MESSAGE_BODY = "test message"
 
+DELAY = 5
+DEDUPE = "banana"
+MSG_ATTRIBUTES = {
+    "Author": {
+        "StringValue": "test-user",
+        "DataType": "String",
+    },
+    "Priority": {
+        "StringValue": "1",
+        "DataType": "Number",
+    },
+}
+
 MESSAGE_ID_KEY = "MessageId"
+
+SEND_MESSAGE_DEFAULTS = {
+    "DelaySeconds": 0,
+    "MessageAttributes": {},
+}
 
 
 @mock_aws
@@ -37,7 +55,7 @@ class TestSqsHook:
     def setup_test_queue(self):
         """Create a test queue before each test."""
         hook = SqsHook(aws_conn_id="aws_default")
-        self.queue_url = hook.get_conn().create_queue(QueueName=QUEUE_NAME)["QueueUrl"]
+        self.queue_url = hook.create_queue(queue_name=QUEUE_NAME)
         yield
 
     @pytest.fixture
@@ -52,7 +70,6 @@ class TestSqsHook:
         queue_name = "test-create-queue"
         queue_url = hook.create_queue(queue_name=queue_name)
 
-        assert queue_url is not None
         assert isinstance(queue_url, str)
         assert queue_name in queue_url
 
@@ -80,7 +97,6 @@ class TestSqsHook:
         """Test sending a message to a queue."""
         response = hook.send_message(queue_url=self.queue_url, message_body=MESSAGE_BODY)
 
-        assert response is not None
         assert isinstance(response, dict)
         assert MESSAGE_ID_KEY in response
         assert "MD5OfMessageBody" in response
@@ -163,7 +179,28 @@ class TestAsyncSqsHook:
         async_conn = await hook.get_async_conn()
         assert async_conn is mock_async_client
 
-    async def test_asend_message(self, hook, mock_get_async_conn, mock_async_client):
+    async def test_asend_message_minimal(self, hook, mock_get_async_conn, mock_async_client):
         response = await hook.asend_message(queue_url=QUEUE_URL, message_body=MESSAGE_BODY)
 
         assert MESSAGE_ID_KEY in response
+        mock_async_client.send_message.assert_called_once_with(
+            MessageBody=MESSAGE_BODY, QueueUrl=QUEUE_URL, **SEND_MESSAGE_DEFAULTS
+        )
+
+    async def test_asend_message_with_attributes(self, hook, mock_get_async_conn, mock_async_client):
+        response = await hook.asend_message(
+            queue_url=QUEUE_URL,
+            message_body=MESSAGE_BODY,
+            message_attributes=MSG_ATTRIBUTES,
+            delay_seconds=DELAY,
+            message_deduplication_id=DEDUPE,
+        )
+
+        assert MESSAGE_ID_KEY in response
+        mock_async_client.send_message.assert_called_once_with(
+            DelaySeconds=DELAY,
+            MessageBody=MESSAGE_BODY,
+            MessageAttributes=MSG_ATTRIBUTES,
+            QueueUrl=QUEUE_URL,
+            MessageDeduplicationId=DEDUPE,
+        )
