cea5e32d83d60762c86c3296bc7177ebb7dfc571
Bugra Ozturk <bugraoz93@users.noreply.github.com>
2025-10-10 01:06:25 +0200

    Bump uv version from 0.9.0 to 0.9.1 (#56512)

--- scripts/ci/install_breeze.sh
+++ scripts/ci/install_breeze.sh
@@ -22,7 +22,7 @@ cd "$( dirname "${BASH_SOURCE[0]}" )/../../"
 PYTHON_ARG=""
 
 PIP_VERSION="25.2"
-UV_VERSION="0.9.0"
+UV_VERSION="0.9.1"
 if [[ ${PYTHON_VERSION=} != "" ]]; then
     PYTHON_ARG="--python=$(which python"${PYTHON_VERSION}") "
 fi
