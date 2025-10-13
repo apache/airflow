1ad3376e62cf6305a79a9931e143e48f6ebdad91
Owen Leung <owen.leung2@gmail.com>
2025-10-13 22:34:04 +0800

    Remove debug lines

--- providers/elasticsearch/tests/conftest.py
+++ providers/elasticsearch/tests/conftest.py
@@ -57,10 +57,6 @@ def es_8_container_url() -> str:
             retry_on_timeout=True,
             max_retries=5,
         )
-        stats = client.cluster.stats()
-        indices = stats.get("indices", {})
-        print(f"stats: {stats})")
-        print(f"indices: {indices})")
         _wait_for_cluster_ready(client, timeout_s=120)
         _ensure_index(client, "airflow-logs", timeout_s=120)
         yield url
