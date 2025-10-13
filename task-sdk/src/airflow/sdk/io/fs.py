81c2bc4ca5669a876002861abfdfb792993c7755
Christos Bisias <christosbis@gmail.com>
2025-10-13 21:56:53 +0300

    fix task-sdk imports for backwards compat

--- task-sdk/src/airflow/sdk/io/fs.py
+++ task-sdk/src/airflow/sdk/io/fs.py
@@ -24,9 +24,13 @@ from typing import TYPE_CHECKING
 
 from fsspec.implementations.local import LocalFileSystem
 
-from airflow._shared.observability.stats import Stats
 from airflow.providers_manager import ProvidersManager
 from airflow.sdk.module_loading import import_string
+
+try:
+    from airflow.sdk._shared.observability.stats import Stats
+except ImportError:
+    from airflow.stats import Stats  # type: ignore[attr-defined,no-redef]
 
 if TYPE_CHECKING:
     from fsspec import AbstractFileSystem
