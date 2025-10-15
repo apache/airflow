341c6fbdc66b042a973122d019a788ed06c99e62
Sean Ghaeli <seanghaeli@gmail.com>
2025-10-15 08:03:43 -0700

    Declare imports in init file

--- providers/http/src/airflow/providers/http/notifications/__init__.py
+++ providers/http/src/airflow/providers/http/notifications/__init__.py
@@ -15,3 +15,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+
+from airflow.providers.http.notifications.http import HttpNotifier
+
+__all__ = ["HttpNotifier"]
