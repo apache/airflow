04c5e23cc6a05b1aaf7c62b6ee1678328ccfaf16
Ephraim Anierobi <splendidzigy24@gmail.com>
2025-10-16 10:56:59 +0100

    fixup! Use RuntimeError instead of AirflowNotFoundException for connections

--- task-sdk/tests/task_sdk/definitions/test_param.py
+++ task-sdk/tests/task_sdk/definitions/test_param.py
@@ -21,8 +21,8 @@ from contextlib import nullcontext
 import pytest
 
 from airflow.sdk.definitions.param import Param, ParamsDict
-from airflow.serialization.definitions.param import SerializedParam
 from airflow.sdk.exceptions import ParamValidationError
+from airflow.serialization.definitions.param import SerializedParam
 from airflow.serialization.serialized_objects import BaseSerialization
 
 
