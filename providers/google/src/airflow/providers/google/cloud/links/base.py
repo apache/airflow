65959c158ed7ffb3e2dde32cef458acf3f633f3f
Kaxil Naik <kaxilnaik@gmail.com>
2025-10-18 18:18:49 +0100

    Simplify version-specific imports in the Google provider (#56793)
    Simplify version-specific imports in the Google provider by using lazy_compat from common.compat provider added in https://github.com/apache/airflow/pull/56790
    for components that are available since Airflow 3.0.

--- providers/google/src/airflow/providers/google/cloud/links/base.py
+++ providers/google/src/airflow/providers/google/cloud/links/base.py
@@ -20,17 +20,8 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, ClassVar
 from urllib.parse import urlparse
 
-from airflow.providers.google.version_compat import (
-    AIRFLOW_V_3_0_PLUS,
-    BaseOperator,
-    BaseOperatorLink,
-    BaseSensorOperator,
-)
-
-if AIRFLOW_V_3_0_PLUS:
-    from airflow.sdk.execution_time.xcom import XCom
-else:
-    from airflow.models.xcom import XCom  # type: ignore[no-redef]
+from airflow.providers.common.compat.lazy_compat import BaseOperatorLink, BaseSensorOperator, XCom
+from airflow.providers.google.version_compat import AIRFLOW_V_3_0_PLUS, BaseOperator
 
 if TYPE_CHECKING:
     from airflow.models.taskinstancekey import TaskInstanceKey
