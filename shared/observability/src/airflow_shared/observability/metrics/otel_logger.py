acee6dd121d88a03a93b77c09f9c770a81a842a5
Christos Bisias <christosbis@gmail.com>
2025-10-19 15:25:24 +0300

    fix incompatible type mypy error

--- shared/observability/src/airflow_shared/observability/metrics/otel_logger.py
+++ shared/observability/src/airflow_shared/observability/metrics/otel_logger.py
@@ -399,6 +399,9 @@ def get_otel_logger(
     # https://opentelemetry.io/docs/specs/otel/configuration/sdk-environment-variables/#periodic-exporting-metricreader
     if interval := os.environ.get("OTEL_METRIC_EXPORT_INTERVAL", conf_interval):
         interval = float(interval)
+    else:
+        # If the env variable is an empty string.
+        interval = None
     log.info("[Metric Exporter] Connecting to OpenTelemetry Collector at %s", endpoint)
     readers = [
         PeriodicExportingMetricReader(
