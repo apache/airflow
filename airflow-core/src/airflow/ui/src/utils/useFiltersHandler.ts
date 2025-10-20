ed7a5901ba5bde84442d2e07cf487622af57f88e
seanyu <seansean988@gmail.com>
2025-10-20 20:53:03 +0800

    delete TASK_STATE

--- airflow-core/src/airflow/ui/src/utils/useFiltersHandler.ts
+++ airflow-core/src/airflow/ui/src/utils/useFiltersHandler.ts
@@ -55,7 +55,6 @@ export type FilterableSearchParamsKeys =
   | SearchParamsKeys.SUBJECT_SEARCH
   | SearchParamsKeys.TASK_ID
   | SearchParamsKeys.TASK_ID_PATTERN
-  | SearchParamsKeys.TASK_STATE
   | SearchParamsKeys.TRIGGERING_USER_NAME_PATTERN
   | SearchParamsKeys.TRY_NUMBER
   | SearchParamsKeys.USER;
