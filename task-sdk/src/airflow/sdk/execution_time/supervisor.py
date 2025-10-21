fdf1c7a436dd372b0f92b55e12b80652e220f55d
gopidesupavan <gopidesupavan@gmail.com>
2025-10-21 15:48:39 +0100

    Add GetDagState endpoint to execution_api

--- task-sdk/src/airflow/sdk/execution_time/supervisor.py
+++ task-sdk/src/airflow/sdk/execution_time/supervisor.py
@@ -79,6 +79,7 @@ from airflow.sdk.execution_time.comms import (
     GetConnection,
     GetDagRun,
     GetDagRunState,
+    GetDagState,
     GetDRCount,
     GetPreviousDagRun,
     GetPreviousTI,
@@ -1459,6 +1460,10 @@ class ActivitySubprocess(WatchedSubprocess):
             dump_opts = {"exclude_unset": True}
         elif isinstance(msg, MaskSecret):
             mask_secret(msg.value, msg.name)
+        elif isinstance(msg, GetDagState):
+            resp = self.client.dags.get_state(
+                dag_id=msg.dag_id,
+            )
         else:
             log.error("Unhandled request", msg=msg)
             self.send_msg(
