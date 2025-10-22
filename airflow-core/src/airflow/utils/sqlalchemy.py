6f19abbe79bd3873f517c3fcb7a62650d3966c47
Lzzz666 <zhenyuliu0825@gmail.com>
2025-10-23 01:07:38 +0800

    Fix: Fix MyPy type errors in manager.py

--- airflow-core/src/airflow/utils/sqlalchemy.py
+++ airflow-core/src/airflow/utils/sqlalchemy.py
@@ -41,6 +41,7 @@ if TYPE_CHECKING:
     from kubernetes.client.models.v1_pod import V1Pod
     from sqlalchemy.exc import OperationalError
     from sqlalchemy.orm import Query, Session
+    from sqlalchemy.sql import Select
     from sqlalchemy.types import TypeEngine
 
     from airflow.typing_compat import Self
@@ -328,14 +329,14 @@ USE_ROW_LEVEL_LOCKING: bool = conf.getboolean("scheduler", "use_row_level_lockin
 
 
 def with_row_locks(
-    query: Query,
+    query: Query[Any] | Select[Any],
     session: Session,
     *,
     nowait: bool = False,
     skip_locked: bool = False,
     key_share: bool = True,
     **kwargs,
-) -> Query:
+) -> Query[Any] | Select[Any]:
     """
     Apply with_for_update to the SQLAlchemy query if row level locking is in use.
 
