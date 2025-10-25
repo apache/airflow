e31f43243b1c1606f24f3f25395eac8f57ab0073
Yun-Ting Chiu <chiuting6@gmail.com>
2025-10-25 22:54:05 +0800

    Move auth dep to provider security and fixtureize dependency_overrides

--- providers/fab/src/airflow/providers/fab/auth_manager/api_fastapi/routes/roles.py
+++ providers/fab/src/airflow/providers/fab/auth_manager/api_fastapi/routes/roles.py
@@ -16,43 +16,33 @@
 # under the License.
 from __future__ import annotations
 
-from fastapi import Depends
-from starlette import status
-from starlette.exceptions import HTTPException
+from typing import TYPE_CHECKING
 
-from airflow.api_fastapi.app import get_auth_manager
+from fastapi import Depends, status
+
 from airflow.api_fastapi.common.router import AirflowRouter
 from airflow.api_fastapi.core_api.openapi.exceptions import create_openapi_http_exception_doc
-from airflow.api_fastapi.core_api.security import get_user
 from airflow.providers.fab.auth_manager.api_fastapi.datamodels.roles import RoleBody, RoleResponse
+from airflow.providers.fab.auth_manager.api_fastapi.security import requires_fab_custom_view
 from airflow.providers.fab.auth_manager.api_fastapi.services.roles import FABAuthManagerRoles
 from airflow.providers.fab.auth_manager.cli_commands.utils import get_application_builder
 from airflow.providers.fab.www.security import permissions
 
+if TYPE_CHECKING:
+    from airflow.providers.fab.auth_manager.api_fastapi.datamodels.roles import RoleBody, RoleResponse
+
+
 roles_router = AirflowRouter(prefix="/fab/v1", tags=["FabAuthManager"])
-
-
-def requires_fab_custom_view(method: str, resource_name: str):
-    def _check(user=Depends(get_user)):
-        if not get_auth_manager().is_authorized_custom_view(
-            method=method, resource_name=resource_name, user=user
-        ):
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Forbidden")
-
-    return _check
 
 
 @roles_router.post(
     "/roles",
-    response_model=RoleResponse,
-    status_code=status.HTTP_200_OK,
     responses=create_openapi_http_exception_doc(
         [
             status.HTTP_400_BAD_REQUEST,
             status.HTTP_401_UNAUTHORIZED,
             status.HTTP_403_FORBIDDEN,
             status.HTTP_409_CONFLICT,
-            status.HTTP_422_UNPROCESSABLE_CONTENT,
             status.HTTP_500_INTERNAL_SERVER_ERROR,
         ]
     ),
