8e2c8c88e60b04fa37f688993e564160cb996a3f
manipatnam <manideeppatnam123@gmail.com>
2025-10-26 22:56:47 +0530

    Add debug logging for kubeconfig serialization in AsyncKubernetesHook

--- providers/cncf/kubernetes/src/airflow/providers/cncf/kubernetes/hooks/kubernetes.py
+++ providers/cncf/kubernetes/src/airflow/providers/cncf/kubernetes/hooks/kubernetes.py
@@ -831,6 +831,7 @@ class AsyncKubernetesHook(KubernetesHook):
                     "object and writing temporary config file with its content",
                 )
                 if isinstance(kubeconfig, dict):
+                    self.log.debug(LOADING_KUBE_CONFIG_FILE_RESOURCE.format("connection kube_config dictionary (serializing)"))
                     kubeconfig = json.dumps(kubeconfig)
                 await temp_config.write(kubeconfig.encode())
                 await temp_config.flush()
