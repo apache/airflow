1cfdebfa47e21cdb4be755eee44141f1e89715a0
Lolhcs-dot <lolhcs@mail.ru>
2025-10-27 15:23:30 +0300

    new dag

--- dev/lolhcs_test/lolhcs-dot.py
+++ dev/lolhcs_test/lolhcs-dot.py
@@ -1,11 +1,12 @@
-from airflow import DAG
-from airflow.utils.dates import days_ago
-import DummyOperator
 import logging
 
-from airflow.sensors.time_delta import TimeDeltaSensor
+import DummyOperator
+from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python_operator import PythonOperator
+from airflow.sensors.time_delta import TimeDeltaSensor
+from airflow.utils.dates import days_ago
+
 DEFAULT_ARGS = {
     'start_date': days_ago(2),
     'owner': 'ds',
@@ -13,11 +14,11 @@ DEFAULT_ARGS = {
 }
 
 with DAG("ds_test",
-    schedule_interval='@daily',
-    default_args=DEFAULT_ARGS,
-    max_active_runs=1,
-    tags=['ds']
-    ) as dag:
+         schedule_interval='@daily',
+         default_args=DEFAULT_ARGS,
+         max_active_runs=1,
+         tags=['ds']
+         ) as dag:
 
 dummy = DummyOperator(task_id="dummy")
 
@@ -27,13 +28,15 @@ echo_ds = BashOperator(
     dag=dag
 )
 
+
 def hello_world_func():
     logging.info("Hello World!")
+
 
 hello_world = PythonOperator(
     task_id="hello_world",
     python_callable=hello_world_func,
     dag=dag
 )
-    }
-    dummy >> [echo_ds, hello_world]
+}
+dummy >> [echo_ds, hello_world]
