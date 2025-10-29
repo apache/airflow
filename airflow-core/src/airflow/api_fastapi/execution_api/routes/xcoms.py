3466f943de0656687eb9b38640437d3e9324eb35
David Blain <info@dabla.be>
2025-10-29 08:19:12 +0100

    Update airflow-core/src/airflow/api_fastapi/execution_api/routes/xcoms.py
    Co-authored-by: Tzu-ping Chung <uranusjr@gmail.com>

--- airflow-core/src/airflow/api_fastapi/execution_api/routes/xcoms.py
+++ airflow-core/src/airflow/api_fastapi/execution_api/routes/xcoms.py
@@ -398,7 +398,7 @@ def set_xcom(
             session=session,
         )
     except ValueError as e:
-        raise HTTPException(status.HTTP_404_NOT_FOUND, f"{e}")
+        raise HTTPException(status.HTTP_404_NOT_FOUND, str(e))
     except TypeError as e:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
