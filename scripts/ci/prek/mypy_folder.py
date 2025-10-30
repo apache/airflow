9665bc9c43cbe971e60e386b1076e15ed4e92546
sanju-hub <saiganeshgurram66@gmail.com>
2025-10-30 08:13:43 +0530

    Address review: Use built-in print as fallback

--- scripts/ci/prek/mypy_folder.py
+++ scripts/ci/prek/mypy_folder.py
@@ -118,6 +118,8 @@ def get_all_files(folder: str) -> list[str]:
     file_spec = "@/files/mypy_files.txt"
     if console:
         console.print(f"[info]Running mypy with {file_spec}")
+    else:
+        print(f"info: Running mypy with {file_spec}")
     return files_to_check
 
 
@@ -133,21 +135,36 @@ if console:
         console.print(all_files_to_check)
     else:
         console.print(f"[info]You cand check the list of files in:[/] {MYPY_FILE_LIST}")
+else:
+    if os.environ.get("CI"):
+        print("info: The content of the file is:")
+        print(all_files_to_check)
+    else:
+        print(f"info: You cand check the list of files in: {MYPY_FILE_LIST}")
 
 print(f"Running mypy with {FILE_ARGUMENT}")
 
 mypy_cmd_parts = [f"TERM=ansi mypy {shlex.quote(FILE_ARGUMENT)}"]
 
 if show_unused_warnings == "true":
-    console.print(
-        "[info]Running mypy with --warn-unused-ignores to display unused ignores, unset environment variable: SHOW_UNUSED_MYPY_WARNINGS to runoff this behaviour"
-    )
+    if console:
+        console.print(
+            "[info]Running mypy with --warn-unused-ignores to display unused ignores, unset environment variable: SHOW_UNUSED_MYPY_WARNINGS to runoff this behaviour"
+        )
+    else:
+        print(
+            "info: Running mypy with --warn-unused-ignores to display unused ignores, unset environment variable: SHOW_UNUSED_MYPY_WARNINGS to runoff this behaviour"
+        )
     mypy_cmd_parts.append("--warn-unused-ignores")
-
 if show_unreachable_warnings == "true":
-    console.print(
-        "[info]Running mypy with --warn-unreachable to display unreachable code, unset environment variable: SHOW_UNREACHABLE_MYPY_WARNINGS to runoff this behaviour"
-    )
+    if console:
+        console.print(
+            "[info]Running mypy with --warn-unreachable to display unreachable code, unset environment variable: SHOW_UNREACHABLE_MYPY_WARNINGS to runoff this behaviour"
+        )
+    else:
+        print(
+            "info: Running mypy with --warn-unreachable to display unreachable code, unset environment variable: SHOW_UNREACHABLE_MYPY_WARNINGS to runoff this behaviour"
+        )
     mypy_cmd_parts.append("--warn-unreachable")
 
 mypy_cmd = " ".join(mypy_cmd_parts)
@@ -166,23 +183,43 @@ res = run_command_via_breeze_shell(
     },
 )
 ci_environment = os.environ.get("CI")
-if res.returncode != 0 and console:
-    if ci_environment:
+if res.returncode != 0:
+    if console:
+        if ci_environment:
+            console.print(
+                "[yellow]You are running mypy with the folders selected. If you want to "
+                "reproduce it locally, you need to run the following command:\n"
+            )
+            console.print("prek --hook-stage manual mypy-<folder> --all-files\n")
+        upgrading = os.environ.get("UPGRADE_TO_NEWER_DEPENDENCIES", "false") != "false"
+        if upgrading:
+            console.print(
+                "[yellow]You are running mypy with the image that has dependencies upgraded automatically.\n"
+            )
+        flag = " --upgrade-to-newer-dependencies" if upgrading else ""
         console.print(
-            "[yellow]You are running mypy with the folders selected. If you want to "
-            "reproduce it locally, you need to run the following command:\n"
+            "[yellow]If you see strange stacktraces above, and can't reproduce it, please run"
+            " this command and try again:\n"
         )
-        console.print("prek --hook-stage manual mypy-<folder> --all-files\n")
-    upgrading = os.environ.get("UPGRADE_TO_NEWER_DEPENDENCIES", "false") != "false"
-    if upgrading:
+        console.print(f"breeze ci-image build --python 3.10{flag}\n")
         console.print(
-            "[yellow]You are running mypy with the image that has dependencies upgraded automatically.\n"
+            "[yellow]You can also run `breeze down --cleanup-mypy-cache` to clean up the cache used.\n"
         )
-    flag = " --upgrade-to-newer-dependencies" if upgrading else ""
-    console.print(
-        "[yellow]If you see strange stacktraces above, and can't reproduce it, please run"
-        " this command and try again:\n"
-    )
-    console.print(f"breeze ci-image build --python 3.10{flag}\n")
-    console.print("[yellow]You can also run `breeze down --cleanup-mypy-cache` to clean up the cache used.\n")
+    else:
+        if ci_environment:
+            print(
+                "You are running mypy with the folders selected. If you want to "
+                "reproduce it locally, you need to run the following command:\n"
+            )
+            print("prek --hook-stage manual mypy-<folder> --all-files\n")
+        upgrading = os.environ.get("UPGRADE_TO_NEWER_DEPENDENCIES", "false") != "false"
+        if upgrading:
+            print("You are running mypy with the image that has dependencies upgraded automatically.\n")
+        flag = " --upgrade-to-newer-dependencies" if upgrading else ""
+        print(
+            "If you see strange stacktraces above, and can't reproduce it, please run"
+            " this command and try again:\n"
+        )
+        print(f"breeze ci-image build --python 3.10{flag}\n")
+        print("You can also run `breeze down --cleanup-mypy-cache` to clean up the cache used.\n")
 sys.exit(res.returncode)
