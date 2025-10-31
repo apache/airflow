ed17d660d53fd0d88c7a8b7f15885233814392ba
Anusha Kovi <parvathi.kovi@gmail.com>
2025-10-31 12:24:57 -0700

    resolve conflicts

--- airflow-core/src/airflow/api_fastapi/common/db/dag_runs.py
+++ airflow-core/src/airflow/api_fastapi/common/db/dag_runs.py
@@ -20,10 +20,15 @@ from __future__ import annotations
 from typing import cast
 
 from sqlalchemy import func, select
+from sqlalchemy.orm import joinedload, selectinload
+from sqlalchemy.orm.interfaces import LoaderOption
 from sqlalchemy.sql import ColumnElement
 
 from airflow.models.dag import DagModel
+from airflow.models.dag_version import DagVersion
 from airflow.models.dagrun import DagRun
+from airflow.models.taskinstance import TaskInstance
+from airflow.models.taskinstancehistory import TaskInstanceHistory
 
 dagruns_select_with_state_count = (
     select(
@@ -36,3 +41,17 @@ dagruns_select_with_state_count = (
     .group_by(DagRun.dag_id, DagRun.state, DagModel.dag_display_name)
     .order_by(DagRun.dag_id)
 )
+
+
+def eager_load_dag_run_for_validation() -> tuple[LoaderOption, ...]:
+    """Construct the eager loading options necessary for a DagRunResponse object."""
+    return (
+        joinedload(DagRun.dag_model),
+        selectinload(DagRun.task_instances)
+        .joinedload(TaskInstance.dag_version)
+        .joinedload(DagVersion.bundle),
+        selectinload(DagRun.task_instances_histories)
+        .joinedload(TaskInstanceHistory.dag_version)
+        .joinedload(DagVersion.bundle),
+        joinedload(DagRun.dag_run_note),
+    )
