166b65706702e6a5586425345b621e2a8df9699e
Itay Adler <itayadler13@gmail.com>
2025-11-09 17:26:19 +0200

    feat: adding new feature to mark task group as success or failed

---
+++ airflow-core/src/airflow/ui/src/components/MarkAs/TaskGroupInstance/MarkTaskGroupInstanceAsDialog.tsx
@@ -0,0 +1,244 @@
+/*!
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+import { Flex, Heading, VStack } from "@chakra-ui/react";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+import { useParams } from "react-router-dom";
+import { useQueryClient } from "@tanstack/react-query";
+
+import {
+  useTaskInstanceServiceGetTaskInstances,
+  useTaskInstanceServiceBulkTaskInstances,
+  useTaskInstanceServiceGetTaskInstancesKey,
+  UseGridServiceGetGridRunsKeyFn,
+  UseGridServiceGetGridTiSummariesKeyFn,
+  useGridServiceGetGridTiSummariesKey,
+} from "openapi/queries";
+import type {
+  LightGridTaskInstanceSummary,
+  TaskInstanceState,
+  BulkBody_BulkTaskInstanceBody_,
+  BulkUpdateAction_BulkTaskInstanceBody_,
+  TaskInstanceCollectionResponse,
+} from "openapi/requests/types.gen";
+import { ActionAccordion } from "src/components/ActionAccordion";
+import { StateBadge } from "src/components/StateBadge";
+import { Button, Dialog, toaster } from "src/components/ui";
+import SegmentedControl from "src/components/ui/SegmentedControl";
+import { useBulkTaskInstances } from "src/queries/useBulkTaskInstances";
+import { usePatchTaskInstanceDryRun } from "src/queries/usePatchTaskInstanceDryRun";
+import { useBulkTaskInstancesDryRun } from "src/queries/useBulkTaskInstancesDryRun";
+
+type Props = {
+  readonly groupTaskInstance: LightGridTaskInstanceSummary;
+  readonly onClose: () => void;
+  readonly open: boolean;
+  readonly state: TaskInstanceState;
+};
+
+const MarkTaskGroupInstanceAsDialog = ({ groupTaskInstance, onClose, open, state }: Props) => {
+  const { t: translate } = useTranslation();
+  const { dagId = "", runId = "" } = useParams();
+  const groupId = groupTaskInstance.task_id;
+  const queryClient = useQueryClient();
+
+  const [selectedOptions, setSelectedOptions] = useState<Array<string>>([]);
+
+  const past = selectedOptions.includes("past");
+  const future = selectedOptions.includes("future");
+  const upstream = selectedOptions.includes("upstream");
+  const downstream = selectedOptions.includes("downstream");
+
+  const [note, setNote] = useState<string>("");
+
+  // Get all task instances in the group
+  const { data: groupTaskInstances } = useTaskInstanceServiceGetTaskInstances(
+    {
+      dagId,
+      dagRunId: runId,
+      taskDisplayNamePattern: groupId,
+    },
+    undefined,
+    {
+      enabled: open,
+    },
+  );
+
+  const groupTaskIds = groupTaskInstances?.task_instances.map((ti) => ti.task_id) ?? [];
+
+  // Build bulk update request
+  const { mutate, isPending } = useBulkTaskInstances({
+    dagId,
+    runId,
+    onSuccess: onClose,
+  });
+  const { data, isPending: isPendingDryRun } = useBulkTaskInstancesDryRun({
+    dagId,
+    dagRunId: runId,
+    options: {
+      enabled: open,
+      refetchOnMount: "always",
+    },
+    requestBody: {
+      actions: [
+        {
+          action: "update",
+          entities: groupTaskIds.map((taskId) => ({
+            task_id: taskId,
+            map_index: null,
+            new_state: state,
+            note: note || null,
+            include_upstream: upstream,
+            include_downstream: downstream,
+            include_future: future,
+            include_past: past,
+          })),
+        },
+      ]
+    },
+  });
+
+  // const taskInstances = [
+  //   ...(data?.update?.errors ?? []),
+  //   ...(data?.update?.success ?? [])
+  // ]
+
+  // const affectedTasks = data ? {
+  //     task_instances: taskInstances,
+  //     total_entries: taskInstances.length
+  //   } as TaskInstanceCollectionResponse : {
+  //   task_instances: [],
+  //   total_entries: 0,
+  //   // create: [],
+  //   // delete: [],
+  //   // update: []
+  // };
+
+  const affectedTasks = data ?? {
+    task_instances: [],
+    total_entries: 0,
+  };
+
+  const handleConfirm = () => {
+    // if (groupTaskIds.length === 0) {
+    //   toaster.create({
+    //     title: translate("toaster.update.error", {
+    //       resourceName: translate("taskGroup"),
+    //     }),
+    //     description: translate("dags:runAndTaskActions.markAs.noTasks"),
+    //     type: "error",
+    //   });
+    //   return;
+    // }
+
+    const bulkBody: BulkBody_BulkTaskInstanceBody_ = {
+      actions: [
+        {
+          action: "update",
+          entities: groupTaskIds.map((taskId) => ({
+            task_id: taskId,
+            map_index: null,
+            new_state: state,
+            note: note || null,
+            include_upstream: upstream,
+            include_downstream: downstream,
+            include_future: future,
+            include_past: past,
+          })),
+        },
+      ],
+    };
+
+    mutate({
+      dagId,
+      dagRunId: runId,
+      requestBody: bulkBody,
+    });
+  };
+
+  console.log(affectedTasks);
+
+  return (
+    <Dialog.Root lazyMount open={open} size="xl">
+      <Dialog.Content backdrop>
+        <Dialog.Header>
+          <VStack align="start" gap={4}>
+            <Heading size="xl">
+              <strong>
+                {translate("dags:runAndTaskActions.markAs.title", {
+                  state,
+                  type: translate("taskGroup"),
+                })}
+                :
+              </strong>{" "}
+              {groupId} <StateBadge state={state} />
+            </Heading>
+          </VStack>
+        </Dialog.Header>
+
+        <Dialog.CloseTrigger />
+
+        <Dialog.Body width="full">
+          <Flex justifyContent="center">
+            <SegmentedControl
+              defaultValues={["downstream"]}
+              multiple
+              onChange={setSelectedOptions}
+              options={[
+                {
+                  label: translate("dags:runAndTaskActions.options.past"),
+                  value: "past",
+                },
+                {
+                  label: translate("dags:runAndTaskActions.options.future"),
+                  value: "future",
+                },
+                {
+                  label: translate("dags:runAndTaskActions.options.upstream"),
+                  value: "upstream",
+                },
+                {
+                  label: translate("dags:runAndTaskActions.options.downstream"),
+                  value: "downstream",
+                },
+              ]}
+            />
+          </Flex>
+          <ActionAccordion
+            affectedTasks={affectedTasks}
+            note={note}
+            setNote={setNote}
+          />
+          <Flex justifyContent="end" mt={3}>
+            <Button
+              colorPalette="brand"
+              disabled={affectedTasks.total_entries === 0}
+              loading={isPending || isPendingDryRun}
+              onClick={handleConfirm}
+            >
+              {translate("modal.confirm")}
+            </Button>
+          </Flex>
+        </Dialog.Body>
+      </Dialog.Content>
+    </Dialog.Root>
+  );
+};
+
+export default MarkTaskGroupInstanceAsDialog;
