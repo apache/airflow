0f3c7e887cd9c69f71cd1b6db5b77ef141d14d77
David Blain <info@dabla.be>
2025-11-14 08:32:17 +0100

    refactor: Fixed typing of failed_trigger in TriggerRunner

--- airflow-core/src/airflow/jobs/triggerer_job_runner.py
+++ airflow-core/src/airflow/jobs/triggerer_job_runner.py
@@ -868,7 +868,7 @@ class TriggerRunner:
     events: PartitionedQueue[int, events.TriggerEvent]
 
     # Outbound queue of failed triggers
-    failed_triggers: KeyedHeadQueue[int, tuple[int, TriggerDetails | None, BaseException | None]]
+    failed_triggers: KeyedHeadQueue[int, tuple[int, BaseTrigger | None, BaseException | None]]
 
     # Should-we-stop flag
     # TODO: set this in a sig-int handler
