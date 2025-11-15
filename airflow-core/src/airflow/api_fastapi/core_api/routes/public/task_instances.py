d322545b717a31f6f6096aff1233f22ac8af1e4b
Itay Adler <itayadler13@gmail.com>
2025-11-15 15:07:53 +0200

    fix: removing the dry run function

--- airflow-core/src/airflow/api_fastapi/core_api/routes/public/task_instances.py
+++ airflow-core/src/airflow/api_fastapi/core_api/routes/public/task_instances.py
@@ -1017,7 +1017,7 @@ def patch_task_instance(
                 task_instance_body=bulk_ti_body,
                 data=data,
                 session=session,
-                commit=True
+                commit=True,
             )
 
         elif key == "note":
