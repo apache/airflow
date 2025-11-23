31dc2caf5fe270ad4410a518d8ce1c9004688450
Jens Scheffler <j_scheffler@gmx.de>
2025-11-23 20:35:18 +0100

    Remove global from task instance session

--- providers/standard/tests/unit/standard/decorators/test_python.py
+++ providers/standard/tests/unit/standard/decorators/test_python.py
@@ -25,7 +25,6 @@ import pytest
 from airflow.exceptions import AirflowException, XComNotFound
 from airflow.models.taskinstance import TaskInstance
 from airflow.models.taskmap import TaskMap
-from airflow.utils.task_instance_session import set_current_task_instance_session
 
 from tests_common.test_utils.version_compat import (
     AIRFLOW_V_3_0_1,
@@ -822,36 +821,35 @@ def test_mapped_render_template_fields(dag_maker, session):
     @task_decorator
     def fn(arg1, arg2): ...
 
-    with set_current_task_instance_session(session=session):
-        with dag_maker(session=session):
-            task1 = BaseOperator(task_id="op1")
-            mapped = fn.partial(arg2="{{ ti.task_id }}").expand(arg1=task1.output)
+    with dag_maker(session=session):
+        task1 = BaseOperator(task_id="op1")
+        mapped = fn.partial(arg2="{{ ti.task_id }}").expand(arg1=task1.output)
 
-        dr = dag_maker.create_dagrun()
-        ti: TaskInstance = dr.get_task_instance(task1.task_id, session=session)
+    dr = dag_maker.create_dagrun()
+    ti: TaskInstance = dr.get_task_instance(task1.task_id, session=session)
 
-        ti.xcom_push(key=XCOM_RETURN_KEY, value=["{{ ds }}"], session=session)
+    ti.xcom_push(key=XCOM_RETURN_KEY, value=["{{ ds }}"], session=session)
 
-        session.add(
-            TaskMap(
-                dag_id=dr.dag_id,
-                task_id=task1.task_id,
-                run_id=dr.run_id,
-                map_index=-1,
-                length=1,
-                keys=None,
-            )
+    session.add(
+        TaskMap(
+            dag_id=dr.dag_id,
+            task_id=task1.task_id,
+            run_id=dr.run_id,
+            map_index=-1,
+            length=1,
+            keys=None,
         )
-        session.flush()
+    )
+    session.flush()
 
-        mapped_ti: TaskInstance = dr.get_task_instance(mapped.operator.task_id, session=session)
-        mapped_ti.map_index = 0
-        assert isinstance(mapped_ti.task, MappedOperator)
-        mapped.operator.render_template_fields(context=mapped_ti.get_template_context(session=session))
-        assert isinstance(mapped_ti.task, BaseOperator)
+    mapped_ti: TaskInstance = dr.get_task_instance(mapped.operator.task_id, session=session)
+    mapped_ti.map_index = 0
+    assert isinstance(mapped_ti.task, MappedOperator)
+    mapped.operator.render_template_fields(context=mapped_ti.get_template_context(session=session))
+    assert isinstance(mapped_ti.task, BaseOperator)
 
-        assert mapped_ti.task.op_kwargs["arg1"] == "{{ ds }}"
-        assert mapped_ti.task.op_kwargs["arg2"] == "fn"
+    assert mapped_ti.task.op_kwargs["arg1"] == "{{ ds }}"
+    assert mapped_ti.task.op_kwargs["arg2"] == "fn"
 
 
 def test_task_decorator_has_wrapped_attr():
