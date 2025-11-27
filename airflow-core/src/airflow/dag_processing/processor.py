2fec150dfbea11998f6b2e6d802b45fe54fbc9e8
Christos Bisias <christosbis@gmail.com>
2025-11-27 09:23:32 +0200

    fix ruff error

--- airflow-core/src/airflow/dag_processing/processor.py
+++ airflow-core/src/airflow/dag_processing/processor.py
@@ -36,8 +36,8 @@ from airflow.callbacks.callback_requests import (
 )
 from airflow.configuration import conf
 from airflow.dag_processing.dagbag import DagBag
-from airflow.sdk.exceptions import TaskNotFound
 from airflow.observability.stats import Stats
+from airflow.sdk.exceptions import TaskNotFound
 from airflow.sdk.execution_time.comms import (
     ConnectionResult,
     DeleteVariable,
