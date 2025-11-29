30ce94089ed29a7adbe7809dc95542efc4f456af
Christos Bisias <christosbis@gmail.com>
2025-11-29 17:44:15 +0200

    fix mypy providers error

--- providers/celery/src/airflow/providers/celery/executors/celery_executor_utils.py
+++ providers/celery/src/airflow/providers/celery/executors/celery_executor_utils.py
@@ -50,10 +50,8 @@ from airflow.utils.net import get_hostname
 from airflow.utils.providers_configuration_loader import providers_configuration_loaded
 
 try:
-    from airflow.observability.stats import Stats
     from airflow.sdk.definitions._internal.dag_parsing_context import _airflow_parsing_context_manager
 except ImportError:
-    from airflow.stats import Stats  # type: ignore[attr-defined,no-redef]
     from airflow.utils.dag_parsing_context import _airflow_parsing_context_manager
 
 log = logging.getLogger(__name__)
