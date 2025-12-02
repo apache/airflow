faf5e83a998e2b63720ee321c4766ede0c7bb746
Ephraim Anierobi <splendidzigy24@gmail.com>
2025-12-02 13:49:59 +0100

    Fix depreccated DagBag import from models.dagbag
    DagBag was moved to dag_processing.dagbag so this import in models
    init should be updated to fix the deprecation that occurs when we
    startup airflow

--- airflow-core/src/airflow/models/__init__.py
+++ airflow-core/src/airflow/models/__init__.py
@@ -99,7 +99,7 @@ __lazy_imports = {
     "BaseXCom": "airflow.sdk.bases.xcom",
     "Callback": "airflow.models.callback",
     "Connection": "airflow.models.connection",
-    "DagBag": "airflow.models.dagbag",
+    "DagBag": "airflow.dag_processing.dagbag",
     "DagModel": "airflow.models.dag",
     "DagRun": "airflow.models.dagrun",
     "DagTag": "airflow.models.dag",
@@ -125,11 +125,11 @@ __lazy_imports = {
 if TYPE_CHECKING:
     # I was unable to get mypy to respect a airflow/models/__init__.pyi, so
     # having to resort back to this hacky method
+    from airflow.dag_processing.dagbag import DagBag
     from airflow.models.base import ID_LEN, Base
     from airflow.models.callback import Callback
     from airflow.models.connection import Connection
     from airflow.models.dag import DagModel, DagTag
-    from airflow.models.dagbag import DagBag
     from airflow.models.dagrun import DagRun
     from airflow.models.dagwarning import DagWarning
     from airflow.models.db_callback_request import DbCallbackRequest
