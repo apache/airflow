b67575f0c7262d9f764f3225fa382a4ad81e80ec
Christos Bisias <christosbis@gmail.com>
2025-12-03 18:50:05 +0200

    fix mypy errors

--- task-sdk/src/airflow/sdk/observability/metrics/statsd_logger.py
+++ task-sdk/src/airflow/sdk/observability/metrics/statsd_logger.py
@@ -49,7 +49,7 @@ def get_statsd_logger(cls) -> SafeStatsdLogger:
 
     return statsd_logger.get_statsd_logger(
         cls,
-        stats_class,
+        stats_class=stats_class,
         host=conf.get("metrics", "statsd_host"),
         port=conf.getint("metrics", "statsd_port"),
         prefix=conf.get("metrics", "statsd_prefix"),
