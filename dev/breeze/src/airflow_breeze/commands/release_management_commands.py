74eee29a26ce0a96ddf3ae3c63028f0c11b3bf62
bugraoz93 <ozturkbugra93@gmail.com>
2025-12-07 18:50:40 +0100

    Update target branch help text for CI image usage

--- dev/breeze/src/airflow_breeze/commands/release_management_commands.py
+++ dev/breeze/src/airflow_breeze/commands/release_management_commands.py
@@ -1370,7 +1370,7 @@ def tag_providers(
 @click.option(
     "--target-branch",
     default="main",
-    help="Target branch to use CI image.",
+    help="Target branch to replace to use latest CI image built in job.",
 )
 @option_verbose
 @option_dry_run
