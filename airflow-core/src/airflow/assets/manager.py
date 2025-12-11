a33128cd1ae29684422084d2a3283f150e54a3f5
Wei Lee <weilee.rx@gmail.com>
2025-12-11 16:32:53 +0800

    feat(asset-partition): add key_share to row lock

--- airflow-core/src/airflow/assets/manager.py
+++ airflow-core/src/airflow/assets/manager.py
@@ -100,6 +100,7 @@ def _acquire_apdr_lock(
                 with_row_locks(
                     query=select(AssetModel.id).where(AssetModel.id == asset_id),
                     session=session,
+                    key_share=True,
                 )
             )
         ) is None:
