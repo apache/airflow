b24854fbe5ec67133d512a0b64584fb1ed9446fe
John Horan <jhoran@zendesk.com>
2025-12-17 17:23:26 +0000

    format

--- providers/cncf/kubernetes/src/airflow/providers/cncf/kubernetes/operators/pod.py
+++ providers/cncf/kubernetes/src/airflow/providers/cncf/kubernetes/operators/pod.py
@@ -909,7 +909,6 @@ class KubernetesPodOperator(BaseOperator):
             last_log_time=last_log_time,
             logging_interval=self.logging_interval,
             trigger_kwargs=self.trigger_kwargs,
-
         )
         container_state = trigger.define_container_state(self.pod)
         if context and (
