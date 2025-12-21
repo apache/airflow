599f6a9bc4834dd99e33787cac09b5acb0fb802d
David Blain <info@dabla.be>
2025-12-21 14:35:44 +0100

    refactor: Removed unused import of functools

--- providers/standard/tests/unit/standard/decorators/test_python.py
+++ providers/standard/tests/unit/standard/decorators/test_python.py
@@ -15,7 +15,6 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-import functools
 import sys
 import typing
 from collections import namedtuple
