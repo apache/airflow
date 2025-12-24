98092816bffc80033b78d9872c4e14a76e301884
David Blain <info@dabla.be>
2025-12-24 12:58:24 +0100

    refactor: Fixed import for Metadata

--- providers/common/compat/src/airflow/providers/common/compat/standard/operators.py
+++ providers/common/compat/src/airflow/providers/common/compat/standard/operators.py
@@ -71,7 +71,7 @@ else:
         from airflow.sdk.definitions.asset.metadata import Metadata
         from airflow.sdk.definitions.mappedoperator import OperatorPartial
     else:
-        from airflow.assets import Metadata
+        from airflow.datasets.metadata import Metadata
         from airflow.decorators import _TaskDecorator
         from airflow.models import BaseOperator
         from airflow.models.mappedoperator import OperatorPartial
