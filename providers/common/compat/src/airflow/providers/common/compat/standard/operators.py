bbd13a5e9290dc8a7fde570a8d28f2f807d6f771
David Blain <info@dabla.be>
2025-12-24 11:51:05 +0100

    refactor: Changed import of old TaskDecorator

--- providers/common/compat/src/airflow/providers/common/compat/standard/operators.py
+++ providers/common/compat/src/airflow/providers/common/compat/standard/operators.py
@@ -72,7 +72,7 @@ else:
         from airflow.sdk.definitions.mappedoperator import OperatorPartial
     else:
         from airflow.assets import Metadata
-        from airflow.decorators.base import _TaskDecorator
+        from airflow.decorators import _TaskDecorator
         from airflow.models import BaseOperator
         from airflow.models.mappedoperator import OperatorPartial
 
