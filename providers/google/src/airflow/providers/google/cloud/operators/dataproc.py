f9f3dee4f358cadf376a560f8dcfbd9ea4efdb7e
Chirodip Lodh Choudhury <chirodiplodh@gmail.com>
2025-12-27 18:28:50 +0530

    fix: added type

--- providers/google/src/airflow/providers/google/cloud/operators/dataproc.py
+++ providers/google/src/airflow/providers/google/cloud/operators/dataproc.py
@@ -996,9 +996,8 @@ class DataprocDeleteClusterOperator(GoogleCloudBaseOperator):
 
     def execute(self, context: Context) -> None:
         hook = DataprocHook(gcp_conn_id=self.gcp_conn_id, impersonation_chain=self.impersonation_chain)
-        op: operation.Operation = None
         try:
-            op = self._delete_cluster(hook)
+            op: operation.Operation = self._delete_cluster(hook)
             if not self.deferrable:
                 hook.wait_for_operation(timeout=self.timeout, result_retry=self.retry, operation=op)
                 self.log.info("Cluster deleted.")
