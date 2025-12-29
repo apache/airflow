eace825bf6d1b8fac4cb9c0f1951c16b2f0f5c74
Amogh Desai <amoghrajesh1999@gmail.com>
2025-12-29 17:53:12 +0530

    use relative paths

--- shared/listeners/src/airflow_shared/listeners/listener.py
+++ shared/listeners/src/airflow_shared/listeners/listener.py
@@ -41,7 +41,7 @@ class ListenerManager:
     """Manage listener registration and provides hook property for calling them."""
 
     def __init__(self):
-        from airflow.listeners.spec import (
+        from spec import (
             asset,
             dagrun,
             importerrors,
