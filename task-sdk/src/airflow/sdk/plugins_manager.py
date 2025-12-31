83a44c5c07b71620c21b0c3df149fc17cb689f77
Amogh Desai <amoghrajesh1999@gmail.com>
2025-12-31 13:53:31 +0530

    sdk wrapper for plugins manager

---
+++ task-sdk/src/airflow/sdk/plugins_manager.py
@@ -0,0 +1,42 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""SDK wrapper for plugins manager."""
+
+from __future__ import annotations
+
+from functools import cache
+from typing import TYPE_CHECKING
+
+from airflow.sdk._shared.plugins_manager.plugins_manager import (
+    integrate_listener_plugins as _integrate_listener_plugins,
+    integrate_macros_plugins as _integrate_macros_plugins,
+)
+
+if TYPE_CHECKING:
+    from airflow.listeners.listener import ListenerManager
+
+
+@cache
+def integrate_macros_plugins() -> None:
+    """Integrates macro plugins."""
+    _integrate_macros_plugins()
+
+
+def integrate_listener_plugins(listener_manager: ListenerManager) -> None:
+    """Add listeners from plugins."""
+    _integrate_listener_plugins(listener_manager)
