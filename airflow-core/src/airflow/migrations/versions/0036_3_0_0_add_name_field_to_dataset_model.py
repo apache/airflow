83ebee8d0e73e7cecc713dffb88c49ee3ee954fc
Georg Richter <georg@mariadb.com>
2026-01-02 12:20:10 +0100

    Fixed migrations for MariaDB:
    - replaced CTE with delete (which is planned for MariaDB 11.12) by simple JOIN
    - removed optional parameters from REGEXP_REPLACE (MariaDB only supports 3 parameters)

--- airflow-core/src/airflow/migrations/versions/0036_3_0_0_add_name_field_to_dataset_model.py
+++ airflow-core/src/airflow/migrations/versions/0036_3_0_0_add_name_field_to_dataset_model.py
@@ -109,13 +109,15 @@ def downgrade():
     # This usually happens when all the dags are turned off.
     op.execute(
         """
-        with unique_dataset as (select min(id) as min_id, uri as uri from dataset group by id),
-        duplicate_dataset_id as (
-            select id from dataset join unique_dataset
-            on dataset.uri = unique_dataset.uri
-            where dataset.id > unique_dataset.min_id
-        )
-        delete from dataset where id in (select * from duplicate_dataset_id)
+        DELETE d
+        FROM dataset d
+        JOIN (
+            SELECT MIN(id) AS min_id, uri
+            FROM dataset
+            GROUP BY uri
+        ) u
+          ON d.uri = u.uri
+         AND d.id > u.min_id;
         """
     )
     with op.batch_alter_table("dataset", schema=None) as batch_op:
