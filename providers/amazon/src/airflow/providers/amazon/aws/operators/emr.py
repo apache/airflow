7349b187f2344d6aed0a1ce5160ae939c8a30073
Ankit Chaurasia <8670962+sunank200@users.noreply.github.com>
2026-01-04 01:47:10 +0545

    Source amazon provider to use airflow.sdk.configuration.conf (#59979)
    * Migrate amazon provider to use airflow.sdk.configuration.conf
    
    * Fix test failures after conf import migration
    
    - Add AWS_CONN_ID to lambda executor test fixture
    - Use CONFIG_DEFAULTS as fallback in ECS executor config to match old behavior
    
    * Add fallback values for AWS Lambda executor config options
    
    * Add fallback value for AWS Lambda executor end_wait_timeout config
    
    * Add fallback value for AWS Lambda executor end_wait_timeout config

--- providers/amazon/src/airflow/providers/amazon/aws/operators/emr.py
+++ providers/amazon/src/airflow/providers/amazon/aws/operators/emr.py
@@ -24,7 +24,6 @@ from datetime import timedelta
 from typing import TYPE_CHECKING, Any
 from uuid import uuid4
 
-from airflow.configuration import conf
 from airflow.exceptions import AirflowProviderDeprecationWarning
 from airflow.providers.amazon.aws.hooks.emr import EmrContainerHook, EmrHook, EmrServerlessHook
 from airflow.providers.amazon.aws.links.emr import (
@@ -58,7 +57,7 @@ from airflow.providers.amazon.aws.utils.waiter import (
 )
 from airflow.providers.amazon.aws.utils.waiter_with_logging import wait
 from airflow.providers.amazon.version_compat import NOTSET, ArgNotSet
-from airflow.providers.common.compat.sdk import AirflowException
+from airflow.providers.common.compat.sdk import AirflowException, conf
 from airflow.utils.helpers import exactly_one, prune_dict
 
 if TYPE_CHECKING:
