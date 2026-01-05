9c1cab5f089d24b13190cdd22452b7dc9ed57afc
Chirodip Lodh Choudhury <chirodip.l.choudhury1@aexp.com>
2026-01-05 16:02:18 +0530

    fix: handling timezone import issues

--- providers/google/src/airflow/providers/google/cloud/operators/dataproc.py
+++ providers/google/src/airflow/providers/google/cloud/operators/dataproc.py
@@ -66,7 +66,7 @@ from airflow.providers.google.common.hooks.base_google import PROVIDE_PROJECT_ID
 
 try:
     from airflow.sdk import timezone
-except ImportError:
+except ModuleNotFoundError:
     from airflow.utils import timezone
 
 if TYPE_CHECKING:
