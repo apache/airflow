54573e23c3f8e0b7d971d9b75efbf0f37e3c6da3
Subham Sangwan <subhamsangwan26@gmail.com>
2026-01-05 19:54:38 +0530

    Fix reviewer feedback: add license, move imports, and fix newsfragments

--- airflow-core/tests/unit/models/test_log.py
+++ airflow-core/tests/unit/models/test_log.py
@@ -1,12 +1,32 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
 from __future__ import annotations
 
 import pytest
+from sqlalchemy import select
+from sqlalchemy.orm import joinedload
 
 from airflow.models.log import Log
 from airflow.operators.empty import EmptyOperator
 from airflow.utils.state import TaskInstanceState
 
 pytestmark = pytest.mark.db_test
+
 
 class TestLogTaskInstanceReproduction:
     def test_log_task_instance_join_correctness(self, dag_maker, session):
@@ -39,8 +59,6 @@ class TestLogTaskInstanceReproduction:
         session.commit()
 
         # Query with joinedload to trigger the relationship join
-        from sqlalchemy import select
-        from sqlalchemy.orm import joinedload
 
         stmt = select(Log).where(Log.id == log.id).options(joinedload(Log.task_instance))
         loaded_log = session.scalar(stmt)
