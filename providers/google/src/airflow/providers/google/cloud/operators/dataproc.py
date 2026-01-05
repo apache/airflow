d3d13841ebb448354d6ade593b073404a265e3c1
Chirodip Lodh Choudhury <chirodip.l.choudhury1@aexp.com>
2026-01-05 18:14:41 +0530

    fix: added back timezone import code

--- providers/google/src/airflow/providers/google/cloud/operators/dataproc.py
+++ providers/google/src/airflow/providers/google/cloud/operators/dataproc.py
@@ -63,11 +63,7 @@ from airflow.providers.google.cloud.triggers.dataproc import (
 )
 from airflow.providers.google.cloud.utils.dataproc import DataprocOperationType
 from airflow.providers.google.common.hooks.base_google import PROVIDE_PROJECT_ID
-
-try:
-    from airflow.sdk import timezone
-except ImportError:
-    from airflow.utils import timezone
+from airflow.sdk import timezone
 
 if TYPE_CHECKING:
     from google.api_core import operation
