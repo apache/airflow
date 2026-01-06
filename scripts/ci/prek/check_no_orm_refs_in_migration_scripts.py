941cc957ea656b19ba97340d968e9abe520f45a8
Zach Gottesman <zach.gottesman@datadoghq.com>
2026-01-06 14:10:41 -0500

    Inspect inheritance of models references in hook

--- scripts/ci/prek/check_no_orm_refs_in_migration_scripts.py
+++ scripts/ci/prek/check_no_orm_refs_in_migration_scripts.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-#
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -16,12 +14,6 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-# /// script
-# requires-python = ">=3.10,<3.11"
-# dependencies = [
-#   "rich>=13.6.0",
-# ]
-# ///
 """
 Check that there are no imports of ORM classes in any of the alembic migration scripts.
 This is to prevent the addition of migration code directly referencing any ORM definition,
@@ -31,7 +23,8 @@ thread at this link: https://github.com/apache/airflow/issues/59871
 
 from __future__ import annotations
 
-import ast
+import importlib
+import inspect
 import os
 import sys
 from pathlib import Path
@@ -39,14 +32,14 @@ from pprint import pformat
 from typing import Final
 
 sys.path.insert(0, str(Path(__file__).parent.resolve()))
-from common_prek_utils import AIRFLOW_CORE_SOURCES_PATH, console
+from common_prek_utils import AIRFLOW_CORE_SOURCES_PATH, console, get_imports_from_file
+
+sys.path.insert(0, AIRFLOW_CORE_SOURCES_PATH.resolve().as_posix())  # make sure airflow-core is importable
+from airflow.models.base import Base
 
 _MIGRATIONS_DIRPATH: Final[Path] = Path(
     os.path.join(AIRFLOW_CORE_SOURCES_PATH, "airflow/migrations/versions")
 )
-# Tuple of fully qualified references within `airflow.models.*` which are allowed to be imported by the
-# migration scripts. Should only allow references to primitives or constants, and NOT ORM class definitions.
-_MODELS_IMPORTS_ALLOWLIST: Final[tuple[str, ...]] = ("airflow.models.ID_LEN",)
 
 
 def main() -> None:
@@ -54,16 +47,18 @@ def main() -> None:
         migration_scripts = [Path(f) for f in sys.argv[1:]]
     else:
         migration_scripts = list(_MIGRATIONS_DIRPATH.glob("**/*.py"))
-    console.print(
-        f"Checking the following modified migration scripts: {pformat([str(path) for path in migration_scripts])}"
-    )
+    console.print("Checking migration scripts ...")
     violations = []
+    bad_script_paths = []
     for script_path in migration_scripts:
-        violations.extend(_find_models_import_violations(script_path=script_path))
+        if violating_imports := _find_models_import_violations(script_path=script_path):
+            violations.extend(violating_imports)
+            bad_script_paths.append(str(script_path))
     if violations:
         for err in violations:
             console.print(f"[red]{err}")
         console.print("\n[red]ORM references detected in one or more migration scripts[/]")
+        console.print(f"[red]Violating script paths: {pformat([p for p in bad_script_paths])}[/]")
         sys.exit(1)
     console.print("[green]No ORM references detected in migration scripts.")
 
@@ -75,36 +70,24 @@ def _find_models_import_violations(script_path: Path) -> list[str]:
     this function uses the heuristic of checking for any non-allowlisted imports from within the
     `airflow.models` module.
     """
-    script_source = script_path.read_text(encoding="utf-8")
     bad_imports = []
-    for node in ast.walk(ast.parse(script_source)):
-        if details := _is_violating_orm_import(node=node):
-            line_no, src = details
-            bad_imports.append(
-                f"Found bad import on line {line_no} in migration script {str(script_path)}: '{src}'"
-            )
+    for import_ref in get_imports_from_file(file_path=script_path, only_top_level=False):
+        if _is_violating_orm_import(import_ref=import_ref):
+            bad_imports.append(f"Found bad import in migration script {str(script_path)}: '{import_ref}'")
     return bad_imports
 
 
-def _is_violating_orm_import(node: ast.AST) -> tuple[int, str] | None:
-    """
-    Return a tuple of line number and line text for the given node, if it is an import of any non-allowlisted object
-    from within `airflow.models`, otherwise return `None`.
-    """
-    # Match "from x import y [as z]"
-    if isinstance(node, ast.ImportFrom) and node.module:
-        fully_qualified_reference = ".".join([node.module] + [alias.name for alias in node.names])
-    # Match "import x.y.z [as w]"
-    elif isinstance(node, ast.Import):
-        fully_qualified_reference = ".".join([alias.name for alias in node.names])
-    else:
-        return None
-
-    if not fully_qualified_reference.startswith("airflow.models"):
-        return None
-    if fully_qualified_reference in _MODELS_IMPORTS_ALLOWLIST:
-        return None
-    return (node.lineno, ast.unparse(node))
+def _is_violating_orm_import(import_ref: str) -> bool:
+    """Return `True` if the imported object is an ORM class from within `airflow.models`, otherwise return `False`."""
+    if not import_ref.startswith("airflow.models"):
+        return False
+    # import the fully qualified reference to check if the reference is a subclass of a declarative base
+    mod_to_import, _, attr_name = import_ref.rpartition(".")
+    referenced_module = importlib.import_module(mod_to_import)
+    referenced_obj = getattr(referenced_module, attr_name)
+    if inspect.isclass(referenced_obj) and referenced_obj in Base.__subclasses__():
+        return True
+    return False
 
 
 if __name__ == "__main__":
