f06c55bd312cec0d2c299f79b36bd4d53ef31290
David Blain <info@dabla.be>
2026-01-08 09:42:05 +0100

    refactor: Increment migration number for add next_trigger_id column

--- airflow-core/src/airflow/migrations/versions/0098_3_2_0_add_next_trigger_id_to_task_instance_table.py
+++ airflow-core/src/airflow/migrations/versions/0098_3_2_0_add_next_trigger_id_to_task_instance_table.py
@@ -20,7 +20,7 @@
 Add ``next_trigger_id`` column to ``task_instance`` table.
 
 Revision ID: 658517c60c7f
-Revises: c47f2e1ab9d4
+Revises: 0b112f49112d
 Create Date: 2025-12-26 12:07:05.849152
 
 """
@@ -31,7 +31,7 @@ import sqlalchemy as sa
 from alembic import op
 
 revision = "658517c60c7f"
-down_revision = "c47f2e1ab9d4"
+down_revision = "0b112f49112d"
 branch_labels = None
 depends_on = None
 airflow_version = "3.2.0"
