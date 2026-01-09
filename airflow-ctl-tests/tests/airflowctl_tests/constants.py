55a823800111d39d42f29499d696669bf5823573
bugraoz93 <ozturkbugra93@gmail.com>
2026-01-09 21:17:52 +0100

    Sync airflowctl to v3-1-test

--- airflow-ctl-tests/tests/airflowctl_tests/constants.py
+++ airflow-ctl-tests/tests/airflowctl_tests/constants.py
@@ -28,5 +28,10 @@ DOCKER_IMAGE = os.environ.get("DOCKER_IMAGE") or DEFAULT_DOCKER_IMAGE
 DOCKER_COMPOSE_HOST_PORT = os.environ.get("HOST_PORT", "localhost:8080")
 
 DOCKER_COMPOSE_FILE_PATH = (
-    AIRFLOW_ROOT_PATH / "airflow-core" / "docs" / "howto" / "docker-compose" / "docker-compose.yaml"
+    AIRFLOW_ROOT_PATH
+    / "airflow-core"
+    / "docs"
+    / "howto"
+    / "docker-compose"
+    / "docker-compose.yaml"
 )
