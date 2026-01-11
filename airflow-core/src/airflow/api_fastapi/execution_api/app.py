e97781756fe5ebd43c28144e1f2ec18f2d67cdb3
Anish <giri.girianish@gmail.com>
2026-01-10 20:03:47 -0600

    rollback to explicit override

--- airflow-core/src/airflow/api_fastapi/execution_api/app.py
+++ airflow-core/src/airflow/api_fastapi/execution_api/app.py
@@ -325,9 +325,32 @@ class InProcessExecutionAPI:
             # Set up dag_bag in app state for dependency injection
             self._app.state.dag_bag = create_dag_bag()
 
+            self._app.state.jwt_generator = _jwt_generator()
+            self._app.state.jwt_validator = _jwt_validator()
+
             self._app.state.svcs_registry = svcs.Registry()
-            self._app.state.svcs_registry.register_value(JWTGenerator, _jwt_generator())
-            self._app.state.svcs_registry.register_value(JWTValidator, _jwt_validator())
+            self._app.state.svcs_registry.register_value(JWTGenerator, self._app.state.jwt_generator)
+            self._app.state.svcs_registry.register_value(JWTValidator, self._app.state.jwt_validator)
+
+            from airflow.api_fastapi.execution_api.deps import _container
+
+            class InProcessContainer:
+                """A container-like object that provides services from app.state."""
+
+                def __init__(self, app_state):
+                    self._app_state = app_state
+
+                async def aget(self, svc_type):
+                    if svc_type is JWTGenerator:
+                        return self._app_state.jwt_generator
+                    if svc_type is JWTValidator:
+                        return self._app_state.jwt_validator
+                    raise KeyError(svc_type)
+
+            async def _inprocess_container():
+                yield InProcessContainer(self._app.state)
+
+            self._app.dependency_overrides[_container] = _inprocess_container
 
             async def always_allow(): ...
 
@@ -356,6 +379,7 @@ class InProcessExecutionAPI:
         self._cm = AsyncExitStack()
 
         asyncio.run_coroutine_threadsafe(start_lifespan(self._cm, self.app), middleware.loop)
+
         return httpx.WSGITransport(app=middleware)  # type: ignore[arg-type]
 
     @cached_property
