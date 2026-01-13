b7518404e61cb9d96d78857f8ee8559fd51de004
Brent Bovenzi <brent.bovenzi@gmail.com>
2026-01-13 12:53:47 -0500

    Remove API error from disabling submit

--- airflow-core/src/airflow/ui/src/components/TriggerDag/TriggerDAGForm.tsx
+++ airflow-core/src/airflow/ui/src/components/TriggerDag/TriggerDAGForm.tsx
@@ -231,12 +231,7 @@ const TriggerDAGForm = ({
           <Button
             colorPalette="brand"
             disabled={
-              Boolean(errors.conf) ||
-              Boolean(errors.date) ||
-              formError ||
-              isPending ||
-              Boolean(errorTrigger) ||
-              dataIntervalInvalid
+              Boolean(errors.conf) || Boolean(errors.date) || formError || isPending || dataIntervalInvalid
             }
             onClick={() => void handleSubmit(onSubmit)()}
           >
