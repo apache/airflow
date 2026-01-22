df6e48011ca6c27257a1ff883e19d45a3978cf6c
David Blain <info@dabla.be>
2026-01-22 19:15:44 +0100

    refactor: Fixed return type of get_command_output method

--- providers/microsoft/winrm/src/airflow/providers/microsoft/winrm/triggers/winrm.py
+++ providers/microsoft/winrm/src/airflow/providers/microsoft/winrm/triggers/winrm.py
@@ -102,7 +102,7 @@ class WinRMCommandOutputTrigger(BaseTrigger):
     def hook(self) -> WinRMHook:
         return WinRMHook(ssh_conn_id=self.ssh_conn_id)
 
-    async def get_command_output(self, conn: Protocol) -> tuple[bytes, bytes, int, bool]:
+    async def get_command_output(self, conn: Protocol) -> tuple[bytes, bytes, int | None, bool]:
         stdout, stderr, return_code, command_done = await asyncio.to_thread(
             self.hook.get_command_output, conn, self.shell_id, self.command_id
         )
