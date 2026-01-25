e60dd75f7531e278aafbab0c4264acc30a50a811
Kamran-imaz <kamranimaz.professional@gmail.com>
2026-01-25 19:49:24 +0530

    handled test files

--- providers/google/src/airflow/providers/google/cloud/hooks/bigtable.py
+++ providers/google/src/airflow/providers/google/cloud/hooks/bigtable.py
@@ -27,7 +27,7 @@ from google.cloud.bigtable import Client, enums
 from google.cloud.bigtable.cluster import Cluster
 from google.cloud.bigtable.instance import Instance
 from google.cloud.bigtable.table import ClusterState, Table
-
+from airflow.providers.common.compat.sdk import AirflowException
 from airflow.providers.google.common.consts import CLIENT_INFO
 from airflow.providers.google.common.hooks.base_google import GoogleBaseHook
 
