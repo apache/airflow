8b4749fb0fabde401a4a4e82b5f289761a4c45d9
Abhijeet Raj Singh <abhijeetrajsingh@Abhijeets-MacBook-Pro-6.local>
2026-01-25 22:54:02 +0530

    Fix middleware order: move GZipMiddleware to end to avoid chunked responses

--- airflow-core/src/airflow/api_fastapi/core_api/app.py
+++ airflow-core/src/airflow/api_fastapi/core_api/app.py
@@ -165,7 +165,8 @@ def init_config(app: FastAPI) -> None:
     # Compress responses greater than 1kB with optimal compression level as 5
     # with level ranging from 1 to 9 with 1 (fastest, least compression)
     # and 9 (slowest, most compression)
-    app.add_middleware(GZipMiddleware, minimum_size=1024, compresslevel=5)
+    
+   
 
     app.state.secret_key = get_signing_key("api", "secret_key")
 
@@ -180,8 +181,11 @@ def init_error_handlers(app: FastAPI) -> None:
 def init_middlewares(app: FastAPI) -> None:
     from airflow.api_fastapi.auth.middlewares.refresh_token import JWTRefreshMiddleware
 
+    
     app.add_middleware(JWTRefreshMiddleware)
     if conf.getboolean("core", "simple_auth_manager_all_admins"):
         from airflow.api_fastapi.auth.managers.simple.middleware import SimpleAllAdminMiddleware
 
         app.add_middleware(SimpleAllAdminMiddleware)
+    app.add_middleware(GZipMiddleware, minimum_size=1024, compresslevel=5)
+
