7468b10b587f9a0960da95f41217f123075e9667
Daniel Standish <15932138+dstandish@users.noreply.github.com>
2026-02-03 15:20:13 -0800

    Remove align param from iter dagrun infos
    I don't think this param does anything. It's only set to False when called from within get_run_ids, within set_state, which is only called when marking tasks as failed. At best it seems it is called in an extremely odd and impossible to understand edge case.  But let's see what the tests say.

--- airflow-core/src/airflow/api/common/mark_tasks.py
+++ airflow-core/src/airflow/api/common/mark_tasks.py
@@ -187,7 +187,7 @@ def get_run_ids(dag: SerializedDAG, run_id: str, future: bool, past: bool, sessi
     else:
         dates = [
             info.logical_date
-            for info in dag.iter_dagrun_infos_between(start_date, end_date, align=False)
+            for info in dag.iter_dagrun_infos_between(start_date, end_date)
             if info.logical_date  # todo: AIP-76 this will not find anything where logical date is null
         ]
         run_ids = [dr.run_id for dr in DagRun.find(dag_id=dag.dag_id, logical_date=dates, session=session)]
