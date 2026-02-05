ceeee65ab92da25673217d0a69547656523decc4
Fury0508 <daan0508@gmail.com>
2026-02-05 11:35:08 +0530

    ran perk--all-files

--- airflow-ctl/src/airflowctl/ctl/cli_config.py
+++ airflow-ctl/src/airflowctl/ctl/cli_config.py
@@ -584,9 +584,12 @@ class CommandFactory:
                     if self._is_primitive_type(type_name=parameter_type):
                         is_bool = parameter_type == "bool"
                         sanitized_key = self._sanitize_arg_parameter_key(parameter_key)
-                        operation_name = operation.get('name')
-                        parent_name = operation.get('parent').name
-                        is_jobs_or_dagrun_list = operation_name == 'list' and parent_name in ['JobsOperations', 'DagRunOperations']
+                        operation_name = operation.get("name")
+                        parent_name = operation.get("parent").name
+                        is_jobs_or_dagrun_list = operation_name == "list" and parent_name in [
+                            "JobsOperations",
+                            "DagRunOperations",
+                        ]
                         is_positional = not is_bool and not has_default and not is_jobs_or_dagrun_list
                         args.append(
                             self._create_arg(
