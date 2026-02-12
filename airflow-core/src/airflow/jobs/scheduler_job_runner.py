7ba7fbc45bd9966f38aa8a876c93f186f6fff736
Ephraim Anierobi <splendidzigy24@gmail.com>
2026-02-12 15:37:59 +0100

    update warning log

--- airflow-core/src/airflow/jobs/scheduler_job_runner.py
+++ airflow-core/src/airflow/jobs/scheduler_job_runner.py
@@ -921,8 +921,8 @@ class SchedulerJobRunner(BaseJobRunner, LoggingMixin):
                 ti.set_state(None, session=session)
                 continue
             if not ti.dag_version_id:
-                self.log.error(
-                    "TaskInstance %s does not have a dag_version_id set, cannot be enqueued",
+                self.log.warning(
+                    "TaskInstance %s does not have a dag_version_id set, cannot be enqueued. This would get unstuck and dag_version_id updated.",
                     ti,
                 )
                 continue
