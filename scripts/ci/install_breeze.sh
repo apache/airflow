400f8b14104289cfb70a09e19fa12721f3681448
Jarek Potiuk <jarek@potiuk.com>
2026-02-28 07:40:39 +0100

    Upgrade to latest released build dependencies
    This reverts commit 34fb41e867486448e49909d0ec9025805cd9b5df.

--- scripts/ci/install_breeze.sh
+++ scripts/ci/install_breeze.sh
@@ -21,8 +21,8 @@ cd "$( dirname "${BASH_SOURCE[0]}" )/../../"
 
 PYTHON_ARG=""
 
-PIP_VERSION="25.3"
-UV_VERSION="0.9.11"
+PIP_VERSION="26.0.1"
+UV_VERSION="0.10.7"
 if [[ ${PYTHON_VERSION=} != "" ]]; then
     PYTHON_ARG="--python=$(which python"${PYTHON_VERSION}") "
 fi
