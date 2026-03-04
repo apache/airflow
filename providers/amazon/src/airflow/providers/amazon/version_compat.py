5165e42014e742201761af6be79f45bd446897e5
vatsrahul1001 <rah.sharma11@gmail.com>
2026-03-04 17:22:36 +0530

    remove provider changes

--- providers/amazon/src/airflow/providers/amazon/version_compat.py
+++ providers/amazon/src/airflow/providers/amazon/version_compat.py
@@ -35,7 +35,6 @@ def get_base_airflow_version_tuple() -> tuple[int, int, int]:
 AIRFLOW_V_3_0_PLUS = get_base_airflow_version_tuple() >= (3, 0, 0)
 AIRFLOW_V_3_1_PLUS: bool = get_base_airflow_version_tuple() >= (3, 1, 0)
 AIRFLOW_V_3_1_1_PLUS: bool = get_base_airflow_version_tuple() >= (3, 1, 1)
-AIRFLOW_V_3_1_8_PLUS: bool = get_base_airflow_version_tuple() >= (3, 1, 8)
 
 if AIRFLOW_V_3_1_PLUS:
     from airflow.sdk import BaseHook
@@ -53,7 +52,6 @@ else:
 __all__ = [
     "AIRFLOW_V_3_0_PLUS",
     "AIRFLOW_V_3_1_PLUS",
-    "AIRFLOW_V_3_1_8_PLUS",
     "BaseHook",
     "BaseOperator",
     "BaseOperatorLink",
