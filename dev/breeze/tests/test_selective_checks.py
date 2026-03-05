320f09f9d336fcfaba45ebf03cc1858c6ca71ef6
Daniel Standish <15932138+dstandish@users.noreply.github.com>
2026-03-05 14:02:20 -0800

    Add otel integration test group (#62880)
    Add dedicated integration test group for otel
    
    Removed celery from the otel test.
    
    Removed other dead code from the otel test.
    
    Retrieve the spans from jaeger api instead of stdout parsing

--- dev/breeze/tests/test_selective_checks.py
+++ dev/breeze/tests/test_selective_checks.py
@@ -1125,7 +1125,7 @@ def assert_outputs_are_printed(expected_outputs: dict[str, str], stderr: str):
                     "upgrade-to-newer-dependencies": "false",
                     "core-test-types-list-as-strings-in-json": ALL_CI_SELECTIVE_TEST_TYPES_AS_JSON,
                     "providers-test-types-list-as-strings-in-json": ALL_PROVIDERS_SELECTIVE_TEST_TYPES_AS_JSON,
-                    "testable-core-integrations": "['kerberos', 'redis']",
+                    "testable-core-integrations": "['kerberos', 'otel', 'redis']",
                     "testable-providers-integrations": "['celery', 'cassandra', 'drill', 'tinkerpop', 'kafka', "
                     "'mongo', 'pinot', 'qdrant', 'redis', 'trino', 'ydb']",
                     "run-mypy": "true",
