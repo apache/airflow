e76becd838148704b408bf5d5afcff896d6112f5
Owen Leung <owen.leung2@gmail.com>
2026-03-06 20:44:24 +0800

    Enable Elasticsearch provider integration tests in CI (#62942)
    * feat(elasticsearch): enable integration test
    
    * feat(elasticsearch): fix breeze ci test
    
    * feat(elasticsearch): fix bad rebase

--- providers/elasticsearch/tests/integration/elasticsearch/log/__init__.py
+++ providers/elasticsearch/tests/integration/elasticsearch/log/__init__.py
@@ -14,6 +14,3 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-from __future__ import annotations
-
-pytest_plugins = "tests_common.pytest_plugin"
