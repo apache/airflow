9fbcca4fb81359ab02657375bb71e27bd4b7fa07
kalluripradeep <KALLURIPRADEEP99@GMAIL.COM>
2026-03-07 20:31:40 +0000

    move comment inline to fix prek import checks

--- airflow/providers/celery/executors/celery_executor.py
+++ airflow/providers/celery/executors/celery_executor.py
@@ -54,10 +54,7 @@ from airflow.cli.cli_config import (
 from airflow.configuration import conf
 from airflow.exceptions import AirflowTaskTimeout
 from airflow.executors.base_executor import BaseExecutor
-# Must be imported at module level to register execute_command with the Celery app
-# and connect the celery_import_modules signal handler at worker startup.
-# See: https://github.com/apache/airflow/issues/63043
-from airflow.providers.celery.executors import celery_executor_utils as _celery_executor_utils  # noqa: F401
+from airflow.providers.celery.executors import celery_executor_utils as _celery_executor_utils  # noqa: F401 # Needed to register execute_command with Celery app at worker startup, see #63043
 from airflow.stats import Stats
 from airflow.utils.state import TaskInstanceState
 
