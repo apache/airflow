e612482848e6c8add5e133f560b5ca3f4039c7aa
Anish Giri <161533316+anishgirianish@users.noreply.github.com>
2026-03-08 20:29:15 -0500

    AIP-76: Implement AllowedKeyMapper for partition key validation (#61931)

--- task-sdk/src/airflow/sdk/__init__.pyi
+++ task-sdk/src/airflow/sdk/__init__.pyi
@@ -62,6 +62,7 @@ from airflow.sdk.definitions.decorators import setup as setup, task as task, tea
 from airflow.sdk.definitions.decorators.task_group import task_group as task_group
 from airflow.sdk.definitions.edges import EdgeModifier as EdgeModifier, Label as Label
 from airflow.sdk.definitions.param import Param as Param
+from airflow.sdk.definitions.partition_mappers.allowed_key import AllowedKeyMapper
 from airflow.sdk.definitions.partition_mappers.base import PartitionMapper
 from airflow.sdk.definitions.partition_mappers.identity import IdentityMapper
 from airflow.sdk.definitions.partition_mappers.product import ProductMapper
@@ -100,6 +101,7 @@ conf: AirflowSDKConfigParser
 
 __all__ = [
     "__version__",
+    "AllowedKeyMapper",
     "Asset",
     "AssetAlias",
     "AssetAll",
