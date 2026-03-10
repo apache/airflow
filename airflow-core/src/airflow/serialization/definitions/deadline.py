dafd498eea39e427b07829d2db6a4c85eb5b1b0f
Amogh Desai <amoghrajesh1999@gmail.com>
2026-03-10 17:10:00 +0530

    adding some context in logs

--- airflow-core/src/airflow/serialization/definitions/deadline.py
+++ airflow-core/src/airflow/serialization/definitions/deadline.py
@@ -99,7 +99,11 @@ class SerializedReferenceModels:
                 )
 
             if extra_kwargs := kwargs.keys() - filtered_kwargs.keys():
-                self.log.debug("Ignoring unexpected parameters: %s", ", ".join(extra_kwargs))
+                self.log.debug(
+                    "%s ignoring unexpected parameters: %s",
+                    self.reference_name,
+                    ", ".join(extra_kwargs),
+                )
 
             base_time = self._evaluate_with(session=session, **filtered_kwargs)
             return base_time + interval if base_time is not None else None
@@ -275,7 +279,11 @@ class SerializedReferenceModels:
                 )
 
             if extra_kwargs := kwargs.keys() - filtered_kwargs.keys():
-                self.log.debug("Ignoring unexpected parameters: %s", ", ".join(extra_kwargs))
+                self.log.debug(
+                    "%s ignoring unexpected parameters: %s",
+                    self.reference_name,
+                    ", ".join(extra_kwargs),
+                )
 
             deadline = self.inner_ref._evaluate_with(session=session, **filtered_kwargs)
             return deadline + interval if deadline is not None else None
