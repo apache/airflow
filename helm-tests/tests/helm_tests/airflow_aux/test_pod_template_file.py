a14494060bb6acc4d0b29e074dfc6d12d0e29df1
Siddharthan P S <sidshas03@gmail.com>
2026-03-10 12:12:19 -0400

    Fix airflow_aux git-sync credentials test for password file envs

--- helm-tests/tests/helm_tests/airflow_aux/test_pod_template_file.py
+++ helm-tests/tests/helm_tests/airflow_aux/test_pod_template_file.py
@@ -274,8 +274,8 @@ class TestPodTemplateFile:
             "valueFrom": {"secretKeyRef": {"name": "user-pass-secret", "key": "GIT_SYNC_USERNAME"}},
         } in jmespath.search("spec.initContainers[0].env", docs[0])
         assert {
-            "name": "GIT_SYNC_PASSWORD",
-            "valueFrom": {"secretKeyRef": {"name": "user-pass-secret", "key": "GIT_SYNC_PASSWORD"}},
+            "name": "GIT_SYNC_PASSWORD_FILE",
+            "value": "/etc/git-secret/credentials/GIT_SYNC_PASSWORD",
         } in jmespath.search("spec.initContainers[0].env", docs[0])
 
         # Testing git-sync v4
@@ -284,8 +284,8 @@ class TestPodTemplateFile:
             "valueFrom": {"secretKeyRef": {"name": "user-pass-secret", "key": "GITSYNC_USERNAME"}},
         } in jmespath.search("spec.initContainers[0].env", docs[0])
         assert {
-            "name": "GITSYNC_PASSWORD",
-            "valueFrom": {"secretKeyRef": {"name": "user-pass-secret", "key": "GITSYNC_PASSWORD"}},
+            "name": "GITSYNC_PASSWORD_FILE",
+            "value": "/etc/git-secret/credentials/GITSYNC_PASSWORD",
         } in jmespath.search("spec.initContainers[0].env", docs[0])
 
     def test_should_set_the_dags_volume_claim_correctly_when_using_an_existing_claim(self):
