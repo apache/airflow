b79aeedbcca1585eb9b0b7024efdf21b0e03e876
wjddn279 <ehwjddn279@naver.com>
2026-03-14 18:48:33 +0900

    fix dag warning alert UI to display newline

--- airflow-core/src/airflow/ui/src/components/WarningAlert.tsx
+++ airflow-core/src/airflow/ui/src/components/WarningAlert.tsx
@@ -33,7 +33,9 @@ export const WarningAlert = ({ warning }: Props) => {
 
   return (
     <Alert data-testid="warning-alert" status="warning">
-      <Flex align="center">{warning?.message}</Flex>
+      <Flex align="center" whiteSpace="pre-wrap">
+        {warning?.message}
+      </Flex>
     </Alert>
   );
 };
