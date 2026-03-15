a732aa3c0d6d0d4327400a7ebec7da7872585655
Dev-iL <6509619+Dev-iL@users.noreply.github.com>
2026-03-15 15:45:11 +0200

    Exclude google

--- dev/breeze/tests/test_selective_checks.py
+++ dev/breeze/tests/test_selective_checks.py
@@ -1329,6 +1329,7 @@ def test_excluded_providers():
                     "3.14": [
                         "amazon",  # Depends on lxml<6
                         "apache.cassandra",  # Enable when the next release after 3.29.3 is available
+                        "google",  # Depends on ray, which does not yet support python 3.14 (due to pydantic v1)
                     ],
                 }
             ),
