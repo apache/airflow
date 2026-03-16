80e973a6598cf99f66f29431294fdb839f3d9e26
Gabriel Demetrios Lafis <33261275+galafis@users.noreply.github.com>
2026-03-16 13:39:29 -0300

    Remove unintended whitespace/docstring changes
    Keep only the targeted fix (import, helper function, and init calls)
    without reformatting unrelated code.

--- providers/apache/beam/src/airflow/providers/apache/beam/triggers/beam.py
+++ providers/apache/beam/src/airflow/providers/apache/beam/triggers/beam.py
@@ -81,6 +81,7 @@ class BeamPipelineBaseTrigger(BaseTrigger):
 
         async_gcs_hook = GCSAsyncHook(gcp_conn_id=gcp_conn_id)
         sync_gcs_hook = await async_gcs_hook.get_sync_hook()
+
         loop = asyncio.get_running_loop()
         # Running synchronous `enter_context()` method in a separate
         # thread using the default executor `None`. The `run_in_executor()` function returns the
@@ -102,18 +103,17 @@ class BeamPythonPipelineTrigger(BeamPipelineBaseTrigger):
     :param variables: Variables passed to the pipeline.
     :param py_file: Path to the python file to execute.
     :param py_options: Additional options.
-    :param py_interpreter: Python version of the Apache Beam pipeline.
-        If ``None``, this defaults to the python3.
-        To track python versions supported by beam and related
-        issues check: https://issues.apache.org/jira/browse/BEAM-1251
+    :param py_interpreter: Python version of the Apache Beam pipeline. If `None`, this defaults to the
+        python3. To track python versions supported by beam and related issues
+        check: https://issues.apache.org/jira/browse/BEAM-1251
     :param py_requirements: Additional python package(s) to install.
         If a value is passed to this parameter, a new virtual environment has been created with
         additional packages installed.
 
         You could also install the apache-beam package if it is not installed on your system, or you want
         to use a different version.
-    :param py_system_site_packages: Whether to include system_site_packages in
-        your virtualenv. See virtualenv documentation for more information.
+    :param py_system_site_packages: Whether to include system_site_packages in your virtualenv.
+        See virtualenv documentation for more information.
         This option is only relevant if the ``py_requirements`` parameter is not None.
     :param runner: Runner on which pipeline will be run. By default, "DirectRunner" is being used.
         Other possible options: DataflowRunner, SparkRunner, FlinkRunner, PortableRunner.
