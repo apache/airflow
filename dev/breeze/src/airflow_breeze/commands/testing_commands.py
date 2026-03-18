f16680fa05762b0078ff4b6d87cb11709c787c83
wjddn279 <ehwjddn279@naver.com>
2026-03-18 19:32:44 +0900

    revert run test not to skip pytest by breeze

--- dev/breeze/src/airflow_breeze/commands/testing_commands.py
+++ dev/breeze/src/airflow_breeze/commands/testing_commands.py
@@ -249,9 +249,10 @@ def _run_test(
     pytest_args.extend(extra_pytest_args)
     # Skip "FOLDER" in case "--ignore=FOLDER" is passed as an argument
     # Which might be the case if we are ignoring some providers during compatibility checks
+    pytest_args_before_skip = pytest_args
     pytest_args = [arg for arg in pytest_args if f"--ignore={arg}" not in pytest_args]
     # If no test directory is left (all positional args were excluded/ignored), skip
-    if pytest_args and pytest_args[0].startswith("--"):
+    if pytest_args_before_skip != pytest_args and pytest_args[0].startswith("--"):
         return 0, f"Skipped test, no tests needed: {shell_params.test_type}"
     run_cmd.extend(pytest_args)
     try:
