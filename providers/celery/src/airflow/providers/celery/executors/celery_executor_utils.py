d45d347cef19a01fa1387513a2f2e4e769e5abe2
ferruzzi <ferruzzi@amazon.com>
2026-03-19 14:45:24 -0700

    Ash fixes - create supervise_workload and move setproctitle in there as well

--- providers/celery/src/airflow/providers/celery/executors/celery_executor_utils.py
+++ providers/celery/src/airflow/providers/celery/executors/celery_executor_utils.py
@@ -217,37 +217,10 @@ def execute_workload(input: str) -> None:
 
     log.info("[%s] Executing workload in Celery: %s", celery_task_id, workload)
 
-    base_url = conf.get("api", "base_url", fallback="/")
-    # If it's a relative URL, use localhost:8080 as the default.
-    if base_url.startswith("/"):
-        base_url = f"http://localhost:8080{base_url}"
-    default_execution_api_server = f"{base_url.rstrip('/')}/execution/"
-
     try:
-        if isinstance(workload, workloads.ExecuteTask):
-            from airflow.sdk.execution_time.supervisor import supervise
+        from airflow.sdk.execution_time.supervisor import supervise_workload
 
-            supervise(
-                # This is the "wrong" ti type, but it duck types the same. TODO: Create a protocol for this.
-                ti=workload.ti,  # type: ignore[arg-type]
-                dag_rel_path=workload.dag_rel_path,
-                bundle_info=workload.bundle_info,
-                token=workload.token,
-                server=conf.get("core", "execution_api_server_url", fallback=default_execution_api_server),
-                log_path=workload.log_path,
-            )
-        elif isinstance(workload, workloads.ExecuteCallback) and AIRFLOW_V_3_2_PLUS:
-            from airflow.sdk.execution_time.callback_supervisor import supervise_callback
-
-            supervise_callback(
-                id=workload.callback.id,
-                callback_path=workload.callback.data.get("path", ""),
-                callback_kwargs=workload.callback.data.get("kwargs", {}),
-                log_path=workload.log_path,
-                bundle_info=workload.bundle_info,
-            )
-        else:
-            raise ValueError(f"CeleryExecutor does not know how to handle {type(workload)}")
+        supervise_workload(workload)
     except Exception as e:
         if AIRFLOW_V_3_1_9_PLUS:
             from airflow.sdk.exceptions import TaskAlreadyRunningError
