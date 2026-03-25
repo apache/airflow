0cad3e02c5e3878de4ee1b56ac81a1ce3698bf57
Amogh Desai <amoghrajesh1999@gmail.com>
2026-03-25 12:13:24 +0530

    adding empty test file

--- shared/serialization/tests/serialization/test_constants.py
+++ shared/serialization/tests/serialization/test_constants.py
@@ -14,3 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+
+def test_dummy():
+  """Dummy test because this module doesn't have any logic, just constants."""
+  pass
