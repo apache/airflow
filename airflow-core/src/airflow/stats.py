4b07e6cffc2a903ada0d7f3bceecacc4eed1e777
Christos Bisias <christosbis@gmail.com>
2026-03-30 22:22:13 +0300

    cleanup airflow/stats.py

--- airflow-core/src/airflow/stats.py
+++ airflow-core/src/airflow/stats.py
@@ -15,16 +15,24 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-"""Deprecated module - stats moved to airflow.observability.stats."""
+"""
+Deprecated module - Stats has been removed, but we are maintaining a shim for backwards compat.
+
+We don't want to encourage any future usage of Stats. This file is to avoid breaking
+any kind of external packages such as 3rd party providers.
+"""
 
 from __future__ import annotations
 
 import warnings
 
+from airflow.sdk.observability.stats import Stats as Stats
 from airflow.utils.deprecation_tools import DeprecatedImportWarning
 
+# This is meant for 3rd party libraries and that's why the sdk module path is suggested.
 warnings.warn(
-    "Importing from 'airflow.stats' is deprecated. Please use 'airflow.observability.stats' instead.",
+    "Using 'from airflow.stats import Stats' is deprecated. "
+    "Please use 'from airflow.sdk.observability import stats' instead.",
     DeprecatedImportWarning,
     stacklevel=2,
 )
