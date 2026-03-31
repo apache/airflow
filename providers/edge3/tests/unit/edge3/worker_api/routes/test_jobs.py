4033bd48866f3bca346bb06b6685135fd12a80e3
gopidesupavan <gopidesupavan@gmail.com>
2026-03-31 22:31:25 +0100

    Fixup tests

--- providers/edge3/tests/unit/edge3/worker_api/routes/test_jobs.py
+++ providers/edge3/tests/unit/edge3/worker_api/routes/test_jobs.py
@@ -81,6 +81,7 @@ class TestJobsApiRoutes:
     @pytest.fixture(autouse=True)
     def setup_test_cases(self, dag_maker, session: Session):
         session.execute(delete(EdgeJobModel))
+        session.execute(delete(EdgeWorkerModel))
         session.commit()
 
     @patch(f"{Stats.__module__}.Stats.incr")
