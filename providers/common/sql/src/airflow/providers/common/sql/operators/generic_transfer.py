8e882ca642e266ddf8ba221344dd17b60e5f9130
Karen Braganza <karenbraganza15@gmail.com>
2026-04-07 15:40:53 -0400

    First commit

--- providers/common/sql/src/airflow/providers/common/sql/operators/generic_transfer.py
+++ providers/common/sql/src/airflow/providers/common/sql/operators/generic_transfer.py
@@ -23,7 +23,7 @@ from typing import TYPE_CHECKING, Any
 
 from airflow.providers.common.compat.sdk import AirflowException, BaseHook, BaseOperator
 from airflow.providers.common.sql.hooks.sql import DbApiHook
-from airflow.providers.common.sql.triggers.sql import SQLExecuteQueryTrigger
+from airflow.providers.common.sql.triggers.sql import SQLGenericTransferTrigger
 
 if TYPE_CHECKING:
     import jinja2
@@ -160,7 +160,7 @@ class GenericTransfer(BaseOperator):
 
         if self.page_size and isinstance(self.sql, str):
             self.defer(
-                trigger=SQLExecuteQueryTrigger(
+                trigger=SQLGenericTransferTrigger(
                     conn_id=self.source_conn_id,
                     hook_params=self.source_hook_params,
                     sql=self.get_paginated_sql(0),
@@ -208,7 +208,7 @@ class GenericTransfer(BaseOperator):
                 self._insert_rows(rows=rows, context=context)
 
                 self.defer(
-                    trigger=SQLExecuteQueryTrigger(
+                    trigger=SQLGenericTransferTrigger(
                         conn_id=self.source_conn_id,
                         hook_params=self.source_hook_params,
                         sql=self.get_paginated_sql(offset),
