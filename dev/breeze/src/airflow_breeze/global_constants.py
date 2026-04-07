cd72a7c268a587dd5fb044423ad46911480c2478
vatsrahul1001 <rah.sharma11@gmail.com>
2026-04-07 18:57:23 +0530

    Airflow 3.2.0 has been released

--- dev/breeze/src/airflow_breeze/global_constants.py
+++ dev/breeze/src/airflow_breeze/global_constants.py
@@ -782,6 +782,12 @@ PROVIDERS_COMPATIBILITY_TESTS_MATRIX: list[dict[str, str | list[str]]] = [
         "remove-providers": "",
         "run-unit-tests": "true",
     },
+    {
+        "python-version": "3.10",
+        "airflow-version": "3.2.0",
+        "remove-providers": "",
+        "run-unit-tests": "true",
+    },
 ]
 
 ALL_PYTHON_VERSION_TO_PATCHLEVEL_VERSION: dict[str, str] = {
