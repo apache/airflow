ac239b17bbc5fdad170a1e59c77d256ccffb27b7
LIU ZHE YOU <zhu424.dev@gmail.com>
2026-04-09 22:32:20 +0800

    Fix java process startup issue

--- airflow-core/src/airflow/dag_processing/processor.py
+++ airflow-core/src/airflow/dag_processing/processor.py
@@ -189,7 +189,7 @@ class BaseDagFileProcessor:
         # We will only store dag_bundle_name but not dag_bundle_path here because it's DagBundle's responsibility to manage the path
         self.target_bundle_name = target_bundle_name
 
-    def can_handle(self, bundle_name: str) -> bool:
+    def can_handle(self, bundle_name: str, path: str | os.PathLike[str]) -> bool:
         """Return ``True`` if this processor should handle the given file."""
         # The Airflow Core DagFileProcessorProcess will pass the bundle_name to see
         return self.target_bundle_name == bundle_name
