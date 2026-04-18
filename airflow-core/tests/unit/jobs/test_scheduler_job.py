b200e0c0a749a0956079707aa49b894aaaa02848
Kaxil Naik <kaxilnaik@gmail.com>
2026-04-18 22:52:04 +0100

    Update test_not_enough_pool_slots for new TaskInstance repr
    After adding ti_id to TaskInstance.__repr__, test_not_enough_pool_slots
    needs to include ti_id in the expected log substring. Same fix pattern
    as test_process_executor_events_with_callback at line 695.

--- airflow-core/tests/unit/jobs/test_scheduler_job.py
+++ airflow-core/tests/unit/jobs/test_scheduler_job.py
@@ -1904,11 +1904,13 @@ class TestSchedulerJob:
         some_pool = Pool(pool="some_pool", slots=2, description="my pool", include_deferred=False)
         session.add(some_pool)
         session.commit()
+        cannot_run_ti_id = dr.task_instances[0].id
         with caplog.at_level(logging.WARNING):
             self.job_runner._executable_task_instances_to_queued(max_tis=32, session=session)
             assert (
-                "Not executing <TaskInstance: "
-                "SchedulerJobTest.test_test_not_enough_pool_slots.cannot_run test [scheduled]>. "
+                f"Not executing <TaskInstance: "
+                f"SchedulerJobTest.test_test_not_enough_pool_slots.cannot_run test [scheduled] "
+                f"ti_id={cannot_run_ti_id}>. "
                 "Requested pool slots (4) are greater than total pool slots: '2' for pool: some_pool"
                 in caplog.text
             )
