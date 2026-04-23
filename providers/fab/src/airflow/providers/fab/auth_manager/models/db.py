4e1a5146feca5e8e8f85fa3ccd92c86dff68296e
Jens Scheffler <95105677+jscheffl@users.noreply.github.com>
2026-04-23 06:54:40 +0200

    Remove global variables in airflow.settings (#61917)
    * Remove global variables in airflow.settings
    
    * Upgrade back-compat for 3.2.2
    
    * Fix upstream changes and tests due to changes
    
    * Fix test
    
    (cherry picked from commit db7eab370eb07e8a4d0ef4d864dadbad7a86c66f)

--- providers/fab/src/airflow/providers/fab/auth_manager/models/db.py
+++ providers/fab/src/airflow/providers/fab/auth_manager/models/db.py
@@ -23,6 +23,7 @@ from sqlalchemy.engine.url import make_url
 from airflow import settings
 from airflow.providers.common.compat.sdk import AirflowException
 from airflow.providers.fab.auth_manager.models import model_metadata
+from airflow.providers.fab.version_compat import AIRFLOW_V_3_2_2_PLUS
 from airflow.utils.db import _offline_migration, print_happy_cat
 from airflow.utils.db_manager import BaseDBManager
 
@@ -66,7 +67,8 @@ class FABDBManager(BaseDBManager):
 
     def create_db_from_orm(self):
         super().create_db_from_orm()
-        db, flask_app = _get_flask_db(settings.SQL_ALCHEMY_CONN)
+        sql_conn = settings.get_sql_alchemy_conn() if AIRFLOW_V_3_2_2_PLUS else settings.SQL_ALCHEMY_CONN
+        db, flask_app = _get_flask_db(sql_conn)
         with flask_app.app_context():
             db.create_all()
 
@@ -89,8 +91,6 @@ class FABDBManager(BaseDBManager):
         _release_metadata_locks_if_supported(self)
 
         # alembic adds significant import time, so we import it lazily
-        if not settings.SQL_ALCHEMY_CONN:
-            raise RuntimeError("The settings.SQL_ALCHEMY_CONN not set. This is a critical assertion.")
         from alembic import command
 
         current_revision = self.get_current_revision()
@@ -105,7 +105,10 @@ class FABDBManager(BaseDBManager):
         config = self.get_alembic_config()
 
         if show_sql_only:
-            if make_url(settings.SQL_ALCHEMY_CONN).get_backend_name() == "sqlite":
+            sql_conn: str = (
+                settings.get_sql_alchemy_conn() if AIRFLOW_V_3_2_2_PLUS else str(settings.SQL_ALCHEMY_CONN)
+            )
+            if make_url(sql_conn).get_backend_name() == "sqlite":
                 raise SystemExit("Offline migration not supported for SQLite.")
             if not from_revision:
                 from_revision = current_revision
@@ -129,9 +132,6 @@ class FABDBManager(BaseDBManager):
                 "applying a downgrade (instead of just generating sql), we always "
                 "downgrade from current revision."
             )
-
-        if not settings.SQL_ALCHEMY_CONN:
-            raise RuntimeError("The settings.SQL_ALCHEMY_CONN not set.")
 
         # alembic adds significant import time, so we import it lazily
         from alembic import command
@@ -158,6 +158,7 @@ class FABDBManager(BaseDBManager):
 
     def drop_tables(self, connection):
         super().drop_tables(connection)
-        db, flask_app = _get_flask_db(settings.SQL_ALCHEMY_CONN)
+        sql_conn = settings.get_sql_alchemy_conn() if AIRFLOW_V_3_2_2_PLUS else settings.SQL_ALCHEMY_CONN
+        db, flask_app = _get_flask_db(sql_conn)
         with flask_app.app_context():
             db.drop_all()
