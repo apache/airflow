d2a25d85eeccffa766c885db4d6454d9bab40f53
LIU ZHE YOU <zhu424.dev@gmail.com>
2026-05-05 11:16:55 +0800

    Go-SDK: Add coordinator-mode Dag parsing

--- go-sdk/example/bundle/main.go
+++ go-sdk/example/bundle/main.go
@@ -37,14 +37,18 @@ type myBundle struct{}
 var _ v1.BundleProvider = (*myBundle)(nil)
 
 func (m *myBundle) RegisterDags(dagbag v1.Registry) error {
-	simpleDag := dagbag.AddDag("simple_dag")
-	simpleDag.AddTask(extract)
-	simpleDag.AddTask(transform)
-	simpleDag.AddTask(load)
+	simpleDag := dagbag.AddDag("simple_dag", v1.DagSpec{
+		Schedule:    "@daily",
+		Description: "Example Go-authored Dag",
+		Tags:        []string{"example", "go-sdk"},
+	})
+	simpleDag.AddTask(extract, v1.TaskSpec{Queue: "go-task", Retries: 2}, nil)
+	simpleDag.AddTask(transform, v1.TaskSpec{Queue: "go-task"}, []string{"extract"})
+	simpleDag.AddTask(load, v1.TaskSpec{Queue: "go-task"}, []string{"transform"})
 
 	// Tasks defined in other packages register through the same dagbag.
 	concurrentDag := dagbag.AddDag("concurrent_xcom_dag")
-	concurrentDag.AddTaskWithName("pull_xcoms_concurrently", concurrentxcom.PullXComsConcurrently)
+	concurrentDag.AddTaskWithName("pull_xcoms_concurrently", concurrentxcom.PullXComsConcurrently, v1.TaskSpec{}, nil)
 
 	bindingDag := dagbag.AddDag("taskflow_binding_dag")
 	bindingDag.AddTaskWithName("make_config", taskflowbinding.MakeConfig)
