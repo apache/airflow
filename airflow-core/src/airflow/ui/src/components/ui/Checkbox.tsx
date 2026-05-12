dddceb38d280fb7c9a01338727b5d3f73e0fddc8
parkhojeong <parkhj062@gmail.com>
2026-05-13 02:49:45 +0900

    UI: Inline checkbox cursor props

--- airflow-core/src/airflow/ui/src/components/ui/Checkbox.tsx
+++ airflow-core/src/airflow/ui/src/components/ui/Checkbox.tsx
@@ -25,18 +25,13 @@ export type CheckboxProps = {
   readonly rootRef?: React.Ref<HTMLLabelElement>;
 } & ChakraCheckbox.RootProps;
 
-const interactiveCursorProps = {
-  _disabled: { cursor: "not-allowed" },
-  cursor: "pointer",
-};
-
 export const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>((props, ref) => {
   const { children, icon, inputProps, rootRef, ...rest } = props;
 
   return (
     <ChakraCheckbox.Root ref={rootRef} {...rest}>
       <ChakraCheckbox.HiddenInput ref={ref} {...inputProps} />
-      <ChakraCheckbox.Control {...interactiveCursorProps}>
+      <ChakraCheckbox.Control _disabled={{ cursor: "not-allowed" }} cursor="pointer">
         {icon ?? <ChakraCheckbox.Indicator />}
       </ChakraCheckbox.Control>
       {children !== undefined && <ChakraCheckbox.Label>{children}</ChakraCheckbox.Label>}
