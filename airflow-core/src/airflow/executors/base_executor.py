b5bb6583d861199df54f9a915f506876b917fec0
Anish <giri.girianish@gmail.com>
2026-05-15 12:42:05 -0500

    Add TODO to flatten executor_queues when compat properties are removed in 4.0

--- airflow-core/src/airflow/executors/base_executor.py
+++ airflow-core/src/airflow/executors/base_executor.py
@@ -245,6 +245,8 @@ class BaseExecutor(LoggingMixin):
 
         self.parallelism: int = parallelism
         self.team_name: str | None = team_name
+        # TODO(airflow 4.0): flatten to dict[WorkloadKey, QueueableWorkload] once the deprecated
+        # queued_tasks / queued_callbacks compat properties are removed.
         self.executor_queues: dict[str, dict[WorkloadKey, QueueableWorkload]] = defaultdict(dict)
         self.running: set[WorkloadKey] = set()
         self.event_buffer: dict[WorkloadKey, EventBufferValueType] = {}
