3a6c0c343239061bb312d3740c7122345435d485
venkatamandavilli-code <venkata.mandavilli@gmail.com>
2026-05-24 15:16:31 -0500

    Update example_erp_migration_pipeline.py

--- airflow/example_dags/example_erp_migration_pipeline.py
+++ airflow/example_dags/example_erp_migration_pipeline.py
@@ -1,4 +1,3 @@
-```python
 # Licensed to the Apache Software Foundation (ASF) under one
 # or more contributor license agreements.  See the NOTICE file
 # distributed with this work for additional information
@@ -88,4 +87,3 @@ with DAG(
     )
 
     extract >> validate >> transform >> load >> reconcile
-```
