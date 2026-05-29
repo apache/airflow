1dbc49cda3c26325a2357976620226503b8d8382
Jens Scheffler <jscheffl@apache.org>
2026-05-29 14:41:18 +0200

    Revert other changes leading to CI errors

--- airflow-core/src/airflow/ui/openapi-gen/requests/types.gen.ts
+++ airflow-core/src/airflow/ui/openapi-gen/requests/types.gen.ts
@@ -1951,9 +1951,6 @@ export type EdgeResponse = {
     is_source_asset?: boolean | null;
 };
 
-/**
- * Define a menu item that can be added to the menu by auth managers or plugins.
- */
 export type ExtraMenuItem = {
     text: string;
     href: string;
