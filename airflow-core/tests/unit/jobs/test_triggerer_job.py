dbe8e668b13ec11c8cd2e77ffe565342cf303e6e
Sean Ghaeli <ghaeli@amazon.com>
2026-06-03 19:50:48 +0000

    Retrigger CI

--- airflow-core/tests/unit/jobs/test_triggerer_job.py
+++ airflow-core/tests/unit/jobs/test_triggerer_job.py
@@ -2867,7 +2867,7 @@ def test_run_trigger_appends_none_seq_for_non_shared_trigger():
 class TestBuildContextFromDagRunData:
     """Tests for TriggerRunner._build_context_from_dag_run_data (triggerer callback context)."""
 
-    _BASE_DAG_RUN_DATA = {
+    _BASE_DAG_RUN_DATA: dict = {
         "dag_id": "example_dag",
         "run_id": "manual__2024-01-01",
         "logical_date": "2024-01-01T00:00:00+00:00",
