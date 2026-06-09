4af60a2f94248fa62c3957cfea08facd3319715b
safaehar <safae.hariri@datadoghq.com>
2026-06-09 11:35:19 +0200

    Fix static checks: register new metrics in registry and apply ruff format

--- airflow-core/src/airflow/jobs/scheduler_job_runner.py
+++ airflow-core/src/airflow/jobs/scheduler_job_runner.py
@@ -1874,7 +1874,6 @@ class SchedulerJobRunner(BaseJobRunner, LoggingMixin):
                 )
                 break
 
-
     def _do_scheduling(self, session: Session) -> int:
         """
         Make the main scheduling decisions.
