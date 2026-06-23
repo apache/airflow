aa25da44a6aed95e6fdc5b528ec3afbe28c0b441
Nathan Hadfield <nathan.hadfield@king.com>
2026-06-23 10:36:01 +0100

    [v3-3-test] Propagate `partition_date` to consumers of partitioned assets (#67285) (cherry picked from commit e9af904a099ea53dbf0f93de90d2259acfe9b489)
    Co-authored-by: Nathan Hadfield <nathan.hadfield@king.com>

--- airflow-core/src/airflow/models/dagrun.py
+++ airflow-core/src/airflow/models/dagrun.py
@@ -433,6 +433,7 @@ class DagRun(Base, LoggingMixin):
             conf=self.conf,
             consumed_asset_events=[],
             partition_key=self.partition_key,
+            partition_date=self.partition_date,
         )
 
     @property
@@ -1089,6 +1090,9 @@ class DagRun(Base, LoggingMixin):
                 attributes["airflow.dag_run.logical_date"] = str(self.logical_date)
             if self.partition_key:
                 attributes["airflow.dag_run.partition_key"] = str(self.partition_key)
+            if self.partition_date:
+                attributes["airflow.dag_run.partition_date"] = self.partition_date.isoformat()
+
             # TODO: make the empty parent context optional. Default should be to
             # nest the dag run span under the currently active parent span (by
             # omitting `context` here); only use the empty `context.Context()` to
