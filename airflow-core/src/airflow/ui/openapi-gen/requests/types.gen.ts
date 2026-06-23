e9af904a099ea53dbf0f93de90d2259acfe9b489
Nathan Hadfield <nathan.hadfield@king.com>
2026-06-23 10:36:01 +0100

    Propagate `partition_date` to consumers of partitioned assets (#67285)

--- airflow-core/src/airflow/ui/openapi-gen/requests/types.gen.ts
+++ airflow-core/src/airflow/ui/openapi-gen/requests/types.gen.ts
@@ -917,6 +917,7 @@ export type DAGRunResponse = {
     bundle_version: string | null;
     dag_display_name: string;
     partition_key: string | null;
+    partition_date: string | null;
 };
 
 /**
