9a8c2d1b49495e817c921f955cfe29e7cd4f5441
PoAn Yang <payang@apache.org>
2026-06-28 14:25:29 +0900

    Add Dag run context fields to the Java SDK Context
    Java tasks could only read the dag id and run id from Context, even though the
    supervisor already sends the full run context (logical date, data interval, run
    type, trigger conf) in StartupDetails.
    
    Signed-off-by: PoAn Yang <payang@apache.org>

--- java-sdk/sdk/src/main/kotlin/org/apache/airflow/sdk/Context.kt
+++ java-sdk/sdk/src/main/kotlin/org/apache/airflow/sdk/Context.kt
@@ -20,17 +20,35 @@
 package org.apache.airflow.sdk
 
 import org.apache.airflow.sdk.execution.comm.StartupDetails
+import java.time.OffsetDateTime
 
 /**
  * Identifies the Dag run that the current task instance belongs to.
  *
  * @property dagId ID of the Dag being run.
  * @property runId Unique identifier for this Dag run.
+ * @property logicalDate A date-time that logically identifies the current Dag run.
+ * @property dataIntervalStart Start of the data interval.
+ * @property dataIntervalEnd End of the data interval.
+ * @property runAfter A date-time tells the scheduler when the Dag run can be scheduled.
+ * @property runType How the run was created.
+ * @property conf The configuration for this run.
  */
 data class DagRun(
   @JvmField val dagId: String,
   @JvmField val runId: String,
-)
+  @JvmField val logicalDate: OffsetDateTime?,
+  @JvmField val dataIntervalStart: OffsetDateTime?,
+  @JvmField val dataIntervalEnd: OffsetDateTime?,
+  @JvmField val runAfter: OffsetDateTime?,
+  @JvmField val runType: String?,
+  @JvmField val conf: Map<String, Any?>,
+) {
+  /**
+   * [logicalDate] as an ISO `yyyy-MM-dd` date string, or `null` when there is no logical date.
+   */
+  val ds: String? get() = logicalDate?.toLocalDate()?.toString()
+}
 
 /**
  * Identifies the task instance that is currently executing.
@@ -63,9 +81,31 @@ data class Context(
   @JvmField val ti: TaskInstance,
 ) {
   internal companion object {
+    private fun toDateTime(value: Any?): OffsetDateTime? =
+      when (value) {
+        is OffsetDateTime -> value
+        is String -> runCatching { OffsetDateTime.parse(value) }.getOrNull()
+        else -> null
+      }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun toConf(value: Any?): Map<String, Any?> = (value as? Map<String, Any?>) ?: emptyMap()
+
     fun from(request: StartupDetails) =
       Context(
-        dagRun = with(request.tiContext.dagRun) { DagRun(dagId, runId) },
+        dagRun =
+          with(request.tiContext.dagRun) {
+            DagRun(
+              dagId = dagId,
+              runId = runId,
+              logicalDate = toDateTime(logicalDate),
+              dataIntervalStart = toDateTime(dataIntervalStart),
+              dataIntervalEnd = toDateTime(dataIntervalEnd),
+              runAfter = toDateTime(runAfter),
+              runType = runType?.toString(),
+              conf = toConf(conf),
+            )
+          },
         ti = with(request.ti) { TaskInstance(dagId, runId, taskId, mapIndex, tryNumber) },
       )
   }
