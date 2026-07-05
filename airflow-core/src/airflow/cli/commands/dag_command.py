b73940aed438c4350c1e14af87de50163bb29d59
Yuseok Jo <yuseok89@gmail.com>
2026-07-05 17:56:16 +0900

    [AIP-94] Mark dags list-runs/list-jobs/report/clear as migrated to airflowctl

--- airflow-core/src/airflow/cli/commands/dag_command.py
+++ airflow-core/src/airflow/cli/commands/dag_command.py
@@ -128,6 +128,7 @@ def dag_delete(args) -> None:
         print("Cancelled")
 
 
+@deprecated_for_airflowctl("airflowctl dags clear")
 @cli_utils.action_cli
 @providers_configuration_loaded
 @provide_session
@@ -694,6 +695,7 @@ def dag_list_import_errors(args, *, session: Session = NEW_SESSION) -> None:
         sys.exit(1)
 
 
+@deprecated_for_airflowctl("airflowctl dags report")
 @cli_utils.action_cli
 @suppress_logs_and_warning
 @providers_configuration_loaded
@@ -729,6 +731,7 @@ def dag_report(args) -> None:
     )
 
 
+@deprecated_for_airflowctl("airflowctl dags list-jobs")
 @cli_utils.action_cli
 @suppress_logs_and_warning
 @providers_configuration_loaded
@@ -760,6 +763,7 @@ def dag_list_jobs(args, dag: DAG | None = None, *, session: Session = NEW_SESSIO
     )
 
 
+@deprecated_for_airflowctl("airflowctl dags list-runs")
 @cli_utils.action_cli
 @suppress_logs_and_warning
 @providers_configuration_loaded
