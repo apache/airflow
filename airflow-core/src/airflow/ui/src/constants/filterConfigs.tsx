0fe6d75378b90b8b436e2c51f0d7eda090739a78
Phinhas Asmelash <phinhasasmelash@gmail.com>
2026-07-20 13:16:29 -0700

    added filters to the backfills tab ui

--- airflow-core/src/airflow/ui/src/constants/filterConfigs.tsx
+++ airflow-core/src/airflow/ui/src/constants/filterConfigs.tsx
@@ -47,6 +47,7 @@ import { TagsFilter } from "src/components/FilterBar/filters/TagsFilter";
 import { TimetableTypeFilter } from "src/components/FilterBar/filters/TimetableTypeFilter";
 import { RunTypeIcon } from "src/components/RunTypeIcon";
 import { StateBadge } from "src/components/StateBadge";
+import { reprocessBehaviors } from "src/constants/reprocessBehaviourParams";
 import {
   dagRunStateOptions,
   dagRunTypeOptions,
@@ -115,6 +116,13 @@ export const useFilterConfigs = () => {
       icon: <MdCode />,
       label: translate("components:versionDetails.bundleVersion"),
       type: FilterTypes.TEXT,
+    },
+    [SearchParamsKeys.COMPLETED_AT_RANGE]: {
+      endKey: SearchParamsKeys.COMPLETED_AT_LTE,
+      icon: <MdDateRange />,
+      label: translate("hitl:filters.completedAt"),
+      startKey: SearchParamsKeys.COMPLETED_AT_GTE,
+      type: FilterTypes.DATERANGE,
     },
     [SearchParamsKeys.CONF_CONTAINS]: {
       hotkeyDisabled: true,
@@ -290,6 +298,18 @@ export const useFilterConfigs = () => {
       min: -1,
       type: FilterTypes.NUMBER,
     },
+    [SearchParamsKeys.MAX_ACTIVE_RUNS_GTE]: {
+      icon: <MdHourglassEmpty />,
+      label: translate("common:filters.maxActiveRunsFrom"),
+      min: 1,
+      type: FilterTypes.NUMBER,
+    },
+    [SearchParamsKeys.MAX_ACTIVE_RUNS_LTE]: {
+      icon: <MdHourglassEmpty />,
+      label: translate("common:filters.maxActiveRunsTo"),
+      min: 1,
+      type: FilterTypes.NUMBER,
+    },
     [SearchParamsKeys.MISSED]: {
       icon: <MdCheckCircle />,
       label: translate("browse:deadlines.filters.status"),
@@ -363,6 +383,15 @@ export const useFilterConfigs = () => {
       label: translate("common:taskInstance.renderedMapIndex"),
       supportsAdvancedSearch: true,
       type: FilterTypes.TEXT,
+    },
+    [SearchParamsKeys.REPROCESS_BEHAVIOR]: {
+      icon: <MdPlayArrow />,
+      label: translate("components:backfill.reprocessBehavior"),
+      options: reprocessBehaviors.map((option) => ({
+        label: translate(option.label),
+        value: option.value,
+      })),
+      type: FilterTypes.SELECT,
     },
     [SearchParamsKeys.RESPONDED_BY_USER_NAME]: {
       hotkeyDisabled: true,
--- airflow-core/src/airflow/ui/src/utils/useFiltersHandler.ts
+++ airflow-core/src/airflow/ui/src/utils/useFiltersHandler.ts
@@ -59,6 +59,7 @@ export type FilterableSearchParamsKeys =
   | SearchParamsKeys.ASSET_EVENT_DATE_RANGE
   | SearchParamsKeys.BODY_SEARCH
   | SearchParamsKeys.BUNDLE_VERSION
+  | SearchParamsKeys.COMPLETED_AT_RANGE
   | SearchParamsKeys.CONF_CONTAINS
   | SearchParamsKeys.CONSUMING_ASSET_PATTERN
   | SearchParamsKeys.CREATED_AT_RANGE
@@ -84,6 +85,8 @@ export type FilterableSearchParamsKeys =
   | SearchParamsKeys.LAST_DAG_RUN_STATE
   | SearchParamsKeys.LOGICAL_DATE_RANGE
   | SearchParamsKeys.MAP_INDEX
+  | SearchParamsKeys.MAX_ACTIVE_RUNS_GTE
+  | SearchParamsKeys.MAX_ACTIVE_RUNS_LTE
   | SearchParamsKeys.MISSED
   | SearchParamsKeys.NAME_PATTERN
   | SearchParamsKeys.NEEDS_REVIEW
@@ -94,6 +97,7 @@ export type FilterableSearchParamsKeys =
   | SearchParamsKeys.POOL_NAME_PATTERN
   | SearchParamsKeys.QUEUE_NAME_PATTERN
   | SearchParamsKeys.RENDERED_MAP_INDEX
+  | SearchParamsKeys.REPROCESS_BEHAVIOR
   | SearchParamsKeys.RESPONDED_BY_USER_NAME
   | SearchParamsKeys.RESPONSE_RECEIVED
   | SearchParamsKeys.RUN_AFTER_RANGE
