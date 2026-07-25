0ad01e061ea3e225cb6525d10b34193ca5fb32df
Jung-Hyun Andrew Kim <jhakimcs03@outlook.com>
2026-07-25 11:20:25 -0700

    update revision head map

--- airflow-core/src/airflow/migrations/versions/0133_3_4_0_add_scope_columns_to_job.py
+++ airflow-core/src/airflow/migrations/versions/0133_3_4_0_add_scope_columns_to_job.py
@@ -20,7 +20,7 @@
 Add team_name and bundle_names scope columns to job table.
 
 Revision ID: f8c2a1d94e03
-Revises: 5a5d3253e946
+Revises: 8d3f1a6b2c47
 Create Date: 2026-07-08 02:00:00.000000
 
 """
@@ -34,7 +34,7 @@ from airflow.utils.sqlalchemy import ExtendedJSON
 
 # revision identifiers, used by Alembic.
 revision = "f8c2a1d94e03"
-down_revision = "7a98f1b7dbd3"
+down_revision = "8d3f1a6b2c47"
 branch_labels = None
 depends_on = None
 airflow_version = "3.4.0"
