da59ecaea662a51d3e311b02d3e810fdc496eb70
Hemkumar Chheda <hemkumar.chheda@astronomer.io>
2026-08-02 23:49:34 +0530

    Address orphan-kill socket cleanup review feedback

--- task-sdk/src/airflow/sdk/execution_time/schema/versions/__init__.py
+++ task-sdk/src/airflow/sdk/execution_time/schema/versions/__init__.py
@@ -37,8 +37,13 @@ def get_bundle() -> VersionBundle:
     """
     from cadwyn import HeadVersion, Version, VersionBundle
 
+    from airflow.sdk.execution_time.schema.versions.v2026_08_02 import (
+        TrackDagProcessorSocketCleanupOwnership,
+    )
+
     return VersionBundle(
         HeadVersion(),
+        Version("2026-08-02", TrackDagProcessorSocketCleanupOwnership),
         Version("2026-06-16"),
     )
 
