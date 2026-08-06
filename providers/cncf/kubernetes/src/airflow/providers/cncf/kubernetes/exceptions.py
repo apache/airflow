1597db09aa9c7a09b71bd6219762a7bc2fba848f
AlejandroMorgante <62363051+AlejandroMorgante@users.noreply.github.com>
2026-08-06 12:14:03 -0300

    Add operator to execute commands in existing Kubernetes Pods
    Reusing a running Pod avoids the startup latency incurred when each task creates a new Pod.

--- providers/cncf/kubernetes/src/airflow/providers/cncf/kubernetes/exceptions.py
+++ providers/cncf/kubernetes/src/airflow/providers/cncf/kubernetes/exceptions.py
@@ -39,3 +39,7 @@ class KubernetesApiError(AirflowException):
 
 class KubernetesApiPermissionError(AirflowException):
     """Raised when an error is encountered while trying access Kubernetes API."""
+
+
+class PodExecException(AirflowException):
+    """Raised when a command cannot be executed successfully in a Kubernetes pod."""
