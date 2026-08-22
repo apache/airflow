9f477fbed864a8af8f5bf82b000a7fc09c39e23a
Christopher P. Anderson <48180628+topherinternational@users.noreply.github.com>
2026-08-22 13:52:06 +0200

    ruff fix

--- providers/opensearch/tests/unit/opensearch/log/test_os_task_handler.py
+++ providers/opensearch/tests/unit/opensearch/log/test_os_task_handler.py
@@ -46,6 +46,7 @@ from airflow.providers.opensearch.log.os_task_handler import (
 )
 from airflow.utils.log.file_task_handler import FileTaskHandler
 from airflow.utils.state import DagRunState, TaskInstanceState
+
 from tests_common.test_utils.config import conf_vars
 from tests_common.test_utils.db import clear_db_dags, clear_db_runs
 from tests_common.test_utils.version_compat import AIRFLOW_V_3_0_PLUS
