c05d9711f49fbdf97af190617b406b33d6661124
Y-C <easoneason0905@gmail.com>
2026-09-14 02:39:44 +0800

    [airflow-ctl/v0-1-test] Fix airflowctl connections test failing on stored connections (#73098)
    (cherry picked from commit 65eb086f40c1e95f848998dcbd1fa573387c06c9)

--- airflow-ctl/src/airflowctl/api/operations.py
+++ airflow-ctl/src/airflowctl/api/operations.py
@@ -546,7 +546,8 @@ class ConnectionsOperations(BaseOperations):
         """Test a connection."""
         try:
             self.response = self.client.post(
-                "connections/test", json=connection.model_dump(mode="json", by_alias=True)
+                "connections/test",
+                json=connection.model_dump(mode="json", by_alias=True, exclude_none=True),
             )
             return ConnectionTestResponse.model_validate_json(self.response.content)
         except ServerResponseError as e:
