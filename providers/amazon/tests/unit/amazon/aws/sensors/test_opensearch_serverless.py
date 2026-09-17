b51623e07a37863164dd98707e015b375bf5ff90
Christopher Iskander <chris.iskander@icloud.com>
2026-09-17 13:37:09 -0400

    Clean up OpenSearch sensor regression test

--- providers/amazon/tests/unit/amazon/aws/sensors/test_opensearch_serverless.py
+++ providers/amazon/tests/unit/amazon/aws/sensors/test_opensearch_serverless.py
@@ -102,18 +102,19 @@ class TestOpenSearchServerlessCollectionActiveSensor:
         sensor = OpenSearchServerlessCollectionActiveSensor(**self.default_op_kwargs, aws_conn_id=None)
         with pytest.raises(AirflowException, match=sensor.FAILURE_MESSAGE):
             sensor.poke({})
+
     def test_deferrable_sensor_forwards_aws_config(self):
         sensor = OpenSearchServerlessCollectionActiveSensor(
-        **self.default_op_kwargs,
-        aws_conn_id="aws-test-custom-conn",
-        region_name="eu-west-1",
-        verify=False,
-        botocore_config={"read_timeout": 42},
-        deferrable=True,
-    )
+            **self.default_op_kwargs,
+            aws_conn_id="aws-test-custom-conn",
+            region_name="eu-west-1",
+            verify=False,
+            botocore_config={"read_timeout": 42},
+            deferrable=True,
+        )
 
         with mock.patch.object(sensor, "defer") as mock_defer:
-             sensor.execute({})
+            sensor.execute({})
 
         trigger = mock_defer.call_args.kwargs["trigger"]
 
