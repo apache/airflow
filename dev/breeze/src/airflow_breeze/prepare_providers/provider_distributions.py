558a59bb245f2d56231a4aba16dd55854c6541c2
Ash Berlin-Taylor <ash@apache.org>
2026-09-23 13:05:11 +0100

    Stop flit from downloading and verifying trove classifiers on every provider
    Pypi was returning some 503's for this today causing CI to fail. To all
    intents and purposes, the PyPI trove classifiers don't change in a way
    that affects us, so making a (number) of network requests to check them
    is unncessary.

--- dev/breeze/src/airflow_breeze/prepare_providers/provider_distributions.py
+++ dev/breeze/src/airflow_breeze/prepare_providers/provider_distributions.py
@@ -251,6 +251,7 @@ def build_provider_distribution(
     build_backend = provider_info.get("build-system", "flit_core")
     build_env = {"SOURCE_DATE_EPOCH": str(get_provider_details(provider_id).source_date_epoch)}
     if build_backend == "flit_core":
+        build_env["FLIT_NO_NETWORK"] = "1"
         command: list[str] = [sys.executable, "-m", "flit", "build", "--use-vcs"]
         console_print(
             "[warning]Workaround wheel-only package bug in flit by building both and removing sdist."
